4869e832ab9feae76c22581bed4abd68fc2ba5d1
Marc Recht <marc@users.sourceforge.net>
2003-03-25 19:00:42 +0000

    pygtk2: Python bindings for GTK+2

---
+++ pygtk2/buildlink2.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/03/25 19:00:42 marc Exp $
+#
+
+.if !defined(PYGTK2_BUILDLINK2_MK)
+PYGTK2_BUILDLINK2_MK=	# defined
+
+.include "../../lang/python/pyversion.mk"
+
+BUILDLINK_PACKAGES+=			pygtk2
+BUILDLINK_DEPENDS.pygtk2?=		${PYPKGPREFIX}-gtk2>=1.99.16
+BUILDLINK_PKGSRCDIR.pygtk2?=		../../x11/pygtk2
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.pygtk2=${PYPKGPREFIX}-gtk2
+BUILDLINK_PREFIX.pygtk2_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.pygtk2+=	include/pygtk-2.0/pygtk/pygtk.h
+BUILDLINK_FILES.pygtk2+=	include/pygtk-2.0/pygobject.h
+BUILDLINK_FILES.pygtk2+=	lib/pkgconfig/pygtk-2.0.pc
+
+.include "../../devel/libglade2/buildlink2.mk"
+.include "../../math/py-Numeric/buildlink2.mk"
+.include "../../x11/gtk2/buildlink2.mk"
+.include "../../wip/gtkglarea2/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	pygtk2-buildlink
+
+pygtk2-buildlink: _BUILDLINK_USE
+
+.endif	# PYGTK2_BUILDLINK2_MK
