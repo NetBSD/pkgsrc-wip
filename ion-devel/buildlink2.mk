126f2eedc48839986e14ae96ed731158fa2deeac
Quentin Garnier <cube@NetBSD.org>
2003-04-03 11:30:37 +0000

    Correct paths since I imported this.

--- ion-devel/buildlink2.mk
+++ ion-devel/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/04/03 09:27:17 cubidou Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/04/03 11:30:37 cubidou Exp $
 
 .if !defined(ION_BUILDLINK2_MK)
 ION_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		ion
 BUILDLINK_DEPENDS.ion?=		ion>=20030327
-BUILDLINK_PKGSRCDIR.ion?=	../../local/ion-devel
+BUILDLINK_PKGSRCDIR.ion?=	../../wip/ion-devel
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.ion=ion
 BUILDLINK_PREFIX.ion_DEFAULT=	${LOCALBASE}
