93967adf8ad9ec8ecf22b7357c144dbd3ce73e32
Lubomir Sedlacik <salo@NetBSD.org>
2003-05-14 01:57:45 +0000

    - DESCR has 80 lines (+add a sentence) - GNU make is not really needed - add build dependency on perl, needed for generating documentation - libpopt is not needed anymore - sync buildlink2.mk with reality - simplify, style nits

--- libopencdk/buildlink2.mk
+++ libopencdk/buildlink2.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/05/13 02:29:36 xtraeme Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/05/14 01:57:45 lsed Exp $
 #
 # This Makefile fragment is included by packages that use libopencdk.
 #
-# This file was created automatically using createbuildlink 2.3.
+# This file was created automatically using createbuildlink 2.6.
 #
 
 .if !defined(LIBOPENCDK_BUILDLINK2_MK)
 LIBOPENCDK_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			libopencdk
-BUILDLINK_DEPENDS.libopencdk?=		libopencdk>=0.4.1
-BUILDLINK_PKGSRCDIR.libopencdk?=		../../devel/libopencdk
+BUILDLINK_DEPENDS.libopencdk?=		libopencdk>=0.4.5
+BUILDLINK_PKGSRCDIR.libopencdk?=		../../wip/libopencdk
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libopencdk=libopencdk
 BUILDLINK_PREFIX.libopencdk_DEFAULT=	${LOCALBASE}
@@ -19,7 +19,6 @@ BUILDLINK_FILES.libopencdk+=	lib/libopencdk.*
 
 .include "../../devel/zlib/buildlink2.mk"
 .include "../../security/libgcrypt/buildlink2.mk"
-.include "../../devel/popt/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	libopencdk-buildlink
 
