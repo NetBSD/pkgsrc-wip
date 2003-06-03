188ecdd5ae206099815490fc6765dbfcbb7beb3f
Juan Romero Pardines <xtraeme@gmail.com>
2003-06-03 02:45:28 +0000

    Updated DirectFB to 0.9.18
    Changes:
    
    0.9.18
    ------
    
    - National Semiconductors Geode driver by Sarma Kolluru <Sarma.Kolluru@nsc.com>.
      Great thanks to NSC for their support!!!
    - nVidia Overlay support by Oliver Schwartz <Oliver.Schwartz@gmx.de>, currently
      hard coded to XBox. For plain nVidia graphics adapter you need to change line
      618ff of nvidia_overlay.c (uncomment line 618, comment out line 620).
    - Added multi application support to SDL backend.
    - Added triple buffering for display layers.
    - Added DPMS support via IDirectFBDisplayLayer::SetScreenPowerMode().
    
    ...

--- directfb/buildlink2.mk
+++ directfb/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/04/14 05:29:56 xtraeme Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/06/03 02:45:28 xtraeme Exp $
 #
 # This Makefile fragment is included by packages that use directfb.
 #
@@ -14,69 +14,15 @@ BUILDLINK_PKGSRCDIR.directfb?=		../../wip/directfb
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.directfb=directfb
 BUILDLINK_PREFIX.directfb_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/clipboard.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/colorhash.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/core.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/core_parts.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/core_system.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/coredefs.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/coretypes.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/fonts.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/fusion/arena.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/fusion/fusion.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/fusion/fusion_types.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/fusion/list.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/fusion/lock.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/fusion/object.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/fusion/property.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/fusion/reactor.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/fusion/ref.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/fusion/shmalloc.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/gfxcard.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/graphics_driver.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/input.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/input_driver.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/layers.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/modules.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/palette.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/sig.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/state.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/surfacemanager.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/surfaces.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/system.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/thread.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/core/windows.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/directfb_internals.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/directfb_version.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/display/idirectfbdisplaylayer.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/display/idirectfbpalette.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/display/idirectfbsurface.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/display/idirectfbsurface_layer.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/display/idirectfbsurface_window.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/gfx/clip.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/gfx/convert.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/gfx/util.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/idirectfb.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/input/idirectfbinputbuffer.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/input/idirectfbinputdevice.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/interface_implementation.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/media/idirectfbdatabuffer.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/media/idirectfbfont.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/media/idirectfbimageprovider.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/media/idirectfbvideoprovider.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/misc/conf.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/misc/cpu_accel.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/misc/gfx_util.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/misc/mem.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/misc/memcpy.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/misc/tree.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/misc/utf8.h
-BUILDLINK_FILES.directfb+=	include/directfb-internal/misc/util.h
+BUILDLINK_FILES.directfb+=	include/directfb-internal/core/*.h
+BUILDLINK_FILES.directfb+=	include/directfb-internal/*.h
+BUILDLINK_FILES.directfb+=	include/directfb-internal/display/*.h
+BUILDLINK_FILES.directfb+=	include/directfb-internal/gfx/*.h
+BUILDLINK_FILES.directfb+=	include/directfb-internal/input/*.h
+BUILDLINK_FILES.directfb+=	include/directfb-internal/media/*.h
+BUILDLINK_FILES.directfb+=	include/directfb-internal/misc/*.h
 BUILDLINK_FILES.directfb+=	include/directfb-internal/windows/idirectfbwindow.h
-BUILDLINK_FILES.directfb+=	include/directfb/dfb_types.h
-BUILDLINK_FILES.directfb+=	include/directfb/directfb.h
-BUILDLINK_FILES.directfb+=	include/directfb/directfb_keyboard.h
-BUILDLINK_FILES.directfb+=	include/directfb/directfb_keynames.h
+BUILDLINK_FILES.directfb+=	include/directfb/*.h
 BUILDLINK_FILES.directfb+=	lib/${PKGNAME}/interfaces/IDirectFBFont/libidirectfbfont_default.*
 BUILDLINK_FILES.directfb+=	lib/${PKGNAME}/interfaces/IDirectFBFont/libidirectfbfont_ft2.*
 BUILDLINK_FILES.directfb+=	lib/${PKGNAME}/interfaces/IDirectFBImageProvider/libidirectfbimageprovider_gif.*
