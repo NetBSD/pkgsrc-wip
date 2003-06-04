71925e9fc1abce013eca72ed761559bc529b2845
Martti Kuparinen <martti@NetBSD.org>
2003-06-04 07:38:14 +0000

    Icon displayer for XFce4.

---
+++ xfce4-iconbox/buildlink2.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/06/04 07:38:15 marttikuparinen Exp $
+#
+# This Makefile fragment is included by packages that use xfce4-iconbox.
+#
+# This file was created automatically using createbuildlink 2.6.
+#
+
+.if !defined(XFCE4_ICONBOX_BUILDLINK2_MK)
+XFCE4_ICONBOX_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			xfce4-iconbox
+BUILDLINK_DEPENDS.xfce4-iconbox?=		xfce4-iconbox>=3.99.20030604
+BUILDLINK_PKGSRCDIR.xfce4-iconbox?=		../../wip/xfce4-iconbox
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.xfce4-iconbox=xfce4-iconbox
+BUILDLINK_PREFIX.xfce4-iconbox_DEFAULT=	${X11PREFIX}
+BUILDLINK_FILES.xfce4-iconbox+=	lib/xfce4/mcs-plugins/xfce4-iconbox_settings.*
+
+.include "../../devel/pkgconfig/buildlink2.mk"
+.include "../../wip/xfce4-mcs-plugins/buildlink2.mk"
+.include "../../devel/glib2/buildlink2.mk"
+.include "../../devel/pkgconfig/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	xfce4-iconbox-buildlink
+
+xfce4-iconbox-buildlink: _BUILDLINK_USE
+
+.endif	# XFCE4_ICONBOX_BUILDLINK2_MK
