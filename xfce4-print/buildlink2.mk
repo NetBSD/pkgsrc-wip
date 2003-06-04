e212ad880ef4198403fbba4417570ccdf0182f9a
Martti Kuparinen <martti@NetBSD.org>
2003-06-04 07:39:56 +0000

    Print dialog for XFce4.

---
+++ xfce4-print/buildlink2.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/06/04 07:39:56 marttikuparinen Exp $
+#
+# This Makefile fragment is included by packages that use xfce4-print.
+#
+# This file was created automatically using createbuildlink 2.6.
+#
+
+.if !defined(XFCE4_PRINT_BUILDLINK2_MK)
+XFCE4_PRINT_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			xfce4-print
+BUILDLINK_DEPENDS.xfce4-print?=		xfce4-print>=3.99.20030604
+BUILDLINK_PKGSRCDIR.xfce4-print?=		../../wip/xfce4-print
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.xfce4-print=xfce4-print
+BUILDLINK_PREFIX.xfce4-print_DEFAULT=	${X11PREFIX}
+BUILDLINK_FILES.xfce4-print+=	lib/libxfprint.*
+
+.include "../../devel/pkgconfig/buildlink2.mk"
+.include "../../wip/xfce4-mcs-plugins/buildlink2.mk"
+.include "../../devel/glib2/buildlink2.mk"
+.include "../../devel/pkgconfig/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	xfce4-print-buildlink
+
+xfce4-print-buildlink: _BUILDLINK_USE
+
+.endif	# XFCE4_PRINT_BUILDLINK2_MK
