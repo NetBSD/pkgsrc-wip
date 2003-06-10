58b9fc3bf959bbfb223569eb44bf775edc53d7a2
Adam Ciarciński <adam@NetBSD.org>
2003-06-10 13:50:23 +0000

    Re-written to use gimp-current, instead of gimp-base

--- gimp-current/buildlink2.mk
+++ gimp-current/buildlink2.mk
@@ -1,24 +1,28 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/05/20 07:55:32 moubctez Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/06/10 13:50:23 moubctez Exp $
 
-.if !defined(GIMP_BASE_BUILDLINK2_MK)
-GIMP_BASE_BUILDLINK2_MK=	# defined
+.if !defined(GIMP_CURRENT_BUILDLINK2_MK)
+GIMP_CURRENT_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=		gimp-base
-BUILDLINK_DEPENDS.gimp-base?=	gimp-base>=1.2.3nb1
-BUILDLINK_PKGSRCDIR.gimp-base?=	../../graphics/gimp-base
-DEPENDS+=		${BUILDLINK_DEPENDS.gimp-base}:../../graphics/gimp-base
+BUILDLINK_PACKAGES+=			gimp-current
+BUILDLINK_DEPENDS.gimp-current?=	gimp-current>=1.3.15
+BUILDLINK_PKGSRCDIR.gimp-current?=	../../wip/gimp-current
 
-EVAL_PREFIX+=		BUILDLINK_PREFIX.gimp-base=gimp-base
-BUILDLINK_PREFIX.gimp-base_DEFAULT=	${X11PREFIX}
-BUILDLINK_FILES.gimp-base=	include/libgimp/*
-BUILDLINK_FILES.gimp-base+=	lib/libgimp.*
-BUILDLINK_FILES.gimp-base+=	lib/libgimpui.*
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gimp-current=gimp-current
+BUILDLINK_PREFIX.gimp-current_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.gimp-current+=	include/gimp-1.3/*
+BUILDLINK_FILES.gimp-current+=	lib/libgck*
+BUILDLINK_FILES.gimp-current+=	lib/libgimp*
 
-.include "../../devel/glib/buildlink2.mk"
-.include "../../x11/gtk/buildlink2.mk"
+.include "../../devel/gettext-lib/buildlink2.mk"
+.include "../../graphics/jpeg/buildlink2.mk"
+.include "../../graphics/mpeg-lib/buildlink2.mk"
+.include "../../graphics/png/buildlink2.mk"
+.include "../../graphics/tiff/buildlink2.mk"
+.include "../../graphics/libart2/buildlink2.mk"
+.include "../../x11/gtk2/buildlink2.mk"
 
-BUILDLINK_TARGETS+=	gimp-base-buildlink
+BUILDLINK_TARGETS+=	gimp-current-buildlink
 
-gimp-base-buildlink: _BUILDLINK_USE
+gimp-current-buildlink: _BUILDLINK_USE
 
-.endif	# GIMP_BASE_BUILDLINK2_MK
+.endif	# GIMP_CURRENT_BUILDLINK2_MK
