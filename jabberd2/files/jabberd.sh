57bd244866f3683c9d906298a5205d72d9e1deb1
Juan Romero Pardines <xtraeme@gmail.com>
2003-06-12 05:29:03 +0000

    OOPS! Fix wrongs PATHS and add PKG_SYSCONFSUBDIR, I think that jabberd works properly.

--- jabberd2/files/jabberd.sh
+++ jabberd2/files/jabberd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: jabberd.sh,v 1.1.1.1 2003/06/12 04:01:38 xtraeme Exp $
+# $NetBSD: jabberd.sh,v 1.2 2003/06/12 05:29:03 xtraeme Exp $
 #
 
 # PROVIDE: jabberd
@@ -12,7 +12,7 @@ name="jabberd"
 rcvar=$name
 command="@PREFIX@/bin/${name}"
 pidfile="/var/run/${help_name}.pid"
-required_files="@PKG_SYSCONFDIR@/jabber.cfg"
+required_files="@PKG_SYSCONFDIR@/jabberd.cfg"
 
 load_rc_config $name
 run_rc_command "$1"
