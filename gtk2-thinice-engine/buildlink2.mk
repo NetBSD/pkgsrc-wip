fa9cd2ade9aa3a5fe534b814a933f4b2b0c786ce
Juan Romero Pardines <xtraeme@gmail.com>
2003-06-23 17:36:29 +0000

    Initial import of gtk2-thinice-engine 2.0.2
    gtk-thinice-engine is a simple, clean engine for GTK+-2.

---
+++ gtk2-thinice-engine/buildlink2.mk
@@ -0,0 +1,27 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/06/23 17:36:29 xtraeme Exp $
+#
+# This Makefile fragment is included by packages that use gtk-thinice-engine.
+#
+# This file was created automatically using createbuildlink 2.6.
+#
+
+.if !defined(GTK_THINICE_ENGINE_BUILDLINK2_MK)
+GTK_THINICE_ENGINE_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			gtk-thinice-engine
+BUILDLINK_DEPENDS.gtk-thinice-engine?=		gtk-thinice-engine>=2.0.2
+BUILDLINK_PKGSRCDIR.gtk-thinice-engine?=		../../wip/gtk-thinice-engine
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gtk-thinice-engine=gtk-thinice-engine
+BUILDLINK_PREFIX.gtk-thinice-engine_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.gtk-thinice-engine+=	lib/gtk-2.0/2.2.0/engines/libthinice.*
+BUILDLINK_FILES.gtk-thinice-engine+=	lib/pkgconfig/gtk-thinice-engine-2.pc
+
+.include "../../devel/pkgconfig/buildlink2.mk"
+.include "../../x11/gtk2/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	gtk-thinice-engine-buildlink
+
+gtk-thinice-engine-buildlink: _BUILDLINK_USE
+
+.endif	# GTK_THINICE_ENGINE_BUILDLINK2_MK
