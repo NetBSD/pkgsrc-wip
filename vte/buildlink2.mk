d4a88a6b8579de79e4c3f7b957c5b450ec44e861
Juan Romero Pardines <xtraeme@gmail.com>
2003-06-24 04:02:16 +0000

    Initial import of vte 0.10.29
    Terminal widget with improved font, internationalisation and accessibility 
    support for the GNOME 2 desktop.  Designed to replace libzvt.
    
    --
    Another package for the GNOME2 metapackage, we need fix the PLIST :)
    And maybe give support to gnome-pty-helper ? 
    Any comment will be welcome :P

---
+++ vte/buildlink2.mk
@@ -0,0 +1,39 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/06/24 04:02:17 xtraeme Exp $
+#
+# This Makefile fragment is included by packages that use vte.
+#
+# This file was created automatically using createbuildlink 2.6.
+#
+
+.if !defined(VTE_BUILDLINK2_MK)
+VTE_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			vte
+BUILDLINK_DEPENDS.vte?=		vte>=0.10.29
+BUILDLINK_PKGSRCDIR.vte?=		../../wip/vte
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.vte=vte
+BUILDLINK_PREFIX.vte_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.vte+=	include/vte/pty.h
+BUILDLINK_FILES.vte+=	include/vte/reaper.h
+BUILDLINK_FILES.vte+=	include/vte/vte.h
+BUILDLINK_FILES.vte+=	include/vte/vteaccess.h
+BUILDLINK_FILES.vte+=	lib/libvte.*
+BUILDLINK_FILES.vte+=	lib/pkgconfig/vte.pc
+BUILDLINK_FILES.vte+=	lib/vte/decset
+BUILDLINK_FILES.vte+=	lib/vte/interpret
+BUILDLINK_FILES.vte+=	lib/vte/iso8859mode
+BUILDLINK_FILES.vte+=	lib/vte/nativeecho
+BUILDLINK_FILES.vte+=	lib/vte/osc
+BUILDLINK_FILES.vte+=	lib/vte/slowcat
+BUILDLINK_FILES.vte+=	lib/vte/utf8echo
+BUILDLINK_FILES.vte+=	lib/vte/utf8mode
+BUILDLINK_FILES.vte+=	lib/vte/window
+
+.include "../../x11/gtk2/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	vte-buildlink
+
+vte-buildlink: _BUILDLINK_USE
+
+.endif	# VTE_BUILDLINK2_MK
