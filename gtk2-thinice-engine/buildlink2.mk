4dd33a8b240595c4056f80242a0ec016c42437cf
Juan Romero Pardines <xtraeme@gmail.com>
2003-06-24 08:10:33 +0000

    Rename gtk -> gtk2, add BUILD_USES_MSGFMT and buildlink of gettext-lib

--- gtk2-thinice-engine/buildlink2.mk
+++ gtk2-thinice-engine/buildlink2.mk
@@ -1,27 +1,28 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/06/23 17:36:29 xtraeme Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/06/24 08:10:33 xtraeme Exp $
 #
-# This Makefile fragment is included by packages that use gtk-thinice-engine.
+# This Makefile fragment is included by packages that use gtk2-thinice-engine.
 #
 # This file was created automatically using createbuildlink 2.6.
 #
 
-.if !defined(GTK_THINICE_ENGINE_BUILDLINK2_MK)
-GTK_THINICE_ENGINE_BUILDLINK2_MK=	# defined
+.if !defined(GTK2_THINICE_ENGINE_BUILDLINK2_MK)
+GTK2_THINICE_ENGINE_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=			gtk-thinice-engine
-BUILDLINK_DEPENDS.gtk-thinice-engine?=		gtk-thinice-engine>=2.0.2
-BUILDLINK_PKGSRCDIR.gtk-thinice-engine?=		../../wip/gtk-thinice-engine
+BUILDLINK_PACKAGES+=			gtk2-thinice-engine
+BUILDLINK_DEPENDS.gtk2-thinice-engine?=		gtk2-thinice-engine>=2.0.2
+BUILDLINK_PKGSRCDIR.gtk2-thinice-engine?=		../../wip/gtk2-thinice-engine
 
-EVAL_PREFIX+=	BUILDLINK_PREFIX.gtk-thinice-engine=gtk-thinice-engine
-BUILDLINK_PREFIX.gtk-thinice-engine_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.gtk-thinice-engine+=	lib/gtk-2.0/2.2.0/engines/libthinice.*
-BUILDLINK_FILES.gtk-thinice-engine+=	lib/pkgconfig/gtk-thinice-engine-2.pc
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gtk2-thinice-engine=gtk2-thinice-engine
+BUILDLINK_PREFIX.gtk2-thinice-engine_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.gtk2-thinice-engine+=	lib/gtk-2.0/2.2.0/engines/libthinice.*
+BUILDLINK_FILES.gtk2-thinice-engine+=	lib/pkgconfig/gtk-thinice-engine-2.pc
 
 .include "../../devel/pkgconfig/buildlink2.mk"
 .include "../../x11/gtk2/buildlink2.mk"
+.include "../../devel/gettext-lib/buildlink2.mk"
 
-BUILDLINK_TARGETS+=	gtk-thinice-engine-buildlink
+BUILDLINK_TARGETS+=	gtk2-thinice-engine-buildlink
 
-gtk-thinice-engine-buildlink: _BUILDLINK_USE
+gtk2-thinice-engine-buildlink: _BUILDLINK_USE
 
-.endif	# GTK_THINICE_ENGINE_BUILDLINK2_MK
+.endif	# GTK2_THINICE_ENGINE_BUILDLINK2_MK
