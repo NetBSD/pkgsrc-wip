5f39d4553b900742b550447fff3fdd5bfdb5b097
Juan Romero Pardines <xtraeme@gmail.com>
2003-06-25 15:37:21 +0000

    Don't run the daemon as root, use the jabberd user.

--- jabberd/files/jabberd.sh
+++ jabberd/files/jabberd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: jabberd.sh,v 1.1 2003/06/05 14:03:28 xtraeme Exp $
+# $NetBSD: jabberd.sh,v 1.2 2003/06/25 15:37:21 xtraeme Exp $
 #
 
 # PROVIDE: jabberd
@@ -15,6 +15,7 @@ command="@PREFIX@/sbin/${name}"
 command_args="-B -H /var/spool/jabberd -c \$required_files"
 pidfile="/var/run/${help_name}.pid"
 required_files="@PKG_SYSCONFDIR@/jabber.xml"
+jabberd_user="jabberd"
 
 load_rc_config $name
 run_rc_command "$1"
