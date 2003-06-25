09c7ca152a6455b87c5646c4001d8a334f36c02a
Juan Romero Pardines <xtraeme@gmail.com>
2003-06-25 15:43:16 +0000

    Added an user/group ezbounce to run the daemon.

--- ezbounce/files/ezbounce.sh
+++ ezbounce/files/ezbounce.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: ezbounce.sh,v 1.1.1.1 2003/04/20 06:27:44 xtraeme Exp $
+# $NetBSD: ezbounce.sh,v 1.2 2003/06/25 15:43:16 xtraeme Exp $
 #
 
 # PROVIDE: ezbounce
@@ -13,6 +13,7 @@ rcvar=$name
 command="@PREFIX@/sbin/${name} @PKG_SYSCONFDIR@/$name.conf"
 pidfile="/var/run/${name}.pid"
 required_files="@PKG_SYSCONFDIR@/$name.conf"
+ezbounce_user="ezbounce"
 
 load_rc_config $name
 run_rc_command "$1"
