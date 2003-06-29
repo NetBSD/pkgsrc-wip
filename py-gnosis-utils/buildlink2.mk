7e68f72dff08d12195a1eff7251f017a30174491
Marc Recht <marc@users.sourceforge.net>
2003-06-29 17:23:14 +0000

    Update to 1.0.7
    * New module gnosis.anon. This module generates "anonyms" for email
      addresses, and forwards any mail sent to those anonyms on to the
      corresponding real addresses. In other words, this is a protocol
      that allows anonymous recipients to get email. Currently alpha-status.
      See http://gnosis.cx/anon for further information.
    
    * Updated gnosis.xml.pickle to handle the new BooleanType in
      Python 2.3. Python <= 2.2 can read these pickles as well.
      Revised family mapping for functions/classes.
    
    * Bugfix in gnosis.xml.pickle.util for classes that are
      loaded from sys.modules.
    
    * Rewrote gnosis.util.introspect.instance_noinit to use more
      robust "new" module and/or __new__() as applicable. Added
      obj_from_classtype to public API.
    
    * Updated xml.objectify to handle namespaces. Additional check
      for passed DOM node. Added new tests.
    
    * New util xmlcat.py for concatenating XML files.

--- py-gnosis-utils/buildlink2.mk
+++ py-gnosis-utils/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/04/13 12:57:00 marc Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/06/29 17:23:14 marc Exp $
 
 .if !defined(GNOSIS_UTILS_BUILDLINK2_MK)
 GNOSIS_UTILS_BUILDLINK2_MK=	# defined
@@ -6,7 +6,7 @@ GNOSIS_UTILS_BUILDLINK2_MK=	# defined
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_PACKAGES+=			Gnosis-Utils
-BUILDLINK_DEPENDS.Gnosis-Utils?=	${PYPKGPREFIX}-Gnosis-Utils>=1.0.6
+BUILDLINK_DEPENDS.Gnosis-Utils?=	${PYPKGPREFIX}-Gnosis-Utils>=1.0.7
 BUILDLINK_PKGSRCDIR.Gnosis-Utils?=	../../wip/py-gnosis-utils
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.Gnosis-Utils=${PYPKGPREFIX}-Gnosis-Utils
