5630ffe6d4309c7a41ec2c4a8e36dc93374ca650
Martti Kuparinen <martti@NetBSD.org>
2003-07-09 18:01:21 +0000

    The latest MySQL 3.23.x

---
+++ mysql3-client/buildlink2.mk
@@ -0,0 +1,26 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/07/09 18:01:22 marttikuparinen Exp $
+
+.if !defined(MYSQL_CLIENT_BUILDLINK2_MK)
+MYSQL_CLIENT_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			mysql-client
+BUILDLINK_DEPENDS.mysql-client?=	mysql-client>=3.23.57
+BUILDLINK_PKGSRCDIR.mysql-client?=	../../wip/mysql3-client
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.mysql-client=mysql-client
+BUILDLINK_PREFIX.mysql-client_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.mysql-client=	include/mysql/*.h
+BUILDLINK_FILES.mysql-client+=	lib/mysql/libmysqlclient.*
+
+FIX_RPATH+=	BUILDLINK_LDFLAGS.mysql-client
+BUILDLINK_LDFLAGS.mysql-client= \
+	-L${BUILDLINK_PREFIX.mysql-client}/lib/mysql			\
+	-Wl,${RPATH_FLAG}${BUILDLINK_PREFIX.mysql-client}/lib/mysql
+
+.include "../../devel/zlib/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	mysql-client-buildlink
+
+mysql-client-buildlink: _BUILDLINK_USE
+
+.endif	# MYSQL_CLIENT_BUILDLINK2_MK
