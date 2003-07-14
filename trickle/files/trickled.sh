10e07559deb6a4553dad642d477c5ce6fc6d99ee
Juan Romero Pardines <xtraeme@gmail.com>
2003-07-14 17:45:57 +0000

    Change harcoded shell to @RCS_SCRIPTS_SHELL@

--- trickle/files/trickled.sh
+++ trickle/files/trickled.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-# $Id: trickled.sh,v 1.2 2003/06/05 00:12:45 xtraeme Exp $
-# $NetBSD: trickled.sh,v 1.2 2003/06/05 00:12:45 xtraeme Exp $
+#!@RCS_SCRIPTS_SHELL@
+# $Id: trickled.sh,v 1.3 2003/07/14 17:45:57 xtraeme Exp $
+# $NetBSD: trickled.sh,v 1.3 2003/07/14 17:45:57 xtraeme Exp $
 #
 
 # PROVIDE: trickled
