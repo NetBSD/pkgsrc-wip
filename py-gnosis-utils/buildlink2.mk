95bf6bdcc32a471f7b8ea864a95213fce0463541
Marc Recht <marc@users.sourceforge.net>
2003-07-27 17:24:50 +0000

    Update Gnosis Utils to 1.1.0. This release contains enhancements to gnosis.xml.objectify. (See the ANNOUNCE file for details.)

--- py-gnosis-utils/buildlink2.mk
+++ py-gnosis-utils/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/06/29 17:23:14 marc Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/07/27 17:24:50 marc Exp $
 
 .if !defined(GNOSIS_UTILS_BUILDLINK2_MK)
 GNOSIS_UTILS_BUILDLINK2_MK=	# defined
@@ -6,7 +6,7 @@ GNOSIS_UTILS_BUILDLINK2_MK=	# defined
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_PACKAGES+=			Gnosis-Utils
-BUILDLINK_DEPENDS.Gnosis-Utils?=	${PYPKGPREFIX}-Gnosis-Utils>=1.0.7
+BUILDLINK_DEPENDS.Gnosis-Utils?=	${PYPKGPREFIX}-Gnosis-Utils>=1.1.0
 BUILDLINK_PKGSRCDIR.Gnosis-Utils?=	../../wip/py-gnosis-utils
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.Gnosis-Utils=${PYPKGPREFIX}-Gnosis-Utils
