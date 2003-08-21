4bf2ce8fd179a2506e04a5f9498d02ffc974ae8a
Juan Romero Pardines <xtraeme@gmail.com>
2003-08-21 18:21:49 +0000

    Add this.

---
+++ gift/buildlink2.mk
@@ -0,0 +1,29 @@
+# $NetBSD: buildlink2.mk,v 1.1 2003/08/21 18:21:49 xtraeme Exp $
+#
+# This Makefile fragment is included by packages that use gift.
+#
+# This file was created automatically using createbuildlink 2.6.
+#
+
+.if !defined(GIFT_BUILDLINK2_MK)
+GIFT_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			gift
+BUILDLINK_DEPENDS.gift?=		gift>=0.11.3
+BUILDLINK_PKGSRCDIR.gift?=		../../wip/gift
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gift=gift
+BUILDLINK_PREFIX.gift_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.gift+=	include/libgift/*.h
+BUILDLINK_FILES.gift+=	lib/libgift.*
+BUILDLINK_FILES.gift+=	lib/libgiftproto.*
+BUILDLINK_FILES.gift+=	lib/pkgconfig/libgift.pc
+
+.include "../../audio/libvorbis/buildlink2.mk"
+.include "../../devel/pkgconfig/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	gift-buildlink
+
+gift-buildlink: _BUILDLINK_USE
+
+.endif	# GIFT_BUILDLINK2_MK
