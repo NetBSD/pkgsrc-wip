d37dc7158d349f6e055e1184948ef9a2a629aa35
Juan Romero Pardines <xtraeme@gmail.com>
2003-09-04 02:35:08 +0000

    Initial import of libgringotts 1.2.1
    libGringotts is a small, easy-to-use, thread-safe C library originally
    developed for Gringotts; its purpose is to encapsulate data (generic: ASCII,
    but also binary data) in an encrypted and compressed structure, to be written
    in a file or used elseway. It makes use of strong encryption algorithms, to
    ensure the data are as safe as possible, and allow the user to have the
    complete control over all the algorithms used in the process. 
    
    For encryptions, libGringotts makes use of the MCrypt and MHash libs by Nikos
    Mavroyanopoulos.
    
    It closes my own PR pkg/19697.

---
+++ libgringotts/buildlink2.mk
@@ -0,0 +1,29 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/09/04 02:35:08 xtraeme Exp $
+#
+# This Makefile fragment is included by packages that use libgringotts.
+#
+# This file was created automatically using createbuildlink 2.6.
+#
+
+.if !defined(LIBGRINGOTTS_BUILDLINK2_MK)
+LIBGRINGOTTS_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			libgringotts
+BUILDLINK_DEPENDS.libgringotts?=		libgringotts>=1.2.1
+BUILDLINK_PKGSRCDIR.libgringotts?=		../../wip/libgringotts
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.libgringotts=libgringotts
+BUILDLINK_PREFIX.libgringotts_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.libgringotts+=	include/libgringotts.h
+BUILDLINK_FILES.libgringotts+=	lib/libgringotts.*
+BUILDLINK_FILES.libgringotts+=	lib/pkgconfig/libgringotts.pc
+
+.include "../../devel/pkgconfig/buildlink2.mk"
+.include "../../security/libmcrypt/buildlink2.mk"
+.include "../../security/mhash/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	libgringotts-buildlink
+
+libgringotts-buildlink: _BUILDLINK_USE
+
+.endif	# LIBGRINGOTTS_BUILDLINK2_MK
