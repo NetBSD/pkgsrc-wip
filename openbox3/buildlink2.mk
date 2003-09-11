24865da055ffdcf6979b596ecf471396174890f6
Julio Merino <jmmv@NetBSD.org>
2003-09-11 08:53:23 +0000

    Use wildcard for include files. Drop pkgconfig dependancy; this is a build dependancy and shouldn't be in a buildlink2.mk file.

--- openbox3/buildlink2.mk
+++ openbox3/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1 2003/09/11 00:37:16 blef Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/09/11 08:53:23 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use openbox.
 #
@@ -14,9 +14,7 @@ BUILDLINK_PKGSRCDIR.openbox?=		../../wip/openbox3
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.openbox=openbox
 BUILDLINK_PREFIX.openbox_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.openbox+=	include/openbox/3.0/openbox/parse.h
-BUILDLINK_FILES.openbox+=	include/openbox/3.0/openbox/render.h
-BUILDLINK_FILES.openbox+=	include/openbox/3.0/openbox/theme.h
+BUILDLINK_FILES.openbox+=	include/openbox/3.0/openbox/*
 BUILDLINK_FILES.openbox+=	lib/libobparser.*
 BUILDLINK_FILES.openbox+=	lib/libobrender.*
 BUILDLINK_FILES.openbox+=	lib/pkgconfig/obparser-3.0.pc
@@ -24,7 +22,6 @@ BUILDLINK_FILES.openbox+=	lib/pkgconfig/obrender-3.0.pc
 
 .include "../../converters/libiconv/buildlink2.mk"
 .include "../../devel/glib2/buildlink2.mk"
-.include "../../devel/pkgconfig/buildlink2.mk"
 .include "../../fonts/Xft2/buildlink2.mk"
 .include "../../textproc/libxml2/buildlink2.mk"
 .include "../../x11/startup-notification/buildlink2.mk"
