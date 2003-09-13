9d052e84a458d9414537b32a385647e8e7fb4d1f
Jeremy C. Reed <reed@reedmedia.net>
2003-09-13 22:47:58 +0000

    rc.subr package is already in pkgsrc, but this has many changes so I am using pkgsrc-wip for testing.

---
+++ rc.subr/files/NETWORKING.sh
@@ -0,0 +1,9 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: NETWORKING.sh,v 1.1.1.1 2003/09/13 22:47:59 jeremy-c-reed Exp $
+#
+
+# PROVIDE: NETWORKING NETWORK
+
+#	This is a dummy dependency, for services which require networking
+#	to be operational before starting.
