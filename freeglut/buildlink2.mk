33724706e4839a82488d283a4f1be017f6b081e4
Juan Romero Pardines <xtraeme@gmail.com>
2003-10-07 16:33:03 +0000

    Upgrade to 2.0.0 FINAL RELEASE!
    Changes:
    
    * You can now control program behavior when a GLUT window is closed by
      the user, rather than simply exiting. Options include:
              o Immediate exit() (old GLUT behavior)
              o glutMainLoop() returns to you
              o continue execution
    * Option to share a rendering context for each new window
    * Interruptable glutMainLoop (glutMainLoopEvent, glutLeaveMainLoop)
    * Window-specific callback functions (Close, WindowManager Close, Menu
      destruction)
    * User-specified data in Windows and Menus
    * Additional font statistics (stroke and bitmap heights...)
    * New font functions to allow the user to print strings instead of one
      character at a time
    * New primitives (RhombicDodecahedron, SierpinskiSponge)
    * Good-looking menus

--- freeglut/buildlink2.mk
+++ freeglut/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/08/19 07:35:48 xtraeme Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/10/07 16:33:03 xtraeme Exp $
 #
 # This Makefile fragment is included by packages that use freeglut.
 #
@@ -18,8 +18,6 @@ BUILDLINK_FILES.freeglut+=	include/GL/freeglut.h
 BUILDLINK_FILES.freeglut+=	include/GL/freeglut_ext.h
 BUILDLINK_FILES.freeglut+=	include/GL/glut.h
 BUILDLINK_FILES.freeglut+=	lib/libglut.*
-
-.include "../../graphics/MesaLib/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	freeglut-buildlink
 
