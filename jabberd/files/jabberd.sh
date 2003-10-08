f1d8c45704609169253c1d45e21da72b8904035e
Juan Romero Pardines <xtraeme@gmail.com>
2003-10-08 00:17:29 +0000

    * Remove pth dependency (I've heard on the jabber mailing list, it's better   to use the pth distribution that comes with jabber) * Use FILES_SUBST for JABBERD_{USER,GROUP,SPOOLDIR,PIDDIR}

--- jabberd/files/jabberd.sh
+++ jabberd/files/jabberd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: jabberd.sh,v 1.3 2003/08/12 17:38:49 xtraeme Exp $
+# $NetBSD: jabberd.sh,v 1.4 2003/10/08 00:17:29 xtraeme Exp $
 #
 
 # PROVIDE: jabberd
@@ -13,9 +13,10 @@ help_name="jabber"
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
 required_files="@PKG_SYSCONFDIR@/jabber.xml"
-command_args="-B -H /var/spool/jabberd -c $required_files"
-pidfile="/var/run/${help_name}.pid"
-jabberd_user="jabberd"
+command_args="-B -H @JABBERD_SPOOLDIR@ -c $required_files"
+pidfile="@JABBERD_PIDDIR@/${help_name}.pid"
+jabberd_user="@JABBERD_USER@"
+jabberd_group="@JABBERD_GROUP@"
 
 load_rc_config $name
 run_rc_command "$1"
