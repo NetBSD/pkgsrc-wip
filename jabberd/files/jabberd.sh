d23d68ca353f0c310c95f518d80008e1b1510d16
Juan Romero Pardines <xtraeme@gmail.com>
2003-10-09 02:26:23 +0000

    * Invert the order of auth methods (digest is better than plain!) * Remove ${name}_group (not needed) * Send the unneeded output of the startup script to /dev/null

--- jabberd/files/jabberd.sh
+++ jabberd/files/jabberd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: jabberd.sh,v 1.4 2003/10/08 00:17:29 xtraeme Exp $
+# $NetBSD: jabberd.sh,v 1.5 2003/10/09 02:26:23 xtraeme Exp $
 #
 
 # PROVIDE: jabberd
@@ -13,10 +13,9 @@ help_name="jabber"
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
 required_files="@PKG_SYSCONFDIR@/jabber.xml"
-command_args="-B -H @JABBERD_SPOOLDIR@ -c $required_files"
+command_args="-B -H @JABBERD_SPOOLDIR@ -c $required_files 2> /dev/null"
 pidfile="@JABBERD_PIDDIR@/${help_name}.pid"
 jabberd_user="@JABBERD_USER@"
-jabberd_group="@JABBERD_GROUP@"
 
 load_rc_config $name
 run_rc_command "$1"
