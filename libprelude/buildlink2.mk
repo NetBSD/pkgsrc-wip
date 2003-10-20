311491ffb0a47db09a1b094cf5156d6f5146f4cd
Adrian Portelli <adrian_p@users.sourceforge.net>
2003-10-20 08:23:15 +0000

    update to v0.8.7

--- libprelude/buildlink2.mk
+++ libprelude/buildlink2.mk
@@ -1,8 +1,8 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/09/28 13:01:15 adrian_p Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/10/20 08:23:15 adrian_p Exp $
 
 BUILDLINK_PACKAGES+=			libprelude
 BUILDLINK_PKGBASE.libprelude+=		libprelude
-BUILDLINK_DEPENDS.libprelude?=		libprelude>=0.8.6
+BUILDLINK_DEPENDS.libprelude?=		libprelude>=0.8.7
 BUILDLINK_PKGSRCDIR.libprelude?=	../../wip/libprelude
 
 EVAL_PREFIX+=				BUILDLINK_PREFIX.libprelude=libprelude
