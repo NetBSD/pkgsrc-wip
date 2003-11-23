71d61d9f758d4bc46ea267ab14abd89cd51bcf54
Jeremy C. Reed <reed@reedmedia.net>
2003-11-23 01:47:34 +0000

    Use wip instead of security directory. Thank you, Adrian.

--- mit-krb5/buildlink2.mk
+++ mit-krb5/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.4 2003/10/14 05:46:33 jeremy-c-reed Exp $
+# $NetBSD: buildlink2.mk,v 1.5 2003/11/23 01:47:34 jeremy-c-reed Exp $
 
 .if !defined(MITKRB5_BUILDLINK2_MK)
 MITKRB5_BUILDLINK2_MK=   # defined
 
 BUILDLINK_PACKAGES+=		mit-krb5
 BUILDLINK_DEPENDS.mit-krb5?=	mit-krb5>=1.3.1
-BUILDLINK_PKGSRCDIR.mit-krb5?=	../../security/mit-krb5
+BUILDLINK_PKGSRCDIR.mit-krb5?=	../../wip/mit-krb5
 
 EVAL_PREFIX+=				BUILDLINK_PREFIX.mit-krb5=mit-krb5
 BUILDLINK_PREFIX.mit-krb5_DEFAULT=	${LOCALBASE}
