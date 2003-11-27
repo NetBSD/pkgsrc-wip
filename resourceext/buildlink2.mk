1eda8f9e78c1f6f73d0bd6b8c730cced997c08a7
Juan Romero Pardines <xtraeme@gmail.com>
2003-11-27 04:09:13 +0000

    Initial import of resourceext-1.0
    This package contains header files and documentation for the resource
    extension.  Library and server implementations are separate.
    
    More information: www.freedesktop.org

---
+++ resourceext/buildlink2.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/11/27 04:09:13 xtraeme Exp $
+#
+# This Makefile fragment is included by packages that use resourceext.
+#
+# This file was created automatically using createbuildlink 2.8.
+#
+
+.if !defined(RESOURCEEXT_BUILDLINK2_MK)
+RESOURCEEXT_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			resourceext
+BUILDLINK_DEPENDS.resourceext?=		resourceext>=1.0
+BUILDLINK_PKGSRCDIR.resourceext?=		../../wip/resourceext
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.resourceext=resourceext
+BUILDLINK_PREFIX.resourceext_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.resourceext+=	include/X11/extensions/XResproto.h
+
+
+BUILDLINK_TARGETS+=	resourceext-buildlink
+
+resourceext-buildlink: _BUILDLINK_USE
+
+.endif	# RESOURCEEXT_BUILDLINK2_MK
