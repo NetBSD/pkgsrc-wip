ee7c99e90379ca302f1c7590b7afd4102062a04d
Jeremy C. Reed <reed@reedmedia.net>
2003-11-28 05:27:46 +0000

    New package:
    libxml++ is a C++ interface for working with XML files. It uses libxml2
    to parse and write the actual XML files. It has a simple and complete API.
    
    (I packaged this so I can now build Passepartout which I am also packaging.)

---
+++ libxml++/buildlink2.mk
@@ -0,0 +1,31 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/11/28 05:27:46 jeremy-c-reed Exp $
+#
+# This Makefile fragment is included by packages that use libxml++.
+#
+# This file was created automatically using createbuildlink 2.8.
+#
+
+.if !defined(LIBXMLXX_BUILDLINK2_MK)
+LIBXMLXX_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			libxmlxx
+BUILDLINK_DEPENDS.libxmlXX?=		libxml++>=0.27.0
+BUILDLINK_PKGSRCDIR.libxmlXX?=		../../new/libxml++
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.libxmlxx=libxmlxx
+BUILDLINK_PREFIX.libxmlxx_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.libxmlxx+=	include/libxml++-1.0/libxml++/*.h
+BUILDLINK_FILES.libxmlxx+=	include/libxml++-1.0/libxml++/exceptions/*.h
+BUILDLINK_FILES.libxmlxx+=	include/libxml++-1.0/libxml++/io/*.h
+BUILDLINK_FILES.libxmlxx+=	include/libxml++-1.0/libxml++/nodes/*.h
+BUILDLINK_FILES.libxmlxx+=	include/libxml++-1.0/libxml++/parsers/*.h
+BUILDLINK_FILES.libxmlxx+=	lib/libio.*
+BUILDLINK_FILES.libxmlxx+=	lib/libxml++-0.1.*
+
+.include "../../textproc/libxml2/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	libxmlxx-buildlink
+
+libxmlxx-buildlink: _BUILDLINK_USE
+
+.endif	# LIBXMLXX_BUILDLINK2_MK
