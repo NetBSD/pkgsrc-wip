93130dad8101a024610c8e600745dab85af07baf
Jeremy C. Reed <reed@reedmedia.net>
2003-12-06 08:37:38 +0000

    Import of ISC DHCP Server.
    (This also provides Makefile.common used for ISC DHCP Client.)
    
    This package provides the ISC Dynamic Host Configuration Protocol (DHCP)
    server and DHCP relay agent.
    
    It includes an DHCP (and BOOTP) server daemon used to distribute network
    information, an Object Management API command shell for querying and
    modifying DHCP server attributes (while it is running), and an agent
    for relaying DHCP and BOOTP requests between subnets.

---
+++ isc-dhcpd/files/site.h
@@ -0,0 +1,5 @@
+#define _PATH_DHCPD_CONF "@@SYSCONFDIR@@/dhcp/dhcpd.conf"
+#define _PATH_DHCPD_PID "@@VAR_DIR@@/run/dhcpd.pid"
+#define _PATH_DHCPD_DB "@@VAR_DIR@@/db/dhcp/dhcpd.leases"
+#define _PATH_DHCLIENT_DB "@@VAR_DIR@@/db/dhcp/dhclient.leases"
+#define DHCPD_LOG_FACILITY LOG_LOCAL1
