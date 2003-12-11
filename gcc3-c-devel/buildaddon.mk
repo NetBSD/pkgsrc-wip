e52f50c25590a828588a7b9b0a5d956a113e572d
John R. Shannon <johnrshannon@users.sourceforge.net>
2003-12-11 22:14:51 +0000

    This is the gcc, version 3.4 (experimental), c compiler. The port is based on the lang/gcc3-* group of pkgs implementing the gcc compiler family.

---
+++ gcc3-c-devel/buildaddon.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildaddon.mk,v 1.1.1.1 2003/12/11 22:14:51 johnrshannon Exp $
+
+.include "../../lang/gcc3-c-devel/Makefile.common"
+
+pre-build:
+	(cd ${WRKSRC}/gcc && ${SETENV} ${MAKE_ENV} ${GMAKE} tconfig.h)
+	${LN} -sf ${GCC_PREFIX}/lib/libiberty.a ${WRKSRC}/libiberty
+	${LN} -sf ${GCC_PREFIX}/bin/gcc ${WRKSRC}/gcc/xgcc
+
+BUILDLINK_DEPENDS.gcc34c?=	gcc3${GCC3_PKGMODIF}-c-3.4-devel
+BUILDLINK_PKGSRCDIR.gcc34c?=	../../lang/gcc3-c-devel
+
+BUILDLINK_PREFIX.gcc34c=	${LOCALBASE}
+
+BUILDLINK_PACKAGES+=	gcc34c
+
+BUILDLINK_FILES.gcc34c+=	${GCC_ARCHSUBDIR}/libgcc.a
+BUILDLINK_FILES.gcc34c+=	${GCC_ARCHSUBDIR}/libgcc_eh.a
+BUILDLINK_FILES.gcc34c+=	${GCC_ARCHSUBDIR#${GCC_VERSION}}/libgcc_s.so
+BUILDLINK_FILES.gcc34c+=	${GCC_ARCHSUBDIR#${GCC_VERSION}}/libgcc_s.so.1
+
+BUILDLINK_TARGETS+=		gcc34c-buildlink
+
+gcc34c-buildlink: _BUILDLINK_USE
