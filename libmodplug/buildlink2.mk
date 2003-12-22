f76034c9ed5c450fd05c5aa0d32aa618ab5df97f
Ben Collver <bencollver@gmail.com>
2003-12-22 03:40:00 +0000

    Change the path of the dependency in buildlink2.mk from audio to wip.

--- libmodplug/buildlink2.mk
+++ libmodplug/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/12/21 21:01:13 bencollver Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/12/22 03:40:00 bencollver Exp $
 
 .if !defined(LIBMODPLUG_BUILDLINK2_MK)
 LIBMODPLUG_BUILDLINK2_MK=		# defined
 
 BUILDLINK_PACKAGES+=			libmodplug
 BUILDLINK_DEPENDS.libmodplug?=		libmodplug>=0.7
-BUILDLINK_PKGSRCDIR.libmodplug?=	../../audio/libmodplug
+BUILDLINK_PKGSRCDIR.libmodplug?=	../../wip/libmodplug
 
 EVAL_PREFIX+=				BUILDLINK_PREFIX.libmodplug=libmodplug
 BUILDLINK_PREFIX.libmodplug_DEFAULT=	${LOCALBASE}
