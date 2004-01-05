9af4670bf2f51245c0c938ff2a8b85b8db14a31d
Quentin Garnier <cube@NetBSD.org>
2004-01-05 10:30:00 +0000

    Update to latest devel version.  The list of changes is too long to be described here.  The most important change is the integration of Lua as a script engine.

--- ion-devel/buildlink2.mk
+++ ion-devel/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/04/03 11:30:37 cubidou Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2004/01/05 10:30:00 cubidou Exp $
 
 .if !defined(ION_BUILDLINK2_MK)
 ION_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		ion
-BUILDLINK_DEPENDS.ion?=		ion>=20030327
+BUILDLINK_DEPENDS.ion?=		ion>=20031211
 BUILDLINK_PKGSRCDIR.ion?=	../../wip/ion-devel
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.ion=ion
