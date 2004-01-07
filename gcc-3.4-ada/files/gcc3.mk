df369ab74c5140beae60a64a4ed7c2b811897a9c
John R. Shannon <johnrshannon@users.sourceforge.net>
2004-01-07 22:49:46 +0000

    This is the Ada compiler from the gcc 3.4 compiler collection. All but 8 of the tests in the Ada compiler valication suite (acats) pass including all of the chapter 9 (tasking) tests when run under NetBSD-CURRENT on Intel x86 hardware.

---
+++ gcc-3.4-ada/files/gcc3.mk
@@ -0,0 +1,7 @@
+# $NetBSD: gcc3.mk,v 1.1 2004/01/07 22:49:46 johnrshannon Exp $
+#
+# make configuration file for @PKGNAME@
+
+CC=		@GCC_PREFIX@/bin/cc
+CPP=		@GCC_PREFIX@/bin/cpp
+ADAC=		@GCC_PREFIX@/bin/gcc
