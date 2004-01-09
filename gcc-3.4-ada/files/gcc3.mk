e1f99341ba41a8b45e7bc3ca35d9a45af1c25ba7
John R. Shannon <johnrshannon@users.sourceforge.net>
2004-01-09 23:24:18 +0000

    Cleanup so it builds without modified compiler.mk

--- gcc-3.4-ada/files/gcc3.mk
+++ gcc-3.4-ada/files/gcc3.mk
@@ -1,7 +1,8 @@
-# $NetBSD: gcc3.mk,v 1.1 2004/01/07 22:49:46 johnrshannon Exp $
+# $NetBSD: gcc3.mk,v 1.2 2004/01/09 23:24:18 johnrshannon Exp $
 #
 # make configuration file for @PKGNAME@
 
 CC=		@GCC_PREFIX@/bin/cc
 CPP=		@GCC_PREFIX@/bin/cpp
 ADAC=		@GCC_PREFIX@/bin/gcc
+PATH:=		@GCC_PREFIX@/bin:${PATH}
