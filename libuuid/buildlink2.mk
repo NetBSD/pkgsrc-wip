10f3e59f2986aabd551db396449ac6c0bf2a32d4
Michal Pasternak <mpasternak@users.sourceforge.net>
2004-01-10 18:05:25 +0000

    This is stand-alone package for libuuid, a part of e2fsprogs package.
    The libuuid library is used to generate unique identifiers for objects that
    may be accessible beyond the local system. The Linux implementation was
    created to uniquely identify ext2 filesystems created by a machine. This
    library generates UUIDs compatible with those created by the Open Software
    Foundation (OSF) Distributed Computing Environment (DCE) utility uuidgen. 
    
    The UUIDs generated by this library can be reasonably expected to be unique
    within a system, and unique across all systems. They could be used, for
    instance, to generate unique HTTP cookies across multiple web servers
    without communication between the servers, and without fear of a name clash.

---
+++ libuuid/buildlink2.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2004/01/10 18:05:25 mpasternak Exp $
+#
+# This Makefile fragment is included by packages that use libuuid.
+#
+
+.if !defined(LIBUUID_BUILDLINK2_MK)
+LIBUUID_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			libuuid
+BUILDLINK_DEPENDS.libuuid?=		libuuid>=1.34
+BUILDLINK_PKGSRCDIR.libuuid?=		../../wip/libuuid
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.libuuid=libuuid
+BUILDLINK_PREFIX.libuuid_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.libuuid+=	include/uuid/*.h
+BUILDLINK_FILES.libuuid+=	lib/libuuid.*
+
+.include "../../devel/gettext-lib/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	libuuid-buildlink
+
+libuuid-buildlink: _BUILDLINK_USE
+
+.endif	# LIBUUID_BUILDLINK2_MK
