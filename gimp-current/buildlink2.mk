e51693a11448fde0c05dc96e68ec8105949420ff
Soren Jacobsen <snj@NetBSD.org>
2004-01-12 03:45:29 +0000

    Nuke logic to disable the help browser.  Somewhere along the line, gtkhtml was made an unconditional dependency (gtkhtml is used for the help browser now; gnome-libs is old and crusty), so this only removes something that was useless to start out with.  If the consensus is that gtkhtml is too heavy (it requires various GNOME libraries) to be used by default, we can do something about it later.
    Update to 2.0pre1.  Changes:
    * Replaced old "About" dialog [Simon]
    * Allow removal of text attributes from text layer [Sven]
    * Add optimisation option to png (clear transparent pixels) [Joao]
    * Add POSIX shared memory implementation, and use it on MacOS X
      [Yosh]
    * Dashed selection and path stroking [Simon]
    * Grey picker in Levels dialog conserves lightness [Bolsh]
    * Created a library for handling thumbnails [Sven]
    * Support for multipage TIFFs [Andrey Kiselev]
    * Added a channel mixer plug-in [Martin Guldahl, Yosh]
    * PDB cleanup and compatibility mode [Mitch]
    * Cleaned up libgimp API [David Odin]
    * Lots of bug fixes

--- gimp-current/buildlink2.mk
+++ gimp-current/buildlink2.mk
@@ -1,24 +1,30 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/06/30 08:28:20 moubctez Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2004/01/12 03:45:29 blef Exp $
+#
+# This Makefile fragment is included by packages that use gimp-current.
+#
+# This file was created automatically using createbuildlink 2.9.
+#
 
 .if !defined(GIMP_CURRENT_BUILDLINK2_MK)
 GIMP_CURRENT_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gimp-current
-BUILDLINK_DEPENDS.gimp-current?=	gimp-current>=1.3.16
-BUILDLINK_PKGSRCDIR.gimp-current?=	../../wip/gimp-current
+BUILDLINK_DEPENDS.gimp-current?=		gimp-current>=2.0pre1
+BUILDLINK_PKGSRCDIR.gimp-current?=		../../wip/gimp-current
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gimp-current=gimp-current
 BUILDLINK_PREFIX.gimp-current_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.gimp-current+=	include/gimp-1.3/*
-BUILDLINK_FILES.gimp-current+=	lib/libgck*
-BUILDLINK_FILES.gimp-current+=	lib/libgimp*
+BUILDLINK_FILES.gimp-current+=	include/gimp-1.3/libgimp/*.h
+BUILDLINK_FILES.gimp-current+=	lib/libgimp-1.3.*
+BUILDLINK_FILES.gimp-current+=	lib/libgimpbase-1.3.*
+BUILDLINK_FILES.gimp-current+=	lib/libgimpcolor-1.3.*
+BUILDLINK_FILES.gimp-current+=	lib/libgimpmath-1.3.*
+BUILDLINK_FILES.gimp-current+=	lib/libgimpmodule-1.3.*
+BUILDLINK_FILES.gimp-current+=	lib/libgimpthumb-1.3.*
+BUILDLINK_FILES.gimp-current+=	lib/libgimpui-1.3.*
+BUILDLINK_FILES.gimp-current+=	lib/libgimpwidgets-1.3.*
 
-.include "../../devel/gettext-lib/buildlink2.mk"
-.include "../../graphics/jpeg/buildlink2.mk"
-.include "../../graphics/mpeg-lib/buildlink2.mk"
-.include "../../graphics/png/buildlink2.mk"
-.include "../../graphics/tiff/buildlink2.mk"
-.include "../../graphics/libart2/buildlink2.mk"
+.include "../../devel/glib2/buildlink2.mk"
 .include "../../x11/gtk2/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	gimp-current-buildlink
