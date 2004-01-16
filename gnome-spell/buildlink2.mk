fbe5ab5933e0ab1a403814fe40c3997fa62fe028
Min Sik Kim <minskim@NetBSD.org>
2004-01-16 09:32:39 +0000

    Sync with Makefile.

--- gnome-spell/buildlink2.mk
+++ gnome-spell/buildlink2.mk
@@ -1,8 +1,8 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2004/01/16 07:55:17 bseklecki Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2004/01/16 09:32:39 minskim Exp $
 #
 # This Makefile fragment is included by packages that use gnome-spell.
 #
-# This file was created automatically using createbuildlink 2.9.
+# This file was created automatically using createbuildlink-3.0.
 #
 
 .if !defined(GNOME_SPELL_BUILDLINK2_MK)
@@ -15,12 +15,13 @@ BUILDLINK_PKGSRCDIR.gnome-spell?=		../../textproc/gnome-spell
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gnome-spell=gnome-spell
 BUILDLINK_PREFIX.gnome-spell_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.gnome-spell+=	lib/bonobo/servers/GNOME_Spell.server
-BUILDLINK_FILES.gnome-spell+=	share/idl/Spell.idl
 BUILDLINK_FILES.gnome-spell+=	lib/gnome-spell/libgnome-spell-component-0.3.*
 BUILDLINK_FILES.gnome-spell+=	lib/gnome-spell/libgnome-spell-component.*
 
+.include "../../net/ORBit2/buildlink2.mk"
 .include "../../devel/libbonobo/buildlink2.mk"
 .include "../../devel/libbonoboui/buildlink2.mk"
+.include "../../devel/libglade2/buildlink2.mk"
 .include "../../devel/libgnomeui/buildlink2.mk"
 .include "../../textproc/aspell/buildlink2.mk"
 .include "../../x11/gtk2/buildlink2.mk"
