1c2873afb16a77a9fe766b5f2ab430ecb6999a23
Marc Recht <marc@users.sourceforge.net>
2004-01-19 21:08:46 +0000

    update to 0.1.17
    Better text support
    -------------------
    This snapshot provides much better text support by implementing the
    following four functions:
    
            cairo_text_extents
            cairo_glyph_extents
            cairo_text_path
            cairo_glyph_path
    
    The text/glyph_extents functions can be used to determine the bounding
    box (and advance) for text as if drawn by show_text/glyphs.
    
    The text/glyph_path objects functions place text shapes on the current
    path, where they can be subsequently manipulated. For example,
    following these functions with cairo_stroke allows outline text to be
    drawn. Calling cairo_clip allows clipping to a text-shaped region.
    
    Combined dependencies
    ---------------------
    The cairo core now depends only on the libpixman library. This single
    library replaces the three previous libraries libic, libpixregion, and
    slim. Thanks to Dave Beckett <dave.beckett@bristol.ac.uk> for all of
    the heavy lifting with this renaming effort.

--- cairo/buildlink2.mk
+++ cairo/buildlink2.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/11/27 19:08:14 marc Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2004/01/19 21:08:46 marc Exp $
+#
 
 .if !defined(CAIRO_BUILDLINK2_MK)
 CAIRO_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			cairo
-BUILDLINK_DEPENDS.cairo?=		cairo>=0.1.13
+BUILDLINK_DEPENDS.cairo?=		cairo>=0.1.17
 BUILDLINK_PKGSRCDIR.cairo?=		../../wip/cairo
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.cairo=cairo
 BUILDLINK_PREFIX.cairo_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.cairo+=	include/cairo-xlib.h
+BUILDLINK_FILES.cairo+=	include/cairo-features.h
 BUILDLINK_FILES.cairo+=	include/cairo.h
 BUILDLINK_FILES.cairo+=	lib/libcairo.*
 
@@ -17,7 +18,7 @@ BUILDLINK_DEPENDS.freetype2=  freetype2>=2.1.5
 
 .include "../../graphics/freetype2/buildlink2.mk"
 .include "../../fonts/Xft2/buildlink2.mk"
-.include "../../wip/libic/buildlink2.mk"
+.include "../../wip/libpixman/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	cairo-buildlink
 
