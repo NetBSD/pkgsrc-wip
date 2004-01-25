1583f3f2b13b9f64bc1564129d78e1d38cf0b32a
Marc Recht <marc@users.sourceforge.net>
2004-01-25 14:36:24 +0000

    catch-up with the pyxml -> py-xml name change in pkgsrc

--- py-gnosis-utils/buildlink2.mk
+++ py-gnosis-utils/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/07/27 17:24:50 marc Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2004/01/25 14:36:25 marc Exp $
 
 .if !defined(GNOSIS_UTILS_BUILDLINK2_MK)
 GNOSIS_UTILS_BUILDLINK2_MK=	# defined
@@ -12,7 +12,7 @@ BUILDLINK_PKGSRCDIR.Gnosis-Utils?=	../../wip/py-gnosis-utils
 EVAL_PREFIX+=	BUILDLINK_PREFIX.Gnosis-Utils=${PYPKGPREFIX}-Gnosis-Utils
 BUILDLINK_PREFIX.Gnosis-Utils_DEFAULT=	${LOCALBASE}
 
-.include "../../textproc/pyxml/buildlink2.mk"
+.include "../../textproc/py-xml/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	Gnosis-Utils-buildlink
 
