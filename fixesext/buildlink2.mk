7443c9f2f4c428e35a58b286ad3d584814d96c80
Min Sik Kim <minskim@NetBSD.org>
2004-01-31 22:23:35 +0000

    Add dependency on xextensions.

--- fixesext/buildlink2.mk
+++ fixesext/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/12/30 20:07:34 cjep Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2004/01/31 22:23:35 minskim Exp $
 #
 # This Makefile fragment is included by packages that use fixesext.
 #
@@ -16,6 +16,8 @@ EVAL_PREFIX+=	BUILDLINK_PREFIX.fixesext=fixesext
 BUILDLINK_PREFIX.fixesext_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.fixesext+=	include/X11/extensions/xfixesproto.h
 BUILDLINK_FILES.fixesext+=	include/X11/extensions/xfixeswire.h
+
+.include "../../wip/xextensions/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	fixesext-buildlink
 
