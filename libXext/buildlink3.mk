5f0f375f75efa1257781a499a09f7117cb793e1f
Min Sik Kim <minskim@NetBSD.org>
2004-02-01 22:37:36 +0000

    bl3ify

---
+++ libXext/buildlink3.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/01 22:37:36 minskim Exp $
+#
+# This Makefile fragment is included by packages that use libXext.
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBXEXT_BUILDLINK3_MK:=	${LIBXEXT_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libXext
+.endif
+
+.if !empty(LIBXEXT_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			libXext
+BUILDLINK_DEPENDS.libXext?=		libXext>=6.4.1
+BUILDLINK_PKGSRCDIR.libXext?=		../../wip/libXext
+
+.include "../../wip/libX11/buildlink3.mk"
+.include "../../wip/xproto/buildlink3.mk"
+.include "../../wip/xextensions/buildlink3.mk"
+
+.endif # LIBXEXT_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
