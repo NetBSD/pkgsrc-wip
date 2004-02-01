0d89e164e35cac53e54a51e8f011530fa65f1404
Min Sik Kim <minskim@NetBSD.org>
2004-02-01 18:55:48 +0000

    regen

--- libX11/buildlink2.mk
+++ libX11/buildlink2.mk
@@ -1,8 +1,8 @@
-# $NetBSD: buildlink2.mk,v 1.2 2003/11/28 04:54:33 jeremy-c-reed Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2004/02/01 18:55:48 minskim Exp $
 #
 # This Makefile fragment is included by packages that use libX11.
 #
-# This file was created automatically using createbuildlink 2.8.
+# This file was created automatically using createbuildlink-3.0.
 #
 
 .if !defined(LIBX11_BUILDLINK2_MK)
@@ -25,6 +25,11 @@ BUILDLINK_FILES.libX11+=	include/X11/Xutil.h
 BUILDLINK_FILES.libX11+=	include/X11/cursorfont.h
 BUILDLINK_FILES.libX11+=	include/X11/region.h
 BUILDLINK_FILES.libX11+=	lib/libX11.*
+
+.include "../../wip/xextensions/buildlink2.mk"
+.include "../../wip/xproto/buildlink2.mk"
+.include "../../wip/xtrans/buildlink2.mk"
+.include "../../wip/libXau/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	libX11-buildlink
 
