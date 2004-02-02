ccddf1178e6706762c8396f646c82d967396c90f
Min Sik Kim <minskim@NetBSD.org>
2004-02-02 02:23:56 +0000

    Synch with official release.

--- libXfixes/buildlink2.mk
+++ libXfixes/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1 2003/11/27 02:42:06 xtraeme Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2004/02/02 02:23:56 minskim Exp $
 #
 # This Makefile fragment is included by packages that use libXfixes.
 #
@@ -18,10 +18,7 @@ BUILDLINK_FILES.libXfixes+=	include/X11/extensions/Xfixes.h
 BUILDLINK_FILES.libXfixes+=	lib/libXfixes.*
 
 .include "../../wip/libX11/buildlink2.mk"
-.include "../../wip/libXau/buildlink2.mk"
 .include "../../wip/fixesext/buildlink2.mk"
-.include "../../wip/xextensions/buildlink2.mk"
-.include "../../wip/xproto/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	libXfixes-buildlink
 
