7d08408748daee9dff60b55d587cdae9ee2376af
Martti Kuparinen <martti@NetBSD.org>
2004-02-03 09:20:15 +0000

    Linux 2.6.1 headers

---
+++ linux-kernel-headers/PLIST.sh
@@ -0,0 +1,139 @@
+@comment $NetBSD: PLIST.sh,v 1.1 2004/02/03 09:20:16 marttikuparinen Exp $
+include/asm/addrspace.h
+include/asm/adx/io.h
+include/asm/bigsur.h
+include/asm/bigsur/bigsur.h
+include/asm/bigsur/io.h
+include/asm/bigsur/serial.h
+include/asm/cat68701/io.h
+include/asm/cpu-sh2/cache.h
+include/asm/cpu-sh2/cacheflush.h
+include/asm/cpu-sh2/watchdog.h
+include/asm/cpu-sh3/addrspace.h
+include/asm/cpu-sh3/cache.h
+include/asm/cpu-sh3/cacheflush.h
+include/asm/cpu-sh3/dma.h
+include/asm/cpu-sh3/freq.h
+include/asm/cpu-sh3/mmu_context.h
+include/asm/cpu-sh3/rtc.h
+include/asm/cpu-sh3/shmparam.h
+include/asm/cpu-sh3/ubc.h
+include/asm/cpu-sh3/watchdog.h
+include/asm/cpu-sh4/addrspace.h
+include/asm/cpu-sh4/cache.h
+include/asm/cpu-sh4/cacheflush.h
+include/asm/cpu-sh4/dma.h
+include/asm/cpu-sh4/freq.h
+include/asm/cpu-sh4/mmu_context.h
+include/asm/cpu-sh4/rtc.h
+include/asm/cpu-sh4/shmparam.h
+include/asm/cpu-sh4/ubc.h
+include/asm/cpu-sh4/watchdog.h
+include/asm/cqreek/cqreek.h
+include/asm/dc_sysasic.h
+include/asm/dmida/io.h
+include/asm/dreamcast/io.h
+include/asm/dreamcast/sysasic.h
+include/asm/ec3104.h
+include/asm/ec3104/ec3104.h
+include/asm/ec3104/io.h
+include/asm/ec3104/keyboard.h
+include/asm/ec3104/serial.h
+include/asm/floppy.h
+include/asm/freq.h
+include/asm/harp/harp.h
+include/asm/harp/io.h
+include/asm/hd64461.h
+include/asm/hd64461/hd64461.h
+include/asm/hd64461/io.h
+include/asm/hd64465.h
+include/asm/hd64465/gpio.h
+include/asm/hd64465/hd64465.h
+include/asm/hd64465/io.h
+include/asm/hd64465_gpio.h
+include/asm/hdreg.h
+include/asm/hitachi_7751se.h
+include/asm/hitachi_se.h
+include/asm/hp6xx/io.h
+include/asm/hw_irq.h
+include/asm/ide.h
+include/asm/init.h
+include/asm/io_7751se.h
+include/asm/io_adx.h
+include/asm/io_bigsur.h
+include/asm/io_cat68701.h
+include/asm/io_dc.h
+include/asm/io_ec3104.h
+include/asm/io_generic.h
+include/asm/io_hd64461.h
+include/asm/io_hd64465.h
+include/asm/io_se.h
+include/asm/io_sh2000.h
+include/asm/io_unknown.h
+include/asm/ipc.h
+include/asm/keyboard-ec3104.h
+include/asm/keyboard.h
+include/asm/kgdb.h
+include/asm/machvec.h
+include/asm/machvec_init.h
+include/asm/mc146818rtc.h
+include/asm/mmzone.h
+include/asm/mpc1211/dma.h
+include/asm/mpc1211/io.h
+include/asm/mpc1211/keyboard.h
+include/asm/mpc1211/m1543c.h
+include/asm/mpc1211/mc146818rtc.h
+include/asm/mpc1211/mpc1211.h
+include/asm/mpc1211/pci.h
+include/asm/numnodes.h
+include/asm/overdrive/fpga.h
+include/asm/overdrive/gt64111.h
+include/asm/overdrive/io.h
+include/asm/overdrive/overdrive.h
+include/asm/pci-sh7751.h
+include/asm/pgtable-2level.h
+include/asm/processor.h
+include/asm/ptrace.h
+include/asm/rwsem.h
+include/asm/saturn/io.h
+include/asm/saturn/smpc.h
+include/asm/se/io.h
+include/asm/se/se.h
+include/asm/se/smc37c93x.h
+include/asm/se7751/io.h
+include/asm/se7751/se7751.h
+include/asm/semaphore-helper.h
+include/asm/serial-bigsur.h
+include/asm/serial-ec3104.h
+include/asm/serial.h
+include/asm/sh2000/io.h
+include/asm/sh2000/sh2000.h
+include/asm/sh_bios.h
+include/asm/sigcontext.h
+include/asm/smc37c93x.h
+include/asm/smp.h
+include/asm/spinlock.h
+include/asm/system.h
+include/asm/ubc.h
+include/asm/ucontext.h
+include/asm/watchdog.h
+@dirrm include/asm/adx
+@dirrm include/asm/bigsur
+@dirrm include/asm/cat68701
+@dirrm include/asm/cpu-sh2
+@dirrm include/asm/cpu-sh3
+@dirrm include/asm/cpu-sh4
+@dirrm include/asm/cqreek
+@dirrm include/asm/dmida
+@dirrm include/asm/dreamcast
+@dirrm include/asm/ec3104
+@dirrm include/asm/harp
+@dirrm include/asm/hd64461
+@dirrm include/asm/hd64465
+@dirrm include/asm/hp6xx
+@dirrm include/asm/mpc1211
+@dirrm include/asm/overdrive
+@dirrm include/asm/saturn
+@dirrm include/asm/se
+@dirrm include/asm/se7751
+@dirrm include/asm/sh2000
