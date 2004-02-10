821158733c0e6a1a42bed2e5a32b41b82bcd224a
Jeremy C. Reed <reed@reedmedia.net>
2004-02-10 18:37:35 +0000

    Change master site (for official release). The version is same, but file is different, so update checksum in distinfo.
    Update HOMEPAGE and improve COMMENT.
    
    Include libXext/buildlink2.mk in Makefile and in buildlink2.mk file.
    
    Bump PKGREVISION.

--- libXres/buildlink2.mk
+++ libXres/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/11/27 04:16:03 xtraeme Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2004/02/10 18:37:35 jeremy-c-reed Exp $
 #
 # This Makefile fragment is included by packages that use libXres.
 #
@@ -9,7 +9,7 @@
 LIBXRES_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			libXres
-BUILDLINK_DEPENDS.libXres?=		libXres>=1.0.1
+BUILDLINK_DEPENDS.libXres?=		libXres>=1.0.1nb1
 BUILDLINK_PKGSRCDIR.libXres?=		../../wip/libXres
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libXres=libXres
@@ -19,6 +19,7 @@ BUILDLINK_FILES.libXres+=	lib/libXRes.*
 
 .include "../../wip/libX11/buildlink2.mk"
 .include "../../wip/libXau/buildlink2.mk"
+.include "../../wip/libXext/buildlink2.mk"
 .include "../../wip/xproto/buildlink2.mk"
 .include "../../wip/xextensions/buildlink2.mk"
 .include "../../wip/resourceext/buildlink2.mk"
