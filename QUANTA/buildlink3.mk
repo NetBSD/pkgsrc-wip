a39a5e887c09229f9fb4699c8de8a221b87195ef
Bruce J.A. Nourish <bjan2@users.sourceforge.net>
2004-02-11 04:49:35 +0000

    Part of previous commit.

---
+++ QUANTA/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/11 04:49:35 bjan2 Exp $
+#
+# This Makefile fragment is included by packages that use QUANTA.
+#
+# This file was created automatically using createbuildlink-3.1.
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+QUANTA_BUILDLINK3_MK:=	${QUANTA_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	QUANTA
+.endif
+
+.if !empty(QUANTA_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			QUANTA
+BUILDLINK_DEPENDS.QUANTA+=		QUANTA>=0.3
+BUILDLINK_PKGSRCDIR.QUANTA?=		../../wip/QUANTA
+
+.endif # QUANTA_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
