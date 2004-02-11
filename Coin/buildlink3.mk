a74af204b91452c1fb2c9813261512aae808c8d9
Bruce J.A. Nourish <bjan2@users.sourceforge.net>
2004-02-11 05:19:35 +0000

    Minor fixes and buildlink3 support.

---
+++ Coin/buildlink3.mk
@@ -0,0 +1,26 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/11 05:19:35 bjan2 Exp $
+#
+# This Makefile fragment is included by packages that use Coin.
+#
+# This file was created automatically using createbuildlink-3.1.
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+COIN_BUILDLINK3_MK:=	${COIN_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	Coin
+.endif
+
+.if !empty(COIN_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			Coin
+BUILDLINK_DEPENDS.Coin+=		Coin>=2.2.1
+BUILDLINK_PKGSRCDIR.Coin?=		../../wip/Coin
+
+.include "../../wip/simage/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+
+.endif # COIN_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
