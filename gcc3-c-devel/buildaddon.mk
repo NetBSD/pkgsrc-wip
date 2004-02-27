322fa862a7ed0980cf5413bf2c140993204d72b8
Min Sik Kim <minskim@NetBSD.org>
2004-02-27 03:20:08 +0000

    gcc3-c-devel is still in wip.

--- gcc3-c-devel/buildaddon.mk
+++ gcc3-c-devel/buildaddon.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildaddon.mk,v 1.2 2003/12/14 22:39:20 blef Exp $
+# $NetBSD: buildaddon.mk,v 1.3 2004/02/27 03:20:08 minskim Exp $
 
 .include "../../wip/gcc3-c-devel/Makefile.common"
 
@@ -8,7 +8,7 @@ pre-build:
 	${LN} -sf ${GCC_PREFIX}/bin/gcc ${WRKSRC}/gcc/xgcc
 
 BUILDLINK_DEPENDS.gcc34c?=	gcc3${GCC3_PKGMODIF}-c-3.4-devel
-BUILDLINK_PKGSRCDIR.gcc34c?=	../../lang/gcc3-c-devel
+BUILDLINK_PKGSRCDIR.gcc34c?=	../../wip/gcc3-c-devel
 
 BUILDLINK_PREFIX.gcc34c=	${LOCALBASE}
 
