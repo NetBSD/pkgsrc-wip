eace736270ba59670c390905ba29a152a4426830
Min Sik Kim <minskim@NetBSD.org>
2004-02-27 05:16:07 +0000

    bl3ify

---
+++ libXcursor/buildlink3.mk
@@ -0,0 +1,27 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/27 05:16:07 minskim Exp $
+#
+# This Makefile fragment is included by packages that use libXcursor.
+#
+# This file was created automatically using createbuildlink-3.1.
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBXCURSOR_BUILDLINK3_MK:=	${LIBXCURSOR_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libXcursor
+.endif
+
+.if !empty(LIBXCURSOR_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			libXcursor
+BUILDLINK_DEPENDS.libXcursor+=		libXcursor>=1.1.1
+BUILDLINK_PKGSRCDIR.libXcursor?=		../../wip/libXcursor
+
+.include "../../wip/xproto/buildlink3.mk"
+.include "../../wip/libX11/buildlink3.mk"
+.include "../../wip/libXrender/buildlink3.mk"
+.include "../../wip/libXfixes/buildlink3.mk"
+
+.endif # LIBXCURSOR_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
