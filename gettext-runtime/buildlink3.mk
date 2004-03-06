21c9e686777d3139e7f280b505d13f99d38fb6c5
Jeremy C. Reed <reed@reedmedia.net>
2004-03-06 07:24:48 +0000

    Use "wip" directory.

--- gettext-runtime/buildlink3.mk
+++ gettext-runtime/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/03/06 07:20:11 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/03/06 07:24:48 jeremy-c-reed Exp $
 #
 # This file was created automatically using createbuildlink-3.2.
 
@@ -14,7 +14,7 @@ BUILDLINK_PACKAGES+=	gettext-runtime
 
 .if !empty(GETTEXT_RUNTIME_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gettext-runtime+=	gettext-runtime>=0.14.1
-BUILDLINK_PKGSRCDIR.gettext-runtime?=	../../devel/gettext-runtime
+BUILDLINK_PKGSRCDIR.gettext-runtime?=	../../wip/gettext-runtime
 
 .endif	# GETTEXT_RUNTIME_BUILDLINK3_MK
 
