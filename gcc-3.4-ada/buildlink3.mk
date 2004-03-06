bd3cdce8432c528d6a6ec7c266984f28c1ae1a15
John R. Shannon <johnrshannon@users.sourceforge.net>
2004-03-06 08:35:03 +0000

    1) Revised buildlink3.mk file to reflect prototype structure in    bsd.buildlink3.mk,v 1.102 2004/03/05 2) Renamed pkg to gccAda-3.4 because gcc-3.4-ada cuaused problems    with pkg_admin

--- gcc-3.4-ada/buildlink3.mk
+++ gcc-3.4-ada/buildlink3.mk
@@ -1,35 +1,35 @@
-# $NetBSD: buildlink3.mk,v 1.5 2004/03/03 22:39:58 johnrshannon Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2004/03/06 08:35:03 johnrshannon Exp $
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
-GCC-3.4-ADA_BUILDLINK3_MK:=	${GCC-3.4-ADA_BUILDLINK3_MK}+
-
-.include "../../mk/bsd.prefs.mk"
-
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH}+
+GCC-3.4-ADA_BUILDLINK3_MK:=   ${GCC-3.4-ADA_BUILDLINK3_MK}+
+ 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	gcc-3.4-ada
+BUILDLINK_DEPENDS+=   gccAda-3.4
 .endif
-
+ 
+BUILDLINK_PACKAGES:=  ${BUILDLINK_PACKAGES:NgccAda-3.4}
+BUILDLINK_PACKAGES+=  gccAda-3.4
+ 
 .if !empty(GCC-3.4-ADA_BUILDLINK3_MK:M+)
 .  if defined(GCC34_INSTALLTO_SUBPREFIX)
-.    if ${GCC34_INSTALLTO_SUBPREFIX} != "gcc-3.4-ada"
+.    if ${GCC34_INSTALLTO_SUBPREFIX} != "gccAda-3.4"
 GCC34_PKGMODIF=			_${GCC34_INSTALLTO_SUBPREFIX}
 .    endif
 .  endif
-BUILDLINK_PACKAGES+=		gcc-3.4-ada
-BUILDLINK_DEPENDS.gcc-3.4-ada+=	gcc-3.4-ada
-BUILDLINK_PKGSRCDIR.gcc-3.4-ada?=	../../wip/gcc-3.4-ada
-BUILDLINK_LIBDIRS.gcc-3.4-ada?=	\
-	lib ${_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.gcc-3.4-ada}\///}
-BUILDLINK_CONTENTS_FILTER.gcc-3.4-ada= \
+BUILDLINK_DEPENDS.gccAda-3.4+=       gccAda-3.4
+BUILDLINK_PKGSRCDIR.gccAda-3.4?=     ../../wip/gcc-3.4-ada
+BUILDLINK_CONTENTS_FILTER.gccAda-3.4= \
 	${EGREP} '(gnat1|bin.*/|include.*/|\.h$$|\.pc$$|lib.*/lib[^/]*$$)'
-
+ 
 # Packages that link against shared libraries need a full dependency.
 .  if defined(USE_GCC_SHLIB)
-BUILDLINK_DEPMETHOD.gcc-3.4-ada+=	full
+BUILDLINK_DEPMETHOD.gccAda-3.4+=	full
 .  else
-BUILDLINK_DEPMETHOD.gcc-3.4-ada?=	build
+BUILDLINK_DEPMETHOD.gccAda-3.4?=	build
 .  endif
 
-.endif	# GCC-3.4-ADA_BUILDLINK3_MK
-
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
+.include "../../mk/pthread.buildlink3.mk"
+ 
+.endif  # GCC-3.4-ADA_BUILDLINK3_MK
+ 
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
