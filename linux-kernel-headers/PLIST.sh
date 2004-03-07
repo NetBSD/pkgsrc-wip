9077dee37b2418ab0a759b322be8b6b81e5be6fb
Jeremy C. Reed <reed@reedmedia.net>
2004-03-07 04:02:20 +0000

    Linux kernel is newer version so some slight PLIST changes, for example: include/asm/segment.h and include/linux/dmapool.h now are included for all architectures.

--- linux-kernel-headers/PLIST.sh
+++ linux-kernel-headers/PLIST.sh
@@ -1,13 +1,17 @@
-@comment $NetBSD: PLIST.sh,v 1.1 2004/02/03 09:20:16 marttikuparinen Exp $
+@comment $NetBSD: PLIST.sh,v 1.2 2004/03/07 04:02:20 jeremy-c-reed Exp $
 include/asm/addrspace.h
 include/asm/adx/io.h
-include/asm/bigsur.h
 include/asm/bigsur/bigsur.h
 include/asm/bigsur/io.h
 include/asm/bigsur/serial.h
 include/asm/cat68701/io.h
+include/asm/cpu-sh2/addrspace.h
 include/asm/cpu-sh2/cache.h
 include/asm/cpu-sh2/cacheflush.h
+include/asm/cpu-sh2/dma.h
+include/asm/cpu-sh2/shmparam.h
+include/asm/cpu-sh2/sigcontext.h
+include/asm/cpu-sh2/ubc.h
 include/asm/cpu-sh2/watchdog.h
 include/asm/cpu-sh3/addrspace.h
 include/asm/cpu-sh3/cache.h
@@ -17,6 +21,7 @@ include/asm/cpu-sh3/freq.h
 include/asm/cpu-sh3/mmu_context.h
 include/asm/cpu-sh3/rtc.h
 include/asm/cpu-sh3/shmparam.h
+include/asm/cpu-sh3/sigcontext.h
 include/asm/cpu-sh3/ubc.h
 include/asm/cpu-sh3/watchdog.h
 include/asm/cpu-sh4/addrspace.h
@@ -27,51 +32,37 @@ include/asm/cpu-sh4/freq.h
 include/asm/cpu-sh4/mmu_context.h
 include/asm/cpu-sh4/rtc.h
 include/asm/cpu-sh4/shmparam.h
+include/asm/cpu-sh4/sigcontext.h
+include/asm/cpu-sh4/sq.h
 include/asm/cpu-sh4/ubc.h
 include/asm/cpu-sh4/watchdog.h
 include/asm/cqreek/cqreek.h
-include/asm/dc_sysasic.h
 include/asm/dmida/io.h
+include/asm/dreamcast/dma.h
 include/asm/dreamcast/io.h
+include/asm/dreamcast/pci.h
 include/asm/dreamcast/sysasic.h
-include/asm/ec3104.h
 include/asm/ec3104/ec3104.h
 include/asm/ec3104/io.h
 include/asm/ec3104/keyboard.h
 include/asm/ec3104/serial.h
+include/asm/flat.h
 include/asm/floppy.h
 include/asm/freq.h
 include/asm/harp/harp.h
 include/asm/harp/io.h
-include/asm/hd64461.h
 include/asm/hd64461/hd64461.h
 include/asm/hd64461/io.h
-include/asm/hd64465.h
 include/asm/hd64465/gpio.h
 include/asm/hd64465/hd64465.h
 include/asm/hd64465/io.h
-include/asm/hd64465_gpio.h
 include/asm/hdreg.h
-include/asm/hitachi_7751se.h
-include/asm/hitachi_se.h
 include/asm/hp6xx/io.h
 include/asm/hw_irq.h
 include/asm/ide.h
 include/asm/init.h
-include/asm/io_7751se.h
-include/asm/io_adx.h
-include/asm/io_bigsur.h
-include/asm/io_cat68701.h
-include/asm/io_dc.h
-include/asm/io_ec3104.h
 include/asm/io_generic.h
-include/asm/io_hd64461.h
-include/asm/io_hd64465.h
-include/asm/io_se.h
-include/asm/io_sh2000.h
-include/asm/io_unknown.h
 include/asm/ipc.h
-include/asm/keyboard-ec3104.h
 include/asm/keyboard.h
 include/asm/kgdb.h
 include/asm/machvec.h
@@ -90,7 +81,6 @@ include/asm/overdrive/fpga.h
 include/asm/overdrive/gt64111.h
 include/asm/overdrive/io.h
 include/asm/overdrive/overdrive.h
-include/asm/pci-sh7751.h
 include/asm/pgtable-2level.h
 include/asm/processor.h
 include/asm/ptrace.h
@@ -103,17 +93,17 @@ include/asm/se/smc37c93x.h
 include/asm/se7751/io.h
 include/asm/se7751/se7751.h
 include/asm/semaphore-helper.h
-include/asm/serial-bigsur.h
-include/asm/serial-ec3104.h
 include/asm/serial.h
 include/asm/sh2000/io.h
 include/asm/sh2000/sh2000.h
 include/asm/sh_bios.h
 include/asm/sigcontext.h
-include/asm/smc37c93x.h
 include/asm/smp.h
+include/asm/snapgear/io.h
 include/asm/spinlock.h
 include/asm/system.h
+include/asm/systemh/7751systemh.h
+include/asm/systemh/io.h
 include/asm/ubc.h
 include/asm/ucontext.h
 include/asm/watchdog.h
@@ -137,3 +127,5 @@ include/asm/watchdog.h
 @dirrm include/asm/se
 @dirrm include/asm/se7751
 @dirrm include/asm/sh2000
+@dirrm include/asm/snapgear
+@dirrm include/asm/systemh
