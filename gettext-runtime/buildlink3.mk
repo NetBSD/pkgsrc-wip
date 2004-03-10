a1b02a4256473e4068c37e22ef49daaea0e74f3f
Jeremy C. Reed <reed@reedmedia.net>
2004-03-10 05:46:30 +0000

    This should use iconv, so include it. (libiconv should be smarter so it won't install or required dependency on glibc systems.)

--- gettext-runtime/buildlink3.mk
+++ gettext-runtime/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/06 07:24:48 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/03/10 05:46:30 jeremy-c-reed Exp $
 #
 # This file was created automatically using createbuildlink-3.2.
 
@@ -15,6 +15,9 @@ BUILDLINK_PACKAGES+=	gettext-runtime
 .if !empty(GETTEXT_RUNTIME_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gettext-runtime+=	gettext-runtime>=0.14.1
 BUILDLINK_PKGSRCDIR.gettext-runtime?=	../../wip/gettext-runtime
+
+# XXX need to only depend on GNU libiconv on non-glibc systems
+.include "../../converters/libiconv/buildlink3.mk"
 
 .endif	# GETTEXT_RUNTIME_BUILDLINK3_MK
 
