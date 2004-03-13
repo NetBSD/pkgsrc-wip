8e3a9f01d3511752b1a01f588777b9b20a782ef8
Min Sik Kim <minskim@NetBSD.org>
2004-03-13 20:44:56 +0000

    Remove unnecessary whitespaces.

--- gcc-3.4-ada/buildlink3.mk
+++ gcc-3.4-ada/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/03/06 08:35:03 johnrshannon Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2004/03/13 20:44:56 minskim Exp $
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH}+
 GCC-3.4-ADA_BUILDLINK3_MK:=   ${GCC-3.4-ADA_BUILDLINK3_MK}+
- 
+
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=   gccAda-3.4
 .endif
- 
+
 BUILDLINK_PACKAGES:=  ${BUILDLINK_PACKAGES:NgccAda-3.4}
 BUILDLINK_PACKAGES+=  gccAda-3.4
- 
+
 .if !empty(GCC-3.4-ADA_BUILDLINK3_MK:M+)
 .  if defined(GCC34_INSTALLTO_SUBPREFIX)
 .    if ${GCC34_INSTALLTO_SUBPREFIX} != "gccAda-3.4"
@@ -20,7 +20,7 @@ BUILDLINK_DEPENDS.gccAda-3.4+=       gccAda-3.4
 BUILDLINK_PKGSRCDIR.gccAda-3.4?=     ../../wip/gcc-3.4-ada
 BUILDLINK_CONTENTS_FILTER.gccAda-3.4= \
 	${EGREP} '(gnat1|bin.*/|include.*/|\.h$$|\.pc$$|lib.*/lib[^/]*$$)'
- 
+
 # Packages that link against shared libraries need a full dependency.
 .  if defined(USE_GCC_SHLIB)
 BUILDLINK_DEPMETHOD.gccAda-3.4+=	full
@@ -29,7 +29,7 @@ BUILDLINK_DEPMETHOD.gccAda-3.4?=	build
 .  endif
 
 .include "../../mk/pthread.buildlink3.mk"
- 
+
 .endif  # GCC-3.4-ADA_BUILDLINK3_MK
- 
+
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
