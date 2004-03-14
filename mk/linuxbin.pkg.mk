42049060107a659ddcd1efc9b3ccc23667e77ead
Michal Pasternak <mpasternak@users.sourceforge.net>
2004-03-14 19:56:10 +0000

    Fix a typo

--- mk/linuxbin.pkg.mk
+++ mk/linuxbin.pkg.mk
@@ -1,7 +1,7 @@
-# $NetBSD: linuxbin.pkg.mk,v 1.11 2004/03/14 19:50:56 mpasternak Exp $
+# $NetBSD: linuxbin.pkg.mk,v 1.12 2004/03/14 19:56:10 mpasternak Exp $
 ###########################################################################
 # 
-# $Id: linuxbin.pkg.mk,v 1.11 2004/03/14 19:50:56 mpasternak Exp $
+# $Id: linuxbin.pkg.mk,v 1.12 2004/03/14 19:56:10 mpasternak Exp $
 #
 # Proposal: how should we deal with Linux binary packages packages
 #
@@ -261,8 +261,8 @@ do-install: warn-on-freebsd
 	cd ${WRKSRC} && ${PAX} -rw -pe * ${EMULDIR}
 .if !defined(SLACK_NO_INSTALL)
 	chroot ${EMULDIR} bin/bash install/doinst.sh
-.endif
 	${RM} -rf ${EMULDIR}/install
+.endif
 .endif
 .else
 .error "Please add support for this kind of package!"
