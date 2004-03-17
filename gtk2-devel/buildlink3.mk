715167a4894ec1f0778b3d64ad7d51c35d8048ff
Michal Pasternak <mpasternak@users.sourceforge.net>
2004-03-17 20:02:42 +0000

    Correct bl3 file

--- gtk2-devel/buildlink3.mk
+++ gtk2-devel/buildlink3.mk
@@ -1,23 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/03/17 11:48:56 mpasternak Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.2.
-# XXX After this file as been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink[23].mk files.
+# $NetBSD: buildlink3.mk,v 1.2 2004/03/17 20:02:42 mpasternak Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-GTK+_BUILDLINK3_MK:=	${GTK+_BUILDLINK3_MK}+
+GTK2_BUILDLINK3_MK:=	${GTK2_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	gtk+
+BUILDLINK_DEPENDS+=	gtk2
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngtk+}
-BUILDLINK_PACKAGES+=	gtk+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngtk2+}
+BUILDLINK_PACKAGES+=	gtk2
 
-.if !empty(GTK+_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gtk++=	gtk+>=2.4.0
-BUILDLINK_PKGSRCDIR.gtk+?=	../../wip/gtk2-devel
+.if !empty(GTK2_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.gtk2+=	gtk2+>=2.4.0
+BUILDLINK_PKGSRCDIR.gtk2?=	../../wip/gtk2-devel
 
 .include "../../wip/pango-devel/buildlink3.mk"
 .include "../../wip/atk-devel/buildlink3.mk"
@@ -28,6 +23,6 @@ BUILDLINK_PKGSRCDIR.gtk+?=	../../wip/gtk2-devel
 .include "../../graphics/png/buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
 
-.endif	# GTK+_BUILDLINK3_MK
+.endif	# GTK2_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
