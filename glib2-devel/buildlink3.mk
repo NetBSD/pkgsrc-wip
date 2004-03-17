f2008819b90e29c78b58a7326fecdfb70d7ea405
Michal Pasternak <mpasternak@users.sourceforge.net>
2004-03-17 13:55:08 +0000

    Correct bl3, NetBSD-current build confirmed (in TODO), add gnu iconv dep on non-NetBSD only

--- glib2-devel/buildlink3.mk
+++ glib2-devel/buildlink3.mk
@@ -1,11 +1,6 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/17 12:03:07 mpasternak Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.3.
-# XXX After this file as been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink[23].mk files.
+# $NetBSD: buildlink3.mk,v 1.3 2004/03/17 13:55:08 mpasternak Exp $
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GLIB2_BUILDLINK3_MK:=	${GLIB2_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
@@ -19,24 +14,8 @@ BUILDLINK_PACKAGES+=	glib2
 BUILDLINK_DEPENDS.glib2+=	glib2>=2.4.0
 BUILDLINK_PKGSRCDIR.glib2?=	../../wip/glib2-devel
 
-.include "../../mk/bsd.prefs.mk"
-
-.ifdef GLIB_USE_GNU_ICONV
-_BUILDLINK_GLIB_USE_GNU_ICONV=		${GLIB_USE_GNU_ICONV}
-.else
-_BUILDLINK_GLIB_USE_GNU_ICONV=		no
-.endif
-
-.if ${OPSYS} == "FreeBSD"
-_BUILDLINK_GLIB_USE_GNU_ICONV=		no
-.endif
-
-.if ${_BUILDLINK_GLIB_USE_GNU_ICONV} == [Nn][Oo]
-CONFIGURE_ARGS+=	--with-libiconv=native
-.else
-CONFIGURE_ARGS+=	--with-libiconv=gnu
+.include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
-.endif
 
 .endif	# GLIB2_BUILDLINK3_MK
 
