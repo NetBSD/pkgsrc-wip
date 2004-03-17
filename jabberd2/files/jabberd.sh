7d9286846232488505f5e20b1f080b8519b030e2
Juan Romero Pardines <xtraeme@gmail.com>
2004-03-17 02:19:12 +0000

    Finish jabberd package doing the following things:
        o Use BUILDLINK_TRANSFORM to fix path for db4 headers.
        o Fix typos of previous commit.
        o Add rc.d scripts for all jabberd services (c2s, router, etc).
        o Minor cleanups to Makefile.
        o Remove pidfiles via stop_postcmd.
    
    I think it's ready for pkgsrc, I'll import it after the freeze.

--- jabberd2/files/jabberd.sh
+++ jabberd2/files/jabberd.sh
@@ -1,19 +1,54 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: jabberd.sh,v 1.6 2004/02/12 06:14:58 xtraeme Exp $
+# $NetBSD: jabberd.sh,v 1.7 2004/03/17 02:19:12 xtraeme Exp $
 #
+# KEYWORD: nostart
 
-# PROVIDE: jabberd
-# REQUIRE: DAEMON LOGIN
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
+fi
 
-. /etc/rc.subr
+rcd_dir=`@DIRNAME@ $0`
+
+# NOTE: run_rc_command sets $rc_arg
+#
+forward_commands()
+{
+	# Backward compat with NetBSD <1.6:
+	[ -z "$rc_arg" ] && rc_arg=$_arg
+
+	for file in $COMMAND_LIST; do
+		$rcd_dir/$file $rc_arg
+	done
+}
+
+reverse_commands()
+{
+	# Backward compat with NetBSD <1.6:
+	[ -z "$rc_arg" ] && rc_arg=$_arg
+
+	REVCOMMAND_LIST=
+	for file in $COMMAND_LIST; do
+		REVCOMMAND_LIST="$file $REVCOMMAND_LIST"
+	done
+	for file in $REVCOMMAND_LIST; do
+		$rcd_dir/$file $rc_arg
+	done
+}
+
+COMMAND_LIST="c2s resolver router s2s sm"
 
 name="jabberd"
-rcvar=$name
-command="@PREFIX@/bin/${name}"
-command_args="-D"
-required_files="@PKG_SYSCONFDIR@/jabberd.cfg"
-jabberd_user="@JABBERD_USER@"
+start_cmd="forward_commands"
+stop_cmd="reverse_commands"
+reload_cmd="forward_commands"
+status_cmd="forward_commands"
+extra_commands="reload status"
 
-load_rc_config $name
-run_rc_command "$1"
+if [ -f /etc/rc.subr ]; then
+	run_rc_command "$1"
+else
+	@ECHO@ -n " ${name}"
+	_arg="$1"
+	${start_cmd}
+fi
