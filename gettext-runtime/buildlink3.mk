57b31dcfe65dd106157093b048242b4dddb7bdb2
Jeremy C. Reed <reed@reedmedia.net>
2004-03-18 15:36:49 +0000

    Update based on gettext-libs/buildlink3.mk.

--- gettext-runtime/buildlink3.mk
+++ gettext-runtime/buildlink3.mk
@@ -1,24 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/03/10 05:46:30 jeremy-c-reed Exp $
-#
-# This file was created automatically using createbuildlink-3.2.
+# $NetBSD: buildlink3.mk,v 1.4 2004/03/18 15:36:49 jeremy-c-reed Exp $
 
-BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-GETTEXT_RUNTIME_BUILDLINK3_MK:=	${GETTEXT_RUNTIME_BUILDLINK3_MK}+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GETTEXT_BUILDLINK3_MK:=	${GETTEXT_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	gettext-runtime
+BUILDLINK_DEPENDS+=	gettext
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngettext-runtime}
-BUILDLINK_PACKAGES+=	gettext-runtime
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngettext}
+BUILDLINK_PACKAGES+=	gettext
 
-.if !empty(GETTEXT_RUNTIME_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gettext-runtime+=	gettext-runtime>=0.14.1
-BUILDLINK_PKGSRCDIR.gettext-runtime?=	../../wip/gettext-runtime
+.if !empty(GETTEXT_BUILDLINK3_MK:M+)
+BUILDLINK_PKGBASE.gettext?=	gettext-runtime
+BUILDLINK_DEPENDS.gettext+=	gettext-runtime>=0.14.1
+BUILDLINK_PKGSRCDIR.gettext?=	../../wip/gettext-runtime
+.endif	# GETTEXT_BUILDLINK3_MK
 
-# XXX need to only depend on GNU libiconv on non-glibc systems
-.include "../../converters/libiconv/buildlink3.mk"
+# libiconv/buildlink3.mk is included by gettext-lib/builtin.mk
+#.include "../../converters/libiconv/buildlink3.mk"
 
-.endif	# GETTEXT_RUNTIME_BUILDLINK3_MK
-
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
