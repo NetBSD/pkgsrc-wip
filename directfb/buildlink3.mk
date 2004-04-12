1a5feac471e97cdfa8b706fbb65954ec4789485a
Soren Jacobsen <snj@NetBSD.org>
2004-04-12 02:44:55 +0000

    Convert to buildlink3.

---
+++ directfb/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/04/12 02:47:02 blef Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+DIRECTFB_BUILDLINK3_MK:=	${DIRECTFB_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	DirectFB
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NDirectFB}
+BUILDLINK_PACKAGES+=	DirectFB
+
+.if !empty(DIRECTFB_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.DirectFB+=	DirectFB>=0.9.17
+BUILDLINK_PKGSRCDIR.DirectFB?=	../../wip/directfb
+.endif	# DIRECTFB_BUILDLINK3_MK
+
+.include "../../devel/SDL/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
