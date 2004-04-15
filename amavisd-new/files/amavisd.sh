a317d0210c53adc648d7134ab81228d94016d913
Marc Recht <marc@users.sourceforge.net>
2004-04-15 19:02:04 +0000

    Bring the script closer to rc.d style. Note: Maybe this script should be splitted in two parts: 1. amavisd 2. amavisd-milter

--- amavisd-new/files/amavisd.sh
+++ amavisd-new/files/amavisd.sh
@@ -1,36 +1,68 @@
-#!@SH@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: amavisd.sh,v 1.2 2004/04/15 19:02:04 marc Exp $
+#
+# PROVIDE: amavisd
+# REQUIRE: DAEMON
+#
+# To start spread at startup, copy this script to /etc/rc.d and set
+# spread=YES in /etc/rc.conf.
+#
 
-if ! PREFIX=$(expr $0 : "\(/.*\)/etc/rc\.d/$(basename $0)\$"); then
-	echo "$0: Cannot determine the PREFIX" >&2
-	exit 1
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
 fi
 
-DIR=@AMAVISDIR@
-PIDFILE=$DIR/amavisd.pid
-SOCKFILES=$DIR/amavis*.sock
-MILTERFLAG=$DIR/use.milter
+rcd_dir=`@DIRNAME@ $0`
 
-case "$1" in
-start)
-	if [ -e ${PREFIX}/etc/amavisd.conf ]; then
-		rm -rf ${SOCKFILES}
-		if [ -e $MILTERFLAG ]; then
-			su @AMAVISUSER@ -c "${PREFIX}/sbin/amavis-milter -D -p $DIR/amavis-milter.sock"
-		fi
-		su @AMAVISUSER@ -c "${PREFIX}/sbin/amavisd > /dev/null 2>&1" && echo -n ' amavisd'
-	fi
-	;;
-stop)
-	if [ -e $MILTERFLAG ]; then
-		killall -u @AMAVISUSER@ amavis-milter >/dev/null 2>&1
-	fi
-	if [ -e $PIDFILE ]; then
-		su @AMAVISUSER@ -c "kill `cat $PIDFILE`" && echo -n ' amavisd'
-	fi
-	;;
-*)
-	echo "Usage: `basename $0` {start|stop}" >&2
-	;;
-esac
+name="amavisd"
+rcvar=$name
+command="@PREFIX@/sbin/${name}"
+command_interpreter="@PERL5@"
+pidfile="@AMAVISDIR@/amavisd.pid"
+required_files="@PKG_SYSCONFDIR@/${name}.conf"
+required_dirs="@AMAVISDIR@"
 
-exit 0
+start_cmd="amavisd_doit start"
+stop_cmd="amavisd_doit stop"
+start_precmd="amavisd_precmd"
+
+amavisd_precmd()
+{
+    @RM@ -f @AMAVISDIR@/amavis*.sock
+}
+
+amavisd_doit ()
+{
+	case $1 in
+	start)	
+	if [ -e @AMAVISDIR@/use.milter ]; then
+		@SU@ @AMAVISUSER@ -c "@PREFIX@/sbin/amavis-milter -D -p @AMAVISDIR@/amavis-milter.sock"
+	fi
+	@SU@ @AMAVISUSER@ -c "@PREFIX@/sbin/amavisd > /dev/null 2>&1" && echo -n ' amavisd'
+	;;
+	stop)
+	milter_pid=	
+	amavisd_pid=`@CAT@ ${pidfile}`
+	if [ -e @AMAVISDIR@/use.milter ]; then
+		milter_pid=$(check_process @PREFIX@/sbin/amavis-milter )
+		kill $milter_pid
+		#killall -u @AMAVISUSER@ amavis-milter >/dev/null 2>&1
+	fi
+	if [ -e $pidfile ]; then
+		@SU@ @AMAVISUSER@ -c "kill ${amavisd_pid}"
+	fi
+
+	wait_for_pids $milter_pid $amavisd_pid
+	;;
+	esac
+
+}
+
+if [ -f /etc/rc.subr -a -f /etc/rc.conf -a -d /etc/rc.d -a -f /etc/rc.d/DAEMON ]
+then
+	load_rc_config $name
+	run_rc_command "$1"
+else
+	amavisd_doit "$1"
+fi
