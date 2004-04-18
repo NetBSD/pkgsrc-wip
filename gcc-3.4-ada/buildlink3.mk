cf7256bf493833787c577edad1351cbf5f25b446
John R. Shannon <johnrshannon@users.sourceforge.net>
2004-04-18 11:41:46 +0000

    gcc changed 3.4 to 3.40 with RC 2 - pkg name changed with it

--- gcc-3.4-ada/buildlink3.mk
+++ gcc-3.4-ada/buildlink3.mk
@@ -1,22 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/03/13 20:44:56 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2004/04/18 11:41:46 johnrshannon Exp $
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH}+
 GCC-3.4-ADA_BUILDLINK3_MK:=   ${GCC-3.4-ADA_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=   gccAda-3.4
+BUILDLINK_DEPENDS+=   gccAda-3.4.0
 .endif
 
 BUILDLINK_PACKAGES:=  ${BUILDLINK_PACKAGES:NgccAda-3.4}
-BUILDLINK_PACKAGES+=  gccAda-3.4
+BUILDLINK_PACKAGES+=  gccAda-3.4.0
 
 .if !empty(GCC-3.4-ADA_BUILDLINK3_MK:M+)
 .  if defined(GCC34_INSTALLTO_SUBPREFIX)
-.    if ${GCC34_INSTALLTO_SUBPREFIX} != "gccAda-3.4"
+.    if ${GCC34_INSTALLTO_SUBPREFIX} != "gccAda-3.4.0"
 GCC34_PKGMODIF=			_${GCC34_INSTALLTO_SUBPREFIX}
 .    endif
 .  endif
-BUILDLINK_DEPENDS.gccAda-3.4+=       gccAda-3.4
+BUILDLINK_DEPENDS.gccAda-3.4+=       gccAda-3.4.0
 BUILDLINK_PKGSRCDIR.gccAda-3.4?=     ../../wip/gcc-3.4-ada
 BUILDLINK_CONTENTS_FILTER.gccAda-3.4= \
 	${EGREP} '(gnat1|bin.*/|include.*/|\.h$$|\.pc$$|lib.*/lib[^/]*$$)'
