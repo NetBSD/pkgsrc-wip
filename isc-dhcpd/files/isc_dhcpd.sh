98c10e8e32e9db746f082173b9422048d107c9f5
Michal Pasternak <mpasternak@users.sourceforge.net>
2004-04-18 17:33:33 +0000

    Correct required_files

--- isc-dhcpd/files/isc_dhcpd.sh
+++ isc-dhcpd/files/isc_dhcpd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-# $NetBSD: isc_dhcpd.sh,v 1.1 2004/04/18 17:22:13 mpasternak Exp $
-# $Id: isc_dhcpd.sh,v 1.1 2004/04/18 17:22:13 mpasternak Exp $
+# $NetBSD: isc_dhcpd.sh,v 1.2 2004/04/18 17:33:33 mpasternak Exp $
+# $Id: isc_dhcpd.sh,v 1.2 2004/04/18 17:33:33 mpasternak Exp $
 
 # PROVIDE: dhcpd
 # REQUIRE: DAEMON
@@ -13,7 +13,7 @@ name="dhcpd"
 rcvar="isc_dhcpd"
 command="@PREFIX@/sbin/${name}"
 pidfile="/var/run/${name}.pid"
-required_files="@PREFIX@/etc/${name}.conf /var/db/${name}.leases"
+required_files="@PREFIX@/etc/${name}/${name}.conf /var/db/${name}.leases"
 
 load_rc_config $name
 run_rc_command "$1"
