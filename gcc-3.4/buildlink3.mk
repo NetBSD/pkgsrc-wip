a9956c29030d4bba945510dcbe37ef8072523e8f
John R. Shannon <johnrshannon@users.sourceforge.net>
2004-04-21 17:19:45 +0000

    Fix buildlink3.mk file so build-only dependancy works

--- gcc-3.4/buildlink3.mk
+++ gcc-3.4/buildlink3.mk
@@ -1,33 +1,34 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/04/21 10:06:07 johnrshannon Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/04/21 17:19:45 johnrshannon Exp $
 
 BUILDLINK_DEPTH:=       ${BUILDLINK_DEPTH}+
-GCC_BUILDLINK3_MK:=     ${GCC_BUILDLINK3_MK}+
-
-.if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=     gcc
-.endif
-
-BUILDLINK_PACKAGES:=    ${BUILDLINK_PACKAGES:Ngcc}
-BUILDLINK_PACKAGES+=    gcc
+GCC34_BUILDLINK3_MK:=  ${GCC34_BUILDLINK3_MK}+
 BUILDLINK_PREFIX.gcc-3.4.0:=${LOCALBASE}/gcc-3.4.0
 
-.if !empty(GCC_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gcc+= gcc>=3.4.0
-BUILDLINK_PKGSRCDIR.gcc?=       ../../wip/gcc-3.4
-.endif  # GCC_BUILDLINK3_MK
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=     gcc34
+.endif
 
-BUILDLINK_CONTENTS_FILTER.gcc= \
+BUILDLINK_PACKAGES:=    ${BUILDLINK_PACKAGES:Ngcc34}
+BUILDLINK_PACKAGES+=    gcc34
+
+.if !empty(GCC34_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.gcc34+=      gcc34>=3.4.0
+BUILDLINK_PKGSRCDIR.gcc34?=    ../../wip/gcc-3.4
+.endif  # GCC34_BUILDLINK3_MK
+
+BUILDLINK_PKGSRCDIR.gcc34?=     ../../wip/gcc-3.4
+BUILDLINK_CONTENTS_FILTER.gcc34= \
 	${EGREP} '(libexec.*|bin.*/|include.*/|\.h$$|\.pc$$|lib.*/lib[^/]*$$)'
 BUILDLINK_ENV+=$PATH:=${BUILDLINK_PREFIX.gcc-3.4.0}:${PATH}
 
-.include "../../converters/libiconv/buildlink3.mk"
-.include "../../mk/pthread.buildlink3.mk"
-
 # Packages that link against shared libraries need a full dependency.
 .  if defined(USE_GCC_SHLIB)
-BUILDLINK_DEPMETHOD.gcc-3.4.0?= full
+BUILDLINK_DEPMETHOD.gcc34?=	full
 .  else
-BUILDLINK_DEPMETHOD.gcc-3.4.0?= build
+BUILDLINK_DEPMETHOD.gcc34?=	build
 .  endif
+
+.include "../../mk/pthread.buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
