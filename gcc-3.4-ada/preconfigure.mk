500fc192c223067344d998daa1acbd3cdde1859e
John R. Shannon <johnrshannon@users.sourceforge.net>
2004-04-30 09:40:28 +0000

    Remove architecture dependancies found when testing on AMD 64

--- gcc-3.4-ada/preconfigure.mk
+++ gcc-3.4-ada/preconfigure.mk
@@ -1,9 +1,7 @@
 	# Create compiler driver scripts in ${WRKDIR}.
-	(cd ${WRKDIR}/.gcc/bin && \
-	for _target_ in cc cpp gcc gccbug gcov gnat gnatbind \
-		gnatbl gnatchop gnatclean gnatfind gnatkr \
-		gnatlink gnatls gnatmake gnatname gnatprep \
-		gnatxref gpr2make gprcmd ; do \
+	(cd ${BUILDLINK_PREFIX.gccAda-3.4.0}/bin && bin_files=`ls *` && \
+	cd ${WRKDIR}/.gcc/bin && \
+	for _target_ in $${bin_files} ; do \
 		${ECHO} '#!${TOOLS_SHELL}' > $${_target_} && \
 		${ECHO} -n "exec ${LOCALBASE}/gccAda-3.4.0/bin/$${_target_}" >> $${_target_} && \
 		${ECHO} ' "$$@"' >> $${_target_} && \
