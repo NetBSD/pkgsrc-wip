2cc2a9c6f6eb8519c3647ca48889f2d7df0f3aca
Min Sik Kim <minskim@NetBSD.org>
2004-06-06 16:58:33 +0000

    Set BUILDLINK_DEPMETHOD to "build" because this package installs .h and .c only.

--- xtrans/buildlink3.mk
+++ xtrans/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/18 15:40:57 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/06/06 16:58:33 minskim Exp $
 #
 # This Makefile fragment is included by packages that use xtrans.
 #
@@ -16,6 +16,7 @@ BUILDLINK_PACKAGES+=	xtrans
 .if !empty(XTRANS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.xtrans?=		xtrans>=0.1
 BUILDLINK_PKGSRCDIR.xtrans?=		../../wip/xtrans
+BUILDLINK_DEPMETHOD.xtrans?=		build
 .endif # XTRANS_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
