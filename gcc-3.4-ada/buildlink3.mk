28c0098c09bde12debabe53260d3072f89add31d
John R. Shannon <johnrshannon@users.sourceforge.net>
2004-06-06 20:03:32 +0000

    Adjusted some paths reflecting return to wip

--- gcc-3.4-ada/buildlink3.mk
+++ gcc-3.4-ada/buildlink3.mk
@@ -1,8 +1,8 @@
-# $NetBSD: buildlink3.mk,v 1.21 2004/06/06 10:17:25 johnrshannon Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2004/06/06 20:03:32 johnrshannon Exp $
 
 BUILDLINK_DEPTH:=       ${BUILDLINK_DEPTH}+
 GCCADA_BUILDLINK3_MK:=  ${GCCADA_BUILDLINK3_MK}+
-BUILDLINK_PREFIX.gcc3-ada:=${LOCALBASE}/gcc3-ada-3.4.0
+BUILDLINK_PREFIX.gcc3-ada:=${LOCALBASE}/gcc-3.4.0-ada
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=     gcc3-ada
@@ -13,7 +13,7 @@ BUILDLINK_PACKAGES+=    gcc3-ada
 
 .if !empty(GCCADA_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gcc3-ada+=      gcc3-ada>=3.4.0
-BUILDLINK_PKGSRCDIR.gcc3-ada?=    ../../lang/gcc3-ada
+BUILDLINK_PKGSRCDIR.gcc3-ada?=    ../../wip/gcc-3.4-ada
 BUILDLINK_ENV+= ADAC=${BUILDLINK_PREFIX.gcc3-ada}/bin/gcc
 _GCC_ARCHDIR!=	${DIRNAME} `${BUILDLINK_PREFIX.gcc3-ada}/bin/gcc --print-libgcc-file-name`
 .  if empty(_GCC_ARCHDIR:M*not_found*)
@@ -23,10 +23,10 @@ BUILDLINK_INCDIRS.gcc3-ada+=       include ${_GCC_ARCHDIR:S/^${BUILDLINK_PREFIX.
 .  endif
 .endif  # GCCADA_BUILDLINK3_MK
 
-BUILDLINK_PKGSRCDIR.gcc3-ada?=     ../../lang/gcc3-ada
+BUILDLINK_PKGSRCDIR.gcc3-ada?=     ../../wip/gcc-3.4-ada
 BUILDLINK_FILES_CMD.gcc3-ada=	(cd  ${BUILDLINK_PREFIX.gcc3-ada} && \
 	${FIND} bin libexec lib \( -type file -o -type link \) -print)
-BUILDLINK_TRANSFORM.gcc3-ada=	-e s:\buildlink:buildlink/gcc3-ada-3.4.0:
+BUILDLINK_TRANSFORM.gcc3-ada=	-e s:\buildlink:buildlink/gcc-3.4.0-ada:
 
 # Packages that link against shared libraries need a full dependency.
 .  if defined(USE_GCC_SHLIB)
