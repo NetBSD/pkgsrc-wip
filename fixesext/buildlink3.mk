caf6adee065bb5a16036ebc4b756398bf5d1da02
Min Sik Kim <minskim@NetBSD.org>
2004-06-09 03:03:34 +0000

    - Regen to follow the new format of buildlink3.mk. - Set BUILDLINK_DEPMETHOD to "build" because this package installs   header files only.

--- fixesext/buildlink3.mk
+++ fixesext/buildlink3.mk
@@ -1,22 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/05/09 04:16:06 minskim Exp $
-#
-# This Makefile fragment is included by packages that use fixesext.
-#
+# $NetBSD: buildlink3.mk,v 1.3 2004/06/09 03:03:34 minskim Exp $
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 FIXESEXT_BUILDLINK3_MK:=	${FIXESEXT_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	fixesext
 .endif
 
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nfixesext}
+BUILDLINK_PACKAGES+=	fixesext
+
 .if !empty(FIXESEXT_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=			fixesext
-BUILDLINK_DEPENDS.fixesext?=		fixesext>=2.0
-BUILDLINK_PKGSRCDIR.fixesext?=		../../wip/fixesext
+BUILDLINK_DEPENDS.fixesext+=	fixesext>=2.0
+BUILDLINK_PKGSRCDIR.fixesext?=	../../wip/fixesext
+BUILDLINK_DEPMETHOD.fixesext?=	build
+.endif	# FIXESEXT_BUILDLINK3_MK
 
 .include "../../x11/xextensions/buildlink3.mk"
-
-.endif # FIXESEXT_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
