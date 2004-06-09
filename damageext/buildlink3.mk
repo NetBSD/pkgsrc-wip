e1dab3b7b73c48588d96c42e555ec9a50321efc2
Min Sik Kim <minskim@NetBSD.org>
2004-06-09 02:58:38 +0000

    Set BUILDLINK_DEPMETHOD to "build" because this package installs header files only.

--- damageext/buildlink3.mk
+++ damageext/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/05/12 03:33:40 blef Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/06/09 02:58:38 minskim Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 DAMAGEEXT_BUILDLINK3_MK:=	${DAMAGEEXT_BUILDLINK3_MK}+
@@ -13,6 +13,7 @@ BUILDLINK_PACKAGES+=	damageext
 .if !empty(DAMAGEEXT_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.damageext+=	damageext>=1.0
 BUILDLINK_PKGSRCDIR.damageext?=	../../wip/damageext
+BUILDLINK_DEPMETHOD.damageext?=	build
 .endif	# DAMAGEEXT_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
