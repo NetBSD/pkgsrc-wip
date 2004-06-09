05cf5e9029598890192f0fc3f2c6c609c1ef2231
Juan Romero Pardines <xtraeme@gmail.com>
2004-06-09 23:23:02 +0000

    Repo-copy of graphics/gd but *without* X11 support, useful for servers.

---
+++ gd-nox11/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/06/09 23:23:02 xtraeme Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+GD_NOX11_BUILDLINK3_MK:=	${GD_NOX11_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gd-nox11
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngd-nox11}
+BUILDLINK_PACKAGES+=	gd-nox11
+
+.if !empty(GD_NOX11_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.gd-nox11+=	gd-nox11>=2.0.25
+BUILDLINK_PKGSRCDIR.gd-nox11?=	../../wip/gd-nox11
+.endif	# GD_NOX11_BUILDLINK3_MK
+
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
