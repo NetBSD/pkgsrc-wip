c4b33e433b0cf405977356bf4aa7f1f7282e557b
Peter Postma <ppostma@users.sourceforge.net>
2004-06-18 14:38:05 +0000

    Add reload target, for reloading the pf configuration.

--- pflkm/files/pf.sh
+++ pflkm/files/pf.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: pf.sh,v 1.3 2004/06/18 14:16:16 ppostma Exp $
+# $NetBSD: pf.sh,v 1.4 2004/06/18 14:38:05 ppostma Exp $
 #
 # PROVIDE: pf
 # REQUIRE: DAEMON
@@ -15,6 +15,8 @@ config="@PKG_SYSCONFDIR@/pf.conf"
 start_cmd="${command} -e -f ${config}"
 stop_cmd="${command} -d"
 status_cmd="${command} -s info"
+reload_cmd="${command} -f ${config}"
+extra_commands="reload"
 
 load_rc_config $name
 run_rc_command "$1"
