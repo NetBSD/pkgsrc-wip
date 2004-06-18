2e53a172565f8d12b7b09893663297f5e2f96a88
Peter Postma <ppostma@users.sourceforge.net>
2004-06-18 14:14:36 +0000

    Use PKG_SYSCONFDIR.

--- pflkm/files/pf.sh
+++ pflkm/files/pf.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: pf.sh,v 1.1 2004/06/09 09:47:14 ppostma Exp $
+# $NetBSD: pf.sh,v 1.2 2004/06/18 14:14:36 ppostma Exp $
 #
 # PROVIDE: pf
 # REQUIRE: DAEMON
@@ -11,7 +11,7 @@
 name="pf"
 rcvar=$name
 command="@PREFIX@/sbin/pfctl"
-config="@PREFIX@/etc/pf.conf"
+config="@PKG_SYSCONFDIR@/etc/pf.conf"
 start_cmd="${command} -e -f ${config}"
 stop_cmd="${command} -d"
 status_cmd="${command} -s info"
