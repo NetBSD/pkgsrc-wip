787b7fda9cf2fa7d0f466623273f2dd02dc9adae
Juan Romero Pardines <xtraeme@gmail.com>
2004-06-19 14:57:39 +0000

    o Install "video.x" required to boot MacOSX. o Be sure to create the nvram file into /tmp, which allow all users to   run pearpc.

--- pearpc/files/pearpc.sh
+++ pearpc/files/pearpc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: pearpc.sh,v 1.1 2004/05/25 06:58:49 xtraeme Exp $
+# $NetBSD: pearpc.sh,v 1.2 2004/06/19 14:57:40 xtraeme Exp $
 
-exec @PREFIX@/bin/ppc @PKG_SYSCONFDIR@/ppccfg.conf
+cd @PREFIX@/share/pearpc && exec ./ppc "$@" @PKG_SYSCONFDIR@/ppccfg.conf
 exit 0
