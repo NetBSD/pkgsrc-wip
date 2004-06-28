7a46c423f968ad0e10f741f3cdc66121cf9cedef
Sergey Svishchev <shattered@NetBSD.org>
2004-06-28 15:45:05 +0000

    Nuke extra whitespace.  Add 'extra_commands="reload"'.

--- smartmontools/files/smartd.sh
+++ smartmontools/files/smartd.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 #
-#	$NetBSD: smartd.sh,v 1.1 2004/05/10 23:25:35 shattered Exp $
+#	$NetBSD: smartd.sh,v 1.2 2004/06/28 15:45:05 shattered Exp $
 #
 # PROVIDE: smartd
 # REQUIRE: DAEMON
 
 . /etc/rc.subr
 
-name="smartd"                      
-rcvar=$name         
+name="smartd"
+rcvar=$name
 command="/usr/pkg/sbin/${name}"
 pidfile="/var/run/${name}.pid"
 smartd_flags="-p $pidfile"
-              
+extra_commands="reload"
+
 load_rc_config $name
 run_rc_command "$1"
