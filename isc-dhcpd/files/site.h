b7b7cf4139376a26d618882c33844764404697e9
Jeremy C. Reed <reed@reedmedia.net>
2004-07-07 19:53:56 +0000

    Use pkgsrc's new VARBASE instead of my VAR_DIR.
    Also use my PKGMANDIR as the place to install man pages.
    (Defaults to normal "man" directory for now. Later plan to integrate
    in pkgsrc. I use it to install to "share/man".)
    
    Also make dhclient point to correct _PATH_DHCLIENT_SCRIPT.

--- isc-dhcpd/files/site.h
+++ isc-dhcpd/files/site.h
@@ -1,5 +1,6 @@
 #define _PATH_DHCPD_CONF "@@SYSCONFDIR@@/dhcp/dhcpd.conf"
-#define _PATH_DHCPD_PID "@@VAR_DIR@@/run/dhcpd.pid"
-#define _PATH_DHCPD_DB "@@VAR_DIR@@/db/dhcp/dhcpd.leases"
-#define _PATH_DHCLIENT_DB "@@VAR_DIR@@/db/dhcp/dhclient.leases"
+#define _PATH_DHCPD_PID "@@VARBASE@@/run/dhcpd.pid"
+#define _PATH_DHCPD_DB "@@VARBASE@@/db/dhcp/dhcpd.leases"
+#define _PATH_DHCLIENT_DB "@@VARBASE@@/db/dhcp/dhclient.leases"
+#define _PATH_DHCLIENT_SCRIPT "@@PREFIX@@/sbin/dhclient-script"
 #define DHCPD_LOG_FACILITY LOG_LOCAL1
