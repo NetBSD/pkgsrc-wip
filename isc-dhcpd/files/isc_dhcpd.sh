2f21012c68858b1c479370308610de827e58139e
Jeremy C. Reed <reed@reedmedia.net>
2004-07-08 15:12:25 +0000

    Use @PKG_SYSCONFDIR@ instead of "@PREFIX@/etc".

--- isc-dhcpd/files/isc_dhcpd.sh
+++ isc-dhcpd/files/isc_dhcpd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-# $NetBSD: isc_dhcpd.sh,v 1.4 2004/07/08 15:03:16 jeremy-c-reed Exp $
-# $Id: isc_dhcpd.sh,v 1.4 2004/07/08 15:03:16 jeremy-c-reed Exp $
+# $NetBSD: isc_dhcpd.sh,v 1.5 2004/07/08 15:12:25 jeremy-c-reed Exp $
+# $Id: isc_dhcpd.sh,v 1.5 2004/07/08 15:12:25 jeremy-c-reed Exp $
 
 # PROVIDE: dhcpd
 # REQUIRE: DAEMON
@@ -13,7 +13,7 @@ name="dhcpd"
 rcvar="isc_dhcpd"
 command="@PREFIX@/sbin/${name}"
 pidfile="@VARBASE@/run/${name}.pid"
-required_files="@PREFIX@/etc/dhcp/${name}.conf @VARBASE@/db/dhcp/${name}.leases"
+required_files="@PKG_SYSCONFDIR@/dhcp/${name}.conf @VARBASE@/db/dhcp/${name}.leases"
 
 load_rc_config $name
 run_rc_command "$1"
