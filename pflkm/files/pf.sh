ecd12e3cbe27606495d8579ea0daebcb34bbd48b
Peter Postma <ppostma@users.sourceforge.net>
2004-08-03 15:07:51 +0000

    Sync with NetBSD -current.

--- pflkm/files/pf.sh
+++ pflkm/files/pf.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: pf.sh,v 1.5 2004/06/18 14:46:20 ppostma Exp $
+# $NetBSD: pf.sh,v 1.6 2004/08/03 15:07:51 ppostma Exp $
 #
 # PROVIDE: pf
 # REQUIRE: DAEMON
@@ -12,11 +12,43 @@ name="pf"
 rcvar=$name
 pfctl="@PREFIX@/sbin/pfctl"
 config="@PKG_SYSCONFDIR@/pf.conf"
-start_cmd="${pfctl} -e -f ${config}"
-stop_cmd="${pfctl} -d"
-status_cmd="${pfctl} -s info"
-reload_cmd="${pfctl} -f ${config}"
-extra_commands="reload"
+start_cmd="pf_start"
+stop_cmd="pf_stop"
+reload_cmd="pf_reload"
+status_cmd="pf_status"
+extra_commands="reload status"
+
+pf_start()
+{
+	echo "Enabling pf firewall."
+	${pfctl} -q -e 
+	if [ -f ${config} ]; then
+		${pfctl} -q -f ${config}
+	else
+		warn "pf.conf not found; no pf rules loaded."
+	fi
+}
+
+pf_stop()
+{
+	echo "Disabling pf firewall."
+	${pfctl} -q -d
+}
+
+pf_reload()
+{
+	echo "Reloading pf rules."
+	if [ -f ${config} ]; then
+		${pfctl} -q -f ${config}
+	else
+		warn "pf.conf not found; no pf rules loaded."
+	fi
+}
+
+pf_status()
+{
+	${pfctl} -s info
+}
 
 load_rc_config $name
 run_rc_command "$1"
