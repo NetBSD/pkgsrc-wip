062cb7772c445947e8c10fcf500578f8240b3094
Julian C. Dunn <jcd@users.sourceforge.net>
2004-08-06 04:57:03 +0000

    Update to amavisd-new-20040701. Patch provided by jlam with a few tweaks by me to correct typos and bump dependency on Perl 5.8 to Perl 5.8.2 (as recommended by author of this software). Complete list of changes:
    The attached patch makes the following modifications to wip/amavisd-new:
    
        (1) Updated amavisd-new to 20040701 (the "2.0" release).
    
        (2) Modified the way the package name is computed to deal with an
            empty PATCHLEVEL.
    
        (3) Renamed the AMAVIS* variables to AMAVIS_* to more closely
            conform to how package variables are named in pkgsrc.
    
        (4) Converted the package to use the brand-new bsd.options.mk
            framework.
    
        (5) Fixed a buildlink3-related issue with respect to referencing
            the prefix of an installed package (libmilter).
    
        (6) Replaced the do-configure and do-build targets with the
            appropriate {CONFIGURE,BUILD}_DIRS settings.
    
        (7) Simplified the do-install target to avoid listing every file
            we need to install.
    
        (8) Added a amavismilter rc.d script that is only installed when
            libmilter support is built.  The script handles starting up
            amavismilter properly to use sendmail+libmilter with amavisd.
    
        (9) Removed the dependency on spamassassin (it's not strictly
            necessary) and replaced it with improved MESSAGE files
            recommending that the user install some content checkers.

--- amavisd-new/files/amavisd.sh
+++ amavisd-new/files/amavisd.sh
@@ -1,69 +1,38 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: amavisd.sh,v 1.3 2004/04/19 03:42:09 jcd Exp $
+# $NetBSD: amavisd.sh,v 1.4 2004/08/06 04:57:03 jcd Exp $
 #
 # PROVIDE: amavisd
 # REQUIRE: DAEMON
 # BEFORE:  mail
-#
-# To start spread at startup, copy this script to /etc/rc.d and set
-# spread=YES in /etc/rc.conf.
 #
 
 if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
-rcd_dir=`@DIRNAME@ $0`
-
 name="amavisd"
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
 command_interpreter="@PERL5@"
-pidfile="@AMAVISDIR@/amavisd.pid"
+pidfile="@AMAVIS_DIR@/amavisd.pid"
 required_files="@PKG_SYSCONFDIR@/${name}.conf"
-required_dirs="@AMAVISDIR@"
+required_dirs="@AMAVIS_DIR@"
+: ${amavisd_user="@AMAVIS_USER@"}
 
-start_cmd="amavisd_doit start"
-stop_cmd="amavisd_doit stop"
 start_precmd="amavisd_precmd"
 
 amavisd_precmd()
 {
-    @RM@ -f @AMAVISDIR@/amavis*.sock
+	@RM@ -f @AMAVIS_DIR@/amavisd.sock
 }
 
-amavisd_doit ()
-{
-	case $1 in
-	start)	
-	if [ -e @AMAVISDIR@/use.milter ]; then
-		@SU@ @AMAVISUSER@ -c "@PREFIX@/sbin/amavis-milter -D -p @AMAVISDIR@/amavis-milter.sock"
-	fi
-	@SU@ @AMAVISUSER@ -c "@PREFIX@/sbin/amavisd > /dev/null 2>&1" && echo -n ' amavisd'
-	;;
-	stop)
-	milter_pid=	
-	amavisd_pid=`@CAT@ ${pidfile}`
-	if [ -e @AMAVISDIR@/use.milter ]; then
-		milter_pid=$(check_process @PREFIX@/sbin/amavis-milter )
-		kill $milter_pid
-		#killall -u @AMAVISUSER@ amavis-milter >/dev/null 2>&1
-	fi
-	if [ -e $pidfile ]; then
-		@SU@ @AMAVISUSER@ -c "kill ${amavisd_pid}"
-	fi
-
-	wait_for_pids $milter_pid $amavisd_pid
-	;;
-	esac
-
-}
-
-if [ -f /etc/rc.subr -a -f /etc/rc.conf -a -d /etc/rc.d -a -f /etc/rc.d/DAEMON ]
-then
+if [ -f /etc/rc.subr -a -f /etc/rc.conf \
+     -a -d /etc/rc.d -a -f /etc/rc.d/DAEMON ]; then
 	load_rc_config $name
 	run_rc_command "$1"
 else
-	amavisd_doit "$1"
+	@ECHO@ -n " ${name}"
+	eval ${start_precmd}          
+	${command} ${amavisd_flags} ${command_args}
 fi
