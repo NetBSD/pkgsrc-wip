709f5270c1799d0b5bf1bf19278771345f30feb7
Min Sik Kim <minskim@NetBSD.org>
2004-08-18 21:09:36 +0000

    Update Cg-compiler to 1.2.1 and take maintainership.  This version has bug fixes and minor feature enhancements only.

--- Cg-compiler/buildlink3.mk
+++ Cg-compiler/buildlink3.mk
@@ -1,22 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/02/27 09:30:42 xtraeme Exp $
-#
-# This Makefile fragment is included by packages that use Cg-compiler-1.2.0402.
-#
-# This file was created automatically using createbuildlink-3.1.
-#
+# $NetBSD: buildlink3.mk,v 1.2 2004/08/18 21:09:37 minskim Exp $
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
-CG_COMPILER_1.2.0402_BUILDLINK3_MK:=	${CG_COMPILER_1.2.0402_BUILDLINK3_MK}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+CG_COMPILER_BUILDLINK3_MK:=	${CG_COMPILER_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	Cg-compiler-1.2.0402
+BUILDLINK_DEPENDS+=	Cg-compiler
 .endif
 
-.if !empty(CG_COMPILER_1.2.0402_BUILDLINK3_MK:M+)
-BUILDLINK_PACKAGES+=				Cg-compiler-1.2.0402
-BUILDLINK_DEPENDS.Cg-compiler-1.2.0402+=	Cg-compiler-1.2.0402>=1900
-BUILDLINK_PKGSRCDIR.Cg-compiler-1.2.0402?=	../../wip/Cg-compiler
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NCg-compiler}
+BUILDLINK_PACKAGES+=	Cg-compiler
 
-.endif # CG_COMPILER_1.2.0402_BUILDLINK3_MK
+.if !empty(CG_COMPILER_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.Cg-compiler+=		Cg-compiler>=1.2.1
+BUILDLINK_PKGSRCDIR.Cg-compiler?=	../../lang/Cg-compiler
+.endif	# CG_COMPILER_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
