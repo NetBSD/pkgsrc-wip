e2d56feec2e8d03868f74ebde3b3095b77058a7b
Jeremy C. Reed <reed@reedmedia.net>
2004-08-27 06:43:02 +0000

    Remove rcorder. An autoconf'd version has been independently added to pkgsrc/pkgtools/rcorder which appears to build, install and work fine for me under Linux. Thank you, Amitai and jlam.

--- rcorder/files/ealloc.h
+++
@@ -1,6 +0,0 @@
-/*	$NetBSD: ealloc.h,v 1.1.1.1 2003/04/28 16:16:38 jeremy-c-reed Exp $	*/
-
-void	*emalloc(size_t);
-char	*estrdup(const char *);
-void	*erealloc(void *, size_t);
-void	*ecalloc(size_t, size_t);
