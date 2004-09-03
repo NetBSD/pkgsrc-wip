4201b161267e11ae8a20ac481563033700861938
Tobias Nygren <tnn@NetBSD.org>
2004-09-03 13:31:01 +0000

    Add a buildlink3.mk to this package.

---
+++ dclib/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/09/03 13:31:01 tnn2 Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+DCLIB_BUILDLINK3_MK:=	${DCLIB_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	dclib
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ndclib}
+BUILDLINK_PACKAGES+=	dclib
+
+.if !empty(DCLIB_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.dclib+=	dclib>=0.3.3
+BUILDLINK_PKGSRCDIR.dclib?=	../../wip/dclib
+.endif	# DCLIB_BUILDLINK3_MK
+
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
