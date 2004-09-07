d4c63f5b64a9d7202970a0dba2bd9930aed68d43
César Catrián C <cetrox@users.sourceforge.net>
2004-09-07 22:36:14 +0000

    FOX is a C++ based Toolkit for developing Graphical User Interfaces easily and effectively. It offers a wide, and growing, collection of Controls, and provides state of the art facilities such as drag and drop, selection, as well as OpenGL widgets for 3D graphical manipulation. FOX also implements icons, images, and user-convenience features such as status line help, and tooltips.
    The actual mantainer of x11/fox was contacted a month ago.

---
+++ fox/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/09/07 22:36:15 cetrox Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+FOX_BUILDLINK3_MK:=	${FOX_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	fox
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nfox}
+BUILDLINK_PACKAGES+=	fox
+
+.if !empty(FOX_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.fox+=	fox>=1.2.9
+BUILDLINK_PKGSRCDIR.fox?=	../../wip/fox.import
+.endif	# FOX_BUILDLINK3_MK
+
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
