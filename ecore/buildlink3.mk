115dbd3b43a3d86715ee97fc1f21437fdf2a39c8
Peter Bex <airhead@users.sourceforge.net>
2004-09-16 15:42:50 +0000

    Add buildlink3 file

---
+++ ecore/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/09/16 15:42:50 airhead Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+ECORE_BUILDLINK3_MK:=	${ECORE_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	ecore
+.endif
+
+BUILDLINK_PACKAGES+=	ecore
+
+.if !empty(ECORE_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.ecore+=	ecore>=1.0.0nb1
+BUILDLINK_PKGSRCDIR.ecore?=	../../wip/ecore
+.endif	# ECORE_BUILDLINK3_MK
+
+.include "../../devel/libtool/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.include "../../wip/evas/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
