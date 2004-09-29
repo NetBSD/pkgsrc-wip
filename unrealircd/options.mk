5015e411ae705adc6b14f16d55b5583d189cfac4
Adrian Portelli <adrian_p@users.sourceforge.net>
2004-09-29 23:25:56 +0000

    This is the same unrealircd that I hacked at @6 months ago.  They have gone release now and I decided to give it another go a pkgsrc'ing it.
    ipv6 support is busted so don't even bother but the rest of the 
    functionality looks ok.   The runtime setuid/setgid hack is mine so
    if that breaks you can blame me.  It will probably sit here for a while
    untill I get a last few issues sorted out with it.  I will also need to get
    the developers to have a look at why ipv6 isn't working correctly.
    
    UnrealIRCd is an IRC server based on the branch of IRCu called Dreamforge,
    formerly used by the DALnet IRC Network. Since development started on it
    (around May 1999) many new features has been added, modified, and many bugs
    fixed. Unreal runs both on *nix platforms, Amiga & Windows 95/98/2k/NT.
    Some features to mention: Channel Halfops, No-color channel mode,
    Strip-colors channel mode, channelmode +q & +a - protect modes, Oper and
    Admin only channels, exception bans, Nokicks mode (channel and user), Flood
    limiter, Channel links, Host cloaking, Net* Tech and Co-Admin modes, Whois
    notifies, Foreign connects monitor, Oper suspend, G:Lines, T:Lines, channel
    restriction (people can only go to these channels if enabled), Trojan/DCC
    Deny, RPING/RPONG, Sethost family, Token in server<->server etc.
    FDlists/High Traffic Mode, SOCKS check on connect, Speeded up server
    synchs, limited WebTV Client support, /vhost, and many dreamforge
    optimations. UnrealIRCd is aimed to be an advanced, not an easy IRCd.
    
    Also includes SSL and IPv6 support.

---
+++ unrealircd/options.mk
@@ -0,0 +1,74 @@
+# $NetBSD: options.mk,v 1.1.1.1 2004/09/29 23:25:56 adrian_p Exp $
+
+.if defined(USE_INET6) && !empty(USE_INET6:M[yY][eE][sS])
+PKG_DEFAULT_OPTIONS+=   inet6
+.endif
+.if defined(USE_SSL) && !empty(USE_SSL:M[yY][eE][sS])
+PKG_DEFAULT_OPTIONS+=	ssl
+.endif
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.unrealircd
+PKG_SUPPORTED_OPTIONS=	inet6 nospoof hub leaf ziplinks remoteinc ssl
+
+.if !defined(PKG_OPTIONS.unrealircd)
+PKG_DEFAULT_OPTIONS+=	hub
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+###
+### Can't be both a leaf and a hub IRC server
+###
+.if !empty(PKG_OPTIONS:Mhub) && !empty(PKG_OPTIONS:Mleaf)
+PKG_FAIL_REASON+=	"The server cannot be both a hub and a leaf." \
+			"Please change ${PKG_OPTIONS_VAR} to one or the other."
+.endif
+
+###
+### Enable IPv6 support 
+###
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--enable-inet6
+.endif
+
+###
+### Enable SSL support
+###
+.if !empty(PKG_OPTIONS:Mssl)
+.	include "../../security/openssl/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-ssl=${SSLBASE}
+.endif
+
+###
+### Enable Anti-Spoofing protection for older OS's with an insecure IPv4 stack
+###
+.if !empty(PKG_OPTIONS:Mnospoof)
+CONFIGURE_ARGS+=-	--enable-nospoof
+.endif
+
+###
+### Compile as a hub or leaf server
+###
+.if !empty(PKG_OPTIONS:Mhub)
+CONFIGURE_ARGS+=	--enable-hub
+.	elif !empty(PKG_OPTIONS:Mleaf)
+	CONFIGURE_ARGS+=	--enable-leaf
+.endif
+
+###
+### Compile in support for ziplinks.  This compresses data sent from
+### server <-> server with zlib.
+###
+.if !empty(PKG_OPTIONS:Mziplinks)
+CONFIGURE_ARGS+=		--enable-ziplinks
+.	include "../../devel/zlib/buildlink3.mk"
+.endif
+
+###
+### Compile in support for remote include files.
+###
+.if !empty(PKG_OPTIONS:Mremoteinc)
+CONFIGURE_ARGS+=		--enable-libcurl
+.	include "../../www/curl/buildlink3.mk"
+.	include "../../wip/c-ares/buildlink3.mk"
+.endif
