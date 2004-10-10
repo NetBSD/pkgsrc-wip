5512139fffd0340613690b64b5b3853b99e59ab1
Jeremy C. Reed <reed@reedmedia.net>
2004-10-10 04:59:44 +0000

    Update to alsa-lib-1.0.6.
    Define ONLY_FOR_PLATFORM for Linux.
    
    ChangeLog in the tarball has not been updated for years.
    
    Also up the BUILDLINK_DEPENDS because of library naming.

--- alsa-lib/buildlink3.mk
+++ alsa-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/04/12 22:32:24 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/10/10 04:59:44 jeremy-c-reed Exp $
 #
 # This file was created automatically using createbuildlink-3.4.
 
@@ -13,7 +13,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nalsa-lib}
 BUILDLINK_PACKAGES+=	alsa-lib
 
 .if !empty(ALSA_LIB_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.alsa-lib+=	alsa-lib>=1.0.4
+BUILDLINK_DEPENDS.alsa-lib+=	alsa-lib>=1.0.6
 BUILDLINK_PKGSRCDIR.alsa-lib?=	../../wip/alsa-lib
 .endif	# ALSA_LIB_BUILDLINK3_MK
 
