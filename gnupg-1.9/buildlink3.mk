1b4d2989dc8f02c1579852e014af4e58671f7ee0
John R. Shannon <johnrshannon@users.sourceforge.net>
2004-10-15 22:18:18 +0000

    Changed so it does not try to use pth. Package tried to use pth and ended with both native threads and pth on NetBSD test machine; this caused some bad behavior. I need to fix the supplied pth test so that pth can be used if available and native pthreads is not.

--- gnupg-1.9/buildlink3.mk
+++ gnupg-1.9/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/10/14 22:31:01 johnrshannon Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.7.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.gnupg2?=	build
+# $NetBSD: buildlink3.mk,v 1.5 2004/10/15 22:18:18 johnrshannon Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GNUPG2_BUILDLINK3_MK:=	${GNUPG2_BUILDLINK3_MK}+
@@ -24,14 +14,5 @@ BUILDLINK_PACKAGES+=	gnupg2
 BUILDLINK_DEPENDS.gnupg2+=	gnupg2>=1.9.11
 BUILDLINK_PKGSRCDIR.gnupg2?=	../../wip/gnupg-1.9
 .endif	# GNUPG2_BUILDLINK3_MK
-
-.include "../../wip/libgpg-error/buildlink3.mk"
-.include "../../security/libgcrypt/buildlink3.mk"
-.include "../../wip/libassuan/buildlink3.mk"
-.include "../../security/libksba/buildlink3.mk"
-.include "../../wip/pinentry/buildlink3.mk"
-.include "../../wip/dirmngr/buildlink3.mk"
-.include "../../devel/pth/buildlink3.mk"
-.include "../../devel/zlib/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
