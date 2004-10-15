4f33fc00b0a35306491baf5dd817a10c0dad7754
John R. Shannon <johnrshannon@users.sourceforge.net>
2004-10-15 22:02:48 +0000

    Removed unnecessary include from buildlink3.mk

--- pinentry/buildlink3.mk
+++ pinentry/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/10/15 17:15:17 johnrshannon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/10/15 22:02:49 johnrshannon Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PINENTRY_BUILDLINK3_MK:=	${PINENTRY_BUILDLINK3_MK}+
@@ -14,9 +14,5 @@ BUILDLINK_PACKAGES+=	pinentry
 BUILDLINK_DEPENDS.pinentry+=	pinentry>=0.7.1
 BUILDLINK_PKGSRCDIR.pinentry?=	../../wip/pinentry
 .endif	# PINENTRY_BUILDLINK3_MK
-
-.include "../../security/libksba/buildlink3.mk"
-.include "../../x11/qt3-libs/buildlink3.mk"
-.include "../../x11/gtk/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
