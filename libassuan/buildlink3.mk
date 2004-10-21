e7becd689a6197f38f73b0bc6d1c716f514c75be
John R. Shannon <johnrshannon@users.sourceforge.net>
2004-10-21 21:39:56 +0000

    Adjusted buildlink paths for libgpg-error from wip to security because package has been updated in pkgsrc tree.

--- libassuan/buildlink3.mk
+++ libassuan/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/10/14 22:32:39 johnrshannon Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/10/21 21:39:56 johnrshannon Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBASSUAN_BUILDLINK3_MK:=	${LIBASSUAN_BUILDLINK3_MK}+
@@ -15,6 +15,6 @@ BUILDLINK_DEPENDS.libassuan+=	libassuan>=0.6.7
 BUILDLINK_PKGSRCDIR.libassuan?=	../../wip/libassuan
 .endif	# LIBASSUAN_BUILDLINK3_MK
 
-.include "../../wip/libgpg-error/buildlink3.mk"
+.include "../../security/libgpg-error/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
