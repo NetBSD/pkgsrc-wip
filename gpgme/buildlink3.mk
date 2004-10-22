46bed5e02240e085d0a30db94751997d7c00ff8e
John R. Shannon <johnrshannon@users.sourceforge.net>
2004-10-22 20:09:20 +0000

    Update to 1.0.1, a bug fix release.

--- gpgme/buildlink3.mk
+++ gpgme/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/10/22 19:34:57 johnrshannon Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/10/22 20:09:20 johnrshannon Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GPGME_BUILDLINK3_MK:=	${GPGME_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	gpgme
 
 .if !empty(GPGME_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gpgme+=	gpgme>=0.9.0
-BUILDLINK_RECOMMENDED.gpgme+=	gpgme>=0.9.0nb1
+BUILDLINK_RECOMMENDED.gpgme+=	gpgme>=1.0.1
 BUILDLINK_PKGSRCDIR.gpgme?=	../../wip/gpgme
 .endif	# GPGME_BUILDLINK3_MK
 
