7a155463b22e6fd6d6168297f368ce0d81b201bd
pancake ;) <poppnk@users.sourceforge.net>
2004-10-22 09:25:15 +0000

    initial import for bluez-libs
    The overall goal of this project is to make an implementation of the
    Bluetooth wireless standards specifications for Linux. The code is
    licensed under the GNU General Public License (GPL) and is now included
    in the Linux 2.4 and Linux 2.6 kernel series.

---
+++ bluez-libs/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/10/22 09:25:15 poppnk Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+BLUEZ_LIBS_BUILDLINK3_MK:=	${BLUEZ_LIBS_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	bluez-libs
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nbluez-libs}
+BUILDLINK_PACKAGES+=	bluez-libs
+
+.if !empty(BLUEZ_LIBS_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.bluez-libs+=	bluez-libs>=2.10
+BUILDLINK_PKGSRCDIR.bluez-libs?=	../../wip/bluez-libs
+.endif	# BLUEZ_LIBS_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
