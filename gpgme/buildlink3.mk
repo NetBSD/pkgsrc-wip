d365497906e6240fca2ea17a1cc2c6508176b66e
John R. Shannon <johnrshannon@users.sourceforge.net>
2004-10-22 19:34:57 +0000

    This is an update to security/gpgme to support SMIME via gpg-devel. GnuPG Made Easy (GPGME) is a library designed to make access to GnuPG easier for applications. It provides a High-Level Crypto API for encryption, decryption, signing, signature verification and key management.

---
+++ gpgme/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/10/22 19:34:57 johnrshannon Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GPGME_BUILDLINK3_MK:=	${GPGME_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gpgme
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngpgme}
+BUILDLINK_PACKAGES+=	gpgme
+
+.if !empty(GPGME_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.gpgme+=	gpgme>=0.9.0
+BUILDLINK_RECOMMENDED.gpgme+=	gpgme>=0.9.0nb1
+BUILDLINK_PKGSRCDIR.gpgme?=	../../wip/gpgme
+.endif	# GPGME_BUILDLINK3_MK
+
+.include "../../security/libgpg-error/buildlink3.mk"
+.include "../../wip/librfuncs/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
