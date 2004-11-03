e586b11e1d73ffa7afadac9ab7373fe5bd153479
Quentin Garnier <cube@NetBSD.org>
2004-11-03 10:40:27 +0000

    Remove the nagios-setup.sh script, and use OWN_DIRS_PERMS framework instead.

--- nagios-base/files/nagios-setup.sh
+++
@@ -1,8 +0,0 @@
-#!@@SH@@
-#
-# Run this script as root to create these necessary runtime directories.
-#
-install -m 0775 -o @@USER@@ -g @@GROUP@@ -d /var/log/nagios
-install -m 0775 -o @@USER@@ -g @@GROUP@@ -d /var/log/nagios/archives
-install -m 0775 -o @@USER@@ -g @@GROUP@@ -d /var/run/nagios
-install -m 0775 -o @@USER@@ -g @@GROUP@@ -d /var/run/nagios/rw
