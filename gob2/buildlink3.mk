12182e3f297089a07c4c31aefae2a43da588b3e8
pancake ;) <poppnk@users.sourceforge.net>
2004-11-11 13:03:08 +0000

    update to 2.0.11 (2.0.10 doesn't exists on ftp)
    add buildlink. I need this lib :)

---
+++ gob2/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2004/11/11 13:03:08 poppnk Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GOB2_BUILDLINK3_MK:=	${GOB2_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gob2
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngob2}
+BUILDLINK_PACKAGES+=	gob2
+
+.if !empty(GOB2_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.gob2+=	gob2>=2.0.10
+BUILDLINK_PKGSRCDIR.gob2?=	../../wip/gob2
+.endif	# GOB2_BUILDLINK3_MK
+
+#.include "../../devel/glib2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
