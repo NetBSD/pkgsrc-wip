20290e6bf781122443f55fb9ae3e2b2274f88dd2
Juan Romero Pardines <xtraeme@gmail.com>
2004-11-30 18:10:24 +0000

    Update to 3.4.2-20040916, review the Changelog file.

--- mingw-gcc/buildlink3.mk
+++ mingw-gcc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/06/20 07:06:21 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/11/30 18:10:24 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MINGW_GCC_BUILDLINK3_MK:=	${MINGW_GCC_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_PACKAGES+=	mingw-gcc
 .include "../../wip/mingw/Makefile.common"
 
 .if !empty(MINGW_GCC_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.mingw-gcc+=	mingw-gcc>=3.3.1
+BUILDLINK_DEPENDS.mingw-gcc+=	mingw-gcc>=3.4.2
 BUILDLINK_PKGSRCDIR.mingw-gcc?=	../../wip/mingw-gcc
 BUILDLINK_TRANSFORM.mingw-gcc+=	-e "s,${MINGW_TARGET}/lib,lib/,g"
 .endif	# MINGW_GCC_BUILDLINK3_MK
