49eb2c09bf4e22bf874f78709b33bd5a92f08c2b
Juan Romero Pardines <xtraeme@gmail.com>
2004-11-30 17:07:26 +0000

    Update to 3.1, new libraries and functions were added.

--- mingw-w32api-bin/buildlink3.mk
+++ mingw-w32api-bin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/06/20 07:06:21 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/11/30 17:07:26 xtraeme Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 MINGW_W32API_BIN_BUILDLINK3_MK:=	${MINGW_W32API_BIN_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_PACKAGES+=	mingw-w32api-bin
 .include "../../wip/mingw/Makefile.common"
 
 .if !empty(MINGW_W32API_BIN_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.mingw-w32api-bin+=	mingw-w32api-bin>=2.4
+BUILDLINK_DEPENDS.mingw-w32api-bin+=	mingw-w32api-bin>=3.1
 BUILDLINK_PKGSRCDIR.mingw-w32api-bin?=	../../wip/mingw-w32api-bin
 BUILDLINK_TRANSFORM.mingw-w32api-bin+=	-e "s,${MINGW_TARGET}/include,include/,g"
 BUILDLINK_TRANSFORM.mingw-w32api-bin+=	-e "s,${MINGW_TARGET}/lib,lib/,g"
