3dc12dff486112f36313a401fb2e089cbe5a7dae
Thomas Klausner <wiz@NetBSD.org>
2004-12-03 17:02:24 +0000

    Prepare for import into pkgsrc.

--- g-wrap2/buildlink3.mk
+++ g-wrap2/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/10/20 18:16:03 lexort Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.7.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.g-wrap?=	build
+# $NetBSD: buildlink3.mk,v 1.2 2004/12/03 17:02:24 thomasklausner Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 G_WRAP_BUILDLINK3_MK:=	${G_WRAP_BUILDLINK3_MK}+
@@ -22,7 +12,7 @@ BUILDLINK_PACKAGES+=	g-wrap
 
 .if !empty(G_WRAP_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.g-wrap+=	g-wrap>=1.9.3
-BUILDLINK_PKGSRCDIR.g-wrap?=	../../wip/g-wrap2
+BUILDLINK_PKGSRCDIR.g-wrap?=	../../devel/g-wrap
 .endif	# G_WRAP_BUILDLINK3_MK
 
 .include "../../lang/guile/buildlink3.mk"
