9084ce69b46aa1ccd8dd7787c21f25b56466d5d0
Hauke Fath <hauke@NetBSD.org>
2005-01-13 13:05:35 +0000

    Adjust comment.

--- c-news/buildlink3.mk
+++ c-news/buildlink3.mk
@@ -1,8 +1,6 @@
-# $NetBSD: buildlink3.mk,v 1.1 2005/01/13 13:04:10 hfath Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/01/13 13:05:35 hfath Exp $
 
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-
+# install static libraries and headers
 BUILDLINK_DEPMETHOD.c-news-CR?=	build
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
