c95dbe848d4014ccef4d25485a1bd74964c17496
pancake ;) <poppnk@users.sourceforge.net>
2005-01-14 19:19:51 +0000

    initial import of java-app module for pkgsrc

---
+++ mk/java-app.mk
@@ -0,0 +1,59 @@
+# This file needs more work
+# @author: pancake<pancake@phreaker.net>
+# 
+# JAVA_APP_TARGETS  All target names space separated
+#
+# JAVA_APP_PATH     Path to install java applications (${PREFIX}/bin by default)
+#
+# JAVA_APP_BIN.x    name of target executable (target name is default)
+#
+# JAVA_APP_MAIN.x   class name to execute (manifest in CLASSPATH by default)
+#
+# JAVA_APP_CLASSPATH.x default classpath for this java application
+#
+# TODO:
+#  better documentation
+#  add jar dependencies with bl3
+#  check JAVA_APP_CLASSPATH when no JAVA_APP_MAIN defined.
+#  80 cols width
+
+JAR_PREFIX?=${PREFIX}/share/classpath/
+
+.if !defined(JAVA_PKG_MK)
+
+JAVA_PKG_MK=	#defined
+
+# default
+JAVA_APP_PATH?=${PREFIX}/bin
+JAVA_APP_WRKDIR?=${WRKDIR}/.java-app/
+
+# main loop
+.for s in ${JAVA_APP_TARGETS}
+
+.PHONY:		install-java-app-${s}
+post-install: 	install-java-app-${s}
+
+.PHONY:		build-java-app-${s}
+post-build:	build-java-app-${s}
+
+JAVA_APP_PATH.${s}?=${JAVA_APP_PATH}
+JAVA_APP_BIN.${s}?=${s}
+
+build-java-app-${s}:
+	@${MKDIR} ${JAVA_APP_WRKDIR}
+	@${ECHO} "#!/bin/sh" > ${JAVA_APP_WRKDIR}/${JAVA_APP_BIN.${s}}
+.if defined(JAVA_APP_MAIN.${s})
+	@${ECHO} "env CLASSPATH=${JAVA_APP_CLASSPATH.${s}} java ${JAVA_APP_MAIN.${s}}" \
+	>> ${JAVA_APP_WRKDIR}/${JAVA_APP_BIN.${s}}
+.else
+	@${ECHO} "java -jar ${JAVA_APP_CLASSPATH}" \
+	>> ${JAVA_APP_WRKDIR}/${JAVA_APP_BIN.${s}}
+.endif
+
+install-java-app-${s}:
+	${INSTALL_SCRIPT} ${JAVA_APP_WRKDIR}/${JAVA_APP_BIN.${s}} ${JAVA_APP_PATH}/
+
+.endfor
+.undef s
+
+.endif
