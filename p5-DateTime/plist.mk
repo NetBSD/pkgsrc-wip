a484a0f9ec0a6104a7d07a474b1a019bc02a525e
Quentin Garnier <cube@NetBSD.org>
2005-01-26 16:29:17 +0000

    Imported to pkgsrc.

--- p5-DateTime/plist.mk
+++
@@ -1,5 +0,0 @@
-.include "../../mk/bsd.prefs.mk"
-
-PERL5_MAN3DIR!=	eval `${PERL5} -V:installsiteman3dir 2>/dev/null`;	\
-		${ECHO} $${installsiteman3dir}
-PLIST_SUBST+=	PERL5_MAN3DIR=${PERL5_MAN3DIR:S/^${PREFIX}\///}
