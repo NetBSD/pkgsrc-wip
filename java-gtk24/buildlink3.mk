e80793bf00f8fc3fbc3171432597b67915f05f01
pancake ;) <poppnk@users.sourceforge.net>
2005-02-27 22:42:59 +0000

    update to 2.6.0

--- java-gtk24/buildlink3.mk
+++ java-gtk24/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/12/30 12:00:41 poppnk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/02/27 22:42:59 poppnk Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 JAVA_GTK24_BUILDLINK3_MK:=	${JAVA_GTK24_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Njava-gtk24}
 BUILDLINK_PACKAGES+=	java-gtk24
 
 .if !empty(JAVA_GTK24_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.java-gtk24+=	java-gtk24>=2.4.4
+BUILDLINK_DEPENDS.java-gtk24+=	java-gtk24>=2.6.0
 BUILDLINK_PKGSRCDIR.java-gtk24?=	../../wip/java-gtk24
 .endif	# JAVA_GTK24_BUILDLINK3_MK
 
