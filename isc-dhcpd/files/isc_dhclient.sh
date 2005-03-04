d6948caf70228538e8264a62c99ce776a30066ae
Hauke Fath <hauke@NetBSD.org>
2005-03-04 15:25:28 +0000

    Provide [start|stop|status] functionality for non-NetBSD platforms. Adapted from the openssh start script.

--- isc-dhcpd/files/isc_dhclient.sh
+++ isc-dhcpd/files/isc_dhclient.sh
@@ -1,7 +1,7 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: isc_dhclient.sh,v 1.3 2005/03/03 11:02:26 hfath Exp $
-# $Id: isc_dhclient.sh,v 1.3 2005/03/03 11:02:26 hfath Exp $
+# $NetBSD: isc_dhclient.sh,v 1.4 2005/03/04 15:25:28 hfath Exp $
+# $Id: isc_dhclient.sh,v 1.4 2005/03/04 15:25:28 hfath Exp $
 
 # PROVIDE: dhclient
 # REQUIRE: network mountcritlocal
@@ -20,10 +20,42 @@ rcvar="isc_dhclient"
 command="@PREFIX@/sbin/${name}"
 pidfile="@VARBASE@/run/${name}.pid"
 
-if [ -f /etc/rc.subr ]; then
-        load_rc_config $name
-        run_rc_command "$1"
+if [ -f /etc/rc.subr -a -f /etc/rc.conf -a -f /etc/rc.d/DAEMON ]
+then
+	load_rc_config $name
+	run_rc_command "$1"
 else
-        @ECHO@ -n " ${name}"
-        ${command} ${isc_dhclient_flags}
+	case ${1:-start} in
+	start)
+		if [ -x ${command} ]
+		then
+			@ECHO@ "Starting ${name}."
+			eval ${start_precmd}
+			eval ${command} ${isc_dhclient_flags} ${command_args}
+		fi
+		;;
+	stop)
+		if [ -f ${pidfile} ]; then
+			pid=`@HEAD@ -1 ${pidfile}`
+			@ECHO@ "Stopping ${name}."
+			kill -TERM ${pid}
+			@RM@ -f ${pidfile}
+		else
+			@ECHO@ "${name} not running?"
+		fi
+		;;
+	restart)
+		( $0 stop )
+		sleep 1
+		$0 start
+		;;
+	status)
+		if [ -f ${pidfile} ]; then
+			pid=`@HEAD@ -1 ${pidfile}`
+			@ECHO@ "${name} is running as pid ${pid}."
+		else
+			@ECHO@ "${name} is not running."
+		fi
+		;;
+	esac
 fi
--- isc-dhcpd/files/isc_dhcpd.sh
+++ isc-dhcpd/files/isc_dhcpd.sh
@@ -1,7 +1,7 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: isc_dhcpd.sh,v 1.6 2005/03/03 11:02:26 hfath Exp $
-# $Id: isc_dhcpd.sh,v 1.6 2005/03/03 11:02:26 hfath Exp $
+# $NetBSD: isc_dhcpd.sh,v 1.7 2005/03/04 15:25:28 hfath Exp $
+# $Id: isc_dhcpd.sh,v 1.7 2005/03/04 15:25:28 hfath Exp $
 
 # PROVIDE: dhcpd
 # REQUIRE: DAEMON
@@ -17,10 +17,47 @@ command="@PREFIX@/sbin/${name}"
 pidfile="@VARBASE@/run/${name}.pid"
 required_files="@PKG_SYSCONFDIR@/dhcp/${name}.conf @VARBASE@/db/dhcp/${name}.leases"
 
-if [ -f /etc/rc.subr ]; then
-        load_rc_config $name
-        run_rc_command "$1"
+if [ -f /etc/rc.subr -a -f /etc/rc.conf -a -f /etc/rc.d/DAEMON ]
+then
+	load_rc_config $name
+	run_rc_command "$1"
 else
-        @ECHO@ -n " ${name}"
-        ${command} ${isc_dhcpd_flags}
+	case ${1:-start} in
+	start)
+		if [ -x ${command} ]
+		then
+			for _f in $required_files; do
+                                if [ ! -r "${_f}" ]; then
+                                        @ECHO@ 1>&2 "$0: WARNING: ${_f} is not readable."
+				fi
+			done
+			@ECHO@ "Starting ${name}."
+			eval ${start_precmd}
+			eval ${command} ${isc_dhcpd_flags} ${command_args}
+		fi
+		;;
+	stop)
+		if [ -f ${pidfile} ]; then
+			pid=`@HEAD@ -1 ${pidfile}`
+			@ECHO@ "Stopping ${name}."
+			kill -TERM ${pid}
+			@RM@ -f ${pidfile}
+		else
+			@ECHO@ "${name} not running?"
+		fi
+		;;
+	restart)
+		( $0 stop )
+		sleep 1
+		$0 start
+		;;
+	status)
+		if [ -f ${pidfile} ]; then
+			pid=`@HEAD@ -1 ${pidfile}`
+			@ECHO@ "${name} is running as pid ${pid}."
+		else
+			@ECHO@ "${name} is not running."
+		fi
+		;;
+	esac
 fi
