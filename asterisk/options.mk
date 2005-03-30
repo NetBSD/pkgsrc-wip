5527bed729ed95d8d03e78c9407ecbd31f280585
Jeff Rizzo <riz@NetBSD.org>
2005-03-30 22:39:01 +0000

    Change PLIST handling for zaptel (and future options, as well) to use PLIST_SUBST, to simplify things greatly.  Suggested by wiz.

--- asterisk/options.mk
+++ asterisk/options.mk
@@ -1,22 +1,14 @@
-# $NetBSD: options.mk,v 1.2 2005/03/30 21:16:17 riz Exp $
+# $NetBSD: options.mk,v 1.3 2005/03/30 22:39:01 riz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.asterisk
 PKG_SUPPORTED_OPTIONS=	zaptel
 .include "../../mk/bsd.options.mk"
 
-# Can't use the default PLIST_SRC handling, since we may need to
-# add PLIST.zaptel
-
-PLIST_SRC=		${PKGDIR}/PLIST.common
-.  if exists(${PKGDIR}/PLIST.${OPSYS})
-PLIST_SRC+=		${PKGDIR}/PLIST.${OPSYS}
-.  endif
-
 .if !empty(PKG_OPTIONS:Mzaptel)
 # zaptel only supported under NetBSD at the moment
 .  include "../../wip/zaptel-netbsd/buildlink3.mk"
 MAKE_FLAGS+=	ASTERISK_USE_ZAPTEL=1
-PLIST_SRC+=		${PKGDIR}/PLIST.zaptel
+PLIST_SUBST+=		ZAPTEL=
+.else
+PLIST_SUBST+=		ZAPTEL="@comment "
 .endif
-
-PLIST_SRC+=		${PKGDIR}/PLIST.common_end
