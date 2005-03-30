efec60c68e873bce196a6e887fa670f7ed06781b
David <daprice@users.sourceforge.net>
2005-03-30 23:37:12 +0000

    changed gdal to gdal-lib

--- gdal-lib/buildlink3.mk
+++ gdal-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2005/03/28 17:09:24 daprice Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2005/03/30 23:37:12 daprice Exp $
 # XXX
 # XXX This file was created automatically using createbuildlink-3.8.
 # XXX After this file has been verified as correct, the comment lines
@@ -8,21 +8,21 @@
 # XXX Packages that only install static libraries or headers should
 # XXX include the following line:
 # XXX
-# XXX	BUILDLINK_DEPMETHOD.gdal?=	build
+# XXX	BUILDLINK_DEPMETHOD.gdal-lib?=	build
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GDAL_BUILDLINK3_MK:=	${GDAL_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	gdal
+BUILDLINK_DEPENDS+=	gdal-lib
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngdal}
-BUILDLINK_PACKAGES+=	gdal
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngdal-lib}
+BUILDLINK_PACKAGES+=	gdal-lib
 
 .if !empty(GDAL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gdal+=	gdal>=1.2.5
-BUILDLINK_PKGSRCDIR.gdal?=	../../wip/gdal-lib
+BUILDLINK_DEPENDS.gdal-lib+=	gdal-lib>=1.2.5
+BUILDLINK_PKGSRCDIR.gdal-lib?=	../../wip/gdal-lib
 .endif	# GDAL_BUILDLINK3_MK
 
 .include "../../misc/proj/buildlink3.mk"
