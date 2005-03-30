992786c2dc8eb707997dad920710431dffd7c85f
David <daprice@users.sourceforge.net>
2005-03-30 16:45:04 +0000

    updated for version 2.0

--- geos/buildlink3.mk
+++ geos/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.2 2005/03/18 21:17:35 daprice Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/03/30 16:45:04 daprice Exp $
 # XXX
-# XXX This file was created automatically using createbuildlink-3.4.
-# XXX After this file as been verified as correct, the comment lines
+# XXX This file was created automatically using createbuildlink-3.8.
+# XXX After this file has been verified as correct, the comment lines
 # XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink[23].mk files.
+# XXX unverified buildlink3.mk files.
 # XXX
 # XXX Packages that only install static libraries or headers should
 # XXX include the following line:
@@ -21,7 +21,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngeos}
 BUILDLINK_PACKAGES+=	geos
 
 .if !empty(GEOS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.geos+=	geos>=1.0.0
+BUILDLINK_DEPENDS.geos+=	geos>=2.0.0
 BUILDLINK_PKGSRCDIR.geos?=	../../wip/geos
 .endif	# GEOS_BUILDLINK3_MK
 
