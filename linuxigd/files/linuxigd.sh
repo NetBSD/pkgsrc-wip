21fe0cdbe6512ada46cf5fdc177ca034878542c1
OBATA Akio <obache@users.sourceforge.net>
2005-04-22 02:30:56 +0000

    Don't add/delete multicast routing in startup script. add messages for multicast routing.

--- linuxigd/files/linuxigd.sh
+++ linuxigd/files/linuxigd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: linuxigd.sh,v 1.1.1.1 2005/03/13 12:48:22 obache Exp $
+# $NetBSD: linuxigd.sh,v 1.2 2005/04/22 02:30:57 obache Exp $
 #
 # PROVIDE: linuxidg
 # REQUIRE: ipnat
@@ -13,23 +13,6 @@ name="linuxigd"
 rcvar=${name}
 command="@PREFIX@/bin/upnpd"
 required_vars="ipnat"
-
-start_precmd="route_add_cmd"
-stop_postcmd="route_del_cmd"
-
-route_add_cmd()
-{
-	if [ ! -z "$linuxigd_intaddr" ]; then
-		route add -net 239.0.0.0 -netmask 255.0.0.0 ${linuxigd_intaddr}
-	fi
-}
-
-route_del_cmd()
-{
-	if [ ! -z "$linuxigd_intaddr" ]; then
-		route delete -net 239.0.0.0 -netmask 255.0.0.0 ${linuxigd_intaddr}
-	fi
-}
 
 load_rc_config ${name}
 run_rc_command "$1"
