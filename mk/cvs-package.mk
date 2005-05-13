68d3eb97d0572ded95b0b318232002888bd70e99
Roland Illig <roland.illig@gmx.de>
2005-05-13 23:18:38 +0000

    Reformatted comments. Fixed variable quoting. Added CVS_TAG variable.

--- mk/cvs-package.mk
+++ mk/cvs-package.mk
@@ -1,59 +1,70 @@
-# $NetBSD: cvs-package.mk,v 1.3 2005/01/11 18:42:30 rillig Exp $
+# $NetBSD: cvs-package.mk,v 1.4 2005/05/13 23:18:38 rillig Exp $
 
 # Written by:
-#    Roland Illig <roland.illig@gmx.de>, 2004.
+#     Roland Illig <roland.illig@gmx.de>, 2004.
 #
 # A package using this file shall define the following variables:
-# CVS_REPOSITORY:	the CVS repository, including anoncvs password
-# CVS_MODULE:		the CVS module to check out
+#     CVS_REPOSITORY:	the CVS repository, including anoncvs password
+#     CVS_MODULE:	the CVS module to check out
 #
-# It can use the following predefined variables:
+# It may define the following variable:
+#     CVS_TAG:		the CVS tag to check out (default: HEAD)
 #
-# CVS_REPOSITORY_GNU
-# CVS_REPOSITORY_NONGNU
-# CVS_REPOSITORY_SOURCEFORGE
+# After inclusion of this file, the following variables are defined:
+#     CVS_REPOSITORY_GNU
+#     CVS_REPOSITORY_NONGNU
+#     CVS_REPOSITORY_SOURCEFORGE
 
 .if !defined(_PKG_MK_CVS_PACKAGE_MK)
 _PKG_MK_CVS_PACKAGE_MK=	# defined
+
+#
+# defaults for user-visible input variables
+#
+
+CVS_TAG?=		HEAD
+DISTFILES?=		# empty
+NO_FETCH?=		yes
+
+#
+# definition of user-visible output variables
+#
 
 # commonly used repositories
 CVS_REPOSITORY_GNU=	:ext:anoncvs@cvs.gnu.org:/cvsroot
 CVS_REPOSITORY_NONGNU=	${CVS_REPOSITORY_GNU}
 CVS_REPOSITORY_SOURCEFORGE= :pserver:anonymous:@cvs.sourceforge.net:/cvsroot
 
-# default values (internal)
+#
+# defaults for internal variables
+#
+
 _CVS_RSH=		ssh
 _CVS_CMD=		cvs
-_CVS_ENV=		CVS_PASSFILE="${_CVS_PASSFILE}"
-_CVS_ENV+=		CVS_RSH="${_CVS_RSH}"
-_CVS_ENV+=		CVSROOT="${_CVS_ROOT}"
+_CVS_ENV=		CVS_PASSFILE=${_CVS_PASSFILE:Q}
+_CVS_ENV+=		CVS_RSH=${_CVS_RSH:Q}
+_CVS_ENV+=		CVSROOT=${CVS_REPOSITORY:Q}
 _CVS_FLAGS=		-Q
-_CVS_CHECKOUT_FLAGS=	-P -d ${DISTNAME}
-_CVS_PASSFILE=		${WRKDIR}/.cvspass
+_CVS_CHECKOUT_FLAGS=	-P -d ${DISTNAME:Q} -r ${CVS_TAG:Q}
+_CVS_PASSFILE=		${WRKDIR}/.cvs_passwords
 .if !defined(CVS_REPOSITORY)
 PKG_FAIL_REASON=	"No CVS repository specified."
 .elif !defined(CVS_MODULE)
 PKG_FAIL_REASON=	"No CVS module specified."
 .else
-_CVS_ROOT=		${CVS_REPOSITORY}
-_CVS_MODULE=		${CVS_MODULE}
 .endif
-
-# default values (public)
-DISTFILES?=		# empty
-NO_FETCH?=		yes
 
 do-extract:
 	${_PKG_DEBUG}${_PKG_SILENT}set -e;				\
 	cd ${WRKDIR};							\
-	case "${CVS_REPOSITORY}" in					\
+	case ${CVS_REPOSITORY:Q} in					\
 	  :pserver:*)							\
 	    if ${TEST} ! -f "${_CVS_PASSFILE}"; then			\
 	      ${TOUCH} "${_CVS_PASSFILE}";				\
 	    fi;								\
-	    ${ENV} ${_CVS_ENV} ${_CVS_CMD} ${_CVS_FLAGS} login;;	\
+	    ${SETENV} ${_CVS_ENV} ${_CVS_CMD} ${_CVS_FLAGS} login;;	\
 	  *) ;;								\
 	esac;								\
-	${ENV} ${_CVS_ENV} ${_CVS_CMD} ${_CVS_FLAGS} checkout ${_CVS_CHECKOUT_FLAGS} ${_CVS_MODULE}
+	${SETENV} ${_CVS_ENV} ${_CVS_CMD} ${_CVS_FLAGS} checkout ${_CVS_CHECKOUT_FLAGS} ${CVS_MODULE}
 
 .endif
