58752be9b22fb25bc7b01a5434cb33ac76b69814
Evaldo Gardenali <udontknow@users.sourceforge.net>
2005-07-20 17:22:14 +0000

    Added optional USB support through libusb

--- openct/options.mk
+++ openct/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1.1.1 2005/07/20 03:46:44 udontknow Exp $
+# $NetBSD: options.mk,v 1.2 2005/07/20 17:22:15 udontknow Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.openct
-PKG_SUPPORTED_OPTIONS=	pcsc-lite
+PKG_SUPPORTED_OPTIONS=	pcsc-lite usb
 
 .include "../../mk/bsd.options.mk"
 
@@ -9,4 +9,8 @@ PKG_SUPPORTED_OPTIONS=	pcsc-lite
 .include "../../wip/pcsc-lite/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--with-pcsclite=no
+.endif
+
+.if !empty(PKG_OPTIONS:Musb)
+.include "../../devel/libusb/buildlink3.mk"
 .endif
