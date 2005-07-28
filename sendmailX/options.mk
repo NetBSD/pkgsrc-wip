0bc1b4e5ada3b74a1e06e469872d483529ec586f
Adrian Portelli <adrian_p@users.sourceforge.net>
2005-07-28 13:27:46 +0000

    - If we are not doing an SSL enabled build set ac_cv_header_openssl_ssl_h=no   so that sendmailX does not run off, spot openssl and then get its self   in a mess realising we have actaully disabled it.

--- sendmailX/options.mk
+++ sendmailX/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.4 2005/07/19 22:49:42 adrian_p Exp $
+# $NetBSD: options.mk,v 1.5 2005/07/28 13:27:46 adrian_p Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sendmailX
 PKG_SUPPORTED_OPTIONS=	tls sasl
@@ -12,6 +12,8 @@ PKG_SUPPORTED_OPTIONS=	tls sasl
 .	include "../../security/openssl/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-TLS
 CFLAGS+=		-DSM_USE_TLS
+.else
+CONFIGURE_ENV+=		ac_cv_header_openssl_ssl_h=no
 .endif
 
 ###
