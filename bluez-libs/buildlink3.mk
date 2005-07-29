d04ef6f317ad513de15bb4990be564fbc73a8cc0
pancake ;) <poppnk@users.sourceforge.net>
2005-07-29 17:55:42 +0000

    up to 2.18

--- bluez-libs/buildlink3.mk
+++ bluez-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/11/27 14:03:00 poppnk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/07/29 17:55:42 poppnk Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 BLUEZ_LIBS_BUILDLINK3_MK:=	${BLUEZ_LIBS_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nbluez-libs}
 BUILDLINK_PACKAGES+=	bluez-libs
 
 .if !empty(BLUEZ_LIBS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.bluez-libs+=	bluez-libs>=2.11
+BUILDLINK_DEPENDS.bluez-libs+=	bluez-libs>=2.18
 BUILDLINK_PKGSRCDIR.bluez-libs?=	../../wip/bluez-libs
 .endif	# BLUEZ_LIBS_BUILDLINK3_MK
 
