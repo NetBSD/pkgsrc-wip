984c51db19b8d86aee380272a9c7d5645c1a605a
Evaldo Gardenali <udontknow@users.sourceforge.net>
2005-09-11 05:44:44 +0000

    fixed package name

--- libobby/buildlink3.mk
+++ libobby/buildlink3.mk
@@ -1,19 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/09/11 05:26:43 udontknow Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/09/11 05:44:44 udontknow Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
-OBBY_BUILDLINK3_MK:=	${OBBY_BUILDLINK3_MK}+
+LIBOBBY_BUILDLINK3_MK:=	${LIBOBBY_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	obby
+BUILDLINK_DEPENDS+=	libobby
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nobby}
-BUILDLINK_PACKAGES+=	obby
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibobby}
+BUILDLINK_PACKAGES+=	libobby
 
-.if !empty(OBBY_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.obby+=	obby>=0.2.0
-BUILDLINK_PKGSRCDIR.obby?=	../../wip/obby
-.endif	# OBBY_BUILDLINK3_MK
+.if !empty(LIBOBBY_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libobby+=	libobby>=0.2.0
+BUILDLINK_PKGSRCDIR.libobby?=	../../wip/libobby
+.endif	# LIBOBBY_BUILDLINK3_MK
 
 .include "../../wip/net6/buildlink3.mk"
 .include "../../devel/libsigc++2/buildlink3.mk"
