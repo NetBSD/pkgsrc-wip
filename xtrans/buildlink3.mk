cac1df3e2e5b0bbfb4c09f69d039310a4f8745cb
Jeremy C. Reed <reed@reedmedia.net>
2005-10-17 18:58:33 +0000

    Update to use xtrans-0.99.0 from release candidate 0 from modular X.org. (xlibs was experimental anyways.)

--- xtrans/buildlink3.mk
+++ xtrans/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/06/06 16:58:33 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2005/10/17 18:58:33 jeremy-c-reed Exp $
 #
 # This Makefile fragment is included by packages that use xtrans.
 #
@@ -14,7 +14,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxtrans}
 BUILDLINK_PACKAGES+=	xtrans
 
 .if !empty(XTRANS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.xtrans?=		xtrans>=0.1
+BUILDLINK_DEPENDS.xtrans?=		xtrans>=0.99.0
 BUILDLINK_PKGSRCDIR.xtrans?=		../../wip/xtrans
 BUILDLINK_DEPMETHOD.xtrans?=		build
 .endif # XTRANS_BUILDLINK3_MK
