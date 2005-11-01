be8a8a6700009507c8ee2d67926d5f54b56d7775
Leonard Schmidt <lems@gmx.net>
2005-11-01 12:51:04 +0000

    Oops, the glib2 dependency belongs into Makefile.common.

--- bmpx/options.mk
+++ bmpx/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2005/11/01 11:54:44 leonardschmidt Exp $
+# $NetBSD: options.mk,v 1.2 2005/11/01 12:51:04 leonardschmidt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.bmpx
 PKG_SUPPORTED_OPTIONS=	gui
@@ -7,10 +7,8 @@ PKG_SUGGESTED_OPTIONS=	gui
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgui)
-BUILDLINK_DEPENDS.glib2+=	glib2>=2.8.0
 BUILDLINK_DEPENDS.libglade2+=	libglade2>=2.5.1
 
-.  include "../../devel/glib2/buildlink3.mk"
 .  include "../../devel/libglade2/buildlink3.mk"
 .  include "../../devel/pango/buildlink3.mk"
 .  include "../../graphics/cairo/buildlink3.mk"
