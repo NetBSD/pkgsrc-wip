0bc0050d7c8f235b6616f950bca2384c664e8cc6
Jeremy C. Reed <reed@reedmedia.net>
2005-11-10 02:44:05 +0000

    Import scrnsaverproto package.
    This provides the ScrnSaver extension headers from modular X.org X11
    project.

---
+++ scrnsaverproto/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/11/10 02:44:06 jeremy-c-reed Exp $
+
+BUILDLINK_DEPMETHOD.scrnsaverproto?=	build
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+SCRNSAVERPROTO_BUILDLINK3_MK:=	${SCRNSAVERPROTO_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	scrnsaverproto
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nscrnsaverproto}
+BUILDLINK_PACKAGES+=	scrnsaverproto
+
+.if !empty(SCRNSAVERPROTO_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.scrnsaverproto+=	scrnsaverproto>=1.0.1
+BUILDLINK_PKGSRCDIR.scrnsaverproto?=	../../wip/scrnsaverproto
+.endif	# SCRNSAVERPROTO_BUILDLINK3_MK
+
+.include "../../wip/xproto/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
