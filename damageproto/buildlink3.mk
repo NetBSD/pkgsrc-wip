06652c4ae7c46290da9b9ac78944e5794acd1bab
Jeremy C. Reed <reed@reedmedia.net>
2005-11-10 01:39:50 +0000

    Import package: damageproto
    This provides the Damage extension headers from modular X.org X11
    project.

---
+++ damageproto/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/11/10 01:39:50 jeremy-c-reed Exp $
+
+BUILDLINK_DEPMETHOD.damageproto?=	build
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+DAMAGEPROTO_BUILDLINK3_MK:=	${DAMAGEPROTO_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	damageproto
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ndamageproto}
+BUILDLINK_PACKAGES+=	damageproto
+
+.if !empty(DAMAGEPROTO_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.damageproto+=	damageproto>=1.0.1
+BUILDLINK_PKGSRCDIR.damageproto?=	../../wip/damageproto
+.endif	# DAMAGEPROTO_BUILDLINK3_MK
+
+.include "../../wip/xproto/buildlink3.mk"
+.include "../../wip/fixesproto/buildlink3.mk" 
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
