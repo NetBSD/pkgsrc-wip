21c05f30109313d21ca54098c2c19b39f03ffdf0
Jeremy C. Reed <reed@reedmedia.net>
2005-11-10 03:33:38 +0000

    Import package: recordproto.
    This provides the Record extension headers from modular X.org X11
    project.

---
+++ recordproto/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/11/10 03:33:38 jeremy-c-reed Exp $
+
+BUILDLINK_DEPMETHOD.recordproto?=	build
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+RECORDPROTO_BUILDLINK3_MK:=	${RECORDPROTO_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	recordproto
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nrecordproto}
+BUILDLINK_PACKAGES+=	recordproto
+
+.if !empty(RECORDPROTO_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.recordproto+=	recordproto>=1.13.1
+BUILDLINK_PKGSRCDIR.recordproto?=	../../wip/recordproto
+.endif	# RECORDPROTO_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
