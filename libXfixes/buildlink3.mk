0c6a8b2745ae80fbf585bacb86f394dbac2efb3e
Jeremy C. Reed <reed@reedmedia.net>
2005-11-10 02:03:51 +0000

    Update to libXfixes-3.0.0 from modular X.org (release candidate 1) instead of the dead xlibs.
    Improve description and comment.
    
    Adds manual page.

--- libXfixes/buildlink3.mk
+++ libXfixes/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/08/17 21:26:57 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/11/10 02:03:51 jeremy-c-reed Exp $
 #
 # This Makefile fragment is included by packages that use libXfixes.
 #
@@ -12,11 +12,10 @@ BUILDLINK_DEPENDS+=	libXfixes
 
 .if !empty(LIBXFIXES_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=			libXfixes
-BUILDLINK_DEPENDS.libXfixes?=		libXfixes>=2.0.0
+BUILDLINK_DEPENDS.libXfixes?=		libXfixes>=3.0.0
 BUILDLINK_PKGSRCDIR.libXfixes?=		../../wip/libXfixes
 
 .include "../../wip/libX11/buildlink3.mk"
-.include "../../x11/fixesext/buildlink3.mk"
 
 .endif # LIBXFIXES_BUILDLINK3_MK
 
