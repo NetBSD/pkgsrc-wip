92082b04013ee823a55947ad02e11b015a8e1cbb
Jeremy C. Reed <reed@reedmedia.net>
2005-11-10 01:51:34 +0000

    Fix path from my testing "new".

--- xf86vidmodeproto/buildlink3.mk
+++ xf86vidmodeproto/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/11/10 01:49:53 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/11/10 01:51:34 jeremy-c-reed Exp $
 
 BUILDLINK_DEPMETHOD.xf86vidmodeproto?=	build
 
@@ -14,7 +14,7 @@ BUILDLINK_PACKAGES+=	xf86vidmodeproto
 
 .if !empty(XF86VIDMODEPROTO_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.xf86vidmodeproto+=	xf86vidmodeproto>=2.2.1
-BUILDLINK_PKGSRCDIR.xf86vidmodeproto?=	../../new/xf86vidmodeproto
+BUILDLINK_PKGSRCDIR.xf86vidmodeproto?=	../../wip/xf86vidmodeproto
 .endif	# XF86VIDMODEPROTO_BUILDLINK3_MK
 
 .include "../../wip/xproto/buildlink3.mk"
