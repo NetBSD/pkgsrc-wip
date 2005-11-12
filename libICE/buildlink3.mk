7839d4b295ae7492cf7e76d38404295a1415c96d
Jeremy C. Reed <reed@reedmedia.net>
2005-11-12 01:31:33 +0000

    Update to modular X.org version (release candidate 2) instead of deprecated xlibs. Sorry no changelog provided here -- this is a work in progress. No longer installs include/X11/ICE/ICEtrans.h.
    Remove buildlink includes of libX11 and libXau and comment out
    threads usage.
    
    Remove all buildlink includes from buildlink3.mk file for now.

--- libICE/buildlink3.mk
+++ libICE/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/10 18:16:17 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/11/12 01:31:33 jeremy-c-reed Exp $
 #
 # This Makefile fragment is included by packages that use libICE.
 #
@@ -14,13 +14,8 @@ BUILDLINK_DEPENDS+=	libICE
 
 .if !empty(LIBICE_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=			libICE
-BUILDLINK_DEPENDS.libICE+=		libICE>=6.3.1nb1
+BUILDLINK_DEPENDS.libICE+=		libICE>=0.99.1
 BUILDLINK_PKGSRCDIR.libICE?=		../../wip/libICE
-
-.include "../../wip/libX11/buildlink3.mk"
-.include "../../wip/libXau/buildlink3.mk"
-.include "../../wip/xproto/buildlink3.mk"
-.include "../../wip/xtrans/buildlink3.mk"
 
 .endif # LIBICE_BUILDLINK3_MK
 
