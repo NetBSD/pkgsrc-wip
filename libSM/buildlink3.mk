70d8e259af0af839c7e8525d7e6e9d153ac1e4cf
Jeremy C. Reed <reed@reedmedia.net>
2005-11-12 01:35:13 +0000

    Improve description and comment. This is the X Session Management Library.
    Update to use modular x.org version (release candidate 2)
    instead of deprecated xlibs. No changes listed here --
    is a work in progress.
    
    Comment out or remove buildlink includes for libX11 and threads.
    Add buildlink include for xproto and xtrans.
    
    Add comment to PLIST.
    
    Remove some includes from buildlink3.mk file.
    Just include libICE.

--- libSM/buildlink3.mk
+++ libSM/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/02/10 18:19:04 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/11/12 01:35:13 jeremy-c-reed Exp $
 #
 # This Makefile fragment is included by packages that use libSM.
 #
@@ -14,15 +14,11 @@ BUILDLINK_DEPENDS+=	libSM
 
 .if !empty(LIBSM_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=			libSM
-BUILDLINK_DEPENDS.libSM+=		libSM>=6.0.1
+BUILDLINK_DEPENDS.libSM+=		libSM>=0.99.2
 BUILDLINK_PKGSRCDIR.libSM?=		../../wip/libSM
 
-.include "../../wip/libICE/buildlink3.mk"
-.include "../../wip/libX11/buildlink3.mk"
-.include "../../mk/pthread.buildlink3.mk"
-
-LIBS+=					${BUILDLINK_LDADD.pthread}
-
 .endif # LIBSM_BUILDLINK3_MK
+
+.include "../../wip/libICE/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
