63cd7eca3e7370fcebff36e58be721db5eddcc06
David Howland <dhowland@users.sourceforge.net>
2005-11-13 05:56:49 +0000

    PyOSD is a python module for displaying text on your X display, much like the "On Screen Displays" used on TVs and some monitors.

---
+++ py-osd/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/11/13 05:56:49 dhowland Exp $
+
+.include "../../lang/python/pyversion.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+PY_OSD_BUILDLINK3_MK:=	${PY_OSD_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	pyosd
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npyosd}
+BUILDLINK_PACKAGES+=	pyosd
+
+.if !empty(PY_OSD_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.pyosd+=	${PYPKGPREFIX}-pyosd>=0.2.14
+BUILDLINK_PKGSRCDIR.pyosd?=	../../wip/py-osd
+.endif	# PY_OSD_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
