3eab462dfebe2c9f4cc8833cb081bb36e1fa05d5
Geert Hendrickx <geert@hendrickx.be>
2005-11-28 13:52:43 +0000

    reindent.

--- jdk14/options.mk
+++ jdk14/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2005/08/11 15:17:05 ghen Exp $
+# $NetBSD: options.mk,v 1.3 2005/11/28 13:52:43 ghen Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.jdk14
 PKG_SUPPORTED_OPTIONS=		jdk14-jce jdk14-plugin
@@ -11,8 +11,8 @@ PKG_OPTIONS_LEGACY_VARS+=	JDK14_USE_JCE:jdk14-jce
 ### Java(TM) Cryptography Extension (JCE)
 ###
 .if !empty(PKG_OPTIONS:Mjdk14-jce)
-DISTFILES+=	jce_policy-1_4_2.zip
-PLIST_SRC+=	PLIST.jce
+DISTFILES+=			jce_policy-1_4_2.zip
+PLIST_SRC+=			PLIST.jce
 post-build:
 	cd ${WRKDIR}/jce ; ${PAX} -rw -pe . ${JDKIMAGEDIR}/jre/lib/security
 .endif
