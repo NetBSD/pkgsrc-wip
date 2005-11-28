b98e019abce4ce46308b698325d8b39e10b1ec05
David Howland <dhowland@users.sourceforge.net>
2005-11-28 07:47:59 +0000

    Further rewriting of joystick plugin.

--- freevo/files/pygamejoy.py
+++ freevo/files/pygamejoy.py
@@ -2,6 +2,28 @@
 # -----------------------------------------------------------------------
 # pygamejoy.py - Alternative joystick plugin by David Howland 
 # -----------------------------------------------------------------------
+# Activate this module with the following lines in your config file:
+#   plugin.remove('joy')
+#   plugin.activate('pygamejoy')
+# Configure this module with lines such as these:
+#   JOY_DEV = 1
+#   JOY_CMDS = {
+#     'a2 neg'         : 'UP'      ,
+#     'a2 pos'         : 'DOWN'    ,
+#     'a1 neg'         : 'LEFT'    ,
+#     'a1 pos'         : 'RIGHT'   ,
+#     'h1 up'          : 'UP'      ,
+#     'h1 down'        : 'DOWN'    ,
+#     'h1 left'        : 'LEFT'    ,
+#     'h1 right'       : 'RIGHT'   ,
+#     'button 1'       : 'PLAY'    ,
+#     'button 2'       : 'PAUSE'   ,
+#     'button 3'       : 'STOP'    ,
+#     'button 4'       : 'ENTER'   ,
+#     'button 9'       : 'EXIT'    ,
+#     'button 10'      : 'SELECT'  ,
+#   }
+# -----------------------------------------------------------------------
 # Freevo - A Home Theater PC framework
 # Copyright (C) 2003 Krister Lagerstrom, et al. 
 # Please see the file freevo/Docs/CREDITS for a complete list of authors.
@@ -63,11 +85,11 @@ class PluginInterface(plugin.DaemonPlugin):
         print "Using joystick:", self.Joystick.get_name()
         print "with: %d axis %d hats %d buttons" % (self.Joystick.get_numaxes(),self.Joystick.get_numhats(),self.Joystick.get_numbuttons())
 
-        self.HMIN = 0
-        self.HMAX = 0
-        self.VMIN = 0
-        self.VMAX = 0
-        self.UnCal = 1
+        self.MIN = []
+        self.MAX = []
+        for i in range(self.Joystick.get_numaxes()):
+	    self.MIN.append(-0.5)
+	    self.MAX.append(0.5)
 
         # ok, joystick is working
         plugin.DaemonPlugin.__init__(self)
@@ -79,46 +101,48 @@ class PluginInterface(plugin.DaemonPlugin):
     def poll(self):
         command = ''
 
-	# buttons are first priority
+        # buttons are first priority
         for i in range(self.Joystick.get_numbuttons()):
             if(self.Joystick.get_button(i)):
-                if ((self.UnCal == 1) & (i == 0)):
-                    self.UnCal = 0
-                else:
-                    button = "button %d" % (i + 1)
-                    command = config.JOY_CMDS.get(button, '')
+                button = "button %d" % (i + 1)
+                command = config.JOY_CMDS.get(button, '')
 
-	# axis are last priority
+        # hats are second priority
         if command == '':
-            for i in range(2):
+	    hatstring = ''
+            for i in range(self.Joystick.get_numhats()):
+                value = self.Joystick.get_hat(i)
+                if(value[1] == 1):
+                    hatstring = "h%d up" % (i + 1)
+                if(value[1] == -1):
+                    hatstring = "h%d down" % (i + 1)
+                if(value[0] == -1):
+                    hatstring = "h%d left" % (i + 1)
+                if(value[0] == 1):
+                    hatstring = "h%d right" % (i + 1)
+            if hatstring != '':
+                command = config.JOY_CMDS.get(hatstring, '')
+
+        # axis are last priority
+        if command == '':
+	    joystring = ''
+            for i in range(self.Joystick.get_numaxes()):
                 value = self.Joystick.get_axis(i)
-                if (i == 0):
-                    if (value > self.HMAX):
-                        self.HMAX = value
-                    if (value < self.HMIN):
-                        self.HMIN = value
-                    if (value > (self.HMAX*0.9)):
-                        if self.UnCal != 1:
-                            command = config.JOY_CMDS['right']
-                    if (value < (self.HMIN*0.9)):
-                        if self.UnCal != 1:
-                            command = config.JOY_CMDS['left']
-                if (i == 1):
-                    if (value > self.VMAX):
-                        self.VMAX = value
-                    if (value < self.VMIN):
-                        self.VMIN = value
-                    if (value > (self.HMAX*0.9)):
-                        if self.UnCal != 1:
-                            command = config.JOY_CMDS['down']
-                    if (value < (self.HMIN*0.9)):
-                        if self.UnCal != 1:
-                            command = config.JOY_CMDS['up']
+                if (value > self.MAX[i]):
+                    self.MAX[i] = value
+                if (value < self.MIN[i]):
+                    self.MIN[i] = value
+                if (value > (self.MAX[i] * 0.8)):
+                    joystring = "a%d pos" % (i + 1)
+                if (value < (self.MIN[i] * 0.8)):
+                    joystring = "a%d neg" % (i + 1)
+            if joystring != '':
+                command = config.JOY_CMDS.get(joystring, '')
 
 	# if we have something to do
         if command != '':
             command = rc.key_event_mapper(command)
             if command:
                 rc.post_event(command)
-		sleep(0.1)
+		sleep(0.2)
 
