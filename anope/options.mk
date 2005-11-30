818fb40cebf5308798b365e6ecb664700b44dbf3
Adrian Portelli <adrian_p@users.sourceforge.net>
2005-11-30 19:40:29 +0000

    Move to ${PREFIX}/lib for installation Simplify options

--- anope/options.mk
+++ anope/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.3 2005/11/28 23:12:39 adrian_p Exp $
+# $NetBSD: options.mk,v 1.4 2005/11/30 19:40:29 adrian_p Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.anope 
-PKG_SUPPORTED_OPTIONS=	mysql unrealircd ircd-hybrid db-encryption
+PKG_OPTIONS_VAR=	PKG_OPTIONS.anope
+PKG_SUPPORTED_OPTIONS=	mysql db-encryption
 
 .include "../../mk/bsd.options.mk"
 
@@ -19,18 +19,4 @@ CONFIGURE_ARGS+=	--without-mysql
 ###
 .if !empty(PKG_OPTIONS:Mmysql) && !empty(PKG_OPTIONS:Mdb-encryption)
 CONFIGURE_ARGS+=	--with-encryption
-.endif
-
-###
-### Use UnrealIRCd IRC server
-###
-.if !empty(PKG_OPTIONS:Munrealircd)
-DEPENDS+=		unrealircd>=3.1:../../wip/unrealircd
-.endif
-
-###
-### Use ircd-hybrid IRC server
-###
-.if !empty(PKG_OPTIONS:Mircd-hybrid)
-DEPENDS+=		ircd-hybrid>=7.0:../../chat/ircd-hybrid
 .endif
