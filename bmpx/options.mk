4f5d807a0a83214f28739323807b0af34d2d40f4
Leonard Schmidt <lems@gmx.net>
2005-12-13 15:44:16 +0000

    options.mk: Gah, ``-gui'' needs GTK2 support, too, because of ``libskinned''.             Also add a ``debug'' option. Makefile.common: Add back x11/gtk2. TODO: Remove GStreamer note; BROKEN in Makefile should be enough.

--- bmpx/options.mk
+++ bmpx/options.mk
@@ -1,10 +1,14 @@
-# $NetBSD: options.mk,v 1.3 2005/11/01 12:54:25 leonardschmidt Exp $
+# $NetBSD: options.mk,v 1.4 2005/12/13 15:44:16 leonardschmidt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.bmpx
-PKG_SUPPORTED_OPTIONS=	gui
+PKG_SUPPORTED_OPTIONS=	debug gui
 PKG_SUGGESTED_OPTIONS=	gui
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug
+.endif
 
 .if !empty(PKG_OPTIONS:Mgui)
 BUILDLINK_DEPENDS.libglade2+=	libglade2>=2.5.1
@@ -12,7 +16,6 @@ BUILDLINK_DEPENDS.libglade2+=	libglade2>=2.5.1
 .  include "../../devel/libglade2/buildlink3.mk"
 .  include "../../devel/pango/buildlink3.mk"
 .  include "../../graphics/cairo/buildlink3.mk"
-.  include "../../x11/gtk2/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-gui
 .else
 CONFIGURE_ARGS+=	--disable-gui
