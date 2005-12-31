041e89c76035fbf9f71e0de30fc39229348f9bf1
Mahesh Chittur <mchittur@users.sourceforge.net>
2005-12-31 03:00:40 +0000

    gdal updated to 1.3.1

--- gdal-lib/buildlink3.mk
+++ gdal-lib/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2005/11/06 10:41:45 minskim Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.8.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.gdal-lib?=	build
+# $NetBSD: buildlink3.mk,v 1.7 2005/12/31 03:00:41 mchittur Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GDAL_LIB_BUILDLINK3_MK:=	${GDAL_LIB_BUILDLINK3_MK}+
@@ -21,18 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngdal-lib}
 BUILDLINK_PACKAGES+=	gdal-lib
 
 .if !empty(GDAL_LIB_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gdal-lib+=	gdal-lib>=1.2.6
-BUILDLINK_PKGSRCDIR.gdal-lib?=	../../wip/gdal-lib
+BUILDLINK_DEPENDS.gdal-lib+=	gdal-lib>=1.3.1
+BUILDLINK_PKGSRCDIR.gdal-lib?=	../../wip_netbsd/gdal-lib
 .endif	# GDAL_LIB_BUILDLINK3_MK
-
-# XXX
-# XXX Uncomment and keep only the buildlink3 lines below which are directly
-# XXX needed for dependencies to compile, link, and run.  If this package
-# XXX provides a wrappered API or otherwise does not expose the APIs of the
-# XXX buildlink3 lines below to dependencies, remove them.
-# XXX
-#.include "../../misc/proj/buildlink3.mk"
-#.include "../../geography/geos/buildlink3.mk"
-#.include "../../textproc/xerces-c/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
