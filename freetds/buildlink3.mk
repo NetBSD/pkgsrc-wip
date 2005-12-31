e76a14bb8740cc4885988ef71d1da16931fa5d8e
Johnny C. Lam <jlamwww@users.sourceforge.net>
2005-12-31 05:10:57 +0000

    This package is used to track the latest snapshot of FreeTDS.
    FreeTDS is a set of libraries that allows programs to natively talk
    to Microsoft SQL Server and Sybase databases.
    
    Technically speaking, FreeTDS is an open source implementation of the
    TDS (Tabular Data Stream) protocol used by these databases for their
    own clients.  It supports many different flavors of the protocol and
    three APIs -- DB-Lib, CT-Lib, and ODBC -- to access it.

---
+++ freetds/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/12/31 05:10:57 jlamwww Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+FREETDS_BUILDLINK3_MK:=	${FREETDS_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	freetds
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nfreetds}
+BUILDLINK_PACKAGES+=	freetds
+
+.if !empty(FREETDS_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.freetds+=	freetds>=0.63nb2
+BUILDLINK_PKGSRCDIR.freetds?=	../../databases/freetds
+.endif	# FREETDS_BUILDLINK3_MK
+
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
