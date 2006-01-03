51133619137e01f8470c8684b80adb5af304e67a
David Brownlee <abs@absd.org>
2006-01-03 10:26:21 +0000

    Update nagios to 1.3 * Notification bug fix for volatile services * Embedded Perl bug fix in mini_epn * Minor bug fix in trends CGI * Bug with in retention of program state * Bug fix in PostgreSQL code * Fixed auto-increment fields in MySQL table creation script * Added service name filter to status CGI * URL encoding bug fix * Bug fix in time change compensation code * Bug fix for segfault in CGIs where plugin output was NULL * Fixed log message error at start of flapping

--- nagios-base/files/nagios.sh
+++ nagios-base/files/nagios.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: nagios.sh,v 1.2 2005/10/28 10:59:00 ykomatsu Exp $
+# $NetBSD: nagios.sh,v 1.3 2006/01/03 10:26:21 absd Exp $
 #
 # PROVIDE: nagios
 # REQUIRE: DAEMON
@@ -13,12 +13,19 @@ name="nagios"
 rcvar=$name
 command="@PREFIX@/bin/${name}"
 pidfile="@VARBASE@/run/${name}/${name}.lock"
+start_precmd="nagios_make_rundir"
 config="@PKG_SYSCONFDIR@/${name}.cfg"
 
 command_args="-d $config"
  
 test_cmd="nagios_test"
 extra_commands="test"
+
+nagios_make_rundir()
+{   
+        mkdir -p "@VARBASE@/var/run/${name}"
+	chown @NAGIOSADM_USER@:@NAGIOSADM_GROUP@ "@VARBASE@/var/run/${name}/rw"
+}
 
 nagios_test()
 {
