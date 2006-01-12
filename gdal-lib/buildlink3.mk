9943c535dd3c1a8902579a14dbbd1b371e26dd8f
Berndt Josef Wulf <bwulf@users.sourceforge.net>
2006-01-12 14:31:13 +0000

    Fixed BUILDLINK_PKGSRCDIR.gdal-lib to point to correct location

--- gdal-lib/buildlink3.mk
+++ gdal-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2005/12/31 03:00:41 mchittur Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2006/01/12 14:31:13 bwulf Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GDAL_LIB_BUILDLINK3_MK:=	${GDAL_LIB_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	gdal-lib
 
 .if !empty(GDAL_LIB_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gdal-lib+=	gdal-lib>=1.3.1
-BUILDLINK_PKGSRCDIR.gdal-lib?=	../../wip_netbsd/gdal-lib
+BUILDLINK_PKGSRCDIR.gdal-lib?=	../../wip/gdal-lib
 .endif	# GDAL_LIB_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
