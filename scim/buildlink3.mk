6e8b684d44d28b65b15697d183cb85f42b303b09
Rui-Xiang Guo <rxg@users.sourceforge.net>
2006-02-01 11:26:04 +0000

    Intial import scim-1.4.4. SCIM is Smart Common Input Method.
    Currently the SCIM project has a wide range of input methods (some may need
    other libraries), covering more than 30 languages, including
    (Simplified/Traditional) Chinese, Japanese, Korean and many European languages.
    What's more, Composing/Dead key support is one of the built-in features.
    In addition, several projects have been established to design IMEngines for
    SCIM and others supply their own SCIM plugins.

---
+++ scim/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/02/01 11:26:04 rxg Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+SCIM_BUILDLINK3_MK:=	${SCIM_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	scim
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nscim}
+BUILDLINK_PACKAGES+=	scim
+
+.if !empty(SCIM_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.scim+=	scim>=1.4.4
+BUILDLINK_RECOMMENDED.scim+=	scim>=1.4.4
+BUILDLINK_PKGSRCDIR.scim?=	../../wip/scim
+.endif	# SCIM_BUILDLINK3_MK
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
