af6b1b36af017e829a6c6e7a15da142ae3b240f1
Matthew Luckie <matthewluckie@users.sourceforge.net>
2006-02-01 09:24:21 +0000

    import of gaim-2.0.0beta2 for testing in pkgsrc-wip before the final 2.0.0 is released.

---
+++ gaim/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.3 2006/02/01 09:24:21 matthewluckie Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GAIM_BUILDLINK3_MK:=	${GAIM_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	gaim
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngaim}
+BUILDLINK_PACKAGES+=	gaim
+
+.if !empty(GAIM_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.gaim+=	gaim>=1.1.2
+BUILDLINK_RECOMMENDED.gaim+=	gaim>=1.5.0nb3
+BUILDLINK_PKGSRCDIR.gaim?=	../../chat/gaim
+
+PRINT_PLIST_AWK+=	/^@dirrm lib\/gaim$$/ \
+				{ print "@comment in gaim: " $$0; next }
+.endif  # GAIM_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
