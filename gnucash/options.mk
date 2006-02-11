551617e6121a49a7364b741503c91457bde8408c
Francis GUDIN <fgudin@nerim.net>
2006-02-11 21:26:42 +0000

    Forgot a location in the previous commit. Thanks Thomas for pointing it !

--- gnucash/options.mk
+++ gnucash/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2006/02/11 20:39:03 fgudin Exp $
+# $NetBSD: options.mk,v 1.3 2006/02/11 21:26:42 fgudin Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnucash
 PKG_SUPPORTED_OPTIONS=	libofx
@@ -9,7 +9,7 @@ PKG_SUPPORTED_OPTIONS=	libofx
 ### Whether to build with the OFX importation module.
 ###
 
-.if !empty(PKG_OPTIONS:Mofx)
+.if !empty(PKG_OPTIONS:Mlibofx)
 .  include "../../finance/libofx/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-ofx
 .endif
