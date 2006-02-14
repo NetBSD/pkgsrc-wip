ba7389815559e30497c2c762a72c8f4ec723b63c
Andreas Hallmann <ahallmann@users.sourceforge.net>
2006-02-14 12:39:27 +0000

    Initial commit of hylafax-devel-4.2.5

---
+++ hylafax-devel/files/hylafax.sh
@@ -0,0 +1,35 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: hylafax.sh,v 1.1 2006/02/14 12:39:27 ahallmann Exp $
+#
+# PROVIDE: hylafax
+# REQUIRE: DAEMON
+
+command=${1:-start}
+
+case ${command} in
+start)
+	if [ -x @PREFIX@/libexec/hfaxd -a -f /var/spool/hylafax/etc/config ] ; then
+	    echo -n 'Starting hfaxd'
+	    @PREFIX@/libexec/hfaxd -i 4559 -o 4557 -s 444 
+
+	    echo ' faxq.'
+	    @PREFIX@/sbin/faxq
+	fi
+	;;
+stop)
+	PIDS=`ps -ax -o pid,command|egrep '[ /](faxq|hfaxd) '|awk '{printf " " $1}'`
+	if [ -n "$PIDS" ];then
+		echo "Stopping hylafax pids$PIDS."
+		kill -TERM $PIDS
+	else
+		echo "hylafax not running?"
+	fi
+	;;
+restart)
+	( $0 stop )
+	sleep 1
+	$0 start
+	;;
+esac
+exit 0
