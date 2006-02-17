90920e1e6931bd336af8711118d0af0ecccc0292
Martti Kuparinen <martti@NetBSD.org>
2006-02-17 06:51:54 +0000

    Fixed warnings found by pkglint -Wall.

--- xfce4-genmon-plugin/buildlink3.mk
+++ xfce4-genmon-plugin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/10/26 06:32:49 dhowland Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2006/02/17 06:51:54 marttikuparinen Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_GENMON_PLUGIN_BUILDLINK3_MK:=	${XFCE4_GENMON_PLUGIN_BUILDLINK3_MK}+
@@ -12,10 +12,10 @@ BUILDLINK_PACKAGES+=	xfce4-genmon-plugin
 
 .if !empty(XFCE4_GENMON_PLUGIN_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.xfce4-genmon-plugin+=	xfce4-genmon-plugin>=1.1
-BUILDLINK_PKGSRCDIR.xfce4-genmon-plugin?=	../../sysutils/xfce4-genmon-plugin
+BUILDLINK_PKGSRCDIR.xfce4-genmon-plugin?=	../../wip/xfce4-genmon-plugin
 .endif	# XFCE4_GENMON_PLUGIN_BUILDLINK3_MK
 
 .include "../../x11/xfce4-panel/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
--- xfce4-taskmanager/buildlink3.mk
+++ xfce4-taskmanager/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/10/27 04:43:27 dhowland Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2006/02/17 06:51:54 marttikuparinen Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XFCE4_TASKMANAGER_BUILDLINK3_MK:=	${XFCE4_TASKMANAGER_BUILDLINK3_MK}+
@@ -12,10 +12,10 @@ BUILDLINK_PACKAGES+=	xfce4-taskmanager
 
 .if !empty(XFCE4_TASKMANAGER_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.xfce4-taskmanager+=	xfce4-appfinder>=0.3.1
-BUILDLINK_PKGSRCDIR.xfce4-taskmanager?=	../../sysutils/xfce4-taskmanager
+BUILDLINK_PKGSRCDIR.xfce4-taskmanager?=	../../wip/xfce4-taskmanager
 .endif	# XFCE4_TASKMANAGER_BUILDLINK3_MK
 
 .include "../../x11/libxfce4gui/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
