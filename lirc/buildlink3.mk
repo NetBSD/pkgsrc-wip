dcb771a9b709ecb1773e08b02c3ae8ae5baa5fb8
pancake ;) <poppnk@users.sourceforge.net>
2006-02-22 15:56:09 +0000

    Initial import of LIRC
    LIRC is a package that supports receiving and sending IR signals of the most
    common IR remote controls. It contains a daemon that decodes and sends IR signals,
    a mouse daemon that translates IR signals to mouse movements and a couple of
    user programs that allow to control your computer with a remote control.

---
+++ lirc/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/02/22 15:56:09 poppnk Exp $
+# XXX
+# XXX This file was created automatically using createbuildlink-3.8.
+# XXX After this file has been verified as correct, the comment lines
+# XXX beginning with "XXX" should be removed.  Please do not commit
+# XXX unverified buildlink3.mk files.
+# XXX
+# XXX Packages that only install static libraries or headers should
+# XXX include the following line:
+# XXX
+# XXX	BUILDLINK_DEPMETHOD.lirc?=	build
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIRC_BUILDLINK3_MK:=	${LIRC_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	lirc
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlirc}
+BUILDLINK_PACKAGES+=	lirc
+
+.if !empty(LIRC_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.lirc+=	lirc>=0.8.0
+BUILDLINK_PKGSRCDIR.lirc?=	../../wip/lirc
+.endif	# LIRC_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
