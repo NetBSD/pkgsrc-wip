69615c99d5f48a8e9687f39df192c2847ffc8180
Thomas Klausner <wiz@NetBSD.org>
2006-02-27 22:59:48 +0000

    Update to 0.97rc1. Add SDL option. News: no idea, sorry. Except for the SDL port.

---
+++ InfoNES/options.mk
@@ -0,0 +1,26 @@
+# $NetBSD: options.mk,v 1.1 2006/02/27 22:59:48 thomasklausner Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.InfoNES
+PKG_OPTIONS_REQUIRED_GROUPS=	frontend
+PKG_OPTIONS_GROUP.frontend=	gtk sdl
+PKG_SUGGESTED_OPTIONS+=	gtk
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgtk)
+SUBST_CLASSES+=		dsp
+SUBST_STAGE.dsp=	post-patch
+SUBST_MESSAGE.dsp=	Adjusting sound device path.
+SUBST_FILES.dsp=	src/linux/InfoNES_System_Linux.cpp
+SUBST_SED.dsp=		-e "s|/dev/dsp|${DEVOSSAUDIO}|"
+
+SRC_SUBDIR=		linux
+
+.include "../../x11/gtk/buildlink3.mk"
+.include "../../mk/ossaudio.buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mgtk)
+SRC_SUBDIR=		sdl
+.include "../../devel/SDL/buildlink3.mk"
+.endif
