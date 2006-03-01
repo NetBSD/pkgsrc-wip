1610a4e5bc40fc3552546cfb25fe815c66a7509e
Jeremy C. Reed <reed@reedmedia.net>
2006-03-01 23:46:54 +0000

    Updated to 1.0.0.
    Add include of SM buildlink3.mk in the buildlink3.mk.
    
    Installs makestrs tool.
    
    Changes:
    
    2005-12-14  Kevin E. Martin  <kem-at-freedesktop-dot-org>
    
    	* configure.ac:
    	Update package version number for final X11R7 release candidate.
    
    2005-12-10  Kevin E. Martin  <kem-at-freedesktop-dot-org>
    
    	* configure.ac:
    	Add configure option to install makestrs and its manpage.
    
    	* util/Makefile.am:
    	Add makestrs man page.
    
    2005-12-07  Kevin E. Martin  <kem-at-freedesktop-dot-org>
    
    	* configure.ac:
    	* xt.pc.in:
    	Add configure option to set the build's app-default dir, and put
    	the result in the xt.pc file for applications to use.
    
    2005-12-06  Kevin E. Martin  <kem-at-freedesktop-dot-org>
    
    	* man/Makefile.am:
    	Change *man_SOURCES ==> *man_PRE to fix autotools warnings.
    
    2005-12-03  Kevin E. Martin  <kem-at-freedesktop-dot-org>
    
    	* configure.ac:
    	Update package version number for X11R7 RC3 release.
    
    2005-12-02  Kevin E. Martin  <kem-at-freedesktop-dot-org>
    
    	* configure.ac:
    	* src/Makefile.am:
    	Add check and cflags for malloc(0) returning NULL.
    
    2005-11-19  Kevin E. Martin  <kem-at-freedesktop-dot-org>
    
    	* xt.pc.in:
    	Update pkgconfig files to separate library build-time dependencies
    	from application build-time dependencies.

--- libXt/buildlink3.mk
+++ libXt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2005/11/12 01:42:32 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2006/03/01 23:46:54 jeremy-c-reed Exp $
 #
 # This Makefile fragment is included by packages that use libXt.
 #
@@ -16,6 +16,8 @@ BUILDLINK_PACKAGES+=	libXt
 .if !empty(LIBXT_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.libXt+=		libXt>=0.99.2
 BUILDLINK_PKGSRCDIR.libXt?=		../../wip/libXt
+
+.include "../../wip/libSM/buildlink3.mk"
 .endif # LIBXT_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
