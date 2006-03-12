cb06d066916aba3feef9db0a45ed330ed939d76e
Pippijn van Steenhoven <pip88nl@gmail.com>
2006-03-12 11:49:23 +0000

    updating to version 1.5 and adding an option to disable pngs

--- jwm/options.mk
+++ jwm/options.mk
@@ -1,10 +1,16 @@
-# $NetBSD: options.mk,v 1.1 2005/11/27 10:08:44 leonardschmidt Exp $
+# $NetBSD: options.mk,v 1.2 2006/03/12 11:49:23 pippijn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.jwm
-PKG_SUPPORTED_OPTIONS=	debug
+PKG_SUPPORTED_OPTIONS=	debug nopng
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-debug
+.endif
+
+.if !empty(PKG_OPTIONS:Mnopng)
+CONFIGURE_ARGS+=        --disable-png
+.else
+CC+=                    -lpng -lz
 .endif
