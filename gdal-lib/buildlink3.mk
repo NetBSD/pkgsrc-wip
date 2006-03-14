b3f44d6e1a6c127b4d035e782136a5fb8247b04e
Thomas Klausner <wiz@NetBSD.org>
2006-03-14 23:37:43 +0000

    Fix path to itself.

--- gdal-lib/buildlink3.mk
+++ gdal-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2006/03/09 19:15:23 mchittur Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2006/03/14 23:37:43 thomasklausner Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GDAL_LIB_BUILDLINK3_MK:=	${GDAL_LIB_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	gdal-lib
 
 .if !empty(GDAL_LIB_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gdal-lib+=	gdal-lib>=1.3.1
-BUILDLINK_PKGSRCDIR.gdal-lib?=	../../wip_netbsd/gdal-lib
+BUILDLINK_PKGSRCDIR.gdal-lib?=	../../wip/gdal-lib
 .endif	# GDAL_LIB_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
