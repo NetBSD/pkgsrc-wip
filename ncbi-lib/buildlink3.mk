4d8f8c91aa380ebd7ca3300ed0afd36b08777801
Mahesh Chittur <mchittur@users.sourceforge.net>
2006-03-28 05:15:17 +0000

    ncbi-lib upgraded to 20060301 build number

--- ncbi-lib/buildlink3.mk
+++ ncbi-lib/buildlink3.mk
@@ -1,6 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2005/08/22 20:22:59 mchittur Exp $
-
-BUILDLINK_DEPMETHOD.ncbi-lib?=	build
+# $NetBSD: buildlink3.mk,v 1.5 2006/03/28 05:15:17 mchittur Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 NCBI_LIB_BUILDLINK3_MK:=	${NCBI_LIB_BUILDLINK3_MK}+
@@ -13,10 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nncbi-lib}
 BUILDLINK_PACKAGES+=	ncbi-lib
 
 .if !empty(NCBI_LIB_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.ncbi-lib+=	ncbi-lib>=20050605
+BUILDLINK_DEPENDS.ncbi-lib+=	ncbi-lib>=20060301
 BUILDLINK_PKGSRCDIR.ncbi-lib?=	../../wip/ncbi-lib
 .endif	# NCBI_LIB_BUILDLINK3_MK
 
-.include "../../mk/x11.buildlink3.mk"
-
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
