a35134c07a58f06d218920edd021d3e403c4fbac
Jeremy C. Reed <reed@reedmedia.net>
2006-03-30 00:03:46 +0000

    Import of libXpm. This conflicts with graphics/xpm. (The distfile for xpm-3.4k is over eight years old.)
    XPM (X PixMap) is a format for storing and retrieving X pixmaps
    to and from files.
    
    This libXpm is from the modular Xorg X11.
    
    This package includes the cxpm(1) tool for checking XPM files.

---
+++ libXpm/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/03/30 00:03:46 jeremy-c-reed Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBXPM_BUILDLINK3_MK:=	${LIBXPM_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libXpm
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NlibXpm}
+BUILDLINK_PACKAGES+=	libXpm
+
+.if !empty(LIBXPM_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libXpm+=	libXpm>=3.5.4.2
+BUILDLINK_PKGSRCDIR.libXpm?=	../../wip/libXpm
+.endif	# LIBXPM_BUILDLINK3_MK
+
+#.include "../../wip/xproto/buildlink3.mk"
+.include "../../wip/libX11/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
