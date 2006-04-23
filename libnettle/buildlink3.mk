567ff8b6a57fca770eee83211a31c994a6af3b92
Aleksey Cheusov <cheusov@users.sourceforge.net>
2006-04-23 11:05:51 +0000

    already verified?

--- libnettle/buildlink3.mk
+++ libnettle/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2006/04/22 13:13:42 cheusov Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.11.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.libnettle?=	build
+# $NetBSD: buildlink3.mk,v 1.2 2006/04/23 11:05:51 cheusov Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBNETTLE_BUILDLINK3_MK:=	${LIBNETTLE_BUILDLINK3_MK}+
