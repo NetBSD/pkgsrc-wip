0b67b3cbc71e97417060839f133f6a019c2d2a4a
Jeremy C. Reed <reed@reedmedia.net>
2006-04-28 06:02:46 +0000

    Include graphics/freetype2/buildlink3.mk. Noticed because is listed in xfont pkg-config file.

--- libXfont/buildlink3.mk
+++ libXfont/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/04/28 05:59:11 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2006/04/28 06:02:46 jeremy-c-reed Exp $
 #
 # This Makefile fragment is included by packages that use libXfont.
 #
@@ -17,6 +17,7 @@ BUILDLINK_PACKAGES+=			libXfont
 BUILDLINK_API_DEPENDS.libXfont+=		libXfont>=1.1.0
 BUILDLINK_PKGSRCDIR.libXfont?=		../../wip/libXfont
 
+.include "../../graphics/freetype2/buildlink3.mk"
 .include "../../wip/libX11/buildlink3.mk"
 .include "../../x11/xproto/buildlink3.mk"
 
