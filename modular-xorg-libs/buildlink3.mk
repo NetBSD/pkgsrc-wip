53ba0f2a35ac05a8525714e8559ea33ee7a7da5c
Jeremy C. Reed <reed@reedmedia.net>
2006-04-29 04:39:33 +0000

    Depend on libXp. I noticed that Xaw3d needs libXp (Print Extension). Bump PKGREVISION.

--- modular-xorg-libs/buildlink3.mk
+++ modular-xorg-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2006/04/28 02:50:14 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2006/04/29 04:39:33 jeremy-c-reed Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MODULAR_XORG_LIBS_BUILDLINK3_MK:=	${MODULAR_XORG_LIBS_BUILDLINK3_MK}+
@@ -18,7 +18,7 @@ BUILDLINK_PACKAGES:=	modular-xorg-libs ${BUILDLINK_PACKAGES}
 
 .if !empty(MODULAR_XORG_LIBS_BUILDLINK3_MK:M+)
 
-BUILDLINK_API_DEPENDS.modular-xorg-libs+=	modular-xorg-libs>=7.0nb5
+BUILDLINK_API_DEPENDS.modular-xorg-libs+=	modular-xorg-libs>=7.0nb6
 BUILDLINK_PKGSRCDIR.modular-xorg-libs?=		../../wip/modular-xorg-libs
 #BUILDLINK_TRANSFORM.modular-xorg-libs+=	-e "s|/${X11ROOT_PREFIX}/|/|"
 
@@ -32,6 +32,7 @@ BUILDLINK_PKGSRCDIR.modular-xorg-libs?=		../../wip/modular-xorg-libs
 .include "../../wip/libXi/buildlink3.mk"
 .include "../../wip/libXinerama/buildlink3.mk"
 .include "../../wip/libXmu/buildlink3.mk"
+.include "../../wip/libXp/buildlink3.mk"
 .include "../../wip/libXt/buildlink3.mk"
 .include "../../wip/libXxf86misc/buildlink3.mk"
 .include "../../wip/libXxf86vm/buildlink3.mk"
