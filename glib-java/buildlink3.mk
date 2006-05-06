a398785f8db7617f9c8ff18532ad5fd57d10a46b
pancake ;) <poppnk@users.sourceforge.net>
2006-05-06 15:31:47 +0000

    * Up to 0.2.4

--- glib-java/buildlink3.mk
+++ glib-java/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/04/06 07:18:23 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2006/05/06 15:31:47 poppnk Exp $
 # XXX
 # XXX This file was created automatically using createbuildlink-3.8.
 # XXX After this file has been verified as correct, the comment lines
@@ -11,7 +11,7 @@
 # XXX	BUILDLINK_DEPMETHOD.glib-java?=	build
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-GLIB_JAVA_BUILDLINK3_MK:=	${GLIB_JAVA_BUILDLINK3_MK}+
+guib_java_BUILDLINK3_MK:=	${guib_java_BUILDLINK3_MK}+
 
 .if !empty(BUILDLINK_DEPTH:M+)
 BUILDLINK_DEPENDS+=	glib-java
@@ -20,10 +20,10 @@ BUILDLINK_DEPENDS+=	glib-java
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nglib-java}
 BUILDLINK_PACKAGES+=	glib-java
 
-.if !empty(GLIB_JAVA_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.glib-java+=	glib-java>=0.2.1
+.if !empty(guib_java_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.glib-java+=	glib-java>=0.2.4
 BUILDLINK_PKGSRCDIR.glib-java?=	../../wip/glib-java
-.endif	# GLIB_JAVA_BUILDLINK3_MK
+.endif	# guib_java_BUILDLINK3_MK
 
 # XXX
 # XXX Uncomment and keep only the buildlink3 lines below which are directly
