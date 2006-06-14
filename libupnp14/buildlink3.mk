619ae7021ada6ecf5cc9a24f7c19daf3854882e4
OBATA Akio <obache@users.sourceforge.net>
2006-06-14 09:55:34 +0000

    Move ftime hack from Makefile to buildlink3.mk

---
+++ libupnp14/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1 2006/06/14 09:55:34 obache Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBUPNP_BUILDLINK3_MK:=	${LIBUPNP_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	libupnp
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibupnp}
+BUILDLINK_PACKAGES+=	libupnp
+
+.if ${LIBUPNP_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.libupnp+=	libupnp>=1.4.0
+BUILDLINK_PKGSRCDIR.libupnp?=	../../wip/libupnp14
+.endif	# LIBUPNP_BUILDLINK3_MK
+
+.include "../../mk/bsd.prefs.mk"
+
+.if ${OPSYS} == "NetBSD" || ${OPSYS} == "FreeBSD" || ${OPSYS} == "OpenBSD" || ${OPSYS} == "DragonFly" || ${OPSYS} == "Darwin" || ${OPSYS} == "BSDOS"
+BUILDLINK_LIBS.libupnp+=	-lcompat     # XXX need this for ftime(3)
+CONFIGURE_ENV+= LIBS=${LIBS:M*:Q}
+.endif
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
