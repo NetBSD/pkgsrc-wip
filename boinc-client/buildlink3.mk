593c1e5c8ae6d3f155210639c0120a73758c705d
Rumko <rumko@NetBSD.org>
2006-06-18 16:51:41 +0000

    Messed up my previous commit, this is the actual BOINC client and as the name of the recently commited package by me suggests, is the package that install the libraries and headers.

---
+++ boinc-client/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/06/18 16:51:41 rumko Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+BOINC_CLIENT_BUILDLINK3_MK:=	${BOINC_CLIENT_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	boinc-client
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nboinc-client}
+BUILDLINK_PACKAGES+=	boinc-client
+
+.if ${BOINC_CLIENT_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.boinc-client+=	boinc-client>=1
+BUILDLINK_PKGSRCDIR.boinc-client?=	../../wip/boinc-client
+.endif	# BOINC_CLIENT_BUILDLINK3_MK
+
+# XXX
+# XXX Uncomment and keep only the buildlink3 lines below which are directly
+# XXX needed for dependencies to compile, link, and run.  If this package
+# XXX provides a wrappered API or otherwise does not expose the APIs of the
+# XXX buildlink3 lines below to dependencies, remove them.
+# XXX
+#.include "../../www/curl/buildlink3.mk"
+#.include "../../security/openssl/buildlink3.mk"
+#.include "../../net/boinc-lib/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
