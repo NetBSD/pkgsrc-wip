721511ea23cae9a0966295069f52c24fac97041f
Emil Sköldberg <emil_s@users.sourceforge.net>
2006-08-25 14:52:07 +0000

    Update givaro to version 3.2.1. I cannot find any information about what has changed unfortunately.
    Fix some pkglint warnings about buildlink3.mk file (with the help
    of createbuildlink).
    
    Add c to USE_LANGUAGES.

--- givaro/buildlink3.mk
+++ givaro/buildlink3.mk
@@ -1,20 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/04/06 07:17:22 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2006/08/25 14:52:07 emil_s Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GIVARO_BUILDLINK3_MK:=	${GIVARO_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	givaro
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngivaro}
 BUILDLINK_PACKAGES+=	givaro
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}givaro
 
-.if !empty(GIVARO_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.givaro+=	givaro>=3.1
+.if ${GIVARO_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.givaro+=	givaro>=3.2.1
 BUILDLINK_PKGSRCDIR.givaro?=	../../wip/givaro
 .endif	# GIVARO_BUILDLINK3_MK
 
 .include "../../devel/gmp/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
