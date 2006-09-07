2315581a49685aaee58a2a9762c7b5661852b2a7
Martti Kuparinen <martti@NetBSD.org>
2006-09-07 10:12:11 +0000

    - xfce4-desktop depends now on xfce4-thunar and dbus-glib - xfce4 doesn't depend directly on xfce4-thunar - more translated files for xfce4-desktop

--- xfce4/buildlink3.mk
+++ xfce4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2006/09/07 08:28:42 marttikuparinen Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2006/09/07 10:12:11 marttikuparinen Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 XFCE4_BUILDLINK3_MK:=	${XFCE4_BUILDLINK3_MK}+
@@ -20,7 +20,6 @@ BUILDLINK_PKGSRCDIR.xfce4?=	../../wip/xfce4
 .include "../../wip/xfce4-icon-theme/buildlink3.mk"
 .include "../../wip/xfce4-print/buildlink3.mk"
 .include "../../wip/xfce4-appfinder/buildlink3.mk"
-.include "../../wip/xfce4-thunar/buildlink3.mk"
 .include "../../wip/xfce4-xarchiver/buildlink3.mk"
 .include "../../wip/xfce4-orage/buildlink3.mk"
 .include "../../wip/xfce4-wm/buildlink3.mk"
