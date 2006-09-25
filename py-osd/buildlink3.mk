d148db2d4ec6b27204c13a24580b2517ffa7ad59
David Howland <dhowland@users.sourceforge.net>
2006-09-25 12:38:59 +0000

    Regenerate buildlink3.mk to make pkglint happy

--- py-osd/buildlink3.mk
+++ py-osd/buildlink3.mk
@@ -1,23 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.3 2006/04/06 07:18:22 jeremy-c-reed Exp $
-
-.include "../../lang/python/pyversion.mk"
+# $NetBSD: buildlink3.mk,v 1.4 2006/09/25 12:38:59 dhowland Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 PY_OSD_BUILDLINK3_MK:=	${PY_OSD_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
-BUILDLINK_DEPENDS+=	pyosd
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	py-osd
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npyosd}
-BUILDLINK_PACKAGES+=	pyosd
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npy-osd}
+BUILDLINK_PACKAGES+=	py-osd
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}py-osd
 
-.if !empty(PY_OSD_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.pyosd+=	${PYPKGPREFIX}-osd>=0.2.14
-BUILDLINK_PKGSRCDIR.pyosd?=	../../wip/py-osd
+.if ${PY_OSD_BUILDLINK3_MK} == "+"
+.include "../../lang/python/pyversion.mk"
+BUILDLINK_API_DEPENDS.py-osd+=	${PYPKGPREFIX}-osd>=0.2.14
+BUILDLINK_PKGSRCDIR.py-osd?=	../../wip/py-osd
+.endif	# PY_OSD_BUILDLINK3_MK
 
 .include "../../x11/xosd/buildlink3.mk"
 
-.endif	# PY_OSD_BUILDLINK3_MK
-
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
