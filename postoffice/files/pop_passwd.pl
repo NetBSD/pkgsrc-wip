16d0d91d8e4b49d5d4ae567239c6cd8600e6f39e
Pierre-Philipp Braun <elge@users.sourceforge.net>
2006-09-28 22:45:45 +0000

    + share/examples/postoffice/pop_passwd.pl

---
+++ postoffice/files/pop_passwd.pl
@@ -0,0 +1,15 @@
+#!/usr/pkg/bin/perl
+# pop_passwd username password >> password file
+
+ $name = $ARGV[0];
+
+ @salt_chars         = ('a'..'z','A'..'Z','0'..'9');
+ $salt               = $salt_chars[rand(62)] . $salt_chars[rand(62)];
+
+ $passwd = crypt ($ARGV[1], $salt);
+
+ print "$name:$passwd\n";
+
+## NOTE: this previously used
+## $salt               = $salt_chars[rand(63)] . $salt_chars[rand(63)];
+## this is wrong!
