591c64dbfb842c448304428cdf4d25155676d849
Martti Kuparinen <martti@NetBSD.org>
2006-10-12 08:22:31 +0000

    Added fam/buildlink3.mk

--- xfce4-thunar/buildlink3.mk
+++ xfce4-thunar/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/10/12 07:42:59 marttikuparinen Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/10/12 08:22:31 marttikuparinen Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XFCE4_THUNAR_BUILDLINK3_MK:=	${XFCE4_THUNAR_BUILDLINK3_MK}+
@@ -22,6 +22,7 @@ BUILDLINK_PKGSRCDIR.xfce4-thunar?=	../../wip/xfce4-thunar
 .include "../../graphics/hicolor-icon-theme/buildlink3.mk"
 .include "../../graphics/libexif/buildlink3.mk"
 .include "../../sysutils/dbus-glib/buildlink3.mk"
+.include "../../sysutils/fam/buildlink3.mk"
 .include "../../wip/xfce4-exo/buildlink3.mk"
 .include "../../wip/xfce4-panel/buildlink3.mk"
 .include "../../x11/startup-notification/buildlink3.mk"
