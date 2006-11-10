66afbf8f9d1cd55c3dedbcab5e976381d6a8af0a
Jeremy C. Reed <reed@reedmedia.net>
2006-11-10 18:08:25 +0000

    Increase version.
    Honor X11_TYPE of "modular" not "modular-xorg" now.
    
    Update links to pkgsrc/x11 versions instead of wip versions
    for most dependencies.

--- modular-xorg-libs/buildlink3.mk
+++ modular-xorg-libs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/04/29 04:39:33 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2006/11/10 18:08:25 jeremy-c-reed Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MODULAR_XORG_LIBS_BUILDLINK3_MK:=	${MODULAR_XORG_LIBS_BUILDLINK3_MK}+
@@ -13,31 +13,32 @@ BUILDLINK_DEPENDS+=	modular-xorg-libs
 #
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmodular-xorg-libs}
 BUILDLINK_PACKAGES:=	modular-xorg-libs ${BUILDLINK_PACKAGES}
+BUILDLINK_ORDER:=       ${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}modular-xorg-libs
 
 .include "../../mk/bsd.prefs.mk"
 
 .if !empty(MODULAR_XORG_LIBS_BUILDLINK3_MK:M+)
 
 BUILDLINK_API_DEPENDS.modular-xorg-libs+=	modular-xorg-libs>=7.0nb6
-BUILDLINK_PKGSRCDIR.modular-xorg-libs?=		../../wip/modular-xorg-libs
+BUILDLINK_PKGSRCDIR.modular-xorg-libs?=		../../x11/modular-xorg-libs
 #BUILDLINK_TRANSFORM.modular-xorg-libs+=	-e "s|/${X11ROOT_PREFIX}/|/|"
 
 .endif  # MODULAR_XORG_LIBS_BUILDLINK3_MK
 
-.include "../../wip/libX11/buildlink3.mk"
-.include "../../wip/libXau/buildlink3.mk"
-.include "../../wip/libXaw/buildlink3.mk"
-.include "../../wip/libXext/buildlink3.mk"
-.include "../../wip/libXfont/buildlink3.mk"
-.include "../../wip/libXi/buildlink3.mk"
-.include "../../wip/libXinerama/buildlink3.mk"
-.include "../../wip/libXmu/buildlink3.mk"
-.include "../../wip/libXp/buildlink3.mk"
-.include "../../wip/libXt/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXau/buildlink3.mk"
+.include "../../x11/libXaw/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXfont/buildlink3.mk"
+.include "../../x11/libXi/buildlink3.mk"
+.include "../../x11/libXinerama/buildlink3.mk"
+.include "../../x11/libXmu/buildlink3.mk"
+.include "../../x11/libXp/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
 .include "../../wip/libXxf86misc/buildlink3.mk"
 .include "../../wip/libXxf86vm/buildlink3.mk"
 .include "../../x11/xextproto/buildlink3.mk"
-.include "../../wip/libxkbfile/buildlink3.mk"
+.include "../../x11/libxkbfile/buildlink3.mk"
 .include "../../wip/libxkbui/buildlink3.mk"
 
 #.include "../../devel/zlib/buildlink3.mk"
