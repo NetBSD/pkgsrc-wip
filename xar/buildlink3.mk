2ba0cebe701e23397dfc4206b29104b4d8ebdd8b
David Howland <dhowland@users.sourceforge.net>
2006-12-02 17:00:57 +0000

    The XAR project aims to provide an easily extensible archive format. Important design decisions include an easily extensible XML table of contents for random access to archived files, storing the toc at the beginning of the archive to allow for efficient handling of streamed archives, the ability to handle files of arbitrarily large sizes, the ability to choose independent encodings for individual files in the archive, the ability to store checksums for individual files in both compressed and uncompressed form, and the ability to query the table of content's rich meta-data.

---
+++ xar/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/12/02 17:00:57 dhowland Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+XAR_BUILDLINK3_MK:=	${XAR_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	xar
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxar}
+BUILDLINK_PACKAGES+=	xar
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xar
+
+.if ${XAR_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.xar+=	xar>=1.4
+BUILDLINK_PKGSRCDIR.xar?=	../../wip/xar
+.endif	# XAR_BUILDLINK3_MK
+
+#.include "../../textproc/libxml2/buildlink3.mk"
+#.include "../../www/curl/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
