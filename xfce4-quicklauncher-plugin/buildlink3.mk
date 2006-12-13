6f2498cb6a29b424e788ea535dad1144c1ab1e9b
David Howland <dhowland@users.sourceforge.net>
2006-12-13 04:55:00 +0000

    Depend on xfce 4.4 from wip, 4.2 from pkgsrc isn't sufficient

--- xfce4-quicklauncher-plugin/buildlink3.mk
+++ xfce4-quicklauncher-plugin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2006/09/07 08:27:47 marttikuparinen Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2006/12/13 04:55:00 dhowland Exp $
 
 BUILDLINK_DEPTH:=				${BUILDLINK_DEPTH}+
 XFCE4_QUICKLAUNCHER_PLUGIN_BUILDLINK3_MK:=	${XFCE4_QUICKLAUNCHER_PLUGIN_BUILDLINK3_MK}+
@@ -16,7 +16,8 @@ BUILDLINK_API_DEPENDS.xfce4-quicklauncher-plugin+=	xfce4-quicklauncher-plugin>=1
 BUILDLINK_PKGSRCDIR.xfce4-quicklauncher-plugin?=	../../wip/xfce4-quicklauncher-plugin
 .endif	# XFCE4_QUICKLAUNCHER_PLUGIN_BUILDLINK3_MK
 
-.include "../../x11/xfce4-panel/buildlink3.mk"
+#.include "../../x11/xfce4-panel/buildlink3.mk"
+.include "../../wip/xfce4-panel/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=				${BUILDLINK_DEPTH:S/+$//}
