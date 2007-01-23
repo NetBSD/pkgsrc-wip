a3e7d6586ec2acd81241cd3c3e23744a6dd2ba76
Blair Sadewitz <bsadewitz@users.sourceforge.net>
2007-01-23 08:24:17 +0000

    This package will track--according to my free time, unfortunately--the most current libXfont tarball available from modular X.org.  I strive to support a reasonable amount of configure options, etc.  However, I am not an experienced pkgsrc developer, so please let me know if I've made any mistakes.
    Thanks,
    
    --Blair

---
+++ libXfont/options.mk
@@ -0,0 +1,21 @@
+# $NetBSD: options.mk,v 1.1 2007/01/23 08:24:17 bsadewitz Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.libXfont
+PKG_SUPPORTED_OPTIONS=	ipv6 libxfont-unix-transport libxfont-tcp-transport
+PKG_SUGGESTED_OPTIONS=	libxfont-unix-transport libxfont-tcp-transport
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mipv6)
+  CONFIGURE_ARGS+=	--enable-ipv6
+.endif
+
+
+.if !empty(PKG_OPTIONS:Mlibxfont-unix-transport)
+  CONFIGURE_ARGS+=	--enable-unix-domain-transport
+.endif
+
+
+.if !empty(PKG_OPTIONS:Mlibxfont-tcp-transport)
+  CONFIGURE_ARGS+=	--enable-tcp-transport
+.endif
