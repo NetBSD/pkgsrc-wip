16e3652be51667c1483ee0a62e68299481843973
Blair Sadewitz <bsadewitz@users.sourceforge.net>
2007-01-23 05:16:10 +0000

    In this revision:
    	--Provide mutually exclusive options for xcb/pthreads
    	--Provide pthread option and use PTHREAD_AUTO_VARS
    	  (removed configure/configure.ac patches for pthreads also)
    	--Provide option for loadable i18n modules
    	--Provide option for secure-rpc (heretofore untested)
    	--Now buildlink to exclusively pkgsrc/x11 dependencies, for the
    	  relevant libraries there are now current. (thanks, joerg)
    
    Thank you for using this package!  Please let me <blair.sadewitz@gmail.com>
    know how it works out for you.

--- libX11/options.mk
+++ libX11/options.mk
@@ -1,13 +1,41 @@
-# $NetBSD: options.mk,v 1.1 2007/01/22 09:40:09 bsadewitz Exp $
+# $NetBSD: options.mk,v 1.2 2007/01/23 05:16:10 bsadewitz Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.libX11
-PKG_SUPPORTED_OPTIONS=	xcb
+PKG_OPTIONS_VAR=		PKG_OPTIONS.libX11
+
+PKG_OPTIONS_OPTIONAL_GROUPS=	threads
+PKG_OPTIONS_GROUP.threads= 	xcb 
+PKG_SUPPORTED_OPTIONS+=		loadable-i18n secure-rpc
+
+CHECK_BUILTIN.pthread:=		yes
+
+.include "../../mk/pthread.builtin.mk"
+.  if !empty(USE_BUILTIN.pthread)
+  PKG_OPTIONS_GROUP.threads+=		pthread
+  PKG_SUGGESTED_OPTIONS=		pthread
+.endif
+
+CHECK_BUILTIN.pthread:=		no
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mxcb)
 . include "../../wip/libxcb/buildlink3.mk"
-  CONFIGURE_ARGS+=	--with-xcb
-.else
-  CONFIGURE_ARGS+=	--without-xcb
+  CONFIGURE_ARGS+=			--with-xcb
+  PKGCONFIG_OVERRIDE+=    		x11-xcb.pc.in
+. else
+  CONFIGURE_ARGS+=			--without-xcb
+.endif
+
+.if !empty(PKG_OPTIONS:Mpthread)
+  PTHREAD_AUTO_VARS=			yes
+. include "../../mk/pthread.buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mloadable-i18n)
+  CONFIGURE_ARGS+=			--enable-loadable-i18n
+. include "../../mk/dlopen.buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONSMsecure-rpc)
+  CONFIGURE_ARGS+=			--enable-secure-rpc
 .endif
