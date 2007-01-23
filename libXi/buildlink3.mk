85bac8c3e8b77ec5771153be4165bc4bff6b74f8
Blair Sadewitz <bsadewitz@users.sourceforge.net>
2007-01-23 13:37:24 +0000

    This package contains the Xi extension. This is the X Input extension library.
    This is part of the X Libraries and Protocol Headers Project at
    freedesktop.org.
    
    This package tracks the most current X.org tarball.

---
+++ libXi/buildlink3.mk
@@ -0,0 +1,29 @@
+# $NetBSD: buildlink3.mk,v 1.7 2007/01/23 13:37:24 bsadewitz Exp $
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${X11_TYPE} != "modular"
+.include "../../mk/x11.buildlink3.mk"
+.else
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBXI_BUILDLINK3_MK:=	${LIBXI_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	libXi
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NlibXi}
+BUILDLINK_PACKAGES+=	libXi
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libXi
+
+.if ${LIBXI_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.libXi+=	libXi>=1.1.0
+BUILDLINK_PKGSRCDIR.libXi?=	../../wip/libXi
+.endif	# LIBXI_BUILDLINK3_MK
+
+.include "../../wip/libX11/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/xproto/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
+.endif
