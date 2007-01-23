2fdbd9ed1aaf46a05ad70114aa76e93c92815ea8
Blair Sadewitz <bsadewitz@users.sourceforge.net>
2007-01-23 13:42:40 +0000

    This package provides the X Window System video extension from the modular X.org project.
    This package attempts to track the most current tarball available from modular
    X.org.

---
+++ libXv/buildlink3.mk
@@ -0,0 +1,32 @@
+# $NetBSD: buildlink3.mk,v 1.7 2007/01/23 13:42:40 bsadewitz Exp $
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${X11_TYPE} != "modular"
+.include "../../mk/x11.buildlink3.mk"
+.else
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBXV_BUILDLINK3_MK:=	${LIBXV_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	libXv
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NlibXv}
+BUILDLINK_PACKAGES+=	libXv
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libXv
+
+.if ${LIBXV_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.libXv+=	libXv>=1.0.3
+BUILDLINK_PKGSRCDIR.libXv?=	../../wip/libXv
+.endif	# LIBXV_BUILDLINK3_MK
+
+.include "../../wip/libX11/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/videoproto/buildlink3.mk"
+.include "../../x11/xextproto/buildlink3.mk"
+.include "../../x11/xproto/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
+.endif
