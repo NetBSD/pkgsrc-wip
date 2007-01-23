0e0e1d31ec5114be25dc4ad26255e58df90f2ff0
Blair Sadewitz <bsadewitz@users.sourceforge.net>
2007-01-23 13:35:09 +0000

    Xft (2.0) provides a client-side font API for X applications. It uses Fontconfig to select fonts and the X protocol for rendering them. When available, Xft uses the Render extension to accelerate text drawing. When Render is not available, Xft uses the core protocol to draw client-side glyphs. This provides completely compatible support of client-side fonts for all X servers.
    Xft (2.0) hides most of the underlying system details so that developers
    can confidently use its API to access client-side fonts in any X
    environment.
    
    I will try to track the most current tarball available.

---
+++ libXft/buildlink3.mk
@@ -0,0 +1,33 @@
+# $NetBSD: buildlink3.mk,v 1.5 2007/01/23 13:35:09 bsadewitz Exp $
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${X11_TYPE} != "modular"
+.include "../../fonts/Xft2/buildlink3.mk"
+.else
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBXFT_BUILDLINK3_MK:=	${LIBXFT_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	libXft
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NlibXft}
+BUILDLINK_PACKAGES+=	libXft
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libXft
+
+.if ${LIBXFT_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.libXft+=	libXft>=2.1.12
+BUILDLINK_PKGSRCDIR.libXft?=	../../wip/libXft
+.endif	# LIBXFT_BUILDLINK3_MK
+
+.include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../x11/libXrender/buildlink3.mk"
+.include "../../wip/libX11/buildlink3.mk"
+.include "../../x11/xproto/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
+
+.endif
