2de5ee121fbae55e4bc96908b12dcb7373918c98
Martti Kuparinen <martti@NetBSD.org>
2007-01-23 14:05:57 +0000

    Upgraded to 1.9.2. Prepare for Xfce 4.4.0.

--- xfce4-quicklauncher-plugin/buildlink3.mk
+++ xfce4-quicklauncher-plugin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/12/13 04:55:00 dhowland Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2007/01/23 14:05:57 marttikuparinen Exp $
 
 BUILDLINK_DEPTH:=				${BUILDLINK_DEPTH}+
 XFCE4_QUICKLAUNCHER_PLUGIN_BUILDLINK3_MK:=	${XFCE4_QUICKLAUNCHER_PLUGIN_BUILDLINK3_MK}+
@@ -12,12 +12,12 @@ BUILDLINK_PACKAGES+=	xfce4-quicklauncher-plugin
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-quicklauncher-plugin
 
 .if ${XFCE4_QUICKLAUNCHER_PLUGIN_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.xfce4-quicklauncher-plugin+=	xfce4-quicklauncher-plugin>=1.9.1
+BUILDLINK_API_DEPENDS.xfce4-quicklauncher-plugin+=	xfce4-quicklauncher-plugin>=1.9.2
 BUILDLINK_PKGSRCDIR.xfce4-quicklauncher-plugin?=	../../wip/xfce4-quicklauncher-plugin
 .endif	# XFCE4_QUICKLAUNCHER_PLUGIN_BUILDLINK3_MK
 
-#.include "../../x11/xfce4-panel/buildlink3.mk"
 .include "../../wip/xfce4-panel/buildlink3.mk"
+.include "../../wip/xfce4-dev-tools/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=				${BUILDLINK_DEPTH:S/+$//}
