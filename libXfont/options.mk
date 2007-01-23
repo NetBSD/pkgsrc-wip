a78bed4e89556efd637c0788f90fd349e6082a2d
Blair Sadewitz <bsadewitz@users.sourceforge.net>
2007-01-23 17:43:50 +0000

    Removed a duplicate PKGCONFIG_OVERRIDE.  Make options more robust by disabling those not explicitly enabled.  Use proper IPV6 option (inet6).
    Bump revision.

--- libXfont/options.mk
+++ libXfont/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2007/01/23 15:52:54 bsadewitz Exp $
+# $NetBSD: options.mk,v 1.3 2007/01/23 17:43:50 bsadewitz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libXfont
 PKG_SUPPORTED_OPTIONS=	inet6 libxfont-unix-transport libxfont-tcp-transport
@@ -6,16 +6,22 @@ PKG_SUGGESTED_OPTIONS=	libxfont-unix-transport libxfont-tcp-transport
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mipv6)
+.if !empty(PKG_OPTIONS:Minet6)
   CONFIGURE_ARGS+=	--enable-ipv6
+.else
+  CONFIGURE_ARGS+=	--disable-ipv6
 .endif
 
 
 .if !empty(PKG_OPTIONS:Mlibxfont-unix-transport)
   CONFIGURE_ARGS+=	--enable-unix-domain-transport
+.else
+  CONFIGURE_ARGS+=	--disable-unix-domain-transport
 .endif
 
 
 .if !empty(PKG_OPTIONS:Mlibxfont-tcp-transport)
   CONFIGURE_ARGS+=	--enable-tcp-transport
+.else
+  CONFIGURE_ARGS+=	--disable-unix-domain-transport
 .endif
