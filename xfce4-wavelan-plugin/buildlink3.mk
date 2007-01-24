a45bd1bde27ed941815aa8124a97cfb6ae5bd281
Martti Kuparinen <martti@NetBSD.org>
2007-01-24 08:26:17 +0000

    WLAN plugin for Xfce.

---
+++ xfce4-wavelan-plugin/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/01/24 08:26:17 marttikuparinen Exp $
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+XFCE4_WAVELAN_PLUGIN_BUILDLINK3_MK:=	${XFCE4_WAVELAN_PLUGIN_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	xfce4-wavelan-plugin
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nxfce4-wavelan-plugin}
+BUILDLINK_PACKAGES+=	xfce4-wavelan-plugin
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-wavelan-plugin
+
+.if ${XFCE4_WAVELAN_PLUGIN_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.xfce4-wavelan-plugin+=	xfce4-wavelan-plugin>=0.5.3
+BUILDLINK_PKGSRCDIR.xfce4-wavelan-plugin?=	../../wip/xfce4-wavelan-plugin
+.endif	# XFCE4_WAVELAN_PLUGIN_BUILDLINK3_MK
+
+.include "../../wip/xfce4-panel/buildlink3.mk"
+.include "../../wip/xfce4-dev-tools/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
