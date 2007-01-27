a6c9b5e319700633699c754d2c8e23d24a1fade5
Rumko <rumko@NetBSD.org>
2007-01-27 14:33:15 +0000

    Now you can run (or should I say, now it works :) ) "nx nxclient --wizard"!

--- nx-client-bin/files/nx.sh
+++ nx-client-bin/files/nx.sh
@@ -5,9 +5,9 @@ export PATH
 LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:@PREFIX@/lib/@NX@"
 export LD_LIBRARY_PATH
 
-if [ "$@" ]
+if [ $# > 0 ]
 then
-	exec /usr/pkg/lib/nx/bin/$@
+	exec /usr/pkg/lib/nx/bin/$*
 else
 	exec /usr/pkg/lib/nx/bin/nxclient
 fi
