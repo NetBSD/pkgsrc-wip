b258b4cd682f6259773056c17eed184bef247d28
Thomas Klausner <wiz@NetBSD.org>
2007-01-28 09:37:52 +0000

    Update to 0.36.1: Lots of changes since previous packaged version. gstreamer is the only supported backend, support for winamp skins was dropped, lots of new features.

--- bmpx/options.mk
+++ bmpx/options.mk
@@ -1,22 +1,10 @@
-# $NetBSD: options.mk,v 1.5 2006/04/06 07:17:02 jeremy-c-reed Exp $
+# $NetBSD: options.mk,v 1.6 2007/01/28 09:37:52 thomasklausner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.bmpx
-PKG_SUPPORTED_OPTIONS=	debug gui
-PKG_SUGGESTED_OPTIONS=	gui
+PKG_SUPPORTED_OPTIONS=	debug
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-debug
-.endif
-
-.if !empty(PKG_OPTIONS:Mgui)
-BUILDLINK_API_DEPENDS.libglade2+=	libglade2>=2.5.1
-
-.  include "../../devel/libglade2/buildlink3.mk"
-.  include "../../devel/pango/buildlink3.mk"
-.  include "../../graphics/cairo/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-gui
-.else
-CONFIGURE_ARGS+=	--disable-gui
 .endif
