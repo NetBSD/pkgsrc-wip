a26ab943c006b4c4b309f6965eb9f3cb345dcbee
Johann Franz <netcap@users.sourceforge.net>
2007-02-06 21:18:28 +0000

    Use ${PREFIX} and ${WRKSRC}, fix pkglint errors, cleanup.

--- guichan/buildlink3.mk
+++ guichan/buildlink3.mk
@@ -1,39 +1,24 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/02/05 00:03:41 pseudo_anim Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.13.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.guichan-0.6.0?=	build
+# $NetBSD: buildlink3.mk,v 1.2 2007/02/06 21:18:28 netcap Exp $
 
-BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-GUICHAN_0.6.0_BUILDLINK3_MK:=	${GUICHAN_0.6.0_BUILDLINK3_MK}+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GUICHAN_BUILDLINK3_MK:=	${GUICHAN_BUILDLINK3_MK}+
 
 .if ${BUILDLINK_DEPTH} == "+"
-BUILDLINK_DEPENDS+=	guichan-0.6.0
+BUILDLINK_DEPENDS+=	guichan
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nguichan-0.6.0}
-BUILDLINK_PACKAGES+=	guichan-0.6.0
-BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}guichan-0.6.0
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nguichan}
+BUILDLINK_PACKAGES+=	guichan
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}guichan
 
-.if ${GUICHAN_0.6.0_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.guichan-0.6.0+=	guichan-0.6.0>=src
-BUILDLINK_PKGSRCDIR.guichan-0.6.0?=	../../wip/guichan
-.endif	# GUICHAN_0.6.0_BUILDLINK3_MK
+.if ${GUICHAN_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.guichan+=	guichan>=0.6.0
+BUILDLINK_PKGSRCDIR.guichan?=	../../wip/guichan
+BUILDLINK_DEPMETHOD.guichan?=	build
+.endif	# GUICHAN_BUILDLINK3_MK
 
-# XXX
-# XXX Uncomment and keep only the buildlink3 lines below which are directly
-# XXX needed for dependencies to compile, link, and run.  If this package
-# XXX provides a wrappered API or otherwise does not expose the APIs of the
-# XXX buildlink3 lines below to dependencies, remove them.
-# XXX
-#.include "../../devel/SDL/buildlink3.mk"
-#.include "../../graphics/SDL_image/buildlink3.mk"
-#.include "../../audio/SDL_mixer/buildlink3.mk"
+.include "../../devel/SDL/buildlink3.mk"
+.include "../../graphics/SDL_image/buildlink3.mk"
+.include "../../audio/SDL_mixer/buildlink3.mk"
 
-BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
