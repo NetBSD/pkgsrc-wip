df41028c5fcd2f923e197c6ee9ff6d130603da60
Aleksey Cheusov <cheusov@users.sourceforge.net>
2007-03-27 20:17:38 +0000

    added: extra commands "unload" and "load"

--- dict-server/files/dictd.sh
+++ dict-server/files/dictd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: dictd.sh,v 1.3 2006/07/18 20:43:11 cheusov Exp $
+# $NetBSD: dictd.sh,v 1.4 2007/03/27 20:17:38 cheusov Exp $
 #
 # PROVIDE: dictd
 # REQUIRE: LOGIN
@@ -8,12 +8,24 @@
 . /etc/rc.subr
 
 name="dictd"
+unload_cmd="dictd_unload"
+load_cmd="dictd_load"
 dictd_flags=${dictd_flags-"--pp '@PATH_TO_M4@ -P'"}
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
 pidfile="/var/run/${name}.pid"
 required_files="@PKG_SYSCONFDIR@/dictd.conf"
-extra_commands="reload"
+extra_commands="reload unload load"
+
+dictd_load (){
+    echo "Loading dictd databases."
+    kill -HUP $(check_pidfile $pidfile $name)
+}
+
+dictd_unload (){
+    echo "Unloading dictd databases."
+    kill -USR1 $(check_pidfile $pidfile $name)
+}
 
 load_rc_config $name
 run_rc_command "$1"
