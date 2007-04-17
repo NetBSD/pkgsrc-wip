8299deec0b6659d0c21a77126195c8d88b767b51
Martti Kuparinen <martti@NetBSD.org>
2007-04-17 14:02:48 +0000

    Updated xfce4-taskmanager to 0.3.2
    * Bug fixes.

--- xfce4-taskmanager/buildlink3.mk
+++ xfce4-taskmanager/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/07/14 11:01:36 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2007/04/17 14:02:48 marttikuparinen Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 XFCE4_TASKMANAGER_BUILDLINK3_MK:=	${XFCE4_TASKMANAGER_BUILDLINK3_MK}+
@@ -12,11 +12,14 @@ BUILDLINK_PACKAGES+=	xfce4-taskmanager
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xfce4-taskmanager
 
 .if ${XFCE4_TASKMANAGER_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.xfce4-taskmanager+=	xfce4-taskmanager>=0.3.1nb1
+BUILDLINK_API_DEPENDS.xfce4-taskmanager+=	xfce4-taskmanager>=0.3.2
 BUILDLINK_PKGSRCDIR.xfce4-taskmanager?=	../../wip/xfce4-taskmanager
 .endif	# XFCE4_TASKMANAGER_BUILDLINK3_MK
 
 .include "../../x11/libxfce4gui/buildlink3.mk"
+.include "../../devel/xfce4-dev-tools/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
