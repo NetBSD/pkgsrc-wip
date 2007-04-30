023a2f3da0b26bb67f4b6d254fe39ccc231600a5
Blair Sadewitz <bsadewitz@users.sourceforge.net>
2007-04-30 15:27:39 +0000

    Doh!  Don't use BUILDLINK_LIBS; we don't need this linked in everywhere (or at least I hope not!).

--- libpthread-stubs/buildlink3.mk
+++ libpthread-stubs/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2007/04/29 20:22:14 bsadewitz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2007/04/30 15:27:39 bsadewitz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PTHREAD_STUBS_BUILDLINK3_MK:=	${PTHREAD_STUBS_BUILDLINK3_MK}+
@@ -14,8 +14,6 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libpthread-stubs
 .if ${PTHREAD_STUBS_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.libpthread-stubs+=	libpthread-stubs>=0.1
 BUILDLINK_PKGSRCDIR.libpthread-stubs?=	../../wip/libpthread-stubs
-BUILDLINK_LDADD.libpthread-stubs=	-lpthread-stubs
-BUILDLINK_LIBS.libpthread-stubs=	${BUILDLINK_LDADD.libpthread-stubs}
 .endif	# PTHREAD_STUBS_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
