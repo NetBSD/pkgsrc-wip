1b53f0dd58eeed0eaeb0217c9ba86f90e4fa44b8
Masao Uebayashi <uebayasi@gmail.com>
2007-06-05 13:53:13 +0000

    Reset $NetBSD$ RCS ids.

--- emacs22/files/site-init.el
+++ emacs22/files/site-init.el
@@ -1,4 +1,4 @@
-;; $NetBSD: site-init.el,v 1.1.1.1 2007/06/05 13:45:15 uebayasi Exp $
+;; $NetBSD: site-init.el,v 1.2 2007/06/05 13:53:13 uebayasi Exp $
 
 (autoload 'send-pr "/usr/share/gnats/send-pr.el" 
   "Command to create and send a problem report." t)
