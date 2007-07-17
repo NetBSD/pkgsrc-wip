2751752dc2c5e4e86cd5a87ffdde61a19402ad28
Georg Schwarz <gschwarz@users.sourceforge.net>
2007-07-17 22:19:51 +0000

    socklen_t hack no longer needed with new version of tftp-hpa

--- tftp-hpa/hacks.mk
+++ tftp-hpa/hacks.mk
@@ -1,16 +1,7 @@
-# $NetBSD: hacks.mk,v 1.1.1.1 2007/01/01 18:40:20 gschwarz Exp $
+# $NetBSD: hacks.mk,v 1.2 2007/07/17 22:19:51 gschwarz Exp $
 
 .if !defined(TFTP-HPA_HACKS_MK)
 TFTP-HPA_HACKS_MK=	# defined
-
-### [Sat Dec 30 22:59:04 CET 2006 : schwarz]
-### configure script fails to detect availability of socklen_t on MacOS X
-### and Solaris
-###
-.  if ${OPSYS} == "Darwin" || ${OPSYS} == "SunOS"
-PKG_HACKS+=	socken_t-presence
-CONFIGURE_ENV+=	ac_cv_type_socklen_t=yes
-.  endif
 
 ### [Mon Jan  1 17:13:15 CET 2007 : schwarz]
 ### configure script fails to detect presence of u_short and u_long on IRIX 5
