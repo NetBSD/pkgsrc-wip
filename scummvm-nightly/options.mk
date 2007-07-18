d9ea8cd5df68ff702e53eb5352cb7e7dbff78b2b
Dieter Baron <dillo@nih.at>
2007-07-18 08:15:33 +0000

    Add option for Drascula engine.

--- scummvm-nightly/options.mk
+++ scummvm-nightly/options.mk
@@ -1,13 +1,16 @@
-# $NetBSD: options.mk,v 1.2 2007/05/21 10:08:11 dillo Exp $
+# $NetBSD: options.mk,v 1.3 2007/07/18 08:15:33 dillo Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scummvm-nightly
-PKG_SUPPORTED_OPTIONS=	scummvm-nightly-cruise scummvm-nightly-lure
-PKG_SUGGESTED_OPTIONS=	scummvm-nightly-cruise scummvm-nightly-lure
+PKG_SUPPORTED_OPTIONS=	scummvm-nightly-cruise scummvm-nightly-drascula scummvm-nightly-lure
+PKG_SUGGESTED_OPTIONS=	scummvm-nightly-cruise scummvm-nightly-drascula scummvm-nightly-lure
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-cruise)
 CONFIGURE_ARGS+=--enable-cruise
+.endif
+.if !empty(PKG_OPTIONS:Mscummvm-nightly-drascula)
+CONFIGURE_ARGS+=--enable-drascula
 .endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-lure)
 CONFIGURE_ARGS+=--enable-lure
