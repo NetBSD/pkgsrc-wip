79c2552379cb5635be7e71958a85291ed2c0b0a1
Jukka Salmi <jukka@users.sourceforge.net>
2007-08-02 23:02:04 +0000

    Import the latest released OpenSSL version (0.9.8e). This is just an updated version of security/openssl from pkgsrc (0.9.7i). Currently quite untested (see TODO).

---
+++ openssl/buildlink3.mk
@@ -0,0 +1,38 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/08/02 23:02:04 jukka Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+OPENSSL_BUILDLINK3_MK:=	${OPENSSL_BUILDLINK3_MK}+
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	openssl
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nopenssl}
+BUILDLINK_PACKAGES+=	openssl
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}openssl
+
+.if !empty(OPENSSL_BUILDLINK3_MK:M+)
+BUILDLINK_API_DEPENDS.openssl+=	openssl>=0.9.6m
+BUILDLINK_ABI_DEPENDS.openssl+=	openssl>=0.9.8e
+#XXX FIXME when in pkgsrc...
+BUILDLINK_PKGSRCDIR.openssl?=	../../wip/openssl
+
+# Ensure that -lcrypt comes before -lcrypto when linking so that the
+# system crypt() routine is used.
+#
+WRAPPER_REORDER_CMDS+=	reorder:l:crypt:crypto
+
+SSLBASE=	${BUILDLINK_PREFIX.openssl}
+BUILD_DEFS+=	SSLBASE
+.endif	# OPENSSL_BUILDLINK3_MK
+
+pkgbase := openssl
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.openssl:Mrsaref)
+.  include "../../security/rsaref/buildlink3.mk"
+.endif
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
