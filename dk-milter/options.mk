e1c89300f5ed3d9f2d67a44b683a4e30bef09c6e
Jukka Salmi <jukka@users.sourceforge.net>
2007-08-06 19:47:29 +0000

    Add an option to use libar (the asynchronous DNS resolver library shipping with dk-milter) instead of libresolv, enabled by default.

--- dk-milter/options.mk
+++ dk-milter/options.mk
@@ -1,7 +1,8 @@
-# $NetBSD: options.mk,v 1.1.1.1 2007/08/02 22:30:09 jukka Exp $
+# $NetBSD: options.mk,v 1.2 2007/08/06 19:47:29 jukka Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dk-milter
-PKG_SUPPORTED_OPTIONS=	inet6
+PKG_SUPPORTED_OPTIONS=	inet6 arlib
+PKG_SUGGESTED_OPTIONS=	arlib
 
 .include "../../mk/bsd.options.mk"
 
@@ -9,7 +10,18 @@ PKG_SUPPORTED_OPTIONS=	inet6
 ### IPv6 support.
 ###
 .if !empty(PKG_OPTIONS:Minet6)
-SUBST_SED.libs+=	-e 's|@INET6@||'
+SUBST_SED.libs+=	-e 's|@INET6@||g'
 .else
-SUBST_SED.libs+=	-e 's|@INET6@|dnl|'
+SUBST_SED.libs+=	-e 's|@INET6@|dnl|g'
+.endif
+
+###
+### Use asynchronous DNS resolver library shipping with dk-milter.
+###
+.if !empty(PKG_OPTIONS:Marlib)
+SUBST_SED.libs+=	-e 's|@RESOLVLIB@||g'
+SUBST_SED.libs+=	-e 's|@ARLIB@||g'
+.else
+SUBST_SED.libs+=	-e 's|@RESOLVLIB@|resolv bind|g'
+SUBST_SED.libs+=	-e 's|@ARLIB@|dnl|g'
 .endif
