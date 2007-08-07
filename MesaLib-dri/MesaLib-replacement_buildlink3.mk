ff7deb5f571e1e73540efa342db60c916327eb35
Blair Sadewitz <bsadewitz@users.sourceforge.net>
2007-08-07 05:01:21 +0000

    *** empty log message ***

--- MesaLib-dri/MesaLib-replacement_buildlink3.mk
+++ MesaLib-dri/MesaLib-replacement_buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: MesaLib-replacement_buildlink3.mk,v 1.2 2007/08/05 04:56:17 bsadewitz Exp $
+# $NetBSD: MesaLib-replacement_buildlink3.mk,v 1.3 2007/08/07 05:01:21 bsadewitz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 MESALIB_BUILDLINK3_MK:=	${MESALIB_BUILDLINK3_MK}+
@@ -12,9 +12,9 @@ BUILDLINK_PACKAGES+=	MesaLib
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}MesaLib
 
 .if ${MESALIB_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.MesaLib+=	MesaLib>=7.0.1
-BUILDLINK_API_DEPENDS.MesaLib+= MesaLib>=6.4.1
 BUILDLINK_API_DEPENDS.MesaLib+= MesaLib>=3.4.2
+BUILDLINK_API_DEPENDS.MesaLib+= MesaLib>=6.4.1nb1
+BUILDLINK_API_DEPENDS.MesaLib+=	MesaLib>=7.0.1
 BUILDLINK_PKGSRCDIR.MesaLib?=	../../graphics/MesaLib
 BUILDLINK_TRANSFORM+=		l:MesaGL:GL
 BUILDLINK_TRANSFORM+=		l:GL:GL:Xxf86vm:Xfixes:Xdamage:drm
