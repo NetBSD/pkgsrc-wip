837f69332a50d54ab55add9d58040ce77595dc67
Thomas Klausner <wiz@NetBSD.org>
2007-09-20 21:37:24 +0000

    Adapt for librsvg rename. Bump PKGREVISION.

--- evas/buildlink3.mk
+++ evas/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2007/04/29 12:50:22 grzela1 Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2007/09/20 21:37:24 thomasklausner Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 EVAS_BUILDLINK3_MK:=	${EVAS_BUILDLINK3_MK}+
@@ -17,7 +17,7 @@ BUILDLINK_PKGSRCDIR.evas?=	../../wip/evas
 .endif	# EVAS_BUILDLINK3_MK
 
 .include	"../../wip/eet/buildlink3.mk"
-.include	"../../graphics/librsvg2/buildlink3.mk"
+.include	"../../graphics/librsvg/buildlink3.mk"
 .include	"../../graphics/libungif/buildlink3.mk"
 .include	"../../graphics/freetype2/buildlink3.mk"
 
