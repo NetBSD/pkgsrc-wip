5e36c84c0a3c09b71a494db4e1aa765b966ac392
Tobias Nygren <tnn@NetBSD.org>
2007-09-23 13:37:21 +0000

    Import mapnik-0.4.0 into pkgsrc-wip. From DESCR: Mapnik is a Free Toolkit for developing mapping applications. Above all Mapnik is about making beautiful maps. It is easily extensible and suitable for both desktop and web development.

---
+++ mapnik/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/09/23 13:37:21 tnn2 Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+MAPNIK_BUILDLINK3_MK:=	${MAPNIK_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	mapnik
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmapnik}
+BUILDLINK_PACKAGES+=	mapnik
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}mapnik
+
+.if ${MAPNIK_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.mapnik+=	mapnik>=0.4.0
+BUILDLINK_PKGSRCDIR.mapnik?=	../../wip/mapnik
+.endif	# MAPNIK_BUILDLINK3_MK
+
+.include "../../devel/boost-headers/buildlink3.mk"
+#.include "../../devel/libltdl/buildlink3.mk"
+#.include "../../devel/zlib/buildlink3.mk"
+#.include "../../graphics/freetype2/buildlink3.mk"
+#.include "../../graphics/jpeg/buildlink3.mk"
+#.include "../../graphics/png/buildlink3.mk"
+#.include "../../graphics/tiff/buildlink3.mk"
+#.include "../../misc/proj/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
