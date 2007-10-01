24b7df4dc064f33532f1c8df7025a954534dd3b5
Geert Hendrickx <geert@hendrickx.be>
2007-10-01 04:22:21 +0000

    Update to Dovecot 1.1 beta2:
    "Several bug/crashfixes. deliver now supports -a parameter (see
    http://wiki.dovecot.org/LDA) which allows Sieve plugin v1.1.2 to use
    envelope :detail "to" checks.
    
    I also did one more API change for mailbox_transaction_commit*().
    
    There's probably something still wrong with SORT command, but other
    than that I've fixed everything that's been reported."
    
    Also this release can again be built with BSD make.

--- dovecot/buildlink3.mk
+++ dovecot/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/09/23 17:49:59 ghen Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2007/10/01 04:22:21 ghen Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 OPENLDAP_BUILDLINK3_MK:=		${OPENLDAP_BUILDLINK3_MK}+
@@ -14,7 +14,7 @@ BUILDLINK_PACKAGES+=			dovecot
 BUILDLINK_ORDER:=			${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}dovecot
 
 .if !empty(OPENLDAP_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.dovecot+=		dovecot>1.0.99
+BUILDLINK_API_DEPENDS.dovecot+=		dovecot>=1.1beta2
 BUILDLINK_PKGSRCDIR.dovecot?=		../../wip/dovecot
 .endif
 
