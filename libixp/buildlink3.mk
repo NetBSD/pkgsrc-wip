70a20f0f4cf7674ea225403e446f2ac7d7943fc1
Adam Hoka <hoka_adam@users.sourceforge.net>
2007-10-14 12:17:41 +0000

    Upgrading to hg snapshot, because wmii-devel needs newer libixp from the project's mercurial repository.

--- libixp/buildlink3.mk
+++ libixp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2007/03/17 22:25:16 cosmedbr Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2007/10/14 12:17:41 hoka_adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBIXP_BUILDLINK3_MK:=	${LIBIXP_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libixp
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libixp
 
 .if ${LIBIXP_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.libixp+=	libixp>=0.2nb1
+BUILDLINK_API_DEPENDS.libixp+=	libixp>=20071013
 BUILDLINK_PKGSRCDIR.libixp?=	../../wip/libixp
 BUILDLINK_DEPMETHOD.libixp?=    build
 .endif	# LIBIXP_BUILDLINK3_MK
