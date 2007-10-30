90a1aa8b171f2238c0cca921c5df31ac8e9b3307
Dieter Baron <dillo@nih.at>
2007-10-30 17:11:24 +0000

    Add option for new engine Igor, enabled by default.

--- scummvm-nightly/options.mk
+++ scummvm-nightly/options.mk
@@ -1,8 +1,10 @@
-# $NetBSD: options.mk,v 1.4 2007/09/20 07:44:39 dillo Exp $
+# $NetBSD: options.mk,v 1.5 2007/10/30 17:11:24 dillo Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scummvm-nightly
-PKG_SUPPORTED_OPTIONS=	scummvm-nightly-cruise scummvm-nightly-drascula
-PKG_SUGGESTED_OPTIONS=	scummvm-nightly-cruise scummvm-nightly-drascula
+PKG_SUPPORTED_OPTIONS=	scummvm-nightly-cruise
+PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-drascula
+PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-igor
+PKG_SUGGESTED_OPTIONS=	${PKG_SUPPORTED_OPTIONS}
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,4 +13,7 @@ CONFIGURE_ARGS+=--enable-cruise
 .endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-drascula)
 CONFIGURE_ARGS+=--enable-drascula
+.endif
+.if !empty(PKG_OPTIONS:Mscummvm-nightly-igor)
+CONFIGURE_ARGS+=--enable-igor
 .endif
