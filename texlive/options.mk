852707998191fcc4f58b8a05edebae0ed79f1a5e
Peter Bex <airhead@users.sourceforge.net>
2007-11-10 20:15:56 +0000

    Add basic Makefile/options.mk structure for texlive meta-pkg

---
+++ texlive/options.mk
@@ -0,0 +1,25 @@
+# $NetBSD: options.mk,v 1.1 2007/11/10 20:15:56 airhead Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.texlive
+PKG_OPTIONS_REQUIRED_GROUPS=	texmf
+PKG_OPTIONS_GROUP.texmf=	texmf-minimal texmf-full
+PKG_SUPPORTED_OPTIONS=		texmf-doc
+PKG_SUGGESTED_OPTIONS=		texmf-minimal
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mtexmf-full)
+DEPENDS+=	texlive-texmf-full>=[0-9]*:../../wip/texlive-texmf-full
+.endif
+
+.if !empty(PKG_OPTIONS:Mtexmf-minimal)
+DEPENDS+=	texlive-texmf-minimal>=[0-9]*:../../wip/texlive-texmf-minimal
+.endif
+
+#
+# texmf-doc can be installed independently of the minimal/full texmf set
+# I guess this means there's no matching doc package for the minimal texmf set
+#
+.if !empty(PKG_OPTIONS:Mtexmf-doc)
+DEPENDS+=	texlive-texmf-doc>=[0-9]*:../../wip/texlive-texmf-doc
+.endif
