5fc06d7ed05ee50ab3fbb82e72c2d64d4b356b81
Adam Hoka <hoka_adam@users.sourceforge.net>
2007-12-02 14:53:40 +0000

    Component required by E17.
    Packaged by Yazzy.

---
+++ efreet/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/12/02 14:53:40 hoka_adam Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+EFREET_BUILDLINK3_MK:=	${EFREET_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	efreet
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nefreet}
+BUILDLINK_PACKAGES+=	efreet
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}efreet
+
+.if ${EFREET_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.efreet+=	efreet>=0.0.3.006
+BUILDLINK_PKGSRCDIR.efreet?=	../../wip/efreet
+.endif	# EFREET_BUILDLINK3_MK
+
+.include        "../../wip/ecore/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
