4d984c3ae5c8dc22f84735ed0059e6b39235c840
Adrian Portelli <adrian_p@users.sourceforge.net>
2007-12-02 18:37:41 +0000

    Remove outdated files

--- isc-dhcpd/files/site.h
+++
@@ -1,6 +0,0 @@
-#define _PATH_DHCPD_CONF "@@SYSCONFDIR@@/dhcp/dhcpd.conf"
-#define _PATH_DHCPD_PID "@@VARBASE@@/run/dhcpd.pid"
-#define _PATH_DHCPD_DB "@@VARBASE@@/db/dhcp/dhcpd.leases"
-#define _PATH_DHCLIENT_DB "@@VARBASE@@/db/dhcp/dhclient.leases"
-#define _PATH_DHCLIENT_SCRIPT "@@PREFIX@@/sbin/dhclient-script"
-#define DHCPD_LOG_FACILITY LOG_LOCAL1
