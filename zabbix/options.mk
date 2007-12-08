7c9b4c915429802d0daf97643d8671df217bd73c
Aleksej Saushev <asau@users.sourceforge.net>
2007-12-08 11:37:15 +0000

    Convert to use options.

--- zabbix/options.mk
+++ zabbix/options.mk
@@ -1,19 +1,60 @@
+PKG_OPTIONS_VAR=		PKG_OPTIONS.zabbix
+PKG_SUPPORTED_OPTIONS+=		zabbix-server zabbix-agent
+PKG_SUPPORTED_OPTIONS+=		pgsql sqlite mysql
+PKG_SUPPORTED_OPTIONS+=		curl
+PKG_SUPPORTED_OPTIONS+=		ldap
+PKG_SUPPORTED_OPTIONS+=		snmp
+PKG_SUPPORTED_OPTIONS+=		zabbix-static
+
+.include "../../mk/bsd.prefs.mk"
+.include "../../mk/bsd.options.mk"
+
+# Default to both, server and agent.
+.if empty(PKG_OPTIONS:Mzabbix-server) && empty(PKG_OPTIONS:Mzabbix-agent)
+PKG_OPTIONS+=	zabbix-server zabbix-agent
+.endif
+.if !empty(PKG_OPTIONS:Mzabbix-server)
 CONFIGURE_ARGS+=	--enable-server
+.endif
+.if !empty(PKG_OPTIONS:Mzabbix-agent)
 CONFIGURE_ARGS+=	--enable-agent
+.endif
 
-#CONFIGURE_ARGS+=
+# DB support is mandatory, default to SQLite
+.if empty(PKG_OPTIONS:Mpgsql) && empty(PKG_OPTIONS:Msqlite) && empty(PKG_OPTIONS:Mmysql)
+PKG_OPTIONS+=	sqlite
+.endif
 
-# DB access is mandatory
-#CONFIGURE_ARGS+=	--with-sqlite3
-#CONFIGURE_ARGS+=	--with-mysql
+.if !empty(PKG_OPTIONS:Mpgsql)
 CONFIGURE_ARGS+=	--with-pgsql
 .include "../../databases/postgresql82-client/buildlink3.mk"
+.endif
+.if !empty(PKG_OPTIONS:Msqlite)
+CONFIGURE_ARGS+=	--with-sqlite3
+.include "../../databases/sqlite3/buildlink3.mk"
+.endif
+.if !empty(PKG_OPTIONS:Mmysql)
+CONFIGURE_ARGS+=	--with-mysql
+.include "../../databases/mysql5-client/buildlink3.mk"
+.endif
 
-#CONFIGURE_ARGS+=	--with-libcurl
+.if !empty(PKG_OPTIONS:Mcurl)
+CONFIGURE_ARGS+=	--with-libcurl
 .include "../../www/curl/buildlink3.mk"
+.endif
 
-#CONFIGURE_ARGS+=	--with-net-snmp
+.if !empty(PKG_OPTIONS:Msnmp)
+CONFIGURE_ARGS+=	--with-net-snmp
+.include "../../net/net-snmp/buildlink3.mk"
+.endif
+#.if !empty(PKG_OPTIONS:Mucd-snmp)
 #CONFIGURE_ARGS+=	--with-ucd-snmp
+#.endif
 
-#CONFIGURE_ARGS+=	--with-ldap
-#CONFIGURE_ARGS+=	--enable-static
+.if !empty(PKG_OPTIONS:Mldap)
+CONFIGURE_ARGS+=	--with-ldap
+.endif
+
+.if !empty(PKG_OPTIONS:Mzabbix-static)
+CONFIGURE_ARGS+=	--enable-static
+.endif
