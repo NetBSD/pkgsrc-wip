99c946ea4882aa849f7d7a23aa9c1c98c06cf491
Eric Schnoebelen <schnoebe@NetBSD.org>
2007-12-09 07:25:35 +0000

    Resurrect the package for development/maintenance of the "cutting-edge" version of jabberd2.

---
+++ jabberd2/files/sm.sh
@@ -0,0 +1,42 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: sm.sh,v 1.5 2007/12/09 07:25:35 schnoebe Exp $
+#
+# PROVIDE: sm
+# REQUIRE: DAEMON c2s
+
+if [ -f /etc/rc.subr ]; then
+	. /etc/rc.subr
+fi
+
+name="sm"
+rcvar=$name
+command="@PREFIX@/bin/${name}"
+required_files="@PKG_SYSCONFDIR@/${name}.xml"
+extra_commands="reload"
+command_args="2>&1 > /dev/null &"
+sm_user="@JABBERD_USER@"
+pidfile="@JABBERD_PIDDIR@/${name}.pid"
+stop_postcmd="remove_pidfile"
+start_precmd="ensure_piddir"
+
+ensure_piddir()
+{
+	mkdir -p @JABBERD_PIDDIR@
+	chown @JABBERD_USER@ @JABBERD_PIDDIR@
+}
+
+remove_pidfile()
+{
+        if [ -f @JABBERD_PIDDIR@/${name}.pid ]; then
+            rm -f @JABBERD_PIDDIR@/${name}.pid
+        fi
+}
+
+if [ -f /etc/rc.subr ]; then
+	load_rc_config $name
+	run_rc_command "$1"
+else
+	@ECHO@ -n " ${name}"
+	${command} ${sm_flags} ${command_args}
+fi
