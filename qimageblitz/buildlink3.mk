908d7df631baab6217624e9ef6a895d88ff9a99b
Mark Davies <mark@ecs.vuw.ac.nz>
2007-12-12 02:47:59 +0000

    Blitz is an interm image effect library that people can use until KDE4.1 is released. KImageEffect, the old image effect class is being dropped for KDE4.0 and the replacement, Quasar, won't be ready until KDE4.1. Blitz gives people something to use in the meantime.

---
+++ qimageblitz/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/12/12 02:47:59 mwdavies Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+QIMAGEBLITZ_BUILDLINK3_MK:=	${QIMAGEBLITZ_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	qimageblitz
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nqimageblitz}
+BUILDLINK_PACKAGES+=	qimageblitz
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}qimageblitz
+
+.if ${QIMAGEBLITZ_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.qimageblitz+=	qimageblitz>=20071203
+BUILDLINK_PKGSRCDIR.qimageblitz?=	../../wip/qimageblitz
+.endif	# QIMAGEBLITZ_BUILDLINK3_MK
+
+.include "../../x11/qt4-libs/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
