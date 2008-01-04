a12bed5bbce04a6bacaa1ab8d30051ad16fce73f
Aleksej Saushev <asau@users.sourceforge.net>
2008-01-04 15:27:48 +0000

    + gdbm

--- clisp-current/options.mk
+++ clisp-current/options.mk
@@ -1,9 +1,11 @@
-$NetBSD: options.mk,v 1.1 2008/01/04 13:38:59 asau Exp $
+$NetBSD: options.mk,v 1.2 2008/01/04 15:27:48 asau Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.clisp-current
 
 PKG_SUPPORTED_OPTIONS+=		ffcall
+
 PKG_SUPPORTED_OPTIONS+=		pgsql	# requires "ffcall"
+PKG_SUPPORTED_OPTIONS+=		gdbm
 
 .include "../../mk/bsd.prefs.mk"
 .include "../../mk/bsd.options.mk"
@@ -14,9 +16,16 @@ PKG_SUPPORTED_OPTIONS+=		pgsql	# requires "ffcall"
 .endif
 
 # DBMS
+#  PostgreSQL:
 .if !empty(PKG_OPTIONS:Mpgsql)
 CONFIGURE_ARGS+=	--with-module=postgresql
 .include "../../mk/pgsql.buildlink3.mk"
 .endif
+#  GDBM
+.if !empty(PKG_OPTIONS:Mgdbm)
+CONFIGURE_ARGS+=	--with-module=gdbm
+.include "../../databases/gdbm/buildlink3.mk"
+.endif
+
 
 CONFIGURE_ARGS+=	${MODULES}
