037703554c3ab9c56951260e938d213202269f0d
Tobias Nygren <tnn@NetBSD.org>
2008-01-19 11:54:23 +0000

    Import first stab at a JDK 1.6.0 package. Warning: incomplete, doesn't quite work yet.

---
+++ jdk16/options.mk
@@ -0,0 +1,41 @@
+# $NetBSD: options.mk,v 1.1.1.1 2008/01/19 11:54:23 tnn2 Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.jdk16
+PKG_SUPPORTED_OPTIONS=		jdk16-jce inet6 # jdk16-plugin
+PKG_SUGGESTED_OPTIONS=		# jdk16-plugin 
+PKG_OPTIONS_LEGACY_VARS+=	JDK16_USE_JCE:jdk16-jce
+
+# fixme
+#.include "../../www/seamonkey/gecko-options.mk"
+.include "../../mk/bsd.options.mk"
+
+###
+### Java(TM) Cryptography Extension (JCE)
+###
+.if !empty(PKG_OPTIONS:Mjdk16-jce)
+DISTFILES+=	jce_policy-6.zip
+PLIST_SRC+=	PLIST.jce
+post-build:
+	cd ${WRKDIR}/jce ; ${PAX} -rw -pp . ${JDKIMAGEDIR}/jre/lib/security
+.endif
+
+
+###
+### Mozilla plugin
+###
+.if !empty(PKG_OPTIONS:Mjdk16-plugin)
+PKG_FAIL_REASON+=	Sorry, the plugin option is broken. Please fix it!
+MOZILLA=	${PKG_OPTIONS:@opt@${PKG_OPTIONS_GROUP.gecko:M${opt}}@}
+MAKE_ENV+=	BROWSER=${MOZILLA:Q}
+MAKE_ENV+=	ALT_MOZILLA_HEADERS_PATH=${LOCALBASE:Q}/include/${MOZILLA}
+
+.include "../../www/seamonkey/gecko-depends.mk"
+# Check: is nspr necessary?
+#.include "../../devel/nspr/buildlink3.mk"
+BUILDLINK_DEPMETHOD.nspr=	build
+PLIST_SRC+=			PLIST.plugin
+MESSAGE_SRC+=			MESSAGE.plugin
+
+.else
+MAKE_ENV+=			BUILD_PLUGIN=false
+.endif
