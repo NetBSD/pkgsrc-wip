e22ea3e4feb4fc63a0c7a24089c7535bd6d1d4fb
Roland Illig <roland.illig@gmx.de>
2008-02-01 08:36:35 +0000

    Fixed some pkglint warnings.

--- python25/buildlink3.mk
+++ python25/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2007/09/13 05:58:46 netcap Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2008/02/01 08:36:35 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PYTHON25_BUILDLINK3_MK:=	${PYTHON25_BUILDLINK3_MK}+
@@ -16,9 +16,9 @@ BUILDLINK_API_DEPENDS.python25+=	python25>=2.5
 BUILDLINK_ABI_DEPENDS.python25?=	python25>=2.5.1
 BUILDLINK_PKGSRCDIR.python25?=	../../wip/python25
 
-.if defined(BUILDLINK_DEPMETHOD.python)
+.  if defined(BUILDLINK_DEPMETHOD.python)
 BUILDLINK_DEPMETHOD.python25?=	${BUILDLINK_DEPMETHOD.python}
-.endif
+.  endif
 
 BUILDLINK_INCDIRS.python25+=	include/python2.5
 BUILDLINK_LIBDIRS.python25+=	lib/python2.5/config
@@ -26,8 +26,8 @@ BUILDLINK_TRANSFORM+=		l:python:python2.5
 
 .endif	# PYTHON25_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
-
 .include "../../mk/dlopen.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
