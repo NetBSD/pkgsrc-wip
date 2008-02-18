c92c1219693394eb9607d631323410c12c608b11
Aleksej Saushev <asau@users.sourceforge.net>
2008-02-18 20:21:02 +0000

    Import development version of WINE. Wine is a program which allows running Microsoft Windows programs (including DOS, Windows 3.x and Win32 executables) on Unix.

---
+++ wine/options.mk
@@ -0,0 +1,24 @@
+# $NetBSD: options.mk,v 1.1.1.1 2008/02/18 20:21:02 asau Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.wine
+PKG_SUPPORTED_OPTIONS=	cups opengl sane
+PKG_SUGGESTED_OPTIONS=	opengl
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mcups)
+.include "../../print/cups/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mopengl)
+PLIST_SUBST+=	WINE_OPENGL=""
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../graphics/glut/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-opengl
+PLIST_SUBST+=	WINE_OPENGL="@comment "
+.endif
+
+.if !empty(PKG_OPTIONS:Msane)
+.include "../../graphics/sane-backends/buildlink3.mk"
+.endif
