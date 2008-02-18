a5b7b416dd0a9724dbfc01f07ed58ac992a5d350
Blair Sadewitz <bsadewitz@users.sourceforge.net>
2008-02-18 09:17:54 +0000

    New and improved xorg server-1.4-branch-based server lovingly crafted by me with the finest barely and hops available.  You're soaking in it. These days an xorg server can require so many patches that I gave up on maintaining them in pkgsrc; I use stgit.  It's really a great tool, and saying a lot because I despise git. ;)
    If anyone's interested in the patch series, ask me and I will email it to you.
    
    I am interested in any and all feedback, accolades, criticisms, burnt
    offerings, etc.  Cheers!

---
+++ modular-xorg-server/buildlink3.mk
@@ -0,0 +1,30 @@
+# $NetBSD: buildlink3.mk,v 1.4 2008/02/18 09:17:54 bsadewitz Exp $
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
+MODULAR_XORG_SERVER_BUILDLINK3_MK:=	${MODULAR_XORG_SERVER_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	modular-xorg-server
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmodular-xorg-server}
+BUILDLINK_PACKAGES+=	modular-xorg-server
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}modular-xorg-server
+
+.if ${MODULAR_XORG_SERVER_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.modular-xorg-server+=	modular-xorg-server>=1.4
+BUILDLINK_PKGSRCDIR.modular-xorg-server?=	../../wip/modular-xorg-server
+.endif	# MODULAR_XORG_SERVER_BUILDLINK3_MK
+
+.include "../../x11/compositeproto/buildlink3.mk"
+.include "../../x11/fontsproto/buildlink3.mk"
+.include "../../x11/inputproto/buildlink3.mk"
+.include "../../x11/pixman/buildlink3.mk"
+.include "../../x11/randrproto/buildlink3.mk"
+.include "../../x11/renderproto/buildlink3.mk"
+.include "../../x11/videoproto/buildlink3.mk"
+.include "../../x11/xextproto/buildlink3.mk"
+.include "../../x11/xf86driproto/buildlink3.mk"
+.include "../../x11/xproto/buildlink3.mk"
+
+BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
---
+++ modular-xorg-server/options.mk
@@ -0,0 +1,59 @@
+# $NetBSD: options.mk,v 1.1 2008/02/18 09:17:54 bsadewitz Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.modular-xorg-server
+PKG_SUPPORTED_OPTIONS=	dbus dri inet6
+# remove after 2007Q4
+PKG_OPTIONS_LEGACY_OPTS=glx:dri
+.if defined(PKG_OPTIONS.xorg-server)
+PKG_LEGACY_OPTIONS+=	${PKG_OPTIONS.xorg-server}
+PKG_OPTIONS_DEPRECATED_WARNINGS+="Deprecated variable PKG_OPTIONS.xorg-server used, use "${PKG_OPTIONS_VAR:Q}" instead."
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mdbus)
+CONFIGURE_ARGS+=	--enable-dbus
+.  include "../../sysutils/dbus/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-dbus
+.endif
+
+.if !empty(PKG_OPTIONS:Mdri)
+DISTFILES=		${DISTNAME}${EXTRACT_SUFX}
+DISTFILES+=		MesaLib-7.0.2.tar.bz2
+SITES.MesaLib-7.0.2.tar.bz2= ${MASTER_SITE_SOURCEFORGE:=mesa3d/}
+MESA_SRC=		${WRKDIR}/Mesa-7.0.2
+CONFIGURE_ARGS+=	--enable-glx
+CONFIGURE_ARGS+=	--enable-aiglx
+CONFIGURE_ARGS+=	--with-mesa-source=${MESA_SRC}
+.if ${MACHINE_ARCH} == "x86_64" || ${MACHINE_ARCH} == "sparc64" || \
+    ${MACHINE_ARCH} == "alpha"
+GLX_DEFINES+=		-D__GLX_ALIGN64
+.endif
+CONFIGURE_ENV+=		GLX_DEFINES=${GLX_DEFINES:M*:Q}
+# glcore.h and dri_interface.h shipped with *proto are older than
+# those in Mesa-7.0.2. Either patch them or trick the build into using
+# the newer ones.
+#BUILDLINK_API_DEPENDS.glproto+= glproto>=1.4.8nb1
+#BUILDLINK_API_DEPENDS.xf86driproto+= xf86driproto>=2.0.3nb1
+PLIST_SUBST+=		USE_DRI=""
+
+dri-post-extract:
+	${LN} -s ${MESA_SRC:Q}/include/GL ${WRKSRC:Q}/GL/glx/GL
+
+.else
+CONFIGURE_ARGS+=	--disable-glx
+PLIST_SUBST+=		USE_DRI="@comment "
+
+dri-post-extract:
+	@${DO_NADA}
+
+# for GLX we already have the Mesa source
+.include "../../graphics/MesaLib/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--enable-ipv6
+.else
+CONFIGURE_ARGS+=	--disable-ipv6
+.endif
