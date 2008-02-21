5db957dc0e830c8f05505926022b3da9b4d92db7
Aleksej Saushev <asau@users.sourceforge.net>
2008-02-21 21:37:23 +0000

    Lisp binary file suffix is implementation dependent.

--- fricas/options.mk
+++ fricas/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2008/02/16 10:23:11 asau Exp $
+# $NetBSD: options.mk,v 1.2 2008/02/21 21:37:23 asau Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.fricas
 PKG_OPTIONS_REQUIRED_GROUPS=	lisp
@@ -10,15 +10,19 @@ PKG_SUGGESTED_OPTIONS+=		clisp
 
 # Select Lisp backend
 .if !empty(PKG_OPTIONS:Mclisp)
+BIN=			fas
 BUILD_DEPENDS+=		clisp>=2.41:../../lang/clisp
 CONFIGURE_ARGS+=	--with-lisp=clisp --with-x=no
 .endif
 .if !empty(PKG_OPTIONS:Msbcl)
+BIN=			fasl
 BUILD_DEPENDS+=		sbcl-[0-9]*:../../lang/sbcl
 CONFIGURE_ARGS+=	--with-lisp=sbcl --with-x=no
 .endif
 
 # Modify PLIST
+PLIST_SUBST+=	BIN=${BIN:Q}
+
 .for opt in clisp sbcl
 .if !empty(PKG_OPTIONS:M${opt})
 PLIST_SUBST+=	${opt}=""
