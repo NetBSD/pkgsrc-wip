b5115c6f91ef3bd4ae5aa96ed0df4679c69ccdea
Daniel Horecki <morr@NetBSD.org>
2008-02-24 05:01:56 +0000

    Correct file

--- e17/buildlink3.mk
+++ e17/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2008/02/24 04:44:06 shindenmorr Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/02/24 05:01:56 shindenmorr Exp $
 # XXX
 # XXX This file was created automatically using createbuildlink-3.13.
 # XXX After this file has been verified as correct, the comment lines
@@ -26,17 +26,11 @@ BUILDLINK_API_DEPENDS.e17+=	e17>=0.16.999.042
 BUILDLINK_PKGSRCDIR.e17?=	../../wip/e17
 .endif	# E17_BUILDLINK3_MK
 
-# XXX
-# XXX Uncomment and keep only the buildlink3 lines below which are directly
-# XXX needed for dependencies to compile, link, and run.  If this package
-# XXX provides a wrappered API or otherwise does not expose the APIs of the
-# XXX buildlink3 lines below to dependencies, remove them.
-# XXX
-#.include "../../wip/ecore/buildlink3.mk"
-#.include "../../wip/edje/buildlink3.mk"
-#.include "../../wip/eet/buildlink3.mk"
-#.include "../../wip/efreet/buildlink3.mk"
-#.include "../../wip/evas/buildlink3.mk"
-#.include "../../graphics/imlib2/buildlink3.mk"
+.include "../../wip/ecore/buildlink3.mk"
+.include "../../wip/edje/buildlink3.mk"
+.include "../../wip/eet/buildlink3.mk"
+.include "../../wip/efreet/buildlink3.mk"
+.include "../../wip/evas/buildlink3.mk"
+.include "../../graphics/imlib2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
