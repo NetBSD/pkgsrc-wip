06709ea9dc39f9a216bdcebeed1d9ad0d2058bf5
Blair Sadewitz <bsadewitz@users.sourceforge.net>
2008-03-03 06:02:04 +0000

    Pull in mesalib/hacks.mk, fix PLIST + builds.

---
+++ glu/hacks.mk
@@ -0,0 +1,4 @@
+.if !defined(GLU_HACKS_MK)
+GLU_HACKS_MK= 	# defined
+.  include "../../MesaLib/hacks.mk"
+.endif
---
+++ glut/hacks.mk
@@ -0,0 +1,4 @@
+.if !defined(GLUT_HACKS_MK)
+GLUT_HACKS_MK= 	# defined
+.  include "../../MesaLib/hacks.mk"
+.endif
