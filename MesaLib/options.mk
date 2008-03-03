b0b92394f379d50bbe4efe9c1efca4d4d44b8103
Blair Sadewitz <bsadewitz@users.sourceforge.net>
2008-03-03 04:36:47 +0000

    More installation goo

--- MesaLib/options.mk
+++ MesaLib/options.mk
@@ -1,7 +1,12 @@
-# $NetBSD: options.mk,v 1.3 2008/03/03 02:20:23 tnn2 Exp $
+# $NetBSD: options.mk,v 1.4 2008/03/03 04:36:47 bsadewitz Exp $
+PKG_OPTIONS_VAR=	PKG_OPTIONS.MesaLib
 
 .  include "../../mk/bsd.fast.prefs.mk"
 
+# Assembler code build configurations
+.if ${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64"
+PKG_SUPPORTED_OPTIONS+= 	${MACHINE_ARCH}
+.endif
 ###
 ### XXX  There are [probably] others, but let's not get crazy just yet.
 ### 	 This will take a while to test for the myriad platforms we 
@@ -12,18 +17,22 @@
     ${MACHINE_ARCH} == "sparc64") && \
     (${OPSYS} == "NetBSD" || ${OPSYS} == "FreeBSD" || ${OPSYS} == "OpenBSD")
     
-PKG_OPTIONS_VAR=	PKG_OPTIONS.MesaLib
-PKG_SUPPORTED_OPTIONS= 	dri
-PKG_SUGGESTED_OPTIONS=	dri # doesn't build without this option
+PKG_SUPPORTED_OPTIONS+= 	dri
 
 .include "../../mk/bsd.options.mk"
 
 ###
-### XXX Yes, this is a bid overly verbose; with Mesa, that can't
+### XXX Yes, this is a bit overly verbose; with Mesa, that can't
 ### 	hurt much.
 ###
+.if !empty(PKG_OPTIONS:Mi386) || !empty(PKG_OPTIONS:Mx86_64)
+BUILD_TARGET_SUFFIX= 	-${MACHINE_ARCH}
+.else
+BUILD_TARGET_SUFFIX= 	# empty
+.endif
+
 .if !empty(PKG_OPTIONS:Mdri)
-BUILD_TARGET= 	pkgsrc-dri
+BUILD_TARGET= 	pkgsrc-dri${BUILD_TARGET_SUFFIX}
 .  include "../../textproc/expat/buildlink3.mk"
 .  include "../../x11/glproto/buildlink3.mk"
 .  include "../../x11/libXdamage/buildlink3.mk"
@@ -32,6 +41,10 @@ BUILD_TARGET= 	pkgsrc-dri
 .  include "../../x11/libXxf86vm/buildlink3.mk"
 .  include "../../x11/xf86driproto/buildlink3.mk"
 .  include "../../x11/xf86vidmodeproto/buildlink3.mk"
+PLIST_SUBST+= 	DRI=""
+.else
+BUILD_TARGET= 	pkgsrc${BUILD_TARGET_SUFFIX}
+PLIST_SUBST+=	DRI="@comment "
 .endif
 
 .endif
