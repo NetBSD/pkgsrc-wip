9582f5972f056daeb6cd22ed68e02fca345adc5e
Emil Sköldberg <emil_s@users.sourceforge.net>
2008-03-06 17:44:04 +0000

    - Update to version 1.4.1 - Fix INSTALL script

--- hs-x11/buildlink3.mk
+++ hs-x11/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/05/18 12:28:30 emil_s Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/03/06 17:44:04 emil_s Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 HS_X11_BUILDLINK3_MK:=	${HS_X11_BUILDLINK3_MK}+
@@ -13,10 +13,11 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}hs-x11
 
 .if ${HS_X11_BUILDLINK3_MK} == "+"
 BUILDLINK_DEPMETHOD.hs-x11?=	build
-BUILDLINK_API_DEPENDS.hs-x11+=	hs-x11>=1.2
+BUILDLINK_API_DEPENDS.hs-x11+=	hs-x11>=1.4
 BUILDLINK_PKGSRCDIR.hs-x11?=	../../wip/hs-x11
 .endif	# HS_X11_BUILDLINK3_MK
 
+.include "../../wip/cabal/buildlink3.mk"
 .include "../../lang/ghc/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
