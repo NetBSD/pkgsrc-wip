7e9c18f8d5b3493ac6092afcdcaf6e50f8b0adad
Emil Sköldberg <emil_s@users.sourceforge.net>
2008-03-06 17:46:32 +0000

    - Update to version 0.4 - Fix INSTALL script - (Hopefully) fix handling of X: libraries and includes

--- hs-x11-extras/buildlink3.mk
+++ hs-x11-extras/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/05/18 12:30:20 emil_s Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/03/06 17:46:32 emil_s Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 HS_X11_EXTRAS_BUILDLINK3_MK:=	${HS_X11_EXTRAS_BUILDLINK3_MK}+
@@ -13,10 +13,11 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}hs-x11-extras
 
 .if ${HS_X11_EXTRAS_BUILDLINK3_MK} == "+"
 BUILDLINK_DEPMETHOD.hs-x11-extras?=	build
-BUILDLINK_API_DEPENDS.hs-x11-extras+=	hs-x11-extras>=0.1
+BUILDLINK_API_DEPENDS.hs-x11-extras+=	hs-x11-extras>=0.4
 BUILDLINK_PKGSRCDIR.hs-x11-extras?=	../../wip/hs-x11-extras
 .endif	# HS_X11_EXTRAS_BUILDLINK3_MK
 
+.include "../../wip/cabal/buildlink3.mk"
 .include "../../wip/hs-x11/buildlink3.mk"
 .include "../../lang/ghc/buildlink3.mk"
 
