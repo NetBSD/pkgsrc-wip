a95a3a279449e8811023021b42a7792e3e9e3417
Tobias Nygren <tnn@NetBSD.org>
2008-03-11 05:27:38 +0000

    - Fix build without DRI. - Always specify the pathname to the .la file when linking with libtool - Even more whitespace cleanup

--- MesaLib/options.mk
+++ MesaLib/options.mk
@@ -1,12 +1,14 @@
-# $NetBSD: options.mk,v 1.5 2008/03/09 12:46:54 tnn2 Exp $
+# $NetBSD: options.mk,v 1.6 2008/03/11 05:27:38 tnn2 Exp $
 PKG_OPTIONS_VAR=	PKG_OPTIONS.MesaLib
+PKG_SUPPORTED_OPTIONS=	# Added to below
 
-.  include "../../mk/bsd.fast.prefs.mk"
+.include "../../mk/bsd.fast.prefs.mk"
 
 # Assembler code build configurations
 .if ${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64"
 PKG_SUPPORTED_OPTIONS+=		${MACHINE_ARCH}
 .endif
+
 ###
 ### XXX  There are [probably] others, but let's not get crazy just yet.
 ### 	 This will take a while to test for the myriad platforms we
@@ -16,21 +18,21 @@ PKG_SUPPORTED_OPTIONS+=		${MACHINE_ARCH}
     ${MACHINE_ARCH} == "powerpc" || ${MACHINE_ARCH} == "sparc" || \
     ${MACHINE_ARCH} == "sparc64") && \
     (${OPSYS} == "NetBSD" || ${OPSYS} == "FreeBSD" || ${OPSYS} == "OpenBSD")
-
 PKG_SUPPORTED_OPTIONS+=		dri
+.endif
 
 .include "../../mk/bsd.options.mk"
 
 ###
 ### XXX Yes, this is a bit overly verbose; with Mesa, that can't hurt much.
 ###
-.  if !empty(PKG_OPTIONS:Mi386) || !empty(PKG_OPTIONS:Mx86_64)
+.if !empty(PKG_OPTIONS:Mi386) || !empty(PKG_OPTIONS:Mx86_64)
 BUILD_TARGET_SUFFIX=	-${MACHINE_ARCH}
-.  else
+.else
 BUILD_TARGET_SUFFIX=	# empty
-.  endif
+.endif
 
-.  if !empty(PKG_OPTIONS:Mdri)
+.if !empty(PKG_OPTIONS:Mdri)
 BUILD_TARGET=	pkgsrc-dri${BUILD_TARGET_SUFFIX}
 .  include "../../textproc/expat/buildlink3.mk"
 .  include "../../x11/glproto/buildlink3.mk"
@@ -41,9 +43,9 @@ BUILD_TARGET=	pkgsrc-dri${BUILD_TARGET_SUFFIX}
 .  include "../../x11/xf86driproto/buildlink3.mk"
 .  include "../../x11/xf86vidmodeproto/buildlink3.mk"
 PLIST_SUBST+=	DRI=""
-.  else
+PLIST_SUBST+=	NODRI="@comment"
+.else
 BUILD_TARGET=	pkgsrc${BUILD_TARGET_SUFFIX}
 PLIST_SUBST+=	DRI="@comment "
-.  endif
-
+PLIST_SUBST+=	NODRI=""
 .endif
