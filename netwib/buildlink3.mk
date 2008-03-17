c41f861c7c8eaf778b009813c2e0a57b5f505872
Tobias Nygren <tnn@NetBSD.org>
2008-03-17 00:14:46 +0000

    Make netwib build by updating to 5.35.0.

--- netwib/buildlink3.mk
+++ netwib/buildlink3.mk
@@ -1,18 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.3 2006/04/06 07:18:13 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2008/03/17 00:14:46 tnn2 Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 NETWIB_BUILDLINK3_MK:=	${NETWIB_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	netwib
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nnetwib}
 BUILDLINK_PACKAGES+=	netwib
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}netwib
 
-.if !empty(NETWIB_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.netwib+=	netwib>=5.16.0
+.if ${NETWIB_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.netwib+=	netwib>=5.35.0
 BUILDLINK_PKGSRCDIR.netwib?=	../../wip/netwib
 .endif	# NETWIB_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+.include "../../devel/libnet10/buildlink3.mk"
+.include "../../net/libpcap/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
