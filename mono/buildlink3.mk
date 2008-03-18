aa4b5a06251e7c29526a6de7aaeb2cc0a270be99
Mihai Chelaru <koifren@users.sourceforge.net>
2008-03-18 19:58:13 +0000

    Resolve conflicts with import of 1.9

---
+++ mono/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.3 2008/03/18 19:58:13 koifren Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+MONO_BUILDLINK3_MK:=	${MONO_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	mono
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmono}
+BUILDLINK_PACKAGES+=	mono
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}mono
+
+.if ${MONO_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.mono+=	mono>=1.2.6
+BUILDLINK_PKGSRCDIR.mono?=	../../lang/mono
+.endif	# MONO_BUILDLINK3_MK
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../textproc/icu/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
