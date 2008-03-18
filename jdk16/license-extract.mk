c418b88f9bc6f541f9b401ee27d38b07ef00a2b4
Tobias Nygren <tnn@NetBSD.org>
2008-03-18 01:55:28 +0000

    Simplify conditional

--- jdk16/license-extract.mk
+++ jdk16/license-extract.mk
@@ -1,4 +1,4 @@
-# $NetBSD: license-extract.mk,v 1.5 2008/02/27 20:41:24 tnn2 Exp $
+# $NetBSD: license-extract.mk,v 1.6 2008/03/18 01:55:28 tnn2 Exp $
 
 .if !defined(BATCH) || !defined(PKG_DEVELOPER)
 INTERACTIVE_STAGE+=	extract
@@ -8,6 +8,7 @@ post-extract:
 	@${CHMOD} -R u+w ${WRKSRC}
 	@cp ${PKGSRCDIR}/wip/jdk16/files/x_x2zip.c ${WRKSRC}
 	@cd ${WRKSRC} && ${CC} -o x_x2zip x_x2zip.c
+.if !defined(BATCH) || !defined(PKG_DEVELOPER)
 	@echo ""
 	@echo "=> You will now be shown two licenses. If you accept the terms"
 	@echo "=> of each license agreement, type "yes" followed by ENTER to"
@@ -15,11 +16,8 @@ post-extract:
 	@echo "=> to abort."
 	@sh -c 'read ANS'
 	cd ${WRKSRC} && unzip -p ${JRL_SRCFILE} LICENSE | more
-.if !defined(BATCH) || !defined(PKG_DEVELOPER)
 	@sh -c 'echo -n "=> Do you accept the terms of the license agreement? "; read ANS; if [ "$$ANS" != "yes" ]; then exit 1; fi'
-.endif
 	cd ${WRKSRC} && unzip -p ${JRL_BINFILE} LICENSE | more
-.if !defined(BATCH) || !defined(PKG_DEVELOPER)
 	@sh -c 'echo -n "=> Do you accept the terms of the license agreement? "; read ANS; if [ "$$ANS" != "yes" ]; then exit 1; fi'
 .endif
 	cd ${WRKSRC} && unzip -q ${JRL_BINFILE} X_X && ./x_x2zip 'YES I ACCEPT THE CLICK THROUGH LICENSE.  '  X_X && unzip -q X_X.zip
