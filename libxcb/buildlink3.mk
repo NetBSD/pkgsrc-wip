8a103d39dcc0bf45e019d910a73b574f94b21ef5
Blair Sadewitz <bsadewitz@users.sourceforge.net>
2008-03-25 20:52:15 +0000

    Bump buildlink3.mk rev.

--- libxcb/buildlink3.mk
+++ libxcb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2008/03/24 21:08:59 bsadewitz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2008/03/25 20:52:15 bsadewitz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBXCB_BUILDLINK3_MK:=	${LIBXCB_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libxcb
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libxcb
 
 .if ${LIBXCB_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.libxcb+=	libxcb>=1.0
+BUILDLINK_API_DEPENDS.libxcb+=	libxcb>=1.1nb2
 BUILDLINK_PKGSRCDIR.libxcb?=	../../wip/libxcb
 .endif	# LIBXCB_BUILDLINK3_MK
 
