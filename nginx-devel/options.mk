2c9bbd5f3b781194721df0087ee276d13eb32c19
Nikolay Kalev <nkalev@gmail.com>
2008-03-29 01:43:33 +0000

    added options dav https flv sub gtools

--- nginx-devel/options.mk
+++ nginx-devel/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1.1.1 2008/03/27 18:54:57 rootcho Exp $
+# $NetBSD: options.mk,v 1.2 2008/03/29 01:43:33 rootcho Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nginx
-PKG_SUPPORTED_OPTIONS=	ssl pcre
+PKG_SUPPORTED_OPTIONS=	ssl pcre https dav flv sub gtools
 PKG_SUGGESTED_OPTIONS=	ssl pcre
 
 .include "../../mk/bsd.options.mk"
@@ -16,4 +16,24 @@ CONFIGURE_ARGS+=	--with-mail_ssl_module
 .else
 CONFIGURE_ARGS+=	--without-pcre
 CONFIGURE_ARGS+=	--without-http_rewrite_module
+.endif
+
+.if !empty(PKG_OPTIONS:Mhttps)
+CONFIGURE_ARGS+=        --with-http_ssl_module
+.endif
+
+.if !empty(PKG_OPTIONS:Mdav)
+CONFIGURE_ARGS+=	--with-http_dav_module
+.endif
+
+.if !empty(PKG_OPTIONS:Mflv)
+CONFIGURE_ARGS+=	--with-http_flv_module
+.endif
+
+.if !empty(PKG_OPTIONS:Msub)
+CONFIGURE_ARGS+=	--with-http_sub_module
+.endif
+
+.if !empty(PKG_OPTIONS:Mgtools)
+CONFIGURE_ARGS+=	--with-google_perftools_module
 .endif
