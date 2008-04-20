47691c7ecb86f679c04b53f643f09633533fbde5
Dieter Baron <dillo@nih.at>
2008-04-20 21:09:38 +0000

    Add options for two new engines: - M4/MADS (Orion Burger, and later others) - MADE (Return to Zork)

--- scummvm-nightly/options.mk
+++ scummvm-nightly/options.mk
@@ -1,9 +1,11 @@
-# $NetBSD: options.mk,v 1.5 2007/10/30 17:11:24 dillo Exp $
+# $NetBSD: options.mk,v 1.6 2008/04/20 21:09:38 dillo Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scummvm-nightly
 PKG_SUPPORTED_OPTIONS=	scummvm-nightly-cruise
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-drascula
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-igor
+PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-m4
+PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-made
 PKG_SUGGESTED_OPTIONS=	${PKG_SUPPORTED_OPTIONS}
 
 .include "../../mk/bsd.options.mk"
@@ -16,4 +18,10 @@ CONFIGURE_ARGS+=--enable-drascula
 .endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-igor)
 CONFIGURE_ARGS+=--enable-igor
+.endif
+.if !empty(PKG_OPTIONS:Mscummvm-nightly-m4)
+CONFIGURE_ARGS+=--enable-m4
+.endif
+.if !empty(PKG_OPTIONS:Mscummvm-nightly-made)
+CONFIGURE_ARGS+=--enable-made
 .endif
