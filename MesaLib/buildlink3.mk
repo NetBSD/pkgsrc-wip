ce46af50cdd8a96c9193514309f9edeaf2c32e2e
Blair Sadewitz <bsadewitz@users.sourceforge.net>
2008-04-22 21:36:38 +0000

    - Remove cairo-xcb.diff, it's b0rked (sm).
    - Add patches and concomitant pkgsrc Makefile rules (rules or code, which
      is the preferred nomenclature?) for:
    
        * Properly restoring the default FPU control word on i386.
        * Using mmapping of anonymous memory to allocate the heap for DRI
          drivers, as well as a variable (specified in BUILD_DEFS) to allow
          the user to override the default.  This was inspired by a patch
          in OpenBSD's xenocara repository by Owain Ainsworth.  Previously,
          this was only enabled on Linux!  On OpenBSD, at least, this fixes
          a number of SIGSEGVs.
        * Using floating-point functions in a platform's libm, e.g. floorf
    
      Please see the newly added patches MesaLib/Makefile and MesaLib/options.mk
      for more information.
    
      I have been very busy with Real Life (tm) lately, so I was only able
      to test this once recently, and made a change or two after that.  Please
      do let me know if there are any problems/regressions.
    
    - Reincarnate TODO with shiny new "tasks of infinite delight".

--- MesaLib/buildlink3.mk
+++ MesaLib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2008/02/26 06:47:36 bsadewitz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2008/04/22 21:36:38 bsadewitz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 MESALIB_BUILDLINK3_MK:=	${MESALIB_BUILDLINK3_MK}+
@@ -14,6 +14,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}MesaLib
 .if !empty(MESALIB_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.MesaLib+=	MesaLib>=3.4.2
 BUILDLINK_ABI_DEPENDS.MesaLib+=	MesaLib>=6.4.1nb1
+BUILDLINK_ABI_DEPENDS.MesaLib+= MesaLib>=7.0.3
 BUILDLINK_PKGSRCDIR.MesaLib?=	../../wip/MesaLib
 
 .include "../../mk/bsd.fast.prefs.mk"
--- MesaLib/options.mk
+++ MesaLib/options.mk
@@ -1,26 +1,42 @@
-# $NetBSD: options.mk,v 1.7 2008/03/20 20:29:23 bsadewitz Exp $
+# $NetBSD: options.mk,v 1.8 2008/04/22 21:36:38 bsadewitz Exp $
+
 PKG_OPTIONS_VAR=	PKG_OPTIONS.MesaLib
-PKG_SUPPORTED_OPTIONS=	# Added to below
+PKG_SUPPORTED_OPTIONS=	mesa-execmem-mmap
 
 # Assembler code build configurations
 .if ${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64"
 PKG_SUPPORTED_OPTIONS+=		${MACHINE_ARCH}
 .endif
-
+###
+### XXX Previously, Mesa only allocated executable memory properly on 
+###	on linux.  On OpenBSD, NetBSD, etc., for instance, pages which
+###	needed to be executable were allocated using malloc(3).  Why
+###	did it only work on linux?  Well, #ifdef __linux__ might do that.
+###	(ARGH!!@#!@#!@#)
+###	See patch-ap for more information.  Please do take note of the 
+###	little hack I added to provide for wiring this memory down
+###	upon setting the MESA_EXECMEM_MMAP environment variable.
+###
+###	You may also define MESA_EXECMEM_HEAP_SIZE in your build
+###	environment to adjust the size of the anonymous executable
+###	memory pool (in bytes).  The default is 10MB.
+###
+.if (!empty(OPSYS:M*BSD) || ${OPSYS} == "DragonFly" || ${OPSYS} == "Linux")
+PKG_SUGGESTED_OPTIONS=	mesa-execmem-mmap
+.endif
 ###
 ### XXX  There are [probably] others, but let's not get crazy just yet.
 ### 	 This will take a while to test for the myriad platforms we
 ### 	 support.
 ###
 .if (${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64" || \
-    ${MACHINE_ARCH} == "powerpc" || ${MACHINE_ARCH} == "sparc" || \
-    ${MACHINE_ARCH} == "sparc64") && \
+     ${MACHINE_ARCH} == "powerpc" || ${MACHINE_ARCH} == "sparc" || \
+     ${MACHINE_ARCH} == "sparc64") && \
     (${OPSYS} == "NetBSD" || ${OPSYS} == "FreeBSD" || ${OPSYS} == "OpenBSD")
 PKG_SUPPORTED_OPTIONS+=		dri
 .endif
 
 .include "../../mk/bsd.options.mk"
-
 ###
 ### XXX Yes, this is a bit overly verbose; with Mesa, that can't hurt much.
 ###
@@ -40,10 +56,18 @@ BUILD_TARGET=	pkgsrc-dri${BUILD_TARGET_SUFFIX}
 .  include "../../x11/libXxf86vm/buildlink3.mk"
 .  include "../../x11/xf86driproto/buildlink3.mk"
 .  include "../../x11/xf86vidmodeproto/buildlink3.mk"
-PLIST_SUBST+=	DRI=""
-PLIST_SUBST+=	NODRI="@comment"
+PLIST.dri=	# empty
 .else
 BUILD_TARGET=	pkgsrc${BUILD_TARGET_SUFFIX}
-PLIST_SUBST+=	DRI="@comment "
-PLIST_SUBST+=	NODRI=""
+PLIST.nodri=	# empty
+.endif
+###
+### XXX This is the default heap size.  Would there be an occasion to 
+### change it?
+###
+MESA_EXECMEM_HEAPSIZE?=	10485760
+.if !empty(PKG_OPTIONS:Mmesa-execmem-mmap)
+CFLAGS+=	-DMESA_EXECMEM_MMAP
+CFLAGS+=	-DEXEC_HEAP_SIZE=${MESA_EXECMEM_HEAP_SIZE:M[0-9]*:Q}
+.  endif
 .endif
