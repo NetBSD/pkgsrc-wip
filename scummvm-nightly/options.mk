48765c903387daa6f97ca86d29c979cd65c0eec3
Dieter Baron <dillo@nih.at>
2008-05-02 11:32:07 +0000

    Drascula engine is now enabled by default, remove option.

--- scummvm-nightly/options.mk
+++ scummvm-nightly/options.mk
@@ -1,8 +1,7 @@
-# $NetBSD: options.mk,v 1.6 2008/04/20 21:09:38 dillo Exp $
+# $NetBSD: options.mk,v 1.7 2008/05/02 11:32:07 dillo Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scummvm-nightly
 PKG_SUPPORTED_OPTIONS=	scummvm-nightly-cruise
-PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-drascula
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-igor
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-m4
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-made
@@ -12,9 +11,6 @@ PKG_SUGGESTED_OPTIONS=	${PKG_SUPPORTED_OPTIONS}
 
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-cruise)
 CONFIGURE_ARGS+=--enable-cruise
-.endif
-.if !empty(PKG_OPTIONS:Mscummvm-nightly-drascula)
-CONFIGURE_ARGS+=--enable-drascula
 .endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-igor)
 CONFIGURE_ARGS+=--enable-igor
