7bf0bb1912dcb06bf6b8f97e55a163524ff469dc
Adam Hoka <hoka_adam@users.sourceforge.net>
2008-05-12 16:03:56 +0000

    Cleanups and some additional features and options. Fixed PLIST for options. Also add live555 support due to demand. Bump revision.

--- vlc-devel/options.mk
+++ vlc-devel/options.mk
@@ -1,23 +1,36 @@
-# $NetBSD: options.mk,v 1.1.1.1 2008/05/07 17:43:03 hoka_adam Exp $
+# $NetBSD: options.mk,v 1.2 2008/05/12 16:03:56 hoka_adam Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.vlc
-PKG_SUPPORTED_OPTIONS=		qt debug # notify faad arts  <- untested, needs plist entry # skins <- seems broken
-PKG_SUGGESTED_OPTIONS=		qt debug # notify skins
+PKG_SUPPORTED_OPTIONS=		qt debug faad arts dbus # skins <- seems broken
+PKG_SUGGESTED_OPTIONS=		qt debug dbus # skins
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=		${PKG_SUPPORTED_OPTIONS}
 
-.if !empty(PKG_OPTIONS:Mnotify)
+## DBUS message bus support
+## also libnotify because it uses dbus
+## so taking them apart would make no sense.
+
+.if !empty(PKG_OPTIONS:Mdbus)
+CONFIGURE_ARGS+=	--enable-dbus
+.include "../../sysutils/dbus/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-notify
 .include "../../sysutils/libnotify/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-dbus
 .endif
+
+## DEBUG build or release build
 
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-debug
+CONFIGURE_ARGS+=	--disable-optimizations
 .else
 CONFIGURE_ARGS+=	--enable-release
 .endif
+
+## SKINS frontend
 
 .if !empty(PKG_OPTIONS:Mskins)
 CONFIGURE_ARGS+=	--enable-skins2
@@ -25,6 +38,8 @@ PLIST.skins=		yes
 .else
 CONFIGURE_ARGS+=	--disable-skins2
 .endif
+
+## The new QT frontend using qt4
 
 .if !empty(PKG_OPTIONS:Mqt)
 .include "../../x11/qt4-libs/buildlink3.mk"
@@ -34,6 +49,8 @@ PLIST.qt=	yes
 CONFIGURE_ARGS+=	--disable-qt4
 .endif
 
+## FAAD support (faad is non-redistributable)
+
 .if !empty(PKG_OPTIONS:Mfaad)
 CONFIGURE_ARGS+=	--enable-faad
 PLIST.faad=		yes
@@ -41,6 +58,8 @@ PLIST.faad=		yes
 .else
 CONFIGURE_ARGS+=	--disable-faad
 .endif
+
+## ARTS audio system support
 
 .if !empty(PKG_OPTIONS:Marts)
 CONFIGURE_ARGS+=	--enable-arts
