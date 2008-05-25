f63cbedc082e1f32ea74d3370793fffa6a82b084
Adam Hoka <hoka_adam@users.sourceforge.net>
2008-05-25 17:16:04 +0000

    * Modularize PLIST, as it has some platform specific files. * Add x11, ssl and dbus options. * Make it use gcc3.4 when host has 4.0 because   it can avoid bugs. (needs confirming) * Add TODO about sending patches upstream.

--- wine/options.mk
+++ wine/options.mk
@@ -1,22 +1,69 @@
-# $NetBSD: options.mk,v 1.1.1.1 2008/02/18 20:21:02 asau Exp $
+# $NetBSD: options.mk,v 1.2 2008/05/25 17:16:04 hoka_adam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.wine
-PKG_SUPPORTED_OPTIONS=	cups opengl sane
-PKG_SUGGESTED_OPTIONS=	opengl
+PKG_SUPPORTED_OPTIONS=	cups opengl sane ssl dbus x11 esound
+PKG_SUGGESTED_OPTIONS=	opengl ssl x11 esound
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mcups)
-.include "../../print/cups/buildlink3.mk"
+PLIST_VARS+=	x11 opengl
+
+.if !empty(PKG_OPTIONS:Mesound)
+.include "../../audio/esound/buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mopengl)
-PLIST_SUBST+=	WINE_OPENGL=""
+.if !empty(PKG_OPTIONS:Mldap)
+.include "../../databases/openldap-client/buildlink3.mk"    
+.else
+CONFIGURE_ARGS+=	--without-ldap
+.endif
+
+.if !empty(PKG_OPTIONS:Mdbus)
+.include "../../sysutils/dbus/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-dbus
+.endif
+
+.if !empty(PKG_OPTIONS:Mcups)
+.include "../../print/cups/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-cups
+.endif
+
+.if !empty(PKG_OPTIONS:Mx11)
+.include "../../x11/libICE/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXcursor/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXi/buildlink3.mk"
+.include "../../x11/libXinerama/buildlink3.mk"
+.include "../../x11/libXrandr/buildlink3.mk"
+.include "../../x11/libXrender/buildlink3.mk"
+.include "../../x11/libXxf86vm/buildlink3.mk"
+.include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/lcms/buildlink3.mk"
+.include "../../graphics/libungif/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+PLIST.x11=	yes
+.else
+CONFIGURE_ARGS+=	--without-dbus
+.endif
+
+.if !empty(PKG_OPTIONS:Mssl)
+.include "../../security/openssl/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-openssl
+.endif
+
+.if !empty(PKG_OPTIONS:Mopengl) && !empty(PKG_OPTIONS:Mx11)
 .include "../../graphics/glu/buildlink3.mk"
 .include "../../graphics/glut/buildlink3.mk"
+PLIST.opengl=	yes
 .else
 CONFIGURE_ARGS+=	--without-opengl
-PLIST_SUBST+=	WINE_OPENGL="@comment "
 .endif
 
 .if !empty(PKG_OPTIONS:Msane)
