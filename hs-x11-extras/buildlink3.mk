c6faccad4ac6849ab892c807ceb63d739d871666
Arnaud Degroote <degroote@NetBSD.org>
2008-06-08 17:47:45 +0000

    With ghc-6.8, we don't need anymore wip/cabal. Update depends in INSTALL file

--- hs-x11-extras/buildlink3.mk
+++ hs-x11-extras/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2008/03/06 17:46:32 emil_s Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2008/06/08 17:47:45 zul_ Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 HS_X11_EXTRAS_BUILDLINK3_MK:=	${HS_X11_EXTRAS_BUILDLINK3_MK}+
@@ -17,7 +17,6 @@ BUILDLINK_API_DEPENDS.hs-x11-extras+=	hs-x11-extras>=0.4
 BUILDLINK_PKGSRCDIR.hs-x11-extras?=	../../wip/hs-x11-extras
 .endif	# HS_X11_EXTRAS_BUILDLINK3_MK
 
-.include "../../wip/cabal/buildlink3.mk"
 .include "../../wip/hs-x11/buildlink3.mk"
 .include "../../lang/ghc/buildlink3.mk"
 
