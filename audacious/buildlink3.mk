cae318dfa5620334bbc0ccd2d2f7a81cbe9e4932
Thomas Klausner <wiz@NetBSD.org>
2008-06-24 15:04:58 +0000

    Fix path to mowgli. Sort lines.

--- audacious/buildlink3.mk
+++ audacious/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2008/03/14 20:37:53 netcap Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2008/06/24 15:04:58 thomasklausner Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 AUDACIOUS_BUILDLINK3_MK:=	${AUDACIOUS_BUILDLINK3_MK}+
@@ -17,12 +17,12 @@ BUILDLINK_PKGSRCDIR.audacious?=	../../wip/audacious
 BUILDLINK_DEPMETHOD.audacious?=	build
 .endif	# AUDACIOUS_BUILDLINK3_MK
 
-.include "../../wip/mowgli/buildlink3.mk"
-.include "../../devel/libmcs/buildlink3.mk"
-.include "../../devel/pango/buildlink3.mk"
-.include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/atk/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/libmcs/buildlink3.mk"
+.include "../../devel/mowgli/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
 .include "../../sysutils/dbus/buildlink3.mk"
 .include "../../sysutils/dbus-glib/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
