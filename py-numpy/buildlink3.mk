bd61ff89518207a449fb771864b1cc6fa0f74a6d
Mark Davies <mark@ecs.vuw.ac.nz>
2008-06-25 10:56:01 +0000

    numpy 1.1.0
    package has only been tried on current NetBSD/i386 with g95 as the fortran
    compiler.
    
    NumPy is a general-purpose array-processing package designed to
    efficiently manipulate large multi-dimensional arrays of arbitrary
    records without sacrificing too much speed for small multi-dimensional
    arrays.  NumPy is built on the Numeric code base and adds features
    introduced by numarray as well as an extended C-API and the ability to
    create arrays of arbitrary type.
    
    There are also basic facilities for discrete fourier transform,
    basic linear algebra and random number generation.

---
+++ py-numpy/buildlink3.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/06/25 10:56:01 mwdavies Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+PY_NUMPY_BUILDLINK3_MK:=	${PY_NUMPY_BUILDLINK3_MK}+
+
+.include "../../lang/python/pyversion.mk"
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	pynumpy
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npynumpy}
+BUILDLINK_PACKAGES+=	pynumpy
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}pynumpy
+
+.if ${PY_NUMPY_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.pynumpy+=	${PYPKGPREFIX}-numpy>=1.0
+BUILDLINK_PKGSRCDIR.pynumpy?=	../../wip/py-numpy
+.endif	# PY_NUMPY_BUILDLINK3_MK
+
+.include "../../math/blas/buildlink3.mk"
+.include "../../math/lapack/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
