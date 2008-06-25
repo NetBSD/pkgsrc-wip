d2f3fc4c8335349e9d08cd945b8f7f1c4bb93f83
Mark Davies <mark@ecs.vuw.ac.nz>
2008-06-25 11:01:46 +0000

    Update of graphics/py-matplotlib to latest version (requires py-numpy)

---
+++ py-matplotlib/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/06/25 11:01:46 mwdavies Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+PY_MATPLOTLIB_BUILDLINK3_MK:=	${PY_MATPLOTLIB_BUILDLINK3_MK}+
+
+.include "../../lang/python/pyversion.mk"
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	pymatplotlib
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npymatplotlib}
+BUILDLINK_PACKAGES+=	pymatplotlib
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}pymatplotlib
+
+.if !empty(PY_MATPLOTLIB_BUILDLINK3_MK:M+)
+BUILDLINK_API_DEPENDS.pymatplotlib+=	${PYPKGPREFIX}-matplotlib>=0.87.2
+BUILDLINK_PKGSRCDIR.pymatplotlib?=	../../wip/py-matplotlib
+.endif	# PY_MATPLOTLIB_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
