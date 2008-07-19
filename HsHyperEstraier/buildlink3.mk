cacdb503136bd099d37c9cd8c7e742f21a2432c9
PHO / phonohawk <phonohawk@users.sourceforge.net>
2008-07-19 08:55:46 +0000

    Import HsHyperEstraier-0.2.1 as wip/HsHyperEstraier.
    HsHyperEstraier is a HyperEstraier binding for Haskell. HyperEstraier
    is an embeddable full text search engine which is supposed to be
    independent to any particular natural languages.

---
+++ HsHyperEstraier/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/07/19 08:55:46 phonohawk Exp $
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+HSHYPERESTRAIER_BUILDLINK3_MK:=	${HSHYPERESTRAIER_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	HsHyperEstraier
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NHsHyperEstraier}
+BUILDLINK_PACKAGES+=	HsHyperEstraier
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}HsHyperEstraier
+
+.if ${HSHYPERESTRAIER_BUILDLINK3_MK} == "+"
+BUILDLINK_DEPMETHOD.HsHyperEstraier?=	build
+BUILDLINK_API_DEPENDS.HsHyperEstraier+=	HsHyperEstraier>=0.2.1
+BUILDLINK_PKGSRCDIR.HsHyperEstraier?=	../../wip/HsHyperEstraier
+.endif	# HSHYPERESTRAIER_BUILDLINK3_MK
+
+.include "../../textproc/hyperestraier/buildlink3.mk"
+.include "../../wip/hs-utf8-string/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
