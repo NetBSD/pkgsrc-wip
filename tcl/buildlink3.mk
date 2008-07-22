d772c439aa18d1e32655e0a796eedf1dd3929b60
Aleksej Saushev <asau@users.sourceforge.net>
2008-07-22 20:31:07 +0000

    Import TCL 8.5 as contributed by Emiliano Gavilan with minor adaptations to WIP.

---
+++ tcl/buildlink3.mk
@@ -0,0 +1,34 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/07/22 20:31:07 asau Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+TCL_BUILDLINK3_MK:=	${TCL_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	tcl
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ntcl}
+BUILDLINK_PACKAGES+=	tcl
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}tcl
+
+.if !empty(TCL_BUILDLINK3_MK:M+)
+BUILDLINK_API_DEPENDS.tcl+=	tcl>=8.5.3
+BUILDLINK_ABI_DEPENDS.tcl+=	tcl>=8.5.3
+BUILDLINK_PKGSRCDIR.tcl?=	../../lang/tcl85
+
+BUILDLINK_FILES.tcl+=	bin/tclsh*
+#
+# Make "-ltcl" resolve into "-ltcl8.5", so that we don't
+# need to patch so many Makefiles.
+#
+BUILDLINK_TRANSFORM+=	l:tcl:tcl8.5
+
+TCLCONFIG_SH?=		${BUILDLINK_PREFIX.tcl}/lib/tclConfig.sh
+
+PRINT_PLIST_AWK+=	/^@dirrm lib\/tcl$$/ \
+				{ print "@comment in tcl: " $$0; next; }
+.endif	# TCL_BUILDLINK3_MK
+
+.include "../../mk/dlopen.buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
