0014162040fd1d931e3d9894835d5b338119cf86
Dieter Baron <dillo@nih.at>
2008-07-23 12:25:10 +0000

    New engine added: tinsel (Discworld 1).

--- scummvm-nightly/options.mk
+++ scummvm-nightly/options.mk
@@ -1,10 +1,11 @@
-# $NetBSD: options.mk,v 1.7 2008/05/02 11:32:07 dillo Exp $
+# $NetBSD: options.mk,v 1.8 2008/07/23 12:25:10 dillo Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scummvm-nightly
 PKG_SUPPORTED_OPTIONS=	scummvm-nightly-cruise
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-igor
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-m4
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-made
+PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-tinsel
 PKG_SUGGESTED_OPTIONS=	${PKG_SUPPORTED_OPTIONS}
 
 .include "../../mk/bsd.options.mk"
@@ -20,4 +21,7 @@ CONFIGURE_ARGS+=--enable-m4
 .endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-made)
 CONFIGURE_ARGS+=--enable-made
+.endif
+.if !empty(PKG_OPTIONS:Mscummvm-nightly-tinsel)
+CONFIGURE_ARGS+=--enable-tinsel
 .endif
