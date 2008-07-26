23051dd477d215b1b3927c7061fe1797e469ff06
OBATA Akio <obache@users.sourceforge.net>
2008-07-26 13:51:53 +0000

    Previous changes are not completed.

--- mingw-w32api-bin/buildlink3.mk
+++ mingw-w32api-bin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2008/07/26 13:37:08 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2008/07/26 13:51:53 obache Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 MINGW_W32API_BIN_BUILDLINK3_MK:=	${MINGW_W32API_BIN_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}mingw-w32api-bin
 
 .include "../../wip/mingw/Makefile.common"
 
-.if (MINGW_W32API_BIN_BUILDLINK3_MK} == "+"
+.if ${MINGW_W32API_BIN_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.mingw-w32api-bin+=	mingw-w32api-bin>=3.1
 BUILDLINK_PKGSRCDIR.mingw-w32api-bin?=	../../wip/mingw-w32api-bin
 BUILDLINK_FNAME_TRANSFORM.mingw-w32api-bin+=	-e "s,${MINGW_TARGET}/include,include/,g"
