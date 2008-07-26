0d1a2dc491477c48f6915c87a9d038e65722e3f0
Mark Davies <mark@ecs.vuw.ac.nz>
2008-07-26 22:42:58 +0000

    kde4.1rc1

--- kdepimlibs4/buildlink3.mk
+++ kdepimlibs4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2008/01/18 04:30:15 tnn2 Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2008/07/26 22:42:58 mwdavies Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 KDEPIMLIBS_BUILDLINK3_MK:=	${KDEPIMLIBS_BUILDLINK3_MK}+
@@ -17,6 +17,7 @@ BUILDLINK_ABI_DEPENDS.kdepimlibs?=	kdepimlibs4>=4.0.0nb1
 BUILDLINK_PKGSRCDIR.kdepimlibs?=	../../wip/kdepimlibs4
 .endif	# KDEPIMLIBS_BUILDLINK3_MK
 
+.include "../../wip/akonadi/buildlink3.mk"
 .include "../../databases/openldap-client/buildlink3.mk"
 .include "../../devel/boost-libs/buildlink3.mk"
 .include "../../security/cyrus-sasl/buildlink3.mk"
