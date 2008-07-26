9525a2c3517bac20e671a1c65a40d1ebccb0b90a
OBATA Akio <obache@users.sourceforge.net>
2008-07-26 13:37:08 +0000

    Fixes some pkglint warings.

--- mingw-w32api-bin/buildlink3.mk
+++ mingw-w32api-bin/buildlink3.mk
@@ -1,22 +1,23 @@
-# $NetBSD: buildlink3.mk,v 1.5 2007/01/17 03:11:30 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2008/07/26 13:37:08 obache Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 MINGW_W32API_BIN_BUILDLINK3_MK:=	${MINGW_W32API_BIN_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	mingw-w32api-bin
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmingw-w32api-bin}
 BUILDLINK_PACKAGES+=	mingw-w32api-bin
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}mingw-w32api-bin
 
 .include "../../wip/mingw/Makefile.common"
 
-.if !empty(MINGW_W32API_BIN_BUILDLINK3_MK:M+)
+.if (MINGW_W32API_BIN_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.mingw-w32api-bin+=	mingw-w32api-bin>=3.1
 BUILDLINK_PKGSRCDIR.mingw-w32api-bin?=	../../wip/mingw-w32api-bin
 BUILDLINK_FNAME_TRANSFORM.mingw-w32api-bin+=	-e "s,${MINGW_TARGET}/include,include/,g"
 BUILDLINK_FNAME_TRANSFORM.mingw-w32api-bin+=	-e "s,${MINGW_TARGET}/lib,lib/,g"
 .endif	# MINGW_W32API_BIN_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
