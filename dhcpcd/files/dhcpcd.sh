6a80d7112791abf68cbd0e814917f8ebfb8da2b6
Roy Marples <roy@marples.name>
2008-07-27 11:14:35 +0000

    Bump to rc3 and fix the rc.d script a little.

--- dhcpcd/files/dhcpcd.sh
+++ dhcpcd/files/dhcpcd.sh
@@ -1,15 +1,8 @@
-#!@RCD_SCRIPTS_SHELL@
-#
-# $NetBSD: dhcpcd.sh,v 1.1.1.1 2008/01/18 23:34:01 hoka_adam Exp $
-#
+#!/bin/sh
 
-# PROVIDE: dhclient
+# PROVIDE: dhcpcd
 # REQUIRE: network mountcritlocal
 # BEFORE:  NETWORKING
-#
-#	Note that there no syslog logging of dhclient messages at boot because
-#	dhclient needs to start before services that syslog depends upon do.
-#
 
 if [ -f /etc/rc.subr ]; then
         . /etc/rc.subr
@@ -19,19 +12,20 @@ name="dhcpcd"
 rcvar="${name}"
 ifn="$2"
 command="@PREFIX@/sbin/${name}"
-pidfile="@VARBASE@/run/dhcpcs-${ifn}.pid"
+command_args="${ifn}"
+pidfile="@VARBASE@/run/${name}-${ifn}.pid"
 start_precmd="dhcpcd_precmd"
 
 dhcpcd_precmd()
 {
 	# Override for $ifn specific flags (see rc.subr for $flags setting)
-	specific=`get_if_var $ifn dhcpcd_flags_IF`
-	if [ -z "$flags" -a -n "$specific" ]; then
-		rc_flags=$specific
+	eval ifn_flags=\$${rcvar}_flags_${ifn}
+	if [ -z "${flags}" -a -n "${ifn_flags}" ]; then
+		rc_flags=${ifn_flags}
 	fi
 
 	# dhcpcd may need local binaries
-	PATH=${PATH}:%%PREFIX%%/sbin
+	PATH=${PATH}:@PREFIX@/sbin
 	export PATH
 }
 
