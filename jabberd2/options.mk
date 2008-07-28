9febf044876027207dee6f6cc6adaa33c8504de2
Eric Schnoebelen <schnoebe@NetBSD.org>
2008-07-28 18:23:56 +0000

    Update to jabberd2 2.2.2.       * SSL certificate loading fixes       * Implemented MIO read throttling in c2s. Now we pause reading         socket instead of disconnection on throttle.       * Removed scod module.       * Added a maximum stanza limit to c2s. This can be used to set an         upper limit on the number of individual requests that can be         made in a given window of time.       * Fix a bug in s2s where xmlns="jabber:client" would get appended         to an incoming stanza even though the stanza already had an         xmlns attribute set.
    For a full ChangeLog see:
    http://svn.xiaoka.com/jabberd2/tags/jabberd-2.2.2/ChangeLog

--- jabberd2/options.mk
+++ jabberd2/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.7 2008/06/24 10:09:53 moubctez Exp $
+# $NetBSD: options.mk,v 1.8 2008/07/28 18:23:56 schnoebe Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.jabberd2
 PKG_OPTIONS_REQUIRED_GROUPS=	auth storage sasl
@@ -9,7 +9,7 @@ PKG_OPTIONS_GROUP.auth+=	auth-db auth-ldap auth-pam
 PKG_OPTIONS_GROUP.storage=	storage-mysql storage-pgsql
 PKG_OPTIONS_GROUP.storage+=	storage-sqlite storage-db
 # SASL implementation
-PKG_OPTIONS_GROUP.sasl=		sasl-cyrus sasl-gnu sasl-scod
+PKG_OPTIONS_GROUP.sasl=		sasl-cyrus sasl-gnu 
 # debugging
 PKG_SUPPORTED_OPTIONS+=		debug
 PKG_SUGGESTED_OPTIONS=		auth-sqlite storage-sqlite sasl-gnu
@@ -26,10 +26,6 @@ CONFIGURE_ARGS+=	--with-sasl=cyrus
 .if !empty(PKG_OPTIONS:Msasl-gnu)
 CONFIGURE_ARGS+=	--with-sasl=gsasl
 .  include "../../security/gsasl/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Msasl-scod)
-CONFIGURE_ARGS+=	--with-sasl=scod
 .endif
 
 .if !empty(PKG_OPTIONS:Mauth-db) || !empty(PKG_OPTIONS:Mstorage-db)
