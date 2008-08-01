17c977842a6eaabb671c643ce6455d7d5814857b
Christian St <athaba@users.sourceforge.net>
2008-08-01 22:57:08 +0000

    Import tor-0.2.1.2 as wip/tor-dev. This is the unstable/alpha version of net/tor.

---
+++ tor-dev/options.mk
@@ -0,0 +1,25 @@
+# $NetBSD: options.mk,v 1.1.1.1 2008/08/01 22:57:08 athaba Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.tor
+PKG_SUPPORTED_OPTIONS=	threads
+
+.if !empty(PTHREAD_TYPE:Mnative)
+PKG_SUGGESTED_OPTIONS+=	threads
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+###
+### This enables new code for threaded operation on NetBSD, OpenBSD, etc.
+### I used {PTHREAD,RESOLV}_AUTO_VARS here for consistency's sake, as
+### I don't trust configure scripts to do this properly on all platforms.
+###
+.if !empty(PKG_OPTIONS:Mthreads)
+CONFIGURE_ARGS+=	--enable-eventdns
+CONFIGURE_ARGS+=	--enable-threads
+PTHREAD_OPTS+=		require
+PTHREAD_AUTO_VARS=	yes
+USE_FEATURES.openssl+=	threads
+.else
+CONFIGURE_ARGS+=	--disable-threads
+.endif
