f57dd2ff6640b81358829dca438cec189025d5c5
Jonathan A. Kollasch <jakllsch@NetBSD.org>
2008-08-05 21:50:19 +0000

    fixes so recent svn revisions will build

--- flashrom/files/pio.h
+++ flashrom/files/pio.h
@@ -1,13 +1,42 @@
 static __inline void
-outb (unsigned char value, unsigned short int port)
+outb(unsigned char value, unsigned short port)
 {
   __asm__ __volatile__ ("outb %b0,%w1": :"a" (value), "Nd" (port));
 }
 
 static __inline unsigned char
-inb (unsigned short int port)
+inb(unsigned short port)
 {
   unsigned char _v;
   __asm__ __volatile__ ("inb %w1,%0":"=a" (_v):"Nd" (port));
   return _v;
 }
+
+static __inline void
+outw(unsigned short value, unsigned short port)
+{
+  __asm__ __volatile__ ("outw %w0,%w1": :"a" (value), "Nd" (port));
+}
+
+static __inline unsigned short
+inw(unsigned short port)
+{
+  unsigned short _v;
+  __asm__ __volatile__ ("inw %w1,%0":"=a" (_v):"Nd" (port));
+  return _v;
+}
+
+static __inline void
+outl(unsigned int value, unsigned short port)
+{
+  __asm__ __volatile__ ("outl %w0,%w1": :"a" (value), "Nd" (port));
+}
+
+static __inline unsigned int
+inl(unsigned short port)
+{
+  unsigned int _v;
+  __asm__ __volatile__ ("inl %1,%0":"=a" (_v):"Nd" (port));
+  return _v;
+}
+
