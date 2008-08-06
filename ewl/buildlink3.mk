a1a7005e9c34f589f695cf1b4aaac8387083cfe2
Marcin M. Jessa <yazzy@yazzy.org>
2008-08-06 18:11:35 +0000

    Updating to newer snapshot

--- ewl/buildlink3.mk
+++ ewl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.2 2008/08/06 12:32:38 yazzy Exp $
+# $NetBSD: buildlink3.mk,v 1.1.1.3 2008/08/06 18:11:35 yazzy Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 EWL_BUILDLINK3_MK:=	${EWL_BUILDLINK3_MK}+
@@ -12,12 +12,13 @@ BUILDLINK_PACKAGES+=	ewl
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}ewl
 
 .if ${EWL_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.ewl+=	ewl>=0.5.1.011nb1
-BUILDLINK_ABI_DEPENDS.ewl?=	ewl>=0.5.1.011nb1
+BUILDLINK_API_DEPENDS.ewl+=	ewl>=0.5.2.042
+BUILDLINK_ABI_DEPENDS.ewl?=	ewl>=0.5.2.042nb1
 BUILDLINK_PKGSRCDIR.ewl?=	../../wip/ewl
 .endif	# EWL_BUILDLINK3_MK
 
 .include "../../wip/efreet/buildlink3.mk"
 .include "../../wip/edje/buildlink3.mk"
+.include "../../wip/epsilon/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
