ba6cd6da7eae10d91a4c546ed9232c8a996947c6
Hasso Tepper <htepper@users.sourceforge.net>
2008-08-07 19:03:31 +0000

    * Update to the latest version - 0.11.5:
      * Apply security fix for cardos driver and extend pkcs15-tool to test
        cards for the security vulnerability and update them (CVE-2008-2235).
      * Build system rewritten (NOTICE: configure options was modified).
        The build system can produce outputs for *NIX, cygwin and native
        windows (using mingw).
      * ruToken now supported.
      * Allow specifying application name for data objects.
      * Basic reader hotplug support.
      * PC/SC library is dynamic linked no longer compile time dependency.
      * PKCS#11 provider is now installed at LIBDIR/pkcs11
      * PKCS#11 - Number of virtual slots moved into configuration.
      * PKCS#11 - Fix fork() compliance.
      * make sign_with_decrypt hack configureable for siemens cards.
    
    * Single PLIST file.
    * Use CONF_FILES for configuration file.
    * DESTDIR ready.

--- opensc/buildlink3.mk
+++ opensc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/04/06 07:18:13 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2008/08/07 19:03:31 htepper Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 OPENSC_BUILDLINK3_MK:=	${OPENSC_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nopensc}
 BUILDLINK_PACKAGES+=	opensc
 
 .if !empty(OPENSC_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.opensc+=	opensc>=0.9.6
+BUILDLINK_API_DEPENDS.opensc+=	opensc>=0.11.5
 BUILDLINK_PKGSRCDIR.opensc?=	../../wip/opensc
 .endif	# OPENSC_BUILDLINK3_MK
 
--- opensc/options.mk
+++ opensc/options.mk
@@ -1,8 +1,9 @@
-# $NetBSD: options.mk,v 1.2 2008/05/06 13:29:33 htepper Exp $
+# $NetBSD: options.mk,v 1.3 2008/08/07 19:03:31 htepper Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.opensc
 PKG_SUPPORTED_OPTIONS=	nsplugin openct pcsc-lite
 PKG_SUGGESTED_OPTIONS=	nsplugin pcsc-lite
+PLIST_VARS+=		nsplugin 
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,17 +12,23 @@ PKG_SUGGESTED_OPTIONS=	nsplugin pcsc-lite
 .include "../../security/pinentry/buildlink3.mk"
 .include "../../x11/libXt/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-nsplugin
-CONFIGURE_ARGS+=	--with-plugin-dir=/usr/pkg/lib/firefox/plugins
+CONFIGURE_ARGS+=	--with-plugindir=/usr/pkg/lib/firefox/plugins
 CONFIGURE_ARGS+=	--with-pinentry=/usr/pkg/bin/pinentry
-PLIST_SRC+=		${PKGDIR}/PLIST.nsplugin
+PLIST.nsplugin=		yes
 .else
 CONFIGURE_ARGS+=	--disable-nsplugin
 .endif
 
 .if !empty(PKG_OPTIONS:Mpcsc-lite)
 .include "../../security/pcsc-lite/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-pcsc
+.else
+CONFIGURE_ARGS+=	--disable-pcsc
 .endif
 
 .if !empty(PKG_OPTIONS:Mopenct)
 .include "../../wip/openct/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-openct
+.else
+CONFIGURE_ARGS+=	--disable-openct
 .endif
