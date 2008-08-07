5891db56b96cd874866d9146a4d640b11d528f4a
Marcin M. Jessa <yazzy@yazzy.org>
2008-08-07 14:14:50 +0000

    updated with createbuildlink

--- esmart/buildlink3.mk
+++ esmart/buildlink3.mk
@@ -1,5 +1,3 @@
-# $NetBSD: buildlink3.mk,v 1.3 2008/03/06 14:54:08 thomasklausner Exp $
-
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 ESMART_BUILDLINK3_MK:=	${ESMART_BUILDLINK3_MK}+
 
@@ -12,16 +10,15 @@ BUILDLINK_PACKAGES+=	esmart
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}esmart
 
 .if ${ESMART_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.esmart+=	esmart>=0.9.0.011
-BUILDLINK_ABI_DEPENDS.esmart?=	esmart>=0.9.0.011nb2
+BUILDLINK_API_DEPENDS.esmart+=	esmart>=0.9.0.042nb1
 BUILDLINK_PKGSRCDIR.esmart?=	../../wip/esmart
 .endif	# ESMART_BUILDLINK3_MK
 
-.include        "../../wip/ecore/buildlink3.mk"
-.include        "../../wip/edje/buildlink3.mk"
-.include        "../../wip/epsilon/buildlink3.mk"
-.include        "../../graphics/jpeg/buildlink3.mk"
-.include        "../../graphics/imlib2/buildlink3.mk"
-.include        "../../devel/libltdl/buildlink3.mk"
+.include "../../wip/ecore/buildlink3.mk"
+.include "../../wip/edje/buildlink3.mk"
+.include "../../wip/epsilon/buildlink3.mk"
+.include "../../devel/libltdl/buildlink3.mk"
+.include "../../graphics/imlib2/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
