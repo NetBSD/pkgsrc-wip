d221ef32ca03d6dc69938a97e295ef2471a382be
Leonardo Taccari <leot@NetBSD.org>
2008-08-09 19:47:19 +0000

    A lot of improvements: DESTDIR support, sdl option (now by default it'll use x11/libXext) and fixed the PLIST too.
    Special thanks to ahoka@!

--- xwelltris/options.mk
+++ xwelltris/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2008/08/09 12:36:27 leot1990 Exp $
+# $NetBSD: options.mk,v 1.2 2008/08/09 19:47:19 leot1990 Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.xwelltris
 PKG_SUPPORTED_OPTIONS=	sdl
@@ -10,5 +10,5 @@ CONFIGURE_ARGS+=	--with-sdl
 .  include "../../devel/SDL/buildlink3.mk"
 .  include "../../graphics/SDL_image/buildlink3.mk"
 .else
-.  include "../../x11/libX11/buildlink3.mk"
+.  include "../../x11/libXext/buildlink3.mk"
 .endif
