681d5cd06811fa556fb0f49e744f1f3b943fcc64
Michael Stapelberg <mstapelberg@users.sourceforge.net>
2008-08-10 21:08:29 +0000

    Version bump wip/rsyslog to latest version, fix various problems Add support for gnutls

--- rsyslog/options.mk
+++ rsyslog/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.2 2006/08/01 09:42:40 bartoszkuzma Exp $
+# $NetBSD: options.mk,v 1.3 2008/08/10 21:08:29 mstapelberg Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.rsyslog
-PKG_SUPPORTED_OPTIONS=	largefile mysql
+PKG_SUPPORTED_OPTIONS=	largefile mysql gnutls
 PKG_SUGGESTED_OPTIONS=	largefile
 
 .include "../../mk/bsd.options.mk"
@@ -16,4 +16,9 @@ FEATURE_LARGEFILE=		# empty
 CFLAGS+=			-DWITH_DB
 LDFLAGS+=			-lmysqlclient
 FEATURE_DB=			# empty
+.endif
+
+.if !empty(PKG_OPTIONS:Mgnutls)
+.include "../../security/gnutls/buildlink3.mk"
+CONFIGURE_ARGS+=		--enable-gnutls
 .endif
