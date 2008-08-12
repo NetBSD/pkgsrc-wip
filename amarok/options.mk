14ef833cf2886a6eb40852f91f639f9ba388bd96
Thomas Klausner <wiz@NetBSD.org>
2008-08-12 12:07:39 +0000

    Import amarok SVN snapshort post-1.86 as wip/amarok.
    AmaroK is a media player for KDE.  There are many media players
    around these days, true. What's missing from most players is a user
    interface, that doesn't get in the way of the user. How many buttons
    do you have to press for simply adding some new tracks to the
    playlist? amaroK tries to be a little different, providing a simple
    drag and drop interface, that really makes playlist handling easy.
    
    This package contains the KDE4 version of amarok.
    
    TODO:
    PLIST needs more fixes, tries to remove too many files.

---
+++ amarok/options.mk
@@ -0,0 +1,53 @@
+# $NetBSD: options.mk,v 1.1.1.1 2008/08/12 12:07:39 thomasklausner Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.amarok
+PKG_SUPPORTED_OPTIONS=	libgpod libmp4v2 libmtp libnjb libvisual mp3tunes mysql plasma
+PKG_SUGGESTED_OPTIONS=	libgpod libmp4v2 libmtp libvisual mp3tunes plasma
+
+# XXX: mysql doesn't work yet, probably needs more cmake magic
+# XXX: libnjb is currently not supported
+
+# XXX: not packaged yet
+# libifp, 1.0.0.2: Support iRiver iFP audio devices <http://ifp-driver.sourceforge.net/libifp/>
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mlibgpod)
+BUILDLINK_API_DEPENDS.libgpod+=	libgpod>=0.6.0nb1
+.include "../../audio/libgpod/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibmp4v2)
+.include "../../multimedia/libmp4v2/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibmtp)
+.include "../../wip/libmtp/buildlink3.mk"
+.endif
+
+#.if !empty(PKG_OPTIONS:Mlibnjb)
+#.include "../../wip/libnjb/buildlink3.mk"
+#.endif
+
+.if !empty(PKG_OPTIONS:Mlibvisual)
+.include "../../audio/libvisual/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mmp3tunes)
+.include "../../chat/loudmouth/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
+# XXX: make sure qt4-libs have glib enabled
+.endif
+
+.if !empty(PKG_OPTIONS:Mmysql)
+MYSQL_VERSIONS_ACCEPTED=	41
+.include "../../mk/mysql.buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mplasma)
+.include "../../wip/kdebase-workspace4/buildlink3.mk"
+.endif
