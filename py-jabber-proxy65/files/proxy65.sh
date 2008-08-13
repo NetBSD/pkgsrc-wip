0a5ba989c364c73a79166cfef67f328940f382df
Eric Schnoebelen <schnoebe@NetBSD.org>
2008-08-13 04:56:31 +0000

    Turn into a package that is likely to work. It now properly installs into twisted 8.1.0, and seems to start up and run. README installed for configuration information, MESSAGE updated to reflect this package, start up script completed to reflect this package.

--- py-jabber-proxy65/files/proxy65.sh
+++ py-jabber-proxy65/files/proxy65.sh
@@ -1,12 +1,29 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: proxy65.sh,v 1.1 2008/08/07 22:17:25 schnoebe Exp $
+# $NetBSD: proxy65.sh,v 1.2 2008/08/13 04:56:31 schnoebe Exp $
 #
-#   startup script for the PyAIMt AOL transport for Jabber
+#   startup script for the proxy65 file transfer proxy service for Jabber
 #   uses the twistd application engine
 #
-# PROVIDE: pyaimt
+# PROVIDE: proxy65
 # REQUIRE: DAEMON s2s
+#
+#
+#	additional items that _must_ be set as part of proxy65_flags
+#		--jid=		[Jabber ID of the service, default proxy65]
+#		--secret=	[shared secret with the S2S]
+#		--rhost=	[IP/Host of the Jabber service router]
+#		--rport=	[port of the Jabber service router, def 6000]
+#		--proxyips=	[proxy address:ports to be advertised/used]
+#				[default :7777]
+#
+#	ex:
+#		proxy6_flags="--jid=proxy.jabber.example.org \
+#				--secret=JabberSecret \
+#				--rhost=jabber.proxy.org \
+#				--rport=5347 \
+#				--proxyips=172.17.233.23:8010"
+#
 #
 
 name="proxy65"
@@ -17,7 +34,6 @@ pidfile="@JABBER_PIDDIR@/${name}.pid"
 logfile="@JABBER_LOGDIR@/${name}.log"
 command_args="-u @JABBER_USER@ --pidfile=${pidfile} -l ${logfile}"
 command_args="${command_args} proxy65"
-command_args="${command_args} "
 stop_postcmd="remove_pidfile"
 start_precmd="ensure_piddir"
 
@@ -40,5 +56,6 @@ if [ -f /etc/rc.subr ]; then
 	run_rc_command "$1"
 else
 	@ECHO@ -n " ${name}"
-	@SU@ ${pyaimt_user} -c "${command} ${pyaimt_flags} ${command_args}"
+	ensure_piddir
+	${command} ${command_args} ${pyaimt_flags} 
 fi
