cb3d6b176a55516d7fbb40912c03ae4029efae73
Eric Schnoebelen <schnoebe@NetBSD.org>
2008-08-15 17:07:24 +0000

    Modify the start up script work in a fashion acceptable to twisted.

--- py-jabber-proxy65/files/proxy65.sh
+++ py-jabber-proxy65/files/proxy65.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: proxy65.sh,v 1.2 2008/08/13 04:56:31 schnoebe Exp $
+# $NetBSD: proxy65.sh,v 1.3 2008/08/15 17:07:24 schnoebe Exp $
 #
 #   startup script for the proxy65 file transfer proxy service for Jabber
 #   uses the twistd application engine
@@ -9,21 +9,19 @@
 # REQUIRE: DAEMON s2s
 #
 #
-#	additional items that _must_ be set as part of proxy65_flags
-#		--jid=		[Jabber ID of the service, default proxy65]
-#		--secret=	[shared secret with the S2S]
-#		--rhost=	[IP/Host of the Jabber service router]
-#		--rport=	[port of the Jabber service router, def 6000]
-#		--proxyips=	[proxy address:ports to be advertised/used]
+#	Additional variables
+#	    proxy65_jid		[Jabber ID of the service, default proxy65]
+#	    proxy65_secret	[shared secret with the S2S]
+#	    proxy65_rhost	[IP/Host of the Jabber service router]
+#	    proxy65_rport	[port of the Jabber service router, def 6000]
+#	    proxy65_ips		[proxy address:ports to be advertised/used]
 #				[default :7777]
 #
 #	ex:
-#		proxy6_flags="--jid=proxy.jabber.example.org \
-#				--secret=JabberSecret \
-#				--rhost=jabber.proxy.org \
-#				--rport=5347 \
-#				--proxyips=172.17.233.23:8010"
-#
+#	    proxy65_jid=proxy.jabber.example.org
+#	    proxy65_secret=JabberSecret
+#	    proxy65_rhost=jabber.example.org
+#	    proxy65_ips=172.18.223.20:8010
 #
 
 name="proxy65"
@@ -34,20 +32,22 @@ pidfile="@JABBER_PIDDIR@/${name}.pid"
 logfile="@JABBER_LOGDIR@/${name}.log"
 command_args="-u @JABBER_USER@ --pidfile=${pidfile} -l ${logfile}"
 command_args="${command_args} proxy65"
-stop_postcmd="remove_pidfile"
-start_precmd="ensure_piddir"
+
+start_precmd="ensure_piddir && merge_args"
+
+merge_args()
+{
+command_args="${command_args} ${proxy65_jid:+--jid=${proxy65_jid}}"
+command_args="${command_args} ${proxy65_secret:+--secret=${proxy65_secret}}"
+command_args="${command_args} ${proxy65_rhost:+--rhost=${proxy65_rhost}}"
+command_args="${command_args} ${proxy65_rport:+--rport=${proxy65_rport}}"
+command_args="${command_args} ${proxy65_ips:+--proxyips=${proxy65_ips}}"
+}
 
 ensure_piddir()
 {
 	mkdir -p @JABBER_PIDDIR@
 	chown @JABBER_USER@ @JABBER_PIDDIR@
-}
-
-remove_pidfile()
-{
-        if [ -f $pidfile ]; then
-            rm -f $pidfile
-        fi
 }
 
 if [ -f /etc/rc.subr ]; then
