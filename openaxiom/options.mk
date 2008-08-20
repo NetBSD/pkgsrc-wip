a0ba16dcfced535156baca7dcd6e75cd460fef90
Aleksej Saushev <asau@users.sourceforge.net>
2008-08-20 03:49:55 +0000

    Bring options from FriCAS: add ECL support, some convenient PLIST substitutions.

--- openaxiom/options.mk
+++ openaxiom/options.mk
@@ -1,33 +1,46 @@
-# $NetBSD: options.mk,v 1.2 2008/05/29 11:34:09 asau Exp $
+# $NetBSD: options.mk,v 1.3 2008/08/20 03:49:55 asau Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.openaxiom
 PKG_OPTIONS_REQUIRED_GROUPS=	lisp
-PKG_OPTIONS_GROUP.lisp=		clisp sbcl
-
-PKG_SUGGESTED_OPTIONS+=		clisp
+PKG_OPTIONS_GROUP.lisp=		clisp sbcl ecl
 
 PKG_SUPPORTED_OPTIONS+=		x11
-PKG_SUGGESTED_OTIONS+=		x11
+
+PKG_SUGGESTED_OPTIONS+=		clisp x11
 
 .include "../../mk/bsd.options.mk"
 
 # Select Lisp backend
 .if !empty(PKG_OPTIONS:Mclisp)
 FASL=			fas
-BUILD_DEPENDS+=		clisp>=2.41:../../lang/clisp
+DEPENDS+=		clisp>=2.41:../../lang/clisp
 CONFIGURE_ARGS+=	--with-lisp=clisp
 .endif
 .if !empty(PKG_OPTIONS:Msbcl)
 FASL=			fasl
-BUILD_DEPENDS+=		sbcl-[0-9]*:../../lang/sbcl
+DEPENDS+=		sbcl-[0-9]*:../../lang/sbcl
 CONFIGURE_ARGS+=	--with-lisp=sbcl
 .endif
+.if !empty(PKG_OPTIONS:Mecl)
+FASL=			fas
+CONFIGURE_ARGS+=	--with-lisp=ecl
+.include "../../lang/ecl/buildlink3.mk"
+.endif
 
-# Modify PLIST
-PLIST_SUBST+=	FASL=${FASL}
+# Fix suffix for "fast load" files:
+PLIST_SUBST+=	FASL=${FASL:Q}
 
+# Generalize "fast load" files
+PRINT_PLIST_AWK+=	{gsub(/\.${FASL}$$/, ".$${FASL}");}
+.if !empty(PKG_OPTIONS:Mclisp)
+# Handle CLISP-specific files
+PRINT_PLIST_AWK+=	{if ($$0 ~ /\.lib$$/) {$$0 = "$${clisp}" $$0;}}
+.endif
+
+# X11
 .if !empty(PKG_OPTIONS:Mx11)
 CONFIGURE_ARGS+=	--with-x=yes
+CONFIGURE_ENV+=		X_LIBS=${LDFLAGS:M*:Q}
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/libXpm/buildlink3.mk"
 .include "../../x11/libXt/buildlink3.mk"
@@ -35,10 +48,10 @@ CONFIGURE_ARGS+=	--with-x=yes
 CONFIGURE_ARGS+=	--with-x=no
 .endif
 
-.for opt in clisp sbcl
-.if !empty(PKG_OPTIONS:M${opt})
+.for opt in clisp sbcl ecl x11
+.  if !empty(PKG_OPTIONS:M${opt})
 PLIST_SUBST+=	${opt}=""
-.else
+.  else
 PLIST_SUBST+=	${opt}="@comment "
-.endif
+.  endif
 .endfor
