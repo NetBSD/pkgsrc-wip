762b845b36e44d7df4e4702f2b4cc044c955828f
M. Negovanovic <milosn@users.sourceforge.net>
2008-09-07 16:42:28 +0000

    Make do-install a bit smarter.

--- io/addon.mk
+++ io/addon.mk
@@ -1,4 +1,4 @@
-# $NetBSD: addon.mk,v 1.1.1.1 2008/09/06 23:30:19 milosn Exp $
+# $NetBSD: addon.mk,v 1.2 2008/09/07 16:42:28 milosn Exp $
 
 .include "../../wip/io/version.mk"
 
@@ -9,6 +9,8 @@ EXTRACT_SUFX=   	.tar.bz2
 NO_CONFIGURE=		yes
 WRKSRC=			${WRKDIR}/io
 
+TARGET_DIR= ${PREFIX}/lib/io/addons/${ADDON_NAME}
+SRC_DIR= ${WRKSRC}/addons/${ADDON_NAME}
 
 SUBST_CLASSES+=			fix-ab
 
@@ -20,20 +22,26 @@ SUBST_SED.fix-ab+=		-e 's,headerSearchPaths := List clone,headerSearchPaths := L
 SUBST_SED.fix-ab+=		-e 's,linkDirPathFlag .. v,linkDirPathFlag .. v .. " ${LINKER_RPATH_FLAG}" .. v,g'
 
 
-#DEPENDS+=			io-vm>=${IO_VERSION}:../../wip/io-vm
-
-
 do-build:
 	cd ${WRKSRC} && ${PREFIX}/bin/io build.io -a ${ADDON_NAME}
 
 do-install:
-	${INSTALL_DATA_DIR} ${PREFIX}/lib/io/addons/${ADDON_NAME}
-	${INSTALL_DATA_DIR} ${PREFIX}/lib/io/addons/${ADDON_NAME}/_build
-	${CP} -rf ${WRKSRC}/addons/${ADDON_NAME}/_build/dll ${PREFIX}/lib/io/addons/${ADDON_NAME}/_build/
-	${CP} -rf ${WRKSRC}/addons/${ADDON_NAME}/_build/lib ${PREFIX}/lib/io/addons/${ADDON_NAME}/_build/
-	${CP} -rf ${WRKSRC}/addons/${ADDON_NAME}/io ${PREFIX}/lib/io/addons/${ADDON_NAME}/
-	#${CP} -rf ${WRKSRC}/addons/${ADDON_NAME}/protos ${PREFIX}/lib/io/addons/${ADDON_NAME}/
-	${CP} -rf ${WRKSRC}/addons/${ADDON_NAME}/depends ${PREFIX}/lib/io/addons/${ADDON_NAME}/
-	${CP} -rf ${WRKSRC}/addons/${ADDON_NAME}/build.io ${PREFIX}/lib/io/addons/${ADDON_NAME}/
+	${INSTALL_DATA_DIR} ${TARGET_DIR}
+	${INSTALL_DATA_DIR} ${TARGET_DIR}/_build
+	if [ -d  ${SRC_DIR}/_build/dll ]; then \
+		${CP} -rf ${SRC_DIR}/_build/dll ${TARGET_DIR}/_build/; \
+	fi
+	if [ -d ${SRC_DIR}/_build/lib ]; then \
+		${CP} -rf ${SRC_DIR}/_build/lib ${TARGET_DIR}/_build/; \
+	fi
+	if [ -d ${SRC_DIR}/io ]; then \
+		${CP} -rf ${SRC_DIR}/io ${TARGET_DIR}/; \
+	fi
+	if [ -e ${SRC_DIR}/depends ]; then \
+		${CP} -f ${SRC_DIR}/depends ${TARGET_DIR}/; \
+	fi
+	if [ -e ${SRC_DIR}/build.io ]; then \
+		${CP} -f ${SRC_DIR}/build.io ${TARGET_DIR}/; \
+	fi
 
 .include "../../wip/io-vm/buildlink3.mk"
