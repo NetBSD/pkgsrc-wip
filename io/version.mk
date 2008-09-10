3a9bc1e62867990aa5d19f48bcb3ca12e3d1e67b
M. Negovanovic <milosn@users.sourceforge.net>
2008-09-10 19:58:55 +0000

    We need a trailing slash :|.

--- io/version.mk
+++ io/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.1.1.1 2008/09/06 23:30:19 milosn Exp $
+# $NetBSD: version.mk,v 1.2 2008/09/10 19:58:55 milosn Exp $
 
 IO_VERSION=	20080904
-IO_MASTER_SITES = http://milosn.no-ip.org/files
+IO_MASTER_SITES = http://milosn.no-ip.org/files/
