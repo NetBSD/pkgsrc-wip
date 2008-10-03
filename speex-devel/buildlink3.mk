03427cbf1dbcfcd33c42047ff5618c8e05621074
Georg Schwarz <gschwarz@users.sourceforge.net>
2008-10-03 20:22:49 +0000

    updated to 1.2rc1

--- speex-devel/buildlink3.mk
+++ speex-devel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/06/20 18:20:21 gschwarz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/10/03 20:22:49 gschwarz Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 SPEEX-DEVEL_BUILDLINK3_MK:=	${SPEEX-DEVEL_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nspeex-devel}
 BUILDLINK_PACKAGES+=	speex-devel
 
 .if !empty(SPEEX-DEVEL_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.speex-devel+=	speex-devel>=1.1.12
+BUILDLINK_API_DEPENDS.speex-devel+=	speex-devel>=1.2rc1
 BUILDLINK_PKGSRCDIR.speex-devel?=	../../wip/speex-devel
 .endif	# SPEEX-DEVEL_BUILDLINK3_MK
 
