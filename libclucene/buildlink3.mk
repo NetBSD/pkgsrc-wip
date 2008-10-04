3df3634c602b0d00fcf3c0e57c9873c9a6b473bc
Mark Kirby <krub1@users.sourceforge.net>
2008-10-04 20:28:36 +0000

    Update to 0.9.21

--- libclucene/buildlink3.mk
+++ libclucene/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2007/12/12 02:26:42 mwdavies Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/10/04 20:28:36 krub1 Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBCLUCENE_BUILDLINK3_MK:=	${LIBCLUCENE_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	libclucene
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libclucene
 
 .if ${LIBCLUCENE_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.libclucene+=	libclucene>=0.9.20
+BUILDLINK_API_DEPENDS.libclucene+=	libclucene>=0.9.21
 BUILDLINK_PKGSRCDIR.libclucene?=	../../wip/libclucene
 .endif	# LIBCLUCENE_BUILDLINK3_MK
 
