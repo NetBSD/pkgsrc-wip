5f7c0b3e8558e2d48ec3b9a3540a0322bf6fe9d5
Marcin M. Jessa <yazzy@yazzy.org>
2008-10-29 22:53:22 +0000

    Updated Makefile and buildink and added empty PLIST

--- eina/buildlink3.mk
+++ eina/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2008/10/29 22:35:58 yazzy Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/10/29 22:53:22 yazzy Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 EINA_BUILDLINK3_MK:=	${EINA_BUILDLINK3_MK}+
@@ -16,6 +16,6 @@ BUILDLINK_API_DEPENDS.eina+=	eina>=0.9.0.050nb1
 BUILDLINK_PKGSRCDIR.eina?=	../../wip/eina
 .endif	# EINA_BUILDLINK3_MK
 
-.include "../../wip/eina/buildlink3.mk"
+.include "../../wip/ecore/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
