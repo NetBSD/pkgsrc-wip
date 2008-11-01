ee92e965edc1fb07f9c32142f70509a2812f7385
Georg Schwarz <gschwarz@users.sourceforge.net>
2008-11-01 07:37:45 +0000

    upgraded tftp-hpa to release 0.49 which adds IPv6 support

--- tftp-hpa/options.mk
+++ tftp-hpa/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1.1.1 2007/01/01 18:40:20 gschwarz Exp $
+# $NetBSD: options.mk,v 1.2 2008/11/01 07:37:45 gschwarz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tftp-hpa
-PKG_SUPPORTED_OPTIONS=	tcpwrappers remap
+PKG_SUPPORTED_OPTIONS=	tcpwrappers remap inet6
 PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
@@ -14,4 +14,10 @@ CONFIGURE_ARGS+=	--without-tcpwrappers
 
 .if empty(PKG_OPTIONS:Mremap)
 CONFIGURE_ARGS+=	--without-remap
+.endif
+
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--with-ipv6
+.else
+CONFIGURE_ARGS+=	--without-ipv6
 .endif
