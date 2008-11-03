1a42b6c57f269edb24870244e41711b08ba925f4
Mark Davies <mark@ecs.vuw.ac.nz>
2008-11-03 11:34:03 +0000

    akonadi now in pkgsrc.

--- kdepimlibs4/buildlink3.mk
+++ kdepimlibs4/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2008/07/26 22:42:58 mwdavies Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2008/11/03 11:34:03 mwdavies Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 KDEPIMLIBS_BUILDLINK3_MK:=	${KDEPIMLIBS_BUILDLINK3_MK}+
@@ -17,9 +17,9 @@ BUILDLINK_ABI_DEPENDS.kdepimlibs?=	kdepimlibs4>=4.0.0nb1
 BUILDLINK_PKGSRCDIR.kdepimlibs?=	../../wip/kdepimlibs4
 .endif	# KDEPIMLIBS_BUILDLINK3_MK
 
-.include "../../wip/akonadi/buildlink3.mk"
 .include "../../databases/openldap-client/buildlink3.mk"
 .include "../../devel/boost-libs/buildlink3.mk"
+.include "../../mail/akonadi/buildlink3.mk"
 .include "../../security/cyrus-sasl/buildlink3.mk"
 .include "../../security/gpgme/buildlink3.mk"
 .include "../../wip/kdelibs4/buildlink3.mk"
