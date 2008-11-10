b074500331830c61768397f4b8426f0845ad972c
Dieter Baron <dillo@nih.at>
2008-11-10 20:48:57 +0000

    Add option for tucker engine (Bud Tucker in Double Trouble).

--- scummvm-nightly/options.mk
+++ scummvm-nightly/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.8 2008/07/23 12:25:10 dillo Exp $
+# $NetBSD: options.mk,v 1.9 2008/11/10 20:48:57 dillo Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scummvm-nightly
 PKG_SUPPORTED_OPTIONS=	scummvm-nightly-cruise
@@ -6,6 +6,7 @@ PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-igor
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-m4
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-made
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-tinsel
+PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-tucker
 PKG_SUGGESTED_OPTIONS=	${PKG_SUPPORTED_OPTIONS}
 
 .include "../../mk/bsd.options.mk"
@@ -23,5 +24,8 @@ CONFIGURE_ARGS+=--enable-m4
 CONFIGURE_ARGS+=--enable-made
 .endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-tinsel)
+CONFIGURE_ARGS+=--enable-tucker
+.endif
+.if !empty(PKG_OPTIONS:Mscummvm-nightly-tucker)
 CONFIGURE_ARGS+=--enable-tinsel
 .endif
