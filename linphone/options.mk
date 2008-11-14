f455a625ea6ccbc37e82cb715101f066b79f6fd0
Thomas Klausner <wiz@NetBSD.org>
2008-11-14 09:39:44 +0000

    Put options into options.mk.

---
+++ linphone/options.mk
@@ -0,0 +1,32 @@
+# $NetBSD: options.mk,v 1.1 2008/11/14 09:39:44 thomasklausner Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.linphone
+PKG_SUPPORTED_OPTIONS=	alsa inet6 linphone-gui linphone-video
+PKG_SUGGESTED_OPTIONS=	linphone-gui
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Malsa)
+CONFIGURE_ARGS+=	--enable-alsa
+.endif
+
+.if empty(PKG_OPTIONS:Mlinphone-gui)
+CONFIGURE_ARGS+=	--enable-gnome_ui=no
+PLIST_SUBST+=	GUI="@comment "
+.else
+PLIST_SUBST+=	GUI=
+CONFIGURE_ARGS+=	--enable-gnome_ui=yes
+.include "../../x11/gnome-panel/buildlink3.mk"
+.endif
+
+.if empty(PKG_OPTIONS:Mlinphone-gui) | empty(PKG_OPTIONS:Mlinphone-video)
+CONFIGURE_ARGS+=	--enable-video=no
+.else
+CONFIGURE_ARGS+=	--enable-video=yes
+.include "../../devel/SDL/buildlink3.mk"
+.include "../../multimedia/ffmpeg/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--enable-ipv6
+.endif
