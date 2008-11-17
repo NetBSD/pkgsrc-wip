3cb650f70337d307d3e8729ac63dfb1d905ce4b4
Yorick Hardy <yorickhardy@gmail.com>
2008-11-17 12:32:32 +0000

    Update to 1.0.18 and add patches to compile on NetBSD.
    For the list of changes between 1.0.16 and 1.0.18 see:
    
     http://www.alsa-project.org/main/index.php/Changes_v1.0.16_v1.0.17
     http://www.alsa-project.org/main/index.php/Changes_v1.0.17_v1.0.18
    
    under the section alsa-lib.

--- alsa-lib/buildlink3.mk
+++ alsa-lib/buildlink3.mk
@@ -1,20 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.3 2006/04/06 07:16:56 jeremy-c-reed Exp $
-#
-# This file was created automatically using createbuildlink-3.4.
+# $NetBSD: buildlink3.mk,v 1.4 2008/11/17 12:32:32 yhardy Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 ALSA_LIB_BUILDLINK3_MK:=	${ALSA_LIB_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	alsa-lib
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nalsa-lib}
 BUILDLINK_PACKAGES+=	alsa-lib
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}alsa-lib
 
-.if !empty(ALSA_LIB_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.alsa-lib+=	alsa-lib>=1.0.6
+.if ${ALSA_LIB_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.alsa-lib+=	alsa-lib>=1.0.18
 BUILDLINK_PKGSRCDIR.alsa-lib?=	../../wip/alsa-lib
 .endif	# ALSA_LIB_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
