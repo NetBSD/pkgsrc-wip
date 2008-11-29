9707d5a8935839a17f94b85a77b3a6607b293c15
OBATA Akio <obache@users.sourceforge.net>
2008-11-29 01:31:38 +0000

    PLIST_VARS must be defined unconditinally.

--- wl-current/options.mk
+++ wl-current/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2008/11/24 02:11:24 makoto Exp $
+# $NetBSD: options.mk,v 1.4 2008/11/29 01:31:38 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.wl
 PKG_SUPPORTED_OPTIONS=	emacs-w3m-current
@@ -6,8 +6,9 @@ PKG_SUPPORTED_OPTIONS=	emacs-w3m-current
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=			emacs-w3m-current
+
 .if !empty(PKG_OPTIONS:Memacs-w3m-current)
 .include "../../wip/emacs-w3m-current/buildlink3.mk"
-PLIST_VARS+=			emacs-w3m-current
 PLIST.emacs-w3m-current=	yes
 .endif
