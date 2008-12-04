1f2183934d94da95217b535761bbc36db9e4eb74
Hasso Tepper <htepper@users.sourceforge.net>
2008-12-04 04:31:00 +0000

    aKode is a simple audio decoding framework that provides a uniform interface to decode the most common audio formats. It also has a direct playback option for a number of audio outputs.

---
+++ akode/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/12/04 04:31:00 htepper Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+AKODE_BUILDLINK3_MK:=	${AKODE_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	akode
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nakode}
+BUILDLINK_PACKAGES+=	akode
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}akode
+
+.if !empty(AKODE_BUILDLINK3_MK:M+)
+BUILDLINK_API_DEPENDS.akode+=	akode>=2.0.2
+BUILDLINK_API_DEPENDS.akode+=	akode>=2.0.2
+BUILDLINK_PKGSRCDIR.akode?=	../../audio/akode
+.endif	# AKODE_BUILDLINK3_MK
+
+.include "../../audio/flac/buildlink3.mk"
+.include "../../audio/libmad/buildlink3.mk"
+.include "../../audio/libvorbis/buildlink3.mk"
+.include "../../devel/libltdl/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
