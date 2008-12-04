15fa79707b18118e95f772a5716b4f0b9787d89c
Thomas Klausner <wiz@NetBSD.org>
2008-12-04 13:16:48 +0000

    Don't link everything against libintl just because avahi-common.la needs it.
    Reduce number of options, it was too much to handle all of them.
    Make gtk a non-option; reduce remainder to python, gdbm, and avahi-howl.
    
    Probably they should be put into separate packages, so if someone
    feels like it, please go ahead.
    
    Fix various PLIST problems.

---
+++ avahi/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1 2008/12/04 13:16:48 thomasklausner Exp $
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+AVAHI_BUILDLINK3_MK:=	${AVAHI_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	avahi
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Navahi}
+BUILDLINK_PACKAGES+=	avahi
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}avahi
+
+.if ${AVAHI_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.avahi+=	avahi>=0.6.23
+BUILDLINK_PKGSRCDIR.avahi?=	../../wip/avahi
+.endif	# AVAHI_BUILDLINK3_MK
+
+#.include "../../textproc/expat/buildlink3.mk"
+#.include "../../wip/libdaemon/buildlink3.mk"
+#.include "../../devel/glib2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
--- avahi/options.mk
+++ avahi/options.mk
@@ -1,99 +1,17 @@
-# $NetBSD: options.mk,v 1.6 2008/12/04 09:59:05 thomasklausner Exp $
+# $NetBSD: options.mk,v 1.7 2008/12/04 13:16:48 thomasklausner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.avahi
 
-PKG_SUPPORTED_OPTIONS=	dbus glib gdbm python gtk python-gtk python-dbus
-PKG_SUPPORTED_OPTIONS+=	avahi-howl expat
-PKG_SUGGESTED_OPTIONS=	expat gtk
+PKG_SUPPORTED_OPTIONS=	avahi-howl gdbm python
 
 .include "../../mk/bsd.options.mk"
-
-###
-### Some basic dependencies for avahi:
-###
-### libdaemon + expat = 		avahi-daemon	(default)
-### libdaemon + expat = 		avahi-dnsconfd	(default)
-### avahi-daemon + dbus = 		libavahi-client
-### avahi-daemon + dbus = 		avahi-utils
-### python + python-gtk + python-dbus = avahi-python
-###
-
-###
-### Enable py-dbus support (implies python and dbus)
-###
-.if !empty(PKG_OPTIONS:Mpython-dbus)
-PKG_OPTIONS+=		python dbus
-.  include "../../lang/python/pyversion.mk"
-.  include "../../sysutils/py-dbus/buildlink3.mk"
-DEPENDS+=		${PYPKGPREFIX}-libxml2-[0-9]*:../../textproc/py-libxml2
-DEPENDS+=		${PYPKGPREFIX}-expat-[0-9]*:../../textproc/py-expat
-PLIST_SRC+=		${PKGDIR}/PLIST.pydbus
-.else
-CONFIGURE_ARGS+=	--disable-python-dbus
-.endif
-
-###
-### Enable pygtk support (implies python and gtk)
-###
-.if !empty(PKG_OPTIONS:Mpython-gtk)
-PKG_OPTIONS+=		python gtk
-.  include "../../x11/py-gtk2/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-pygtk
-.endif
 
 ###
 ### Enable compatibility layer for HOWL
 ###
 .if !empty(PKG_OPTIONS:Mavahi-howl)
 CONFIGURE_ARGS+=	--enable-compat-howl
-.endif
-
-###
-### Enable expat support
-###
-.if !empty(PKG_OPTIONS:Mexpat)
-.  include "../../textproc/expat/buildlink3.mk"
-PLIST_SRC+=		${PKGDIR}/PLIST.expat
-.else
-CONFIGURE_ARGS+=	--disable-expat
-.endif
-
-###
-### Enable gtk support (implies glib)
-### This will get you avahi-discover-standalone
-###
-.if !empty(PKG_OPTIONS:Mgtk)
-PKG_OPTIONS+=		glib
-.  include "../../devel/libglade/buildlink3.mk"
-.  include "../../x11/gtk2/buildlink3.mk"
-PLIST_SRC+=		${PKGDIR}/PLIST.gtk
-.if !empty(PKG_OPTIONS:Mdbus)
-PLIST_SRC+=		${PKGDIR}/PLIST.dbusgtk
-.endif
-.else
-CONFIGURE_ARGS+=	--disable-gtk
-.endif
-
-###
-### Enable glib support
-###
-.if !empty(PKG_OPTIONS:Mglib)
-.  include "../../devel/glib2/buildlink3.mk"
-PLIST_SRC+=		${PKGDIR}/PLIST.glib
-.else
-CONFIGURE_ARGS+=	--disable-glib
-.endif
-
-###
-### Enable python support
-###
-.if !empty(PKG_OPTIONS:Mpython)
-.  include "../../lang/python/application.mk"
-PYTHON_VERSIONS_ACCEPTED=	25 24
-PLIST_SUBST+=		PYSITELIB=${PYSITELIB}
-.else
-CONFIGURE_ARGS+=	--disable-python
+PLIST_SRC+=		${PKGDIR}/PLIST.howl
 .endif
 
 ###
@@ -106,29 +24,25 @@ CONFIGURE_ARGS+=	--disable-gdbm
 .endif
 
 ###
-### Enable DBUS support
+### Enable python support
 ###
-.if !empty(PKG_OPTIONS:Mdbus)
-.  include "../../sysutils/dbus/buildlink3.mk"
-PLIST_SRC+=		${PKGDIR}/PLIST.dbus
-CONF_FILES+=		${PREFIX}/share/examples/avahi/avahi-dbus.conf \
-			${PREFIX}/etc/dbus-1/system.d/avahi-dbus.conf
+.if !empty(PKG_OPTIONS:Mpython)
+.  include "../../lang/python/application.mk"
+.  include "../../lang/python/extension.mk"
+PYTHON_VERSIONS_ACCEPTED=	25 24
+PLIST_SUBST+=			PYSITELIB=${PYSITELIB}
+.  include "../../sysutils/py-dbus/buildlink3.mk"
+.  include "../../x11/py-gtk2/buildlink3.mk"
+DEPENDS+=		${PYPKGPREFIX}-libxml2-[0-9]*:../../textproc/py-libxml2
+DEPENDS+=		${PYPKGPREFIX}-expat-[0-9]*:../../textproc/py-expat
+### If python and gdbm are enabled we need py-gdbm as well
+.  if !empty(PKG_OPTIONS:Mgdbm)
+DEPENDS+=		${PYPKGPREFIX}-gdbm-[0-9]*:../../databases/py-gdbm
+PLIST_SRC+=		${PKGDIR}/PLIST.pygdbm
+.  endif
+PLIST_SRC+=		${PKGDIR}/PLIST.python
 .else
-CONFIGURE_ARGS+=	--disable-dbus
-.endif
-
-###
-### If python, python-gtk and python-dbus are all enabled you get avahi-python
-###
-.if !empty(PKG_OPTIONS:Mpython) && !empty(PKG_OPTIONS:Mpython-gtk) \
-	&& !empty(PKG_OPTIONS:Mpython-dbus)
-PLIST_SRC+=		${PKGDIR}/PLIST.avahipy
-.endif
-
-###
-### If python, and gdbm are enabled we need py-gdbm as well
-###
-.if !empty(PKG_OPTIONS:Mpython) && !empty(PKG_OPTIONS:Mgdbm)
-DEPENDS+=	${PYPKGPREFIX}-gdbm-[0-9]*:../../databases/py-gdbm
-PLIST_SRC+=	${PKGDIR}/PLIST.pygdbm
+CONFIGURE_ARGS+=	--disable-python
+CONFIGURE_ARGS+=	--disable-python-dbus
+CONFIGURE_ARGS+=	--disable-pygtk
 .endif
