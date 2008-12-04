ba390c56182e6d20054fa69f3dc69ab6ad692df7
Thomas Klausner <wiz@NetBSD.org>
2008-12-04 13:19:07 +0000

    Pull in glib2 and gtk2, leaked by some installed header files.

--- avahi/buildlink3.mk
+++ avahi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2008/12/04 13:16:48 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2008/12/04 13:19:07 thomasklausner Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 AVAHI_BUILDLINK3_MK:=	${AVAHI_BUILDLINK3_MK}+
@@ -16,8 +16,7 @@ BUILDLINK_API_DEPENDS.avahi+=	avahi>=0.6.23
 BUILDLINK_PKGSRCDIR.avahi?=	../../wip/avahi
 .endif	# AVAHI_BUILDLINK3_MK
 
-#.include "../../textproc/expat/buildlink3.mk"
-#.include "../../wip/libdaemon/buildlink3.mk"
-#.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
