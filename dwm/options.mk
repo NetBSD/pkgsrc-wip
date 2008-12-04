4605b388cf41813dc20463aa3ecd17820cf12457
Leonardo Taccari <leot@NetBSD.org>
2008-12-04 21:44:28 +0000

    Update to 5.3.
    Changes (from Anselm R. Garbe's dwm@suckless.org ML announce):
      This release contains the spawn() version of Neale and several
      bugfixes regarding the NOBORDER handling in 5.2, and a new config.h
      option which allows to set use server grabs during mouse based
      resizals/movements.

--- dwm/options.mk
+++ dwm/options.mk
@@ -1,20 +1,19 @@
-# $NetBSD: options.mk,v 1.1.1.1 2008/09/09 21:52:26 leot1990 Exp $
+# $NetBSD: options.mk,v 1.2 2008/12/04 21:44:28 leot1990 Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.dwm
 PKG_SUPPORTED_OPTIONS=		xinerama
-PKG_SUGGESTED_OPTIONS=		xinerama
 
 .include "../../mk/bsd.options.mk"
 
 # If we don't want the Xinerama support we delete XINERAMALIBS and
 # XINERAMAFLAGS lines, otherwise the Xinerama support is the default.
 .if !empty(PKG_OPTIONS:Mxinerama)
-.  include "../../x11/libXinerama/buildlink3.mk"
-.else
 SUBST_CLASSES+=		options
 SUBST_STAGE.options=	pre-build
 SUBST_MESSAGE.options=	Toggle the Xinerama support
 SUBST_FILES.options=	config.mk
-SUBST_SED.options+=	-e '/^XINERAMA/ d'
+SUBST_SED.options+=	-e '/XINERAMA/ s/^\#//'
+.  include "../../x11/libXinerama/buildlink3.mk"
+.else
 .  include "../../x11/libX11/buildlink3.mk"
 .endif
