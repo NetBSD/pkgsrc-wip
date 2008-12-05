0285eeab724e9b9331196c595a7075c14b93698c
Hasso Tepper <htepper@users.sourceforge.net>
2008-12-05 10:06:39 +0000

    Various small pkgsrc related fixes almost everywhere. Move mozilla signer plugin from ${PREFIX}/lib/firefox/plugins to ${PREFIX}/lib/mozilla/plugins. There should be no need to avoid builtin openssl. Bump PKGREVISION.

--- opensc/buildlink3.mk
+++ opensc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2008/08/07 19:03:31 htepper Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2008/12/05 10:06:39 htepper Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 OPENSC_BUILDLINK3_MK:=	${OPENSC_BUILDLINK3_MK}+
@@ -9,10 +9,17 @@ BUILDLINK_DEPENDS+=	opensc
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nopensc}
 BUILDLINK_PACKAGES+=	opensc
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}opensc
 
 .if !empty(OPENSC_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.opensc+=	opensc>=0.11.5
 BUILDLINK_PKGSRCDIR.opensc?=	../../wip/opensc
+pkgbase := opensc
+.include "../../mk/pkg-build-options.mk"
 .endif	# OPENSC_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+.if !empty(PKG_BUILD_OPTIONS.opensc:Mopenct)
+.include "../../wip/openct/buildlink3.mk"
+.endif
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
--- opensc/options.mk
+++ opensc/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.4 2008/08/08 19:45:06 htepper Exp $
+# $NetBSD: options.mk,v 1.5 2008/12/05 10:06:39 htepper Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.opensc
 PKG_SUPPORTED_OPTIONS=	nsplugin openct pcsc-lite
 PKG_SUGGESTED_OPTIONS=	nsplugin pcsc-lite
-PLIST_VARS+=		nsplugin 
+PLIST_VARS+=		nsplugin
 
 .include "../../mk/bsd.options.mk"
 
@@ -12,8 +12,8 @@ PLIST_VARS+=		nsplugin
 .include "../../security/pinentry/buildlink3.mk"
 .include "../../x11/libXt/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-nsplugin
-CONFIGURE_ARGS+=	--with-plugindir=/usr/pkg/lib/firefox/plugins
-CONFIGURE_ARGS+=	--with-pinentry=/usr/pkg/bin/pinentry
+CONFIGURE_ARGS+=	--with-plugindir=${PREFIX}/lib/mozilla/plugins
+CONFIGURE_ARGS+=	--with-pinentry=${PREFIX}/bin/pinentry
 PLIST.nsplugin=		yes
 .else
 CONFIGURE_ARGS+=	--disable-nsplugin
@@ -22,7 +22,7 @@ CONFIGURE_ARGS+=	--disable-nsplugin
 .if !empty(PKG_OPTIONS:Mpcsc-lite)
 .include "../../security/pcsc-lite/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-pcsc
-CONFIGURE_ARGS+=	--with-pcsc-provider=/usr/pkg/lib/libpcsclite.so
+CONFIGURE_ARGS+=	--with-pcsc-provider=${PREFIX}/lib/libpcsclite.so
 .else
 CONFIGURE_ARGS+=	--disable-pcsc
 .endif
