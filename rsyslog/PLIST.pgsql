d3524144cdd9a6aeb09413060dddc77dbf7fc0fb
Bartosz Kuzma <bartosz.kuzma@gmail.com>
2008-12-07 14:16:32 +0000

    Update to 3.21.9.

---
+++ rsyslog/PLIST.pgsql
@@ -0,0 +1,3 @@
+@comment $NetBSD: PLIST.pgsql,v 1.1 2008/12/07 14:16:32 bartoszkuzma Exp $
+lib/rsyslog/ompgsql.la
+share/examples/rsyslog/createDB-pgsql.sql
--- rsyslog/options.mk
+++ rsyslog/options.mk
@@ -1,24 +1,46 @@
-# $NetBSD: options.mk,v 1.3 2008/08/10 21:08:29 mstapelberg Exp $
+# $NetBSD: options.mk,v 1.4 2008/12/07 14:16:32 bartoszkuzma Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.rsyslog
-PKG_SUPPORTED_OPTIONS=	largefile mysql gnutls
+PKG_SUPPORTED_OPTIONS=	gnutls gssapi largefile mail mysql pgsql snmp
 PKG_SUGGESTED_OPTIONS=	largefile
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mlargefile)
-CFLAGS+=			-DWITH_LARGEFILE
-FEATURE_LARGEFILE=		# empty
-.endif
-
-.if !empty(PKG_OPTIONS:Mmysql)
-.include "../../mk/mysql.buildlink3.mk"
-CFLAGS+=			-DWITH_DB
-LDFLAGS+=			-lmysqlclient
-FEATURE_DB=			# empty
+CONFIGURE_ARGS+=		--enable-largefile
 .endif
 
 .if !empty(PKG_OPTIONS:Mgnutls)
 .include "../../security/gnutls/buildlink3.mk"
 CONFIGURE_ARGS+=		--enable-gnutls
+PLIST_SRC+=			PLIST.gnutls
+.endif
+
+.if !empty(PKG_OPTIONS:Mgssapi)
+.include "../../mk/krb5.buildlink3.mk"
+CONFIGURE_ARGS+=		--enable-gssapi-krb5
+PLIST_SRC+=			PLIST.gssapi
+.endif
+
+.if !empty(PKG_OPTIONS:Mmail)
+CONFIGURE_ARGS+=		--enable-mail
+PLIST_SRC+=			PLIST.mail
+.endif
+
+.if !empty(PKG_OPTIONS:Mmysql)
+.include "../../mk/mysql.buildlink3.mk"
+CONFIGURE_ARGS+=		--enable-mysql
+PLIST_SRC+=			PLIST.mysql
+.endif
+
+.if !empty(PKG_OPTIONS:Mpgsql)
+.include "../../mk/pgsql.buildlink3.mk"
+CONFIGURE_ARGS+=		--enable-pgsql
+PLIST_SRC+=			PLIST.pgsql
+.endif
+
+.if !empty(PKG_OPTIONS:Msnmp)
+.include "../../net/net-snmp/buildlink3.mk"
+CONFIGURE_ARGS+=		--enable-snmp
+PLIST_SRC+=			PLIST.snmp
 .endif
