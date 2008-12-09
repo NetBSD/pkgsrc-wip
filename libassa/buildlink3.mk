07769230b26936d4ad4758ff34953d9827cfbdeb
Bartosz Kuzma <bartosz.kuzma@gmail.com>
2008-12-09 21:40:00 +0000

    Update to 3.5.0.
    Changes since 3.4.2:
      * Fixed bug #1716602 (Acceptor doesn't properly cleanup failed
        SH::open() call).
      * Fixed bug #1716593 ("options" section name should be case-insensitive)
      * Fixed bug #1716596 (GenServer::init() fails to convert args to full path).
      * Fixed bug #1716668 (Assure_exit() macro won't compile).
      * Fixed bug #1716588 (assa-genesis generates proper app version).
      * Added a no-op virtual destructor to SigHandler base class (bug #1716597).
      * Fixed 'maxfd+1' miscalculation bug in Reactor's select() method.
      * Fixed assa.spec.in to fit FC extras strict spec rules.
      * Patch to compile with GCC 4.3 (Martin Michlmayr).

--- libassa/buildlink3.mk
+++ libassa/buildlink3.mk
@@ -1,17 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/10/09 17:56:00 bartoszkuzma Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2008/12/09 21:40:00 bartoszkuzma Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBASSA_BUILDLINK3_MK:=	${LIBASSA_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	libassa
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibassa}
 BUILDLINK_PACKAGES+=	libassa
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libassa
 
-.if !empty(LIBASSA_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.libassa+=	libassa>=3.4.2
+.if ${LIBASSA_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.libassa+=	libassa>=3.5.0
 BUILDLINK_PKGSRCDIR.libassa?=	../../wip/libassa
 .endif	# LIBASSA_BUILDLINK3_MK
 
