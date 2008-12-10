b137efbcf637786e791c861647ad14008c16156d
X400 <x400@users.sourceforge.net>
2008-12-10 00:31:53 +0000

    Import callweaver-1.2.0.1 as wip/callweaver.
    CallWeaver is a community-driven, vendor-independent, cross-platform,
    open source, PBX software project (formerly known as OpenPBX.org).
    
    It was originally derived from Asterisk. Now it supports analog and
    digital PSTN telephony, multi-protocol voice over IP telephony, fax,
    software-fax, T.38 fax over IP and many telephony applications such as
    IVR, conferencing and callcenter queue management.

--- callweaver/options.mk
+++ callweaver/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2008/12/05 15:39:43 x400 Exp $
+# $NetBSD: options.mk,v 1.1.1.2 2008/12/10 00:31:53 x400 Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.callweaver
 PKG_SUPPORTED_OPTIONS=	mysql pgsql
@@ -8,8 +8,6 @@ PKG_SUPPORTED_OPTIONS=	mysql pgsql
 .if !empty(PKG_OPTIONS:Mmysql)
 .include "../../databases/mysql5-client/buildlink3.mk"
 PLIST_SRC+=		PLIST.mysql
-CONF_FILES+=		${PKG_SYSCONFDIR}/cdr_mysql.conf \
-			${PKG_SYSCONFDIR}/res_mysql.conf
 
 CONFIGURE_ARGS+=	--enable-mysql --with-app_sql_mysql --with-cdr_mysql --with-res_config_mysql
 .endif
@@ -17,8 +15,6 @@ CONFIGURE_ARGS+=	--enable-mysql --with-app_sql_mysql --with-cdr_mysql --with-res
 .if !empty(PKG_OPTIONS:Mpgsql)
 .include "../../databases/postgresql83-client/buildlink3.mk"
 PLIST_SRC+=		PLIST.pgsql
-CONF_FILES+=		${PKG_SYSCONFDIR}/cdr_pgsql.conf \
-			${PKG_SYSCONFDIR}/res_pgsql.conf
 
 CONFIGURE_ARGS+=	--enable-postgresql --with-app_sql_postgres --with-cdr_pgsql --with-res_config_pgsql
 .endif
