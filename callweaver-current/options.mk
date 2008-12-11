59c3663c6d573cfda7d80cfc4e1acb8cc416f6f9
X400 <x400@users.sourceforge.net>
2008-12-11 18:55:08 +0000

    Import callweaver-RC-1.1.99.20081210 as wip/callweaver-current.
    CallWeaver is a community-driven, vendor-independent, cross-platform,
    open source, PBX software project (formerly known as OpenPBX.org).
    
    It was originally derived from Asterisk. Now it supports analog and
    digital PSTN telephony, multi-protocol voice over IP telephony, fax,
    software-fax, T.38 fax over IP and many telephony applications such as
    IVR, conferencing and callcenter queue management.

---
+++ callweaver-current/options.mk
@@ -0,0 +1,18 @@
+# $NetBSD: options.mk,v 1.1.1.1 2008/12/11 18:55:08 x400 Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.callweaver
+PKG_SUPPORTED_OPTIONS=	mysql pgsql
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mmysql)
+.include "../../databases/mysql5-client/buildlink3.mk"
+PLIST_SRC+=		PLIST.mysql
+CONFIGURE_ARGS+=	--enable-mysql --with-app_sql_mysql --with-cdr_mysql --with-res_config_mysql
+.endif
+
+.if !empty(PKG_OPTIONS:Mpgsql)
+.include "../../databases/postgresql83-client/buildlink3.mk"
+PLIST_SRC+=		PLIST.pgsql
+CONFIGURE_ARGS+=	--enable-postgresql --with-app_sql_postgres --with-cdr_pgsql --with-res_config_pgsql
+.endif
