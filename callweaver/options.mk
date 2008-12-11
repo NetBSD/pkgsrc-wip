c4bd01c1297a061ca9bd4f026b6d766567b88ece
X400 <x400@users.sourceforge.net>
2008-12-11 08:27:29 +0000

    Import callweaver-1.2.0.1 as wip/callweaver.
    CallWeaver is a community-driven, vendor-independent, cross-platform,
    open source, PBX software project (formerly known as OpenPBX.org).
    
    It was originally derived from Asterisk. Now it supports analog and
    digital PSTN telephony, multi-protocol voice over IP telephony, fax,
    software-fax, T.38 fax over IP and many telephony applications such as
    IVR, conferencing and callcenter queue management.

--- callweaver/options.mk
+++ callweaver/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.2 2008/12/10 00:31:53 x400 Exp $
+# $NetBSD: options.mk,v 1.1.1.3 2008/12/11 08:27:29 x400 Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.callweaver
 PKG_SUPPORTED_OPTIONS=	mysql pgsql
@@ -8,13 +8,11 @@ PKG_SUPPORTED_OPTIONS=	mysql pgsql
 .if !empty(PKG_OPTIONS:Mmysql)
 .include "../../databases/mysql5-client/buildlink3.mk"
 PLIST_SRC+=		PLIST.mysql
-
 CONFIGURE_ARGS+=	--enable-mysql --with-app_sql_mysql --with-cdr_mysql --with-res_config_mysql
 .endif
 
 .if !empty(PKG_OPTIONS:Mpgsql)
 .include "../../databases/postgresql83-client/buildlink3.mk"
 PLIST_SRC+=		PLIST.pgsql
-
 CONFIGURE_ARGS+=	--enable-postgresql --with-app_sql_postgres --with-cdr_pgsql --with-res_config_pgsql
 .endif
