a716517949f1852293dd45faf784a08f20f351a3
X400 <x400@users.sourceforge.net>
2008-12-18 09:11:19 +0000

    *** empty log message ***

--- callweaver/options.mk
+++ callweaver/options.mk
@@ -1,16 +1,20 @@
-# $NetBSD: options.mk,v 1.1.1.4 2008/12/17 14:44:52 x400 Exp $
+# $NetBSD: options.mk,v 1.2 2008/12/18 09:11:19 x400 Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.callweaver
 PKG_SUPPORTED_OPTIONS=	mysql pgsql
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=		mysql pgsql
+
 .if !empty(PKG_OPTIONS:Mmysql)
 .include "../../databases/mysql5-client/buildlink3.mk"
+PLIST.mysql=		yes
 CONFIGURE_ARGS+=	--enable-mysql --with-app_sql_mysql --with-cdr_mysql --with-res_config_mysql
 .endif
 
 .if !empty(PKG_OPTIONS:Mpgsql)
 .include "../../databases/postgresql83-client/buildlink3.mk"
+PLIST.pgsql=		yes
 CONFIGURE_ARGS+=	--enable-postgresql --with-app_sql_postgres --with-cdr_pgsql --with-res_config_pgsql
 .endif
