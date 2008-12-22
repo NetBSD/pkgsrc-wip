4d584802fc8cd4898fec0ffe3e4c6526d696c2f0
Petar Bogdanovic <petar@smokva.net>
2008-12-22 17:26:14 +0000

    Import dcc-1.3.99 as wip/dcc.
    The Distributed Checksum Clearinghouses or DCC is an anti-spam content filter
    that runs on a variety of operating systems. As of the middle of 2007, it
    involves millions of users, more than six hundred thousand client computer
    systems, and more than 250 servers collecting and counting checksums related to
    more than 300 million mail messages on week days. The counts can be used by
    SMTP servers and mail user agents to detect and reject or filter spam or
    unsolicited bulk mail. DCC servers exchange or "flood" common checksums. The
    checksums include values that are constant across common variations in bulk
    messages, including "personalizations".

---
+++ dcc/options.mk
@@ -0,0 +1,46 @@
+# $NetBSD: options.mk,v 1.1.1.1 2008/12/22 17:26:14 pebog Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.dcc
+PKG_SUPPORTED_OPTIONS=	dccd dccifd dccm
+PKG_SUGGESTED_OPTIONS=	dccifd
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS=		dccd dccifd dccm dns-helper
+
+###
+### Don't build dccd, the DCC server.
+###
+.if empty(PKG_OPTIONS:Mdccd)
+CONFIGURE_ARGS+=	--disable-server
+.else
+CONF_FILES+=		${DCC_EGDIR}/flod ${DCC_HOME}/flod \
+			${DCC_EGDIR}/grey_flod ${DCC_HOME}/grey_flod \
+			${DCC_EGDIR}/whitelist ${DCC_HOME}/whitelist \
+			${DCC_EGDIR}/grey_whitelist ${DCC_HOME}/grey_whitelist
+PLIST.dccd=		yes
+RCD_SCRIPTS+=		dccd
+.endif
+
+###
+### Don't build dccifd, the DCC program interface.
+###
+.if empty(PKG_OPTIONS:Mdccifd)
+CONFIGURE_ARGS+=	--disable-dccifd
+.else
+PLIST.dccifd=		yes
+PLIST.dns-helper=	yes
+RCD_SCRIPTS+=		dccifd
+.endif
+
+###
+### Don't build dccm, the DCC milter.
+###
+.if empty(PKG_OPTIONS:Mdccm)
+CONFIGURE_ARGS+=	--disable-dccm
+.else
+.include "../../mail/libmilter/buildlink3.mk"
+PLIST.dccm=		yes
+PLIST.dns-helper=	yes
+RCD_SCRIPTS+=		dccm
+.endif
