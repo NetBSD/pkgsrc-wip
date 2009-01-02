d5a1a5b76c0766e37b13641b6a2d603494cf300a
Dieter Baron <dillo@nih.at>
2009-01-02 23:31:56 +0000

    Saga 2 is now disabled by default, make it an option.

--- scummvm-nightly/options.mk
+++ scummvm-nightly/options.mk
@@ -1,10 +1,11 @@
-# $NetBSD: options.mk,v 1.13 2008/12/17 08:48:25 dillo Exp $
+# $NetBSD: options.mk,v 1.14 2009/01/02 23:31:56 dillo Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scummvm-nightly
 PKG_SUPPORTED_OPTIONS=	scummvm-nightly-cruise
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-igor
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-m4
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-made
+PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-saga2
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-tinsel
 PKG_SUGGESTED_OPTIONS=	${PKG_SUPPORTED_OPTIONS}
 
@@ -21,6 +22,9 @@ CONFIGURE_ARGS+=--enable-m4
 .endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-made)
 CONFIGURE_ARGS+=--enable-made
+.endif
+.if !empty(PKG_OPTIONS:Mscummvm-nightly-saga2)
+CONFIGURE_ARGS+=--enable-saga2
 .endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-tinsel)
 CONFIGURE_ARGS+=--enable-tinsel
