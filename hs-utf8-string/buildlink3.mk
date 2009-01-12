43f2da0e77d0d6eb80593cb444bb97cf520c940c
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-01-12 06:32:32 +0000

    Updated to utf8-string 0.3.3

--- hs-utf8-string/buildlink3.mk
+++ hs-utf8-string/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/07/19 08:42:54 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/01/12 06:32:32 phonohawk Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 HS_UTF8_STRING_BUILDLINK3_MK:=	${HS_UTF8_STRING_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}hs-utf8-string
 
 .if ${HS_UTF8_STRING_BUILDLINK3_MK} == "+"
 BUILDLINK_DEPMETHOD.hs-utf8-string?=	build
-BUILDLINK_API_DEPENDS.hs-utf8-string+=	hs-utf8-string>=0.3.1.1
+BUILDLINK_API_DEPENDS.hs-utf8-string+=	hs-utf8-string>=0.3.3
 BUILDLINK_PKGSRCDIR.hs-utf8-string?=	../../wip/hs-utf8-string
 .endif	# HS_UTF8_STRING_BUILDLINK3_MK
 
