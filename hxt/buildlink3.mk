409e831c9eca62c0e87c37fc86d0b791d0ddf921
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-01-13 13:11:58 +0000

    Updated to HXT 8.2.0

--- hxt/buildlink3.mk
+++ hxt/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/07/20 07:01:06 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/01/13 13:11:58 phonohawk Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 HXT_BUILDLINK3_MK:=	${HXT_BUILDLINK3_MK}+
@@ -13,11 +13,14 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}hxt
 
 .if ${HXT_BUILDLINK3_MK} == "+"
 BUILDLINK_DEPMETHOD.hxt?=	build
-BUILDLINK_API_DEPENDS.hxt+=	hxt>=8.1.0
+BUILDLINK_API_DEPENDS.hxt+=	hxt>=8.2.0
 BUILDLINK_PKGSRCDIR.hxt?=	../../wip/hxt
 .endif	# HXT_BUILDLINK3_MK
 
 .include "../../wip/hs-curl/buildlink3.mk"
+.include "../../wip/hs-network/buildlink3.mk"
+.include "../../wip/hs-parallel/buildlink3.mk"
+.include "../../wip/parsec/buildlink3.mk"
 .include "../../wip/tagsoup/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
