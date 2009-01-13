82884c0696b78c4106656448e461f84b527ed0b6
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-01-13 22:36:38 +0000

    Updated to hslogger 1.0.7

--- hslogger/buildlink3.mk
+++ hslogger/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/07/20 07:57:59 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/01/13 22:36:38 phonohawk Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 HSLOGGER_BUILDLINK3_MK:=	${HSLOGGER_BUILDLINK3_MK}+
@@ -13,8 +13,12 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}hslogger
 
 .if ${HSLOGGER_BUILDLINK3_MK} == "+"
 BUILDLINK_DEPMETHOD.hslogger?=	build
-BUILDLINK_API_DEPENDS.hslogger+=	hslogger>=1.0.5
+BUILDLINK_API_DEPENDS.hslogger+=	hslogger>=1.0.7
 BUILDLINK_PKGSRCDIR.hslogger?=	../../wip/hslogger
 .endif	# HSLOGGER_BUILDLINK3_MK
+
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-network/buildlink3.mk"
+.include "../../wip/hs-time/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
