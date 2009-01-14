dd4f05d4cb0dd5cfa9769fa3f8bdfe7f1e11687d
OBATA Akio <obache@users.sourceforge.net>
2009-01-14 13:42:41 +0000

    Update scim to 1.4.7.
    Take maintainership.

--- scim/buildlink3.mk
+++ scim/buildlink3.mk
@@ -1,18 +1,27 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/09/18 18:20:42 dhowland Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2009/01/14 13:42:41 obache Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 SCIM_BUILDLINK3_MK:=	${SCIM_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	scim
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nscim}
 BUILDLINK_PACKAGES+=	scim
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}scim
 
-.if !empty(SCIM_BUILDLINK3_MK:M+)
+.if ${SCIM_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.scim+=	scim>=1.4.4
 BUILDLINK_PKGSRCDIR.scim?=	../../wip/scim
+
+PRINT_PLIST_AWK+=	/^@dirrm lib\/scim-1.0\/1.4.0$$/ { next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/scim-1.0$$/ { next; }
+PRINT_PLIST_AWK+=	/^@dirrm lib\/scim-1.0\/1.4.0\/(SetupUI|IMEngine|Helper|FrontEnd|Filter|Config)$$/ \
+				{print "@comment in scim: " $$0; next; }
+PRINT_PLIST_AWK+=	/^@dirrm share\/scim$$/ { next; }
+PRINT_PLIST_AWK+=	/^@dirrm share\/scim\/icons$$/ \
+				{print "@comment in scim: " $$0; next; }
 .endif	# SCIM_BUILDLINK3_MK
 
 .include "../../devel/gettext-lib/buildlink3.mk"
