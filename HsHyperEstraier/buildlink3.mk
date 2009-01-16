ce2cd0162abbc2a228f59402d7b7041eb11b0eb6
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-01-16 13:11:32 +0000

    Update to HsHyperEstraier 0.3

--- HsHyperEstraier/buildlink3.mk
+++ HsHyperEstraier/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/07/19 08:55:46 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/01/16 13:11:32 phonohawk Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 HSHYPERESTRAIER_BUILDLINK3_MK:=	${HSHYPERESTRAIER_BUILDLINK3_MK}+
@@ -13,11 +13,12 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}HsHyperEstraier
 
 .if ${HSHYPERESTRAIER_BUILDLINK3_MK} == "+"
 BUILDLINK_DEPMETHOD.HsHyperEstraier?=	build
-BUILDLINK_API_DEPENDS.HsHyperEstraier+=	HsHyperEstraier>=0.2.1
+BUILDLINK_API_DEPENDS.HsHyperEstraier+=	HsHyperEstraier>=0.3
 BUILDLINK_PKGSRCDIR.HsHyperEstraier?=	../../wip/HsHyperEstraier
 .endif	# HSHYPERESTRAIER_BUILDLINK3_MK
 
 .include "../../textproc/hyperestraier/buildlink3.mk"
+.include "../../wip/hs-network/buildlink3.mk"
 .include "../../wip/hs-utf8-string/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
