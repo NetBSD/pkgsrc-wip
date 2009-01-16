575ca9d50cac069830c989223b8faabfb1a340b6
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-01-16 08:43:19 +0000

    Update to Lucu 0.2

--- Lucu/buildlink3.mk
+++ Lucu/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/07/20 07:49:50 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/01/16 08:43:19 phonohawk Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LUCU_BUILDLINK3_MK:=	${LUCU_BUILDLINK3_MK}+
@@ -12,14 +12,18 @@ BUILDLINK_PACKAGES+=	Lucu
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}Lucu
 
 .if ${LUCU_BUILDLINK3_MK} == "+"
-BUILDLINK_DEPMETHOD.Lucu?=	build
-BUILDLINK_API_DEPENDS.Lucu+=	Lucu>=0.1
+BUILDLINK_API_DEPENDS.Lucu+=	Lucu>=0.2
 BUILDLINK_PKGSRCDIR.Lucu?=	../../wip/Lucu
 .endif	# LUCU_BUILDLINK3_MK
 
 .include "../../wip/HsOpenSSL/buildlink3.mk"
+.include "../../wip/haskell-src/buildlink3.mk"
 .include "../../wip/hs-dataenc/buildlink3.mk"
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-network/buildlink3.mk"
+.include "../../wip/hs-time/buildlink3.mk"
 .include "../../wip/hs-zlib/buildlink3.mk"
 .include "../../wip/hxt/buildlink3.mk"
+.include "../../wip/stm/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
