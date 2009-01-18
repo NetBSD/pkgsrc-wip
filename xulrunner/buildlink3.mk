bc40f6b82c6a69071bd20eb50b16bec9de54a262
Tobias Nygren <tnn@NetBSD.org>
2009-01-18 20:45:09 +0000

    update to xulrunner-1.9.0.5. Don't know if it works, let me know if you try it.

--- xulrunner/buildlink3.mk
+++ xulrunner/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2008/06/01 15:22:33 tnn2 Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/01/18 20:45:09 tnn2 Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 XULRUNNER_BUILDLINK3_MK:=	${XULRUNNER_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	xulrunner
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}xulrunner
 
 .if ${XULRUNNER_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=1.9rc1
+BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=1.9.0.5
 BUILDLINK_PKGSRCDIR.xulrunner?=	../../wip/xulrunner
 .endif	# XULRUNNER_BUILDLINK3_MK
 
