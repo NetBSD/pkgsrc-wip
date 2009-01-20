b07120a776500da6fce6b54cedea31b79352193f
Piotr Meyer <aniou+pkgsrc@smutek.pl>
2009-01-20 20:18:42 +0000

    Number of file descriptors should correspond (N*2?) with worker_connections. Changes: - note in nginx.conf - start_precmd with ulimit in startup script

--- nginx/files/nginx.sh
+++ nginx/files/nginx.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: nginx.sh,v 1.3 2008/05/06 11:32:22 rootcho Exp $
+# $NetBSD: nginx.sh,v 1.4 2009/01/20 20:18:42 anielski Exp $
 #
 # PROVIDE: nginx
 # REQUIRE: DAEMON
@@ -12,6 +12,7 @@ rcvar=${name}
 command="@PREFIX@/sbin/${name}"
 required_files="@PKG_SYSCONFDIR@/${name}.conf"
 pidfile="@VARBASE@/run/${name}.pid"
+start_precmd="ulimit -n 2048"
 
 load_rc_config $name
 run_rc_command "$1"
