8ca77f56f575ae440e30131bc44c482376a682c8
Roy Marples <roy@marples.name>
2009-01-22 14:33:27 +0000

    Update to dhcpcd-4.99.10
    New features include:
     * works on >1 interface
     * detects interface addition/removal

--- dhcpcd/files/dhcpcd.sh
+++ dhcpcd/files/dhcpcd.sh
@@ -4,30 +4,12 @@
 # REQUIRE: network mountcritlocal
 # BEFORE:  NETWORKING
 
-if [ -f /etc/rc.subr ]; then
-        . /etc/rc.subr
-fi
+$_rc_subr_loaded . /etc/rc.subr
 
 name="dhcpcd"
-rcvar="${name}"
-ifn="$2"
+rcvar=$name
 command="@PREFIX@/sbin/${name}"
-command_args="${ifn}"
-pidfile="@VARBASE@/run/${name}-${ifn}.pid"
-start_precmd="dhcpcd_precmd"
+pidfile="@VARBASE@/run/${name}.pid"
 
-dhcpcd_precmd()
-{
-	# Override for $ifn specific flags (see rc.subr for $flags setting)
-	eval ifn_flags=\$${rcvar}_flags_${ifn}
-	if [ -z "${flags}" -a -n "${ifn_flags}" ]; then
-		rc_flags=${ifn_flags}
-	fi
-
-	# dhcpcd may need local binaries
-	PATH=${PATH}:@PREFIX@/sbin
-	export PATH
-}
-
-load_rc_config $rcvar
+load_rc_config $name
 run_rc_command "$1"
