cd48ac02a11eba1e6baf25ef7fc5ad8ac59528f2
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-01-30 14:45:11 +0000

    Wrong package name "regex-base" in the previous revision. Correct: "hs-regex-base"

--- hs-regex-base/buildlink3.mk
+++ hs-regex-base/buildlink3.mk
@@ -1,21 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/01/30 14:38:32 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/01/30 14:47:25 phonohawk Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
-REGEX_BASE_BUILDLINK3_MK:=	${REGEX_BASE_BUILDLINK3_MK}+
+HS_REGEX_BASE_BUILDLINK3_MK:=	${HS_REGEX_BASE_BUILDLINK3_MK}+
 
 .if ${BUILDLINK_DEPTH} == "+"
-BUILDLINK_DEPENDS+=	regex-base
+BUILDLINK_DEPENDS+=	hs-regex-base
 .endif
 
-BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nregex-base}
-BUILDLINK_PACKAGES+=	regex-base
-BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}regex-base
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nhs-regex-base}
+BUILDLINK_PACKAGES+=	hs-regex-base
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}hs-regex-base
 
-.if ${REGEX_BASE_BUILDLINK3_MK} == "+"
-BUILDLINK_DEPMETHOD.regex-base?=	build
-BUILDLINK_API_DEPENDS.regex-base+=	regex-base>=0.93.1
-BUILDLINK_PKGSRCDIR.regex-base?=	../../wip/hs-regex-base
-.endif	# REGEX_BASE_BUILDLINK3_MK
+.if ${HS_REGEX_BASE_BUILDLINK3_MK} == "+"
+BUILDLINK_DEPMETHOD.hs-regex-base?=	build
+BUILDLINK_API_DEPENDS.hs-regex-base+=	hs-regex-base>=0.93.1
+BUILDLINK_PKGSRCDIR.hs-regex-base?=	../../wip/hs-regex-base
+.endif	# HS_REGEX_BASE_BUILDLINK3_MK
 
 .include "../../wip/hs-mtl/buildlink3.mk"
 
