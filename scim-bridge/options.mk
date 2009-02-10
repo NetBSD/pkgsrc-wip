587567864ba532194b49fcafbee229cc61e74ed1
OBATA Akio <obache@users.sourceforge.net>
2009-02-10 12:20:49 +0000

    Import scim-bridge-0.4.15.2 as wip/scim-bridge.
    Scim-bridge is wrapper library for SCIM, written in C. It is firstly written to
    solve problems occured by the ABI transition of GNU libstdc++ between 3.* branch
    and 4.* branch. That's not the problem in the most of cases because you usually
    used the same compiler for SCIM and GUI apps.

---
+++ scim-bridge/options.mk
@@ -0,0 +1,34 @@
+# $NetBSD: options.mk,v 1.1.1.1 2009/02/10 12:20:49 obache Exp $
+#
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.scim-bridge
+PKG_SUPPORTED_OPTIONS=	gtk qt
+PKG_SUGGESTED_OPTIONS=	gtk
+
+PLIST_VARS=	gtk qt
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgtk)
+.include "../../x11/gtk2/modules.mk"
+CONFIGURE_ARGS+=	--enable-gtk2-immodule
+PLIST.gtk=		yes
+.else
+CONFIGURE_ARGS+=	--disable-gtk2-immodule
+.endif
+
+#.if !empty(PKG_OPTIONS:Mqt3)
+#.include "../../x11/qt3-libs/buildlink3.mk"
+#CONFIGURE_ARGS+=	--enable-qt3-immodule
+#PLIST.qt=		yes
+#.else
+#CONFIGURE_ARGS+=	--disable-qt3-immodule
+#.endif
+
+.if !empty(PKG_OPTIONS:Mqt)
+.include "../../x11/qt4-libs/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-qt4-immodule
+PLIST.qt=		yes
+.else
+CONFIGURE_ARGS+=	--disable-qt4-immodule
+.endif
