5b5d69b9a4a468cb4972d2d38ee677e35ca7274c
Dieter Baron <dillo@nih.at>
2009-02-17 15:02:04 +0000

    Add option for new SCI engine (taken from FreeSCI). The cabal is complete now.

--- scummvm-nightly/options.mk
+++ scummvm-nightly/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.14 2009/01/02 23:31:56 dillo Exp $
+# $NetBSD: options.mk,v 1.15 2009/02/17 15:02:04 dillo Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scummvm-nightly
 PKG_SUPPORTED_OPTIONS=	scummvm-nightly-cruise
@@ -6,6 +6,7 @@ PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-igor
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-m4
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-made
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-saga2
+PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-sci
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-tinsel
 PKG_SUGGESTED_OPTIONS=	${PKG_SUPPORTED_OPTIONS}
 
@@ -25,6 +26,9 @@ CONFIGURE_ARGS+=--enable-made
 .endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-saga2)
 CONFIGURE_ARGS+=--enable-saga2
+.endif
+.if !empty(PKG_OPTIONS:Mscummvm-nightly-sci)
+CONFIGURE_ARGS+=--enable-sci
 .endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-tinsel)
 CONFIGURE_ARGS+=--enable-tinsel
