2d79ff8ea3ff437423a3a8edd4ef72b4ef93b69b
Eric Schnoebelen <schnoebe@NetBSD.org>
2009-02-17 00:46:43 +0000

    Oops, forgot to update the start up script and the DESCR as part of the import.

--- py-jabber-punjab/files/punjab.sh
+++ py-jabber-punjab/files/punjab.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: punjab.sh,v 1.1.1.1 2009/02/15 04:09:54 schnoebe Exp $
+# $NetBSD: punjab.sh,v 1.2 2009/02/17 00:46:43 schnoebe Exp $
 #
 #   startup script for the proxy65 file transfer proxy service for Jabber
 #   uses the twistd application engine
@@ -10,38 +10,38 @@
 #
 #
 #	Additional variables
-#	    proxy65_jid		[Jabber ID of the service, default proxy65]
-#	    proxy65_secret	[shared secret with the S2S]
-#	    proxy65_rhost	[IP/Host of the Jabber service router]
-#	    proxy65_rport	[port of the Jabber service router, def 6000]
-#	    proxy65_ips		[proxy address:ports to be advertised/used]
+#	    punjab_jid		[Jabber ID of the service, default proxy65]
+#	    punjab_secret	[shared secret with the S2S]
+#	    punjab_rhost	[IP/Host of the Jabber service router]
+#	    punjab_rport	[port of the Jabber service router, def 6000]
+#	    punjab_ips		[proxy address:ports to be advertised/used]
 #				[default :7777]
 #
 #	ex:
-#	    proxy65_jid=proxy.jabber.example.org
-#	    proxy65_secret=JabberSecret
-#	    proxy65_rhost=jabber.example.org
-#	    proxy65_ips=172.18.223.20:8010
+#	    punjab_jid=proxy.jabber.example.org
+#	    punjab_secret=JabberSecret
+#	    punjab_rhost=jabber.example.org
+#	    punjab_ips=172.18.223.20:8010
 #
 
-name="proxy65"
+name="punjab"
 rcvar=$name
 command=@PREFIX@/bin/twistd
 command_interpreter="@PYTHONBIN@"
 pidfile="@JABBER_PIDDIR@/${name}.pid"
 logfile="@JABBER_LOGDIR@/${name}.log"
 command_args="-u @JABBER_USER@ --pidfile=${pidfile} -l ${logfile}"
-command_args="${command_args} proxy65"
+command_args="${command_args} punjab"
 
 start_precmd="ensure_piddir && merge_args"
 
 merge_args()
 {
-command_args="${command_args} ${proxy65_jid:+--jid=${proxy65_jid}}"
-command_args="${command_args} ${proxy65_secret:+--secret=${proxy65_secret}}"
-command_args="${command_args} ${proxy65_rhost:+--rhost=${proxy65_rhost}}"
-command_args="${command_args} ${proxy65_rport:+--rport=${proxy65_rport}}"
-command_args="${command_args} ${proxy65_ips:+--proxyips=${proxy65_ips}}"
+command_args="${command_args} ${punjab_jid:+--jid=${punjab_jid}}"
+command_args="${command_args} ${punjab_secret:+--secret=${punjab_secret}}"
+command_args="${command_args} ${punjab_rhost:+--rhost=${punjab_rhost}}"
+command_args="${command_args} ${punjab_rport:+--rport=${punjab_rport}}"
+command_args="${command_args} ${punjab_ips:+--proxyips=${punjab_ips}}"
 }
 
 ensure_piddir()
@@ -57,5 +57,5 @@ if [ -f /etc/rc.subr ]; then
 else
 	@ECHO@ -n " ${name}"
 	ensure_piddir
-	${command} ${command_args} ${pyaimt_flags} 
+	${command} ${command_args} ${punjab_flags} 
 fi
