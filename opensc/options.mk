d2a39ece4a234196d3852f0b48f6980b129a9f0e
Hasso Tepper <htepper@users.sourceforge.net>
2009-02-22 19:30:13 +0000

    Fix the pcsc provider path.

--- opensc/options.mk
+++ opensc/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.6 2009/02/18 10:16:04 htepper Exp $
+# $NetBSD: options.mk,v 1.7 2009/02/22 19:30:13 htepper Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.opensc
 PKG_SUPPORTED_OPTIONS=	openct pcsc-lite
@@ -9,7 +9,7 @@ PKG_SUGGESTED_OPTIONS=	pcsc-lite
 .if !empty(PKG_OPTIONS:Mpcsc-lite)
 .include "../../security/pcsc-lite/buildlink3.mk"
 CONFIGURE_ARGS+=	--enable-pcsc
-CONFIGURE_ARGS+=	--with-pcsc-provider=${PREFIX}/lib/libpcsclite.so
+CONFIGURE_ARGS+=	--with-pcsc-provider=${BUILDLINK_PREFIX.pcsc-lite}/lib/libpcsclite.la
 .else
 CONFIGURE_ARGS+=	--disable-pcsc
 .endif
