ce024514f5df4e7affa3247c68999346838f6cfb
Hasso Tepper <htepper@users.sourceforge.net>
2009-02-23 08:26:45 +0000

    Split up the package. Various fixes mainly from OpenBSD and FreeBSD ports. Bump PKGREVISION.

--- akode/buildlink3.mk
+++ akode/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2008/12/05 14:03:53 htepper Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/02/23 08:26:45 htepper Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 AKODE_BUILDLINK3_MK:=	${AKODE_BUILDLINK3_MK}+
@@ -16,9 +16,6 @@ BUILDLINK_API_DEPENDS.akode+=	akode>=2.0.2
 BUILDLINK_PKGSRCDIR.akode?=	../../wip/akode
 .endif	# AKODE_BUILDLINK3_MK
 
-.include "../../audio/flac/buildlink3.mk"
-.include "../../audio/libmad/buildlink3.mk"
-.include "../../audio/libvorbis/buildlink3.mk"
 .include "../../devel/libltdl/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
