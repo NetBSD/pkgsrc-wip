f3ded46720e873dbf2ed17750d46b4c2cd682dd1
Petar Bogdanovic <petar@smokva.net>
2009-02-25 12:31:54 +0000

    Revamp/simplify the package:
       - Remove all options except dccm.
       - Override do-install, remove patch-aa.
       - Use SPECIAL_PERMS, add user-destdir support.
    
    
    Add note from author:
    
       On Thu, Jan 29, 2009 at 06:27:03PM +0000,
       Vernon Schryver wrote:
       >
       > There should be no patches in whatever
       > you distribute. Updatedcc must work.

--- dcc/options.mk
+++ dcc/options.mk
@@ -1,46 +1,22 @@
-# $NetBSD: options.mk,v 1.2 2008/12/30 15:05:21 pebog Exp $
+# $NetBSD: options.mk,v 1.3 2009/02/25 12:31:54 pebog Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dcc
-PKG_SUPPORTED_OPTIONS=	dccd dccifd dccm
-PKG_SUGGESTED_OPTIONS=	dccd dccifd
+PKG_SUPPORTED_OPTIONS=	dccm
+PKG_SUGGESTED_OPTIONS=	
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS=		dccd dccifd dccm dns-helper
+PLIST_VARS=		dccm
 
 ###
-### Don't build dccd, the DCC server.
-###
-.if empty(PKG_OPTIONS:Mdccd)
-CONFIGURE_ARGS+=	--disable-server
-.else
-CONF_FILES+=		${DCC_EGDIR}/flod ${DCC_HOME}/flod \
-			${DCC_EGDIR}/grey_flod ${DCC_HOME}/grey_flod \
-			${DCC_EGDIR}/whitelist ${DCC_HOME}/whitelist \
-			${DCC_EGDIR}/grey_whitelist ${DCC_HOME}/grey_whitelist
-PLIST.dccd=		yes
-RCD_SCRIPTS+=		dccd
-.endif
-
-###
-### Don't build dccifd, the DCC program interface.
-###
-.if empty(PKG_OPTIONS:Mdccifd)
-CONFIGURE_ARGS+=	--disable-dccifd
-.else
-PLIST.dccifd=		yes
-PLIST.dns-helper=	yes
-RCD_SCRIPTS+=		dccifd
-.endif
-
-###
-### Don't build dccm, the DCC milter.
+### Enable dccm, the DCC milter interface.
 ###
 .if empty(PKG_OPTIONS:Mdccm)
 CONFIGURE_ARGS+=	--disable-dccm
 .else
-.include "../../mail/libmilter/buildlink3.mk"
-PLIST.dccm=		yes
-PLIST.dns-helper=	yes
 RCD_SCRIPTS+=		dccm
+PLIST.dccm=		yes
+dccm-install:
+	${INSTALL_PROGRAM} ${WRKSRC}/dccm/dccm ${DESTDIR}${PREFIX}/libexec/
+.include "../../mail/libmilter/buildlink3.mk"
 .endif
