b291875133d806cf1994a07259f10d4e3f499dbb
Dieter Baron <dillo@nih.at>
2009-03-08 10:27:10 +0000

    MADE engine is enabled by default. Add option for new LOL sub-engine.

--- scummvm-nightly/options.mk
+++ scummvm-nightly/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.15 2009/02/17 15:02:04 dillo Exp $
+# $NetBSD: options.mk,v 1.16 2009/03/08 10:27:10 dillo Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scummvm-nightly
 PKG_SUPPORTED_OPTIONS=	scummvm-nightly-cruise
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-igor
+PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-lol
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-m4
-PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-made
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-saga2
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-sci
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-tinsel
@@ -18,11 +18,11 @@ CONFIGURE_ARGS+=--enable-cruise
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-igor)
 CONFIGURE_ARGS+=--enable-igor
 .endif
+.if !empty(PKG_OPTIONS:Mscummvm-nightly-lol)
+CONFIGURE_ARGS+=--enable-lol
+.endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-m4)
 CONFIGURE_ARGS+=--enable-m4
-.endif
-.if !empty(PKG_OPTIONS:Mscummvm-nightly-made)
-CONFIGURE_ARGS+=--enable-made
 .endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-saga2)
 CONFIGURE_ARGS+=--enable-saga2
