5cf954952564af4f6e4bd7f1248d4df35ea3aabf
dieter roelants <dieter@bsdusr.net>
2009-03-16 18:29:17 +0000

    Change buildlink pkgsrc dir to wip.

--- tango-icon-theme/buildlink3.mk
+++ tango-icon-theme/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/03/16 18:25:07 roelants Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/03/16 18:29:17 roelants Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 TANGO_ICON_THEME_BUILDLINK3_MK:=	${TANGO_ICON_THEME_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}tango-icon-theme
 
 .if ${TANGO_ICON_THEME_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.tango-icon-theme+=	tango-icon-theme>=0.8.90
-BUILDLINK_PKGSRCDIR.tango-icon-theme?=	../../graphics/tango-icon-theme
+BUILDLINK_PKGSRCDIR.tango-icon-theme?=	../../wip/tango-icon-theme
 .endif	# TANGO_ICON_THEME_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH:S/+$//}
