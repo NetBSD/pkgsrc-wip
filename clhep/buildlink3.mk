2c60ea3a0dff1f01bb0713fbc3371bced4f4eab3
Jörg Sonnenberger <joerg@NetBSD.org>
2009-03-22 17:13:05 +0000

    Fix up from old version without include guard comment.

--- clhep/buildlink3.mk
+++ clhep/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2009/03/20 19:43:40 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2009/03/22 17:13:05 jsonn Exp $
 
 BUILDLINK_TREE+=	clhep
 
@@ -8,7 +8,6 @@ CLHEP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.clhep+=	clhep>=1.9.3.0
 BUILDLINK_PKGSRCDIR.clhep?=	../../wip/clhep
 BUILDLINK_DEPMETHOD.clhep?=	build
-.endif
 .endif # CLHEP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-clhep
