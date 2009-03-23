66742d6a71d946a12f64edf219d8d13d66b540dd
Michael Stapelberg <mstapelberg@users.sourceforge.net>
2009-03-23 11:50:29 +0000

    Forgot buildlink3.mk

--- libxcb/buildlink3.mk
+++ libxcb/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2009/03/20 19:43:49 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2009/03/23 11:51:01 mstapelberg Exp $
 
 BUILDLINK_TREE+=	libxcb
 
 .if !defined(LIBXCB_BUILDLINK3_MK)
 LIBXCB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxcb+=	libxcb>=1.1nb2
+BUILDLINK_API_DEPENDS.libxcb+=	libxcb>=1.1.93
 BUILDLINK_PKGSRCDIR.libxcb?=	../../wip/libxcb
 
 .include "../../wip/xcb-proto/buildlink3.mk"
