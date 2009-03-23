ce82b514e546b1df37854592297931ec69adfbbe
Michael Stapelberg <mstapelberg@users.sourceforge.net>
2009-03-23 12:59:13 +0000

    Update buildlink3.mk for the new format

--- libxcb/buildlink3.mk
+++ libxcb/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/03/23 11:51:01 mstapelberg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2009/03/23 12:59:13 mstapelberg Exp $
 
 BUILDLINK_TREE+=	libxcb
 
 .if !defined(LIBXCB_BUILDLINK3_MK)
 LIBXCB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxcb+=	libxcb>=1.1.93
+BUILDLINK_API_DEPENDS.libxcb?=	libxcb>=1.1.93
 BUILDLINK_PKGSRCDIR.libxcb?=	../../wip/libxcb
 
 .include "../../wip/xcb-proto/buildlink3.mk"
--- xcb-proto/buildlink3.mk
+++ xcb-proto/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2009/03/23 11:50:29 mstapelberg Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2009/03/23 13:00:00 mstapelberg Exp $
 
 BUILDLINK_TREE+=	xcb-proto
 
 .if !defined(XCB_PROTO_BUILDLINK3_MK)
 XCB_PROTO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xcb-proto+=	xcb-proto>=1.4
+BUILDLINK_API_DEPENDS.xcb-proto?=	xcb-proto>=1.4
 BUILDLINK_PKGSRCDIR.xcb-proto?=	../../wip/xcb-proto
 BUILDLINK_DEPMETHOD.xcb-proto?=	build
 .endif # XCB_PROTO_BUILDLINK3_MK
