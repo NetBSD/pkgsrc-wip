753e2457142edadc8e6a7a53d1d1194188dfd0aa
Michael Stapelberg <mstapelberg@users.sourceforge.net>
2009-03-23 12:55:55 +0000

    Update buildlink3.mk to use the new format

--- xcb-util/buildlink3.mk
+++ xcb-util/buildlink3.mk
@@ -1,12 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/23 11:55:49 mstapelberg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/03/23 12:55:55 mstapelberg Exp $
 
 BUILDLINK_TREE+=	xcb-util
 
 .if !defined(XCB_UTIL_BUILDLINK3_MK)
 XCB_UTIL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xcb-util+=	xcb-util>=0.3.3
-BUILDLINK_PKGSRCDIR.xcb-util?=	../../wip/xcb-util
+BUILDLINK_API_DEPENDS.xcb-util?=	xcb-util>=0.3.3
+BUILDLINK_PKGSRCDIR.xcb-util?=		../../wip/xcb-util
+BUILDLINK_DEPMETHOD.xcb-utl?=		build
 
 .include "../../wip/libxcb/buildlink3.mk"
 .endif # XCB_UTIL_BUILDLINK3_MK
