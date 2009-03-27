41c3df015ec5573c7f5211c113698a914fe271dc
Thomas Klausner <wiz@NetBSD.org>
2009-03-27 13:22:05 +0000

    Fix broken bl3.mk file.

--- libuuid/buildlink3.mk
+++ libuuid/buildlink3.mk
@@ -1,6 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/03/20 19:43:49 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/03/27 13:22:05 thomasklausner Exp $
 
 BUILDLINK_TREE+=	libuuid
+
+.if !defined(LIBUUID_BUILDLINK3_MK)
+LIBUUID_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libuuid+=	libuuid>=1.36
 BUILDLINK_PKGSRCDIR.libuuid?=	../../wip/libuuid
