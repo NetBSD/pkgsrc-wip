068476ad450bf4e128c47114725eb5c137436fd2
Thomas Klausner <wiz@NetBSD.org>
2009-03-27 10:57:56 +0000

    Fix, was broken during new-world conversion.

--- p2kmoto/buildlink3.mk
+++ p2kmoto/buildlink3.mk
@@ -1,6 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:43:51 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/03/27 10:57:56 thomasklausner Exp $
 
 BUILDLINK_TREE+=	p2kmoto
+
+.if !defined(P2KMOTO_BUILDLINK3_MK)
+P2KMOTO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.p2kmoto+=	p2kmoto>=0.1rc1
 BUILDLINK_PKGSRCDIR.p2kmoto?=	../../wip/p2kmoto
