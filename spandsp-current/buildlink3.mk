67c504fdec8322f29fe4fd591ee8c31a8713f31b
X400 <x400@users.sourceforge.net>
2009-04-02 09:08:49 +0000

    *** empty log message ***

--- spandsp-current/buildlink3.mk
+++ spandsp-current/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:43:52 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/04/02 09:08:50 x400 Exp $
 
 BUILDLINK_TREE+=	spandsp
 
 .if !defined(SPANDSP_BUILDLINK3_MK)
 SPANDSP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.spandsp+=	spandsp>=0.0.6pre2
+BUILDLINK_API_DEPENDS.spandsp+=	spandsp>=0.0.6pre7
 BUILDLINK_PKGSRCDIR.spandsp?=	../../wip/spandsp-current
 
 .include "../../graphics/tiff/buildlink3.mk"
