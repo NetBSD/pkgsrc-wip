307df34f89c3010bf6b5a77993fe29584853f9d3
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2009-04-11 23:24:19 +0000

    Change syntaxe conforme buildlink3.mk python style

--- py-webkit/buildlink3.mk
+++ py-webkit/buildlink3.mk
@@ -1,16 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/04/04 14:48:17 jihbed Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/04/11 23:24:19 jihbed Exp $
 #
 
-BUILDLINK_TREE+=	py25-webkitgtk
+BUILDLINK_TREE+=	pywebkitgtk
 
-.if !defined(PY25_WEBKITGTK_BUILDLINK3_MK)
-PY25_WEBKITGTK_BUILDLINK3_MK:=
+.if !defined(PY_WEBKITGTK_BUILDLINK3_MK)
+PY_WEBKITGTK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.py25-webkitgtk+=	py25-webkitgtk>=1.1.4
-BUILDLINK_PKGSRCDIR.py25-webkitgtk?=	../../category/py-webkit
+.include "../../lang/python/pyversion.mk"
+
+BUILDLINK_API_DEPENDS.pywebkitgtk+=	${PYPKGPREFIX}-webkitgtk>=1.1.4
+BUILDLINK_PKGSRCDIR.pywebkitgtk?=	../../wip/py-webkit
 
 .include "../../x11/py-gtk2/buildlink3.mk"
 .include "../../www/webkit-gtk/buildlink3.mk"
-.endif	# PY25_WEBKITGTK_BUILDLINK3_MK
+.endif	# PY_WEBKITGTK_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-py25-webkitgtk
+BUILDLINK_TREE+=	-py-webkitgtk
