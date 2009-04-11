1f27d9d8f7b112e4a7333814b1d1e1cb432ca190
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2009-04-11 12:50:04 +0000

    Import libiptcdata-1.0.3 as wip/libipctdata.
    Libiptcdata is a library, for manipulating the International Press
    Telecommunications Council (IPTC) metadata tored within multimedia
    files such as images. The library provides routines for parsing
    viewing, modifying, and saving this metadata.

---
+++ libipctdata/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/04/11 12:50:04 jihbed Exp $
+
+BUILDLINK_TREE+=	libiptcdata
+
+.if !defined(LIBIPTCDATA_BUILDLINK3_MK)
+LIBIPTCDATA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libiptcdata+=	libiptcdata>=1.0.3
+BUILDLINK_PKGSRCDIR.libiptcdata?=	../../wip/libipctdata
+
+
+.endif	# LIBIPTCDATA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libiptcdata
