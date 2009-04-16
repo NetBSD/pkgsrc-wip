9b6399245e3b0d1ff5786f6b8af0e0fa43dbed49
Thomas Klausner <wiz@NetBSD.org>
2009-04-16 21:42:51 +0000

    neon update to 0.28.x changed ABI, so do a recursive bump, PKGREVISION++.
    Noted by obache@ on pkgsrc-changes@

--- HsSVN/buildlink3.mk
+++ HsSVN/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:43:39 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2009/04/16 21:42:51 thomasklausner Exp $
 
 BUILDLINK_TREE+=	HsSVN
 
@@ -7,6 +7,7 @@ HSSVN_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.HsSVN?=	build
 BUILDLINK_API_DEPENDS.HsSVN+=	HsSVN>=0.4
+BUILDLINK_ABI_DEPENDS.HsSVN?=	HsSVN>=0.4
 BUILDLINK_PKGSRCDIR.HsSVN?=	../../wip/HsSVN
 
 .include "../../wip/hs-mtl/buildlink3.mk"
