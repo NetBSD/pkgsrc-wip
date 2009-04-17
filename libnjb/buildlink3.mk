6ee73bb557b66cf29471a446a5b46de41c845298
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2009-04-17 18:01:36 +0000

    o New version 2.2.5 to 2.2.6 	o Remove patch-aa, replace by CHECK_PORTABILITY_SKIP

--- libnjb/buildlink3.mk
+++ libnjb/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:43:48 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/04/17 18:01:36 jihbed Exp $
 
 BUILDLINK_TREE+=	libnjb
 
 .if !defined(LIBNJB_BUILDLINK3_MK)
 LIBNJB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libnjb+=	libnjb>=2.2.5
+BUILDLINK_API_DEPENDS.libnjb+=	libnjb>=2.2.6
 BUILDLINK_PKGSRCDIR.libnjb?=	../../wip/libnjb
 
 .include "../../devel/libusb/buildlink3.mk"
