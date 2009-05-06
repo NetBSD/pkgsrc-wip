d2f812d3b8be0829b383db19bde1fcc8e98c63d0
Aleksey Cheusov <cheusov@users.sourceforge.net>
2009-05-06 10:20:00 +0000

    fixed: 1 open conditional

--- libtlen2/buildlink3.mk
+++ libtlen2/buildlink3.mk
@@ -1,6 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:43:49 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/05/06 10:21:03 cheusov Exp $
 
 BUILDLINK_TREE+=	libtlen2
+
+.if !defined(LIBTLEN2_BUILDLINK3_MK)
+LIBTLEN2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libtlen2+=	libtlen2>=20040301
 BUILDLINK_PKGSRCDIR.libtlen2?=	../../wip/libtlen2
