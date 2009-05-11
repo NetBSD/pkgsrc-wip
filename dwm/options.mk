a63f4162d75b412c228ec102c14eaca5ab0615aa
Leonardo Taccari <leot@NetBSD.org>
2009-05-11 19:21:08 +0000

    Fixed compilation issues with PKG_OPTIONS.dwm (it was possible to correctly build dwm-5.5 only with "xinerama" option). A big thank you to Thomas Klausner who reported the issue.

--- dwm/options.mk
+++ dwm/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2009/02/08 16:52:48 leot1990 Exp $
+# $NetBSD: options.mk,v 1.4 2009/05/11 19:21:08 leot1990 Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.dwm
 PKG_SUPPORTED_OPTIONS=		xinerama
@@ -8,12 +8,12 @@ PKG_SUPPORTED_OPTIONS=		xinerama
 # If we don't want the Xinerama support we delete XINERAMALIBS and
 # XINERAMAFLAGS lines, otherwise the Xinerama support is the default.
 .if !empty(PKG_OPTIONS:Mxinerama)
+.include "../../x11/libXinerama/buildlink3.mk"
+.else
 SUBST_CLASSES+=		options
 SUBST_STAGE.options=	pre-build
 SUBST_MESSAGE.options=	Toggle the Xinerama support
 SUBST_FILES.options=	config.mk
-SUBST_SED.options+=	-e '/XINERAMA/ s/^\#//'
-.include "../../x11/libXinerama/buildlink3.mk"
-.else
+SUBST_SED.options+=	-e 's/^XINERAMA/\#&/'
 .include "../../x11/libX11/buildlink3.mk"
 .endif
