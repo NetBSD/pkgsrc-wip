635b434ec725984e5e61a760c8a8626af3c46bae
Emile `iMil' Heitor <imil@home.imil.net>
2009-05-23 19:08:29 +0000

    updated to 20090523. Fixed package exact match, break on non-available package. Added strsep.c for SunOS.

--- pkgin/options.mk
+++ pkgin/options.mk
@@ -1,15 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2009/05/21 15:57:51 imilh Exp $
+# $NetBSD: options.mk,v 1.2 2009/05/23 19:08:29 imilh Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pkgin
-PKG_SUPPORTED_OPTIONS=	gzip-summary bzip2-summary
-PKG_SUGGESTED_OPTIONS=	bzip2-summary
+PKG_SUPPORTED_OPTIONS=	gzip-summary
+PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
 
+# by default, pkg_summary extention is set to bz2
 .if !empty(PKG_OPTIONS:Mgzip-summary)
 CPPFLAGS+=	-DSUMEXT=\"gz\"
-.endif
-
-.if !empty(PKG_OPTIONS:Mbzip2-summary)
-CPPFLAGS+=	-DSUMEXT=\"bz2\"
 .endif
