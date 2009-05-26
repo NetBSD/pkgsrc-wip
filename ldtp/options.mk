fbe7aa2b141e6f4c21d91230458f3f1234be4f66
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2009-05-26 09:40:20 +0000

    o Update version 1.5.1 to 1.6.0 	o Removed: the two options removed in options.mk (py-imaging and py-statgrab) 	o Modified: written new options.mk insert two option (pythonmodules and localization) 	o action: actived pythonmodules options 	o action: disabled localization option because no fixed complication problem

--- ldtp/options.mk
+++ ldtp/options.mk
@@ -1,13 +1,31 @@
-# $NetBSD: options.mk,v 1.1.1.1 2009/05/25 15:26:09 jihbed Exp $
+# $NetBSD: options.mk,v 1.2 2009/05/26 09:40:20 jihbed Exp $
+
+.include "../../mk/bsd.prefs.mk"
 
 PKG_OPTIONS_VAR =	PKG_OPTIONS.ldtp
-PKG_SUPPORTED_OPTIONS=	pystatgrab pyimaging
-PKG_SUGGESTED_OPTIONS=	pyimaging
+PKG_SUPPORTED_OPTIONS=	pythonmodules localization
+PKG_SUGGESTED_OPTIONS=	pythonmodules
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	${PKG_SUPPORTED_OPTIONS}
+.if !empty(PKG_OPTIONS:Mpythonmodules)
+CONFIGURE_ARGS+=	--with-pythonmodules
+. include "../../lang/python/extension.mk"
+PLIST_SUBST+=			PYSITELIB=${PYSITELIB}
+.  include "../../devel/libglade/buildlink3.mk"
+.  include "../../devel/py-at-spi/buildlink3.mk"
+.  include "../../devel/py-gobject/buildlink3.mk"
+.  include "../../net/py-ORBit/buildlink3.mk"
+.  include "../../x11/py-gnome2/buildlink3.mk"
+.  include "../../x11/py-gnome2-desktop/buildlink3.mk"
+.  include "../../x11/py-gtk2/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-pythonmodules
+.endif
 
-.if !empty(PKG_OPTIONS:Mpyimaging)
-.include "../../graphics/py-imaging/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mlocalization)
+CONFIGURE_ARGS+=	--enable-localization
+DEPENDS+=	gettext-tools-[0-9]*:../../devel/gettext-tools
+.else
+CONFIGURE_ARGS+=	--enable-localization=no
 .endif
