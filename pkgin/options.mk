0002e1b5443b271f44fba5884ef26c061d6d4011
Emile `iMil' Heitor <imil@home.imil.net>
2009-05-29 13:31:38 +0000

    updated to 20090529. Alphabetically ordered lists, package status tagging when searching.

--- pkgin/options.mk
+++ pkgin/options.mk
@@ -1,8 +1,7 @@
-# $NetBSD: options.mk,v 1.2 2009/05/23 19:08:29 imilh Exp $
+# $NetBSD: options.mk,v 1.3 2009/05/29 13:31:38 imilh Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pkgin
 PKG_SUPPORTED_OPTIONS=	gzip-summary
-PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
 
