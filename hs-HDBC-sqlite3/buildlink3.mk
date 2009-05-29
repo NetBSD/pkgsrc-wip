d41b6538b2d9ae22b5395b5faf55a43fb53c03fe
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-05-29 05:50:22 +0000

    Import hs-HDBC-sqlite3-2.1.0.0 as wip/hs-HDBC-sqlite3.
    This is the Sqlite v3 driver for HDBC, the generic database access
    system for Haskell.

---
+++ hs-HDBC-sqlite3/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/05/29 05:50:22 phonohawk Exp $
+
+BUILDLINK_TREE+=	hs-HDBC-sqlite3
+
+.if !defined(HS_HDBC_SQLITE3_BUILDLINK3_MK)
+HS_HDBC_SQLITE3_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-HDBC-sqlite3+=	hs-HDBC-sqlite3>=2.1.0.0
+BUILDLINK_PKGSRCDIR.hs-HDBC-sqlite3?=	../../wip/hs-HDBC-sqlite3
+
+.include "../../wip/hs-convertible/buildlink3.mk"
+.include "../../wip/hs-HDBC/buildlink3.mk"
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-time/buildlink3.mk"
+.include "../../wip/hs-utf8-string/buildlink3.mk"
+.include "../../databases/sqlite3/buildlink3.mk"
+.endif	# HS_HDBC_SQLITE3_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-HDBC-sqlite3
