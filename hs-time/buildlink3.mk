6205703ac6cdd3da9ed4692a764d79c702be64cb
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-05-29 04:44:52 +0000

    Upstream update to time-1.1.2.4

--- hs-time/buildlink3.mk
+++ hs-time/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:43:43 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/05/29 04:44:52 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-time
 
 .if !defined(HS_TIME_BUILDLINK3_MK)
 HS_TIME_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.hs-time?=	build
-BUILDLINK_API_DEPENDS.hs-time+=	hs-time>=1.1.2.2
+BUILDLINK_API_DEPENDS.hs-time+=	hs-time>=1.1.2.4
 BUILDLINK_PKGSRCDIR.hs-time?=	../../wip/hs-time
-.endif # HS_TIME_BUILDLINK3_MK
+.endif	# HS_TIME_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-time
