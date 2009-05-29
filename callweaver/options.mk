c2fa215dc5054f6c99d9eff89066b1a93d11cee0
X400 <x400@users.sourceforge.net>
2009-05-29 12:58:35 +0000

    *** empty log message ***

--- callweaver/options.mk
+++ callweaver/options.mk
@@ -1,11 +1,11 @@
-# $NetBSD: options.mk,v 1.2 2008/12/18 09:11:19 x400 Exp $
+# $NetBSD: options.mk,v 1.3 2009/05/29 13:29:21 x400 Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.callweaver
-PKG_SUPPORTED_OPTIONS=	mysql pgsql
+PKG_SUPPORTED_OPTIONS=	mysql pgsql sipsecurity
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		mysql pgsql
+PLIST_VARS+=		mysql pgsql sipsecurity
 
 .if !empty(PKG_OPTIONS:Mmysql)
 .include "../../databases/mysql5-client/buildlink3.mk"
@@ -17,4 +17,9 @@ CONFIGURE_ARGS+=	--enable-mysql --with-app_sql_mysql --with-cdr_mysql --with-res
 .include "../../databases/postgresql83-client/buildlink3.mk"
 PLIST.pgsql=		yes
 CONFIGURE_ARGS+=	--enable-postgresql --with-app_sql_postgres --with-cdr_pgsql --with-res_config_pgsql
+.endif
+
+.if !empty(PKG_OPTIONS:Msipsecurity)
+PLIST.sipsecurity=	yes
+CONFIGURE_ARGS+=	--enable-sip-tcp-support --enable-srtp
 .endif
--- spandsp/buildlink3.mk
+++ spandsp/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/05/29 12:21:11 x400 Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/05/29 12:58:35 x400 Exp $
 
 BUILDLINK_TREE+=	spandsp
 
 .if !defined(SPANDSP_BUILDLINK3_MK)
 SPANDSP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.spandsp+=	spandsp>=0.0.6pre7
+BUILDLINK_API_DEPENDS.spandsp+=	spandsp>=0.0.6pre12
 BUILDLINK_PKGSRCDIR.spandsp?=	../../wip/spandsp
 
 .include "../../graphics/tiff/buildlink3.mk"
