189e2d8cdf16367f9322c30685cab3557adda5e2
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-06-02 07:09:41 +0000

    Follow the renaming of wip/parsec to wip/hs-parsec

--- hs-network/buildlink3.mk
+++ hs-network/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/05/23 07:31:23 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/06/02 07:09:41 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-network
 
@@ -8,7 +8,7 @@ HS_NETWORK_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=2.2.1.2
 BUILDLINK_PKGSRCDIR.hs-network?=	../../wip/hs-network
 
-.include "../../wip/parsec/buildlink3.mk"
+.include "../../wip/hs-parsec/buildlink3.mk"
 .endif	# HS_NETWORK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-network
