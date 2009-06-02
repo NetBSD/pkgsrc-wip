f849d88a4afc7d23e31d4a88d8d93e6bb2f427ec
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-06-02 07:51:55 +0000

    Upstream update to hs-curl-1.3.5

--- hs-curl/buildlink3.mk
+++ hs-curl/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:43:42 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/06/02 07:51:55 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-curl
 
 .if !defined(HS_CURL_BUILDLINK3_MK)
 HS_CURL_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.hs-curl?=	build
-BUILDLINK_API_DEPENDS.hs-curl+=	hs-curl>=1.3.4
+BUILDLINK_API_DEPENDS.hs-curl+=	hs-curl>=1.3.5
 BUILDLINK_PKGSRCDIR.hs-curl?=	../../wip/hs-curl
 
 .include "../../www/curl/buildlink3.mk"
-.endif # HS_CURL_BUILDLINK3_MK
+.endif	# HS_CURL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-curl
