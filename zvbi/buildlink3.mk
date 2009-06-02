7657ec8d09d2f4612e13812e68db5ffe0b2dd46d
ndb <noud4@users.sourceforge.net>
2009-06-02 12:47:26 +0000

    modular X11_TYPE added

--- zvbi/buildlink3.mk
+++ zvbi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/05/06 10:23:59 cheusov Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/06/02 12:47:26 noud4 Exp $
 
 BUILDLINK_TREE+=	zvbi
 
@@ -9,6 +9,9 @@ BUILDLINK_API_DEPENDS.zvbi+=	zvbi>=0.2.16
 BUILDLINK_PKGSRCDIR.zvbi?=	../../wip/zvbi
 
 .include "../../textproc/libunicode/buildlink3.mk"
+.if ${X11_TYPE} == "modular"
+.include "../../x11/libX11/buildlink3.mk"
+.endif # X11_TYPE
 .endif # ZVBI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-zvbi
