11f97c5e8db4f62f91538111ec3b0c985989d737
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-06-02 06:56:47 +0000

    I committed a wrong buildlink3.mk. Sorry

--- hs-tagsoup/buildlink3.mk
+++ hs-tagsoup/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/06/02 06:49:30 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/06/02 06:56:47 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-tagsoup
 
 .if !defined(HS_TAGSOUP_BUILDLINK3_MK)
 HS_TAGSOUP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.6nb1
+BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.6
 BUILDLINK_PKGSRCDIR.hs-tagsoup?=	../../wip/tagsoup
 
 .include "../../wip/hs-mtl/buildlink3.mk"
