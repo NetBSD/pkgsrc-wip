b8ca44b3fb6713bd4e50c432513fb92d4b6ab3e4
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-06-02 06:49:30 +0000

    Import hs-tagsoup-0.6 as wip/hs-tagsoup.
    TagSoup is a library for extracting information out of unstructured
    HTML code, sometimes known as tag-soup. The HTML does not have to be
    well formed, or render properly within any particular framework. This
    library is for situations where the author of the HTML is not
    cooperating with the person trying to extract the information, but is
    also not trying to hide the information.

---
+++ hs-tagsoup/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/06/02 06:49:30 phonohawk Exp $
+
+BUILDLINK_TREE+=	hs-tagsoup
+
+.if !defined(HS_TAGSOUP_BUILDLINK3_MK)
+HS_TAGSOUP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.6nb1
+BUILDLINK_PKGSRCDIR.hs-tagsoup?=	../../wip/tagsoup
+
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-network/buildlink3.mk"
+.endif	# HS_TAGSOUP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-tagsoup
