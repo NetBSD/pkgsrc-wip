20e2ec4841d07083f5d41e062d16c2744cd6eff5
ndb <noud4@users.sourceforge.net>
2009-06-02 14:39:03 +0000

    adjusted X11_TYPE

--- zvbi/buildlink3.mk
+++ zvbi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2009/06/02 12:47:26 noud4 Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2009/06/02 14:39:03 noud4 Exp $
 
 BUILDLINK_TREE+=	zvbi
 
@@ -9,8 +9,13 @@ BUILDLINK_API_DEPENDS.zvbi+=	zvbi>=0.2.16
 BUILDLINK_PKGSRCDIR.zvbi?=	../../wip/zvbi
 
 .include "../../textproc/libunicode/buildlink3.mk"
+
+.include "../../mk/bsd.fast.prefs.mk"
+
 .if ${X11_TYPE} == "modular"
 .include "../../x11/libX11/buildlink3.mk"
+.else
+.include "../../mk/x11.buildlink3.mk"
 .endif # X11_TYPE
 .endif # ZVBI_BUILDLINK3_MK
 
