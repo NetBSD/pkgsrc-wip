1c8b2b1e2d157aec9e9f8163f9870e0d58104c71
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-06-03 07:07:38 +0000

    Import hs-haskell-src-1.0.1.3 as wip/hs-haskell-src.
    Facilities for manipulating Haskell source code: an abstract syntax,
    lexer, parser and pretty-printer.

---
+++ hs-haskell-src/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/06/03 07:07:38 phonohawk Exp $
+
+BUILDLINK_TREE+=	hs-haskell-src
+
+.if !defined(HS_HASKELL_SRC_BUILDLINK3_MK)
+HS_HASKELL_SRC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-haskell-src+=	hs-haskell-src>=1.0.1.3
+BUILDLINK_PKGSRCDIR.hs-haskell-src?=	../../wip/hs-haskell-src
+.endif	# HS_HASKELL_SRC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-haskell-src
