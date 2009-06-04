861509ef6d5eb7c5844172a45803dcb8a401869f
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-06-04 05:26:33 +0000

    Import hs-Lucu-0.3 as wip/hs-Lucu.
    Lucu is an HTTP daemonic library. It can be embedded in any Haskell
    program and runs in an independent thread. Lucu is not a replacement
    for Apache. It is intended to be used to create an efficient web-based
    application without messing around FastCGI. It is also intended to be
    run behind a reverse-proxy so it doesn't have some facilities like
    logging, client filtering or such like.

---
+++ hs-Lucu/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/06/04 05:26:33 phonohawk Exp $
+
+BUILDLINK_TREE+=	hs-Lucu
+
+.if !defined(HS_LUCU_BUILDLINK3_MK)
+HS_LUCU_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-Lucu+=	hs-Lucu>=0.3
+BUILDLINK_PKGSRCDIR.hs-Lucu?=	../../wip/hs-Lucu
+
+.include "../../wip/hs-HsOpenSSL/buildlink3.mk"
+.include "../../wip/hs-haskell-src/buildlink3.mk"
+.include "../../wip/hs-dataenc/buildlink3.mk"
+.include "../../wip/hs-hxt/buildlink3.mk"
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-network/buildlink3.mk"
+.include "../../wip/hs-stm/buildlink3.mk"
+.include "../../wip/hs-time/buildlink3.mk"
+.include "../../wip/hs-zlib/buildlink3.mk"
+.endif	# HS_LUCU_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-Lucu
