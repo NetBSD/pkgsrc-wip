7bd7a151a9b597e3f71af389734b3d145abe6605
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-06-05 04:41:23 +0000

    It turned out that haskeline depends on libiconv.

--- hs-haskeline/buildlink3.mk
+++ hs-haskeline/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/06/05 04:28:16 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/06/05 04:41:23 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-haskeline
 
@@ -8,6 +8,7 @@ HS_HASKELINE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-haskeline+=	hs-haskeline>=0.6.1.6
 BUILDLINK_PKGSRCDIR.hs-haskeline?=	../../wip/hs-haskeline
 
+.include "../../converters/libiconv/buildlink3.mk"
 .include "../../wip/hs-extensible-exceptions/buildlink3.mk"
 .include "../../wip/hs-mtl/buildlink3.mk"
 .include "../../wip/hs-utf8-string/buildlink3.mk"
