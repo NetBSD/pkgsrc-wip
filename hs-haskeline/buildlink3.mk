453d43937d9f98879fb22d0148740ad2e7da6c97
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-06-05 04:28:16 +0000

    Import hs-haskeline-0.6.1.6 as wip/hs-haskeline.
    Haskeline provides a user interface for line input in command-line
    programs. This library is similar in purpose to readline, but since it
    is written in Haskell it is (hopefully) more easily used in other
    Haskell programs.
    
    Haskeline runs both on POSIX-compatible systems and on Windows.

---
+++ hs-haskeline/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/06/05 04:28:16 phonohawk Exp $
+
+BUILDLINK_TREE+=	hs-haskeline
+
+.if !defined(HS_HASKELINE_BUILDLINK3_MK)
+HS_HASKELINE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-haskeline+=	hs-haskeline>=0.6.1.6
+BUILDLINK_PKGSRCDIR.hs-haskeline?=	../../wip/hs-haskeline
+
+.include "../../wip/hs-extensible-exceptions/buildlink3.mk"
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-utf8-string/buildlink3.mk"
+.endif	# HS_HASKELINE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-haskeline
