f493bb5fa97e048599549fddc9b189ad6bc8b71d
Jörg Sonnenberger <joerg@NetBSD.org>
2009-06-07 21:27:03 +0000

    Update to latest e17 snapshot

--- edje/buildlink3.mk
+++ edje/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.16 2009/03/20 19:43:40 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2009/06/07 21:27:03 jsonn Exp $
 
 BUILDLINK_TREE+=	edje
 
 .if !defined(EDJE_BUILDLINK3_MK)
 EDJE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.edje+=	edje>=0.9.9.043
-BUILDLINK_ABI_DEPENDS.edje?=	edje>=0.9.9.043nb1
+BUILDLINK_API_DEPENDS.edje+=	edje>=0.9.9.060
 BUILDLINK_PKGSRCDIR.edje?=	../../wip/edje
 
 .include	"../../devel/eet/buildlink3.mk"
