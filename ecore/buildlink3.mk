fbe0ea8230e6f5d0c1bd571a71deec9d3901a4c6
Jörg Sonnenberger <joerg@NetBSD.org>
2009-06-07 21:27:34 +0000

    Update to latest e17 snapshot. Build xrender and composite frontends.

--- ecore/buildlink3.mk
+++ ecore/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.14 2009/03/20 19:43:40 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2009/06/07 21:27:34 jsonn Exp $
 
 BUILDLINK_TREE+=	ecore
 
 .if !defined(ECORE_BUILDLINK3_MK)
 ECORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ecore+=	ecore>=0.9.9.041
-BUILDLINK_ABI_DEPENDS.ecore?=	ecore>=0.9.9.042nb1
+BUILDLINK_API_DEPENDS.ecore+=	ecore>=0.9.9.060
 BUILDLINK_PKGSRCDIR.ecore?=	../../wip/ecore
 
 .include "../../devel/eet/buildlink3.mk"
@@ -15,9 +14,12 @@ BUILDLINK_PKGSRCDIR.ecore?=	../../wip/ecore
 .include "../../graphics/evas-buffer/buildlink3.mk"
 .include "../../graphics/evas-software-x11/buildlink3.mk"
 .include "../../www/curl/buildlink3.mk"
+.include "../../x11/libXScrnSaver/buildlink3.mk"
+.include "../../x11/libXcomposite/buildlink3.mk"
 .include "../../x11/libXdamage/buildlink3.mk"
 .include "../../x11/libXp/buildlink3.mk"
-.include "../../x11/libXScrnSaver/buildlink3.mk"
+.include "../../x11/libXrandr/buildlink3.mk"
+.include "../../x11/libXrender/buildlink3.mk"
 .endif # ECORE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ecore
