0b8368d07883d005e7bafcf74391f05fb05addb9
Jörg Sonnenberger <joerg@NetBSD.org>
2009-06-07 21:53:06 +0000

    Update to 2009/04/22 snapshot.

--- edbus/buildlink3.mk
+++ edbus/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:43:40 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/06/07 21:53:06 jsonn Exp $
 
 BUILDLINK_TREE+=	edbus
 
 .if !defined(EDBUS_BUILDLINK3_MK)
 EDBUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.edbus+=	edbus>=0.01
+BUILDLINK_API_DEPENDS.edbus+=	edbus>=0.05
 BUILDLINK_PKGSRCDIR.edbus?=	../../wip/edbus
 .endif # EDBUS_BUILDLINK3_MK
 
