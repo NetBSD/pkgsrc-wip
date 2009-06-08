dbf300c7932f264731e33b36de39fe0a404b5395
Jörg Sonnenberger <joerg@NetBSD.org>
2009-06-08 15:31:06 +0000

    Needs dbus recursively

--- edbus/buildlink3.mk
+++ edbus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/06/07 21:53:06 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2009/06/08 15:31:06 jsonn Exp $
 
 BUILDLINK_TREE+=	edbus
 
@@ -7,6 +7,8 @@ EDBUS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.edbus+=	edbus>=0.05
 BUILDLINK_PKGSRCDIR.edbus?=	../../wip/edbus
+
+.include "../../sysutils/dbus/buildlink3.mk"
 .endif # EDBUS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-edbus
