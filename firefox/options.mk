45bc03b0537f0b1f156d13129158660007d4e00c
Tobias Nygren <tnn@NetBSD.org>
2009-06-19 22:25:13 +0000

    PLIST fix

--- firefox/options.mk
+++ firefox/options.mk
@@ -1,7 +1,9 @@
-# $NetBSD: options.mk,v 1.1 2009/06/19 12:44:24 tnn2 Exp $
+# $NetBSD: options.mk,v 1.2 2009/06/19 22:25:13 tnn2 Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.firefox35
 PKG_SUPPORTED_OPTIONS=	debug official-mozilla-branding mozilla-jemalloc
+
+PLIST_VARS+=		jemalloc branding
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,7 +13,7 @@ PKG_SUGGESTED_OPTIONS+=	mozilla-jemalloc
 
 .if !empty(PKG_OPTIONS:Mmozilla-jemalloc)
 CONFIGURE_ARGS+=	--enable-jemalloc
-PLIST.mozilla-jemalloc=	yes
+PLIST.jemalloc=		yes
 .else
 CONFIGURE_ARGS+=	--disable-jemalloc
 .endif
@@ -24,6 +26,7 @@ CONFIGURE_ARGS+=	--disable-debug
 
 .if !empty(PKG_OPTIONS:Mofficial-mozilla-branding)
 CONFIGURE_ARGS+=	--enable-official-branding
+PLIST.branding=		yes
 LICENSE=		mozilla-trademark-license
 RESTRICTED=		Trademark holder prohibits distribution of modified versions.
 NO_BIN_ON_CDROM=	${RESTRICTED}
