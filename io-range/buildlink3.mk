f639901d6f8948fd1e4e24bc48dc854cef0b3585
M. Negovanovic <milosn@users.sourceforge.net>
2009-07-04 19:26:21 +0000

    New buildlink3.mk.

---
+++ io-range/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2009/07/04 19:26:21 milosn Exp $
+
+BUILDLINK_TREE+=        io-range
+
+.if !defined(IORANGE_BUILDLINK3_MK)
+IORANGE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.io-range+=	io-range>=20080904
+BUILDLINK_ABI_DEPENDS.io-range+=	io-range>=20080904
+BUILDLINK_PKGSRCDIR.io-range?=		../../wip/io-range
+
+BUILDLINK_LIBDIRS.io-range+=		lib/io/addons/Range/_build/dll
+BUILDLINK_INCDIRS.io-range+=		include/io/Range
+.endif # IORANGE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=        -io-range
