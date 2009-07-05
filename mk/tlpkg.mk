df93bf20ca03e7f8b1a1524098808c6613bd950d
Min Sik Kim <minskim@NetBSD.org>
2009-07-05 22:05:35 +0000

    Add a Makefile fragment for TeX Live 2009 packages.
    After TeX Live 2009 is released, this file should be moved to
    pkgsrc/print/texlive.

---
+++ mk/tlpkg.mk
@@ -0,0 +1,83 @@
+# $NetBSD: tlpkg.mk,v 1.1 2009/07/05 22:05:35 minskim Exp $
+#
+# This Makefile fragment is inteded to be included by packages that build
+# TeX Live packages.
+#
+# Package-settable variables:
+#
+# TEX_FORMAT_NAMES
+#	See ../../print/texlive-tetex/format.mk.
+#
+# TEX_HYPHEN_DAT
+#	See ../../print/texlive-tetex/hyphen.mk.
+#
+# TEX_HYPHEN_DEF
+#	See ../../print/texlive-tetex/hyphen.mk.
+#
+# TEX_MAP_FILES
+#	See ../../print/texlive-tetex/map.mk.
+#
+# TEX_MIXEDMAP_FILES
+#	See ../../print/texlive-tetex/map.mk.
+#
+# TEX_TEXMF_DIRS
+#	See ../../print/kpathsea/texmf.mk.
+#
+# TEXLIVE_IGNORE_PATTERNS
+#	glob patterns for files and directories in ${WRKSRC} that
+#	should not be installed.
+
+CATEGORIES?=	print
+MASTER_SITES?=	ftp://ftp.math.utah.edu/pub/texlive/tlnet/archive/
+DIST_SUBDIR?=	${PKGNAME_NOREV}
+EXTRACT_SUFX?=	.tar.xz
+
+HOMEPAGE?=	http://www.tug.org/texlive/
+
+PKG_DESTDIR_SUPPORT=	user-destdir
+
+USE_TOOLS+=	pax
+
+NO_BUILD?=	yes
+WRKSRC?=	${WRKDIR}
+
+.PHONY: _texlive-set-permission:
+_texlive-set-permission:
+.for _pat in ${TEXLIVE_IGNORE_PATTERNS}
+	${RM} -rf ${WRKSRC}/${_pat}
+.endfor
+.for _texmf in texmf texmf-dist texmf-doc
+	if [ -d ${WRKSRC}/${_texmf} ]; then \
+	  ${FIND} ${WRKSRC}/${_texmf} -type d -exec ${CHMOD} ${PKGDIRMODE} {} \; ; \
+	  ${FIND} ${WRKSRC}/${_texmf} -type f -exec ${CHMOD} ${SHAREMODE} {} \; ; \
+	fi
+.endfor
+
+.PHONY: _texlive-install:
+_texlive-install:
+.for _texmf in texmf texmf-dist
+	if [ -d ${WRKSRC}/${_texmf} ]; then \
+		cd ${WRKSRC} && \
+		pax -rwpm -s ',.*\.orig$$,,' \
+			${_texmf} ${DESTDIR}${PREFIX}/share; \
+	fi
+.endfor
+	if [ -d ${WRKSRC}/texmf-doc ]; then \
+		cd ${WRKSRC} && \
+		pax -rwpm -s ',.*\.orig$$,,' texmf-doc/* \
+			${DESTDIR}${PREFIX}/share/doc/texmf; \
+	fi
+
+.include "../../print/kpathsea/texmf.mk"
+.if !empty(TEX_FORMAT_NAMES)
+.  include "../../print/texlive-tetex/format.mk"
+.endif
+.if !empty(TEX_HYPHEN_DAT) || !empty(TEX_HYPHEN_DEF)
+.  include "../../print/texlive-tetex/hyphen.mk"
+.endif
+.if !empty(TEX_MAP_FILES) || !empty(TEX_MIXEDMAP_FILES)
+.  include "../../print/texlive-tetex/map.mk"
+.endif
+
+post-extract: _texlive-set-permission
+do-install: _texlive-install
