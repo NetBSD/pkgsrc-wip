762eda7ca64ab20b0ae402d5977a234d2ac91e92
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-07-27 04:08:29 +0000

    Import hs-HsSVN-0.4.1 as wip/hs-HsHSVN.
    HsSVN is a (part of) Subversion binding for Haskell. Currently it can
    do most things related to the Subversion FS but others are left
    uncovered.

---
+++ hs-HsHSVN/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/07/27 04:08:29 phonohawk Exp $
+
+BUILDLINK_TREE+=	hs-HsSVN
+
+.if !defined(HS_HSSVN_BUILDLINK3_MK)
+HS_HSSVN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-HsSVN+=	hs-HsSVN>=0.4.1
+BUILDLINK_PKGSRCDIR.hs-HsSVN?=	../../wip/hs-HsHSVN
+
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-stm/buildlink3.mk"
+.include "../../devel/subversion-base/buildlink3.mk"
+.endif	# HS_HSSVN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-HsSVN
