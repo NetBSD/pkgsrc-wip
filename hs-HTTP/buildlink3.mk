e926ecd6337ad7cd3c5545f55a34faae6244a53c
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-07-27 03:37:08 +0000

    Upstream update to HTTP-4000.0.7

--- hs-HTTP/buildlink3.mk
+++ hs-HTTP/buildlink3.mk
@@ -1,16 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:43:42 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/07/27 03:37:08 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-HTTP
 
 .if !defined(HS_HTTP_BUILDLINK3_MK)
 HS_HTTP_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.hs-HTTP?=	build
-BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.0.1
+BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.0.7
 BUILDLINK_PKGSRCDIR.hs-HTTP?=	../../wip/hs-HTTP
 
 .include "../../wip/hs-network/buildlink3.mk"
-.include "../../wip/parsec/buildlink3.mk"
-.endif # HS_HTTP_BUILDLINK3_MK
+.include "../../wip/hs-parsec/buildlink3.mk"
+.endif	# HS_HTTP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-HTTP
