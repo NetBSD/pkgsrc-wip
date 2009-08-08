c8419d97e777250bfc3a9f3e6dd188079200a590
OBATA Akio <obache@users.sourceforge.net>
2009-08-08 05:36:12 +0000

    Good-bye @dirrm.

--- scim/buildlink3.mk
+++ scim/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:43:52 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2009/08/08 05:36:12 obache Exp $
 
 BUILDLINK_TREE+=	scim
 
@@ -7,14 +7,6 @@ SCIM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.scim+=	scim>=1.4.7
 BUILDLINK_PKGSRCDIR.scim?=	../../wip/scim
-
-PRINT_PLIST_AWK+=	/^@dirrm lib\/scim-1.0\/1.4.0$$/ { next; }
-PRINT_PLIST_AWK+=	/^@dirrm lib\/scim-1.0$$/ { next; }
-PRINT_PLIST_AWK+=	/^@dirrm lib\/scim-1.0\/1.4.0\/(SetupUI|IMEngine|Helper|FrontEnd|Filter|Config)$$/ \
-				{print "@comment in scim: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/scim$$/ { next; }
-PRINT_PLIST_AWK+=	/^@dirrm share\/scim\/icons$$/ \
-				{print "@comment in scim: " $$0; next; }
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
