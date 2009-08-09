6b80a1693d60aab2bdfdef6b1d3ea5f79697eced
Arnaud Degroote <degroote@NetBSD.org>
2009-08-09 18:16:17 +0000

    Update from 1.4.1 to 1.4.5

--- hs-x11/buildlink3.mk
+++ hs-x11/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/05/20 00:58:41 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/08/09 18:16:17 zul_ Exp $
 
 BUILDLINK_TREE+=	hs-x11
 
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	hs-x11
 HS_X11_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.hs-x11?=	build
-BUILDLINK_API_DEPENDS.hs-x11+=	hs-x11>=1.4.1
-BUILDLINK_ABI_DEPENDS.hs-x11?=	hs-x11>=1.4.1nb1
+BUILDLINK_API_DEPENDS.hs-x11+=	hs-x11>=1.4.5
+BUILDLINK_ABI_DEPENDS.hs-x11?=	hs-x11>=1.4.5
 BUILDLINK_PKGSRCDIR.hs-x11?=	../../wip/hs-x11
 
 .include "../../lang/ghc/buildlink3.mk"
