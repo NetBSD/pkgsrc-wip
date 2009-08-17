1d38cf960f34d1b4ef0eaaebe42925ea500b9773
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2009-08-17 20:50:34 +0000

    pkgsrc-changes: 	-------------- 	o update version

--- py-webkit/buildlink3.mk
+++ py-webkit/buildlink3.mk
@@ -1,15 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/08/12 02:32:03 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2009/08/17 20:50:34 jihbed Exp $
 
 BUILDLINK_TREE+=	py-webkitgtk
 
 .if !defined(PY_WEBKITGTK_BUILDLINK3_MK)
 PY_WEBKITGTK_BUILDLINK3_MK:=
 
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_API_DEPENDS.py-webkitgtk+=	${PYPKGPREFIX}-webkitgtk>=1.1.4
-BUILDLINK_ABI_DEPENDS.py-webkitgtk?=	py25-webkitgtk>=1.1.5nb1
+BUILDLINK_API_DEPENDS.py-webkitgtk+=	${PYPKGPREFIX}-webkitgtk>=1.1.6
 BUILDLINK_PKGSRCDIR.py-webkitgtk?=	../../wip/py-webkit
+
 
 .include "../../x11/py-gtk2/buildlink3.mk"
 .include "../../www/webkit-gtk/buildlink3.mk"
