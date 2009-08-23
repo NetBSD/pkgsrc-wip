49b7c4ee9a00410a9811aabe4caf1e622252f9b1
Evaldo Gardenali <udontknow@users.sourceforge.net>
2009-08-23 21:06:33 +0000

    Implement option inet6

--- mozldap/options.mk
+++ mozldap/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.2 2009/08/23 20:45:21 udontknow Exp $
+# $NetBSD: options.mk,v 1.3 2009/08/23 21:06:33 udontknow Exp $
 
 PKG_OPTIONS_VAR=        PKG_OPTIONS.mozldap
-PKG_SUPPORTED_OPTIONS=  sasl
+PKG_SUPPORTED_OPTIONS=  sasl inet6
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,4 +10,8 @@ PKG_SUPPORTED_OPTIONS=  sasl
 CONFIGURE_ARGS+=	--with-sasl-inc=${DESTDIR}${PREFIX}/include/sasl
 CONFIGURE_ARGS+=	--with-sasl-lib=${DESTDIR}${PREFIX}/lib
 .include "../../security/cyrus-sasl/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mipv6)
+CONFIGURE_ARGS+=	--enable-ipv6
 .endif
