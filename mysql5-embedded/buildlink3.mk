758f95aba23f581c651bb2b7b46010117db09842
Thomas Klausner <wiz@NetBSD.org>
2009-08-25 16:26:01 +0000

    Split this off from mysql*; install minimal files. Update to 5.1.36, supposedly supported by amarok.

--- mysql5-embedded/buildlink3.mk
+++ mysql5-embedded/buildlink3.mk
@@ -1,16 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:43:51 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/08/25 16:26:01 thomasklausner Exp $
 
 BUILDLINK_TREE+=	mysql-embedded
 
 .if !defined(MYSQL_EMBEDDED_BUILDLINK3_MK)
 MYSQL_EMBEDDED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mysql-embedded+=	mysql-embedded>=5.0.67
+BUILDLINK_API_DEPENDS.mysql-embedded+=	mysql-embedded>=5.1.36
 BUILDLINK_PKGSRCDIR.mysql-embedded?=	../../wip/mysql5-embedded
 
-.include "../../databases/mysql5-client/buildlink3.mk"
-.include "../../security/tcp_wrappers/buildlink3.mk"
-.include "../../devel/zlib/buildlink3.mk"
+_BLNK_PASSTHRU_DIRS+=		${LOCALBASE}/mysqld/lib
 .endif # MYSQL_EMBEDDED_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mysql-embedded
