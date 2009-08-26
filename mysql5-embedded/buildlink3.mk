baeaa3de0e633fc058d0d3d7bf551fcd265d7211
Thomas Klausner <wiz@NetBSD.org>
2009-08-26 10:54:38 +0000

    Cleanup. Also install mysqlclient library (for amarok, like this whole package...)

--- mysql5-embedded/buildlink3.mk
+++ mysql5-embedded/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/08/25 16:26:01 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/08/26 10:54:38 thomasklausner Exp $
 
 BUILDLINK_TREE+=	mysql-embedded
 
 .if !defined(MYSQL_EMBEDDED_BUILDLINK3_MK)
 MYSQL_EMBEDDED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mysql-embedded+=	mysql-embedded>=5.1.36
+BUILDLINK_API_DEPENDS.mysql-embedded+=	mysql-embedded>=5.1.36nb1
 BUILDLINK_PKGSRCDIR.mysql-embedded?=	../../wip/mysql5-embedded
 
 _BLNK_PASSTHRU_DIRS+=		${LOCALBASE}/mysqld/lib
