1912b56840858733249d3722f754bd0e1b249a21
Thomas Klausner <wiz@NetBSD.org>
2009-09-01 12:33:10 +0000

    Add buildlink3.mk file.

--- openal-soft/buildlink3.mk
+++ openal-soft/buildlink3.mk
@@ -1,15 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:43:51 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/09/01 12:33:10 thomasklausner Exp $
 
 BUILDLINK_TREE+=	openal-soft
 
 .if !defined(OPENAL_SOFT_BUILDLINK3_MK)
 OPENAL_SOFT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.openal-soft+=	openal-soft>=1.2.218
-#BUILDLINK_PKGSRCDIR.openal-soft?=	../../audio/openal-soft
+BUILDLINK_API_DEPENDS.openal-soft+=	openal-soft>=1.5.304
 BUILDLINK_PKGSRCDIR.openal-soft?=	../../wip/openal-soft
-
-.include "../../mk/dlopen.buildlink3.mk"
-.endif # OPENAL_SOFT_BUILDLINK3_MK
+.endif	# OPENAL_SOFT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-openal-soft
