d99c277ee956ecc3ae58e0a96ee04b9b160e5ec3
Thomas Klausner <wiz@NetBSD.org>
2009-09-04 21:59:18 +0000

    Add buildlink3.mk.

---
+++ liblastfm/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2009/09/04 21:59:18 thomasklausner Exp $
+
+BUILDLINK_TREE+=	liblastfm
+
+.if !defined(LIBLASTFM_BUILDLINK3_MK)
+LIBLASTFM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.liblastfm+=	liblastfm>=0.3.0
+BUILDLINK_PKGSRCDIR.liblastfm?=	../../wip/liblastfm
+
+.include "../../audio/libsamplerate/buildlink3.mk"
+.include "../../math/fftwf/buildlink3.mk"
+.include "../../x11/qt4-libs/buildlink3.mk"
+.include "../../x11/qt4-tools/buildlink3.mk"
+.endif	# LIBLASTFM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-liblastfm
