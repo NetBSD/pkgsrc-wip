a5fe298ebb82d3ac8a3ed848cd96717453de8f28
Jörg Sonnenberger <joerg@NetBSD.org>
2009-09-11 23:07:28 +0000

    Regen

--- elementary/buildlink3.mk
+++ elementary/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/09/11 23:05:15 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/09/11 23:07:28 jsonn Exp $
 
-BUILDLINK_TREE+=	edbus
+BUILDLINK_TREE+=	elementary
 
-.if !defined(EDBUS_BUILDLINK3_MK)
-EDBUS_BUILDLINK3_MK:=
+.if !defined(ELEMENTARY_BUILDLINK3_MK)
+ELEMENTARY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.edbus+=	edbus>=0.5.0.062
-BUILDLINK_PKGSRCDIR.edbus?=	../../wip/edbus
+BUILDLINK_API_DEPENDS.elementary+=	elementary>=0.5.1.0
+BUILDLINK_PKGSRCDIR.elementary?=	../../wip/elementary
 
-.include "../../sysutils/dbus/buildlink3.mk"
-.endif # EDBUS_BUILDLINK3_MK
+.include "../../wip/ecore/buildlink3.mk"
+.include "../../wip/edje/buildlink3.mk"
+.endif	# ELEMENTARY_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-edbus
+BUILDLINK_TREE+=	-elementary
