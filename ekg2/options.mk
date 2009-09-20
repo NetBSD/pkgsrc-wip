0341337c61dc7627802cd694a6b875ee5ee44f05
Daniel Horecki <morr@NetBSD.org>
2009-09-20 22:05:14 +0000

    PLIST cleanup, properly include libgnutls-config.mk so it can be used.

--- ekg2/options.mk
+++ ekg2/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2007/04/16 19:00:24 shindenmorr Exp $
+# $NetBSD: options.mk,v 1.2 2009/09/20 22:05:14 shindenmorr Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ekg2
 PKG_SUPPORTED_OPTIONS=	libgadu jabber
@@ -9,14 +9,11 @@ PKG_SUGGESTED_OPTIONS=	libgadu jabber
 .if !empty(PKG_OPTIONS:Mjabber)
 .include "../../textproc/expat/buildlink3.mk"
 .include "../../security/gnutls/buildlink3.mk"
-PLIST_SUBST+=	JABBER=""
-.else
-PLIST_SUBST+=	JABBER="@comment "
+.include "../../security/gnutls/libgnutls-config.mk"
+PLIST.jabber=	yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mlibgadu)
 .include "../../wip/libgadu/buildlink3.mk"
-PLIST_SUBST+=	GG=""
-.else
-PLIST_SUBST+=	GG="@comment "
+PLIST.gg+=	yes
 .endif
