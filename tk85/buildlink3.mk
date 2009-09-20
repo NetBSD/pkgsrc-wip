c7036873583386d21e8636eeb714cc810732d32d
Daniel Horecki <morr@NetBSD.org>
2009-09-20 21:32:24 +0000

    forgot about that file.

--- tk85/buildlink3.mk
+++ tk85/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:43:52 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/09/20 21:32:24 shindenmorr Exp $
 
 BUILDLINK_TREE+=	tk
 
 .if !defined(TK_BUILDLINK3_MK)
 TK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.tk+=	tk>=8.5.3
-BUILDLINK_ABI_DEPENDS.tk+=	tk>=8.5.3
+BUILDLINK_API_DEPENDS.tk+=	tk>=8.5.7
+BUILDLINK_ABI_DEPENDS.tk+=	tk>=8.5.7
 BUILDLINK_PKGSRCDIR.tk?=	../../wip/tk85
 
 BUILDLINK_FILES.tk+=	bin/wish*
