de215bc1f54fa74af2c3886955a35f5ba4a7e202
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-09-23 09:26:41 +0000

    Import hs-haskore-0.1 as wip/hs-haskore.
    Compose music using programming features. Output in MIDI, CSound,
    SuperCollider or as an audio signal.

---
+++ hs-haskore/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/09/23 09:26:41 phonohawk Exp $
+
+BUILDLINK_TREE+=	hs-haskore
+
+.if !defined(HS_HASKORE_BUILDLINK3_MK)
+HS_HASKORE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-haskore+=	hs-haskore>=0.1
+BUILDLINK_PKGSRCDIR.hs-haskore?=	../../wip/hs-haskore
+
+.include "../../wip/hs-data-accessor/buildlink3.mk"
+.include "../../wip/hs-event-list/buildlink3.mk"
+.include "../../wip/hs-haskell-src/buildlink3.mk"
+.include "../../wip/hs-HUnit/buildlink3.mk"
+.include "../../wip/hs-markov-chain/buildlink3.mk"
+.include "../../wip/hs-midi/buildlink3.mk"
+.include "../../wip/hs-non-negative/buildlink3.mk"
+.include "../../wip/hs-parsec/buildlink3.mk"
+.include "../../wip/hs-transformers/buildlink3.mk"
+.include "../../wip/hs-utility-ht/buildlink3.mk"
+.endif	# HS_HASKORE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-haskore
