f738bbd1baf4b93205b52a2569e685321da6e028
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-09-23 07:04:49 +0000

    Upstream update to binary-0.5.0.2

--- hs-binary/buildlink3.mk
+++ hs-binary/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/05/21 04:50:37 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2009/09/23 07:04:49 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-binary
 
 .if !defined(HS_BINARY_BUILDLINK3_MK)
 HS_BINARY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-binary+=	hs-binary>=0.5.0.1
+BUILDLINK_API_DEPENDS.hs-binary+=	hs-binary>=0.5.0.2
 BUILDLINK_PKGSRCDIR.hs-binary?=	../../wip/hs-binary
 .endif # HS_BINARY_BUILDLINK3_MK
 
