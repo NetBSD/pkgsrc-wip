6265f8529ee026e756b4d3776e83fa5c79a9ea6a
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-09-24 06:41:03 +0000

    Bump revision to nb2

--- hs-html/buildlink3.mk
+++ hs-html/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:43:42 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/09/24 06:41:03 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-html
 
 .if !defined(HS_HTML_BUILDLINK3_MK)
 HS_HTML_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.hs-html?=	build
-BUILDLINK_API_DEPENDS.hs-html+=	hs-html>=1.0.1.2
+BUILDLINK_API_DEPENDS.hs-html+=	hs-html>=1.0.1.2nb2
 BUILDLINK_PKGSRCDIR.hs-html?=	../../wip/hs-html
-.endif # HS_HTML_BUILDLINK3_MK
+.endif	# HS_HTML_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-html
