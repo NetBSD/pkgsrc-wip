76f8720862bf3d91ae1f2fe96ce377d29f46b997
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-09-24 09:32:19 +0000

    add missing dependency to devel/ncurses

--- hs-terminfo/buildlink3.mk
+++ hs-terminfo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/09/24 06:11:22 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/09/24 09:32:19 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-terminfo
 
@@ -8,6 +8,7 @@ HS_TERMINFO_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-terminfo+=	hs-terminfo>=0.3.1.1
 BUILDLINK_PKGSRCDIR.hs-terminfo?=	../../wip/hs-terminfo
 
+.include "../../devel/ncurses/buildlink3.mk"
 .include "../../wip/hs-extensible-exceptions/buildlink3.mk"
 .endif	# HS_TERMINFO_BUILDLINK3_MK
 
