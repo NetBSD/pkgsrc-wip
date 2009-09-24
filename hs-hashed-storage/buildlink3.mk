6523c9e088d10bf465899e08e64cc24a15e2a735
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-09-24 05:35:33 +0000

    Import hs-hashed-storage-0.3.8 as wip/hs-hashed-storage.
    Support code for reading and manipulating hashed file storage (where
    each file and directory is associated with a cryptographic hash, for
    corruption-resistant storage and fast comparisons).
    
    The supported storage formats include darcs hashed pristine, a plain
    filesystem tree and an indexed plain tree (where the index maintains
    hashes of the plain files and directories).

---
+++ hs-hashed-storage/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/09/24 05:35:33 phonohawk Exp $
+
+BUILDLINK_TREE+=	hs-hashed-storage
+
+.if !defined(HS_HASHED_STORAGE_BUILDLINK3_MK)
+HS_HASHED_STORAGE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hashed-storage+=	hs-hashed-storage>=0.3.8
+BUILDLINK_PKGSRCDIR.hs-hashed-storage?=	../../wip/hs-hashed-storage
+
+.include "../../wip/hs-extensible-exceptions/buildlink3.mk"
+.include "../../wip/hs-mmap/buildlink3.mk"
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-zlib/buildlink3.mk"
+.endif	# HS_HASHED_STORAGE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hashed-storage
