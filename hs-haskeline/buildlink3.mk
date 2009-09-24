e6c1fdaf1177d9b87c1bf47ca65851cd46b56d03
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-09-24 06:22:54 +0000

    Upstream update to haskeline-0.6.2.1

--- hs-haskeline/buildlink3.mk
+++ hs-haskeline/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/06/05 04:41:23 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/09/24 06:22:54 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-haskeline
 
 .if !defined(HS_HASKELINE_BUILDLINK3_MK)
 HS_HASKELINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-haskeline+=	hs-haskeline>=0.6.1.6
+BUILDLINK_API_DEPENDS.hs-haskeline+=	hs-haskeline>=0.6.2.1
 BUILDLINK_PKGSRCDIR.hs-haskeline?=	../../wip/hs-haskeline
 
-.include "../../converters/libiconv/buildlink3.mk"
 .include "../../wip/hs-extensible-exceptions/buildlink3.mk"
 .include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-terminfo/buildlink3.mk"
 .include "../../wip/hs-utf8-string/buildlink3.mk"
 .endif	# HS_HASKELINE_BUILDLINK3_MK
 
