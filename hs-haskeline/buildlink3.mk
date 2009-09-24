af95e220b91d7efed6df02c34b96ab81e8ebfcc4
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-09-24 10:14:14 +0000

    added missing dependency to converters/libiconv

--- hs-haskeline/buildlink3.mk
+++ hs-haskeline/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/09/24 06:22:54 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/09/24 10:14:14 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-haskeline
 
@@ -8,6 +8,7 @@ HS_HASKELINE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-haskeline+=	hs-haskeline>=0.6.2.1
 BUILDLINK_PKGSRCDIR.hs-haskeline?=	../../wip/hs-haskeline
 
+.include "../../converters/libiconv/buildlink3.mk"
 .include "../../wip/hs-extensible-exceptions/buildlink3.mk"
 .include "../../wip/hs-mtl/buildlink3.mk"
 .include "../../wip/hs-terminfo/buildlink3.mk"
