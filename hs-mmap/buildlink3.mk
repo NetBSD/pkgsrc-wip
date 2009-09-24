e1f1337fc320bf67d1c791154b1c5464cb8f1941
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-09-24 05:27:50 +0000

    Import hs-mmap-0.4.1 as wip/hs-mmap.
    This library provides a wrapper to mmap(2) or MapViewOfFile, allowing
    files or devices to be lazily loaded into memory as strict or lazy
    ByteStrings, ForeignPtrs or plain Ptrs, using the virtual memory
    subsystem to do on-demand loading. Modifications are also supported.

---
+++ hs-mmap/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/09/24 05:27:50 phonohawk Exp $
+
+BUILDLINK_TREE+=	hs-mmap
+
+.if !defined(HS_MMAP_BUILDLINK3_MK)
+HS_MMAP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-mmap+=	hs-mmap>=0.4.1
+BUILDLINK_PKGSRCDIR.hs-mmap?=	../../wip/hs-mmap
+.endif	# HS_MMAP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-mmap
