1f38cbcd18530186437c6aa325939039a32e6e1b
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-09-24 08:12:58 +0000

    update buildlink3.mk

--- ghc/buildlink3.mk
+++ ghc/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/07/02 08:57:03 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2009/09/24 08:12:58 phonohawk Exp $
 
 BUILDLINK_TREE+=	ghc
 
 .if !defined(GHC_BUILDLINK3_MK)
 GHC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ghc+=	ghc>=6.10.3
+BUILDLINK_API_DEPENDS.ghc+=	ghc>=6.10.4
 BUILDLINK_PKGSRCDIR.ghc?=	../../wip/ghc
 
 .include "../../devel/gmp/buildlink3.mk"
