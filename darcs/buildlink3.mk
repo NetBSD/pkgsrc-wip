ec1923403f0c2c4b85308c27e7ecebf38e877084
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-09-24 08:34:39 +0000

    Upstream update to darcs-2.3.1

--- darcs/buildlink3.mk
+++ darcs/buildlink3.mk
@@ -1,18 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:43:40 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/09/24 08:34:39 phonohawk Exp $
 
 BUILDLINK_TREE+=	darcs
 
 .if !defined(DARCS_BUILDLINK3_MK)
 DARCS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.darcs+=	darcs>=2.2.0
+BUILDLINK_API_DEPENDS.darcs+=	darcs>=2.3.1
 BUILDLINK_PKGSRCDIR.darcs?=	../../wip/darcs
 
+.include "../../wip/hs-hashed-storage/buildlink3.mk"
+.include "../../wip/hs-haskeline/buildlink3.mk"
 .include "../../wip/hs-html/buildlink3.mk"
 .include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-parsec/buildlink3.mk"
 .include "../../wip/hs-regex-compat/buildlink3.mk"
-.include "../../wip/parsec/buildlink3.mk"
-.include "../../www/curl/buildlink3.mk"
-.endif # DARCS_BUILDLINK3_MK
+.include "../../wip/hs-utf8-string/buildlink3.mk"
+.endif	# DARCS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-darcs
---
+++ darcs/options.mk
@@ -0,0 +1,62 @@
+# $NetBSD: options.mk,v 1.1 2009/09/24 08:34:39 phonohawk Exp $
+#
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.darcs
+PKG_SUPPORTED_OPTIONS=	zlib terminfo color mmap
+
+PKG_OPTIONS_OPTIONAL_GROUPS+=	http-lib
+PKG_OPTIONS_GROUP.http-lib=		curl http
+
+PKG_SUGGESTED_OPTIONS=	http zlib terminfo color mmap
+
+.include "../../mk/bsd.options.mk"
+
+#
+# Curl or HTTP
+#
+.if !empty(PKG_OPTIONS:Mcurl)
+CONFIGURE_ARGS+= -fcurl -fcurl-pipelining -f-http
+.include "../../www/curl/buildlink3.mk"
+.elif !empty(PKG_OPTIONS:Mhttp)
+CONFIGURE_ARGS+= -f-curl -fhttp
+.include "../../wip/hs-HTTP/buildlink3.mk"
+.endif
+
+#
+# Zlib
+#
+.if !empty(PKG_OPTIONS:Mzlib)
+CONFIGURE_ARGS+= -fzlib
+.include "../../wip/hs-zlib/buildlink3.mk"
+.else
+CONFIGURE_ARGS+= -f-zlib
+.endif
+
+#
+# terminfo
+#
+.if !empty(PKG_OPTIONS:Mterminfo)
+CONFIGURE_ARGS+= -fterminfo
+.include "../../wip/hs-terminfo/buildlink3.mk"
+.else
+CONFIGURE_ARGS+= -f-terminfo
+.endif
+
+#
+# color
+#
+.if !empty(PKG_OPTIONS:Mcolor)
+CONFIGURE_ARGS+= -fcolor
+.else
+CONFIGURE_ARGS+= -f-color
+.endif
+
+#
+# mmap
+#
+.if !empty(PKG_OPTIONS:Mmmap)
+CONFIGURE_ARGS+= -fmmap
+.include "../../wip/hs-mmap/buildlink3.mk"
+.else
+CONFIGURE_ARGS+= -f-mmap
+.endif
