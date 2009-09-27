63e075697719f3a22f3f254dccd02a1e0061859c
Soren Jacobsen <snj@NetBSD.org>
2009-09-27 16:08:08 +0000

    Fix options handling, requested by martin.

--- mpg123/options.mk
+++ mpg123/options.mk
@@ -1,40 +1,34 @@
-# $NetBSD: options.mk,v 1.7 2007/12/28 02:54:36 bsadewitz Exp $
-#
+# $NetBSD: options.mk,v 1.8 2009/09/27 16:08:09 blef Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.mpg123
+PKG_OPTIONS_REQUIRED_GROUPS=	cpu
+PKG_OPTIONS_GROUP.cpu=		x86-dither generic-nofpu altivec generic-fpu
 PKG_SUPPORTED_OPTIONS=		mpg123-no-fifo
 
 .include "../../mk/bsd.fast.prefs.mk"
 
-MPG123_OPTIONS.fpu=	generic_fixed generic_fpu generic_nofpu
-
-PKG_OPTIONS_GROUP.i386=		x86_dither
-PKG_OPTIONS_GROUP.powerpc=	altivec
-
-PKG_OPTIONS_OPTIONAL_GROUPS=	${MACHINE_ARCH}
-
-PKG_SUGGESTED_OPTIONS.${MACHINE_ARCH}?=	generic_fpu
-PKG_SUGGESTED_OPTIONS.arm=		generic_fixed
-PKG_SUGGESTED_OPTIONS.arm32=		generic_fixed
-PKG_SUGGESTED_OPTIONS.i386=		x86_dither
-PKG_SUGGESTED_OPTIONS.powerpc=		altivec
-
-.if defined(PKG_OPTIONS_GROUP.${MACHINE_ARCH})
-PKG_OPTIONS_GROUP.fpu=		${MPG123_OPTIONS.fpu}
-PKG_OPTIONS_OPTIONAL_GROUPS+=	fpu
+.if (${MACHINE_ARCH} == "i386")
+PKG_SUGGESTED_OPTIONS+=		x86-dither
+.elif (${MACHINE_ARCH} == "arm") || (${MACHINE_ARCH} == "arm32")
+PKG_SUGGESTED_OPTIONS+=		generic-nofpu
+.elif (${MACHINE_ARCH} == "powerpc")
+PKG_SUGGESTED_OPTIONS+=		altivec
 .else
-PKG_OPTIONS_SET.fpu=		${MPG123_OPTIONS.fpu}
-PKG_OPTIONS_NONEMPTY_SETS+=	${MPG123_OPTIONS.fpu}
+PKG_SUGGESTED_OPTIONS+=		generic-fpu
 .endif
-
-PKG_SUGGESTED_OPTIONS=	${PKG_SUGGESTED_OPTIONS.${MACHINE_ARCH}}
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mgeneric_*)
-CONFIGURE_ARGS+=	--with-cpu=${PKG_OPTIONS:Mgeneric_*)
-.elif defined(PKG_OPTIONS_GROUP.${MACHINE_ARCH})
-CONFIGURE_ARGS+=	--with-cpu=${PKG_OPTIONS_GROUP.${MACHINE_ARCH}:@.o.@${PKG_OPTIONS:M${.o.}}@}
+.if !empty(PKG_OPTIONS:Mgeneric-nofpu)
+CONFIGURE_ARGS+=	--with-cpu=generic_nofpu
+.elif !empty(PKG_OPTIONS:Mgeneric-fpu)
+CONFIGURE_ARGS+=	--with-cpu=generic_fpu
+.elif !empty(PKG_OPTIONS:Maltivec)
+CONFIGURE_ARGS+=	--with-cpu=altivec
+.elif !empty(PKG_OPTIONS:Mx86-dither)
+CONFIGURE_ARGS+=	--with-cpu=x86_dither
+.elif !empty(PKG_OPTIONS:Maltivec)
+CONFIGURE_ARGS+=	--with-cpu=altivec
 .endif
 
 .if !empty(PKG_OPTIONS:Mmpg123-no-fifo)
