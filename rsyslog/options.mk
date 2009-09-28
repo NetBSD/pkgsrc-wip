5b76b53916c5010e21a8a2d07d5b6efed41347d2
Bartosz Kuzma <bartosz.kuzma@gmail.com>
2009-09-28 22:32:45 +0000

    Add LICENSE. Remove @dirrm. Reorganize PLIST.
    Changes since 3.20.11:
    
    Version 3.22.1 [v3-stable] (rgerhards), 2009-07-02
    - bugfix: invalid error message issued if $inlcudeConfig was on an empty
      set of files (e.g. *.conf, where none such files existed)
      thanks to Michael Biebl for reporting this bug
    - bugfix: when run in foreground (but not in debug mode), a
      debug message ("DoDie called") was emitted at shutdown. Removed.
      thanks to Michael Biebl for reporting this bug
    - bugfix: some garbagge was emitted to stderr on shutdown. This
      garbage consisted of file names, which were written during
      startup (key point: not a pointer error)
      thanks to Michael Biebl for reporting this bug
    - bugfix: startup and shutdown message were emitted to stdout
      thanks to Michael Biebl for reporting this bug
    - bugfix: error messages were not emitted to stderr in forked mode
      (stderr and stdo are now kept open across forks)
    - bugfix: internal messages were emitted to whatever file had fd2 when
      rsyslogd ran in forked mode (as usual!)
      Thanks to varmojfekoj for the patch
    - small enhancement: config validation run now exits with code 1 if an
      error is detected. This change is considered important but small enough
      to apply it directly to the stable version. [But it is a border case,
      the change requires more code than I had hoped. Thus I have NOT tried
      to actually catch all cases, this is left for the current devel
      releases, if necessary]
    - bugfix: light and full delay watermarks had invalid values, badly
      affecting performance for delayable inputs
    - bugfix: potential segfault issue when multiple $UDPServerRun directives
      are specified. Thanks to Michael Biebl for helping to debug this one.
    - relaxed GnuTLS version requirement to 1.4.0 after confirmation from the
      field that this version is sufficient
    - bugfix: parser did not properly handle empty structured data
    - bugfix: invalid mutex release in msg.c (detected under thread debugger,
      seems not to have any impact on actual deployments)
    Version 3.22.0 [v3-stable] (rgerhards), 2009-04-21
    This is the first stable release that includes the full functionality
    of the 3.21.x version tree.
    - bugfix: $InputTCPMaxSessions config directive was accepted, but not
      honored. This resulted in a fixed upper limit of 200 connections.
    - bugfix: the default for $DirCreateMode was 0644, and as such wrong.
      It has now been changed to 0700. For some background, please see
      http://lists.adiscon.net/pipermail/rsyslog/2009-April/001986.html
    - bugfix: ompgsql did not detect problems in sql command execution
      this could cause loss of messages. The handling was correct if the
      connection broke, but not if there was a problem with statement
      execution. The most probable case for such a case would be invalid
      sql inside the template, and this is now much easier to diagnose.

--- rsyslog/options.mk
+++ rsyslog/options.mk
@@ -1,8 +1,10 @@
-# $NetBSD: options.mk,v 1.4 2008/12/07 14:16:32 bartoszkuzma Exp $
+# $NetBSD: options.mk,v 1.5 2009/09/28 22:32:45 bartoszkuzma Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.rsyslog
 PKG_SUPPORTED_OPTIONS=	gnutls gssapi largefile mail mysql pgsql snmp
 PKG_SUGGESTED_OPTIONS=	largefile
+
+PLIST_VARS+=		gnutls gssapi mail mysql pgsql snmp
 
 .include "../../mk/bsd.options.mk"
 
@@ -13,34 +15,34 @@ CONFIGURE_ARGS+=		--enable-largefile
 .if !empty(PKG_OPTIONS:Mgnutls)
 .include "../../security/gnutls/buildlink3.mk"
 CONFIGURE_ARGS+=		--enable-gnutls
-PLIST_SRC+=			PLIST.gnutls
+PLIST.gnutls=			yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mgssapi)
 .include "../../mk/krb5.buildlink3.mk"
 CONFIGURE_ARGS+=		--enable-gssapi-krb5
-PLIST_SRC+=			PLIST.gssapi
+PLIST.gssapi=			yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mmail)
 CONFIGURE_ARGS+=		--enable-mail
-PLIST_SRC+=			PLIST.mail
+PLIST.mail=			yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mmysql)
 .include "../../mk/mysql.buildlink3.mk"
 CONFIGURE_ARGS+=		--enable-mysql
-PLIST_SRC+=			PLIST.mysql
+PLIST.mysql=			yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mpgsql)
 .include "../../mk/pgsql.buildlink3.mk"
 CONFIGURE_ARGS+=		--enable-pgsql
-PLIST_SRC+=			PLIST.pgsql
+PLIST.pgsql=			yes
 .endif
 
 .if !empty(PKG_OPTIONS:Msnmp)
 .include "../../net/net-snmp/buildlink3.mk"
 CONFIGURE_ARGS+=		--enable-snmp
-PLIST_SRC+=			PLIST.snmp
+PLIST.snmp=			yes
 .endif
