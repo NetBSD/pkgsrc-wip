4e7db301845c167a782a498d7812864c0a9f7d31
Fredrik Pettai <pettai@NetBSD.org>
2009-09-29 21:22:03 +0000

    Fixed buildlink file based on fixed PKGNAME

--- softhsm/buildlink3.mk
+++ softhsm/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2009/09/29 20:44:23 pettai Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/09/29 21:22:03 pettai Exp $
 
-BUILDLINK_TREE+=	softhsm-1.0.0
+BUILDLINK_TREE+=	softhsm
 
-.if !defined(SOFTHSM_1.0.0_BUILDLINK3_MK)
-SOFTHSM_1.0.0_BUILDLINK3_MK:=
+.if !defined(SOFTHSM_BUILDLINK3_MK)
+SOFTHSM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.softhsm-1.0.0+=	softhsm-1.0.0>=RC3nb20090926
-BUILDLINK_PKGSRCDIR.softhsm-1.0.0?=	../../wip/softhsm
+BUILDLINK_API_DEPENDS.softhsm+=	softhsm>=1.0.0rc3nb20090929
+BUILDLINK_PKGSRCDIR.softhsm?=	../../wip/softhsm
 
 .include "../../databases/sqlite3/buildlink3.mk"
-.include "../../wip/botan/buildlink3.mk"
-.endif	# SOFTHSM_1.0.0_BUILDLINK3_MK
+.include "../../security/botan/buildlink3.mk"
+.endif	# SOFTHSM_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-softhsm-1.0.0
+BUILDLINK_TREE+=	-softhsm
