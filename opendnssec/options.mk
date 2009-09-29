28b03b732614e9688bf814b2fee636fa3aa80ba5
Fredrik Pettai <pettai@NetBSD.org>
2009-09-29 08:14:37 +0000

    Added license, breakout auditor + dependency to ruby

---
+++ opendnssec/options.mk
@@ -0,0 +1,29 @@
+# $NetBSD: options.mk,v 1.1 2009/09/29 08:14:37 pettai Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.opendnssec
+PKG_SUPPORTED_OPTIONS=		auditor softhsm
+PKG_SUGGESTED_OPTIONS=		auditor
+
+.include "../../mk/bsd.options.mk"
+
+###
+### Kasp Auditor
+###
+.if empty(PKG_OPTIONS:Mauditor)
+CONFIGURE_ARGS+=	--disable-auditor
+.else
+.include "../../lang/ruby/buildlink3.mk"
+
+DEPENDS+=	rubygems:../../misc/rubygems
+DEPENDS+=	${RUBY_PKGPREFIX}-dnsruby:../../wip/ruby-dnsruby
+.endif
+
+###
+### Kasp Auditor
+###
+.if empty(PKG_OPTIONS:Msofthsm)
+
+.else
+
+DEPENDS+=	softhsm:../../wip/softhsm
+.endif
