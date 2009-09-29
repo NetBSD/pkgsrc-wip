77232df11e7d0e28efb7ea5a40ead39675a5d1cc
Fredrik Pettai <pettai@NetBSD.org>
2009-09-29 21:54:44 +0000

    Fixed small cleanups

--- opendnssec/options.mk
+++ opendnssec/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2009/09/29 08:14:37 pettai Exp $
+# $NetBSD: options.mk,v 1.2 2009/09/29 21:54:44 pettai Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.opendnssec
 PKG_SUPPORTED_OPTIONS=		auditor softhsm
@@ -19,11 +19,12 @@ DEPENDS+=	${RUBY_PKGPREFIX}-dnsruby:../../wip/ruby-dnsruby
 .endif
 
 ###
-### Kasp Auditor
+### SoftHSM
 ###
 .if empty(PKG_OPTIONS:Msofthsm)
 
 .else
 
-DEPENDS+=	softhsm:../../wip/softhsm
+#DEPENDS+=	softhsm:../../wip/softhsm
+.include "../../wip/softhsm/buildlink3.mk"
 .endif
