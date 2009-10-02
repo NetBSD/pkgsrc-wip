ce2a78f2bddccd367703744d25d490413f5d5b31
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-10-02 01:54:10 +0000

    upstream update to convertible-1.0.5

--- hs-convertible/buildlink3.mk
+++ hs-convertible/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/05/29 05:24:07 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/10/02 01:54:10 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-convertible
 
 .if !defined(HS_CONVERTIBLE_BUILDLINK3_MK)
 HS_CONVERTIBLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-convertible+=	hs-convertible>=1.0.1
+BUILDLINK_API_DEPENDS.hs-convertible+=	hs-convertible>=1.0.5
 BUILDLINK_PKGSRCDIR.hs-convertible?=	../../wip/hs-convertible
 
 .include "../../wip/hs-mtl/buildlink3.mk"
