4bd84706fbc357c07d831bfdc42971f7958d4d11
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-10-02 07:40:28 +0000

    upstream update to HsOpenSSL-0.6.5

--- hs-HsOpenSSL/buildlink3.mk
+++ hs-HsOpenSSL/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/06/03 04:10:48 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/10/02 07:40:28 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-HsOpenSSL
 
 .if !defined(HS_HSOPENSSL_BUILDLINK3_MK)
 HS_HSOPENSSL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HsOpenSSL+=	hs-HsOpenSSL>=0.6.1
+BUILDLINK_API_DEPENDS.hs-HsOpenSSL+=	hs-HsOpenSSL>=0.6.5
 BUILDLINK_PKGSRCDIR.hs-HsOpenSSL?=	../../wip/hs-HsOpenSSL
 
 .include "../../security/openssl/buildlink3.mk"
