9dd8ec1a6dfb77b292ca42ce1d11e93f97ec2ad4
Thomas Klausner <wiz@NetBSD.org>
2009-10-04 21:42:40 +0000

    Install support files. Fixes amarok. Hint from Big O, thanks a lot!
    Bump PKGREVISION and depend on it.

--- mysql5-embedded/buildlink3.mk
+++ mysql5-embedded/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/10/03 17:17:08 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/10/04 21:42:40 thomasklausner Exp $
 
 BUILDLINK_TREE+=	mysql-embedded
 
 .if !defined(MYSQL_EMBEDDED_BUILDLINK3_MK)
 MYSQL_EMBEDDED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mysql-embedded+=	mysql-embedded>=5.1.36nb1
+BUILDLINK_API_DEPENDS.mysql-embedded+=	mysql-embedded>=5.1.36nb3
 BUILDLINK_PKGSRCDIR.mysql-embedded?=	../../wip/mysql5-embedded
 
 MYSQLD_PATH=		${BUILDLINK_PREFIX.mysql-embedded}/mysqld/lib/mysql
