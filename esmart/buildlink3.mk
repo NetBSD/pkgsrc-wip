20e78bc0372d6991e39d08a18465040f1aa34091
Aleksey Cheusov <cheusov@users.sourceforge.net>
2009-10-11 12:50:32 +0000

    fix: wip/eina was imported to devel/

--- esmart/buildlink3.mk
+++ esmart/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:43:41 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2009/10/11 12:50:32 cheusov Exp $
 
 BUILDLINK_TREE+=	esmart
 
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.esmart+=	esmart>=0.9.0.42nb2
 BUILDLINK_PKGSRCDIR.esmart?=	../../wip/esmart
 
 .include "../../wip/ecore/buildlink3.mk"
-.include "../../wip/eina/buildlink3.mk"
+.include "../../devel/eina/buildlink3.mk"
 .include "../../wip/edje/buildlink3.mk"
 .include "../../wip/epsilon/buildlink3.mk"
 .include "../../devel/libltdl/buildlink3.mk"
