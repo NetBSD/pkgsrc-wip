b6abad8a8801c5f12766dc91a041376616caf558
fulvio ciriaco <fulviociriaco@users.sourceforge.net>
2009-10-12 18:33:17 +0000

    adjusted PLIST adjusted Makefile so that boot from mlton is feasible, when and if my patch to pkgsrc is accepted.

--- mlton-svn/buildlink3.mk
+++ mlton-svn/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2009/10/10 19:54:51 fulviociriaco Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/10/12 18:33:17 fulviociriaco Exp $
 
 BUILDLINK_DEPMETHOD.mlton?=	build
 
@@ -7,11 +7,11 @@ BUILDLINK_TREE+=	mlton
 .if !defined(MLTON_BUILDLINK3_MK)
 MLTON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mlton+=	mlton>=1.0
+BUILDLINK_API_DEPENDS.mlton+=	mlton-*
 BUILDLINK_PKGSRCDIR.mlton?=	../../wip/mlton-svn
 BUILDLINK_INCDIRS.mlton= lib/mlton/include
 BUILDLINK_LIBDIRS.mlton= lib/mlton/self
-BUILDLINK_CONTENTS_FILTER.mlton= ${EGREP} '.*\.a$$|.*\.h$$' 
+#BUILDLINK_CONTENTS_FILTER.mlton= ${EGREP} '.*\.a$$|.*\.h$$' 
 .include "../../devel/gmp/buildlink3.mk"
 .endif	# MLTON_BUILDLINK3_MK
 
