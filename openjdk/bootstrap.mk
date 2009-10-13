8cdd897b1c368d2ad405af3397736506625ced23
Tobias Nygren <tnn@NetBSD.org>
2009-10-13 17:01:52 +0000

    - switch to MASTER_SITE_LOCAL (java.net is known to remove old distfiles) - repack the source .zip to .tar.bz2 (saves 45MB.) - tar up the certificates - reorganize bootstrap.mk so it only downloads the binaries we actually need - Add bootstrap support for more platforms:   - NetBSD 4.* i386   - NetBSD 6.* amd64 (really 5.99.*)

--- openjdk/bootstrap.mk
+++ openjdk/bootstrap.mk
@@ -1,29 +1,46 @@
-# $NetBSD: bootstrap.mk,v 1.5 2009/09/26 07:48:10 tnn2 Exp $
+# $NetBSD: bootstrap.mk,v 1.6 2009/10/13 17:01:52 tnn2 Exp $
 
-ONLY_FOR_PLATFORM=	NetBSD-5.*-i386 NetBSD-5.*-x86_64
+ONLY_FOR_PLATFORM=	NetBSD-[456].*-i386 NetBSD-[456].*-x86_64
 
-BOOTKIT.i386=		bootstrap-jdk-7-bin-netbsd-5-i386-20090926.tar.bz2
-SITES.${BOOTKIT.i386}=	http://mx1.nygren.pp.se/distfiles/
-DISTFILES+=		${BOOTKIT.i386}
+BOOT.nb4-i386=		bootstrap-jdk-7-bin-netbsd-4-i386-20091012.tar.bz2
+# not yet
+#BOOT.nb4-amd64=		bootstrap-jdk-7-bin-netbsd-4-amd64-20091013.tar.bz2
+BOOT.nb5-i386=		bootstrap-jdk-7-bin-netbsd-5-i386-20090926.tar.bz2
+BOOT.nb5-amd64=		bootstrap-jdk-7-bin-netbsd-5-amd64-20090925.tar.bz2
+# not yet
+#BOOT.nb6-i386=		bootstrap-jdk-7-bin-netbsd-6-i386-20091013.tar.bz2
+BOOT.nb6-amd64=		bootstrap-jdk-7-bin-netbsd-6-amd64-20091013.tar.bz2
 
-BOOTKIT.amd64=		bootstrap-jdk-7-bin-netbsd-5-amd64-20090925.tar.bz2
-SITES.${BOOTKIT.amd64}=	http://mx1.nygren.pp.se/distfiles/
-DISTFILES+=		${BOOTKIT.amd64}
+.if !empty(MACHINE_PLATFORM:MNetBSD-4.[0-8]*-i386) || make(distinfo)
+DISTFILES+=		${BOOT.nb4-i386}
+EXTRACT_ONLY+=		${BOOT.nb4-i386}
+.endif
 
-.if !empty(MACHINE_PLATFORM:MNetBSD-5.*-i386)
-EXTRACT_ONLY+=		${BOOTKIT.i386}
+.if !empty(MACHINE_PLATFORM:MNetBSD-4.[0-8]*-amd64) || make(distinfo)
+DISTFILES+=		${BOOT.nb4-amd64}
+EXTRACT_ONLY+=		${BOOT.nb4-amd64}
+.endif
+
+.if !empty(MACHINE_PLATFORM:MNetBSD-5.[0-8]*-i386) || make(distinfo)
+DISTFILES+=		${BOOT.nb5-i386}
+EXTRACT_ONLY+=		${BOOT.nb5-i386}
+.endif
+
+.if !empty(MACHINE_PLATFORM:MNetBSD-5.[0-8]*-x86_64) || make(distinfo)
+DISTFILES+=		${BOOT.nb5-amd64}
+EXTRACT_ONLY+=		${BOOT.nb5-amd64}
+.endif
+
+.if !empty(MACHINE_PLATFORM:MNetBSD-6.[0-8]*-i386) || make(distinfo) || \
+    !empty(MACHINE_PLATFORM:MNetBSD-5.99.*-i386)
+DISTFILES+=		${BOOT.nb6-i386}
+EXTRACT_ONLY+=		${BOOT.nb6-i386}
+.endif
+
+.if !empty(MACHINE_PLATFORM:MNetBSD-6.[0-8]*-x86_64) || make(distinfo) || \
+    !empty(MACHINE_PLATFORM:MNetBSD-5.99.*-x86_64)
+DISTFILES+=		${BOOT.nb6-amd64}
+EXTRACT_ONLY+=		${BOOT.nb6-amd64}
+.endif
+
 ALT_BOOTDIR=		${WRKDIR}/bootstrap
-.endif
-
-.if !empty(MACHINE_PLATFORM:MNetBSD-5.*-x86_64)
-EXTRACT_ONLY+=		${BOOTKIT.amd64}
-ALT_BOOTDIR=		${WRKDIR}/bootstrap
-.endif
-
-.if !exists(/usr/lib/libstdc++.so.6) || !exists(/usr/lib/libpthread.so.0)
-PKG_FAIL_REASON+=	"Using NetBSD-current? If so, this package can build a native JDK for you."
-PKG_FAIL_REASON+=	"But during the build, two compatibility libraries from NetBSD 5.0 must"
-PKG_FAIL_REASON+=	"be available. Please fetch libstdc++.so.6 and libpthread.so.0 from the"
-PKG_FAIL_REASON+=	"NetBSD 5.0 distribution and drop them in /usr/lib . After the package"
-PKG_FAIL_REASON+=	"Has been built you can remove them."
-.endif
