45601f9cd6fade69852a3b6298db568b20eed5db
Eric Schnoebelen <schnoebe@NetBSD.org>
2009-11-03 00:19:28 +0000

    Bring up to date with Poco 1.3.5, make some  of the modules more configurable.

--- poco/buildlink3.mk
+++ poco/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:43:51 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/11/03 00:19:28 schnoebe Exp $
 
 BUILDLINK_TREE+=	poco
 
 .if !defined(POCO_BUILDLINK3_MK)
 POCO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.poco+=	poco>=1.3.3p1
+BUILDLINK_API_DEPENDS.poco+=	poco>=1.3.5
 BUILDLINK_PKGSRCDIR.poco?=	../../wip/poco
 
 .include "../../security/openssl/buildlink3.mk"
