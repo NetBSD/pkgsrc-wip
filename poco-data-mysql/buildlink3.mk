85429cca93a4a40ee676c63064388fdaa2608e9b
Eric Schnoebelen <schnoebe@NetBSD.org>
2009-11-05 05:08:41 +0000

    Correct the BUILDLINK_{API_DEPENDS,PKGSRCDIR} variables.

--- poco-data-mysql/buildlink3.mk
+++ poco-data-mysql/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/11/03 00:17:55 schnoebe Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/11/05 05:08:41 schnoebe Exp $
 
 BUILDLINK_TREE+=	poco-data-mysql
 
 .if !defined(POCO_DATA_MYSQL_BUILDLINK3_MK)
 POCO_DATA_MYSQL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.poco-data-mysql+=	poco-data>=1.3.5
-BUILDLINK_PKGSRCDIR.poco-data-mysql?=	../../wip/poco-data
+BUILDLINK_API_DEPENDS.poco-data-mysql+=	poco-data-mysql>=1.3.5
+BUILDLINK_PKGSRCDIR.poco-data-mysql?=	../../wip/poco-data-mysql
 
 .include "../../databases/mysql5-client/buildlink3.mk"
 .include "../../wip/poco-data/buildlink3.mk"
--- poco-data-sqlite/buildlink3.mk
+++ poco-data-sqlite/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/11/03 00:16:26 schnoebe Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/11/05 05:09:44 schnoebe Exp $
 
 BUILDLINK_TREE+=	poco-data-sqlite
 
 .if !defined(POCO_DATA_SQLITE_BUILDLINK3_MK)
 POCO_DATA_SQLITE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.poco-data-sqlite+=	poco-data>=1.3.5
-BUILDLINK_PKGSRCDIR.poco-data-sqlite?=	../../wip/poco-data
+BUILDLINK_API_DEPENDS.poco-data-sqlite+=	poco-data-sqlite>=1.3.5
+BUILDLINK_PKGSRCDIR.poco-data-sqlite?=	../../wip/poco-data-sqlite
 
 .include "../../wip/poco-data/buildlink3.mk"
 .endif # POCO_DATA_SQLITE_BUILDLINK3_MK
