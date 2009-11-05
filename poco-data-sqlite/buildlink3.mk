53f83e483222586808d1ac9aabaf3ade6f5739f6
Eric Schnoebelen <schnoebe@NetBSD.org>
2009-11-05 05:23:04 +0000

    Make  sure to pull in the sqlite3/buildlink3.mk in our buildlink3.mk.

--- poco-data-sqlite/buildlink3.mk
+++ poco-data-sqlite/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/11/05 05:09:44 schnoebe Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/11/05 05:23:04 schnoebe Exp $
 
 BUILDLINK_TREE+=	poco-data-sqlite
 
@@ -8,6 +8,7 @@ POCO_DATA_SQLITE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.poco-data-sqlite+=	poco-data-sqlite>=1.3.5
 BUILDLINK_PKGSRCDIR.poco-data-sqlite?=	../../wip/poco-data-sqlite
 
+.include "../../databases/sqlite3/buildlink3.mk"
 .include "../../wip/poco-data/buildlink3.mk"
 .endif # POCO_DATA_SQLITE_BUILDLINK3_MK
 
