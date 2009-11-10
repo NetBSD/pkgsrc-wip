df402d303897e73ac55135850847e128354178c5
Eric Schnoebelen <schnoebe@NetBSD.org>
2009-11-10 21:28:30 +0000

    update to 0.14-rc1 of Psi.

--- psi/options.mk
+++ psi/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1 2007/12/27 16:43:44 schnoebe Exp $
+# $NetBSD: options.mk,v 1.2 2009/11/10 21:28:30 schnoebe Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.psi
-PKG_SUPPORTED_OPTIONS=	aspell
+PKG_SUPPORTED_OPTIONS=	aspell enchant
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,4 +10,11 @@ PKG_SUPPORTED_OPTIONS=	aspell
 CONFIGURE_ARGS+=	--with-aspell-inc=${BUILDLINK_PREFIX.aspell}/include
 CONFIGURE_ARGS+=	--with-aspell-lib=${BUILDLINK_PREFIX.aspell}/lib
 .include "../../textproc/aspell/buildlink3.mk"
+.endif
+
+# enable aspell
+.if empty(PKG_OPTIONS:Menchant)
+CONFIGURE_ARGS+=	--disable-enchant
+.else
+.include "../../textproc/enchant/buildlink3.mk"
 .endif
