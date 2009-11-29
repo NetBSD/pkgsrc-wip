8d58858a0fd3e4196e11237cdc2f327b62169d06
Tobias Nygren <tnn@NetBSD.org>
2009-11-29 00:22:31 +0000

    add the usual package options

---
+++ seamonkey/options.mk
@@ -0,0 +1,49 @@
+# $NetBSD: options.mk,v 1.6 2009/11/29 00:22:31 tnn2 Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.seamonkey
+PKG_SUPPORTED_OPTIONS=	debug mozilla-jemalloc gnome
+
+PLIST_VARS+=	gnome debug
+
+.if ${OPSYS} == "Linux" || ${OPSYS} == "SunOS"
+PKG_SUGGESTED_OPTIONS+=	mozilla-jemalloc
+.endif
+
+.if !empty(MACHINE_ARCH:Mi386) || !empty(MACHINE_ARCH:Msparc) || \
+	!empty(MACHINE_ARCH:Marm)
+PKG_SUPPORTED_OPTIONS+=	mozilla-jit
+PKG_SUGGESTED_OPTIONS+=	mozilla-jit
+NANOJIT_ARCH.i386=	i386
+NANOJIT_ARCH.arm=	ARM
+NANOJIT_ARCH.sparc=	Sparc
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgnome)
+.include "../../devel/libgnomeui/buildlink3.mk"
+.include "../../sysutils/gnome-vfs/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-gnomevfs --enable-dbus --enable-gnomeui
+PLIST.gnome=		yes
+.else
+CONFIGURE_ARGS+=	--disable-gnomevfs --disable-dbus --disable-gnomeui
+.endif
+
+.if !empty(PKG_OPTIONS:Mmozilla-jemalloc)
+CONFIGURE_ARGS+=	--enable-jemalloc
+.else
+CONFIGURE_ARGS+=	--disable-jemalloc
+.endif
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug
+PLIST.debug=		yes
+.else
+CONFIGURE_ARGS+=	--disable-debug
+.endif
+
+.if !empty(PKG_OPTIONS:Mmozilla-jit)
+CONFIGURE_ARGS+=	--enable-jit
+.else
+CONFIGURE_ARGS+=	--disable-jit
+.endif
