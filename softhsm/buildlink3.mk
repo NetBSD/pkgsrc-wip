d8d70062c6af1f777211fc8a376d55833309d0c1
Fredrik Pettai <pettai@NetBSD.org>
2009-12-04 22:03:00 +0000

    Changelog:
    * Bugfix: Signing could not be done when another application had a lock on the database.
    * Bugfix: Check if a session were able to create a connection to the database.

--- softhsm/buildlink3.mk
+++ softhsm/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/09/29 21:22:03 pettai Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/12/04 22:03:00 pettai Exp $
 
 BUILDLINK_TREE+=	softhsm
 
 .if !defined(SOFTHSM_BUILDLINK3_MK)
 SOFTHSM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.softhsm+=	softhsm>=1.0.0rc3nb20090929
+BUILDLINK_API_DEPENDS.softhsm+=	softhsm>=1.1.1
 BUILDLINK_PKGSRCDIR.softhsm?=	../../wip/softhsm
 
 .include "../../databases/sqlite3/buildlink3.mk"
