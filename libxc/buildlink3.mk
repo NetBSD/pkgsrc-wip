07ce819272bab12182a6805cb48556bfd938f199
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2009-12-05 21:06:58 +0000

    Import libxc-1.0 as wip/libxc.
    Libxc is the ETSF library of exchange-correlation functionals. The aim is to
    provide a portable, well tested and reliable set of exchange and correlation
    functionals that can be used by all the ETSF codes and also other codes.

---
+++ libxc/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/12/05 21:06:58 jihbed Exp $
+
+BUILDLINK_TREE+=	libxc
+
+.if !defined(LIBXC_BUILDLINK3_MK)
+LIBXC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libxc+=	libxc>=1.0
+BUILDLINK_PKGSRCDIR.libxc?=	../../wip/libxc
+.endif	# LIBXC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libxc
