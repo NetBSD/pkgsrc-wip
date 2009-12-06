96475332bf134f9c4404ed6523ea228a320bf1a7
OBATA Akio <obache@users.sourceforge.net>
2009-12-06 13:21:17 +0000

    Jumped to 1.4.x branch.

--- milter-manager/files/milter-manager.sh
+++ milter-manager/files/milter-manager.sh
@@ -11,7 +11,7 @@ name="milter_manager"
 rcvar=$name
 command="@PREFIX@/sbin/milter-manager"
 pidfile="@VARBASE@/run/milter-manager/milter-manager.pid"
-command_args="--pid-file ${pidfile} --daemon --user-name @MILTER_MANAGER_USER@ --group-name @MILTER_MANAGER_GROUP@ --socket-group-name @MILTER_MANAGER_SOCKET_GROUP@"
+command_args="--daemon"
 extra_command="reload"
 
 load_rc_config ${name}
