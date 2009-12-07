4585f42b5eee76049101201acb6b74c85eb7fb75
Thomas Klausner <wiz@NetBSD.org>
2009-12-07 23:57:03 +0000

    Sync options with available configure options.

--- scummvm-nightly/options.mk
+++ scummvm-nightly/options.mk
@@ -1,13 +1,14 @@
-# $NetBSD: options.mk,v 1.19 2009/08/15 17:02:53 dillo Exp $
+# $NetBSD: options.mk,v 1.20 2009/12/07 23:57:03 thomasklausner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scummvm-nightly
 PKG_SUPPORTED_OPTIONS=	scummvm-nightly-cruise
-PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-igor
+PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-groovie2
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-lol
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-m4
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-saga2
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-sci
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-sci32
+PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-teenagent
 PKG_SUPPORTED_OPTIONS+=	scummvm-nightly-tinsel
 PKG_SUGGESTED_OPTIONS=	${PKG_SUPPORTED_OPTIONS}
 
@@ -16,8 +17,8 @@ PKG_SUGGESTED_OPTIONS=	${PKG_SUPPORTED_OPTIONS}
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-cruise)
 CONFIGURE_ARGS+=--enable-cruise
 .endif
-.if !empty(PKG_OPTIONS:Mscummvm-nightly-igor)
-CONFIGURE_ARGS+=--enable-igor
+.if !empty(PKG_OPTIONS:Mscummvm-nightly-groovie2)
+CONFIGURE_ARGS+=--enable-groovie2
 .endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-lol)
 CONFIGURE_ARGS+=--enable-lol
@@ -33,6 +34,9 @@ CONFIGURE_ARGS+=--enable-sci
 .endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-sci32)
 CONFIGURE_ARGS+=--enable-sci32
+.endif
+.if !empty(PKG_OPTIONS:Mscummvm-nightly-teenagent)
+CONFIGURE_ARGS+=--enable-teenagent
 .endif
 .if !empty(PKG_OPTIONS:Mscummvm-nightly-tinsel)
 CONFIGURE_ARGS+=--enable-tinsel
