bb553608fe3941e1423bc0164bbef4f09f8f2640
Fredrik Pettai <pettai@NetBSD.org>
2009-12-13 23:12:56 +0000

    Some cleanup, added sigver debug option.

--- softhsm/options.mk
+++ softhsm/options.mk
@@ -1,15 +1,14 @@
-# $NetBSD: options.mk,v 1.1 2009/11/07 22:45:18 pettai Exp $
+# $NetBSD: options.mk,v 1.2 2009/12/13 23:12:56 pettai Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.softhsm
-PKG_SUPPORTED_OPTIONS=		64bit
+PKG_SUPPORTED_OPTIONS=		sigver
 PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
 
 ###
-### 64-bit
+### Signature verification for debugging
 ###
-.if !empty(PKG_OPTIONS:M64bit)
-CONFIGURE_ARGS+=	--enable-64bit
-
+.if !empty(PKG_OPTIONS:Msigver)
+CONFIGURE_ARGS+=	--enable-sigver
 .endif
