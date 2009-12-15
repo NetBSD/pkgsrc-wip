1793157d2c1db53d85fa8117d6acff92df696d42
Filip Hajny <filip@joyent.com>
2009-12-15 11:51:51 +0000

    Updated sphinxsearch to 0.9.9.
    One year worth of changes. Most notable ones:
    
      * Index merging is now much more stable.
      * Sphinx now has official IANA assigned ports, 9312 for native API
        and 9306 for SphinxQL.
    
    See full release log:
    
      http://sphinxsearch.com/docs/current.html#rel099

--- sphinxsearch/buildlink3.mk
+++ sphinxsearch/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/10/01 13:37:31 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/12/15 11:51:51 fhajny Exp $
 
 BUILDLINK_TREE+=			sphinxsearch
 
 .if !defined(SPHINXSEARCH_BUILDLINK3_MK)
 SPHINXSEARCH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.sphinxsearch+=	sphinxsearch>=0.9.8.1nb1
-BUILDLINK_PKGSRCDIR.sphinxsearch?=	../../joyent/sphinxsearch
+BUILDLINK_API_DEPENDS.sphinxsearch+=	sphinxsearch>=0.9.9
+BUILDLINK_PKGSRCDIR.sphinxsearch?=	../../wip/sphinxsearch
 
 .endif	# SPHINXSEARCH_BUILDLINK3_MK
 
