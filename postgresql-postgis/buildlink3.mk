eaba919ff49d0e9d7b6d39fbcbfe1c90e4511162
Adam Ciarciński <adam@NetBSD.org>
2009-12-17 10:17:47 +0000

    buildlink3.mk again

--- postgresql-postgis/buildlink3.mk
+++ postgresql-postgis/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2009/12/17 10:11:35 moubctez Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/12/17 10:17:47 moubctez Exp $
 
 BUILDLINK_TREE+=	postgresql84-postgis
 
 .if !defined(POSTGRESQL84_POSTGIS_BUILDLINK3_MK)
 POSTGRESQL84_POSTGIS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.postgresql84-postgis+=	postgresql84-postgis>=1.4.1
+BUILDLINK_API_DEPENDS.postgresql84-postgis+=	postgresql84-postgis>=1.4
 BUILDLINK_PKGSRCDIR.postgresql84-postgis?=	../../wip/postgresql-postgis
 
 .include "../../geography/geos/buildlink3.mk"
