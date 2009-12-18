6cf97027a8a0f46d5f8f63b274247e817001fff5
Olaf Seibert <rhialto@falu.nl>
2009-12-18 13:35:32 +0000

    Import e-uae-0.8.29wip4 as wip/e-uae.
    Newer "-WIP4" version than in emulators/e-uae, but nevertheless it hasn't
    been updated in nearly 3 years.
    
    This is a fork of UAE, the Ubiquitous Amiga Emulator, with an emulation core
    based on WinUAE. It attempts to bring many of the great features of WinUAE
    to non-Windows platforms. This version now finally has a name, E-UAE, since
    that's what everybody was calling it anyway. The 'E' can stand for anything
    you fancy. Experimental, extreme, exciting, egalitarian, eggplant...
    
    E-UAE is open-source software and is made available under the terms of the
    GPL. See the COPYING file included with the E-UAE archive for details.
    
    E-UAE is based on the work of dozens of contributors including Bernd Schmidt
    (the original author and maintainer of UAE), Bernie Meyer (the author of the
    x86 JIT compiler), Toni Wilen (the current maintainer of WinUAE), and many
    more.
    
    To make full use of E-UAE you will need access to an image of some version
    of the Amiga Kickstart ROM (although UAE does include a ROM emulation which
    may work with some old games). The Amiga Kickstart ROMs are copyrighted
    material and may not be freely distributed (so don't ask me for a copy). If
    you don't have an Amiga from which you can legally make a ROM image, a
    selection of ROM images are available for purchase online from Cloanto
    (publishers of the Amiga Forever distribution). See
    http://www.amigaforever.com/

---
+++ e-uae/options.mk
@@ -0,0 +1,31 @@
+# $NetBSD: options.mk,v 1.1.1.1 2009/12/18 13:35:32 rhialto Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.e-uae
+PKG_SUPPORTED_OPTIONS=	gtk sdl x11
+PKG_SUGGESTED_OPTIONS=	gtk sdl
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgtk)
+.include "../../x11/gtk2/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Msdl)
+CONFIGURE_ARGS+=	--with-sdl --with-sdl-gfx
+.include "../../devel/SDL/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mx11)
+CONFIGURE_ARGS+=	--enable-dga --enable-vidmode
+
+BUILDLINK_DEPMETHOD.libXt?=	build
+
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libxkbfile/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/libXxf86dga/buildlink3.mk"
+.include "../../x11/libXxf86vm/buildlink3.mk"
+.include "../../x11/xextproto/buildlink3.mk"
+.endif
