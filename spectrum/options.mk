2f96ab178f8865e723a933cf70d046d1976409fd
Eric Schnoebelen <schnoebe@NetBSD.org>
2009-12-25 04:59:56 +0000

    Add debug options; change SUBST_FILES.cfgpath files.

--- spectrum/options.mk
+++ spectrum/options.mk
@@ -1,8 +1,9 @@
-# $NetBSD: options.mk,v 1.1.1.1 2009/11/10 22:49:49 schnoebe Exp $
+# $NetBSD: options.mk,v 1.2 2009/12/25 04:59:56 schnoebe Exp $
 
 PKG_OPTIONS_VAR=    		PKG_OPTIONS.spectrum
-PKG_OPTIONS_REQUIRED_GROUPS=	storage
+PKG_OPTIONS_REQUIRED_GROUPS=	storage debug
 PKG_OPTIONS_GROUP.storage=	storage-mysql storage-sqlite
+PKG_OPTIONS_GROUP.debug=	debug
 
 PKG_SUGGESTED_OPTIONS=		storage-sqlite
 
@@ -18,4 +19,9 @@ PLIST.sqlite=	yes
 .if !empty(PKG_OPTIONS:Mstorage-mysql)
 . include "../../wip/poco-data-mysql/buildlink3.mk"
 PLIST.mysql=	yes
+.endif
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CMAKE_ARGS+=	-DCMAKE_BUILD_TYPE=Debug
+. include "../../devel/cppunit/buildlink3.mk"
 .endif
