bc316d6f3e9214e121007ea0a7b5f779bd165d50
PHO / phonohawk <phonohawk@users.sourceforge.net>
2009-12-25 05:49:51 +0000

    Upstream update to GHC 6.12.1

--- ghc/buildlink3.mk
+++ ghc/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/09/24 08:12:58 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2009/12/25 05:49:51 phonohawk Exp $
 
 BUILDLINK_TREE+=	ghc
 
 .if !defined(GHC_BUILDLINK3_MK)
 GHC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ghc+=	ghc>=6.10.4
+BUILDLINK_API_DEPENDS.ghc+=	ghc>=6.12.1
 BUILDLINK_PKGSRCDIR.ghc?=	../../wip/ghc
 
+.include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/gmp/buildlink3.mk"
 .endif	# GHC_BUILDLINK3_MK
 
---
+++ ghc/files/bootstrap.mk
@@ -0,0 +1,18 @@
+GHC = false
+GHC_PKG_INPLACE =
+GHC_CABAL_INPLACE =
+DUMMY_GHC_INPLACE =
+UNLIT =
+NO_INCLUDE_DEPS = YES
+GhcUnregisterised = YES
+GhcLibWays = v
+GhcRTSWays =
+SplitObjs = NO
+GhcWithNativeCodeGen = NO
+GhcWithInterpreter = NO
+GhcWithSMP = NO
+ghc_stage2_v_EXTRA_CC_OPTS += -Llibraries/integer-gmp/gmp -L[[PREFIX]]/lib -lm -liconv
+utils/ghc-pkg_dist-install_v_EXTRA_CC_OPTS += -Llibraries/integer-gmp/gmp -L[[PREFIX]]/lib -lm -liconv -ltermcap
+
+SRC_CC_OPTS += -I[[PREFIX]]/include
+INTEGER_LIBRARY = integer-simple
