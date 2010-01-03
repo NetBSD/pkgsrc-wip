1737311ef36be536d78a02d95a851026d466fe2d
Pierre Allegraud <pallegra@users.sourceforge.net>
2010-01-03 17:57:53 +0000

    Use wip/libdrm.

--- gl/dri.mk
+++ gl/dri.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dri.mk,v 1.1.1.1 2010/01/03 14:32:17 pallegra Exp $
+# $NetBSD: dri.mk,v 1.2 2010/01/03 17:57:53 pallegra Exp $
 
 BUILDLINK_API_DEPENDS.libdrm+= libdrm>=2.4.15
 .  include "../../textproc/expat/buildlink3.mk"
@@ -6,7 +6,7 @@ BUILDLINK_API_DEPENDS.libdrm+= libdrm>=2.4.15
 .  include "../../x11/glproto/buildlink3.mk"
 .  include "../../x11/libXdamage/buildlink3.mk"
 .  include "../../x11/libXfixes/buildlink3.mk"
-.  include "../../x11/libdrm/buildlink3.mk"
+.  include "../../wip/libdrm/buildlink3.mk"
 .  include "../../x11/libXxf86vm/buildlink3.mk"
 .  include "../../x11/xf86driproto/buildlink3.mk"
 .  include "../../x11/xf86vidmodeproto/buildlink3.mk"
