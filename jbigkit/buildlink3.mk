08d5182ccb2599cf24e8ce85211c68c7bc6ecc53
OBATA Akio <obache@users.sourceforge.net>
2010-01-04 13:32:28 +0000

    libtoolized.

--- jbigkit/buildlink3.mk
+++ jbigkit/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:43:43 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2010/01/04 13:32:29 obache Exp $
 
 BUILDLINK_TREE+=	jbigkit
 
 .if !defined(JBIGKIT_BUILDLINK3_MK)
 JBIGKIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.jbigkit+=	jbigkit>=1.6
+BUILDLINK_API_DEPENDS.jbigkit+=	jbigkit>=2.0
 BUILDLINK_PKGSRCDIR.jbigkit?=	../../wip/jbigkit
-BUILDLINK_DEPMETHOD.jbigkit?=	build
 .endif # JBIGKIT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-jbigkit
