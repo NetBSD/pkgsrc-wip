2581c3e45dc0800c35b1580517b3e5394f903456
Tobias Nygren <tnn@NetBSD.org>
2010-01-11 11:42:47 +0000

    Update to Firefox 3.6 release candidate 1.

--- xulrunner/buildlink3.mk
+++ xulrunner/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/12/02 15:03:01 tnn2 Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2010/01/11 11:42:48 tnn2 Exp $
 
 BUILDLINK_TREE+=	xulrunner
 
 .if !defined(XULRUNNER_BUILDLINK3_MK)
 XULRUNNER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=1.9.2b4
-BUILDLINK_ABI_DEPENDS.xulrunner?=	xulrunner>=1.9.2b4
+BUILDLINK_API_DEPENDS.xulrunner+=	xulrunner>=1.9.2rc1
 BUILDLINK_PKGSRCDIR.xulrunner?=		../../wip/xulrunner
 
 pkgbase := xulrunner
--- xulrunner/dist.mk
+++ xulrunner/dist.mk
@@ -1,10 +1,10 @@
-# $NetBSD: dist.mk,v 1.2 2009/12/19 23:58:17 tnn2 Exp $
+# $NetBSD: dist.mk,v 1.3 2010/01/11 11:42:48 tnn2 Exp $
 #
 # used by wip/xulrunner/Makefile
 # used by wip/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	3.6b5
+FIREFOX_VER=	3.6rc1
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
