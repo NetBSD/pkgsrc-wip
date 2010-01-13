3a55fec3571cccf7652cfbc432adcdd9243a3aa2
X400 <x400@users.sourceforge.net>
2010-01-13 13:21:04 +0000

    *** empty log message ***

--- callweaver/options.mk
+++ callweaver/options.mk
@@ -1,11 +1,11 @@
-# $NetBSD: options.mk,v 1.4 2009/10/15 16:19:32 x400 Exp $
+# $NetBSD: options.mk,v 1.5 2010/01/13 13:21:04 x400 Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.callweaver
-PKG_SUPPORTED_OPTIONS=	jabber mysql pgsql sipsecurity
+PKG_SUPPORTED_OPTIONS=	jabber mysql pgsql sipsecurity snmp
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		jabber mysql pgsql sipsecurity
+PLIST_VARS+=		jabber mysql pgsql sipsecurity snmp
 
 .if !empty(PKG_OPTIONS:Mjabber)
 .include "../../chat/loudmouth/buildlink3.mk"
@@ -28,4 +28,10 @@ CONFIGURE_ARGS+=	--enable-postgresql --with-app_sql_postgres --with-cdr_pgsql --
 .if !empty(PKG_OPTIONS:Msipsecurity)
 PLIST.sipsecurity=	yes
 CONFIGURE_ARGS+=	--enable-sip-tcp-support --enable-srtp
+.endif
+
+.if !empty(PKG_OPTIONS:Msnmp)
+.include "../../net/net-snmp/buildlink3.mk"
+PLIST.snmp=		yes
+CONFIGURE_ARGS+=	--with-res_snmp
 .endif
