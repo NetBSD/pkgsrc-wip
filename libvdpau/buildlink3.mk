528bce418526ef09e39d6ba25cf397fbb3f6dbe3
Aleksey Cheusov <cheusov@users.sourceforge.net>
2010-01-14 11:15:29 +0000

    bl3 for linking

---
+++ libvdpau/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2010/01/14 11:15:29 cheusov Exp $
+BUILDLINK_TREE+=	libvdpau
+
+.if !defined(LIBVDPAU_BUILDLINK3_MK)
+LIBVDPAU_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libvdpau+=	libvdpau>=0.3
+BUILDLINK_PKGSRCDIR.libvdpau?=	../../wip/libvdpau
+
+# XXX Uncomment and keep only the buildlink3 lines below which are directly
+# XXX needed for dependencies to compile, link, and run.  If this package
+# XXX provides a wrappered API or otherwise does not expose the APIs of the
+# XXX buildlink3 lines below to dependencies, remove them.
+# XXX
+.include "../../x11/libX11/buildlink3.mk"
+.endif	# LIBVDPAU_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libvdpau
