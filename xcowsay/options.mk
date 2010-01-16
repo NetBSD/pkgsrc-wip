a9392aebd562fc96c379ebeff82d7c2ea74cde54
Leonardo Taccari <leot@NetBSD.org>
2010-01-16 20:34:54 +0000

    Update to xcowsay-1.2.
    Changes:
     - new --image option allows you to use your own images instead of the
       cow
     - new --monitor option that forces the cow to appear on a particular
       monitor
     - improved config file support
     - lots of minor bugs fixed

--- xcowsay/options.mk
+++ xcowsay/options.mk
@@ -1,7 +1,6 @@
-# $NetBSD: options.mk,v 1.1.1.1 2008/10/10 19:38:49 leot1990 Exp $
+# $NetBSD: options.mk,v 1.2 2010/01/16 20:34:54 leot1990 Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.xcowsay
-
 PKG_SUPPORTED_OPTIONS=		dbus
 
 .include "../../mk/bsd.options.mk"
