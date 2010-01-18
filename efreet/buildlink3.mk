cfd4598ea7f20925480e1b8b7135b4fc091ac927
Jörg Sonnenberger <joerg@NetBSD.org>
2010-01-18 19:40:12 +0000

    Update to 2009/12 snapshot.

--- efreet/buildlink3.mk
+++ efreet/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2010/01/18 09:38:19 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2010/01/18 19:40:12 jsonn Exp $
 
 BUILDLINK_TREE+=	efreet
 
 .if !defined(EFREET_BUILDLINK3_MK)
 EFREET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.efreet+=	efreet>=0.5.0.062
-BUILDLINK_ABI_DEPENDS.efreet?=	efreet>=0.5.0.062nb3
+BUILDLINK_API_DEPENDS.efreet?=	efreet>=0.5.0.063
 BUILDLINK_PKGSRCDIR.efreet?=	../../wip/efreet
 
 .include        "../../wip/ecore/buildlink3.mk"
--- elementary/buildlink3.mk
+++ elementary/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2010/01/18 09:38:19 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2010/01/18 19:41:05 jsonn Exp $
 
 BUILDLINK_TREE+=	elementary
 
 .if !defined(ELEMENTARY_BUILDLINK3_MK)
 ELEMENTARY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.elementary+=	elementary>=0.5.1.0
-BUILDLINK_ABI_DEPENDS.elementary?=	elementary>=0.5.1.0nb2
+BUILDLINK_API_DEPENDS.elementary+=	elementary>=0.6.0.63
 BUILDLINK_PKGSRCDIR.elementary?=	../../wip/elementary
 
 .include "../../wip/ecore/buildlink3.mk"
