a24d0b0886f25ca0246a2131dcee8b6543155bc2
Tobias Nygren <tnn@NetBSD.org>
2010-01-20 07:53:21 +0000

    Update to firefox-3.6rc2.

--- xulrunner/dist.mk
+++ xulrunner/dist.mk
@@ -1,10 +1,10 @@
-# $NetBSD: dist.mk,v 1.3 2010/01/11 11:42:48 tnn2 Exp $
+# $NetBSD: dist.mk,v 1.4 2010/01/20 07:53:21 tnn2 Exp $
 #
 # used by wip/xulrunner/Makefile
 # used by wip/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	3.6rc1
+FIREFOX_VER=	3.6rc2
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
