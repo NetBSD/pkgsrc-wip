e5ae42f7cbefe079f44f4126a1fe3f9ae5189c9b
Gary D Duzan <gary@duzan.org>
2010-01-31 19:35:50 +0000

    Add kmymoney-svn, the Qt4/KDE4 development version of KMymoney.

---
+++ kmymoney-svn/options.mk
@@ -0,0 +1,14 @@
+# $NetBSD: options.mk,v 1.1 2010/01/31 19:35:50 gduzan Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.kmymoney2
+PKG_SUPPORTED_OPTIONS=	libofx 
+PKG_SUGGESTED_OPTIONS=	libofx
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		libofx #sqlite3
+
+.if !empty(PKG_OPTIONS:Mlibofx)
+.include "../../finance/libofx/buildlink3.mk"
+PLIST.libofx=		yes
+.endif
