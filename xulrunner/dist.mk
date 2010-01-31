7e9dc42921cd12e34245a3cea83413251bc8c2a8
Tobias Nygren <tnn@NetBSD.org>
2010-01-31 21:13:25 +0000

    merge from pkgsrc

--- xulrunner/dist.mk
+++ xulrunner/dist.mk
@@ -1,14 +1,16 @@
-# $NetBSD: dist.mk,v 1.5 2010/01/22 23:44:19 tnn2 Exp $
+# $NetBSD: dist.mk,v 1.6 2010/01/31 21:13:25 tnn2 Exp $
 #
 # used by wip/xulrunner/Makefile
 # used by wip/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	3.6
+FIREFOX_VER=	3.6	# .${MOZ_BRANCH_MINOR}
+MOZ_BRANCH=	1.9.2
+MOZ_BRANCH_MINOR=	# empty
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
 DISTINFO_FILE=	${.CURDIR}/../../wip/xulrunner/distinfo
 PATCHDIR=	${.CURDIR}/../../wip/xulrunner/patches
 
-WRKSRC=		${WRKDIR}/mozilla-1.9.2
+WRKSRC=		${WRKDIR}/mozilla-${MOZ_BRANCH}
