fef41e6e8735def8c3e841bc0cc598d059af1004
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-02-04 06:53:54 +0000

    Upstream update to HsSVN-0.4.2

--- hs-HsSVN/buildlink3.mk
+++ hs-HsSVN/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/01/17 12:03:26 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2010/02/04 06:53:54 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-HsSVN
 
 .if !defined(HS_HSSVN_BUILDLINK3_MK)
 HS_HSSVN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HsSVN+=	hs-HsSVN>=0.4.1
-BUILDLINK_ABI_DEPENDS.hs-HsSVN?=	hs-HsSVN>=0.4.1nb1
+BUILDLINK_API_DEPENDS.hs-HsSVN+=	hs-HsSVN>=0.4.2
 BUILDLINK_PKGSRCDIR.hs-HsSVN?=	../../wip/hs-HsSVN
 
 .include "../../wip/hs-mtl/buildlink3.mk"
