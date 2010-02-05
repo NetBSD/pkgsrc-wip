4487af7565a426205d395c752fbce0aaaf75ceef
Jonathan A. Kollasch <jakllsch@NetBSD.org>
2010-02-05 21:36:22 +0000

    Update to remctl-2.15.
    Changes since 2.14:
    
     Allow subcommand to be omitted on the remctl command line, which
     sends a command without a subcommand. This makes available on the
     command line functionality that was already available via the
     library API.
    
     Add the special keyword EMPTY for the subcommand field in the
     remctld configuration file, specifying that this line should only
     match commands with no subcommands.
    
     Allow use of ALL in the command field in the remctld configuration
     file as well as the subcommand field, matching all commands.
    
     Fix read of uninitialized memory caused by moving one character
     beyond the beginning of the buffer when parsing blank lines in
     ACL files.
    
     Use a socket_type typedef rather than int directly to store the
     file descriptors of sockets and, on Windows, typedef that to SOCKET
     instead of int. Update the function signatures of the network
     utility functions appropriately. Compare socket_type variables
     against an INVALID_SOCKET define instead of -1. Fixes portability
     issues to 64-bit Windows. Thanks, Jeffrey Altman.
    
     For the Windows build, get the current version number from
     configure.ac rather than configure so that the Windows build
     scripts work from a Git checkout. Link with the correct GSS-API
     library for 64-bit Windows builds. Correct or suppress multiple
     warnings. Thanks, Jeffrey Altman.
    
     Enable Automake silent rules. For a quieter build, pass the
     --enable-silent-rules option to configure or build with make V=0.
    
     Update to rra-c-util 2.1:
    
     * Revert separation of die into a separate object file.
     * Fall back on manual library probing if krb5-config doesn't work.
     * Don't try to use a non-executable krb5-config for GSS-API probes.
     * Suppress error output from krb5-config GSS-API probes.
     * Prefer KRB5_CONFIG over a path constructed from --with-gssapi.
     * Fix network test suite failures when IPv6 is available but disabled.
    
     Update to C TAP Harness 1.1:
    
     * Summarize results at the end of test execution.

--- remctl/buildlink3.mk
+++ remctl/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/05/23 15:49:21 jakllsch Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2010/02/05 21:36:22 jakllsch Exp $
 
 BUILDLINK_TREE+=	remctl
 
 .if !defined(REMCTL_BUILDLINK3_MK)
 REMCTL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.remctl+=	remctl>=2.14
+BUILDLINK_API_DEPENDS.remctl+=	remctl>=2.15
 BUILDLINK_PKGSRCDIR.remctl?=	../../wip/remctl
 .endif # REMCTL_BUILDLINK3_MK
 
