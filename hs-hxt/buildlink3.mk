752b713780769d1ec78cfc0036677741a040bfd9
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-02-09 04:23:04 +0000

    Upstream update to hxt-8.5.0

--- hs-hxt/buildlink3.mk
+++ hs-hxt/buildlink3.mk
@@ -1,18 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/01/17 12:03:27 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2010/02/09 04:23:04 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-hxt
 
 .if !defined(HS_HXT_BUILDLINK3_MK)
 HS_HXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hxt+=	hs-hxt>=8.3.0
-BUILDLINK_ABI_DEPENDS.hs-hxt?=	hs-hxt>=8.3.2nb1
+BUILDLINK_API_DEPENDS.hs-hxt+=	hs-hxt>=8.5.0
 BUILDLINK_PKGSRCDIR.hs-hxt?=	../../wip/hs-hxt
 
-.include "../../wip/hs-HUnit/buildlink3.mk"
 .include "../../wip/hs-curl/buildlink3.mk"
+.include "../../wip/hs-deepseq/buildlink3.mk"
 .include "../../wip/hs-network/buildlink3.mk"
-.include "../../wip/hs-parallel/buildlink3.mk"
 .include "../../wip/hs-parsec/buildlink3.mk"
 .include "../../wip/hs-tagsoup/buildlink3.mk"
 .endif	# HS_HXT_BUILDLINK3_MK
