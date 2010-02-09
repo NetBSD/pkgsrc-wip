e011f239167278c50643c2ba29381a7d70663b07
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-02-09 03:31:31 +0000

    Upstream update to parallel-2.2.0.1

--- hs-parallel/buildlink3.mk
+++ hs-parallel/buildlink3.mk
@@ -1,12 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/06/02 07:54:25 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2010/02/09 03:31:31 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-parallel
 
 .if !defined(HS_PARALLEL_BUILDLINK3_MK)
 HS_PARALLEL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-parallel+=	hs-parallel>=1.1.0.1
+BUILDLINK_API_DEPENDS.hs-parallel+=	hs-parallel>=2.2.0.1
 BUILDLINK_PKGSRCDIR.hs-parallel?=	../../wip/hs-parallel
+
+.include "../../wip/hs-deepseq/buildlink3.mk"
 .endif	# HS_PARALLEL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-parallel
