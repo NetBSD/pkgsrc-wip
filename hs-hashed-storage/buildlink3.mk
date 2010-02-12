fcdfecb36cd336ad2cf0df9189affe5fd8f9877c
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-02-12 05:42:53 +0000

    Upstream update to hashed-storage-0.4.6

--- hs-hashed-storage/buildlink3.mk
+++ hs-hashed-storage/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2010/01/24 03:15:06 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2010/02/12 05:42:53 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-hashed-storage
 
 .if !defined(HS_HASHED_STORAGE_BUILDLINK3_MK)
 HS_HASHED_STORAGE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hashed-storage+=	hs-hashed-storage>=0.4.3
+BUILDLINK_API_DEPENDS.hs-hashed-storage+=	hs-hashed-storage>=0.4.6
 BUILDLINK_PKGSRCDIR.hs-hashed-storage?=	../../wip/hs-hashed-storage
 
 .include "../../wip/hs-dataenc/buildlink3.mk"
