663c60d06c6c3dce34ebe40ab252fcad1b6ee9b0
Tobias Nygren <tnn@NetBSD.org>
2010-02-16 15:44:53 +0000

    drop debug option, not very useful anymore.

--- firefox/options.mk
+++ firefox/options.mk
@@ -1,15 +1,9 @@
-# $NetBSD: options.mk,v 1.5 2009/12/02 15:03:02 tnn2 Exp $
+# $NetBSD: options.mk,v 1.6 2010/02/16 15:44:53 tnn2 Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.firefox
-PKG_SUPPORTED_OPTIONS=	debug official-mozilla-branding
+PKG_SUPPORTED_OPTIONS=	official-mozilla-branding
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Mdebug)
-CONFIGURE_ARGS+=	--enable-debug
-.else
-CONFIGURE_ARGS+=	--disable-debug
-.endif
 
 .if !empty(PKG_OPTIONS:Mofficial-mozilla-branding)
 CONFIGURE_ARGS+=	--enable-official-branding
