6c82f6f47f0c5d5b66b7ab7e7d671cf43ec063c8
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-02-19 06:44:40 +0000

    Upstream update to binary-strict-0.4.8

--- hs-binary-strict/buildlink3.mk
+++ hs-binary-strict/buildlink3.mk
@@ -1,12 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/10/02 03:23:36 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/02/19 06:44:40 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-binary-strict
 
 .if !defined(HS_BINARY_STRICT_BUILDLINK3_MK)
 HS_BINARY_STRICT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-binary-strict+=	hs-binary-strict>=0.4.5
+BUILDLINK_API_DEPENDS.hs-binary-strict+=	hs-binary-strict>=0.4.8
 BUILDLINK_PKGSRCDIR.hs-binary-strict?=	../../wip/hs-binary-strict
+
+.include "../../wip/hs-mtl/buildlink3.mk"
 .endif	# HS_BINARY_STRICT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-binary-strict
