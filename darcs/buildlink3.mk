5c41e4bcdfe7c7b83c5e0ed1318032c6099d682c
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-02-19 09:52:53 +0000

    Stop mmap-0.5.* from being linked together with mmap-0.4.*

--- darcs/buildlink3.mk
+++ darcs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2010/02/19 03:50:27 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2010/02/19 09:52:53 phonohawk Exp $
 
 BUILDLINK_TREE+=	darcs
 
 .if !defined(DARCS_BUILDLINK3_MK)
 DARCS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.darcs+=	darcs>=2.3.99.2
+BUILDLINK_API_DEPENDS.darcs+=	darcs>=2.3.99.2nb1
 BUILDLINK_PKGSRCDIR.darcs?=	../../wip/darcs
 
 .include "../../wip/hs-hashed-storage/buildlink3.mk"
