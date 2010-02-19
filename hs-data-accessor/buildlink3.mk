fdad9831007c490618bab31f2b3b6b59c1b24c57
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-02-19 06:24:26 +0000

    Upstream update to data-accessor-0.2.1.2

--- hs-data-accessor/buildlink3.mk
+++ hs-data-accessor/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/09/23 05:19:11 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/02/19 06:24:26 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-data-accessor
 
 .if !defined(HS_DATA_ACCESSOR_BUILDLINK3_MK)
 HS_DATA_ACCESSOR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-data-accessor+=	hs-data-accessor>=0.2.1
+BUILDLINK_API_DEPENDS.hs-data-accessor+=	hs-data-accessor>=0.2.1.2
 BUILDLINK_PKGSRCDIR.hs-data-accessor?=	../../wip/hs-data-accessor
 
 .include "../../wip/hs-transformers/buildlink3.mk"
