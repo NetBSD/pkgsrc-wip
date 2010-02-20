c3f0479cfd51c8d4ee0b24a722b5885ff09f1c7f
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2010-02-20 22:45:12 +0000

    Import linbox-1.1.7 as wip/linbox.
    LinBox is a C++ template library for exact, high-performance linear algebra
    computation with dense, sparse, and structured matrices over the integers
    and over finite fields.

---
+++ linbox/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/02/20 22:45:12 jihbed Exp $
+
+BUILDLINK_TREE+=	linbox
+
+.if !defined(LINBOX_BUILDLINK3_MK)
+LINBOX_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.linbox+=	linbox>=1.1.7
+BUILDLINK_PKGSRCDIR.linbox?=	../../wip/linbox
+
+
+.include "../../math/blas/buildlink3.mk"
+.include "../../wip/givaro/buildlink3.mk"
+.endif	# LINBOX_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-linbox
