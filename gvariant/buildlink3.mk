22d1fe97def191a45692d020731b7e2674dc9c0f
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2010-02-25 12:54:20 +0000

    Import gvariant-0 as wip/gvariant.
    GVariant is a new datatype in glib that stores complex values of many types
    (any type that can traditionally be sent over D-Bus). GVariant is allowing
    development of many exciting projects (present and future) including
    GSettings, the  GConf replacement.

---
+++ gvariant/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/02/25 12:54:20 jihbed Exp $
+
+BUILDLINK_TREE+=	gvariant
+
+.if !defined(GVARIANT_BUILDLINK3_MK)
+GVARIANT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gvariant+=	gvariant>=0
+BUILDLINK_PKGSRCDIR.gvariant?=	../../wip/gvariant
+
+
+.include "../../devel/glib2/buildlink3.mk"
+.endif	# GVARIANT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gvariant
