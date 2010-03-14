4dbe224f9fe73c25f077726a1557cba58ada9f3e
Daniel Horecki <morr@NetBSD.org>
2010-03-14 23:03:55 +0000

    Update to newest snapshot, required to support new GG.

--- libgadu/buildlink3.mk
+++ libgadu/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:43:44 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2010/03/14 23:03:55 shindenmorr Exp $
 
 BUILDLINK_TREE+=	libgadu
 
 .if !defined(LIBGADU_BUILDLINK3_MK)
 LIBGADU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgadu+=	libgadu>=1.7.1
-BUILDLINK_ABI_DEPENDS.libgadu+=	libgadu>=1.7.1
+BUILDLINK_API_DEPENDS.libgadu+=	libgadu>=1.9.0
+BUILDLINK_ABI_DEPENDS.libgadu+=	libgadu>=1.9.0
 BUILDLINK_PKGSRCDIR.libgadu?=	../../wip/libgadu
 .endif # LIBGADU_BUILDLINK3_MK
 
