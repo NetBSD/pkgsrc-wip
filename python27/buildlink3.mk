80113c4a8c2322d4cfde2b380c8d8fb0d45c248d
OBATA Akio <obache@users.sourceforge.net>
2010-03-17 13:09:05 +0000

    Import python27-2.7alpha4 as wip/python27.
    Currently, just a stub, copy lang/python26 and some adjustments.

---
+++ python27/buildlink3.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/03/17 13:09:07 obache Exp $
+
+BUILDLINK_TREE+=	python27
+
+.if !defined(PYTHON27_BUILDLINK3_MK)
+PYTHON27_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.python27+=	python27>=2.7
+BUILDLINK_PKGSRCDIR.python27?=		../../wip/python27
+
+.  if defined(BUILDLINK_DEPMETHOD.python)
+BUILDLINK_DEPMETHOD.python27?=	${BUILDLINK_DEPMETHOD.python}
+.  endif
+
+BUILDLINK_INCDIRS.python27+=	include/python2.7
+BUILDLINK_LIBDIRS.python27+=	lib/python2.7/config
+BUILDLINK_TRANSFORM+=		l:python:python2.7
+
+.include "../../mk/dlopen.buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
+.endif # PYTHON27_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-python27
