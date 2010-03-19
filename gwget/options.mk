1ba74166e7e126d2e67ccc33afdd7df1e0962339
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2010-03-19 19:40:00 +0000

    Import gwget-1.0.4 as wip/gwget.
    Gwget is a download manager for the Gnome Desktop. The main features are:
    
    * Resume: By default, gwget tries to continue any download.
    * Notification: Gwget tries to use the Gnome notification area support,
      if available. You can close the main window and gwget runs in the background.
    * Recursivity: Gwget detects when you put a html, php, asp or a web page dir in
      the url to download, and ask you to only download certain files (multimedia,
      only the index, and so on).
    * Drag & Drop: You can d&d a url to the main gwget window or the notification
      area icon to add a new download.

---
+++ gwget/options.mk
@@ -0,0 +1,25 @@
+# $NetBSD: options.mk,v 1.1.1.1 2010/03/19 19:40:00 jihbed Exp $
+#
+
+PKG_OPTIONS_VAR=                PKG_OPTIONS.gwget
+PKG_SUPPORTED_OPTIONS=          libnotify epiphany-extension epiphany-version
+PKG_SUGGESTED_OPTIONS=          libnotify epiphany-extension epiphany-version
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty (PKG_OPTIONS:Mepiphany-extension)
+DEPENDS+=	epiphany-extensions>=2.28.1:../../www/epiphany-extensions
+CONFIGURE_ARGS+=	--enable-epiphany-extension
+.endif
+
+.if !empty(PKG_OPTIONS:Mepiphany-version)
+CONFIGURE_ARGS+=	--with-epiphany-version
+.  include "../../www/epiphany/buildlink3.mk"
+.endif
+
+.if !empty (PKG_OPTIONS:Mlibnotify)
+BUILDLINK_API_DEPENDS.libnotify+=	libnotify>=0.3.2
+.  include "../../sysutils/libnotify/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-libnotify
+.endif
