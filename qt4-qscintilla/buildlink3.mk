0f1b8eb7f5d6d170dc2caf95378311fae79a3b01
Helge Muehlmeier <helgoman@users.sourceforge.net>
2010-03-19 09:18:15 +0000

    Update to new version 2.4.3

--- qt4-qscintilla/buildlink3.mk
+++ qt4-qscintilla/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/03/01 20:51:59 helgoman Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/03/19 09:18:15 helgoman Exp $
 
 BUILDLINK_TREE+=	qt4-qscintilla
 
 .if !defined(QT4_QSCINTILLA_BUILDLINK3_MK)
 QT4_QSCINTILLA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qt4-qscintilla+=	qt4-qscintilla>=2.4.2nb1
+BUILDLINK_API_DEPENDS.qt4-qscintilla+=	qt4-qscintilla>=2.4.3nb1
 BUILDLINK_PKGSRCDIR.qt4-qscintilla?=	../../wip/qt4-qscintilla
 
 .include "../../x11/qt4-libs/buildlink3.mk"
