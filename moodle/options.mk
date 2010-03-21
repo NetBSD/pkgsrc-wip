644012831ce284cfabb4a781a3bd6f4d5ed24296
Wen Heping <wen@NetBSD.org>
2010-03-21 15:19:02 +0000

    Import moodle-1.9.7 as wip/moodle.
    Moodle is a course management system (CMS) - a free, Open Source software
    package designed using sound pedagogical principles, to help educators
    create effective online learning communities. You can use it on any
    computer you have handy (including webhosts), yet it can scale from a

---
+++ moodle/options.mk
@@ -0,0 +1,20 @@
+# $NetBSD: options.mk,v 1.1.1.1 2010/03/21 15:19:15 uccwen Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.moodle
+
+PKG_OPTIONS_REQUIRED_GROUPS=	db
+PKG_OPTIONS_GROUP.db=		mysql pgsql
+
+PKG_SUPPORTED_OPTIONS+=	mysql pgsql
+PKG_SUGGESTED_OPTIONS=	mysql
+
+.include "../../mk/bsd.options.mk"
+
+###
+### Use mysql or postgresql backend
+###
+.if !empty(PKG_OPTIONS:Mmysql)
+DEPENDS+=	${PHP_PKG_PREFIX}-mysql>=4.3.10:../../databases/php-mysql
+.elif !empty(PKG_OPTIONS:Mpgsql)
+DEPENDS+=	${PHP_PKG_PREFIX}-pgsql>=5:../../databases/php-pgsql
+.endif
