a5c3f16acf2c0c77b3a5174bcf4899e83c4bfa3f
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-03-27 05:49:30 +0000

    Upstream update to Lucu-0.6

--- hs-Lucu/buildlink3.mk
+++ hs-Lucu/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2010/03/16 05:14:08 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2010/03/27 05:49:30 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-Lucu
 
 .if !defined(HS_LUCU_BUILDLINK3_MK)
 HS_LUCU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-Lucu+=	hs-Lucu>=0.5
+BUILDLINK_API_DEPENDS.hs-Lucu+=	hs-Lucu>=0.6
 BUILDLINK_PKGSRCDIR.hs-Lucu?=	../../wip/hs-Lucu
 
 .include "../../wip/hs-HsOpenSSL/buildlink3.mk"
@@ -15,6 +15,7 @@ BUILDLINK_PKGSRCDIR.hs-Lucu?=	../../wip/hs-Lucu
 .include "../../wip/hs-mtl/buildlink3.mk"
 .include "../../wip/hs-network/buildlink3.mk"
 .include "../../wip/hs-stm/buildlink3.mk"
+.include "../../wip/hs-time-http/buildlink3.mk"
 .include "../../wip/hs-zlib/buildlink3.mk"
 .endif	# HS_LUCU_BUILDLINK3_MK
 
