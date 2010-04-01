e4a9e3d010143df4622565fa8bdf31c850466c72
Aleksej Saushev <asau@users.sourceforge.net>
2010-04-01 01:47:28 +0000

    Update to OpenMPI 1.5 branch snapshot rev. 22914 Our getifaddrs changes were applied.

--- openmpi/options.mk
+++ openmpi/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2009/12/12 15:22:24 asau Exp $
+# $NetBSD: options.mk,v 1.2 2010/04/01 01:47:28 asau Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.openmpi
 PKG_SUPPORTED_OPTIONS=	debug openmpi-vampirtrace
@@ -14,6 +14,7 @@ CONFIGURE_ARGS+=	--enable-debug
 .if !empty(PKG_OPTIONS:Mopenmpi-vampirtrace)
 CONFLICTS+=	libotf-[0-9]*
 PLIST_SRC+=	PLIST.vt
+.include "../../graphics/freetype2/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--enable-contrib-no-build=vt
 .endif
