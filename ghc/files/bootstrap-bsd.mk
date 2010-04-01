107ab9641924b96e59d1ea975eed17177f9c2cab
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-04-01 07:33:13 +0000

    GHC depends on some ncurses specific functions.

--- ghc/files/bootstrap-bsd.mk
+++ ghc/files/bootstrap-bsd.mk
@@ -11,8 +11,8 @@ SplitObjs = NO
 GhcWithNativeCodeGen = NO
 GhcWithInterpreter = NO
 GhcWithSMP = NO
-ghc_stage2_v_EXTRA_CC_OPTS += -Llibraries/integer-gmp/gmp -L[[PREFIX]]/lib -lm -liconv -lutil -lrt
-utils/ghc-pkg_dist-install_v_EXTRA_CC_OPTS += -Llibraries/integer-gmp/gmp -L[[PREFIX]]/lib -lm -liconv -lutil -lrt -ltermcap
+ghc_stage2_v_EXTRA_CC_OPTS += -L[[PREFIX]]/lib -lm -liconv -lutil -lrt
+utils/ghc-pkg_dist-install_v_EXTRA_CC_OPTS += -L[[PREFIX]]/lib -lm -liconv -lutil -lrt -lncurses
 
 SRC_CC_OPTS += -I[[PREFIX]]/include
 INTEGER_LIBRARY = integer-simple
--- ghc/files/bootstrap.mk
+++ ghc/files/bootstrap.mk
@@ -11,8 +11,8 @@ SplitObjs = NO
 GhcWithNativeCodeGen = NO
 GhcWithInterpreter = NO
 GhcWithSMP = NO
-ghc_stage2_v_EXTRA_CC_OPTS += -Llibraries/integer-gmp/gmp -L[[PREFIX]]/lib -lm -liconv
-utils/ghc-pkg_dist-install_v_EXTRA_CC_OPTS += -Llibraries/integer-gmp/gmp -L[[PREFIX]]/lib -lm -liconv -ltermcap
+ghc_stage2_v_EXTRA_CC_OPTS += -L[[PREFIX]]/lib -lm -liconv
+utils/ghc-pkg_dist-install_v_EXTRA_CC_OPTS += -L[[PREFIX]]/lib -lm -liconv -lncurses
 
 SRC_CC_OPTS += -I[[PREFIX]]/include
 INTEGER_LIBRARY = integer-simple
