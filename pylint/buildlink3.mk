0661e3f74753fd2b95a4096b3e82e39b9de2c3c1
Helge Muehlmeier <helgoman@users.sourceforge.net>
2010-04-05 16:09:40 +0000

    Update to version 0.20.0

--- pylint/buildlink3.mk
+++ pylint/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/03/13 13:05:44 helgoman Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/04/05 16:09:40 helgoman Exp $
 
 BUILDLINK_TREE+=	pylint
 
 .if !defined(PYLINT_BUILDLINK3_MK)
 PYLINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.pylint+=	pylint>=0.19.0nb1
+BUILDLINK_API_DEPENDS.pylint+=	pylint>=0.20.0nb1
 BUILDLINK_PKGSRCDIR.pylint?=	../../wip/pylint
 .endif	# PYLINT_BUILDLINK3_MK
 
