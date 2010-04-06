3003dacc056737100061f3df431ee54197ceae98
OBATA Akio <obache@users.sourceforge.net>
2010-04-06 11:50:37 +0000

    Update ibus to 1.3.1.

--- ibus/buildlink3.mk
+++ ibus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2010/01/10 13:10:21 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2010/04/06 11:50:37 obache Exp $
 #
 
 BUILDLINK_TREE+=	ibus
@@ -7,6 +7,7 @@ BUILDLINK_TREE+=	ibus
 IBUS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ibus+=	ibus>=1.2.0.20091024
+BUILDLINK_ABI_DEPENDS.ibus?=	ibus>=1.3.1
 BUILDLINK_PKGSRCDIR.ibus?=	../../wip/ibus
 
 .include "../../devel/gettext-lib/buildlink3.mk"
