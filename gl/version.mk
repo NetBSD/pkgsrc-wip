6e7a0104ee0fed0232c6d5e921045ebed1f140f4
Pierre Allegraud <pallegra@users.sourceforge.net>
2010-04-09 12:55:12 +0000

    update to Mesa-7.7.1
    Mesa 7.7.1 is a bug-fix release.
    
    Mesa 7.7.1 implements the OpenGL 2.1 API, but the version reported by
    glGetString(GL_VERSION) depends on the particular driver being used.
    Some drivers don't support all the features required in OpenGL 2.1.

--- gl/version.mk
+++ gl/version.mk
@@ -1,8 +1,8 @@
-# $NetBSD: version.mk,v 1.1.1.1 2010/01/03 14:32:17 pallegra Exp $
+# $NetBSD: version.mk,v 1.2 2010/04/09 12:55:12 pallegra Exp $
 # used by wip/gl/Makefile.common
 # used by wip/MesaLib/Makefile
 
 MESA_DISTNAME=		MesaLib-${MESA_VERSION}
 MESA_SITES=		ftp://freedesktop.org/pub/mesa/${MESA_VERSION}/
-MESA_VERSION=		7.7
+MESA_VERSION=		7.7.1
 MESA_PKGVERSION=	${MESA_VERSION}
