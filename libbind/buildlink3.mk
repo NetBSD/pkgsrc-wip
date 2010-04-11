5543aed18dfdb12f9d93dcb5c35efa34c545ad35
OBATA Akio <obache@users.sourceforge.net>
2010-04-11 09:36:06 +0000

    static library, build dependency.

--- libbind/buildlink3.mk
+++ libbind/buildlink3.mk
@@ -1,10 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/11/19 05:59:52 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/04/11 09:36:06 obache Exp $
 
 BUILDLINK_TREE+=	libbind
 
 .if !defined(LIBBIND_BUILDLINK3_MK)
 LIBBIND_BUILDLINK3_MK:=
 
+BUILDLINK_DEPMETHOD.libbind?=	build
 BUILDLINK_API_DEPENDS.libbind+=	libbind>=6.0
 BUILDLINK_PKGSRCDIR.libbind?=	../../wip/libbind
 .endif	# LIBBIND_BUILDLINK3_MK
