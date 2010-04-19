ece52f12cc137b775dd8036da46722e39232a5c7
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2010-04-19 09:59:11 +0000

    upgrade version 0.42 to 0.44, remove ABI

--- py-logilab-common/buildlink3.mk
+++ py-logilab-common/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/07/06 22:33:21 jihbed Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2010/04/19 09:59:11 jihbed Exp $
 
 BUILDLINK_TREE+=	py-logilab-common
 
 .if !defined(PY_LOGILAB_COMMON_BUILDLINK3_MK)
 PY_LOGILAB_COMMON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.py-logilab-common+=	${PYPKGPREFIX}-logilab-common>=0.42.0
-BUILDLINK_ABI_DEPENDS.py-logilab-common+=	${PYPKGPREFIX}-logilab-common>=2.41.0
+BUILDLINK_API_DEPENDS.py-logilab-common+=	${PYPKGPREFIX}-logilab-common>=0.44.0
 BUILDLINK_PKGSRCDIR.py-logilab-common?=	../../wip/py-logilab-common
 
 .endif	# PY_LOGILAB_COMMON_BUILDLINK3_MK
