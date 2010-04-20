9d18c6808724669f94bbcb7addefb8bdb5497cd0
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-04-20 08:29:23 +0000

    Import rrdtool-1.4.3 as wip/rrdtool.
    Round Robin Database Tools
    ==========================
    
    It is pretty easy to gather status information from all sorts of things,
    ranging from the temperature in your office to the number of octets which
    have passed through the FDDI interface of your router. But it is not so
    trivial to store this data in a efficient and systematic manner. This is
    where RRDtool kicks in. It lets you log and analyze the data you gather from
    all kinds of data-sources (DS). The data analysis part of RRDtool is based
    on the ability to quickly generate graphical representations of the data
    values collected over a definable time period.

---
+++ rrdtool/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/04/20 08:29:23 phonohawk Exp $
+
+BUILDLINK_TREE+=	rrdtool
+
+.if !defined(RRDTOOL_BUILDLINK3_MK)
+RRDTOOL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.rrdtool+=	rrdtool>=1.4.3
+BUILDLINK_PKGSRCDIR.rrdtool?=	../../databases/rrdtool
+
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/pixman/buildlink3.mk"
+.endif	# RRDTOOL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-rrdtool
