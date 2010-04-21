869fadc09e5e0322cecedc79c8b4c3fb30c26eb3
Tobias Nygren <tnn@NetBSD.org>
2010-04-21 12:51:26 +0000

    Update to firefox-3.7a5pre snapshot.

--- xulrunner/dist.mk
+++ xulrunner/dist.mk
@@ -1,11 +1,11 @@
-# $NetBSD: dist.mk,v 1.13 2010/04/01 14:18:44 tnn2 Exp $
+# $NetBSD: dist.mk,v 1.14 2010/04/21 12:51:26 tnn2 Exp $
 #
 # used by wip/xulrunner/Makefile
 # used by wip/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	3.7a4pre${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	1.9.3a4pre
+FIREFOX_VER=	3.7a5pre${MOZ_BRANCH_MINOR}
+MOZ_BRANCH=	1.9.3a5pre
 MOZ_BRANCH_MINOR=	# empty
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
@@ -16,6 +16,6 @@ PATCHDIR=	${.CURDIR}/../../wip/xulrunner/patches
 WRKSRC=		${WRKDIR}/mozilla-${MOZ_BRANCH}
 
 # snapshot overrides
-DISTNAME=	6a398023b956
+DISTNAME=	cdc8bf25220e
 MASTER_SITES=	http://hg.mozilla.org/mozilla-central/archive/
-WRKSRC=		${WRKDIR}/mozilla-central-6a398023b956
+WRKSRC=		${WRKDIR}/mozilla-central-cdc8bf25220e
