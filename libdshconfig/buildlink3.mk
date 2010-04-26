a2ffd03fe16e3c92772e807ff193e0caf4d1eaed
Daniel Horecki <morr@NetBSD.org>
2010-04-26 15:38:29 +0000

    correct buildlink3.mk file

--- libdshconfig/buildlink3.mk
+++ libdshconfig/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:43:44 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2010/04/26 15:38:29 shindenmorr Exp $
 
 BUILDLINK_TREE+=	libdshconfig
 
 .if !defined(LIBDSHCONFIG_BUILDLINK3_MK)
 LIBDSHCONFIG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libdshconfig+=	libdshconfig>=0.20.9
-BUILDLINK_DEPMETHOD.libdshconfig?=	build
+BUILDLINK_API_DEPENDS.libdshconfig+=	libdshconfig>=0.20.13
 BUILDLINK_PKGSRCDIR.libdshconfig?=	../../wip/libdshconfig
-.endif # LIBDSHCONFIG_BUILDLINK3_MK
+.endif	# LIBDSHCONFIG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libdshconfig
