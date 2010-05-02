894746bafd2ddbc9523ace9fda931353956517b7
Thomas Klausner <wiz@NetBSD.org>
2010-05-02 14:31:42 +0000

    Fix path to itself.

--- libepc/buildlink3.mk
+++ libepc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/05/02 10:26:16 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/05/02 14:31:42 thomasklausner Exp $
 
 BUILDLINK_TREE+=	libepc
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libepc
 LIBEPC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libepc+=	libepc>=0.3.11
-BUILDLINK_PKGSRCDIR.libepc?=	../../devel/libepc
+BUILDLINK_PKGSRCDIR.libepc?=	../../wip/libepc
 
 .include "../../net/libsoup24/buildlink3.mk"
 .endif	# LIBEPC_BUILDLINK3_MK
