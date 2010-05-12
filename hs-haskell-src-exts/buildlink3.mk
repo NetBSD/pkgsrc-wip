d4620b2eb0db248146d46570f54c04dd25070e46
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-05-12 07:45:15 +0000

    Forgot to update buildlink3.mk

--- hs-haskell-src-exts/buildlink3.mk
+++ hs-haskell-src-exts/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2010/02/02 06:17:12 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2010/05/12 07:45:15 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-haskell-src-exts
 
 .if !defined(HS_HASKELL_SRC_EXTS_BUILDLINK3_MK)
 HS_HASKELL_SRC_EXTS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.8.0
+BUILDLINK_API_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.9
 BUILDLINK_PKGSRCDIR.hs-haskell-src-exts?=	../../wip/hs-haskell-src-exts
 
 .include "../../wip/hs-cpphs/buildlink3.mk"
