32f270399b449dc5906cd858f9f357f6ae04ffa9
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-05-13 05:17:31 +0000

    Upstream update to haskelldb-hdbc-0.13

--- hs-haskelldb-hdbc/buildlink3.mk
+++ hs-haskelldb-hdbc/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/05/29 05:40:08 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/05/13 05:17:31 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-haskelldb-hdbc
 
 .if !defined(HS_HASKELLDB_HDBC_BUILDLINK3_MK)
 HS_HASKELLDB_HDBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-haskelldb-hdbc+=	hs-haskelldb-hdbc>=0.12
+BUILDLINK_API_DEPENDS.hs-haskelldb-hdbc+=	hs-haskelldb-hdbc>=0.13
 BUILDLINK_PKGSRCDIR.hs-haskelldb-hdbc?=	../../wip/hs-haskelldb-hdbc
 
 .include "../../wip/hs-convertible/buildlink3.mk"
