98bb5638071f4dc606aa5a717d86968c55d73449
OBATA Akio <obache@users.sourceforge.net>
2010-05-13 11:33:07 +0000

    Import groonga-0.7.0 as wip/groonga.
    gronnga is an open-source filltext search engine and column store.
    It lets you write high-performance applications that requires fulltext search.

---
+++ groonga/options.mk
@@ -0,0 +1,43 @@
+# $NetBSD: options.mk,v 1.1.1.1 2010/05/13 11:33:07 obache Exp $
+#
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.groonga
+PKG_SUPPORTED_OPTIONS=	mecab tests zlib lzo
+PKG_SUGGESTED_OPTIONS=  mecab tests
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mmecab)
+CONFIGURE_ARGS+=	--with-mecab
+CONFIGURE_ARGS+=	--with-mecab-config=${BUILDLINK_PREFIX.mecab}/bin/mecab-config
+.include "../../textproc/mecab/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-mecab
+.endif
+
+.if !empty(PKG_OPTIONS:Mtests)
+CONFIGURE_ARGS+=	--with-ruby=${RUBY}
+CONFIGURE_ARGS+=	--with-cutter
+USE_TOOLS+=		gmake
+TEST_TARGET=		check
+BUILDLINK_DEPMETHOD.${RUBY_BASE}+=	build
+.include "../../lang/ruby/buildlink3.mk"
+.include "../../wip/cutter/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-ruby
+CONFIGURE_ARGS+=	--without-cutter
+.endif
+
+.if !empty(PKG_OPTIONS:Mzlib)
+CONFIGURE_ARGS+=	--with-zlib
+.include "../../devel/zlib/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-zlib
+.endif
+
+.if !empty(PKG_OPTIONS:Mlzo)
+CONFIGURE_ARGS+=	--with-lzo
+.include "../../archivers/lzo/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-lzo
+.endif
