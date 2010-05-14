db9fcfdd2549769cf3d878f87f90a36f716c2bbb
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-05-14 05:20:30 +0000

    Upstream update to haskelldb-hdbc-sqlite3-0.13

--- hs-haskelldb-hdbc-sqlite3/buildlink3.mk
+++ hs-haskelldb-hdbc-sqlite3/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/05/29 06:17:55 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/05/14 05:20:30 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-haskelldb-hdbc-sqlite3
 
 .if !defined(HS_HASKELLDB_HDBC_SQLITE3_BUILDLINK3_MK)
 HS_HASKELLDB_HDBC_SQLITE3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-haskelldb-hdbc-sqlite3+=	hs-haskelldb-hdbc-sqlite3>=0.12
+BUILDLINK_API_DEPENDS.hs-haskelldb-hdbc-sqlite3+=	hs-haskelldb-hdbc-sqlite3>=0.13
 BUILDLINK_PKGSRCDIR.hs-haskelldb-hdbc-sqlite3?=	../../wip/hs-haskelldb-hdbc-sqlite3
 
 .include "../../wip/hs-haskelldb/buildlink3.mk"
