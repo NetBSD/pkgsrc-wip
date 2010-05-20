2531bda22f3925b5c583e5ad8e54b2eaae9611ee
OBATA Akio <obache@users.sourceforge.net>
2010-05-20 05:16:08 +0000

    need to buildlink ffmpeg.

--- libdlna/buildlink3.mk
+++ libdlna/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/05/20 05:14:46 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/05/20 05:16:08 obache Exp $
 #
 
 BUILDLINK_TREE+=	libdlna
@@ -9,7 +9,7 @@ LIBDLNA_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libdlna+=	libdlna>=0.2.3
 BUILDLINK_PKGSRCDIR.libdlna?=	../../wip/libdlna
 
-#.include "../../multimedia/ffmpeg/buildlink3.mk"
+.include "../../multimedia/ffmpeg/buildlink3.mk"
 .endif	# LIBDLNA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libdlna
