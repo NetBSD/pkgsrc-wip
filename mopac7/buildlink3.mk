1297fe040d18e2732a823446cc65c74192b6ddd6
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2010-05-22 23:08:12 +0000

    Import libmopac7-1.11 as wip/mopac7.
    MOPAC provides routines to solve the electronic structure of molecules on a
    semi-empirical level. Available methods include MNDO, MINDO/3, AM1 and PM3.

---
+++ mopac7/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/05/22 23:08:12 jihbed Exp $
+
+BUILDLINK_TREE+=	libmopac7
+
+.if !defined(LIBMOPAC7_BUILDLINK3_MK)
+LIBMOPAC7_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libmopac7+=	libmopac7>=1.11
+BUILDLINK_PKGSRCDIR.libmopac7?=	../../wip/mopac7
+.endif	# LIBMOPAC7_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libmopac7
