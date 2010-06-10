9f265b50ae789ab1725f9f711292ce93dc454396
Marko Schütz Schmuck <marko_schuetz@users.sourceforge.net>
2010-06-10 23:19:28 +0000

    updating OGRE

---
+++ ogre/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2010/06/10 23:19:28 marko_schuetz Exp $
+
+BUILDLINK_TREE+=	ogre
+
+.if !defined(OGRE_BUILDLINK3_MK)
+OGRE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ogre+=	ogre>=1.6.3
+BUILDLINK_ABI_DEPENDS.ogre+=	ogre>=1.6.3nb1
+BUILDLINK_PKGSRCDIR.ogre?=	../../devel/ogre
+
+.include "../../archivers/zziplib/buildlink3.mk"
+.include "../../devel/devIL/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../x11/libXaw/buildlink3.mk"
+.endif # OGRE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ogre
