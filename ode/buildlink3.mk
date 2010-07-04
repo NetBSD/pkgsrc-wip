7680559be80fcc375b76cdad6b62a2c4f4ab438a
David DEMELIER <hftmarkand@users.sourceforge.net>
2010-07-04 16:38:10 +0000

    Updated to 0.11.1

--- ode/buildlink3.mk
+++ ode/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:43:51 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2010/07/04 16:38:10 hftmarkand Exp $
 
 BUILDLINK_TREE+=	ode
 
 .if !defined(ODE_BUILDLINK3_MK)
 ODE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ode+=		ode>=0.7
-BUILDLINK_ABI_DEPENDS.ode?=	ode>=0.7
+BUILDLINK_API_DEPENDS.ode+=	ode>=0.11.1
+BUILDLINK_ABI_DEPENDS.ode+=	ode>=0.7
 BUILDLINK_PKGSRCDIR.ode?=	../../wip/ode
 .endif # ODE_BUILDLINK3_MK
 
