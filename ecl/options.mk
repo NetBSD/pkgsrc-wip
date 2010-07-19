ea6a526da32f3322ae8d6a54b8b39c26fe151215
Aleksej Saushev <asau@users.sourceforge.net>
2010-07-19 00:22:07 +0000

    Sync with lang/ecl, only turn more options by default.

--- ecl/options.mk
+++ ecl/options.mk
@@ -1,9 +1,11 @@
-# $NetBSD: options.mk,v 1.1 2009/04/05 10:02:56 asau Exp $
+# $NetBSD: options.mk,v 1.2 2010/07/19 00:22:07 asau Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.ecl
-PKG_SUPPORTED_OPTIONS+=		threads unicode
-PKG_SUGGESTED_OPTIONS+=		unicode
-# Threads are off, since threaded ECL build crashes on NetBSD and FreeBSD.
+PKG_SUPPORTED_OPTIONS+=		threads unicode ffi clx
+PKG_SUGGESTED_OPTIONS+=		unicode ffi clx
+# Unicode support proved to break Axioms.
+# Threads are off, since threaded ECL requires threads support
+# in Boehm GC (off by default).
 
 .include "../../mk/bsd.options.mk"
 
@@ -14,6 +16,11 @@ CONFIGURE_ARGS+=	--enable-threads --enable-debug
 CONFIGURE_ENV+=		THREAD_CFLAGS=${PTHREAD_CFLAGS:Q}
 CONFIGURE_ENV+=		THREAD_LDLAGS=${BUILDLINK_LDLAGS.pthread:Q}
 CONFIGURE_ENV+=		THREAD_LIBS=${BUILDLINK_LIBS.pthread:Q}
+.if ${OPSYS} == "FreeBSD" || ${OPSYS} == "Linux" || ${OPSYS} == "Darwin"
+CONFIGURE_ARGS+=	--with-__thread=yes
+.else
+CONFIGURE_ARGS+=	--with-__thread=no
+.endif
 .include "../../mk/pthread.buildlink3.mk"
 .endif
 
@@ -21,3 +28,19 @@ CONFIGURE_ENV+=		THREAD_LIBS=${BUILDLINK_LIBS.pthread:Q}
 CONFIGURE_ARGS+=	--enable-unicode
 PLIST_SRC+=		PLIST.unicode
 .endif
+
+.if !empty(PKG_OPTIONS:Mffi)
+.include "../../devel/libffi/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mclx)
+CONFIGURE_ARGS+=	--with-clx
+.endif
+
+PLIST_VARS+=		clx
+
+.for option in clx
+.  if !empty(PKG_OPTIONS:M${option})
+PLIST.${option}=	yes
+.  endif
+.endfor
