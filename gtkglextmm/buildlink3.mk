e3f1a1b48a610c49da1051e20c0f9b5937941ef3
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2010-08-11 10:41:28 +0000

    Import gtkglextmm-1.2.0 as wip/gtkglextmm.
    gtkglextmm is C++ wrapper for GtkGLExt, OpenGL Extension to GTK

---
+++ gtkglextmm/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/08/11 10:41:28 jihbed Exp $
+
+BUILDLINK_TREE+=	gtkglextmm
+
+.if !defined(GTKGLEXTMM_BUILDLINK3_MK)
+GTKGLEXTMM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gtkglextmm+=	gtkglextmm>=1.2.0
+BUILDLINK_PKGSRCDIR.gtkglextmm?=	../../category/gtkglextmm
+
+.include "../../graphics/gtkglext/buildlink3.mk"
+.include "../../x11/gtkmm/buildlink3.mk"
+.endif	# GTKGLEXTMM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gtkglextmm
