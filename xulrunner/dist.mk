8c88d6459b40c9dda9099d4c36447aa99b00d353
Tobias Nygren <tnn@NetBSD.org>
2010-08-19 08:25:17 +0000

    bump to ffx 4.0b3

--- xulrunner/dist.mk
+++ xulrunner/dist.mk
@@ -1,11 +1,11 @@
-# $NetBSD: dist.mk,v 1.17 2010/08/03 20:58:00 tnn2 Exp $
+# $NetBSD: dist.mk,v 1.18 2010/08/19 08:25:17 tnn2 Exp $
 #
 # used by wip/xulrunner/Makefile
 # used by wip/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	4.0b2${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	2.0b2
+FIREFOX_VER=	4.0b3${MOZ_BRANCH_MINOR}
+MOZ_BRANCH=	2.0b3
 MOZ_BRANCH_MINOR=	# empty
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
