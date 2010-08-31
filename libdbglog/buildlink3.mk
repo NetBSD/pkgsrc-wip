bed7a79367bf8888756883110c17b04ec3d63a7d
Ondrej Tuma <ondratu@users.sourceforge.net>
2010-08-31 08:46:10 +0000

    Import libdbglog-1.5.1 as wip/libdbglog.
    dbgLog (debug logger) is a C++ library for debugging purposes and runtime
    logging to file with extension for Python and PHP. A simple solution for
    logging same way in many applications using different programming languages.

---
+++ libdbglog/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/08/31 08:46:10 ondratu Exp $
+
+BUILDLINK_TREE+=	libdbglog
+
+.if !defined(LIBDBGLOG_BUILDLINK3_MK)
+LIBDBGLOG_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libdbglog+=	libdbglog>=1.5.1
+BUILDLINK_PKGSRCDIR.libdbglog?=	../../wip/libdbglog
+.endif	# LIBDBGLOG_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libdbglog
