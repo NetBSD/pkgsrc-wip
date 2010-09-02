1041c90f3f6de3a1b66131159c53f51cbca1bac0
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2010-09-02 12:26:06 +0000

    Import libscscp-0.6.1 as wip/libscscp.
    Library of the Symbolic Computation Software Composibility Protocol

---
+++ libscscp/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/09/02 12:26:06 jihbed Exp $
+
+BUILDLINK_TREE+=	libscscp
+
+.if !defined(LIBSCSCP_BUILDLINK3_MK)
+LIBSCSCP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libscscp+=	libscscp>=0.6.1
+BUILDLINK_PKGSRCDIR.libscscp?=	../../wip/libscscp
+
+.include "../../textproc/libxml2/buildlink3.mk"
+.endif	# LIBSCSCP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libscscp
