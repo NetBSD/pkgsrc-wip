29c1ac107dcca3383245118990a6e161dec17c3f
Tobias Nygren <tnn@NetBSD.org>
2010-09-11 12:28:45 +0000

    Update to firefox-4.0b5.

--- xulrunner/dist.mk
+++ xulrunner/dist.mk
@@ -1,11 +1,11 @@
-# $NetBSD: dist.mk,v 1.19 2010/08/25 14:51:20 tnn2 Exp $
+# $NetBSD: dist.mk,v 1.20 2010/09/11 12:28:45 tnn2 Exp $
 #
 # used by wip/xulrunner/Makefile
 # used by wip/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	4.0b4${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	2.0b4
+FIREFOX_VER=	4.0b5${MOZ_BRANCH_MINOR}
+MOZ_BRANCH=	2.0b5
 MOZ_BRANCH_MINOR=	# empty
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
