2d4607ef92c542f226d9c546dd11ee2e2676f12c
Volkmar Seifert <vs@dimensionv.de>
2010-10-31 15:34:53 +0000

    Review comments incorporated.

--- libpar2/buildlink3.mk
+++ libpar2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2010/10/28 20:29:25 mjoellnir Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/10/31 15:34:53 mjoellnir Exp $
 
 BUILDLINK_TREE+=	libpar2
 
@@ -8,8 +8,7 @@ LIBPAR2_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libpar2+=	libpar2>=0.2
 BUILDLINK_PKGSRCDIR.libpar2?=	../../wip/libpar2
 
-BUILDLINK_INCDIRS.libpar2= include/libpar2
-BUILDLINK_LIBDIRS.libpar2= lib/libpar2
+BUILDLINK_INCDIRS.libpar2+= include/libpar2
 
 .include "../../devel/libsigc++/buildlink3.mk"
 .endif	# LIBPAR2_BUILDLINK3_MK
