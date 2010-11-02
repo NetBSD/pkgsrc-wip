73ba8800e59fee57d5a9cc543e9b78e9d4613658
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-11-02 16:50:07 +0000

    Upstream update to MonadCatchIO-transformers-0.2.2.0

--- hs-MonadCatchIO-transformers/buildlink3.mk
+++ hs-MonadCatchIO-transformers/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/03/03 09:42:58 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/11/02 16:50:07 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-MonadCatchIO-transformers
 
 .if !defined(HS_MONADCATCHIO_TRANSFORMERS_BUILDLINK3_MK)
 HS_MONADCATCHIO_TRANSFORMERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-MonadCatchIO-transformers+=	hs-MonadCatchIO-transformers>=0.0.2.0
+BUILDLINK_API_DEPENDS.hs-MonadCatchIO-transformers+=	hs-MonadCatchIO-transformers>=0.2.2.0
 BUILDLINK_PKGSRCDIR.hs-MonadCatchIO-transformers?=	../../wip/hs-MonadCatchIO-transformers
 
 .include "../../wip/hs-transformers/buildlink3.mk"
