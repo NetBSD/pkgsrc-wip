381eb367a90e0a6bc7bfc50869ad2ec2deab67b0
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-11-02 16:11:16 +0000

    Upstream update to transformers-0.2.2.0

--- hs-transformers/buildlink3.mk
+++ hs-transformers/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/09/23 05:12:01 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/11/02 16:11:16 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-transformers
 
 .if !defined(HS_TRANSFORMERS_BUILDLINK3_MK)
 HS_TRANSFORMERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-transformers+=	hs-transformers>=0.1.4.0
+BUILDLINK_API_DEPENDS.hs-transformers+=	hs-transformers>=0.2.2.0
 BUILDLINK_PKGSRCDIR.hs-transformers?=	../../wip/hs-transformers
 .endif	# HS_TRANSFORMERS_BUILDLINK3_MK
 
