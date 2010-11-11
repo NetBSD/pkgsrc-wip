8e837afbacd3ec44e52a575a5b9e29028a663a0f
Tobias Nygren <tnn@NetBSD.org>
2010-11-11 14:11:54 +0000

    Update to firefox-4.0b7.

--- xulrunner/dist.mk
+++ xulrunner/dist.mk
@@ -1,11 +1,11 @@
-# $NetBSD: dist.mk,v 1.21 2010/09/21 11:54:26 tnn2 Exp $
+# $NetBSD: dist.mk,v 1.22 2010/11/11 14:11:54 tnn2 Exp $
 #
 # used by wip/xulrunner/Makefile
 # used by wip/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	4.0b6${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	2.0b6
+FIREFOX_VER=	4.0b7${MOZ_BRANCH_MINOR}
+MOZ_BRANCH=	2.0b7
 MOZ_BRANCH_MINOR=	# empty
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
@@ -16,6 +16,6 @@ PATCHDIR=	${.CURDIR}/../../wip/xulrunner/patches
 WRKSRC=		${WRKDIR}/mozilla-central
 
 # snapshot overrides
-#DISTNAME=	0e0e2516f04e
+#DISTNAME=	297086a0fb61
 #MASTER_SITES=	http://hg.mozilla.org/mozilla-central/archive/
-#WRKSRC=	${WRKDIR}/mozilla-central-0e0e2516f04e
+#WRKSRC=	${WRKDIR}/mozilla-central-297086a0fb61
