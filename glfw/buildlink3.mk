bc31f60ade8b6d1893a452df6b3e8dd196d6e771
Ryo ONODERA <ryoon@NetBSD.org>
2010-11-18 10:02:47 +0000

    Add dependencies. Add buildlink3.mk.

---
+++ glfw/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2010/11/18 10:02:47 ryo-on Exp $
+
+BUILDLINK_TREE+=	glfw
+
+.if !defined(GLFW_BUILDLINK3_MK)
+GLFW_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.glfw+=	glfw>=2.7
+BUILDLINK_PKGSRCDIR.glfw?=	../../wip/glfw
+
+#.include "../../graphics/glu/buildlink3.mk"
+#.include "../../graphics/MesaLib/buildlink3.mk"
+#.include "../../x11/libX11/buildlink3.mk"
+#.include "../../x11/libXrandr/buildlink3.mk"
+.endif	# GLFW_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-glfw
