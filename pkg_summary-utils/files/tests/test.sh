89cec5feb71bd6a76f3ed3eb86c486e7821cd021
Aleksey Cheusov <cheusov@users.sourceforge.net>
2010-11-18 22:41:50 +0000

    Option "-c" was added to pkg_lint_summary. Man page for pkg_lint_summary was updated.

--- pkg_summary-utils/files/tests/test.sh
+++ pkg_summary-utils/files/tests/test.sh
@@ -2838,6 +2838,21 @@ cmp 'pkg_lint_summary -Ap #4' \
 d: not_found pkg_online-server-0.5.0nb2 <- wip/pkg_online pkg_online-0.5.0nb2
 '
 
+pkg_lint_summary -cd src_summary12.txt | sort |
+cmp 'pkg_lint_summary -Ap #5' \
+'c: conflict awk-pkgsrc-dewey-[0-9]* wip/awk-pkgsrc-dewey awk-pkgsrc-dewey-0.5.6 <- wip/pkg_summary-utils pkg_summary-utils-0.49beta2
+c: conflict egcs-current-19980608 somewhere/egcs-current egcs-current-19980608 <- lang/f2c f2c-20090411nb5
+c: conflict glibtool-[0-9]* somewhere/glibtool glibtool-1.2.3 <- devel/libtool-base libtool-base-2.2.6bnb4
+c: conflict libtool<=1.3.5nb11 somewhere/libtool libtool-1.3.4 <- devel/libtool-base libtool-base-2.2.6bnb4
+c: conflict libtool<=1.3.5nb11 somewhere/libtool133 libtool-1.3.3 <- devel/libtool-base libtool-base-2.2.6bnb4
+c: conflict paexec<0.15.0 wip/paexec paexec-0.13.0 <- wip/pkg_summary-utils pkg_summary-utils-0.49beta2
+c: conflict pkg_cmp_summary-[0-9]* somewhere/pkg_cmp_summary pkg_cmp_summary-0.0.1 <- wip/pkg_summary-utils pkg_summary-utils-0.49beta2
+c: conflict pkg_src_update_summary-[0-9]* somewhere/pkg_src_update_summary pkg_src_update_summary-0.0.2 <- wip/pkg_summary-utils pkg_summary-utils-0.49beta2
+c: conflict pkg_summary4view-[0-9]* somewhere/pkg_summary4view pkg_summary4view-0.0.4 <- wip/pkg_summary-utils pkg_summary-utils-0.49beta2
+c: conflict pkg_update_summary-[0-9]* somewhere/pkg_update_summary pkg_update_summary-0.0.3 <- wip/pkg_summary-utils pkg_summary-utils-0.49beta2
+d: not_found paexec>=0.15.0 <- wip/distbb distbb-0.38.2
+'
+
 # pkg_subgraph_deps
 pkg_subgraph_deps -f src_pkgs.txt src_deps.txt | sort |
 cmp 'pkg_subgraph_deps #1' \
