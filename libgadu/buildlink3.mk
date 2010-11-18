6e91365e86ba3bc8a96efbba981f4d59f9a58626
Daniel Horecki <morr@NetBSD.org>
2010-11-18 14:57:06 +0000

    update buildlink3.mk with rest of package

--- libgadu/buildlink3.mk
+++ libgadu/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2010/03/14 23:03:55 shindenmorr Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2010/11/18 14:57:06 shindenmorr Exp $
 
 BUILDLINK_TREE+=	libgadu
 
 .if !defined(LIBGADU_BUILDLINK3_MK)
 LIBGADU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgadu+=	libgadu>=1.9.0
-BUILDLINK_ABI_DEPENDS.libgadu+=	libgadu>=1.9.0
+BUILDLINK_API_DEPENDS.libgadu+=	libgadu>=1.9.0rc2
+BUILDLINK_ABI_DEPENDS.libgadu+=	libgadu>=1.9.0rc2
 BUILDLINK_PKGSRCDIR.libgadu?=	../../wip/libgadu
 .endif # LIBGADU_BUILDLINK3_MK
 
