b9eb35b1935b0e924fbdbe11e537782567f8232b
Pierre Allegraud <pallegra@users.sourceforge.net>
2010-11-22 23:39:34 +0000

    Import glsl-7.9 as wip/glsl.
    glsl_compiler is a GLSL compiler, and glcpp is a GLSL "C" preprocessor.
    This is a simple preprocessor designed to provide the preprocessing
    needs of the GLSL language. The requirements for this preprocessor are
    specified in the GLSL 1.30 specification availble from:
    http://www.opengl.org/registry/doc/GLSLangSpec.Full.1.30.10.pdf

---
+++ glsl/version.mk
@@ -0,0 +1,9 @@
+# $NetBSD: version.mk,v 1.1.1.1 2010/11/22 23:39:34 pallegra Exp $
+# used by wip/gl/Makefile.common
+# used by wip/MesaLib/Makefile
+
+MESA_DISTNAME=		MesaLib-${MESA_VERSION}
+MESA_SITES=		ftp://freedesktop.org/pub/mesa/${MESA_VERSION}/
+MESA_VERSION=		7.9
+MESA_DEMOS_VERSION=	7.8.2
+MESA_PKGVERSION=	${MESA_VERSION}
