aacbd43eb760f7ce947dc7e1751127ef472cf2cb
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-12-02 13:06:43 +0000

    Upstream update to GHC 7.0.1

--- ghc/buildlink3.mk
+++ ghc/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2010/05/10 01:59:14 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2010/12/02 13:06:43 phonohawk Exp $
 
 BUILDLINK_TREE+=	ghc
 
 .if !defined(GHC_BUILDLINK3_MK)
 GHC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ghc+=	ghc>=6.12.2
+BUILDLINK_API_DEPENDS.ghc+=	ghc>=7.0
 BUILDLINK_PKGSRCDIR.ghc?=	../../wip/ghc
 
 .include "../../converters/libiconv/buildlink3.mk"
