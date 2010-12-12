aa99824814a9f3efef84a84e669f6404c5200421
Ryo ONODERA <ryoon@NetBSD.org>
2010-12-12 20:33:47 +0000

    Add missing PLIST.doc=yes line. Support doc option.

--- quvi/options.mk
+++ quvi/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2010/06/25 21:23:34 ryo-on Exp $
+# $NetBSD: options.mk,v 1.2 2010/12/12 20:33:47 ryo-on Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.quvi
 PKG_SUPPORTED_OPTIONS+=	doc
@@ -8,6 +8,7 @@ PKG_SUPPORTED_OPTIONS+=	doc
 .if !empty(PKG_OPTIONS:Mdoc)
 BUILD_DEPENDS+=		doxygen>=1.4.7:../../devel/doxygen
 CONFIGURE_ARGS+=	--with-doc=yes
+PLIST.doc=		yes
 .else
 CONFIGURE_ARGS+=	--with-doc=no
 .endif
