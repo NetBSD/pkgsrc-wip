53665012ad9dfe4beb4bdcc89f075d021c3cd24a
Aleksej Saushev <asau@users.sourceforge.net>
2010-12-17 03:07:46 +0000

    Introduce threads, turn them on. While here, fix comment.

--- mpi-ch/options.mk
+++ mpi-ch/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2010/03/06 18:28:40 asau Exp $
+# $NetBSD: options.mk,v 1.2 2010/12/17 03:07:46 asau Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mpich
-PKG_SUPPORTED_OPTIONS=	debug mpich-mpd
-PKG_SUGGESTED_OPTIONS=	#
+PKG_SUPPORTED_OPTIONS=	debug mpich-mpd threads
+PKG_SUGGESTED_OPTIONS=	threads
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,7 +11,12 @@ CONFIGURE_ARGS+=	--enable-g=debug	# problems
 INSTALL_UNSTRIPPED=	yes
 .endif
 
-# Allow coexistance of OpenMPI and libotf by disabling OpenMPI parts:
+.if !empty(PKG_OPTIONS:Mthreads)
+CONFIGURE_ARGS+=	--with-thread-package=posix # aka pthreads
+.include "../../mk/pthread.buildlink3.mk"
+.endif
+
+# Allow coexistance of MPICH2 and musicpd by disabling MPICH2 parts:
 .if !empty(PKG_OPTIONS:Mmpich-mpd)
 CONFLICTS+=	musicpd-[0-9]*	# installs bin/mpd
 REPLACE_PYTHON+=	src/pm/mpd/*.py src/pm/mpd/test/*.py
