8793c77308fccc8bd5302b48fa71343163d38816
Aleksej Saushev <asau@users.sourceforge.net>
2010-12-17 03:06:42 +0000

    Use external OpenPA.

--- mpi-ch/buildlink3.mk
+++ mpi-ch/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2010/02/21 18:04:07 asau Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2010/12/17 03:06:42 asau Exp $
 
 BUILDLINK_TREE+=	mpich2
 
@@ -8,7 +8,7 @@ MPICH2_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.mpich2+=	mpich2>=1.2.99.6307
 BUILDLINK_PKGSRCDIR.mpich2?=	../../wip/mpi-ch
 
-#.include "../../parallel/openpa/buildlink3.mk"
+.include "../../parallel/openpa/buildlink3.mk"
 .endif	# MPICH2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mpich2
