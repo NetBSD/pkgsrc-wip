a16ccaad4dc442621b47104538b8775b5359bb56
Aleksej Saushev <asau@users.sourceforge.net>
2010-12-21 20:54:24 +0000

    Bring emulators/wine-devel into work area.

---
+++ wine/buildlink3.mk
@@ -0,0 +1,24 @@
+# $NetBSD: buildlink3.mk,v 1.5 2010/12/21 20:54:24 asau Exp $
+
+BUILDLINK_TREE+=	wine
+
+.if !defined(WINE_BUILDLINK3_MK)
+WINE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.wine+=	wine>=0.9.6nb1
+BUILDLINK_ABI_DEPENDS.wine?=	wine>=1.1.16nb2
+BUILDLINK_PKGSRCDIR.wine?=	../../emulators/wine
+
+pkgbase:= wine
+.include "../../mk/pkg-build-options.mk"
+
+.include "../../graphics/freetype2/buildlink3.mk"
+.if !empty(PKG_BUILD_OPTIONS.wine:Mopengl)
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../graphics/glut/buildlink3.mk"
+.endif
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/libungif/buildlink3.mk"
+.endif # WINE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-wine
