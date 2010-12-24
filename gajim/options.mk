4f36685b5362011e6246cf9d856a116b90398087
Thomas Klausner <wiz@NetBSD.org>
2010-12-24 00:00:05 +0000

    Update to 0.14.1 and get rid of lots of stuff that doesn't seem to be necessary any longer -- at least, configure doesn't recognize the options any longer and gajim works fine without the patches.
    Changes since 0.12.1:
    
    Gajim 0.14.1 (26 October 2010)
    
      * Fix changing account name
      * Fix sending XHTML
      * Fix GnomeKayring usage
      * Fix some GPG bugs
      * Minor bugfixes
    
    Gajim 0.14 (02 September 2010)
    
      * Jingle audio / video chat
      * Improve Startup time
      * Copy emoticons, LaTeX expressions when they are selected
      * Fix status icon transparency by using gtk.statusicon
      * Groupchat auto-rejoin
      * geolocation (with geoclue)
      * use XDG standards
      * SCRAM-SHA-1 and SASL EXTERNAL authentication
      * MUC captcha
      * Lots of refactoring
    
    Gajim 0.13.4 (02 April 2010)
    
      * Add japanese translation
      * Fix some TLS connection
      * Don't raise a lot of "DB Error" dialog
      * Fix contact synchronisation
      * Minor fixes
    
    Gajim 0.13.3 (23 February 2010)
    
      * Fix facebook xmpp server connection
      * Fix copy / paste with Ctrl+C on non-latin keyboard
      * Fix sending PEP information when connecting
      * Fix parsing HTML messages that have ascii markup
    
    Gajim 0.13.2 (14 January 2010)
    
      * Fix some translations
      * Fix string comparison according to locales
      * Fix resizing of groupchat occupant treeview
      * Fix some gnomekeyring glitches
      * better SRV usage with libasyncns
      * copy emoticons when we copy / paste in conversations
    
    Gajim 0.13.1 (28 November 2009)
    
      * Fix a bug when no account exists and bonjour is not available
      * Fix a bug when opening advanced option in MUC
      * Fix a bug when using non-BOSH proxies
    
    Gajim 0.13 (24 November 2009)
    
      * Improve gtkspell (fix memleak)
      * BOSH connection
      * Roster versioning
      * Ability to send contacts
      * GUI to send XHTML messages
      * Improve sessions handling
      * pubsub storage (for bookmarks)
      * Ability to select account when joining a groupchat
      * Better Gnome keyring support
      * Ability to ignore occupants in groupchats
      * Ability to show / hide self contact row
      * Automatically go away when screensaver is enabled under windows
      * Ability to enable / disable accounts
      * better URL recognition
      * groupchat autoreconnect
      * Store passwords in KDE wallet if available
      * Better MUC errors handling
      * Fix sound player launch (don't create zombies anymore)
      * Optional shell like completion
      * New color theme
    
    Gajim 0.12.5 (08 August 2009)
    
      * Don't depend on GTK 2.14
    
    Gajim 0.12.4 (07 August 2009)
    
      * Fix History manager
      * Fix file transfer proxy discovering at connection
      * Improve filetransfer with IPv6
      * Fix zeroconf when receiving badly encoded info
    
    Gajim 0.12.3 (12 June 2009)
    
      * Fix PLAIN authentication (in particular with Gtalk
      * fix PEP discovery
    
    Gajim 0.12.2 (07 June 2009)
    
      * Better keepalive / ping behaviour
      * Fix custom port handling
      * Improve error messages handling
      * Totem support for played music
      * Fix SSL with some servers
      * Handle XFCE notification-daemon
      * Restore old behaviour of click on systray: left click to open events
      * Network manager 0.7 support
      * Move logs file under windows to $APPDATA/gajim
      * Improve Kerberos support
      * Many bugfixes here and there
      * Add -c option to history_manager

--- gajim/options.mk
+++ gajim/options.mk
@@ -1,39 +1,10 @@
-# $NetBSD: options.mk,v 1.4 2009/01/07 23:17:17 tonnerre Exp $
+# $NetBSD: options.mk,v 1.5 2010/12/24 00:00:05 thomasklausner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gajim
-PKG_SUPPORTED_OPTIONS=	avahi dbus gnome gnome-keyring gtkspell ssl trayicon
-PKG_SUGGESTED_OPTIONS=	avahi dbus gtkspell ssl trayicon
+PKG_SUPPORTED_OPTIONS=	avahi gnome gnome-keyring ssl
+PKG_SUGGESTED_OPTIONS=	avahi ssl
 
 .include "../../mk/bsd.options.mk"
-
-# gtkspell
-.if !empty(PKG_OPTIONS:Mgtkspell)
-.include "../../textproc/gtkspell/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-gtkspell
-PLIST_SUBST+=	GTKSPELL=""
-.else
-CONFIGURE_ARGS+=	--disable-gtkspell
-PLIST_SUBST+=	GTKSPELL="@comment "
-.endif
-
-# dbus
-.if !empty(PKG_OPTIONS:Mdbus)
-.include "../../sysutils/py-dbus/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-remote
-PLIST_SUBST+=	REMOTE=""
-.else
-CONFIGURE_ARGS+=	--disable-remote
-PLIST_SUBST+=	REMOTE="@comment "
-.endif
-
-# trayicon
-.if !empty(PKG_OPTIONS:Mtrayicon)
-CONFIGURE_ARGS+=	--enable-trayicon
-PLIST_SUBST+=	TRAYICON=""
-.else
-CONFIGURE_ARGS+=	--disable-trayicon
-PLIST_SUBST+=	TRAYICON="@comment "
-.endif
 
 # ssl
 .if !empty(PKG_OPTIONS:Mssl)
