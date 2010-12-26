88e0a6169df731dc076438ca70d20c6c8a2b9e14
Francois Tigeot <ftigeot@wolfpond.org>
2010-12-26 12:27:10 +0000

    Remove the jdk15-plugin option. It is still broken and it has been many years since I've found a useful use for java browser plugins.

--- jdk15/options.mk
+++ jdk15/options.mk
@@ -1,8 +1,7 @@
-# $NetBSD: options.mk,v 1.16 2010/11/27 14:04:52 ftigeot Exp $
+# $NetBSD: options.mk,v 1.17 2010/12/26 12:27:10 ftigeot Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.jdk15
-PKG_SUPPORTED_OPTIONS=		sun-jre-jce jdk15-plugin inet6
-#PKG_SUGGESTED_OPTIONS=		jdk15-plugin
+PKG_SUPPORTED_OPTIONS=		sun-jre-jce inet6
 
 #.include "../../www/seamonkey/gecko-options.mk"
 .include "../../mk/bsd.options.mk"
@@ -19,25 +18,4 @@ DISTFILES+=	jce_policy-1_5_0.zip
 PLIST.jce=	yes
 post-build:
 	cd ${WRKDIR}/jce ; pax -rw -pp . ${JDKIMAGEDIR}/jre/lib/security
-.endif
-
-
-###
-### Mozilla plugin
-###
-.if !empty(PKG_OPTIONS:Mjdk15-plugin)
-
-MOZILLA=	${PKG_OPTIONS:@opt@${PKG_OPTIONS_GROUP.gecko:M${opt}}@}
-MAKE_ENV+=	BROWSER=${MOZILLA:Q}
-MAKE_ENV+=	ALT_MOZILLA_HEADERS_PATH=${PREFIX:Q}/include/${MOZILLA}
-
-#.include "../../www/seamonkey/gecko-depends.mk"
-# Check: is nspr necessary?
-#.include "../../devel/nspr/buildlink3.mk"
-BUILDLINK_DEPMETHOD.nspr=	build
-PLIST.plugin=		yes
-MESSAGE_SRC+=		MESSAGE.plugin
-
-.else
-MAKE_ENV+=		BUILD_PLUGIN=false
 .endif
