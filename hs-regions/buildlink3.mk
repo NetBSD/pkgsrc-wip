28f6d3ffae8e453d6be6df47459248c99deba686
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-12-30 08:31:08 +0000

    Upstream update to regions-0.8

--- hs-regions/buildlink3.mk
+++ hs-regions/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/11/02 17:00:10 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2010/12/30 08:31:08 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-regions
 
 .if !defined(HS_REGIONS_BUILDLINK3_MK)
 HS_REGIONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-regions+=	hs-regions>=0.7.0.1
+BUILDLINK_API_DEPENDS.hs-regions+=	hs-regions>=0.8
 BUILDLINK_PKGSRCDIR.hs-regions?=	../../wip/hs-regions
 
 .include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
-.include "../../wip/hs-MonadCatchIO-transformers/buildlink3.mk"
+.include "../../wip/hs-monad-peel/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_REGIONS_BUILDLINK3_MK
 
