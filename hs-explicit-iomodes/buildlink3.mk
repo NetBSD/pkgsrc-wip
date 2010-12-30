1a56075b10655ccbf04387f03ba6a3281ae2bf29
PHO / phonohawk <phonohawk@users.sourceforge.net>
2010-12-30 08:39:10 +0000

    Upstream update to explicit-iomodes-0.6.0.1

--- hs-explicit-iomodes/buildlink3.mk
+++ hs-explicit-iomodes/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/11/02 16:46:30 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/12/30 08:39:10 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-explicit-iomodes
 
 .if !defined(HS_EXPLICIT_IOMODES_BUILDLINK3_MK)
 HS_EXPLICIT_IOMODES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-explicit-iomodes+=	hs-explicit-iomodes>=0.5
+BUILDLINK_API_DEPENDS.hs-explicit-iomodes+=	hs-explicit-iomodes>=0.6
 BUILDLINK_PKGSRCDIR.hs-explicit-iomodes?=	../../wip/hs-explicit-iomodes
 
 .include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
