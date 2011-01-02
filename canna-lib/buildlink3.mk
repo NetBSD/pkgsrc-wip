212806bc9f6ed846989d804f9206dc976996676e
Makoto Fujiwara <makoto@ki.nu>
2011-01-02 08:49:59 +0000

    inputmethod/canna* for 3.7p3, intending to bump version from 3.6pl4. pkglint flags many points, but they are due to minimum changes from previous version.

---
+++ canna-lib/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/01/02 08:49:59 makoto Exp $
+
+BUILDLINK_TREE+=	Canna-lib
+
+.if !defined(CANNA_LIB_BUILDLINK3_MK)
+CANNA_LIB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.Canna-lib+=	Canna-lib>=3.6pl4
+BUILDLINK_PKGSRCDIR.Canna-lib?=	../../wip/canna-lib
+.endif # CANNA_LIB_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-Canna-lib
