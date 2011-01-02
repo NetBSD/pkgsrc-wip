2cea5611b6d0b5637545b931c1e13605c5e747df
Helge Muehlmeier <helgoman@users.sourceforge.net>
2011-01-02 21:03:45 +0000

    Update package to 0.53.0

--- py-logilab-common/buildlink3.mk
+++ py-logilab-common/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2010/04/19 09:59:11 jihbed Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/01/02 21:03:45 helgoman Exp $
 
 BUILDLINK_TREE+=	py-logilab-common
 
 .if !defined(PY_LOGILAB_COMMON_BUILDLINK3_MK)
 PY_LOGILAB_COMMON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.py-logilab-common+=	${PYPKGPREFIX}-logilab-common>=0.44.0
+BUILDLINK_API_DEPENDS.py-logilab-common+=	${PYPKGPREFIX}-logilab-common>=0.53.0
 BUILDLINK_PKGSRCDIR.py-logilab-common?=	../../wip/py-logilab-common
 
 .endif	# PY_LOGILAB_COMMON_BUILDLINK3_MK
