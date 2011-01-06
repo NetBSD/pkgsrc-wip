a3dc3bd6aef6623a4b3c8a837022a3e9ee3e42ba
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-01-06 15:47:29 +0000

    Upstream update to pcre-light-0.4

--- hs-pcre-light/buildlink3.mk
+++ hs-pcre-light/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:43:42 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/01/06 15:48:06 phonohawk Exp $
 #
 
 BUILDLINK_TREE+=	hs-pcre-light
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	hs-pcre-light
 HS_PCRE_LIGHT_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.hs-pcre-light?=	build
-BUILDLINK_API_DEPENDS.hs-pcre-light+=	hs-pcre-light>=0.3.1
+BUILDLINK_API_DEPENDS.hs-pcre-light+=	hs-pcre-light>=0.4
 BUILDLINK_PKGSRCDIR.hs-pcre-light?=	../../wip/hs-pcre-light
 
 .include "../../devel/pcre/buildlink3.mk"
