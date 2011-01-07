11501762833ec89e7298683cb63fdcf53b7dc6aa
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-01-07 10:08:05 +0000

    fix buildlink3.mk

--- hs-HTTP/buildlink3.mk
+++ hs-HTTP/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/09/24 07:22:12 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2011/01/07 10:08:05 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-HTTP
 
 .if !defined(HS_HTTP_BUILDLINK3_MK)
 HS_HTTP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.0.8
+BUILDLINK_API_DEPENDS.hs-HTTP+=	hs-HTTP>=4000.1
 BUILDLINK_PKGSRCDIR.hs-HTTP?=	../../wip/hs-HTTP
 
 .include "../../wip/hs-mtl/buildlink3.mk"
