d50058edd740861c529079296dbea07c69339cad
Grégoire Sutre <gsutre@NetBSD.org>
2011-01-09 15:21:50 +0000

    Update to latest trunk.  Use official bzr trunk and fix names via substitutions, instead of following personal branch.

--- grub2-current/download.mk
+++ grub2-current/download.mk
@@ -1,11 +1,12 @@
-# $NetBSD: download.mk,v 1.4 2010/06/07 00:55:25 gregoire Exp $
+# $NetBSD: download.mk,v 1.5 2011/01/09 15:21:50 gregoire Exp $
 #
 
 BUILD_DEPENDS+=		bzr>=1.0:../../devel/bzr
 BUILD_DEPENDS+=		rsync>=3.0:../../net/rsync
 
 DISTFILES?=		# empty
-BZR_REPOSITORY=		http://bzr.savannah.gnu.org/r/grub/people/gsutre/fixes
+BZR_REPOSITORY=		http://bzr.savannah.gnu.org/r/grub/trunk/grub
+BZR_REVISION=		3038
 UNIFONT_URL=		http://unifoundry.com/unifont-5.1.20080820.pcf.gz
 UNIFONT_EXT=		pcf.gz
 
@@ -16,24 +17,30 @@ do-bzr-extract:
 	${RUN} set -e;								\
 	cd ${WRKDIR};								\
 	${STEP_MSG} "Exporting from bazaar repository "${BZR_REPOSITORY};	\
-	bzr export ${DISTNAME} ${BZR_REPOSITORY}
+	bzr export -r ${BZR_REVISION} ${DISTNAME} ${BZR_REPOSITORY}
 
-post-extract:
-	cd ${WRKSRC} && rsync -Lrtvz  translationproject.org::tp/latest/grub/ po
+post-extract: do-get-translations
+
+.PHONY: do-get-translations
+do-get-translations:
+	cd ${WRKSRC} && rsync -Lrtvz translationproject.org::tp/latest/grub/ po
+	cd ${WRKSRC}/po && (ls *.po | sed -e 's,\.po$$,,') > LINGUAS
 .if !empty(PKG_OPTIONS:Mgrub-mkfont)
 	cd ${WRKSRC} && ftp -o unifont.${UNIFONT_EXT} ${UNIFONT_URL}
 .endif
 
-pre-configure:
+pre-configure: do-autogen
+
+.PHONY: do-autogen
+do-autogen:
 	cd ${WRKSRC} && ./autogen.sh
 
-# The script autogen.sh uses python and ruby.
+# The script autogen.sh uses python.
 PYTHON_FOR_BUILD_ONLY=	yes
 SUBST_CLASSES+=		fix-py
 SUBST_STAGE.fix-py=	pre-configure
-SUBST_MESSAGE.fix-py=	Appending python version in autogen.sh.
+SUBST_MESSAGE.fix-py=	Replacing python executable in autogen.sh.
 SUBST_FILES.fix-py=	autogen.sh
-SUBST_SED.fix-py=	-e 's,^python,python${PYVERSSUFFIX},g'
-BUILD_DEPENDS+=		ruby>=1.6:../../lang/ruby
+SUBST_SED.fix-py=	-e 's,^python,${PYTHONBIN},g'
 
 .include "../../lang/python/pyversion.mk"
