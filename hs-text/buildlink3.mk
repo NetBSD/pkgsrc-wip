76066a1d6f8377378cb3599b0ee7ab9203140697
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-01-12 02:40:24 +0000

    Seems like I imported a completely different package. Sorry.

--- hs-text/buildlink3.mk
+++ hs-text/buildlink3.mk
@@ -1,12 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/01/12 01:28:44 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/01/12 02:40:24 phonohawk Exp $
 
-BUILDLINK_TREE+=	hs-tar
+BUILDLINK_TREE+=	hs-text
 
-.if !defined(HS_TAR_BUILDLINK3_MK)
-HS_TAR_BUILDLINK3_MK:=
+.if !defined(HS_TEXT_BUILDLINK3_MK)
+HS_TEXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tar+=	hs-tar>=0.3
-BUILDLINK_PKGSRCDIR.hs-tar?=	../../wip/hs-text
-.endif	# HS_TAR_BUILDLINK3_MK
+BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=0.11
+BUILDLINK_PKGSRCDIR.hs-text?=	../../wip/hs-text
 
-BUILDLINK_TREE+=	-hs-tar
+.include "../../wip/hs-deepseq/buildlink3.mk"
+.endif	# HS_TEXT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-text
