4f5beedfb155f819a003bd960541ec837cd4fd06
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-01-12 02:34:04 +0000

    Upstream update to deepseq-1.1.0.2

--- hs-deepseq/buildlink3.mk
+++ hs-deepseq/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/02/09 03:25:07 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/01/12 02:34:04 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-deepseq
 
 .if !defined(HS_DEEPSEQ_BUILDLINK3_MK)
 HS_DEEPSEQ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-deepseq+=	hs-deepseq>=1.1.0.0
+BUILDLINK_API_DEPENDS.hs-deepseq+=	hs-deepseq>=1.1
 BUILDLINK_PKGSRCDIR.hs-deepseq?=	../../wip/hs-deepseq
 .endif	# HS_DEEPSEQ_BUILDLINK3_MK
 
