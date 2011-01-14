fc8a54adb7692ac7d204edc3130692d017bf00c6
Aleksej Saushev <asau@users.sourceforge.net>
2011-01-14 21:32:26 +0000

    Compat package name is "suse100_compat".

--- mk/linuxbin.pkg.mk
+++ mk/linuxbin.pkg.mk
@@ -1,10 +1,10 @@
-# $NetBSD: linuxbin.pkg.mk,v 1.26 2008/02/03 16:32:26 tnn2 Exp $
+# $NetBSD: linuxbin.pkg.mk,v 1.27 2011/01/14 21:32:26 asau Exp $
 
 BROKEN=		Please update linuxbin.pkg.mk for the new emulator framework.
 
 ###########################################################################
 #
-# $Id: linuxbin.pkg.mk,v 1.26 2008/02/03 16:32:26 tnn2 Exp $
+# $Id: linuxbin.pkg.mk,v 1.27 2011/01/14 21:32:26 asau Exp $
 #
 # Proposal: how should we deal with Linux binary packages packages
 #
@@ -83,7 +83,7 @@ LINUX_BASE_PREFERRED?=		suse
 
 LINUX_BASE_REQUIRED?=		${LINUX_BASE_PREFERRED}
 
-LINUX_BASE_PREFIX.suse?=	suse
+LINUX_BASE_PREFIX.suse?=	suse100
 LINUX_BASE_VERSION.suse=	9.1
 
 LINUX_BASE_PREFIX.slackware?=	slackware
