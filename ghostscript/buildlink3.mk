f255e5233353acf2c87cbc967a429664b1cc0bba
Makoto Fujiwara <makoto@ki.nu>
2011-01-26 08:24:16 +0000

    Import ghostscript-9.00 as wip/ghostscript. This is (almost minimum) upgrade from 8.71nb7 and two changes included: (1) options.mk has option: disable-compile-inits (PR/44400) (2) patche-be is added to suppress noisy warning for __func__ duplicate def.
    Ghostscript is the well-known PostScript interpreter which is available for
    all common and most esoteric platforms and supports many different printers
    and some displays.
    
    This is the latest version of Ghostscript, formerly being distributed
    under Aladdin Free Public License and released under GPL.  The main
    ghostscript version now includes support for cups, enabled by the
    "cups" option (which is off by default), and thus it is no longer
    necessary to use ghostscript-esp.

---
+++ ghostscript/buildlink3.mk
@@ -0,0 +1,33 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/01/26 08:24:16 makoto Exp $
+
+BUILDLINK_TREE+=	ghostscript
+
+.if !defined(GHOSTSCRIPT_BUILDLINK3_MK)
+GHOSTSCRIPT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ghostscript+=	ghostscript>=8.63nb1
+BUILDLINK_ABI_DEPENDS.ghostscript?=	ghostscript>=8.71nb7
+BUILDLINK_PKGSRCDIR.ghostscript?=	../../print/ghostscript
+
+.include "../../graphics/png/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+
+pkgbase := ghostscript
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.ghostscript:Mcups)
+.include "../../print/cups/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ghostscript:Mfontconfig)
+.include "../../fonts/fontconfig/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.ghostscript:Mx11)
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.endif
+.endif # GHOSTSCRIPT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ghostscript
---
+++ ghostscript/options.mk
@@ -0,0 +1,52 @@
+# $NetBSD: options.mk,v 1.1.1.1 2011/01/26 08:24:17 makoto Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.ghostscript
+PKG_SUPPORTED_OPTIONS=	x11 cups debug fontconfig disable-compile-inits
+PKG_SUGGESTED_OPTIONS=	x11 fontconfig
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mx11)
+CONFIGURE_ARGS+=	--with-x
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-x
+.endif
+
+PLIST_VARS+=		cups
+.if !empty(PKG_OPTIONS:Mcups)
+CONFIGURE_ARGS+=	--enable-cups
+PLIST.cups=		yes
+INSTALL_TARGET+=	install-cups
+
+CUPS_CONFDIR?=	${PKG_SYSCONFBASEDIR}/cups
+CUPS_EGDIR=	${PREFIX}/share/examples/cups
+CONF_FILES+=	${CUPS_EGDIR}/pstoraster.convs ${CUPS_CONFDIR}/pstoraster.convs
+
+SUBST_CLASSES+=		cupsetc
+SUBST_STAGE.cupsetc=	post-extract
+SUBST_MESSAGE.cupsetc=	Fixing CUPS etc directory path to install as example
+SUBST_FILES.cupsetc=	cups/cups.mak
+SUBST_SED.cupsetc=	-e 's|$$(CUPSSERVERROOT)|${CUPS_EGDIR}|g'
+
+.include "../../print/cups/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-cups
+.endif
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug
+.endif
+
+.if !empty(PKG_OPTIONS:Mfontconfig)
+.include "../../fonts/fontconfig/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-fontconfig
+.endif
+
+.if !empty(PKG_OPTIONS:Mdisable-compile-inits)
+CONFIGURE_ARGS+=        --disable-compile-inits
+.endif
