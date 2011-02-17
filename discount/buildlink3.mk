291845a09b9293ba52fd066a895d6dedb0a5c52f
Sébastien BOCAHU <zecrazytux@users.sourceforge.net>
2011-02-17 21:01:41 +0000

    Upgraded to version 2.0.6: Add new extras and a shared library.

--- discount/buildlink3.mk
+++ discount/buildlink3.mk
@@ -1,14 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/11/03 22:10:57 zecrazytux Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/02/17 21:01:41 zecrazytux Exp $
 
 BUILDLINK_TREE+=	discount
-
-BUILDLINK_DEPMETHOD.discount?=  build
 
 .if !defined(DISCOUNT_BUILDLINK3_MK)
 DISCOUNT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.discount+=	discount>=2.0.1
-BUILDLINK_PKGSRCDIR.discount?=	../../wip/discount
+BUILDLINK_PKGSRCDIR.discount?=		../../wip/discount
 .endif	# DISCOUNT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-discount
