ac1f742e5eb396cffd67a0ad99fc6c979c35c6cd
Georg Schwarz <gschwarz@users.sourceforge.net>
2011-02-19 17:14:58 +0000

    updated libexosip to 3.5.0

--- libexosip/buildlink3.mk
+++ libexosip/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:43:44 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/02/19 17:14:58 gschwarz Exp $
 
 BUILDLINK_TREE+=	libexosip2
 
 .if !defined(LIBEXOSIP2_BUILDLINK3_MK)
 LIBEXOSIP2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libexosip2+=	libeXosip2>=3.2.0
+BUILDLINK_API_DEPENDS.libexosip2+=	libeXosip2>=3.5.0
 BUILDLINK_PKGSRCDIR.libexosip2?=	../../wip/libexosip
 .endif # LIBEXOSIP2_BUILDLINK3_MK
 
