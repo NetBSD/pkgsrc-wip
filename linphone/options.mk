050233557fdb5ba813ed90a849c2f2f06d7c6426
Georg Schwarz <gschwarz@users.sourceforge.net>
2011-03-02 21:52:00 +0000

    upgraded to linphone 3.3.2

--- linphone/options.mk
+++ linphone/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2009/02/15 16:16:33 thomasklausner Exp $
+# $NetBSD: options.mk,v 1.3 2011/03/02 21:52:00 gschwarz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.linphone
 PKG_SUPPORTED_OPTIONS=	alsa inet6 linphone-gui linphone-video
@@ -12,6 +12,8 @@ CONFIGURE_ARGS+=	--enable-alsa
 
 .if empty(PKG_OPTIONS:Mlinphone-gui)
 CONFIGURE_ARGS+=	--enable-gnome_ui=no
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../devel/libglade/buildlink3.mk"
 PLIST_SUBST+=	GUI="@comment "
 .else
 PLIST_SUBST+=	GUI=
