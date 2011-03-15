ed477726718ab5e160de675ab54b1adc45d7d383
Tobias Nygren <tnn@NetBSD.org>
2011-03-15 20:18:48 +0000

    Update to ffx 4.0rc1

--- xulrunner/dist.mk
+++ xulrunner/dist.mk
@@ -1,11 +1,11 @@
-# $NetBSD: dist.mk,v 1.26 2011/03/05 17:09:39 tnn2 Exp $
+# $NetBSD: dist.mk,v 1.27 2011/03/15 20:18:48 tnn2 Exp $
 #
 # used by wip/xulrunner/Makefile
 # used by wip/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	4.0b12${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	2.0b12
+FIREFOX_VER=	4.0rc1${MOZ_BRANCH_MINOR}
+MOZ_BRANCH=	2.0rc1
 MOZ_BRANCH_MINOR=	# empty
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
@@ -13,7 +13,7 @@ EXTRACT_SUFX=	.tar.bz2
 DISTINFO_FILE=	${.CURDIR}/../../wip/xulrunner/distinfo
 PATCHDIR=	${.CURDIR}/../../wip/xulrunner/patches
 
-WRKSRC=		${WRKDIR}/mozilla-central
+WRKSRC=		${WRKDIR}/mozilla-2.0
 
 # snapshot overrides
 #DISTNAME=	77979cd6890d
