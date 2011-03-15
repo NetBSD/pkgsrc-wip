5d9e302b05c386fd7c35787a3271f22e1710ed59
Ryo ONODERA <ryoon@NetBSD.org>
2011-03-15 19:16:03 +0000

    * Fix GCONF_SCHEMAS lines. * Accept upstream's patch (workaround).

--- gnome-chemistry-utils/patches/patch-openbabel_babelserv.cc
+++ gnome-chemistry-utils/patches/patch-openbabel_babelserv.cc
@@ -1,24 +1,32 @@
-$NetBSD: patch-openbabel_babelserv.cc,v 1.1 2011/03/13 11:00:17 ryo-on Exp $
+$NetBSD: patch-openbabel_babelserv.cc,v 1.2 2011/03/15 19:16:03 ryo-on Exp $
 
-* This is workaround.
+* For the platform that does not support POLLRDHUP. This is workaround.
 
 --- openbabel/babelserv.cc.orig	2011-02-20 06:40:58.000000000 +0000
 +++ openbabel/babelserv.cc
-@@ -112,7 +112,7 @@ int main (int argc, char *argv[])
+@@ -112,7 +112,11 @@ int main (int argc, char *argv[])
  					return -5;
  				}
  				_fds.fd = service_socket;
--				_fds.events = POLLIN | POLLRDHUP;
-+				_fds.events = POLLIN | POLLHUP;
++#ifdef POLLRDHUP
+ 				_fds.events = POLLIN | POLLRDHUP;
++#else
++				_fds.events = POLLIN;
++#endif
  				_fds.revents = 0;
  				fds.push_back (_fds);
  				sockets[service_socket] = new BabelSocket (service_socket);
-@@ -127,7 +127,7 @@ int main (int argc, char *argv[])
+@@ -127,11 +131,13 @@ int main (int argc, char *argv[])
  						deleted.insert (i);
  					}
  				}
--				if (fds[i].revents & POLLRDHUP) {
-+				if (fds[i].revents & POLLHUP) {
++#ifdef POLLRDHUP
+ 				if (fds[i].revents & POLLRDHUP) {
  					delete sockets[fds[i].fd];
  					sockets.erase (fds[i].fd);
  					deleted.insert (i);
+ 				}
++#endif
+ 				fds[i].revents = 0;
+ 			}
+ 			// remove closed sockets
