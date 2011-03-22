2da8c9e6b20d0fcfbb108dec2e50d2bfb4f3186a
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-03-22 06:10:38 +0000

    Upstream update to synfigstudio-0.62.02

--- synfigstudio/buildlink3.mk
+++ synfigstudio/buildlink3.mk
@@ -1,21 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.5 2010/06/13 22:47:20 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2011/03/22 06:10:38 phonohawk Exp $
 
 BUILDLINK_TREE+=	synfigstudio
 
 .if !defined(SYNFIGSTUDIO_BUILDLINK3_MK)
 SYNFIGSTUDIO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.synfigstudio+=	synfigstudio>=0.61.08
-BUILDLINK_ABI_DEPENDS.synfigstudio?=	synfigstudio>=0.61.08nb2
+BUILDLINK_API_DEPENDS.synfigstudio+=	synfigstudio>=0.62.02
 BUILDLINK_PKGSRCDIR.synfigstudio?=	../../wip/synfigstudio
 
-.include "../../devel/ETL/buildlink3.mk"
+.include "../../wip/ETL/buildlink3.mk"
 .include "../../devel/libsigc++/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/glibmm/buildlink3.mk"
 .include "../../wip/synfig/buildlink3.mk"
 .include "../../x11/gtkmm/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
-.endif # SYNFIGSTUDIO_BUILDLINK3_MK
+.endif	# SYNFIGSTUDIO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-synfigstudio
