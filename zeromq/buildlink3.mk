c297c922cdbf61f944d610e81f2df5fe5f324bf1
OBATA Akio <obache@users.sourceforge.net>
2011-04-04 14:06:35 +0000

    require libuuid for Linux and Solaris.

--- zeromq/buildlink3.mk
+++ zeromq/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/04/04 14:00:46 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/04/04 14:06:35 obache Exp $
 #
 
 BUILDLINK_TREE+=	zeromq
@@ -8,6 +8,13 @@ ZEROMQ_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.zeromq+=	zeromq>=2.0.11
 BUILDLINK_PKGSRCDIR.zeromq?=	../../wip/zeromq
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} == "Linux" || ${OPSYS} == "SunOS"
+.include "../../devel/libuuid/buildlink3.mk"
+.endif
+
 .endif	# ZEROMQ_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-zeromq
