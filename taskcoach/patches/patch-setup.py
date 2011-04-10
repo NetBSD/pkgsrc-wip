ecd18c7c2f2221cdd6e0d80f9bcc1d63ef747d1c
Ryo ONODERA <ryoon@NetBSD.org>
2011-04-10 15:09:49 +0000

    Update to 1.2.13
    Release 1.2.13 - April 9, 2011
    This is a mixed bugfix and feature release.
    
    Bugs fixed:
    - The notes section was missing from the help contents. (SF#3241219)
    - Mark subtasks of tasks that have uncompleted prerequisites as inactive
      (grey). (SF#3237286)
    - In the setup script, don't assume Mac OS X when the operating system isn't
      Linux or Windows. There's also the possibility the user is using BSD.
      (SF#3236769)
    - Don't force users to accept the license since the license doesn't actually
      require users to accept it (Windows). (SF#3247643)
    - Upon startup, effort viewers would always show the column header popup menu
      for detail mode, even when the effort viewer would be in aggregate mode
      (i.e. effort aggregated per day, per week or per month).
    - Ask the user for confirmation before overwriting existing files.
      (SF#2794041)
    - When adding an item, automatically select it. When removing an item with a
      parent, automatically select it. Make sure keyboard navigation (up and
      down) correctly moves the selection after marking a task completed.
      (SF#3056999)
    
    Features added:
    - Allow for filtering by category via the View->Filter menu. This also adds a
      menu item for showing all items regardless of category, with Ctrl-R (R for
      Reset) as keyboard shortcut.
    - When possible, try to keep the task duration when changing its start date.
    - Tasks in the calendar view can now be dragged and dropped and resized to
      change their dates.
    
    Feature changed:
    - For consistency with the menu item for resetting category filters, the
      keyboard shortcut for resetting all filters is now Shift-Ctrl-R (R for
      Reset).

--- taskcoach/patches/patch-setup.py
+++ taskcoach/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.1.1.1 2011/03/11 12:32:11 ryo-on Exp $
+$NetBSD: patch-setup.py,v 1.2 2011/04/10 15:09:49 ryo-on Exp $
 
 * Support NetBSD
 
---- setup.py.orig	2011-03-10 19:40:39.000000000 +0000
+--- setup.py.orig	2011-04-09 09:12:39.000000000 +0000
 +++ setup.py
-@@ -83,7 +83,7 @@ if 'debian' in current_dist or 'ubuntu' 
-         ('share/pixmaps', ['icons.in/taskcoach.png'])]
+@@ -76,7 +76,7 @@ for language in languages:
+     setupOptions['classifiers'].append('Natural Language :: %s'%language)
  
  system = platform.system()
 -if system == 'Linux':
 +if (system == 'Linux') or (system == 'NetBSD'):
      setupOptions['package_data'] = {'taskcoachlib': ['bin.in/linux/_pysyncml.so']}
- elif system == 'Windows':
-     setupOptions['scripts'].append('taskcoach.pyw')
+     # Add data files for Debian-based systems:
+     current_dist = [dist.lower() for dist in platform.dist()]
