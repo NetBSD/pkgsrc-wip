0a93434fb6232027e00b3e4b5175e2943303e6fd
Bartosz Kuzma <bartosz.kuzma@gmail.com>
2011-04-18 13:42:33 +0000

    Update to 0.3.1.
    Changes:
     * API extensions
     * brought tag handling a bit inline with upcoming 0.6 draft CEE spec

--- libee/buildlink3.mk
+++ libee/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/04/14 09:06:55 bartoszkuzma Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/04/18 13:42:33 bartoszkuzma Exp $
 
 BUILDLINK_TREE+=	libee
 
 .if !defined(LIBEE_BUILDLINK3_MK)
 LIBEE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libee+=	libee>=0.3.0
-BUILDLINK_ABI_DEPENDS.libee+=	libee>=0.3.0
+BUILDLINK_API_DEPENDS.libee+=	libee>=0.3.1
+BUILDLINK_ABI_DEPENDS.libee+=	libee>=0.3.1
 BUILDLINK_PKGSRCDIR.libee?=	../../wip/libee
 
 .endif # LIBEE_BUILDLINK3_MK
