cef0f683ed5c7a4a6a6a5586d544439ab090438b
Bartosz Kuzma <bartosz.kuzma@gmail.com>
2011-04-18 13:41:31 +0000

    Update to version 0.3.1.
    Changes:
     * added -t option to normalizer so that only messages with a specified
       tag will be output
     * bugfix: abort if a tag was assigned to a message without any fields
       parsed out (uncommon scenario)
     * bugfix: mem leak on parse tree destruct - associated tags were not deleted
     * bugfix: potential abort in normalizer due to misadressing in debug
       message generation

--- liblognorm/buildlink3.mk
+++ liblognorm/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/04/14 09:09:46 bartoszkuzma Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/04/18 13:41:31 bartoszkuzma Exp $
 
 BUILDLINK_TREE+=	liblognorm
 
 .if !defined(LIBLOGNORM_BUILDLINK3_MK)
 LIBLOGNORM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liblognorm+=	liblognorm>=0.3.0
-BUILDLINK_ABI_DEPENDS.liblognorm+=	liblognorm>=0.3.0
+BUILDLINK_API_DEPENDS.liblognorm+=	liblognorm>=0.3.1
+BUILDLINK_ABI_DEPENDS.liblognorm+=	liblognorm>=0.3.1
 BUILDLINK_PKGSRCDIR.liblognorm=		../../wip/liblognorm
 
 .endif # LIBLOGNORM_BUILDLINK3_MK
