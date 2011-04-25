787d3714232874385d31ec115e94a465d0cd8978
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2011-04-25 23:03:16 +0000

    o fixed PKGNAME

--- minuit2/buildlink3.mk
+++ minuit2/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/04/02 22:10:45 jihbed Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/04/25 23:03:16 jihbed Exp $
 
-BUILDLINK_TREE+=	Minuit2
+BUILDLINK_TREE+=	minuit2
 
 .if !defined(MINUIT2_BUILDLINK3_MK)
 MINUIT2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.Minuit2+=	Minuit2>=5.28.00
-BUILDLINK_PKGSRCDIR.Minuit2?=	../../wip/minuit2
+BUILDLINK_API_DEPENDS.minuit2+=	minuit2>=5.28.00
+BUILDLINK_PKGSRCDIR.minuit2?=	../../wip/minuit2
 .endif	# MINUIT2_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-Minuit2
+BUILDLINK_TREE+=	-minuit2
