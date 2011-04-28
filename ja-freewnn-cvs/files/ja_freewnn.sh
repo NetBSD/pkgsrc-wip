26ac7d5d8ce3e6c6439455537a50c3797287d0b8
Makoto Fujiwara <makoto@ki.nu>
2011-04-28 13:48:14 +0000

    Import ja-freewnn-1.1.0_pl22nb20110428 as wip/ja-freewnn-cvs. (As FreeWnn has some updates on its CVS Repository, cvs version is imported to give opportunity to test the functions of it.)
    FreeWnn Open Source version of Wnn.
    This distribution contains Wnn Japanese/Chinese/Korean
    Input System.  Wnn is a network-extensible Kana-to-Kanji conversion system
    and was jointly developed and released by the Software Research Group
    of Kyoto University Research Institute for Mathematical Science, OMRON
    Corporation and Astec, Inc.

---
+++ ja-freewnn-cvs/files/ja_freewnn.sh
@@ -0,0 +1,34 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD: ja_freewnn.sh,v 1.1.1.1 2011/04/28 13:48:14 makoto Exp $
+#
+# PROVIDE: freewnn
+# REQUIRE: DAEMON
+
+. /etc/rc.subr
+
+name="freewnn"
+command_start="@PREFIX@/bin/Wnn4/jserver"
+command_stop="@PREFIX@/bin/Wnn4/wnnkill"
+pidfile=""
+
+cmd=${1:-start}
+
+case ${cmd} in
+start)
+    if [ -x ${command_start} ]; then
+	echo "Starting ${name}."
+        ${command_start} >/dev/null 2>&1
+    fi
+    ;;
+stop)
+    if [ -x ${command_stop} ]; then
+        echo "Stopping ${name}."
+        ${command_stop} >/dev/null 2>&1
+    fi
+    ;;
+*)
+    :
+    ;;
+esac
+exit 0
