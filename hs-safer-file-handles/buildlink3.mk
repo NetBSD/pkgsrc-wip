94c737ae9b9ca78655d8661dd409b53b896045cf
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-05-10 23:38:03 +0000

    Upstream update to safer-file-handles-0.10

--- hs-safer-file-handles/buildlink3.mk
+++ hs-safer-file-handles/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/12/30 08:52:32 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/05/10 23:38:03 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-safer-file-handles
 
 .if !defined(HS_SAFER_FILE_HANDLES_BUILDLINK3_MK)
 HS_SAFER_FILE_HANDLES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-safer-file-handles+=	hs-safer-file-handles>=0.9
+BUILDLINK_API_DEPENDS.hs-safer-file-handles+=	hs-safer-file-handles>=0.10
 BUILDLINK_PKGSRCDIR.hs-safer-file-handles?=	../../wip/hs-safer-file-handles
 
 .include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
 .include "../../wip/hs-explicit-iomodes/buildlink3.mk"
-.include "../../wip/hs-monad-peel/buildlink3.mk"
+.include "../../wip/hs-monad-control/buildlink3.mk"
 .include "../../wip/hs-pathtype/buildlink3.mk"
 .include "../../wip/hs-regional-pointers/buildlink3.mk"
 .include "../../wip/hs-regions/buildlink3.mk"
