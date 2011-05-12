7f230a032eac69418443a7c615c88673f68b22ca
David Brownlee <abs@absd.org>
2011-05-12 10:28:15 +0000

    Add missing libXcursor libXinerama libXtst buildlink3s

--- ecore/buildlink3.mk
+++ ecore/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2010/01/18 19:36:28 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2011/05/12 10:28:15 absd Exp $
 
 BUILDLINK_TREE+=	ecore
 
@@ -8,18 +8,21 @@ ECORE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.ecore+=	ecore>=0.9.9.063
 BUILDLINK_PKGSRCDIR.ecore?=	../../wip/ecore
 
-.include "../../devel/eet/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
-.include "../../graphics/evas/buildlink3.mk"
+.include "../../devel/eet/buildlink3.mk"
 .include "../../graphics/evas-buffer/buildlink3.mk"
 .include "../../graphics/evas-software-x11/buildlink3.mk"
+.include "../../graphics/evas/buildlink3.mk"
 .include "../../www/curl/buildlink3.mk"
 .include "../../x11/libXScrnSaver/buildlink3.mk"
 .include "../../x11/libXcomposite/buildlink3.mk"
+.include "../../x11/libXcursor/buildlink3.mk"
 .include "../../x11/libXdamage/buildlink3.mk"
+.include "../../x11/libXinerama/buildlink3.mk"
 .include "../../x11/libXp/buildlink3.mk"
 .include "../../x11/libXrandr/buildlink3.mk"
 .include "../../x11/libXrender/buildlink3.mk"
+.include "../../x11/libXtst/buildlink3.mk"
 .endif # ECORE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ecore
