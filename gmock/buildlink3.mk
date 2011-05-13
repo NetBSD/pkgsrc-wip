bc2afcba8c8e3a3a43fd4107e5933cd2d1e597f2
Ryo ONODERA <ryoon@NetBSD.org>
2011-05-13 10:58:41 +0000

    Fix wip/gtest to devel/googletest

--- gmock/buildlink3.mk
+++ gmock/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/05/12 12:10:15 ryo-on Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/05/13 11:02:56 ryo-on Exp $
 
 BUILDLINK_TREE+=	gmock
 
@@ -8,7 +8,7 @@ GMOCK_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gmock+=	gmock>=1.6.0
 BUILDLINK_PKGSRCDIR.gmock?=	../../wip/gmock
 
-.include "../../wip/gtest/buildlink3.mk"
+.include "../../devel/googletest/buildlink3.mk"
 .endif	# GMOCK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gmock
