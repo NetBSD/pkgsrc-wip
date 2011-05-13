e6f7426629d1eae6c7e3b996f9e8201416d7911a
Ryo ONODERA <ryoon@NetBSD.org>
2011-05-13 04:29:35 +0000

    Import FlightCrew-0.7.1 as wip/FlightCrew.
    * Now with non-C locale, core is dumped on NetBSD.
    
    FlightCrew is a C++, cross-platform, native code epub validator.
    It is composed of three parts:
    
    * FlightCrew, the validation library;
    * FlightCrew-cli, the command-line front-end to the FlightCrew library;
    * FlightCrew-gui, the GUI front-end to the FlightCrew library;

---
+++ FlightCrew/patches/patch-src_FlightCrew_tests_misc__tests_ConvertUtf8PathToBoostPath__test.cpp
@@ -0,0 +1,10 @@
+$NetBSD: patch-src_FlightCrew_tests_misc__tests_ConvertUtf8PathToBoostPath__test.cpp,v 1.1.1.1 2011/05/13 04:29:35 ryo-on Exp $
+
+--- src/FlightCrew/tests/misc_tests/ConvertUtf8PathToBoostPath_test.cpp.orig	2010-11-11 10:05:50.000000000 +0000
++++ src/FlightCrew/tests/misc_tests/ConvertUtf8PathToBoostPath_test.cpp
+@@ -1,4 +1,4 @@
+-﻿/************************************************************************
++/************************************************************************
+ **
+ **  Copyright (C) 2010  Strahinja Markovic
+ **
