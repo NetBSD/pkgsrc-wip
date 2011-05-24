221c114f8a8e9b4fc44c923a55c1ffd297c25a0d
Makoto Fujiwara <makoto@ki.nu>
2011-05-24 01:46:51 +0000

    (1) MESSAGE fix typo on PATH (example -> examples)
    (2) PLIST add one entry
      +${EMACS_LISPPREFIX}/tc-2.3.1/tc-is22.el
    
    (3) patches/patch-lisp_tc-sysdep.el (+ distinfo)
       Upstream has change. but that is not enough
       So patch is still there with minor mod.
       And correct the comment for the patch.

--- tc-svn/patches/patch-lisp_tc-sysdep.el
+++ tc-svn/patches/patch-lisp_tc-sysdep.el
@@ -1,14 +1,16 @@
-$NetBSD: patch-lisp_tc-sysdep.el,v 1.1.1.1 2011/05/23 13:37:52 makoto Exp $
+$NetBSD: patch-lisp_tc-sysdep.el,v 1.2 2011/05/24 01:46:51 makoto Exp $
 
 To work with emacs-22 or later
+You will get following error if C-s tried for incremental search.
+Symbol's function definition is void: isearch-last-command-char
 
---- lisp/tc-sysdep.el.orig	2011-04-14 05:04:48.000000000 +0000
+--- lisp/tc-sysdep.el.orig	2011-05-24 01:21:00.000000000 +0000
 +++ lisp/tc-sysdep.el
 @@ -228,7 +228,7 @@ BODY should be a list of lisp expression
  ;;;
  ;;; Fix incompatibilities between 18 and 19.
  ;;;
--(if (string-match "^\\(19\\|2[01]\\)" emacs-version)
+-(if (string-match "^\\(19\\|2[0123]\\)" emacs-version)
 +(if (string-match "^\\(19\\|2[0-9]\\)" emacs-version)
      (progn
        (defun tcode-redo-command (ch)
