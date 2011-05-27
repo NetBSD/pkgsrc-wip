24d4991fcbb2976d55c29e925d3c9443560000f5
Eric Schnoebelen <schnoebe@NetBSD.org>
2011-05-27 03:22:19 +0000

    Correct the buildlink internal constructs.

--- py-construct/buildlink3.mk
+++ py-construct/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/05/15 04:18:09 schnoebe Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/05/27 03:22:19 schnoebe Exp $
 
 BUILDLINK_TREE+=	pyconstruct
 
-.if !defined(PY_NUMPY_BUILDLINK3_MK)
-PY_NUMPY_BUILDLINK3_MK:=
+.if !defined(PY_CONSTRUCT_BUILDLINK3_MK)
+PY_CONSTRUCT_BUILDLINK3_MK:=
 
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.pyconstruct+=	${PYPKGPREFIX}-construct>=2.04
 BUILDLINK_PKGSRCDIR.pyconstruct?=	../../wip/py-construct
 
-.endif # PY_NUMPY_BUILDLINK3_MK
+.endif # PY_CONSTRUCT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-pyconstruct
