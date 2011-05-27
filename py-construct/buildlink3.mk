78dfcf793b61b103a36be1e5fd15cca536489075
Eric Schnoebelen <schnoebe@NetBSD.org>
2011-05-27 03:49:58 +0000

    pkglint cleanup.

--- py-construct/buildlink3.mk
+++ py-construct/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/05/27 03:22:19 schnoebe Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/05/27 03:49:58 schnoebe Exp $
 
-BUILDLINK_TREE+=	pyconstruct
+BUILDLINK_TREE+=	py-construct
 
 .if !defined(PY_CONSTRUCT_BUILDLINK3_MK)
 PY_CONSTRUCT_BUILDLINK3_MK:=
 
 .include "../../lang/python/pyversion.mk"
 
-BUILDLINK_API_DEPENDS.pyconstruct+=	${PYPKGPREFIX}-construct>=2.04
-BUILDLINK_PKGSRCDIR.pyconstruct?=	../../wip/py-construct
+BUILDLINK_API_DEPENDS.py-construct+=	${PYPKGPREFIX}-construct>=2.04
+BUILDLINK_PKGSRCDIR.py-construct?=	../../wip/py-construct
 
 .endif # PY_CONSTRUCT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-pyconstruct
+BUILDLINK_TREE+=	-py-construct
