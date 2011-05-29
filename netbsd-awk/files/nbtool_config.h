25eb9f4eac7a3e804a557824eabf216c3640af0a
Aleksey Cheusov <cheusov@users.sourceforge.net>
2011-05-29 08:56:50 +0000

    Remove patch. Use files/nbtool_config.h instead

--- netbsd-awk/files/nbtool_config.h
+++ netbsd-awk/files/nbtool_config.h
@@ -1,3 +1,16 @@
+#ifndef _NBTOOL_CONFIG_H_
+#define _NBTOOL_CONFIG_H_
+
 #include <stdint.h>
 
 #define __UNCONST(a)   ((void *) (intptr_t) (const void *) (a))
+
+#ifndef HAVE_FUNC1_FPURGE
+int fpurge (FILE *stream);
+#endif
+
+#ifndef HAVE_FUNC3_STRLCAT
+size_t strlcat(char *dst, const char *src, size_t size);
+#endif
+
+#endif // _NBTOOL_CONFIG_H_
