836879e1a26176657a91798d75bbe83443ce365f
Pierre Allegraud <pallegra@users.sourceforge.net>
2011-05-30 20:22:51 +0000

    update to i3-tree-pr3
    from RELEASE-NOTES-tree-pr3:
    What should be working in this release?
    ---------------------------------------
    
     - Basic window management, navigation, moving
     - Fullscreen mode, correct aspect ratio
     - Stacked/Tabbed layout, floating mode
     - Splitting (for fancy layouts), resizing
     - Restarts (preserving the layout), crash handler
     - RandR changes, keyboard layout changes
     - Dock clients
     - Assignments
     - i3bar, get it from http://git.merovius.de/
    
    What is not working in this release?
    ------------------------------------
    
     - Configfile compatibility

--- i3-devel/patches/patch-common.mk
+++ i3-devel/patches/patch-common.mk
@@ -1,11 +1,12 @@
-$NetBSD: patch-common.mk,v 1.1 2011/03/11 12:16:10 pallegra Exp $
+$NetBSD: patch-common.mk,v 1.2 2011/05/30 20:22:51 pallegra Exp $
 
---- common.mk.orig	2011-03-07 19:24:40.000000000 +0000
+--- common.mk.orig	2011-05-28 20:13:18.000000000 +0000
 +++ common.mk
-@@ -1,11 +1,11 @@
+@@ -1,12 +1,12 @@
  UNAME=$(shell uname)
 -DEBUG=1
 +DEBUG?=1
+ COVERAGE=0
  INSTALL=install
 -PREFIX=/usr
 +PREFIX?=/usr
@@ -17,8 +18,8 @@ $NetBSD: patch-common.mk,v 1.1 2011/03/11 12:16:10 pallegra Exp $
 +SYSCONFDIR?=$(PREFIX)/etc
  endif
  # The escaping is absurd, but we need to escape for shell, sed, make, define
- GIT_VERSION:="tree-pr2 (2011-03-07, branch \\\"tree\\\")"
-@@ -52,8 +52,8 @@ LDFLAGS += $(call ldflags_for_lib, xcb-r
+ GIT_VERSION:="tree-pr3 (2011-05-28, branch \\\"tree\\\")"
+@@ -62,8 +62,8 @@ LDFLAGS += $(call ldflags_for_lib, xcb-r
  LDFLAGS += $(call ldflags_for_lib, xcb, xcb)
  LDFLAGS += $(call ldflags_for_lib, xcursor, Xcursor)
  LDFLAGS += $(call ldflags_for_lib, x11, X11)
@@ -26,6 +27,6 @@ $NetBSD: patch-common.mk,v 1.1 2011/03/11 12:16:10 pallegra Exp $
 -LDFLAGS += $(call ldflags_for_lib, libev, ev)
 +LDFLAGS += -lyajl
 +LDFLAGS += -lev
- LDFLAGS += -L/usr/local/lib -L/usr/pkg/lib
  
  ifeq ($(UNAME),NetBSD)
+ # We need -idirafter instead of -I to prefer the system’s iconv over GNU libiconv
