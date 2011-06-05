ac262d4c71b8ce4338dd9244cf36905fb5af20b1
Grégoire Sutre <gsutre@NetBSD.org>
2011-06-05 21:59:32 +0000

    Recreate the package, based on grub2-current.

--- grub2/options.mk
+++ grub2/options.mk
@@ -1,38 +1,40 @@
-# $NetBSD: options.mk,v 1.3 2009/12/18 11:03:44 gregoire Exp $
+# $NetBSD: options.mk,v 1.4 2011/06/05 21:59:32 gregoire Exp $
 #
 
 #
 # Description of options (taken from configure --help):
-# grub-mm-debug		include memory manager debugging
-# grub-emu		build and install the `grub-emu' debugging utility
-# grub-mkfont		build and install the `grub-mkfont' utility
+# debug			include memory manager debugging
+# freetype		build and install the `grub-mkfont' utility
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.grub2
-PKG_SUPPORTED_OPTIONS=	grub-mm-debug grub-emu grub-mkfont
-PKG_SUGGESTED_OPTIONS=	grub-mm-debug grub-emu grub-mkfont
+PKG_SUPPORTED_OPTIONS=	debug freetype
+PKG_SUGGESTED_OPTIONS=	freetype
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		emu mkfont
+PLIST_VARS+=		freetype
 
-.if !empty(PKG_OPTIONS:Mgrub-mm-debug)
+UNIFONT_URL=		http://unifoundry.com/unifont-5.1.20080820.pcf.gz
+UNIFONT_EXT=		pcf.gz
+
+post-extract: do-download-unifont
+.PHONY: do-download-unifont
+
+.if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-mm-debug
 .else
 CONFIGURE_ARGS+=	--disable-mm-debug
 .endif
 
-.if !empty(PKG_OPTIONS:Mgrub-emu)
-CONFIGURE_ARGS+=	--enable-grub-emu
-PLIST.emu=		yes
-.else
-CONFIGURE_ARGS+=	--disable-grub-emu
-.endif
-
-.if !empty(PKG_OPTIONS:Mgrub-mkfont)
+.if !empty(PKG_OPTIONS:Mfreetype)
 CONFIGURE_ARGS+=	--enable-grub-mkfont
-PLIST.mkfont=		yes
+PLIST.freetype=		yes
+USE_TOOLS+=		ftp
+do-download-unifont:
+	cd ${WRKSRC} && ftp -o unifont.${UNIFONT_EXT} ${UNIFONT_URL}
 .include "../../graphics/freetype2/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-grub-mkfont
+do-download-unifont:
 .endif
