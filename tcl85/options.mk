eabbd1883b46deec4e20befdd963618f222f18d2
Aleksej Saushev <asau@users.sourceforge.net>
2011-06-07 20:44:24 +0000

    Rename options variable to "tcl". Rename options to standard ones: "threaded" -> "threads", "memdebug" -> "debug".

--- tcl85/options.mk
+++ tcl85/options.mk
@@ -1,16 +1,16 @@
-# $NetBSD: options.mk,v 1.2 2011/06/07 20:41:26 asau Exp $
+# $NetBSD: options.mk,v 1.3 2011/06/07 20:44:24 asau Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.tcltk85
-PKG_SUPPORTED_OPTIONS=	threaded memdebug
-PKG_SUGGESTED_OPTIONS=	threaded
+PKG_OPTIONS_VAR=	PKG_OPTIONS.tcl
+PKG_SUPPORTED_OPTIONS=	threads debug
+PKG_SUGGESTED_OPTIONS=	threads
 
 .include "../../mk/bsd.options.mk"
 
-.  if !empty(PKG_OPTIONS:Mmemdebug)
+.  if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=	--enable-symbols=mem
 .  endif
 
-.  if !empty(PKG_OPTIONS:Mthreaded)
+.  if !empty(PKG_OPTIONS:Mthreads)
 CONFIGURE_ARGS+=	--enable-threads=on
 .  else
 CONFIGURE_ARGS+=	--enable-threads=off
