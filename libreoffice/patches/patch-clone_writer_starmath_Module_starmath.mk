cbd801ea2afd9062c2dc390b52065a055acfed22
Francois Tigeot <ftigeot@wolfpond.org>
2011-06-15 17:20:20 +0000

    Update to a newer snapshot. Among other fixes, the platform/netbsd.mk patch has been integrated upstream and libadabas disabled on all platforms.

--- libreoffice/patches/patch-clone_writer_starmath_Module_starmath.mk
+++ libreoffice/patches/patch-clone_writer_starmath_Module_starmath.mk
@@ -1,14 +1,16 @@
-$NetBSD: patch-clone_writer_starmath_Module_starmath.mk,v 1.1.1.1 2011/05/18 11:50:18 ftigeot Exp $
+$NetBSD: patch-clone_writer_starmath_Module_starmath.mk,v 1.2 2011/06/15 17:20:21 ftigeot Exp $
 
---- clone/writer/starmath/Module_starmath.mk.orig	2011-05-11 12:24:36 +0000
+--- clone/writer/starmath/Module_starmath.mk.orig	2011-06-14 15:12:29 +0000
 +++ clone/writer/starmath/Module_starmath.mk
-@@ -29,10 +29,6 @@ $(eval $(call gb_Module_add_targets,sm,\
+@@ -29,12 +29,6 @@ $(eval $(call gb_Module_add_targets,sm,\
      Package_uiconfig \
  ))
  
+-ifneq ($(OS),OPENBSD)
 -$(eval $(call gb_Module_add_check_targets,sm,\
 -    CppunitTest_starmath_qa_cppunit \
 -))
+-endif
 -
  $(eval $(call gb_Module_add_subsequentcheck_targets,sm,\
      JunitTest_starmath_unoapi \
