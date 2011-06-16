e7a162865b5ebf7fe5080615358d482a7ebbc818
Makoto Fujiwara <makoto@ki.nu>
2011-06-16 03:08:54 +0000

    PKGNAME corrected to 1.11alpha22  (from 1.1.1_pl22) so that it is greater than existing one (1.10) and that it will be less than hopefully-upcoming 1.11

--- ja-freewnn-lib/buildlink3.mk
+++ ja-freewnn-lib/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/06/01 15:57:33 makoto Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/06/16 03:08:54 makoto Exp $
 
 BUILDLINK_TREE+=	ja-FreeWnn-lib
 
 .if !defined(JA_FREEWNN_LIB_BUILDLINK3_MK)
 JA_FREEWNN_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ja-FreeWnn-lib+=	ja-freewnn-lib>=1.1.1
+BUILDLINK_API_DEPENDS.ja-FreeWnn-lib+=	ja-freewnn-lib>=1.11alpha22
 BUILDLINK_PKGSRCDIR.ja-FreeWnn-lib?=	../../wip/ja-freewnn-lib
 BUILDLINK_DEPMETHOD.ja-FreeWnn-lib?=	build
 .endif # JA_FREEWNN_LIB_BUILDLINK3_MK
