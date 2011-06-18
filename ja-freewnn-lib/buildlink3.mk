81558c80ee7d7ad9bbdcd5d41d35b35d101135d9
OBATA Akio <obache@users.sourceforge.net>
2011-06-18 11:29:24 +0000

    Change PKGNAME to same as pkgsrc/inputmethod one.

--- ja-freewnn-lib/buildlink3.mk
+++ ja-freewnn-lib/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2011/06/18 02:59:38 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2011/06/18 11:30:50 obache Exp $
 
 BUILDLINK_TREE+=	ja-FreeWnn-lib
 
 .if !defined(JA_FREEWNN_LIB_BUILDLINK3_MK)
 JA_FREEWNN_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ja-FreeWnn-lib+=	ja-freewnn-lib>=1.11alpha22
+BUILDLINK_API_DEPENDS.ja-FreeWnn-lib+=	ja-FreeWnn-lib>=1.11alpha22
 BUILDLINK_PKGSRCDIR.ja-FreeWnn-lib?=	../../wip/ja-freewnn-lib
 .endif # JA_FREEWNN_LIB_BUILDLINK3_MK
 
