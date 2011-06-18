6f529d215aced3179e4dbb69cbde4be7106c9ecf
OBATA Akio <obache@users.sourceforge.net>
2011-06-18 02:59:38 +0000

    installing shlib, runtime dependency by default.

--- ja-freewnn-lib/buildlink3.mk
+++ ja-freewnn-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/06/16 03:08:54 makoto Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/06/18 02:59:38 obache Exp $
 
 BUILDLINK_TREE+=	ja-FreeWnn-lib
 
@@ -7,7 +7,6 @@ JA_FREEWNN_LIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ja-FreeWnn-lib+=	ja-freewnn-lib>=1.11alpha22
 BUILDLINK_PKGSRCDIR.ja-FreeWnn-lib?=	../../wip/ja-freewnn-lib
-BUILDLINK_DEPMETHOD.ja-FreeWnn-lib?=	build
 .endif # JA_FREEWNN_LIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ja-FreeWnn-lib
