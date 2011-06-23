26c7fdc2c7369d6aef3022e9d3705e1ff97c8441
Francois Tigeot <ftigeot@wolfpond.org>
2011-06-23 17:19:37 +0000

    Hardcode pkgsrc and native X11 library directories for NetBSD.

---
+++ libreoffice/patches/patch-clone_libs-core_desktop_scripts_soffice.sh
@@ -0,0 +1,12 @@
+$NetBSD: patch-clone_libs-core_desktop_scripts_soffice.sh,v 1.1 2011/06/23 17:19:38 ftigeot Exp $
+
+--- clone/libs-core/desktop/scripts/soffice.sh.orig	2011-06-14 22:00:33.000000000 +0000
++++ clone/libs-core/desktop/scripts/soffice.sh
+@@ -87,6 +87,7 @@ NetBSD|OpenBSD|FreeBSD|DragonFly)
+     sd_prog1="$sd_prog/../basis-link/program"
+     sd_prog2="$sd_prog/../basis-link/ure-link/lib"
+     LD_LIBRARY_PATH=$sd_prog1:$sd_prog2${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
++    LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/pkg/lib:/usr/X11R7/lib
+     JAVA_HOME=$(javaPathHelper -h libreoffice-java 2> /dev/null)
+     export LD_LIBRARY_PATH
+     if [ -n "${JAVA_HOME}" ]; then
