4f48b5ec09de438e4b1c8dadd62a9a49e33549f5
Reinoud Zandijk <reinoud@NetBSD.org>
2011-06-29 14:20:28 +0000

    Fix buildlink3 references to point to wip

--- libxfce4ui/buildlink3.mk
+++ libxfce4ui/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2011/06/29 10:33:57 reinoudz70 Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/06/29 14:20:28 reinoudz70 Exp $
 
 BUILDLINK_TREE+=	libxfce4ui
 
@@ -8,8 +8,8 @@ LIBXFCE4UI_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libxfce4ui+=	libxfce4ui>=4.8.0
 BUILDLINK_PKGSRCDIR.libxfce4ui?=	../../wip/libxfce4ui
 
-.include "../../devel/xfconf/buildlink3.mk"
-.include "../../x11/libxfce4util/buildlink3.mk"
+.include "../../wip/xfconf/buildlink3.mk"
+.include "../../wip/libxfce4util/buildlink3.mk"
 .include "../../x11/startup-notification/buildlink3.mk"
 .endif # LIBXFCE4UI_BUILDLINK3_MK
 
