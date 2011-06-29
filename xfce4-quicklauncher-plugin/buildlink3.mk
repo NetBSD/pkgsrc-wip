36e2af4d5bb0df0958f0bd46c5e9d1a6c6df2820
Reinoud Zandijk <reinoud@NetBSD.org>
2011-06-29 19:53:29 +0000

    Oops, forgot its body!

---
+++ xfce4-quicklauncher-plugin/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.7 2011/06/29 19:53:29 reinoudz70 Exp $
+
+BUILDLINK_TREE+=	xfce4-quicklauncher-plugin
+
+.if !defined(XFCE4_QUICKLAUNCHER_PLUGIN_BUILDLINK3_MK)
+XFCE4_QUICKLAUNCHER_PLUGIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.xfce4-quicklauncher-plugin+=	xfce4-quicklauncher-plugin>=1.9.4
+BUILDLINK_ABI_DEPENDS.xfce4-quicklauncher-plugin?=	xfce4-quicklauncher-plugin>=1.9.4nb6
+BUILDLINK_PKGSRCDIR.xfce4-quicklauncher-plugin?=	../../sysutils/xfce4-quicklauncher-plugin
+
+.include "../../wip/xfce4-panel/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.endif # XFCE4_QUICKLAUNCHER_PLUGIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-xfce4-quicklauncher-plugin
