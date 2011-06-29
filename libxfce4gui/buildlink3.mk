f6ce852657d66ade36af4a549b58343f4e99ab60
Reinoud Zandijk <reinoud@NetBSD.org>
2011-06-29 10:45:24 +0000

    Add libxfce4gui compatibility package skeleton for xfce 4.8

---
+++ libxfce4gui/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.11 2011/06/29 10:45:24 reinoudz70 Exp $
+
+BUILDLINK_TREE+=	libxfce4gui
+
+.if !defined(LIBXFCE4GUI_BUILDLINK3_MK)
+LIBXFCE4GUI_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libxfce4gui+=	libxfce4gui>=4.6.0
+BUILDLINK_ABI_DEPENDS.libxfce4gui?=	libxfce4gui>=4.6.1nb5
+BUILDLINK_PKGSRCDIR.libxfce4gui?=	../../x11/libxfce4gui
+
+.include "../../devel/glib2/buildlink3.mk"
+#.include "../../devel/xfconf/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/libxfce4util/buildlink3.mk"
+.include "../../x11/startup-notification/buildlink3.mk"
+.endif # LIBXFCE4GUI_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libxfce4gui
