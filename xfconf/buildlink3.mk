58610a501ad795f38ddc19a52452c2ef42f481d4
Reinoud Zandijk <reinoud@NetBSD.org>
2011-06-29 19:35:51 +0000

    Fix buildlink3 reference to make it point to wip

--- xfconf/buildlink3.mk
+++ xfconf/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2011/06/29 10:03:40 reinoudz70 Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/06/29 19:35:51 reinoudz70 Exp $
 
 BUILDLINK_TREE+=	xfconf
 
@@ -7,7 +7,7 @@ XFCONF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xfconf+=	xfconf>=4.6.0
 BUILDLINK_ABI_DEPENDS.xfconf?=	xfconf>=4.6.1nb5
-BUILDLINK_PKGSRCDIR.xfconf?=	../../devel/xfconf
+BUILDLINK_PKGSRCDIR.xfconf?=	../../wip/xfconf
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../sysutils/dbus-glib/buildlink3.mk"
