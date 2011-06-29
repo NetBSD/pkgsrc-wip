2529f32d0e3a548f71afe0aa42e010f3e9c12c12
Reinoud Zandijk <reinoud@NetBSD.org>
2011-06-29 12:41:45 +0000

    Add xfce4-panel pacakge skeleton for xfce 4.8

---
+++ xfce4-panel/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.10 2011/06/29 12:41:45 reinoudz70 Exp $
+
+BUILDLINK_TREE+=	xfce4-panel
+
+.if !defined(XFCE4_PANEL_BUILDLINK3_MK)
+XFCE4_PANEL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.xfce4-panel+=	xfce4-panel>=4.6.0
+BUILDLINK_ABI_DEPENDS.xfce4-panel?=	xfce4-panel>=4.6.2nb6
+BUILDLINK_PKGSRCDIR.xfce4-panel?=	../../x11/xfce4-panel
+
+.include "../../devel/glib2/buildlink3.mk"
+#.include "../../x11/libxfce4gui/buildlink3.mk"
+.include "../../x11/libxfce4util/buildlink3.mk"
+.endif # XFCE4_PANEL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-xfce4-panel
