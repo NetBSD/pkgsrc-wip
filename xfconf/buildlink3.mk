4c75a32d7a01eaef2638c2c3f33f008545001d98
Reinoud Zandijk <reinoud@NetBSD.org>
2011-06-29 10:03:40 +0000

    Add xfconf template package for xfce 4.8

---
+++ xfconf/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2011/06/29 10:03:40 reinoudz70 Exp $
+
+BUILDLINK_TREE+=	xfconf
+
+.if !defined(XFCONF_BUILDLINK3_MK)
+XFCONF_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.xfconf+=	xfconf>=4.6.0
+BUILDLINK_ABI_DEPENDS.xfconf?=	xfconf>=4.6.1nb5
+BUILDLINK_PKGSRCDIR.xfconf?=	../../devel/xfconf
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../sysutils/dbus-glib/buildlink3.mk"
+.include "../../sysutils/dbus/buildlink3.mk"
+.endif	# XFCONF_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-xfconf
