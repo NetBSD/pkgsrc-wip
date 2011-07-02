e8774053b95a41c2b080f6c24a91ecdd35afd16d
Reinoud Zandijk <reinoud@NetBSD.org>
2011-07-02 20:11:49 +0000

    Add the xfce4ui library just in case

--- xfce4-panel/buildlink3.mk
+++ xfce4-panel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2011/06/30 13:48:00 reinoudz70 Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2011/07/02 20:11:49 reinoudz70 Exp $
 
 BUILDLINK_TREE+=	xfce4-panel
 
@@ -10,7 +10,7 @@ BUILDLINK_ABI_DEPENDS.xfce4-panel?=	xfce4-panel>=4.8.5
 BUILDLINK_PKGSRCDIR.xfce4-panel?=	../../wip/xfce4-panel
 
 .include "../../devel/glib2/buildlink3.mk"
-#.include "../../x11/libxfce4gui/buildlink3.mk"
+.include "../../wip/libxfce4ui/buildlink3.mk"
 .include "../../wip/libxfce4util/buildlink3.mk"
 .endif # XFCE4_PANEL_BUILDLINK3_MK
 
