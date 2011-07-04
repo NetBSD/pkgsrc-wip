5f0daec96e571f1e0acca728a5ba90ce43b67626
Reinoud Zandijk <reinoud@NetBSD.org>
2011-07-04 14:57:34 +0000

    Add xfce4-icon-theme package skelleton for xfce 4.8

---
+++ xfce4-icon-theme/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.9 2011/07/04 14:57:34 reinoudz70 Exp $
+
+BUILDLINK_TREE+=	xfce4-icon-theme
+
+.if !defined(XFCE4_ICON_THEME_BUILDLINK3_MK)
+XFCE4_ICON_THEME_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.xfce4-icon-theme+=	xfce4-icon-theme>=4.4.3
+BUILDLINK_ABI_DEPENDS.xfce4-icon-theme?=	xfce4-icon-theme>=4.4.3nb4
+BUILDLINK_PKGSRCDIR.xfce4-icon-theme?=	../../graphics/xfce4-icon-theme
+
+.include "../../graphics/hicolor-icon-theme/buildlink3.mk"
+#.include "../../x11/libSM/buildlink3.mk"
+#.include "../../x11/gtk2/buildlink3.mk"
+#.include "../../devel/glib2/buildlink3.mk"
+.endif # XFCE4_ICON_THEME_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-xfce4-icon-theme
