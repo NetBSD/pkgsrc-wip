5083769300d27862df12419c21f63ac8ad77cb14
Guillaume Lasmayous <gls@NetBSD.org>
2011-07-05 19:14:43 +0000

    Import xfce4-thunar-media-tags-plugin-0.1.2 as wip/xfce4-thunar-media-tags.
    The Thunar Media Tags Plugin (thunar-media-tags-plugin) adds special
    features for media files to the Thunar File Manager.
    
    Currently, these are:
      * a so-called bulk renamer, which allows users to rename multiple
        audio files at once, based on their tags (e.g. ID3 or OGG/Vorbis),
      * a special media file page for the file properties dialog, which
        displays detailed information about quality, length etc.,
      * and finally, an audio tag editor which is reachable from both,
        renamer and the properties page.

---
+++ xfce4-thunar-media-tags/patches/patch-thunar-plugin_tag-renamer.c
@@ -0,0 +1,56 @@
+$NetBSD: patch-thunar-plugin_tag-renamer.c,v 1.1.1.1 2011/07/05 19:14:43 guigui2 Exp $
+
+http://svn.debian.org/viewsvn/pkg-xfce/goodies/branches/experimental/thunar-media-tags-plugin/debian/patches/
+Port to thunarx-2
+
+--- thunar-plugin/tag-renamer.c.orig	2006-06-17 19:10:05.000000000 +0000
++++ thunar-plugin/tag-renamer.c
+@@ -116,9 +116,6 @@ struct _TagRenamer
+ {
+   ThunarxRenamer __parent__;
+
+-  /* Widgets */
+-  GtkTooltips     *tooltips;
+-
+   /* Properties */
+   TagRenamerFormat format;
+   gboolean         replace_spaces;
+@@ -209,10 +206,6 @@ tag_renamer_init (TagRenamer *tag_rename
+   GEnumClass     *klass;
+   gint            n;
+
+-  /* Allocate shared tooltips */
+-  tag_renamer->tooltips = gtk_tooltips_new ();
+-  exo_gtk_object_ref_sink (GTK_OBJECT (tag_renamer->tooltips));
+-
+   table = gtk_table_new (2, 3, FALSE);
+   gtk_table_set_row_spacings (GTK_TABLE (table), 6);
+   gtk_table_set_col_spacings (GTK_TABLE (table), 12);
+@@ -245,14 +238,14 @@ tag_renamer_init (TagRenamer *tag_rename
+
+   button = gtk_check_button_new_with_mnemonic (_("_Underscores"));
+   exo_mutual_binding_new (G_OBJECT (button), "active", G_OBJECT (tag_renamer), "replace-spaces");
+-  gtk_tooltips_set_tip (tag_renamer->tooltips, button, _("Activating this option will replace all spaces in the target filename "
+-        "with underscores."), NULL);
++  gtk_widget_set_tooltip_text (GTK_WIDGET (button), _("Activating this option will replace all spaces in the target filename "
++        "with underscores."));
+   gtk_table_attach (GTK_TABLE (table), button, 2, 3, 0, 1, GTK_FILL, 0, 0, 0);
+   gtk_widget_show (button);
+
+   button = gtk_check_button_new_with_mnemonic (_("_Lowercase"));
+   exo_mutual_binding_new (G_OBJECT (button), "active", G_OBJECT (tag_renamer), "lowercase");
+-  gtk_tooltips_set_tip (tag_renamer->tooltips, button, _("If you activate this, the resulting filename will only contain lowercase letters."), NULL);
++  gtk_widget_set_tooltip_text (GTK_WIDGET (button), _("If you activate this, the resulting filename will only contain lowercase letters."));
+   gtk_table_attach (GTK_TABLE (table), button, 2, 3, 1, 2, GTK_FILL, 0, 0, 0);
+   gtk_widget_show (button);
+
+@@ -272,9 +265,6 @@ tag_renamer_finalize (GObject *object)
+ {
+   TagRenamer *tag_renamer = TAG_RENAMER (object);
+
+-  /* release the tooltips */
+-  g_object_unref (G_OBJECT (tag_renamer->tooltips));
+-
+   /* Free strings */
+   g_free (tag_renamer->artist);
+   g_free (tag_renamer->title);
