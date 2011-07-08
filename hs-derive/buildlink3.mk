d09a2ed4fc00abf690d7665c6a28efddaa6af0cf
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-07-08 09:07:45 +0000

    Upstream update to derive-2.5.2

--- hs-derive/buildlink3.mk
+++ hs-derive/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/10/01 06:51:23 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/07/08 09:07:45 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-derive
 
 .if !defined(HS_DERIVE_BUILDLINK3_MK)
 HS_DERIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-derive+=	hs-derive>=2.0.1
+BUILDLINK_API_DEPENDS.hs-derive+=	hs-derive>=2.5
 BUILDLINK_PKGSRCDIR.hs-derive?=	../../wip/hs-derive
 
 .include "../../wip/hs-haskell-src-exts/buildlink3.mk"
-.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-syb/buildlink3.mk"
+.include "../../wip/hs-transformers/buildlink3.mk"
 .include "../../wip/hs-uniplate/buildlink3.mk"
 .endif	# HS_DERIVE_BUILDLINK3_MK
 
