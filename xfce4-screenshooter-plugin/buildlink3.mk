8d738edf31ba7efed445f00e8012a9936b44d49c
Reinoud Zandijk <reinoud@NetBSD.org>
2011-07-10 16:47:05 +0000

    Add xfce4-screenshooter package template for xfce 4.8

---
+++ xfce4-screenshooter-plugin/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.3 2011/07/10 16:47:05 reinoudz70 Exp $
+
+BUILDLINK_TREE+=	xfce4-screenshooter-plugin
+
+.if !defined(XFCE4_SCREENSHOOTER_PLUGIN_BUILDLINK3_MK)
+XFCE4_SCREENSHOOTER_PLUGIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.xfce4-screenshooter-plugin+=	xfce4-screenshooter-plugin>=1.7.9
+BUILDLINK_ABI_DEPENDS.xfce4-screenshooter-plugin?=	xfce4-screenshooter-plugin>=1.7.9
+BUILDLINK_PKGSRCDIR.xfce4-screenshooter-plugin?=	../../wip/xfce4-screenshooter-plugin
+
+.include "../../wip/xfce4-panel/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.endif # XFCE4_SCREENSHOOTER_PLUGIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-xfce4-screenshooter-plugin
