4f9c35ed906e324ce169e576c59e8f21f39852d2
Aleksej Saushev <asau@users.sourceforge.net>
2011-07-11 13:10:49 +0000

    Update to openmpi-1.5.4a1r24868.

--- openmpi/patches/patch-opal_mca_backtrace_execinfo_backtrace__execinfo.c
+++ openmpi/patches/patch-opal_mca_backtrace_execinfo_backtrace__execinfo.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-opal_mca_backtrace_execinfo_backtrace__execinfo.c,v 1.1 2011/05/29 21:25:50 asau Exp $
+$NetBSD: patch-opal_mca_backtrace_execinfo_backtrace__execinfo.c,v 1.2 2011/07/11 13:10:49 asau Exp $
 
 --- opal/mca/backtrace/execinfo/backtrace_execinfo.c.orig	2011-05-25 01:00:07.000000000 +0000
 +++ opal/mca/backtrace/execinfo/backtrace_execinfo.c
@@ -6,7 +6,7 @@ $NetBSD: patch-opal_mca_backtrace_execinfo_backtrace__execinfo.c,v 1.1 2011/05/2
  #include "opal_config.h"
  
  #include <stdio.h>
-+#if !defined(__NetBSD__)
++#ifdef HAVE_EXECINFO_H
  #include <execinfo.h>
 +#endif
  
