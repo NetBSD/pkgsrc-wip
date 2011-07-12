e1281b894e6faacb56936b457ace02b850b275bd
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-07-12 09:58:49 +0000

    buildlink2.mk: missing dependency on devel/glib2

--- hs-glib/buildlink3.mk
+++ hs-glib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 09:47:40 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/07/12 09:58:49 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-glib
 
@@ -8,6 +8,7 @@ HS_GLIB_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-glib+=	hs-glib>=0.12
 BUILDLINK_PKGSRCDIR.hs-glib?=	../../wip/hs-glib
 
+.include "../../devel/glib2/buildlink3.mk"
 .endif	# HS_GLIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-glib
