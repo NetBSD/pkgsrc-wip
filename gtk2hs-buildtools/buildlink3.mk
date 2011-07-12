dafc818539c3b82f697287e83938719af8b08d80
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-07-12 08:53:30 +0000

    Wrong buildlink3.mk for gtk2hs-buildtools. Sorry

--- gtk2hs-buildtools/buildlink3.mk
+++ gtk2hs-buildtools/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 08:43:14 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/07/12 08:53:30 phonohawk Exp $
 
-BUILDLINK_TREE+=	hs-gtk2hs-buildtools
+BUILDLINK_TREE+=	gtk2hs-buildtools
 
-.if !defined(HS_GTK2HS_BUILDTOOLS_BUILDLINK3_MK)
-HS_GTK2HS_BUILDTOOLS_BUILDLINK3_MK:=
+.if !defined(GTK2HS_BUILDTOOLS_BUILDLINK3_MK)
+GTK2HS_BUILDTOOLS_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.hs-gtk2hs-buildtools?=	build
-BUILDLINK_API_DEPENDS.hs-gtk2hs-buildtools+=	hs-gtk2hs-buildtools>=0.12
-BUILDLINK_PKGSRCDIR.hs-gtk2hs-buildtools?=	../../wip/gtk2hs-buildtools
-.endif	# HS_GTK2HS_BUILDTOOLS_BUILDLINK3_MK
+BUILDLINK_DEPMETHOD.gtk2hs-buildtools?=	build
+BUILDLINK_API_DEPENDS.gtk2hs-buildtools+=	gtk2hs-buildtools>=0.12
+BUILDLINK_PKGSRCDIR.gtk2hs-buildtools?=	../../wip/gtk2hs-buildtools
+.endif	# GTK2HS_BUILDTOOLS_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-hs-gtk2hs-buildtools
+BUILDLINK_TREE+=	-gtk2hs-buildtools
