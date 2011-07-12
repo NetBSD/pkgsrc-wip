8e705b49f87d3624169071a5e5693c88e54d91ac
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-07-12 03:53:36 +0000

    Upstream update to haskell-src-1.0.1.4

--- hs-haskell-src/buildlink3.mk
+++ hs-haskell-src/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/02/17 15:02:38 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/07/12 03:53:36 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-haskell-src
 
 .if !defined(HS_HASKELL_SRC_BUILDLINK3_MK)
 HS_HASKELL_SRC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-haskell-src+=	hs-haskell-src>=1.0.1.3
+BUILDLINK_API_DEPENDS.hs-haskell-src+=	hs-haskell-src>=1.0
 BUILDLINK_PKGSRCDIR.hs-haskell-src?=	../../wip/hs-haskell-src
 
+.include "../../wip/hs-syb/buildlink3.mk"
 .endif	# HS_HASKELL_SRC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-haskell-src
