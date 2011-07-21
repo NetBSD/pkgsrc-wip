415695c0e00325393de32a3541315a2900663283
Francois Tigeot <ftigeot@wolfpond.org>
2011-07-21 05:16:36 +0000

    Update to a newer snapshot

--- libreoffice/patches/patch-clone_calc_sc_Module_sc.mk
+++ libreoffice/patches/patch-clone_calc_sc_Module_sc.mk
@@ -1,13 +1,14 @@
-$NetBSD: patch-clone_calc_sc_Module_sc.mk,v 1.1.1.1 2011/05/18 11:50:18 ftigeot Exp $
+$NetBSD: patch-clone_calc_sc_Module_sc.mk,v 1.2 2011/07/21 05:16:36 ftigeot Exp $
 
 --- clone/calc/sc/Module_sc.mk.orig	2011-05-11 12:36:55 +0000
 +++ clone/calc/sc/Module_sc.mk
-@@ -33,9 +33,4 @@ $(eval $(call gb_Module_add_targets,sc,\
+@@ -33,10 +33,4 @@ $(eval $(call gb_Module_add_targets,sc,\
  	Package_xml \
  ))
  
 -$(eval $(call gb_Module_add_check_targets,sc,\
 -	CppunitTest_sc_ucalc \
+-	CppunitTest_sc_test_filters \
 -))
 -
 -
