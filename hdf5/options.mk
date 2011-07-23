b05439b29d879172e0a2bb1509db0a2d22c4bbf6
Alexander Nasonov <alnsn@NetBSD.org>
2011-07-23 22:42:36 +0000

    Initial import of wip/hdf5 version 1.8.7.

---
+++ hdf5/options.mk
@@ -0,0 +1,19 @@
+# $NetBSD: options.mk,v 1.1.1.1 2011/07/23 22:42:36 alnsn Exp $
+
+PKG_OPTIONS_VAR=PKG_OPTIONS.hdf5
+PKG_SUPPORTED_OPTIONS=	szip threads
+# szip is non-Free, so leave out by default
+PKG_SUGGESTED_OPTIONS=
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mszip)
+CONFIGURE_ARGS+=	--with-szlib
+.include "../../archivers/szip/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mthreads)
+CONFIGURE_ARGS+=	--enable-threadsafe
+# XXX libraries should not be linked against -lpthread
+.include "../../mk/pthread.buildlink3.mk"
+.endif
