0236b362289d74bf6b49b917fd782bddfc1f3676
Ryo ONODERA <ryoon@NetBSD.org>
2011-07-25 13:50:56 +0000

    New API is added.

--- libisofs/buildlink3.mk
+++ libisofs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/06/20 20:05:13 ryo-on Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/07/25 13:50:56 ryo-on Exp $
 
 BUILDLINK_TREE+=	libisofs
 
 .if !defined(LIBISOFS_BUILDLINK3_MK)
 LIBISOFS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libisofs+=	libisofs>=1.0.6
+BUILDLINK_API_DEPENDS.libisofs+=	libisofs>=1.1.2
 BUILDLINK_PKGSRCDIR.libisofs?=	../../wip/libisofs
 
 .include "../../devel/zlib/buildlink3.mk"
