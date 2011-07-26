f7f9ae077311fd25687cfe935585dfec8907f779
Ryo ONODERA <ryoon@NetBSD.org>
2011-07-26 09:22:06 +0000

    Update to 1.2.24.
    * Try to support both X11_TYPE.

--- taskcoach/patches/patch-setup.py
+++ taskcoach/patches/patch-setup.py
@@ -1,8 +1,8 @@
-$NetBSD: patch-setup.py,v 1.2 2011/04/10 15:09:49 ryo-on Exp $
+$NetBSD: patch-setup.py,v 1.3 2011/07/26 09:22:07 ryo-on Exp $
 
 * Support NetBSD
 
---- setup.py.orig	2011-04-09 09:12:39.000000000 +0000
+--- setup.py.orig	2011-04-30 15:01:33.000000000 +0000
 +++ setup.py
 @@ -76,7 +76,7 @@ for language in languages:
      setupOptions['classifiers'].append('Natural Language :: %s'%language)
