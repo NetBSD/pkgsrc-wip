158024ec4ed52d2909fc79fefc44c813ad5ca582
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-07-27 05:44:33 +0000

    Import hs-hxt-regex-xmlschema-9.0.0 as wip/hs-hxt-regex-xmlschema.
    This library supports full W3C XML Schema regular expressions
    inclusive all Unicode character sets and blocks. The complete grammar
    can be found under <http://www.w3.org/TR/xmlschema11-2/#regexs>. It is
    implemented by the technique of derivations of regular
    expressions. The W3C syntax is extended to support not only union of
    regular sets, but also intersection, set difference, exor. Matching of
    subexpressions is also supported. The library can be used for
    constricting lightweight scanners and tokenizers. It is a standalone
    library, no external regex libraries are used. This package is a
    substitute for the old regex-xmlschema package.

---
+++ hs-hxt-regex-xmlschema/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/27 05:44:33 phonohawk Exp $
+
+BUILDLINK_TREE+=	hs-hxt-regex-xmlschema
+
+.if !defined(HS_HXT_REGEX_XMLSCHEMA_BUILDLINK3_MK)
+HS_HXT_REGEX_XMLSCHEMA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hxt-regex-xmlschema+=	hs-hxt-regex-xmlschema>=9.0
+BUILDLINK_PKGSRCDIR.hs-hxt-regex-xmlschema?=	../../wip/hs-hxt-regex-xmlschema
+
+.include "../../wip/hs-hxt-charproperties/buildlink3.mk"
+.include "../../wip/hs-parsec/buildlink3.mk"
+.endif	# HS_HXT_REGEX_XMLSCHEMA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hxt-regex-xmlschema
