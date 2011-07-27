076b47decb158c1d82d34734f683917b5a82bcae
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-07-27 05:23:07 +0000

    Import hs-hxt-charproperties-9.1.0 as wip/hs-hxt-charproperties.
    Character proprties defined by XML and Unicode standards. These
    modules contain predicates for Unicode blocks and char proprties and
    character predicates defined by XML. Supported Unicode version is
    5.2.0.

---
+++ hs-hxt-charproperties/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/27 05:23:07 phonohawk Exp $
+
+BUILDLINK_TREE+=	hs-hxt-charproperties
+
+.if !defined(HS_HXT_CHARPROPERTIES_BUILDLINK3_MK)
+HS_HXT_CHARPROPERTIES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-hxt-charproperties+=	hs-hxt-charproperties>=9.1
+BUILDLINK_PKGSRCDIR.hs-hxt-charproperties?=	../../wip/hs-hxt-charproperties
+.endif	# HS_HXT_CHARPROPERTIES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-hxt-charproperties
