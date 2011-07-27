d221bb2370ba8d99ec0660edf8bb9445cb91dbf2
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-07-27 06:25:18 +0000

    Upstream update to stm-2.2.0.1

--- hs-stm/buildlink3.mk
+++ hs-stm/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/06/04 04:27:06 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/07/27 06:25:18 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-stm
 
 .if !defined(HS_STM_BUILDLINK3_MK)
 HS_STM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-stm+=	hs-stm>=2.1.1.2
+BUILDLINK_API_DEPENDS.hs-stm+=	hs-stm>=2.2
 BUILDLINK_PKGSRCDIR.hs-stm?=	../../wip/hs-stm
 .endif	# HS_STM_BUILDLINK3_MK
 
