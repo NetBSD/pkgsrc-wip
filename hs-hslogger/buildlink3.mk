01f5b117f846d665be4ae6d75da8872ef7c139ee
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-07-28 12:55:38 +0000

    Upstream update to hslogger-1.1.4

--- hs-hslogger/buildlink3.mk
+++ hs-hslogger/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/01/24 03:36:07 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/07/28 12:55:38 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-hslogger
 
 .if !defined(HS_HSLOGGER_BUILDLINK3_MK)
 HS_HSLOGGER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hslogger+=	hs-hslogger>=1.0.7
+BUILDLINK_API_DEPENDS.hs-hslogger+=	hs-hslogger>=1.1.4
 BUILDLINK_PKGSRCDIR.hs-hslogger?=	../../wip/hs-hslogger
 
 .include "../../wip/hs-mtl/buildlink3.mk"
