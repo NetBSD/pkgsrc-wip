f73a0082ef11bac642d9e59d09c9e926e9339944
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-07-28 10:20:19 +0000

    Upstream update to HsHyperEstraier-0.4

--- hs-HsHyperEstraier/buildlink3.mk
+++ hs-HsHyperEstraier/buildlink3.mk
@@ -1,15 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.3 2010/02/04 06:58:22 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/07/28 10:20:19 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-HsHyperEstraier
 
 .if !defined(HS_HSHYPERESTRAIER_BUILDLINK3_MK)
 HS_HSHYPERESTRAIER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HsHyperEstraier+=	hs-HsHyperEstraier>=0.3.2
+BUILDLINK_API_DEPENDS.hs-HsHyperEstraier+=	hs-HsHyperEstraier>=0.4
 BUILDLINK_PKGSRCDIR.hs-HsHyperEstraier?=	../../wip/hs-HsHyperEstraier
 
 .include "../../textproc/hyperestraier/buildlink3.mk"
+.include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
 .include "../../wip/hs-network/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
 .endif	# HS_HSHYPERESTRAIER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-HsHyperEstraier
