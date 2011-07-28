4b4897394b1a7046f82d6b650fcf925badb90750
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-07-28 13:14:13 +0000

    Upstream update to hxt-xpath-9.1.1

--- hs-hxt-xpath/buildlink3.mk
+++ hs-hxt-xpath/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/02/09 12:02:07 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/07/28 13:14:13 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-hxt-xpath
 
 .if !defined(HS_HXT_XPATH_BUILDLINK3_MK)
 HS_HXT_XPATH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hxt-xpath+=	hs-hxt-xpath>=8.5.0
+BUILDLINK_API_DEPENDS.hs-hxt-xpath+=	hs-hxt-xpath>=9.1
 BUILDLINK_PKGSRCDIR.hs-hxt-xpath?=	../../wip/hs-hxt-xpath
 
 .include "../../wip/hs-hxt/buildlink3.mk"
