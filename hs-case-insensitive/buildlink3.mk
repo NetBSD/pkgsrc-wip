396634c832574ac4b22b6325ce8c18dedd63acaf
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-07-29 16:25:46 +0000

    Import hs-case-insensitive-0.3 as wip/hs-case-insensitive.
    The module Data.CaseInsensitive provides the CI type constructor which
    can be parameterised by a string-like type like: String, ByteString,
    Text, etc.. Comparisons of values of the resulting type will be
    insensitive to cases.

---
+++ hs-case-insensitive/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/29 16:25:46 phonohawk Exp $
+
+BUILDLINK_TREE+=	hs-case-insensitive
+
+.if !defined(HS_CASE_INSENSITIVE_BUILDLINK3_MK)
+HS_CASE_INSENSITIVE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=0.3
+BUILDLINK_PKGSRCDIR.hs-case-insensitive?=	../../wip/hs-case-insensitive
+
+.include "../../wip/hs-hashable/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
+.endif	# HS_CASE_INSENSITIVE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-case-insensitive
