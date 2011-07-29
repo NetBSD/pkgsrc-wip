19a227f311b6558c7d7a386181be866f71cb23f4
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-07-29 20:04:57 +0000

    Import hs-base64-bytestring-0.1.0.2 as wip/hs-base64-bytestring.
    Fast base64 encoding and deconding for ByteStrings

---
+++ hs-base64-bytestring/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/29 20:04:57 phonohawk Exp $
+
+BUILDLINK_TREE+=	hs-base64-bytestring
+
+.if !defined(HS_BASE64_BYTESTRING_BUILDLINK3_MK)
+HS_BASE64_BYTESTRING_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=0.1
+BUILDLINK_PKGSRCDIR.hs-base64-bytestring?=	../../wip/hs-base64-bytestring
+.endif	# HS_BASE64_BYTESTRING_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-base64-bytestring
