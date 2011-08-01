07129f8e41f9a5ba16463a5dfe1762a90104e435
Pierre Pronchery <khorben@defora.org>
2011-08-01 12:36:20 +0000

    Packaged DeforaOS libDesktop 0.0.5

--- deforaos-libdesktop/buildlink3.mk
+++ deforaos-libdesktop/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/02/24 13:21:10 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/08/01 12:36:20 khorben Exp $
 
 BUILDLINK_TREE+=	deforaos-libdesktop
 
 .if !defined(DEFORAOS_LIBDESKTOP_BUILDLINK3_MK)
 DEFORAOS_LIBDESKTOP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.0.1
+BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.0.5
 BUILDLINK_PKGSRCDIR.deforaos-libdesktop?=	../../wip/deforaos-libdesktop
 
+.include "../../wip/deforaos-libsystem/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .endif	# DEFORAOS_LIBDESKTOP_BUILDLINK3_MK
 
