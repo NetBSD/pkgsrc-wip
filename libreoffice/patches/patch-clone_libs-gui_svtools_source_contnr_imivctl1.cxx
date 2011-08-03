de5e71582e83c9ed54b5b33417106e05d65950a6
Thomas Klausner <wiz@NetBSD.org>
2011-08-03 09:58:46 +0000

    Add comment: Note: upstream fixed this by changing the prototype in imivctl.hxx instead -- beware when updating libreoffice to a newer snapshot.

--- libreoffice/patches/patch-clone_libs-gui_svtools_source_contnr_imivctl1.cxx
+++ libreoffice/patches/patch-clone_libs-gui_svtools_source_contnr_imivctl1.cxx
@@ -1,4 +1,7 @@
-$NetBSD: patch-clone_libs-gui_svtools_source_contnr_imivctl1.cxx,v 1.1 2011/07/27 16:46:25 asau Exp $
+$NetBSD: patch-clone_libs-gui_svtools_source_contnr_imivctl1.cxx,v 1.2 2011/08/03 09:58:46 thomasklausner Exp $
+
+Note: upstream fixed this by changing the prototype in imivctl.hxx instead --
+beware when updating libreoffice to a newer snapshot.
 
 --- clone/libs-gui/svtools/source/contnr/imivctl1.cxx.orig	2011-07-15 14:53:56.000000000 +0000
 +++ clone/libs-gui/svtools/source/contnr/imivctl1.cxx
