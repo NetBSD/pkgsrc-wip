bbba1ea2986467ac2f7b403e943ddd7d080e9c66
Hauke Fath <hauke@NetBSD.org>
2011-08-04 11:49:17 +0000

    Import netatalk20-2.0.5 as wip/netatalk20.
    Netatalk is a freely-available, kernel level implementation of the AppleTalk
    Protocol Suite, originally for BSD-derived systems. A *NIX/*BSD system
    running netatalk is capable of serving many macintosh clients simultaneously
    as an AppleTalk router, AppleShare file server (AFP), *NIX/*BSD print server,
    and for accessing AppleTalk printers via Printer Access Protocol (PAP).
    Included are a number of minor printing and debugging utilities.
    
    Since AppleTalk support in newer Netatalk is increasingly suffering
    from bit rot, import an up-to-date (wrt. pkgsrc) Netatalk 2.0 package.

---
+++ netatalk20/options.mk
@@ -0,0 +1,50 @@
+# $NetBSD: options.mk,v 1.1.1.1 2011/08/04 11:49:17 hfath Exp $
+#
+PKG_OPTIONS_VAR=	PKG_OPTIONS.netatalk
+PKG_SUPPORTED_OPTIONS=	cups debug kerberos pam slp
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mcups)
+.include "../../print/cups/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-cups
+.else
+CONFIGURE_ARGS+=	--disable-cups
+.endif
+
+PLIST_VARS+=		debug
+.if !empty(PKG_OPTIONS:Mdebug)
+# Debugging
+CONFIGURE_ARGS+=	--enable-debug1
+CONFIGURE_ARGS+=	--enable-debug
+CONFIGURE_ARGS+=	--enable-debugging
+CFLAGS+=		-g
+INSTALL_UNSTRIPPED=	yes
+.endif
+
+PLIST_VARS+=		gssapi
+.if !empty(PKG_OPTIONS:Mkerberos)
+.include "../../mk/krb5.buildlink3.mk"
+CONFIGURE_ARGS+=	--with-gssapi --enable-krbV-uam
+CONFIGURE_ENV+=		GSSAPI_LIBS="-lkrb5 -lroken -lasn1 -lcrypto -lcom_err"
+PLIST.gssapi=		yes
+.else
+CONFIGURE_ARGS+=	--without-gssapi
+.endif
+
+PLIST_VARS+=		pam
+.if !empty(PKG_OPTIONS:Mpam)
+.include "../../mk/pam.buildlink3.mk"
+CONFIGURE_ARGS+=	--with-pam
+PLIST.pam=		yes
+MESSAGE_SRC+=		MESSAGE MESSAGE.pam
+.else
+CONFIGURE_ARGS+=	--without-pam
+.endif
+
+.if !empty(PKG_OPTIONS:Mslp)
+.include "../../net/openslp/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-srvloc
+.else
+CONFIGURE_ARGS+=	--disable-srvloc
+.endif
