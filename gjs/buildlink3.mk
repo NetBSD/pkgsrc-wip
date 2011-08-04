22ceaba1437cb682ce9871fdbc5c9e93627b9152
Ryo ONODERA <ryoon@NetBSD.org>
2011-08-04 15:41:03 +0000

    Import gjs-0.7.14 as wip/gjs.
    Gjs is a Javascript binding for GNOME. It's mainly based on
    Spidermonkey javascript engine and the GObject introspection
    framework.

---
+++ gjs/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/08/04 15:41:03 ryo-on Exp $
+
+BUILDLINK_TREE+=	gjs
+
+.if !defined(GJS_BUILDLINK3_MK)
+GJS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gjs+=	gjs>=0.7.14
+BUILDLINK_PKGSRCDIR.gjs?=	../../wip/gjs
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/gobject-introspection/buildlink3.mk"
+.include "../../wip/spidermonkey185/buildlink3.mk"
+#.include "../../graphics/cairo-gobject/buildlink3.mk"
+.include "../../sysutils/dbus-glib/buildlink3.mk"
+.endif	# GJS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gjs
