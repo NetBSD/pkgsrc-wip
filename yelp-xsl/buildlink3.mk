5c38a44bff94836a0bc6a18096bb37a9d4f02626
OBATA Akio <obache@users.sourceforge.net>
2011-08-05 14:13:57 +0000

    No need to buildlink with libxml2 and libxslt.

--- yelp-xsl/buildlink3.mk
+++ yelp-xsl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2011/07/31 21:47:45 jihbed Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/08/05 14:13:57 obache Exp $
 
 BUILDLINK_TREE+=	yelp-xsl
 
@@ -8,8 +8,6 @@ YELP_XSL_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.yelp-xsl+=	yelp-xsl>=2.31.1
 BUILDLINK_PKGSRCDIR.yelp-xsl?=	../../wip/yelp-xsl
 
-.include "../../textproc/libxml2/buildlink3.mk"
-.include "../../textproc/libxslt/buildlink3.mk"
 .endif	# YELP_XSL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-yelp-xsl
