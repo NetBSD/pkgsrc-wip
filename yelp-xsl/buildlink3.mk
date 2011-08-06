9e56fcb49d57eada0da23d05dd95d7081da32f9b
OBATA Akio <obache@users.sourceforge.net>
2011-08-06 06:57:24 +0000

    Update yelp-xsl to 3.0.2.

--- yelp-xsl/buildlink3.mk
+++ yelp-xsl/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/08/05 14:13:57 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/08/06 06:57:24 obache Exp $
 
 BUILDLINK_TREE+=	yelp-xsl
 
 .if !defined(YELP_XSL_BUILDLINK3_MK)
 YELP_XSL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.yelp-xsl+=	yelp-xsl>=2.31.1
+BUILDLINK_API_DEPENDS.yelp-xsl+=	yelp-xsl>=3.0.2
 BUILDLINK_PKGSRCDIR.yelp-xsl?=	../../wip/yelp-xsl
 
 .endif	# YELP_XSL_BUILDLINK3_MK
