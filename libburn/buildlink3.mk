1363d93a3165f134933d1feca35ad97e415fc5ce
Ryo ONODERA <ryoon@NetBSD.org>
2011-08-09 13:46:56 +0000

    Update BUILDLINK_API_DEPENDS

--- libburn/buildlink3.mk
+++ libburn/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/06/20 20:02:52 ryo-on Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/08/09 13:46:56 ryo-on Exp $
 
 BUILDLINK_TREE+=	libburn
 
 .if !defined(LIBBURN_BUILDLINK3_MK)
 LIBBURN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libburn+=	libburn>=1.1.0
+BUILDLINK_API_DEPENDS.libburn+=	libburn>=1.1.4
 BUILDLINK_PKGSRCDIR.libburn?=	../../wip/libburn
 .endif	# LIBBURN_BUILDLINK3_MK
 
