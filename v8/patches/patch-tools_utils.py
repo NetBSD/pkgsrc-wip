fdfd38204bd7b0c79be1f6dc59a7809a3f0065a4
Matthew Sporleder <mspo@NetBSD.org>
2011-09-10 19:57:09 +0000

    updated patches to head builds.  Also submitted upstream

--- v8/patches/patch-tools_utils.py
+++ v8/patches/patch-tools_utils.py
@@ -1,4 +1,4 @@
-$NetBSD: patch-ab,v 1.1.1.1 2011/05/02 20:16:09 genolopolis Exp $
+$NetBSD: patch-tools_utils.py,v 1.1 2011/09/10 19:57:09 genolopolis Exp $
 
 --- tools/utils.py.orig	2011-05-02 18:26:32.000000000 +0000
 +++ tools/utils.py
