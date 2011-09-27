fe82c36462ac17100157c5cdeaef3b05a3090372
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-09-27 14:24:34 +0000

    Upstream update to vector-0.9

--- hs-vector/buildlink3.mk
+++ hs-vector/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/01/08 04:59:25 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/09/27 14:24:34 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-vector
 
 .if !defined(HS_VECTOR_BUILDLINK3_MK)
 HS_VECTOR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.7
+BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.9
 BUILDLINK_PKGSRCDIR.hs-vector?=	../../wip/hs-vector
 
 .include "../../wip/hs-primitive/buildlink3.mk"
