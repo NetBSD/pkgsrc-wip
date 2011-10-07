45ab896362068c2f71bcc0a2aca75b1767b2ea0e
Ryo ONODERA <ryoon@NetBSD.org>
2011-10-07 08:59:38 +0000

    Import adobe-flash-plugin-11.0.1.152 as wip/adobe-flash-plugin11.
    Netscape plugin for Adobe Flash player.  This package contains
    a plugin that enables web browsers to render the Flash format.
    
    The nspluginwrapper option (enabled by default) permits the use of
    the the Linux x86 flash player in Firefox browsers on other x86
    systems.
    
    This package requires emul.linux.kern.osrelease = 2.6.18 or later.
    This means this package won't work on NetBSD 5.x or older systems!

---
+++ adobe-flash-plugin11/options.mk
@@ -0,0 +1,25 @@
+# $NetBSD: options.mk,v 1.1.1.1 2011/10/07 08:59:38 ryo-on Exp $
+#
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.adobe-flash-plugin
+PKG_SUPPORTED_OPTIONS=	nspluginwrapper
+
+# XXXX: If nspluginwrapper is enabled and this package is installed after
+# firefox (or similar) it will install a system wide wrapped plugin into
+# @PREFIX/lib/<browser>/plugins. If it is installed before a browser
+# it will install a plugin for the current user in ~/.mozilla/plugins/
+# or similar. This is inconsistant and potentially confusing.
+
+.include "../../mk/bsd.prefs.mk"
+
+.if ${MACHINE_ARCH} == "i386" && ${OPSYS} != "Linux"
+PKG_SUGGESTED_OPTIONS=  nspluginwrapper
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mnspluginwrapper)
+DEPENDS+= nspluginwrapper>0:../../www/nspluginwrapper
+INSTALL_TEMPLATES+=	${PKGDIR}/INSTALL.nspluginwrapper
+DEINSTALL_TEMPLATES+=	${PKGDIR}/INSTALL.nspluginwrapper
+.endif
