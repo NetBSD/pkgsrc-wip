68429308db3dcd9143fcdf00b11a92cfa4fe9d96
Ryo ONODERA <ryoon@NetBSD.org>
2011-10-11 12:37:48 +0000

    Update to 2.3.1
    Disable install of documentation.
    
    Changelog:
    17.09.2011 - 2.3.1
    Fixed minor issue in mode table builder.
    
    16.09.2011 - 2.3.1
    Fixed minor issue in interpreter.
    
    11.09.2011 - 2.3.0
    Fixed minor issue in code generator.
    
    09.09.2011 - 2.3.0
    Fixed medium issue in mode checker.
    
    06.09.2011 - 2.3.0
    Fixed minor issue in the scanner.
    
    02.09.2011 - 2.3.0
    24.08.2011 - 2.3.0
    Fixed minor issues in mode checker.
    
    18.07.2011 - 2.3.0
    Alpha: non-local environs.
    
    14.07.2011 - 2.3.0
    Alpha: ghost elements for empty rows.
    
    09.07.2011 - 2.3.0
    New self-consistency criterion for the mode table builder.

--- algol68g/options.mk
+++ algol68g/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2011/07/26 22:50:55 rhialto Exp $
+# $NetBSD: options.mk,v 1.2 2011/10/11 12:37:48 ryo-on Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.a68g
 PKG_SUPPORTED_OPTIONS=	plotutils pgsql
@@ -8,15 +8,16 @@ PKG_SUGGESTED_OPTIONS=	plotutils pgsql
 
 .if !empty(PKG_OPTIONS:Mplotutils)
 .  include "../../graphics/plotutils/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-plotutils
 .else
-CONFIGURE_ARGS+=       --without-plotutils
+CONFIGURE_ARGS+=	--without-plotutils
 .endif
 
 # It seems that when including PostgreSQL support it doesn't actually
 # need to have it installed at build time, just at run time.
 # So maybe this DEPENDS is undesirable.
 .if !empty(PKG_OPTIONS:Mpgsql)
-#DEPENDS+=	postgresql-[0-9]*:../../databases/postgresql90
+CONFIGURE_ARGS+=	--with-pgsql
 .else
-#CONFIGURE_ARGS+=       --without-pgsql
+CONFIGURE_ARGS+=	--without-pgsql
 .endif
