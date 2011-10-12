c3ea48d588fea96e4dbb228247159369ee9e0cdd
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-10-12 19:02:55 +0000

    Upstream update to SHA-1.5.0.0

--- hs-SHA/buildlink3.mk
+++ hs-SHA/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/09/24 02:05:29 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/10/12 19:02:55 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-SHA
 
 .if !defined(HS_SHA_BUILDLINK3_MK)
 HS_SHA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-SHA+=	hs-SHA>=1.4.0
+BUILDLINK_API_DEPENDS.hs-SHA+=	hs-SHA>=1.5.0
 BUILDLINK_PKGSRCDIR.hs-SHA?=	../../wip/hs-SHA
 
 .include "../../wip/hs-binary/buildlink3.mk"
