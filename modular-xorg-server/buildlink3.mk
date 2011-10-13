60888c09bc364b95b42cf21eb5a0d71d6dea27a9
Makoto Fujiwara <makoto@ki.nu>
2011-10-13 15:30:18 +0000

    Add missing files by importing once-existed-pkg

---
+++ modular-xorg-server/buildlink3.mk
@@ -0,0 +1,27 @@
+# $NetBSD: buildlink3.mk,v 1.7 2011/10/13 15:30:18 makoto Exp $
+
+BUILDLINK_TREE+=	modular-xorg-server
+
+.if !defined(MODULAR_XORG_SERVER_BUILDLINK3_MK)
+MODULAR_XORG_SERVER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.modular-xorg-server+=	modular-xorg-server>=1.6.0
+BUILDLINK_ABI_DEPENDS.modular-xorg-server+=	modular-xorg-server>=1.6.5nb8
+BUILDLINK_PKGSRCDIR.modular-xorg-server?=	../../x11/modular-xorg-server
+
+.include "../../sysutils/libpciaccess/buildlink3.mk"
+.include "../../x11/pixman/buildlink3.mk"
+
+.include "../../x11/compositeproto/buildlink3.mk"
+.include "../../x11/dri2proto/buildlink3.mk"
+.include "../../x11/fontsproto/buildlink3.mk"
+.include "../../x11/inputproto/buildlink3.mk"
+.include "../../x11/randrproto/buildlink3.mk"
+.include "../../x11/renderproto/buildlink3.mk"
+.include "../../x11/videoproto/buildlink3.mk"
+.include "../../x11/xextproto/buildlink3.mk"
+.include "../../x11/xf86driproto/buildlink3.mk"
+.include "../../x11/xproto/buildlink3.mk"
+.endif # MODULAR_XORG_SERVER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-modular-xorg-server
