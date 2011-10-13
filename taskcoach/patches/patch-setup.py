164b79c68039dcf6175781d88ccd7a0911c5c74b
Ryo ONODERA <ryoon@NetBSD.org>
2011-10-13 11:31:48 +0000

    Update to 1.2.29.
    bug fix
    Added new features.
    * storing password is encrypted.
    * SyncML support is improved.
    * and so on.

--- taskcoach/patches/patch-setup.py
+++ taskcoach/patches/patch-setup.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-setup.py,v 1.3 2011/07/26 09:22:07 ryo-on Exp $
+$NetBSD: patch-setup.py,v 1.4 2011/10/13 11:31:48 ryo-on Exp $
 
 * Support NetBSD
 
---- setup.py.orig	2011-04-30 15:01:33.000000000 +0000
+--- setup.py.orig	2011-09-18 19:48:34.000000000 +0000
 +++ setup.py
-@@ -76,7 +76,7 @@ for language in languages:
+@@ -64,7 +64,7 @@ for language in languages:
      setupOptions['classifiers'].append('Natural Language :: %s'%language)
  
  system = platform.system()
 -if system == 'Linux':
-+if (system == 'Linux') or (system == 'NetBSD'):
-     setupOptions['package_data'] = {'taskcoachlib': ['bin.in/linux/_pysyncml.so']}
++if system == 'Linux' or (system == 'NetBSD') or (system == 'DragonFly'):
      # Add data files for Debian-based systems:
      current_dist = [dist.lower() for dist in platform.dist()]
+     if 'debian' in current_dist or 'ubuntu' in current_dist:
