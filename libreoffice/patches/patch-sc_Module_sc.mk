46dd26c614f699d8bbd7cc4f1c1ed274e0a412f8
Francois Tigeot <ftigeot@wolfpond.org>
2011-10-24 14:17:45 +0000

    Update to snapshot 2011-10-17

--- libreoffice/patches/patch-sc_Module_sc.mk
+++ libreoffice/patches/patch-sc_Module_sc.mk
@@ -1,14 +1,16 @@
-$NetBSD: patch-sc_Module_sc.mk,v 1.1 2011/09/09 08:21:12 ftigeot Exp $
+$NetBSD: patch-sc_Module_sc.mk,v 1.2 2011/10/24 14:17:46 ftigeot Exp $
 
---- sc/Module_sc.mk.orig	2011-08-30 15:00:33.000000000 +0000
+--- sc/Module_sc.mk.orig	2011-10-17 16:49:48.000000000 +0000
 +++ sc/Module_sc.mk
-@@ -34,10 +34,4 @@ $(eval $(call gb_Module_add_targets,sc,\
+@@ -34,12 +34,4 @@ $(eval $(call gb_Module_add_targets,sc,\
  	Package_xml \
  ))
  
 -$(eval $(call gb_Module_add_check_targets,sc,\
+-	CppunitTest_sc_filters_test \
 -	CppunitTest_sc_ucalc \
--	CppunitTest_sc_test_filters \
+-	RdbTarget_sc_filters_test \
+-	RdbTarget_sc_ucalc \
 -))
 -
 -
--- libreoffice/patches/patch-sd_Module_sd.mk
+++ libreoffice/patches/patch-sd_Module_sd.mk
@@ -1,15 +1,13 @@
-$NetBSD: patch-sd_Module_sd.mk,v 1.1 2011/08/30 20:41:34 ftigeot Exp $
+$NetBSD: patch-sd_Module_sd.mk,v 1.2 2011/10/24 14:17:46 ftigeot Exp $
 
---- sd/Module_sd.mk.orig	2011-08-23 13:05:22.000000000 +0000
+--- sd/Module_sd.mk.orig	2011-10-17 16:49:48.000000000 +0000
 +++ sd/Module_sd.mk
-@@ -40,10 +40,6 @@ $(eval $(call gb_Module_add_targets,sd,\
+@@ -40,7 +40,7 @@ $(eval $(call gb_Module_add_targets,sd,\
      Package_xml \
  ))
  
--$(eval $(call gb_Module_add_check_targets,sd,\
--    CppunitTest_sd_uimpress \
--))
--
- $(eval $(call gb_Module_add_subsequentcheck_targets,sd,\
-     JunitTest_sd_unoapi \
- ))
+-ifneq ($(OS),DRAGONFLY)
++ifeq (,$(filter DRAGONFLY NETBSD,$(OS)))
+ $(eval $(call gb_Module_add_check_targets,sd,\
+     CppunitTest_sd_uimpress \
+     RdbTarget_sd_uimpress \
