7238d8116596074f2f0a76b9b3cb12bb22a46f9f
Blue Rats <rodent@NetBSD.org>
2011-10-29 01:41:26 +0000

    Make sure ccrtp >=2.0.0

--- ccrtp/buildlink3.mk
+++ ccrtp/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:43:40 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2011/10/29 01:41:26 othyro Exp $
 
 BUILDLINK_TREE+=	ccrtp
 
 .if !defined(CCRTP_BUILDLINK3_MK)
 CCRTP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ccrtp+=	ccrtp>=1.5.0
+BUILDLINK_API_DEPENDS.ccrtp+=	ccrtp>=2.0.0
 BUILDLINK_PKGSRCDIR.ccrtp?=	../../wip/ccrtp
 
 .include "../../wip/commoncpp2/buildlink3.mk"
