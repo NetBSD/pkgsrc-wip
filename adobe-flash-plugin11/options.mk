872bbcf986c309edaff254ca80f915f64f405f6f
Ryo ONODERA <ryoon@NetBSD.org>
2011-10-29 21:03:11 +0000

    * Make pkglint happy. * Add commented-out DEPENDS=libflashsupport line.

--- adobe-flash-plugin11/options.mk
+++ adobe-flash-plugin11/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2011/10/09 12:13:23 ryo-on Exp $
+# $NetBSD: options.mk,v 1.4 2011/10/29 21:03:11 ryo-on Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.adobe-flash-plugin
@@ -14,14 +14,15 @@ PKG_SUPPORTED_OPTIONS=	nspluginwrapper
 
 .if ${OPSYS} != "Linux"
 .  if ${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64"
-PKG_SUGGESTED_OPTIONS=  nspluginwrapper
+PKG_SUGGESTED_OPTIONS=	nspluginwrapper
 .  endif
 .endif
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mnspluginwrapper)
-DEPENDS+= nspluginwrapper>0:../../www/nspluginwrapper
+DEPENDS+=	nspluginwrapper>0:../../www/nspluginwrapper
+#DEPENDS+=	libflashsupport{,-pulse}>0:../../multimedia/libflashsupport
 INSTALL_TEMPLATES+=	${PKGDIR}/INSTALL.nspluginwrapper
 DEINSTALL_TEMPLATES+=	${PKGDIR}/INSTALL.nspluginwrapper
 .endif
