5303a340c7ec23b86deb898cafee23a73c8cac30
Blue Rats <rodent@NetBSD.org>
2011-10-29 11:18:56 +0000

    Needs to pull in ucommon too.

--- ccrtp/buildlink3.mk
+++ ccrtp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2011/10/29 01:41:26 othyro Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2011/10/29 11:18:56 othyro Exp $
 
 BUILDLINK_TREE+=	ccrtp
 
@@ -8,6 +8,7 @@ CCRTP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.ccrtp+=	ccrtp>=2.0.0
 BUILDLINK_PKGSRCDIR.ccrtp?=	../../wip/ccrtp
 
+.include "../../wip/ucommon/buildlink3.mk"
 .include "../../wip/commoncpp2/buildlink3.mk"
 .endif # CCRTP_BUILDLINK3_MK
 
