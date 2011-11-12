956568bfffb8a99334d442e46f90f853047a51e2
Aleksej Saushev <asau@users.sourceforge.net>
2011-11-12 14:42:37 +0000

    Bring WINE 1.3.26 from pkgsrc.

--- wine/buildlink3.mk
+++ wine/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2010/12/26 05:01:24 davesainty Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2011/11/12 14:42:37 asau Exp $
 
 BUILDLINK_TREE+=	wine
 
 .if !defined(WINE_BUILDLINK3_MK)
 WINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.wine+=	wine>=0.9.6nb1
-BUILDLINK_ABI_DEPENDS.wine?=	wine>=1.1.16nb2
+BUILDLINK_API_DEPENDS.wine+=	wine>=1.3.0
+BUILDLINK_ABI_DEPENDS.wine?=	wine>=1.3.26nb1
 BUILDLINK_PKGSRCDIR.wine?=	../../emulators/wine
 
 pkgbase:= wine
@@ -17,8 +17,8 @@ pkgbase:= wine
 .include "../../graphics/glu/buildlink3.mk"
 .include "../../graphics/glut/buildlink3.mk"
 .endif
+.include "../../mk/giflib.buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
-.include "../../graphics/libungif/buildlink3.mk"
 .endif # WINE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-wine
