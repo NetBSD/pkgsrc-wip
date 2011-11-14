74fbcae5f1fcc46704642c9586a0b2117ad52f41
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-11-14 09:51:34 +0000

    Upstream update to case-insensitive-0.4

--- hs-case-insensitive/buildlink3.mk
+++ hs-case-insensitive/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/29 16:25:46 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/11/14 09:51:34 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-case-insensitive
 
 .if !defined(HS_CASE_INSENSITIVE_BUILDLINK3_MK)
 HS_CASE_INSENSITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=0.3
+BUILDLINK_API_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=0.4
 BUILDLINK_PKGSRCDIR.hs-case-insensitive?=	../../wip/hs-case-insensitive
 
 .include "../../wip/hs-hashable/buildlink3.mk"
