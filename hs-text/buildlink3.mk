f7949d877205bcaaa3542faa33aa4d5536255427
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-11-14 09:39:47 +0000

    Upstream update to text-0.11.1.9

--- hs-text/buildlink3.mk
+++ hs-text/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/01/12 02:40:24 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/11/14 09:39:47 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-text
 
 .if !defined(HS_TEXT_BUILDLINK3_MK)
 HS_TEXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=0.11
+BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=0.11.1
 BUILDLINK_PKGSRCDIR.hs-text?=	../../wip/hs-text
 
 .include "../../wip/hs-deepseq/buildlink3.mk"
