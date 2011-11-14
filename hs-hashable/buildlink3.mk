e5f02fd02f9603a7dbad49b34bb6a40dffc06650
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-11-14 09:30:24 +0000

    Upstream update to hashable-1.1.2.2

--- hs-hashable/buildlink3.mk
+++ hs-hashable/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/29 16:19:15 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/11/14 09:30:24 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-hashable
 
 .if !defined(HS_HASHABLE_BUILDLINK3_MK)
 HS_HASHABLE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.1
+BUILDLINK_API_DEPENDS.hs-hashable+=	hs-hashable>=1.1.2
 BUILDLINK_PKGSRCDIR.hs-hashable?=	../../wip/hs-hashable
 
 .include "../../wip/hs-text/buildlink3.mk"
