410b71b477a04dc14793ec990d7f3dd40d9b694c
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-11-15 22:41:47 +0000

    Upstream update to ascii-0.0.4

--- hs-ascii/buildlink3.mk
+++ hs-ascii/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/11/14 10:05:00 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2011/11/15 22:41:47 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-ascii
 
 .if !defined(HS_ASCII_BUILDLINK3_MK)
 HS_ASCII_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ascii+=	hs-ascii>=0.0.3
+BUILDLINK_API_DEPENDS.hs-ascii+=	hs-ascii>=0.0.4
 BUILDLINK_PKGSRCDIR.hs-ascii?=	../../wip/hs-ascii
 
 .include "../../wip/hs-blaze-builder/buildlink3.mk"
 .include "../../wip/hs-case-insensitive/buildlink3.mk"
+.include "../../wip/hs-hashable/buildlink3.mk"
 .include "../../wip/hs-text/buildlink3.mk"
 .endif	# HS_ASCII_BUILDLINK3_MK
 
