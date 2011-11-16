2b7d97f4747b625947e22ba6f40d9face62ae276
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-11-16 17:37:51 +0000

    Upstream update to uniplate-1.6.5

--- hs-uniplate/buildlink3.mk
+++ hs-uniplate/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/02/17 14:58:38 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/11/16 17:37:51 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-uniplate
 
 .if !defined(HS_UNIPLATE_BUILDLINK3_MK)
 HS_UNIPLATE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-uniplate+=	hs-uniplate>=1.6
+BUILDLINK_API_DEPENDS.hs-uniplate+=	hs-uniplate>=1.6.5
 BUILDLINK_PKGSRCDIR.hs-uniplate?=	../../wip/hs-uniplate
 
 .include "../../wip/hs-syb/buildlink3.mk"
