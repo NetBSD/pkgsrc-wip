f060d9c43d1e7979158bbbe4dcddfdaabf96f6f7
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-11-16 16:49:52 +0000

    Upstream update to cpphs-1.13.2

--- hs-cpphs/buildlink3.mk
+++ hs-cpphs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2011/10/03 12:16:20 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2011/11/16 16:49:53 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-cpphs
 
 .if !defined(HS_CPPHS_BUILDLINK3_MK)
 HS_CPPHS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.13
+BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.13.2
 BUILDLINK_PKGSRCDIR.hs-cpphs?=	../../wip/hs-cpphs
 .endif	# HS_CPPHS_BUILDLINK3_MK
 
