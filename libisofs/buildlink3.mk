3dfc75bda3a68d70a0cca224c129d4ab827fa5bf
Ryo ONODERA <ryoon@NetBSD.org>
2011-11-22 17:13:03 +0000

    Bump BUILDLINK_API_DEPENDS to  libisofs>=1.1.6

--- libisofs/buildlink3.mk
+++ libisofs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/07/25 13:50:56 ryo-on Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2011/11/22 17:13:03 ryo-on Exp $
 
 BUILDLINK_TREE+=	libisofs
 
 .if !defined(LIBISOFS_BUILDLINK3_MK)
 LIBISOFS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libisofs+=	libisofs>=1.1.2
+BUILDLINK_API_DEPENDS.libisofs+=	libisofs>=1.1.6
 BUILDLINK_PKGSRCDIR.libisofs?=	../../wip/libisofs
 
 .include "../../devel/zlib/buildlink3.mk"
