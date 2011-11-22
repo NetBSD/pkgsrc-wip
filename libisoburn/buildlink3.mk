b3bc633b2893447602de33a7e70cc30e60f65ee9
Ryo ONODERA <ryoon@NetBSD.org>
2011-11-22 17:11:04 +0000

    Update to 1.1.8
    Changelog:
    libisoburn novelties:
    
    - none
    
    xorriso novelties:
    
    * Info document and man page for xorrecord
    
    * New option -sleep
    
    * Enabled recognition of QEMU DVD-ROM 0.12
    
    * Enabled out-of the box use of xorriso on Linux guest on qemu virtio-blk-pci

--- libisoburn/buildlink3.mk
+++ libisoburn/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/03/12 10:50:32 ryo-on Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/11/22 17:11:04 ryo-on Exp $
 
 BUILDLINK_TREE+=	libisoburn
 
 .if !defined(LIBISOBURN_BUILDLINK3_MK)
 LIBISOBURN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libisoburn+=	libisoburn>=1.0.4.00
+BUILDLINK_API_DEPENDS.libisoburn+=	libisoburn>=1.1.8
 BUILDLINK_PKGSRCDIR.libisoburn?=	../../wip/libisoburn
 
 .include "../../wip/libisofs/buildlink3.mk"
