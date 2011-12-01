6420d8d70a1ab5d931c7d643891e81bb8ad51e97
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-12-01 22:50:49 +0000

    Upstream update to data-default-0.3.0

--- hs-data-default/buildlink3.mk
+++ hs-data-default/buildlink3.mk
@@ -1,12 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/11/02 16:38:18 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/12/01 22:50:49 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-data-default
 
 .if !defined(HS_DATA_DEFAULT_BUILDLINK3_MK)
 HS_DATA_DEFAULT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-data-default+=	hs-data-default>=0.2
+BUILDLINK_API_DEPENDS.hs-data-default+=	hs-data-default>=0.3.0
 BUILDLINK_PKGSRCDIR.hs-data-default?=	../../wip/hs-data-default
+
+.include "../../wip/hs-dlist/buildlink3.mk"
 .endif	# HS_DATA_DEFAULT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-data-default
