4ec880a81061984eb7f1b6d02cb2318fc51e31ec
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-12-01 23:03:39 +0000

    Upstream update to semigroups-0.8

--- hs-semigroups/buildlink3.mk
+++ hs-semigroups/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/05/10 23:24:47 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/12/01 23:03:39 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-semigroups
 
 .if !defined(HS_SEMIGROUPS_BUILDLINK3_MK)
 HS_SEMIGROUPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.5
+BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.8
 BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../wip/hs-semigroups
 .endif	# HS_SEMIGROUPS_BUILDLINK3_MK
 
