db39cc6c563213e262e9787a6cc781fd47c61716
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-12-05 04:57:58 +0000

    Upstream update to convertible-text-0.4.0

--- hs-convertible-text/buildlink3.mk
+++ hs-convertible-text/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/12/02 06:26:41 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/12/05 04:57:58 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-convertible-text
 
 .if !defined(HS_CONVERTIBLE_TEXT_BUILDLINK3_MK)
 HS_CONVERTIBLE_TEXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-convertible-text+=	hs-convertible-text>=0.3.0
+BUILDLINK_API_DEPENDS.hs-convertible-text+=	hs-convertible-text>=0.4.0
 BUILDLINK_PKGSRCDIR.hs-convertible-text?=	../../wip/hs-convertible-text
 
 .include "../../wip/hs-attempt/buildlink3.mk"
