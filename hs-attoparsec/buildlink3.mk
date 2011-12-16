9175883bd8e22f6ba7ea112d0107b1c318c2a150
PHO / phonohawk <phonohawk@users.sourceforge.net>
2011-12-16 13:06:15 +0000

    Upstream update to attoparsec-0.10.1.0

--- hs-attoparsec/buildlink3.mk
+++ hs-attoparsec/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/30 05:04:54 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/12/16 13:06:15 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-attoparsec
 
 .if !defined(HS_ATTOPARSEC_BUILDLINK3_MK)
 HS_ATTOPARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.9
+BUILDLINK_API_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.10.1
 BUILDLINK_PKGSRCDIR.hs-attoparsec?=	../../wip/hs-attoparsec
 
 .include "../../wip/hs-deepseq/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
 .endif	# HS_ATTOPARSEC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-attoparsec
