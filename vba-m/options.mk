141155d077abc14b079141de9ab1d0290ed0219d
Blue Rats <rodent@NetBSD.org>
2011-12-28 06:56:35 +0000

    Import vba-m-1.8.0.1054 as wip/vba-m.
    VBA-M is a fork from the now inactive VisualBoy Advance project, with goals to
    improve the compatibility and features of the emulator with:
    
    * Improved Save Handling
    * Improved Archive Support
    * Improved Input Capabilities
    * New built-in Pixel Filters
    * KEGA Fusion Filter Plugin Support
    * Multithreaded Direct3D
    * Fixed Vsync for Direct3D
    * ARM Thumb Optimised
    * Blargg's GB_APU
    * OpenAL and XAudio support
    * UPS Patch support
    * JoyBus support

---
+++ vba-m/options.mk
@@ -0,0 +1,52 @@
+# $NetBSD: options.mk,v 1.1.1.1 2011/12/28 06:56:35 othyro Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.vba-m
+PKG_SUPPORTED_OPTIONS=	debug ffmpeg gtk lirc nls sdl
+PKG_SUGGESTED_OPTIONS=	debug ffmpeg gtk nls sdl
+PLIST_VARS+=		gtk nls
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CMAKE_ARGS+=    -DENABLE_DEBUGGER=ON
+.else
+CMAKE_ARGS+=	-DENABLE_DEBUGGER=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mffmpeg)
+.include "../../multimedia/ffmpeg/buildlink3.mk"
+CMAKE_ARGS+=	-DENABLE_FFMPEG=ON
+.else
+CMAKE_ARGS+=	-DENABLE_FFMPEG=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mgtk)
+.include "../../sysutils/desktop-file-utils/desktopdb.mk"
+.include "../../wip/gtkglextmm/buildlink3.mk"
+CMAKE_ARGS+=	-DENABLE_GTK=ON
+PLIST.gtk=	yes
+.else
+CMAKE_ARGS+=	-DENABLE_GTK=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mlirc)
+.include "../../wip/lirc/buildlink3.mk"
+CMAKE_ARGS+=	-DENABLE_LIRC=ON
+.else
+CMAKE_ARGS+=	-DENABLE_LIRC=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mnls)
+.include "../../devel/gettext-lib/buildlink3.mk"
+CMAKE_ARGS+=	-DENABLE_NLS=ON
+PLIST.nls=	yes
+.else
+CMAKE_ARGS+=	-DENABLE_NLS=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Msdl)
+.include "../../devel/SDL/buildlink3.mk"
+CMAKE_ARGS+=	-DENABLE_SDL=ON
+.else
+CMAKE_ARGS+=	-DENABLE_SDL=OFF
+.endif
