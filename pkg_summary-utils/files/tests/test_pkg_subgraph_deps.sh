3967f22d525afff2cb15af13d24adfd7c7d7b89f
Aleksey Cheusov <cheusov@users.sourceforge.net>
2011-12-28 09:32:57 +0000

    pkg_subgraph_deps: new option -1 was added, more regression tests

--- pkg_summary-utils/files/tests/test_pkg_subgraph_deps.sh
+++ pkg_summary-utils/files/tests/test_pkg_subgraph_deps.sh
@@ -14,6 +14,16 @@ wip/runawk wip/paexec
 wip/runawk wip/pkg_summary-utils
 '
 
+pkg_subgraph_deps -1 -f src_pkgs.txt src_deps.txt | sort |
+cmp 'pkg_subgraph_deps #1.1' \
+'textproc/dictem
+wip/libuxre wip/heirloom-awk
+wip/runawk wip/awk-pkgsrc-dewey
+wip/runawk wip/distbb
+wip/runawk wip/paexec
+wip/runawk wip/pkg_summary-utils
+'
+
 pkg_subgraph_deps -f src_pkgs.txt -r src_deps.txt | sort |
 cmp 'pkg_subgraph_deps #2' \
 'devel/libtool-base wip/libuxre
@@ -21,6 +31,14 @@ lang/f2c devel/libtool-base
 textproc/dictem
 wip/heirloom-common wip/heirloom-doc
 wip/heirloom-doc wip/heirloom-libcommon
+wip/heirloom-libcommon wip/libuxre
+wip/runawk
+'
+
+pkg_subgraph_deps -1 -f src_pkgs.txt -r src_deps.txt | sort |
+cmp 'pkg_subgraph_deps #2.1' \
+'devel/libtool-base wip/libuxre
+textproc/dictem
 wip/heirloom-libcommon wip/libuxre
 wip/runawk
 '
@@ -35,11 +53,26 @@ wip/pkg_summary-utils wip/pkg_online-client
 wip/pkg_summary-utils wip/pkg_online-server
 '
 
+pkg_subgraph_deps -x1 -f src_pkgs.txt src_deps.txt | sort |
+cmp 'pkg_subgraph_deps #3.1' \
+'wip/awk-pkgsrc-dewey
+wip/distbb
+wip/heirloom-awk
+wip/paexec
+wip/pkg_summary-utils
+'
+
 pkg_subgraph_deps -rx -fsrc_pkgs.txt src_deps.txt | sort |
 cmp 'pkg_subgraph_deps #4' \
 'lang/f2c devel/libtool-base
 wip/heirloom-common wip/heirloom-doc
 wip/heirloom-doc wip/heirloom-libcommon
+'
+
+pkg_subgraph_deps -1rx -fsrc_pkgs.txt src_deps.txt | sort |
+cmp 'pkg_subgraph_deps #4.1' \
+'devel/libtool-base
+wip/heirloom-libcommon
 '
 
 pkg_subgraph_deps -xv -f src_pkgs.txt src_deps.txt | sort |
@@ -103,6 +136,44 @@ wip/libuxre
 wip/runawk
 '
 
-pkg_subgraph_deps -r -p'wip/pkg_online-client,wip/distbb' src_deps.txt | sort |
+pkg_subgraph_deps -r -p'wip/pkg_online-client,wip/distbb' src_deps.txt |
+sort |
 cmp 'pkg_subgraph_deps #8' \
 ''
+
+pkg_subgraph_deps -r -p'wip/pkg_online-client wip/distbb' src_deps.txt |
+sort |
+cmp 'pkg_subgraph_deps #8.1' \
+'devel/gmake textproc/dict-client
+devel/libmaa textproc/dict-client
+devel/libmaa wip/paexec
+devel/libtool-base devel/libmaa
+devel/libtool-base textproc/dict-client
+devel/pipestatus wip/distbb
+devel/pipestatus wip/pkg_online-client
+devel/pipestatus wip/pkg_summary-utils
+lang/f2c devel/libtool-base
+net/netcat wip/pkg_online-client
+textproc/dict-client wip/pkg_online-client
+wip/awk-pkgsrc-dewey wip/pkg_summary-utils
+wip/paexec wip/distbb
+wip/pkg_summary-utils wip/distbb
+wip/pkg_summary-utils wip/pkg_online-client
+wip/runawk wip/awk-pkgsrc-dewey
+wip/runawk wip/distbb
+wip/runawk wip/paexec
+wip/runawk wip/pkg_summary-utils
+'
+
+pkg_subgraph_deps -r1 -p'wip/pkg_online-client wip/distbb' src_deps.txt |
+sort |
+cmp 'pkg_subgraph_deps #8.2' \
+'devel/pipestatus wip/distbb
+devel/pipestatus wip/pkg_online-client
+net/netcat wip/pkg_online-client
+textproc/dict-client wip/pkg_online-client
+wip/paexec wip/distbb
+wip/pkg_summary-utils wip/distbb
+wip/pkg_summary-utils wip/pkg_online-client
+wip/runawk wip/distbb
+'
