728870597ce9bb6623230ca57dd097ec0d610f9b
Gary D Duzan <gary@duzan.org>
2012-01-02 20:02:32 +0000

    Add libalkimia, a kde4 library for use by finance applications. Required by current kmymoney2 release, package update in progress.

---
+++ libalkimia/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/01/02 20:02:32 gduzan Exp $
+
+BUILDLINK_TREE+=	libalkimia
+
+.if !defined(LIBALKIMIA_BUILDLINK3_MK)
+LIBALKIMIA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libalkimia+=	libalkimia>=4.0.0
+BUILDLINK_ABI_DEPENDS.libalkimia?=	libalkimia>=4.3.1
+BUILDLINK_PKGSRCDIR.libalkimia?=	../../wip/libalkimia
+
+.include "../../devel/gmp/buildlink3.mk"
+.include "../../meta-pkgs/kde4/kde4.mk"
+.include "../../x11/kdelibs4/buildlink3.mk"
+
+
+.endif # KDELIBS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libalkimia
