3fa70c0198e3de6f208367c2fb265676fe7950c2
Frédéric Fauberteau <triaxx@users.sourceforge.net>
2012-01-11 07:19:01 +0000

    Add options to choose several database backend.

--- semanticscuttle/options.mk
+++ semanticscuttle/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.1.1.1 2012/01/10 10:09:14 triaxx Exp $
+# $NetBSD: options.mk,v 1.2 2012/01/11 07:19:01 triaxx Exp $
 
 PKG_OPTIONS_VAR= PKG_OPTIONS.semanticscuttle
 
 PKG_OPTIONS_REQUIRED_GROUPS= db
-PKG_OPTIONS_GROUP.db= mysql
+PKG_OPTIONS_GROUP.db= mysql pgsql sqlite oracle
 
 PKG_SUGGESTED_OPTIONS= mysql
 
@@ -15,4 +15,27 @@ PKG_SUGGESTED_OPTIONS= mysql
 .if !empty(PKG_OPTIONS:Mmysql)
 .include "../../mk/mysql.buildlink3.mk"
 DEPENDS+= ${PHP_PKG_PREFIX}-mysqli:../../databases/php-mysqli
+.endif
+
+###
+### PostgreSQL backend
+###
+.if !empty(PKG_OPTIONS:Mpgsql)
+.include "../../mk/pgsql.buildlink3.mk"
+DEPENDS+= ${PHP_PKG_PREFIX}-pgsql:../../databases/php-pgsql
+.endif
+
+###
+### SQLite backend
+###
+.if !empty(PKG_OPTIONS:Msqlite)
+.include "../../databases/sqlite/buildlink3.mk"
+DEPENDS+=	${PHP_PKG_PREFIX}-sqlite:../../databases/php-sqlite
+.endif
+
+###
+### Oracle backend
+###
+.if !empty(PKG_OPTIONS:Moracle)
+DEPENDS+=	${PHP_PKG_PREFIX}-oci8:../../databases/php-oci8
 .endif
