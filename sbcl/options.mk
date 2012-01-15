4345517c23064ec2bb90b774d6e70db123937621
Aleksej Saushev <asau@users.sourceforge.net>
2012-01-15 22:22:41 +0000

    Remove broken "release" option.

--- sbcl/options.mk
+++
@@ -1,7 +0,0 @@
-# $NetBSD: options.mk,v 1.1 2012/01/15 22:14:40 dukzcry Exp $
-
-PKG_OPTIONS_VAR=		PKG_OPTIONS.sbcl
-PKG_SUPPORTED_OPTIONS=		release
-PKG_SUGGESTED_OPTIONS=
-
-.include "../../mk/bsd.options.mk"
