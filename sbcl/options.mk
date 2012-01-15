55cf53490a1cdb38173dcbc5f86488e097186e5a
Artem Falcon <lomka@gero.in>
2012-01-15 22:11:59 +0000

    Let it build and work on DragonFly. Also create 'release' option. Update to 1.0.55.

---
+++ sbcl/options.mk
@@ -0,0 +1,7 @@
+# $NetBSD: options.mk,v 1.1 2012/01/15 22:14:40 dukzcry Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.sbcl
+PKG_SUPPORTED_OPTIONS=		release
+PKG_SUGGESTED_OPTIONS=
+
+.include "../../mk/bsd.options.mk"
