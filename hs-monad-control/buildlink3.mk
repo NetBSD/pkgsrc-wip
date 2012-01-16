87ca8d4c35dbb60b81ad7d66264bcb27dca2414e
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-01-16 05:16:09 +0000

    Upstream update to monad-control-0.3.1

--- hs-monad-control/buildlink3.mk
+++ hs-monad-control/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/05/10 12:56:48 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/01/16 05:16:09 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-monad-control
 
 .if !defined(HS_MONAD_CONTROL_BUILDLINK3_MK)
 HS_MONAD_CONTROL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-monad-control+=	hs-monad-control>=0.2
+BUILDLINK_API_DEPENDS.hs-monad-control+=	hs-monad-control>=0.3.1
 BUILDLINK_PKGSRCDIR.hs-monad-control?=	../../wip/hs-monad-control
 
 .include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
+.include "../../wip/hs-transformers-base/buildlink3.mk"
 .endif	# HS_MONAD_CONTROL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-monad-control
