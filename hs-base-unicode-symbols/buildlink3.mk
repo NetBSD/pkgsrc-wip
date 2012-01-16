e070e6e90bf3bed8885dc3e7150af2eb6bcca685
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-01-16 05:06:53 +0000

    Upstream update to base-unicode-symbols-0.2.2.3

--- hs-base-unicode-symbols/buildlink3.mk
+++ hs-base-unicode-symbols/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/05/10 12:19:14 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2012/01/16 05:06:53 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-base-unicode-symbols
 
 .if !defined(HS_BASE_UNICODE_SYMBOLS_BUILDLINK3_MK)
 HS_BASE_UNICODE_SYMBOLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base-unicode-symbols+=	hs-base-unicode-symbols>=0.2
+BUILDLINK_API_DEPENDS.hs-base-unicode-symbols+=	hs-base-unicode-symbols>=0.2.2
 BUILDLINK_PKGSRCDIR.hs-base-unicode-symbols?=	../../wip/hs-base-unicode-symbols
 .endif	# HS_BASE_UNICODE_SYMBOLS_BUILDLINK3_MK
 
