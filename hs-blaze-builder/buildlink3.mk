2f9e4d4525d3153f4f34f4a27b79b844b8e70267
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-01-17 12:47:29 +0000

    Upstream update to blaze-builder-0.3.0.2

--- hs-blaze-builder/buildlink3.mk
+++ hs-blaze-builder/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/29 16:09:52 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/01/17 12:47:29 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-blaze-builder
 
 .if !defined(HS_BLAZE_BUILDER_BUILDLINK3_MK)
 HS_BLAZE_BUILDER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.3
+BUILDLINK_API_DEPENDS.hs-blaze-builder+=	hs-blaze-builder>=0.3.0
 BUILDLINK_PKGSRCDIR.hs-blaze-builder?=	../../wip/hs-blaze-builder
 
 .include "../../wip/hs-text/buildlink3.mk"
