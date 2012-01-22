51d7f77cd2f4caba60fd29424659ed63b481aee3
Mark Davies <mark@ecs.vuw.ac.nz>
2012-01-22 21:04:29 +0000

    Add hack to work around build link tree not being built correctly.

--- kdelibs4/hacks.mk
+++ kdelibs4/hacks.mk
@@ -1,6 +1,10 @@
-# $NetBSD: hacks.mk,v 1.1 2011/05/13 23:25:15 mwdavies Exp $
+# $NetBSD: hacks.mk,v 1.2 2012/01/22 21:04:29 mwdavies Exp $
 
 .if ${OPSYS} == "NetBSD"
 post-configure:
 	cp ${FILESDIR}/kshareddatacache.cpp ${WRKSRC}/kdecore/util/kshareddatacache.cpp
 .endif
+
+# fix up buildlinks failure to build link tree correctly
+post-wrapper:
+	ln -s netbsd-g++ ${BUILDLINK_DIR}/qt4/mkspecs/default
