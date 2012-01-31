981b66c9d1b89970b449680d310d789cebcf927d
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-01-31 13:09:05 +0000

    Upstream update to quickcheck-instances-0.2.0

--- hs-quickcheck-instances/buildlink3.mk
+++ hs-quickcheck-instances/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/01/17 14:21:07 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/01/31 13:09:06 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-quickcheck-instances
 
 .if !defined(HS_QUICKCHECK_INSTANCES_BUILDLINK3_MK)
 HS_QUICKCHECK_INSTANCES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-quickcheck-instances+=	hs-quickcheck-instances>=0.1.0
+BUILDLINK_API_DEPENDS.hs-quickcheck-instances+=	hs-quickcheck-instances>=0.2.0
 BUILDLINK_PKGSRCDIR.hs-quickcheck-instances?=	../../wip/hs-quickcheck-instances
 
 .include "../../wip/hs-QuickCheck/buildlink3.mk"
