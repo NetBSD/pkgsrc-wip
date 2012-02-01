ca179a0cadd545e7cb5138becceccf29e85fb52d
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-02-01 16:55:58 +0000

    Upstream update to attempt-0.4.0

--- hs-attempt/buildlink3.mk
+++ hs-attempt/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/12/02 06:08:39 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/02/01 16:55:58 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-attempt
 
 .if !defined(HS_ATTEMPT_BUILDLINK3_MK)
 HS_ATTEMPT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-attempt+=	hs-attempt>=0.3.1
+BUILDLINK_API_DEPENDS.hs-attempt+=	hs-attempt>=0.4.0
 BUILDLINK_PKGSRCDIR.hs-attempt?=	../../wip/hs-attempt
 
 .include "../../wip/hs-failure/buildlink3.mk"
