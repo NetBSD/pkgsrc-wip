9f4d6a92fa46cf3079a7be449747e2fbd6855246
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-02-01 15:49:18 +0000

    Upstream update to failure-0.2.0

--- hs-failure/buildlink3.mk
+++ hs-failure/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/01/16 05:45:09 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/02/01 15:49:18 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-failure
 
 .if !defined(HS_FAILURE_BUILDLINK3_MK)
 HS_FAILURE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-failure+=	hs-failure>=0.1.2
+BUILDLINK_API_DEPENDS.hs-failure+=	hs-failure>=0.2.0
 BUILDLINK_PKGSRCDIR.hs-failure?=	../../wip/hs-failure
 
 .include "../../wip/hs-transformers/buildlink3.mk"
