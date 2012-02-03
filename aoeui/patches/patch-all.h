36f561e3f3573c4ce8ff4b3fb492f40f78f9f308
Aleksej Lebedev <root@zta.lk>
2012-02-03 17:07:30 +0000

    Updated to 1.6

--- aoeui/patches/patch-all.h
+++ aoeui/patches/patch-all.h
@@ -1,16 +1,15 @@
-$NetBSD: patch-all.h,v 1.2 2012/02/03 16:12:02 realzhtw Exp $
+$NetBSD: patch-all.h,v 1.3 2012/02/03 17:07:30 realzhtw Exp $
 
---- all.h.orig	2010-10-14 21:48:53.000000000 +0000
+--- all.h.orig	2011-11-10 18:10:25.000000000 +0000
 +++ all.h
-@@ -23,8 +23,10 @@
+@@ -23,7 +23,9 @@
  #include <sys/stat.h>
  #include <sys/time.h>
  #include <sys/wait.h>
--#ifdef __APPLE__
-+#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__NetBSD__)
- # include <util.h>
-+#elif defined(__FreeBSD__) || defined(__DragonFly__)
+-#if defined __APPLE__ || defined BSD
++#if defined __FreeBSD__
 +# include <libutil.h>
++#elif defined __APPLE__ || defined __NetBSD__ || defined __OpenBSD__ || defined __DragonFly__
+ # include <util.h>
  #else
  # include <pty.h>
- #endif
