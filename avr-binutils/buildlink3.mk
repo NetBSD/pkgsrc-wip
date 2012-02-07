44ce304504baf1e051dd1ae75ab775559cb724f3
Makoto Fujiwara <makoto@ki.nu>
2012-02-07 04:44:08 +0000

    Place wip/avr-* reference instead of cross/avr-* reference, so that testing on wip directory assures true dependency. Those references need back to cross/avr-* when importing to cross/ directory.

--- avr-binutils/buildlink3.mk
+++ avr-binutils/buildlink3.mk
@@ -1,11 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/02/07 03:15:19 makoto Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/02/07 04:44:08 makoto Exp $
 
 BUILDLINK_TREE+=	avr-binutils
 
 .if !defined(AVR_BINUTILS_BUILDLINK3_MK)
 AVR_BINUTILS_BUILDLINK3_MK:=
-
-#.include "../../cross/avr/Makefile.common"
 
 BUILDLINK_API_DEPENDS.avr-binutils+=	avr-binutils>=2.22
 BUILDLINK_PKGSRCDIR.avr-binutils?=		../../wip/avr-binutils
