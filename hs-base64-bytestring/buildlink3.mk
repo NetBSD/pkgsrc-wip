6b4ff39a563426580aa3c872d3d60457f492a1cb
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-02-11 04:43:19 +0000

    Upstream update to base64-bytestring-0.1.1.0

--- hs-base64-bytestring/buildlink3.mk
+++ hs-base64-bytestring/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/10/17 23:39:29 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/02/11 04:43:19 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-base64-bytestring
 
 .if !defined(HS_BASE64_BYTESTRING_BUILDLINK3_MK)
 HS_BASE64_BYTESTRING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=0.1.0
+BUILDLINK_API_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=0.1.1
 BUILDLINK_PKGSRCDIR.hs-base64-bytestring?=	../../wip/hs-base64-bytestring
 .endif	# HS_BASE64_BYTESTRING_BUILDLINK3_MK
 
