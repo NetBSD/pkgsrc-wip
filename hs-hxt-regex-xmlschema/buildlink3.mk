d3c7b1b481aea7689bfd96ea237622a7608b19ec
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-02-11 04:15:36 +0000

    Upstream update to hxt-regex-xmlschema-9.0.4

--- hs-hxt-regex-xmlschema/buildlink3.mk
+++ hs-hxt-regex-xmlschema/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/27 05:44:33 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/02/11 04:15:36 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-hxt-regex-xmlschema
 
 .if !defined(HS_HXT_REGEX_XMLSCHEMA_BUILDLINK3_MK)
 HS_HXT_REGEX_XMLSCHEMA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hxt-regex-xmlschema+=	hs-hxt-regex-xmlschema>=9.0
+BUILDLINK_API_DEPENDS.hs-hxt-regex-xmlschema+=	hs-hxt-regex-xmlschema>=9.0.4
 BUILDLINK_PKGSRCDIR.hs-hxt-regex-xmlschema?=	../../wip/hs-hxt-regex-xmlschema
 
 .include "../../wip/hs-hxt-charproperties/buildlink3.mk"
