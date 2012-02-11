15b93936d1531305c773456a025669979ccc3506
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-02-11 03:35:33 +0000

    Upstream update to unix-compat-0.3.0.1

--- hs-unix-compat/buildlink3.mk
+++ hs-unix-compat/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 08:22:49 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/02/11 03:35:33 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-unix-compat
 
 .if !defined(HS_UNIX_COMPAT_BUILDLINK3_MK)
 HS_UNIX_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.2
+BUILDLINK_API_DEPENDS.hs-unix-compat+=	hs-unix-compat>=0.3.0
 BUILDLINK_PKGSRCDIR.hs-unix-compat?=	../../wip/hs-unix-compat
 .endif	# HS_UNIX_COMPAT_BUILDLINK3_MK
 
