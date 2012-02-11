01a702d7a91619f3af3ef0ced4ce6fd8c19d5b83
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-02-11 04:13:14 +0000

    Upstream update to hs-parsec-3.1.2

--- hs-parsec/buildlink3.mk
+++ hs-parsec/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.4 2011/03/03 01:45:43 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2012/02/11 04:13:14 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-parsec
 
 .if !defined(HS_PARSEC_BUILDLINK3_MK)
 HS_PARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-parsec+=	hs-parsec>=3.1
+BUILDLINK_API_DEPENDS.hs-parsec+=	hs-parsec>=3.1.2
 BUILDLINK_PKGSRCDIR.hs-parsec?=	../../wip/hs-parsec
 
 .include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
 .endif	# HS_PARSEC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-parsec
