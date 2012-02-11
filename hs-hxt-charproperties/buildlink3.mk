d647a21cc131cffc99a9fe0686876ed9ec4ab5ef
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-02-11 03:45:46 +0000

    Upstream update to hxt-charproperties-9.1.1

--- hs-hxt-charproperties/buildlink3.mk
+++ hs-hxt-charproperties/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/27 05:23:07 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/02/11 03:45:46 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-hxt-charproperties
 
 .if !defined(HS_HXT_CHARPROPERTIES_BUILDLINK3_MK)
 HS_HXT_CHARPROPERTIES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hxt-charproperties+=	hs-hxt-charproperties>=9.1
+BUILDLINK_API_DEPENDS.hs-hxt-charproperties+=	hs-hxt-charproperties>=9.1.1
 BUILDLINK_PKGSRCDIR.hs-hxt-charproperties?=	../../wip/hs-hxt-charproperties
 .endif	# HS_HXT_CHARPROPERTIES_BUILDLINK3_MK
 
