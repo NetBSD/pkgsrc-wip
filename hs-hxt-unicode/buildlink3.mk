ef0ac4ca34a1e04982d6e95e45ffdeb1996c7b79
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-02-11 04:16:58 +0000

    Upstream update to hxt-unicode-9.0.2

--- hs-hxt-unicode/buildlink3.mk
+++ hs-hxt-unicode/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/27 05:52:11 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/02/11 04:16:58 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-hxt-unicode
 
 .if !defined(HS_HXT_UNICODE_BUILDLINK3_MK)
 HS_HXT_UNICODE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hxt-unicode+=	hs-hxt-unicode>=9.0
+BUILDLINK_API_DEPENDS.hs-hxt-unicode+=	hs-hxt-unicode>=9.0.2
 BUILDLINK_PKGSRCDIR.hs-hxt-unicode?=	../../wip/hs-hxt-unicode
 
 .include "../../wip/hs-hxt-charproperties/buildlink3.mk"
