be106a607f715315875a9e445e253573c481b7ef
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-02-11 04:51:12 +0000

    Upstream update to happy-1.18.9

--- happy/buildlink3.mk
+++ happy/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/01/14 07:01:50 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2012/02/11 04:51:12 phonohawk Exp $
 
 BUILDLINK_TREE+=	happy
 
@@ -6,8 +6,9 @@ BUILDLINK_TREE+=	happy
 HAPPY_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.happy?=	build
-BUILDLINK_API_DEPENDS.happy+=	happy>=1.18
+BUILDLINK_API_DEPENDS.happy+=	happy>=1.18.9
 BUILDLINK_PKGSRCDIR.happy?=	../../wip/happy
-.endif # HAPPY_BUILDLINK3_MK
+
+.endif	# HAPPY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-happy
