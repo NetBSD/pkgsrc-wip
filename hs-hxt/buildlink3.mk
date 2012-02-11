64acdaad1858bb81477996cc80a5d86f9ed144e8
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-02-11 04:38:13 +0000

    Upstream update to hxt-9.2.0

--- hs-hxt/buildlink3.mk
+++ hs-hxt/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2011/07/27 06:22:04 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2012/02/11 04:38:13 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-hxt
 
 .if !defined(HS_HXT_BUILDLINK3_MK)
 HS_HXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hxt+=	hs-hxt>=9.1
+BUILDLINK_API_DEPENDS.hs-hxt+=	hs-hxt>=9.2.0
 BUILDLINK_PKGSRCDIR.hs-hxt?=	../../wip/hs-hxt
 
 .include "../../wip/hs-binary/buildlink3.mk"
@@ -13,6 +13,7 @@ BUILDLINK_PKGSRCDIR.hs-hxt?=	../../wip/hs-hxt
 .include "../../wip/hs-hxt-charproperties/buildlink3.mk"
 .include "../../wip/hs-hxt-regex-xmlschema/buildlink3.mk"
 .include "../../wip/hs-hxt-unicode/buildlink3.mk"
+.include "../../wip/hs-mtl/buildlink3.mk"
 .include "../../wip/hs-network/buildlink3.mk"
 .include "../../wip/hs-parsec/buildlink3.mk"
 .endif	# HS_HXT_BUILDLINK3_MK
