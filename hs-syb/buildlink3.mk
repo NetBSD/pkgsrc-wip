212f7d89841fb746b97967dd6f67736ed22039a9
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-02-11 04:44:54 +0000

    Upstream update to syb-0.3.6

--- hs-syb/buildlink3.mk
+++ hs-syb/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/12/30 07:24:04 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/02/11 04:44:54 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-syb
 
 .if !defined(HS_SYB_BUILDLINK3_MK)
 HS_SYB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-syb+=	hs-syb>=0.3
+BUILDLINK_API_DEPENDS.hs-syb+=	hs-syb>=0.3.6
 BUILDLINK_PKGSRCDIR.hs-syb?=	../../wip/hs-syb
 .endif	# HS_SYB_BUILDLINK3_MK
 
