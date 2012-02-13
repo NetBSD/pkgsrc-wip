c1297843b0235d67e7c6e2fcbd60d533f634175e
Bartosz Kuzma <bartosz.kuzma@gmail.com>
2012-02-13 13:50:13 +0000

    - Provide reasonable and useful configuration file (based on rsyslog.conf    and syslog.conf from NetBSD 5.1)  - New rc.d script - it correctly handles socket chroot dirs (like syslogd do)  - Install HTML documentation.

--- rsyslog5/files/rsyslogd.sh
+++ rsyslog5/files/rsyslogd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: rsyslogd.sh,v 1.1.1.1 2011/04/18 13:46:22 bartoszkuzma Exp $
+# $NetBSD: rsyslogd.sh,v 1.2 2012/02/13 13:50:13 bartoszkuzma Exp $
 # NetBSD: syslogd,v 1.15 2004/10/11 13:29:52 lukem Exp
 #
 
@@ -15,19 +15,21 @@ fi
 name="rsyslogd"
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
-pidfile="/var/run/${name}.pid"
+pidfile="@VARBASE@/run/${name}.pid"
 required_files="@PKG_SYSCONFDIR@/rsyslog.conf"
 start_precmd="rsyslogd_precmd"
 extra_commands="reload"
 
-_sockfile="/var/run/syslogd.sockets"
+rsyslogd_flags="-c 5"
+
+_sockfile="@VARBASE@/run/rsyslogd.sockets"
 
 rsyslogd_precmd()
 {
 	#	Transitional symlink for old binaries
 	#
 	if [ ! -h /dev/log ]; then
-		ln -sf /var/run/log /dev/log
+		ln -sf @VARBASE@/run/log /dev/log
 	fi
 
 	#	Create default list of syslog sockets to watch
@@ -44,19 +46,12 @@ rsyslogd_precmd()
 		load_rc_config ${_l}
 		eval _ldir=\$${_l}_chrootdir
 		if checkyesno $_l && [ -n "$_ldir" ]; then
-			echo "${_ldir}/var/run/log" >> $_sockfile
+			echo "\$AddUnixListenSocket ${_ldir}@VARBASE@/run/log" >> $_sockfile
 		fi
 	    )
 	done
 
-	#	If other sockets have been provided, change run_rc_command()'s
-	#	internal copy of $rsyslogd_flags to force use of specific
-	#	syslogd sockets.
-	#
-	if [ -s $_sockfile ]; then
-		echo "/var/run/log" >> $_sockfile
-		rc_flags="-P $_sockfile $rc_flags"
-	fi
+	echo "\$SystemLogSocketName @VARBASE@/run/log" >> $_sockfile
 
 	return 0
 }
