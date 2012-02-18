7c33eab595f93ffcc376f88a617618b5f8838a38
ISIHARA Takanori <ishit@users.sourceforge.net>
2012-02-18 21:31:43 +0000

    Add PKG_DESTDIR_SUPPORT. Update to Polipo 1.0.4.1.
    from CHANGELOG:
    31 January 2010: Polipo 1.0.4.1:
      Cherry-picked fixes from 1.0.5
      * Fixed an integer overflow that may lead to a crash
        (http://secunia.com/advisories/37607/). Discovered by Jeremy Brown.
        (CVE-2009-4413)
      * Fixed a crash that occurs when a server sends a malformed
        Cache-Control: header (CVE-2009-3305). Patch from Stefan Fritsch.
      * Prevent an infinite loop when a bodyless 204 or 1xx response is encountered.
      * Don't crash when we get an error while waiting for 100 continue status.

--- polipo/patches/patch-client.c
+++ polipo/patches/patch-client.c
@@ -1,21 +1,11 @@
-$NetBSD: patch-client.c,v 1.1 2012/02/18 20:20:37 ishit Exp $
+$NetBSD: patch-client.c,v 1.2 2012/02/18 21:31:43 ishit Exp $
 
-fix security issues.
-fix CVE-2009-3305 from Debian BTS.
-fix CVE-2011-3596 from Tor Project git repository.
+fix security issue (CVE-2011-3596).
+patch from Tor Project git repository.
 cf.https://bugzilla.redhat.com/show_bug.cgi?id=CVE-2011-3596
 
 --- client.c.orig	2008-01-08 12:56:45.000000000 +0000
 +++ client.c
-@@ -978,7 +978,7 @@ httpClientDiscardBody(HTTPConnectionPtr 
-     if(connection->bodylen < 0)
-         goto fail;
- 
--    if(connection->bodylen + connection->reqbegin < connection->reqlen) {
-+    if(connection->bodylen < connection->reqlen - connection->reqbegin) {
-         connection->reqbegin += connection->bodylen;
-         connection->bodylen = 0;
-     } else {
 @@ -987,7 +987,6 @@ httpClientDiscardBody(HTTPConnectionPtr 
          connection->reqlen = 0;
          httpConnectionDestroyReqbuf(connection);
