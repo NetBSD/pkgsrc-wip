bca194e9457321b583a690f18e4b597b0889e8a6
Guillaume Lasmayous <gls@NetBSD.org>
2012-02-19 16:49:33 +0000

    Update wip/libxfce4util to 4.8.2.
    pkgsrc changes:
    - adjust bl3.
    
    Upstream changes:
    
    4.8.2
    =====
    - Change the version string to 4.8.
    - Fixed incorrect assertion in xfce_strjoin (bug #7122).
    - Fix "empty declaration" compiler warnings.
    - Update FSF address and add missing licenses.
    - Fix undef in libxfce4util.h header.
    - Remove the internal slice allocator macros.
    - Use chain to free slice instead of a loop.
    - Translation updates (ko, ru, sk, zh_CN, nl).

--- libxfce4util/buildlink3.mk
+++ libxfce4util/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2011/06/29 13:29:27 reinoudz70 Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2012/02/19 16:49:33 guigui2 Exp $
 
 BUILDLINK_TREE+=	libxfce4util
 
 .if !defined(LIBXFCE4UTIL_BUILDLINK3_MK)
 LIBXFCE4UTIL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxfce4util+=	libxfce4util>=4.6.0
-BUILDLINK_ABI_DEPENDS.libxfce4util?=	libxfce4util>=4.6.1nb4
+BUILDLINK_API_DEPENDS.libxfce4util+=	libxfce4util>=4.8.0
+BUILDLINK_ABI_DEPENDS.libxfce4util?=	libxfce4util>=4.8.0
 BUILDLINK_PKGSRCDIR.libxfce4util?=	../../wip/libxfce4util
 
 .include "../../devel/glib2/buildlink3.mk"
