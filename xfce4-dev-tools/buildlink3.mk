c8b313282387627315deeb13e75d1fbab1bb363d
Guillaume Lasmayous <gls@NetBSD.org>
2012-02-19 16:45:59 +0000

    Adjust buildlink3 file.

--- xfce4-dev-tools/buildlink3.mk
+++ xfce4-dev-tools/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2011/06/29 09:37:23 reinoudz70 Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2012/02/19 16:45:59 guigui2 Exp $
 
 BUILDLINK_TREE+=	xfce4-dev-tools
 
 .if !defined(XFCE4_DEV_TOOLS_BUILDLINK3_MK)
 XFCE4_DEV_TOOLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xfce4-dev-tools+=	xfce4-dev-tools>=4.6.0
-BUILDLINK_PKGSRCDIR.xfce4-dev-tools?=	../../devel/xfce4-dev-tools
+BUILDLINK_API_DEPENDS.xfce4-dev-tools+=	xfce4-dev-tools>=4.8.0
+BUILDLINK_PKGSRCDIR.xfce4-dev-tools?=	../../wip/xfce4-dev-tools
 BUILDLINK_DEPMETHOD.xfce4-dev-tools?=	build
 .endif # XFCE4_DEV_TOOLS_BUILDLINK3_MK
 
