239b2ce4bab01ee83a69cc1957c6f229f502225c
Reinoud Zandijk <reinoud@NetBSD.org>
2012-02-26 19:55:46 +0000

    Update dependencies and buildlink

--- opencsg/buildlink3.mk
+++ opencsg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/02/24 21:17:45 reinoudz70 Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/02/26 19:55:46 reinoudz70 Exp $
 
 BUILDLINK_TREE+=	opencsg
 
@@ -6,9 +6,9 @@ BUILDLINK_TREE+=	opencsg
 OPENCSG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.opencsg+=	opencsg>=1.3.2
-BUILDLINK_ABI_DEPENDS.opencsg?=	opencsg>=1.3.2
 BUILDLINK_PKGSRCDIR.opencsg?=	../../wip/opencsg
 
-.endif # OPENCSG_BUILDLINK3_MK
+.include "../../graphics/glew/buildlink3.mk"
+.endif	# OPENCSG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-opencsg
