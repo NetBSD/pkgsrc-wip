35d32cf3847d4c999e79c77a7bab911e2f0c064c
Mike M. Volokhov <bubuchka@users.sourceforge.net>
2012-02-26 01:47:20 +0000

    The simply reverted patch is obsolete - recreate it from scratch.

--- kicad/patches/patch-CMakeModules_config.h.cmake
+++ kicad/patches/patch-CMakeModules_config.h.cmake
@@ -1,8 +1,9 @@
-$NetBSD: patch-CMakeModules_config.h.cmake,v 1.3 2012/02/26 01:42:15 bubuchka Exp $
+$NetBSD: patch-CMakeModules_config.h.cmake,v 1.4 2012/02/26 01:47:20 bubuchka Exp $
 
-$ fixes compile problem due to iso646
---- ./CMakeModules/config.h.cmake.orig	2011-05-29 05:59:41.000000000 +0000
-+++ ./CMakeModules/config.h.cmake
+Fix compile problem due to ciso646.
+
+--- CMakeModules/config.h.cmake.orig	2011-11-27 11:43:14.000000000 +0000
++++ CMakeModules/config.h.cmake
 @@ -32,7 +32,7 @@
  #cmakedefine HAVE_ISO646_H
  
@@ -11,4 +12,4 @@ $ fixes compile problem due to iso646
 +#include <ciso646>
  #endif
  
- #cmakedefine HAVE_STRINGS_H
+ #if defined( HAVE_STRCASECMP )
