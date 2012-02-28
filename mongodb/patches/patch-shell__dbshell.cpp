4549c528e8c24c4d6ee202d33956f682c3cb181e
Bartosz Kuzma <bartosz.kuzma@gmail.com>
2012-02-28 19:57:46 +0000

    Add support for DragonFlyBSD.

--- mongodb/patches/patch-shell__dbshell.cpp
+++ mongodb/patches/patch-shell__dbshell.cpp
@@ -1,4 +1,4 @@
-$NetBSD: patch-shell__dbshell.cpp,v 1.1 2012/02/22 11:36:27 bartoszkuzma Exp $
+$NetBSD: patch-shell__dbshell.cpp,v 1.2 2012/02/28 19:57:46 bartoszkuzma Exp $
 
 --- shell/dbshell.cpp.orig	2011-12-14 17:18:48.000000000 +0000
 +++ shell/dbshell.cpp
@@ -7,7 +7,7 @@ $NetBSD: patch-shell__dbshell.cpp,v 1.1 2012/02/22 11:36:27 bartoszkuzma Exp $
  
  
 -#if defined(USE_LINENOISE) && !defined(__freebsd__) && !defined(__openbsd__) && !defined(_WIN32)
-+#if defined(USE_LINENOISE) && !defined(__freebsd__) && !defined(__netbsd) && !defined(__openbsd__) && !defined(_WIN32)
++#if defined(USE_LINENOISE) && !defined(__freebsd__) && !defined(__netbsd__) && !defined(__openbsd__) && !defined(_WIN32)
  // this is for ctrl-c handling
  #include <setjmp.h>
  jmp_buf jbuf;
