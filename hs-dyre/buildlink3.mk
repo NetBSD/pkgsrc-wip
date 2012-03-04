40770f508c29377cf8198bbd7675849a81a07481
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-04 07:51:42 +0000

    binary is bundled in ghc-7.4.1

--- hs-dyre/buildlink3.mk
+++ hs-dyre/buildlink3.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/08 10:10:44 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/04 07:51:42 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-dyre
 
 .if !defined(HS_DYRE_BUILDLINK3_MK)
 HS_DYRE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-dyre+=	hs-dyre>=0.8
+BUILDLINK_API_DEPENDS.hs-dyre+=	hs-dyre>=0.8.6
 BUILDLINK_PKGSRCDIR.hs-dyre?=	../../wip/hs-dyre
 
-.include "../../wip/hs-binary/buildlink3.mk"
 .include "../../wip/hs-executable-path/buildlink3.mk"
 .include "../../wip/hs-ghc-paths/buildlink3.mk"
 .include "../../wip/hs-io-storage/buildlink3.mk"
