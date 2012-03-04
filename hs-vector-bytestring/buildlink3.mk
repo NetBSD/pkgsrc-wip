23903bc7de7383e8d17966a197de767d1fe9fbb1
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-04 06:05:51 +0000

    deepseq is bundled in ghc-7.4.1

--- hs-vector-bytestring/buildlink3.mk
+++ hs-vector-bytestring/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/02/02 13:11:52 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/04 06:05:51 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-vector-bytestring
 
@@ -8,7 +8,6 @@ HS_VECTOR_BYTESTRING_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.hs-vector-bytestring+=	hs-vector-bytestring>=0.0.0
 BUILDLINK_PKGSRCDIR.hs-vector-bytestring?=	../../wip/hs-vector-bytestring
 
-.include "../../wip/hs-deepseq/buildlink3.mk"
 .include "../../wip/hs-primitive/buildlink3.mk"
 .include "../../wip/hs-vector/buildlink3.mk"
 .endif	# HS_VECTOR_BYTESTRING_BUILDLINK3_MK
