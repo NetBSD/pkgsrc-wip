954db41740d9e46b66d10ab29c3929075f588165
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-04 05:54:43 +0000

    Upstream update to bytestring-trie-0.2.3

--- hs-bytestring-trie/buildlink3.mk
+++ hs-bytestring-trie/buildlink3.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/10/02 03:26:52 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/04 05:54:43 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-bytestring-trie
 
 .if !defined(HS_BYTESTRING_TRIE_BUILDLINK3_MK)
 HS_BYTESTRING_TRIE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-bytestring-trie+=	hs-bytestring-trie>=0.1.4
+BUILDLINK_API_DEPENDS.hs-bytestring-trie+=	hs-bytestring-trie>=0.2.3
 BUILDLINK_PKGSRCDIR.hs-bytestring-trie?=	../../wip/hs-bytestring-trie
 
-.include "../../wip/hs-binary/buildlink3.mk"
 .endif	# HS_BYTESTRING_TRIE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-bytestring-trie
