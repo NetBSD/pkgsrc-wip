766f17b3e988c470ab6c331c28eb1a5cc27ae4f9
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-04 08:37:16 +0000

    Upstream update to event-list-0.1

--- hs-event-list/buildlink3.mk
+++ hs-event-list/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/02/19 08:20:55 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/03/04 08:37:16 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-event-list
 
 .if !defined(HS_EVENT_LIST_BUILDLINK3_MK)
 HS_EVENT_LIST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-event-list+=	hs-event-list>=0.0.10.1
+BUILDLINK_API_DEPENDS.hs-event-list+=	hs-event-list>=0.1
 BUILDLINK_PKGSRCDIR.hs-event-list?=	../../wip/hs-event-list
 
+.include "../../wip/hs-QuickCheck/buildlink3.mk"
 .include "../../wip/hs-non-negative/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
 .include "../../wip/hs-utility-ht/buildlink3.mk"
