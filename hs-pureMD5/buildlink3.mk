1af12b4a370d54464a51f2aecd8b8994905f9c29
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-04 08:17:53 +0000

    binary is bundled in ghc-7.4.1

--- hs-pureMD5/buildlink3.mk
+++ hs-pureMD5/buildlink3.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 07:43:06 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/04 08:17:53 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-pureMD5
 
 .if !defined(HS_PUREMD5_BUILDLINK3_MK)
 HS_PUREMD5_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pureMD5+=	hs-pureMD5>=2.1
+BUILDLINK_API_DEPENDS.hs-pureMD5+=	hs-pureMD5>=2.1.0
 BUILDLINK_PKGSRCDIR.hs-pureMD5?=	../../wip/hs-pureMD5
 
-.include "../../wip/hs-binary/buildlink3.mk"
 .include "../../wip/hs-cereal/buildlink3.mk"
 .include "../../wip/hs-crypto-api/buildlink3.mk"
 .include "../../wip/hs-tagged/buildlink3.mk"
