2ea79ef30e2d40ffb8a2ee4f3d3620456651d6de
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-04 07:44:46 +0000

    Upstream update to vty-4.7.0.10

--- hs-vty/buildlink3.mk
+++ hs-vty/buildlink3.mk
@@ -1,19 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 14:38:14 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/04 07:44:46 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-vty
 
 .if !defined(HS_VTY_BUILDLINK3_MK)
 HS_VTY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vty+=	hs-vty>=4.7
+BUILDLINK_API_DEPENDS.hs-vty+=	hs-vty>=4.7.0
 BUILDLINK_PKGSRCDIR.hs-vty?=	../../wip/hs-vty
 
-.include "../../wip/hs-deepseq/buildlink3.mk"
 .include "../../wip/hs-mtl/buildlink3.mk"
 .include "../../wip/hs-parallel/buildlink3.mk"
 .include "../../wip/hs-parsec/buildlink3.mk"
 .include "../../wip/hs-terminfo/buildlink3.mk"
 .include "../../wip/hs-utf8-string/buildlink3.mk"
+.include "../../wip/hs-vector/buildlink3.mk"
 .endif	# HS_VTY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-vty
