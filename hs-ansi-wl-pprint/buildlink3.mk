61e380c798b7a7ad654b54bfd5c6a459565119e3
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-04 08:53:38 +0000

    Upstream update to ansi-wl-pprint-0.6.4

--- hs-ansi-wl-pprint/buildlink3.mk
+++ hs-ansi-wl-pprint/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/12/04 12:32:54 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/04 08:53:39 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-ansi-wl-pprint
 
 .if !defined(HS_ANSI_WL_PPRINT_BUILDLINK3_MK)
 HS_ANSI_WL_PPRINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ansi-wl-pprint+=	hs-ansi-wl-pprint>=0.6.3
+BUILDLINK_API_DEPENDS.hs-ansi-wl-pprint+=	hs-ansi-wl-pprint>=0.6.4
 BUILDLINK_PKGSRCDIR.hs-ansi-wl-pprint?=	../../wip/hs-ansi-wl-pprint
 
 .include "../../wip/hs-ansi-terminal/buildlink3.mk"
