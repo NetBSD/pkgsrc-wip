fd4ea473145cd58e32db5ae5538170fff5ba01f2
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-04 07:27:17 +0000

    Upstream update to mmap-0.5.7

--- hs-mmap/buildlink3.mk
+++ hs-mmap/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/01/07 09:31:57 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2012/03/04 07:27:17 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-mmap
 
 .if !defined(HS_MMAP_BUILDLINK3_MK)
 HS_MMAP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mmap+=	hs-mmap>=0.5
+BUILDLINK_API_DEPENDS.hs-mmap+=	hs-mmap>=0.5.7
 BUILDLINK_PKGSRCDIR.hs-mmap?=	../../wip/hs-mmap
 .endif	# HS_MMAP_BUILDLINK3_MK
 
