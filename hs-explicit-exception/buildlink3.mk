ee02548a00a9bdb55d3b20c85712bcfdd3db341b
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-04 08:40:09 +0000

    Upstream update to explicit-exception-0.1.6

--- hs-explicit-exception/buildlink3.mk
+++ hs-explicit-exception/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/09/23 06:48:34 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/04 08:40:09 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-explicit-exception
 
 .if !defined(HS_EXPLICIT_EXCEPTION_BUILDLINK3_MK)
 HS_EXPLICIT_EXCEPTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-explicit-exception+=	hs-explicit-exception>=0.1.4
+BUILDLINK_API_DEPENDS.hs-explicit-exception+=	hs-explicit-exception>=0.1.6
 BUILDLINK_PKGSRCDIR.hs-explicit-exception?=	../../wip/hs-explicit-exception
 
 .include "../../wip/hs-transformers/buildlink3.mk"
