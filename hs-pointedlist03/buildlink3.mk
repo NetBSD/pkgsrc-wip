70327b1f772d1c572f8b3b2acde3f789d17be58f
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-04 06:10:32 +0000

    binary is bundled in ghc-7.4.1

--- hs-pointedlist03/buildlink3.mk
+++ hs-pointedlist03/buildlink3.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 16:18:23 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/04 06:10:32 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-pointedlist03
 
 .if !defined(HS_POINTEDLIST03_BUILDLINK3_MK)
 HS_POINTEDLIST03_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pointedlist03+=	hs-pointedlist03>=0.3
+BUILDLINK_API_DEPENDS.hs-pointedlist03+=	hs-pointedlist03>=0.3.5
 BUILDLINK_PKGSRCDIR.hs-pointedlist03?=	../../wip/hs-pointedlist03
 
-.include "../../wip/hs-binary/buildlink3.mk"
 .include "../../wip/hs-data-accessor/buildlink3.mk"
 .endif	# HS_POINTEDLIST03_BUILDLINK3_MK
 
