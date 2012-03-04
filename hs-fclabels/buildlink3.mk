c542d3eb7bef9a4b20c032730eb8737f62b42e80
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-04 07:06:36 +0000

    Upstream update to fclabels-1.0.4

--- hs-fclabels/buildlink3.mk
+++ hs-fclabels/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 05:06:10 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/04 07:06:36 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-fclabels
 
 .if !defined(HS_FCLABELS_BUILDLINK3_MK)
 HS_FCLABELS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-fclabels+=	hs-fclabels>=0.11
+BUILDLINK_API_DEPENDS.hs-fclabels+=	hs-fclabels>=1.0.4
 BUILDLINK_PKGSRCDIR.hs-fclabels?=	../../wip/hs-fclabels
 
 .include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_FCLABELS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-fclabels
