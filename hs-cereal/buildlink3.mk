45a7f29b1867a67ceb7b6e8246ff6ef0c8cfd5d2
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-04 07:59:16 +0000

    Upstream update to cereal-0.3.5.1

--- hs-cereal/buildlink3.mk
+++ hs-cereal/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 07:13:22 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/04 07:59:16 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-cereal
 
 .if !defined(HS_CEREAL_BUILDLINK3_MK)
 HS_CEREAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cereal+=	hs-cereal>=0.3
+BUILDLINK_API_DEPENDS.hs-cereal+=	hs-cereal>=0.3.5
 BUILDLINK_PKGSRCDIR.hs-cereal?=	../../wip/hs-cereal
 .endif	# HS_CEREAL_BUILDLINK3_MK
 
