bfde9e4f1a1cdab6d01ebbea0befde90dc34ac48
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-04 08:22:29 +0000

    Upstream update to utility-ht-0.0.8

--- hs-utility-ht/buildlink3.mk
+++ hs-utility-ht/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/05/10 12:36:51 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/03/04 08:22:29 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-utility-ht
 
 .if !defined(HS_UTILITY_HT_BUILDLINK3_MK)
 HS_UTILITY_HT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-utility-ht+=	hs-utility-ht>=0.0
+BUILDLINK_API_DEPENDS.hs-utility-ht+=	hs-utility-ht>=0.0.8
 BUILDLINK_PKGSRCDIR.hs-utility-ht?=	../../wip/hs-utility-ht
 .endif	# HS_UTILITY_HT_BUILDLINK3_MK
 
