dbb6e6917be9e464c9d1e1c6d5143de5412d5749
Fredrik Pettai <pettai@NetBSD.org>
2012-03-07 23:01:05 +0000

    Version 2.7 (released 2011-11-23)
    * Verify server signatures per default when a key is provided. The old
    behavior can be restored with ykclient_set_verify_signature (ykc, 0);
    
    * Return YKCLIENT_BAD_SERVER_SIGNATURE on missing signature.
    
    Version 2.6 (released 2011-06-06)
    
    * Use fprintf+exit instead of glibc-ism errx.
    
    * Indent code and replace // comments with /* */ comments (for C89).
    
    * Fix dates of releases for 2.4 and 2.5 below (2011 not 2010).
    
    Version 2.5 (released 2011-05-31)
    
    * Implement almost all of Validation protocol 2.0.
    The part still known to be missing is parallell querys to all validation
    servers using curl-multi.
    
    Version 2.4 (released 2011-03-02)
    
    * Add possibility to supply validation service URL.
    
    * Make Validation protocol 2.0 servers not reject our requests.
    They would reject our requests because they did not have a nonce
    parameter.
    
    * Add new API ykclient_set_ca_path to set a trusted CA certificates path.
    
    * Fix issue with insecure rpath in the ykclient executable.
    
    * Documentation converted to asciidoc after move to Github.

--- ykclient/buildlink3.mk
+++ ykclient/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/04/06 06:00:13 marttikuparinen Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/07 23:01:05 pettai Exp $
 
 BUILDLINK_TREE+=	ykclient
 
 .if !defined(YKCLIENT_BUILDLINK3_MK)
 YKCLIENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ykclient+=	ykclient>=2.3
+BUILDLINK_API_DEPENDS.ykclient+=	ykclient>=2.7
 BUILDLINK_PKGSRCDIR.ykclient?=	../../wip/ykclient
 
 .include "../../www/curl/buildlink3.mk"
