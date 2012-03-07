583074aecf9652298bf26e409b02d5781f1278b5
Fredrik Pettai <pettai@NetBSD.org>
2012-03-07 22:49:41 +0000

    Version 1.7 (released 2010-11-24)
    * yubikey.h: Possible to use from C++ using extern namespace scoping.
    
    Version 1.6 (released 2010-08-03)
    
    * New API to generate OTPs.
    The API is yubikey_generate.  An helper function yubikey_aes_encrypt
    was added too.  Added YUBIKEY_OTP_SIZE constant for OTP buffer size.
    
    * ykgenerate: New tool to generate OTPs.
    
    * ykdebug/ykparse: The old tool "ykdebug" has been renamed to "ykparse".

--- libyubikey/buildlink3.mk
+++ libyubikey/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/03/26 13:25:36 marttikuparinen Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/07 22:49:41 pettai Exp $
 
 BUILDLINK_TREE+=	libyubikey
 
 .if !defined(LIBYUBIKEY_BUILDLINK3_MK)
 LIBYUBIKEY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libyubikey+=	libyubikey>=1.5
+BUILDLINK_API_DEPENDS.libyubikey+=	libyubikey>=1.7
 BUILDLINK_PKGSRCDIR.libyubikey?=	../../wip/libyubikey
 .endif	# LIBYUBIKEY_BUILDLINK3_MK
 
