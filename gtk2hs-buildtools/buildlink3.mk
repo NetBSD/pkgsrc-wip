11ab5329eec9722fb83f24f556cf9b8678c19691
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-11 07:30:30 +0000

    Upstream update to gtk2hs-buildtools-0.12.3

--- gtk2hs-buildtools/buildlink3.mk
+++ gtk2hs-buildtools/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/07/12 08:53:30 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/03/11 07:30:30 phonohawk Exp $
 
 BUILDLINK_TREE+=	gtk2hs-buildtools
 
@@ -6,8 +6,10 @@ BUILDLINK_TREE+=	gtk2hs-buildtools
 GTK2HS_BUILDTOOLS_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.gtk2hs-buildtools?=	build
-BUILDLINK_API_DEPENDS.gtk2hs-buildtools+=	gtk2hs-buildtools>=0.12
+BUILDLINK_API_DEPENDS.gtk2hs-buildtools+=	gtk2hs-buildtools>=0.12.3
 BUILDLINK_PKGSRCDIR.gtk2hs-buildtools?=	../../wip/gtk2hs-buildtools
+
+.include "../../wip/hs-random/buildlink3.mk"
 .endif	# GTK2HS_BUILDTOOLS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gtk2hs-buildtools
