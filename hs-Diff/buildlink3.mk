3fec89f4538b4bb1e226b001a5a4a5746e84d308
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-11 05:59:00 +0000

    Upstream update to Diff-0.1.3

--- hs-Diff/buildlink3.mk
+++ hs-Diff/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/08 09:13:08 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/11 05:59:00 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-Diff
 
 .if !defined(HS_DIFF_BUILDLINK3_MK)
 HS_DIFF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-Diff+=	hs-Diff>=0.1
+BUILDLINK_API_DEPENDS.hs-Diff+=	hs-Diff>=0.1.3
 BUILDLINK_PKGSRCDIR.hs-Diff?=	../../wip/hs-Diff
 .endif	# HS_DIFF_BUILDLINK3_MK
 
