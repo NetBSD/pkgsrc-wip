c3591325caa3a217af30e23c259e398faf421434
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-11 08:17:52 +0000

    Upstream update to gtk-0.12.3

--- hs-gtk/buildlink3.mk
+++ hs-gtk/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 13:54:38 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/11 08:17:52 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-gtk
 
 .if !defined(HS_GTK_BUILDLINK3_MK)
 HS_GTK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-gtk+=	hs-gtk>=0.12
+BUILDLINK_API_DEPENDS.hs-gtk+=	hs-gtk>=0.12.3
 BUILDLINK_PKGSRCDIR.hs-gtk?=	../../wip/hs-gtk
 
 .include "../../wip/hs-cairo/buildlink3.mk"
