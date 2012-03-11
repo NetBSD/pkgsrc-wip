5fd5cb0bf0b1d49acd498a48db4b6ef17c79cc30
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-11 06:44:55 +0000

    Upstream update to hint-0.3.3.4

--- hs-hint/buildlink3.mk
+++ hs-hint/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 04:15:47 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/11 06:44:55 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-hint
 
 .if !defined(HS_HINT_BUILDLINK3_MK)
 HS_HINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hint+=	hs-hint>=0.3
+BUILDLINK_API_DEPENDS.hs-hint+=	hs-hint>=0.3.3
 BUILDLINK_PKGSRCDIR.hs-hint?=	../../wip/hs-hint
 
 .include "../../wip/hs-MonadCatchIO-mtl/buildlink3.mk"
@@ -13,6 +13,7 @@ BUILDLINK_PKGSRCDIR.hs-hint?=	../../wip/hs-hint
 .include "../../wip/hs-ghc-paths/buildlink3.mk"
 .include "../../wip/hs-haskell-src/buildlink3.mk"
 .include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-random/buildlink3.mk"
 .include "../../wip/hs-utf8-string/buildlink3.mk"
 .endif	# HS_HINT_BUILDLINK3_MK
 
