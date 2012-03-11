616b2815a4812e371faab26967e59c66a1009d82
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-11 07:39:34 +0000

    Upstream update to glib-0.12.3

--- hs-glib/buildlink3.mk
+++ hs-glib/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/07/12 09:58:49 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/03/11 07:39:34 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-glib
 
 .if !defined(HS_GLIB_BUILDLINK3_MK)
 HS_GLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-glib+=	hs-glib>=0.12
+BUILDLINK_API_DEPENDS.hs-glib+=	hs-glib>=0.12.3
 BUILDLINK_PKGSRCDIR.hs-glib?=	../../wip/hs-glib
 
 .include "../../devel/glib2/buildlink3.mk"
