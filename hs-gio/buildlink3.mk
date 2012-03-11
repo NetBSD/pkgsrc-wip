53463a7f1ac69eee94c1d42d7cb634f22cef79d7
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-11 07:54:16 +0000

    Upstream update to gio-0.12.3

--- hs-gio/buildlink3.mk
+++ hs-gio/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 13:00:06 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/11 07:54:16 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-gio
 
 .if !defined(HS_GIO_BUILDLINK3_MK)
 HS_GIO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-gio+=	hs-gio>=0.12
+BUILDLINK_API_DEPENDS.hs-gio+=	hs-gio>=0.12.3
 BUILDLINK_PKGSRCDIR.hs-gio?=	../../wip/hs-gio
 
 .include "../../wip/hs-glib/buildlink3.mk"
