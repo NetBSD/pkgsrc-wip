3b28d0105018db3a20918348c4948e241db808d7
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-11 06:06:25 +0000

    Upstream update to ghc-mtl-1.0.1.1

--- hs-ghc-mtl/buildlink3.mk
+++ hs-ghc-mtl/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 04:06:28 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/11 06:06:25 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-ghc-mtl
 
 .if !defined(HS_GHC_MTL_BUILDLINK3_MK)
 HS_GHC_MTL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ghc-mtl+=	hs-ghc-mtl>=1.0
+BUILDLINK_API_DEPENDS.hs-ghc-mtl+=	hs-ghc-mtl>=1.0.1
 BUILDLINK_PKGSRCDIR.hs-ghc-mtl?=	../../wip/hs-ghc-mtl
 
 .include "../../wip/hs-MonadCatchIO-mtl/buildlink3.mk"
