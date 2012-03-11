b219c264c97b6f9b2db9541f46fd90cd7e9bdb54
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-11 06:03:11 +0000

    Upstream update to MonadCatchIO-mtl-0.3.0.4

--- hs-MonadCatchIO-mtl/buildlink3.mk
+++ hs-MonadCatchIO-mtl/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 03:58:57 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/11 06:03:11 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-MonadCatchIO-mtl
 
 .if !defined(HS_MONADCATCHIO_MTL_BUILDLINK3_MK)
 HS_MONADCATCHIO_MTL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-MonadCatchIO-mtl+=	hs-MonadCatchIO-mtl>=0.3
+BUILDLINK_API_DEPENDS.hs-MonadCatchIO-mtl+=	hs-MonadCatchIO-mtl>=0.3.0
 BUILDLINK_PKGSRCDIR.hs-MonadCatchIO-mtl?=	../../wip/hs-MonadCatchIO-mtl
 
 .include "../../wip/hs-mtl/buildlink3.mk"
