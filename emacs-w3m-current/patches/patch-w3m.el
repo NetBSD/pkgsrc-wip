a4f14c397c1aa9f14e309b5e43b2a2c6b6b8bb33
Makoto Fujiwara <makoto@ki.nu>
2012-03-11 02:18:22 +0000

    (1) Makefile     emacs23 was missing on the list of EMACS_VERSION_ACCEPTED. (2) patches     Merge two patches to one, to-be-patched files were the same.     Is it right to merge ? (3) TODO     Tested on Emacs 23 as well.

--- emacs-w3m-current/patches/patch-w3m.el
+++ emacs-w3m-current/patches/patch-w3m.el
@@ -1,6 +1,9 @@
-$NetBSD: patch-w3m.el,v 1.2 2012/03/02 23:52:39 makoto Exp $
+$NetBSD: patch-w3m.el,v 1.3 2012/03/11 02:18:22 makoto Exp $
 
-Use an absolute path in invoking w3m.
+(1) Use an absolute path in invoking w3m.
+(2) The Makefile in www/emacs-w3m puts icon directory to 
+   share/emacs/etc/${EMACS_MAJOR_VERSION} while elisp code may not find
+   the location. This adjusts that problem.
 
 --- w3m.el.orig	2011-09-07 23:15:48.000000000 +0000
 +++ w3m.el
@@ -20,3 +23,14 @@ Use an absolute path in invoking w3m.
  		       (w3m-which-command "w3mmee")
  		       (w3m-which-command "w3m-m17n"))))
        (when command
+@@ -828,6 +828,10 @@
+ 		     (throw 'found-dir dir))
+ 		 (if (file-directory-p
+ 		      (setq dir
++			    (concat (expand-file-name "../../etc/w3m/" path) (number-to-string emacs-major-version ) "/")))
++		     (throw 'found-dir dir))
++		 (if (file-directory-p
++		      (setq dir
+ 			    (expand-file-name "../etc/w3m/icons/" path)))
+ 		     (throw 'found-dir dir)))))))
+      (and (fboundp 'locate-data-directory)
