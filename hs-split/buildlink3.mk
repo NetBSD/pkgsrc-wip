a79449c61b3282f32a2e5d69d61cb7e8a30ccca2
PHO / phonohawk <phonohawk@users.sourceforge.net>
2012-03-11 07:00:57 +0000

    Upstream update to split-0.1.4.2

--- hs-split/buildlink3.mk
+++ hs-split/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 08:15:51 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/11 07:00:57 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-split
 
 .if !defined(HS_SPLIT_BUILDLINK3_MK)
 HS_SPLIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.1
+BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.1.4
 BUILDLINK_PKGSRCDIR.hs-split?=	../../wip/hs-split
 .endif	# HS_SPLIT_BUILDLINK3_MK
 
