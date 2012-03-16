d9595bb58792efa694c5d07a66c4691676f68160
Thomas Klausner <wiz@NetBSD.org>
2012-03-16 11:47:22 +0000

    Depend on current version.

--- libexttextcat/buildlink3.mk
+++ libexttextcat/buildlink3.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink3.mk,v 1.1 2011/10/16 21:24:05 ftigeot Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/16 11:47:22 thomasklausner Exp $
 
 BUILDLINK_TREE+=	libexttextcat
 
 .if !defined(LIBEXTTEXTCAT_BUILDLINK3_MK)
 LIBEXTTEXTCAT_BUILDLINK3_MK:=
-BUILDLINK_API_DEPENDS.libexttextcat+=	libexttextcat>=2.2
+BUILDLINK_API_DEPENDS.libexttextcat+=	libexttextcat>=3.2
 BUILDLINK_PKGSRCDIR.libexttextcat?=	../../wip/libexttextcat
 .endif
 
