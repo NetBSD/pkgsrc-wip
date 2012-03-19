1e11df8f44a18083a67939a2fd0ddb85c7a80902
Mike M. Volokhov <bubuchka@users.sourceforge.net>
2012-03-19 00:24:20 +0000

    Request declared leveldb version. Don't know how to handle git though.

--- leveldb/buildlink3.mk
+++ leveldb/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/03/18 20:57:50 bubuchka Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/19 00:24:20 bubuchka Exp $
 
 BUILDLINK_TREE+=	leveldb
 
 .if !defined(LEVELDB_BUILDLINK3_MK)
 LEVELDB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.leveldb+=	leveldb-[0-9]*
+BUILDLINK_API_DEPENDS.leveldb+=	leveldb>=1.2
 BUILDLINK_PKGSRCDIR.leveldb?=	../../wip/leveldb
 .endif	# LEVELDB_BUILDLINK3_MK
 
