f20c131b6163d2297da07a56dc5cef6f00b89786
Jason Bacon <jwbacon@tds.net>
2012-03-27 17:11:23 +0000

    Fix buildlink.

--- ufc/buildlink3.mk
+++ ufc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/27 16:00:57 outpaddling Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/03/27 17:11:23 outpaddling Exp $
 
 BUILDLINK_TREE+=	ufc
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ufc
 UFC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ufc+=	ufc>=2.0.5
-BUILDLINK_PKGSRCDIR.ufc?=	../../jb-wip/ufc
+BUILDLINK_PKGSRCDIR.ufc?=	../../wip/ufc
 .endif	# UFC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ufc
