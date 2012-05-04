331dd61a72e36ae65f5444b96c450e25470147e8
ndb <noud4@users.sourceforge.net>
2012-05-04 15:38:34 +0000

    PKG_SUGGESTED_OPTIONS=lua (so this builds again).

--- desmume/options.mk
+++ desmume/options.mk
@@ -1,16 +1,16 @@
-# $NetBSD: options.mk,v 1.1 2012/04/20 08:16:54 othyro Exp $
+# $NetBSD: options.mk,v 1.2 2012/05/04 15:38:34 noud4 Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.desmume
 PKG_SUPPORTED_OPTIONS=	lua openal
-PKG_SUGGESTED_OPTIONS=	# blank
+PKG_SUGGESTED_OPTIONS=	lua
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mlua)
 .include "../../lang/lua/buildlink3.mk"
-#CFLAGS+=		-DHAVE_LUA=1 # needs lua-engine.h ?
+#CFLAGS+=		-DHAVE_LUA=1
 .else
-CFLAGS+=		-DHAVE_LUA=0
+CFLAGS+=		-DHAVE_LUA=0	# XXX needs lua-engine.h ?
 .endif
 
 .if !empty(PKG_OPTIONS:Mopenal)
