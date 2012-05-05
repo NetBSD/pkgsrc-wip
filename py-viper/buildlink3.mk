9d44c874f5c1fe31653c241e00c486514d7aa56e
ndb <noud4@users.sourceforge.net>
2012-05-05 14:49:34 +0000

    Correct wip path.

--- py-viper/buildlink3.mk
+++ py-viper/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/27 15:56:16 outpaddling Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/05/05 14:49:34 noud4 Exp $
 
 BUILDLINK_TREE+=	py26-viper
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	py26-viper
 PY26_VIPER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.py26-viper+=	py26-viper>=1.0.0
-BUILDLINK_PKGSRCDIR.py26-viper?=	../../jb-wip/py-viper
+BUILDLINK_PKGSRCDIR.py26-viper?=	../../wip/py-viper
 .endif	# PY26_VIPER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-py26-viper
