08d9f24dd13bd734b9c77c469a43be049fe7db69
Filip Hajny <filip@joyent.com>
2012-05-11 09:14:39 +0000

    Updated Passenger to 3.0.12.
    Changes:
    
    - [Apache] Support Apache 2.4. The event MPM is now also supported.
    - [Nginx] Preferred Nginx version upgraded to 1.0.15.
    - [Nginx] Preferred PCRE version upgraded to 8.30.
    - [Nginx] Fixed compatibility with Nginx < 1.0.10.
    - [Nginx] Nginx is now installed with http_gzip_static_module by default.
    - [Nginx] Fixed a memory disclosure security problem.
    - [Nginx] passenger_show_version_in_header now hides the Phusion Passenger
      version number from the ‘Server:’ header too.
    - Fixed a /proc deprecation warning on Linux kernel >= 3.0.

---
+++ ap2-passenger/patches/patch-ext__boost__config__stdlib__libstdcpp3.hpp
@@ -0,0 +1,16 @@
+$NetBSD: patch-ext__boost__config__stdlib__libstdcpp3.hpp,v 1.1 2012/05/11 09:14:40 fhajny Exp $
+
+Fix for boost threads and GCC 4.7, as per:
+https://svn.boost.org/trac/boost/ticket/6165
+--- ext/boost/config/stdlib/libstdcpp3.hpp.orig	2012-05-11 08:21:03.384282290 +0000
++++ ext/boost/config/stdlib/libstdcpp3.hpp
+@@ -31,7 +31,8 @@
+ 
+ #ifdef __GLIBCXX__ // gcc 3.4 and greater:
+ #  if defined(_GLIBCXX_HAVE_GTHR_DEFAULT) \
+-        || defined(_GLIBCXX__PTHREADS)
++        || defined(_GLIBCXX__PTHREADS) \
++        || defined(_GLIBCXX_HAS_GTHREADS)
+       //
+       // If the std lib has thread support turned on, then turn it on in Boost
+       // as well.  We do this because some gcc-3.4 std lib headers define _REENTANT
--- ap2-passenger/patches/patch-ext__common__EventedMessageServer.h
+++ ap2-passenger/patches/patch-ext__common__EventedMessageServer.h
@@ -1,14 +1,14 @@
-$NetBSD: patch-ext__common__EventedMessageServer.h,v 1.1 2011/11/30 11:10:31 fhajny Exp $
+$NetBSD: patch-ext__common__EventedMessageServer.h,v 1.2 2012/05/11 09:14:40 fhajny Exp $
 
-Typo in macro.
+Explicitly error out if HAS_ALLOCA_H_ not defined.
 --- ext/common/EventedMessageServer.h.orig	2011-11-28 11:51:11.000000000 +0000
 +++ ext/common/EventedMessageServer.h
-@@ -29,7 +29,7 @@
- #include <ev++.h>
- #include <cstdarg>
+@@ -31,6 +31,8 @@
  #include <cstdlib>
--#ifdef HAS_ALLOCA_H_
-+#ifdef HAS_ALLOCA_H
+ #ifdef HAS_ALLOCA_H_
  	#include <alloca.h>
++#else
++	#error "alloca is required."
  #endif
  #include "EventedServer.h"
+ #include "MessageReadersWriters.h"
