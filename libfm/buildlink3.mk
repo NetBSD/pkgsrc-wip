132346b9510349b977c37a5e287e143a5b8a18ef
Jeremy C. Reed <reed@reedmedia.net>
2012-05-15 03:23:27 +0000

    this is in wip

--- libfm/buildlink3.mk
+++ libfm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/05/15 03:20:47 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/05/15 03:23:27 jeremy-c-reed Exp $
 
 BUILDLINK_TREE+=	libfm
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libfm
 LIBFM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libfm+=	libfm>=0.1.15
-BUILDLINK_PKGSRCDIR.libfm?=	../../new/libfm
+BUILDLINK_PKGSRCDIR.libfm?=	../../wip/libfm
 
 #.include "../../devel/glib2/buildlink3.mk"
 .endif	# LIBFM_BUILDLINK3_MK
