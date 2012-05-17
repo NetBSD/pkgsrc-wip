ad59f9a7c9d5d1f9ca73b14d1383184e457e4ee0
Hauke Fath <hauke@NetBSD.org>
2012-05-17 16:36:23 +0000

    Update to symon 2.85 --
    18/02/2012 - 2.85
    
       - lib/diskname rewrite -- return possible disknames for Linux and FreeBSD
         based on a number of predefined path patterns
    
       - platform/Linux allow /dev/mapper disknames (Edho Arief)
    
    14/01/2012 - 2.84
    
       - platform/Linux/disk probes now handles '/' in arguments (Bostjan Skufca)
    
       - platform/FreeBSD/disk probes now allow args not in '/dev' (Edho Arief),
         and support diskname by ufs name, ufs id, gpt id.
    
       - platform/FreeBSD/sm_mem.c: sysctl call could overwrite swap count (Edho
         Arief)
    
       - platform/NetBSD/Makefile.inc: update linker flags for finding librrd in
         /usr/pkg
    
    01/05/2011 - 2.83
    
       - Added flukso probe
    
       - platform/OpenBSD/sm_io.c updated to support uids (Stuart Henderson)
    
       - platform/Linux/sm_df.c checks if /etc/mtab could be openend
         (Lars Kotthoff)
    
       - platform/Linux: Support diskname by id, label, path or uuid for smart, io
         and df probes.

--- symon/patches/patch-platform_NetBSD_sm_smart.c
+++ symon/patches/patch-platform_NetBSD_sm_smart.c
@@ -1,10 +1,8 @@
-$NetBSD: patch-platform_NetBSD_sm_smart.c,v 1.1 2011/04/18 14:51:27 hfath Exp $
+$NetBSD: patch-platform_NetBSD_sm_smart.c,v 1.2 2012/05/17 16:36:23 hfath Exp $
 
-Add missing struct field
-
---- platform/NetBSD/sm_smart.c.orig	2010-01-23 20:56:31.000000000 +0000
+--- platform/NetBSD/sm_smart.c.orig	2012-01-14 14:05:06.000000000 +0000
 +++ platform/NetBSD/sm_smart.c
-@@ -63,11 +63,12 @@ static struct atareq smart_cmd = {
+@@ -64,11 +64,12 @@ static struct atareq smart_cmd = {
  };
  
  /* per drive storage structure */
@@ -16,5 +14,5 @@ Add missing struct field
      int failed;
 +    struct atareq cmd;
      struct smart_values data;
- };
- 
+ } *smart_devs = NULL;
+ static int smart_size = 0;
