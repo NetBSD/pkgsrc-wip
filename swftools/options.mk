7c09f4d258039eec08533ec08da8b6f2c793b399
OBATA Akio <obache@users.sourceforge.net>
2012-05-25 14:19:26 +0000

    AVI2SWF is not included anymore in the latest version.

--- swftools/options.mk
+++ swftools/options.mk
@@ -1,20 +1,13 @@
-# $NetBSD: options.mk,v 1.1 2010/01/01 23:08:05 thomasklausner Exp $
+# $NetBSD: options.mk,v 1.2 2012/05/25 14:19:26 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.swftools
 PKG_SUPPORTED_OPTIONS=	lame
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	avifile
-
 .if !empty(PKG_OPTIONS:Mlame)
 CONFIGURE_ARGS+=	--enable-lame
 .include "../../audio/lame/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-lame
-.endif
-
-.if !empty(PKG_OPTIONS:Mavifile)
-PLIST.avifile=		yes
-.include "../../wip/avifile-devel/buildlink3.mk"
 .endif
