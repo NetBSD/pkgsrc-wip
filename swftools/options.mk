13a50805415e891843e2faaab45630d0b8ce7eb3
OBATA Akio <obache@users.sourceforge.net>
2012-05-26 09:39:28 +0000

    fix detection of lame.

--- swftools/options.mk
+++ swftools/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2012/05/25 14:19:26 obache Exp $
+# $NetBSD: options.mk,v 1.3 2012/05/26 09:39:28 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.swftools
 PKG_SUPPORTED_OPTIONS=	lame
@@ -7,6 +7,7 @@ PKG_SUPPORTED_OPTIONS=	lame
 
 .if !empty(PKG_OPTIONS:Mlame)
 CONFIGURE_ARGS+=	--enable-lame
+BUILDLINK_INCDIRS.lame=	include/lame
 .include "../../audio/lame/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-lame
