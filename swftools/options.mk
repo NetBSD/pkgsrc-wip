4e19c379fec705e8b90a037f53f6d3d0116acaf0
OBATA Akio <obache@users.sourceforge.net>
2012-05-26 10:18:20 +0000

    Add pdf support option.

--- swftools/options.mk
+++ swftools/options.mk
@@ -1,9 +1,11 @@
-# $NetBSD: options.mk,v 1.3 2012/05/26 09:39:28 obache Exp $
+# $NetBSD: options.mk,v 1.4 2012/05/26 10:18:20 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.swftools
-PKG_SUPPORTED_OPTIONS=	lame
+PKG_SUPPORTED_OPTIONS=	lame pdf
 
 .include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	pdf
 
 .if !empty(PKG_OPTIONS:Mlame)
 CONFIGURE_ARGS+=	--enable-lame
@@ -11,4 +13,11 @@ BUILDLINK_INCDIRS.lame=	include/lame
 .include "../../audio/lame/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-lame
+.endif
+
+.if !empty(PKG_OPTIONS:Mpdf)
+.include "../../print/pdflib-lite/buildlink3.mk"
+PLIST.pdf=	yes
+.else
+CONFIGURE_ENV+=		DISABLEPDF2PDF=true
 .endif
