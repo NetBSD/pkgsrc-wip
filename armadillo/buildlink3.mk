e545f2caa28a9f49f5895946aca576278b34a480
Adam Ciarciński <adam@NetBSD.org>
2012-07-02 13:37:54 +0000

    Revbump after updating boost

--- armadillo/buildlink3.mk
+++ armadillo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/03/23 02:25:57 outpaddling Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/07/02 13:37:54 moubctez Exp $
 
 BUILDLINK_TREE+=	armadillo
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	armadillo
 ARMADILLO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.armadillo+=	armadillo>=2.2.1
+BUILDLINK_ABI_DEPENDS.armadillo?=	armadillo>=2.2.1nb1
 BUILDLINK_PKGSRCDIR.armadillo?=	../../wip/armadillo
 
 .include "../../math/blas/buildlink3.mk"
--- cgal/buildlink3.mk
+++ cgal/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/10/28 14:11:15 hfath Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/07/02 13:37:55 moubctez Exp $
 
 BUILDLINK_TREE+=	cgal
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	cgal
 CGAL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cgal+=	cgal>=3.7
+BUILDLINK_ABI_DEPENDS.cgal?=	cgal>=3.7nb1
 BUILDLINK_PKGSRCDIR.cgal?=	../../wip/cgal
 
 .include "../../graphics/glu/buildlink3.mk"
