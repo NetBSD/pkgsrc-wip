7f5da62dbeae2874a72045b095f0caf97c29c94c
Vitaly Shevtsov <lokivit@users.sourceforge.net>
2012-07-03 06:35:37 +0000

    Update to 0.16.999.55225. More recent version of E17 requires more recent version of EFL.

--- e17/buildlink3.mk
+++ e17/buildlink3.mk
@@ -1,18 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.12 2010/06/13 22:46:59 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2012/07/03 06:35:37 lokivit Exp $
 
 BUILDLINK_TREE+=	e17
 
 .if !defined(E17_BUILDLINK3_MK)
 E17_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.e17+=	e17>=0.16.999.063
-BUILDLINK_ABI_DEPENDS.e17?=	e17>=0.16.999.063
+BUILDLINK_API_DEPENDS.e17+=	e17>=0.16.999.55225
+BUILDLINK_ABI_DEPENDS.e17?=	e17>=0.16.999.55225
 BUILDLINK_PKGSRCDIR.e17?=	../../wip/e17
 
-.include "../../wip/ecore/buildlink3.mk"
-.include "../../wip/edje/buildlink3.mk"
+.include "../../devel/ecore/buildlink3.mk"
+.include "../../graphics/edje/buildlink3.mk"
 .include "../../devel/eet/buildlink3.mk"
-.include "../../wip/efreet/buildlink3.mk"
+.include "../../sysutils/efreet/buildlink3.mk"
 .include "../../graphics/evas/buildlink3.mk"
 .include "../../graphics/evas-buffer/buildlink3.mk"
 .include "../../graphics/imlib2/buildlink3.mk"
