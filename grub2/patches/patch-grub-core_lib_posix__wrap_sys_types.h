71833e3ee61798cf75c43518ae3a3e9f2f440657
Grégoire Sutre <gsutre@NetBSD.org>
2012-07-28 23:20:28 +0000

    Fix build failure occurring when compiling with lang/gcc44 (and only with the pkgsrc framework).

---
+++ grub2/patches/patch-grub-core_lib_posix__wrap_sys_types.h
@@ -0,0 +1,13 @@
+$NetBSD: patch-grub-core_lib_posix__wrap_sys_types.h,v 1.1 2012/07/28 23:20:28 gregoire Exp $
+
+--- grub-core/lib/posix_wrap/sys/types.h.orig	2012-05-28 15:52:37.000000000 +0000
++++ grub-core/lib/posix_wrap/sys/types.h
+@@ -21,7 +21,7 @@
+ 
+ #include <grub/misc.h>
+ 
+-#ifndef __APPLE__
++#if !(defined(__APPLE__) || defined(__NetBSD__))
+ typedef grub_size_t size_t;
+ #else
+ #include <stddef.h>
---
+++ grub2/patches/patch-grub-core_lib_posix__wrap_wchar.h
@@ -0,0 +1,16 @@
+$NetBSD: patch-grub-core_lib_posix__wrap_wchar.h,v 1.1 2012/07/28 23:20:28 gregoire Exp $
+
+--- grub-core/lib/posix_wrap/wchar.h.orig	2012-02-08 20:34:24.000000000 +0000
++++ grub-core/lib/posix_wrap/wchar.h
+@@ -29,7 +29,11 @@ enum
+   };
+ 
+ /* UCS-4.  */
++#if !defined(__NetBSD__)
+ typedef grub_int32_t wchar_t;
++#else
++#include <stddef.h>
++#endif
+ 
+ typedef struct mbstate {
+   grub_uint32_t code;
