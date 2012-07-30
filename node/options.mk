6f6c11f2c0754952936cd0b5116b433ce48e0c2a
Jonathan Perkin <jperkin@pkgsrc.org>
2012-07-30 14:20:27 +0000

    Use tabs.

--- node/options.mk
+++ node/options.mk
@@ -1,19 +1,19 @@
-# $NetBSD: options.mk,v 1.3 2012/06/15 11:33:28 fhajny Exp $
+# $NetBSD: options.mk,v 1.4 2012/07/30 14:20:27 jonperkin Exp $
 
-PKG_OPTIONS_VAR=       PKG_OPTIONS.node
-PKG_SUPPORTED_OPTIONS= openssl dtrace
-PKG_SUGGESTED_OPTIONS= openssl
+PKG_OPTIONS_VAR=	PKG_OPTIONS.node
+PKG_SUPPORTED_OPTIONS=	openssl dtrace
+PKG_SUGGESTED_OPTIONS=	openssl
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdtrace)
-CONFIGURE_ARGS+=       --with-dtrace
+CONFIGURE_ARGS+=	--with-dtrace
 .endif
 
 .if !empty(PKG_OPTIONS:Mopenssl)
 .include "../../security/openssl/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=       --without-openssl
+CONFIGURE_ARGS+=	--without-openssl
 .endif
 
 .if empty(PKG_OPTIONS:Msnapshot)
