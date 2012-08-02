2cdbe9afbfee6c5c8f29c9d939201af4f4a1f65c
Makoto Fujiwara <makoto@ki.nu>
2012-08-02 14:38:50 +0000

    Resolve inactivate-input-method to deactivate-input-method problem for 24.1.50

--- tc-svn/patches/patch-lisp_tc-sysdep.el
+++ tc-svn/patches/patch-lisp_tc-sysdep.el
@@ -1,11 +1,14 @@
-$NetBSD: patch-lisp_tc-sysdep.el,v 1.3 2012/07/30 13:50:31 makoto Exp $
+$NetBSD: patch-lisp_tc-sysdep.el,v 1.4 2012/08/02 14:38:50 makoto Exp $
+(1)
+ To work with emacs-24.2 or later
+ You will get following error if C-s tried for incremental search.
+ Symbol's function definition is void: isearch-last-command-char
+ 
+(2) 24.2 or later
+ convert inactivate-input-method to deactivate-input-method
 
-To work with emacs-22 or later
-You will get following error if C-s tried for incremental search.
-Symbol's function definition is void: isearch-last-command-char
-
---- lisp/tc-sysdep.el.orig	2012-07-30 22:24:44.000000000 +0900
-+++ lisp/tc-sysdep.el	2012-07-30 22:29:16.000000000 +0900
+--- lisp/tc-sysdep.el.orig	2012-08-02 16:43:08.000000000 +0900
++++ lisp/tc-sysdep.el	2012-08-02 23:29:10.000000000 +0900
 @@ -228,7 +228,7 @@
  ;;;
  ;;; Fix incompatibilities between 18 and 19.
@@ -24,3 +27,44 @@ Symbol's function definition is void: isearch-last-command-char
        (or (boundp 'search-upper-case)
  	  (setq search-upper-case 'not-yanks)))
    ;; NEmacs
+@@ -489,10 +489,10 @@
+ 	(setq slot (cons input-method info))
+ 	(setq input-method-alist (cons slot input-method-alist))))))
+ 
+-(unless (fboundp 'inactivate-input-method)
+-  (defun inactivate-input-method ()
+-    (if inactivate-current-input-method-function
+-	(funcall inactivate-current-input-method-function))
++(unless (fboundp 'deactivate-input-method)
++  (defun deactivate-input-method ()
++    (if deactivate-current-input-method-function
++	(funcall deactivate-current-input-method-function))
+     (setq current-input-method nil)))
+ 
+ (unless (fboundp 'activate-input-method)
+@@ -500,13 +500,13 @@
+   (make-variable-buffer-local 'default-input-method)
+   (defvar current-input-method nil)
+   (make-variable-buffer-local 'current-input-method)
+-  (defvar inactivate-current-input-method-function nil)
+-  (make-variable-buffer-local 'inactivate-current-input-method-function)
++  (defvar deactivate-current-input-method-function nil)
++  (make-variable-buffer-local 'deactivate-current-input-method-function)
+ 
+   (defun activate-input-method (input-method)
+     (if (and current-input-method
+ 	     (not (string= current-input-method input-method)))
+-	(inactivate-input-method))
++	(deactivate-input-method))
+     (let* ((slot (assoc input-method input-method-alist)))
+       (apply (nth 2 slot) input-method (nthcdr 5 slot))
+       (setq current-input-method input-method))))
+@@ -518,7 +518,7 @@
+   (defun toggle-input-method (&optional arg)
+     (interactive "P")
+     (if (and current-input-method (not arg))
+-	(inactivate-input-method)
++	(deactivate-input-method)
+       (if (null default-input-method)
+ 	  (let* ((completion-ignore-case t)
+ 		 (im (completing-read "Input method: " 
