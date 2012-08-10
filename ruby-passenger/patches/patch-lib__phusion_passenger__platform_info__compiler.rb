5635ff3a339e5c0061bdb10495b4c7c0fb9796b9
Filip Hajny <filip@joyent.com>
2012-08-10 11:37:23 +0000

    Update Passenger to 3.0.15
    Release 3.0.15
     * Updated documentation.
     * Updated website links.
    
    Release 3.0.14
     * [Apache] Fixed a long-standing mod_rewrite-related problem.
       Some mod_rewrite rules would not work, but it depends on the exact
       mod_rewrite configuration so it would work for some people but not
       for others. Issue #563. Thanks a lot to cedricmaion for providing
       information on the nature of the bug and to peter.nash55 for
       providing a VM that allowed us to reproduce the problem.
     * [Nginx] Preferred Nginx version to 1.2.2.
       The previously preferred version was 1.2.1.
     * Cleared some confusing terminology in the documentation.
     * Fixed some Ruby 1.9 encoding problems.
    
    Release 3.0.13
     * [Nginx] Preferred Nginx version upgraded to 1.2.1.
     * Fixed compilation problems on FreeBSD 6.4. Fixes issue #766.
     * Fixed compilation problems on GCC >= 4.6.
     * Fixed compilation problems on OpenIndiana and Solaris 11. Fixes issue #742.
     * Union Station-related bug fixes.
     * Sending the soft termination signal twice to application processes
       no longer makes them crash. Patch contributed by Ian Ehlert.

--- ruby-passenger/patches/patch-lib__phusion_passenger__platform_info__compiler.rb
+++ ruby-passenger/patches/patch-lib__phusion_passenger__platform_info__compiler.rb
@@ -1,31 +1,13 @@
-$NetBSD: patch-lib__phusion_passenger__platform_info__compiler.rb,v 1.1.1.1 2012/05/16 14:08:19 fhajny Exp $
+$NetBSD: patch-lib__phusion_passenger__platform_info__compiler.rb,v 1.2 2012/08/10 11:37:24 fhajny Exp $
 
 Fix compilation on recent SunOS platforms (OpenIndiana/SmartOS).
---- lib/phusion_passenger/platform_info/compiler.rb.orig	2011-11-28 11:51:11.000000000 +0000
+--- lib/phusion_passenger/platform_info/compiler.rb.orig	2012-08-01 11:31:04.000000000 +0000
 +++ lib/phusion_passenger/platform_info/compiler.rb
-@@ -143,9 +143,14 @@ module PlatformInfo
- 		
- 		if RUBY_PLATFORM =~ /solaris/
- 			flags << '-pthreads'
+@@ -150,7 +150,6 @@ module PlatformInfo
+ 				flags << '-D_XOPEN_SOURCE=500 -D_XPG4_2 -D__EXTENSIONS__ -D__SOLARIS__ -D_FILE_OFFSET_BITS=64'
+ 				flags << '-D__SOLARIS9__ -DBOOST__STDC_CONSTANT_MACROS_DEFINED' if RUBY_PLATFORM =~ /solaris2.9/
+ 			end
 -			flags << '-D_XOPEN_SOURCE=500 -D_XPG4_2 -D__EXTENSIONS__ -D__SOLARIS__ -D_FILE_OFFSET_BITS=64'
-+			if RUBY_PLATFORM =~ /solaris2.11/
-+				# skip the _XOPEN_SOURCE and _XPG4_2 definitions in later versions of Solaris / OpenIndiana
-+				flags << '-D__EXTENSIONS__ -D__SOLARIS__ -D_FILE_OFFSET_BITS=64'
-+			else
-+				flags << '-D_XOPEN_SOURCE=500 -D_XPG4_2 -D__EXTENSIONS__ -D__SOLARIS__ -D_FILE_OFFSET_BITS=64'
-+				flags << '-D__SOLARIS9__ -DBOOST__STDC_CONSTANT_MACROS_DEFINED' if RUBY_PLATFORM =~ /solaris2.9/
-+			end
  			flags << '-DBOOST_HAS_STDINT_H' unless RUBY_PLATFORM =~ /solaris2.9/
--			flags << '-D__SOLARIS9__ -DBOOST__STDC_CONSTANT_MACROS_DEFINED' if RUBY_PLATFORM =~ /solaris2.9/
  			flags << '-mcpu=ultrasparc' if RUBY_PLATFORM =~ /sparc/
  		elsif RUBY_PLATFORM =~ /openbsd/
- 			flags << '-DBOOST_HAS_STDINT_H -D_GLIBCPP__PTHREADS'
-@@ -158,7 +163,7 @@ module PlatformInfo
- 			flags << '-DBOOST_SP_USE_PTHREADS'
- 		end
- 		
--		flags << '-DHAS_ALLOCA_H' if has_alloca_h?
-+		flags << '-DHAS_ALLOCA_H_' if has_alloca_h?
- 		flags << '-DHAS_SFENCE' if supports_sfence_instruction?
- 		flags << '-DHAS_LFENCE' if supports_lfence_instruction?
- 		
