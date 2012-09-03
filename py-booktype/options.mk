0d88e4fed4a41f811bf5f50b17be8b41961919db
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2012-09-03 23:10:51 +0000

    Import py27-booktype-1.5 as wip/py-booktype.
    FLOSS Manuals collaborative book writing tool

---
+++ py-booktype/options.mk
@@ -0,0 +1,29 @@
+# $NetBSD: options.mk,v 1.1 2012/09/03 23:10:51 jihbed Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.booktype
+PKG_SUPPORTED_OPTIONS=	mysql pgsql sqlite3
+PKG_SUGGESTED_OPTIONS=	pgsql sqlite3
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	${PKG_SUPPORTED_OPTIONS}
+
+.if !empty(PKG_OPTIONS:Mmysql)
+DEPENDS+=	${PYPKGPREFIX}-mysqldb-[0-9]*:../../databases/py-mysqldb
+PLIST.mysql=	yes
+.endif
+
+.if !empty(PKG_OPTIONS:Moracle)
+DEPENDS+=	${PYPKGPREFIX}-cx_Oracle-[0-9]*:../../databases/py-cx_Oracle
+PLIST.oracle=	yes
+.endif
+
+.if !empty(PKG_OPTIONS:Mpgsql)
+DEPENDS+=	${PYPKGPREFIX}-psycopg2-[0-9]*:../../databases/py-psycopg2
+PLIST.pgsql=	yes
+.endif
+
+.if !empty(PKG_OPTIONS:Msqlite)
+DEPENDS+=	${PYPKGPREFIX}-sqlite2-[0-9]*:../../databases/py-sqlite3
+PLIST.sqlite=	yes
+.endif
