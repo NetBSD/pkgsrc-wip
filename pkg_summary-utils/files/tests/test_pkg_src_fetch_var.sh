4be0a946471dc1a36dc9515f3e65086db1547193
Aleksey Cheusov <cheusov@users.sourceforge.net>
2012-09-12 19:57:51 +0000

    pkg_src_fetch_var   - now uses power_getopt.awk   - option -v was removed, use equivalent -f instead!

--- pkg_summary-utils/files/tests/test_pkg_src_fetch_var.sh
+++ pkg_summary-utils/files/tests/test_pkg_src_fetch_var.sh
@@ -4,9 +4,3 @@ normalize_version |
 cmp 'pkg_src_fetch_var #1' \
 '+	xxkb-1.11	x11/xxkb	cheusov@NetBSD.org
 '
-
-echo x11/xxkb | pkg_src_fetch_var -v'PKGNAME PKGPATH MAINTAINER' |
-normalize_version |
-cmp 'pkg_src_fetch_var #2' \
-'+	xxkb-1.11	x11/xxkb	cheusov@NetBSD.org
-'
