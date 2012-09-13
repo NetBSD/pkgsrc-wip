75a630372973069cc8ad36c40b9a8b2a662ebaad
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2012-09-13 00:31:14 +0000

    forcing dependency

---
+++ argtable2/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/09/13 00:31:14 jihbed Exp $
+
+BUILDLINK_TREE+=	argtable2
+
+.if !defined(ARGTABLE2_BUILDLINK3_MK)
+ARGTABLE2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.argtable2+=	argtable2>=2.13
+BUILDLINK_PKGSRCDIR.argtable2?=	../../wip/argtable2
+.endif	# ARGTABLE2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-argtable2
