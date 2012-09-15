1e69b863d3fc708cce6b20e20bc5e80f3ba5b342
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2012-09-15 23:59:12 +0000

    Import dune-geometry-2.2.0 as wip/dune-geometry.
    includes everything related to the DUNE reference elements.
    This includes the reference elements themselves, mappings on
    the reference elements (geometries), and quadratures

---
+++ dune-geometry/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.1 2012/09/15 23:59:12 jihbed Exp $
+
+BUILDLINK_TREE+=	dune-geometry
+
+.if !defined(DUNE_GEOMETRY_BUILDLINK3_MK)
+DUNE_GEOMETRY_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.dune-geometry+=	dune-geometry>=2.2.0
+BUILDLINK_PKGSRCDIR.dune-geometry?=	../../wip/dune-geometry
+
+#.include "../../wip/dune-common/buildlink3.mk"
+#.include "../../devel/gmp/buildlink3.mk"
+#.include "../../devel/boost-headers/buildlink3.mk"
+#.include "../../math/blas/buildlink3.mk"
+.endif	# DUNE_GEOMETRY_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-dune-geometry
