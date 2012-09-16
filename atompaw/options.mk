aced5fabea11956ff20fea56cbb25ca5e15955c1
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2012-09-16 09:45:47 +0000

    Import atompaw-3.0.1.8 as wip/atompaw.
    Projector Augmented Wave code for electronic structure calculations

---
+++ atompaw/options.mk
@@ -0,0 +1,19 @@
+# $NetBSD: options.mk,v 1.1 2012/09/16 09:45:47 jihbed Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.atompaw
+PKG_SUPPORTED_OPTIONS=	libxc linalg
+PKG_SUGGESTED_OPTIONS=	libxc linalg
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mlibxc)
+CONFIGURE_ARGS+=	--with-libxc-incs
+CONFIGURE_ARGS+=	--with-libxc-libs
+.include "../../wip/libxc/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mlinalg)
+CONFIGURE_ARGS+=	--with-blas="-L${BUILDLINK_PREFIX.blas}/lib -lblas"
+.include "../../math/blas/buildlink3.mk"
+.include "../../math/lapack/buildlink3.mk"
+.endif
