b64a9e660325fd249d265f179a6aa5ce31286e97
Mike M. Volokhov <bubuchka@users.sourceforge.net>
2012-09-24 01:08:57 +0000

    Whoops, API was changed in between 1.2 -> 1.5.

--- leveldb/buildlink3.mk
+++ leveldb/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/03/19 00:24:20 bubuchka Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/09/24 01:08:57 bubuchka Exp $
 
 BUILDLINK_TREE+=	leveldb
 
 .if !defined(LEVELDB_BUILDLINK3_MK)
 LEVELDB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.leveldb+=	leveldb>=1.2
+BUILDLINK_API_DEPENDS.leveldb+=	leveldb>=1.5
 BUILDLINK_PKGSRCDIR.leveldb?=	../../wip/leveldb
 .endif	# LEVELDB_BUILDLINK3_MK
 
