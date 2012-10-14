84a7cd39d01c0b5bfd6b8a8495833ef4e3d80f21
Thomas Klausner <wiz@NetBSD.org>
2012-10-14 13:30:45 +0000

    Use lang/tcl instead of (older) wip/tcl85.

--- tk85-itk/buildlink3.mk
+++ tk85-itk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/10/14 13:26:56 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/10/14 13:30:45 thomasklausner Exp $
 
 BUILDLINK_TREE+=	tk85-itk
 
@@ -11,7 +11,7 @@ BUILDLINK_PKGSRCDIR.tk85-itk?=	../../wip/tk85-itk
 .include "../../x11/libXt/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../wip/tcl85-itcl/buildlink3.mk"
-.include "../../wip/tcl85/buildlink3.mk"
+.include "../../lang/tcl/buildlink3.mk"
 .include "../../x11/tk/buildlink3.mk"
 .endif	# TK85_ITK_BUILDLINK3_MK
 
