540dfee7577126e0af5136e431cb0891c73dd035
Aleksej Saushev <asau@users.sourceforge.net>
2012-10-15 19:21:45 +0000

    harminv moved into pkgsrc.

--- meep/buildlink3.mk
+++ meep/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/03/22 21:24:26 outpaddling Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/10/15 19:21:45 asau Exp $
 
 BUILDLINK_TREE+=	meep
 
@@ -8,7 +8,7 @@ MEEP_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.meep+=	meep>=1.1.1
 BUILDLINK_PKGSRCDIR.meep?=	../../wip/meep
 
-.include "../../wip/harminv/buildlink3.mk"
+.include "../../math/harminv/buildlink3.mk"
 .include "../../wip/libctl/buildlink3.mk"
 .include "../../devel/hdf5/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
