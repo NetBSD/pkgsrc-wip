3949f65849ee3f2e696963f5f6f887799e4e5962
Aleksej Saushev <asau@users.sourceforge.net>
2012-10-18 01:15:20 +0000

    Bump API version.

--- openmpi/buildlink3.mk
+++ openmpi/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:43:51 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/10/18 01:15:20 asau Exp $
 
 BUILDLINK_TREE+=	openmpi
 
 .if !defined(OPENMPI_BUILDLINK3_MK)
 OPENMPI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.openmpi+=	openmpi>=1.2.6
+BUILDLINK_API_DEPENDS.openmpi+=	openmpi>=1.5.1
 BUILDLINK_PKGSRCDIR.openmpi?=	../../wip/openmpi
 .endif # OPENMPI_BUILDLINK3_MK
 
