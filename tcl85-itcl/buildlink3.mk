1cb8d0e944ad744a60e0398fcd0cb99127676ae3
Blue Rats <rodent@NetBSD.org>
2012-10-22 21:34:57 +0000

    Removed wip/tcl-tclOO library linking.

--- tcl85-itcl/buildlink3.mk
+++ tcl85-itcl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/10/14 13:26:56 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/10/22 21:34:57 othyro Exp $
 
 BUILDLINK_TREE+=	tcl85-itcl
 
@@ -8,7 +8,6 @@ TCL85_ITCL_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.tcl85-itcl+=	tcl85-itcl>=3.4.1
 BUILDLINK_PKGSRCDIR.tcl85-itcl?=	../../wip/tcl85-itcl
 
-.include "../../wip/tcl-tclOO/buildlink3.mk"
 .include "../../x11/tk/buildlink3.mk"
 .endif	# TCL85_ITCL_BUILDLINK3_MK
 
