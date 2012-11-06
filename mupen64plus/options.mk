1ddb45f65f66a3be1af1c17ddf5303092436832c
Blue Rats <rodent@NetBSD.org>
2012-11-06 14:36:57 +0000

    Added debug option.

--- mupen64plus/options.mk
+++ mupen64plus/options.mk
@@ -1,10 +1,14 @@
-# $NetBSD: options.mk,v 1.1.1.1 2011/12/13 15:11:47 othyro Exp $
+# $NetBSD: options.mk,v 1.2 2012/11/06 14:36:57 othyro Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mupen64plus
-PKG_SUPPORTED_OPTIONS=	lirc samplerate
+PKG_SUPPORTED_OPTIONS=	debug lirc samplerate
 PKG_SUGGESTED_OPTIONS=	samplerate
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CFLAGS+=	-DDEBUG=1
+.endif
 
 .if !empty(PKG_OPTIONS:Mlirc)
 .include "../../wip/lirc/buildlink3.mk"
