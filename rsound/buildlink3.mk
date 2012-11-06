323185c6925f27eda980fb3fe26dd5a533f23e03
Blue Rats <rodent@NetBSD.org>
2012-11-06 14:45:00 +0000

    Added all PKG_BUILD_OPTIONS.

--- rsound/buildlink3.mk
+++ rsound/buildlink3.mk
@@ -1,9 +1,44 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/10/03 22:45:58 othyro Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/11/06 14:50:30 othyro Exp $
 
 BUILDLINK_TREE+=	rsound
 
 .if !defined(RSOUND_BUILDLINK3_MK)
 RSOUND_BUILDLINK3_MK:=
+
+pkgbase := rsound
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS:Malsa)
+.include "../../audio/alsa-lib/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mjack)
+.include "../../audio/jack/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mlibao)
+.include "../../audio/libao/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Msamplerate)
+.include "../../audio/libsamplerate/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mopenal)
+.include "../../audio/openal/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Moss)
+.include "../../mk/oss.buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mportaudio)
+.include "../../audio/portaudio/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mpulseaudio)
+.include "../../audio/pulseaudio/buildlink3.mk"
+.endif
 
 BUILDLINK_API_DEPENDS.rsound+=	rsound>=1.1
 BUILDLINK_PKGSRCDIR.rsound?=	../../wip/rsound
--- twinkle/buildlink3.mk
+++ twinkle/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2010/06/13 22:47:20 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2012/11/06 14:45:00 othyro Exp $
 
 BUILDLINK_TREE+=	twinkle
 
@@ -8,6 +8,25 @@ TWINKLE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.twinkle+=	twinkle>=1.1
 BUILDLINK_ABI_DEPENDS.twinkle?=	twinkle>=1.4.2nb4
 BUILDLINK_PKGSRCDIR.twinkle?=	../../wip/twinkle
+
+pkgbase := twinkle
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS:Milbc)
+.include "../../wip/ilbc-rfc3951/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mkde)
+.include "../../x11/kdelibs3/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mspeex)
+.include "../../audio/speex/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS:Mzrtp)
+.include "../../wip/libzrtpcpp/buildlink3.mk"
+.endif
 
 .include "../../x11/qt3-tools/buildlink3.mk"
 .include "../../x11/qt3-libs/buildlink3.mk"
