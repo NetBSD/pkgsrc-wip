180b37e6172fa51b145ab546fa4f9c67d37147e8
Blue Rats <rodent@NetBSD.org>
2012-11-11 13:36:11 +0000

    Removed commented line.

--- mupen64plus/options.mk
+++ mupen64plus/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2012/11/06 14:36:57 othyro Exp $
+# $NetBSD: options.mk,v 1.3 2012/11/11 13:36:11 othyro Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mupen64plus
 PKG_SUPPORTED_OPTIONS=	debug lirc samplerate
@@ -18,5 +18,4 @@ CFLAGS+=	-DLIRC=1
 .if !empty(PKG_OPTIONS:Msamplerate)
 .include "../../audio/libsamplerate/buildlink3.mk"
 .else
-#CONFIGURE_ARGS+=	--without-ilbc
 .endif
