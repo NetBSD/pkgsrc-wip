b148723c1b4c05b2a642d8674dcce5c139b204da
Jason Bacon <jwbacon@tds.net>
2012-11-11 22:24:02 +0000

    Conform to conventional naming of installed files

--- swig2/buildlink3.mk
+++ swig2/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/03/27 17:48:00 outpaddling Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/11/11 22:24:02 outpaddling Exp $
 
-BUILDLINK_TREE+=	swig
+BUILDLINK_TREE+=	swig2
 
 .if !defined(SWIG_BUILDLINK3_MK)
 SWIG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.swig+=	swig>=2.0.4
-BUILDLINK_PKGSRCDIR.swig?=	../../wip/swig2
+BUILDLINK_API_DEPENDS.swig2+=	swig>=2.0.8
+BUILDLINK_PKGSRCDIR.swig2?=	../../devel/swig2
 
 .include "../../devel/pcre/buildlink3.mk"
 .endif	# SWIG_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-swig
+BUILDLINK_TREE+=	-swig2
