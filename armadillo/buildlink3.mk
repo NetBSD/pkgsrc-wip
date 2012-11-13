9d4235a534596cec92cd03965ef68eda6cf6251c
Jason Bacon <jwbacon@tds.net>
2012-11-13 23:56:10 +0000

    Disable old patch from v2

--- armadillo/buildlink3.mk
+++ armadillo/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.3 2012/07/02 13:37:54 moubctez Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2012/11/13 23:56:10 outpaddling Exp $
 
 BUILDLINK_TREE+=	armadillo
 
 .if !defined(ARMADILLO_BUILDLINK3_MK)
 ARMADILLO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.armadillo+=	armadillo>=2.2.1
-BUILDLINK_ABI_DEPENDS.armadillo?=	armadillo>=2.2.1nb1
-BUILDLINK_PKGSRCDIR.armadillo?=	../../wip/armadillo
+BUILDLINK_API_DEPENDS.armadillo+=	armadillo>=3.4.4
+BUILDLINK_ABI_DEPENDS.armadillo?=	armadillo>=3.4.4
+BUILDLINK_PKGSRCDIR.armadillo?=	../../math/armadillo
 
 .include "../../math/blas/buildlink3.mk"
 .include "../../math/lapack/buildlink3.mk"
