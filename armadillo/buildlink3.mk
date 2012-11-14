246bee8e96e9b402a0337c64ca4ebbeb8bab4bad
Jason Bacon <jwbacon@tds.net>
2012-11-14 00:26:22 +0000

    Unbreak buildlink

--- armadillo/buildlink3.mk
+++ armadillo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2012/11/13 23:56:10 outpaddling Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2012/11/14 00:26:22 outpaddling Exp $
 
 BUILDLINK_TREE+=	armadillo
 
@@ -7,7 +7,7 @@ ARMADILLO_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.armadillo+=	armadillo>=3.4.4
 BUILDLINK_ABI_DEPENDS.armadillo?=	armadillo>=3.4.4
-BUILDLINK_PKGSRCDIR.armadillo?=	../../math/armadillo
+BUILDLINK_PKGSRCDIR.armadillo?=	../../wip/armadillo
 
 .include "../../math/blas/buildlink3.mk"
 .include "../../math/lapack/buildlink3.mk"
