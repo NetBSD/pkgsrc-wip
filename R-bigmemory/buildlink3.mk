724e570b1a67871a73c7532900ff6950954fafe9
Mike M. Volokhov <bubuchka@users.sourceforge.net>
2012-11-15 23:16:08 +0000

    Update R-bigmemory to version 4.3.0.
    Major changes unknown, but R-bigmemory.sri dependency introduced.

--- R-bigmemory/buildlink3.mk
+++ R-bigmemory/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2011/09/17 21:33:29 bubuchka Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/11/15 23:16:08 bubuchka Exp $
 
 BUILDLINK_TREE+=	Rbigmemory
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	Rbigmemory
 RBIGMEMORY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.Rbigmemory+=	R-bigmemory>=4.2.11
-BUILDLINK_ABI_DEPENDS.Rbigmemory+=	R-bigmemory>=4.2.11
+BUILDLINK_ABI_DEPENDS.Rbigmemory+=	R-bigmemory>=4.3.0
 BUILDLINK_PKGSRCDIR.Rbigmemory?=	../../wip/R-bigmemory
 .endif # RBIGMEMORY_BUILDLINK3_MK
 
