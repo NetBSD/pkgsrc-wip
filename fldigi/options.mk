d39d00d812d6e5d2b64316d0cc31ca3bc839b132
Greg Troxel <gdt@lexort.com>
2012-11-15 22:23:06 +0000

    With the deprecation of fltk 1.1, replace the fltk13 option with an fltk11 option, defaulting to off, so that fltk13 is the default.
    Try to make the build deterministic by not overriding the bl3 version
    rules.  Now, asking for fltk11 should fail if 1.3 is installed.

--- fldigi/options.mk
+++ fldigi/options.mk
@@ -1,18 +1,17 @@
-# $NetBSD: options.mk,v 1.2 2012/11/10 12:55:40 lexort Exp $
+# $NetBSD: options.mk,v 1.3 2012/11/15 22:23:06 lexort Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fldigi
-PKG_SUPPORTED_OPTIONS=	fltk13
-PKG_SUGGESTED_OPTIONS=	fltk13
+PKG_SUPPORTED_OPTIONS=	fltk11
+PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mfltk13)
+# Force building with requested version.
+.if !empty(PKG_OPTIONS:Mfltk11)
+# fltk11 option: use unmaintained version 1.1
+.include "../../x11/fltk/buildlink3.mk"
+.else
+# NOT fltk11 option: use version 1.3
 # With this option, UTF-8 functionalities are fully supported.
 .include "../../x11/fltk13/buildlink3.mk"
-.else
-.include "../../x11/fltk/buildlink3.mk"
-# above asks for <1.3, but this version is OK with =1.3
-# This means if fltk13 is already installed by some reason,
-# this package builds fine with that new version.
-BUILDLINK_API_DEPENDS.fltk+=	fltk>=1.1.5rc1
 .endif
