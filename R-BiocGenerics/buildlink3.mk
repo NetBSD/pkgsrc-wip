bc68b8915f1cc4a2c6a389f639dcdb93a65dbad1
Mike M. Volokhov <bubuchka@users.sourceforge.net>
2012-11-16 00:07:43 +0000

    Update R-BiocGenerics to 0.4.0. Changes unknown.

--- R-BiocGenerics/buildlink3.mk
+++ R-BiocGenerics/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/05/10 10:31:32 bubuchka Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/11/16 00:07:44 bubuchka Exp $
 
 BUILDLINK_TREE+=	RBiocGenerics
 
 .if !defined(RBIOCGENERICS_BUILDLINK3_MK)
 RBIOCGENERICS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.RBiocGenerics+=	R-BiocGenerics>=0.2.0
-BUILDLINK_ABI_DEPENDS.RBiocGenerics+=	R-BiocGenerics>=0.2.0
+BUILDLINK_API_DEPENDS.RBiocGenerics+=	R-BiocGenerics>=0.4.0
+BUILDLINK_ABI_DEPENDS.RBiocGenerics+=	R-BiocGenerics>=0.4.0
 BUILDLINK_PKGSRCDIR.RBiocGenerics?=	../../wip/R-BiocGenerics
 .endif # RBIOCGENERICS_BUILDLINK3_MK
 
