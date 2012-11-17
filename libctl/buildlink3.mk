d5f766906262f1ebba1453dd7715cd29f6221274
Jason Bacon <jwbacon@tds.net>
2012-11-17 18:31:46 +0000

    Clean up for commit

--- libctl/buildlink3.mk
+++ libctl/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2012/10/15 20:06:16 asau Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2012/11/17 18:31:46 outpaddling Exp $
 
 BUILDLINK_TREE+=	libctl
 
 .if !defined(LIBCTL_BUILDLINK3_MK)
 LIBCTL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libctl+=	libctl>=3.1
+BUILDLINK_API_DEPENDS.libctl+=	libctl>=3.2.1
 BUILDLINK_PKGSRCDIR.libctl?=	../../wip/libctl
 
 .include "../../lang/guile/buildlink3.mk"
