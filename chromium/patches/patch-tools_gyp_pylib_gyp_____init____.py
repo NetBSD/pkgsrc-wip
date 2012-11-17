7ba8dd4e08e0be41c35eaf86a1ffbd5e3a56445e
Gary D Duzan <gary@duzan.org>
2012-11-17 02:57:11 +0000

    Build on netbsd6 platform

--- chromium/patches/patch-tools_gyp_pylib_gyp_____init____.py
+++ chromium/patches/patch-tools_gyp_pylib_gyp_____init____.py
@@ -1,8 +1,8 @@
-$NetBSD: patch-tools_gyp_pylib_gyp_____init____.py,v 1.1 2011/04/28 03:09:03 rxg Exp $
+$NetBSD: patch-tools_gyp_pylib_gyp_____init____.py,v 1.2 2012/11/17 02:57:12 gduzan Exp $
 
 --- tools/gyp/pylib/gyp/__init__.py.orig	2011-04-13 08:13:20.000000000 +0000
 +++ tools/gyp/pylib/gyp/__init__.py
-@@ -313,9 +313,11 @@ def main(args):
+@@ -313,9 +313,12 @@ def main(args):
        options.formats = [ {'darwin':   'xcode',
                             'win32':    'msvs',
                             'cygwin':   'msvs',
@@ -11,6 +11,7 @@ $NetBSD: patch-tools_gyp_pylib_gyp_____init____.py,v 1.1 2011/04/28 03:09:03 rxg
                             'freebsd8': 'make',
                             'linux2':   'make',
 +                           'netbsd5':  'make',
++                           'netbsd6':  'make',
                             'openbsd4': 'make',
                             'sunos5':   'make',}[sys.platform] ]
  
