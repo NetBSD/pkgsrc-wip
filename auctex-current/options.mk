e1d626b561cda57d97304836265a666d84e7826a
Krister Walfridsson <kristerw@NetBSD.org>
2012-11-17 10:35:17 +0000

    Add version to DEPENDS.

--- auctex-current/options.mk
+++ auctex-current/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2010/10/13 13:06:29 makoto Exp $
+# $NetBSD: options.mk,v 1.2 2012/11/17 10:35:17 kristerw Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.auctex_current
 PKG_SUPPORTED_OPTIONS=	ja-ptex
@@ -7,8 +7,8 @@ PKG_SUGGESTED_OPTIONS=
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mja-ptex)
-DEPENDS+=		ja-ptex:../../print/ja-ptex
+DEPENDS+=		ja-ptex-[0-9]*:../../print/ja-ptex
 .else
-DEPENDS+=		teTeX:../../print/teTeX
+DEPENDS+=		teTeX-[0-9]*:../../print/teTeX
 # tex-latex-bin and tex-graphics ?
 .endif
