c205905bd5791d741915fabdb2d7e05990e4ddc4
Jason Bacon <jwbacon@tds.net>
2012-11-20 17:00:57 +0000

    Minor patch update

--- dolfin/patches/patch-cmake_modules-FindCHOLMOD.cmake
+++ dolfin/patches/patch-cmake_modules-FindCHOLMOD.cmake
@@ -1,4 +1,4 @@
-$NetBSD: patch-cmake_modules_FindCHOLMOD.cmake,v 1.2 2012/11/18 23:52:13 outpaddling Exp $
+$NetBSD: patch-cmake_modules-FindCHOLMOD.cmake,v 1.1 2012/11/20 17:00:57 outpaddling Exp $
 
 --- cmake/modules/FindCHOLMOD.cmake.orig	2011-12-07 21:21:22.000000000 +0000
 +++ cmake/modules/FindCHOLMOD.cmake
@@ -6,7 +6,7 @@ $NetBSD: patch-cmake_modules_FindCHOLMOD.cmake,v 1.2 2012/11/18 23:52:13 outpadd
  if (LAPACK_FOUND)
    set(CHOLMOD_LIBRARIES ${CHOLMOD_LIBRARIES} ${LAPACK_LIBRARIES})
  endif()
-+set(CHOLMOD_LIBRARIES ${CHOLMOD_LIBRARIES} $ENV{CHOLMOD_DIR}/lib/libsuitesparseconfig.a)
++set(CHOLMOD_LIBRARIES ${CHOLMOD_LIBRARIES} $ENV{LOCALBASE}/lib/libsuitesparseconfig.a)
  
  mark_as_advanced(
    CHOLMOD_INCLUDE_DIRS
