e27dc636b09511ddc3a6c6c7a09e650f28431ae1
Jeremy C. Reed <reed@reedmedia.net>
2012-11-24 02:25:05 +0000

    upgrade to 1.1.0
    set _NETBSD_SOURCE for fnmatch(3) FNM_CASEFOLD
    get rid of debugging configure flag
    
    add hack to buildlink3.mk to find the include headers
    
    no complete changes history to provide

--- libfm/buildlink3.mk
+++ libfm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/05/15 03:23:27 jeremy-c-reed Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2012/11/24 02:25:05 jeremy-c-reed Exp $
 
 BUILDLINK_TREE+=	libfm
 
@@ -9,6 +9,13 @@ BUILDLINK_API_DEPENDS.libfm+=	libfm>=0.1.15
 BUILDLINK_PKGSRCDIR.libfm?=	../../wip/libfm
 
 #.include "../../devel/glib2/buildlink3.mk"
+
+BUILDLINK_TARGETS+=	buildlink-include-libfm
+
+.PHONY: buildlink-include-libfm
+buildlink-include-libfm:
+	${LN} -s ${BUILDLINK_PREFIX.libfm}/include/libfm-1.0 ${BUILDLINK_DIR}/include/libfm
+
 .endif	# LIBFM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libfm
