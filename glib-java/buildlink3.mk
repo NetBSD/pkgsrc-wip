c9a1ce38280bfb7750ba66d24ba53a840a82f92e
Blue Rats <rodent@NetBSD.org>
2012-12-04 02:33:35 +0000

    Removed comments and included glib2.

--- glib-java/buildlink3.mk
+++ glib-java/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2009/05/06 09:27:04 cheusov Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.12.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.glib-java?=	build
+# $NetBSD: buildlink3.mk,v 1.9 2012/12/04 02:33:35 othyro Exp $
 
 BUILDLINK_TREE+=	glib-java
 
@@ -18,13 +8,7 @@ GLIB_JAVA_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.glib-java+=	glib-java>=0.2.5
 BUILDLINK_PKGSRCDIR.glib-java?=	../../wip/glib-java
 
-# XXX
-# XXX Uncomment and keep only the buildlink3 lines below which are directly
-# XXX needed for dependencies to compile, link, and run.  If this package
-# XXX provides a wrappered API or otherwise does not expose the APIs of the
-# XXX buildlink3 lines below to dependencies, remove them.
-# XXX
-#.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
 
 .endif # GLIB_JAVA_BUILDLINK3_MK
 
