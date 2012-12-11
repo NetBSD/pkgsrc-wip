fc8a84157f88cb43e97b8acbebf4065b506b8493
Petar Bogdanovic <petar@smokva.net>
2012-12-11 00:39:42 +0000

    Upgrade to 0.4.
    0.4: 2012-12-06
    
      BUGFIXES
    
      * Fixed possible format string reevaluation in log.c
      * Fixed handling of NULL host addresses
    
    
    0.3: 2012-11-26
    
      FEATURES
    
      * Moco now supports dumping all database tables
      * Counter_penpal uses greylist_src as MTA address
      * Log Message-ID in default logging
      * Consistent logging format
      * Added pipe action (EXPERIMENTAL)
      * Identify servers public suffix used for greylisting (Petar Bogdanovic)
      * Support LDFLAGS #7 (Petar Bogdanovic)
    
    
      BUGFIXES
    
      * Update bug in counter.c
      * Bad message size bug
      * Ignored cf_default_deadline #10
      * Typo in sht.c #8 (Petar Bogdanovic)
    
    
    0.2: 2012-06-03
    
      FEATURES
    
      * New greylist logging format
      * Mopher control socket
      * Mopher control socket client moco (EXPERIMENTAL)
      * Improved Makefiles (Petar Bogdanovic)
    
    
      BUGFIXES
    
      * Garbeled remaining attempts #1
      * Bad spamd header format #3
      * ACL evaluation bug #5

--- mopher/files/mopherd.sh
+++ mopher/files/mopherd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: mopherd.sh,v 1.1.1.1 2012/05/12 13:29:06 pebog Exp $
+# $NetBSD: mopherd.sh,v 1.2 2012/12/11 00:39:45 pebog Exp $
 #
 # PROVIDE: mopherd
 # REQUIRE: DAEMON
@@ -9,10 +9,16 @@
 $_rc_subr_loaded . /etc/rc.subr
 
 name=mopherd
-rcvar=$name
-pidfile=@VARBASE@/run/$name.pid
-command=@PREFIX@/sbin/$name
+rcvar=mopherd
+pidfile=@MOPHER_RUN@/mopherd.pid
+start_precmd=${name}_precmd
+command=@PREFIX@/sbin/mopherd
 command_args="-p $pidfile"
 
-load_rc_config $name
+mopherd_precmd()
+{
+	@INSTALL@ -d -o @MOPHER_USER@ -g @MOPHER_GROUP@ @MOPHER_RUN@
+}
+
+load_rc_config mopherd
 run_rc_command "$1"
