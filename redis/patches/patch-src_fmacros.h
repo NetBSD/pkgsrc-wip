a33938ebcc79aec890e56b1aa3c4bbac3975bf4a
Filip Hajny <filip@joyent.com>
2012-12-15 20:39:54 +0000

    Update redis to 2.6.7.
    Too many changes, see 2.4 changelog:
    
    https://raw.github.com/antirez/redis/2.4/00-RELEASENOTES
    
    and 2.6 changelog:
    
    https://raw.github.com/antirez/redis/2.6/00-RELEASENOTES

--- redis/patches/patch-src_fmacros.h
+++ redis/patches/patch-src_fmacros.h
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_fmacros.h,v 1.1 2012/02/29 11:34:43 fhajny Exp $
+$NetBSD: patch-src_fmacros.h,v 1.2 2012/12/15 20:39:54 fhajny Exp $
 
 NetBSD needs _XOPEN_SOURCE>=600.
 
---- src/fmacros.h.orig	2012-02-22 13:23:43.000000000 +0000
+--- src/fmacros.h.orig	2012-12-03 11:53:09.000000000 +0000
 +++ src/fmacros.h
-@@ -3,7 +3,7 @@
- 
- #define _BSD_SOURCE
+@@ -36,7 +36,7 @@
+ #define _GNU_SOURCE
+ #endif
  
 -#if defined(__linux__) || defined(__OpenBSD__)
 +#if defined(__linux__) || defined(__OpenBSD__) || defined(__NetBSD__)
