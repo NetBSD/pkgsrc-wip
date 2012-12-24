fc192b8e472538615c883479d8fabc044e142553
Pierre Pronchery <khorben@defora.org>
2012-12-24 03:18:05 +0000

    Packaged DeforaOS libDesktop 0.0.9.
    It is primarily a bugfix-release, with some memory leaks addressed.

--- deforaos-libdesktop/buildlink3.mk
+++ deforaos-libdesktop/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2012/09/01 16:16:35 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2012/12/24 03:18:05 khorben Exp $
+#
 
 BUILDLINK_TREE+=	deforaos-libdesktop
 
 .if !defined(DEFORAOS_LIBDESKTOP_BUILDLINK3_MK)
 DEFORAOS_LIBDESKTOP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.0.7
+BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.0.9
 BUILDLINK_PKGSRCDIR.deforaos-libdesktop?=	../../wip/deforaos-libdesktop
 
 .include "../../wip/deforaos-libsystem/buildlink3.mk"
