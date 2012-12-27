e08279e41e56199c20e9355d6cc946a9ca738919
Blue Rats <rodent@NetBSD.org>
2012-12-27 05:25:31 +0000

    Website lied - 3.0.1 was not the latest stable. This version (3.4.2) isn't either, but it's the latest stable that pkgsrc-2012Q3 can support. Q4 will have updated glibmm and gtk3, and then we can update this package to 3.6.0.

--- gtkmm3/buildlink3.mk
+++ gtkmm3/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/12/27 04:35:34 othyro Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2012/12/27 05:25:31 othyro Exp $
 
 BUILDLINK_TREE+=	gtkmm3
 
 .if !defined(GTKMM3_BUILDLINK3_MK)
 GTKMM3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gtkmm3+=	gtkmm3>=3.0.1
+BUILDLINK_API_DEPENDS.gtkmm3+=	gtkmm3>=3.4.2
 BUILDLINK_PKGSRCDIR.gtkmm3?=	../../wip/gtkmm3
 
 .include "../../textproc/libxslt/buildlink3.mk"
