c0100ff28f14ab3e684fbb51b3b56687ea4855e3
dieter roelants <dieter@bsdusr.net>
2012-12-30 19:51:57 +0000

    Update efreet to 1.7.4 + steal maintainership.
    NEWS since 1.1.0 (which is in pkgsrc/sysutils/):
    
    Changes since Efreet 1.7.3:
    
    Fixes:
            * Add O_CLOEXEC to opened fds
            * Add XML output to doc
            * Add installation rule for doc
            * Fix build on Windows platforms.
    
    Changes since Efreet 1.7.2:
    
    Fixes:
    	- Fix alloca
    
    Changes since Efreet 1.7.1:
    
    Fixes:
        * Allow unquoted values in user-dirs.dirs
    
    Changes since Efreet 1.7.0:
    
    Fixes:
        * Fix insane delay added to desktop/icon updates.
    
    Changes since Efreet 1.2.0:
    
    Improvements:
        * Faster mime type loading.
    
    Additions:
        * Support XDG_DESKTOP_DIR
        * efreet_lang_reset() for refreshing language variables and
          caches after locale switches
    
    Changes since Efreet 1.1.0:
    
    Improvements:
        * use eina_file_direct_ls() instead of opendir().
        * compilation warnings fixed.
        * documentation improvements/fixes

--- efreet/buildlink3.mk
+++ efreet/buildlink3.mk
@@ -1,14 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.10 2010/01/18 19:40:12 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2012/12/30 19:51:57 roelants Exp $
 
 BUILDLINK_TREE+=	efreet
 
 .if !defined(EFREET_BUILDLINK3_MK)
 EFREET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.efreet?=	efreet>=0.5.0.063
+BUILDLINK_API_DEPENDS.efreet+=	efreet>=1.7.0
 BUILDLINK_PKGSRCDIR.efreet?=	../../wip/efreet
 
 .include        "../../wip/ecore/buildlink3.mk"
+.include        "../../wip/eet/buildlink3.mk"
+.include        "../../wip/eina/buildlink3.mk"
 .endif # EFREET_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-efreet
