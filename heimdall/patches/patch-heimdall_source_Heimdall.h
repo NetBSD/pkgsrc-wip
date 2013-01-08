80b093b45a4a0ce527eecf40fc483cc8294c6221
Blue Rats <rodent@NetBSD.org>
2013-01-08 22:07:07 +0000

    Import heimdall-1.3.2 as wip/heimdall.
    Heimdall is a cross-platform open-source tool suite used to flash firmware (aka
    ROMs) onto Samsung Galaxy S devices.

---
+++ heimdall/patches/patch-heimdall_source_Heimdall.h
@@ -0,0 +1,15 @@
+$NetBSD: patch-heimdall_source_Heimdall.h,v 1.1 2013/01/08 22:07:07 othyro Exp $
+
+Add NetBSD support.
+
+--- heimdall/source/Heimdall.h.orig	2012-03-31 01:22:17.000000000 +0000
++++ heimdall/source/Heimdall.h
+@@ -27,7 +27,7 @@
+ 
+ #include "../config.h"
+ 
+-#if defined(OS_DARWIN) || defined(OS_LINUX)
++#if defined(OS_DARWIN) || defined(OS_LINUX) || defined(OS_NETBSD)
+ #include <unistd.h>
+ #define Sleep(t) usleep(1000*t)
+ #else
