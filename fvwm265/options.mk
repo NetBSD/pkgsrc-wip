28d0b73e393e5dcb7a1d8729c34b716a183ce948
Makoto Fujiwara <makoto@ki.nu>
2013-01-15 01:59:57 +0000

    fvwm-2.6.5 is added as wip/fvwm265 for testing. Sorry for version-fixed name.

---
+++ fvwm265/options.mk
@@ -0,0 +1,21 @@
+# $NetBSD: options.mk,v 1.1 2013/01/15 01:59:57 makoto Exp $
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.fvwm
+PKG_SUPPORTED_OPTIONS=		gtk rplay
+PKG_OPTIONS_LEGACY_VARS+=	FVWM2_USE_GTK:gtk
+PKG_OPTIONS_LEGACY_VARS+=	FVWM2_USE_RPLAY:rplay
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		gtk
+
+.if !empty(PKG_OPTIONS:Mgtk)
+.  include "../../x11/gtk/buildlink3.mk"
+PLIST.gtk=		yes
+.else
+CONFIGURE_ARGS+=	--without-gtk-prefix
+.endif
+
+.if !empty(PKG_OPTIONS:Mrplay)
+.  include "../../audio/rplay/buildlink3.mk"
+.endif
