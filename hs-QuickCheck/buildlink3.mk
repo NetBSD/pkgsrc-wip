a0513a1b7c014aed40f2ed9a6bdcb00c4a66e263
PHO / phonohawk <phonohawk@users.sourceforge.net>
2013-01-15 09:24:49 +0000

    Upstream update to QuickCheck-2.5.1.1

--- hs-QuickCheck/buildlink3.mk
+++ hs-QuickCheck/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.6 2012/01/17 02:27:10 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2013/01/15 09:24:49 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-QuickCheck
 
 .if !defined(HS_QUICKCHECK_BUILDLINK3_MK)
 HS_QUICKCHECK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.4.2
+BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.5.1
 BUILDLINK_PKGSRCDIR.hs-QuickCheck?=	../../wip/hs-QuickCheck
 
+.include "../../wip/hs-random/buildlink3.mk"
 .endif	# HS_QUICKCHECK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-QuickCheck
