5631639c211865b809739a34b65bba4d40267ed1
PHO / phonohawk <phonohawk@users.sourceforge.net>
2013-01-15 09:04:32 +0000

    Upstream update to primitive-0.5.0.1

--- hs-primitive/buildlink3.mk
+++ hs-primitive/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2012/02/02 12:44:22 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/01/15 09:04:32 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-primitive
 
 .if !defined(HS_PRIMITIVE_BUILDLINK3_MK)
 HS_PRIMITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.4.1
+BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.5
 BUILDLINK_PKGSRCDIR.hs-primitive?=	../../wip/hs-primitive
 .endif	# HS_PRIMITIVE_BUILDLINK3_MK
 
