f98a1ff63cd6ff1856d0c028cc3fde449ecab45e
PHO / phonohawk <phonohawk@users.sourceforge.net>
2013-01-15 13:07:03 +0000

    Upstream update to hscolour-1.20.3

--- hs-hscolour/buildlink3.mk
+++ hs-hscolour/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/07/08 03:37:41 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/01/15 13:07:03 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-hscolour
 
 .if !defined(HS_HSCOLOUR_BUILDLINK3_MK)
 HS_HSCOLOUR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hscolour+=	hs-hscolour>=1.19
+BUILDLINK_API_DEPENDS.hs-hscolour+=	hs-hscolour>=1.20.3
 BUILDLINK_PKGSRCDIR.hs-hscolour?=	../../wip/hs-hscolour
 .endif	# HS_HSCOLOUR_BUILDLINK3_MK
 
