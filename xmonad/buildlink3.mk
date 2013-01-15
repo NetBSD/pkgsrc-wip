fb3695ab6a1eb64de882d630eeb95c75b2d4c004
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-01-15 12:15:46 +0000

    buildlink3.mk: update BUILDLINK_API_DEPENDS,and fix include.

--- xmonad/buildlink3.mk
+++ xmonad/buildlink3.mk
@@ -1,15 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.5 2013/01/09 23:27:32 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2013/01/15 12:15:46 szptvlfn Exp $
 
 BUILDLINK_TREE+=	xmonad
 
 .if !defined(XMONAD_BUILDLINK3_MK)
 XMONAD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xmonad+=	xmonad>=0.8.1
-BUILDLINK_ABI_DEPENDS.xmonad?=	xmonad>=0.8.1
+BUILDLINK_API_DEPENDS.xmonad+=	xmonad>=0.11
 BUILDLINK_PKGSRCDIR.xmonad?=	../../wip/xmonad
 
-.include "../../lang/ghc/buildlink3.mk"
-.endif # XMONAD_BUILDLINK3_MK
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-utf8-string/buildlink3.mk"
+.include "../../wip/hs-x11/buildlink3.mk"
+.endif	# XMONAD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xmonad
