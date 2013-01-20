a9b2a773342ceca8d11265dba36484421f973cf8
Makoto Fujiwara <makoto@ki.nu>
2013-01-20 09:46:48 +0000

    Add many options and activate them for default (The purpose is testing full option).
    PKG_SUPPORTED_OPTIONS= fribidi gdk-imlib gnome gtk png rplay svg stroke xrender xcursor
     xft2
    PKG_SUGGESTED_OPTIONS= fribidi gdk-imlib gnome gtk png rplay svg stroke xrender xcursor
     xft2

--- fvwm265/options.mk
+++ fvwm265/options.mk
@@ -1,13 +1,34 @@
-# $NetBSD: options.mk,v 1.3 2013/01/18 13:52:51 makoto Exp $
+# $NetBSD: options.mk,v 1.4 2013/01/20 09:46:48 makoto Exp $
 
-PKG_OPTIONS_VAR=		PKG_OPTIONS.fvwm2
-PKG_SUPPORTED_OPTIONS=		gtk rplay xrender xcursor xft2
+PKG_OPTIONS_VAR=		PKG_OPTIONS.fvwm
 PKG_OPTIONS_LEGACY_VARS+=	FVWM2_USE_GTK:gtk
 PKG_OPTIONS_LEGACY_VARS+=	FVWM2_USE_RPLAY:rplay
+PKG_SUPPORTED_OPTIONS=		fribidi gdk-imlib gnome gtk png rplay svg stroke xrender xcursor xft2
+PKG_SUGGESTED_OPTIONS=		fribidi gdk-imlib gnome gtk png rplay svg stroke xrender xcursor xft2
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=		gtk
+
+.if !empty(PKG_OPTIONS:Mfribidi)
+.  include "../../converters/fribidi/buildlink3.mk"
+#DEPENDS+=		fribidi-[0-9]*:../../misc/fribidi
+.else
+CONFIGURE_ARGS+=	--disable-bidi
+.endif
+
+.if !empty(PKG_OPTIONS:Mgdk-imlib)
+#.include "../../graphics/gdk-pixbuf/buildlink3.mk"
+# not found in pkgsrc yet (?)
+.else
+CONFIGURE_ARGS+=        --disable-gdk
+.endif
+
+.if !empty(PKG_OPTIONS:Mgnome)
+.include "../../devel/libgnome/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=        --disable-gnome
+.endif
 
 .if !empty(PKG_OPTIONS:Mgtk)
 .  include "../../x11/gtk/buildlink3.mk"
@@ -16,9 +37,29 @@ PLIST.gtk=		yes
 CONFIGURE_ARGS+=	--without-gtk-prefix
 .endif
 
+.if !empty(PKG_OPTIONS:Mpng)
+.include "../../graphics/png/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=        --disable-png
+.endif
+
 .if !empty(PKG_OPTIONS:Mrplay)
 .  include "../../audio/rplay/buildlink3.mk"
 .endif
+
+.if !empty(PKG_OPTIONS:Msvg)
+# With SVG image support?             no: Failed to run test program
+#.include "../../graphics/librsvg/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=        --disable-rsvg
+.endif
+
+# libstroke ?
+# .if !empty(PKG_OPTIONS:Mstroke)
+# .include "../../x11/libXrender/buildlink3.mk"
+# .else
+# CONFIGURE_ARGS+=        --disable-stroke
+# .endif
 
 .if !empty(PKG_OPTIONS:Mxcursor)
 CONFIGURE_ARGS+=	--enable-xcursor
