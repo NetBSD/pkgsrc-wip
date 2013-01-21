d6c9de27cab093f36182b0ee6153d9236e97f205
Blue Rats <rodent@NetBSD.org>
2013-01-21 20:59:36 +0000

    Updated to latest stable: +LICENSE; Buildlink'd check; Filled in USE_TOOLS; Fixed buildlink3.mk; Regenerated PLIST and distinfo; Passes pkglint; Now, uses DESTDIR for install.

--- libgxim/buildlink3.mk
+++ libgxim/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/04/08 14:52:50 jihbed Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/01/21 20:59:36 othyro Exp $
 
 BUILDLINK_TREE+=	libgxim
 
@@ -8,8 +8,12 @@ LIBGXIM_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libgxim+=	libgxim>=0.3.3
 BUILDLINK_PKGSRCDIR.libgxim?=	../../wip/libgxim
 
+.include "../../devel/check/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
+.include "../../lang/ruby/buildlink3.mk"
+.include "../../sysutils/dbus/buildlink3.mk"
 .include "../../sysutils/dbus-glib/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
 
 .endif	# LIBGXIM_BUILDLINK3_MK
 
