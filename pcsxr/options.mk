eca874e589fbc693d7899ae3b491d1fa22875de0
Blue Rats <rodent@NetBSD.org>
2013-01-21 20:36:04 +0000

    Add --disable-nls to CONFIGURE_ARGS for non-NLS builds so locale files are not installed. Correctly made libXcursor a build dependency. Bump PKGREVISION.

--- pcsxr/options.mk
+++ pcsxr/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2012/09/22 16:45:49 othyro Exp $
+# $NetBSD: options.mk,v 1.2 2013/01/21 20:36:04 othyro Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pcsxr
 PKG_SUPPORTED_OPTIONS=	nls
@@ -10,4 +10,6 @@ PLIST_VARS+=		nls
 .if !empty(PKG_OPTIONS:Mnls)
 .include "../../devel/gettext-lib/buildlink3.mk"
 PLIST.nls=	yes
+.else
+CONFIGURE_ARGS+=	--disable-nls
 .endif
