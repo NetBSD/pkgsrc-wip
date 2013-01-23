9769b96ba20b80df003a4deff8ed5a73035e9dc3
Thomas Klausner <wiz@NetBSD.org>
2013-01-23 23:53:38 +0000

    regen, so pkglint is happy.

--- protobuf-c/buildlink3.mk
+++ protobuf-c/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/01/13 00:42:39 wongk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/01/23 23:53:38 thomasklausner Exp $
 
 BUILDLINK_TREE+=	protobuf-c
 
-.if !defined(PROTOBUF_BUILDLINK3_MK)
-PROTOBUF_BUILDLINK3_MK:=
+.if !defined(PROTOBUF_C_BUILDLINK3_MK)
+PROTOBUF_C_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.protobuf-c+=	protobuf-c>=0.15
 BUILDLINK_PKGSRCDIR.protobuf-c?=	../../wip/protobuf-c
 
-.endif	# PROTOBUF_BUILDLINK3_MK
+.endif	# PROTOBUF_C_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-protobuf-c
