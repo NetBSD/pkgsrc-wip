57f6e62f8840a3eb1025a487442f4b35b82e09d8
Filip Hajny <filip@joyent.com>
2013-01-23 14:53:48 +0000

    Update node.js to 0.8.18
    2013.01.18, Version 0.8.18 (Stable)
    * npm: Upgrade to v1.2.2
    * dns: make error message match errno (Dan Milon)
    * tls: follow RFC6125 more stricly (Fedor Indutny)
    * buffer: reject negative SlowBuffer offsets (Ben Noordhuis)
    * install: add simplejson fallback (Chris Dent)
    * http: fix "Cannot call method 'emit' of null" (Ben Noordhuis)
    
    2013.01.09, Version 0.8.17 (Stable), c50c33e9397d7a0a8717e8ce7530572907c054ad
    * npm: Upgrade to v1.2.0
      - peerDependencies (Domenic Denicola)
      - node-gyp v0.8.2 (Nathan Rajlich)
      - Faster installs from github user/project shorthands (Nathan Zadoks)
    * typed arrays: fix 32 bit size/index overflow (Ben Noordhuis)
    * http: Improve performance of single-packet responses (Ben Noordhuis)
    * install: fix openbsd man page location (Ben Noordhuis)
    * http: bubble up parser errors to ClientRequest (Brian White)

--- node/patches/patch-tools_install.py
+++ node/patches/patch-tools_install.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-tools_install.py,v 1.2 2013/01/07 14:58:40 fhajny Exp $
+$NetBSD: patch-tools_install.py,v 1.3 2013/01/23 14:53:48 fhajny Exp $
 
 Install man pages under the right directory.
---- tools/install.py.orig	2012-12-12 22:44:54.000000000 +0000
-+++ tools/install.py
-@@ -201,7 +201,7 @@ def files(action):
-   if 'freebsd' in sys.platform:
+--- tools/install.py.orig	2013-01-18 20:15:41.000000000 +0000
++++ tools/install.py	2013-01-23 13:57:39.128772373 +0000
+@@ -206,7 +206,7 @@
+   if 'freebsd' in sys.platform or 'openbsd' in sys.platform:
      action(['doc/node.1'], 'man/man1/')
    else:
 -    action(['doc/node.1'], 'share/man/man1/')
