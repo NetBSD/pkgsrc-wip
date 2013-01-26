9ff3c2c35b11897221e3b0794cc85be44bb77ec7
Pierre Pronchery <khorben@defora.org>
2013-01-26 01:10:43 +0000

    Building with Gtk+ 3 from now on

--- deforaos-libdesktop/buildlink3.mk
+++ deforaos-libdesktop/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2012/12/24 03:18:05 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2013/01/26 01:10:43 khorben Exp $
 #
 
 BUILDLINK_TREE+=	deforaos-libdesktop
@@ -6,11 +6,11 @@ BUILDLINK_TREE+=	deforaos-libdesktop
 .if !defined(DEFORAOS_LIBDESKTOP_BUILDLINK3_MK)
 DEFORAOS_LIBDESKTOP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.0.9
+BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.0.9nb1
 BUILDLINK_PKGSRCDIR.deforaos-libdesktop?=	../../wip/deforaos-libdesktop
 
 .include "../../wip/deforaos-libsystem/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
 .endif	# DEFORAOS_LIBDESKTOP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-deforaos-libdesktop
