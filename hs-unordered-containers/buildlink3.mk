7e758cfb1d8c4b0da96037de8a69cce4dd035ab4
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-02-21 20:17:40 +0000

    Update to 0.2.3.0

--- hs-unordered-containers/buildlink3.mk
+++ hs-unordered-containers/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/04 06:45:47 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/02/21 20:17:40 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-unordered-containers
 
 .if !defined(HS_UNORDERED_CONTAINERS_BUILDLINK3_MK)
 HS_UNORDERED_CONTAINERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.1.4
+BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.3.0
 BUILDLINK_PKGSRCDIR.hs-unordered-containers?=	../../wip/hs-unordered-containers
 
 .include "../../wip/hs-hashable/buildlink3.mk"
