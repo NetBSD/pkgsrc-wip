722cd9a59ec7d3449dbc4f2ed286e2a57198fc20
Krister Walfridsson <kristerw@NetBSD.org>
2013-02-22 22:50:40 +0000

    Use devel/libctl instead of the removed wip/libctl.

--- mpb/buildlink3.mk
+++ mpb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/05/11 20:00:48 outpaddling Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/02/22 22:51:42 kristerw Exp $
 # XXX	BUILDLINK_DEPMETHOD.mpb?=	build
 
 BUILDLINK_TREE+=	mpb
@@ -14,7 +14,7 @@ BUILDLINK_PKGSRCDIR.mpb?=	../../wip/mpb
 .include "../../devel/hdf5/buildlink3.mk"
 .include "../../math/fftw2/buildlink3.mk"
 .include "../../devel/readline/buildlink3.mk"
-.include "../../wip/libctl/buildlink3.mk"
+.include "../../devel/libctl/buildlink3.mk"
 .endif	# MPB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mpb
