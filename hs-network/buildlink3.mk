169118d353e196196c9eb9fdf135846f796a37d7
PHO / phonohawk <phonohawk@users.sourceforge.net>
2013-02-23 13:27:10 +0000

    Upstream update to network-2.4.1.2

--- hs-network/buildlink3.mk
+++ hs-network/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2013/01/15 12:09:44 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2013/02/23 13:27:10 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-network
 
 .if !defined(HS_NETWORK_BUILDLINK3_MK)
 HS_NETWORK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=2.4
+BUILDLINK_API_DEPENDS.hs-network+=	hs-network>=2.4.1
 BUILDLINK_PKGSRCDIR.hs-network?=	../../wip/hs-network
 
 .include "../../wip/hs-parsec/buildlink3.mk"
