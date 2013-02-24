9c72a7a1d48c8152c40b5f5c36095799c698a453
Aleksej Saushev <asau@users.sourceforge.net>
2013-02-24 19:15:53 +0000

    Update to MPICH 3.0.1

--- mpi-ch/buildlink3.mk
+++ mpi-ch/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.8 2010/12/17 03:06:42 asau Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2013/02/24 19:15:53 asau Exp $
 
-BUILDLINK_TREE+=	mpich2
+BUILDLINK_TREE+=	mpich
 
-.if !defined(MPICH2_BUILDLINK3_MK)
-MPICH2_BUILDLINK3_MK:=
+.if !defined(MPICH_BUILDLINK3_MK)
+MPICH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mpich2+=	mpich2>=1.2.99.6307
-BUILDLINK_PKGSRCDIR.mpich2?=	../../wip/mpi-ch
+BUILDLINK_API_DEPENDS.mpich+=	mpich>=3
+BUILDLINK_PKGSRCDIR.mpich?=	../../wip/mpi-ch
 
 .include "../../parallel/openpa/buildlink3.mk"
-.endif	# MPICH2_BUILDLINK3_MK
+.endif	# MPICH_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-mpich2
+BUILDLINK_TREE+=	-mpich
