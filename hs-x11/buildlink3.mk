a5e1052b6672d201f531c26db238e220371302be
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-02-26 12:51:12 +0000

    Update to 1.6.1.1

--- hs-x11/buildlink3.mk
+++ hs-x11/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.9 2013/01/15 13:39:25 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2013/02/26 12:51:12 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-x11
 
 .if !defined(HS_X11_BUILDLINK3_MK)
 HS_X11_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-x11+=	hs-x11>=1.6.0.2
+BUILDLINK_API_DEPENDS.hs-x11+=	hs-x11>=1.6.1.1
 BUILDLINK_PKGSRCDIR.hs-x11?=	../../wip/hs-x11
 
+.include "../../wip/hs-data-default/buildlink3.mk"
 .endif	# HS_X11_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-x11
