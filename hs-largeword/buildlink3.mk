f869af2b90b039730a8657371fcec2e40ee96ad5
PHO / phonohawk <phonohawk@users.sourceforge.net>
2013-02-26 14:15:24 +0000

    Upstream update to largeword-1.0.4

--- hs-largeword/buildlink3.mk
+++ hs-largeword/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/03/04 08:02:28 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/02/26 14:15:24 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-largeword
 
 .if !defined(HS_LARGEWORD_BUILDLINK3_MK)
 HS_LARGEWORD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-largeword+=	hs-largeword>=1.0.1
+BUILDLINK_API_DEPENDS.hs-largeword+=	hs-largeword>=1.0.4
 BUILDLINK_PKGSRCDIR.hs-largeword?=	../../wip/hs-largeword
 .endif	# HS_LARGEWORD_BUILDLINK3_MK
 
