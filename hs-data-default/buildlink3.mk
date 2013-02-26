ff5e5738e47233f9b89d906d0e5c92d58c522138
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-02-26 12:54:08 +0000

    Update to 0.5.0

--- hs-data-default/buildlink3.mk
+++ hs-data-default/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/12/01 22:50:49 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/02/26 12:54:08 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-data-default
 
 .if !defined(HS_DATA_DEFAULT_BUILDLINK3_MK)
 HS_DATA_DEFAULT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-data-default+=	hs-data-default>=0.3.0
+BUILDLINK_API_DEPENDS.hs-data-default+=	hs-data-default>=0.5.0
 BUILDLINK_PKGSRCDIR.hs-data-default?=	../../wip/hs-data-default
 
 .include "../../wip/hs-dlist/buildlink3.mk"
