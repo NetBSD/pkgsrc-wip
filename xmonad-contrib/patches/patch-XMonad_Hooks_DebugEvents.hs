6a9e79933e2c1e66b72c0482951a4058b800cdc1
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-02-28 15:27:50 +0000

    fix patch from upstream

--- xmonad-contrib/patches/patch-XMonad_Hooks_DebugEvents.hs
+++ xmonad-contrib/patches/patch-XMonad_Hooks_DebugEvents.hs
@@ -1,18 +1,11 @@
-$NetBSD: patch-XMonad_Hooks_DebugEvents.hs,v 1.1 2013/02/26 22:17:45 szptvlfn Exp $
+$NetBSD: patch-XMonad_Hooks_DebugEvents.hs,v 1.2 2013/02/28 15:27:50 szptvlfn Exp $
 
 for wip/hs-x11-1.6.1.1
+refine on this patch, from http://code.haskell.org/XMonadContrib/
 
---- XMonad/Hooks/DebugEvents.hs.orig	2013-01-01 01:32:44.000000000 +0000
+--- XMonad/Hooks/DebugEvents.hs.orig    2013-01-01 01:32:44.000000000 +0000
 +++ XMonad/Hooks/DebugEvents.hs
-@@ -29,6 +29,7 @@ import           XMonad.Hooks.DebugKeyEv
- import           XMonad.Util.DebugWindow                     (debugWindow)
- 
- -- import           Graphics.X11.Xlib.Extras.GetAtomName        (getAtomName)
-+import           Graphics.X11.Xlib.Atom                      ()
- 
- import           Control.Exception.Extensible         as E
- import           Control.Monad.State
-@@ -42,27 +43,12 @@ import           Codec.Binary.UTF8.Strin
+@@ -42,27 +42,12 @@ import           Codec.Binary.UTF8.Strin
  import           Data.Maybe                                  (fromMaybe)
  import           Data.Monoid
  import           Foreign
@@ -22,7 +15,7 @@ for wip/hs-x11-1.6.1.1
  import           System.Exit
  import           System.IO
  import           System.Process
- 
+
 --- this should move into X11
 -foreign import ccall unsafe "XGetAtomName"
 -  xGetAtomName :: Display -> Atom -> IO CString
