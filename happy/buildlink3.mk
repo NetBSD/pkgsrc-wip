8fa94499da98262cd89d628eaa393dfc23d2b255
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-03-01 20:47:28 +0000

    Update to 1.18.10

--- happy/buildlink3.mk
+++ happy/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2013/01/09 23:34:20 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2013/03/01 20:47:28 szptvlfn Exp $
 
 BUILDLINK_TREE+=	happy
 
 .if !defined(HAPPY_BUILDLINK3_MK)
 HAPPY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.happy+=	happy>=1.18.9
+BUILDLINK_API_DEPENDS.happy+=	happy>=1.18.10
 BUILDLINK_PKGSRCDIR.happy?=	../../wip/happy
 
 .endif	# HAPPY_BUILDLINK3_MK
