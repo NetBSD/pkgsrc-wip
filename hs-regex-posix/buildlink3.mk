9f0580de755ff1190d84792ad8e1359c37a19532
PHO / phonohawk <phonohawk@users.sourceforge.net>
2013-03-02 10:58:52 +0000

    Upstream update to regex-posix-0.95.2

--- hs-regex-posix/buildlink3.mk
+++ hs-regex-posix/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2011/12/04 12:56:44 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2013/03/02 10:58:52 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-regex-posix
 
 .if !defined(HS_REGEX_POSIX_BUILDLINK3_MK)
 HS_REGEX_POSIX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-regex-posix+=	hs-regex-posix>=0.95.1
+BUILDLINK_API_DEPENDS.hs-regex-posix+=	hs-regex-posix>=0.95.2
 BUILDLINK_PKGSRCDIR.hs-regex-posix?=	../../wip/hs-regex-posix
 
 .include "../../wip/hs-regex-base/buildlink3.mk"
