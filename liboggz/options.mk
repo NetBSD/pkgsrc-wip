19dbe6b49ba34c725232dba6252ec1f4475b28ee
Blue Rats <rodent@NetBSD.org>
2013-03-02 23:22:31 +0000

    Rename tests option to valgrind.

--- liboggz/options.mk
+++ liboggz/options.mk
@@ -1,12 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2013/03/02 15:31:33 othyro Exp $
+# $NetBSD: options.mk,v 1.2 2013/03/02 23:22:31 othyro Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.liboggz
-PKG_SUPPORTED_OPTIONS=	tests
+PKG_SUPPORTED_OPTIONS=	valgrind
 PKG_SUGGESTED_OPTIONS+=	# blank
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mtests)
+.if !empty(PKG_OPTIONS:Mvalgrind)
 BUILD_DEPENDS+=	valgrind-[0-9]*:../../devel/valgrind
 CONFIGURE_ARGS+=	--enable-valgrind-testing
 .else
