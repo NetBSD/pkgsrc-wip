1059fc22cf6bec166d54ad58a19e56b62d00921a
PHO / phonohawk <phonohawk@users.sourceforge.net>
2013-03-02 11:28:48 +0000

    Upstream update to test-framework-quickcheck2-0.3.0.1

--- hs-test-framework-quickcheck2/buildlink3.mk
+++ hs-test-framework-quickcheck2/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/12/04 13:40:06 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/03/02 11:28:48 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-test-framework-quickcheck2
 
 .if !defined(HS_TEST_FRAMEWORK_QUICKCHECK2_BUILDLINK3_MK)
 HS_TEST_FRAMEWORK_QUICKCHECK2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-test-framework-quickcheck2+=	hs-test-framework-quickcheck2>=0.2.10
+BUILDLINK_API_DEPENDS.hs-test-framework-quickcheck2+=	hs-test-framework-quickcheck2>=0.3.0
 BUILDLINK_PKGSRCDIR.hs-test-framework-quickcheck2?=	../../wip/hs-test-framework-quickcheck2
 
 .include "../../wip/hs-QuickCheck/buildlink3.mk"
+.include "../../wip/hs-extensible-exceptions/buildlink3.mk"
 .include "../../wip/hs-test-framework/buildlink3.mk"
 .endif	# HS_TEST_FRAMEWORK_QUICKCHECK2_BUILDLINK3_MK
 
