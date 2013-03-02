bf75d3d674d1ee660dfd03e09ba155911e5febaf
PHO / phonohawk <phonohawk@users.sourceforge.net>
2013-03-02 11:18:57 +0000

    Upstream update to HUnit-1.2.5.1

--- hs-HUnit/buildlink3.mk
+++ hs-HUnit/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/12/04 13:27:16 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/03/02 11:18:57 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-HUnit
 
 .if !defined(HS_HUNIT_BUILDLINK3_MK)
 HS_HUNIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HUnit+=	hs-HUnit>=1.2.4
+BUILDLINK_API_DEPENDS.hs-HUnit+=	hs-HUnit>=1.2.5
 BUILDLINK_PKGSRCDIR.hs-HUnit?=	../../wip/hs-HUnit
 .endif	# HS_HUNIT_BUILDLINK3_MK
 
