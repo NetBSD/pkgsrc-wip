8ac68fa261c73e93ac00e4721081a073c58d212f
Blue Rats <rodent@NetBSD.org>
2013-03-02 23:21:08 +0000

    Move flac, speex, and vorbis to options.mk, but enabled by default. Rename tests option to valgrind.

--- libfishsound/buildlink3.mk
+++ libfishsound/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/03/02 15:41:45 othyro Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/03/02 23:21:08 othyro Exp $
 
 BUILDLINK_TREE+=	libfishsound
 
@@ -8,11 +8,23 @@ LIBFISHSOUND_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libfishsound+=	libfishsound>=1.0.0
 BUILDLINK_PKGSRCDIR.libfishsound?=	../../wip/libfishsound
 
+pkgbase := libfishsound
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.libfishsound:Mflac)
 .include "../../audio/flac/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.libfishsound:Mspeex)
+.include "../../audio/speex/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.libfishsound:Mvorbis)
+.include "../../audio/libvorbis/buildlink3.mk"
+.endif
+
 .include "../../wip/liboggz/buildlink3.mk"
 .include "../../audio/libsndfile/buildlink3.mk"
-.include "../../audio/speex/buildlink3.mk"
-.include "../../audio/libvorbis/buildlink3.mk"
 .endif	# LIBFISHSOUND_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libfishsound
--- libfishsound/options.mk
+++ libfishsound/options.mk
@@ -1,14 +1,33 @@
-# $NetBSD: options.mk,v 1.1 2013/03/02 15:41:45 othyro Exp $
+# $NetBSD: options.mk,v 1.2 2013/03/02 23:21:08 othyro Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libfish
-PKG_SUPPORTED_OPTIONS=	tests
-PKG_SUGGESTED_OPTIONS+=	# blank
+PKG_SUPPORTED_OPTIONS=	flac speex valgrind vorbis
+PKG_SUGGESTED_OPTIONS+=	flac speex vorbis
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mtests)
+.if !empty(PKG_OPTIONS:Mflac)
+BUILDLINK_DEPMETHOD.flac=       build
+.include "../../audio/flac/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-flac
+.endif
+
+.if !empty(PKG_OPTIONS:Mspeex)
+.include "../../audio/speex/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-speex
+.endif
+
+.if !empty(PKG_OPTIONS:Mvalgrind)
 BUILD_DEPENDS+=	valgrind-[0-9]*:../../devel/valgrind
 CONFIGURE_ARGS+=	--enable-valgrind-testing
 .else
 CONFIGURE_ARGS+=	--disable-valgrind-testing
+.endif
+
+.if !empty(PKG_OPTIONS:Mvorbis)
+.include "../../audio/libvorbis/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-vorbis
 .endif
