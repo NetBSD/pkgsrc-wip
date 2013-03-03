b80299e17d2eb4ba1b4918053cbfd05de1965d03
Blue Rats <rodent@NetBSD.org>
2013-03-03 17:27:53 +0000

    Had this update sitting around and forgot about it - updated to latest released. Added NLS PKG_OPTION and PKG_BUILD_OPTION. Updated patch. Extra include in PLIST. Regenerated distinfo. Package ChangeLog:
    http://pxlib.cvs.sourceforge.net/viewvc/pxlib/pxlib/ChangeLog?revision=1.79

--- pxlib/buildlink3.mk
+++ pxlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/05/06 10:14:27 cheusov Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/03/03 17:27:54 othyro Exp $
 
 BUILDLINK_TREE+=	pxlib
 
@@ -7,6 +7,13 @@ PXLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pxlib+=	pxlib>=0.2.0
 BUILDLINK_PKGSRCDIR.pxlib?=	../../wip/pxlib
+
+pkgbase := pxlib
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.pxlib:Mnls)
+.include "../../devel/gettext-lib/buildlink3.mk"
+.endif
 
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../converters/recode/buildlink3.mk"
---
+++ pxlib/options.mk
@@ -0,0 +1,16 @@
+# $NetBSD: options.mk,v 1.1 2013/03/03 17:27:54 othyro Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.pxlib
+PKG_SUPPORTED_OPTIONS=	nls
+PKG_SUGGESTED_OPTIONS+=	nls
+PLIST_VARS+=		nls
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mnls)
+.include "../../devel/gettext-lib/buildlink3.mk"
+USE_TOOLS+=		gmake
+PLIST.nls=		yes
+.else
+CONFIGURE_ARGS+=	--disable-nls
+.endif
