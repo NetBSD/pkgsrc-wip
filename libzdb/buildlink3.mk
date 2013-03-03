af19f2cc29917211362e9765787dbbd1889ac7cc
Tobias Nygren <tnn@NetBSD.org>
2013-03-03 17:28:16 +0000

    Use pkg-build-options to pull in link dependencies in bl3.mk

--- libzdb/buildlink3.mk
+++ libzdb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/08/19 18:09:29 udontknow Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/03/03 17:28:16 tnn2 Exp $
 
 BUILDLINK_TREE+=	libzdb
 
@@ -7,6 +7,23 @@ LIBZDB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libzdb+=	libzdb>=2.6
 BUILDLINK_PKGSRCDIR.libzdb?=	../../wip/libzdb
+
+pkgbase := libzdb
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.libzdb:Msqlite)
+.  include "../../databases/sqlite3/buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.libzdb:Mpgsql)
+.  include "../../mk/pgsql.buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.libzdb:Mmysql)
+.  include "../../mk/mysql.buildlink3.mk"
+.endif
+.if !empty(PKG_BUILD_OPTIONS.libzdb:Mssl)
+.  include "../../security/openssl/buildlink3.mk"
+.endif
+
 .endif	# LIBZDB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libzdb
