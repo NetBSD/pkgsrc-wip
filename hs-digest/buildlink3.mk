1e5bfb507d5bfd136c9c26c3d9d77c35fde68078
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-03-05 22:48:06 +0000

    Update to 0.0.1.2

--- hs-digest/buildlink3.mk
+++ hs-digest/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/04/15 21:00:23 zecrazytux Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/03/05 22:48:06 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-digest
 
 .if !defined(HS_DIGEST_BUILDLINK3_MK)
 HS_DIGEST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-digest+=	hs-digest>=0.0.0.5
+BUILDLINK_API_DEPENDS.hs-digest+=	hs-digest>=0.0.1
 BUILDLINK_PKGSRCDIR.hs-digest?=	../../wip/hs-digest
 .endif	# HS_DIGEST_BUILDLINK3_MK
 
