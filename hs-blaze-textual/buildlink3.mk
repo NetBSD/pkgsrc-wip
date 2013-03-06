c835740e71244dba5be2b880c32b5caff73526ff
PHO / phonohawk <phonohawk@users.sourceforge.net>
2013-03-06 15:13:21 +0000

    Upstream update to blaze-textual-0.2.0.8

--- hs-blaze-textual/buildlink3.mk
+++ hs-blaze-textual/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/09/27 15:13:01 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/03/06 15:13:21 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-blaze-textual
 
 .if !defined(HS_BLAZE_TEXTUAL_BUILDLINK3_MK)
 HS_BLAZE_TEXTUAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-blaze-textual+=	hs-blaze-textual>=0.2
+BUILDLINK_API_DEPENDS.hs-blaze-textual+=	hs-blaze-textual>=0.2.0
 BUILDLINK_PKGSRCDIR.hs-blaze-textual?=	../../wip/hs-blaze-textual
 
 .include "../../wip/hs-blaze-builder/buildlink3.mk"
