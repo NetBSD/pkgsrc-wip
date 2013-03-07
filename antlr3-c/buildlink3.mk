f436acb5e73873b517573ee59fada70e42e40217
Blue Rats <rodent@NetBSD.org>
2013-03-07 11:54:12 +0000

    Fix library name.

--- antlr3-c/buildlink3.mk
+++ antlr3-c/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/03/07 11:51:40 othyro Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/03/07 11:54:12 othyro Exp $
 
-BUILDLINK_TREE+=	libantlr3c
+BUILDLINK_TREE+=	antlr3-c
 
-.if !defined(LIBANTLR3C_BUILDLINK3_MK)
-LIBANTLR3C_BUILDLINK3_MK:=
+.if !defined(ANTLR3_C_BUILDLINK3_MK)
+ANTLR3_C_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libantlr3c+=	libantlr3c>=3.4
-BUILDLINK_PKGSRCDIR.libantlr3c?=	../../wip/antlr3-c
-.endif	# LIBANTLR3C_BUILDLINK3_MK
+BUILDLINK_API_DEPENDS.antlr3-c+=	antlr3-c>=3.4
+BUILDLINK_PKGSRCDIR.antlr3-c?=	../../wip/antlr3-c
+.endif	# ANTLR3_C_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-libantlr3c
+BUILDLINK_TREE+=	-antlr3-c
