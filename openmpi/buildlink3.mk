3ab1d143c52fbafef269caa11e7a9d307b37fe24
Aleksej Saushev <asau@users.sourceforge.net>
2013-03-08 18:39:26 +0000

    Export installation prefix to other packages.

--- openmpi/buildlink3.mk
+++ openmpi/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/01/25 03:22:09 asau Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/03/08 18:39:26 asau Exp $
 
 BUILDLINK_TREE+=	openmpi
 
@@ -7,6 +7,9 @@ OPENMPI_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.openmpi+=	openmpi>=1.5.1
 BUILDLINK_PKGSRCDIR.openmpi?=	../../wip/openmpi
+
+FIND_PREFIX:=	BUILDLINK_PREFIX.openmpi=openmpi
+.include "../../mk/find-prefix.mk"
 
 .include "../../parallel/hwloc/buildlink3.mk"
 .endif # OPENMPI_BUILDLINK3_MK
