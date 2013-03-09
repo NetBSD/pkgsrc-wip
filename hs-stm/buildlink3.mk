e4bcfc9455f9b14062dbc064073232219fdb5c65
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-03-09 22:32:47 +0000

    Changes in version 2.4.2
        Added Control.Concurrent.STM.TSem (transactional semaphore)
    
    Changes in version 2.4.1
    
        Added Applicative/Alternative instances of STM for GHC <7.0
    
    Changes in version 2.4
    
        Added Control.Concurrent.STM.TQueue (a faster TChan)
    
        Added Control.Concurrent.STM.TBQueue (a bounded channel based on TQueue)
    
        TChan has an Eq instances
    
        Added newBroadcastTChan and newBroadcastTChanIO
    
        Some performance improvements for TChan
    
        Added cloneTChan

--- hs-stm/buildlink3.mk
+++ hs-stm/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/07/27 06:25:18 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/03/09 22:32:47 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-stm
 
 .if !defined(HS_STM_BUILDLINK3_MK)
 HS_STM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-stm+=	hs-stm>=2.2
+BUILDLINK_API_DEPENDS.hs-stm+=	hs-stm>=2.4.2
 BUILDLINK_PKGSRCDIR.hs-stm?=	../../wip/hs-stm
 .endif	# HS_STM_BUILDLINK3_MK
 
