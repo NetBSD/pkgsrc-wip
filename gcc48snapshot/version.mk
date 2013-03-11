153f19a2c5eb5d3fdac53ebc5a1d31fef0c6a4af
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2013-03-11 14:42:31 +0000

    Update to 20130310 snapshot. Update patch-gcc_makefile.in from lang/gcc47.

--- gcc48snapshot/version.mk
+++ gcc48snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.3 2013/03/05 19:22:22 keckhardt Exp $
-GCC_DIST_VERSION:=4.8-20130303
+# $NetBSD: version.mk,v 1.4 2013/03/11 14:42:31 keckhardt Exp $
+GCC_DIST_VERSION:=4.8-20130310
