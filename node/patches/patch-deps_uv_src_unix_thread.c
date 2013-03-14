799d60522836d08b2355d211eae53e0dff7059ed
Hiramatsu Yoshifumi <jgoamakf@users.sourceforge.net>
2013-03-14 11:40:03 +0000

    Add comment.

--- node/patches/patch-deps_uv_src_unix_thread.c
+++ node/patches/patch-deps_uv_src_unix_thread.c
@@ -1,5 +1,6 @@
-$NetBSD: patch-deps_uv_src_unix_thread.c,v 1.1 2013/03/14 11:38:28 jgoamakf Exp $
+$NetBSD: patch-deps_uv_src_unix_thread.c,v 1.2 2013/03/14 11:40:03 jgoamakf Exp $
 
+Older NetBSD does not have pthread_condattr_setclock.
 --- deps/uv/src/unix/thread.c.orig	2013-03-14 11:27:01.000000000 +0900
 +++ deps/uv/src/unix/thread.c	2013-03-14 11:45:57.000000000 +0900
 @@ -283,8 +283,10 @@
