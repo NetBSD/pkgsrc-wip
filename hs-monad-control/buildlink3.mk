e931bf679549c274c9ece3c964c7b7a2f2fba4af
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-03-16 05:58:36 +0000

    Update to 0.3.2
    ver 0.3.2
      Added defaultLiftWith,defaultRestoreT

--- hs-monad-control/buildlink3.mk
+++ hs-monad-control/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/01/16 05:16:09 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/03/16 05:58:36 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-monad-control
 
 .if !defined(HS_MONAD_CONTROL_BUILDLINK3_MK)
 HS_MONAD_CONTROL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-monad-control+=	hs-monad-control>=0.3.1
+BUILDLINK_API_DEPENDS.hs-monad-control+=	hs-monad-control>=0.3.2
 BUILDLINK_PKGSRCDIR.hs-monad-control?=	../../wip/hs-monad-control
 
 .include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
