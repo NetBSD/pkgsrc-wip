b5d2b3000a4f4fd7fa628e01280428c1d7bb0fd1
Filip Hajny <filip@joyent.com>
2013-03-21 21:41:04 +0000

    2013.03.21, Version 0.10.1 (Stable) * npm: upgrade to 1.2.15 * crypto: Improve performance of non-stream APIs (Fedor Indutny) * tls: always reset this.ssl.error after handling (Fedor Indutny) * tls: Prevent mid-stream hangs (Fedor Indutny, isaacs) * net: improve arbitrary tcp socket support (Ben Noordhuis) * net: handle 'finish' event only after 'connect' (Fedor Indutny) * http: Don't hot-path end() for large buffers (isaacs) * fs: Missing cb errors are deprecated, not a throw (isaacs) * fs: make write/appendFileSync correctly set file mode (Raymond Feng) * stream: Return self from readable.wrap (isaacs) * stream: Never call decoder.end() multiple times (Gil Pedersen) * windows: enable watching signals with process.on('SIGXYZ') (Bert Belder) * node: revert removal of MakeCallback (Trevor Norris) * node: Unwrap without aborting in handle fd getter (isaacs)

--- node/patches/patch-tools_install.py
+++ node/patches/patch-tools_install.py
@@ -1,19 +1,10 @@
-$NetBSD: patch-tools_install.py,v 1.4 2013/03/13 13:06:33 fhajny Exp $
+$NetBSD: patch-tools_install.py,v 1.5 2013/03/21 21:41:04 fhajny Exp $
 
 Install man pages under the right directory.
 
---- tools/install.py.orig	2013-03-11 00:36:28.000000000 +0000
+--- tools/install.py.orig	2013-03-21 01:15:04.000000000 +0000
 +++ tools/install.py
-@@ -119,7 +119,7 @@ def npm_files(action):
-     assert(0) # unhandled action type
- 
- def files(action):
--  action(['doc/node.1'], 'share/man/man1/')
-+  action(['doc/node.1'], '@PKGMANDIR@/man1/')
-   action(['out/Release/node'], 'bin/node')
- 
-   # install unconditionally, checking if the platform supports dtrace doesn't
-@@ -130,7 +130,7 @@ def files(action):
+@@ -129,7 +129,7 @@ def files(action):
    if 'freebsd' in sys.platform or 'openbsd' in sys.platform:
      action(['doc/node.1'], 'man/man1/')
    else:
