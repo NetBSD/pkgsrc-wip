74216b8aca85937bffb7dee658310cf50bd84825
Jason Bacon <jwbacon@tds.net>
2013-03-22 13:57:12 +0000

    Clean up

--- armadillo/buildlink3.mk
+++ armadillo/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2012/11/14 00:26:22 outpaddling Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2013/03/22 13:57:12 outpaddling Exp $
 
 BUILDLINK_TREE+=	armadillo
 
 .if !defined(ARMADILLO_BUILDLINK3_MK)
 ARMADILLO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.armadillo+=	armadillo>=3.4.4
-BUILDLINK_ABI_DEPENDS.armadillo?=	armadillo>=3.4.4
+BUILDLINK_API_DEPENDS.armadillo+=	armadillo>=3.80.1
+BUILDLINK_ABI_DEPENDS.armadillo?=	armadillo>=3.80.1
 BUILDLINK_PKGSRCDIR.armadillo?=	../../wip/armadillo
 
 .include "../../math/blas/buildlink3.mk"
