e7c483e5d7798f6e742d31a19ad19f68153d7dd7
Jason Bacon <jwbacon@tds.net>
2013-03-23 14:53:14 +0000

    Bump version, fix man prefix

--- py-instant/buildlink3.mk
+++ py-instant/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2012/05/12 14:33:39 outpaddling Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/03/23 14:53:14 outpaddling Exp $
 
-BUILDLINK_TREE+=	instant
+BUILDLINK_TREE+=	py-instant
 
 .if !defined(PY_INSTANT_BUILDLINK3_MK)
 PY_INSTANT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.instant+=	${PYPKGPREFIX}-instant>=1.0.0
-BUILDLINK_PKGSRCDIR.instant?=	../../wip/py-instant
+BUILDLINK_API_DEPENDS.py-instant+=	${PYPKGPREFIX}-instant>=1.0.0
+BUILDLINK_PKGSRCDIR.py-instant?=	../../wip/py-instant
 .endif	# PY_INSTANT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-instant
+BUILDLINK_TREE+=	-py-instant
