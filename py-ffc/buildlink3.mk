cbf63afe5274e98aa929bb94cc3e65d63f5b0e66
Jason Bacon <jwbacon@tds.net>
2013-03-23 13:33:57 +0000

    Clean up pkglint issues

--- py-ffc/buildlink3.mk
+++ py-ffc/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2012/05/12 14:33:38 outpaddling Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/03/23 13:33:57 outpaddling Exp $
 
-BUILDLINK_TREE+=	ffc
+BUILDLINK_TREE+=	py-ffc
 
 .if !defined(PY_FFC_BUILDLINK3_MK)
 PY_FFC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ffc+=	${PYPKGPREFIX}-ffc>=1.0.0
-BUILDLINK_PKGSRCDIR.ffc?=	../../wip/py-ffc
+BUILDLINK_API_DEPENDS.py-ffc+=	${PYPKGPREFIX}-ffc>=1.0.0
+BUILDLINK_PKGSRCDIR.py-ffc?=	../../wip/py-ffc
 .endif	# PY_FFC_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-ffc
+BUILDLINK_TREE+=	-py-ffc
