e06ef8b10455cac38a9cf6d6e5d721a0c1178a97
Blue Rats <rodent@NetBSD.org>
2013-03-28 21:25:06 +0000

    Removed .else block for the doc option. This might have been making the bulk build fail.

--- uprof/options.mk
+++ uprof/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2013/03/10 00:12:56 othyro Exp $
+# $NetBSD: options.mk,v 1.2 2013/03/28 21:25:06 othyro Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.uprof
 PKG_SUPPORTED_OPTIONS=	doc
@@ -12,7 +12,4 @@ PLIST_VARS+=		doc
 CONFIGURE_ARGS+=	--enable-gtk-doc=yes --enable-gtk-doc-html=yes
 CONFIGURE_ARGS+=	--enable-gtk-doc-pdf=yes
 PLIST.doc=		yes
-.else
-CONFIGURE_ARGS+=	--enable-gtk-doc=no --enable-gtk-doc-html=no
-CONFIGURE_ARGS+=	--enable-gtk-doc-pdf=no
 .endif
