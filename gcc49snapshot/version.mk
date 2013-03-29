d98e4e8ed2cb2959192903f27f29008332506e5b
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2013-03-29 15:55:55 +0000

    Revert to 20130319. Snapshot 20130324 builds on NetBSD 6.0 i386 but fails on current amd64 with the same errors as reported for FreeBSD in PR 56714 (always_inline in libitm).

--- gcc49snapshot/version.mk
+++ gcc49snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.2 2013/03/26 19:17:22 keckhardt Exp $
-GCC_DIST_VERSION:=4.9-20130324
+# $NetBSD: version.mk,v 1.3 2013/03/29 15:55:55 keckhardt Exp $
+GCC_DIST_VERSION:=4.9-20130319
