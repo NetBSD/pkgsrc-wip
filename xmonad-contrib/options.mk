6ea1ada74d0806d104582519e8eb51e37241fbdc
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-03-31 00:59:49 +0000

    Makefile:   fix a mismatch between `lintpkgsrc -i` and package name, sorry. options.mk:   change for option description.

--- xmonad-contrib/options.mk
+++ xmonad-contrib/options.mk
@@ -1,12 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2013/01/05 14:40:49 szptvlfn Exp $
+# $NetBSD: options.mk,v 1.2 2013/03/31 00:59:49 szptvlfn Exp $
 
 PKG_OPTIONS_VAR=    PKG_OPTIONS.xmonad-contrib
-PKG_SUPPORTED_OPTIONS= X11-xft
-PKG_SUGGESTED_OPTIONS= X11-xft
+PKG_SUPPORTED_OPTIONS= xft2
+PKG_SUGGESTED_OPTIONS= xft2
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:MX11-xft)
+.if !empty(PKG_OPTIONS:Mxft2)
 .include "../../wip/hs-x11-xft/buildlink3.mk"
 .else
 post-configure:
