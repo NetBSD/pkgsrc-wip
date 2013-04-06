58d2c82b2434bdfd56b98ff1bd5b59f1ab473ff9
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2013-04-06 16:42:39 +0000

    Update to 20130404. Add test_summary.log

--- gcc48snapshot/version.mk
+++ gcc48snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.7 2013/04/03 11:47:25 keckhardt Exp $
-GCC48S_DIST_VERSION:=4.8-20130328
+# $NetBSD: version.mk,v 1.8 2013/04/06 16:42:39 keckhardt Exp $
+GCC48S_DIST_VERSION:=4.8-20130404
