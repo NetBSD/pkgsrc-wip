9f849c2494b4e362b39d75c2fb346fcd94efd194
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-04-12 19:26:31 +0000

    Update to 3.0.2

--- hs-comonad/buildlink3.mk
+++ hs-comonad/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/02/01 16:03:07 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/04/12 19:26:31 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-comonad
 
 .if !defined(HS_COMONAD_BUILDLINK3_MK)
 HS_COMONAD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-comonad+=	hs-comonad>=1.1.1
+BUILDLINK_API_DEPENDS.hs-comonad+=	hs-comonad>=3.0.2
 BUILDLINK_PKGSRCDIR.hs-comonad?=	../../wip/hs-comonad
 
 .include "../../wip/hs-semigroups/buildlink3.mk"
