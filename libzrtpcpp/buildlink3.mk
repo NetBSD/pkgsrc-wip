7182f02464dcdca89f75a81a246c1527faa27e91
Blue Rats <rodent@NetBSD.org>
2013-04-15 01:19:06 +0000

    Remove buildlink3.mk inclusion for openssl, as this dependency is found automatically by including ccrtp's buildlink3.mk. Comment inclusion of commoncpp2's buildlink3.mk. It's not needed for this package; however further testing with twinkle is required.

--- libzrtpcpp/buildlink3.mk
+++ libzrtpcpp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2010/01/17 12:03:28 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/04/15 01:19:06 othyro Exp $
 
 BUILDLINK_TREE+=	libzrtpcpp
 
@@ -10,8 +10,7 @@ BUILDLINK_ABI_DEPENDS.libzrtpcpp?=	libzrtpcpp>=1.4.6nb1
 BUILDLINK_PKGSRCDIR.libzrtpcpp?=	../../wip/libzrtpcpp
 
 .include "../../wip/ccrtp/buildlink3.mk"
-.include "../../wip/commoncpp2/buildlink3.mk"
-.include "../../security/openssl/buildlink3.mk"
+#.include "../../wip/commoncpp2/buildlink3.mk"
 .endif # LIBZRTPCPP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libzrtpcpp
