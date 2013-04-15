cfd52b34fd6bfb477e9a4542e84d0bceedcf0994
Blue Rats <rodent@NetBSD.org>
2013-04-15 01:09:13 +0000

    Include buildlink3.mk files for pthread, dlopen, and openssl, after analysis on dynamic object dependencies and installed headers.

--- ucommon/buildlink3.mk
+++ ucommon/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2011/10/29 01:11:26 othyro Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/04/15 01:09:13 othyro Exp $
 
 BUILDLINK_TREE+=	ucommon
 
@@ -8,6 +8,9 @@ UCOMMON_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.ucommon+=	ucommon>=5.0.6
 BUILDLINK_PKGSRCDIR.ucommon?=	../../wip/ucommon
 
+.include "../../mk/dlopen.buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
 .endif # UCOMMON_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ucommon
