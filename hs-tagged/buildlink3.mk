66c11515598d324d5582e3786f92e70a2da288a0
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-04-16 11:51:45 +0000

    Update to 0.4.5
    CHANGELOG:
    0.4.5
      Added witness
    0.4.4
      Actually working polymorphic kind support
    0.4.3
      Added polymorphic kind support

--- hs-tagged/buildlink3.mk
+++ hs-tagged/buildlink3.mk
@@ -1,15 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.3 2011/12/01 23:05:07 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/04/16 11:51:45 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-tagged
 
 .if !defined(HS_TAGGED_BUILDLINK3_MK)
 HS_TAGGED_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tagged+=	hs-tagged>=0.2.3
+BUILDLINK_API_DEPENDS.hs-tagged+=	hs-tagged>=0.4.5
 BUILDLINK_PKGSRCDIR.hs-tagged?=	../../wip/hs-tagged
 
-.include "../../wip/hs-data-default/buildlink3.mk"
-.include "../../wip/hs-semigroups/buildlink3.mk"
 .endif	# HS_TAGGED_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-tagged
