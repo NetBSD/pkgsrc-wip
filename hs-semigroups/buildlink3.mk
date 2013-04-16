c2a5e245d0863f9ee7d06fa5b267274d2ecfabad
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-04-16 22:26:01 +0000

    Update to 0.9.1
    CHANGELOG:
    0.9.1
      Added timesN.
      Rename `times` to `timesN` to avoid name-clashing with another library.

--- hs-semigroups/buildlink3.mk
+++ hs-semigroups/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/02/23 15:23:18 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/04/16 22:26:01 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-semigroups
 
 .if !defined(HS_SEMIGROUPS_BUILDLINK3_MK)
 HS_SEMIGROUPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.9
+BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.9.1
 BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../wip/hs-semigroups
 
 .include "../../wip/hs-nats/buildlink3.mk"
