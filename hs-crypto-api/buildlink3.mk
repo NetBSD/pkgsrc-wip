ed2520f8099431002bdae7163fad4b177ca626fa
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-04-16 11:06:42 +0000

    Update to 0.11
    CHANGELOG:
    0.10.2
      Update bytestring dep
      Import CInt constructor because GHC HEAD is evidently inferior to 7.4 in some manner
    0.10.1
      Updated build deps
    0.10
      Add Typeable and Exception instances to GenError in response to users.
      Added throw left

--- hs-crypto-api/buildlink3.mk
+++ hs-crypto-api/buildlink3.mk
@@ -1,17 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/03/04 08:14:38 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/04/16 11:06:42 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-crypto-api
 
 .if !defined(HS_CRYPTO_API_BUILDLINK3_MK)
 HS_CRYPTO_API_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-crypto-api+=	hs-crypto-api>=0.9
+BUILDLINK_API_DEPENDS.hs-crypto-api+=	hs-crypto-api>=0.11
 BUILDLINK_PKGSRCDIR.hs-crypto-api?=	../../wip/hs-crypto-api
 
-.include "../../wip/hs-cereal/buildlink3.mk"
-.include "../../wip/hs-entropy/buildlink3.mk"
-.include "../../wip/hs-largeword/buildlink3.mk"
-.include "../../wip/hs-tagged/buildlink3.mk"
 .endif	# HS_CRYPTO_API_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-crypto-api
