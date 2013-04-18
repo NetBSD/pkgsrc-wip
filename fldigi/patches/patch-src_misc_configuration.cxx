8131bd748ebcd1338c82f95e1d1bfd654509e22f
Makoto Fujiwara <makoto@ki.nu>
2013-04-18 13:48:28 +0000

    Bump version 3.21.69 to 3.21.70 ---------- Sat Apr 13 14:48:19 2013 -0500     Version 3.21.70       * Maintenance release
    Sat Apr 13 17:51:18 2013 -0500
        Contestia/Olivia
          * Quick change / custom dialog
          * Corrected Contestia callback in confdialog.fl
          * Corrected resetOLIVIA function
    
    Sat Apr 13 14:36:19 2013 -0500
        Tabs Colors
          * Added ID / QRZ tabs to color schema
    
    Sat Apr 13 07:17:48 2013 -0500, Author: Arvo Järve <arvo@softshark.ee>
        Double Click bug fix
          * Double click text selection for transfer to QSO record fields
    
    Fri Apr 12 18:09:26 2013 -0500
        RTTY update
          * Removed code used for filter tests
          * Added CWI suppression, select decoding of
            - Mark-Space
            - Mark only
            - Space only
    ----------

--- fldigi/patches/patch-src_misc_configuration.cxx
+++ fldigi/patches/patch-src_misc_configuration.cxx
@@ -1,4 +1,4 @@
-$NetBSD: patch-src_misc_configuration.cxx,v 1.1 2013/04/15 05:52:56 makoto Exp $
+$NetBSD: patch-src_misc_configuration.cxx,v 1.2 2013/04/18 13:48:28 makoto Exp $
 
 For DragonFly build to fix following problem:
 misc/configuration.cxx: In member function 'void configuration::testCommPorts()':
@@ -6,12 +6,12 @@ misc/configuration.cxx:934:2: error: 'glob_t' was not declared in this scope
 misc/configuration.cxx:934:9: error: expected ';' before 'gbuf'
 gmake[3]: *** [fldigi-configuration.o] Error 1
 
---- src/misc/configuration.cxx.orig	2013-04-05 03:46:42.000000000 +0900
-+++ src/misc/configuration.cxx	2013-04-15 03:53:20.000000000 +0900
-@@ -927,7 +927,7 @@ void configuration::testCommPorts()
+--- src/misc/configuration.cxx.orig	2013-04-12 23:24:15.000000000 +0900
++++ src/misc/configuration.cxx	2013-04-18 21:29:15.000000000 +0900
+@@ -925,7 +925,7 @@ void configuration::testCommPorts()
+ 
+ #if defined(__WOE32__)
  #  define TTY_MAX 255
- #elif defined(__APPLE__)
- 	glob_t gbuf;
 -#elif defined(__OpenBSD__) || defined(__NetBSD__)
 +#elif defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
  #  define TTY_MAX 4
