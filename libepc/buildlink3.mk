e102690fb0e1b311927fd9ca5cdfc19b496add1e
Blue Rats <rodent@NetBSD.org>
2013-04-18 02:19:33 +0000

    Use devel/libuuid instead of ancient version in wip/. Add all required dependencies to Makefile and buildlink3.mk. Ready to import back into HEAD, wiz@.

--- libepc/buildlink3.mk
+++ libepc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2010/06/13 22:47:10 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/04/18 02:19:33 othyro Exp $
 
 BUILDLINK_TREE+=	libepc
 
@@ -9,7 +9,56 @@ BUILDLINK_API_DEPENDS.libepc+=	libepc>=0.3.11
 BUILDLINK_ABI_DEPENDS.libepc?=	libepc>=0.3.11nb1
 BUILDLINK_PKGSRCDIR.libepc?=	../../wip/libepc
 
+.include "../../devel/at-spi2-core/buildlink3.mk"
+.include "../../devel/atk/buildlink3.mk"
+.include "../../devel/at-spi2-atk/buildlink3.mk"
+.include "../../x11/libXcomposite/buildlink3.mk"
+.include "../../x11/libXcursor/buildlink3.mk"
+.include "../../x11/libXrandr/buildlink3.mk"
+.include "../../x11/libXi/buildlink3.mk"
+.include "../../x11/libXinerama/buildlink3.mk"
+.include "../../x11/libdrm/buildlink3.mk"
+.include "../../x11/libXfixes/buildlink3.mk"
+.include "../../x11/libXdamage/buildlink3.mk"
+.include "../../x11/libXxf86vm/buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../graphics/cairo-gobject/buildlink3.mk"
+.include "../../graphics/gdk-pixbuf2/buildlink3.mk"
+.include "../../fonts/harfbuzz/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXrender/buildlink3.mk"
+.include "../../x11/libXdmcp/buildlink3.mk"
+.include "../../x11/libXau/buildlink3.mk"
+.include "../../x11/libxcb/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../textproc/expat/buildlink3.mk"
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../x11/pixman/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.include "../../mk/dlopen.buildlink3.mk"
+#.include "../../textproc/gtk-doc/buildlink3.mk"
+.include "../../archivers/xz/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../devel/libffi/buildlink3.mk"
 .include "../../net/libsoup24/buildlink3.mk"
+.include "../../devel/libuuid/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../devel/gmp/buildlink3.mk"
+.include "../../security/nettle/buildlink3.mk"
+.include "../../security/libtasn1/buildlink3.mk"
+.include "../../security/gnutls/buildlink3.mk"
+.include "../../devel/pcre/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+.include "../../sysutils/dbus/buildlink3.mk"
+.include "../../net/avahi/buildlink3.mk"
 .endif	# LIBEPC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libepc
