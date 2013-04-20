fd8351ab590599bd44d2cb7ed593ee9bb7317a0f
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-04-20 04:35:40 +0000

    Update to 0.4
    CHANGELOG:
    0.4
      Added instance Contravariant Proxy
    
    0.3
      Added Backwards and Reverse instances for transformers 0.3
      Added instance (Functor f, Contravariant g) => Contravariant (Compose f g).
      (This is non-canonical, but is necessary to support other packages.)
      Added Functor instances to ComposeFC and ComposeCF for use when modeling phantom type parameters caused mixing Functor + Contravariant.
    
    0.2
      added missing category instance. breaks API though, so required major version bump

--- hs-contravariant/buildlink3.mk
+++ hs-contravariant/buildlink3.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/02/01 16:11:36 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/04/20 04:35:40 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-contravariant
 
 .if !defined(HS_CONTRAVARIANT_BUILDLINK3_MK)
 HS_CONTRAVARIANT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-contravariant+=	hs-contravariant>=0.1.3
+BUILDLINK_API_DEPENDS.hs-contravariant+=	hs-contravariant>=0.4
 BUILDLINK_PKGSRCDIR.hs-contravariant?=	../../wip/hs-contravariant
 
-.include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_CONTRAVARIANT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-contravariant
