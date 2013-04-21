67735eaa6493a29f11e288be25cc9ca14dab43ca
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-04-21 19:27:32 +0000

    Update to 0.3.0.0
    Changes:
    0.3.0.0
      Support transformers-0.3
    0.2.2.3
      Change license to BSD3 (Takusen)
    0.2.2.1
      Add warnings to MonadCatchIO instances for ContT and ErrorT

--- hs-MonadCatchIO-transformers/buildlink3.mk
+++ hs-MonadCatchIO-transformers/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/11/02 16:50:07 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/04/21 19:27:32 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-MonadCatchIO-transformers
 
 .if !defined(HS_MONADCATCHIO_TRANSFORMERS_BUILDLINK3_MK)
 HS_MONADCATCHIO_TRANSFORMERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-MonadCatchIO-transformers+=	hs-MonadCatchIO-transformers>=0.2.2.0
+BUILDLINK_API_DEPENDS.hs-MonadCatchIO-transformers+=	hs-MonadCatchIO-transformers>=0.3
 BUILDLINK_PKGSRCDIR.hs-MonadCatchIO-transformers?=	../../wip/hs-MonadCatchIO-transformers
 
 .include "../../wip/hs-transformers/buildlink3.mk"
