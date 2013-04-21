54d2d4b7307bc1a28e95f1a252963f226cac6970
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-04-21 22:21:31 +0000

    Update to 0.6
    CHANGELOG:
    0.6
    ---
    * instance Exception Void
    * instance Generic Void
    * instance Hashable Void

--- hs-void/buildlink3.mk
+++ hs-void/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/03/09 12:52:03 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/04/21 22:21:31 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-void
 
 .if !defined(HS_VOID_BUILDLINK3_MK)
 HS_VOID_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-void+=	hs-void>=0.5.12
+BUILDLINK_API_DEPENDS.hs-void+=	hs-void>=0.6
 BUILDLINK_PKGSRCDIR.hs-void?=	../../wip/hs-void
 
 .endif	# HS_VOID_BUILDLINK3_MK
