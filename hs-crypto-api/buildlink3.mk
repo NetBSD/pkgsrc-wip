68146b11c9accc2d2efd929cd9eda26f14861fc3
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-04-24 01:02:15 +0000

    Update to 0.12
    changes:
    0.12
      Fix compile errors/warnings due to imports.
      Build-Depends; Remove upper bounds

--- hs-crypto-api/buildlink3.mk
+++ hs-crypto-api/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/04/16 11:06:42 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/04/24 01:02:15 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-crypto-api
 
 .if !defined(HS_CRYPTO_API_BUILDLINK3_MK)
 HS_CRYPTO_API_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-crypto-api+=	hs-crypto-api>=0.11
+BUILDLINK_API_DEPENDS.hs-crypto-api+=	hs-crypto-api>=0.12
 BUILDLINK_PKGSRCDIR.hs-crypto-api?=	../../wip/hs-crypto-api
 
 .endif	# HS_CRYPTO_API_BUILDLINK3_MK
