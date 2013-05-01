ea4147620991a791b791621fa8b216e891311365
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-05-01 12:50:32 +0000

    Update to 1.0.2
    CHANGES:
    1.0.2
      Modified for Dependencies
      Import CInt and CSize constructors
    
    1.0.1
      Modified for Dependencies

--- hs-cautious-file/buildlink3.mk
+++ hs-cautious-file/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 15:19:24 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/05/01 12:50:32 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cautious-file
 
 .if !defined(HS_CAUTIOUS_FILE_BUILDLINK3_MK)
 HS_CAUTIOUS_FILE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cautious-file+=	hs-cautious-file>=1.0
+BUILDLINK_API_DEPENDS.hs-cautious-file+=	hs-cautious-file>=1.0.2
 BUILDLINK_PKGSRCDIR.hs-cautious-file?=	../../wip/hs-cautious-file
 .endif	# HS_CAUTIOUS_FILE_BUILDLINK3_MK
 
