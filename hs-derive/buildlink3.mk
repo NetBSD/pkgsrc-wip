f6d5c93c6ed67a7bb8384dc2f9e5a95555311672
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-05-06 19:18:22 +0000

    Update to 2.5.11
    CHANGES:
    2.5.11
        Support GHC 7.6
    2.5.10
        Add derivation for Lens
        Modify the Typeable derivation to use mkTyCon3
    2.5.9
        Support the Template Haskell Unpacked constructor
    2.5.8
        Allow haskell-src-exts-1.13.*
    2.5.7
        Allow haskell-src-exts-1.12.*
    2.5.6
        Update the copyright year
        Allow transformers-0.3.*

--- hs-derive/buildlink3.mk
+++ hs-derive/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2012/03/04 07:00:01 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/05/06 19:18:22 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-derive
 
 .if !defined(HS_DERIVE_BUILDLINK3_MK)
 HS_DERIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-derive+=	hs-derive>=2.5.5
+BUILDLINK_API_DEPENDS.hs-derive+=	hs-derive>=2.5.11
 BUILDLINK_PKGSRCDIR.hs-derive?=	../../wip/hs-derive
 
 .include "../../wip/hs-haskell-src-exts/buildlink3.mk"
