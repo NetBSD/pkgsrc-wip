8009d0d25b6e310ccaef0c08e5a0fb3cbe1b9b81
Blue Rats <rodent@NetBSD.org>
2013-05-06 17:00:26 +0000

    List only dependencies which are linked by one of the libraries or included in one of the headers.

--- libkate/buildlink3.mk
+++ libkate/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/03/03 00:01:24 othyro Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/05/06 17:00:26 othyro Exp $
 
 BUILDLINK_TREE+=	libkate
 
@@ -11,15 +11,7 @@ BUILDLINK_PKGSRCDIR.libkate?=	../../wip/libkate
 pkgbase := libkate
 .include "../../mk/pkg-build-options.mk"
 
-.if !empty(PKG_BUILD_OPTIONS.libkate:Moggz)
-.include "../../wip/liboggz/buildlink3.mk"
-.endif
-
-.include "../../x11/py-wxWidgets/buildlink3.mk"
-.include "../../lang/python/application.mk"
-.include "../../lang/python/extension.mk"
 .include "../../mk/dlopen.buildlink3.mk"
-.include "../../graphics/png/buildlink3.mk"
 .include "../../multimedia/libogg/buildlink3.mk"
 .endif	# LIBKATE_BUILDLINK3_MK
 
