20a642183776e46c4aa421fc5b220137c7e98a0f
Blue Rats <rodent@NetBSD.org>
2013-05-08 23:41:05 +0000

    Add alsa to PKG_SUGGESTION_OPTIONS (it's supposed to be optional, but isn't  - will fix this).

--- dolphin/options.mk
+++ dolphin/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.2 2013/05/08 02:53:13 othyro Exp $
+# $NetBSD: options.mk,v 1.3 2013/05/08 23:41:05 othyro Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dolphin
 PKG_SUPPORTED_OPTIONS=	alsa bluez cg debug ffmpeg glew gtk2 libao openal opengl
 PKG_SUPPORTED_OPTIONS+=	portaudio pulseaudio sdl sdl2 sfml+ soil threads xrandr
 PKG_SUPPORTED_OPTIONS+=	x11 zlib
-PKG_SUGGESTED_OPTIONS+=	ffmpeg glew gtk2 libao openal opengl pulseaudio sdl sdl2
+PKG_SUGGESTED_OPTIONS+=	alsa ffmpeg glew gtk2 libao openal pulseaudio sdl sdl2
 PKG_SUGGESTED_OPTIONS+=	sfml+ threads xrandr x11 zlib
 
 # XXX wxGTK29 (wxUnicode) is not yet supported. Package and test.
