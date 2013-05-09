33ea20d42d5c1a75b6cb38e07a40ab8255c87c2d
Blue Rats <rodent@NetBSD.org>
2013-05-09 00:24:09 +0000

    Build with wip/portaudio instead of audio/portaudio. Requires libportaudio2 for microphone support. Added soil to PKG_SUGGESTED_OPTIONS.

--- dolphin/options.mk
+++ dolphin/options.mk
@@ -1,11 +1,11 @@
-# $NetBSD: options.mk,v 1.3 2013/05/08 23:41:05 othyro Exp $
+# $NetBSD: options.mk,v 1.4 2013/05/09 00:24:09 othyro Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dolphin
 PKG_SUPPORTED_OPTIONS=	alsa bluez cg debug ffmpeg glew gtk2 libao openal opengl
 PKG_SUPPORTED_OPTIONS+=	portaudio pulseaudio sdl sdl2 sfml+ soil threads xrandr
 PKG_SUPPORTED_OPTIONS+=	x11 zlib
-PKG_SUGGESTED_OPTIONS+=	alsa ffmpeg glew gtk2 libao openal pulseaudio sdl sdl2
-PKG_SUGGESTED_OPTIONS+=	sfml+ threads xrandr x11 zlib
+PKG_SUGGESTED_OPTIONS+=	alsa ffmpeg glew gtk2 libao openal portaudio pulseaudio
+PKG_SUGGESTED_OPTIONS+=	sdl sdl2 sfml+ soil threads xrandr x11 zlib
 
 # XXX wxGTK29 (wxUnicode) is not yet supported. Package and test.
 
@@ -55,7 +55,7 @@ CMAKE_ARGS+=		-DENCODE_FRAMEDUMPS=OFF
 .endif
 
 .if !empty(PKG_OPTIONS:Mportaudio)
-.include "../../audio/portaudio/buildlink3.mk"
+.include "../../wip/portaudio/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mpulseaudio)
