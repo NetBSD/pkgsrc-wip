2e530ea48e1493f6b907c058d1caf9a11abd4d2f
Makoto Fujiwara <makoto@ki.nu>
2013-05-11 14:04:22 +0000

    Import nspr-4.9.6 as wip/nspr. (Just lazy not upgrading pkgsrc proper, although it may be simple.)
    Netscape Portable Runtime (NSPR) provides a platform-neutral API for system
    level and libc like functions. The API is used in the Mozilla client, many of
    Netscape/AOL/iPlanet's and other software offerings.

---
+++ nspr/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.4 2013/05/11 14:04:23 makoto Exp $
+
+BUILDLINK_TREE+=	nspr
+
+.if !defined(NSPR_BUILDLINK3_MK)
+NSPR_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.9.3.2
+BUILDLINK_ABI_DEPENDS.nspr+=	nspr>=4.9.2beta1
+BUILDLINK_PKGSRCDIR.nspr?=	../../devel/nspr
+
+BUILDLINK_FILES.nspr+=          lib/nspr/*
+BUILDLINK_FILES.nspr+=          include/nspr/*
+BUILDLINK_FILES.nspr+=          include/nspr/obsolete/*
+
+BUILDLINK_LIBDIRS.nspr+=	lib/nspr
+BUILDLINK_RPATHDIRS.nspr+=	lib/nspr
+
+BUILDLINK_INCDIRS.nspr+=	include/nspr
+
+.endif	# NSPR_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-nspr
---
+++ nspr/patches/patch-nsprpub_pr_src_md_unix_uxrng.c
@@ -0,0 +1,13 @@
+$NetBSD: patch-nsprpub_pr_src_md_unix_uxrng.c,v 1.1 2013/05/11 14:04:23 makoto Exp $
+
+--- nsprpub/pr/src/md/unix/uxrng.c.orig	2012-05-18 21:55:29.000000000 +0000
++++ nsprpub/pr/src/md/unix/uxrng.c
+@@ -94,7 +94,7 @@ GetHighResClock(void *buf, size_t maxbyt
+ 
+ #elif (defined(LINUX) || defined(FREEBSD) || defined(__FreeBSD_kernel__) \
+     || defined(NETBSD) || defined(__NetBSD_kernel__) || defined(OPENBSD) \
+-    || defined(SYMBIAN) || defined(__GNU__))
++    || defined(SYMBIAN) || defined(__GNU__)) || defined(__DragonFly__)
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <fcntl.h>
