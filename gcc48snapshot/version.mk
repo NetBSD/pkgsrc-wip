2d3f2bccae33d580bd27d8497581d6a496eb3f5b
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2013-05-14 15:56:28 +0000

    Update to snapshot 20130509.

--- gcc48snapshot/version.mk
+++ gcc48snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.12 2013/05/03 19:40:33 keckhardt Exp $
-GCC48S_DIST_VERSION:=4.8-20130502
+# $NetBSD: version.mk,v 1.13 2013/05/14 15:56:28 keckhardt Exp $
+GCC48S_DIST_VERSION:=4.8-20130509
