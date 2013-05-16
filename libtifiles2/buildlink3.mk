6276694a9802a18c9cad4d049f005a5510622356
Blue Rats <rodent@NetBSD.org>
2013-05-16 10:56:24 +0000

    Moved gettext-lib to global dependency rather than PKG_OPTION (NLS).

--- libtifiles2/buildlink3.mk
+++ libtifiles2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/12/30 01:46:38 othyro Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/05/16 10:56:24 othyro Exp $
 
 BUILDLINK_TREE+=	libtifiles2
 
@@ -12,10 +12,7 @@ pkgbase := libtifiles2
 .include "../../mk/pkg-build-options.mk"
 
 
-.if !empty(PKG_BUILD_OPTIONS.libtifiles2:Mnls)
 .include "../../devel/gettext-lib/buildlink3.mk"
-.endif
-
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../wip/libticonv/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
--- libtifiles2/options.mk
+++ libtifiles2/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2012/12/30 01:46:38 othyro Exp $
+# $NetBSD: options.mk,v 1.2 2013/05/16 10:56:24 othyro Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libtifiles2
 PKG_SUPPORTED_OPTIONS=	nls
@@ -8,7 +8,6 @@ PLIST_VARS+=		nls
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mnls)
-.include "../../devel/gettext-lib/buildlink3.mk"
 PLIST.nls=		yes
 .else
 CONFIGURE_ARGS+=	--disable-nls
