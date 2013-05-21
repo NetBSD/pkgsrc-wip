2723777c48d19384ce43287048de549da53a28eb
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-05-21 11:11:15 +0000

    Update to 0.9.2
    CHANGELOG:
    0.9.2
      Added a DefaultSignature for (<>) in terms of mappend

--- hs-semigroups/buildlink3.mk
+++ hs-semigroups/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/04/16 22:26:01 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/05/21 11:11:15 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-semigroups
 
 .if !defined(HS_SEMIGROUPS_BUILDLINK3_MK)
 HS_SEMIGROUPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.9.1
+BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.9.2
 BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../wip/hs-semigroups
 
 .include "../../wip/hs-nats/buildlink3.mk"
