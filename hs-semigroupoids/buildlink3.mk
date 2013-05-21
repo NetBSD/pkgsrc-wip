d9843c25bc49f7894172d3086b418be29bc3bdac
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-05-21 12:09:03 +0000

    Update to 3.0.2
    CHANGELOG:
    3.0.2
      * Tightened the upper bounds slightly to enable PVP compliance while
      retaining a flexible development cycle
      * Raised the upper bound on `contravariant`
    3.0.1
      * Refactored directory layout

--- hs-semigroupoids/buildlink3.mk
+++ hs-semigroupoids/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/02/01 16:28:59 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/05/21 12:09:03 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-semigroupoids
 
 .if !defined(HS_SEMIGROUPOIDS_BUILDLINK3_MK)
 HS_SEMIGROUPOIDS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=1.2.6
+BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=3.0.2
 BUILDLINK_PKGSRCDIR.hs-semigroupoids?=	../../wip/hs-semigroupoids
 
 .include "../../wip/hs-comonad/buildlink3.mk"
