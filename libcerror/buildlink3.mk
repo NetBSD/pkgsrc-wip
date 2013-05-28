ecdf4399dde545c2bd746ff3b2377e3e26ece2c6
Blue Rats <rodent@NetBSD.org>
2013-05-28 12:14:20 +0000

    Updated to latest release. Removed PKG_BUILD_OPTIONS from buildlink3.mk, as library doesn't link against libintl with NLS option. From ChangeLog:
    * added FORMAT_MESSAGE_IGNORE_INSERTS to FormatMessage calls
    * textual changes
    * updated dependencies
    * updated msvscpp files
    * changes to .gitignore

--- libcerror/buildlink3.mk
+++ libcerror/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/01/10 15:43:51 othyro Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/05/28 12:14:20 othyro Exp $
 
 BUILDLINK_TREE+=	libcerror
 
@@ -7,14 +7,6 @@ LIBCERROR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libcerror+=	libcerror>=20121222
 BUILDLINK_PKGSRCDIR.libcerror?=	../../wip/libcerror
-
-pkgbase := libcerror
-.include "../../mk/pkg-build-options.mk"
-
-
-.if !empty(PKG_BUILD_OPTIONS.libcerror:Mnls)
-.include "../../devel/gettext-lib/buildlink3.mk"
-.endif
 
 .endif	# LIBCERROR_BUILDLINK3_MK
 
