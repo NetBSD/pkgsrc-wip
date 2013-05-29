fb4bd5c6c0ec5dc1a8e7c185b6d1778168b34480
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2013-05-29 05:42:16 +0000

    Update to 4.8.1 release candidate 20130527. Add ctype patches from lang/gcc48.

--- gcc48snapshot/version.mk
+++ gcc48snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.13 2013/05/14 15:56:28 keckhardt Exp $
-GCC48S_DIST_VERSION:=4.8-20130509
+# $NetBSD: version.mk,v 1.14 2013/05/29 05:42:17 keckhardt Exp $
+GCC48S_DIST_VERSION:=4.8.1-RC-20130527
