cdc3e5d14a9ce432b8dffbcdafb7e7422911f209
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2013-05-29 17:48:17 +0000

    Update to snapshot 20130526. Add ctype patches from lang/gcc48.

--- gcc49snapshot/version.mk
+++ gcc49snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.7 2013/05/14 18:52:58 keckhardt Exp $
-GCC49S_DIST_VERSION:=4.9-20130512
+# $NetBSD: version.mk,v 1.8 2013/05/29 17:48:17 keckhardt Exp $
+GCC49S_DIST_VERSION:=4.9-20130526
