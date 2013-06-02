143a39f53fc6107b484197f35719c1de9a671135
Pierre Pronchery <khorben@defora.org>
2013-06-02 14:43:38 +0000

    Fix building against wip's Mesa packages - otherwise pkg_install complains when checking for dependencies
    "works for me"

--- MesaLib/buildlink3.mk
+++ MesaLib/buildlink3.mk
@@ -1,11 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2010/01/03 15:52:45 pallegra Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2013/06/02 14:43:38 khorben Exp $
 
 BUILDLINK_TREE+=	MesaLib
 
 .if !defined(MESALIB_BUILDLINK3_MK)
 MESALIB_BUILDLINK3_MK:=
 
-#BUILDLINK_API_DEPENDS.MesaLib+=	MesaLib>=7.7
+BUILDLINK_API_DEPENDS.MesaLib+=	MesaLib>=3.4.2
+BUILDLINK_ABI_DEPENDS.MesaLib+=	MesaLib>=7.7
 BUILDLINK_PKGSRCDIR.MesaLib?=	../../wip/MesaLib
 
 .include "../../wip/dri/buildlink3.mk"
