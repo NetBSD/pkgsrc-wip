faaed2bfc7f501e6e375280479b99fdcf7e96e02
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-06-04 20:01:34 +0000

    fix patch

--- opensmtpd/patches/patch-smtpd_queue_fsqueue.c
+++ opensmtpd/patches/patch-smtpd_queue_fsqueue.c
@@ -1,17 +1,18 @@
-$NetBSD: patch-smtpd_queue_fsqueue.c,v 1.1 2013/05/26 20:34:34 szptvlfn Exp $
+$NetBSD: patch-smtpd_queue_fsqueue.c,v 1.2 2013/06/04 20:01:34 szptvlfn Exp $
 
 NetBSD use statvfs, not statfs.
 
 --- smtpd/queue_fsqueue.c.orig	2013-05-16 15:19:59.000000000 +0000
 +++ smtpd/queue_fsqueue.c
-@@ -28,6 +28,10 @@
+@@ -26,6 +26,12 @@
+ #ifdef HAVE_SYS_STATFS_H
+ #include <sys/statfs.h>
  #endif
- #include <sys/param.h>
- #include <sys/mount.h>
-+#ifdef __NetBSD__
++#ifdef HAVE_SYS_STATVFS_H
 +#include <sys/statvfs.h>
++#ifdef __NetBSD__
 +#define statfs statvfs
 +#endif
- 
- #include <ctype.h>
- #include <err.h>
++#endif
+ #include <sys/param.h>
+ #include <sys/mount.h>
