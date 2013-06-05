1ac5c3f40bba3080dc322a73d37f0b6a606b7f9c
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-06-05 22:11:21 +0000

    Update to 0.95.1
    change:
    regex-compat.cabal: update Build-Depends

--- hs-regex-compat/buildlink3.mk
+++ hs-regex-compat/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2011/01/07 10:15:58 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/06/05 22:11:21 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-regex-compat
 
 .if !defined(HS_REGEX_COMPAT_BUILDLINK3_MK)
 HS_REGEX_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-regex-compat+=	hs-regex-compat>=0.93
+BUILDLINK_API_DEPENDS.hs-regex-compat+=	hs-regex-compat>=0.95
 BUILDLINK_PKGSRCDIR.hs-regex-compat?=	../../wip/hs-regex-compat
 
 .include "../../wip/hs-regex-base/buildlink3.mk"
