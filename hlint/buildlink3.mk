f517e826209a3c2dd5dbf8f6df0382bc1d8e7b8b
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-06-06 22:43:49 +0000

    Update to 1.8.46
    Changelog for HLint
    1.8.46
        Remove incorrect isPrefixOf hints
        #586, add span/break/takeWhile/dropWhile hints
        #588, add sort/reverse hints
        #601, add replicate/map/repeat hints
        Add a hint about reverse/reverse
        Add side as an alias for _
        Add hint as an alias for error

--- hlint/buildlink3.mk
+++ hlint/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2013/05/12 23:58:21 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2013/06/06 22:43:49 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hlint
 
 .if !defined(HLINT_BUILDLINK3_MK)
 HLINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8.45
+BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8.46
 BUILDLINK_PKGSRCDIR.hlint?=	../../wip/hlint
 
 .include "../../wip/hs-cpphs/buildlink3.mk"
