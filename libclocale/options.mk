1764acf5ada7dc1c0584563bd229f6aba3d3a26b
Blue Rats <rodent@NetBSD.org>
2013-06-08 13:14:02 +0000

    Add static option to package.

--- libclocale/options.mk
+++ libclocale/options.mk
@@ -1,16 +1,21 @@
-# $NetBSD: options.mk,v 1.1 2013/05/29 03:01:04 othyro Exp $
+# $NetBSD: options.mk,v 1.2 2013/06/08 13:14:02 othyro Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libclocale
-PKG_SUPPORTED_OPTIONS=	nls wide-curses
-PKG_SUGGESTED_OPTIONS+=	nls wide-curses
+PKG_SUPPORTED_OPTIONS=	nls static wide-curses
+PKG_SUGGESTED_OPTIONS+=	nls static wide-curses
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mnls)
-.include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-nls
+.endif
+
+.if !empty(PKG_OPTIONS:Mstatic)
+CONFIGURE_ARGS+=	--enable-static=yes
+.else
+CONFIGURE_ARGS+=        --enable-static=no
 .endif
 
 .if !empty(PKG_OPTIONS:Mwide-curses)
