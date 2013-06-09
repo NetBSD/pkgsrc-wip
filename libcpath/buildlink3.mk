8028f1ea9d8af49aa882ecadd00772e48d725502
Blue Rats <rodent@NetBSD.org>
2013-06-09 22:01:57 +0000

    Updated to latest release, 20130609. Fix buildlink3.mk to pull in all the libraries that libcpath links against. From ChangeLog:
    20130609
    * fix in .pc.in file
    
    20130509
    * removed CRT functions
    * updated dependencies
    
    20130413
    * updated dependencies
    
    20130404
    * textual changes
    * updated dependencies
    
    20130327
    * small updates
    
    20130113
    * updated msvscpp files
    
    20130103
    * 2013 update

--- libcpath/buildlink3.mk
+++ libcpath/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/06/08 12:49:58 othyro Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/06/09 22:01:57 othyro Exp $
 
 BUILDLINK_TREE+=	libcpath
 
@@ -8,10 +8,10 @@ LIBCPATH_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libcpath+=	libcpath>=20121225
 BUILDLINK_PKGSRCDIR.libcpath?=	../../wip/libcpath
 
-#.include "../../wip/libuna/buildlink3.mk"
-#.include "../../wip/libcsplit/buildlink3.mk"
-#.include "../../wip/libclocale/buildlink3.mk"
-#.include "../../wip/libcerror/buildlink3.mk"
+.include "../../wip/libuna/buildlink3.mk"
+.include "../../wip/libcsplit/buildlink3.mk"
+.include "../../wip/libclocale/buildlink3.mk"
+.include "../../wip/libcerror/buildlink3.mk"
 .endif	# LIBCPATH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libcpath
