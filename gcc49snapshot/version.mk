b852b7e20840604f6cb5ce628e89271401359af9
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2013-06-12 20:43:41 +0000

    Snapshot 20130609. Add test_summary.log. Remove obsolete patch for libitm.

--- gcc49snapshot/version.mk
+++ gcc49snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.9 2013/06/05 05:06:23 keckhardt Exp $
-GCC49S_DIST_VERSION:=4.9-20130602
+# $NetBSD: version.mk,v 1.10 2013/06/12 20:43:41 keckhardt Exp $
+GCC49S_DIST_VERSION:=4.9-20130609
