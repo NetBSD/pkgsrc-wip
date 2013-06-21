201ea0fb67af050d20f20b469a4b81e8a94eb1e2
Jason Bacon <jwbacon@tds.net>
2013-06-21 18:56:28 +0000

    Generate PLIST.Linux.

--- openmpi163/buildlink3.mk
+++ openmpi163/buildlink3.mk
@@ -1,6 +1,6 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/06/21 17:26:49 outpaddling Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/06/21 18:56:28 outpaddling Exp $
 
-BUILDLINK_TREE+=	openmpi163
+BUILDLINK_TREE+=	openmpi
 
 .if !defined(OPENMPI_BUILDLINK3_MK)
 OPENMPI_BUILDLINK3_MK:=
@@ -10,6 +10,8 @@ BUILDLINK_PKGSRCDIR.openmpi?=	../../wip/openmpi163
 
 MPI_PREFIX?=	$(BUILDLINK_PREFIX.openmpi)
 FIND_PREFIX:=	BUILDLINK_PREFIX.openmpi=openmpi
+
+# Make sure dependent ports use correct MPI compiler wrappers
 PREPEND_PATH+=	$(MPI_PREFIX)/bin
 
 .include "../../mk/find-prefix.mk"
@@ -17,4 +19,4 @@ PREPEND_PATH+=	$(MPI_PREFIX)/bin
 .include "../../parallel/hwloc/buildlink3.mk"
 .endif # OPENMPI_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-openmpi163
+BUILDLINK_TREE+=	-openmpi
