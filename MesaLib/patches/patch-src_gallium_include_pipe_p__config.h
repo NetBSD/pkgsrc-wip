90984a6560449a3da2d693809393a1b8472884d5
Makoto Fujiwara <makoto@ki.nu>
2013-06-21 15:22:55 +0000

    Import MesaLib-7.11.2nb1 as wip/MesaLib.
    MesaLib is a 3-D graphics library with an API which is very similar to
    that of OpenGL*.  To the extent that Mesa utilizes the OpenGL command syntax
    or state machine, it is being used with authorization from Silicon Graphics,
    Inc.  However, the author makes no claim that Mesa is in any way a
    compatible replacement for OpenGL or associated with Silicon Graphics, Inc.
    
    This is to support powerpc ARCH.

---
+++ MesaLib/patches/patch-src_gallium_include_pipe_p__config.h
@@ -0,0 +1,31 @@
+$NetBSD: patch-src_gallium_include_pipe_p__config.h,v 1.1 2013/06/21 15:22:56 makoto Exp $
+
+Adjust Endianness for SunOS and PowerPC.:
+
+--- Mesa-7.11.2/src/gallium/include/pipe/p_config.h.orig	2011-10-15 09:43:58.000000000 +0900
++++ src/gallium/include/pipe/p_config.h	2013-06-10 15:35:08.000000000 +0900
+@@ -99,7 +99,7 @@
+ #endif
+ #endif
+ 
+-#if defined(__ppc__) || defined(__ppc64__) || defined(__PPC__)
++#if defined(__ppc__) || defined(__ppc64__) || defined(__PPC__) || defined(__powerpc__)
+ #define PIPE_ARCH_PPC
+ #if defined(__ppc64__) || defined(__PPC64__)
+ #define PIPE_ARCH_PPC_64
+@@ -129,6 +129,15 @@
+ # define PIPE_ARCH_BIG_ENDIAN
+ #endif
+ 
++#elif defined(__sun)
++#include <sys/isa_defs.h>
++
++#if defined(_LITTLE_ENDIAN)
++# define PIPE_ARCH_LITTLE_ENDIAN
++#elif defined(_BIG_ENDIAN)
++# define PIPE_ARCH_BIG_ENDIAN
++#endif
++
+ #else
+ 
+ #if defined(PIPE_ARCH_X86) || defined(PIPE_ARCH_X86_64)
---
+++ MesaLib/patches/patch-src_mesa_main_compiler.h
@@ -0,0 +1,33 @@
+$NetBSD: patch-src_mesa_main_compiler.h,v 1.1 2013/06/21 15:22:56 makoto Exp $
+
+--- src/mesa/main/compiler.h.orig	2011-11-23 01:09:52.000000000 +0000
++++ src/mesa/main/compiler.h
+@@ -248,7 +248,17 @@ extern "C" {
+  * Try to use a runtime test instead.
+  * For now, only used by some DRI hardware drivers for color/texel packing.
+  */
+-#if defined(BYTE_ORDER) && defined(BIG_ENDIAN) && BYTE_ORDER == BIG_ENDIAN
++#ifdef __sun
++#include <sys/isa_defs.h>
++#elif defined(__OpenBSD__) || defined(__NetBSD__)
++#include <machine/endian.h>
++#elif defined(__FreeBSD__) || defined(__DragonFly__)
++#include <sys/endian.h>
++#endif
++
++#if (defined(BYTE_ORDER) && defined(BIG_ENDIAN) && BYTE_ORDER == BIG_ENDIAN) ||\
++	(defined(_BYTE_ORDER) && defined(_BIG_ENDIAN) && _BYTE_ORDER == _BIG_ENDIAN) ||\
++	(defined(__sun) && defined(_BIG_ENDIAN))
+ #if defined(__linux__)
+ #include <byteswap.h>
+ #define CPU_TO_LE32( x )	bswap_32( x )
+@@ -263,6 +273,9 @@ static INLINE GLuint CPU_TO_LE32(GLuint
+            ((x & 0x00ff0000) >>  8) |
+            ((x & 0xff000000) >> 24));
+ }
++#elif defined(__sun)
++#include <sys/byteorder.h>
++#define CPU_TO_LE32( x )        BSWAP_32( x )
+ #else /*__linux__ */
+ #include <sys/endian.h>
+ #define CPU_TO_LE32( x )	bswap32( x )
