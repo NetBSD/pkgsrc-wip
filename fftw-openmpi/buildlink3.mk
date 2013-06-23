01b373dba6da1554e4638443d0270128091e3e23
Jason Bacon <jwbacon@tds.net>
2013-06-23 17:36:22 +0000

    Change MPI to wip/openmpi163

--- fftw-openmpi/buildlink3.mk
+++ fftw-openmpi/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/02/26 14:51:28 outpaddling Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/06/23 17:36:22 outpaddling Exp $
 
-BUILDLINK_TREE+=	fftw-mpi
+BUILDLINK_TREE+=	fftw-openmpi
 
 .if !defined(FFTW_BUILDLINK3_MK)
 FFTW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fftw-mpi+=	fftw-mpi>=3.0
-BUILDLINK_ABI_DEPENDS.fftw-mpi+=	fftw-mpi>=3.1nb1
-BUILDLINK_PKGSRCDIR.fftw-mpi?=	../../wip/fftw-mpi
+BUILDLINK_API_DEPENDS.fftw-openmpi+=	fftw-openmpi>=3.0
+BUILDLINK_ABI_DEPENDS.fftw-openmpi+=	fftw-openmpi>=3.1nb1
+BUILDLINK_PKGSRCDIR.fftw-openmpi?=	../../wip/fftw-openmpi
 
 .include "../../wip/openmpi-prefixed/buildlink3.mk"
 .endif # FFTW_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-fftw-mpi
+BUILDLINK_TREE+=	-fftw-openmpi
