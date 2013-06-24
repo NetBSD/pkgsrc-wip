284866df497b6aea94b077eb3037c5c7b0badb37
ndb <noud4@users.sourceforge.net>
2013-06-24 09:38:15 +0000

    update to 0.8.18.

--- meta-tracker/buildlink3.mk
+++ meta-tracker/buildlink3.mk
@@ -1,33 +1,105 @@
-# $NetBSD: buildlink3.mk,v 1.2 2010/06/13 22:47:11 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/06/24 09:38:15 noud4 Exp $
 
 BUILDLINK_TREE+=	meta-tracker
 
 .if !defined(META_TRACKER_BUILDLINK3_MK)
 META_TRACKER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.meta-tracker+=	meta-tracker>=0.6.96
-BUILDLINK_ABI_DEPENDS.meta-tracker?=	meta-tracker>=0.6.96nb1
+BUILDLINK_API_DEPENDS.meta-tracker+=	meta-tracker>=0.9.0
+BUILDLINK_ABI_DEPENDS.meta-tracker+=	meta-tracker>=0.9.38
 BUILDLINK_PKGSRCDIR.meta-tracker?=	../../wip/meta-tracker
 
-.include "../../databases/qdbm/buildlink3.mk"
-.include "../../databases/sqlite3/buildlink3.mk"
-.include "../../devel/exempi/buildlink3.mk"
-.include "../../devel/glib2/buildlink3.mk"
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mgstreamer)
+.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
+.elif !empty(PKG_BUILD_OPTIONS.meta-tracker:Mxine)
+.include "../../multimedia/xine-lib/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mdoc)
+.include "../../graphics/graphviz/buildlink3.mk"
+.include "../../textproc/gtk-doc/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mtests)
+.include "../../lang/python/application.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mhal)
+.include "../../sysutils/hal/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Munac)
+.include "../../textproc/unac/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Menca)
+.include "../../textproc/enca/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mgnome)
+.include "../../devel/libgee/buildlink3.mk"
+.include "../../lang/vala018/buildlink3.mk"
+.include "../../security/gnome-keyring/buildlink3.mk"
+.include "../../x11/gnome-panel/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mpdf)
+.include "../../print/poppler-includes/buildlink3.mk"
+.include "../../print/poppler-glib/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mjpeg)
+.include "../../mk/jpeg.buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mtiff)
+.include "../../graphics/tiff/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mvorbis)
+.include "../../audio/libvorbis/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mflac)
+.include "../../audio/flac/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mgsf)
 .include "../../devel/libgsf/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mplaylist)
+.include "../../multimedia/totem-pl-parser/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mrss)
+.include "../../wip/libgrss/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mevolution)
+.include "../../mail/evolution/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mnautilus)
+.include "../../sysutils/nautilus/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mmp3)
+.include "../../audio/id3lib/buildlink3.mk"
+.endif
+
+.if !empty(PKG_BUILD_OPTIONS.meta-tracker:Mxmp)
+.include "../../devel/exempi/buildlink3.mk"
+.endif
+
+.include "../../sysutils/desktop-file-utils/desktopdb.mk"
+.include "../../databases/sqlite3/buildlink3.mk"
+.include "../../devel/libuuid/buildlink3.mk"
 .include "../../devel/pango/buildlink3.mk"
 .include "../../graphics/libexif/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
-.include "../../graphics/tiff/buildlink3.mk"
-.include "../../mail/gmime24/buildlink3.mk"
-.include "../../multimedia/gstreamer0.10/buildlink3.mk"
-.include "../../print/poppler-glib/buildlink3.mk"
-.include "../../sysutils/dbus/buildlink3.mk"
 .include "../../sysutils/dbus-glib/buildlink3.mk"
-.include "../../sysutils/hal/buildlink3.mk"
-.include "../../sysutils/libnotify/buildlink3.mk"
-.include "../../textproc/enca/buildlink3.mk"
-.include "../../textproc/libxml2/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
 .endif	# META_TRACKER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-meta-tracker
--- meta-tracker/options.mk
+++ meta-tracker/options.mk
@@ -1,35 +1,102 @@
-# $NetBSD: options.mk,v 1.2 2013/06/18 11:36:25 noud4 Exp $
+# $NetBSD: options.mk,v 1.3 2013/06/24 09:38:16 noud4 Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.meta-tracker
-PKG_SUPPORTED_OPTIONS=	unac gnome
-PKG_SUPPORTED_OPTIONS+=	vorbis playlist gstreamer
-PKG_SUPPORTED_OPTIONS+=	kmail
-#PKG_SUPPORTED_OPTIONS+=	rss	# XXX broken, libgrss lateron..
-PKG_SUGGESTED_OPTIONS=	unac gnome
-PKG_SUGGESTED_OPTIONS+=	vorbis playlist gstreamer
+PKG_OPTIONS_VAR=		PKG_OPTIONS.meta-tracker
+PKG_SUPPORTED_OPTIONS+=		doc tests hal unac enca gnome
+PKG_OPTIONS_OPTIONAL_GROUPS=	video
+PKG_OPTIONS_GROUP.video=	gstreamer xine
+PKG_SUPPORTED_OPTIONS+=		pdf jpeg tiff vorbis flac html gsf playlist
+PKG_SUPPORTED_OPTIONS+=		rss evolution kmail
+PKG_SUPPORTED_OPTIONS+=		nautilus
+PKG_SUPPORTED_OPTIONS+=		mp3 xmp
+
+#PKG_SUGGESTED_OPTIONS+=		hal unac enca gnome
+#PKG_SUGGESTED_OPTIONS+=		pdf jpeg tiff vorbis flac html gsf playlist
+#PKG_SUGGESTED_OPTIONS+=		gstreamer
+#PKG_SUGGESTED_OPTIONS+=		rss evolution kmail
+#PKG_SUGGESTED_OPTIONS+=		nautilus
+#PKG_SUGGESTED_OPTIONS+=		mp3 xmp
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=			${PKG_SUPPORTED_OPTIONS} mplayer totem
 
+.if !empty(PKG_OPTIONS:Mgstreamer)
+PLIST.gstreamer=	yes
+CONFIGURE_ARGS+=	--enable-gstreamer-tagreadbin
+CONFIGURE_ARGS+=	--enable-gstreamer-helix
+.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
+.elif !empty(PKG_OPTIONS:Mxine)
+PLIST.xine=		yes
+.include "../../multimedia/xine-lib/buildlink3.mk"
+.else
+PLIST.mplayer=		yes
+PLIST.totem=		yes
+.endif
+
+.if !empty(PKG_OPTIONS:Mdoc)
+CONFIGURE_ARGS+=	--enable-gtk-doc=yes
+PLIST_SRC+=		${PKGDIR}/PLIST ${PKGDIR}/PLIST.doc
+.include "../../graphics/graphviz/buildlink3.mk"
+.include "../../textproc/gtk-doc/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mtests)
+REPLACE_PYTHON+=	tests/functional-tests/*.py \
+			tests/tracker-writeback/01-writeback.py
+PLIST.tests=		yes
+PLIST_SRC+=		${PKGDIR}/PLIST ${PKGDIR}/PLIST.tests
+.include "../../lang/python/application.mk"
+.else
+CONFIGURE_ARGS+=	--disable-unit-tests
+CONFIGURE_ARGS+=	--disable-functional-tests
+.endif
+
+.if !empty(PKG_OPTIONS:Mhal)
+.include "../../sysutils/hal/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-hal
+.endif
+
 .if !empty(PKG_OPTIONS:Munac)
-CONFIGURE_ARGS+=	--enable-unac
-.include "../../textproc/unac/buildlink3.mk" 
+.include "../../textproc/unac/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Menca)
+.include "../../textproc/enca/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mgnome)
 PLIST.gnome=		yes
-BUILDLINK_API_DEPENDS.libglade+=	libglade>=2.5
-.include "../../devel/libglade/buildlink3.mk"
-.include "../../devel/libgnome/buildlink3.mk"
-.include "../../devel/libgnomeui/buildlink3.mk"
-.include "../../x11/gnome-desktop/buildlink3.mk"
+.include "../../devel/libgee/buildlink3.mk"
+.include "../../lang/vala018/buildlink3.mk"
+.include "../../security/gnome-keyring/buildlink3.mk"
+.include "../../x11/gnome-panel/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=	--disable-deskbar-applet
-.include "../../devel/libglade/buildlink3.mk"		# XXX gtk
+CONFIGURE_ARGS+=	--enable-tracker-status-icon=no
+CONFIGURE_ARGS+=	--enable-tracker-preferences=no
 .endif
 
 ### Metadata Extractors:
+
+.if !empty(PKG_OPTIONS:Mpdf)
+PLIST.pdf=		yes
+CONFIGURE_ARGS+=	--enable-poppler-glib
+.include "../../print/poppler-includes/buildlink3.mk"
+.include "../../print/poppler-glib/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mjpeg)
+PLIST.jpeg=		yes
+CONFIGURE_ARGS+=	--enable-libjpeg
+.include "../../mk/jpeg.buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mtiff)
+PLIST.tiff=		yes
+PLIST.jpeg=		yes
+.include "../../graphics/tiff/buildlink3.mk"
+.endif
 
 .if !empty(PKG_OPTIONS:Mvorbis)
 PLIST.vorbis=		yes
@@ -37,30 +104,62 @@ CONFIGURE_ARGS+=	--enable-libvorbis
 .include "../../audio/libvorbis/buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mgstreamer)
-PLIST.gstreamer=	yes
-CONFIGURE_ARGS+=	--enable-gstreamer-tagreadbin
-#CONFIGURE_ARGS+=	--enable-gstreamer-helix	# XXX broken
-.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
+.if !empty(PKG_OPTIONS:Mflac)
+PLIST.flac=		yes
+CONFIGURE_ARGS+=	--enable-libflac
+.include "../../audio/flac/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mhtml)
+PLIST.html=		yes
 .else
-PLIST.mplayer=		yes
-PLIST.totem=		yes
+CONFIGURE_ARGS+=	--disable-libxml2
+.endif
+
+.if !empty(PKG_OPTIONS:Mgsf)
+PLIST.gsf=		yes
+.include "../../devel/libgsf/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mplaylist)
 PLIST.playlist=		yes
-CONFIGURE_ARGS+=	--enable-playlist
 .include "../../multimedia/totem-pl-parser/buildlink3.mk"
+.endif
+
+### Data Miners:
+
+.if !empty(PKG_OPTIONS:Mrss)
+PLIST.rss=		yes
+.include "../../wip/libgrss/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mevolution)
+PLIST.evolution=	yes
+.include "../../mail/evolution/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mkmail)
+PLIST.kmail=		yes
+CONFIGURE_ARGS+=	--enable-miner-kmail
+.else
+CONFIGURE_ARGS+=	--enable-miner-kmail=no
 .endif
 
 ### Plugins:
 
-.if !empty(PKG_OPTIONS:Mkmail)
-PLIST.kmail=		yes
-CONFIGURE_ARGS+=	--enable-kmail-push-module
+.if !empty(PKG_OPTIONS:Mnautilus)
+PLIST.nautilus=		yes
+.include "../../sysutils/nautilus/buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mrss)
-PLIST.rss=		yes
-CONFIGURE_ARGS+=	--enable-rss-push-module
+### Writeback:
+
+.if !empty(PKG_OPTIONS:Mmp3)
+.include "../../audio/id3lib/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mxmp)
+PLIST.xmp=		yes
+CONFIGURE_ARGS+=	--enable-exempi
+.include "../../devel/exempi/buildlink3.mk"
 .endif
