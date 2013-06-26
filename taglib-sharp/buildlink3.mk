846d3652f1a90aa7bff968081eb4372c102cff5f
ndb <noud4@users.sourceforge.net>
2013-06-26 15:35:33 +0000

    bump to 2.0.3.7

--- taglib-sharp/buildlink3.mk
+++ taglib-sharp/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2013/06/17 12:45:13 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2013/06/26 15:35:33 noud4 Exp $
 
 BUILDLINK_TREE+=	taglib-sharp
 
 .if !defined(TAGLIB_SHARP_BUILDLINK3_MK)
 TAGLIB_SHARP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.taglib-sharp+=	taglib-sharp>=2.0.3.2
-BUILDLINK_ABI_DEPENDS.taglib-sharp?=	taglib-sharp>=2.0.3.2nb4
+BUILDLINK_API_DEPENDS.taglib-sharp+=	taglib-sharp>=2.0.3.7
+BUILDLINK_ABI_DEPENDS.taglib-sharp?=	taglib-sharp>=2.0.3.7
 BUILDLINK_PKGSRCDIR.taglib-sharp?=	../../wip/taglib-sharp
 
 
