8bf2f62229b921a8cb5d0a305a10995c086f2de2
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-07-17 18:10:33 +0000

    Update to 3.1
    CHANGELOG:
    3.1
    * Added the [rectangular band](http://en.wikipedia.org/wiki/Band_(mathematics)#Rectangular_bands) `Semigroupoid` for `(,)`. Would that make it a Bandoid?
    
    3.0.3
    * Claim to be `Trustworthy` where necessary

--- hs-semigroupoids/buildlink3.mk
+++ hs-semigroupoids/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/05/21 12:09:03 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/07/17 18:10:33 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-semigroupoids
 
 .if !defined(HS_SEMIGROUPOIDS_BUILDLINK3_MK)
 HS_SEMIGROUPOIDS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=3.0.2
+BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=3.1
 BUILDLINK_PKGSRCDIR.hs-semigroupoids?=	../../wip/hs-semigroupoids
 
 .include "../../wip/hs-comonad/buildlink3.mk"
