67553e34edff5e3e0584e0540d1c69e22bf526cb
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-15 04:38:03 +0000

    Update to 1.6.11
    CHANGES:
    1.6.11
        Work around more excessive strictness, gives 10x speed improvement

--- hs-uniplate/buildlink3.mk
+++ hs-uniplate/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2013/03/02 13:51:06 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2013/08/15 04:38:03 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-uniplate
 
 .if !defined(HS_UNIPLATE_BUILDLINK3_MK)
 HS_UNIPLATE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-uniplate+=	hs-uniplate>=1.6.10
+BUILDLINK_API_DEPENDS.hs-uniplate+=	hs-uniplate>=1.6.11
 BUILDLINK_PKGSRCDIR.hs-uniplate?=	../../wip/hs-uniplate
 
 .include "../../wip/hs-hashable/buildlink3.mk"
