e90424adab9239c9e394e4d1c112e32be659e423
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-16 21:42:28 +0000

    Update to 1.17
    CHANGELOG:
    Version 1.17
    ------------
      * recursively evaluate #if expressions after macro expansion (fix)

--- hs-cpphs/buildlink3.mk
+++ hs-cpphs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.8 2013/02/28 22:29:58 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2013/08/16 21:42:29 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cpphs
 
 .if !defined(HS_CPPHS_BUILDLINK3_MK)
 HS_CPPHS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.16
+BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.17
 BUILDLINK_PKGSRCDIR.hs-cpphs?=	../../wip/hs-cpphs
 .endif	# HS_CPPHS_BUILDLINK3_MK
 
