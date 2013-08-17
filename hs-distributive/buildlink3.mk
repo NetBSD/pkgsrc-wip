f6730c2d522d96b6eaade30426f9fd76538c1537
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-17 20:48:31 +0000

    Update to 0.3.1
    CHANGELOG:
    0.3.1
      - Minor documentation fix
    0.3
      - Added instances for Control.Applicative.Backwards and
    	Data.Functor.Reverse from transformers 0.3, taking them from
    	transformers-compat if necessary for transformers 0.2
    0.2.2
      - allow compatibility with the platform; transformers
    0.2.1
      - dep bump; transformers

--- hs-distributive/buildlink3.mk
+++ hs-distributive/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/02/01 16:18:18 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/08/17 20:48:31 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-distributive
 
 .if !defined(HS_DISTRIBUTIVE_BUILDLINK3_MK)
 HS_DISTRIBUTIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-distributive+=	hs-distributive>=0.2.0
+BUILDLINK_API_DEPENDS.hs-distributive+=	hs-distributive>=0.3.1
 BUILDLINK_PKGSRCDIR.hs-distributive?=	../../wip/hs-distributive
 
 .include "../../wip/hs-transformers/buildlink3.mk"
