23749c86e5daf3ed6f035e421ccb68fbd09e3924
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-17 20:02:53 +0000

    Update to 0.2.2
    CHANGES:
    * 0.2.2 (14 April 2013)
    
      - Add 'dropInnerBlanks' combinator for dropping blank chunks between
        consecutive delimiters while still keeping the delimiters separate.
    
    * 0.2.1.3 (28 March 2013)
    
      - bump upper bound to allow base-4.7
    
    * 0.2.1.2 (28 January 2013)
    
      - Patch from Daniel Wagner to make splitting lazier when using
        keepDelimsR.  Previously nothing was output until encountering a
        delimiter; now it can start outputting a Text chunk before
        reaching a delimiter.
    
    * 0.2.1.1 (24 September 2012)
    
      - Update this CHANGES file with details from the past two releases.
    
    * 0.2.1.0 (24 September 2012)
    
      - Go back to generic definition of 'build' (reverses change
        introduced in 0.1.4.3), for simplicity and Haskell2010 compliance.
    
    * 0.2.0.0 (21 August 2012)
    
      - test with GHC 7.6.1 and bump base dependency to allow base-4.6
    
      - deprecate synonyms: sepBy, sepByOneOf, unintercalate, chunk
    
      - rename splitEvery to chunksOf
    
      - unify Delimiter definition, and get rid of GADTs extension
    
    * 0.1.4.3 (7 June 2012)
    
      - Import 'build' function from GHC.Exts instead of defining it by
        hand, which can lead to some speedups (since GHC has special
        rewriting rules for the version in GHC.Exts).  Of course this ties
        it to GHC; if you want to build split under some other compiler,
        let me know and I can add some CPP directives to define 'build'
        conditionally.
    
      - Remove unnecessary Rank2Types extension.

--- hs-split/buildlink3.mk
+++ hs-split/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/03/11 07:00:57 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/08/17 20:02:54 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-split
 
 .if !defined(HS_SPLIT_BUILDLINK3_MK)
 HS_SPLIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.1.4
+BUILDLINK_API_DEPENDS.hs-split+=	hs-split>=0.2.2
 BUILDLINK_PKGSRCDIR.hs-split?=	../../wip/hs-split
 .endif	# HS_SPLIT_BUILDLINK3_MK
 
