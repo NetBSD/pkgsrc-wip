de70c55af0d5e9b4a4f711bd7ddd7a9f4fba5094
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-21 17:12:22 +0000

    Update to 1.3.13
    1.3.13
      Add support for extracting characters with a custom function.
    1.3.12
      Add derived Typeable and Data instances for the XML types.
    
    1.3.11
      Don't automatically escape all non-ASCII characters.
    	This looses a bit of generality in that we are producing a unicode
    	string, that can't really be turned uinto ASCII form. On the other hand,
    	it is not clear that there is much use to support this, while
    	non-ASCII characters are quite common in modern usage, and it is not
    	nice if you have to look at them in encoded form.

--- hs-xml/buildlink3.mk
+++ hs-xml/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/12/04 13:04:14 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/08/21 17:12:22 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-xml
 
 .if !defined(HS_XML_BUILDLINK3_MK)
 HS_XML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-xml+=	hs-xml>=1.3.10
+BUILDLINK_API_DEPENDS.hs-xml+=	hs-xml>=1.3.13
 BUILDLINK_PKGSRCDIR.hs-xml?=	../../wip/hs-xml
 
 .include "../../wip/hs-text/buildlink3.mk"
