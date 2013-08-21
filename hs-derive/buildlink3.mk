a71b9d2302242523c1d6c7f460a951e3d52cdb2a
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-21 13:14:20 +0000

    Update to 2.5.12
    CHANGES:
    2.5.12
      Upgrade to haskell-src-exts-1.14.*

--- hs-derive/buildlink3.mk
+++ hs-derive/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/05/06 19:18:22 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/08/21 13:14:20 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-derive
 
 .if !defined(HS_DERIVE_BUILDLINK3_MK)
 HS_DERIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-derive+=	hs-derive>=2.5.11
+BUILDLINK_API_DEPENDS.hs-derive+=	hs-derive>=2.5.12
 BUILDLINK_PKGSRCDIR.hs-derive?=	../../wip/hs-derive
 
 .include "../../wip/hs-haskell-src-exts/buildlink3.mk"
