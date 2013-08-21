7fe2b17e055eff5cd16fb5fa2ad6aaa1351953eb
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-21 15:44:27 +0000

    Update to 0.12.8
    CHANGES:
    0.12.8
        #560, space leak (fixed)
    0.12.7
        Allow deepseq-1.3
        Allow QuickCheck-2.5
        Support bytestring-0.10 (NFData instances added)
    0.12.6
        #515, don't crash on malformed characters (use ? instead)
    0.12.5
        Add optRawTag to Render, to ensure script's don't get escaped
    0.12.4
        #487, fix the behaviour of ~== for TagComment and others
    0.12.3
        GHC 7.2 compatibility
    0.12.2
        Add StringLike instances for Text
    0.12.1
        Add parseOptionsEntities and improve documentation
    0.12
        Upgrade to QuickCheck 2.4.*
        Export toTagRep
        Make the -download flag off by default
        Eliminate HTTP dependency
        Eliminate mtl dependency
    0.11.1
        Support --flags=-download to eliminate the network dependency
    0.11
        #326, <a "foo"> is no longer treated as an attribute
        Add Eq/Ord instances to Tree
        Don't mark Text.HTML.TagSoup.Tree as preliminary
        #325, \r should be treated as whitespace
    0.10.1
        #322, don't change ' to &apos; in render (do what the docs say)
    0.10
        Improve the cabal file, make the test program off by default
        Expose Text.HTML.TagSoup.Match again (was hidden accidentally)
    0.9
        #294, let optEntityData see if there was a ';' (CHANGES API)
        Numeric/hex entities in attributes were misparsed
        #149, add escapeHTML function

--- hs-tagsoup/buildlink3.mk
+++ hs-tagsoup/buildlink3.mk
@@ -1,15 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.4 2010/02/09 04:12:57 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/08/21 15:44:27 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-tagsoup
 
 .if !defined(HS_TAGSOUP_BUILDLINK3_MK)
 HS_TAGSOUP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.8
+BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.12.8
 BUILDLINK_PKGSRCDIR.hs-tagsoup?=	../../wip/hs-tagsoup
 
-.include "../../wip/hs-mtl/buildlink3.mk"
-.include "../../wip/hs-network/buildlink3.mk"
 .endif	# HS_TAGSOUP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-tagsoup
