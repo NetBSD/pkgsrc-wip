56885e551de0c6c1c95db6861f4fc6077b5130e2
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-21 18:47:59 +0000

    Update to 1.0.0.1
    changes:
    1.0.0.1
      - reChunkIn: Fix insufficient laziness
          The redundant case for [y] was always forcing one more input chunk to
          be evaluated than necessary to generate an output chunk.
          Signed-off-by: Anders Kaseorg <andersk@mit.edu>
    
      - typo of "decoding"
      - Simplify reChunkWith
      - Add lazy benchmarks
      - Improve encoding performance by 5%
      - Improve decodeLenient performance by 50%
    
    1.0.0.0
      - Refactor the testsuite a little
      - Add .Lazy variants of the two Base64 modules
    
    0.1.2.0
      - Ensure that joinWith really does what it claims.
    
    0.1.1.3
      - Bump version constraint on bytestring
      - trustworthy flags; fix BM
    
    0.1.1.1
      - Fix performance regression in encoding.
          The encoding table wasn't being let-floated when URL encoding support was
          added, and so it was being regenerated on every call, with catastrophic
          performance consequences (600x slowdown for small inputs on my machine).

--- hs-base64-bytestring/buildlink3.mk
+++ hs-base64-bytestring/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2012/02/11 04:43:19 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/08/21 18:47:59 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-base64-bytestring
 
 .if !defined(HS_BASE64_BYTESTRING_BUILDLINK3_MK)
 HS_BASE64_BYTESTRING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=0.1.1
+BUILDLINK_API_DEPENDS.hs-base64-bytestring+=	hs-base64-bytestring>=1.0.0
 BUILDLINK_PKGSRCDIR.hs-base64-bytestring?=	../../wip/hs-base64-bytestring
 .endif	# HS_BASE64_BYTESTRING_BUILDLINK3_MK
 
