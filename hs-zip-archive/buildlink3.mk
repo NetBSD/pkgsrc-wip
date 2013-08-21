8e04d5af69b3013bccc2ab4601a303af457842c9
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-21 20:42:22 +0000

    Update to 0.1.3.4
    changes:
    0.1.3.4
      - mv Codec/Archive/Zip.hs src/Codec/Archive/Zip.hs
    
    0.1.3.3
      - Add back binary 0.5 support
    
    0.1.3.2
      - Fixed digital signature magic numbers
    
    0.1.3.1
      - fix build failure against directory-1.2 (missing liftM)
          Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>
    
    0.1.3
      - Rewrote to use new binary API (binary >= 0.6).
          Closes https://github.com/jgm/zip-archive/issues/7
          Closes https://github.com/jgm/zip-archive/issues/4
      - Removed unneeded pragma.
      - Removed unnecessary import.
    
    0.1.2.1
      - Added proper cabal test suite.
      - Constrain binary to < 0.6, since 0.6 removes lookAhead.
          We'll need to find a workaround going forward, but for now
          the constraint is needed in order for zip-archive to compile.
      - Support directory 1.2
          The type of getModificationTime has changed, leading to some CPP
    
    0.1.1.8
      - Fix parsing of "version needed to extract" field
          This field consists of two bytes, upper one indicating host OS and
          lower one indicating version of Zip (de)compressor. Tomake sure if
          one can unpack given archive it's only lower byte which is needed to
          be checked, but library used to check both.
    
    0.1.1.7
      - Correctly calculate length of file paths.
          Be sure to use zipifyFilePath and convert to UTF8 before
          calculating length.
      - Fixed problems with zipifyFilePath:
          + Don't ever put drive in zip file path!
          + Don't put leading "./" supplied by some versions of
            System.FilePath.splitDirectories.
      - Fixed Test.hs so it will pick up right zip executable on Mac OSX.
    
    0.1.1.6
      - epochTimeToMSDOSDateTime: return minimum DOS datetime for earlier epoch times.
          The previous behavior was just to crash. Epoch times start in 1970,
          while DOS datetimes start in 1980.
          Returning the minimum seems the best solution,
          since you simply can't create a zip archive entry with an earlier time.
          This is also the solution used by zip30.
          Thanks to Radoslav Dorcik for reporting.
    
    0.1.1.5
      - Fixed warnings uncovered by GHC 6.12.
    
    0.1.1.4
      - Added explicit upper bound for base version
      - Removed -O2 option
      - Version bump in Zip executable
      - Handle data descriptor record
          OpenOffice-created zip archives sometimes use the "data descriptor
          record" to store checksums and lengths.
          getLocalFile now checks the general purpose bit flags to see
          if there is a data descriptor record; if there is,
          it ignores the compressed size field and instead reads data
          until the data descriptor record is encountered.
    
          This patch should fix problems reading ODS and other OpenOffice created
          zip archives.
          Thanks to Joel Lehtone for calling the problem to my attention.

--- hs-zip-archive/buildlink3.mk
+++ hs-zip-archive/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/04/15 21:02:55 zecrazytux Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/08/21 20:42:22 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-zip-archive
 
 .if !defined(HS_ZIP_ARCHIVE_BUILDLINK3_MK)
 HS_ZIP_ARCHIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.1.1.3
+BUILDLINK_API_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.1.3.4
 BUILDLINK_PKGSRCDIR.hs-zip-archive?=	../../wip/hs-zip-archive
 .endif	# HS_ZIP_ARCHIVE_BUILDLINK3_MK
 
