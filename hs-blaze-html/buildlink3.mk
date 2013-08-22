9da663ea6beca513ec7f06dc5bd2f3012724b4a3
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-22 19:54:54 +0000

    Update to 0.6.1.1
    changes:
    0.6.1.1
      - Bump QuickCheck version
    0.6.0.0
      - Make base, keygen, source leaf elements
    0.5.1.3
      - Bump test-framework dependency
    0.5.1.2
      - Bump test-framework dependency
    0.5.1.0
      - Add XHTML5 option, patch by Hugo Landau
      - Fix issue regarding docTypeHtml in lowercase
          Closes https://github.com/jaspervdj/blaze-html/issues/66
      - Add preEscapedToHtml
      - Merge pull request #71 from ianbollinger/master
          Increment upper bound on bytestring for GHC 7.6
    0.5.0.0
      - Add new renderer modules
    0.4.3.4
      - Merge pull request #59 from alevy/master
          Add trustworthy flags to renderers and HTML combinator generators
    0.4.3.3
      - Merge pull request #62 from trofi/master
         .cabal: add tests to sdist
           Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>
      - Remove mistaken comment
    0.4.3.2
      - Fix compilation with -Werror
    0.4.3.1
      - Export extra Text renderer functions
    0.4.3.0
      - Added XHtml1.{Strict,Transitional,FrameSet} modules.
         * Modified Util/GenerateHtmlCombinators to generate these automatically.
         * HtmlVariant now includes a selfClosing field; if True, leaf tags will be self-closed.
         * Modified cabal file to include the new modules in exported modules.
      - Merge remote-tracking branch 'sol/master'
         Update example in documentation of Text.Blaze
           * Use toHtml instead of string
           * Add missing import for forM_

--- hs-blaze-html/buildlink3.mk
+++ hs-blaze-html/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/12/01 09:58:22 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/08/22 19:54:54 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-blaze-html
 
 .if !defined(HS_BLAZE_HTML_BUILDLINK3_MK)
 HS_BLAZE_HTML_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-blaze-html+=	hs-blaze-html>=0.4.2
+BUILDLINK_API_DEPENDS.hs-blaze-html+=	hs-blaze-html>=0.6.1
 BUILDLINK_PKGSRCDIR.hs-blaze-html?=	../../wip/hs-blaze-html
 
 .include "../../wip/hs-blaze-builder/buildlink3.mk"
