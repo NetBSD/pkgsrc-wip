78e28a484b12239ff1e3e0641acb128815c43d94
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-27 13:44:48 +0000

    Update to 0.11.2 for CVE-2013-1436
    changes:
    0.11.2
      - CVE-2013-1436
    
    0.11.1
      - Upgrade to X11 >= 1.6.1

--- xmonad-contrib/options.mk
+++ xmonad-contrib/options.mk
@@ -1,10 +1,12 @@
-# $NetBSD: options.mk,v 1.2 2013/03/31 00:59:49 szptvlfn Exp $
+# $NetBSD: options.mk,v 1.3 2013/08/27 13:44:48 szptvlfn Exp $
 
 PKG_OPTIONS_VAR=    PKG_OPTIONS.xmonad-contrib
 PKG_SUPPORTED_OPTIONS= xft2
 PKG_SUGGESTED_OPTIONS= xft2
 
 .include "../../mk/bsd.options.mk"
+
+USE_TOOLS+=	patch
 
 .if !empty(PKG_OPTIONS:Mxft2)
 .include "../../wip/hs-x11-xft/buildlink3.mk"
