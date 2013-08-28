ad2ba913a04c559a08494f3f58194cd51f1f3e8c
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-28 08:55:11 +0000

    Update to 0.6.4
    changes:
    0.6.4
      - Added Text/TeXMath/Pandoc to repository.
          Closes https://github.com/jgm/texmath/issues/18
      - Added Text.TeXMath.Pandoc module.
          This factors out some of the code formerly in
          Text.Pandoc.Readers.TeXMath.

--- hs-texmath/buildlink3.mk
+++ hs-texmath/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/08/22 22:39:20 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/08/28 08:55:12 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-texmath
 
 .if !defined(HS_TEXMATH_BUILDLINK3_MK)
 HS_TEXMATH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.6.3
+BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.6.4
 BUILDLINK_PKGSRCDIR.hs-texmath?=	../../wip/hs-texmath
 
 .endif	# HS_TEXMATH_BUILDLINK3_MK
