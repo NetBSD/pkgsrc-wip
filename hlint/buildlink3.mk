480807a7313cd1aec4f0f57df2cbc26505bd302a
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-29 18:40:38 +0000

    Update to 1.8.51
    CHANGES:
    1.8.51
        Add a test that unkwnon extensions are not touched
        Upgrade to haskell-src-exts-1.14
    1.8.50
        Eliminate upper bounds on all dependencies
        #617, fix up notIn to take account of Template Haskell variables
        #573, suggest removing various deriving language extensions
    1.8.49
        Remove ^^ ==> ** hint
        Remove a duplicate sqrt hint
        Ensure that --test failures throws an error
        Fix up the copyright year in --help
    1.8.48
        Brackets at the root of annotations are fine
        Reduce a few more lambda expressions
    1.8.47
        #613, compatibility with base-4.7

--- hlint/buildlink3.mk
+++ hlint/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2013/06/06 22:43:49 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2013/08/29 18:40:38 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hlint
 
 .if !defined(HLINT_BUILDLINK3_MK)
 HLINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8.46
+BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8.51
 BUILDLINK_PKGSRCDIR.hlint?=	../../wip/hlint
 
 .include "../../wip/hs-cpphs/buildlink3.mk"
