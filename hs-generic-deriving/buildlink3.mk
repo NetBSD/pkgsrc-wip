e0314ee12d114c3922f3013700c28005c4b6949c
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-31 04:51:52 +0000

    Update to 1.6.1
    changes:
    1.6.1
      - version 1.6 --> 1.6.1
    1.6
      - Disable |Generic1 f => GCopoint f| instance
      - Add generic Copoint
      - Merge pull request https://github.com/dreixel/generic-deriving/issues/9
          Minor modifications for building with latest Template-Haskell 2.9

--- hs-generic-deriving/buildlink3.mk
+++ hs-generic-deriving/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/04/12 20:24:15 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/08/31 04:51:52 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-generic-deriving
 
 .if !defined(HS_GENERIC_DERIVING_BUILDLINK3_MK)
 HS_GENERIC_DERIVING_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-generic-deriving+=	hs-generic-deriving>=1.5.0
+BUILDLINK_API_DEPENDS.hs-generic-deriving+=	hs-generic-deriving>=1.6.1
 BUILDLINK_PKGSRCDIR.hs-generic-deriving?=	../../wip/hs-generic-deriving
 .endif	# HS_GENERIC_DERIVING_BUILDLINK3_MK
 
