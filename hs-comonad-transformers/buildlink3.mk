5645ff197a117e807f8bfcb635e6e20aed883427
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-08-31 06:56:55 +0000

    Update to 3.1
    CHANGELOG:
    3.1
    * More instances for `Coproduct`
    
    3.0.4
    * Removed unnecessary constraints in `Control.Comonad.Trans.Store`.
    
    3.0.3
    * Marked modules Trustworthy as needed.
    
    3.0.2
    * Merge pull request https://github.com/ekmett/comonad-transformers/issues/1
        made this GHC 7.8-ready
    
    3.0.1
    * Removed upper bounds on my other dependencies
    * Directory layout change
    * Added support files
    * Travis build notification to IRC
    
    3.0.0.1
    * dep bump
    
    3.0
    * Upgraded to comonad 3.0
    * Upgraded to semigroupoids 3.0
    
    2.1.2
    * Added experiment
    
    2.1.1.1
    * dep bump
    
    2.1.1
    * allow compatibility with the haskell platform
    
    2.1
    * Removed Discont
    * Switched to mkTyCon3
    * Upgraded to transformers 0.3
    
    2.0.3
    * dep bump

--- hs-comonad-transformers/buildlink3.mk
+++ hs-comonad-transformers/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/02/01 16:38:18 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/08/31 06:56:55 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-comonad-transformers
 
 .if !defined(HS_COMONAD_TRANSFORMERS_BUILDLINK3_MK)
 HS_COMONAD_TRANSFORMERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-comonad-transformers+=	hs-comonad-transformers>=2.0.2
+BUILDLINK_API_DEPENDS.hs-comonad-transformers+=	hs-comonad-transformers>=3.1
 BUILDLINK_PKGSRCDIR.hs-comonad-transformers?=	../../wip/hs-comonad-transformers
 
 .include "../../wip/hs-comonad/buildlink3.mk"
