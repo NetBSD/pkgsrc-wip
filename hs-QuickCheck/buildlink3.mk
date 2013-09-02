756b435e30c4613af2ae3dd7fe28bb951e5c6b57
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-02 12:07:48 +0000

    Update to 2.6
    changelog:
    QuickCheck (2.6) 2013-03-07
    	* Add convenience Function instances for up to 7-tuples
    	* Make stderr line buffered to reduce console I/O.
    	* Return a flag to say whether the test case was interrupted.

--- hs-QuickCheck/buildlink3.mk
+++ hs-QuickCheck/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2013/01/15 09:24:49 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2013/09/02 12:07:48 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-QuickCheck
 
 .if !defined(HS_QUICKCHECK_BUILDLINK3_MK)
 HS_QUICKCHECK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.5.1
+BUILDLINK_API_DEPENDS.hs-QuickCheck+=	hs-QuickCheck>=2.6
 BUILDLINK_PKGSRCDIR.hs-QuickCheck?=	../../wip/hs-QuickCheck
 
 .include "../../wip/hs-random/buildlink3.mk"
