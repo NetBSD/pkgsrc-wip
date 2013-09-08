0ac92004863fb89bb444b556aaa612029839d012
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-08 08:50:58 +0000

    Update to 0.2.2.2
    changes:
    0.2.2.2
      -  Merge pull request #7 from hamishmack/master
           Revert "Use template haskell to fix the call to withTempDirectory in Setup.hs so that it works with Cabal 1.17"
             It turns out this will not be changing in 1.18 (official Cabal release), so we need to revert this change.
             https://github.com/haskell/cabal/issues/1387
      - Merge pull request #5 from qrilka/patch-1
          prevent emphasis for '/dev/urandom' path in haddock
    0.2.2.1
      - Merge pull request #3 from hamishmack/master
          Use template haskell to fix the call to withTempDirectory in Setup.hs so that it works with Cabal 1.17
    0.2.2
      - Add build-time configuration to check for RDRAND support.
      - use RDRand for long-running (CryptHandle) requests.
      - Add RDRAND instruction support.

--- hs-entropy/buildlink3.mk
+++ hs-entropy/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/12 07:21:00 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/09/08 08:50:58 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-entropy
 
 .if !defined(HS_ENTROPY_BUILDLINK3_MK)
 HS_ENTROPY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-entropy+=	hs-entropy>=0.2
+BUILDLINK_API_DEPENDS.hs-entropy+=	hs-entropy>=0.2.2
 BUILDLINK_PKGSRCDIR.hs-entropy?=	../../wip/hs-entropy
 .endif	# HS_ENTROPY_BUILDLINK3_MK
 
