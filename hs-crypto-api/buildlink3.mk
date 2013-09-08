c04504c6ff8825875829fe5b23115070ef367c9c
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-08 07:36:43 +0000

    Update to 0.12.2.2
    changes:
    0.12.2.2
      - Merge pull request #20 from kazu-yamamoto/master
        stop using reproxy.
        GHC 7.8 provides Data.Proxy and "tagged" will stop exporting its Data.Proxy. Unfortunately, GHC 7.8's Data.Proxy does not have "reproxy".
      - Merge pull request #19 from singpolyma/master
        Export the non-IO key generators again.
    
    0.12.2.1 ( 0.12.1 --> 0.12.2.1 )
      - lambda
    
    0.12.1
      - Add keyLengthBytes tag.
      - Add a new i2bs_unsized (by request)

--- hs-crypto-api/buildlink3.mk
+++ hs-crypto-api/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/04/24 01:02:15 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/09/08 07:36:43 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-crypto-api
 
 .if !defined(HS_CRYPTO_API_BUILDLINK3_MK)
 HS_CRYPTO_API_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-crypto-api+=	hs-crypto-api>=0.12
+BUILDLINK_API_DEPENDS.hs-crypto-api+=	hs-crypto-api>=0.12.2
 BUILDLINK_PKGSRCDIR.hs-crypto-api?=	../../wip/hs-crypto-api
 
 .endif	# HS_CRYPTO_API_BUILDLINK3_MK
