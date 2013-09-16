fc95ce29c2cd1d7f0fee6a02cca4e2ff5aa0d252
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-16 21:25:56 +0000

    Import aeson-0.6.2.0 as wip/hs-aeson.
    A JSON parsing and encoding library optimized for ease of use and high
    performance.
    (A note on naming: in Greek mythology, Aeson was the father of Jason.)

---
+++ hs-aeson/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/09/16 21:25:56 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-aeson
+
+.if !defined(HS_AESON_BUILDLINK3_MK)
+HS_AESON_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-aeson+=	hs-aeson>=0.6.2
+BUILDLINK_PKGSRCDIR.hs-aeson?=	../../wip/hs-aeson
+
+.include "../../wip/hs-attoparsec/buildlink3.mk"
+.include "../../wip/hs-blaze-builder/buildlink3.mk"
+.include "../../wip/hs-hashable/buildlink3.mk"
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-syb/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
+.include "../../wip/hs-unordered-containers/buildlink3.mk"
+.include "../../wip/hs-vector/buildlink3.mk"
+.endif	# HS_AESON_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-aeson
