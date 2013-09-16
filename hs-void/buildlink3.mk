fec92a5e8f4af95b4802fe6185b8d427185e2cbf
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-16 22:40:38 +0000

    Update to 0.6.1
    changes:
    0.6.1
      - Claim to be merely Trustworthy -- as Data.Hashable isn't.
      - Claim to be Safe

--- hs-void/buildlink3.mk
+++ hs-void/buildlink3.mk
@@ -1,13 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/04/21 22:21:31 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/09/16 22:40:40 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-void
 
 .if !defined(HS_VOID_BUILDLINK3_MK)
 HS_VOID_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-void+=	hs-void>=0.6
+BUILDLINK_API_DEPENDS.hs-void+=	hs-void>=0.6.1
 BUILDLINK_PKGSRCDIR.hs-void?=	../../wip/hs-void
 
+.include "../../wip/hs-hashable/buildlink3.mk"
+.include "../../wip/hs-semigroups/buildlink3.mk"
 .endif	# HS_VOID_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-void
