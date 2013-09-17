b203e1d37757ea180088d36b0b146d518ff4efc5
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-17 20:29:26 +0000

    Update to 0.11
    CHANGELOG:
    0.11
    * Added the missing instance for `HashSet`.
    
    0.10
    * Added support for `unordered-containers`, `bytestring` and `text`.

--- hs-semigroups/buildlink3.mk
+++ hs-semigroups/buildlink3.mk
@@ -1,14 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.5 2013/05/21 11:11:15 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2013/09/17 20:29:26 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-semigroups
 
 .if !defined(HS_SEMIGROUPS_BUILDLINK3_MK)
 HS_SEMIGROUPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.9.2
+BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.11
 BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../wip/hs-semigroups
 
+.include "../../wip/hs-hashable/buildlink3.mk"
 .include "../../wip/hs-nats/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
+.include "../../wip/hs-unordered-containers/buildlink3.mk"
 .endif	# HS_SEMIGROUPS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-semigroups
