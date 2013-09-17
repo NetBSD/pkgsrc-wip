0184d7b9b024dd205e0043565973daf8e55c475e
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-17 21:15:03 +0000

    Update to 1.19
    changelog:
    1.19
    * necessary changes to work with GHC 7.8
    
    1.18.11
    - Avoid triggering GHC's -fwarn-lazy-unlifted-bindings
    - Merge branch 'master' of github.com:simonmar/happy
      Get rid of warnings in Setup.lhs
      Turn on -Wall (and remove defaulting warning) and cleanup warnings
      Corrected section "Using Happy with GHCi"
    - tests/Makefile: fix precious files
        Avoids rerunning part of passed tests.
        Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>
    - tests/Makefile: switch to explicit package imports
        To avoid collisions like:
        monaderror.agc.hs:6:8:
            Ambiguous module name `Control.Monad.Error':
              it was found in multiple packages:
              monads-tf-0.1.0.1 monads-fd-0.2.0.0 mtl-2.1.2
        Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- happy/buildlink3.mk
+++ happy/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.6 2013/03/01 20:47:28 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2013/09/17 21:15:03 szptvlfn Exp $
 
 BUILDLINK_TREE+=	happy
 
 .if !defined(HAPPY_BUILDLINK3_MK)
 HAPPY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.happy+=	happy>=1.18.10
+BUILDLINK_API_DEPENDS.happy+=	happy>=1.19
 BUILDLINK_PKGSRCDIR.happy?=	../../wip/happy
 
+.include "../../wip/hs-mtl/buildlink3.mk"
 .endif	# HAPPY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-happy
