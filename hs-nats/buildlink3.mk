a5974d43dfddf0ac7fc8bb336dab27635d2aa3e4
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-17 20:09:32 +0000

    Update to 0.1.2
    changes:
    0.1.2
      - export natural from Numeric.Natural.
      - INLINE all the things
      - added `natural`
      - Try for support for old `Cabal` versions (1.8 for Debian stable) for issue
        https://github.com/ekmett/nats/issues/1

--- hs-nats/buildlink3.mk
+++ hs-nats/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/02/23 15:15:19 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/09/17 20:09:32 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-nats
 
 .if !defined(HS_NATS_BUILDLINK3_MK)
 HS_NATS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-nats+=	hs-nats>=0.1
+BUILDLINK_API_DEPENDS.hs-nats+=	hs-nats>=0.1.2
 BUILDLINK_PKGSRCDIR.hs-nats?=	../../wip/hs-nats
 .endif	# HS_NATS_BUILDLINK3_MK
 
