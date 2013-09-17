a6dfc7bffed2507a12015ad14642354fb678a885
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-17 21:27:00 +0000

    Update to 3.1.0
    Changes in 3.1.0:
    * necessary changes to work with GHC 7.8.1

--- alex/buildlink3.mk
+++ alex/buildlink3.mk
@@ -1,12 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/01/09 23:35:34 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/09/17 21:27:00 szptvlfn Exp $
 
 BUILDLINK_TREE+=	alex
 
 .if !defined(ALEX_BUILDLINK3_MK)
 ALEX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.alex+=	alex>=3.0
+BUILDLINK_API_DEPENDS.alex+=	alex>=3.1
 BUILDLINK_PKGSRCDIR.alex?=	../../wip/alex
+
+.include "../../wip/hs-QuickCheck/buildlink3.mk"
 .endif	# ALEX_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-alex
