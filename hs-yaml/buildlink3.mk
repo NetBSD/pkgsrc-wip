d0530972c30b3dea498d08c6479439d583172234
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-18 00:09:16 +0000

    Import yaml-0.8.5 as wip/hs-yaml.
    Provides support for parsing and emitting Yaml documents.
    
    This package includes the full libyaml C library version 0.1.2 by Kirill
    Simonov (http://pyyaml.org/wiki/LibYAML) in the package so you don't need
    to worry about any non-Haskell dependencies.
    
    The package is broken down into two modules. Data.Yaml provides a
    high-level interface based around the JSON datatypes provided by the aeson
    package. Text.Libyaml provides a lower-level, streaming interface. For most
    users, Data.Yaml is recommended.

---
+++ hs-yaml/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/09/18 00:09:16 szptvlfn Exp $
+
+BUILDLINK_TREE+=	hs-yaml
+
+.if !defined(HS_YAML_BUILDLINK3_MK)
+HS_YAML_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.hs-yaml+=	hs-yaml>=0.8.5
+BUILDLINK_PKGSRCDIR.hs-yaml?=	../../wip/hs-yaml
+
+.include "../../wip/hs-aeson/buildlink3.mk"
+.include "../../wip/hs-attoparsec/buildlink3.mk"
+.include "../../wip/hs-conduit/buildlink3.mk"
+.include "../../wip/hs-data-default/buildlink3.mk"
+.include "../../wip/hs-resourcet/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
+.include "../../wip/hs-transformers/buildlink3.mk"
+.include "../../wip/hs-unordered-containers/buildlink3.mk"
+.include "../../wip/hs-vector/buildlink3.mk"
+.endif	# HS_YAML_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-hs-yaml
