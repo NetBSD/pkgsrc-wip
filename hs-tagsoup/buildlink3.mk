d91614fd8f6791db7294b0360d0c1564ee0cbfa1
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-19 10:29:19 +0000

    Update to 0.13
    CHANGES:
    0.13
        #616, extend to all HTML5 entities
        Optimise lookupNamedEntity
        Replace escapeXMLChar with escapeXML
        Change all Entity functions to return String, not Int or Char

--- hs-tagsoup/buildlink3.mk
+++ hs-tagsoup/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.5 2013/08/21 15:44:27 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2013/09/19 10:29:19 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-tagsoup
 
 .if !defined(HS_TAGSOUP_BUILDLINK3_MK)
 HS_TAGSOUP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.12.8
+BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13
 BUILDLINK_PKGSRCDIR.hs-tagsoup?=	../../wip/hs-tagsoup
 
+.include "../../wip/hs-text/buildlink3.mk"
 .endif	# HS_TAGSOUP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-tagsoup
