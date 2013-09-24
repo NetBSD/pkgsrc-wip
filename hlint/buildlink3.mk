1fc715ccd1e44fb048625286426314e13f8bc060
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-24 23:58:39 +0000

    Update to 1.8.53
    CHANGES:
    1.8.53
        Fix some corner cases when suggesting foldr etc.
        #517, don't introduce new free variables in a replacement
    1.8.52
        #2, Generic is not newtype derivable

--- hlint/buildlink3.mk
+++ hlint/buildlink3.mk
@@ -1,16 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.8 2013/08/29 18:40:38 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2013/09/24 23:58:39 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hlint
 
 .if !defined(HLINT_BUILDLINK3_MK)
 HLINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8.51
+BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8.53
 BUILDLINK_PKGSRCDIR.hlint?=	../../wip/hlint
 
 .include "../../wip/hs-cpphs/buildlink3.mk"
 .include "../../wip/hs-haskell-src-exts/buildlink3.mk"
 .include "../../wip/hs-hscolour/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
 .include "../../wip/hs-uniplate/buildlink3.mk"
 .endif	# HLINT_BUILDLINK3_MK
