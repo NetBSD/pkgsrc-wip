e0baee1c039ab46ec6885533acb0e435e28c63b6
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-25 13:09:44 +0000

    Update to 0.5.1.0
    changes:
    0.5.1.0
     - Fix build with GHC >= 7.7 (base 4.7)

--- hs-primitive/buildlink3.mk
+++ hs-primitive/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/01/15 09:04:32 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/09/25 13:09:44 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-primitive
 
 .if !defined(HS_PRIMITIVE_BUILDLINK3_MK)
 HS_PRIMITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.5
+BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.5.1
 BUILDLINK_PKGSRCDIR.hs-primitive?=	../../wip/hs-primitive
 .endif	# HS_PRIMITIVE_BUILDLINK3_MK
 
