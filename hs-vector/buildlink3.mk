62553b02967c1c8367205e48e5a887926811e237
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-25 13:13:13 +0000

    Update to 0.10.9.0
    changes:
    0.10.9.0
     - Implement poly-kinded Typeable
      This patch makes the Data.Typeable.Typeable class work with arguments of any
      kind. In particular, this removes the Typeable1..7 class hierarchy, greatly
      simplyfing the whole Typeable story. Also added is the AutoDeriveTypeable
      language extension, which will automatically derive Typeable for all types and
      classes declared in that module. Since there is now no good reason to give
      handwritten instances of the Typeable class, those are ignored (for backwards
      compatibility), and a warning is emitted.
    
      The old, kind-* Typeable class is now called OldTypeable, and lives in the
      Data.OldTypeable module. It is deprecated, and should be removed in some future
      version of GHC.
    
     - Make inplace fusion work on Streams rather than Bundles
     - Readd Fusion.Stream.Monadic and use it Bundle.Monadic
     - Add internal checks
     - Adapt tests to new names and modules
     - Finish Stream -> Bundle renaming
     - Rename Facets to Bundle
     - Work around bug in ghc-7.6.1

--- hs-vector/buildlink3.mk
+++ hs-vector/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/01/15 09:18:41 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/09/25 13:13:13 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-vector
 
 .if !defined(HS_VECTOR_BUILDLINK3_MK)
 HS_VECTOR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.10
+BUILDLINK_API_DEPENDS.hs-vector+=	hs-vector>=0.10.9
 BUILDLINK_PKGSRCDIR.hs-vector?=	../../wip/hs-vector
 
 .include "../../wip/hs-primitive/buildlink3.mk"
