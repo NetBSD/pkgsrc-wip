1d1eba9f0d031e67e49ef33e5e3ace9e7f22ffe5
Ryo ONODERA <ryoon@NetBSD.org>
2013-09-26 10:08:06 +0000

    hsqldb18 is needed in runtime.

--- libreoffice4/options.mk
+++ libreoffice4/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2013/09/26 08:55:58 thomasklausner Exp $
+# $NetBSD: options.mk,v 1.4 2013/09/26 10:08:06 ryo-on Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libreoffice4
 PKG_SUPPORTED_OPTIONS=	java
@@ -13,7 +13,7 @@ USE_JAVA=		yes
 USE_JAVA2=		yes
 BUILD_DEPENDS+=	apache-ant-[0-9]*:../../devel/apache-ant
 FIND_PREFIX:=		ANTDIR=apache-ant
-BUILD_DEPENDS+=	hsqldb18-[0-9]*:../../wip/hsqldb18
+DEPENDS+=	hsqldb18-[0-9]*:../../wip/hsqldb18
 FIND_PREFIX:=		HSQLDB_SYSDIR=hsqldb18
 .include "../../mk/find-prefix.mk"
 CONFIGURE_ARGS+=	--enable-ext-wiki-publisher \
