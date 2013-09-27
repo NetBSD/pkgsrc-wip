018865345c1c0b08e7f97c526d922dc35527dfc5
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-27 13:50:59 +0000

    Update to 0.2.3.3
    changes:
    0.2.3.3
     - -Wall police
     - HashMap: Compatibility with GHC >= 7.7

--- hs-unordered-containers/buildlink3.mk
+++ hs-unordered-containers/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/02/21 20:17:40 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/09/27 13:50:59 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-unordered-containers
 
 .if !defined(HS_UNORDERED_CONTAINERS_BUILDLINK3_MK)
 HS_UNORDERED_CONTAINERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.3.0
+BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.3
 BUILDLINK_PKGSRCDIR.hs-unordered-containers?=	../../wip/hs-unordered-containers
 
 .include "../../wip/hs-hashable/buildlink3.mk"
