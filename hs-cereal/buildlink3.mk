9d8f36dd5d93bef10d2c495142bf7b50ce8907e0
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-09-28 01:02:27 +0000

    Update to 0.4.0.0
    changes:
    0.4.0.0
     - Update version number for release
       * Major bump is due to new instances for types in Data.Monoid
     - Merge pull request #18 from audreyt/patch-1
         Unbreak cereal for GHC 7.6.3
     - Merge pull request #14 from gcross/master
         Instances for newtype wrappers in Data.Monoid
     - Merge pull request #13 from alpmestan/master
         Add -O2 to the ghc build options
     - Fix a warning in ghc-7.4.1
     - Add the remaining input to the Fail constructor
    0.3.5.2
     - Restructure runGetLazy to properly deal with the end of input
     - Merge pull request #4 from meiersi/fix-remove-unnecessary-unpack
         remove unnecessary UNPACK pragma
     - Merge pull request #3 from meiersi/improve-encoding-performance
         Improve encoding performance for lists, sequences, and trees.
           - slight improvement 5% for Data.Tree encoding
           - improved speed of Data.Sequence encoding
               It's about a factor 1.5 faster. Thanks to using 'foldMap' instead of 'mapM' it is slightly faster than the version in 'binary-0.5.0.2'.
           - improve the speed of list encoding by up to a factor of 2
    	       It is better to traverse the list twice than to explicitly construct a closure representing the encoding of all its elements, as this avoids a significant amout of allocation. We gain some additional speed by recursing over builders only instead of recursing in the 'Put' monad.
    
    		   Another candidate for the first kind of optimization is 'putSeqOf' and one for the second kind of optimization is 'putTreeOf'.
    
     - Merge pull request #2 from meiersi/fix-lazy-bytestring-length-conversion
         make types explicit and avoid unnecessary roundtrip
     - Warning removal
     - Add check in getBytes for negative length
     - Use the more efficient unsafeShiftR instead of shiftR when base >= 4.5
     - Fixed compilation with recent base.

--- hs-cereal/buildlink3.mk
+++ hs-cereal/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/03/04 07:59:16 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/09/28 01:02:27 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cereal
 
 .if !defined(HS_CEREAL_BUILDLINK3_MK)
 HS_CEREAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cereal+=	hs-cereal>=0.3.5
+BUILDLINK_API_DEPENDS.hs-cereal+=	hs-cereal>=0.4
 BUILDLINK_PKGSRCDIR.hs-cereal?=	../../wip/hs-cereal
 .endif	# HS_CEREAL_BUILDLINK3_MK
 
