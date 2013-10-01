7afab4c960dd9269ba098a596293efaa8c0a326e
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2013-10-01 12:08:43 +0000

    Snapshot 20130929. Adjust gcc_config.gcc to upstream changes.

--- gcc49snapshot/version.mk
+++ gcc49snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.18 2013/08/28 17:16:14 keckhardt Exp $
-GCC49S_DIST_VERSION:=4.9-20130825
+# $NetBSD: version.mk,v 1.19 2013/10/01 12:08:43 keckhardt Exp $
+GCC49S_DIST_VERSION:=4.9-20130929
