e523d092351d6e8fdf0bba5c4d9045b28bbb3526
PHO / phonohawk <phonohawk@users.sourceforge.net>
2013-10-01 10:12:25 +0000

    nb1: workaround for a breakage on recent libraries

--- hs-time-http/buildlink3.mk
+++ hs-time-http/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/03/27 05:17:36 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/10/01 10:12:25 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-time-http
 
 .if !defined(HS_TIME_HTTP_BUILDLINK3_MK)
 HS_TIME_HTTP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-time-http+=	hs-time-http>=0.1
+BUILDLINK_API_DEPENDS.hs-time-http+=	hs-time-http>=0.1.0
 BUILDLINK_PKGSRCDIR.hs-time-http?=	../../wip/hs-time-http
 
 .include "../../wip/hs-parsec/buildlink3.mk"
