d6df07d0141356e8050e1b8acc92257e51e01716
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-10-14 10:54:17 +0000

    Update to 4.0.1
    CHANGELOG:
    4.0.1
     - Give signatures involving an Iso newtype for the various isomorphisms in
       Composition and Rep
    4.0
     * Merged the contents of `profunctor-extras` into `profunctors`.

--- hs-profunctors/buildlink3.mk
+++ hs-profunctors/buildlink3.mk
@@ -1,13 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/05/22 18:46:21 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/10/14 10:54:17 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-profunctors
 
 .if !defined(HS_PROFUNCTORS_BUILDLINK3_MK)
 HS_PROFUNCTORS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-profunctors+=	hs-profunctors>=3.3.0.1
+BUILDLINK_API_DEPENDS.hs-profunctors+=	hs-profunctors>=4.0.1
 BUILDLINK_PKGSRCDIR.hs-profunctors?=	../../wip/hs-profunctors
 
+.include "../../wip/hs-comonad/buildlink3.mk"
+.include "../../wip/hs-semigroupoids/buildlink3.mk"
+.include "../../wip/hs-tagged/buildlink3.mk"
+.include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_PROFUNCTORS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-profunctors
