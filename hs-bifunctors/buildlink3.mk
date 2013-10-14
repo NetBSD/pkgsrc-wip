79723e12810f7b0c4f5927b4315f866fc801a173
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-10-14 10:48:50 +0000

    Update to 4.1.0.1
    CHANGELOG:
    4.1.0.1
    * Updated to BSD 2-clause license
    4.1
    * Added product bifunctors
    4.0
    * Compatibility with `semigroupoids` 4.0

--- hs-bifunctors/buildlink3.mk
+++ hs-bifunctors/buildlink3.mk
@@ -1,13 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/05/21 12:35:34 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/10/14 10:48:50 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-bifunctors
 
 .if !defined(HS_BIFUNCTORS_BUILDLINK3_MK)
 HS_BIFUNCTORS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-bifunctors+=	hs-bifunctors>=3.2.0
+BUILDLINK_API_DEPENDS.hs-bifunctors+=	hs-bifunctors>=4.1
 BUILDLINK_PKGSRCDIR.hs-bifunctors?=	../../wip/hs-bifunctors
 
+.include "../../wip/hs-semigroupoids/buildlink3.mk"
+.include "../../wip/hs-semigroups/buildlink3.mk"
+.include "../../wip/hs-tagged/buildlink3.mk"
 .endif	# HS_BIFUNCTORS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-bifunctors
