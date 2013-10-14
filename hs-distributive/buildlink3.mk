4ac4abe27799587a4a5fabfba16fa59e8695a1cb
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-10-14 10:32:25 +0000

    Update to 0.3.2
    changes:
    0.3.2
     - motivate the lack of need for "Coapplicative"

--- hs-distributive/buildlink3.mk
+++ hs-distributive/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/08/17 20:48:31 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/10/14 10:32:25 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-distributive
 
 .if !defined(HS_DISTRIBUTIVE_BUILDLINK3_MK)
 HS_DISTRIBUTIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-distributive+=	hs-distributive>=0.3.1
+BUILDLINK_API_DEPENDS.hs-distributive+=	hs-distributive>=0.3.2
 BUILDLINK_PKGSRCDIR.hs-distributive?=	../../wip/hs-distributive
 
 .include "../../wip/hs-transformers/buildlink3.mk"
+.include "../../wip/hs-transformers-compat/buildlink3.mk"
 .endif	# HS_DISTRIBUTIVE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-distributive
