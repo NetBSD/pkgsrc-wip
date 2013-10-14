69d2cb8dcf4379d3846505a44df1716ac55798ed
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-10-14 10:37:34 +0000

    Update to 4.0
    CHANGELOG:
    4.0
    * Merged the contents of `comonad-transformers` and `comonads-fd` into this package.

--- hs-comonad/buildlink3.mk
+++ hs-comonad/buildlink3.mk
@@ -1,14 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/07/13 11:34:04 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/10/14 10:37:34 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-comonad
 
 .if !defined(HS_COMONAD_BUILDLINK3_MK)
 HS_COMONAD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-comonad+=	hs-comonad>=3.1
+BUILDLINK_API_DEPENDS.hs-comonad+=	hs-comonad>=4.0
 BUILDLINK_PKGSRCDIR.hs-comonad?=	../../wip/hs-comonad
 
+.include "../../wip/hs-contravariant/buildlink3.mk"
+.include "../../wip/hs-distributive/buildlink3.mk"
+.include "../../wip/hs-mtl/buildlink3.mk"
 .include "../../wip/hs-semigroups/buildlink3.mk"
+.include "../../wip/hs-tagged/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_COMONAD_BUILDLINK3_MK
 
