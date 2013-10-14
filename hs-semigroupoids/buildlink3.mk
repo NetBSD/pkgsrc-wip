714379a73f6c096df0b4106cf6bf2f11652cd5f9
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-10-14 10:42:40 +0000

    Update to 4.0
    changes:
    4.0
     - Moved semigroupoid instances from the comonad-transformers as we moved them
       into comonad.
     - Merged the contents of semigroupoid-extras.
     - Subsumed the `groupoids` package

--- hs-semigroupoids/buildlink3.mk
+++ hs-semigroupoids/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/07/17 18:10:33 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/10/14 10:42:40 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-semigroupoids
 
 .if !defined(HS_SEMIGROUPOIDS_BUILDLINK3_MK)
 HS_SEMIGROUPOIDS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=3.1
+BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=4.0
 BUILDLINK_PKGSRCDIR.hs-semigroupoids?=	../../wip/hs-semigroupoids
 
 .include "../../wip/hs-comonad/buildlink3.mk"
 .include "../../wip/hs-contravariant/buildlink3.mk"
+.include "../../wip/hs-distributive/buildlink3.mk"
 .include "../../wip/hs-semigroups/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_SEMIGROUPOIDS_BUILDLINK3_MK
