236e7d3fa36dad21363da1f976e135d92782feaf
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-10-20 16:15:09 +0000

    Update to 1.12.3
    changes:
    1.12.3
     - Use "c" instead of "v" for JSON contents of tagged element.
     - To/FromJSON instance: Use "t" instead of "tag", "v" instead of "val".
     - Use {"tag": "Str", "val": "foo"} format for JSON.

--- hs-pandoc-types/buildlink3.mk
+++ hs-pandoc-types/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/10/12 23:51:28 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/10/20 16:15:10 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-pandoc-types
 
 .if !defined(HS_PANDOC_TYPES_BUILDLINK3_MK)
 HS_PANDOC_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.2
+BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.3
 BUILDLINK_PKGSRCDIR.hs-pandoc-types?=	../../wip/hs-pandoc-types
 
 .include "../../wip/hs-aeson/buildlink3.mk"
