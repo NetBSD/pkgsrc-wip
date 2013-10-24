5c7038f13d6b003d26faf46cb59f8fd58b045109
Olaf Seibert <rhialto@falu.nl>
2013-10-24 16:28:55 +0000

    Fix copy/paste error.

--- pan2-git/options.mk
+++ pan2-git/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2013/10/23 21:32:28 rhialto Exp $
+# $NetBSD: options.mk,v 1.2 2013/10/24 16:28:55 rhialto Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.pan2_git
 PKG_OPTIONS_REQUIRED_GROUPS=	gtkversion
@@ -8,7 +8,6 @@ PKG_SUGGESTED_OPTIONS=		gtk2
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgtk3)
-.include "../../audio/flac/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-gtk3
 .include "../../x11/gtk3/buildlink3.mk"
 .else
