7f42a19375787c88549f2acd9ef91e8032990e74
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-10-25 23:33:13 +0000

    Update to 0.2.2.4
    changes:
    0.2.2.2 => 0.2.2.4
     - Assume GHC (not gcc) and test RDRAND
     - Merge pull request #4 from singpolyma/master
       ( #4 => https://github.com/TomMD/entropy/pull/4 )
         Support QNX by using lower-level I/O

--- hs-entropy/buildlink3.mk
+++ hs-entropy/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/09/08 08:50:58 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/10/25 23:33:13 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-entropy
 
 .if !defined(HS_ENTROPY_BUILDLINK3_MK)
 HS_ENTROPY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-entropy+=	hs-entropy>=0.2.2
+BUILDLINK_API_DEPENDS.hs-entropy+=	hs-entropy>=0.2.2.4
 BUILDLINK_PKGSRCDIR.hs-entropy?=	../../wip/hs-entropy
 .endif	# HS_ENTROPY_BUILDLINK3_MK
 
