291e3d893d2cb68e348234ea85e07d260373d6bc
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-10-25 23:29:17 +0000

    Update to 0.4.1
    changes:
    0.4.1
     - Fix testsuite for newer GHCs (fixes #23)
    0.4.0
     - Both Typeable1 and Typeable2 are simply Typeable for GHC v7.7 and up
     - Update test outputs wrt Typeable changes
     - Use Cabal's support for test suites
     - Use LANGUAGE pragmas instead of OPTIONS_GHC/Cabal extensions, add .ghci
     - Fix compile errors in test suite

--- hs-syb/buildlink3.mk
+++ hs-syb/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/03/02 13:23:36 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/10/25 23:29:17 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-syb
 
 .if !defined(HS_SYB_BUILDLINK3_MK)
 HS_SYB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-syb+=	hs-syb>=0.3.7
+BUILDLINK_API_DEPENDS.hs-syb+=	hs-syb>=0.4.1
 BUILDLINK_PKGSRCDIR.hs-syb?=	../../wip/hs-syb
 .endif	# HS_SYB_BUILDLINK3_MK
 
