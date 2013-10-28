3676ebaabc2639f8efac8717f5da6b047c36515d
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2013-10-28 22:11:48 +0000

    Snapshot 20131027.

--- gcc49snapshot/version.mk
+++ gcc49snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.20 2013/10/18 10:52:54 keckhardt Exp $
-GCC49S_DIST_VERSION:=4.9-20131013
+# $NetBSD: version.mk,v 1.21 2013/10/28 22:11:48 keckhardt Exp $
+GCC49S_DIST_VERSION:=4.9-20131027
