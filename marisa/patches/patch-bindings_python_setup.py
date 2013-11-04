8bf30800bdb54ceb787f3ef938bf207169fa2ce1
ISIHARA Takanori <ishit@users.sourceforge.net>
2013-11-04 00:49:31 +0000

    Add patches/patch-bindings_python_setup.py.

---
+++ marisa/patches/patch-bindings_python_setup.py
@@ -0,0 +1,11 @@
+$NetBSD: patch-bindings_python_setup.py,v 1.1 2013/11/04 00:49:31 ishit Exp $
+
+--- bindings/python/setup.py.orig	2011-05-14 05:20:52.000000000 +0000
++++ bindings/python/setup.py
+@@ -5,5 +5,6 @@ marisa_module = Extension("_marisa",
+                           libraries=["marisa"])
+ 
+ setup(name = "marisa",
++      version = "0.2.4",
+       ext_modules = [marisa_module],
+       py_modules = ["marisa"])
