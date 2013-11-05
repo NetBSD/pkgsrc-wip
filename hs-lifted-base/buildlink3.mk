31458278b9ac992b3134438c382f8a71a1e79c4f
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-11-05 13:16:17 +0000

    Update to 0.2.1.1
    changes:
    0.2.1.1
     - Fix preprocessor directives for GHC 7.6.3

--- hs-lifted-base/buildlink3.mk
+++ hs-lifted-base/buildlink3.mk
@@ -1,13 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/08/17 08:00:20 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/11/05 13:16:17 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-lifted-base
 
 .if !defined(HS_LIFTED_BASE_BUILDLINK3_MK)
 HS_LIFTED_BASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lifted-base+=	hs-lifted-base>=0.2.1.0
+BUILDLINK_API_DEPENDS.hs-lifted-base+=	hs-lifted-base>=0.2.1.1
 BUILDLINK_PKGSRCDIR.hs-lifted-base?=	../../wip/hs-lifted-base
 
+.include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
+.include "../../wip/hs-monad-control/buildlink3.mk"
+.include "../../wip/hs-transformers-base/buildlink3.mk"
 .endif	# HS_LIFTED_BASE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-lifted-base
