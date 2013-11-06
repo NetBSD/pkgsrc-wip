c620547a220975f1c4f360946a8bcee180b58a78
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-11-06 12:06:44 +0000

    Update to 0.1.5.2
    changes from README:
     - The 0.1.5.2 release fixes some missing documentation.
     - The 0.1.5.1 release fixes some warnings and stuff.
     - The 0.1.5 release includes the "soft dash" option, giving the ability to
       allow positional arguments to begin with a dash if possible.
     - The 0.1.4 release includes the ability to mix optional and required
       positional arguments.
     - The various 0.1.3.x point releases include bug fixes and various
       extra-minor enhancements. See the Git log.
     - The 0.1.3 release includes more uniform and usable error handling.

--- hs-parseargs/buildlink3.mk
+++ hs-parseargs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/05/30 09:46:56 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/11/06 12:06:44 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-parseargs
 
 .if !defined(HS_PARSEARGS_BUILDLINK3_MK)
 HS_PARSEARGS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-parseargs+=	hs-parseargs>=0.1.2
+BUILDLINK_API_DEPENDS.hs-parseargs+=	hs-parseargs>=0.1.5
 BUILDLINK_PKGSRCDIR.hs-parseargs?=	../../wip/hs-parseargs
 .endif	# HS_PARSEARGS_BUILDLINK3_MK
 
