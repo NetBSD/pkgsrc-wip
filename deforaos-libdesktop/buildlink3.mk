be540ceeb2a7214dca312796d312d2fc9c85afaa
Pierre Pronchery <khorben@defora.org>
2013-11-06 01:22:23 +0000

    Mark the API change in libDesktop 0.0.10

--- deforaos-libdesktop/buildlink3.mk
+++ deforaos-libdesktop/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2013/01/26 01:10:43 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2013/11/06 01:22:23 khorben Exp $
 #
 
 BUILDLINK_TREE+=	deforaos-libdesktop
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	deforaos-libdesktop
 .if !defined(DEFORAOS_LIBDESKTOP_BUILDLINK3_MK)
 DEFORAOS_LIBDESKTOP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.0.9nb1
+BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.0.10
 BUILDLINK_PKGSRCDIR.deforaos-libdesktop?=	../../wip/deforaos-libdesktop
 
 .include "../../wip/deforaos-libsystem/buildlink3.mk"
