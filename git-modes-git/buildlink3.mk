ce80dfb8b62c1575904f1c8a9660bd29b307cca9
Makoto Fujiwara <makoto@ki.nu>
2013-11-07 03:04:59 +0000

    Import git-modes-git-0.1nb20131107 as wip/git-modes-git.
    Emacs library for magit.el

---
+++ git-modes-git/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/11/07 03:04:59 makoto Exp $
+#
+BUILDLINK_TREE+=	git_modes
+
+.if !defined(GIT_MODES_BUILDLINK3_MK)
+GIT_MODES_BUILDLINK3_MK:=
+BUILDLINK_API_DEPENDS.git_modes?= git-modes-git-[0-9]*
+BUILDLINK_PKGSRCDIR.git_modes?=	../../wip/git-modes-git
+
+BUILDLINK_CONTENTS_FILTER.git_modes= ${EGREP} '.*\.el$$|.*\.elc$$'
+
+.endif # GIT_MODES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-git_modes
