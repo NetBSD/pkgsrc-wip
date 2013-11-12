5e26fbbc82cb23dabb0112e85d5d564616de015b
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-11-12 12:54:08 +0000

    Update to 3.2.0.4
    changelog:
    3.2.0.4  Nov 2013
      * Update package description to Cabal 1.10 format
      * Add support for GHC 7.8
      * Drop support for GHCs older than GHC 7.0.1
      * Add NOINLINE pragmas to `parBuffer`, `parList`, and `evalBuffer`
      to make RULEs more likely to fire

--- hs-parallel/buildlink3.mk
+++ hs-parallel/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2012/03/04 05:50:47 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2013/11/12 12:54:08 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-parallel
 
 .if !defined(HS_PARALLEL_BUILDLINK3_MK)
 HS_PARALLEL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-parallel+=	hs-parallel>=3.2.0
+BUILDLINK_API_DEPENDS.hs-parallel+=	hs-parallel>=3.2.0.4
 BUILDLINK_PKGSRCDIR.hs-parallel?=	../../wip/hs-parallel
-
 .endif	# HS_PARALLEL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-parallel
