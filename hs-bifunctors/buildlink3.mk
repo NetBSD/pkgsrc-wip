da55d5b7daa4d3d50c901b6b7b497000e00334b4
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-11-12 10:33:29 +0000

    Update to 4.1.1
    HANGELOG:
    4.1.1
    * Added `Data.Bifunctor.Join`
    * Fixed improper lower bounds on `base`

--- hs-bifunctors/buildlink3.mk
+++ hs-bifunctors/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/10/14 10:48:50 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/11/12 10:33:29 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-bifunctors
 
 .if !defined(HS_BIFUNCTORS_BUILDLINK3_MK)
 HS_BIFUNCTORS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-bifunctors+=	hs-bifunctors>=4.1
+BUILDLINK_API_DEPENDS.hs-bifunctors+=	hs-bifunctors>=4.1.1
 BUILDLINK_PKGSRCDIR.hs-bifunctors?=	../../wip/hs-bifunctors
 
 .include "../../wip/hs-semigroupoids/buildlink3.mk"
