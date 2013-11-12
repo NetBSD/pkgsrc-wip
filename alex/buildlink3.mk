a9b432c9af11ddb83df7d15e299618f49f460177
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-11-12 10:03:28 +0000

    Update to 3.1.2
    Changes in 3.1.2:
        Add missing file to extra-source-files
    Changes in 3.1.1:
    	Bug fixes (#24, #30, #31, #32)
    
    ( #32 => https://github.com/simonmar/alex/pull/32 )
    ( #31 => https://github.com/simonmar/alex/pull/31 )
    ( #30 => https://github.com/simonmar/alex/pull/30 )
    ( #24 => https://github.com/simonmar/alex/pull/24 )

--- alex/buildlink3.mk
+++ alex/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/09/17 21:27:00 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/11/12 10:03:28 szptvlfn Exp $
 
 BUILDLINK_TREE+=	alex
 
 .if !defined(ALEX_BUILDLINK3_MK)
 ALEX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.alex+=	alex>=3.1
+BUILDLINK_API_DEPENDS.alex+=	alex>=3.1.2
 BUILDLINK_PKGSRCDIR.alex?=	../../wip/alex
 
 .include "../../wip/hs-QuickCheck/buildlink3.mk"
