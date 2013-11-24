a95e5a51c4db19a650e0d1bf7e525dc1dc47b7c1
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-11-24 12:13:33 +0000

    Update to 0.3.1.0
    changes:
    0.3.1.0
     - Update to base-4.7
     - Add deprecation warnings
     - Add new module Control.Monad.CatchIO.Try

--- hs-MonadCatchIO-transformers/buildlink3.mk
+++ hs-MonadCatchIO-transformers/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/04/21 19:27:32 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/11/24 12:13:33 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-MonadCatchIO-transformers
 
 .if !defined(HS_MONADCATCHIO_TRANSFORMERS_BUILDLINK3_MK)
 HS_MONADCATCHIO_TRANSFORMERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-MonadCatchIO-transformers+=	hs-MonadCatchIO-transformers>=0.3
+BUILDLINK_API_DEPENDS.hs-MonadCatchIO-transformers+=	hs-MonadCatchIO-transformers>=0.3.1
 BUILDLINK_PKGSRCDIR.hs-MonadCatchIO-transformers?=	../../wip/hs-MonadCatchIO-transformers
 
+.include "../../wip/hs-monads-tf/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_MONADCATCHIO_TRANSFORMERS_BUILDLINK3_MK
 
