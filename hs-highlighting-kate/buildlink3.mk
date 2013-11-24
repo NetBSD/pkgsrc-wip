8bc7719f5d61f63f7b043af012c76aef92d53db4
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-11-24 12:26:16 +0000

    Update to 0.5.5.1
    changes:
    0.5.5.1
     - Added Markdown to list in README.
     - Fixed bug in haskell.xml: FilePath listed as a function!
     - Added modelines and markdown to cabal file.
     - Added markdown.xml from kate repo.
     - Updated xml files from kate repo.
       Added modelines.xml, now used in some of them instead of alert.

--- hs-highlighting-kate/buildlink3.mk
+++ hs-highlighting-kate/buildlink3.mk
@@ -1,13 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/08/22 20:12:30 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/11/24 12:26:16 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-highlighting-kate
 
 .if !defined(HS_HIGHLIGHTING_KATE_BUILDLINK3_MK)
 HS_HIGHLIGHTING_KATE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-highlighting-kate+=	hs-highlighting-kate>=0.5.5
+BUILDLINK_API_DEPENDS.hs-highlighting-kate+=	hs-highlighting-kate>=0.5.5.1
 BUILDLINK_PKGSRCDIR.hs-highlighting-kate?=	../../wip/hs-highlighting-kate
 
+.include "../../wip/hs-blaze-html/buildlink3.mk"
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-parsec/buildlink3.mk"
+.include "../../wip/hs-pcre-light/buildlink3.mk"
 .endif	# HS_HIGHLIGHTING_KATE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-highlighting-kate
