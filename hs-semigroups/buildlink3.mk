247797f2dbc05040856e1db2569ff0e05a554d23
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-11-26 11:42:01 +0000

    Update to 0.12
    CHANGELOG:
    0.12
    * Added an instance for `Const r`.
    * Added `some1`

--- hs-semigroups/buildlink3.mk
+++ hs-semigroups/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2013/09/17 20:29:26 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2013/11/26 11:42:01 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-semigroups
 
 .if !defined(HS_SEMIGROUPS_BUILDLINK3_MK)
 HS_SEMIGROUPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.11
+BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.12
 BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../wip/hs-semigroups
 
 .include "../../wip/hs-hashable/buildlink3.mk"
