5bda7c29164487803dbba6265ffa065e228c0c0b
Makoto Fujiwara <makoto@ki.nu>
2013-11-27 01:52:36 +0000

    Add json option (with textproc/jansson).

--- lldpd/options.mk
+++ lldpd/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.3 2013/11/27 01:26:59 makoto Exp $
+# $NetBSD: options.mk,v 1.4 2013/11/27 01:52:36 makoto Exp $
 PKG_OPTIONS_VAR=	PKG_OPTIONS.lldpd
-PKG_SUPPORTED_OPTIONS=	snmp xml
-PKG_SUGGESTED_OPTIONS=	snmp xml
+PKG_SUPPORTED_OPTIONS=	snmp xml json
+PKG_SUGGESTED_OPTIONS=	snmp xml json
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Msnmp)
@@ -15,4 +15,9 @@ CONFIGURE_ARGS+=	--with-snmp
 .if !empty(PKG_OPTIONS:Mxml)
 .	include "../../textproc/libxml2/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-xml
+.endif
+
+.if !empty(PKG_OPTIONS:Mjson)
+.	include "../../textproc/jansson/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-json
 .endif
