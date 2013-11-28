9cda035bfa48e49704dfdd3af2d0470d32b86fb4
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-11-28 13:23:46 +0000

    Update to 1.19.2
    CHANGES:
    1.19.2
        * fixes for clang (XCode 5)

--- happy/buildlink3.mk
+++ happy/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.8 2013/11/12 10:00:26 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2013/11/28 13:23:46 szptvlfn Exp $
 
 BUILDLINK_TREE+=	happy
 
 .if !defined(HAPPY_BUILDLINK3_MK)
 HAPPY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.happy+=	happy>=1.19.1
+BUILDLINK_API_DEPENDS.happy+=	happy>=1.19.2
 BUILDLINK_PKGSRCDIR.happy?=	../../wip/happy
 
 .include "../../wip/hs-mtl/buildlink3.mk"
