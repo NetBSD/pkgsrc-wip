de092f9467ada6e96ae977f853cb5c904b323d04
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-11-28 13:21:51 +0000

    Update to 3.1.3
    Changes in 3.1.3:
     * Fix for clang (XCode 5)

--- alex/buildlink3.mk
+++ alex/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/11/12 10:03:28 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/11/28 13:21:51 szptvlfn Exp $
 
 BUILDLINK_TREE+=	alex
 
 .if !defined(ALEX_BUILDLINK3_MK)
 ALEX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.alex+=	alex>=3.1.2
+BUILDLINK_API_DEPENDS.alex+=	alex>=3.1.3
 BUILDLINK_PKGSRCDIR.alex?=	../../wip/alex
 
 .include "../../wip/hs-QuickCheck/buildlink3.mk"
