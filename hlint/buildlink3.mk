30c7f24684a6deae16aaff76070d0b9c959f28e0
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-11-28 13:19:34 +0000

    Update to 1.8.54
    Changelog:
    1.8.54
        Fix a bug when suggesting const

--- hlint/buildlink3.mk
+++ hlint/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2013/09/24 23:58:39 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2013/11/28 13:19:34 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hlint
 
 .if !defined(HLINT_BUILDLINK3_MK)
 HLINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8.53
+BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8.54
 BUILDLINK_PKGSRCDIR.hlint?=	../../wip/hlint
 
 .include "../../wip/hs-cpphs/buildlink3.mk"
