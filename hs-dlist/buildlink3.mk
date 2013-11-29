02e4af9f31358dc2863cd8a74a7b8a2d3a5ce319
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-11-29 14:05:15 +0000

    Update to 0.6
    Change Log
    ==========
    
    Version 0.6 (2013-11-29) *Black Friday*
    ---------------------------------------
    
    #### Development changes
    
    * Maintenance and development taken over by Sean Leather
    * Migrate repository from http://code.haskell.org/~dons/code/dlist/ to
      https://github.com/spl/dlist
    
    #### Package changes
    
    * Stop supporting `base < 2`
    * Fix tests and use `cabal test`
    * Add scripts for running `hpc`
    * Update documentation
    
    #### New features
    
    * New type class instances: `Eq`, `Ord`, `Read`, `Show`, `Alternative`,
      and `Foldable`
    * New function `apply` to use instead of `unDL`
    
    #### Deprecations
    
    * Deprecate DList constructor and record selector to make it abstract
      (see [#4](https://github.com/spl/dlist/issues/4))
    * Deprecate `maybeReturn` which is not directly relevant to dlists

--- hs-dlist/buildlink3.mk
+++ hs-dlist/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/07/08 09:23:29 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/11/29 14:05:15 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-dlist
 
 .if !defined(HS_DLIST_BUILDLINK3_MK)
 HS_DLIST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-dlist+=	hs-dlist>=0.5
+BUILDLINK_API_DEPENDS.hs-dlist+=	hs-dlist>=0.6
 BUILDLINK_PKGSRCDIR.hs-dlist?=	../../wip/hs-dlist
 .endif	# HS_DLIST_BUILDLINK3_MK
 
