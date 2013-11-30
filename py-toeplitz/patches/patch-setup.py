9e634e044d56aa4fa8f2aa1ca1a4dd4f679097d8
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2013-11-30 16:08:02 +0000

    UPDATE 0.1 --> 0.1.2 	please test package!

--- py-toeplitz/patches/patch-setup.py
+++ py-toeplitz/patches/patch-setup.py
@@ -1,20 +1,20 @@
-$NetBSD: patch-setup.py,v 1.1 2013/11/05 19:00:52 jihbed Exp $
+$NetBSD: patch-setup.py,v 1.2 2013/11/30 16:08:02 jihbed Exp $
 
---- setup.py.orig	2013-11-05 19:21:06.000000000 +0100
-+++ setup.py	2013-11-05 19:21:27.000000000 +0100
-@@ -23,6 +23,7 @@
- .. _John Burkhardt's website: http://people.sc.fsu.edu/~jburkardt/f_src/toeplitz/toeplitz.html
- """
- 
+--- setup.py.orig	2013-11-30 16:58:45.000000000 +0100
++++ setup.py	2013-11-30 16:59:12.000000000 +0100
+@@ -1,4 +1,6 @@
+ #!/usr/bin/env python
++
 +import sys
- 
  from numpy.distutils.core import Extension, setup
  
-@@ -38,6 +39,6 @@
-       license='MIT license',
+ VERSION='0.1.2'
+@@ -22,7 +24,7 @@
+       license='MIT',
        url='https://github.com/trichter/toeplitz',
        ext_modules=[ext],
 -      scripts=['scripts/toeplitz-runtests'],
 +      scripts=['scripts/toeplitz-runtests'+sys.version[0:3]],
-       requires=['numpy']
+       requires=['numpy'],
+       include_package_data=True
        )
