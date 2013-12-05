1167fa17da6bb8c90775ad326fb78da53ce254b5
PHO / phonohawk <phonohawk@users.sourceforge.net>
2013-12-05 03:10:15 +0000

    Upstream update to tar-0.4.0.1

--- hs-tar/buildlink3.mk
+++ hs-tar/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/10/01 06:58:51 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/12/05 03:10:15 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-tar
 
 .if !defined(HS_TAR_BUILDLINK3_MK)
 HS_TAR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tar+=	hs-tar>=0.3.1.0
+BUILDLINK_API_DEPENDS.hs-tar+=	hs-tar>=0.4.0
 BUILDLINK_PKGSRCDIR.hs-tar?=	../../wip/hs-tar
 .endif	# HS_TAR_BUILDLINK3_MK
 
