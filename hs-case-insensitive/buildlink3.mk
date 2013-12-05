91547454c122b5e575e74086b404cc425919cca9
PHO / phonohawk <phonohawk@users.sourceforge.net>
2013-12-05 09:04:28 +0000

    Relax build-depends to compile with text-1.0.*

--- hs-case-insensitive/buildlink3.mk
+++ hs-case-insensitive/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/11/25 10:54:27 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2013/12/05 09:04:28 phonohawk Exp $
 
 BUILDLINK_TREE+=	hs-case-insensitive
 
 .if !defined(HS_CASE_INSENSITIVE_BUILDLINK3_MK)
 HS_CASE_INSENSITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.1.0.1
+BUILDLINK_API_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.1.0
 BUILDLINK_PKGSRCDIR.hs-case-insensitive?=	../../wip/hs-case-insensitive
 
 .include "../../wip/hs-hashable/buildlink3.mk"
