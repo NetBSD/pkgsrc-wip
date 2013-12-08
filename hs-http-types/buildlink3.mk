c8558405470911e8417655f1da0cc108c8807b22
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-12-08 19:05:51 +0000

    Update to 0.8.3
    changes:
    0.8.3
     - remove upper boundary on text, may text simply never break anything

--- hs-http-types/buildlink3.mk
+++ hs-http-types/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/11/25 11:31:34 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2013/12/08 19:05:51 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-http-types
 
 .if !defined(HS_HTTP_TYPES_BUILDLINK3_MK)
 HS_HTTP_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-http-types+=	hs-http-types>=0.8.2
+BUILDLINK_API_DEPENDS.hs-http-types+=	hs-http-types>=0.8.3
 BUILDLINK_PKGSRCDIR.hs-http-types?=	../../wip/hs-http-types
 
 .include "../../wip/hs-blaze-builder/buildlink3.mk"
