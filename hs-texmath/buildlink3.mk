b70ce20856a6129c65131bc3b7a5edb5a9608e18
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-12-08 01:17:32 +0000

    Update to 0.6.5.2
    changes:
    0.6.5.2
     - Fixed serious bug handling commands.
       Support \not\in.
       Added tests.
    0.6.5.1
     - Support \not\subset, etc. Closes #22.
     - Added nsubset, nsubseteq, nsupset, nsupseteq. Closes #23.
    0.6.5
     - texMathToPandoc: Use $$ delimiters for display math in fallback.
     ( #22 => https://github.com/jgm/texmath/issues/22 )
     ( #23 => https://github.com/jgm/texmath/issues/23 )

--- hs-texmath/buildlink3.mk
+++ hs-texmath/buildlink3.mk
@@ -1,13 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/08/28 08:55:12 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/12/08 01:17:32 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-texmath
 
 .if !defined(HS_TEXMATH_BUILDLINK3_MK)
 HS_TEXMATH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.6.4
+BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.6.5
 BUILDLINK_PKGSRCDIR.hs-texmath?=	../../wip/hs-texmath
 
+.include "../../wip/hs-pandoc-types/buildlink3.mk"
+.include "../../wip/hs-parsec/buildlink3.mk"
+.include "../../wip/hs-syb/buildlink3.mk"
+.include "../../wip/hs-xml/buildlink3.mk"
 .endif	# HS_TEXMATH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-texmath
