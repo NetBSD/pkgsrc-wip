d2b733effbc18e4bdde8bcf6ae2bb88c351904e6
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-12-09 22:10:52 +0000

    Update to 0.1.4
    changes:
    0.1.4
     - Added OptDestination to ZipOptions.
         Modified extractFilesFromArchive so that OptDestination will
         be used as the base path, if given.

--- hs-zip-archive/buildlink3.mk
+++ hs-zip-archive/buildlink3.mk
@@ -1,12 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/08/21 20:42:22 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/12/09 22:10:52 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-zip-archive
 
 .if !defined(HS_ZIP_ARCHIVE_BUILDLINK3_MK)
 HS_ZIP_ARCHIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.1.3.4
+BUILDLINK_API_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.1.4
 BUILDLINK_PKGSRCDIR.hs-zip-archive?=	../../wip/hs-zip-archive
+
+.include "../../wip/hs-digest/buildlink3.mk"
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-utf8-string/buildlink3.mk"
+.include "../../wip/hs-zlib/buildlink3.mk"
 .endif	# HS_ZIP_ARCHIVE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-zip-archive
