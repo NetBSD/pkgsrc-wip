8e63ba9915c43fb21c1721a628492467811b1cf3
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-12-10 13:37:42 +0000

    Update to 0.10.7
    CHANGES:
    0.10.7
        #1, fix timestamps in .tar.gz dist file
     ( #1 => https://github.com/ndmitchell/cmdargs/issues/1 )

--- hs-cmdargs/buildlink3.mk
+++ hs-cmdargs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/12/05 23:05:28 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/12/10 13:37:42 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cmdargs
 
 .if !defined(HS_CMDARGS_BUILDLINK3_MK)
 HS_CMDARGS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.6
+BUILDLINK_API_DEPENDS.hs-cmdargs+=	hs-cmdargs>=0.10.7
 BUILDLINK_PKGSRCDIR.hs-cmdargs?=	../../wip/hs-cmdargs
 
 .include "../../wip/hs-transformers/buildlink3.mk"
