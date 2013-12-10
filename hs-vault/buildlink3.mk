44a0e31432d857612d480a87b20c0442e0b65c97
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-12-10 21:51:15 +0000

    Update to 0.3.0.3
    Changelog:
    0.3.0.3
     - Bump version of base.

--- hs-vault/buildlink3.mk
+++ hs-vault/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/09/12 20:11:38 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/12/10 21:51:16 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-vault
 
@@ -7,6 +7,9 @@ HS_VAULT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.hs-vault+=	hs-vault>=0.3
 BUILDLINK_PKGSRCDIR.hs-vault?=	../../wip/hs-vault
+
+.include "../../wip/hs-hashable/buildlink3.mk"
+.include "../../wip/hs-unordered-containers/buildlink3.mk"
 .endif	# HS_VAULT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-vault
