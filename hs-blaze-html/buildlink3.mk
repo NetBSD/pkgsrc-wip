0d4519b6e797eb8c65fd0715fe7125185121d71f
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-12-10 13:35:40 +0000

    Update to 0.6.1.2
    changes:
    0.6.1.2
     - Bump text dependency

--- hs-blaze-html/buildlink3.mk
+++ hs-blaze-html/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/08/22 19:54:54 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/12/10 13:35:41 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-blaze-html
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.hs-blaze-html+=	hs-blaze-html>=0.6.1
 BUILDLINK_PKGSRCDIR.hs-blaze-html?=	../../wip/hs-blaze-html
 
 .include "../../wip/hs-blaze-builder/buildlink3.mk"
+.include "../../wip/hs-blaze-markup/buildlink3.mk"
 .include "../../wip/hs-text/buildlink3.mk"
 .endif	# HS_BLAZE_HTML_BUILDLINK3_MK
 
