4f380b87c8ca5b11d51c6e984ce9b9eb3db59e49
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2013-12-18 16:03:45 +0000

    Snapshot 20131215. Adjust patch for broken cabs on NetBSD for long double complex.

--- gcc49snapshot/patches/patch-gcc_fortran_f95-lang.c
+++ gcc49snapshot/patches/patch-gcc_fortran_f95-lang.c
@@ -1,20 +1,21 @@
-$NetBSD: patch-gcc_fortran_f95-lang.c,v 1.1 2013/03/24 10:15:20 keckhardt Exp $
+$NetBSD: patch-gcc_fortran_f95-lang.c,v 1.2 2013/12/18 16:03:46 keckhardt Exp $
 
-No NetBSD the 'cabs' and 'cabsf' functions are are __RENAME so that
-thay have '__c99_' prefixes.
-
---- gcc/fortran/f95-lang.c.orig	2012-01-17 14:34:37.000000000 +0000
+--- gcc/fortran/f95-lang.c.orig	2013-11-12 20:26:43.000000000 +0000
 +++ gcc/fortran/f95-lang.c
-@@ -819,10 +819,17 @@ gfc_init_builtin_functions (void)
+@@ -713,12 +713,21 @@ gfc_init_builtin_functions (void)
+   gfc_define_builtin ("__builtin_truncf", mfunc_float[0],
+ 		      BUILT_IN_TRUNCF, "truncf", ATTR_CONST_NOTHROW_LEAF_LIST);
  
++#if defined(__NetBSD__)
++  gfc_define_builtin ("__builtin_cabsl", func_clongdouble_longdouble, 
++		      BUILT_IN_CABSL, "__c99_cabsl", ATTR_CONST_NOTHROW_LEAF_LIST);
++  gfc_define_builtin ("__builtin_cabs", func_cdouble_double, 
++		      BUILT_IN_CABS, "__c99_cabs", ATTR_CONST_NOTHROW_LEAF_LIST);
++  gfc_define_builtin ("__builtin_cabsf", func_cfloat_float, 
++		      BUILT_IN_CABSF, "__c99_cabsf", ATTR_CONST_NOTHROW_LEAF_LIST);
++#else
    gfc_define_builtin ("__builtin_cabsl", func_clongdouble_longdouble, 
  		      BUILT_IN_CABSL, "cabsl", ATTR_CONST_NOTHROW_LEAF_LIST);
-+#if defined(__NetBSD__)
-+  gfc_define_builtin ("__builtin_cabs", func_cdouble_double, 
-+		     BUILT_IN_CABS, "__c99_cabs", ATTR_CONST_NOTHROW_LEAF_LIST);
-+  gfc_define_builtin ("__builtin_cabsf", func_cfloat_float, 
-+		     BUILT_IN_CABSF, "__c99_cabsf", ATTR_CONST_NOTHROW_LEAF_LIST);
-+#else
    gfc_define_builtin ("__builtin_cabs", func_cdouble_double, 
  		      BUILT_IN_CABS, "cabs", ATTR_CONST_NOTHROW_LEAF_LIST);
    gfc_define_builtin ("__builtin_cabsf", func_cfloat_float, 
--- gcc49snapshot/version.mk
+++ gcc49snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.23 2013/11/27 06:32:33 keckhardt Exp $
-GCC49S_DIST_VERSION:=4.9-20131124
+# $NetBSD: version.mk,v 1.24 2013/12/18 16:03:46 keckhardt Exp $
+GCC49S_DIST_VERSION:=4.9-20131215
