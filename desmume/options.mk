0d569465377a3341f3463b054a59675b73b4ffa5
Blue Rats <rodent@NetBSD.org>
2013-12-19 03:25:56 +0000

    Updated to latest release. Added patchset to make this build and install correctly. Added nls PKG_OPTION and adjusted PLIST accordingly. Upstream named DISTFILE with .tar.tar extension, but it's a gzip'd tar file. Added glib2 as a dependency and made libSoundTouch a full dependency.

--- desmume/options.mk
+++ desmume/options.mk
@@ -1,16 +1,25 @@
-# $NetBSD: options.mk,v 1.2 2012/05/04 15:38:34 noud4 Exp $
+# $NetBSD: options.mk,v 1.3 2013/12/19 03:25:56 othyro Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.desmume
-PKG_SUPPORTED_OPTIONS=	lua openal
-PKG_SUGGESTED_OPTIONS=	lua
+PKG_SUPPORTED_OPTIONS=	lua nls openal
+PKG_SUGGESTED_OPTIONS=	lua nls
+PLIST_VARS+=		nls
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mlua)
+LUA_VERSIONS_ACCEPTED=	51
 .include "../../lang/lua/buildlink3.mk"
 #CFLAGS+=		-DHAVE_LUA=1
 .else
 CFLAGS+=		-DHAVE_LUA=0	# XXX needs lua-engine.h ?
+.endif
+
+.if !empty(PKG_OPTIONS:Mnls)
+.include "../../devel/gettext-lib/buildlink3.mk"
+PLIST.nls=		yes
+.else
+CONFIGURE_ARGS+=	--disable-nls
 .endif
 
 .if !empty(PKG_OPTIONS:Mopenal)
---
+++ desmume/patches/patch-src_arm__jit.cpp
@@ -0,0 +1,16 @@
+$NetBSD: patch-src_arm__jit.cpp,v 1.1 2013/12/19 03:25:57 othyro Exp $
+
+Set correct type for popcount().
+
+--- src/arm_jit.cpp.orig	2013-11-28 00:37:17.000000000 +0000
++++ src/arm_jit.cpp
+@@ -2067,7 +2067,8 @@ static int OP_SWPB(const u32 i) { return
+ //-----------------------------------------------------------------------------
+ //   LDMIA / LDMIB / LDMDA / LDMDB / STMIA / STMIB / STMDA / STMDB
+ //-----------------------------------------------------------------------------
+-static u32 popcount(u32 x)
++
++extern u32 popcount(u32 x)
+ {
+ 	uint32_t pop = 0;
+ 	for(; x; x>>=1)
