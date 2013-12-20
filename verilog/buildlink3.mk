177443f0315109fe1d700168baedae91be59c6db
Makoto Fujiwara <makoto@ki.nu>
2013-12-20 13:33:27 +0000

    Import verilog-0.9.7 as wip/verilog.
    Icarus Verilog is a Verilog simulation and synthesis tool. It operates as a
    compiler, compiling source code writen in Verilog (IEEE-1364) into some target
    format. For batch simulation, the compiler can generate C++ code that is
    compiled and linked with a run time library (called "vvm") then executed as
    a command to run the simulation. For synthesis, the compiler generates
    netlists in the desired format.
    
    The compiler proper is intended to parse and elaborate design descriptions
    written to the IEEE standard IEEE Std 1364-1995. This is a fairly large and
    complex standard, so it will take some time for it to get there, but that's
    the goal. I'll be tracking the upcoming IEEE Std 1364-1999 revision as well,
    and some -1999 features will creep in.

---
+++ verilog/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/12/20 13:33:27 makoto Exp $
+
+BUILDLINK_TREE+=	verilog
+
+.if !defined(VERILOG_BUILDLINK3_MK)
+VERILOG_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.verilog+=	verilog>=0.8.1nb1
+BUILDLINK_ABI_DEPENDS.verilog+=	verilog>=0.8.7nb1
+BUILDLINK_PKGSRCDIR.verilog?=	../../cad/verilog
+
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../devel/gperf/buildlink3.mk"
+.include "../../mk/readline.buildlink3.mk"
+.endif # VERILOG_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-verilog
