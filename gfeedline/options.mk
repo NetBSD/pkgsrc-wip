60fe7fa80455eece18b7fc150091f6cd1ae7eb39
ndb <noud4@users.sourceforge.net>
2013-12-22 17:04:23 +0000

    Get the html files rights right at install.

--- gfeedline/options.mk
+++ gfeedline/options.mk
@@ -1,18 +1,17 @@
-# $NetBSD: options.mk,v 1.1 2013/12/21 15:14:09 noud4 Exp $
+# $NetBSD: options.mk,v 1.2 2013/12/22 17:04:23 noud4 Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gfeedline
 PKG_SUPPORTED_OPTIONS=	proxy spellcheck
-PKG_SUGGESTED_OPTIONS=	# TODO
+PKG_SUGGESTED_OPTIONS=	
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mproxy)
-DEPENDS+=	${PYPKGPREFIX}-libproxy-[0-9]*:../../net/py-libproxy
+#DEPENDS+=	${PYPKGPREFIX}-libproxy-[0-9]*:../../wip/py-libproxy		# TODO
 .endif
 
-#gnome-sushi
+#gnome-sushi									# TODO
 
 .if !empty(PKG_OPTIONS:Mspellcheck)
-# not tested yet
-DEPENDS+=	${PYPKGPREFIX}-gtkspellcheck-[0-9]*:../..//py-gtkspellcheck
+#DEPENDS+=	${PYPKGPREFIX}-gtkspellcheck-[0-9]*:../../wip/py-gtkspellcheck	# TODO
 .endif
