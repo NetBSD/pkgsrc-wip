7eb8870a4307d941a20c4d61c1fe4a4de1d0d31f
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-12-25 19:12:26 +0000

    Update to 0.6.1
    CHANGELOG:
    Version 0.6.1
    * `BoldIntensity` no longer changes background color on Windows
    * `setSGR []` was not equivalent to `setSGR [Reset]` on Windows, even though it
      should be according to the documentation. This is now fixed.

--- hs-ansi-terminal/buildlink3.mk
+++ hs-ansi-terminal/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/03/02 09:56:07 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/12/25 19:12:26 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-ansi-terminal
 
 .if !defined(HS_ANSI_TERMINAL_BUILDLINK3_MK)
 HS_ANSI_TERMINAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.6
+BUILDLINK_API_DEPENDS.hs-ansi-terminal+=	hs-ansi-terminal>=0.6.1
 BUILDLINK_PKGSRCDIR.hs-ansi-terminal?=	../../wip/hs-ansi-terminal
 .endif	# HS_ANSI_TERMINAL_BUILDLINK3_MK
 
