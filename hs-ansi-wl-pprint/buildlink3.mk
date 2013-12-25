2f885ff011b76c70808c4136461323723ec10812
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-12-25 19:08:08 +0000

    Update to 0.6.7
    changes:
    0.6.7
     - Merge pull request #5 from sjindel/master
       * Several algorithmic improvements to the Wadler-Leijen
       pretty-printing algorithm.
        1) Addition of the @Fail@ primitive, representing a document that can not be
           rendered. @Fail@ is a left unit of @Union@ and does not `fit' into any page
           width. It allows the construction of documents that can not be flattened, for
           example comments that must be followed by a newline. It also alows the
           construction of hard lines, which are guaranteed to be rendered as a newline.
        2) Addition of the @FlatAlt@ primitive, representing a document which changes
           under flattening. This unifies the linebreak combinators and allows for more
           flexible linebreak to be created, for example linebreaks in a do-block that
           should become semicolons when flattened.
        3) Addition of a more intelligent rendering algorithm, which partially solves
           the lookahead problem caused by deeply nested structures where a single-line
           lookahead will not always allow a document to be formatted within the
           pagewidth.
     ( #5 => https://github.com/batterseapower/ansi-wl-pprint/pull/5 )

--- hs-ansi-wl-pprint/buildlink3.mk
+++ hs-ansi-wl-pprint/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/03/02 10:37:16 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2013/12/25 19:08:08 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-ansi-wl-pprint
 
 .if !defined(HS_ANSI_WL_PPRINT_BUILDLINK3_MK)
 HS_ANSI_WL_PPRINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ansi-wl-pprint+=	hs-ansi-wl-pprint>=0.6.6
+BUILDLINK_API_DEPENDS.hs-ansi-wl-pprint+=	hs-ansi-wl-pprint>=0.6.7
 BUILDLINK_PKGSRCDIR.hs-ansi-wl-pprint?=	../../wip/hs-ansi-wl-pprint
 
 .include "../../wip/hs-ansi-terminal/buildlink3.mk"
