6050a6dc5e0c95af4d3e4e2945e3e067fef9d4e2
Ryosuke Moro <szptvlfn@NetBSD.org>
2013-12-27 22:08:28 +0000

    Update to 4.0.2
    CHANGELOG:
    4.0.2
    * Added `assoc` to `Data.Profunctor.Composition` so that we have all 3 associators.

--- hs-profunctors/buildlink3.mk
+++ hs-profunctors/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/10/14 10:54:17 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2013/12/27 22:08:28 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-profunctors
 
 .if !defined(HS_PROFUNCTORS_BUILDLINK3_MK)
 HS_PROFUNCTORS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-profunctors+=	hs-profunctors>=4.0.1
+BUILDLINK_API_DEPENDS.hs-profunctors+=	hs-profunctors>=4.0.2
 BUILDLINK_PKGSRCDIR.hs-profunctors?=	../../wip/hs-profunctors
 
 .include "../../wip/hs-comonad/buildlink3.mk"
