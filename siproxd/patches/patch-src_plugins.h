b069c2f9dd2445113eb220229ec1dec2aed94a5a
Emile `iMil' Heitor <imil@home.imil.net>
2013-12-28 14:42:28 +0000

    Fixed siproxd build on NetBSD. Missing sys/time.h + wrong patch checksums

--- siproxd/patches/patch-src_plugins.h
+++ siproxd/patches/patch-src_plugins.h
@@ -1,14 +1,19 @@
-$NetBSD: patch-src_plugins.h,v 1.1 2012/04/07 10:13:28 gschwarz Exp $
+$NetBSD: patch-src_plugins.h,v 1.2 2013/12/28 14:42:29 imilh Exp $
 
---- src/plugins.h.orig	2012-04-07 13:40:18.000000000 +0200
-+++ src/plugins.h	2012-04-07 13:41:34.000000000 +0200
-@@ -28,6 +28,10 @@
+using libtool 2.x
+http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/net-misc/siproxd/files/siproxd-libtool-2.4.patch?view=markup
+
+--- src/plugins.h.orig	2011-05-28 15:47:15.000000000 +0000
++++ src/plugins.h
+@@ -28,6 +28,12 @@
     #include <ltdl.h>
  #endif
  
-+/* libtool 2.2 / 2.4 */
 +#ifndef lt__PROGRAM__LTX_preloaded_symbols
 +#define lt__PROGRAM__LTX_preloaded_symbols lt_libltdl_LTX_preloaded_symbols
++#endif
++#ifndef lt_libltdl_LTX_preloaded_symbols
++extern LT_DLSYM_CONST lt_dlsymlist lt__PROGRAM__LTX_preloaded_symbols[];
 +#endif
  
  /* Plugins must return STS_SUCCESS / SUCCESS_FAILURE */
