51c5ce881bc90ff0768efb423b5892d57b8ab65d
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-01-01 08:08:28 +0000

    Update to 0.13
    changes:
    0.13
     - Remove CWC code for now
     - Fix up some comments
     - Expand Exception module with Crypto.Random functions and comments.
     - Derive 'Data' on ReseedInfo
     - Export functions to generate an IV.
     - Derive Data
     - Add an exceptions-based interface
     - First implementation of reference cwc mode
       We still need cwcLazy and to test the current implementation along with
       document that it isn't a performant implementation.
     - Allow a random gen for asym decryption
     - Move all but auth modes over to Crypto.Classes.

--- hs-crypto-api/buildlink3.mk
+++ hs-crypto-api/buildlink3.mk
@@ -1,13 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.5 2013/09/08 07:36:43 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2014/01/01 08:08:28 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-crypto-api
 
 .if !defined(HS_CRYPTO_API_BUILDLINK3_MK)
 HS_CRYPTO_API_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-crypto-api+=	hs-crypto-api>=0.12.2
+BUILDLINK_API_DEPENDS.hs-crypto-api+=	hs-crypto-api>=0.13
 BUILDLINK_PKGSRCDIR.hs-crypto-api?=	../../wip/hs-crypto-api
 
+.include "../../wip/hs-cereal/buildlink3.mk"
+.include "../../wip/hs-entropy/buildlink3.mk"
+.include "../../wip/hs-tagged/buildlink3.mk"
+.include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_CRYPTO_API_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-crypto-api
