2c41040906a26b2fccbb5ca21f242131d8a31d25
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-01-01 08:01:39 +0000

    Update to 1.0.1
    changes:
    1.0.1
     - Polished @duairc's work in `Control.Monad.Trans.Compose`
       * Merge pull request #7 from duairc/composet
     - Merge pull request #5 from duairc/data-functor-instances
       Add MFunctor instances for the Compose, Product, Backwards and Lift
       types from the transformers package.
     - Merge pull request #6 from duairc/listt-instances
       Add MFunctor and MMonad instances for ListT
     - Merge pull request #2 from fizruk/master
       Added generalize function
    
    ( #7 => https://github.com/Gabriel439/Haskell-MMorph-Library/pull/7 )
    ( #6 => https://github.com/Gabriel439/Haskell-MMorph-Library/pull/6 )
    ( #5 => https://github.com/Gabriel439/Haskell-MMorph-Library/pull/5 )
    ( #2 => https://github.com/Gabriel439/Haskell-MMorph-Library/pull/2 )

--- hs-mmorph/buildlink3.mk
+++ hs-mmorph/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/09/16 23:03:58 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/01/01 08:01:39 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-mmorph
 
 .if !defined(HS_MMORPH_BUILDLINK3_MK)
 HS_MMORPH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.0
+BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.1
 BUILDLINK_PKGSRCDIR.hs-mmorph?=	../../wip/hs-mmorph
 
 .include "../../wip/hs-transformers/buildlink3.mk"
