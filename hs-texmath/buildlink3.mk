9c8cf52c91d2176c2461bac6ead9afeee5f26401
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-01-08 21:42:30 +0000

    Update to 0.6.6
    changelog:
    0.6.6
      * Insert braces around macro expansions to prevent breakage (#7).
      * Support \operatorname and \DeclareMathOperator (rekka) (#17).
      * Support \providecommand (#15).
      * Fixed spacing bugs in pandoc rendering (#24).
      * Ignore \hline at end of array row instead of failing (#19).
    ( #24 => https://github.com/jgm/texmath/issues/24 )
    ( #19 => https://github.com/jgm/texmath/issues/19 )
    ( #17 => https://github.com/jgm/texmath/pull/17 )
    ( #15 => https://github.com/jgm/texmath/issues/15 )
    ( #7 => https://github.com/jgm/texmath/issues/7 )

--- hs-texmath/buildlink3.mk
+++ hs-texmath/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/12/08 01:17:32 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/01/08 21:42:32 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-texmath
 
 .if !defined(HS_TEXMATH_BUILDLINK3_MK)
 HS_TEXMATH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.6.5
+BUILDLINK_API_DEPENDS.hs-texmath+=	hs-texmath>=0.6.6
 BUILDLINK_PKGSRCDIR.hs-texmath?=	../../wip/hs-texmath
 
 .include "../../wip/hs-pandoc-types/buildlink3.mk"
