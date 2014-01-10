1e8cb81cabf305be65b8fae13faaa34fcc6184f2
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-01-10 23:01:01 +0000

    Update to 0.2
    changes:
    0.2
     - Depend on text instead of utf8-string.
     - Added Binary instance for Archive.
       This will allow use of decodeOrFail from recent Data.Binary
       for an error-catching decode.

--- hs-zip-archive/buildlink3.mk
+++ hs-zip-archive/buildlink3.mk
@@ -1,16 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/12/09 22:10:52 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/01/10 23:01:01 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-zip-archive
 
 .if !defined(HS_ZIP_ARCHIVE_BUILDLINK3_MK)
 HS_ZIP_ARCHIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.1.4
+BUILDLINK_API_DEPENDS.hs-zip-archive+=	hs-zip-archive>=0.2
 BUILDLINK_PKGSRCDIR.hs-zip-archive?=	../../wip/hs-zip-archive
 
 .include "../../wip/hs-digest/buildlink3.mk"
 .include "../../wip/hs-mtl/buildlink3.mk"
-.include "../../wip/hs-utf8-string/buildlink3.mk"
+.include "../../wip/hs-text/buildlink3.mk"
 .include "../../wip/hs-zlib/buildlink3.mk"
 .endif	# HS_ZIP_ARCHIVE_BUILDLINK3_MK
 
