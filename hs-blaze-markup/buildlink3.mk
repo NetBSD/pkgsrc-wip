29a73b06e68e0f35c59bec796f939d80eddb1c0b
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-01-14 21:20:34 +0000

    Update to 0.5.2.1
    changes:
    0.5.2.1
     - Add CHANGELOG to Extra-source-files
    0.5.2.0
     * Provide ToHtml and ToValue instances for Int32, Int64, Word, Word32,
       and Word64  Closes #8
    0.5.1.7
     - Merge pull request #7 from DougBurke/patch-1
       Bump text dependency
    ( #8 => https://github.com/jaspervdj/blaze-markup/issues/8 )
    ( #7 => https://github.com/jaspervdj/blaze-markup/pull/7 )

--- hs-blaze-markup/buildlink3.mk
+++ hs-blaze-markup/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/12/10 13:31:36 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/01/14 21:20:34 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-blaze-markup
 
 .if !defined(HS_BLAZE_MARKUP_BUILDLINK3_MK)
 HS_BLAZE_MARKUP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-blaze-markup+=	hs-blaze-markup>=0.5.1
+BUILDLINK_API_DEPENDS.hs-blaze-markup+=	hs-blaze-markup>=0.5.2
 BUILDLINK_PKGSRCDIR.hs-blaze-markup?=	../../wip/hs-blaze-markup
 
 .include "../../wip/hs-blaze-builder/buildlink3.mk"
