a79c44eb708989d16943efd06e52adb452f66096
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-01-14 21:25:04 +0000

    Update to 0.5.6
    changelog:
    highlighting-kate 0.5.6 (07 Jan 2014)
     * Added restructuredtext.
     * Updated rust syntax definition.
     * Changed lua highlighting so variables/constants not highlighted
       as keywords.
     * Updated kate syntax definitions for python, cmake.

--- hs-highlighting-kate/buildlink3.mk
+++ hs-highlighting-kate/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/11/24 12:26:16 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/01/14 21:25:04 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-highlighting-kate
 
 .if !defined(HS_HIGHLIGHTING_KATE_BUILDLINK3_MK)
 HS_HIGHLIGHTING_KATE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-highlighting-kate+=	hs-highlighting-kate>=0.5.5.1
+BUILDLINK_API_DEPENDS.hs-highlighting-kate+=	hs-highlighting-kate>=0.5.6
 BUILDLINK_PKGSRCDIR.hs-highlighting-kate?=	../../wip/hs-highlighting-kate
 
 .include "../../wip/hs-blaze-html/buildlink3.mk"
