5e7e8e05d107a76caf4c1596a41e4f786a850b13
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-01-15 23:28:42 +0000

    Update to 0.1.1
    changes:
    0.1.1
     - version bump due to attoparsec 0.11.1

--- hs-http-attoparsec/buildlink3.mk
+++ hs-http-attoparsec/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/11/26 23:52:59 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/01/15 23:28:42 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-http-attoparsec
 
 .if !defined(HS_HTTP_ATTOPARSEC_BUILDLINK3_MK)
 HS_HTTP_ATTOPARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-http-attoparsec+=	hs-http-attoparsec>=0.1.0
+BUILDLINK_API_DEPENDS.hs-http-attoparsec+=	hs-http-attoparsec>=0.1.1
 BUILDLINK_PKGSRCDIR.hs-http-attoparsec?=	../../wip/hs-http-attoparsec
 
 .include "../../wip/hs-attoparsec/buildlink3.mk"
