8d68b70c652a06cf2de547290d1266e3bd661178
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-01-15 23:26:13 +0000

    Update to 0.11.1.0
    changelog:
    0.11.1.0
        * New dependency: the scientific package.  This allows us to parse
          numbers much more efficiently.
        * peekWord8', peekChar': new primitive parsers that allow
          single-character lookahead.

--- hs-attoparsec/buildlink3.mk
+++ hs-attoparsec/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/02/23 14:34:04 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/01/15 23:26:13 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-attoparsec
 
 .if !defined(HS_ATTOPARSEC_BUILDLINK3_MK)
 HS_ATTOPARSEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.10.4
+BUILDLINK_API_DEPENDS.hs-attoparsec+=	hs-attoparsec>=0.11.1
 BUILDLINK_PKGSRCDIR.hs-attoparsec?=	../../wip/hs-attoparsec
 
+.include "../../wip/hs-scientific/buildlink3.mk"
 .include "../../wip/hs-text/buildlink3.mk"
 .endif	# HS_ATTOPARSEC_BUILDLINK3_MK
 
