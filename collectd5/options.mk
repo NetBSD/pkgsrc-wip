14fe85d6eb4b3897dc76bc913a4368500edb4977
Filip Hajny <filip@joyent.com>
2014-01-18 20:21:58 +0000

    Fix build on NetBSD: - patch lack of float_t in 'df' - disable 'swap' - switch to full libltdl bl3

--- collectd5/options.mk
+++ collectd5/options.mk
@@ -1,36 +1,36 @@
-# $NetBSD: options.mk,v 1.1 2014/01/18 19:13:53 fhajny Exp $
+# $NetBSD: options.mk,v 1.2 2014/01/18 20:21:58 fhajny Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.collectd
-PKG_SUPPORTED_OPTIONS=	cpu df interface load memory perl python swap syslog
-PKG_SUGGESTED_OPTIONS+=	cpu df interface load memory swap syslog
+PKG_SUPPORTED_OPTIONS=	cpu df interface load memory perl python syslog uptime
+PKG_SUGGESTED_OPTIONS+=	cpu df interface load memory syslog uptime
 
 .if ${OPSYS} == "NetBSD"
-PKG_SUPPORTED_OPTIONS+=	contextswitch pf tcpconns uptime users
-PKG_SUGGESTED_OPTIONS+=	contextswitch pf tcpconns uptime users
+PKG_SUPPORTED_OPTIONS+=	contextswitch pf tcpconns users
+PKG_SUGGESTED_OPTIONS+=	contextswitch pf tcpconns users
 .endif
 
 .if ${OPSYS} == "FreeBSD"
-PKG_SUPPORTED_OPTIONS+=	contextswitch pf processes tcpconns uptime zfs-arc
-PKG_SUGGESTED_OPTIONS+=	contextswitch pf processes tcpconns uptime zfs-arc
+PKG_SUPPORTED_OPTIONS+=	contextswitch pf processes swap tcpconns zfs-arc
+PKG_SUGGESTED_OPTIONS+=	contextswitch pf processes swap tcpconns zfs-arc
 .endif
 
 .if ${OPSYS} == "Darwin"
-PKG_SUPPORTED_OPTIONS+=	apple-sensors battery contextswitch disk processes	\
-			tcpconns uptime users
-PKG_SUGGESTED_OPTIONS+=	apple-sensors battery contextswitch disk processes	\
-			tcpconns uptime users
+PKG_SUPPORTED_OPTIONS+=	apple-sensors battery contextswitch disk processes swap	\
+			tcpconns users
+PKG_SUGGESTED_OPTIONS+=	apple-sensors battery contextswitch disk processes swap	\
+			tcpconns users
 .endif
 
 .if ${OPSYS} == "SunOS"
-PKG_SUPPORTED_OPTIONS+=	disk nfs uptime users zfs-arc
-PKG_SUGGESTED_OPTIONS+=	disk nfs uptime users zfs-arc
+PKG_SUPPORTED_OPTIONS+=	disk nfs swap users zfs-arc
+PKG_SUGGESTED_OPTIONS+=	disk nfs swap users zfs-arc
 .endif
 
 .include "../../mk/bsd.options.mk"
 
 PLIST_VARS+=		apple-sensors battery contextswitch cpu df disk		\
-			interface load memory nfs perl pf python swap syslog	\
-			tcpconns uptime users zfs-arc
+			interface load memory nfs perl pf processes python swap	\
+			syslog tcpconns uptime users zfs-arc
 
 .for option in ${PLIST_VARS}
 .  if !empty(PKG_OPTIONS:M${option})
