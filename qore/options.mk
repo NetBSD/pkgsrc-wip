5cc5065125468fd44ad53f7d90fe66d700153a12
Niclas Rosenvik <nros@NetBSD.org>
2014-01-20 18:54:16 +0000

    Added doc option and made sure that MIT and BSD licenses are with in distribution.

--- qore/options.mk
+++ qore/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.3 2014/01/15 17:34:23 nros Exp $
+# $NetBSD: options.mk,v 1.4 2014/01/20 18:54:16 nros Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.qore
-PKG_SUPPORTED_OPTIONS=	debug
-
+PKG_SUPPORTED_OPTIONS=	debug doc
+PLIST_VARS+=		doc
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdebug)
@@ -10,3 +10,9 @@ CONFIGURE_ARGS+=        --enable-debug
 .else
 CONFIGURE_ARGS+=        --disable-debug
 .endif
+
+.if !empty(PKG_OPTIONS:Mdoc)
+INSTALL_TARGET=		install install-html-local
+PLIST.doc=		yes
+.endif
+
