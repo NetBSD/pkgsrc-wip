012d21cfbe51fe38dc06cb650d46578f3a720c4f
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-01-26 14:11:00 +0000

    Update to 3.15.2
    ChangeLog:
    v3.15.2
    Bug fixes:
    - The `(husk random)` library's `randint` function no longer throws a
    runtime error when called.
    - The `newline` function now accepts a port as an optional second argument,
    instead of throwing an error.

--- hs-husk-scheme/buildlink3.mk
+++ hs-husk-scheme/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/01/20 13:59:59 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/01/26 14:11:00 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-husk-scheme
 
 .if !defined(HS_HUSK_SCHEME_BUILDLINK3_MK)
 HS_HUSK_SCHEME_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-husk-scheme+=	hs-husk-scheme>=3.15.1
+BUILDLINK_API_DEPENDS.hs-husk-scheme+=	hs-husk-scheme>=3.15.2
 BUILDLINK_PKGSRCDIR.hs-husk-scheme?=	../../wip/hs-husk-scheme
 
 .include "../../wip/hs-ghc-paths/buildlink3.mk"
