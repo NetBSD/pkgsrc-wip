6f537ad157a01f4f0c48134e0469ae089b1ae0e0
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-01-28 22:56:28 +0000

    Update to 0.13.1
    CHANGES:
    0.13.1
     - #562, treat <script> specially as per HTML5
    ( #562 => http://code.google.com/p/ndmitchell/issues/detail?id=562 )

--- hs-tagsoup/buildlink3.mk
+++ hs-tagsoup/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.6 2013/09/19 10:29:19 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2014/01/28 22:56:28 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-tagsoup
 
 .if !defined(HS_TAGSOUP_BUILDLINK3_MK)
 HS_TAGSOUP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13
+BUILDLINK_API_DEPENDS.hs-tagsoup+=	hs-tagsoup>=0.13.1
 BUILDLINK_PKGSRCDIR.hs-tagsoup?=	../../wip/hs-tagsoup
 
 .include "../../wip/hs-text/buildlink3.mk"
