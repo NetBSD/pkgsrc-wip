cb073456dbb6e170fc9677e9c7a3e0978d7b86c9
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-01-29 22:28:56 +0000

    Update to 1.4
    CHANGELOG:
    # 1.4
    * Changed the behavior of the $(1) template haskell splices for Exp to use
    a Proxy rather than value-level numbers. This is more consistent with the
    role of this library and the other could always be generated via sa splice
    anyways.

--- hs-reflection/buildlink3.mk
+++ hs-reflection/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/05/22 19:12:43 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/01/29 22:28:56 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-reflection
 
 .if !defined(HS_REFLECTION_BUILDLINK3_MK)
 HS_REFLECTION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-reflection+=	hs-reflection>=1.3.2
+BUILDLINK_API_DEPENDS.hs-reflection+=	hs-reflection>=1.4
 BUILDLINK_PKGSRCDIR.hs-reflection?=	../../wip/hs-reflection
 
+.include "../../wip/hs-tagged/buildlink3.mk"
 .endif	# HS_REFLECTION_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-reflection
