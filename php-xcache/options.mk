168487d57446a0abdf3ada5a2fadfc85b90b7daf
Matthias Scheler <tron@NetBSD.org>
2014-01-30 13:15:03 +0000

    Prefix all option names with "xcache-" as they are very specific to this particular package.

--- php-xcache/options.mk
+++ php-xcache/options.mk
@@ -1,36 +1,36 @@
-# $NetBSD: options.mk,v 1.1 2010/12/30 18:44:14 mjoellnir Exp $
+# $NetBSD: options.mk,v 1.2 2014/01/30 13:15:03 trontor Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.php-xcache
 
-PKG_SUPPORTED_OPTIONS=	constant optimizer coverager assembler disassembler encoder decoder
-PKG_SUGGESTED_OPTIONS=	constant optimizer coverager
+PKG_SUPPORTED_OPTIONS=	xcache-constant xcache-optimizer xcache-coverager xcache-assembler xcache-disassembler xcache-encoder xcache-decoder
+PKG_SUGGESTED_OPTIONS=	xcache-constant xcache-optimizer xcache-coverager
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mconstant)
+.if !empty(PKG_OPTIONS:Mxcache-constant)
 CONFIGURE_ARGS+=	--enable-xcache-constant
 .endif
 
-.if !empty(PKG_OPTIONS:Moptimizer)
+.if !empty(PKG_OPTIONS:Mxcache-optimizer)
 CONFIGURE_ARGS+=	--enable-xcache-optimizer
 .endif
 
-.if !empty(PKG_OPTIONS:Mcoverager)
+.if !empty(PKG_OPTIONS:Mxcache-coverager)
 CONFIGURE_ARGS+=	--enable-xcache-coverager
 .endif
 
-.if !empty(PKG_OPTIONS:Massembler)
+.if !empty(PKG_OPTIONS:Mxcache-assembler)
 CONFIGURE_ARGS+=	--enable-xcache-assembler
 .endif
 
-.if !empty(PKG_OPTIONS:Mdisassembler)
+.if !empty(PKG_OPTIONS:Mxcache-disassembler)
 CONFIGURE_ARGS+=	--enable-xcache-disassembler
 .endif
 
-.if !empty(PKG_OPTIONS:Mencoder)
+.if !empty(PKG_OPTIONS:Mxcache-encoder)
 CONFIGURE_ARGS+=	--enable-xcache-encoder
 .endif
 
-.if !empty(PKG_OPTIONS:Mdecoder)
+.if !empty(PKG_OPTIONS:Mxcache-decoder)
 CONFIGURE_ARGS+=	--enable-xcache-decoder
 .endif
