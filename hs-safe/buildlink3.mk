8f020160192a6283d607896dccf71e70a3115cd3
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-02-01 10:10:33 +0000

    Update to 0.3.4
    CHANGES:
    0.3.4
        #1, improve the string clipping in readNote
    ( #1 => https://github.com/ndmitchell/safe/issues/1 )

--- hs-safe/buildlink3.mk
+++ hs-safe/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/11/25 11:20:27 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/02/01 10:10:33 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-safe
 
 .if !defined(HS_SAFE_BUILDLINK3_MK)
 HS_SAFE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-safe+=	hs-safe>=0.3.3
+BUILDLINK_API_DEPENDS.hs-safe+=	hs-safe>=0.3.4
 BUILDLINK_PKGSRCDIR.hs-safe?=	../../wip/hs-safe
 .endif	# HS_SAFE_BUILDLINK3_MK
 
