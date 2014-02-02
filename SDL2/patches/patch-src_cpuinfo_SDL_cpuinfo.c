41f60952c9b25c8fa6b6604e662b42f205dd71a9
Blue Rats <rodent@NetBSD.org>
2014-02-02 21:44:31 +0000

    Updated to latest minor revision, 2.0.1.8147. Shared library name changes. Updated patch-CMakeLists.txt due to source file change.

--- SDL2/patches/patch-src_cpuinfo_SDL_cpuinfo.c
+++ SDL2/patches/patch-src_cpuinfo_SDL_cpuinfo.c
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_cpuinfo_SDL_cpuinfo.c,v 1.1 2013/12/29 05:51:35 othyro Exp $
+$NetBSD: patch-src_cpuinfo_SDL_cpuinfo.c,v 1.2 2014/02/02 21:44:32 othyro Exp $
 
 Fix building on NetBSD and OpenBSD.
 
---- src/cpuinfo/SDL_cpuinfo.c.orig	2013-11-02 19:57:32.000000000 +0000
+--- src/cpuinfo/SDL_cpuinfo.c.orig	2014-02-02 04:07:55.000000000 +0000
 +++ src/cpuinfo/SDL_cpuinfo.c
 @@ -621,7 +621,7 @@ SDL_GetSystemRAM(void)
  #endif
