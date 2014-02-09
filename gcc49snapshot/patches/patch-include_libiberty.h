fc432b931eb4e9f5936895e0c6e65cfa031597fb
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2014-02-09 08:51:51 +0000

    Integrate patches from lang/gcc48.

--- gcc49snapshot/patches/patch-include_libiberty.h
+++ gcc49snapshot/patches/patch-include_libiberty.h
@@ -1,14 +1,15 @@
-$NetBSD: patch-include_libiberty.h,v 1.2 2013/11/09 20:22:09 keckhardt Exp $
+$NetBSD: patch-include_libiberty.h,v 1.3 2014/02/09 08:51:51 keckhardt Exp $
 
---- include/libiberty.h.orig	2013-10-29 13:15:48.000000000 +0000
+--- include/libiberty.h.orig	2014-01-21 15:09:10.000000000 +0000
 +++ include/libiberty.h
-@@ -106,7 +106,15 @@ extern int countargv (char**);
+@@ -106,7 +106,16 @@ extern int countargv (char**);
     to find the declaration so provide a fully prototyped one.  If it
     is 1, we found it so don't provide any declaration at all.  */
  #if !HAVE_DECL_BASENAME
 -#if defined (__GNU_LIBRARY__ ) || defined (__linux__) || defined (__FreeBSD__) || defined (__OpenBSD__) || defined(__NetBSD__) || defined (__CYGWIN__) || defined (__CYGWIN32__) || defined (__MINGW32__) || defined (HAVE_DECL_BASENAME)
 +#if defined (__GNU_LIBRARY__ ) \
 +|| defined (__linux__) \
++|| defined (__Dragonfly__) \
 +|| defined (__FreeBSD__) \
 +|| defined (__OpenBSD__) \
 +|| defined(__NetBSD__) \
