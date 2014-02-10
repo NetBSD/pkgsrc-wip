d6cf0f1417b40fa1bc65dd3d17bfe8d650b9b013
Tim Larson <christtrekker@users.sourceforge.net>
2014-02-10 23:19:28 +0000

    testing

--- fontaine/patches/patch-src_orthographies_Emoticons.h
+++ fontaine/patches/patch-src_orthographies_Emoticons.h
@@ -1,6 +1,6 @@
---- /dev/null	2014-02-05 21:17:19.000000000 -0600
-+++ src/orthographies/Emoticons.h	2014-02-05 21:33:06.000000000 -0600
-@@ -0,0 +1,63 @@
+--- /dev/null	2014-02-10 10:19:28.000000000 -0600
++++ src/orthographies/Emoticons.h	2014-02-10 10:36:24.000000000 -0600
+@@ -0,0 +1,81 @@
 +//
 +// The Fontaine Font Analysis Project 
 +// 
@@ -13,6 +13,9 @@
 +
 +//
 +// Emoticons.h
++// consisting of faces, hearts, gestures, kisses, and two truly abstract
++// symbols only, lest it expand to include every pictograph and dingbat in 
++// Unicode
 +//
 +
 +#ifndef ORTHOGRAPHY_DATA
@@ -30,10 +33,25 @@
 +UINT32 values[]={
 +	START_RANGE_PAIR,
 +	0x2639,0x263B, // Miscellaneous Symbols
++	0x2661,
++	0x2665,
++	START_RANGE_PAIR,
++	0x270A,0x270C, // Dingbats
++	0x2764,
++	START_RANGE_PAIR,
++	0x1F44A,0x1F44F, // Misc. Symbols and Pictographs
++	0x1F48B,
++	0x1F48F,
++	START_RANGE_PAIR,
++	0x1F494,0x1F49C,
++	0x1F4A2,
++	0x1F4AB, 
++	START_RANGE_PAIR,
++	0x1F58F,0x1F596,
 +	START_RANGE_PAIR,
 +	0x1F600,0x1F642, // Emoticons
 +	START_RANGE_PAIR,
-+	0x1F645,0x1F64F, // Emoticons
++	0x1F645,0x1F64F,
 +	END_OF_DATA
 +};
 +
