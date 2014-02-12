d533f7b77e5e52b20b9a2f014f8550c61f9877e2
Thomas Klausner <wiz@NetBSD.org>
2014-02-12 09:29:55 +0000

    Update to 1.9.8. Use github versioned tarball, set LICENSE, mark as build-only dependency in buildlink3.mk, remove unnecessary USE_LANGUAGES, install documentation, install all headers.
    Version 1.9.8 (2013-03-10)
        HASH_REPLACE now in uthash (thanks, Nick Vatamaniuc!)
        fixed clang warnings (thanks wynnw!)
        fixed utarray_insert when inserting past array end (thanks Rob Willett!)
        you can now find uthash on GitHub
        there’s a uthash Google Group
        uthash has been downloaded 29,000+ times since 2006 on SourceForge

--- uthash/buildlink3.mk
+++ uthash/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/12/13 00:46:40 noud4 Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/02/12 09:29:55 thomasklausner Exp $
 
 BUILDLINK_TREE+=	uthash
 
 .if !defined(UTHASH_BUILDLINK3_MK)
 UTHASH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.uthash+=	uthash>=1.9.4
+BUILDLINK_API_DEPENDS.uthash+=	uthash>=1.9.8
 BUILDLINK_PKGSRCDIR.uthash?=	../../wip/uthash
-
-.endif # UTHASH_BUILDLINK3_MK
+BUILDLINK_DEPMETHOD.uthash?=	build
+.endif	# UTHASH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-uthash
