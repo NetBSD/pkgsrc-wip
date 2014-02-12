13ec27876aafd725d08e49e2de012ae86105b39e
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-02-12 23:27:33 +0000

    Update to 0.12.2
    CHANGELOG:
    0.12.2
    * Vastly widened the dependency bound on `text` and `bytestring`.
    0.12.1
    * Updated to support the new version of `text`.
    * Added `transpose`, `sortBy` and `sortWith`.

--- hs-semigroups/buildlink3.mk
+++ hs-semigroups/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2013/11/26 11:42:01 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2014/02/12 23:27:33 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-semigroups
 
 .if !defined(HS_SEMIGROUPS_BUILDLINK3_MK)
 HS_SEMIGROUPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.12
+BUILDLINK_API_DEPENDS.hs-semigroups+=	hs-semigroups>=0.12.2
 BUILDLINK_PKGSRCDIR.hs-semigroups?=	../../wip/hs-semigroups
 
 .include "../../wip/hs-hashable/buildlink3.mk"
