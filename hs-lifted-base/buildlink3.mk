7f5ac15d2b7b7f6aecb1a0905290d26f852d4c6f
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-02-13 13:05:44 +0000

    Update to 0.2.2.0
    changes:
    0.2.2.0
     - Refactor Foreign.Marshal.Utils.Lifted a bit
     - Merge branch 'master' of git://github.com/mikesteele81/lifted-base
    0.2.1.2
     - Removed unnecessary import of Monad

--- hs-lifted-base/buildlink3.mk
+++ hs-lifted-base/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/11/05 13:16:17 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/02/13 13:05:44 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-lifted-base
 
 .if !defined(HS_LIFTED_BASE_BUILDLINK3_MK)
 HS_LIFTED_BASE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-lifted-base+=	hs-lifted-base>=0.2.1.1
+BUILDLINK_API_DEPENDS.hs-lifted-base+=	hs-lifted-base>=0.2.2
 BUILDLINK_PKGSRCDIR.hs-lifted-base?=	../../wip/hs-lifted-base
 
 .include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
