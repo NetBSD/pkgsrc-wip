b0a97f52c57987299a4ec9ace87a090f3a42e2d1
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-02-13 13:07:45 +0000

    Update to 0.4.10
    changes:
    0.4.10
     - Rename withResource to with (thanks @Gabriel439)
     - Resource monad

--- hs-resourcet/buildlink3.mk
+++ hs-resourcet/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/10/13 08:23:39 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/02/13 13:07:45 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-resourcet
 
 .if !defined(HS_RESOURCET_BUILDLINK3_MK)
 HS_RESOURCET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=0.4.9
+BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=0.4.10
 BUILDLINK_PKGSRCDIR.hs-resourcet?=	../../wip/hs-resourcet
 
 .include "../../wip/hs-lifted-base/buildlink3.mk"
