f8ccf8c976279d7b5da440ae6cfcb55738b4add9
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-02-13 13:03:19 +0000

    Update to 1.0.2
    changes:
    1.0.2
     - Guarded Control.Applicative.Lift with ifdef
       This is also necessary to support `transformers-0.2.0.0`
     - Cleaned up pragmas
     - Added CPP guards for `Backwards` instances
       This lets `mmorph` build against `transformers-0.2.*`

--- hs-mmorph/buildlink3.mk
+++ hs-mmorph/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/01/01 08:01:39 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/02/13 13:03:19 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-mmorph
 
 .if !defined(HS_MMORPH_BUILDLINK3_MK)
 HS_MMORPH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.1
+BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.2
 BUILDLINK_PKGSRCDIR.hs-mmorph?=	../../wip/hs-mmorph
 
 .include "../../wip/hs-transformers/buildlink3.mk"
