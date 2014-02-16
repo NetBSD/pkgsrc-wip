f92d49d05a104f7bcaa2f6084260760af6c453f8
Niclas Rosenvik <nros@NetBSD.org>
2014-02-16 15:56:54 +0000

    Changed option, docs always installed and in a more conforming location.

--- qore-opengl-module/options.mk
+++ qore-opengl-module/options.mk
@@ -1,21 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2014/02/14 21:03:04 nros Exp $
+# $NetBSD: options.mk,v 1.2 2014/02/16 15:56:54 nros Exp $
 #
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.qore
-PKG_SUPPORTED_OPTIONS=	debug doc
-PLIST_VARS+=		doc
+PKG_OPTIONS_VAR=	PKG_OPTIONS.qore-opengl-module
+PKG_SUPPORTED_OPTIONS=	debug
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdebug)
 CONFIGURE_ARGS+=        --enable-debug
 .else
 CONFIGURE_ARGS+=        --disable-debug
-.endif
-
-.if !empty(PKG_OPTIONS:Mdoc)
-INSTALLATION_DIRS+=	share/qore-opengl-module/docs/html
-PLIST.doc=		yes
-post-install:
-	${INSTALL_DATA} ${WRKSRC}/docs/opengl-module-doc.html \
-		${DESTDIR}${PREFIX}/share/qore-opengl-module/docs/html
 .endif
