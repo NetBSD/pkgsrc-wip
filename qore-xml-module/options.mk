0a6d235d3268caf31a9a92d69fbeda74b76148b1
Niclas Rosenvik <nros@NetBSD.org>
2014-02-16 19:38:23 +0000

    Removed doxygen dependecy for the docs. Changed installation dir for docs and license. Removed doc option, docs are now always installed. Patched so that the installer doesn't install .a files. Taking maintainership since I imported the package. Removed usage of wip/qore/Makefiel.common and wip/qore/options.mk. Added missing PTHREAD_OPTS. Bumping pkgrevision.

---
+++ qore-xml-module/options.mk
@@ -0,0 +1,12 @@
+# $NetBSD: options.mk,v 1.1 2014/02/16 19:38:23 nros Exp $
+#
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.qore-xml-module
+PKG_SUPPORTED_OPTIONS=	debug
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=        --enable-debug
+.else
+CONFIGURE_ARGS+=        --disable-debug
+.endif
