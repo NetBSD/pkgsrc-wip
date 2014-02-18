89e6afefe54314231659ea87e75e0ee101a82714
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-02-18 14:29:41 +0000

    Update to 0.11.1
    CHANGES:
    0.11.1
        #94, GHC 7.8 support
        Add a Config module
        #89, support :: as a build rule separator
    ( #94 => https://github.com/ndmitchell/shake/issues/94 )
    ( #89 => https://github.com/ndmitchell/shake/issues/89 )

--- hs-shake/buildlink3.mk
+++ hs-shake/buildlink3.mk
@@ -1,16 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/02/01 10:14:49 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/02/18 14:29:41 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-shake
 
 .if !defined(HS_SHAKE_BUILDLINK3_MK)
 HS_SHAKE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-shake+=	hs-shake>=0.11
+BUILDLINK_API_DEPENDS.hs-shake+=	hs-shake>=0.11.1
 BUILDLINK_PKGSRCDIR.hs-shake?=	../../wip/hs-shake
 
 .include "../../wip/hs-hashable/buildlink3.mk"
 .include "../../wip/hs-random/buildlink3.mk"
-.include "../../wip/hs-text/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
 .include "../../wip/hs-unordered-containers/buildlink3.mk"
 .include "../../wip/hs-utf8-string/buildlink3.mk"
