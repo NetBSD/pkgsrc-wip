11eb5e441f86dce809b93a8f5d962d5028ee31ab
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-02-18 14:27:19 +0000

    Update to 1.18.1
    CHANGELOG:
    Version 1.18
    * better lexing of Template Haskell single quotes (thanks to Stephan Wehr)
    * (1.18.1): fix incomplete pattern match

--- hs-cpphs/buildlink3.mk
+++ hs-cpphs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2013/08/16 21:42:29 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2014/02/18 14:27:19 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cpphs
 
 .if !defined(HS_CPPHS_BUILDLINK3_MK)
 HS_CPPHS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.17
+BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.1
 BUILDLINK_PKGSRCDIR.hs-cpphs?=	../../wip/hs-cpphs
 .endif	# HS_CPPHS_BUILDLINK3_MK
 
