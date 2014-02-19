1b88646aa52a6763ee792a71b5d3539b75188ffa
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-02-19 09:16:31 +0000

    Update to 1.8.57
    CHANGES:
    1.8.57
        #6, add a preview of an API
        #331, improve parse error locations for literate Haskell
    1.8.56
        Remove support for GHC 6.12 and below
        #317, tone down the void hint
        #16, match not . not (and reverse . reverse etc)
        Suggest <$> instead of fmap f $ ...
        Tweak some priorities, make >=> a warn and void an error
        #3, make top of the file ANN pragmas work
        #10, add a suggestion to use unlines
        #11, add a few hints about characters
        #8, add CHANGES.txt to the Cabal package

--- hlint/buildlink3.mk
+++ hlint/buildlink3.mk
@@ -1,17 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.11 2013/11/29 14:01:37 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2014/02/19 09:16:31 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hlint
 
 .if !defined(HLINT_BUILDLINK3_MK)
 HLINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8.55
+BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8.57
 BUILDLINK_PKGSRCDIR.hlint?=	../../wip/hlint
 
 .include "../../wip/hs-cpphs/buildlink3.mk"
 .include "../../wip/hs-haskell-src-exts/buildlink3.mk"
 .include "../../wip/hs-hscolour/buildlink3.mk"
-.include "../../wip/hs-text/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
 .include "../../wip/hs-uniplate/buildlink3.mk"
 .endif	# HLINT_BUILDLINK3_MK
