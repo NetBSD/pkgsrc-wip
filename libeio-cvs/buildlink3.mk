4dd771fd216d50afbadc3c16028637f98e826299
Makoto Fujiwara <makoto@ki.nu>
2014-02-20 05:44:55 +0000

    pbulk does not accept single word, (found with wip/ruby-passenger).

--- libeio-cvs/buildlink3.mk
+++ libeio-cvs/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/02/02 14:01:40 makoto Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/02/20 05:44:55 makoto Exp $
 
 BUILDLINK_TREE+=	libeio
 
 .if !defined(LIBEIO_BUILDLINK3_MK)
 LIBEIO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libeio+=	libeio
-BUILDLINK_ABI_DEPENDS.libeio+=	libeio
+BUILDLINK_API_DEPENDS.libeio+=	libeio>=4.0
+BUILDLINK_ABI_DEPENDS.libeio+=	libeio>=4.0
 BUILDLINK_PKGSRCDIR.libeio?=	../../wip/libeio-cvs
 
 BUILDLINK_LIBDIRS.libeio+=	lib/eio
