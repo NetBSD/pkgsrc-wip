b03c420693bd660e51c32556cf80033e0516c922
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-02-25 22:19:36 +0000

    Update to 0.1.3
    changes:
    0.1.3
     - Change `binary` upper bound from 0.6 to 0.8

--- hs-timezone-olson/buildlink3.mk
+++ hs-timezone-olson/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/02/21 10:39:02 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/02/25 22:19:36 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-timezone-olson
 
 .if !defined(HS_TIMEZONE_OLSON_BUILDLINK3_MK)
 HS_TIMEZONE_OLSON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-timezone-olson+=	hs-timezone-olson>=0.1.2
+BUILDLINK_API_DEPENDS.hs-timezone-olson+=	hs-timezone-olson>=0.1.3
 BUILDLINK_PKGSRCDIR.hs-timezone-olson?=	../../wip/hs-timezone-olson
 
 .include "../../wip/hs-extensible-exceptions/buildlink3.mk"
