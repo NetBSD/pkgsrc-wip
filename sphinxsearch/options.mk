3a7b2c7113fc37eeb7df340c4620267b39d9afcd
Filip Hajny <filip@joyent.com>
2014-03-05 10:31:09 +0000

    Update sphinxsearch to 2.1.6. Fix build on NetBSD. Force libiconv support as standard.
    Changes in 2.1.6:
    - fixed #1882, race of periodic and forced FLUSHing on an RT index
    - fixed #1881, quorum syntax with '.' as blended char
    - fixed #1880, crash on multiquery with one incorrect query
    - fixed #1876, crash on words with large code points and infix searches
    - fixed #1875, fixed crash on adding documents with long words in dict=keyword index with morphology and infixes enabled
    - fixed #1857, crash in arabic stemmer
    - fixed evaluating of LCS by an expression ranker

--- sphinxsearch/options.mk
+++ sphinxsearch/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.2 2009/10/02 16:43:34 fhajny Exp $
+# $NetBSD: options.mk,v 1.3 2014/03/05 10:31:09 fhajny Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.sphinx-search
 PKG_OPTIONS_NONEMPTY_SETS=	db
 PKG_OPTIONS_SET.db=		mysql pgsql
-PKG_SUPPORTED_OPTIONS=		iconv
-PKG_SUGGESTED_OPTIONS=		mysql iconv
+PKG_SUPPORTED_OPTIONS=		#
+PKG_SUGGESTED_OPTIONS=		mysql
 
 .include "../../mk/bsd.options.mk"
 
@@ -20,12 +20,4 @@ CONFIGURE_ARGS+=	--with-pgsql
 .  include "../../mk/pgsql.buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-pgsql
-.endif
-
-.if !empty(PKG_OPTIONS:Miconv)
-CONFIGURE_ARGS+=	--with-iconv
-LDFLAGS+=		-liconv
-.  include "../../converters/libiconv/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-iconv
 .endif
--- sphinxsearch/patches/patch-src_sphinxstd.h
+++ sphinxsearch/patches/patch-src_sphinxstd.h
@@ -1,16 +1,18 @@
-$NetBSD: patch-src_sphinxstd.h,v 1.2 2013/05/31 20:45:15 fhajny Exp $
+$NetBSD: patch-src_sphinxstd.h,v 1.3 2014/03/05 10:31:09 fhajny Exp $
 
 Portability fixes.
 
---- src/sphinxstd.h.orig	2013-04-18 15:15:44.000000000 +0000
+--- src/sphinxstd.h.orig	2014-01-22 15:16:21.000000000 +0000
 +++ src/sphinxstd.h
-@@ -67,6 +67,11 @@ typedef int __declspec("SAL_nokernel") _
+@@ -75,6 +75,13 @@ typedef int __declspec("SAL_nokernel") _
  #endif
  #endif
  
 +#ifdef __NetBSD__
 +#define pthread_mutexattr_setpshared(m,a)	(0)
 +#define pthread_condattr_setpshared(m,a)	(0)
++#include "sys/signal.h"
++#define PTHREAD_STACK_MIN MINSIGSTKSZ
 +#endif
 +
  /////////////////////////////////////////////////////////////////////////////
