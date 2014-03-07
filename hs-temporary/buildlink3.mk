ddec77a9f0b73c7c363a0b131232e495f671b3f7
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-03-07 12:40:24 +0000

    Update to 1.2.0.1
    changes:
    1.2, 1.2.0.1
     - Merge branch 'master' of https://github.com/cmears/temporary
    1.1.2.5
     - Merge pull request #8 from xich/master
       Fix for 7.8
    ( #8 => https://github.com/batterseapower/temporary/pull/8 )

--- hs-temporary/buildlink3.mk
+++ hs-temporary/buildlink3.mk
@@ -1,12 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/08/22 23:11:16 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/03/07 12:40:24 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-temporary
 
 .if !defined(HS_TEMPORARY_BUILDLINK3_MK)
 HS_TEMPORARY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-temporary+=	hs-temporary>=1.1.2.4
+BUILDLINK_API_DEPENDS.hs-temporary+=	hs-temporary>=1.2.0
 BUILDLINK_PKGSRCDIR.hs-temporary?=	../../wip/hs-temporary
+
+.include "../../wip/hs-exceptions/buildlink3.mk"
+.include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_TEMPORARY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-temporary
