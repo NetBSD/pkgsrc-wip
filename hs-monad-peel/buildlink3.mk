7d4eaa24d24edd5f10312586f15559192bbc48f0
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-03-07 12:42:22 +0000

    Update to 0.1.1
    changes:
    0.1.1
     - Relax transformers dependency
     - Add liftOp and liftOp_
     - Remove notes about monadic side effects in bracketOnError and onException

--- hs-monad-peel/buildlink3.mk
+++ hs-monad-peel/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/02/21 10:39:01 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/03/07 12:42:22 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-monad-peel
 
 .if !defined(HS_MONAD_PEEL_BUILDLINK3_MK)
 HS_MONAD_PEEL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-monad-peel+=	hs-monad-peel>=0.1
+BUILDLINK_API_DEPENDS.hs-monad-peel+=	hs-monad-peel>=0.1.1
 BUILDLINK_PKGSRCDIR.hs-monad-peel?=	../../wip/hs-monad-peel
 
 .include "../../wip/hs-extensible-exceptions/buildlink3.mk"
