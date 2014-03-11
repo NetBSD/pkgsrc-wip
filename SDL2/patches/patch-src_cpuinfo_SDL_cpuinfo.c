4789ada973a817709d6f4bdd27899ce3f839e91b
Blue Rats <rodent@NetBSD.org>
2014-03-11 03:47:37 +0000

    Updated to latest minor revision, 8297. One patch required updating.

--- SDL2/patches/patch-src_cpuinfo_SDL_cpuinfo.c
+++ SDL2/patches/patch-src_cpuinfo_SDL_cpuinfo.c
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_cpuinfo_SDL_cpuinfo.c,v 1.3 2014/02/17 00:08:35 othyro Exp $
+$NetBSD: patch-src_cpuinfo_SDL_cpuinfo.c,v 1.4 2014/03/11 03:47:37 othyro Exp $
 
 Fix building on NetBSD and OpenBSD.
 
---- src/cpuinfo/SDL_cpuinfo.c.orig	2014-02-07 21:35:46.000000000 +0000
+--- src/cpuinfo/SDL_cpuinfo.c.orig	2014-03-01 18:07:06.000000000 +0000
 +++ src/cpuinfo/SDL_cpuinfo.c
-@@ -653,7 +653,7 @@ SDL_GetSystemRAM(void)
+@@ -665,7 +665,7 @@ SDL_GetSystemRAM(void)
  #endif
  #ifdef HAVE_SYSCTLBYNAME
          if (SDL_SystemRAM <= 0) {
--#ifdef __FreeBSD__
-+#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
+-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
++#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__OpenBSD__)
  #ifdef HW_REALMEM
              int mib[2] = {CTL_HW, HW_REALMEM};
  #else
