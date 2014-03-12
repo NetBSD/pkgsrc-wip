e906f8c30791231ccbc7a3b60f87308ff852bb61
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-03-12 23:09:15 +0000

    Update to 1.1.0.0
    CHANGELOG:
    1.1.0.0
     - Use exceptions package instead of MonadCatchIO-mtl
     - Builds with GHC 7.8.x

--- hs-ghc-mtl/buildlink3.mk
+++ hs-ghc-mtl/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2012/03/11 06:06:25 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/03/12 23:09:15 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-ghc-mtl
 
 .if !defined(HS_GHC_MTL_BUILDLINK3_MK)
 HS_GHC_MTL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ghc-mtl+=	hs-ghc-mtl>=1.0.1
+BUILDLINK_API_DEPENDS.hs-ghc-mtl+=	hs-ghc-mtl>=1.1.0
 BUILDLINK_PKGSRCDIR.hs-ghc-mtl?=	../../wip/hs-ghc-mtl
 
-.include "../../wip/hs-MonadCatchIO-mtl/buildlink3.mk"
+.include "../../wip/hs-exceptions/buildlink3.mk"
+.include "../../wip/hs-extensible-exceptions/buildlink3.mk"
 .include "../../wip/hs-mtl/buildlink3.mk"
 .endif	# HS_GHC_MTL_BUILDLINK3_MK
 
