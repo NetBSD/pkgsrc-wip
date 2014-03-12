5f0c53102ff8a6c6f4010b7fc21eb74c8342ec88
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-03-12 23:11:13 +0000

    Update to 0.4.0.0
    CHANGELOG:
    0.4.0.0
     * Compiles with ghc 7.8
     * Fixed an issue where "P" was available as a qualified version of Prelude
       (thanks to Samuel Gélineau)
     * Uses exceptions package instead of MonadCatchIO-mtl (API breakage expected)
     * No longer depends on haskell-src
     * Changelog should now appear in Hackage
     * Integrated unit tests with cabal

--- hs-hint/buildlink3.mk
+++ hs-hint/buildlink3.mk
@@ -1,18 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/02/21 10:39:01 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/03/12 23:11:13 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-hint
 
 .if !defined(HS_HINT_BUILDLINK3_MK)
 HS_HINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hint+=	hs-hint>=0.3.3
+BUILDLINK_API_DEPENDS.hs-hint+=	hs-hint>=0.4.0
 BUILDLINK_PKGSRCDIR.hs-hint?=	../../wip/hs-hint
 
-.include "../../wip/hs-MonadCatchIO-mtl/buildlink3.mk"
+.include "../../wip/hs-exceptions/buildlink3.mk"
 .include "../../wip/hs-extensible-exceptions/buildlink3.mk"
 .include "../../wip/hs-ghc-mtl/buildlink3.mk"
 .include "../../wip/hs-ghc-paths/buildlink3.mk"
-.include "../../wip/hs-haskell-src/buildlink3.mk"
 .include "../../wip/hs-mtl/buildlink3.mk"
 .include "../../wip/hs-random/buildlink3.mk"
 .include "../../wip/hs-utf8-string/buildlink3.mk"
