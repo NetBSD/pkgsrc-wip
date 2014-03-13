d96581edf299706fde5ecad9a157e102f8e5dad9
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-03-13 12:15:51 +0000

    Update to 1.8.58
    CHANGES:
    1.8.58
        Move the API to Language.Haskell.HLint2
        #638, ensure $! doesn't break strictness with strict fields
        #24, don't remove DeriveFunctor even when only on a newtype
        #22, turn off UnboxedTuples by default
        #21, strip /* C style */ comments
        #635 and #18, require cpphs-1.18.1
        Switch to CmdArgs for command line parsing
        Remove -x as a synonym for --cross
    ( #638 => http://code.google.com/p/ndmitchell/issues/detail?id=638 )
    ( #635 => http://code.google.com/p/ndmitchell/issues/detail?id=635 )
    ( #24 => https://github.com/ndmitchell/hlint/issues/24 )
    ( #22 => https://github.com/ndmitchell/hlint/issues/22 )
    ( #21 => https://github.com/ndmitchell/hlint/issues/21 )
    ( #18 => https://github.com/ndmitchell/hlint/issues/18 )

--- hlint/buildlink3.mk
+++ hlint/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.12 2014/02/19 09:16:31 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2014/03/13 12:15:51 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hlint
 
 .if !defined(HLINT_BUILDLINK3_MK)
 HLINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8.57
+BUILDLINK_API_DEPENDS.hlint+=	hlint>=1.8.58
 BUILDLINK_PKGSRCDIR.hlint?=	../../wip/hlint
 
+.include "../../wip/hs-cmdargs/buildlink3.mk"
 .include "../../wip/hs-cpphs/buildlink3.mk"
 .include "../../wip/hs-haskell-src-exts/buildlink3.mk"
 .include "../../wip/hs-hscolour/buildlink3.mk"
