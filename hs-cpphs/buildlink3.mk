edc4ffb5ffd1b261478207565fc602e74b333726
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-03-16 12:21:07 +0000

    Update to 1.18.3
    CHANGELOG:
    * (1.18.2): bugfix for erroneous boolean intepretation of some macro
                expansions in #if clauses
    * (1.18.3): further rewrites of the #if expression parser

--- hs-cpphs/buildlink3.mk
+++ hs-cpphs/buildlink3.mk
@@ -1,12 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.10 2014/02/18 14:27:19 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2014/03/16 12:21:08 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cpphs
 
 .if !defined(HS_CPPHS_BUILDLINK3_MK)
 HS_CPPHS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.1
+BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.3
 BUILDLINK_PKGSRCDIR.hs-cpphs?=	../../wip/hs-cpphs
+
+.include "../../wip/hs-polyparse/buildlink3.mk"
 .endif	# HS_CPPHS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-cpphs
