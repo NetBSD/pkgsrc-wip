efa41200b8c0ca3a6fc0093f7c5b3a0541bc8cec
Thomas Klausner <wiz@NetBSD.org>
2014-03-23 13:54:43 +0000

    One patch merged upstream, new test patch included instead.

--- bup-git/patches/patch-t_test-ls.sh
+++ bup-git/patches/patch-t_test-ls.sh
@@ -1,19 +1,35 @@
-$NetBSD: patch-t_test-ls.sh,v 1.1 2014/03/21 21:03:44 thomasklausner Exp $
+$NetBSD: patch-t_test-ls.sh,v 1.2 2014/03/23 13:54:43 thomasklausner Exp $
 
-Fix group test, from upstream.
+Symlink size varies in some cases, ignore it.
+From upstream.
 
---- t/test-ls.sh.orig	2014-03-21 01:16:29.000000000 +0000
+--- t/test-ls.sh.orig	2014-03-23 13:40:22.000000000 +0000
 +++ t/test-ls.sh
-@@ -132,9 +132,10 @@ symlink_date="$(WVPASS echo "$symlink_da
+@@ -123,14 +123,22 @@ d--------- ?/? 0 1970-01-01 00:00 src/"
+ 
+ symlink_mode="$(WVPASS ls -l src/symlink | cut -b -10)" || exit $?
+ 
+-symlink_size="$(WVPASS python -c "import os
+-print os.lstat('src/symlink').st_size")" || exit $?
+-
+-symlink_date="$(WVPASS bup ls -l src/latest"$tmpdir"/src | grep symlink)" || exit $?
+-symlink_date="$(WVPASS echo "$symlink_date" \
++symlink_bup_info="$(WVPASS bup ls -l src/latest"$tmpdir"/src | grep symlink)" \
++    || exit $?
++symlink_date="$(WVPASS echo "$symlink_bup_info" \
+   | WVPASS perl -ne 'm/.*? (\d+) (\d\d\d\d-\d\d-\d\d \d\d:\d\d)/ and print $2')" \
      || exit $?
  
++if test "$(uname -s)" != NetBSD; then
++    symlink_size="$(WVPASS python -c "import os
++print os.lstat('src/symlink').st_size")" || exit $?
++else
++    # NetBSD appears to return varying sizes, so for now, just ignore it.
++    symlink_size="$(WVPASS echo "$symlink_bup_info" \
++      | WVPASS perl -ne 'm/.*? (\d+) (\d\d\d\d-\d\d-\d\d \d\d:\d\d)/ and print $1')" \
++        || exit $?
++fi
++
  uid="$(id -u)" || exit $?
--gid="$(id -g)" || exit $?
-+gid="$(python -c 'import os; print os.stat("src").st_gid')" || exit $?
+ gid="$(python -c 'import os; print os.stat("src").st_gid')" || exit $?
  user="$(id -un)" || exit $?
--group="$(id -gn)" || exit $?
-+group="$(python -c 'import grp, os, stat                                                                                                                                                                         
-+print grp.getgrgid(os.stat("src").st_gid)[0]')"
- 
- WVPASSEQ "$(bup ls -l src/latest"$tmpdir"/src | tr -s ' ' ' ')" \
- "-rwx------ $user/$group 0 1969-07-20 20:18 executable
