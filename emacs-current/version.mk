86e4cebf785cb7c3d1338156da5e1e4e402b1d26
Makoto Fujiwara <makoto@ki.nu>
2014-04-02 11:16:39 +0000

    Bump version 24.3.50 to 24.4.50 Adjust PLIST as following
    -share/emacs/${PKGVERSION}/lisp/emacs-lisp/gulp.el{.gz,c}
    +share/emacs/${PKGVERSION}/lisp/obsolete/gulp.el{.gz,c}
    
    -share/emacs/${PKGVERSION}/lisp/gnus/gnus-setup.el{.gz,c}
    -share/emacs/${PKGVERSION}/lisp/progmodes/cap-words.el{.gz,c}
    -share/emacs/${PKGVERSION}/lisp/term/apollo.el{.gz,c}
    -share/emacs/${PKGVERSION}/lisp/term/vt102.el{.gz,c}
    -share/emacs/${PKGVERSION}/lisp/term/vt125.el{.gz,c}
    -share/emacs/${PKGVERSION}/lisp/term/vt201.el{.gz,c}
    -share/emacs/${PKGVERSION}/lisp/term/vt220.el{.gz,c}
    -share/emacs/${PKGVERSION}/lisp/term/vt240.el{.gz,c}
    -share/emacs/${PKGVERSION}/lisp/term/vt300.el{.gz,c}
    -share/emacs/${PKGVERSION}/lisp/term/vt320.el{.gz,c}
    -share/emacs/${PKGVERSION}/lisp/term/vt400.el{.gz,c}
    -share/emacs/${PKGVERSION}/lisp/term/vt420.el{.gz,c}
    
    +info/vhdl-mode.info.gz
    +share/emacs/${PKGVERSION}/lisp/gnus/gnus-cloud.el{.gz,c}
    +share/emacs/${PKGVERSION}/lisp/url/url-tramp{.gz,c}

--- emacs-current/version.mk
+++ emacs-current/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD: version.mk,v 1.7 2013/10/03 07:21:50 makoto Exp $
+# $NetBSD: version.mk,v 1.8 2014/04/02 11:16:40 makoto Exp $
 
 _EMACS_FLAVOR=	emacs
 _EMACS_REQD=	emacs24>=24
@@ -6,5 +6,5 @@ _EMACS_PKGDEP.base=
 _EMACS_PKGDEP.leim=
 
 _EMACS_VERSION_MAJOR=	24
-_EMACS_VERSION_MINOR=	3
+_EMACS_VERSION_MINOR=	4
 _EMACS_VERSION_MICRO=	50
