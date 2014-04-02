d2acddcac23aac59d9e06fdfa3c810f4d6258cef
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-04-02 20:56:20 +0000

    Update to 0.3.4.1
    ChangeLog:
    28 Mar 2014
    	Release 0.3.4.1
    	Fix build with ghc-7.8 broken by changes in HList 0.3.4
    
    20 Feb 2014
    	Release 0.3.4
    	RecordPuns add ( ) syntax
    	Use the tagged package: this removes LVPair and lowercase proxy.
    
    03 Feb 2014
        Release 0.3.2.0
    	Relax hspec dependency
    
    29 Jan 2014
    	Release 0.3.1.0
    	Work with newer GHC.TypeLits (SingI and similar were moved out)
    
    05 Nov 2013
    	Release 0.3.0.1
    	fix `cabal test'
    
    01 Nov 2013
    	Release 0.3
    	pun quasiquote supports nested records
    	remove HStagedEq
    
    30 Oct 2013
    	Another large patch from Adam Vogt
    	Connect HList with Lens. The Data instance for HList
    	treats HList as a list; a Data instance for HListFlat
    	treats HList as a tuple.
    
    24 Oct 2013
    	Another large patch from Adam Vogt
    	Added HFoldl, HScanr, Data instances for HList, finished the
    	update of Record, the Keyword example
    	becomes full-fledged part of HList
    
    17 Sep 2013
    	Another large patch from Adam Vogt
    	Added HReplicate, SameLength constraint (used by HSequence and HMap),
    	FunCxt for interpreting context applications,
    	convenient syntax for record patterns,
    	clean-up of ApplyAB and general clean-up
    
    31 Aug 2013
    	Large patch from Adam Vogt
    	 Improving the Apply class (ApplyAB) to help type inference
    	 and avoid specifying many type sigatures.
    	 Fix a few of broken code files, in particular RecordAdv.
    
    28 Aug 2013
    	Large patch from Adam Vogt
    	 moved files that need attention to broken/
    	 put HSequence in with HList.HList: fixed the function
    	 added Label6 with GHC Symbols
    	 fix HZip, Variant, examples/TIPTransformM.hs
    	 Many Haddoc changes and beautifications
    	 format some examples for doctest
    
    27 Aug 2013
    	Adam Vogt's patch greatly simplifying makeLabels.
    	Updated TIC.hs
    
    15 Nov 2012
    	Ported basic Records.
    
    31 Oct 2012
    	New file HList/HList.hs contains the implementation of
    	heterogeneous lists. HListPrelude.hs defines operations
    	that work across all heterogeneous collections (lists, TIP,
    	Records, etc).
    	General clean-up, removing obsolete code.
    	Code re-written up to Records.
    
    25 Oct 2012
    	hUnfold
    	hProjectByHNats and hProjectAwayByHNats now take type-level
    	list of naturals as the argument. They are implemented in terms
    	of Unfold. More computation is made purely type-level.
    
    24 Oct 2012
    	Nat, Bool and [*] kinds
    	HList is now GADT
    	Rewriting FakePrelude, HListPrelude, HArray using type families
    	to a large extent, and getting rid of functional dependencies
    
    
    12 Oct 2011
    	Getting rid of overlapping instances in HOccurs
    	Using only one TypeEq. This is the only place with overlapping instances
    	Using ~ operator instead of TypeCast. Deprecating TypeCast
    	Rearrangements of code, removing obsolete code
    	Making sure everything works under GHC 7.0.4
    
    
    19 May 2010
    	Beautification, release and tag 0.2.3 by Adam Vogt
    
    26 April 2010
    	Patches from Adam Vogt to format documentation for Haddoc and
    	to expose MakeLabels
    	Release 0.2.1 by Gwern Branwen
    
    29 March 2010
    	examples/TIPTransformM.hs, the monadic version of
    	TIPTransform.hs
    
    17 February 2010
    	Two patches from Adam Vogt to get the code compile on ghc-6.12
    	and to make Record Typeable instances nonstrict
    
    10-14 January 2010
            Reverse parameter order for .@.
    	Changed fixity <++
    	Pushed uses of LANGUAGE pragmas further for OOHaskell examples
    
    16 September 2009
    	Added TypeCastGeneric3, TypeEqGeneric3 -- the instances of
    	typeCast and TypeEq that don't require separate compilation.
    	They are like TypeCastGeneric2, TypeEqGeneric2, but integrated
    	with the FakePrelude.
    	Added the example file TIPTransform.hs, a variation on the
    	keyword argument problem posed by Andrew U. Frank.
    	Adjusted the LANGUAGE pragmas, made the examples run without
    	the global -fglasgow-exts

--- hs-HList/buildlink3.mk
+++ hs-HList/buildlink3.mk
@@ -1,12 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/04/21 03:29:41 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/04/02 20:56:21 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-HList
 
 .if !defined(HS_HLIST_BUILDLINK3_MK)
 HS_HLIST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-HList+=	hs-HList>=0.2
+BUILDLINK_API_DEPENDS.hs-HList+=	hs-HList>=0.3.4
 BUILDLINK_PKGSRCDIR.hs-HList?=	../../wip/hs-HList
+
+.include "../../wip/hs-mtl/buildlink3.mk"
+.include "../../wip/hs-tagged/buildlink3.mk"
 .endif	# HS_HLIST_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-HList
