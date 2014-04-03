8ba018b5f8e60f229af1485d9259c0d2559866ea
Niclas Rosenvik <nros@NetBSD.org>
2014-04-03 16:50:12 +0000

    More libraries used by libtool.

--- qore/buildlink3.mk
+++ qore/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/04/03 15:36:49 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/04/03 16:50:12 nros Exp $
 #
 
 BUILDLINK_TREE+=	qore
@@ -9,8 +9,10 @@ QORE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.qore+=	qore>=0.8.8
 BUILDLINK_PKGSRCDIR.qore?=	../../wip/qore
 
-.include "../../devel/pcre/buildlink3.mk"
+.include "../../archivers/bzip2/buildlink3.mk"
 .include "../../devel/libexecinfo/buildlink3.mk"
+.include "../../devel/pcre/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk
 .include "../../math/mpfr/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
 .endif	# QORE_BUILDLINK3_MK
