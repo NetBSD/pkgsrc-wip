363f08319ea2de9bbe82e7911738f46d278fd1ff
Thomas Klausner <wiz@NetBSD.org>
2014-04-03 09:38:57 +0000

    Remove Mesa, pkgsrc version is newer.

--- gl/version.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: version.mk,v 1.3 2010/08/18 10:27:27 pallegra Exp $
-# used by wip/gl/Makefile.common
-# used by wip/MesaLib/Makefile
-
-MESA_DISTNAME=		MesaLib-${MESA_VERSION}
-MESA_SITES=		ftp://freedesktop.org/pub/mesa/${MESA_VERSION}/
-MESA_VERSION=		7.8.2
-MESA_PKGVERSION=	${MESA_VERSION}
