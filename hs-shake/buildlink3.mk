1746b3ff33232e3718636ea429181e8c935403f0
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-04-16 23:05:31 +0000

    Update to 0.11.4
    CHANGES:
    0.11.4
        Work with QuickCheck-2.7 (which defines ===)
        #100, handle GraphViz missing gracefully
        Fix up the profiling report generation
        #99, add getEnvWithDefault
    0.11.3
        #97, fix a serialisation bug when > 254 arguments to need
    ( #97 => https://github.com/ndmitchell/shake/pull/97 )
    ( #99 => https://github.com/ndmitchell/shake/pull/99 )
    ( #100 => https://github.com/ndmitchell/shake/issues/100 )

--- hs-shake/buildlink3.mk
+++ hs-shake/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/02/19 09:14:10 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/04/16 23:05:31 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-shake
 
 .if !defined(HS_SHAKE_BUILDLINK3_MK)
 HS_SHAKE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-shake+=	hs-shake>=0.11.2
+BUILDLINK_API_DEPENDS.hs-shake+=	hs-shake>=0.11.4
 BUILDLINK_PKGSRCDIR.hs-shake?=	../../wip/hs-shake
 
 .include "../../wip/hs-hashable/buildlink3.mk"
