1a30e3df95500fb84e8aeda1b6435c9f9d4526c4
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-04-24 12:33:49 +0000

    Update to 0.1.0.2
    changes:
    0.1.0.2
     - bump upper bound for `transformers`

--- hs-monads-tf/buildlink3.mk
+++ hs-monads-tf/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/04/18 21:22:38 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/04/24 12:33:49 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-monads-tf
 
 .if !defined(HS_MONADS_TF_BUILDLINK3_MK)
 HS_MONADS_TF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-monads-tf+=	hs-monads-tf>=0.1.0.1
+BUILDLINK_API_DEPENDS.hs-monads-tf+=	hs-monads-tf>=0.1.0
 BUILDLINK_PKGSRCDIR.hs-monads-tf?=	../../wip/hs-monads-tf
 
+.include "../../wip/hs-transformers/buildlink3.mk"
 .endif	# HS_MONADS_TF_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-monads-tf
