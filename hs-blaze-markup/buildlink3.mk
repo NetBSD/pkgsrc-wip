70bdad2085bed94da4441c1b8dc7e1379f417c25
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-04-25 13:27:15 +0000

    Update to 0.6.1.0
    CHANGELOG:
    - 0.6.1.0
        * Add the `null` query to Text.Blaze.Internal.
    - 0.6.0.0
        * Add the operator (!?) for nicely setting conditional attributes

--- hs-blaze-markup/buildlink3.mk
+++ hs-blaze-markup/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/01/14 21:20:34 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/04/25 13:27:15 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-blaze-markup
 
 .if !defined(HS_BLAZE_MARKUP_BUILDLINK3_MK)
 HS_BLAZE_MARKUP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-blaze-markup+=	hs-blaze-markup>=0.5.2
+BUILDLINK_API_DEPENDS.hs-blaze-markup+=	hs-blaze-markup>=0.6.1
 BUILDLINK_PKGSRCDIR.hs-blaze-markup?=	../../wip/hs-blaze-markup
 
 .include "../../wip/hs-blaze-builder/buildlink3.mk"
