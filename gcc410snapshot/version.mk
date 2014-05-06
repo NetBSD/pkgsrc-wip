4127d9e55e5c225386a4cb13786ff124b2441de3
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2014-05-06 15:44:41 +0000

    Snapshot 20140504.

--- gcc410snapshot/version.mk
+++ gcc410snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.1 2014/04/29 14:14:34 keckhardt Exp $
-GCC410S_DIST_VERSION:=4.10-20140427
+# $NetBSD: version.mk,v 1.2 2014/05/06 15:44:41 keckhardt Exp $
+GCC410S_DIST_VERSION:=4.10-20140504
