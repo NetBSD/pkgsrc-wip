bf42dfc6d112f7bde0bd429270075dec5dbd1186
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-05-08 00:36:38 +0000

    Update to 0.2.4.0 pkgsrc changes: static -> dynamic changes from https://github.com/tibbe/unordered-containers: 0.2.4.0  - Avoid code duplication in insert    The call to Array.insert was duplicated in both branches of    bitmapIndexedOrFull, due to being lazily evaluated.    Fixes #76.  - Add missing docs  - Merge branch 'master' of https://github.com/hrdinka/unordered-containers    Conflicts: 	Data/HashMap/Strict.hs

--- hs-unordered-containers/buildlink3.mk
+++ hs-unordered-containers/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/09/27 13:50:59 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/05/08 00:36:38 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-unordered-containers
 
 .if !defined(HS_UNORDERED_CONTAINERS_BUILDLINK3_MK)
 HS_UNORDERED_CONTAINERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.3
+BUILDLINK_API_DEPENDS.hs-unordered-containers+=	hs-unordered-containers>=0.2.4
 BUILDLINK_PKGSRCDIR.hs-unordered-containers?=	../../wip/hs-unordered-containers
 
 .include "../../wip/hs-hashable/buildlink3.mk"
