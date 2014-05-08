ff5879d407d992876b6f6b4a43dc2ce544b66c4d
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-05-08 00:30:49 +0000

    Update to 4.2 pkgsrc changes: static -> dynamic CHANGELOG: 4.2 --- * `transformers 0.4` compatibility
    4.1
    ---
    * Fixed the 'Typeable' instance for 'Cokleisli on GHC 7.8.1
    
    4.0.1
    -----
    * Fixes to avoid warnings on GHC 7.8.1

--- hs-comonad/buildlink3.mk
+++ hs-comonad/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/10/14 10:37:34 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/05/08 00:30:49 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-comonad
 
 .if !defined(HS_COMONAD_BUILDLINK3_MK)
 HS_COMONAD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-comonad+=	hs-comonad>=4.0
+BUILDLINK_API_DEPENDS.hs-comonad+=	hs-comonad>=4.2
 BUILDLINK_PKGSRCDIR.hs-comonad?=	../../wip/hs-comonad
 
 .include "../../wip/hs-contravariant/buildlink3.mk"
