03cc0b1c56cc4afbe32c3cc2441636ac4f621503
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-05-08 00:38:31 +0000

    Update to 1.1.1.1 pkgsrc changes: static -> dynamic
    changelog:
    1.1.1.1
    - changelog -> changelog.md
    1.1.1.0
    * The Data.Data instance now allows gunfold to work, via a virtual
      pack constructor
    
    * dropEnd, takeEnd: new functions
    
    * Comparing the length of a Text against a number can now
      short-circuit in more cases
    
    1.1.0.1
    * streamDecodeUtf8: fixed gh-70, did not return all unconsumed bytes
      in single-byte chunks
    
    1.1.0.0
    * encodeUtf8: Performance is improved by up to 4x.
    
    * encodeUtf8Builder, encodeUtf8BuilderEscaped: new functions,
      available only if bytestring >= 0.10.4.0 is installed, that allow
      very fast and flexible encoding of a Text value to a bytestring
      Builder.
    
      As an example of the performance gain to be had, the
      encodeUtf8BuilderEscaped function helps to double the speed of JSON
      encoding in the latest version of aeson! (Note: if all you need is a
      plain ByteString, encodeUtf8 is still the faster way to go.)
    
    * All of the internal module hierarchy is now publicly exposed.  If a
      module is in the .Internal hierarchy, or is documented as internal,
      use at your own risk - there are no API stability guarantees for
      internal modules!
    
    1.0.0.1
    * decodeUtf8: Fixed a regression that caused us to incorrectly
      identify truncated UTF-8 as valid (gh-61)

--- hs-text/buildlink3.mk
+++ hs-text/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2013/12/05 03:05:29 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2014/05/08 00:38:31 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-text
 
 .if !defined(HS_TEXT_BUILDLINK3_MK)
 HS_TEXT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=1.0.0
+BUILDLINK_API_DEPENDS.hs-text+=	hs-text>=1.1.1
 BUILDLINK_PKGSRCDIR.hs-text?=	../../wip/hs-text
-
 .endif	# HS_TEXT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-text
