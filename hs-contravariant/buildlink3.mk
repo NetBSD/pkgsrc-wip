43dc2b43ccaf5382cda082004546973009442514
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-05-08 00:32:53 +0000

    Update to 0.5.2 pkgsrc changes: static -> dynamic CHANGELOG: 0.5.2 * Updated transformers-compat version bound to ensure instances are
    0.5.1
    -----
    * `transformers` 0.4 compatibility
    
    0.5
    ---
    * Added `(>$)`
    * Added instances for `GHC.Generics`

--- hs-contravariant/buildlink3.mk
+++ hs-contravariant/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/02/21 10:39:01 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/05/08 00:32:53 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-contravariant
 
 .if !defined(HS_CONTRAVARIANT_BUILDLINK3_MK)
 HS_CONTRAVARIANT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-contravariant+=	hs-contravariant>=0.4.4
+BUILDLINK_API_DEPENDS.hs-contravariant+=	hs-contravariant>=0.5.2
 BUILDLINK_PKGSRCDIR.hs-contravariant?=	../../wip/hs-contravariant
 
 .include "../../wip/hs-tagged/buildlink3.mk"
