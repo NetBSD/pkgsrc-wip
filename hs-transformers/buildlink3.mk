f50274c737b26db980ff0b43a7af5cec4d8fef6b
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-05-08 00:28:53 +0000

    Update to 0.4.1.0 pkgsrc changes: static -> dynamic changelog: 0.4.1.0 Ross Paterson <ross@soi.city.ac.uk> May 2014     * Reverted to record syntax for newtypes until next major release
    0.4.0.0 Ross Paterson <ross@soi.city.ac.uk> May 2014
        * Added Sum type
        * Added modify', a strict version of modify, to the state monads
        * Added ExceptT and deprecated ErrorT
        * Added infixr 9 `Compose` to match (.)
        * Added Eq, Ord, Read and Show instances where possible
        * Replaced record syntax for newtypes with separate inverse functions
        * Added delimited continuation functions to ContT
        * Added instance Alternative IO to ErrorT
        * Handled disappearance of Control.Monad.Instances

--- hs-transformers/buildlink3.mk
+++ hs-transformers/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/01/15 11:49:55 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/05/08 00:28:53 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-transformers
 
 .if !defined(HS_TRANSFORMERS_BUILDLINK3_MK)
 HS_TRANSFORMERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-transformers+=	hs-transformers>=0.3
+BUILDLINK_API_DEPENDS.hs-transformers+=	hs-transformers>=0.4.1
 BUILDLINK_PKGSRCDIR.hs-transformers?=	../../wip/hs-transformers
 .endif	# HS_TRANSFORMERS_BUILDLINK3_MK
 
