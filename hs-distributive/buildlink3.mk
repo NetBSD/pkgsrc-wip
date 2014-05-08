5803aea5a51919a96969eaeaba0bba62696eaf66
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-05-08 00:34:42 +0000

    Update to 0.4.4 pkgsrc changes: static -> dynamic CHANGELOG: 0.4.4 ----- * `transformers 0.4` compatibility
    0.4.3.1
    -----
    * Fixed builds with older versions of GHC
    
    0.4.2
    -------
    * Added `Data.Distributive.Generic`.
    
    0.4.1
    -----
    * `Control.Monad.Instances` is deprecated in GHC 7.8. Don't import it there.
    
    0.4
    ---
    * Added support for `Data.Tagged` and `Data.Proxy`.

--- hs-distributive/buildlink3.mk
+++ hs-distributive/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/10/14 10:32:25 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/05/08 00:34:42 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-distributive
 
 .if !defined(HS_DISTRIBUTIVE_BUILDLINK3_MK)
 HS_DISTRIBUTIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-distributive+=	hs-distributive>=0.3.2
+BUILDLINK_API_DEPENDS.hs-distributive+=	hs-distributive>=0.4.4
 BUILDLINK_PKGSRCDIR.hs-distributive?=	../../wip/hs-distributive
 
+.include "../../wip/hs-tagged/buildlink3.mk"
 .include "../../wip/hs-transformers/buildlink3.mk"
 .include "../../wip/hs-transformers-compat/buildlink3.mk"
 .endif	# HS_DISTRIBUTIVE_BUILDLINK3_MK
