ff2ae1cf8921a9515130f8277a4b19b4334bc904
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-05-08 00:29:55 +0000

    Update to 2.2.0.1 pkgsrc changes: static -> dynamic CHANGELOG: 2.2.0.1 ------- * Fixed a bug caused by the change in how `transformers` 0.4 exports its data types.   We will now export `runFooT` for each transformer again!
    2.2
    ---
    * `transformers` 0.4 support
    * Added instances for `ExceptT`
    * Added `modify'` to `Control.Monad.State.*`
    
    2.1.3.1
    -------
    * Avoid importing `Control.Monad.Instances` on GHC 7.8 to build without deprecation warnings.
    
    2.1.3
    -----
    * Removed the now-irrelevant `Error` constraint from the `MonadError` instance for `Either e`.

--- hs-mtl/buildlink3.mk
+++ hs-mtl/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.7 2013/01/15 11:51:23 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2014/05/08 00:29:55 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-mtl
 
 .if !defined(HS_MTL_BUILDLINK3_MK)
 HS_MTL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mtl+=	hs-mtl>=2.1.2
+BUILDLINK_API_DEPENDS.hs-mtl+=	hs-mtl>=2.2.0
 BUILDLINK_PKGSRCDIR.hs-mtl?=	../../wip/hs-mtl
 
 .include "../../wip/hs-transformers/buildlink3.mk"
