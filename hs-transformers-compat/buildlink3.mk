b0ecd61aef72cc13007284edda0da05eada257af
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-05-08 00:31:52 +0000

    Update to 0.3 pkgsrc changes: static -> dynamic CHANGELOG: 0.3 --- * Added the instances for `Data.Functor.Classes` from `transformers 0.4` * Switched `Control.Applicative.Backwards` and `Data.Functor.Reverse` to   the split constructor/accessor style from `transformers 0.4`.
    0.2
    ---
    * Added the new types and classes from `transformers 0.4`

--- hs-transformers-compat/buildlink3.mk
+++ hs-transformers-compat/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/02/21 10:39:02 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/05/08 00:31:52 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-transformers-compat
 
 .if !defined(HS_TRANSFORMERS_COMPAT_BUILDLINK3_MK)
 HS_TRANSFORMERS_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.1.1.1
+BUILDLINK_API_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.3
 BUILDLINK_PKGSRCDIR.hs-transformers-compat?=	../../wip/hs-transformers-compat
 
 .include "../../wip/hs-transformers/buildlink3.mk"
