28fe3566f39b38ce1c7a987941943e0d306ecbd6
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-05-08 00:39:25 +0000

    Update to 0.1.3 pkgsrc changes: static -> dynamic CHANGELOG: 0.1.3 ----- * Added support for GHC 7.8's `bitSizeMaybe`

--- hs-nats/buildlink3.mk
+++ hs-nats/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2013/09/17 20:09:32 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/05/08 00:39:25 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-nats
 
 .if !defined(HS_NATS_BUILDLINK3_MK)
 HS_NATS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-nats+=	hs-nats>=0.1.2
+BUILDLINK_API_DEPENDS.hs-nats+=	hs-nats>=0.1.3
 BUILDLINK_PKGSRCDIR.hs-nats?=	../../wip/hs-nats
 .endif	# HS_NATS_BUILDLINK3_MK
 
