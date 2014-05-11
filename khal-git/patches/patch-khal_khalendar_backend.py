32ca07229971e26009a9ca0c9e797876d61780d9
Thomas Klausner <wiz@NetBSD.org>
2014-05-11 09:00:00 +0000

    Update for git changes. Add a patch for python-3 support.

---
+++ khal-git/patches/patch-khal_khalendar_backend.py
@@ -0,0 +1,13 @@
+$NetBSD: patch-khal_khalendar_backend.py,v 1.1 2014/05/11 09:00:00 thomasklausner Exp $
+
+--- khal/khalendar/backend.py.orig	2014-05-11 08:49:28.000000000 +0000
++++ khal/khalendar/backend.py
+@@ -126,7 +126,7 @@ class SQLiteDb(object):
+         if not path.isdir(dbdir):
+             try:
+                 logging.debug('trying to create the directory for the db')
+-                makedirs(dbdir, mode=0770)
++                makedirs(dbdir, mode=0o770)
+                 logging.debug('success')
+             except OSError as error:
+                 logging.fatal('failed to create {0}: {1}'.format(dbdir, error))
