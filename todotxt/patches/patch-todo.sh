d2c5a262a4bcd7c66b3856a8a85a01b126341fd1
tty` <ttys@users.sourceforge.net>
2014-05-13 14:01:03 +0000

    bump up todo.sh

--- todotxt/patches/patch-todo.sh
+++ todotxt/patches/patch-todo.sh
@@ -1,8 +1,8 @@
-$NetBSD: patch-todo.sh,v 1.1 2014/05/13 11:43:34 ttys Exp $
+$NetBSD: patch-todo.sh,v 1.2 2014/05/13 14:01:03 ttys Exp $
 
 Change the path for bash.
 
---- todo.sh.orig	2012-03-26 18:56:04.000000000 +0000
+--- todo.sh.orig	2014-05-13 12:31:26.000000000 +0000
 +++ todo.sh
 @@ -1,4 +1,4 @@
 -#! /bin/bash
