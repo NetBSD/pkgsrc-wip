9c24d618fbf69b0dfa24d6497f325d3295e61525
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2014-05-14 21:57:06 +0000

    added new dependency: cython, numba 	modified patch: added new scripts (dolo, dolo-julia) 	remove py-sympy 	tags (py-matplotlib) extra-dependency

--- py-dolo/patches/patch-setup.py
+++ py-dolo/patches/patch-setup.py
@@ -1,19 +1,19 @@
-$NetBSD: patch-setup.py,v 1.1 2012/09/10 22:11:35 jihbed Exp $
+$NetBSD: patch-setup.py,v 1.2 2014/05/14 21:57:06 jihbed Exp $
 
---- setup.py.orig	2012-09-10 22:28:50.000000000 +0100
-+++ setup.py	2012-09-10 22:32:45.000000000 +0100
-@@ -1,4 +1,5 @@
- from setuptools import setup, find_packages
+--- setup.py.orig	2014-05-14 22:12:44.000000000 +0100
++++ setup.py	2014-05-14 22:17:16.000000000 +0100
+@@ -1,3 +1,5 @@
 +import sys
++
+ from setuptools import setup, find_packages
+ from distutils.extension import Extension
  
- __version__ = "0.4-dev"
+@@ -19,7 +21,7 @@
  
-@@ -7,7 +8,7 @@
-     version = __version__,
-     packages = find_packages('src'),
-     package_dir = {'':'src'},
--    scripts = ['src/bin/dolo-recs.py', 'src/bin/dolo-matlab.py'],
-+    scripts = ['src/bin/dolo-recs.py'+sys.version[0:3], 'src/bin/dolo-matlab.py'+sys.version[0:3]],
-     install_requires = ["pyyaml","sympy","numpy","matplotlib"],
-     author = "Pablo Winant",
-     author_email = "pablo.winant@gmail.com",
+     include_dirs = [np.get_include()],
+ 
+-    scripts = ['bin/dolo-recs', 'bin/dolo-matlab', 'bin/dolo-julia', 'bin/dolo'],
++    scripts = ['bin/dolo-recs'+sys.version[0:3], 'bin/dolo-matlab'+sys.version[0:3], 'bin/dolo-julia'+sys.version[0:3], 'bin/dolo'+sys.version[0:3]],
+ 
+     install_requires = ["pyyaml","numba","numpy","cython"],
+ 
