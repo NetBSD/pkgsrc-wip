92dd0eaf9321c1d44fae0c8b0fbdfc115fc76fa8
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-05-17 20:27:18 +0000

    Update to 1.6.1.2 pkgsrc changes: - static -> dynamic - more appropriate CATEGORIES, and   next will be changed from hs-x11 to hs-X11, adviced by obache@ Changes from https://github.com/haskell-pkg-janitors/X11 1.6.1.2  - issue 20: depend on less stuff

--- hs-x11/buildlink3.mk
+++ hs-x11/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2013/02/26 12:51:12 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2014/05/17 20:27:18 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-x11
 
 .if !defined(HS_X11_BUILDLINK3_MK)
 HS_X11_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-x11+=	hs-x11>=1.6.1.1
+BUILDLINK_API_DEPENDS.hs-x11+=	hs-x11>=1.6.1
 BUILDLINK_PKGSRCDIR.hs-x11?=	../../wip/hs-x11
 
 .include "../../wip/hs-data-default/buildlink3.mk"
