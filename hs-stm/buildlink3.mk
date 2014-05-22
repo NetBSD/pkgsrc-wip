3f505178fe999821ba7f5423629598400eeb8af9
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-05-22 21:33:55 +0000

    Update to 2.4.3 pkgsrc changes: static -> dynamic changelog: ## 2.4.3  *Mar 2014*
      * Update behaviour of `newBroadcastTChanIO` to match
        `newBroadcastTChan` in causing an error on a read from the
        broadcast channel
    
      * Add `mkWeakTVar`
    
      * Add `isFullTBQueue`
    
      * Fix `TChan` created via `newBroadcastTChanIO` to throw same
        exception on a `readTChan` as when created via `newBroadcastTChan`
    
      * Update to Cabal 1.10 format

--- hs-stm/buildlink3.mk
+++ hs-stm/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/03/09 22:32:47 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/05/22 21:33:55 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-stm
 
 .if !defined(HS_STM_BUILDLINK3_MK)
 HS_STM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-stm+=	hs-stm>=2.4.2
+BUILDLINK_API_DEPENDS.hs-stm+=	hs-stm>=2.4.3
 BUILDLINK_PKGSRCDIR.hs-stm?=	../../wip/hs-stm
 .endif	# HS_STM_BUILDLINK3_MK
 
