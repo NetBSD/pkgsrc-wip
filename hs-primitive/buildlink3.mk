2bfc22157589ca5e43c889d1e3a6b956a2ad0229
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-05-22 21:31:26 +0000

    Update to 0.5.3.0 pkgsrc changes: static -> dynamic changelog: ## Changes in version 0.5.3.0
     * Implement `cloneArray` and `cloneMutableArray` primitives
       (with fall-back implementations for GHCs prior to version 7.2.1)
    
    ## Changes in version 0.5.2.1
    
     * Add strict variants of `MutVar` modification functions
       `atomicModifyMutVar'` and `modifyMutVar'`
    
     * Fix compilation on Solaris 10 with GNU C 3.4.3

--- hs-primitive/buildlink3.mk
+++ hs-primitive/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2013/09/25 13:09:44 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2014/05/22 21:31:26 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-primitive
 
 .if !defined(HS_PRIMITIVE_BUILDLINK3_MK)
 HS_PRIMITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.5.1
+BUILDLINK_API_DEPENDS.hs-primitive+=	hs-primitive>=0.5.3
 BUILDLINK_PKGSRCDIR.hs-primitive?=	../../wip/hs-primitive
 .endif	# HS_PRIMITIVE_BUILDLINK3_MK
 
