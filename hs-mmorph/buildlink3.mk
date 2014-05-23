4a9ce013c64701cec91149ee1e19a26f04a2c161
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-05-23 21:46:05 +0000

    Update to 1.0.3 pkgsrc changes: static -> dynamic Changes from https://github.com/Gabriel439/Haskell-MMorph-Library 1.0.3  - Merge pull request #12 from snoyberg/master    transformers 0.4

--- hs-mmorph/buildlink3.mk
+++ hs-mmorph/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/05/15 21:49:09 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/05/23 21:46:05 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-mmorph
 
 .if !defined(HS_MMORPH_BUILDLINK3_MK)
 HS_MMORPH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.2
+BUILDLINK_API_DEPENDS.hs-mmorph+=	hs-mmorph>=1.0.3
 BUILDLINK_PKGSRCDIR.hs-mmorph?=	../../wip/hs-mmorph
 
 .include "../../devel/hs-transformers/buildlink3.mk"
