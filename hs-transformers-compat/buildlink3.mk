df04e0054ac46360577db29313630a1ac831498d
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-05-24 21:59:45 +0000

    Update to 0.3.3 pkgsrc changes: fix mis-targeted bomb CHANGELOG: 0.3.3 - cosmetic change 0.3.1 ----- * `transformers 0.4.1` compatibility

--- hs-transformers-compat/buildlink3.mk
+++ hs-transformers-compat/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/05/15 21:49:11 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/05/24 21:59:45 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-transformers-compat
 
 .if !defined(HS_TRANSFORMERS_COMPAT_BUILDLINK3_MK)
 HS_TRANSFORMERS_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.3
-BUILDLINK_PKGSRCDIR.hs-transformers-compat?=	../../devel/hs-transformers-compat
+BUILDLINK_API_DEPENDS.hs-transformers-compat+=	hs-transformers-compat>=0.3.3
+BUILDLINK_PKGSRCDIR.hs-transformers-compat?=	../../wip/hs-transformers-compat
 
 .include "../../devel/hs-transformers/buildlink3.mk"
 .endif	# HS_TRANSFORMERS_COMPAT_BUILDLINK3_MK
