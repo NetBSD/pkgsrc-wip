567a41ee319d2e07432e5789e80c4e38ba96c985
Thomas Klausner <wiz@NetBSD.org>
2014-05-25 15:57:26 +0000

    Adapt to upstream changes.

--- khal-git/patches/patch-khal_khalendar_backend.py
+++ khal-git/patches/patch-khal_khalendar_backend.py
@@ -1,15 +1,15 @@
-$NetBSD: patch-khal_khalendar_backend.py,v 1.2 2014/05/20 14:06:47 thomasklausner Exp $
+$NetBSD: patch-khal_khalendar_backend.py,v 1.3 2014/05/25 15:57:26 thomasklausner Exp $
 
 Python-3.x compatibility.
 
---- khal/khalendar/backend.py.orig	2014-05-11 08:49:28.000000000 +0000
+--- khal/khalendar/backend.py.orig	2014-05-25 12:16:08.000000000 +0000
 +++ khal/khalendar/backend.py
-@@ -126,7 +126,7 @@ class SQLiteDb(object):
+@@ -128,7 +128,7 @@ class SQLiteDb(object):
          if not path.isdir(dbdir):
              try:
-                 logging.debug('trying to create the directory for the db')
+                 logger.debug('trying to create the directory for the db')
 -                makedirs(dbdir, mode=0770)
 +                makedirs(dbdir, mode=0o770)
-                 logging.debug('success')
+                 logger.debug('success')
              except OSError as error:
-                 logging.fatal('failed to create {0}: {1}'.format(dbdir, error))
+                 logger.fatal('failed to create {0}: {1}'.format(dbdir, error))
