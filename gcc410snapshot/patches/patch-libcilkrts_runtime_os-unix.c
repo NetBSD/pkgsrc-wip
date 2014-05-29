ad66fb17794a67165b214796c81ce34e22275bf2
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2014-05-29 07:11:47 +0000

    Snashot 20140525. Obsolete patches for Dragonfly removed. Congratulations to John Marino for adding DragonflyBSD support upstream.

--- gcc410snapshot/patches/patch-libcilkrts_runtime_os-unix.c
+++ gcc410snapshot/patches/patch-libcilkrts_runtime_os-unix.c
@@ -1,26 +1,28 @@
-$NetBSD: patch-libcilkrts_runtime_os-unix.c,v 1.1 2014/04/29 14:14:34 keckhardt Exp $
+$NetBSD: patch-libcilkrts_runtime_os-unix.c,v 1.2 2014/05/29 07:11:47 keckhardt Exp $
 
---- libcilkrts/runtime/os-unix.c.orig	2013-10-29 18:37:47.000000000 +0000
+--- libcilkrts/runtime/os-unix.c.orig	2014-05-21 11:08:58.000000000 +0000
 +++ libcilkrts/runtime/os-unix.c
-@@ -54,7 +54,7 @@
- #elif defined __APPLE__
- #   include <sys/sysctl.h>
+@@ -56,7 +56,9 @@
      // Uses sysconf(_SC_NPROCESSORS_ONLN) in verbose output
+ #elif defined  __DragonFly__
+ // No additional include files
 -#elif defined  __FreeBSD__
-+#elif defined  __FreeBSD__ || defined __NetBSD__
++#elif defined  __FreeBSD__ 
++// No additional include files
++#elif defined  __NetBSD__ 
  // No additional include files
  #elif defined __CYGWIN__
  // Cygwin on Windows - no additional include files
-@@ -366,7 +366,7 @@ COMMON_SYSDEP int __cilkrts_hardware_cpu
+@@ -376,7 +378,7 @@ COMMON_SYSDEP int __cilkrts_hardware_cpu
      assert((unsigned)count == count);
  
      return count;
--#elif defined  __FreeBSD__ || defined __CYGWIN__
-+#elif defined  __FreeBSD__ || defined __NetBSD__ || defined __CYGWIN__
+-#elif defined  __FreeBSD__ || defined __CYGWIN__ || defined __DragonFly__
++#elif defined  __FreeBSD__ || defined __CYGWIN__ || defined __DragonFly__ || defined __NetBSD__
      int ncores = sysconf(_SC_NPROCESSORS_ONLN);
  
      return ncores;
-@@ -390,7 +390,7 @@ COMMON_SYSDEP void __cilkrts_sleep(void)
+@@ -400,7 +402,7 @@ COMMON_SYSDEP void __cilkrts_sleep(void)
  
  COMMON_SYSDEP void __cilkrts_yield(void)
  {
--- gcc410snapshot/version.mk
+++ gcc410snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.4 2014/05/21 14:27:21 keckhardt Exp $
-GCC410S_DIST_VERSION:=4.10-20140518
+# $NetBSD: version.mk,v 1.5 2014/05/29 07:11:47 keckhardt Exp $
+GCC410S_DIST_VERSION:=4.10-20140525
