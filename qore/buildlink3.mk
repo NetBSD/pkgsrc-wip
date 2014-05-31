56d716c0380fa74eeb23ec05dfffabdd0de352d4
Niclas Rosenvik <nros@NetBSD.org>
2014-05-31 19:12:07 +0000

    Added libiconv buildlink3 dependency needed by platforms that do not have iconv in libc such ad freebsd 9.

--- qore/buildlink3.mk
+++ qore/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/04/03 16:54:07 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/05/31 19:12:07 nros Exp $
 #
 
 BUILDLINK_TREE+=	qore
@@ -10,6 +10,7 @@ BUILDLINK_API_DEPENDS.qore+=	qore>=0.8.8
 BUILDLINK_PKGSRCDIR.qore?=	../../wip/qore
 
 .include "../../archivers/bzip2/buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/libexecinfo/buildlink3.mk"
 .include "../../devel/pcre/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
