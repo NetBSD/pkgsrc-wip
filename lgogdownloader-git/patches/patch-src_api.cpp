f1c10877bde99859968bbeff0c3a47af72c4a0e8
Thomas Klausner <wiz@NetBSD.org>
2014-06-11 19:03:53 +0000

    Remove patches that were integrated upstream.

--- lgogdownloader-git/patches/patch-src_api.cpp
+++ lgogdownloader-git/patches/patch-src_api.cpp
@@ -1,24 +1,15 @@
-$NetBSD: patch-src_api.cpp,v 1.2 2014/05/25 12:58:41 thomasklausner Exp $
+$NetBSD: patch-src_api.cpp,v 1.3 2014/06/11 19:03:53 thomasklausner Exp $
 
 Fix path to jsoncpp headers.
 
---- src/api.cpp.orig	2014-04-24 10:00:00.000000000 +0000
+--- src/api.cpp.orig	2014-06-11 17:49:36.000000000 +0000
 +++ src/api.cpp
-@@ -9,7 +9,8 @@
+@@ -9,7 +9,7 @@
  #include <cstdio>
  #include <cstdlib>
  #include <sstream>
 -#include <jsoncpp/json/json.h>
 +#include <json/json.h>
-+#include <unistd.h>
  
  size_t writeMemoryCallback(char *ptr, size_t size, size_t nmemb, void *userp) {
      std::ostringstream *stream = (std::ostringstream*)userp;
-@@ -232,6 +233,7 @@ std::string API::getResponse(const std::
-         std::cerr << "DEBUG INFO (API::getResponse)" << std::endl << "URL: " << url << std::endl;
-     #endif
-     std::ostringstream memory;
-+    usleep(50000); // wait 50ms
- 
-     curl_easy_setopt(curlhandle, CURLOPT_URL, url.c_str());
-     curl_easy_setopt(curlhandle, CURLOPT_NOPROGRESS, 1);
--- lgogdownloader-git/patches/patch-src_downloader.cpp
+++ lgogdownloader-git/patches/patch-src_downloader.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-src_downloader.cpp,v 1.2 2014/05/25 12:58:41 thomasklausner Exp $
+$NetBSD: patch-src_downloader.cpp,v 1.3 2014/06/11 19:03:53 thomasklausner Exp $
 
 Fix path to jsoncpp headers.
 
---- src/downloader.cpp.orig	2014-04-24 10:00:00.000000000 +0000
+--- src/downloader.cpp.orig	2014-06-11 17:49:36.000000000 +0000
 +++ src/downloader.cpp
 @@ -20,7 +20,7 @@
  #include <boost/regex.hpp>
@@ -13,27 +13,3 @@ Fix path to jsoncpp headers.
  #include <htmlcxx/html/ParserDom.h>
  #include <htmlcxx/html/Uri.h>
  
-@@ -1389,12 +1389,22 @@ CURLcode Downloader::beginDownload()
- std::string Downloader::getResponse(const std::string& url)
- {
-     std::ostringstream memory;
-+    usleep(50000); // wait 50ms
- 
-     curl_easy_setopt(curlhandle, CURLOPT_URL, url.c_str());
-     curl_easy_setopt(curlhandle, CURLOPT_NOPROGRESS, 1);
-     curl_easy_setopt(curlhandle, CURLOPT_WRITEFUNCTION, Downloader::writeMemoryCallback);
-     curl_easy_setopt(curlhandle, CURLOPT_WRITEDATA, &memory);
--    CURLcode result = curl_easy_perform(curlhandle);
-+
-+    CURLcode result;
-+    this->retries = 0;
-+    do
-+    {
-+        result = curl_easy_perform(curlhandle);
-+    }
-+    while ((result != CURLE_OK) && (this->retries++ < config.iRetries));
-+    this->retries = 0; // reset retries counter
-+
-     curl_easy_setopt(curlhandle, CURLOPT_WRITEFUNCTION, Downloader::writeData);
-     curl_easy_setopt(curlhandle, CURLOPT_NOPROGRESS, 0);
-     std::string response = memory.str();
