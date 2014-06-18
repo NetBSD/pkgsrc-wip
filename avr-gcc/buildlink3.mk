e32438271fd79d101fadc637ba41b33463a5d27a
Makoto Fujiwara <makoto@ki.nu>
2014-06-18 05:08:59 +0000

    Update avr-gcc 4.7.3 to 4.8.3 - libiberty.a will be installed as     lib/gcc/avr/${PKGVERSION}/libiberty.a   (previous 4.5.3, and 4.7.3 did not have the one).   and it is moved from lib/libiberty.a to above to avoid confilct to   other cross and native tools.
    From grepping 'avr' from NEWS file:
    ---------------------------------
       On AVR, the configure option --with-avrlibc supported since GCC 4.7.2
       is turned on per default for all non-RTEMS configurations. This option
       arranges for a better integration of [3]AVR Libc with avr-gcc. For
       technical details, see [4]PR54461. To turn off the option in non-RTEMS
       configurations, use --with-avrlibc=no. If the compiler is configured
       for RTEMS, the option is always turned off.
    
         * Support has been added for the AVR-specific configure option
           --with-avrlibc=yes in order to arrange for better integration of
           [3]AVR-Libc. This configure option is supported in avr-gcc 4.7.2
           and newer and will only take effect in non-RTEMS configurations. If
           avr-gcc is configured for RTEMS, the option will be ignored which
           is the same as specifying --with-avrlibc=no. See [4]PR54461 for
           more technical details.
    
       3. http://www.nongnu.org/avr-libc/
       4. http://gcc.gnu.org/PR54461
    
    #include <avr/io.h> /* Port Definitions from AVR-LibC */
    
         * [297]14064 [avr-elf] -fdata-sections triggers ICE
         * [302]16884 [avr-elf] [fweb related] bug while initializing

--- avr-gcc/buildlink3.mk
+++ avr-gcc/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2012/02/07 03:22:47 makoto Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/06/18 05:08:59 makoto Exp $
 
 BUILDLINK_TREE+=	avr-gcc
 
 .if !defined(AVR_GCC_BUILDLINK3_MK)
 AVR_GCC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.avr-gcc+=		avr-gcc>=4.5.3
+BUILDLINK_API_DEPENDS.avr-gcc+=		avr-gcc>=4.8.3
 BUILDLINK_PKGSRCDIR.avr-gcc?=		../../wip/avr-gcc
-BUILDLINK_FNAME_TRANSFORM.avr-gcc+=	-e "s,${AVR_TARGET}/lib,lib/,g"
+BUILDLINK_FNAME_TRANSFORM.avr-gcc+=	-e "s,${AVR_TARGET}/lib/,lib/,g"
 
-.include "../../wip/avr-binutils/buildlink3.mk"
 .endif # AVR_GCC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-avr-gcc
