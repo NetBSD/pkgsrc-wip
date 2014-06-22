0cea026152c1b623d8c9e5b48cc7bf6add98edbc
Niclas Rosenvik <nros@NetBSD.org>
2014-06-22 10:07:43 +0000

    Update qore to version 0.8.11. Removed devel/libexecinfo buildlink because I belive this might cause problems when building under smartos.
    Release notes:
    
    Release Summary
     Minor bugfix release for UNIX, major bugfixes for Windows
    
    New Features in Qore
    
     added the Qore::AFMap and Qore::AFStrMap constants
    
     WebUtil updates:
        added logic to the default file serving code to determine if the
        file is a binary or text file from the MIME type
    
     HTTPClient::sendWithSendCallback() and
     HTTPClient::sendWithSendCallback() updated such that if a response is
     received while the chunked send operation is still in progress, an
     error is assumed, the send operation is aborted, and the response header is
     read immediately
    
    Bug Fixes in Qore
    
     Windows fixes:
    
        fixed TimeZone copying to use the standard name instead of the
        display name so that the info can be found in the registry
    
        Util module fixes:
          fixed get_random_string() on Windows
          fixed absolute_path_windows()
    
        HttpServer module fixes:
          when binding a wildcard address with AF_UNSPEC on Windows with
          HttpServer::addListeners() and both IPv6 and IPv4 addresses are
          returned, bind both addresses since Windows doesn't direct the
          IPv4 requests to the wildcard-bound IPv6 listener
    
        fixed file reading by always opening in binary mode
    
        added support for the WSAECONNABORTED socket error
    
        replaced Mime::MultiPartMessage::getRandomString() with
    
        Util::get_random_string() to make it work on Windows
    
     fixed a bug in the DBI layer where calling SQLStatement::describe()
     would crash when called with an older module that did not implement
     this method
    
     other fixes in the Util module (in addition to the Windows-specific
     fixes above):
    
        fixed parse_to_qore_value() with hashes with a comma in the first
        key name
    
        read from /dev/urandom instead of /dev/random since reads from the
        latter can block for long periods to fill the entropy pool
    
     do not start signal thread after a fork() if signal handling is
     enabled, pthread_create() is not async-signal safe (on FreeBSD at least
     this reliably causes segfaults)

--- qore/buildlink3.mk
+++ qore/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2014/05/31 19:12:07 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2014/06/22 10:07:43 nros Exp $
 #
 
 BUILDLINK_TREE+=	qore
@@ -6,12 +6,11 @@ BUILDLINK_TREE+=	qore
 .if !defined(QORE_BUILDLINK3_MK)
 QORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qore+=	qore>=0.8.8
+BUILDLINK_API_DEPENDS.qore+=	qore>=0.8.11 # API version 0.18
 BUILDLINK_PKGSRCDIR.qore?=	../../wip/qore
 
 .include "../../archivers/bzip2/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
-.include "../../devel/libexecinfo/buildlink3.mk"
 .include "../../devel/pcre/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../math/mpfr/buildlink3.mk"
