7acf1f5deceeb24ba673ec92464fabc19321a7b4
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-06-26 22:00:39 +0000

    Update to 1.15.0.1 pkgsrc changes: static -> dynamic CHANGELOG: 1.15.0 --> 1.15.0.1 ===============
    * Eliminate inexhaustive patterns (and consequent bugs)
    * The test suite for 1.15.0 wouldn't build due to a missing file in the source
      distribution. Fixed.
    
    1.14.0.1 --> 1.15.0
    ===============
    
    * Add support for extensions:
      - MultiWayIf
      - LambdaCase
      - DataKinds
    * Remove support for old (deprecated) Generics
    * Derive GHC's Generic instances for datatypes
    * Derive some missing Data and Typeable instances
    * Multiple bug fixes
    * Add missing Functor and Applicative instances for monads
    * Remove support for GHCs older than 7.4

--- hs-haskell-src-exts/buildlink3.mk
+++ hs-haskell-src-exts/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.9 2013/08/21 13:08:12 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2014/06/26 22:00:39 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-haskell-src-exts
 
 .if !defined(HS_HASKELL_SRC_EXTS_BUILDLINK3_MK)
 HS_HASKELL_SRC_EXTS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.14
+BUILDLINK_API_DEPENDS.hs-haskell-src-exts+=	hs-haskell-src-exts>=1.15.0
 BUILDLINK_PKGSRCDIR.hs-haskell-src-exts?=	../../wip/hs-haskell-src-exts
 
 .include "../../wip/hs-cpphs/buildlink3.mk"
