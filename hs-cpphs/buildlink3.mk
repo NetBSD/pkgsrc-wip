66415f232dfa14a5e41db13f4a7d0a02b6b51f0f
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-06-26 22:00:18 +0000

    Update to 1.18.5 pkgsrc changes: static -> dynamic CHANGELOG: * (1.18.4): fix the accidental flipping of comment-stripping behaviour             with --cpp -traditional flags * (1.18.5): fix a bug with windows filepath directory separators in a             #include

--- hs-cpphs/buildlink3.mk
+++ hs-cpphs/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.12 2014/05/29 22:11:20 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2014/06/26 22:00:18 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-cpphs
 
 .if !defined(HS_CPPHS_BUILDLINK3_MK)
 HS_CPPHS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.3
+BUILDLINK_API_DEPENDS.hs-cpphs+=	hs-cpphs>=1.18.5
 BUILDLINK_PKGSRCDIR.hs-cpphs?=	../../wip/hs-cpphs
 
 .include "../../textproc/hs-polyparse/buildlink3.mk"
