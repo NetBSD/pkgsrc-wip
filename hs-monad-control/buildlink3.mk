e8aeff64c50dc856fe541b90b05333ebbb2f77c4
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-06-27 21:29:13 +0000

    Update to 0.3.3.0 pkgsrc changes: static -> dynamic Changes from https://github.com/basvandijk/monad-control 0.3.3.0  - Depend on the correct transformers-base version  - Support transformers-0.4.0.0  - Drop unicode syntax and symbols

--- hs-monad-control/buildlink3.mk
+++ hs-monad-control/buildlink3.mk
@@ -1,14 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/05/15 21:49:09 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/06/27 21:29:13 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-monad-control
 
 .if !defined(HS_MONAD_CONTROL_BUILDLINK3_MK)
 HS_MONAD_CONTROL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-monad-control+=	hs-monad-control>=0.3.2
+BUILDLINK_API_DEPENDS.hs-monad-control+=	hs-monad-control>=0.3.3
 BUILDLINK_PKGSRCDIR.hs-monad-control?=	../../wip/hs-monad-control
 
-.include "../../wip/hs-base-unicode-symbols/buildlink3.mk"
 .include "../../devel/hs-transformers/buildlink3.mk"
 .include "../../devel/hs-transformers-base/buildlink3.mk"
 .endif	# HS_MONAD_CONTROL_BUILDLINK3_MK
