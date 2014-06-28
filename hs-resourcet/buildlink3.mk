13a75b46bedbe54600fba9fb21a3b78a860dae00
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-06-28 22:09:59 +0000

    Update to 1.1.2.2
    Changes from https://github.com/snoyberg/conduit/tree/master/resourcet
    1.1.2.2
     - transformers 0.4
    1.1.2.1
     - exceptions 0.6 #153
    1.1.2
     - mkAcquireType
    1.1.1
     - MonadCatch instance c/o @jwiegley
    1.1.0.1
     - Minor documentation fix
       `MonadUnsafeIO` is no longer a requirement for `MonadResource`
    1.1.0
     - Excptions version bound change
     - Switch to MonadThrow from exceptions
     - Create the Data.Acquire module
     - Kill MonadUnsafeIO, move MonadActive to Data.Conduit.Lazy
    0.4.10.2
     - Cleanup actions should be masked (fixes #144)
    0.4.10.1
     - Remove an extra backslash

--- hs-resourcet/buildlink3.mk
+++ hs-resourcet/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.6 2014/05/29 22:11:20 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2014/06/28 22:09:59 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-resourcet
 
 .if !defined(HS_RESOURCET_BUILDLINK3_MK)
 HS_RESOURCET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=0.4.10
+BUILDLINK_API_DEPENDS.hs-resourcet+=	hs-resourcet>=1.1.2
 BUILDLINK_PKGSRCDIR.hs-resourcet?=	../../wip/hs-resourcet
 
+.include "../../devel/hs-exceptions/buildlink3.mk"
 .include "../../wip/hs-lifted-base/buildlink3.mk"
 .include "../../devel/hs-mmorph/buildlink3.mk"
 .include "../../wip/hs-monad-control/buildlink3.mk"
