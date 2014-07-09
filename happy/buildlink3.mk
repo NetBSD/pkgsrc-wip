4754657a91060d02dc7550451c49b0e78ac38bfd
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-07-09 19:23:25 +0000

    Update to 1.19.4
    CHANGES:
    1.19.4
            * fix for GHC 7.10 (Applicative/Monad, #19, #21)
    1.19.3
            * fix for GHC 7.2 (#16)

--- happy/buildlink3.mk
+++ happy/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2014/05/16 20:47:07 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2014/07/09 19:23:25 szptvlfn Exp $
 
 BUILDLINK_TREE+=	happy
 
 .if !defined(HAPPY_BUILDLINK3_MK)
 HAPPY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.happy+=	happy>=1.19.2
+BUILDLINK_API_DEPENDS.happy+=	happy>=1.19.4
 BUILDLINK_PKGSRCDIR.happy?=	../../wip/happy
 
 .include "../../devel/hs-mtl/buildlink3.mk"
