9428b60de14ed6635a3ea2a552804d0c57ad2ccc
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-07-12 21:43:41 +0000

    Update to 0.3.6
    CHANGES:
    0.3.6
        #6, remove unnecessary Ord constraints from maximumBy/minimumBy
    0.3.5
        Add Safe elemIndexJust/findIndexJust functions
        Add Safe scan functions
        Add Safe minimumBy/maximumBy functions
        Add a module of Exact functions
        Add Foldable minimum functions
        Clean up the Foldable module, deprecate the Safe variants

--- hs-safe/buildlink3.mk
+++ hs-safe/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/02/01 10:10:33 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/07/12 21:43:41 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-safe
 
 .if !defined(HS_SAFE_BUILDLINK3_MK)
 HS_SAFE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-safe+=	hs-safe>=0.3.4
+BUILDLINK_API_DEPENDS.hs-safe+=	hs-safe>=0.3.6
 BUILDLINK_PKGSRCDIR.hs-safe?=	../../wip/hs-safe
 .endif	# HS_SAFE_BUILDLINK3_MK
 
