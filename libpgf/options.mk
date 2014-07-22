353f1b9dd38d962052f68b9430c449eea4102e96
Thomas Klausner <wiz@NetBSD.org>
2014-07-22 17:33:43 +0000

    Fix PLIST for doxygen-1.8.7 and depend on it.

--- libpgf/options.mk
+++ libpgf/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2014/06/29 19:27:17 nros Exp $
+# $NetBSD: options.mk,v 1.3 2014/07/22 17:33:43 thomasklausner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libpgf
 PKG_SUPPORTED_OPTIONS=	doc
@@ -8,7 +8,7 @@ PLIST_VARS+=		doc
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdoc)
-BUILD_DEPENDS+=		doxygen>=1.7.4:../../devel/doxygen
+BUILD_DEPENDS+=		doxygen>=1.8.7:../../devel/doxygen
 PLIST.doc=		yes
 CONFIGURE_ARGS+=	--enable-doc
 .endif
