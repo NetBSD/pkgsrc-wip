4bc3c93c78787099e8996dcf587cc2fa488648f4
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-07-26 21:49:27 +0000

    Update to 0.3.7
    CHANGES:
    0.3.7
        Add Def variants of the Exact functions

--- hs-safe/buildlink3.mk
+++ hs-safe/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/07/12 21:43:41 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/07/26 21:49:27 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-safe
 
 .if !defined(HS_SAFE_BUILDLINK3_MK)
 HS_SAFE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-safe+=	hs-safe>=0.3.6
+BUILDLINK_API_DEPENDS.hs-safe+=	hs-safe>=0.3.7
 BUILDLINK_PKGSRCDIR.hs-safe?=	../../wip/hs-safe
 .endif	# HS_SAFE_BUILDLINK3_MK
 
