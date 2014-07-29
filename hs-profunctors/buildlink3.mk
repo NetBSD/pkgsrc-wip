b239df61ff9864b4e0c34d6338e8a330f99ee5df
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-07-29 21:37:04 +0000

    Update to 4.2
    CHANGELOG:
    4.2
    ---
    * Renamed `-|` to `ProfunctorAdjunction` because GHC 7.4 still exists in the wild.
    * Renamed `-/->` to `:->` for the same reason. Also the former was confusing as they conflated profunctor homomorphisms and profunctors themselves.
    
    4.1
    ---
    * Flipped the order of 'Procompose'
    * Added the notion of Monads and Comonads on the category of profunctors.
    * Added 'Cayley' which takes normal Haskell Monads and Comonads to a 'ProfunctorMonad' and 'ProfunctorComonad' respectively. Cayley is also known as the 'static arrow' construction
    * Added 'Closed' which is adjoint to 'Strong'.
    * Added 'Closure' which freely adjoins 'Closed' to any 'Profunctor'.
    * Added 'Tambara' which freely adjoins 'Strong' to any 'Profunctor'.
    * Added 'Cotambara' which freely adjoins 'Choice' to any 'Profunctor'.
    * Under the new 'Procompose' the old 'Rift' is now 'Ran', and the old 'Lift' was misnamed. It is now 'Rift'
    
    4.0.3
    -----
    * Added `Data.Profunctor.Lift` containing the left Kan lift of a profunctor.

--- hs-profunctors/buildlink3.mk
+++ hs-profunctors/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.5 2014/05/17 21:00:08 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2014/07/29 21:37:04 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-profunctors
 
 .if !defined(HS_PROFUNCTORS_BUILDLINK3_MK)
 HS_PROFUNCTORS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-profunctors+=	hs-profunctors>=4.0.2
+BUILDLINK_API_DEPENDS.hs-profunctors+=	hs-profunctors>=4.2
 BUILDLINK_PKGSRCDIR.hs-profunctors?=	../../wip/hs-profunctors
 
 .include "../../wip/hs-comonad/buildlink3.mk"
+.include "../../wip/hs-distributive/buildlink3.mk"
 .include "../../wip/hs-semigroupoids/buildlink3.mk"
 .include "../../devel/hs-tagged/buildlink3.mk"
 .include "../../devel/hs-transformers/buildlink3.mk"
