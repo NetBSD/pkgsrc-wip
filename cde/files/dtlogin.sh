95467cdb37190d9755dbc2f8971011d07ea365a4
OBATA Akio <obache@users.sourceforge.net>
2014-07-30 09:44:55 +0000

    reload is supported

--- cde/files/dtlogin.sh
+++ cde/files/dtlogin.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: dtlogin.sh,v 1.1 2014/07/28 13:56:46 obache Exp $
+# $NetBSD: dtlogin.sh,v 1.2 2014/07/30 09:44:55 obache Exp $
 #
 # PROVIDE: dtlogin
 # REQUIRE: DAEMON LOGIN
@@ -14,6 +14,7 @@ rcvar=$name
 command="@PREFIX@/dt/bin/dtlogin"
 command_args="-daemon"
 pidfile="@VARBASE@/dt/Xpid"
+extra_commands="reload"
 
 load_rc_config $name
 run_rc_command "$1"
