54b0c8333a9dd498f1c7e3b077a2b35d108ec43f
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-07-31 21:47:36 +0000

    Update to 0.7.1.3
    Changelog:
    Changed in version 0.7.1.3:
       * Add support for transformers-0.4.0.0.
    
    Changed in version 0.7.1.2:
       * Require ghc>=7.0.1.
       * Allow building with terminfo-0.4.

--- hs-haskeline/buildlink3.mk
+++ hs-haskeline/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2013/12/05 02:50:08 phonohawk Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2014/07/31 21:47:36 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-haskeline
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.hs-haskeline+=	hs-haskeline>=0.7.1
 BUILDLINK_PKGSRCDIR.hs-haskeline?=	../../wip/hs-haskeline
 
 .include "../../wip/hs-terminfo/buildlink3.mk"
+.include "../../devel/hs-transformers/buildlink3.mk"
 .endif	# HS_HASKELINE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-hs-haskeline
