cb4646e9e96bba614f175d6b3d0ea9ff1dc694fb
Greg Troxel <gdt@lexort.com>
2014-08-01 23:35:12 +0000

    Half-baked postgis2 package.
    I've had this lying around for a long time.  I'm putting it in wip so
    that others can work on it as well.  The notion is that it enter
    pkgsrc as postresql-postgis2 so that anyone still using postgis 1.x
    can migrate, and because it's likely some future version will need to
    be parallel-buildable (even if not parallel-installable).

---
+++ postgresql-postgis2/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/08/01 23:35:12 lexort Exp $
+
+BUILDLINK_TREE+=	postgresql-postgis
+
+.if !defined(POSTGRESQL_POSTGIS_BUILDLINK3_MK)
+POSTGRESQL_POSTGIS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.postgresql-postgis+=	postgresql${PGSQL_VERSION}-postgis>=1.4
+BUILDLINK_PKGSRCDIR.postgresql-postgis?=	../../databases/postgresql-postgis
+
+PGSQL_VERSIONS_ACCEPTED=	91 90 84
+
+.include "../../geography/geos/buildlink3.mk"
+.include "../../geography/proj/buildlink3.mk"
+.include "../../mk/pgsql.buildlink3.mk"
+.endif	# POSTGRESQL_POSTGIS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-postgresql-postgis
