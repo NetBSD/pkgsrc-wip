9a20f012e6926e743a4598865238968f4da2f5cd
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-08-13 21:34:15 +0000

    Update to 0.13.2.0
    ChangeLog:
    Changes in 0.13.2.0
    
      * Generators for beta, Bernoully, Dirichlet and categorical distributions
        added.
    
      * Functions for generating random shuffles added.
    
    Changes in 0.13.1.2
    
      * GHC 7.9 support

--- hs-mwc-random/buildlink3.mk
+++ hs-mwc-random/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2014/05/26 22:08:04 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/08/13 21:34:15 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-mwc-random
 
 .if !defined(HS_MWC_RANDOM_BUILDLINK3_MK)
 HS_MWC_RANDOM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-mwc-random+=	hs-mwc-random>=0.13.1
+BUILDLINK_API_DEPENDS.hs-mwc-random+=	hs-mwc-random>=0.13.2
 BUILDLINK_PKGSRCDIR.hs-mwc-random?=	../../wip/hs-mwc-random
 
 .include "../../devel/hs-primitive/buildlink3.mk"
