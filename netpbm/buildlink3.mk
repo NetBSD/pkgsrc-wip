248d70dd8ecd97aed696a396653c215bd0584339
Thomas Klausner <wiz@NetBSD.org>
2014-09-01 21:59:49 +0000

    Import netpbm-10.67.03 as wip/netpbm.
    Netpbm is a toolkit for conversion of images between a variety of
    different formats, as well as to allow a few basic image operations.
    Netpbm is based on the widely spread Pbmplus package (release: 10 Dec
    91).  On top of that, a lot of improvements and additions have been
    made. After the latest release of Pbmplus, a lot of additional filters
    have been circulating on the net. The aim of Netpbm was, to collect
    these and to turn them into a package. This work has been performed by
    a group of programmers all over the world.

---
+++ netpbm/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/09/01 21:59:49 thomasklausner Exp $
+
+BUILDLINK_TREE+=	netpbm
+
+.if !defined(NETPBM_BUILDLINK3_MK)
+NETPBM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.netpbm+=	netpbm>=10.11.6
+BUILDLINK_ABI_DEPENDS.netpbm+=	netpbm>=10.35.80nb6
+BUILDLINK_PKGSRCDIR.netpbm?=	../../graphics/netpbm
+
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+.endif # NETPBM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-netpbm
