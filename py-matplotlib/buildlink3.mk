af22d967c810bd5e1fa283c26ca68a2c8f95e017
Makoto Fujiwara <makoto@ki.nu>
2014-09-01 13:57:15 +0000

    Correct buildlink directory from graphics to wip.

--- py-matplotlib/buildlink3.mk
+++ py-matplotlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/09/01 13:53:58 makoto Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/09/01 13:57:15 makoto Exp $
 
 BUILDLINK_TREE+=	pymatplotlib
 
@@ -8,7 +8,7 @@ PYMATPLOTLIB_BUILDLINK3_MK:=
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.pymatplotlib+=	${PYPKGPREFIX}-matplotlib>=0.87.2
-BUILDLINK_PKGSRCDIR.pymatplotlib?=	../../graphics/py-matplotlib
+BUILDLINK_PKGSRCDIR.pymatplotlib?=	../../wip/py-matplotlib
 .endif # PYMATPLOTLIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-pymatplotlib
