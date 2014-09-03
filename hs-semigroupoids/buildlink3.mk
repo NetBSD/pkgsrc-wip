d696a17b73fd23d3a01859a7b1196ac4a122093c
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-09-03 21:16:18 +0000

    Update to 4.2
    CHANGELOG:
    4.2
    ---
    * Backported `Foldable`/`Traversable` instances from `lens`
    
    4.1
    ---
    * `Foldable1`/`Traversable1` for tuples

--- hs-semigroupoids/buildlink3.mk
+++ hs-semigroupoids/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2014/09/02 21:00:54 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2014/09/03 21:16:18 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-semigroupoids
 
 .if !defined(HS_SEMIGROUPOIDS_BUILDLINK3_MK)
 HS_SEMIGROUPOIDS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=4.0.4
-BUILDLINK_ABI_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=4.0.4
+BUILDLINK_API_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=4.2
+BUILDLINK_ABI_DEPENDS.hs-semigroupoids+=	hs-semigroupoids>=4.2
 BUILDLINK_PKGSRCDIR.hs-semigroupoids?=	../../wip/hs-semigroupoids
 
 .include "../../wip/hs-comonad/buildlink3.mk"
