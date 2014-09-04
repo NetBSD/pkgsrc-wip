fffa66478d397cb343bed2f1427de981a82b4ae0
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-09-04 21:32:55 +0000

    Update to 1.2.0.0
    Changes from https://github.com/basvandijk/case-insensitive
    1.2.0.0
     - Derived a Data instance for CI

--- hs-case-insensitive/buildlink3.mk
+++ hs-case-insensitive/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.8 2014/08/29 14:10:09 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2014/09/04 21:32:55 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-case-insensitive
 
 .if !defined(HS_CASE_INSENSITIVE_BUILDLINK3_MK)
 HS_CASE_INSENSITIVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.1.0
-BUILDLINK_ABI_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.1.0.3
+BUILDLINK_API_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.2.0
+BUILDLINK_ABI_DEPENDS.hs-case-insensitive+=	hs-case-insensitive>=1.2.0.0
 BUILDLINK_PKGSRCDIR.hs-case-insensitive?=	../../wip/hs-case-insensitive
 
 .include "../../devel/hs-hashable/buildlink3.mk"
