324eef8fa5a07aad449efe8a508813552b0cf2c9
Thomas Klausner <wiz@NetBSD.org>
2014-09-06 23:11:12 +0000

    Import py27-xpra-0.14.4 as wip/py-xpra. Packages fine, completely untested.
    Xpra is 'screen for X', and more: it allows you to run X programs,
    usually on a remote host and direct their display to your local
    machine. It also allows you to display existing desktop sessions
    remotely.  Xpra is "rootless" or "seamless", and sessions can be
    accessed over SSH, or password protected and encrypted over plain
    TCP sockets.  Xpra adapts to bandwidth constraints and is fully
    open-source.

---
+++ py-xpra/patches/patch-xpra_x11_gtk__x11_gdk__bindings.pyx
@@ -0,0 +1,15 @@
+$NetBSD: patch-xpra_x11_gtk__x11_gdk__bindings.pyx,v 1.1 2014/09/06 23:11:12 thomasklausner Exp $
+
+Fix path to glib2 headers.
+
+--- xpra/x11/gtk_x11/gdk_bindings.pyx.orig	2014-08-19 08:19:09.000000000 +0000
++++ xpra/x11/gtk_x11/gdk_bindings.pyx
+@@ -65,7 +65,7 @@ init_pygtk()
+ # GObject
+ ###################################
+ 
+-cdef extern from "glib-2.0/glib-object.h":
++cdef extern from "glib-object.h":
+     ctypedef struct cGObject "GObject":
+         pass
+ 
---
+++ py-xpra/patches/patch-xpra_x11_gtk__x11_gdk__display__source.pyx
@@ -0,0 +1,15 @@
+$NetBSD: patch-xpra_x11_gtk__x11_gdk__display__source.pyx,v 1.1 2014/09/06 23:11:12 thomasklausner Exp $
+
+Fix path to glib2 headers.
+
+--- xpra/x11/gtk_x11/gdk_display_source.pyx.orig	2014-08-19 08:19:09.000000000 +0000
++++ xpra/x11/gtk_x11/gdk_display_source.pyx
+@@ -38,7 +38,7 @@ init_pygtk()
+ ###################################
+ # GObject
+ ###################################
+-cdef extern from "glib-2.0/glib-object.h":
++cdef extern from "glib-object.h":
+     ctypedef struct cGObject "GObject":
+         pass
+ 
