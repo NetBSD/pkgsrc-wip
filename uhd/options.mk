66360347807c80a14d1795d742976427ff6f6a6e
Makoto Fujiwara <makoto@ki.nu>
2014-09-07 09:43:34 +0000

    Add new option manual, and add BUILD_DEPENDS for it.

--- uhd/options.mk
+++ uhd/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2014/09/07 05:13:42 makoto Exp $
+# $NetBSD: options.mk,v 1.2 2014/09/07 09:43:34 makoto Exp $
 ### Set options
-PKG_OPTIONS_VAR=        PKG_OPTIONS.uhd
-PKG_SUPPORTED_OPTIONS=  doxygen
-PKG_SUGGESTED_OPTIONS=	doxygen
+PKG_OPTIONS_VAR=	PKG_OPTIONS.uhd
+PKG_SUPPORTED_OPTIONS=	doxygen manual
+PKG_SUGGESTED_OPTIONS=	doxygen manual
 
 .include	"../../mk/bsd.options.mk"
 
@@ -10,3 +10,9 @@ PKG_SUGGESTED_OPTIONS=	doxygen
 DEPENDS+=		doxygen-[0-9]*:../../devel/doxygen
 PLIST_SRC+=		${PKGDIR}/PLIST.doxygen
 .endif
+
+.if !empty(PKG_OPTIONS:Mmanual)
+BUILD_DEPENDS+=		${PYPKGPREFIX}-docutils-[0-9]*:../../textproc/py-docutils
+PLIST_SRC+=		${PKGDIR}/PLIST.manual
+.endif
+
