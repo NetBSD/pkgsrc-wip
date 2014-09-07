cd284be5943f81ec8cf37f100f562329e4b6517d
Makoto Fujiwara <makoto@ki.nu>
2014-09-07 10:50:07 +0000

    1. (options.mk)     tabify and sorting ".include ../../category/package" lines 2. (Makefile) add following argument for cmake +       -DUHD_LIBRARIES=${PREFIX}/lib/libuhd.so \ +       -DUHD_INCLUDE_DIRS=${PREFIX}/include \ 3. Packaged in non-CHROOTed environment 4. (TODO)    Some note added related to #2 above.

--- gnuradio-core/options.mk
+++ gnuradio-core/options.mk
@@ -1,44 +1,45 @@
-# $NetBSD: options.mk,v 1.5 2014/09/07 00:45:08 makoto Exp $
+# $NetBSD: options.mk,v 1.6 2014/09/07 10:50:07 makoto Exp $
 ### Set options
-PKG_OPTIONS_VAR=        PKG_OPTIONS.gnuradio-core
-PKG_SUPPORTED_OPTIONS=  ctrlport digital doxygen qtgui runtime utils wxgui
-PKG_SUPPORTED_OPTIONS+= fec atsc comedi channels noaa pager trellis uhd
-PKG_SUPPORTED_OPTIONS+= fcd wavelet vocoder video-sdl audio-companion
-PKG_SUPPORTED_OPTIONS+= gnuaudio_runtime sphinx
-PKG_SUPPORTED_OPTIONS+= default
+PKG_OPTIONS_VAR=	PKG_OPTIONS.gnuradio-core
+PKG_SUPPORTED_OPTIONS=	ctrlport digital doxygen qtgui runtime utils wxgui
+PKG_SUPPORTED_OPTIONS+=	fec atsc comedi channels noaa pager trellis uhd
+PKG_SUPPORTED_OPTIONS+=	fcd wavelet vocoder video-sdl audio-companion
+PKG_SUPPORTED_OPTIONS+=	gnuaudio_runtime sphinx
+PKG_SUPPORTED_OPTIONS+=	default
 
 # lists checked combination here, having # in leftmost column has a problem.
 # To judge the result quickly, see the log after just 'make configure'
 # (but more to 'make package' for real completion)
 
-PKG_SUGGESTED_OPTIONS=  ctrlport 	 doxygen  	             wxgui
-PKG_SUGGESTED_OPTIONS=           digital	 
-PKG_SUGGESTED_OPTIONS=  	 	          	             wxgui
+PKG_SUGGESTED_OPTIONS=	ctrlport	 doxygen		     wxgui
+PKG_SUGGESTED_OPTIONS=		 digital	 
+PKG_SUGGESTED_OPTIONS=						     wxgui
 # not wholly checked yet
 # linux only, not checked
-#KG_SUGGESTED_OPTIONS=           comedi
+#KG_SUGGESTED_OPTIONS=		 comedi
 # ICE_FOUND= (empty) Ice-3.5.1 at http://doc.zeroc.com/display/Ice/Home
 #KG_SUGGESTED_OPTIONS=	ctrlport
 # packaged:
-PKG_SUGGESTED_OPTIONS=  digital fec wxgui
-PKG_SUGGESTED_OPTIONS=  digital fec wxgui                            uhd
-PKG_SUGGESTED_OPTIONS=  fec
-PKG_SUGGESTED_OPTIONS=      atsc
-PKG_SUGGESTED_OPTIONS=                   channels
-PKG_SUGGESTED_OPTIONS=                            noaa
-PKG_SUGGESTED_OPTIONS=                                 pager
-PKG_SUGGESTED_OPTIONS=                                       trellis
-PKG_SUGGESTED_OPTIONS=                                               uhd
-PKG_SUGGESTED_OPTIONS=  fcd
-PKG_SUGGESTED_OPTIONS=       wavelet
-PKG_SUGGESTED_OPTIONS=                  vocoder
-PKG_SUGGESTED_OPTIONS= 				 	      utils
-#KG_SUGGESTED_OPTIONS= 				qtgui
+PKG_SUGGESTED_OPTIONS=	digital fec wxgui
+PKG_SUGGESTED_OPTIONS=	digital fec wxgui			     uhd
+PKG_SUGGESTED_OPTIONS=	fec
+PKG_SUGGESTED_OPTIONS=	    atsc
+PKG_SUGGESTED_OPTIONS=			 channels
+PKG_SUGGESTED_OPTIONS=				  noaa
+PKG_SUGGESTED_OPTIONS=				       pager
+PKG_SUGGESTED_OPTIONS=					     trellis
+PKG_SUGGESTED_OPTIONS=						     uhd
+PKG_SUGGESTED_OPTIONS=	fcd
+PKG_SUGGESTED_OPTIONS=	     wavelet
+PKG_SUGGESTED_OPTIONS=			vocoder
+PKG_SUGGESTED_OPTIONS=					      utils
+PKG_SUGGESTED_OPTIONS=						     wxgui
+#KG_SUGGESTED_OPTIONS=				qtgui
 # no effect now (default ?)
-#PKG_SUGGESTED_OPTIONS= 				      runtime
+#PKG_SUGGESTED_OPTIONS=					      runtime
 PKG_SUGGESTED_OPTIONS=	doxygen
-PKG_SUGGESTED_OPTIONS= 				video-sdl
-PKG_SUGGESTED_OPTIONS= 				             audio-companion
+PKG_SUGGESTED_OPTIONS=				video-sdl
+PKG_SUGGESTED_OPTIONS=					     audio-companion
 # this 'default' is of upstream definition
 PKG_SUGGESTED_OPTIONS=	default
 
@@ -55,9 +56,9 @@ PLIST_SRC+=		${PKGDIR}/PLIST.fec
 .if !empty(PKG_OPTIONS:Maudio-companion)
 GR_ENABLE_LIST+=	-DENABLE_GRC=True
 PLIST_SRC+=		${PKGDIR}/PLIST.audio-companion
-.include 	"../../audio/jack/buildlink3.mk"
-.include 	"../../audio/alsa-lib/buildlink3.mk"
-.include 	"../../x11/py-gtk2/buildlink3.mk"
+.include	"../../audio/jack/buildlink3.mk"
+.include	"../../audio/alsa-lib/buildlink3.mk"
+.include	"../../x11/py-gtk2/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mchannels)
@@ -114,7 +115,7 @@ PLIST_SRC+=		${PKGDIR}/PLIST.qtgui
 # following two lines are NOT effective ?
 # DEPENDS+=		qt4-[0-9]*:../../x11/qt4/
 # .include "../../x11/qt4-libs/buildlink3.mk"
-.include 	"../../x11/qwt-qt4/buildlink3.mk"
+.include	"../../x11/qwt-qt4/buildlink3.mk"
 # --   Dependency QT4_FOUND =
 # --   Dependency QWT_FOUND = FALSE
 .endif
@@ -159,15 +160,15 @@ PLIST_SRC+=		${PKGDIR}/PLIST.video-sdl
 .if !empty(PKG_OPTIONS:Mvocoder)
 GR_ENABLE_LIST+=	-DENABLE_GR_VOCODER=True
 PLIST_SRC+=		${PKGDIR}/PLIST.vocoder
-.include 	"../../audio/alsa-lib/buildlink3.mk"
-.include 	"../../audio/jack/buildlink3.mk"
+.include	"../../audio/alsa-lib/buildlink3.mk"
+.include	"../../audio/jack/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mwavelet)
 GR_ENABLE_LIST+=	-DENABLE_GR_WAVELET=True
 PLIST_SRC+=		${PKGDIR}/PLIST.wavelet
-.include 	"../../audio/alsa-lib/buildlink3.mk"
-.include 	"../../audio/jack/buildlink3.mk"
+.include	"../../audio/alsa-lib/buildlink3.mk"
+.include	"../../audio/jack/buildlink3.mk"
 .include	"../../math/gsl/buildlink3.mk"
 .endif
 
@@ -175,18 +176,23 @@ PLIST_SRC+=		${PKGDIR}/PLIST.wavelet
 GR_ENABLE_LIST+=	-DENABLE_GR_WXGUI=True
 GR_ENABLE_LIST+=	-DENABLE_PYTHON=True
 PLIST_SRC+=		${PKGDIR}/PLIST.wxgui
+.include	"../../x11/wxGTK28/buildlink3.mk"
+.include	"../../x11/py-wxWidgets/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mdefault)
 # please note NOT +=, but =
 GR_ENABLE_LIST=		-DENABLE_DEFAULT=True
-.include	"../../wip/uhd/buildlink3.mk"
+.include	"../../audio/alsa-lib/buildlink3.mk"
+.include	"../../audio/jack/buildlink3.mk"
 .include	"../../devel/SDL/buildlink3.mk"
 .include	"../../devel/cppunit/buildlink3.mk"
 .include	"../../devel/libusb1/buildlink3.mk"
 .include	"../../math/gsl/buildlink3.mk"
-.include 	"../../audio/jack/buildlink3.mk"
-.include 	"../../audio/alsa-lib/buildlink3.mk"
+.include	"../../wip/uhd/buildlink3.mk"
+.include	"../../x11/py-gtk2/buildlink3.mk"
+.include	"../../x11/py-wxWidgets/buildlink3.mk"
+.include	"../../x11/wxGTK28/buildlink3.mk"
 BUILD_DEPENDS+=		${PYPKGPREFIX}-sphinx-[0-9]*:../../textproc/py-sphinx
 
 PLIST_SRC+=		${PKGDIR}/PLIST.atsc
