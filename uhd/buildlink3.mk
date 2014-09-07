ad424640a24eb2807b7957dc57799ace6c59b1dd
Makoto Fujiwara <makoto@ki.nu>
2014-09-07 03:33:52 +0000

    correction to left out "uhd-git" string in buildlink3.mk

--- uhd/buildlink3.mk
+++ uhd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/09/06 05:23:28 makoto Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/09/07 03:33:52 makoto Exp $
 
 BUILDLINK_TREE+=        uhd
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=        uhd
 UHD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.uhd+=    uhd>=2.22
-BUILDLINK_PKGSRCDIR.uhd?=              ../../wip/uhd-git
+BUILDLINK_PKGSRCDIR.uhd?=              ../../wip/uhd
 .endif # UHD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=        -uhd
