9b95b5a9a07cec369c8d3be5f9d34ec080e3a5fd
Thomas Klausner <wiz@NetBSD.org>
2014-09-08 12:37:30 +0000

    depend on netatalk22 instead of non-existent netatalk

--- natali/buildlink3.mk
+++ natali/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/05/06 10:13:22 cheusov Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/09/08 12:37:30 thomasklausner Exp $
 
 BUILDLINK_TREE+=	natali
 
@@ -8,7 +8,7 @@ NATALI_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.natali+=	natali>=1.25
 BUILDLINK_PKGSRCDIR.natali?=	../../wip/natali
 
-.include "../../net/netatalk/buildlink3.mk"
+.include "../../net/netatalk22/buildlink3.mk"
 .endif # NATALI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-natali
