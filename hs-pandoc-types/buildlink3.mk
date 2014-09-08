56cb271250c6f34e1a4300cae2bdbcc01b455f27
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-09-08 21:25:27 +0000

    Update to 1.12.4.1
    Changes from https://github.com/jgm/pandoc-types
    1.12.4.1
     - relax cabal constraint for aeson
    1.12.4
     - Added NFData instances for use with deepseq
    1.12.3.3
     - Added Walkable instances for Citation.
       This address pandoc #1269 -- previously `walk` did not enter
       into Citation prefixes and suffixes.

--- hs-pandoc-types/buildlink3.mk
+++ hs-pandoc-types/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.7 2014/09/07 21:10:04 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2014/09/08 21:25:27 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-pandoc-types
 
 .if !defined(HS_PANDOC_TYPES_BUILDLINK3_MK)
 HS_PANDOC_TYPES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.3
-BUILDLINK_ABI_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.3.2
+BUILDLINK_API_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.4
+BUILDLINK_ABI_DEPENDS.hs-pandoc-types+=	hs-pandoc-types>=1.12.4.1
 BUILDLINK_PKGSRCDIR.hs-pandoc-types?=	../../wip/hs-pandoc-types
 
 .include "../../converters/hs-aeson/buildlink3.mk"
+.include "../../devel/hs-deepseq-generics/buildlink3.mk"
 .include "../../devel/hs-syb/buildlink3.mk"
 .endif	# HS_PANDOC_TYPES_BUILDLINK3_MK
 
