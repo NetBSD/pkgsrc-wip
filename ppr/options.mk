77acf34cccbd5f9052280e66f164452feaa33b22
Thomas Klausner <wiz@NetBSD.org>
2014-09-08 20:37:40 +0000

    Fix option name. DEPEND on netpbm unless we're linking against it.

--- ppr/options.mk
+++ ppr/options.mk
@@ -1,6 +1,6 @@
-# $NetBSD: options.mk,v 1.1 2006/01/15 17:34:34 hfath Exp $
+# $NetBSD: options.mk,v 1.2 2014/09/08 20:37:40 thomasklausner Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.tiff
+PKG_OPTIONS_VAR=	PKG_OPTIONS.ppr
 PKG_SUPPORTED_OPTIONS=	netpbm
 PKG_DEFAULT_OPTIONS=	netpbm
 .include "../../mk/bsd.options.mk"
@@ -9,5 +9,5 @@ PKG_DEFAULT_OPTIONS=	netpbm
 ### Create filters using netpbm?
 ###
 .if !empty(PKG_OPTIONS:Mnetpbm)
-.include "../../graphics/netpbm/buildlink3.mk"
+DEPENDS+=	netpbm-[0-9]*:../../graphics/netpbm
 .endif
