ea9f3125319a3bf864b40eaf4d64968a2e884fa9
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-09-09 19:08:47 +0000

    Update to 1.2.1.0
    CHANGELOG:
    1.2.1.0
    
        Uses exceptions-0.6 (or later), where mask/unmask were moved to a new class
    
    1.2.0.0
    
        Uses exceptions-0.4 (or later), where throwM was moved to its own class

--- hs-ghc-mtl/buildlink3.mk
+++ hs-ghc-mtl/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.7 2014/08/29 14:10:13 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2014/09/09 19:08:47 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-ghc-mtl
 
 .if !defined(HS_GHC_MTL_BUILDLINK3_MK)
 HS_GHC_MTL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-ghc-mtl+=	hs-ghc-mtl>=1.1.0
-BUILDLINK_ABI_DEPENDS.hs-ghc-mtl+=	hs-ghc-mtl>=1.1.0.0
+BUILDLINK_API_DEPENDS.hs-ghc-mtl+=	hs-ghc-mtl>=1.2.1
+BUILDLINK_ABI_DEPENDS.hs-ghc-mtl+=	hs-ghc-mtl>=1.2.1.0
 BUILDLINK_PKGSRCDIR.hs-ghc-mtl?=	../../wip/hs-ghc-mtl
 
 .include "../../devel/hs-exceptions/buildlink3.mk"
