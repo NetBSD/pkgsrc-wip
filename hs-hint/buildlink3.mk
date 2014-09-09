8ff4655e4a84558df079b28749ffb7c2be05be73
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-09-09 21:16:07 +0000

    Update to 0.4.2.0
    CHANGELOG:
    0.4.2.0
    -------
    
     * Based on exceptions-0.6
    
    0.4.1.0
    -------
    
     * Based on exceptions-0.4

--- hs-hint/buildlink3.mk
+++ hs-hint/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2014/08/29 14:10:22 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2014/09/09 21:16:07 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-hint
 
 .if !defined(HS_HINT_BUILDLINK3_MK)
 HS_HINT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-hint+=	hs-hint>=0.4.0
-BUILDLINK_ABI_DEPENDS.hs-hint+=	hs-hint>=0.4.0.0
+BUILDLINK_API_DEPENDS.hs-hint+=	hs-hint>=0.4.2
+BUILDLINK_ABI_DEPENDS.hs-hint+=	hs-hint>=0.4.2.0
 BUILDLINK_PKGSRCDIR.hs-hint?=	../../wip/hs-hint
 
 .include "../../devel/hs-exceptions/buildlink3.mk"
