7dc6b093277663a7654fba66839248e53cab99ef
Makoto Fujiwara <makoto@ki.nu>
2014-09-09 07:48:43 +0000

    (Upstream update 3.7.2.1 to 3.7.5) ChangeLog for 3.7.5   http://gnuradio.org/redmine/projects/gnuradio/wiki/ChangeLogV3_7_5 ChangeLog for 3.7.2   http://gnuradio.org/redmine/projects/gnuradio/wiki/ChangeLogV3_7_2
    (pkgsrc)
    convert cmake command at do-configure: to USE_CMAKE= yes style
    (Not all combinations of options are verified yet, sorry).

--- gnuradio-core/options.mk
+++ gnuradio-core/options.mk
@@ -1,60 +1,55 @@
-# $NetBSD: options.mk,v 1.6 2014/09/07 10:50:07 makoto Exp $
+# $NetBSD: options.mk,v 1.7 2014/09/09 07:48:44 makoto Exp $
 ### Set options
-PKG_OPTIONS_VAR=	PKG_OPTIONS.gnuradio-core
-PKG_SUPPORTED_OPTIONS=	ctrlport digital doxygen qtgui runtime utils wxgui
-PKG_SUPPORTED_OPTIONS+=	fec atsc comedi channels noaa pager trellis uhd
-PKG_SUPPORTED_OPTIONS+=	fcd wavelet vocoder video-sdl audio-companion
-PKG_SUPPORTED_OPTIONS+=	gnuaudio_runtime sphinx
+PKG_OPTIONS_VAR=	PKG_OPTIONS.gnuradio
+
+PKG_SUPPORTED_OPTIONS+=	atsc
+PKG_SUPPORTED_OPTIONS+=	audio-companion
+PKG_SUPPORTED_OPTIONS+=	channels
+#                       linux only, not checked
+PKG_SUPPORTED_OPTIONS+=	comedi
+#                       ICE_FOUND= (empty) Ice-3.5.1 at
+#                       http://doc.zeroc.com/display/Ice/Home
+PKG_SUPPORTED_OPTIONS+=	ctrlport
+PKG_SUPPORTED_OPTIONS+=	digital
+PKG_SUPPORTED_OPTIONS+=	doxygen
+PKG_SUPPORTED_OPTIONS+=	dtvx
+PKG_SUPPORTED_OPTIONS+=	fcd
+PKG_SUPPORTED_OPTIONS+=	fec
+PKG_SUPPORTED_OPTIONS+=	gnuaudio_runtime
+PKG_SUPPORTED_OPTIONS+=	noaa
+PKG_SUPPORTED_OPTIONS+=	pager
+PKG_SUPPORTED_OPTIONS+=	qtgui
+#                       (default ?)
+PKG_SUPPORTED_OPTIONS+=	runtime
+PKG_SUPPORTED_OPTIONS+=	sphinx
+PKG_SUPPORTED_OPTIONS+=	trellis
+PKG_SUPPORTED_OPTIONS+=	uhd
+PKG_SUPPORTED_OPTIONS+=	utils
+PKG_SUPPORTED_OPTIONS+=	video-sdl
+PKG_SUPPORTED_OPTIONS+=	vocoder
+PKG_SUPPORTED_OPTIONS+=	wavelet
+PKG_SUPPORTED_OPTIONS+=	wxgui
+PKG_SUPPORTED_OPTIONS+=	zeromq
+
+# this 'default' is of upstream definition
 PKG_SUPPORTED_OPTIONS+=	default
 
-# lists checked combination here, having # in leftmost column has a problem.
-# To judge the result quickly, see the log after just 'make configure'
-# (but more to 'make package' for real completion)
-
-PKG_SUGGESTED_OPTIONS=	ctrlport	 doxygen		     wxgui
-PKG_SUGGESTED_OPTIONS=		 digital	 
-PKG_SUGGESTED_OPTIONS=						     wxgui
-# not wholly checked yet
-# linux only, not checked
-#KG_SUGGESTED_OPTIONS=		 comedi
-# ICE_FOUND= (empty) Ice-3.5.1 at http://doc.zeroc.com/display/Ice/Home
-#KG_SUGGESTED_OPTIONS=	ctrlport
-# packaged:
-PKG_SUGGESTED_OPTIONS=	digital fec wxgui
-PKG_SUGGESTED_OPTIONS=	digital fec wxgui			     uhd
-PKG_SUGGESTED_OPTIONS=	fec
-PKG_SUGGESTED_OPTIONS=	    atsc
-PKG_SUGGESTED_OPTIONS=			 channels
-PKG_SUGGESTED_OPTIONS=				  noaa
-PKG_SUGGESTED_OPTIONS=				       pager
-PKG_SUGGESTED_OPTIONS=					     trellis
-PKG_SUGGESTED_OPTIONS=						     uhd
-PKG_SUGGESTED_OPTIONS=	fcd
-PKG_SUGGESTED_OPTIONS=	     wavelet
-PKG_SUGGESTED_OPTIONS=			vocoder
-PKG_SUGGESTED_OPTIONS=					      utils
-PKG_SUGGESTED_OPTIONS=						     wxgui
-#KG_SUGGESTED_OPTIONS=				qtgui
-# no effect now (default ?)
-#PKG_SUGGESTED_OPTIONS=					      runtime
-PKG_SUGGESTED_OPTIONS=	doxygen
-PKG_SUGGESTED_OPTIONS=				video-sdl
-PKG_SUGGESTED_OPTIONS=					     audio-companion
-# this 'default' is of upstream definition
+# next line means really fully available
 PKG_SUGGESTED_OPTIONS=	default
+PKG_SUGGESTED_OPTIONS=  doxygen
 
 .include	"../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Matsc)
-GR_ENABLE_LIST+=	-DENABLE_GR_ATSC=True
-GR_ENABLE_LIST+=	-DENABLE_GR_FEC=True
+CMAKE_ARGS+=	-DENABLE_GR_ATSC=True
+CMAKE_ARGS+=	-DENABLE_GR_FEC=True
 PLIST_SRC+=		${PKGDIR}/PLIST.atsc
 PLIST_SRC+=		${PKGDIR}/PLIST.fec
 .endif
 
 # in the configure log, it is said gnuradio-companion
 .if !empty(PKG_OPTIONS:Maudio-companion)
-GR_ENABLE_LIST+=	-DENABLE_GRC=True
+CMAKE_ARGS+=	-DENABLE_GRC=True
 PLIST_SRC+=		${PKGDIR}/PLIST.audio-companion
 .include	"../../audio/jack/buildlink3.mk"
 .include	"../../audio/alsa-lib/buildlink3.mk"
@@ -62,55 +57,55 @@ PLIST_SRC+=		${PKGDIR}/PLIST.audio-companion
 .endif
 
 .if !empty(PKG_OPTIONS:Mchannels)
-GR_ENABLE_LIST+=	-DENABLE_GR_CHANNELS=True
+CMAKE_ARGS+=	-DENABLE_GR_CHANNELS=True
 PLIST_SRC+=		${PKGDIR}/PLIST.channels
 .endif
 
 .if !empty(PKG_OPTIONS:Mcomedi)
-GR_ENABLE_LIST+=	-DENABLE_GR_COMEDI=True
+CMAKE_ARGS+=	-DENABLE_GR_COMEDI=True
 PLIST_SRC+=		${PKGDIR}/PLIST.comedi
 .endif
 
 # nothing changed:
 # .if !empty(PKG_OPTIONS:Mctrlport)
-# GR_ENABLE_LIST+=	-DENABLE_GR_CTRLPORT=True
+# CMAKE_ARGS+=	-DENABLE_GR_CTRLPORT=True
 # PLIST_SRC+=		${PKGDIR}/PLIST.ctrlport
 # .endif
 
 .if !empty(PKG_OPTIONS:Mdigital)
-GR_ENABLE_LIST+=	-DENABLE_GR_DIGITAL=True
+CMAKE_ARGS+=	-DENABLE_GR_DIGITAL=True
 PLIST_SRC+=		${PKGDIR}/PLIST.digital
 .endif
 
 .if !empty(PKG_OPTIONS:Mdoxygen)
-GR_ENABLE_LIST+=	-DENABLE_DOXYGEN=True
+CMAKE_ARGS+=	-DENABLE_DOXYGEN=True
 PLIST_SRC+=		${PKGDIR}/PLIST.doxygen
 BUILD_DEPENDS+=		${PYPKGPREFIX}-sphinx-[0-9]*:../../textproc/py-sphinx
 .endif
 
 .if !empty(PKG_OPTIONS:Mfcd)
 .include	"../../devel/libusb1/buildlink3.mk"
-GR_ENABLE_LIST+=	-DENABLE_GR_FCD=True
+CMAKE_ARGS+=	-DENABLE_GR_FCD=True
 PLIST_SRC+=		${PKGDIR}/PLIST.fcd
 .endif
 
 .if !empty(PKG_OPTIONS:Mfec)
-GR_ENABLE_LIST+=	-DENABLE_GR_FEC=True
+CMAKE_ARGS+=	-DENABLE_GR_FEC=True
 PLIST_SRC+=		${PKGDIR}/PLIST.fec
 .endif
 
 .if !empty(PKG_OPTIONS:Mnoaa)
-GR_ENABLE_LIST+=	-DENABLE_GR_NOAA=True
+CMAKE_ARGS+=	-DENABLE_GR_NOAA=True
 PLIST_SRC+=		${PKGDIR}/PLIST.noaa
 .endif
 
 .if !empty(PKG_OPTIONS:Mpager)
-GR_ENABLE_LIST+=	-DENABLE_GR_PAGER=True
+CMAKE_ARGS+=	-DENABLE_GR_PAGER=True
 PLIST_SRC+=		${PKGDIR}/PLIST.pager
 .endif
 
 .if !empty(PKG_OPTIONS:Mqtgui)
-GR_ENABLE_LIST+=	-DENABLE_QTGUI=True
+CMAKE_ARGS+=	-DENABLE_QTGUI=True
 PLIST_SRC+=		${PKGDIR}/PLIST.qtgui
 # following two lines are NOT effective ?
 # DEPENDS+=		qt4-[0-9]*:../../x11/qt4/
@@ -121,51 +116,52 @@ PLIST_SRC+=		${PKGDIR}/PLIST.qtgui
 .endif
 
 .if !empty(PKG_OPTIONS:Msphinx)
-GR_ENABLE_LIST+=	-DENABLE_SPHINX=True
+CMAKE_ARGS+=	-DENABLE_SPHINX=True
 PLIST_SRC+=		${PKGDIR}/PLIST.sphinx
 BUILD_DEPENDS+=		${PYPKGPREFIX}-sphinx-[0-9]*:../../textproc/py-sphinx
 .endif
 
 .if !empty(PKG_OPTIONS:Mtrellis)
-GR_ENABLE_LIST+=	-DENABLE_GR_TRELLIS=True
-GR_ENABLE_LIST+=	-DENABLE_GR_DIGITAL=True
+CMAKE_ARGS+=	-DENABLE_GR_TRELLIS=True
+CMAKE_ARGS+=	-DENABLE_GR_DIGITAL=True
 PLIST_SRC+=		${PKGDIR}/PLIST.trellis
 .endif
 
 .if !empty(PKG_OPTIONS:Muhd)
-GR_ENABLE_LIST+=	-DENABLE_GR_UHD=True
+CMAKE_ARGS+=	-DENABLE_GR_UHD=True
 PLIST_SRC+=		${PKGDIR}/PLIST.uhd
-.include	"../../wip/uhd/buildlink3.mk"
+.include	"../../ham/uhd/buildlink3.mk"
 .include	"../../devel/cppunit/buildlink3.mk"
 .include	"../../devel/libusb1/buildlink3.mk"
 .include	"../../math/gsl/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mruntime)
-GR_ENABLE_LIST+=	-DENABLE_RUNTIME=True
+CMAKE_ARGS+=	-DENABLE_RUNTIME=True
 PLIST_SRC+=		${PKGDIR}/PLIST.runtime
 .endif
 
 .if !empty(PKG_OPTIONS:Mutils)
-GR_ENABLE_LIST+=	-DENABLE_GR_UTILS=True
+CMAKE_ARGS+=	-DENABLE_GR_UTILS=True
 PLIST_SRC+=		${PKGDIR}/PLIST.utils
 .endif
 
 .if !empty(PKG_OPTIONS:Mvideo-sdl)
-GR_ENABLE_LIST+=	-DENABLE_GR_VIDEO_SDL=True
+CMAKE_ARGS+=	-DENABLE_GR_VIDEO_SDL=True
 PLIST_SRC+=		${PKGDIR}/PLIST.video-sdl
 .include	"../../devel/SDL/buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mvocoder)
-GR_ENABLE_LIST+=	-DENABLE_GR_VOCODER=True
+.if !empty(PKG_OPTIONS:Mvocoder) || !empty(PKG_OPTIONS:Mdefault)
+CMAKE_ARGS+=	-DENABLE_GR_VOCODER=True
 PLIST_SRC+=		${PKGDIR}/PLIST.vocoder
 .include	"../../audio/alsa-lib/buildlink3.mk"
 .include	"../../audio/jack/buildlink3.mk"
+.include	"../../audio/gsm/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mwavelet)
-GR_ENABLE_LIST+=	-DENABLE_GR_WAVELET=True
+CMAKE_ARGS+=	-DENABLE_GR_WAVELET=True
 PLIST_SRC+=		${PKGDIR}/PLIST.wavelet
 .include	"../../audio/alsa-lib/buildlink3.mk"
 .include	"../../audio/jack/buildlink3.mk"
@@ -173,16 +169,21 @@ PLIST_SRC+=		${PKGDIR}/PLIST.wavelet
 .endif
 
 .if !empty(PKG_OPTIONS:Mwxgui)
-GR_ENABLE_LIST+=	-DENABLE_GR_WXGUI=True
-GR_ENABLE_LIST+=	-DENABLE_PYTHON=True
+CMAKE_ARGS+=	-DENABLE_GR_WXGUI=True
+CMAKE_ARGS+=	-DENABLE_PYTHON=True
 PLIST_SRC+=		${PKGDIR}/PLIST.wxgui
 .include	"../../x11/wxGTK28/buildlink3.mk"
 .include	"../../x11/py-wxWidgets/buildlink3.mk"
 .endif
 
+.if !empty(PKG_OPTIONS:Mzeromq)
+CMAKE_ARGS+=	-DENABLE_GR_ZEROMQ=True
+PLIST_SRC+=		${PKGDIR}/PLIST.zeromq
+.endif
+
 .if !empty(PKG_OPTIONS:Mdefault)
-# please note NOT +=, but =
-GR_ENABLE_LIST=		-DENABLE_DEFAULT=True
+# please note NOT +=, but = , override everything (but is it default ?)
+CMAKE_ARGS=		-DENABLE_DEFAULT=True
 .include	"../../audio/alsa-lib/buildlink3.mk"
 .include	"../../audio/jack/buildlink3.mk"
 .include	"../../devel/SDL/buildlink3.mk"
@@ -204,11 +205,13 @@ PLIST_SRC+=		${PKGDIR}/PLIST.digital
 PLIST_SRC+=		${PKGDIR}/PLIST.doxygen
 PLIST_SRC+=		${PKGDIR}/PLIST.fcd
 PLIST_SRC+=		${PKGDIR}/PLIST.fec
+PLIST_SRC+=		${PKGDIR}/PLIST.gnuaudio_runtime
 PLIST_SRC+=		${PKGDIR}/PLIST.noaa
 PLIST_SRC+=		${PKGDIR}/PLIST.pager
 PLIST_SRC+=		${PKGDIR}/PLIST.python
 PLIST_SRC+=		${PKGDIR}/PLIST.qtgui
 PLIST_SRC+=		${PKGDIR}/PLIST.runtime
+PLIST_SRC+=		${PKGDIR}/PLIST.sphinx
 PLIST_SRC+=		${PKGDIR}/PLIST.trellis
 PLIST_SRC+=		${PKGDIR}/PLIST.uhd
 PLIST_SRC+=		${PKGDIR}/PLIST.utils
@@ -216,5 +219,7 @@ PLIST_SRC+=		${PKGDIR}/PLIST.video-sdl
 PLIST_SRC+=		${PKGDIR}/PLIST.vocoder
 PLIST_SRC+=		${PKGDIR}/PLIST.wavelet
 PLIST_SRC+=		${PKGDIR}/PLIST.wxgui
+PLIST_SRC+=		${PKGDIR}/PLIST.zeromq
+
 PLIST_SRC+=		${PKGDIR}/PLIST.default
 .endif
