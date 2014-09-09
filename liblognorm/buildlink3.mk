47fc3f4758f4023def5f075237f06c5bf2b3f6e0
Filip Hajny <filip@joyent.com>
2014-09-09 11:32:55 +0000

    Fix the upper version limit for liblognorm<1.

--- liblognorm/buildlink3.mk
+++ liblognorm/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2011/04/18 13:41:31 bartoszkuzma Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/09/09 11:32:55 fhajny Exp $
 
 BUILDLINK_TREE+=	liblognorm
 
 .if !defined(LIBLOGNORM_BUILDLINK3_MK)
 LIBLOGNORM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liblognorm+=	liblognorm>=0.3.1
-BUILDLINK_ABI_DEPENDS.liblognorm+=	liblognorm>=0.3.1
+BUILDLINK_API_DEPENDS.liblognorm+=	liblognorm>=0.3.1<1.0.0
 BUILDLINK_PKGSRCDIR.liblognorm=		../../wip/liblognorm
 
 .endif # LIBLOGNORM_BUILDLINK3_MK
