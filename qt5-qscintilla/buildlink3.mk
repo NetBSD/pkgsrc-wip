8d510cf064ea050c715a1f392c55469984793517
Niclas Rosenvik <nros@NetBSD.org>
2014-09-25 17:46:26 +0000

    Importing qscintilla for qt5 as qt5-qscintilla. Qscintilla2 is a port to Qt of Neil Hodgson's Scintilla C++ editor component.

---
+++ qt5-qscintilla/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/09/25 17:46:26 nros Exp $
+
+BUILDLINK_TREE+=	qt5-qscintilla
+
+.if !defined(QT5_QSCINTILLA_BUILDLINK3_MK)
+QT5_QSCINTILLA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt5-qscintilla+=	qt5-qscintilla>=2.8.4
+BUILDLINK_ABI_DEPENDS.qt5-qscintilla+=	qt5-qscintilla>=2.8.4
+BUILDLINK_PKGSRCDIR.qt5-qscintilla?=	../../wip/qt5-qscintilla
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# QT5_QSCINTILLA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt5-qscintilla
