958734f369b33ca370ca02f7ae1e112c1d44b20d
Yakovetsky VS <yvs4sf@users.sourceforge.net>
2014-09-26 09:51:23 +0000

    Update to git:bb9046a

--- mtr-graph/options.mk
+++ mtr-graph/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2014/09/25 14:05:46 yvs4sf Exp $
+# $NetBSD: options.mk,v 1.2 2014/09/26 09:51:23 yvs4sf Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mtr-graph
 PKG_SUPPORTED_OPTIONS=	gtk
@@ -7,6 +7,7 @@ PKG_SUPPORTED_OPTIONS+=	ipinfo
 PKG_OPTIONS_OPTIONAL_GROUPS+=	graphcairo
 PKG_OPTIONS_GROUP.graphcairo+=	graphcairo-xcb
 PKG_OPTIONS_GROUP.graphcairo+=	graphcairo-xlib
+PKG_SUPPORTED_OPTIONS+=	libidn
 PKG_SUGGESTED_OPTIONS=	inet6
 PKG_SUGGESTED_OPTIONS+=	ipinfo
 PKG_SUGGESTED_OPTIONS+=	graphcairo-xcb
@@ -43,4 +44,9 @@ USE_TOOLS+=		pkg-config
 .include "../../x11/libX11/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-graphcairo-xlib
 .endif
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibidn)
+.include "../../devel/libidn/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-libidn
 .endif
