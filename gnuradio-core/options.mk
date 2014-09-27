19d3ed991e9c41df7451c03c147bd9103d98e279
Makoto Fujiwara <makoto@ki.nu>
2014-09-27 08:09:00 +0000

    Add option filter_design (and enabled as default), which asks for math/py-scipy.

--- gnuradio-core/options.mk
+++ gnuradio-core/options.mk
@@ -1,14 +1,14 @@
-# $NetBSD: options.mk,v 1.11 2014/09/21 12:30:23 makoto Exp $
+# $NetBSD: options.mk,v 1.12 2014/09/27 08:09:00 makoto Exp $
 
 PKG_OPTIONS_VAR=        PKG_OPTIONS.gnuradio
-PKG_SUPPORTED_OPTIONS=  ninja
+PKG_SUPPORTED_OPTIONS=  ninja filter-design
 
 # Adding ninja to following line (now) stops
 # [164/850] cd /PATH/wip/gnuradio-core/work/gnuradio-3.7.5/build/gr-audio/swig && ""
 # FAILED: cd /PATH/wip/gnuradio-core/work/gnuradio-3.7.5/build/gr-audio/swig && ""
 # : permission denied
 
-PKG_SUGGESTED_OPTIONS=
+PKG_SUGGESTED_OPTIONS= filter-design
 
 .include "../../mk/bsd.options.mk"
 
@@ -21,4 +21,9 @@ do-build:
 .else
 do-build:
 	(cd ${WRKSRC}/build; ${GMAKE})
+.endif
+
+.if !empty(PKG_OPTIONS:Mfilter-design)
+# Running gr_filter_design asks for the package
+DEPENDS+=		${PYPKGPREFIX}-scipy-[0-9]*:../../math/py-scipy
 .endif
