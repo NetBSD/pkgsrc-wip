406408aeda1a9e5832c276a565342ce7d79ebb46
Thomas Klausner <wiz@NetBSD.org>
2014-09-28 14:16:55 +0000

    Fix buildlink3.mk file.

--- nspr-hg/buildlink3.mk
+++ nspr-hg/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/09/28 14:14:58 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/09/28 14:16:55 thomasklausner Exp $
 
 BUILDLINK_TREE+=	nspr
 
 .if !defined(NSPR_BUILDLINK3_MK)
 NSPR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nspr+=	nspr>=20140901
+BUILDLINK_API_DEPENDS.nspr+=	nspr-hg>=2014.09.01
 BUILDLINK_PKGSRCDIR.nspr?=	../../wip/nspr-hg
 
 BUILDLINK_FILES.nspr+=          lib/nspr/*
