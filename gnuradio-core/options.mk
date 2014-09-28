cc94fd8238719687e0497b4a66f59d91de4f771f
Makoto Fujiwara <makoto@ki.nu>
2014-09-28 14:50:45 +0000

    Rename option, ninja -> ninja-build.

--- gnuradio-core/options.mk
+++ gnuradio-core/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.12 2014/09/27 08:09:00 makoto Exp $
+# $NetBSD: options.mk,v 1.13 2014/09/28 14:50:45 makoto Exp $
 
 PKG_OPTIONS_VAR=        PKG_OPTIONS.gnuradio
-PKG_SUPPORTED_OPTIONS=  ninja filter-design
+PKG_SUPPORTED_OPTIONS=  ninja-build filter-design
 
-# Adding ninja to following line (now) stops
+# Adding ninja-build to following line (now) stops
 # [164/850] cd /PATH/wip/gnuradio-core/work/gnuradio-3.7.5/build/gr-audio/swig && ""
 # FAILED: cd /PATH/wip/gnuradio-core/work/gnuradio-3.7.5/build/gr-audio/swig && ""
 # : permission denied
@@ -12,7 +12,7 @@ PKG_SUGGESTED_OPTIONS= filter-design
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mninja)
+.if !empty(PKG_OPTIONS:Mninja-build)
 CMAKE_ARGS+=	-GNinja
 BUILD_DEPENDS+=	ninja-build-[0-9]*:../../devel/ninja-build
 
