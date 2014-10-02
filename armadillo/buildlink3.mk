79599e05218891aa9c9e9c8c6a3e5cd5b804dcd8
Wen Heping <wen@NetBSD.org>
2014-10-02 15:31:38 +0000

    Update to 4.45.2

--- armadillo/buildlink3.mk
+++ armadillo/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2013/03/22 13:57:12 outpaddling Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2014/10/02 15:31:39 uccwen Exp $
 
 BUILDLINK_TREE+=	armadillo
 
 .if !defined(ARMADILLO_BUILDLINK3_MK)
 ARMADILLO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.armadillo+=	armadillo>=3.80.1
-BUILDLINK_ABI_DEPENDS.armadillo?=	armadillo>=3.80.1
+BUILDLINK_API_DEPENDS.armadillo+=	armadillo>=4.45.2
+BUILDLINK_ABI_DEPENDS.armadillo?=	armadillo>=4.45.2
 BUILDLINK_PKGSRCDIR.armadillo?=	../../wip/armadillo
 
 .include "../../math/blas/buildlink3.mk"
