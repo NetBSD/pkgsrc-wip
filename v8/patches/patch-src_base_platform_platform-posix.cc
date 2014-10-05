91b0b609fdc5bee66bb74eb0ffb3997fb7c7b2a6
Havard Eidnes <he@NetBSD.org>
2014-10-05 11:12:29 +0000

    Add a patch segment to force the use of the IEEE 754 variant of the math library on NetBSD.

--- v8/patches/patch-src_base_platform_platform-posix.cc
+++ v8/patches/patch-src_base_platform_platform-posix.cc
@@ -1,11 +1,22 @@
-$NetBSD: patch-src_base_platform_platform-posix.cc,v 1.1 2014/08/23 17:44:12 heidnes Exp $
+$NetBSD: patch-src_base_platform_platform-posix.cc,v 1.2 2014/10/05 11:12:29 heidnes Exp $
 
 Use pthread_self() instead of non-portable SYS_gettid.
 Provide a default for PTHREAD_STACK_MIN if it's missing (NetBSD...)
+Also force IEEE 754 version of libm.
 
---- src/base/platform/platform-posix.cc.orig	2014-08-08 13:04:31.000000000 +0000
+--- src/base/platform/platform-posix.cc.orig	2014-08-21 07:04:57.000000000 +0000
 +++ src/base/platform/platform-posix.cc
-@@ -324,7 +324,7 @@ int OS::GetCurrentThreadId() {
+@@ -210,6 +210,9 @@ void OS::Initialize(int64_t random_seed,
+   }
+   g_hard_abort = hard_abort;
+   g_gc_fake_mmap = gc_fake_mmap;
++#ifdef __NetBSD__
++  _LIB_VERSION = _IEEE_;	/* Force IEEE 754 version of libm */
++#endif
+ }
+ 
+ 
+@@ -324,7 +327,7 @@ int OS::GetCurrentThreadId() {
  #if defined(ANDROID)
    return static_cast<int>(syscall(__NR_gettid));
  #else
@@ -14,7 +25,7 @@ Provide a default for PTHREAD_STACK_MIN if it's missing (NetBSD...)
  #endif  // defined(ANDROID)
  }
  
-@@ -511,6 +511,10 @@ class Thread::PlatformData {
+@@ -511,6 +514,10 @@ class Thread::PlatformData {
    Mutex thread_creation_mutex_;
  };
  
@@ -25,7 +36,7 @@ Provide a default for PTHREAD_STACK_MIN if it's missing (NetBSD...)
  Thread::Thread(const Options& options)
      : data_(new PlatformData),
        stack_size_(options.stack_size()),
-@@ -532,7 +536,7 @@ static void SetThreadName(const char* na
+@@ -532,7 +539,7 @@ static void SetThreadName(const char* na
    pthread_set_name_np(pthread_self(), name);
  #elif V8_OS_NETBSD
    STATIC_ASSERT(Thread::kMaxThreadNameLength <= PTHREAD_MAX_NAMELEN_NP);
