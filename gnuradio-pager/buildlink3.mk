8c30d828827da3c1866c88b8f99aece71baeadc1
Makoto Fujiwara <makoto@ki.nu>
2014-10-05 12:47:43 +0000

    (DESCR)   - Cleanup. Correction. Simplify (Makefile)   - Cleanup. Correction. Simplify of COMMENT= line (gnuradio-pager/buildlink3.mk)   - Correction (core -> pager) (gnuradio-default/Makefile)   - Use Following line instead of ${INSTALL_DATA_DIR} command     +INSTALLATION_DIRS+= ${EGDIR}

--- gnuradio-pager/buildlink3.mk
+++ gnuradio-pager/buildlink3.mk
@@ -1,23 +1,23 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/09/29 15:25:54 makoto Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/10/05 12:47:44 makoto Exp $
 
-BUILDLINK_TREE+=	gnuradio-core
+BUILDLINK_TREE+=	gnuradio-pager
 
-.if !defined(GNURADIO_CORE_BUILDLINK3_MK)
-GNURADIO_CORE_BUILDLINK3_MK:=
+.if !defined(GNURADIO_PAGER_BUILDLINK3_MK)
+GNURADIO_PAGER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-core+=	gnuradio-core>=3.0.4
-BUILDLINK_ABI_DEPENDS.gnuradio-core+=	gnuradio-core>=3.3.0nb10
-BUILDLINK_PKGSRCDIR.gnuradio-core?=	../../wip/gnuradio-core
+BUILDLINK_API_DEPENDS.gnuradio-pager+=	gnuradio-pager>=3.7.5
+BUILDLINK_ABI_DEPENDS.gnuradio-pager+=	gnuradio-pager>=3.7.5
+BUILDLINK_PKGSRCDIR.gnuradio-pager?=	../../wip/gnuradio-pager
 
-.include "../../devel/boost-headers/buildlink3.mk"
-.include "../../devel/boost-libs/buildlink3.mk"
-.include "../../devel/cppunit/buildlink3.mk"
-.include "../../devel/swig2/buildlink3.mk"
-.include "../../lang/python/application.mk"
-.include "../../lang/python/extension.mk"
-.include "../../lang/python/pyversion.mk"
-.include "../../math/fftwf/buildlink3.mk"
-.include "../../math/gsl/buildlink3.mk"
-.endif # GNURADIO_CORE_BUILDLINK3_MK
+#.include "../../devel/boost-headers/buildlink3.mk"
+#.include "../../devel/boost-libs/buildlink3.mk"
+#.include "../../devel/cppunit/buildlink3.mk"
+#.include "../../devel/swig2/buildlink3.mk"
+#.include "../../lang/python/application.mk"
+#.include "../../lang/python/extension.mk"
+#.include "../../lang/python/pyversion.mk"
+#.include "../../math/fftwf/buildlink3.mk"
+#.include "../../math/gsl/buildlink3.mk"
+.endif # GNURADIO_PAGER_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-gnuradio-core
+BUILDLINK_TREE+=	-gnuradio-pager
