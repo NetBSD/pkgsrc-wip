538efb719ee3a14bbef31bcb19f6cb644075695f
Makoto Fujiwara <makoto@ki.nu>
2014-10-07 11:45:09 +0000

    Correction to API/ABI_DEPENDS+ >= 3.7.5

--- gnuradio-core/buildlink3.mk
+++ gnuradio-core/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.4 2014/09/29 15:25:54 makoto Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2014/10/07 11:45:09 makoto Exp $
 
 BUILDLINK_TREE+=	gnuradio-core
 
 .if !defined(GNURADIO_CORE_BUILDLINK3_MK)
 GNURADIO_CORE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-core+=	gnuradio-core>=3.0.4
-BUILDLINK_ABI_DEPENDS.gnuradio-core+=	gnuradio-core>=3.3.0nb10
+BUILDLINK_API_DEPENDS.gnuradio-core+=	gnuradio-core>=3.7.5
+BUILDLINK_ABI_DEPENDS.gnuradio-core+=	gnuradio-core>=3.7.5
 BUILDLINK_PKGSRCDIR.gnuradio-core?=	../../wip/gnuradio-core
 .endif # GNURADIO_CORE_BUILDLINK3_MK
 
