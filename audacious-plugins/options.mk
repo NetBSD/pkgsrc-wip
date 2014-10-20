b47b5e613181e7984a3a938c38addbb207bad743
Makoto Fujiwara <makoto@ki.nu>
2014-10-20 13:58:24 +0000

    Instead of updating audio/audacious-plugin 3.5.1 to 3.5.2, update this wip/audacious-plugin to 3.5.2. The release note for 3.5.1 to 3.5.2 will be the same as audio/audacious 3.5.1 to 3.5.2,   ----------------------------------   (upstream) Update 3.5.1 to 3.5.2     The final release in the 3.5 series contains updated translations     and fixes for a few more bugs, including #457, #458, and     #462. Meanwhile, we are working hard on Audacious 3.6.

--- audacious-plugins/options.mk
+++ audacious-plugins/options.mk
@@ -1,14 +1,59 @@
-# $NetBSD: options.mk,v 1.5 2014/04/18 16:01:58 thomasklausner Exp $
+# $NetBSD: options.mk,v 1.6 2014/10/20 13:58:24 makoto Exp $
 
-PKG_OPTIONS_VAR=        PKG_OPTIONS.audacious-plugins
-PKG_SUPPORTED_OPTIONS=  gtk3
-PKG_SUGGESTED_OPTIONS=
+PKG_OPTIONS_VAR=	PKG_OPTIONS.audacious-plugins
+PKG_SUPPORTED_OPTIONS=	arts esound inet6 jack midi pulseaudio sid
+PKG_SUGGESTED_OPTIONS+=	inet6 pulseaudio
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mgtk3)
-CONFIGURE_ARGS+=        --enable-gtk3
-.include "../../x11/gtk3/buildlink3.mk"
+PLIST_VARS+=	sid
+.if !empty(PKG_OPTIONS:Msid)
+.  include "../../audio/libsidplay/buildlink3.mk"
+PLIST.sid=	yes
 .else
-CONFIGURE_ARGS+=        --disable-gtk3
+CONFIGURE_ARGS+=	--disable-sid
+.endif
+
+PLIST_VARS+=	arts
+.if !empty(PKG_OPTIONS:Marts)
+.  include "../../audio/arts/buildlink3.mk"
+PLIST.arts=	yes
+.else
+CONFIGURE_ARGS+=	--disable-arts
+.endif
+
+PLIST_VARS+=	esd
+.if !empty(PKG_OPTIONS:Mesound)
+.  include "../../audio/esound/buildlink3.mk"
+PLIST.esd=	yes
+.else
+CONFIGURE_ARGS+=	--disable-esd
+.endif
+
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--enable-ipv6
+.endif
+
+PLIST_VARS+=	jack
+.if !empty(PKG_OPTIONS:Mjack)
+.  include "../../audio/jack/buildlink3.mk"
+PLIST.jack=	yes
+.else
+CONFIGURE_ARGS+=	--disable-jack
+.endif
+
+PLIST_VARS+=	midi
+.if !empty(PKG_OPTIONS:Mmidi)
+BUILD_DEPENDS+=		timidity>=0:../../audio/timidity
+PLIST.midi=	yes
+.else
+CONFIGURE_ARGS+=	--disable-timidity
+.endif
+
+PLIST_VARS+=	pulse
+.if !empty(PKG_OPTIONS:Mpulseaudio)
+.  include "../../audio/pulseaudio/buildlink3.mk"
+PLIST.pulse=	yes
+.else
+CONFIGURE_ARGS+=	--disable-pulse
 .endif
