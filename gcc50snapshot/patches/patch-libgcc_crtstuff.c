6e4d6b6fc6fa9ed71b9d31d200f19dcb993cbfd1
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2014-10-22 04:38:44 +0000

    Fix wrongly placed bracket in patch-libgcc_crtstuff.c

--- gcc50snapshot/patches/patch-libgcc_crtstuff.c
+++ gcc50snapshot/patches/patch-libgcc_crtstuff.c
@@ -1,13 +1,13 @@
-$NetBSD: patch-libgcc_crtstuff.c,v 1.1 2014/10/19 10:50:57 keckhardt Exp $
+$NetBSD: patch-libgcc_crtstuff.c,v 1.2 2014/10/22 04:38:44 keckhardt Exp $
 
---- libgcc/crtstuff.c.orig	2014-05-21 11:08:58.000000000 +0000
+--- libgcc/crtstuff.c.orig	2014-09-22 00:49:01.000000000 +0000
 +++ libgcc/crtstuff.c
-@@ -80,7 +80,8 @@ call_ ## FUNC (void)					\
+@@ -81,7 +81,8 @@ call_ ## FUNC (void)					\
  #endif
  
  #if defined(TARGET_DL_ITERATE_PHDR) && \
 -   (defined(__DragonFly__) || defined(__FreeBSD__))
-+   (defined(__DragonFly__) || defined(__FreeBSD__)) \
++   (defined(__DragonFly__) || defined(__FreeBSD__) || \
 +   (defined(__NetBSD__) || defined(__OpenBSD__))
  #define BSD_DL_ITERATE_PHDR_AVAILABLE
  #endif
