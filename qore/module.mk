fe7f2382d7061220b12300dddcd508910406bbe2
Niclas Rosenvik <nros@NetBSD.org>
2014-10-25 13:18:36 +0000

    Removed patch-Makefile.in isr cp -r in Makefile instead. Added new PLIST var to module.mk. Bump pkgrevision.

--- qore/module.mk
+++ qore/module.mk
@@ -1,4 +1,4 @@
-# $NetBSD: module.mk,v 1.3 2014/06/02 16:35:08 nros Exp $
+# $NetBSD: module.mk,v 1.4 2014/10/25 13:18:36 nros Exp $
 #
 # This file should be included in the makefile of packages that install
 # Qore modules.
@@ -6,11 +6,15 @@
 # PLIST variables:
 #
 # QORE_VERSION 		 To be used by packages that install files in
-# 			 lib/qore-modules/${QORE_VERSION}/
+# 			 ${QORE_MODULE_DIR}/${QORE_VERSION}/
 #
 # QORE_MODULE_API	 This should be used by packages that install
 # 			 modules that uses the compatible module api
 #			 version.
+#
+# QORE_MODULE_DIR	 The directory where binary modules are 
+#			 installed relative to ${PREFIX}.
+#
 #
 # QORE_LATEST_MODULE_API This should be used by packages that install
 #			 modules that uses the latest module api
@@ -21,7 +25,9 @@
 qore-module: post-install
 qore-version=${PKG_INFO} -E qore | ${SED} -e s/qore-// -e s/nb.//
 qore-module-api=qore --module-api
+qore-module-dir=qore --module-dir
 qore-latest-module-api=qore --latest-module-api
 PLIST_SUBST+=QORE_VERSION="${qore-version:sh}"
 PLIST_SUBST+=QORE_MODULE_API="${qore-module-api:sh}"
+PLIST_SUBST+=QORE_MODULE_DIR="${qore-module-dir:sh:C,^${PREFIX}/,,W}"
 PLIST_SUBST+=QORE_LATEST_MODULE_API="${qore-latest-module-api:sh}"
