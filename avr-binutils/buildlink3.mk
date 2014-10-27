adbfd4e29c426bf1244f73c7db872c4e7694104a
Makoto Fujiwara <makoto@ki.nu>
2014-10-27 08:56:17 +0000

    Sync with cross/avr-binutils, and add patch for ksh issue

--- avr-binutils/buildlink3.mk
+++ avr-binutils/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.3 2013/07/07 04:36:16 makoto Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2014/10/27 08:56:17 makoto Exp $
 
 BUILDLINK_TREE+=	avr-binutils
 
 .if !defined(AVR_BINUTILS_BUILDLINK3_MK)
 AVR_BINUTILS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.avr-binutils+=	avr-binutils>=2.22
-BUILDLINK_PKGSRCDIR.avr-binutils?=		../../wip/avr-binutils
+BUILDLINK_API_DEPENDS.avr-binutils+=	avr-binutils>=2.23
+BUILDLINK_PKGSRCDIR.avr-binutils?=		../../cross/avr-binutils
 #BUILDLINK_FNAME_TRANSFORM.avr-binutils+=	-e "s,${AVR_TARGET}/lib,lib/,g"
 
 .endif # AVR_BINUTILS_BUILDLINK3_MK
