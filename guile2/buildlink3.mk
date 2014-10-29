e0f00b8e0cbcd729f1f5c9cd15417c7202bd710a
Thomas Klausner <wiz@NetBSD.org>
2014-10-29 23:23:07 +0000

    dd buildlink3.mk file.

---
+++ guile2/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.1 2014/10/29 23:23:07 thomasklausner Exp $
+
+BUILDLINK_TREE+=	guile2
+
+.if !defined(GUILE2_BUILDLINK3_MK)
+GUILE2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.guile2+=	guile>=2.0
+BUILDLINK_PKGSRCDIR.guile2?=	../../wip/guile2
+
+.include "../../devel/gmp/buildlink3.mk"
+.include "../../devel/libltdl/buildlink3.mk"
+.include "../../devel/readline/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../textproc/libunistring/buildlink3.mk"
+.include "../../devel/boehm-gc/buildlink3.mk"
+.endif # GUILE2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-guile2
