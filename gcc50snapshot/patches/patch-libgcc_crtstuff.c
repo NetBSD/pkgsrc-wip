0da0de8727ff60c2ea8892b16bc8e0bc97849f8f
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2014-11-04 09:09:28 +0000

    Add debian problem to TODO.

--- gcc50snapshot/patches/patch-libgcc_crtstuff.c
+++ gcc50snapshot/patches/patch-libgcc_crtstuff.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-libgcc_crtstuff.c,v 1.2 2014/10/22 04:38:44 keckhardt Exp $
+$NetBSD: patch-libgcc_crtstuff.c,v 1.3 2014/11/04 09:09:28 keckhardt Exp $
 
 --- libgcc/crtstuff.c.orig	2014-09-22 00:49:01.000000000 +0000
 +++ libgcc/crtstuff.c
@@ -8,7 +8,7 @@ $NetBSD: patch-libgcc_crtstuff.c,v 1.2 2014/10/22 04:38:44 keckhardt Exp $
  #if defined(TARGET_DL_ITERATE_PHDR) && \
 -   (defined(__DragonFly__) || defined(__FreeBSD__))
 +   (defined(__DragonFly__) || defined(__FreeBSD__) || \
-+   (defined(__NetBSD__) || defined(__OpenBSD__))
++   defined(__NetBSD__) || defined(__OpenBSD__))
  #define BSD_DL_ITERATE_PHDR_AVAILABLE
  #endif
   
