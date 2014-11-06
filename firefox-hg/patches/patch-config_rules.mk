4b265b51025cf89e576c311b0fba7ded8b43757a
Thomas Klausner <wiz@NetBSD.org>
2014-11-06 15:47:24 +0000

    dtrace patches were integrated. Adapt PLIST for recent changes.

--- firefox-hg/patches/patch-config_rules.mk
+++ firefox-hg/patches/patch-config_rules.mk
@@ -1,8 +1,8 @@
-$NetBSD: patch-config_rules.mk,v 1.2 2014/10/30 13:54:23 thomasklausner Exp $
+$NetBSD: patch-config_rules.mk,v 1.3 2014/11/06 15:47:24 thomasklausner Exp $
 
---- config/rules.mk.orig	2014-10-30 12:50:07.000000000 +0000
+--- config/rules.mk.orig	2014-11-06 09:56:12.000000000 +0000
 +++ config/rules.mk
-@@ -778,6 +778,7 @@ endif
+@@ -770,6 +770,7 @@ endif
  ifdef DTRACE_PROBE_OBJ
  EXTRA_DEPS += $(DTRACE_PROBE_OBJ)
  OBJS += $(DTRACE_PROBE_OBJ)
@@ -10,21 +10,3 @@ $NetBSD: patch-config_rules.mk,v 1.2 2014/10/30 13:54:23 thomasklausner Exp $
  endif
  
  $(filter %.$(LIB_SUFFIX),$(LIBRARY)): $(OBJS) $(STATIC_LIBS_DEPS) $(filter %.$(LIB_SUFFIX),$(EXTRA_LIBS)) $(EXTRA_DEPS) $(GLOBAL_DEPS)
-@@ -814,7 +815,7 @@ ifdef DTRACE_PROBE_OBJ
- ifndef DTRACE_LIB_DEPENDENT
- NON_DTRACE_OBJS := $(filter-out $(DTRACE_PROBE_OBJ),$(OBJS))
- $(DTRACE_PROBE_OBJ): $(NON_DTRACE_OBJS)
--	dtrace -G -C -s $(MOZILLA_DTRACE_SRC) -o $(DTRACE_PROBE_OBJ) $(NON_DTRACE_OBJS)
-+	dtrace -x nolibs -G -C -s $(MOZILLA_DTRACE_SRC) -o $(DTRACE_PROBE_OBJ) $(NON_DTRACE_OBJS)
- endif
- endif
- endif
-@@ -832,7 +833,7 @@ ifndef INCREMENTAL_LINKER
- endif
- ifdef DTRACE_LIB_DEPENDENT
- ifndef XP_MACOSX
--	dtrace -G -C -s $(MOZILLA_DTRACE_SRC) -o  $(DTRACE_PROBE_OBJ) $(shell $(EXPAND_LIBS) $(MOZILLA_PROBE_LIBS))
-+	dtrace -x nolibs -G -C -s $(MOZILLA_DTRACE_SRC) -o  $(DTRACE_PROBE_OBJ) $(shell $(EXPAND_LIBS) $(MOZILLA_PROBE_LIBS))
- endif
- 	$(EXPAND_MKSHLIB) $(SHLIB_LDSTARTFILE) $(OBJS) $(SUB_SHLOBJS) $(DTRACE_PROBE_OBJ) $(MOZILLA_PROBE_LIBS) $(RESFILE) $(LDFLAGS) $(WRAP_LDFLAGS) $(STATIC_LIBS) $(SHARED_LIBS) $(EXTRA_DSO_LDOPTS) $(MOZ_GLUE_LDFLAGS) $(EXTRA_LIBS) $(OS_LIBS) $(SHLIB_LDENDFILE)
- 	@$(RM) $(DTRACE_PROBE_OBJ)
