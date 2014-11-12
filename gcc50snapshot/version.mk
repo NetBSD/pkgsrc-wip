08c596bd11ce6e274fbf0ac8de595632584df9d8
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2014-11-12 15:40:56 +0000

    Snapshot 20141109. Add new patch in libgo. Test result from NetBSD 7.

--- gcc50snapshot/version.mk
+++ gcc50snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.2 2014/10/29 06:31:39 keckhardt Exp $
-GCC50S_DIST_VERSION:=5-20141026
+# $NetBSD: version.mk,v 1.3 2014/11/12 15:40:56 keckhardt Exp $
+GCC50S_DIST_VERSION:=5-20141109
