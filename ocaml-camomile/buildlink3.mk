542245f09c324a56f190ed277968b9e6dc4610d6
Hiramatsu Yoshifumi <jgoamakf@users.sourceforge.net>
2014-11-21 06:14:26 +0000

    Use mk/ocaml.mk.

--- ocaml-camomile/buildlink3.mk
+++ ocaml-camomile/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/10/23 07:43:40 jgoamakf Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/11/21 06:14:26 jgoamakf Exp $
 
 BUILDLINK_TREE+=	ocaml-camomile
 
@@ -7,9 +7,6 @@ OCAML_CAMOMILE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-camomile+=	ocaml-camomile>=0.8.4
 BUILDLINK_PKGSRCDIR.ocaml-camomile?=	../../wip/ocaml-camomile
-
-.include "../../lang/ocaml/buildlink3.mk"
-
 .endif	# OCAML_CAMOMILE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ocaml-camomile
