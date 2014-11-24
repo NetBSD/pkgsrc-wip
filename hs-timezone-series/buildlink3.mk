68c58d833714f68be42f0fd74e4fe9ca05e50a93
Ryosuke Moro <szptvlfn@NetBSD.org>
2014-11-24 13:59:47 +0000

    Update to 0.1.3
    changes (does not provide):
    simplify

--- hs-timezone-series/buildlink3.mk
+++ hs-timezone-series/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/08/29 14:10:29 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2014/11/24 13:59:47 szptvlfn Exp $
 
 BUILDLINK_TREE+=	hs-timezone-series
 
 .if !defined(HS_TIMEZONE_SERIES_BUILDLINK3_MK)
 HS_TIMEZONE_SERIES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.hs-timezone-series+=	hs-timezone-series>=0.1.2
-BUILDLINK_ABI_DEPENDS.hs-timezone-series+=	hs-timezone-series>=0.1.2
+BUILDLINK_API_DEPENDS.hs-timezone-series+=	hs-timezone-series>=0.1.3
+BUILDLINK_ABI_DEPENDS.hs-timezone-series+=	hs-timezone-series>=0.1.3
 BUILDLINK_PKGSRCDIR.hs-timezone-series?=	../../wip/hs-timezone-series
 .endif	# HS_TIMEZONE_SERIES_BUILDLINK3_MK
 
