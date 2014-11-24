a75710f4a49fe5252a4b89a30ad912e80b75035d
Makoto Fujiwara <makoto@ki.nu>
2014-11-24 22:40:15 +0000

    Update 5.0 to 5.99.1

--- pulseaudio/patches/patch-src_daemon_main.c
+++ pulseaudio/patches/patch-src_daemon_main.c
@@ -1,18 +1,18 @@
-$NetBSD: patch-src_daemon_main.c,v 1.2 2014/05/06 11:47:25 thomasklausner Exp $
+$NetBSD: patch-src_daemon_main.c,v 1.3 2014/11/24 22:40:15 makoto Exp $
 
 Only display Linux-specific warning on Linux.
 
---- src/daemon/main.c.orig	2014-02-25 14:01:58.000000000 +0000
-+++ src/daemon/main.c
-@@ -1002,10 +1002,12 @@ int main(int argc, char *argv[]) {
+--- src/daemon/main.c~	2014-11-25 01:06:34.000000000 +0900
++++ src/daemon/main.c	2014-11-25 01:07:27.000000000 +0900
+@@ -999,10 +999,12 @@ int main(int argc, char *argv[]) {
  
      pa_disable_sigpipe();
  
 +#if defined(__linux__)
      if (pa_rtclock_hrtimer())
-         pa_log_info(_("Fresh high-resolution timers available! Bon appetit!"));
+         pa_log_info("Fresh high-resolution timers available! Bon appetit!");
      else
-         pa_log_info(_("Dude, your kernel stinks! The chef's recommendation today is Linux with high-resolution timers enabled!"));
+         pa_log_info("Dude, your kernel stinks! The chef's recommendation today is Linux with high-resolution timers enabled!");
 +#endif
  
      if (conf->lock_memory) {
