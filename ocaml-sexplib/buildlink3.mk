76d1c011e20d5a1f0287d977403583abc61ddb46
Hiramatsu Yoshifumi <jgoamakf@users.sourceforge.net>
2014-11-25 09:41:54 +0000

    Remove unused lines.

--- ocaml-sexplib/buildlink3.mk
+++ ocaml-sexplib/buildlink3.mk
@@ -5,9 +5,6 @@ OCAML_SEXPLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-sexplib+=	ocaml-sexplib>=112.01.00
 BUILDLINK_PKGSRCDIR.ocaml-sexplib?=	../../wip/ocaml-sexplib
-
-.include "../../devel/ocaml-type_conv/buildlink3.mk"
-.include "../../lang/ocaml/buildlink3.mk"
 .endif	# OCAML_SEXPLIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ocaml-sexplib
