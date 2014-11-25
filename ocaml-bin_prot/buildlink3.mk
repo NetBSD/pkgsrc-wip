a214f0ad1efcd3cf413181988e0631b87f4e4537
Hiramatsu Yoshifumi <jgoamakf@users.sourceforge.net>
2014-11-25 07:51:13 +0000

    Remove unnecessary .include.

--- ocaml-bin_prot/buildlink3.mk
+++ ocaml-bin_prot/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/11/16 22:46:18 jgoamakf Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/11/25 07:51:13 jgoamakf Exp $
 BUILDLINK_TREE+=	ocaml-bin_prot
 
 .if !defined(OCAML_BIN_PROT_BUILDLINK3_MK)
@@ -6,10 +6,6 @@ OCAML_BIN_PROT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-bin_prot+=	ocaml-bin_prot>=112.01.00
 BUILDLINK_PKGSRCDIR.ocaml-bin_prot?=	../../wip/ocaml-bin_prot
-
-.include "../../devel/ocaml-type_conv/buildlink3.mk"
-.include "../../lang/camlp4/buildlink3.mk"
-.include "../../lang/ocaml/buildlink3.mk"
 .endif	# OCAML_BIN_PROT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ocaml-bin_prot
--- ocaml-comparelib/buildlink3.mk
+++ ocaml-comparelib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/11/16 22:48:46 jgoamakf Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2014/11/25 08:12:05 jgoamakf Exp $
 BUILDLINK_TREE+=	ocaml-comparelib
 
 .if !defined(OCAML_COMPARELIB_BUILDLINK3_MK)
@@ -6,9 +6,6 @@ OCAML_COMPARELIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-comparelib+=	ocaml-comparelib>=109.60.00
 BUILDLINK_PKGSRCDIR.ocaml-comparelib?=	../../wip/ocaml-comparelib
-
-.include "../../devel/ocaml-type_conv/buildlink3.mk"
-.include "../../lang/ocaml/buildlink3.mk"
 .endif	# OCAML_COMPARELIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ocaml-comparelib
