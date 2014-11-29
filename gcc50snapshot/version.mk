ded397cedb7ef7a47391cbaba2edee1adf24468b
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2014-11-29 23:40:17 +0000

    Snapshot 20141123. Add patch for detection of locale support in gfortran.

--- gcc50snapshot/version.mk
+++ gcc50snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.3 2014/11/12 15:40:56 keckhardt Exp $
-GCC50S_DIST_VERSION:=5-20141109
+# $NetBSD: version.mk,v 1.4 2014/11/29 23:40:17 keckhardt Exp $
+GCC50S_DIST_VERSION:=5-20141123
