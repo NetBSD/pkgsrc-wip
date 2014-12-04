9dfd18df0dd80187f61ae4861ae1609993cf1eff
Kamil Rytarowski <kamil@NetBSD.org>
2014-12-04 01:07:08 +0000

    OpenSSL is disabled by default in ./configure
    There is no proper support for --without-(open)ssl in ./configure

--- radare2/options.mk
+++ radare2/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2014/12/03 22:32:17 krytarowski Exp $
+# $NetBSD: options.mk,v 1.2 2014/12/04 01:07:08 krytarowski Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.radare2
 PKG_SUPPORTED_OPTIONS=		ssl
@@ -10,6 +10,4 @@ PKG_SUGGESTED_OPTIONS=		ssl
 .if !empty(PKG_OPTIONS:Mssl)
 .  include "../../security/openssl/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-openssl
-.else
-CONFIGURE_ARGS+=	--without-ssl
 .endif
