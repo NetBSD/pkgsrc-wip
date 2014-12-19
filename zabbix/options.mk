7be9ba4d9cf882ec081d7a8f821332d6c77e352a
Havard Eidnes <he@NetBSD.org>
2014-12-19 13:57:28 +0000

    Commit my work-in-progress for zabbix.
    Update to version 2.4.2.
    
    Split out zabbix-agent and zabbix-server as separate packages;
    the main zabbix dir doesn't create a package itself.
    
    The zabbix-agent has been tested and appears to work fine.
    
    Zabbix-server by default compiles with sqlite3 support which
    you probably don't want to use.  Separate packages for the
    proxy agent, the frontend, and possibly separate server packages
    depending on database choice remains to be done.

--- zabbix/options.mk
+++ zabbix/options.mk
@@ -1,6 +1,5 @@
-# $NetBSD: options.mk,v 1.6 2008/03/02 13:00:13 hoka_adam Exp $
+# $NetBSD: options.mk,v 1.7 2014/12/19 13:57:28 heidnes Exp $
 PKG_OPTIONS_VAR=		PKG_OPTIONS.zabbix
-PKG_SUPPORTED_OPTIONS+=		zabbix-server zabbix-agent
 PKG_SUPPORTED_OPTIONS+=		pgsql sqlite mysql
 PKG_SUPPORTED_OPTIONS+=		curl
 PKG_SUPPORTED_OPTIONS+=		ldap
@@ -9,17 +8,6 @@ PKG_SUPPORTED_OPTIONS+=		zabbix-static
 
 .include "../../mk/bsd.prefs.mk"
 .include "../../mk/bsd.options.mk"
-
-# Default to both, server and agent.
-.if empty(PKG_OPTIONS:Mzabbix-server) && empty(PKG_OPTIONS:Mzabbix-agent)
-PKG_OPTIONS+=	zabbix-server zabbix-agent
-.endif
-.if !empty(PKG_OPTIONS:Mzabbix-server)
-CONFIGURE_ARGS+=	--enable-server
-.endif
-.if !empty(PKG_OPTIONS:Mzabbix-agent)
-CONFIGURE_ARGS+=	--enable-agent
-.endif
 
 # DB support is mandatory, default to SQLite
 .if empty(PKG_OPTIONS:Mpgsql) && empty(PKG_OPTIONS:Msqlite) && empty(PKG_OPTIONS:Mmysql)
