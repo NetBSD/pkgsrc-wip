ec89471d3ab16da83e8b56662d056df2dd7e87a4
Niclas Rosenvik <nros@NetBSD.org>
2014-12-28 15:57:09 +0000

    Added buildink3.mk references for more plugins in Makefile. Added patches from editors/abiword. Added patches for wordperfect and wpg plugins to that they build with wpd-0.10 and librevenge.

---
+++ abiword/patches/patch-src_af_util_xp_ut__iconv.h
@@ -0,0 +1,19 @@
+$NetBSD: patch-src_af_util_xp_ut__iconv.h,v 1.1 2014/12/28 15:57:09 nros Exp $
+
+--- src/af/util/xp/ut_iconv.h.orig	2013-04-07 13:53:03.000000000 +0000
++++ src/af/util/xp/ut_iconv.h
+@@ -30,8 +30,14 @@
+ #include "ut_types.h"
+ #endif
+ 
++#include <sys/param.h>
++
+ typedef void * UT_iconv_t;
+ 
++#if defined(__NetBSD__) && __NetBSD_Version__ >= 200000000
++struct __tag_iconv_t { }; /* XXX: for catch/throw (ISO C++ 15.1)*/
++#endif
++
+ #define UT_ICONV_INVALID ((UT_iconv_t)(-1))
+ 
+ #ifdef __cplusplus
---
+++ abiword/patches/patch-src_af_util_xp_ut__misc.cpp
@@ -0,0 +1,22 @@
+$NetBSD: patch-src_af_util_xp_ut__misc.cpp,v 1.1 2014/12/28 15:57:09 nros Exp $
+
+--- src/af/util/xp/ut_misc.cpp.orig	2013-04-07 13:53:03.000000000 +0000
++++ src/af/util/xp/ut_misc.cpp
+@@ -589,7 +589,7 @@ const gchar ** UT_splitPropsToArray(gcha
+ 		}
+ 
+ 		char * semi = NULL;
+-		const char * p = pProps;
++		char * p = pProps;
+  		while((semi = (char *) strchr(p, ';')))
+ 		{
+ 			*semi = 0;
+@@ -603,7 +603,7 @@ const gchar ** UT_splitPropsToArray(gcha
+ 		const gchar ** pPropsArray = new const gchar *[2 * iPropCount + 1];
+ 		UT_return_val_if_fail( pPropsArray, NULL );
+ 	
+-		const char * pStart = pProps;
++		char * pStart = pProps;
+ 
+ 		// we want to include the 0-terminator
+ 		for(i = 0; i <= iLen; i++)
