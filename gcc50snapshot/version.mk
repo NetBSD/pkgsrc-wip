8ab2a982598c409c158c1f7d06fa9b158b94ce96
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2014-12-29 22:58:45 +0000

    Snapshot 20141228.

--- gcc50snapshot/version.mk
+++ gcc50snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.8 2014/12/23 06:56:50 keckhardt Exp $
-GCC50S_DIST_VERSION:=5-20141221
+# $NetBSD: version.mk,v 1.9 2014/12/29 22:58:45 keckhardt Exp $
+GCC50S_DIST_VERSION:=5-20141228
