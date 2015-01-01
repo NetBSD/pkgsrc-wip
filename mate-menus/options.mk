4c05cf8440a0dd388a23ac9c6a9dc9b8154cb320
OBATA Akio <obache@users.sourceforge.net>
2015-01-01 08:17:39 +0000

    Import mate-menus-1.8.0 as wip/mate-menus.
    mate-menus contains the libmate-menu library, the layout configuration
    files for the MATE menu, as well as a simple menu editor.
    
    The libmate-menu library implements the "Desktop Menu Specification"
    from freedesktop.org.
    
    mate-menus is a fork of gnome-menus.

---
+++ mate-menus/options.mk
@@ -0,0 +1,27 @@
+# $NetBSD: options.mk,v 1.1 2015/01/01 08:17:39 obache Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.mate-menus
+PKG_SUPPORTED_OPTIONS=	introspection python
+PKG_SUGGESTED_OPTIONS=	introspection python
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	introspection python
+
+.if !empty(PKG_OPTIONS:Mpython)
+PY_PATCHPLIST=		yes
+.include "../../lang/python/extension.mk"
+CONFIGURE_ARGS+=	--enable-python
+PLIST.python=		yes
+.else
+CONFIGURE_ARGS+=	--disable-python
+.endif
+
+.if !empty(PKG_OPTIONS:Mintrospection)
+BUILDLINK_DEPMETHOD.gobject-introspection+=	build
+.include "../../devel/gobject-introspection/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-introspection
+PLIST.introspection=	yes
+.else
+CONFIGURE_ARGS+=	--disable-introspection
+.endif
