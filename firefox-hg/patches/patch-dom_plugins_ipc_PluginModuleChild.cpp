a75b5d82a944bd7aefeaeb0992976ccb9aae6b82
Thomas Klausner <wiz@NetBSD.org>
2015-01-01 18:55:21 +0000

    Adapt patch for upstream changes.

--- firefox-hg/patches/patch-dom_plugins_ipc_PluginModuleChild.cpp
+++ firefox-hg/patches/patch-dom_plugins_ipc_PluginModuleChild.cpp
@@ -1,8 +1,8 @@
-$NetBSD: patch-dom_plugins_ipc_PluginModuleChild.cpp,v 1.2 2014/10/30 13:54:23 thomasklausner Exp $
+$NetBSD: patch-dom_plugins_ipc_PluginModuleChild.cpp,v 1.3 2015/01/01 18:55:21 thomasklausner Exp $
 
---- dom/plugins/ipc/PluginModuleChild.cpp.orig	2014-10-30 12:50:01.000000000 +0000
+--- dom/plugins/ipc/PluginModuleChild.cpp.orig	2015-01-01 03:13:47.000000000 +0000
 +++ dom/plugins/ipc/PluginModuleChild.cpp
-@@ -286,7 +286,7 @@ PluginModuleChild::InitForChrome(const s
+@@ -315,7 +315,7 @@ PluginModuleChild::InitForChrome(const s
  
      // TODO: use PluginPRLibrary here
  
@@ -11,7 +11,7 @@ $NetBSD: patch-dom_plugins_ipc_PluginModuleChild.cpp,v 1.2 2014/10/30 13:54:23 t
      mShutdownFunc =
          (NP_PLUGINSHUTDOWN) PR_FindFunctionSymbol(mLibrary, "NP_Shutdown");
  
-@@ -1841,7 +1841,7 @@ PluginModuleChild::AnswerNP_GetEntryPoin
+@@ -1877,7 +1877,7 @@ PluginModuleChild::AnswerNP_GetEntryPoin
      AssertPluginThread();
      MOZ_ASSERT(mIsChrome);
  
@@ -20,12 +20,12 @@ $NetBSD: patch-dom_plugins_ipc_PluginModuleChild.cpp,v 1.2 2014/10/30 13:54:23 t
      return true;
  #elif defined(OS_WIN) || defined(OS_MACOSX)
      *_retval = mGetEntryPointsFunc(&mFunctions);
-@@ -1869,7 +1869,7 @@ PluginModuleChild::AnswerNP_Initialize(N
-     SendBackUpXResources(FileDescriptor(xSocketFd));
+@@ -1922,7 +1922,7 @@ PluginModuleChild::DoNP_Initialize(const
  #endif
  
+     NPError result;
 -#if defined(OS_LINUX) || defined(OS_BSD)
 +#if defined(OS_LINUX) || defined(OS_BSD) || defined(OS_SOLARIS)
-     *_retval = mInitializeFunc(&sBrowserFuncs, &mFunctions);
-     return true;
+     result = mInitializeFunc(&sBrowserFuncs, &mFunctions);
  #elif defined(OS_WIN) || defined(OS_MACOSX)
+     result = mInitializeFunc(&sBrowserFuncs);
