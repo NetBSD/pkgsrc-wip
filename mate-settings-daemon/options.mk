01813289f9f0956eb15186140369059974f73be9
OBATA Akio <obache@users.sourceforge.net>
2015-01-02 05:09:47 +0000

    Add polkit option, enabled by default.

--- mate-settings-daemon/options.mk
+++ mate-settings-daemon/options.mk
@@ -1,11 +1,14 @@
-# $NetBSD: options.mk,v 1.1 2015/01/01 08:25:54 obache Exp $
+# $NetBSD: options.mk,v 1.2 2015/01/02 05:09:47 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mate-settings-daemon
+PKG_SUPPORTED_OPTIONS=	polkit
 PKG_OPTIONS_OPTIONAL_GROUPS+=	audio
 PKG_OPTIONS_GROUP.audio=	pulseaudio gstreamer
-PKG_SUGGESTED_OPTIONS=	gstreamer
+PKG_SUGGESTED_OPTIONS=	polkit gstreamer
 
 .include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	polkit
 
 .if !empty(PKG_OPTIONS:Mpulseaudio)
 .include "../../audio/pulseaudio/buildlink3.mk"
@@ -20,4 +23,15 @@ CONFIGURE_ARGS+=	--disable-pulse
 CONFIGURE_ARGS+=	--enable-gstreamer
 .else
 CONFIGURE_ARGS+=	--disable-gstreamer
+.endif
+
+.if !empty(PKG_OPTIONS:Mpolkit)
+.include "../../wip/polkit/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-polkit
+PLIST.polkit=	yes
+MAKE_DIRS+=	${PKG_SYSCONFDIR}/dbus-1/system.d
+CONF_FILES+=	${PREFIX}/share/examples/${PKGBASE}/dbus-1/system.d/org.mate.SettingsDaemon.DateTimeMechanism.conf \
+		${PKG_SYSCONFDIR}/dbus-1/system.d/org.mate.SettingsDaemon.DateTimeMechanism.conf
+.else
+CONFIGURE_ARGS+=	--disable-polkit
 .endif
