64ef5178c6d37cd66e69e911857c2b5379676762
OBATA Akio <obache@users.sourceforge.net>
2015-01-06 09:30:10 +0000

    Import consolekit-0.4.5nb1 as wip/consolekit.
    ConsoleKit is a framework for keeping track of the various users, sessions,
    and seats present on a system. It provides a mechanism for software to react
    to changes of any of these items or of any of the metadata associated with
    them.

---
+++ consolekit/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/01/06 09:30:10 obache Exp $
+
+BUILDLINK_TREE+=	consolekit
+
+.if !defined(CONSOLEKIT_BUILDLINK3_MK)
+CONSOLEKIT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.consolekit+=	consolekit>=0.3.0
+BUILDLINK_ABI_DEPENDS.consolekit+=	consolekit>=0.3.0nb9
+BUILDLINK_PKGSRCDIR.consolekit?=	../../sysutils/consolekit
+
+.include "../../sysutils/dbus/buildlink3.mk"
+.include "../../sysutils/dbus-glib/buildlink3.mk"
+.endif # CONSOLEKIT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-consolekit
