2ef713ef65b52288779d1a8453f546fd27092e40
OBATA Akio <obache@users.sourceforge.net>
2015-01-08 06:09:32 +0000

    fixes dependencies

--- eom/options.mk
+++ eom/options.mk
@@ -1,17 +1,10 @@
-# $NetBSD: options.mk,v 1.1 2015/01/01 08:12:48 obache Exp $
+# $NetBSD: options.mk,v 1.2 2015/01/08 06:09:33 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.eog
-PKG_SUPPORTED_OPTIONS=	dbus jpeg lcms python
-PKG_SUGGESTED_OPTIONS=	dbus jpeg lcms python
+PKG_SUPPORTED_OPTIONS=	dbus exif jpeg lcms python rsvg xmp
+PKG_SUGGESTED_OPTIONS=	dbus exif jpeg lcms python rsvg xmp
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Mjpeg)
-.include "../../mk/jpeg.buildlink3.mk"
-CONFIGURE_ARGS+=	--with-libjpeg
-.else
-CONFIGURE_ARGS+=	--without-libjpeg
-.endif
 
 .if !empty(PKG_OPTIONS:Mdbus)
 .include "../../sysutils/dbus-glib/buildlink3.mk"
@@ -20,9 +13,24 @@ CONFIGURE_ARGS+=	--with-dbus
 CONFIGURE_ARGS+=	--without-dbus
 .endif
 
+.if !empty(PKG_OPTIONS:Mexif)
+.include "../../graphics/libexif/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-libexif
+.else
+CONFIGURE_ARGS+=	--without-libexif
+.endif
+
+.if !empty(PKG_OPTIONS:Mjpeg)
+.include "../../mk/jpeg.buildlink3.mk"
+CONFIGURE_ARGS+=	--with-libjpeg
+.else
+CONFIGURE_ARGS+=	--without-libjpeg
+.endif
+
 .if !empty(PKG_OPTIONS:Mlcms)
 .include "../../graphics/lcms2/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-cms
+.include "../../graphics/libexif/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-cms
 .endif
@@ -35,3 +43,18 @@ CONFIGURE_ARGS+=	--enable-python
 .else
 CONFIGURE_ARGS+=	--disable-python
 .endif
+
+.if !empty(PKG_OPTIONS:Mrsvg)
+.include "../../graphics/librsvg/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-librsvg
+.else
+CONFIGURE_ARGS+=	--without-librsvg
+.endif
+
+.if !empty(PKG_OPTIONS:Mxmp)
+.include "../../devel/exempi/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-xmp
+.else
+CONFIGURE_ARGS+=	--without-xmp
+.endif
+
