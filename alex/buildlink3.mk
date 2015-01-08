025e8244fd16c4f1b193527835e77b51895fa60a
Ryosuke Moro <szptvlfn@NetBSD.org>
2015-01-08 11:56:33 +0000

    Update to 3.1.4
    Changes in 3.1.4:
    * Add Applicative/Functor instances for GHC 7.10

--- alex/buildlink3.mk
+++ alex/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2014/08/29 14:09:59 szptvlfn Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2015/01/08 11:56:33 szptvlfn Exp $
 
 BUILDLINK_TREE+=	alex
 
 .if !defined(ALEX_BUILDLINK3_MK)
 ALEX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.alex+=	alex>=3.1.3
-BUILDLINK_ABI_DEPENDS.alex+=	alex>=3.1.3
+BUILDLINK_API_DEPENDS.alex+=	alex>=3.1.4
+BUILDLINK_ABI_DEPENDS.alex+=	alex>=3.1.4
 BUILDLINK_PKGSRCDIR.alex?=	../../wip/alex
 
 .include "../../wip/hs-QuickCheck/buildlink3.mk"
