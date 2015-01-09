b8343adb4b47587b91d3411edde799851cb6e839
Filip Hajny <filip@joyent.com>
2015-01-09 14:03:41 +0000

    Update phantomjs to 1.9.8. Add doc and examples files. - Change default SSL protocol to TLSv1 to address POODLE (issue 12655) - Fixed building on OS X 10.10 Yosemite (issue 12622) - Backported crash fix when exit (issue 11642, 12431)

--- phantomjs/patches/patch-src_qt_preconfig.sh
+++ phantomjs/patches/patch-src_qt_preconfig.sh
@@ -1,17 +1,17 @@
-$NetBSD: patch-src_qt_preconfig.sh,v 1.1 2013/06/18 11:02:23 fhajny Exp $
+$NetBSD: patch-src_qt_preconfig.sh,v 1.2 2015/01/09 14:03:41 fhajny Exp $
 
 Add support for SunOS. Only configure here, delegate build to pkgsrc.
---- src/qt/preconfig.sh.orig	2013-06-03 23:11:50.000000000 +0000
+--- src/qt/preconfig.sh.orig	2014-10-23 04:27:58.000000000 +0000
 +++ src/qt/preconfig.sh
-@@ -1,5 +1,7 @@
+@@ -1,6 +1,6 @@
  #!/usr/bin/env bash
  
+-COMPILE_JOBS=4
 +set -e
-+
- COMPILE_JOBS=4
  
  QT_CFG=''
-@@ -18,6 +20,14 @@ else
+ QT_CFG+=' -opensource'          # Use the open-source license
+@@ -18,6 +18,14 @@ else
      QT_CFG+=' -qpa'             # X11-less with QPA (aka Lighthouse)
  fi
  
@@ -26,13 +26,3 @@ Add support for SunOS. Only configure here, delegate build to pkgsrc.
  QT_CFG+=' -release'             # Build only for release (no debugging support)
  QT_CFG+=' -fast'                # Accelerate Makefiles generation
  QT_CFG+=' -nomake demos'        # Don't build with the demos
-@@ -107,9 +117,3 @@ done
- export MAKEFLAGS=-j$COMPILE_JOBS
- 
- ./configure -prefix $PWD $QT_CFG
--make -j$COMPILE_JOBS
--
--# Build text codecs
--pushd src/plugins/codecs/
--make -j$COMPILE_JOBS
--popd
