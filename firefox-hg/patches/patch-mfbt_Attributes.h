476ba2af1740961b9dc0ea9bbb4b158c249cc97f
Thomas Klausner <wiz@NetBSD.org>
2015-01-10 17:44:42 +0000

    Make patch apply again.

--- firefox-hg/patches/patch-mfbt_Attributes.h
+++ firefox-hg/patches/patch-mfbt_Attributes.h
@@ -1,16 +1,16 @@
-$NetBSD: patch-mfbt_Attributes.h,v 1.2 2015/01/09 09:08:09 thomasklausner Exp $
+$NetBSD: patch-mfbt_Attributes.h,v 1.3 2015/01/10 17:44:42 thomasklausner Exp $
 
---- mfbt/Attributes.h.orig	2015-01-09 02:12:36.000000000 +0000
+--- mfbt/Attributes.h.orig	2015-01-10 14:42:03.000000000 +0000
 +++ mfbt/Attributes.h
 @@ -50,6 +50,7 @@
   * don't indicate support for them here, due to
   * http://stackoverflow.com/questions/20498142/visual-studio-2013-explicit-keyword-bug
   */
 +#  define MOZ_HAVE_CXX11_ALIGNAS
- #  define MOZ_HAVE_CXX11_DELETE
  #  define MOZ_HAVE_CXX11_FINAL         final
  #  define MOZ_HAVE_CXX11_OVERRIDE
-@@ -73,6 +74,9 @@
+ #  define MOZ_HAVE_NEVER_INLINE          __declspec(noinline)
+@@ -72,6 +73,9 @@
  #  ifndef __has_extension
  #    define __has_extension __has_feature /* compatibility, for older versions of clang */
  #  endif
@@ -20,7 +20,7 @@ $NetBSD: patch-mfbt_Attributes.h,v 1.2 2015/01/09 09:08:09 thomasklausner Exp $
  #  if __has_extension(cxx_constexpr)
  #    define MOZ_HAVE_CXX11_CONSTEXPR
  #  endif
-@@ -94,6 +98,9 @@
+@@ -90,6 +94,9 @@
  #  endif
  #elif defined(__GNUC__)
  #  if defined(__GXX_EXPERIMENTAL_CXX0X__) || __cplusplus >= 201103L
