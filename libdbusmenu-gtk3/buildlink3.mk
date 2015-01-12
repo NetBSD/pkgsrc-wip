902736aebd543a1aabd703d955e7e741f5a39892
OBATA Akio <obache@users.sourceforge.net>
2015-01-12 12:34:46 +0000

    fixes pkgbase.

--- libdbusmenu-gtk3/buildlink3.mk
+++ libdbusmenu-gtk3/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/01/12 11:32:43 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/01/12 12:34:46 obache Exp $
 
-BUILDLINK_TREE+=	libdbusmenu-gtk
+BUILDLINK_TREE+=	libdbusmenu-gtk3
 
-.if !defined(LIBDBUSMENU_GTK_BUILDLINK3_MK)
-LIBDBUSMENU_GTK_BUILDLINK3_MK:=
+.if !defined(LIBDBUSMENU_GTK3_BUILDLINK3_MK)
+LIBDBUSMENU_GTK3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libdbusmenu-gtk+=	libdbusmenu-gtk>=12.10.2
-BUILDLINK_PKGSRCDIR.libdbusmenu-gtk?=	../../wip/libdbusmenu-gtk
+BUILDLINK_API_DEPENDS.libdbusmenu-gtk3+=	libdbusmenu-gtk3>=12.10.2
+BUILDLINK_PKGSRCDIR.libdbusmenu-gtk3?=	../../wip/libdbusmenu-gtk3
 
 .include "../../wip/libdbusmenu-glib/buildlink3.mk"
-.endif	# LIBDBUSMENU_GTK_BUILDLINK3_MK
+.endif	# LIBDBUSMENU_GTK3_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-libdbusmenu-gtk
+BUILDLINK_TREE+=	-libdbusmenu-gtk3
