39630fd01a682911a958733397af4aa51d99e142
OBATA Akio <obache@users.sourceforge.net>
2015-01-12 02:28:49 +0000

    Just xsltproc tool is required to build.

--- libdbusmenu/buildlink3.mk
+++ libdbusmenu/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/01/12 02:23:10 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/01/12 02:28:49 obache Exp $
 
 BUILDLINK_TREE+=	libdbusmenu
 
@@ -8,7 +8,6 @@ LIBDBUSMENU_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libdbusmenu+=	libdbusmenu>=12.10.2
 BUILDLINK_PKGSRCDIR.libdbusmenu?=	../../wip/libdbusmenu
 
-.include "../../textproc/libxslt/buildlink3.mk"
 .include "../../textproc/json-glib/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
