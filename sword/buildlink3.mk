bbe1d484a935f1b7cadd782cc86f10f25cfa6117
Niclas Rosenvik <nros@NetBSD.org>
2015-01-23 23:14:48 +0000

    Install sword.conf in share/examples/sword and let pkginstall framework install sword.conf in pkg_sysconfdir.
    Update sword to version 1.7.4.
    Changelog:
    
    API ChangeLog
    
    15-Dec-2014	DM Smith <dmsmith@crosswire.org>
    	Fixed endless loop in osis2mod for some inputs.
    
    15-Dec-2014	DM Smith <dmsmith@crosswire.org>
    	Added support for Psalm Book divisions using <div type=majorSection>
    	from GHellings
    
    03-Dec-2014	Karl Kleinpaste <charcoal@users.sf.net>
    	Corrected anomalous OSIS highlighting output w/GHellings' patches in
    		src/modules/filters/osis{plain,xhtml}.cpp

--- sword/buildlink3.mk
+++ sword/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2014/12/18 19:58:22 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/01/23 23:14:48 nros Exp $
 
 BUILDLINK_TREE+=	sword
 
 .if !defined(SWORD_BUILDLINK3_MK)
 SWORD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.sword+=	sword>=1.7.3
+BUILDLINK_API_DEPENDS.sword+=	sword>=1.7.4
 BUILDLINK_PKGSRCDIR.sword?=	../../wip/sword
 
 .include "../../textproc/icu/buildlink3.mk"
