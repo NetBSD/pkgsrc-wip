0c68d105eab4ffc8f18e78086882c9e159fc66ae
OBATA Akio <obache@users.sourceforge.net>
2015-01-24 01:36:38 +0000

    let pam to be as a option.

--- polkit/options.mk
+++ polkit/options.mk
@@ -1,12 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2015/01/01 07:45:49 obache Exp $
+# $NetBSD: options.mk,v 1.2 2015/01/24 01:36:38 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.polkit
-PKG_SUPPORTED_OPTIONS=	introspection
-PKG_SUGGESTED_OPTIONS=	introspection
+PKG_SUPPORTED_OPTIONS=	introspection pam
+PKG_SUGGESTED_OPTIONS=	introspection pam
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	introspection
+PLIST_VARS+=	introspection pam
 
 .if !empty(PKG_OPTIONS:Mintrospection)
 BUILDLINK_DEPMETHOD.gobject-introspection+=	build
@@ -15,4 +15,18 @@ CONFIGURE_ARGS+=	--enable-introspection
 PLIST.introspection=	yes
 .else
 CONFIGURE_ARGS+=	--disable-introspection
+.endif
+
+.if !empty(PKG_OPTIONS:Mpam)
+.include "../../mk/pam.buildlink3.mk"
+CONFIGURE_ARGS+=	--with-authfw=pam
+CONFIGURE_ARGS+=	--with-pam-module-dir=${PREFIX}/lib/security
+PLIST.pam=	yes
+.include "../../mk/bsd.prefs.mk"
+.  if ${OPSYS} == "NetBSD"
+CONFIGURE_ARGS+=	--with-pam-include=system
+.  endif
+.else
+CONFIGURE_ARGS+=	--with-authfw=shadow
+CONFIGURE_ARGS+=	--disable-pam-module
 .endif
