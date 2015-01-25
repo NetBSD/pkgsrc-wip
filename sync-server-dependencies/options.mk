3045540cab70892d37a0912c65d3a0a6b9c09633
Thomas Klausner <wiz@NetBSD.org>
2015-01-25 20:05:54 +0000

    Switch to py-mod_wsgi, untested, but ap2-wsgi was removed

--- sync-server-dependencies/options.mk
+++ sync-server-dependencies/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2013/09/27 15:47:18 thomasklausner Exp $
+# $NetBSD: options.mk,v 1.2 2015/01/25 20:05:54 thomasklausner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sync-server-dependencies
 PKG_SUPPORTED_OPTIONS=	apache
@@ -7,6 +7,6 @@ PKG_SUGGESTED_OPTIONS=	apache
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mapache)
-DEPENDS+=	${APACHE_PKG_PREFIX}-${PYPKGPREFIX}-wsgi-[0-9]*:../../www/ap2-wsgi
+DEPENDS+=	${PYPKGPREFIX}-mod_wsgi-[0-9]*:../../www/py-mod_wsgi
 .include "../../mk/apache.mk"
 .endif
