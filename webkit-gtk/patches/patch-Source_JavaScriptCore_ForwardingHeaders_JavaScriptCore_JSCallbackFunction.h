43b7b26414f18dc35497da7b7b6c149f345ca348
Leonardo Taccari <leot@NetBSD.org>
2015-01-25 20:41:38 +0000

    Now it should build succesfully... I will rebuild it and install to see if everything works correctly.

--- webkit-gtk/patches/patch-Source_JavaScriptCore_ForwardingHeaders_JavaScriptCore_JSCallbackFunction.h
+++ webkit-gtk/patches/patch-Source_JavaScriptCore_ForwardingHeaders_JavaScriptCore_JSCallbackFunction.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-Source_JavaScriptCore_ForwardingHeaders_JavaScriptCore_JSCallbackFunction.h,v 1.1 2015/01/24 01:33:07 leot1990 Exp $
+$NetBSD: patch-Source_JavaScriptCore_ForwardingHeaders_JavaScriptCore_JSCallbackFunction.h,v 1.2 2015/01/25 20:41:38 leot1990 Exp $
 
---- Source/JavaScriptCore/ForwardingHeaders/JavaScriptCore/JSCallbackFunction.h.orig	2015-01-23 17:43:40.000000000 +0000
+--- Source/JavaScriptCore/ForwardingHeaders/JavaScriptCore/JSCallbackFunction.h.orig	2015-01-25 16:10:07.000000000 +0000
 +++ Source/JavaScriptCore/ForwardingHeaders/JavaScriptCore/JSCallbackFunction.h
 @@ -0,0 +1 @@
 +#include <JavaScriptCore/API/JSCallbackFunction.h>
---
+++ webkit-gtk/patches/patch-Source_cmake_OptionsGTK.cmake
@@ -0,0 +1,23 @@
+$NetBSD: patch-Source_cmake_OptionsGTK.cmake,v 1.1 2015/01/25 20:41:38 leot1990 Exp $
+
+Do not conflict with CMAKE_MODULE_PATH during the linking phase of
+libwebkit2gtk-4.0.so:
+
+ Linking CXX shared library ../../lib/libwebkit2gtk-4.0.so
+ ld:/usr/pkgsrc/wip/webkit-gtk/work/.buildlink/cmake-Modules:1: ignoring invalid character `3' in script
+ [...]
+ ld:/usr/pkgsrc/wip/webkit-gtk/work/.buildlink/cmake-Modules:1: syntax error in VERSION script
+ Source/WebKit2/CMakeFiles/WebKit2.dir/build.make:16139: recipe for target 'lib/libwebkit2gtk-4.0.so.37.2.6' failed
+ [...]
+
+--- Source/cmake/OptionsGTK.cmake.orig	2015-01-15 10:53:03.000000000 +0000
++++ Source/cmake/OptionsGTK.cmake
+@@ -98,7 +98,7 @@ if (DEVELOPER_MODE)
+ else ()
+     set(ENABLE_TOOLS OFF)
+     WEBKIT_OPTION_DEFAULT_PORT_VALUE(ENABLE_API_TESTS OFF)
+-    set(WebKit2_VERSION_SCRIPT "-Wl,--version-script,${CMAKE_MODULE_PATH}/gtksymbols.filter")
++    set(WebKit2_VERSION_SCRIPT "-Wl,--version-script,${CMAKE_SOURCE_DIR}/Source/cmake/gtksymbols.filter")
+ endif ()
+ 
+ if (CMAKE_SYSTEM_NAME MATCHES "Linux")
