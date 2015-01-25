817687e861dc6de10aacc1eed79dee76a87060ba
Leonardo Taccari <leot@NetBSD.org>
2015-01-25 20:44:41 +0000

    Patch committed accidently. Not needed.

--- webkit-gtk/patches/patch-Source_JavaScriptCore_ForwardingHeaders_JavaScriptCore_JSCallbackFunction.h
+++
@@ -1,6 +0,0 @@
-$NetBSD: patch-Source_JavaScriptCore_ForwardingHeaders_JavaScriptCore_JSCallbackFunction.h,v 1.2 2015/01/25 20:41:38 leot1990 Exp $
-
---- Source/JavaScriptCore/ForwardingHeaders/JavaScriptCore/JSCallbackFunction.h.orig	2015-01-25 16:10:07.000000000 +0000
-+++ Source/JavaScriptCore/ForwardingHeaders/JavaScriptCore/JSCallbackFunction.h
-@@ -0,0 +1 @@
-+#include <JavaScriptCore/API/JSCallbackFunction.h>
