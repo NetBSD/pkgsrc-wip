9f131cd44c01246ae1fa28b91273fbf0a6d49112
Thomas Klausner <wiz@NetBSD.org>
2015-02-02 13:13:26 +0000

    Update to 0.9.5.
    version 0.9.5:
    
    - Added animation support.
    - added bpgview utility.
    - bpgenc: fixed support of some JPEG parameter combinations
    - fixed JS 8 bit only decoder and renamed it to bpgdec8.js
    - libbpg: added CMYK output format

--- libbpg/patches/patch-bpgenc.c
+++ libbpg/patches/patch-bpgenc.c
@@ -1,16 +1,16 @@
-$NetBSD: patch-bpgenc.c,v 1.1 2015/01/10 09:41:23 thomasklausner Exp $
+$NetBSD: patch-bpgenc.c,v 1.2 2015/02/02 13:13:26 thomasklausner Exp $
 
 Use getopt_long instead of getopt_long_only since it's more portable
 (e.g., exists on NetBSD).
 
---- bpgenc.c.orig	2014-12-18 21:10:00.000000000 +0000
+--- bpgenc.c.orig	2015-01-11 16:17:07.000000000 +0000
 +++ bpgenc.c
-@@ -2155,7 +2155,7 @@ int main(int argc, char **argv)
-     premultiplied_alpha = 0;
+@@ -2721,7 +2721,7 @@ int main(int argc, char **argv)
+     frame_delay_file = NULL;
      
      for(;;) {
--        c = getopt_long_only(argc, argv, "q:o:hf:c:vm:b:e:", long_opts, &option_index);
-+        c = getopt_long(argc, argv, "q:o:hf:c:vm:b:e:", long_opts, &option_index);
+-        c = getopt_long_only(argc, argv, "q:o:hf:c:vm:b:e:a", long_opts, &option_index);
++        c = getopt_long(argc, argv, "q:o:hf:c:vm:b:e:a", long_opts, &option_index);
          if (c == -1)
              break;
          switch(c) {
