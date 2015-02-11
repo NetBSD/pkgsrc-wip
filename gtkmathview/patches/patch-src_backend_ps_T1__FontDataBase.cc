5a7257f46a9a8501823ab6a417e1f2a6e51da816
Niclas Rosenvik <nros@NetBSD.org>
2015-02-11 12:03:53 +0000

    Change C includes to c* C++ style includes. Fix build on gcc 4.7 and above.

--- gtkmathview/patches/patch-src_backend_ps_T1__FontDataBase.cc
+++ gtkmathview/patches/patch-src_backend_ps_T1__FontDataBase.cc
@@ -1,12 +1,14 @@
-$NetBSD: patch-src_backend_ps_T1__FontDataBase.cc,v 1.1 2014/12/26 21:16:29 nros Exp $
-* strcmp need string.h
+$NetBSD: patch-src_backend_ps_T1__FontDataBase.cc,v 1.2 2015/02/11 12:03:53 nros Exp $
+* strcmp need cstring
 --- src/backend/ps/T1_FontDataBase.cc.orig	2007-08-17 10:02:40.000000000 +0000
 +++ src/backend/ps/T1_FontDataBase.cc
-@@ -21,6 +21,7 @@
+@@ -20,7 +20,8 @@
+ #include <config.h>
  #include <map>
  #include <string>
- #include <stdlib.h>
-+#include <string.h>
+-#include <stdlib.h>
++#include <cstdlib>
++#include <cstring>
  #include <iostream>
  #include <list>
  
--- gtkmathview/patches/patch-src_engine_mathml_mathVariantAux.cc
+++ gtkmathview/patches/patch-src_engine_mathml_mathVariantAux.cc
@@ -1,12 +1,12 @@
-$NetBSD: patch-src_engine_mathml_mathVariantAux.cc,v 1.1 2014/12/26 21:16:29 nros Exp $
-* strcmp needs string.h
+$NetBSD: patch-src_engine_mathml_mathVariantAux.cc,v 1.2 2015/02/11 12:03:53 nros Exp $
+* strcmp needs cstring
 --- src/engine/mathml/mathVariantAux.cc.orig	2007-08-17 10:02:38.000000000 +0000
 +++ src/engine/mathml/mathVariantAux.cc
 @@ -20,6 +20,8 @@
  
  #include <cassert>
  
-+#include <string.h>
++#include <cstring>
 +
  #include "mathVariantAux.hh"
  
