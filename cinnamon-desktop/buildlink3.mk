82c9f1bdc377097fe594d6c81d36e0fd6ce2573a
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2015-02-19 17:44:48 +0000

    +buildlink3.mk in cinnamon-desktop

---
+++ cinnamon-desktop/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/02/19 17:44:48 jihbed Exp $
+
+BUILDLINK_TREE+=	cinnamon-desktop
+
+.if !defined(CINNAMON_DESKTOP_BUILDLINK3_MK)
+CINNAMON_DESKTOP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.cinnamon-desktop+=	cinnamon-desktop>=2.2.3
+BUILDLINK_PKGSRCDIR.cinnamon-desktop?=	../../wip/cinnamon-desktop
+
+.include "../../x11/gtk3/buildlink3.mk"
+.include "../../x11/libxkbfile/buildlink3.mk"
+.include "../../x11/xkeyboard-config/buildlink3.mk"
+.endif	# CINNAMON_DESKTOP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-cinnamon-desktop
