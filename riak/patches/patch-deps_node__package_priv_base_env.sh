4b98a37df180d45c28c630fe353abf4d6617d523
Filip Hajny <filip@joyent.com>
2015-02-23 17:32:17 +0000

    Update riak to 2.0.5.
    2.0.5
    - Adding --format=csv to allow some administrative commands to be output
      as csv files
    - Sequential tuning Improving leveldb for when a single database (vnode)
      receives a large write volume. This situation commonly occurs during
      a Riak handoff operation.
    - Add optional timeout for handle_handoff_data callback and improve logging
    - L0 Realtime connection balancing. N v3 repl realtime connections are chosen
      randomly which can lead to large imbalances. Previous implementations did
      a better job of ensuring there was good balance. The goal of this work is
      to bring that back to v3 realtime replication.
    - If Solr complains that the index exists, update Riak's metadata to reflect it
    - node_get_fsm_time and node_put_fsm_time stats are incorrect.
      node_get_fsm_time and node_put_fsm_time values are an order of magnitude
      smaller than 2.0.2
    - Other bugfixes
    
    2.0.4
    - Improve AAE fullsync by estimating total number of keys. This allows for
      better sizing of the bloom filter when performing a bloom-fold-based exchange
      and enhances the adaptive exchange logic that was added previously in Riak
      2.0.1. In that version, the new riak_repl.fullsync_direct_percentage_limit
      setting was added, which allowed the limit to be specified as a percentage of
      total keys rather than as a fixed number.
    - Added last_fullsync_complete stat tracking
    - Expose AAE tree status in the logs
    - Improve AAE fullsync by using breadth-first exchange
    - Riak now uses Exometer for collecting metrics and has an improved CLI for
      finding and displaying them
    - Internal components can process partial configurations as if they were in the
      global config file. This supports the handoff visibility changes.
    - Riak now uses Clique for managing CLI commands. The initial commands covered
      exist to increase visibility into the handoff process and provide Cuttlefish
      config support from the CLI.
    - Fix stats process crash if no leader
    - Address some minor bugs around establishing SSL connections
    - 2.0 port of AAE transient filesystem failures
    - Fix error/retry exit counts on location down messages
    - Fix deadlock when performing AAE fullsync over SSL (Erlang VM patch)
    - Prevent servers from accepting SSLv3 connections (Erlang VM patch)
    - The map Data Type is now more efficient when merging
    - Fix a case in which sibling explosion could occur during handoff
    - Special handling for the net_ticktime setting in admin scripts
    - Add a missing function clause in riak_kv_node that could result in crashes
    - Avoid timeouts when handoff sender is folding over a large number of keys
    - No more extra work for handoff sender after TCP error makes that work useless
    - Report error when failing to open file instead of crashing when calling
      riak_core_util:replace_file/2
    - Ensure creation of ensembles when strongly consistent bucket types
      with different n_vals from default bucket type do not yet have buckets
    - Avoid SSL deadlocks that occur when sending data bidirectionally using Erlang
      SSL sockets. The fix is a patch to the Erlang VM shipped with the build.
    
    2.0.3:
    - Introduce exometer metrics into 2.0
    - Remove afunix from EXOMETER_PACKAGES.
    - remove afunix from reltool.config
    
    Full changelog:
    
    https://github.com/basho/riak/blob/2.0/RELEASE-NOTES.md

--- riak/patches/patch-deps_node__package_priv_base_env.sh
+++ riak/patches/patch-deps_node__package_priv_base_env.sh
@@ -1,9 +1,19 @@
-$NetBSD: patch-deps_node__package_priv_base_env.sh,v 1.3 2014/11/27 10:35:46 fhajny Exp $
+$NetBSD: patch-deps_node__package_priv_base_env.sh,v 1.4 2015/02/23 17:32:17 fhajny Exp $
 
 Substitute whoami for id across SunOS platforms.
+Let us substitute run dir.
 
---- deps/node_package/priv/base/env.sh.orig	2014-10-14 17:01:01.000000000 +0000
+--- deps/node_package/priv/base/env.sh.orig	2015-02-11 20:15:28.000000000 +0000
 +++ deps/node_package/priv/base/env.sh
+@@ -30,7 +30,7 @@ APP_VERSION={{app_version}}
+ # Variables needed to support creation of .pid files
+ # PID directory and pid file name of this app
+ # ex: /var/run/riak & /var/run/riak/riak.pid
+-RUN_DIR="/var/run" # for now hard coded unless we find a platform that differs
++RUN_DIR="@RIAK_RUN@" # for now hard coded unless we find a platform that differs
+ PID_DIR=$RUN_DIR/$RUNNER_SCRIPT
+ PID_FILE=$PID_DIR/$RUNNER_SCRIPT.pid
+ 
 @@ -44,7 +44,11 @@ fi
  # Registered process to wait for to consider start a success
  WAIT_FOR_PROCESS={{runner_wait_process}}
