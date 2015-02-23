ea421db28997dcbd42b9185eebd1d5be922f05df
Yakovetsky VS <yvs4sf@users.sourceforge.net>
2015-02-23 09:45:33 +0000

    Update to git:4fa13db (add color/histogram)

--- mtr-graph/options.mk
+++ mtr-graph/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2014/09/26 09:51:23 yvs4sf Exp $
+# $NetBSD: options.mk,v 1.3 2015/02/23 09:45:33 yvs4sf Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mtr-graph
 PKG_SUPPORTED_OPTIONS=	gtk
@@ -8,6 +8,7 @@ PKG_OPTIONS_OPTIONAL_GROUPS+=	graphcairo
 PKG_OPTIONS_GROUP.graphcairo+=	graphcairo-xcb
 PKG_OPTIONS_GROUP.graphcairo+=	graphcairo-xlib
 PKG_SUPPORTED_OPTIONS+=	libidn
+PKG_SUPPORTED_OPTIONS+=	unicode
 PKG_SUGGESTED_OPTIONS=	inet6
 PKG_SUGGESTED_OPTIONS+=	ipinfo
 PKG_SUGGESTED_OPTIONS+=	graphcairo-xcb
@@ -49,4 +50,8 @@ CONFIGURE_ARGS+=	--with-graphcairo-xlib
 .if !empty(PKG_OPTIONS:Mlibidn)
 .include "../../devel/libidn/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-libidn
+.endif
+
+.if !empty(PKG_OPTIONS:Municode)
+CONFIGURE_ARGS+=	--with-unicode
 .endif
