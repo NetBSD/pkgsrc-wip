ca617497a429feae4616ff26fca1396929d86992
Tobias Nygren <tnn@NetBSD.org>
2015-03-03 17:50:10 +0000

    radeonsi needs libLLVM

--- MesaLib/options.mk
+++ MesaLib/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.14 2015/03/03 17:45:35 tnn2 Exp $
+# $NetBSD: options.mk,v 1.15 2015/03/03 17:50:10 tnn2 Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.MesaLib
 PKG_SUPPORTED_OPTIONS=		llvm
@@ -23,9 +23,6 @@ GALLIUM_DRIVERS+=	ilo
 # AMD Radeon r600
 GALLIUM_DRIVERS+=	r600
 
-# AMD Canary Islands GPUs, needs --with-egl-platforms=drm
-# GALLIUM_DRIVERS+=	radeonsi
-
 # Qualcomm SnapDragon, libdrm_freedreno.pc
 # GALLIUM_DRIVERS+=	freedreno
 
@@ -35,9 +32,14 @@ GALLIUM_DRIVERS+=	r600
 # nVidia, doesn't currently build
 # GALLIUM_DRIVERS+=	nouveau
 
+CONFIGURE_ARGS+=	--with-egl-platforms=x11,drm
 CONFIGURE_ARGS+=	--with-gallium-drivers=${GALLIUM_DRIVERS:ts,}
+
 .if !empty(PKG_OPTIONS:Mllvm)
+# AMD Radeon r300
 GALLIUM_DRIVERS+=	r300
+# AMD Canary Islands GPUs
+GALLIUM_DRIVERS+=	radeonsi
 CONFIGURE_ARGS+=	--enable-gallium-llvm
 CONFIGURE_ARGS+=	--enable-r600-llvm-compiler
 # XXX: for libLLVM. You should turn the shlib PKG_OPTION on for now.
