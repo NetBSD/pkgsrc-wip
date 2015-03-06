3be0dffaa0ba27ae99dbde41ce721c7ed0547c10
Yakovetsky VS <yvs4sf@users.sourceforge.net>
2015-03-06 10:35:31 +0000

    Update to git:cbb1851 (System's own getopt)

--- mtr-graph/options.mk
+++ mtr-graph/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2015/02/23 09:45:33 yvs4sf Exp $
+# $NetBSD: options.mk,v 1.4 2015/03/06 10:35:31 yvs4sf Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mtr-graph
 PKG_SUPPORTED_OPTIONS=	gtk
@@ -9,9 +9,12 @@ PKG_OPTIONS_GROUP.graphcairo+=	graphcairo-xcb
 PKG_OPTIONS_GROUP.graphcairo+=	graphcairo-xlib
 PKG_SUPPORTED_OPTIONS+=	libidn
 PKG_SUPPORTED_OPTIONS+=	unicode
+PKG_SUPPORTED_OPTIONS+=	sys-getopt
 PKG_SUGGESTED_OPTIONS=	inet6
 PKG_SUGGESTED_OPTIONS+=	ipinfo
 PKG_SUGGESTED_OPTIONS+=	graphcairo-xcb
+PKG_SUGGESTED_OPTIONS+=	unicode
+PKG_SUGGESTED_OPTIONS+=	sys-getopt
 
 .include "../../mk/bsd.options.mk"
 
@@ -54,4 +57,8 @@ CONFIGURE_ARGS+=	--with-libidn
 
 .if !empty(PKG_OPTIONS:Municode)
 CONFIGURE_ARGS+=	--with-unicode
+.endif
+
+.if !empty(PKG_OPTIONS:Msys-getopt)
+CONFIGURE_ARGS+=	--with-sys-getopt
 .endif
