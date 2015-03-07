1b7e732f4dd5c33e2b4930e8361acbc2e1e82fc4
Kamel Ibn Aziz Derouiche <kamel.derouiche@gmail.com>
2015-03-07 22:18:24 +0000

    added keyword 'probabilistic programming updated doc added 2015 in copyright redefine dict in Python2 for using efficient iterator methods added mode method aca03eebf080 fix bug on CPT with context-specific independence; add getAleaLeavesSet and _getLeaChildren methods; refactor reset method corrected withProb method added Lea FOSDEM15 presentation Added tag 2.0.0 for changeset 613c68acdfa6 version Lea 2.0.0 updated API doc updated poisson method, added binom, bernoulli, interval methods Added tag 2.0.0-beta.6 for changeset e7d5ec60e411 version Lea 2.0.0-beta.6 optimize random generator; memoize cumul functions; improve means for Python 2/3 portability; split Alea _vps attribute into _vs and _ps fix mean of deltatime in Python 2.x Added tag 2.0.0-beta.5 for changeset 66362ef7ae4b updated HTML doc rename 'integral' by 'cumul' version Lea 2.0.0-beta.5 added histogram + refactoring string representation methods cleanup markov Added tag 2.0.0-beta.4 for changeset 8273fdffa6f3 updated HTML doc

--- py-lea/patches/patch-setup.py
+++ py-lea/patches/patch-setup.py
@@ -1,12 +1,11 @@
-$NetBSD: patch-setup.py,v 1.1 2014/05/04 00:27:26 jihbed Exp $
+$NetBSD: patch-setup.py,v 1.2 2015/03/07 22:18:24 jihbed Exp $
 
---- setup.py.orig	2014-05-04 01:23:19.000000000 +0100
-+++ setup.py	2014-05-04 01:24:01.000000000 +0100
-@@ -44,6 +44,4 @@
-                       'alea',
-                       'tlea',
+--- setup.py.orig	2015-03-07 18:31:16.000000000 +0000
++++ setup.py
+@@ -55,6 +55,4 @@ setup( name = 'lea',
+                       'leapp_console',
+                       'leapp_translator',
                        'license' ] ,
 -       data_files = [('.', [ 'COPYING',
 -                             'COPYING.LESSER' ] ) ],
        )
-\ No newline at end of file
