d0ea5159ecd805edf9cb7e46160a648b32353f2f
Niclas Rosenvik <nros@NetBSD.org>
2015-03-08 19:31:55 +0000

    options.mk tab cleanup.

--- bibletime/options.mk
+++ bibletime/options.mk
@@ -1,10 +1,9 @@
-# $NetBSD: options.mk,v 1.2 2015/03/08 18:29:18 nros Exp $
-#
+# $NetBSD: options.mk,v 1.3 2015/03/08 19:31:55 nros Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.bibletime
-PKG_OPTIONS_REQUIRED_GROUPS= qt
-PKG_OPTIONS_GROUP.qt=	qt4 qt5
-PKG_SUGGESTED_OPTIONS=	qt4
+PKG_OPTIONS_VAR=		PKG_OPTIONS.bibletime
+PKG_OPTIONS_REQUIRED_GROUPS= 	qt
+PKG_OPTIONS_GROUP.qt=		qt4 qt5
+PKG_SUGGESTED_OPTIONS=		qt4
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mqt5)
