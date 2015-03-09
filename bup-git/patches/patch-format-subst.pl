35be7576d280d3bfb1cafa9e43532708176e6a0b
Thomas Klausner <wiz@NetBSD.org>
2015-03-09 22:36:41 +0000

    Sync with upstream.

--- bup-git/patches/patch-format-subst.pl
+++ bup-git/patches/patch-format-subst.pl
@@ -1,16 +1,16 @@
-$NetBSD: patch-format-subst.pl,v 1.1 2014/07/09 04:02:05 thomasklausner Exp $
+$NetBSD: patch-format-subst.pl,v 1.2 2015/03/09 22:36:41 thomasklausner Exp $
 
 The shallow checkouts used by pkgsrc do not have tag information,
-so git describe will alwways fail. Ignore the error.
+so git describe will always fail. Ignore the error.
 
---- format-subst.pl.orig	2014-07-09 00:48:14.000000000 +0000
+--- format-subst.pl.orig	2015-03-09 18:56:24.000000000 +0000
 +++ format-subst.pl
 @@ -19,7 +19,7 @@ while (<>) {
      s{
  	\$Format:\%d\$
      }{
--	my $tag = fix(ex('git describe --match="[0-9]*"'));
-+	my $tag = fix('git describe --match="[0-9]*"');
+-	my $tag = fix(ex('git describe --always --match="[0-9]*"'));
++	my $tag = fix('git describe --always --match="[0-9]*"');
  	"(tag: bup-$tag)"
      }ex;
      
