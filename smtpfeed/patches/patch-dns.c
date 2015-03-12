0eefda961fdedc9e2560c1aa9123572956539391
Makoto Fujiwara <makoto@ki.nu>
2015-03-12 01:05:05 +0000

    show_query is only in dns.c, deleting patch-extern.h.

--- smtpfeed/patches/patch-dns.c
+++ smtpfeed/patches/patch-dns.c
@@ -1,10 +1,19 @@
-$NetBSD: patch-dns.c,v 1.1 2015/03/11 14:37:02 makoto Exp $
+$NetBSD: patch-dns.c,v 1.2 2015/03/12 01:05:05 makoto Exp $
 
-Returning nothing
+show_query() returning nothing
 
---- dns.c.orig	2015-03-11 23:30:10.000000000 +0900
-+++ dns.c	2015-03-11 23:29:27.000000000 +0900
-@@ -2222,6 +2222,7 @@ long opt;	/* option described in map fil
+--- dns.c.orig	2015-02-17 23:01:03.000000000 +0900
++++ dns.c	2015-03-12 10:02:48.000000000 +0900
+@@ -42,6 +42,8 @@ static char *_id_ = "$Id: dns.c,v 1.90 2
+ # include "common.h"
+ # include "extern.h"
+ 
++void show_query(u_char* t, u_char* b, u_char* e);
++
+ #if PACKETSZ > 1024
+ #define MAXPACKET	PACKETSZ
+ #else
+@@ -2222,6 +2224,7 @@ long opt;	/* option described in map fil
  	return 0;
  }
  
