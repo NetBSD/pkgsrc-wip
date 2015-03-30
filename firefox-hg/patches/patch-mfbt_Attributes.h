52f6648b8351d163eebb058a00320545fb1ae62b
Thomas Klausner <wiz@NetBSD.org>
2015-03-30 08:09:09 +0000

    Make patch apply again. Add patch for test == operator. Doesn't package for me though.

--- firefox-hg/patches/patch-mfbt_Attributes.h
+++ firefox-hg/patches/patch-mfbt_Attributes.h
@@ -1,16 +1,16 @@
-$NetBSD: patch-mfbt_Attributes.h,v 1.3 2015/01/10 17:44:42 thomasklausner Exp $
+$NetBSD: patch-mfbt_Attributes.h,v 1.4 2015/03/30 08:09:10 thomasklausner Exp $
 
---- mfbt/Attributes.h.orig	2015-01-10 14:42:03.000000000 +0000
+--- mfbt/Attributes.h.orig	2015-03-24 09:13:57.000000000 +0000
 +++ mfbt/Attributes.h
 @@ -50,6 +50,7 @@
   * don't indicate support for them here, due to
   * http://stackoverflow.com/questions/20498142/visual-studio-2013-explicit-keyword-bug
   */
 +#  define MOZ_HAVE_CXX11_ALIGNAS
- #  define MOZ_HAVE_CXX11_FINAL         final
- #  define MOZ_HAVE_CXX11_OVERRIDE
  #  define MOZ_HAVE_NEVER_INLINE          __declspec(noinline)
-@@ -72,6 +73,9 @@
+ #  define MOZ_HAVE_NORETURN              __declspec(noreturn)
+ #  ifdef __clang__
+@@ -70,6 +71,9 @@
  #  ifndef __has_extension
  #    define __has_extension __has_feature /* compatibility, for older versions of clang */
  #  endif
@@ -20,13 +20,13 @@ $NetBSD: patch-mfbt_Attributes.h,v 1.3 2015/01/10 17:44:42 thomasklausner Exp $
  #  if __has_extension(cxx_constexpr)
  #    define MOZ_HAVE_CXX11_CONSTEXPR
  #  endif
-@@ -90,6 +94,9 @@
+@@ -83,6 +87,9 @@
+ #    define MOZ_HAVE_NORETURN            __attribute__((noreturn))
  #  endif
  #elif defined(__GNUC__)
++#  if MOZ_GCC_VERSION_AT_LEAST(4, 8, 0)
++#    define MOZ_HAVE_CXX11_ALIGNAS
++#  endif
  #  if defined(__GXX_EXPERIMENTAL_CXX0X__) || __cplusplus >= 201103L
-+#    if MOZ_GCC_VERSION_AT_LEAST(4, 8, 0)
-+#      define MOZ_HAVE_CXX11_ALIGNAS
-+#    endif
- #    if MOZ_GCC_VERSION_AT_LEAST(4, 7, 0)
- #      define MOZ_HAVE_CXX11_OVERRIDE
- #      define MOZ_HAVE_CXX11_FINAL       final
+ #      define MOZ_HAVE_CXX11_CONSTEXPR
+ #      define MOZ_HAVE_EXPLICIT_CONVERSION
