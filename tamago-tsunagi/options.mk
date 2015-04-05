181ada5e528d938c43f5af3fa4da35c28a4745d2
Makoto Fujiwara <makoto@ki.nu>
2015-04-05 07:25:45 +0000

    Activate freewnn as a default. 1.11alpha does not satisfy >=1.11. so use 1.10 instead.

--- tamago-tsunagi/options.mk
+++ tamago-tsunagi/options.mk
@@ -1,12 +1,12 @@
-# $NetBSD: options.mk,v 1.1 2015/04/04 15:15:44 makoto Exp $
+# $NetBSD: options.mk,v 1.2 2015/04/05 07:25:45 makoto Exp $
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tamago-tsunagi
 PKG_SUPPORTED_OPTIONS=	freewnn canna sj3 anthy
-PKG_SUGGESTED_OPTIONS=	canna sj3 anthy
+PKG_SUGGESTED_OPTIONS=	freewnn canna sj3 anthy
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mfreewnn)
-DEPENDS+=	ja-FreeWnn-server>=1.11:../../inputmethod/ja-freewnn-server
+DEPENDS+=	ja-FreeWnn-server>=1.10:../../inputmethod/ja-freewnn-server
 .endif
 
 .if !empty(PKG_OPTIONS:Mcanna)
