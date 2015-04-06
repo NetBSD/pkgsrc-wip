58bc50e0b71fa9ed63f21d0a4afc8de084269453
Kamil Rytarowski <kamil@NetBSD.org>
2015-04-06 00:49:14 +0000

    Clone keybinder to keybinder3 (GTK3+ version)
    The Keybinder package contains a library for registering global keyboard
    shortcuts.
    Keybinder works with GTK-based applications using the X Window System.
    
    The library contains:
    - A C library, libkeybinder
    - Python bindings, python-keybinder
    - An examples directory with programs in C, Lua, Python and Vala.
    
    There are: Python and Lua bindings.

---
+++ keybinder3/PLIST.lua
@@ -0,0 +1,2 @@
+@comment $NetBSD: PLIST.lua,v 1.1 2015/04/06 00:49:14 krytarowski Exp $
+lib/lua/5.1/keybinder.so
---
+++ keybinder3/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/04/06 00:49:14 krytarowski Exp $
+
+BUILDLINK_TREE+=	keybinder
+
+.if !defined(KEYBINDER_BUILDLINK3_MK)
+KEYBINDER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.keybinder+=	keybinder>=0.3.0
+BUILDLINK_PKGSRCDIR.keybinder?=	../../wip/keybinder
+
+.include "../../devel/gobject-introspection/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# KEYBINDER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-keybinder
