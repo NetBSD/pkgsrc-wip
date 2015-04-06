71a155de3d36c9fe7e78de483d145dafc7922ff6
Kamil Rytarowski <kamil@NetBSD.org>
2015-04-06 10:40:32 +0000

    Adapt package for reuse ../../wip/keybinder common parts.
    Work-in-progress

--- keybinder3/PLIST.lua
+++
@@ -1,2 +0,0 @@
-@comment $NetBSD: PLIST.lua,v 1.1 2015/04/06 00:49:14 krytarowski Exp $
-lib/lua/5.1/keybinder.so
--- keybinder3/buildlink3.mk
+++ keybinder3/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/04/06 00:49:14 krytarowski Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/04/06 10:40:32 krytarowski Exp $
 
-BUILDLINK_TREE+=	keybinder
+BUILDLINK_TREE+=	keybinder3
 
-.if !defined(KEYBINDER_BUILDLINK3_MK)
-KEYBINDER_BUILDLINK3_MK:=
+.if !defined(KEYBINDER3_BUILDLINK3_MK)
+KEYBINDER3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.keybinder+=	keybinder>=0.3.0
-BUILDLINK_PKGSRCDIR.keybinder?=	../../wip/keybinder
+BUILDLINK3_API_DEPENDS.keybinder+=	keybinder>=0.3.0
+BUILDLINK3_PKGSRCDIR.keybinder?=	../../wip/keybinder
 
 .include "../../devel/gobject-introspection/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
-.endif	# KEYBINDER_BUILDLINK3_MK
+.endif	# KEYBINDER3_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-keybinder
+BUILDLINK_TREE+=	-keybinder3
