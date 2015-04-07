d0a27fac09c8d45ba36d58e3572ec299688000e3
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2015-04-07 20:55:44 +0000

    Snapshot 20150329. This will be my last commit. I no longer use NetBSD.

--- gcc50snapshot/version.mk
+++ gcc50snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.19 2015/03/17 06:08:56 keckhardt Exp $
-GCC50S_DIST_VERSION:=5-20150315
+# $NetBSD: version.mk,v 1.20 2015/04/07 20:55:44 keckhardt Exp $
+GCC50S_DIST_VERSION:=5-20150329
