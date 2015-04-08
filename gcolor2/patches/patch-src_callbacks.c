71a10a28692fb6ef627f763a68c609a5b9450ace
Kamil Rytarowski <kamil@NetBSD.org>
2015-04-08 22:54:16 +0000

    Improve patches for missing includes. Import changes from Fedora.
    Add patch to replace gdk_pixbuf_unref() -> g_object_unref().

--- gcolor2/patches/patch-src_callbacks.c
+++ gcolor2/patches/patch-src_callbacks.c
@@ -1,11 +1,17 @@
-$NetBSD: patch-src_callbacks.c,v 1.1 2015/04/08 22:27:05 krytarowski Exp $
+$NetBSD: patch-src_callbacks.c,v 1.2 2015/04/08 22:54:16 krytarowski Exp $
 
 Add missing header for strlen(3) and strspn(3).
 
+Backport additional changes from Fedora.
+http://pkgs.fedoraproject.org/cgit/gcolor2.git/tree/gcolor2-0.4-missing-includes.patch
+
 --- src/callbacks.c.orig	2005-07-12 18:06:12.000000000 +0000
 +++ src/callbacks.c
-@@ -4,6 +4,7 @@
+@@ -2,8 +2,10 @@
+ #  include <config.h>
+ #endif
  
++#include <glib/gprintf.h>
  #include <gtk/gtk.h>
  #include <stdio.h>
 +#include <string.h>
