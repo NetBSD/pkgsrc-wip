f5dd0c8b6fa7c16e7186e3dffd635d4754c335eb
Kamil Rytarowski <kamil@NetBSD.org>
2015-04-09 10:59:52 +0000

    pkglint -Wall -Call

--- py-terminator/patches/patch-setup.py
+++ py-terminator/patches/patch-setup.py
@@ -1,5 +1,8 @@
-$NetBSD: patch-setup.py,v 1.1 2014/03/30 20:49:14 krytarowski Exp $
+$NetBSD: patch-setup.py,v 1.2 2015/04/09 11:03:37 krytarowski Exp $
+
 Use the right path for man-pages in pkgsrc.
+
+Install the man pages in the right directory.
 
 --- setup.py.orig	2014-03-29 19:45:44.000000000 +0000
 +++ setup.py
