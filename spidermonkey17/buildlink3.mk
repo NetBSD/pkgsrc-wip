c88af6c7d7092efb4f36495642c95c98957a117c
Kamil Rytarowski <kamil@NetBSD.org>
2015-04-10 18:29:00 +0000

    Enable libffi in buildlink3.mk

--- spidermonkey17/buildlink3.mk
+++ spidermonkey17/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2015/04/10 11:43:49 krytarowski Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2015/04/10 18:29:00 krytarowski Exp $
 
 BUILDLINK_TREE+=	spidermonkey17
 
@@ -8,7 +8,7 @@ SPIDERMONKEY17_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.spidermonkey17+=	spidermonkey17>=17.0.0
 BUILDLINK_PKGSRCDIR.spidermonkey17?=	../../wip/spidermonkey17
 
-#.include "../../devel/libffi/buildlink3.mk"
+.include "../../devel/libffi/buildlink3.mk"
 .include "../../devel/nspr/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .endif	# SPIDERMONKEY17_BUILDLINK3_MK
