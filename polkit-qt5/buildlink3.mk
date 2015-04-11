202a496db05666c523557f58c1993f6dce47bc61
Kamil Rytarowski <kamil@NetBSD.org>
2015-04-11 16:12:57 +0000

    Import polkit-qt-0.9.4 from security/polkit-qt as wip/polkit-qt4
    Polkit-qt aims to make it easy for Qt developers to take advantage of
    PolicyKit API. It is a convenience wrapper around QAction and QAbstractButton
    that lets you integrate those two components easily with PolicyKit.
    
    Polkit-qt is splitted in two libraries: polkit-qt-core and polkit-qt-gui.
    
    polkit-qt-core lets you control actions and authentication without a GUI,
    with some very simple functions. It also lets you retrieve and control useful
    informations on the PolicyKit context. It includes the classes Auth and
    Context.
    
    polkit-qt-gui lets you easily associate GUI items with PolicyKit actions.
    By some simple wrapper classes you are able to associate QAction and
    QAbstractButton to a PolicyKit action, and get their properties changed
    accordingly to PolicyKit result. It includes the classes Action, ActionButton
    and ActionButtons

---
+++ polkit-qt5/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/04/11 16:12:57 krytarowski Exp $
+
+BUILDLINK_TREE+=	polkit-qt
+
+.if !defined(POLKIT_QT_BUILDLINK3_MK)
+POLKIT_QT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.polkit-qt+=	polkit-qt>=0.9.2
+BUILDLINK_ABI_DEPENDS.polkit-qt+=	polkit-qt>=0.9.4nb17
+BUILDLINK_PKGSRCDIR.polkit-qt?=	../../security/polkit-qt
+
+.include "../../security/policykit/buildlink3.mk"
+.include "../../x11/qt4-libs/buildlink3.mk"
+.endif	# POLKIT_QT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-polkit-qt
