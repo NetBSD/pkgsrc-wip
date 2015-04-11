bb2b121f67a121ef8f07a953f86b32a4f770da2d
Kamil Rytarowski <kamil@NetBSD.org>
2015-04-11 16:54:26 +0000

    Fix buildlink3.mk

--- polkit-qt5/buildlink3.mk
+++ polkit-qt5/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/04/11 16:36:24 krytarowski Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2015/04/11 16:54:26 krytarowski Exp $
 
-BUILDLINK_TREE+=	polkit-qt5
+BUILDLINK_TREE+=	polkit-qt5-1
 
 .if !defined(POLKIT_QT5_BUILDLINK3_MK)
 POLKIT_QT5_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.polkit-qt5+=	polkit-qt5>=0.112.0
-BUILDLINK_ABI_DEPENDS.polkit-qt5+=	polkit-qt5>=0.112.0
-BUILDLINK_PKGSRCDIR.polkit-qt5?=	../../wip/polkit-qt5
+BUILDLINK_API_DEPENDS.polkit-qt5-1+=	polkit-qt5-1>=0.112.0
+BUILDLINK_ABI_DEPENDS.polkit-qt5-1+=	polkit-qt5-1>=0.112.0
+BUILDLINK_PKGSRCDIR.polkit-qt5-1?=	../../wip/polkit-qt5
 
 .include "../../x11/qt5-dbus/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
@@ -15,4 +15,4 @@ BUILDLINK_PKGSRCDIR.polkit-qt5?=	../../wip/polkit-qt5
 .include "../../wip/polkit/buildlink3.mk"
 .endif	# POLKIT_QT5_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-polkit-qt5
+BUILDLINK_TREE+=	-polkit-qt5-1
