4ed7733e4954429afbe9389cb1e63e7d4fab97e8
Kamil Rytarowski <kamil@NetBSD.org>
2015-04-11 18:26:05 +0000

    Rename lxqt-mount to liblxqt-mount
    liblxqt-mount-0.9.0
    
    Library used to manage removable devices in the LXQt desktop.

--- liblxqt-mount/buildlink3.mk
+++ liblxqt-mount/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/04/11 15:58:49 krytarowski Exp $
+# $NetBSD: buildlink3.mk,v 1.1 2015/04/11 18:26:06 krytarowski Exp $
 
 BUILDLINK_TREE+=	liblxqt-mount
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	liblxqt-mount
 LIBLXQT_MOUNT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.liblxqt-mount+=	liblxqt-mount>=0.9.0
-BUILDLINK_PKGSRCDIR.liblxqt-mount?=	../../wip/lxqt-mount
+BUILDLINK_PKGSRCDIR.liblxqt-mount?=	../../wip/liblxqt-mount
 
 .include "../../x11/qt5-dbus/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
