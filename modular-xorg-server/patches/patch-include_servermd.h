594fb188a9ae58d76b5d9923e63ff877ff494946
Tobias Nygren <tnn@NetBSD.org>
2015-04-12 09:04:04 +0000

    Redo previous in a way that makes drivers build w/o hacks.

--- modular-xorg-server/patches/patch-include_servermd.h
+++ modular-xorg-server/patches/patch-include_servermd.h
@@ -1,4 +1,4 @@
-$NetBSD: patch-include_servermd.h,v 1.1 2015/04/11 21:27:14 tnn2 Exp $
+$NetBSD: patch-include_servermd.h,v 1.2 2015/04/12 09:04:04 tnn2 Exp $
 
 Try to fix broken X_BYTE_ORDER handling for big endian.
 Probably broken here:
@@ -6,11 +6,17 @@ http://cgit.freedesktop.org/xorg/xserver/commit/?id=4302484ef2e67b2e05756c7ee083
 
 --- include/servermd.h.orig	2015-01-17 23:42:52.000000000 +0000
 +++ include/servermd.h
-@@ -52,10 +52,10 @@ SOFTWARE.
+@@ -52,10 +52,16 @@ SOFTWARE.
  #error xserver code must include dix-config.h before any other headers
  #endif
  
 -#if X_BYTE_ORDER == X_LITTLE_ENDIAN
++#ifndef X_LITTLE_ENDIAN
++#define X_LITTLE_ENDIAN 1234
++#endif
++#ifndef X_BIG_ENDIAN
++#define X_BIG_ENDIAN 4321
++#endif
 +#if X_BYTE_ORDER == X_LITTLE_ENDIAN && defined(X_LITTLE_ENDIAN)
  #define IMAGE_BYTE_ORDER        LSBFirst
  #define BITMAP_BIT_ORDER        LSBFirst
