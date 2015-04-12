5c846e67b5b108dff7f8272911956520d28653d7
Tobias Nygren <tnn@NetBSD.org>
2015-04-12 11:50:52 +0000

    Fix --enable-glx-tls with clang. From FreeBSD.

--- MesaLib/options.mk
+++ MesaLib/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.22 2015/04/05 16:34:37 tnn2 Exp $
+# $NetBSD: options.mk,v 1.23 2015/04/12 11:50:52 tnn2 Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.MesaLib
 PKG_SUPPORTED_OPTIONS=		llvm dri
@@ -28,20 +28,11 @@ PLIST_VARS+=		dri swrast_dri i915_dri nouveau_dri i965_dri radeon_dri r200_dri
 
 .if !empty(PKG_OPTIONS:Mdri)
 
-# XXX this crashes clang:
-#
-#In file included from entry.c:49:
-#./entry_x86-64_tls.h:66:1: warning: tentative array definition assumed to have
-#      one element
-#x86_64_entry_start[];
-#^
-#fatal error: error in backend: symbol 'x86_64_entry_start' is already defined
-#clang: error: clang frontend command failed with exit code 70 (use -v to see invocation)
-#.if !empty(MACHINE_PLATFORM:MNetBSD-[0-5].*)
-#CONFIGURE_ARGS+=	--disable-glx-tls
-#.else
-#CONFIGURE_ARGS+=	--enable-glx-tls
-#.endif
+.if !empty(MACHINE_PLATFORM:MNetBSD-[0-5].*)
+CONFIGURE_ARGS+=	--disable-glx-tls
+.else
+CONFIGURE_ARGS+=	--enable-glx-tls
+.endif
 
 PLIST.dri=	yes
 BUILDLINK_DEPMETHOD.libpciaccess=      full
