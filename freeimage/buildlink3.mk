38e4cb11f84de2ffa08a897b7a119f12fe7bd7a8
Niclas Rosenvik <nros@NetBSD.org>
2015-04-14 19:04:39 +0000

    Updated freeimage to version 3.17.0.
    Changelog(release notes):
    
    Release 3.17.0 adds many improvements for ICO, TIFF, PNG, HDR, TGA, GIF, RAW,
    JXR, JP2, Exif plugins.It comes with new conversion functions for RGBA16/RGBAF
    formats, as well as new functions used to deal with external pixel buffers
    (ConvertFromRawBitsEx, CreateView). Other useful added functions are
    ReslaleRect, GetMemorySize, SetMetadataKeyValue and the FIQ_LFPQUANT
    quantizer algorithm. The library has been updated with the new LibPNG (1.6.16)
    , LibTIFF (4.0.4), LibRaw (0.17.a1), LibWebP (0.4.2), OpenEXR (2.2.0).

--- freeimage/buildlink3.mk
+++ freeimage/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/11/07 18:42:18 nros Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/04/14 19:04:39 nros Exp $
 
 BUILDLINK_TREE+=	freeimage
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	freeimage
 FREEIMAGE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.freeimage+=	freeimage>=3.15.4
+BUILDLINK_ABI_DEPENDS.freeimage+=	freeimage>=3.17.0
 BUILDLINK_PKGSRCDIR.freeimage?=	../../wip/freeimage
 .endif	# FREEIMAGE_BUILDLINK3_MK
 
