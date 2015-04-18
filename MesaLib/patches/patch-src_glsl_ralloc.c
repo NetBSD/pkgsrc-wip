0f0a77cdb663a12d5c03b475fc1941cd538c8706
Tobias Nygren <tnn@NetBSD.org>
2015-04-18 18:05:42 +0000

    restore some patches from pkgsrc related to exit time segfault of qt5 apps

---
+++ MesaLib/patches/patch-src_glsl_ralloc.c
@@ -0,0 +1,23 @@
+$NetBSD: patch-src_glsl_ralloc.c,v 1.1 2015/04/18 18:05:42 tnn2 Exp $
+
+* Fix exit time segfault of qt5 application with modular xorg
+
+--- src/util/ralloc.c.orig	2014-10-03 03:59:51.000000000 +0000
++++ src/util/ralloc.c
+@@ -285,7 +285,7 @@ ralloc_parent(const void *ptr)
+ 
+ static void *autofree_context = NULL;
+ 
+-static void
++static void __attribute__((__destructor__))
+ autofree(void)
+ {
+    ralloc_free(autofree_context);
+@@ -296,7 +296,6 @@ ralloc_autofree_context(void)
+ {
+    if (unlikely(autofree_context == NULL)) {
+       autofree_context = ralloc_context(NULL);
+-      atexit(autofree);
+    }
+    return autofree_context;
+ }
---
+++ MesaLib/patches/patch-src_mesa_main_context.c
@@ -0,0 +1,40 @@
+$NetBSD: patch-src_mesa_main_context.c,v 1.1 2015/04/18 18:05:42 tnn2 Exp $
+
+* Fix exit time segfault of qt5 application with modular xorg
+
+--- src/mesa/main/context.c.orig	2015-03-28 18:20:39.000000000 +0000
++++ src/mesa/main/context.c
+@@ -378,10 +378,10 @@ mtx_t OneTimeLock = _MTX_INITIALIZER_NP;
+  *
+  * \sa _math_init().
+  */
++static GLbitfield api_init_mask = 0x0;
+ static void
+ one_time_init( struct gl_context *ctx )
+ {
+-   static GLbitfield api_init_mask = 0x0;
+ 
+    mtx_lock(&OneTimeLock);
+ 
+@@ -428,14 +428,16 @@ one_time_init( struct gl_context *ctx )
+ 
+    mtx_unlock(&OneTimeLock);
+ 
+-   /* Hopefully atexit() is widely available.  If not, we may need some
+-    * #ifdef tests here.
+-    */
+-   atexit(_mesa_destroy_shader_compiler);
+-
+    dummy_enum_func();
+ }
+ 
++static void __attribute__((__destructor__))
++one_time_fini(void)
++{
++  if (api_init_mask)
++    _mesa_destroy_shader_compiler();
++}
++
+ 
+ /**
+  * Initialize fields of gl_current_attrib (aka ctx->Current.*)
