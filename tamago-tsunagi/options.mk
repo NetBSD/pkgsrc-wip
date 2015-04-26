5c2847616b42548b9698af2621bb57b034c87754
Makoto Fujiwara <makoto@ki.nu>
2015-04-26 10:14:45 +0000

    - Honor ${EMACS_PKGNAME_PREFIX} for PKGNAME - Add emacs*/modules.mk - Remove trailing empty lines

--- tamago-tsunagi/options.mk
+++ tamago-tsunagi/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2015/04/05 07:25:45 makoto Exp $
+# $NetBSD: options.mk,v 1.3 2015/04/26 10:14:45 makoto Exp $
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tamago-tsunagi
 PKG_SUPPORTED_OPTIONS=	freewnn canna sj3 anthy
 PKG_SUGGESTED_OPTIONS=	freewnn canna sj3 anthy
@@ -20,5 +20,3 @@ DEPENDS+=	sj3-server-[0-9]*:../../inputmethod/sj3-server
 .if !empty(PKG_OPTIONS:Manthy)
 DEPENDS+=	anthy-[0-9]*:../../inputmethod/anthy
 .endif
-
-
