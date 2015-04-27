cd5a85197f427ac5a63bb8b63fcf8a03235e42bc
Jonathan Buschmann <jonthn+pkgsrc@pinacea.com>
2015-04-27 21:17:15 +0000

    Import ctags-5.8nb20150427 as wip/ctags-git.
    Exuberant Ctags generates an index (or tag) file of source language
    objects in source files that allows these items to be quickly and
    easily located by a text editor or other utility.
    
    Alternatively, it can generate a cross reference file which lists,
    in human-readable form, information about the various objects found
    in a set of source code files.
    
    Supported languages include: Ada, Assembler, ASP, Awk, C, C++, Clojure, CSS,
    BETA, COBOL, D, Eiffel, Falcon, Fortran, Go, Java, Json, Lisp, Make, Pascal,
    Perl, PHP, Python, REXX, Ruby, Rust, Scheme, Shell scripts (Bourne/Korn/Z),
    S-Lang, Tcl, Vim and YACC/Bison.

---
+++ ctags-git/options.mk
@@ -0,0 +1,27 @@
+# $NetBSD: options.mk,v 1.1 2015/04/27 21:17:15 jonthn Exp $
+
+PKG_OPTIONS_VAR=                PKG_OPTIONS.ctags
+PKG_SUPPORTED_OPTIONS=          exctags emacs
+
+.include "../../mk/bsd.options.mk"
+
+# Package-specific option-handling
+
+###
+### Use prefixed program name 'exctags'
+###
+.if !empty(PKG_OPTIONS:Mexctags)
+CONFIGURE_ARGS+=	--program-prefix=ex
+PLIST_SUBST+=		PREFIX_PROGRAM="ex"
+.else
+PLIST_SUBST+=		PREFIX_PROGRAM=""
+.endif
+
+###
+### Generate tool/link for emacs support
+###
+PLIST_VARS+=	emacs
+.if !empty(PKG_OPTIONS:Memacs)
+CONFIGURE_ARGS+=	--enable-etags
+PLIST.emacs=	yes
+.endif
