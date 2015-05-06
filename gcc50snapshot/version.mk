f03b55061572dba902f9546aef63a8fa05d4c87c
Kai-Uwe Eckhardt <keckhardt@users.sourceforge.net>
2015-05-06 05:25:00 +0000

    Update to GCC 5.1.1 snapshot 20150428.

--- gcc50snapshot/version.mk
+++ gcc50snapshot/version.mk
@@ -1,2 +1,2 @@
-# $NetBSD: version.mk,v 1.20 2015/04/07 20:55:44 keckhardt Exp $
-GCC50S_DIST_VERSION:=5-20150329
+# $NetBSD: version.mk,v 1.21 2015/05/06 05:25:00 keckhardt Exp $
+GCC50S_DIST_VERSION:=5-20150428
