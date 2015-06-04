9b6f2a7487ae89b1d97b43fa05e231c23a23bc74
Thomas Klausner <wiz@NetBSD.org>
2015-06-04 13:11:03 +0000

    Make patches apply again. Does not build for me on NetBSD due to: configure: error: System SQLite library is not compiled with SQLITE_ENABLE_DBSTAT_VTAB.

--- firefox-hg/patches/patch-dom_media_AudioStream.cpp
+++ firefox-hg/patches/patch-dom_media_AudioStream.cpp
@@ -1,9 +1,9 @@
-$NetBSD: patch-dom_media_AudioStream.cpp,v 1.1 2014/10/27 22:53:23 thomasklausner Exp $
+$NetBSD: patch-dom_media_AudioStream.cpp,v 1.2 2015/06/04 13:11:03 thomasklausner Exp $
 
---- dom/media/AudioStream.cpp.orig	2014-10-27 20:46:32.000000000 +0000
+--- dom/media/AudioStream.cpp.orig	2015-06-04 12:35:48.000000000 +0000
 +++ dom/media/AudioStream.cpp
-@@ -14,7 +14,6 @@
- #include "mozilla/Mutex.h"
+@@ -15,7 +15,6 @@
+ #include "mozilla/Snprintf.h"
  #include <algorithm>
  #include "mozilla/Telemetry.h"
 -#include "soundtouch/SoundTouch.h"
