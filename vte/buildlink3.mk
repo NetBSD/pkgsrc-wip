db79f3be8dc2870657424d7d2531c06a61bf24ab
Kamil Rytarowski <kamil@NetBSD.org>
2015-06-04 23:17:14 +0000

    Import pristine vte-0.28.1 from x11/vte
    Terminal widget with improved font, internationalization and
    accessibility support for the GNOME 2 desktop.

---
+++ vte/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.1 2015/06/04 23:17:14 krytarowski Exp $
+
+BUILDLINK_TREE+=	vte
+
+.if !defined(VTE_BUILDLINK3_MK)
+VTE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.vte+=	vte>=0.14.0
+BUILDLINK_ABI_DEPENDS.vte+=	vte>=0.28.1nb16
+BUILDLINK_PKGSRCDIR.vte?=	../../x11/vte
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/pango/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../mk/termcap.buildlink3.mk"
+.endif # VTE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-vte
