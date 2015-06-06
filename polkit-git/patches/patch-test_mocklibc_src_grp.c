abed1923a6ac3f0b7d4cd927c905717e04a1df83
Kamil Rytarowski <kamil@NetBSD.org>
2015-06-06 23:10:23 +0000

    Import polkit-0.112 from wip/polkit as wip/polkit-git
    It will be used for tracking the git release.
    
    PolicyKit is a toolkit for defining and handling authorizations.  It
    is used for allowing unprivileged processes to speak to privileged
    processes.

---
+++ polkit-git/patches/patch-test_mocklibc_src_grp.c
@@ -0,0 +1,19 @@
+$NetBSD: patch-test_mocklibc_src_grp.c,v 1.1 2015/06/06 23:10:23 krytarowski Exp $
+
+* XXX: no fgetgrent(3)
+
+--- test/mocklibc/src/grp.c.orig	2013-02-02 03:22:52.000000000 +0000
++++ test/mocklibc/src/grp.c
+@@ -27,6 +27,12 @@
+ 
+ static FILE *global_stream = NULL;
+ 
++#ifdef __NetBSD__
++static struct group *fgetgrent(FILE *stream) {
++  return NULL;
++}
++#endif
++
+ void setgrent(void) {
+   if (global_stream)
+     endgrent();
