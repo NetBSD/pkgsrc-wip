4eed7b73c950d31579ef2e386328959aafb0f421
Leonardo Taccari <leot@NetBSD.org>
2015-06-13 02:16:06 +0000

    Update wip/zathura-mupdf to zathura-mupdf-0.2.8.
    Changes:
     * Update for mupdf 1.7 API changes
     * Locate and extract images from pages
     * Parse entries in the document information dictionary
     * Handle additional link types in the outline
     * Enable EPUB support

--- zathura-mupdf/patches/patch-document.c
+++ zathura-mupdf/patches/patch-document.c
@@ -1,57 +1,15 @@
-$NetBSD: patch-document.c,v 1.1 2015/04/18 17:09:54 leot1990 Exp $
+$NetBSD: patch-document.c,v 1.2 2015/06/13 02:16:07 leot1990 Exp $
 
-Add mupdf-1.7 support.
+Correct the glib-2.0 include header file path.
 
---- document.c.orig	2014-10-16 22:21:41.000000000 +0000
+--- document.c.orig	2015-05-27 15:18:11.000000000 +0000
 +++ document.c
-@@ -51,14 +51,14 @@ pdf_document_open(zathura_document_t* do
-   }
+@@ -6,7 +6,7 @@
+ #include <mupdf/xps.h>
+ #include <mupdf/pdf.h>
  
-   /* authenticate if password is required and given */
--  if (fz_needs_password(mupdf_document->document) != 0) {
--    if (password == NULL || fz_authenticate_password(mupdf_document->document, (char*) password) == 0) {
-+  if (fz_needs_password(mupdf_document->ctx, mupdf_document->document) != 0) {
-+    if (password == NULL || fz_authenticate_password(mupdf_document->ctx, mupdf_document->document, (char*) password) == 0) {
-       error = ZATHURA_ERROR_INVALID_PASSWORD;
-       goto error_free;
-     }
-   }
+-#include <glib-2.0/glib.h>
++#include <glib.h>
  
--  zathura_document_set_number_of_pages(document, fz_count_pages(mupdf_document->document));
-+  zathura_document_set_number_of_pages(document, fz_count_pages(mupdf_document->ctx, mupdf_document->document));
-   zathura_document_set_data(document, mupdf_document);
+ #include "plugin.h"
  
-   return error;
-@@ -67,10 +67,10 @@ error_free:
- 
-   if (mupdf_document != NULL) {
-     if (mupdf_document->document != NULL) {
--      fz_close_document(mupdf_document->document);
-+      fz_drop_document(mupdf_document->ctx, mupdf_document->document);
-     }
-     if (mupdf_document->ctx != NULL) {
--      fz_free_context(mupdf_document->ctx);
-+      fz_drop_context(mupdf_document->ctx);
-     }
- 
-     free(mupdf_document);
-@@ -90,8 +90,8 @@ pdf_document_free(zathura_document_t* do
-     return ZATHURA_ERROR_INVALID_ARGUMENTS;
-   }
- 
--  fz_close_document(mupdf_document->document);
--  fz_free_context(mupdf_document->ctx);
-+  fz_drop_document(mupdf_document->ctx, mupdf_document->document);
-+  fz_drop_context(mupdf_document->ctx);
-   free(mupdf_document);
-   zathura_document_set_data(document, NULL);
- 
-@@ -112,7 +112,7 @@ pdf_document_save_as(zathura_document_t*
-      * options. */
- 
-     fz_write_options opts = { 0 }; /* just use the default options */
--    fz_write_document(mupdf_document->document, (char*) path, &opts);
-+    fz_write_document(mupdf_document->ctx, mupdf_document->document, (char*) path, &opts);
-   } fz_catch (mupdf_document->ctx) {
-     return ZATHURA_ERROR_UNKNOWN;
-   }
