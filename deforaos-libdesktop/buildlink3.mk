6aa9f11ec916dd932447c9186a539244942eb154
Pierre Pronchery <khorben@defora.org>
2015-06-13 21:01:39 +0000

    Bump the buildlink API as well

--- deforaos-libdesktop/buildlink3.mk
+++ deforaos-libdesktop/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2015/01/27 00:10:05 khorben Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2015/06/13 21:01:39 khorben Exp $
 #
 
 BUILDLINK_TREE+=	deforaos-libdesktop
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	deforaos-libdesktop
 .if !defined(DEFORAOS_LIBDESKTOP_BUILDLINK3_MK)
 DEFORAOS_LIBDESKTOP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.1.0
+BUILDLINK_API_DEPENDS.deforaos-libdesktop+=	deforaos-libdesktop>=0.2.0
 BUILDLINK_PKGSRCDIR.deforaos-libdesktop?=	../../wip/deforaos-libdesktop
 
 .include "../../devel/deforaos-libsystem/buildlink3.mk"
