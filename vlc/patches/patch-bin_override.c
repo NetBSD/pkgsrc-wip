0eced965fc327ddd9e6dab84bf23ca305656724d
Ian D. Leroux <idleroux@fastmail.fm>
2015-07-05 12:38:39 +0000

    Remove unnecessary patches, briefly document patch-compat_Makefile.in
    At this point, vlc (with video output set to "X11 video output (XCB)")
    works for me.

--- vlc/patches/patch-bin_override.c
+++ vlc/patches/patch-bin_override.c
@@ -1,4 +1,4 @@
-$NetBSD: patch-bin_override.c,v 1.1 2015/05/01 06:24:13 idleroux Exp $
+$NetBSD: patch-bin_override.c,v 1.2 2015/07/05 12:38:39 idleroux Exp $
 
 --- bin/override.c.orig	2014-06-14 15:14:12.000000000 +0000
 +++ bin/override.c
@@ -11,12 +11,3 @@ $NetBSD: patch-bin_override.c,v 1.1 2015/05/01 06:24:13 idleroux Exp $
   && (defined (__ELF__) && !defined (__sun__))
  /* Solaris crashes on printf("%s", NULL); which is legal, but annoying. */
  
-@@ -125,7 +125,7 @@ static void *getsym (const char *name)
-  *
-  * Some evil libraries modify the environment. We currently ignore the calls as
-  * they could crash the process. This may cause funny behaviour though. */
--int putenv (char *str)
-+int putenv (const char *str)
- {
-     if (override)
-     {
