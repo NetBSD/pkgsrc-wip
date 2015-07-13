0b4c05414d21bfd6c30043a21a9611b87057fd84
Thomas Klausner <wiz@NetBSD.org>
2015-07-13 15:11:25 +0000

    Update to 0.9.12, switch to py-boost.
    2015-07-07  0.9.12 Cristy  <quetzlzacatenango@image...>
      * Ensure PythonMagick compiles against ImageMagick 6.9.1-0 and above.
    
    2014-08-13  0.9.11 Cristy  <quetzlzacatenango@image...>
      * Ensure PythonMagick compiles against ImageMagick 6.8.9-6 and above.
    
    2014-03-16  0.9.10 Cristy  <quetzlzacatenango@image...>
      * Support  MagickCore::ColorspaceType enum (reference
        http://www.imagemagick.org/discourse-server/viewtopic.php?f=2&t=25229).
    
    2014-01-06  0.9.9 Cristy  <quetzlzacatenango@image...>
      * Update uto latest autoconf / automake macros.
    
    2012-08-18  0.9.8 Cristy  <quetzlzacatenango@image...>
      * Support 'strip' method reference
        http://www.imagemagick.org/discourse-server/viewtopic.php?f=2&t=21896).
    
    2011-08-10  0.9.7 Cristy  <quetzlzacatenango@image...>
      * Fix problems with class inheritance for the Drawable* classes (reference
        http://www.imagemagick.org/discourse-server/viewtopic.php?f=1&t=19278).
    
    2011-03-10  0.9.6 Cristy  <quetzlzacatenango@image...>
      * Boost causes an ambiguity with ssize_t when building PythonMagick.  Add
        patch from nico.schloemer@gmail... to fix the build exception.
    
    2011-03-10  0.9.5 Cristy  <quetzlzacatenango@image...>
      * Boost causes an ambiguity with ssize_t when building PythonMagick.  Add
        patch from Arfrever@gento... to fix the build exception.

--- py-PythonMagick/buildlink3.mk
+++ py-PythonMagick/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2010/06/13 22:47:14 thomasklausner Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2015/07/13 15:11:25 thomasklausner Exp $
 
 BUILDLINK_TREE+=	py-PythonMagick
 
@@ -10,7 +10,7 @@ BUILDLINK_ABI_DEPENDS.py-PythonMagick?=	${PYPKGPREFIX}-PythonMagick>=0.9.1nb3
 BUILDLINK_PKGSRCDIR.py-PythonMagick?=	../../wip/py-PythonMagick
 
 .include "../../graphics/ImageMagick/buildlink3.mk"
-.include "../../devel/boost-python/buildlink3.mk"
+.include "../../devel/py-boost/buildlink3.mk"
 .endif	# PY_PYTHONMAGICK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-py-PythonMagick
