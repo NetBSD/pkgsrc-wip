72c5cea987b8bfc6b52a1602f8b723f7ad9513c7
Jonathan Buschmann <jonthn+pkgsrc@pinacea.com>
2015-08-10 22:59:31 +0000

    Update to 0.0.38
    Changes since 0.0.37:
    
        support async command and eval #111
        call vimscript functions using vim_call_function #130
        fix AttributeError due to ui events change #133
    
    Changes since 0.0.36:
    
        handle decoding for rplugin methods the same as for api calls. #129

--- py-neovim/patches/patch-setup.py
+++ py-neovim/patches/patch-setup.py
@@ -1,10 +1,10 @@
-$NetBSD: patch-setup.py,v 1.1 2015/06/23 21:41:02 jonthn Exp $
+$NetBSD: patch-setup.py,v 1.2 2015/08/10 22:59:31 jonthn Exp $
 
 Remove installation of experimental pynvim
 
---- setup.py.orig	2015-06-23 21:22:56.000000000 +0000
-+++ setup.py
-@@ -29,11 +29,6 @@ if platform.python_implementation() != '
+--- setup.py.orig	2015-08-06 23:39:20.000000000 +0200
++++ setup.py	2015-08-11 00:28:58.000000000 +0200
+@@ -29,11 +29,6 @@
          except ImportError:
              pass
  
@@ -14,5 +14,5 @@ Remove installation of experimental pynvim
 -    entry_points['console_scripts'] = ['pynvim=neovim.ui.cli:main [GUI]']
 -
  setup(name='neovim',
-       version='0.0.36',
+       version='0.0.38',
        description='Python client to neovim',
