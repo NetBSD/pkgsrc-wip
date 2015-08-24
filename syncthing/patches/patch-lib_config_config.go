7b1f8f90dc4e7de86e208d4bfd998105b2b5c653
Thomas Klausner <wiz@NetBSD.org>
2015-08-24 10:46:20 +0000

    Update to 0.11.21. Fixes build with go-1.5.
    Carry over patch from previous pkgsrc version, even though it only
    changes a config default value (without adapting the docs).
    
    Changes:
    v0.11.21
    
        Recover from 'corrupted or incomplete CURRENT file' etc (#2017, @calmh)
        Retain standard streams over restart (#2155, @calmh)
        Add unit test for staggered versioning (#2165, @Zillode)
        Newest first should be different from oldest first (#2161, @calmh)
        Fix time zone error in staggered versioning (#2165, @alex2108)
    
    v0.11.20
    
        Rename 'delete' to 'remove' (#1007, @Zillode)
        Fix editing device address (#2129, @calmh)
        Use grid instead of three-column (#2130, @calmh)
        Stop folder when running out of disk space (#2057, @Zillode)
        Remove double slashes in directives (#2143, @uok)
        Change to Font Awesome icon font (#2138, @uok)
    
    v0.11.19
    
        Properly rename config files during integration tests (#1769, @Zillode)
        Add timeout for peek (#1035, @AudriusButkevicius)
        Handle multiple case insensitivity prefixes in ignores (#2134, @calmh)
    
    v0.11.18
    
        Update credits for dependencies (#2082, @Zillode)
        Use protocol provided buffers for requests (#1157, @AudriusButkevicius)
    
    v0.11.17
    
        Determine conflict winner based on change type and modification time (#1848, @calmh)
        Optionally ignore remote deletes (#1254, @calmh)
        Add devices without restart (#2083, @calmh)
        Add folders without restart (#2063, @calmh)
    
    v0.11.16
    
        Allow #editIgnores to scroll in browser (#2041, @canton7)
        Don't let folder ID escape into HTML tag ID:s (#2059, @calmh)
        Improve info text for device addresses (#2044, @uok)
    
    v0.11.15
    
        v0.11.14 doesn't correctly save its config on Windows. This fixes that. Woops. :confused:
        Revert "Merge pull request #2053 from calmh/atomicwriter" (#2058, @calmh)
    
    
    v0.11.14
    
        Fix advanced option saving (#2042, @AudriusButkevicius)
        Update protocol package (#2040, @AudriusButkevicius)
        Update panic instructions (#2039, @AudriusButkevicius)
        Remove one of two emits of events.DeviceConnected (#2034, @calmh)
    
    
    v0.11.13
    
        Use bootstrap grid instead of column-count:3 for aligning checkboxes (#2029, @canton7)
    
    
    v0.11.12
    
        Show FolderErrors result in UI (#1437, @calmh)
        Fix CSRF tests (#2009, @AudriusButkevicius)
        Fixed add device button being overlapped by footer (#1950, @brgmnn)
        Preserve setgid bit on local directories (#2012, @brgmnn)
        Implement "advanced configuration" dialog (#2010, @calmh)
        Add link to home page (#1993, #1999, @calmh)
    
    v0.11.11
    
        Serialize scans and pulls (#1391, @calmh)
        Don't rename duplicate folders (#1675, @calmh)
        Trim string slices upon loading config (#1750, @AudriusButkevicius)
        Make ping timeout configurable (#1751, @AudriusButkevicius)
    
    v0.11.10
    
        Dont warn about irrelevant symlinks, print path to culprit (#1945, @calmh)
        Add label next to "Last file received" (#1952, @AudriusButkevicius)
        Corrected API response when resetting folder (#1976, @Zillode)
        Don't start model until services have been added (#1969, @calmh)
        Sanitize rescan interval values (#1978, @calmh)
    
    v0.11.9
    
        Start folders before GUI/API (#1918, @calmh)
        Correctly handle nil error in verbose logging (#1921, @calmh)
        More resilient broadcast handling (#1907, @calmh)
        Add trash can file versioning (#1931, @calmh)
        Correctly set and clear errors for missing folders (#1937, @AudriusButkevicius)

--- syncthing/patches/patch-lib_config_config.go
+++ syncthing/patches/patch-lib_config_config.go
@@ -1,8 +1,8 @@
-$NetBSD: patch-internal-config-config.go,v 1.2 2015/04/25 16:59:22 tnn2 Exp $
+$NetBSD: patch-lib_config_config.go,v 1.1 2015/08/24 10:46:20 thomasklausner Exp $
 
---- internal/config/config.go.orig	2015-04-22 00:37:18.000000000 +0000
-+++ internal/config/config.go
-@@ -233,7 +233,7 @@ type OptionsConfiguration struct {
+--- lib/config/config.go.orig	2015-08-16 13:29:48.000000000 +0000
++++ lib/config/config.go
+@@ -229,7 +229,7 @@ type OptionsConfiguration struct {
  	URAccepted              int      `xml:"urAccepted" json:"urAccepted"` // Accepted usage reporting version; 0 for off (undecided), -1 for off (permanently)
  	URUniqueID              string   `xml:"urUniqueID" json:"urUniqueId"` // Unique ID for reporting purposes, regenerated when UR is turned on.
  	RestartOnWakeup         bool     `xml:"restartOnWakeup" json:"restartOnWakeup" default:"true"`
