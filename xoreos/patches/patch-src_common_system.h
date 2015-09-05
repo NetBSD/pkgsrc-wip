7e8ed5533a562f6750c701525460c63d946266b6
Yorick Hardy <yorickhardy@gmail.com>
2015-09-05 23:04:36 +0000

    wip/xoreos: Note and/or integrate patches from the github repo.

--- xoreos/patches/patch-src_common_system.h
+++ xoreos/patches/patch-src_common_system.h
@@ -1,7 +1,9 @@
-$NetBSD: patch-src_common_system.h,v 1.1 2015/09/05 16:05:10 yhardy Exp $
+$NetBSD: patch-src_common_system.h,v 1.2 2015/09/05 23:04:36 yhardy Exp $
 
 Make sure that std::rand() is defined for all the files
-where it is needed.
+where it is needed. Should actually be done this way:
+
+https://github.com/xoreos/xoreos/commit/d678a6f7f9a9850123a512da89efe6f3a7d3e0e1
 
 --- src/common/system.h.orig	2015-07-27 08:05:52.000000000 +0000
 +++ src/common/system.h
