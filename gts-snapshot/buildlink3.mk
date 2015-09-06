eeb2b96095b0a4909dd82d2bb57a17a9230bc4b8
Sergey Litvinov <slitvinov@users.sourceforge.net>
2015-09-06 14:31:49 +0000

    Update of pkgsrc files

--- gts-snapshot/buildlink3.mk
+++ gts-snapshot/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/08/30 22:42:37 slitvinov Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2015/09/06 14:31:49 slitvinov Exp $
 
 BUILDLINK_TREE+=	gts
 
@@ -8,9 +8,8 @@ GTS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gts+=	gts>=0.7.6
 BUILDLINK_PKGSRCDIR.gts?=	../../wip/gts-snapshot
 
-
 .include "../../devel/glib2/buildlink3.mk"
-
+.include "../../graphics/netpbm/buildlink3.mk"
 .endif	# GTS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gts
