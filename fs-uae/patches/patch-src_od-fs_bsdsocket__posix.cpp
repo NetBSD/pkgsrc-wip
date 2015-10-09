f48bf281bd2179690bed9724c564562d32a65537
Olaf Seibert <rhialto@falu.nl>
2015-10-09 12:43:25 +0200

    Update fs-uae to release 2.6.1.
    From http://fs-uae.net/2015/09/20/fs-uae-2-6-1-released:
    
    Improvements over version 2.4.x include:
    
        Updated emulation code from WinUAE 3.1.0.
        Support for PPC emulation (can run AmigaOS 4.x) via QEMU-UAE plugin.
        More emulated hardware support (Toccata, A2061, Picasso IV, #).
        Better support for modern OS versions due to using SDL 2.0.
        Built-in support for multiple displays, and fixes for HiDPI displays.
        The Launcher supports modern OS versions better due to using QT 5.x.
        Much improved FS-UAE Launcher user interface.
        FS-UAE mouse integration (Workbench cursor follows host cursor).
        Updated internal kickstart replacement from AROS.
        Dual joystick/mouse mode for joystick port 0.
        Official builds for SteamOS (Linux + Steam runtime).
        OS X apps are now digitally signed (Apple Developer ID).
        Support for JIT direct memory access (much faster JIT).
        Many bug fixes.
        # and a lot of other improvements. See the changelog entries for (But for all details concerning changes since 2.4.1, you can read through the changelogs for FS-UAE and FS-UAE Launcher for all the details.
    
    Support for IPF disk images is now provided by a plugin which you need to download separately. [[ not included in pkgsrc ]]

--- fs-uae/patches/patch-src_od-fs_bsdsocket__posix.cpp
+++ fs-uae/patches/patch-src_od-fs_bsdsocket__posix.cpp
@@ -1,10 +1,10 @@
-$NetBSD: patch-src_od-fs_bsdsocket__posix.cpp,v 1.1 2013/10/25 22:02:17 rhialto Exp $
+$NetBSD: patch-src_od-fs_bsdsocket__posix.cpp,v 1.5 2014/12/24 20:17:47 rhialto Exp $
 
 u_int8_t * won't automagically become const char *.
 
---- src/od-fs/bsdsocket_posix.cpp.orig	2013-06-25 19:21:16.000000000 +0000
+--- src/od-fs/bsdsocket_posix.cpp.orig	2015-09-20 19:51:19.000000000 +0000
 +++ src/od-fs/bsdsocket_posix.cpp
-@@ -929,7 +929,7 @@ static void *bsdlib_threadfunc (void *ar
+@@ -926,7 +926,7 @@ static void *bsdlib_threadfunc (void *ar
          break;
  
          case 7: {
