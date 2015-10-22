b0f9721c6cbe7b49d9e8fa7440a76d478da2642f
Pierre Pronchery <khorben@defora.org>
2015-10-23 00:39:31 +0200

    Package olsrd 0.6.8
    The CHANGELOG is very big, and a lot happened since then too.

--- olsrd/patches/patch-src_cfgparser_local.mk
+++ olsrd/patches/patch-src_cfgparser_local.mk
@@ -2,7 +2,7 @@ $NetBSD: patch-src_cfgparser_local.mk,v 1.1 2012/09/28 22:44:11 khorben Exp $
 
 Fixed invocation of flex
 
---- src/cfgparser/local.mk.orig	2010-03-31 17:03:53.000000000 +0000
+--- src/cfgparser/local.mk.orig	2014-12-29 11:17:09.000000000 +0000
 +++ src/cfgparser/local.mk
 @@ -45,6 +45,8 @@ SRCS += $(foreach file,olsrd_conf oparse
  OBJS += $(foreach file,olsrd_conf oparse oscan cfgfile_gen,$(C)$(file).o)
@@ -11,5 +11,5 @@ Fixed invocation of flex
 +FLEX ?= flex
 +
  $(C)oscan.c: $(C)oscan.lex $(C)Makefile
- 	$(FLEX) -Cem -o"$@-tmp" "$<"
- 	sed	-e '/^static/s/yy_get_next_buffer[\(][\)]/yy_get_next_buffer(void)/' \
+ ifeq ($(VERBOSE),0)
+ 	@echo "[FLEX] $@"
