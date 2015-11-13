6160aad9d819145e89e2dba81e7ecfc7b53c3e33
Leonardo Taccari <leot@NetBSD.org>
2015-11-13 10:39:23 +0100

    glfw: Update wip/glfw to 3.1.2.
    Changes:
    3.1.2
    -----
    GLFW 3.1.2 is available for download. It adds fixes for a large number of bugs
    that together affect all supported platforms, as well as dynamic loading work
    that simplifies compilation and linking.
    
    3.1.1
    -----
    GLFW 3.1.1 is available for download. It adds fixes for a number of bugs that
    together affect all supported platforms, most notably workarounds for bugs in
    some popular window managers.
    
    3.1
    ---
    GLFW 3.1 is available for download. It adds improved documentation, support for
    custom system cursors, file drop events, main thread wake-up, window frame size
    retrieval, floating windows, character input with modifier keys, single buffered
    windows, build improvements and fixes for a large number of bugs.
    
    3.0.4
    -----
    GLFW 3.0.4 is available for download. It adds fixes for a number of bugs that
    together affect all supported platforms, most notably MinGW compilation issues
    and cursor mode issues on OS X.
    
    3.0.3
    -----
    GLFW 3.0.3 is available for download. It adds fixes for a number of bugs that
    together affect all supported platforms, most notably MinGW compilation issues
    and cursor mode issues on OS X.
    
    3.0.2
    -----
    GLFW 3.0.2 is available for download. It adds support for OpenGL 4 and precise
    scrolling deltas on OS X and fixes for a number of bugs that together affect all
    supported platforms.
    
    3.0.1
    -----
    GLFW 3.0.1 is available for download. It adds fixes for a number of bugs that
    together affect all supported platforms.
    
    3.0
    ---
    GLFW 3.0 is available for download. It adds support for multiple windows,
    multiple monitors, clipboard text I/O, gamma ramps, better error reporting, more
    event callbacks, undecorated windows, joystick names, a new build system using
    CMake, UTF-8 for all strings, experimental support for EGL and OpenGL ES and
    more.

--- glfw/buildlink3.mk
+++ glfw/buildlink3.mk
@@ -5,13 +5,16 @@ BUILDLINK_TREE+=	glfw
 .if !defined(GLFW_BUILDLINK3_MK)
 GLFW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.glfw+=	glfw>=2.7
+BUILDLINK_API_DEPENDS.glfw+=	glfw>=3.1.2
 BUILDLINK_PKGSRCDIR.glfw?=	../../wip/glfw
 
-#.include "../../graphics/glu/buildlink3.mk"
-#.include "../../graphics/MesaLib/buildlink3.mk"
-#.include "../../x11/libX11/buildlink3.mk"
-#.include "../../x11/libXrandr/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../x11/libICE/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXrandr/buildlink3.mk"
+.include "../../x11/libxkbcommon/buildlink3.mk"
 .endif	# GLFW_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-glfw
