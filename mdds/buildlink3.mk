c7eac8b6c5b7a6ea4aa1341dd278131833ac0804
Leonardo Taccari <leot@NetBSD.org>
2015-12-02 20:52:14 +0100

    mdds: simplify and misc cosmetic fixes
    o Simplify the package: get rid of our do-install and use the upstream target
    instead (needed "gmake") and update PLIST accordingly
    o Update the HOMEPAGE
    o Get rid of various comments in buildlink3.mk and uncomment boost-libs bl3.
    o Minor cosmetic fixes thanks to pkglint -Wall.

--- mdds/buildlink3.mk
+++ mdds/buildlink3.mk
@@ -1,4 +1,5 @@
 # $NetBSD$
+
 BUILDLINK_TREE+=	mdds
 
 .if !defined(MDDS_BUILDLINK3_MK)
@@ -7,14 +8,7 @@ MDDS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.mdds+=	mdds>=1.0.0
 BUILDLINK_PKGSRCDIR.mdds?=	../../wip/mdds
 
-# XXX
-# XXX Uncomment and keep only the buildlink3 lines below which are directly
-# XXX needed for dependencies to compile, link, and run.  If this package
-# XXX provides a wrappered API or otherwise does not expose the APIs of the
-# XXX buildlink3 lines below to dependencies, remove them.
-# XXX
-#.include "../../devel/boost-headers/buildlink3.mk"
-#.include "../../devel/boost-libs/buildlink3.mk"
+.include "../../devel/boost-libs/buildlink3.mk"
 .endif	# MDDS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mdds
