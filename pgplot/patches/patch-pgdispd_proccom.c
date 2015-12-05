b490ff8ed7e3205dd450b739eb129b208e0d618c
Sergey Litvinov <slitvinov@gmail.com>
2015-12-05 18:09:45 +0000

    Add missing dependency. Remove unused header.

--- pgplot/patches/patch-pgdispd_proccom.c
+++ pgplot/patches/patch-pgdispd_proccom.c
@@ -4,7 +4,18 @@ Compiler was complaining about an extra definition
 
 --- pgdispd/proccom.c.orig	1994-08-22 20:33:35.000000000 +0000
 +++ pgdispd/proccom.c
-@@ -163,7 +163,6 @@ int *retbuflen;	/* the length of retbuf
+@@ -93,10 +93,6 @@ static char rcsid[]="@(#)$Id: proccom.c,
+ #include <sys/types.h>
+ #include <netinet/in.h>
+ 
+-#ifndef VMS
+-#include <values.h>
+-#endif
+-
+ #ifdef sun
+ #include <memory.h>
+ #endif
+@@ -163,7 +159,6 @@ int *retbuflen;	/* the length of retbuf 
  	void drawline();	/* draw a line in the bitmap window */
  	Pixmap XCreatePixmap();
  
