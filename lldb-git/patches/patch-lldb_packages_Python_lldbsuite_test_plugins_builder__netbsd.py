a798c6f86d103f9a1e02cabc098058dd8112f740
Kamil Rytarowski <n54@gmx.com>
2015-12-05 23:01:07 +0100

    lldb-git: Remove merged patches upstream

--- lldb-git/patches/patch-lldb_packages_Python_lldbsuite_test_plugins_builder__netbsd.py
+++
@@ -1,9 +0,0 @@
-$NetBSD$
-
---- lldb/packages/Python/lldbsuite/test/plugins/builder_netbsd.py.orig	2015-11-30 03:53:10.000000000 +0000
-+++ lldb/packages/Python/lldbsuite/test/plugins/builder_netbsd.py
-@@ -0,0 +1,4 @@
-+from builder_base import *
-+
-+def buildDsym(sender=None, architecture=None, compiler=None, dictionary=None, clean=True):
-+    return False
