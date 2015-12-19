69cf48e566157060c583444e57fc0a38973adb48
Kamil Rytarowski <n54@gmx.com>
2015-12-20 00:38:24 +0100

    pulseaudio-git: Revamp patch for eliminating __WORDSIZE
    Proposed COMMIT_MSG for upstream:
    
    NetBSD: Stop depending upon nonstandard __WORDSIZE
    
    There is no way to check CPU type in a portable way across ABIs.
    
    Assume if pointers are 64-bit that CPU is capable to perform fast
    64-bit operations. Add an extra check to handle x32-ABI.
    
    PulseAudio by default builds with -Wundef. If we add -Werror=undef this
    missing define is fatal. By default build log is full of entries like:
    
    In file included from ./pulsecore/core.h:47:0,
                     from ./pulsecore/module.h:31,
                     from ./pulsecore/sink-input.h:31,
                     from pulsecore/sound-file-stream.c:36:
    ./pulsecore/sample-util.h: In function 'pa_mult_s16_volume':
    ./pulsecore/sample-util.h:58:5: warning: "__WORDSIZE" is not defined [-Wundef]
     #if __WORDSIZE == 64 || ((ULONG_MAX) > (UINT_MAX))
         ^
    
    (NetBSD-7.99.21 with default GCC 4.8.5)
    
    This change fixes build issues on NetBSD.
    
    This also address a bug reported by Shawn Walker from Oracle (possibly Solaris):
    Bug 90880 - builds can fail due to non-portable glibc-specific internal macro usage

--- pulseaudio-git/patches/patch-src_pulsecore_sample-util.h
+++ pulseaudio-git/patches/patch-src_pulsecore_sample-util.h
@@ -1,21 +1,13 @@
-$NetBSD: patch-src_pulsecore_sample-util.h,v 1.1 2014/06/09 13:08:19 ryoon Exp $
+$NetBSD$
 
-Define glibc-specific (?) __WORDSIZE.
-
---- src/pulsecore/sample-util.h.orig	2015-11-28 09:05:41.000000000 +0000
+--- src/pulsecore/sample-util.h.orig	2015-12-05 22:03:14.000000000 +0000
 +++ src/pulsecore/sample-util.h
-@@ -32,6 +32,14 @@
- #include <pulsecore/memblock.h>
- #include <pulsecore/memchunk.h>
+@@ -55,7 +55,7 @@ void pa_deinterleave(const void *src, vo
+ void pa_sample_clamp(pa_sample_format_t format, void *dst, size_t dstr, const void *src, size_t sstr, unsigned n);
  
-+#if !defined __WORDSIZE
-+# if defined __LP64__
-+#  define __WORDSIZE     64
-+# else
-+#  define __WORDSIZE     32
-+# endif
-+#endif
-+
- typedef struct pa_silence_cache {
-     pa_memblock* blocks[PA_SAMPLE_MAX];
- } pa_silence_cache;
+ static inline int32_t pa_mult_s16_volume(int16_t v, int32_t cv) {
+-#if __WORDSIZE == 64 || ((ULONG_MAX) > (UINT_MAX))
++#if HAVE_FAST_64BIT_OPERATIONS
+     /* Multiply with 64 bit integers on 64 bit platforms */
+     return (v * (int64_t) cv) >> 16;
+ #else
--- pulseaudio-git/patches/patch-src_tests_mult-s16-test.c
+++ pulseaudio-git/patches/patch-src_tests_mult-s16-test.c
@@ -1,26 +1,20 @@
-$NetBSD: patch-src_tests_mult-s16-test.c,v 1.1 2014/06/09 13:08:19 ryoon Exp $
+$NetBSD$
 
-Define glibc-specific (?) __WORDSIZE.
-
---- src/tests/mult-s16-test.c.orig	2015-12-07 11:45:13.000000000 +0000
+--- src/tests/mult-s16-test.c.orig	2015-12-05 22:03:14.000000000 +0000
 +++ src/tests/mult-s16-test.c
-@@ -23,6 +23,7 @@
- #include <unistd.h>
- #include <stdlib.h>
- #include <math.h>
-+#include <limits.h>
- 
- #include <pulse/rtclock.h>
- #include <pulsecore/random.h>
-@@ -93,9 +94,9 @@ int main(int argc, char *argv[]) {
+@@ -93,12 +93,10 @@ int main(int argc, char *argv[]) {
      if (!getenv("MAKE_CHECK"))
          pa_log_set_level(PA_LOG_DEBUG);
  
 -#if __WORDSIZE == 64 || ((ULONG_MAX) > (UINT_MAX))
-+#if (SIZEOF_VOIDP * CHAR_BIT) == 64
-     pa_log_debug("This seems to be 64-bit code.");
+-    pa_log_debug("This seems to be 64-bit code.");
 -#elif  __WORDSIZE == 32
-+#elif (SIZEOF_VOIDP * CHAR_BIT) == 32
-     pa_log_debug("This seems to be 32-bit code.");
+-    pa_log_debug("This seems to be 32-bit code.");
++#if HAVE_FAST_64BIT_OPERATIONS
++    pa_log_debug("Detected CPU with fast 64-bit operations.");
  #else
-     pa_log_debug("Don't know if this is 32- or 64-bit code.");
+-    pa_log_debug("Don't know if this is 32- or 64-bit code.");
++    pa_log_debug("Not detected CPU with fast 64-bit operations.");
+ #endif
+ 
+     s = suite_create("Mult-s16");
