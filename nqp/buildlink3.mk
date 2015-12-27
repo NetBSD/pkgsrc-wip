6cc13d88e3b244d3b138902afc624eceb1a83cdc
Benny Siegert <bsiegert@NetBSD.org>
2015-12-27 13:09:09 +0100

    Update nqp (Not Quite Perl) to 2015.11.
    There was no changelog. Now uses only MoarVM instead of Parrot.
    This is part of the "Christmas 2015" Perl 6 release.

--- nqp/buildlink3.mk
+++ nqp/buildlink3.mk
@@ -5,11 +5,11 @@ BUILDLINK_TREE+=	nqp
 .if !defined(NQP_BUILDLINK3_MK)
 NQP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nqp+=		nqp>=2013.08
-BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2013.08nb5
+BUILDLINK_API_DEPENDS.nqp+=		nqp>=2015.11
+BUILDLINK_ABI_DEPENDS.nqp+=		nqp>=2015.11
 BUILDLINK_PKGSRCDIR.nqp?=		../../lang/nqp
 
-.include "../../lang/parrot/buildlink3.mk"
+.include "../../wip/MoarVM/buildlink3.mk"
 .endif # NQP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-nqp
