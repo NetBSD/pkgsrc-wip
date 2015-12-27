ad55e137e1db4c4ed0f1b4d1a5d74debccf8602d
Benny Siegert <bsiegert@NetBSD.org>
2015-12-27 23:44:31 +0100

    New package, rakudo.
    This is basically rakudo-star without the star, so it does not include
    panda (the package manager) and several modules.

---
+++ rakudo/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.5 2015/04/06 08:17:15 adam Exp $
+
+BUILDLINK_TREE+=	rakudo
+
+.if !defined(RAKUDO_BUILDLINK3_MK)
+RAKUDO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.rakudo+=		rakudo>=2015.12
+BUILDLINK_ABI_DEPENDS.rakudo+=		rakudo>=2015.12
+BUILDLINK_PKGSRCDIR.rakudo?=		../../wip/rakudo
+
+.include "../../lang/nqp/buildlink3.mk"
+.endif # RAKUDO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-rakudo
