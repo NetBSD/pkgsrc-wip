0146dd64c6f34489f61b1a5f25e8cafa6a31bdac
Kamil Rytarowski <n54@gmx.com>
2015-12-28 00:48:28 +0100

    rubberband: Switch to audio/vamp-plugin-sdk (dependency has been imported to pkgsrc)

--- rubberband/buildlink3.mk
+++ rubberband/buildlink3.mk
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.rubberband+=	rubberband>=1.8.1
 BUILDLINK_PKGSRCDIR.rubberband?=	../../wip/rubberband
 
 .include "../../audio/ladspa/buildlink3.mk"
-.include "../../wip/vamp-plugin-sdk/buildlink3.mk"
+.include "../../audio/vamp-plugin-sdk/buildlink3.mk"
 .include "../../math/fftw/buildlink3.mk"
 .include "../../audio/libsamplerate/buildlink3.mk"
 .endif	# RUBBERBAND_BUILDLINK3_MK
