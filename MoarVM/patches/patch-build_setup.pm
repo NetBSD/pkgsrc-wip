23a66795b761433f518ff95d43d741c24ab3f52c
Thomas Klausner <wiz@NetBSD.org>
2015-12-28 22:15:00 +0100

    MoarVM: Fix build and packaging on NetBSD.

---
+++ MoarVM/patches/patch-build_setup.pm
@@ -0,0 +1,22 @@
+$NetBSD$
+
+pkgsrc uses gmake everywhere, so use gmake syntax.
+Not for upstreaming.
+
+--- build/setup.pm.orig	2015-12-25 12:37:32.000000000 +0000
++++ build/setup.pm
+@@ -171,12 +171,12 @@ our %TC_BSD = (
+     %TC_POSIX,
+ 
+     mknoisy => <<'TERM',
+-.if $(NOISY) != 1
++ifneq ($(NOISY), 1)
+ MSG = @echo
+ CMD = @
+ NOOUT = > @nul@
+ NOERR = 2> @nul@
+-.endif
++endif
+ TERM
+ );
+ 
