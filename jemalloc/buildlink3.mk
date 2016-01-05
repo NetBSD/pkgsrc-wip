18ffbcfe3891323a6e1cfeb77e445711583bab58
Jonathan Buschmann <jonthn+pkgsrc@pinacea.com>
2016-01-05 22:15:40 +0100

    jemalloc: Update to 4.0.4
     Switch to release version distributed on Github
     This change library so.name from .1 to .2
     TODO: Update to 4.0.4 (instead of 4.0.1) was done

--- jemalloc/buildlink3.mk
+++ jemalloc/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	jemalloc
 .if !defined(JEMALLOC_BUILDLINK3_MK)
 JEMALLOC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.jemalloc+=	jemalloc>=3.6.0
+BUILDLINK_API_DEPENDS.jemalloc+=	jemalloc>=4.0.0
 BUILDLINK_PKGSRCDIR.jemalloc?=	../../wip/jemalloc
 .endif	# JEMALLOC_BUILDLINK3_MK
 
