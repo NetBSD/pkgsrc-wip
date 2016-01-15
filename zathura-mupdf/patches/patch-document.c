5451bbef620dee1df758778d92c81588b4cf67e5
Leonardo Taccari <leot@NetBSD.org>
2016-01-15 23:57:12 +0100

    zathura-mupdf: Add a patch to fix the colorspace (and regen patch-document.c)
    This seems a regression of the recent 0.2.9 release. In order to reproduce it
    just open a PDF that contains color and you will notice that "red" and "blue"
    are splitted.
    
    XXX: I should report it upstream!

--- zathura-mupdf/patches/patch-document.c
+++ zathura-mupdf/patches/patch-document.c
@@ -2,7 +2,7 @@ $NetBSD: patch-document.c,v 1.2 2015/06/13 02:16:07 leot1990 Exp $
 
 Correct the glib-2.0 include header file path.
 
---- document.c.orig	2015-05-27 15:18:11.000000000 +0000
+--- document.c.orig	2015-12-22 22:38:09.000000000 +0000
 +++ document.c
 @@ -6,7 +6,7 @@
  #include <mupdf/xps.h>
