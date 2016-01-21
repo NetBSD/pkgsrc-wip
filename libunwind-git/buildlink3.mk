4313ad74453e562ba81026a594d6b2905114102e
Kamil Rytarowski <n54@gmx.com>
2016-01-21 14:34:07 +0100

    Import libunwind-3.9.0nb20160121 as wip/libunwind-git.
    LLVM libunwind

---
+++ libunwind-git/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libunwind
+
+.if !defined(LIBUNWIND_BUILDLINK3_MK)
+libunwind_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libunwind+=	libunwind>=3.9.0nb20151127
+BUILDLINK_PKGSRCDIR.libunwind?=	../../wip/libunwind-git
+
+.include "../../wip/llvm-git/buildlink3.mk"
+.endif	# LIBUNWIND_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libunwind
