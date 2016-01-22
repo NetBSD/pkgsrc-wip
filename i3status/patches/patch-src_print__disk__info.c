abfe73ffcb67e9cdadc689a3c1cdce9bcf19032d
Mateusz Poszwa <old4@o2.pl>
2016-01-22 01:46:38 +0100

    i3status: Update wip/i3status to 2.10.
    Changes:
     * man: fix typo in battery example
     * Pass the "instance" JSON property key for tztime blocks
     * Fix build (and build warnings) on FreeBSD
     * Fix build on OpenBSD
     * path_exists, run_watch: add format_down
     * volume: add PulseAudio support. This significantly reduces battery
       consumption on systems that use PulseAudio, which is the default on any
       modern Linux desktop.
     * wireless/OpenBSD: use the maximum rssi, not the scaled percentage for the
       signal level.
     * make: Add support for differing PREFIX and MANPREFIX.
     * battery: make output HH:MM across all platforms.
     * battery: clamp the percentage at 100% when last_full_capacity is enabled.
     * Use format_not_mounted if the directory for a disk_info block does not
       exist.
     • disk_info/OS X: Fix build.
     * volume/OpenBSD: use SndIO.
     * eth, wireless: Use "color_degraded" when interface has no IP.
     * eth, wireless/FreeBSD: Fix for recent FreeBSD versions.
     * wireless/Linux: switch from libiw to libnl. This allows you to run i3status
       on kernels which don’t have the wext compatibility module enabled.
     * battery/FreeBSD: properly detect charging status
     * eth: first_eth_interface: ignore MAC addresses, look for IPv4/IPv6 addresses
     * Implement support for Pango markup.
     * format detection: recognise swaybar

--- i3status/patches/patch-src_print__disk__info.c
+++ i3status/patches/patch-src_print__disk__info.c
@@ -1,30 +1,53 @@
 $NetBSD: patch-src_print__disk__info.c,v 1.1 2015/04/07 21:11:11 nros Exp $
+
 * mntent.h and setmntent stuff is linux only
---- src/print_disk_info.c.orig	2015-03-22 17:03:23.000000000 +0000
+* NetBSD has statvfs structure, not statfs
+
+--- src/print_disk_info.c.orig	2016-01-01 18:51:19.000000000 +0000
 +++ src/print_disk_info.c
-@@ -3,7 +3,9 @@
- #include <stdlib.h>
- #include <string.h>
- #include <ctype.h>
-+#if defined(__linux__)
- #include <mntent.h>
-+#endif
- #include <stdint.h>
+@@ -7,11 +7,11 @@
  #include <sys/stat.h>
  #include <sys/statvfs.h>
-@@ -126,6 +128,7 @@ void print_disk_info(yajl_gen json_gen, 
-     if (statvfs(path, &buf) == -1)
-         return;
- 
+ #include <sys/types.h>
+-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || (__OpenBSD__) || defined(__DragonFly__) || defined(__APPLE__)
 +#if defined(__linux__)
-     if (format_not_mounted != NULL) {
-         FILE *mntentfile = setmntent("/etc/mtab", "r");
-         struct mntent *m;
-@@ -144,6 +147,7 @@ void print_disk_info(yajl_gen json_gen, 
++#include <mntent.h>
++#else
+ #include <sys/param.h>
+ #include <sys/mount.h>
+-#else
+-#include <mntent.h>
+ #endif
+ #include <yajl/yajl_gen.h>
+ #include <yajl/yajl_version.h>
+@@ -116,12 +116,7 @@ void print_disk_info(yajl_gen json_gen, 
+ 
+     INSTANCE(path);
+ 
+-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__OpenBSD__) || defined(__DragonFly__) || defined(__APPLE__)
+-    struct statfs buf;
+-
+-    if (statfs(path, &buf) == -1)
+-        return;
+-#else
++#if defined(__linux__)
+     struct statvfs buf;
+ 
+     if (statvfs(path, &buf) == -1) {
+@@ -145,6 +140,16 @@ void print_disk_info(yajl_gen json_gen, 
+             format = format_not_mounted;
          }
      }
++#elif defined(__NetBSD__)
++    struct statvfs buf;
++
++    if (statvfs(path, &buf) == -1)
++        format = format_not_mounted;
++#else
++    struct statfs buf;
++
++    if (statfs(path, &buf) == -1)
++        return;
  #endif
-+#endif
  
      if (low_threshold > 0 && below_threshold(buf, prefix_type, threshold_type, low_threshold)) {
-         START_COLOR("color_bad");
