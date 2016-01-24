672fefa65b634c4990ddc05a5d0f4c5ff37f1169
Kamil Rytarowski <n54@gmx.com>
2016-01-25 00:42:26 +0100

    corert-git: Add required cast to <const char**> in iconv(3)

--- corert-git/patches/patch-src_Native_Runtime_unix_PalRedhawkUnix.cpp
+++ corert-git/patches/patch-src_Native_Runtime_unix_PalRedhawkUnix.cpp
@@ -10,3 +10,21 @@ $NetBSD$
  
  #if !HAVE_SYSCONF && !HAVE_SYSCTL
  #error Neither sysconf nor sysctl is present on the current system
+@@ -334,7 +335,7 @@ int UTF8ToWideChar(const char* bytes, in
+     size_t inbufbytesleft = len;
+     size_t outbufbytesleft = bufLen;
+ 
+-    int rc = iconv(cd, &inbuf, &inbufbytesleft, &outbuf, &outbufbytesleft);
++    int rc = iconv(cd, const_cast<const char **>(&inbuf), &inbufbytesleft, &outbuf, &outbufbytesleft);
+     if (rc == -1)
+     {
+         fprintf(stderr, "iconv_open failed with %d\n", errno);
+@@ -360,7 +361,7 @@ int WideCharToUTF8(const wchar_t* chars,
+     size_t inbufbytesleft = len;
+     size_t outbufbytesleft = bufLen;
+ 
+-    int rc = iconv(cd, &inbuf, &inbufbytesleft, &outbuf, &outbufbytesleft);
++    int rc = iconv(cd, const_cast<const char **>(&inbuf), &inbufbytesleft, &outbuf, &outbufbytesleft);
+     if (rc == -1)
+     {
+         fprintf(stderr, "iconv_open failed with %d\n", errno);
