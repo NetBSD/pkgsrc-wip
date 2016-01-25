fd5e23c36fc69bc666790d06735e86b2b3fa1bae
Kamil Rytarowski <n54@gmx.com>
2016-01-25 01:57:58 +0100

    corert-git: Remove merged patch

--- corert-git/patches/patch-src_Native_Runtime_unix_PalRedhawkUnix.cpp
+++ corert-git/patches/patch-src_Native_Runtime_unix_PalRedhawkUnix.cpp
@@ -1,16 +1,8 @@
 $NetBSD$
 
---- src/Native/Runtime/unix/PalRedhawkUnix.cpp.orig	2016-01-23 11:26:49.000000000 +0000
+--- src/Native/Runtime/unix/PalRedhawkUnix.cpp.orig	2016-01-25 00:17:12.000000000 +0000
 +++ src/Native/Runtime/unix/PalRedhawkUnix.cpp
-@@ -33,6 +33,7 @@
- #include <sys/stat.h>
- #include <fcntl.h>
- #include <sys/time.h>
-+#include <cstdarg>
- 
- #if !HAVE_SYSCONF && !HAVE_SYSCTL
- #error Neither sysconf nor sysctl is present on the current system
-@@ -334,7 +335,7 @@ int UTF8ToWideChar(const char* bytes, in
+@@ -335,7 +335,7 @@ int UTF8ToWideChar(const char* bytes, in
      size_t inbufbytesleft = len;
      size_t outbufbytesleft = bufLen;
  
@@ -19,7 +11,7 @@ $NetBSD$
      if (rc == -1)
      {
          fprintf(stderr, "iconv_open failed with %d\n", errno);
-@@ -360,7 +361,7 @@ int WideCharToUTF8(const wchar_t* chars,
+@@ -361,7 +361,7 @@ int WideCharToUTF8(const wchar_t* chars,
      size_t inbufbytesleft = len;
      size_t outbufbytesleft = bufLen;
  
