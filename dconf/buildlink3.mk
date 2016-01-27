226171669bc9a7005730029a6425477255cd4c5f
coypu <coypu@sdf.org>
2016-01-27 14:21:52 +0200

    dconf: updated to 0.24.0

--- dconf/buildlink3.mk
+++ dconf/buildlink3.mk
@@ -5,9 +5,8 @@ BUILDLINK_TREE+=	dconf
 .if !defined(DCONF_BUILDLINK3_MK)
 DCONF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.dconf+=	dconf>=0.11.0
-BUILDLINK_ABI_DEPENDS.dconf?=	dconf>=0.14.0
-BUILDLINK_PKGSRCDIR.dconf?=	../../devel/dconf
+BUILDLINK_ABI_DEPENDS.dconf+=	dconf>=0.24.0
+BUILDLINK_PKGSRCDIR.dconf?=	../../wip/dconf
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../sysutils/dbus/buildlink3.mk"
