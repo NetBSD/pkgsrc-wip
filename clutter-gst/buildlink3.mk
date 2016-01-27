05615a18b70424566554c49c12905e620703bb74
coypu <coypu@sdf.org>
2016-01-27 12:19:24 +0200

    clutter-gst: updated to 3.0.14, added TODO

--- clutter-gst/buildlink3.mk
+++ clutter-gst/buildlink3.mk
@@ -1,18 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.30 2015/04/25 14:23:37 tnn Exp $
-#
+# $NetBSD$
 
 BUILDLINK_TREE+=	clutter-gst
 
 .if !defined(CLUTTER_GST_BUILDLINK3_MK)
 CLUTTER_GST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clutter-gst+=	clutter-gst>=2.0.0
-BUILDLINK_ABI_DEPENDS.clutter-gst?=	clutter-gst>=2.0.12nb1
-BUILDLINK_PKGSRCDIR.clutter-gst?=	../../multimedia/clutter-gst
+BUILDLINK_API_DEPENDS.clutter-gst+=	clutter-gst>=3.0.14
+BUILDLINK_PKGSRCDIR.clutter-gst?=	../../wip/clutter-gst
 
 .include "../../graphics/clutter/buildlink3.mk"
+.include "../../graphics/cogl/buildlink3.mk"
 .include "../../multimedia/gstreamer1/buildlink3.mk"
 .include "../../multimedia/gst-plugins1-base/buildlink3.mk"
-.endif # CLUTTER_GST_BUILDLINK3_MK
+.endif
 
 BUILDLINK_TREE+=	-clutter-gst
