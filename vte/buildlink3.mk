89460b026a0cc5b07b17e61be67ad2807ddfe40e
coypu <coypu@sdf.org>
2016-01-27 09:05:38 +0200

    vte: updated to 0.42.1, changed buildlink

--- vte/buildlink3.mk
+++ vte/buildlink3.mk
@@ -1,18 +1,33 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/06/04 23:17:14 krytarowski Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	vte
 
 .if !defined(VTE_BUILDLINK3_MK)
 VTE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vte+=	vte>=0.14.0
-BUILDLINK_ABI_DEPENDS.vte+=	vte>=0.28.1nb16
-BUILDLINK_PKGSRCDIR.vte?=	../../x11/vte
+BUILDLINK_API_DEPENDS.vte+=	vte>=0.42.1
+BUILDLINK_PKGSRCDIR.vte?=	../../wip/vte
 
+# XXX
+# XXX Uncomment and keep only the buildlink3 lines below which are directly
+# XXX needed for dependencies to compile, link, and run.  If this package
+# XXX provides a wrappered API or otherwise does not expose the APIs of the
+# XXX buildlink3 lines below to dependencies, remove them.
+# XXX
+#.include "../../devel/gperf/buildlink3.mk"
+.include "../../security/gnutls/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+#.include "../../lang/vala/buildlink3.mk"
+#.include "../../devel/gobject-introspection/buildlink3.mk"
+#.include "../../textproc/gtk-doc/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/pango/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
-.include "../../mk/termcap.buildlink3.mk"
-.endif # VTE_BUILDLINK3_MK
+#.include "../../devel/pango/buildlink3.mk"
+#.include "../../fonts/fontconfig/buildlink3.mk"
+#.include "../../graphics/freetype2/buildlink3.mk"
+#.include "../../x11/libSM/buildlink3.mk"
+#.include "../../x11/libX11/buildlink3.mk"
+#.include "../../x11/libXft/buildlink3.mk"
+#.include "../../x11/libXt/buildlink3.mk"
+.endif	# VTE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-vte
