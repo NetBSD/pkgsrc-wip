1758942e80fe126854e3c71f570ccfb368d18266
coypu <coypu@sdf.org>
2016-01-27 15:48:40 +0200

    Import empathy as wip/empathy

---
+++ empathy/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.38 2015/04/25 14:20:27 tnn Exp $
+
+BUILDLINK_TREE+=	empathy
+
+.if !defined(EMPATHY_BUILDLINK3_MK)
+EMPATHY_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.empathy+=	empathy>=2.24.1nb1
+BUILDLINK_ABI_DEPENDS.empathy+=	empathy>=2.34.0nb43
+BUILDLINK_PKGSRCDIR.empathy?=	../../chat/empathy
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../mail/evolution-data-server/buildlink3.mk"
+.include "../../sysutils/dbus/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif # EMPATHY_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-empathy
