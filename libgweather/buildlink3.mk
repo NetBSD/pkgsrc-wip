c5578f16058ebb77aa5ab6347ad431eabb41d928
coypu <coypu@sdf.org>
2016-01-28 23:54:34 +0200

    libgweather: updated to 3.18.1

--- libgweather/buildlink3.mk
+++ libgweather/buildlink3.mk
@@ -1,16 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.38 2015/06/07 14:14:04 youri Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	libgweather
 
 .if !defined(LIBGWEATHER_BUILDLINK3_MK)
 LIBGWEATHER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgweather+=	libgweather>=2.22.0
-BUILDLINK_ABI_DEPENDS.libgweather+=	libgweather>=2.30.3nb29
-BUILDLINK_PKGSRCDIR.libgweather?=	../../devel/libgweather
+BUILDLINK_API_DEPENDS.libgweather+=	libgweather>=3.18.1
+BUILDLINK_PKGSRCDIR.libgweather?=	../../wip/libgweather
 
-.include "../../net/libsoup/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
-.endif # LIBGWEATHER_BUILDLINK3_MK
+.endif	# LIBGWEATHER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libgweather
