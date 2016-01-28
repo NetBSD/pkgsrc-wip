72631feb3cb820752b7f5a3b822150314554251c
coypu <coypu@sdf.org>
2016-01-29 00:07:45 +0200

    evolution-data-sesrver: fixed buildlink

--- evolution-data-server/buildlink3.mk
+++ evolution-data-server/buildlink3.mk
@@ -1,24 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.57 2015/07/11 17:16:18 wiz Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	evolution-data-server
 
 .if !defined(EVOLUTION_DATA_SERVER_BUILDLINK3_MK)
 EVOLUTION_DATA_SERVER_BUILDLINK3_MK:=
 
-BUILDLINK_ABI_DEPENDS.evolution-data-server+=	evolution-data-server>=3.18.4
-BUILDLINK_PKGSRCDIR.evolution-data-server?=	../../mail/evolution-data-server
+BUILDLINK_API_DEPENDS.evolution-data-server+=	evolution-data-server>=3.18.4
+BUILDLINK_PKGSRCDIR.evolution-data-server?=	../../wip/evolution-data-server
 
-BDB_ACCEPTED?=		db4 db5
+.include "../../security/libgnome-keyring/buildlink3.mk"
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../devel/GConf/buildlink3.mk"
 .include "../../devel/nss/buildlink3.mk"
 .include "../../net/libsoup/buildlink3.mk"
 .include "../../net/ORBit2/buildlink3.mk"
-.include "../../net/libgdata0.6/buildlink3.mk"
-.include "../../time/libical/buildlink3.mk"
 .include "../../security/libgnome-keyring/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
-.include "../../mk/bdb.buildlink3.mk"
-.endif # EVOLUTION_DATA_SERVER_BUILDLINK3_MK
+.include "../../time/libical/buildlink3.mk"
+.endif	# EVOLUTION_DATA_SERVER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-evolution-data-server
