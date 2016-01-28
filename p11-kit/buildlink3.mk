dbac4751b8eb437537210350c85eddd2c94f5437
coypu <coypu@sdf.org>
2016-01-28 11:19:56 +0200

    p11-kit: updated to 0.23.2

--- p11-kit/buildlink3.mk
+++ p11-kit/buildlink3.mk
@@ -1,12 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/02/03 14:43:12 drochner Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	p11-kit
 
 .if !defined(P11_KIT_BUILDLINK3_MK)
 P11_KIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.p11-kit+=	p11-kit>=0.10
-BUILDLINK_PKGSRCDIR.p11-kit?=	../../security/p11-kit
+BUILDLINK_API_DEPENDS.p11-kit+=	p11-kit>=0.23.2
+BUILDLINK_PKGSRCDIR.p11-kit?=	../../wip/p11-kit
+
+#.include "../../security/libtasn1/buildlink3.mk"
+#.include "../../devel/libffi/buildlink3.mk"
 .endif	# P11_KIT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-p11-kit
