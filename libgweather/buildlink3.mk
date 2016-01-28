1baa28eb9c8ad7cea061948156906f749348d411
coypu <coypu@sdf.org>
2016-01-28 17:25:12 +0200

    Import libgweather as wip/libgweather

---
+++ libgweather/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.38 2015/06/07 14:14:04 youri Exp $
+
+BUILDLINK_TREE+=	libgweather
+
+.if !defined(LIBGWEATHER_BUILDLINK3_MK)
+LIBGWEATHER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libgweather+=	libgweather>=2.22.0
+BUILDLINK_ABI_DEPENDS.libgweather+=	libgweather>=2.30.3nb29
+BUILDLINK_PKGSRCDIR.libgweather?=	../../devel/libgweather
+
+.include "../../net/libsoup/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif # LIBGWEATHER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libgweather
