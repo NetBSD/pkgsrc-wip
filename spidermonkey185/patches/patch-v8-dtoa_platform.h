e6e4a357f1899a6e24eaf610a98eeb52fa1e4f16
Leonardo Taccari <leot@NetBSD.org>
2016-01-29 17:40:12 +0100

    spidermonkey185: various cosmetic fixes and improvements
    o Append the version to the various binaries and install headers in
      <js-1.8.5/...> instead of <js/...> to avoid confusion with other
      spidermonkeys and for consistency.
    o Adjust js-config, mozjs185.pc and PLIST for the above change
    o Fix possible PLIST issues for MACHINE_ARCHes different from i386 or x86_64,
      from FreeBSD port.
    o Add a newline in the patches (thanks pkglint) and various minor cosmetic
      fixes.
    
    PKGREVISION++

--- spidermonkey185/patches/patch-v8-dtoa_platform.h
+++ spidermonkey185/patches/patch-v8-dtoa_platform.h
@@ -1,6 +1,7 @@
 $NetBSD: patch-v8-dtoa_platform.h,v 1.1 2014/02/03 13:52:53 fhajny Exp $
 
 fpclassify is defined on Illumos.
+
 --- v8-dtoa/platform.h.orig	2011-03-31 19:08:36.000000000 +0000
 +++ v8-dtoa/platform.h
 @@ -67,7 +67,9 @@ int strncasecmp(const char* s1, const ch
