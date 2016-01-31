0bbadca8b62a61c38819dc19e0030f5872ba98ca
Mark Davies <mark@ecs.vuw.ac.nz>
2016-01-31 15:19:29 +1300

    kconfig: Add kconfig-5.18.0 as wip/kconfig
    KConfig provides an advanced configuration system. It is made of two parts:
    KConfigCore and KConfigGui.
    
    KConfigCore provides access to the configuration files themselves. It features:
    
    - Code generation: describe your configuration in an XML file, and use
      kconfig_compiler to generate classes that read and write configuration
      entries.
    - Cascading configuration files (global settings overridden by local settings).
    - Optional shell expansion support.
    - The ability to lock down configuration options.
    
    KConfigGui provides a way to hook widgets to the configuration so that they are
    automatically initialized from the configuration and automatically propagate
    their changes to their respective configuration files.

---
+++ kconfig/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	kconfig
+
+.if !defined(KCONFIG_BUILDLINK3_MK)
+KCONFIG_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kconfig+=	kconfig>=5.18.0
+BUILDLINK_PKGSRCDIR.kconfig?=	../../wip/kconfig
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KCONFIG_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kconfig
