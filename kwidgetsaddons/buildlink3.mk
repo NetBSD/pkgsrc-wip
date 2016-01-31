846487c2ad66959a21646a29301138fce584eba3
Mark Davies <mark@ecs.vuw.ac.nz>
2016-01-31 18:22:23 +1300

    kwidgetsaddons: fix typo

--- kwidgetsaddons/buildlink3.mk
+++ kwidgetsaddons/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	kwidgetsaddons
 KWIDGETSADDONS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kwidgetsaddons+=	kwidgetsaddons>=5.18.0
-BUILDLINK_PKGSRCDIR.kwidgetsaddons?=	../../wip/kwidgetaddons
+BUILDLINK_PKGSRCDIR.kwidgetsaddons?=	../../wip/kwidgetsaddons
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# KWIDGETSADDONS_BUILDLINK3_MK
