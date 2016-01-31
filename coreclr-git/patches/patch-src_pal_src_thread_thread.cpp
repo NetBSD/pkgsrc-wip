e23023449e9f141ef7428af71ddf08cc46ab3d7a
Kamil Rytarowski <n54@gmx.com>
2016-01-31 19:33:42 +0100

    coreclr-git: Remove merged (in a better version) patches

--- coreclr-git/patches/patch-src_pal_src_thread_thread.cpp
+++ coreclr-git/patches/patch-src_pal_src_thread_thread.cpp
@@ -1,19 +1,2 @@
 $NetBSD$
 
---- src/pal/src/thread/thread.cpp.orig	2016-01-28 19:04:13.000000000 +0000
-+++ src/pal/src/thread/thread.cpp
-@@ -53,8 +53,12 @@ Abstract:
- #include <limits.h>
- #if HAVE_SYS_LWP_H
- #include <sys/lwp.h>
--// If we don't have sys/lwp.h but do expect to use _lwp_self, declare it to silence compiler warnings
--#elif HAVE__LWP_SELF
-+#endif
-+#if HAVE_LWP_H
-+#include <lwp.h>
-+#endif
-+// If we don't have lwp.h, sys/lwp.h but do expect to use _lwp_self, declare it to silence compiler warnings
-+#if !HAVE_SYS_LWP_H && !HAVE_LWP_H && HAVE__LWP_SELF
- extern "C" int _lwp_self ();
- #endif // HAVE_LWP_H
- 
