5fb0b5467f9bbaec66c36eb1d56f93fbc17254f2
Mark Davies <mark@ecs.vuw.ac.nz>
2016-01-31 23:46:10 +1300

    kcoreaddons: build on NetBSD
    Build where KSDC_TIMEOUTS_SUPPORTED is not defined.

---
+++ kcoreaddons/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	kcoreaddons
+
+.if !defined(KCOREADDONS_BUILDLINK3_MK)
+KCOREADDONS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kcoreaddons+=	kcoreaddons>=5.18.0
+BUILDLINK_PKGSRCDIR.kcoreaddons?=	../../wip/kcoreaddons
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KCOREADDONS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kcoreaddons
