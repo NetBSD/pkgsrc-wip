3e248b5535a87d4998d950561c930d20d799ebc5
Mark Davies <mark@ecs.vuw.ac.nz>
2016-01-31 15:43:21 +1300

    kguiaddons: update wip/kguiaddons to 5.18.0
    Bring inline with other frameworks packages.

--- kguiaddons/buildlink3.mk
+++ kguiaddons/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/04/11 14:44:40 krytarowski Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	kguiaddons
 
 .if !defined(KGUIADDONS_BUILDLINK3_MK)
 KGUIADDONS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kguiaddons+=	kguiaddons>=5.9.0
+BUILDLINK_API_DEPENDS.kguiaddons+=	kguiaddons>=5.18.0
 BUILDLINK_PKGSRCDIR.kguiaddons?=	../../wip/kguiaddons
 
 .include "../../x11/qt5-qtx11extras/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# KGUIADDONS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-kguiaddons
