7cd1ff2728a745b27104beeee5f7fe3241b7ead6
Kamil Rytarowski <n54@gmx.com>
2016-02-01 02:59:42 +0100

    coreclr-git: Eliminate merged patches

--- coreclr-git/patches/patch-src_pal_src_thread_context.cpp
+++ coreclr-git/patches/patch-src_pal_src_thread_context.cpp
@@ -1,6 +1,6 @@
 $NetBSD$
 
---- src/pal/src/thread/context.cpp.orig	2016-01-31 18:28:53.000000000 +0000
+--- src/pal/src/thread/context.cpp.orig	2016-02-01 01:55:12.000000000 +0000
 +++ src/pal/src/thread/context.cpp
 @@ -200,7 +200,7 @@ BOOL CONTEXT_GetRegisters(DWORD processI
          if (ptrace((__ptrace_request)PT_GETREGS, processId, (caddr_t) &ptrace_registers, 0) == -1)
@@ -20,29 +20,3 @@ $NetBSD$
  #endif
          {
              ASSERT("Failed ptrace(PT_GETREGS, processId:%d) errno:%d (%s)\n",
-@@ -634,6 +634,7 @@ DWORD CONTEXTGetExceptionCodeForSignal(c
-                 case SEGV_MAPERR:   // Address not mapped to object
-                 case SEGV_ACCERR:   // Invalid permissions for mapped object
-                     return EXCEPTION_ACCESS_VIOLATION;
-+#ifdef SI_KERNEL
-                 case SI_KERNEL:
-                 {
-                     // Identify privileged instructions that are not identified as such by the system
-@@ -647,6 +648,7 @@ DWORD CONTEXTGetExceptionCodeForSignal(c
-                     }
-                     // fall through
-                 }
-+#endif
-                 default:
-                     break;
-             }
-@@ -665,7 +667,9 @@ DWORD CONTEXTGetExceptionCodeForSignal(c
-         case SIGTRAP:
-             switch (siginfo->si_code)
-             {
-+#ifdef SI_KERNEL
-                 case SI_KERNEL:
-+#endif
-                 case SI_USER:
-                 case TRAP_BRKPT:    // Process breakpoint
-                     return EXCEPTION_BREAKPOINT;
--- coreclr-git/patches/patch-src_pal_src_thread_thread.cpp
+++ coreclr-git/patches/patch-src_pal_src_thread_thread.cpp
@@ -1,18 +1,2 @@
 $NetBSD$
 
---- src/pal/src/thread/thread.cpp.orig	2016-01-31 18:28:53.000000000 +0000
-+++ src/pal/src/thread/thread.cpp
-@@ -621,11 +621,13 @@ CorUnix::InternalCreateThread(
-         dwStackSize = CPalThread::s_dwDefaultThreadStackSize;
-     }
- 
-+#ifdef PTHREAD_STACK_MIN
-     if (PTHREAD_STACK_MIN > pthreadStackSize)
-     {
-         WARN("default stack size is reported as %d, but PTHREAD_STACK_MIN is "
-              "%d\n", pthreadStackSize, PTHREAD_STACK_MIN);
-     }
-+#endif
-     
-     if (pthreadStackSize < dwStackSize)
-     {
