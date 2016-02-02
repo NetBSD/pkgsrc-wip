6e60618b7e9c3f81eb5b0abb2ee14a9c17e37a4f
Mark Davies <mark@ecs.vuw.ac.nz>
2016-02-02 19:11:25 +1300

    kdbusaddons: add buildlink3.mk

---
+++ kdbusaddons/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	kdbusaddons
+
+.if !defined(KDBUSADDONS_BUILDLINK3_MK)
+KDBUSADDONS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kdbusaddons+=	kdbusaddons>=5.18.0
+BUILDLINK_PKGSRCDIR.kdbusaddons?=	../../wip/kdbusaddons
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtx11extras/buildlink3.mk"
+.endif	# KDBUSADDONS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kdbusaddons
