04845c46e8cac6aacec61b6f4c23aeb1f02932d0
Kamil Rytarowski <n54@gmx.com>
2016-02-02 11:20:09 +0100

    coreclr-git: Eliminate not needed patches any more

--- coreclr-git/patches/patch-src_pal_src_thread_context.cpp
+++ coreclr-git/patches/patch-src_pal_src_thread_context.cpp
@@ -1,6 +1,6 @@
 $NetBSD$
 
---- src/pal/src/thread/context.cpp.orig	2016-02-01 01:55:12.000000000 +0000
+--- src/pal/src/thread/context.cpp.orig	2016-02-02 10:08:55.000000000 +0000
 +++ src/pal/src/thread/context.cpp
 @@ -19,6 +19,8 @@ Abstract:
  
@@ -11,24 +11,6 @@ $NetBSD$
  #include "pal/palinternal.h"
  #include "pal/dbgmsg.h"
  #include "pal/context.h"
-@@ -200,7 +202,7 @@ BOOL CONTEXT_GetRegisters(DWORD processI
-         if (ptrace((__ptrace_request)PT_GETREGS, processId, (caddr_t) &ptrace_registers, 0) == -1)
- #elif HAVE_BSD_REGS_T
-         struct reg ptrace_registers;
--        if (ptrace(PT_GETREGS, processId, (caddr_t) &ptrace_registers, 0) == -1)
-+        if (PAL_PTRACE(PT_GETREGS, processId, &ptrace_registers, 0) == -1)
- #endif
-         {
-             ASSERT("Failed ptrace(PT_GETREGS, processId:%d) errno:%d (%s)\n",
-@@ -349,7 +351,7 @@ CONTEXT_SetThreadContext(
- #if HAVE_PT_REGS
-         if (ptrace((__ptrace_request)PT_GETREGS, dwProcessId, (caddr_t)&ptrace_registers, 0) == -1)
- #elif HAVE_BSD_REGS_T
--        if (ptrace(PT_GETREGS, dwProcessId, (caddr_t)&ptrace_registers, 0) == -1)
-+        if (PAL_PTRACE(PT_GETREGS, dwProcessId, &ptrace_registers, 0) == -1)
- #endif
-         {
-             ASSERT("Failed ptrace(PT_GETREGS, processId:%d) errno:%d (%s)\n",
 @@ -1351,3 +1353,5 @@ DBG_FlushInstructionCache(
  
      return TRUE;
--- coreclr-git/patches/patch-src_pal_src_thread_thread.cpp
+++ coreclr-git/patches/patch-src_pal_src_thread_thread.cpp
@@ -1,14 +1,2 @@
 $NetBSD$
 
---- src/pal/src/thread/thread.cpp.orig	2016-02-01 01:51:45.000000000 +0000
-+++ src/pal/src/thread/thread.cpp
-@@ -1413,7 +1413,9 @@ CorUnix::GetThreadTimesInternal(
- 
-     pTargetThread->Lock(pThread);
- 
-+#if 0
-     if (pthread_getcpuclockid(pTargetThread->GetPThreadSelf(), &cid) != 0)
-+#endif
-     {
-         ASSERT("Unable to get clock from thread\n", hThread);
-         SetLastError(ERROR_INTERNAL_ERROR);
