380a659cee71fc761fd6f8a7719ac5598f294e84
Kamil Rytarowski <n54@gmx.com>
2016-02-04 02:45:22 +0100

    coreclr-git: Remove system headers from tests and build 98% of sources

---
+++ coreclr-git/patches/patch-src_pal_tests_palsuite_threading_GetThreadTimes_test1_test1.c
@@ -0,0 +1,21 @@
+$NetBSD$
+
+--- src/pal/tests/palsuite/threading/GetThreadTimes/test1/test1.c.orig	2016-01-28 19:04:13.000000000 +0000
++++ src/pal/tests/palsuite/threading/GetThreadTimes/test1/test1.c
+@@ -13,9 +13,7 @@
+ **
+ **=========================================================*/
+ 
+-#define _GNU_SOURCE
+ #include <palsuite.h>
+-#include <sched.h>
+ 
+ int __cdecl main(int argc, char *argv[]) {
+     int ret = FAIL;
+@@ -102,6 +100,3 @@ int __cdecl main(int argc, char *argv[])
+ EXIT:
+     return ret;
+ }
+-
+-
+-
