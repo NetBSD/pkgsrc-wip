0f0e2c254da568284648a92d369adce1b3329f9e
Kamil Rytarowski <n54@gmx.com>
2016-02-04 23:15:27 +0100

    coreclr-git: Improve patch for fgetpos(3) fsetpos(3)
    Thanks Peter Jas (@jasonwilliams200OK) for hacking session.

--- coreclr-git/patches/patch-src_pal_src_cruntime_file.cpp
+++ coreclr-git/patches/patch-src_pal_src_cruntime_file.cpp
@@ -2,21 +2,34 @@ $NetBSD$
 
 --- src/pal/src/cruntime/file.cpp.orig	2016-01-28 19:04:13.000000000 +0000
 +++ src/pal/src/cruntime/file.cpp
-@@ -840,7 +840,7 @@ PAL_fgetpos (
-     PAL_fpos_t *pos
- )
- {
--#ifdef __LINUX__
-+#if defined(__LINUX__) || defined(__NetBSD__)
+@@ -844,6 +844,15 @@ PAL_fgetpos (
      // TODO: implement for Linux if required
      ASSERT(FALSE);
      return -1;
-@@ -886,7 +886,7 @@ PAL_fsetpos (
-     const PAL_fpos_t *pos
- )
- {
--#ifdef __LINUX__
-+#if defined(__LINUX__) || defined(__NetBSD__)
++#elif defined(__NetBSD__)
++    off_t native_pos;
++
++    if ((native_pos = ftello(f->bsdFilePtr)) == -1)
++        return -1;
++
++    *pos = native_pos;
++
++    return 0;
+ #else
+     int    nRetVal = -1;
+     fpos_t native_pos;
+@@ -890,6 +899,14 @@ PAL_fsetpos (
      // TODO: implement for Linux if required
      ASSERT(FALSE);
      return  -1;
++#elif defined(__NetBSD__)
++    off_t native_pos;
++
++    native_pos = *pos;
++    if (fseeko(f->bsdFilePtr, native_pos, SEEK_SET) == -1)
++        return -1;
++
++    return 0;
+ #else
+     int    nRetVal = -1;
+     fpos_t native_pos;
