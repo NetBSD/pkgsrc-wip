85e9ff48549ea8b0a50e8b004d240f56733c1f8e
Kamil Rytarowski <n54@gmx.com>
2016-02-06 20:23:19 +0100

    libunwind-git: Canonicalize paths

--- libunwind-git/buildlink3.mk
+++ libunwind-git/buildlink3.mk
@@ -3,7 +3,7 @@
 BUILDLINK_TREE+=	libunwind
 
 .if !defined(LIBUNWIND_BUILDLINK3_MK)
-libunwind_BUILDLINK3_MK:=
+LIBUNWIND_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libunwind+=	libunwind>=3.9.0nb20151127
 BUILDLINK_PKGSRCDIR.libunwind?=	../../wip/libunwind-git
