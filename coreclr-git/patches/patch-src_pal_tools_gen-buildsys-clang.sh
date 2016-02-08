f51788d1b9806595e891692f3aa6e823aa6dfd98
Kamil Rytarowski <n54@gmx.com>
2016-02-08 04:21:02 +0100

    coreclr-git: Pass env variables via Makefile (thanks riastradh@)

--- coreclr-git/patches/patch-src_pal_tools_gen-buildsys-clang.sh
+++ coreclr-git/patches/patch-src_pal_tools_gen-buildsys-clang.sh
@@ -2,17 +2,7 @@ $NetBSD$
 
 --- src/pal/tools/gen-buildsys-clang.sh.orig	2016-01-18 21:51:39.000000000 +0000
 +++ src/pal/tools/gen-buildsys-clang.sh
-@@ -133,6 +133,9 @@ if [[ -n "$CROSSCOMPILE" ]]; then
-     cmake_extra_defines="$cmake_extra_defines -DCMAKE_TOOLCHAIN_FILE=$1/cross/$build_arch/toolchain.cmake"
- fi
- 
-+# Allow to specify additional parameters via environment variables.
-+
-+$CMAKE_ENV_EXTRA \
- cmake \
-   -G "$generator" \
-   "-DCMAKE_USER_MAKE_RULES_OVERRIDE=$1/src/pal/tools/clang-compiler-override.txt" \
-@@ -144,4 +147,5 @@ cmake \
+@@ -144,4 +144,5 @@ cmake \
    "-DCMAKE_ENABLE_CODE_COVERAGE=$code_coverage" \
    "-DCLR_CMAKE_BUILD_TESTS=$build_tests" \
    $cmake_extra_defines \
