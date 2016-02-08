3be312e662b9bdae7c5f5560289e6c227c4ad87d
Kamil Rytarowski <n54@gmx.com>
2016-02-08 03:34:01 +0100

    coreclr-git: Cleanup bash string injection (still broken)

--- coreclr-git/patches/patch-src_pal_tools_gen-buildsys-clang.sh
+++ coreclr-git/patches/patch-src_pal_tools_gen-buildsys-clang.sh
@@ -2,24 +2,19 @@ $NetBSD$
 
 --- src/pal/tools/gen-buildsys-clang.sh.orig	2016-01-18 21:51:39.000000000 +0000
 +++ src/pal/tools/gen-buildsys-clang.sh
-@@ -3,6 +3,8 @@
- # This file invokes cmake and generates the build system for gcc.
- #
- 
-+set -x
-+
- if [ $# -lt 4 -o $# -gt 7 ]
- then
-   echo "Usage..."
-@@ -133,7 +135,10 @@ if [[ -n "$CROSSCOMPILE" ]]; then
+@@ -133,6 +133,9 @@ if [[ -n "$CROSSCOMPILE" ]]; then
      cmake_extra_defines="$cmake_extra_defines -DCMAKE_TOOLCHAIN_FILE=$1/cross/$build_arch/toolchain.cmake"
  fi
  
--cmake \
-+cmake_extra_env="$cmake_extra_env $CMAKE_ENV_EXTRA"
-+cmake_extra_defines="$cmake_extra_defines $CMAKE_ARGS_EXTRA"
++# Allow to specify additional parameters via environment variables.
 +
-+$cmake_extra_env cmake \
++$CMAKE_ENV_EXTRA \
+ cmake \
    -G "$generator" \
    "-DCMAKE_USER_MAKE_RULES_OVERRIDE=$1/src/pal/tools/clang-compiler-override.txt" \
-   "-DCMAKE_AR=$llvm_ar" \
+@@ -144,4 +147,5 @@ cmake \
+   "-DCMAKE_ENABLE_CODE_COVERAGE=$code_coverage" \
+   "-DCLR_CMAKE_BUILD_TESTS=$build_tests" \
+   $cmake_extra_defines \
++  $CMAKE_ARGS_EXTRA \
+   "$1"
