b8ad6eb7b65f49e41858e056011eab0e41866e47
Thomas Klausner <wiz@NetBSD.org>
2016-02-08 14:24:26 +0100

    nss-hg: adapt patch to upstream changes.

--- nss-hg/patches/patch-lib_dbm_src_mktemp.c
+++ nss-hg/patches/patch-lib_dbm_src_mktemp.c
@@ -2,14 +2,14 @@ $NetBSD$
 
 Missing ctype(3) casts.
 
---- lib/dbm/src/mktemp.c.orig	2016-01-30 10:42:11.000000000 +0000
+--- lib/dbm/src/mktemp.c.orig	2016-02-08 07:30:39.000000000 +0000
 +++ lib/dbm/src/mktemp.c
-@@ -137,7 +137,7 @@ _gettemp(char *path, register int *doope
- 			if (*trv == 'z')
- 				*trv++ = 'a';
- 			else {
--				if (isdigit(*trv))
-+				if (isdigit((unsigned char)*trv))
- 					*trv = 'a';
- 				else
- 					++*trv;
+@@ -138,7 +138,7 @@ _gettemp(char *path, register int *doope
+             if (*trv == 'z')
+                 *trv++ = 'a';
+             else {
+-                if (isdigit(*trv))
++                if (isdigit((unsigned char)*trv))
+                     *trv = 'a';
+                 else
+                     ++*trv;
