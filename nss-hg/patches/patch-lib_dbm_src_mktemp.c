9eb46251eee362775e84b4f30f549d5943203705
coypu <coypu@sdf.org>
2016-02-09 03:25:58 +0200

    nss-hg: Moved ctype patch, don't destroy distinfo

--- nss-hg/patches/patch-lib_dbm_src_mktemp.c
+++ nss-hg/patches/patch-lib_dbm_src_mktemp.c
@@ -2,14 +2,14 @@ $NetBSD$
 
 Missing ctype(3) casts.
 
---- lib/dbm/src/mktemp.c.orig	2016-02-08 07:30:39.000000000 +0000
+--- lib/dbm/src/mktemp.c.orig	2016-02-08 17:31:58.000000000 +0000
 +++ lib/dbm/src/mktemp.c
 @@ -138,7 +138,7 @@ _gettemp(char *path, register int *doope
              if (*trv == 'z')
                  *trv++ = 'a';
              else {
 -                if (isdigit(*trv))
-+                if (isdigit((unsigned char)*trv))
++                if (isdigit((unsigned char) *trv))
                      *trv = 'a';
                  else
                      ++*trv;
