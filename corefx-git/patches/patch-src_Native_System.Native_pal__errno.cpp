671831074421760679c9496060863d779af46923
jasonwilliams200ok <necmon@yahoo.com>
2016-02-09 02:59:48 +0000

    corefx-git: Remove the uncommon errno symbols based on the conversation that transpired at dotnet/corefx#5983.

--- corefx-git/patches/patch-src_Native_System.Native_pal__errno.cpp
+++ corefx-git/patches/patch-src_Native_System.Native_pal__errno.cpp
@@ -2,55 +2,39 @@ $NetBSD$
 
 --- src/Native/System.Native/pal_errno.cpp.orig	2016-02-07 13:49:29.000000000 +0000
 +++ src/Native/System.Native/pal_errno.cpp
-@@ -6,7 +6,6 @@
- #include "pal_errno.h"
- #include "pal_utilities.h"
- 
--#include <errno.h>
- #include <string.h>
- #include <assert.h>
- 
-@@ -126,8 +125,10 @@ extern "C" Error SystemNative_ConvertErr
+@@ -126,8 +126,6 @@ extern "C" Error SystemNative_ConvertErr
              return PAL_ENOTDIR;
          case ENOTEMPTY:
              return PAL_ENOTEMPTY;
-+#ifdef ENOTRECOVERABLE
-         case ENOTRECOVERABLE:
-             return PAL_ENOTRECOVERABLE;
-+#endif
+-        case ENOTRECOVERABLE:
+-            return PAL_ENOTRECOVERABLE;
          case ENOTSOCK:
              return PAL_ENOTSOCK;
          case ENOTSUP:
-@@ -138,8 +139,10 @@ extern "C" Error SystemNative_ConvertErr
+@@ -138,8 +136,6 @@ extern "C" Error SystemNative_ConvertErr
              return PAL_ENXIO;
          case EOVERFLOW:
              return PAL_EOVERFLOW;
-+#ifdef EOWNERDEAD
-         case EOWNERDEAD:
-             return PAL_EOWNERDEAD;
-+#endif
+-        case EOWNERDEAD:
+-            return PAL_EOWNERDEAD;
          case EPERM:
              return PAL_EPERM;
          case EPIPE:
-@@ -298,8 +301,10 @@ extern "C" int32_t SystemNative_ConvertE
+@@ -298,8 +294,6 @@ extern "C" int32_t SystemNative_ConvertE
              return ENOTDIR;
          case PAL_ENOTEMPTY:
              return ENOTEMPTY;
-+#ifdef ENOTRECOVERABLE
-         case PAL_ENOTRECOVERABLE:
-             return ENOTRECOVERABLE;
-+#endif
+-        case PAL_ENOTRECOVERABLE:
+-            return ENOTRECOVERABLE;
          case PAL_ENOTSOCK:
              return ENOTSOCK;
          case PAL_ENOTSUP:
-@@ -310,8 +315,10 @@ extern "C" int32_t SystemNative_ConvertE
+@@ -310,8 +304,6 @@ extern "C" int32_t SystemNative_ConvertE
              return ENXIO;
          case PAL_EOVERFLOW:
              return EOVERFLOW;
-+#ifdef EOWNERDEAD
-         case PAL_EOWNERDEAD:
-             return EOWNERDEAD;
-+#endif
+-        case PAL_EOWNERDEAD:
+-            return EOWNERDEAD;
          case PAL_EPERM:
              return EPERM;
          case PAL_EPIPE:
