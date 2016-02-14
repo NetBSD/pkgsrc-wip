ed763f2f1fb0a561f3bf3d8538f5ef30d54d4b8b
Mark Davies <mark@ecs.vuw.ac.nz>
2016-02-15 00:37:25 +1300

    polkit-qt5: fix pkgname and dependencies

--- polkit-qt5/buildlink3.mk
+++ polkit-qt5/buildlink3.mk
@@ -1,17 +1,16 @@
 # $NetBSD: buildlink3.mk,v 1.3 2015/04/11 16:54:26 krytarowski Exp $
 
-BUILDLINK_TREE+=	polkit-qt5-1
+BUILDLINK_TREE+=	polkit-qt5
 
 .if !defined(POLKIT_QT5_BUILDLINK3_MK)
 POLKIT_QT5_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.polkit-qt5-1+=	polkit-qt5-1>=0.112.0
-BUILDLINK_ABI_DEPENDS.polkit-qt5-1+=	polkit-qt5-1>=0.112.0
-BUILDLINK_PKGSRCDIR.polkit-qt5-1?=	../../wip/polkit-qt5
+BUILDLINK_API_DEPENDS.polkit-qt5+=	polkit-qt5>=0.112.0
+BUILDLINK_ABI_DEPENDS.polkit-qt5+=	polkit-qt5>=0.112.0
+BUILDLINK_PKGSRCDIR.polkit-qt5?=	../../wip/polkit-qt5
 
-.include "../../x11/qt5-qtbase/buildlink3.mk"
-.include "../../security/policykit/buildlink3.mk"
 .include "../../wip/polkit/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# POLKIT_QT5_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-polkit-qt5-1
+BUILDLINK_TREE+=	-polkit-qt5
