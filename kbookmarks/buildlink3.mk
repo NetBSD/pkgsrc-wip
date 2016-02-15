38de6ec0e3df46c5d2a207909f2364fd98d44277
Mark Davies <mark@ecs.vuw.ac.nz>
2016-02-15 22:30:48 +1300

    kbookmarks: Add kbookmarks 5.19.0
    KBookmarks lets you access and manipulate bookmarks stored using the
    XBEL format (http://pyxml.sourceforge.net/topics/xbel/)
    
    The most common use for bookmarks is web browsers, but this can also
    be useful in any application where local files or URLs can be saved as
    bookmarks.

---
+++ kbookmarks/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	kbookmarks
+
+.if !defined(KBOOKMARKS_BUILDLINK3_MK)
+KBOOKMARKS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kbookmarks+=	kbookmarks>=5.19.0
+BUILDLINK_PKGSRCDIR.kbookmarks?=	../../wip/kbookmarks
+
+.include "../../wip/kxmlgui/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KBOOKMARKS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kbookmarks
