dedd79b861cb817b204129f4067ca626dcb20d01
Mark Davies <mark@ecs.vuw.ac.nz>
2016-02-15 22:20:03 +1300

    kiconthemes: Add kiconthemes 5.19.0
    This library contains classes to improve the handling of icons
    in applications using the KDE Frameworks. Provided are:
    
    - KIconDialog: Dialog class to let the user select an icon
        from the list of installed icons.
    - KIconButton: Custom button class that displays an icon.
        When clicking it, the user can change it using the icon dialog.
    - KIconEffect: Applies various colorization effects to icons,
        which can be used to create selected/disabled icon images.
    
    Other classes in this repository are used internally by the icon
    theme configuration dialogs, and should not be used by applications.

---
+++ kiconthemes/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	kiconthemes
+
+.if !defined(KICONTHEMES_BUILDLINK3_MK)
+KICONTHEMES_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kiconthemes+=	kiconthemes>=5.19.0
+BUILDLINK_PKGSRCDIR.kiconthemes?=	../../wip/kiconthemes
+
+.include "../../wip/kconfigwidgets/buildlink3.mk"
+.include "../../wip/kitemviews/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtsvg/buildlink3.mk"
+.endif	# KICONTHEMES_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kiconthemes
