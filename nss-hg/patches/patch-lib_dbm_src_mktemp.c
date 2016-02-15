7fb7e6c405076ff02b2088d76316b3acff134c74
Thomas Klausner <wiz@NetBSD.org>
2016-02-15 11:43:35 +0100

    nss-hg: make patch apply again

--- nss-hg/patches/patch-lib_dbm_src_mktemp.c
+++ nss-hg/patches/patch-lib_dbm_src_mktemp.c
@@ -2,14 +2,14 @@ $NetBSD$
 
 Missing ctype(3) casts.
 
---- lib/dbm/src/mktemp.c.orig	2016-02-14 09:23:25.931810131 +0000
+--- lib/dbm/src/mktemp.c.orig	2016-02-15 10:09:28.000000000 +0000
 +++ lib/dbm/src/mktemp.c
-@@ -137,7 +137,7 @@ _gettemp(char *path, register int *doope
- 			if (*trv == 'z')
- 				*trv++ = 'a';
- 			else {
--				if (isdigit(*trv))
-+				if (isdigit((unsigned char)*trv))
- 					*trv = 'a';
- 				else
- 					++*trv;
+@@ -138,7 +138,7 @@ _gettemp(char *path, register int *doope
+             if (*trv == 'z')
+                 *trv++ = 'a';
+             else {
+-                if (isdigit(*trv))
++                if (isdigit((unsigned char)*trv))
+                     *trv = 'a';
+                 else
+                     ++*trv;
