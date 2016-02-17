506a29df17257b424c708df990aca3d6636f1dd9
Filip Hajny <filip@joyent.com>
2016-02-17 13:50:07 +0000

    Update oce to 0.17.1.
    This version is binary compatible with 0CE 0.17.
    
    - Mingw64 (5.2.0) and Mingw32 (4.8.1, 4.9.3) fixes
    - OCE_USE_STATIC_MSVC_RUNTIME advanced option to use static version
      of the MSVC run-time library
    - Revert change in HLR inverted flag
    - Fix nmake build
    - Patch from setting correct tolerance.
    - Replace gp_Ax1 reference with direct value. Fixes #559
    - VS2015 fix
    - Fix for static building on MSVC
    - New oce-win-bundle handling
    - ppc64le fix
    - Build OCE and run tests on appveyor.com service.

--- oce/patches/patch-src_MeshVS_MeshVS__MeshPrsBuilder.cxx
+++ oce/patches/patch-src_MeshVS_MeshVS__MeshPrsBuilder.cxx
@@ -1,16 +1,16 @@
 $NetBSD: patch-src_MeshVS_MeshVS__MeshPrsBuilder.cxx,v 1.1 2015/06/25 12:41:09 fhajny Exp $
 
 SunOS needs alloca.h.
---- src/MeshVS/MeshVS_MeshPrsBuilder.cxx.orig	2015-03-08 22:19:42.000000000 +0000
+--- src/MeshVS/MeshVS_MeshPrsBuilder.cxx.orig	2016-01-02 05:11:52.000000000 +0000
 +++ src/MeshVS/MeshVS_MeshPrsBuilder.cxx
-@@ -56,6 +56,10 @@
- #include <NCollection_Map.hxx>
- #include <NCollection_Vector.hxx>
- 
+@@ -61,6 +61,10 @@
+   #define alloca __builtin_alloca 
+   #endif
+ #endif
++
 +#if defined(__sun)
 +#include <alloca.h>
 +#endif
-+
  //================================================================
  // Function : Constructor MeshVS_MeshPrsBuilder
  // Purpose  :
