d846b4a64d904a3d98e4b7f503b883928fe235ec
Thomas Klausner <wiz@NetBSD.org>
2016-02-19 16:49:17 +0100

    wanderer: improve PKGNAME and fix some compilation warnings.

---
+++ wanderer/patches/patch-wand__head.h
@@ -0,0 +1,14 @@
+$NetBSD$
+
+Include header for exit(3).
+
+--- wand_head.h.orig	2016-01-12 23:04:53.000000000 +0000
++++ wand_head.h
+@@ -21,6 +21,7 @@
+ 
+ 
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <ncurses.h>
+ #include <string.h>
+ #include <fcntl.h>
