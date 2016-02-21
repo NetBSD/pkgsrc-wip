3c95e8e26875c2c1ee67e3d379622d4db26d4b16
coypu <coypu@sdf.org>
2016-02-21 19:22:28 +0200

    Import telepathy-logger from pkgsrc

---
+++ telepathy-logger/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD: buildlink3.mk,v 1.15 2015/04/06 08:17:13 adam Exp $
+
+BUILDLINK_TREE+=	telepathy-logger
+
+.if !defined(TELEPATHY_LOGGER_BUILDLINK3_MK)
+TELEPATHY_LOGGER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.telepathy-logger+=	telepathy-logger>=0.1.4
+BUILDLINK_ABI_DEPENDS.telepathy-logger+=	telepathy-logger>=0.2.7nb15
+BUILDLINK_PKGSRCDIR.telepathy-logger?=	../../chat/telepathy-logger
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/GConf/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../chat/telepathy-glib/buildlink3.mk"
+.include "../../databases/sqlite3/buildlink3.mk"
+.endif	# TELEPATHY_LOGGER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-telepathy-logger
