c57e1e4a1dfe629c8f955fc92eff8425b40393f4
Tobias Nygren <tnn@NetBSD.org>
2016-02-21 17:31:20 +0100

    llvm*: drop unused libxml2 dependency

--- llvm-git/buildlink3.mk
+++ llvm-git/buildlink3.mk
@@ -5,10 +5,9 @@ BUILDLINK_TREE+=	llvm
 .if !defined(LLVM_BUILDLINK3_MK)
 LLVM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.llvm+=	llvm>=3.7.0nb20151127
+BUILDLINK_API_DEPENDS.llvm+=	llvm>=3.9.0
 BUILDLINK_PKGSRCDIR.llvm?=	../../wip/llvm-git
 
-.include "../../textproc/libxml2/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .endif	# LLVM_BUILDLINK3_MK
 
--- llvm/buildlink3.mk
+++ llvm/buildlink3.mk
@@ -8,7 +8,6 @@ LLVM_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.llvm+=	llvm>=3.8.0rc1
 BUILDLINK_PKGSRCDIR.llvm?=	../../wip/llvm
 
-.include "../../textproc/libxml2/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .endif	# LLVM_BUILDLINK3_MK
 
