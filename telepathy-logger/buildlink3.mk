479f9a3770aa6edd384d29ed2a3ee672418200da
coypu <coypu@sdf.org>
2016-02-21 20:48:03 +0200

    telepathy-logger: don't use telepathy-glib

--- telepathy-logger/buildlink3.mk
+++ telepathy-logger/buildlink3.mk
@@ -7,12 +7,11 @@ TELEPATHY_LOGGER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.telepathy-logger+=	telepathy-logger>=0.1.4
 BUILDLINK_ABI_DEPENDS.telepathy-logger+=	telepathy-logger>=0.8.2
-BUILDLINK_PKGSRCDIR.telepathy-logger?=	../../chat/telepathy-logger
+BUILDLINK_PKGSRCDIR.telepathy-logger?=	../../wip/telepathy-logger
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/GConf/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
-.include "../../chat/telepathy-glib/buildlink3.mk"
 .include "../../databases/sqlite3/buildlink3.mk"
 .endif	# TELEPATHY_LOGGER_BUILDLINK3_MK
 
