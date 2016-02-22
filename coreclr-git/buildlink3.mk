b581b82a699e2c533f4efa0df67ad6b619d679b5
Kamil Rytarowski <n54@gmx.com>
2016-02-23 00:47:28 +0100

    coreclr-git: Fix buildlink against custom paths with headers

--- coreclr-git/buildlink3.mk
+++ coreclr-git/buildlink3.mk
@@ -8,6 +8,12 @@ CORECLR_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.coreclr+=	coreclr>=0.0.0nb20160222
 BUILDLINK_PKGSRCDIR.coreclr?=	../../wip/coreclr-git
 
+BUILDLINK_INCDIRS.coreclr+=	CoreCLR/inc
+BUILDLINK_INCDIRS.coreclr+=	CoreCLR/gcinfo
+BUILDLINK_FILES.coreclr+=	CoreCLR/gcinfo/gcinfoencoder.cpp
+BUILDLINK_FILES.coreclr+=	CoreCLR/inc/*.h
+BUILDLINK_FILES.coreclr+=	CoreCLR/inc/*.def
+
 .include "../../textproc/icu/buildlink3.mk"
 .include "../../wip/llvm-git/buildlink3.mk"
 .include "../../wip/lldb-git/buildlink3.mk"
