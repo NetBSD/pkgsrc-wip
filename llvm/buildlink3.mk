e0a8a8ded5807696275cbef8a6f239c4f833f214
Tobias Nygren <tnn@NetBSD.org>
2016-02-22 22:47:31 +0100

    on second thought, it's better to use a DEPENDS line than abusing BUILDLINK_API_DEPENDS

--- llvm/buildlink3.mk
+++ llvm/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	llvm
 .if !defined(LLVM_BUILDLINK3_MK)
 LLVM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.llvm+=	llvm-3.8.[0-9]*
+BUILDLINK_API_DEPENDS.llvm+=	llvm>=3.8.0
 BUILDLINK_PKGSRCDIR.llvm?=	../../wip/llvm
 
 .include "../../devel/zlib/buildlink3.mk"
