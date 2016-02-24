bb6be8b418861341aac4bbd9cc8b8f0d58655b2e
Guillaume Delpierre <gde@llew.me>
2016-02-24 14:51:15 +0100

    Add patch for setup.py in order to correct utf8 issue

--- py-cookiecutter/patches/patch-setup.py
+++ py-cookiecutter/patches/patch-setup.py
@@ -1,16 +1,21 @@
-$NetBSD$
-
-Set encoding for history file to make this build in C locales.
-https://github.com/audreyr/cookiecutter/pull/638
-
---- setup.py.orig	2016-02-24 08:53:51.329247880 +0000
-+++ setup.py
-@@ -23,7 +23,7 @@ if sys.argv[-1] == 'tag':
+--- setup.py.orig	2016-02-16 08:36:10.000000000 +0100
++++ setup.py	2016-02-16 08:34:17.000000000 +0100
+@@ -23,9 +23,6 @@
  with open('README.rst') as readme_file:
      readme = readme_file.read()
  
 -with open('HISTORY.rst') as history_file:
-+with open('HISTORY.rst', encoding="utf-8") as history_file:
-     history = history_file.read().replace('.. :changelog:', '')
- 
+-    history = history_file.read().replace('.. :changelog:', '')
+-
  requirements = [
+     'future>=0.15.2',
+     'binaryornot>=0.2.0',
+@@ -34,7 +31,7 @@
+     'whichcraft>=0.1.1'
+ ]
+ 
+-long_description = readme + '\n\n' + history
++long_description = readme
+ 
+ if sys.argv[-1] == 'readme':
+     print(long_description)
