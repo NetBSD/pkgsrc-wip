78809c4de46afe5829da2d46085ab679d2870ab8
Havard Eidnes <he@NetBSD.org>
2016-02-25 16:45:54 +0100

    Reenable the IOS module, it's included in this package. FortiOS and NX-OS remain disabled because prerequisites have yet to be packaged.

--- py-napalm/patches/patch-napalm_____init____.py
+++ py-napalm/patches/patch-napalm_____init____.py
@@ -1,34 +1,28 @@
 $NetBSD$
 
+Comment out modules where we've not packaged the prerequisites yet.
+
 --- napalm/__init__.py.orig	2016-02-08 08:22:24.000000000 +0000
 +++ napalm/__init__.py
-@@ -15,10 +15,10 @@
+@@ -15,8 +15,8 @@
  from eos import EOSDriver
  from iosxr import IOSXRDriver
  from junos import JunOSDriver
 -from fortios import FortiOSDriver
 -from nxos import NXOSDriver
--from ibm import IBMDriver
--from ios import IOSDriver
 +# from fortios import FortiOSDriver
 +# from nxos import NXOSDriver
-+# from ibm import IBMDriver
-+# from ios import IOSDriver
+ from ibm import IBMDriver
+ from ios import IOSDriver
  
- def get_network_driver(vendor):
-     driver_mapping = {
-@@ -28,10 +28,10 @@ def get_network_driver(vendor):
+@@ -28,8 +28,8 @@ def get_network_driver(vendor):
          'IOSXR': IOSXRDriver,
          'JUNOS': JunOSDriver,
          'JUNIPER': JunOSDriver,
 -        'FORTIOS': FortiOSDriver,
 -        'NXOS': NXOSDriver,
--        'IBM': IBMDriver,
--        'IOS' : IOSDriver,
 +#        'FORTIOS': FortiOSDriver,
 +#        'NXOS': NXOSDriver,
-+#        'IBM': IBMDriver,
-+#        'IOS' : IOSDriver,
+         'IBM': IBMDriver,
+         'IOS' : IOSDriver,
      }
-     try:
-         return driver_mapping[vendor.upper()]
