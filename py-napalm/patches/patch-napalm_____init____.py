18bfb4abe60686dfc26eb6eab324e02624cb2c5e
Havard Eidnes <he@NetBSD.org>
2016-02-26 10:54:53 +0100

    Actually, the IBM blade server module depends on external modules we have not yet packaged, so comment that out for now.

--- py-napalm/patches/patch-napalm_____init____.py
+++ py-napalm/patches/patch-napalm_____init____.py
@@ -4,25 +4,29 @@ Comment out modules where we've not packaged the prerequisites yet.
 
 --- napalm/__init__.py.orig	2016-02-08 08:22:24.000000000 +0000
 +++ napalm/__init__.py
-@@ -15,8 +15,8 @@
+@@ -15,9 +15,9 @@
  from eos import EOSDriver
  from iosxr import IOSXRDriver
  from junos import JunOSDriver
 -from fortios import FortiOSDriver
 -from nxos import NXOSDriver
+-from ibm import IBMDriver
 +# from fortios import FortiOSDriver
 +# from nxos import NXOSDriver
- from ibm import IBMDriver
++# from ibm import IBMDriver
  from ios import IOSDriver
  
-@@ -28,8 +28,8 @@ def get_network_driver(vendor):
+ def get_network_driver(vendor):
+@@ -28,9 +28,9 @@ def get_network_driver(vendor):
          'IOSXR': IOSXRDriver,
          'JUNOS': JunOSDriver,
          'JUNIPER': JunOSDriver,
 -        'FORTIOS': FortiOSDriver,
 -        'NXOS': NXOSDriver,
+-        'IBM': IBMDriver,
 +#        'FORTIOS': FortiOSDriver,
 +#        'NXOS': NXOSDriver,
-         'IBM': IBMDriver,
++#         'IBM': IBMDriver,
          'IOS' : IOSDriver,
      }
+     try:
