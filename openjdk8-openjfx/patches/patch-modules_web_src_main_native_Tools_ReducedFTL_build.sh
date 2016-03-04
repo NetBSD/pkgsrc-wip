0d32efe86eca11fc564b8107b3d64ec33cf561d7
Tobias Nygren <tnn@NetBSD.org>
2016-03-04 02:05:58 +0100

    Import openjdk8-openjfx-0nb20160304 as wip/openjdk8-openjfx.
    OpenJFX is an open source, next generation client application platform for
    desktop and embedded systems based on JavaSE.
    
    XXX does not build yet.
    Terrible build framework and lots of Linux specific stuff to sort out.

---
+++ openjdk8-openjfx/patches/patch-modules_web_src_main_native_Tools_ReducedFTL_build.sh
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- modules/web/src/main/native/Tools/ReducedFTL/build.sh.orig	2016-03-03 23:18:49.000000000 +0000
++++ modules/web/src/main/native/Tools/ReducedFTL/build.sh
+@@ -3,7 +3,7 @@
+ set -e
+ set -x
+ 
+-if test "x${LLVM_PATH}" == "x"
++if test "x${LLVM_PATH}" = "x"
+ then
+     configPath="llvm-config"
+ else
