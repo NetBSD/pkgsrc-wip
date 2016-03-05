1ffd19008148e8803ac8f4e9022eae80016e2f96
coypu <coypu@sdf.org>
2016-03-05 11:23:47 +0200

    import grilo from pkgsrc

---
+++ grilo/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.10 2015/06/07 14:14:04 youri Exp $
+
+BUILDLINK_TREE+=	grilo
+
+.if !defined(GRILO_BUILDLINK3_MK)
+GRILO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.grilo+=	grilo>=0.2.2
+BUILDLINK_ABI_DEPENDS.grilo?=	grilo>=0.2.7nb3
+BUILDLINK_PKGSRCDIR.grilo?=	../../net/grilo
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../net/libsoup/buildlink3.mk"
+.endif	# GRILO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-grilo
