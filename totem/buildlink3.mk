07d7802a158da6a0f2d5ce6be1d578dcf110af68
coypu <coypu@sdf.org>
2016-03-05 10:36:27 +0200

    Import totem from pkgsrc

---
+++ totem/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.35 2015/04/25 14:23:41 tnn Exp $
+
+BUILDLINK_TREE+=	totem
+
+.if !defined(TOTEM_BUILDLINK3_MK)
+TOTEM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.totem+=	{totem,totem-xine}>=1.4.0
+BUILDLINK_ABI_DEPENDS.totem+=	totem>=2.32.0nb31
+BUILDLINK_PKGSRCDIR.totem?=	../../multimedia/totem
+
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif # TOTEM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-totem
