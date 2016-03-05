d00aeb5d56ef1353ef1768cf4c07da07cf06e1e1
coypu <coypu@sdf.org>
2016-03-05 11:34:10 +0200

    totem-pl-parser: buildlink to wip

--- totem-pl-parser/buildlink3.mk
+++ totem-pl-parser/buildlink3.mk
@@ -7,7 +7,7 @@ TOTEM_PL_PARSER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.totem-pl-parser+=	totem-pl-parser>=2.22.1
 BUILDLINK_ABI_DEPENDS.totem-pl-parser+=	totem-pl-parser>=3.4.5nb1
-BUILDLINK_PKGSRCDIR.totem-pl-parser?=	../../multimedia/totem-pl-parser
+BUILDLINK_PKGSRCDIR.totem-pl-parser?=	../../wip/totem-pl-parser
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../mail/gmime/buildlink3.mk"
