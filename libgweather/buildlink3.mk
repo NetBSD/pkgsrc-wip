b18622804b44d43e8c2f5c3e9d665cf2b0a85a3a
coypu <coypu@sdf.org>
2016-03-06 01:15:31 +0200

    libgweather: downgrade to 3.16.1

--- libgweather/buildlink3.mk
+++ libgweather/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libgweather
 .if !defined(LIBGWEATHER_BUILDLINK3_MK)
 LIBGWEATHER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgweather+=	libgweather>=3.18.1
+BUILDLINK_API_DEPENDS.libgweather+=	libgweather>=3.16.1
 BUILDLINK_PKGSRCDIR.libgweather?=	../../wip/libgweather
 
 .endif	# LIBGWEATHER_BUILDLINK3_MK
