22bfffcab5dc9eba5b94511c3f11dd7b20cf4a7b
coypu <coypu@sdf.org>
2016-03-07 21:23:15 +0200

    gnome-settings-daemon: broken update to 3.16.4

--- gnome-settings-daemon/buildlink3.mk
+++ gnome-settings-daemon/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=	gnome-settings-daemon
 .if !defined(GNOME_SETTINGS_DAEMON_BUILDLINK3_MK)
 GNOME_SETTINGS_DAEMON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnome-settings-daemon+=	gnome-settings-daemon>=2.22.0
-BUILDLINK_ABI_DEPENDS.gnome-settings-daemon+=	gnome-settings-daemon>=2.32.1nb13
-BUILDLINK_PKGSRCDIR.gnome-settings-daemon?=	../../sysutils/gnome-settings-daemon
+BUILDLINK_API_DEPENDS.gnome-settings-daemon+=	gnome-settings-daemon>=3.16.0
+BUILDLINK_ABI_DEPENDS.gnome-settings-daemon+=	gnome-settings-daemon>=3.16.1nb13
+BUILDLINK_PKGSRCDIR.gnome-settings-daemon?=	../../wip/gnome-settings-daemon
 
 .include "../../sysutils/dbus-glib/buildlink3.mk"
 .endif # GNOME_SETTINGS_DAEMON_BUILDLINK3_MK
