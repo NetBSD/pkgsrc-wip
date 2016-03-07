9d92ce7c4ac06407b64b79048f10803964a86c21
coypu <coypu@sdf.org>
2016-03-07 21:23:25 +0200

    geoclue: updated buildlink

--- geoclue/buildlink3.mk
+++ geoclue/buildlink3.mk
@@ -5,9 +5,8 @@ BUILDLINK_TREE+=	geoclue
 .if !defined(GEOCLUE_BUILDLINK3_MK)
 GEOCLUE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.geoclue+=	geoclue>=0.12.0
-BUILDLINK_ABI_DEPENDS.geoclue+=	geoclue>=0.12.0nb10
-BUILDLINK_PKGSRCDIR.geoclue?=	../../geography/geoclue
+BUILDLINK_API_DEPENDS.geoclue+=	geoclue>=2.2.0
+BUILDLINK_PKGSRCDIR.geoclue?=	../../wip/geoclue
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../sysutils/dbus-glib/buildlink3.mk"
