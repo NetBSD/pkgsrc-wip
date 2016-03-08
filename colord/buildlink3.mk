d7ca5292202db843a2a21b3f0cffda45e701a536
Thomas Klausner <wiz@NetBSD.org>
2016-03-08 13:31:04 +0100

    colord: add minimal buildlink3.mk

---
+++ colord/buildlink3.mk
@@ -0,0 +1,28 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	colord
+
+.if !defined(COLORD_BUILDLINK3_MK)
+COLORD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.colord+=	colord>=1.2.12
+BUILDLINK_PKGSRCDIR.colord?=	../../wip/colord
+
+# XXX
+# XXX Uncomment and keep only the buildlink3 lines below which are directly
+# XXX needed for dependencies to compile, link, and run.  If this package
+# XXX provides a wrappered API or otherwise does not expose the APIs of the
+# XXX buildlink3 lines below to dependencies, remove them.
+# XXX
+#.include "../../print/cups/buildlink3.mk"
+#.include "../../wip/geocode-glib/buildlink3.mk"
+#.include "../../devel/nss/buildlink3.mk"
+#.include "../../graphics/lcms2/buildlink3.mk"
+#.include "../../databases/sqlite3/buildlink3.mk"
+#.include "../../wip/polkit/buildlink3.mk"
+#.include "../../devel/libdbusmenu-qt/buildlink3.mk"
+#.include "../../sysutils/dbus/buildlink3.mk"
+#.include "../../devel/gobject-introspection/buildlink3.mk"
+.endif	# COLORD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-colord
