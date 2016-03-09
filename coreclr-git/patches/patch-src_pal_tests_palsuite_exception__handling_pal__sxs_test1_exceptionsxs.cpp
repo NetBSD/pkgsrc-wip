ebe2c6e27969928fc1d6239a4644c7133c13f327
Kamil Rytarowski <n54@gmx.com>
2016-03-09 01:28:20 +0100

    coreclr-git: Reenable PAL sxs_test1

---
+++ coreclr-git/patches/patch-src_pal_tests_palsuite_exception__handling_pal__sxs_test1_exceptionsxs.cpp
@@ -0,0 +1,22 @@
+$NetBSD$
+
+--- src/pal/tests/palsuite/exception_handling/pal_sxs/test1/exceptionsxs.cpp.orig	2016-02-27 21:09:29.000000000 +0000
++++ src/pal/tests/palsuite/exception_handling/pal_sxs/test1/exceptionsxs.cpp
+@@ -31,7 +31,7 @@ extern "C" int InitializeDllTest2();
+ extern "C" int DllTest1();
+ extern "C" int DllTest2();
+ 
+-#if !defined(__FreeBSD__) && !defined(__NetBSD__)
++#if !defined(__FreeBSD__)
+ 
+ bool bSignal = false;
+ bool bCatch = false;
+@@ -73,7 +73,7 @@ void sigsegv_handler(int code, siginfo_t
+ 
+ int __cdecl main(int argc, char *argv[])
+ {
+-#if !defined(__FreeBSD__) && !defined(__NetBSD__)
++#if !defined(__FreeBSD__)
+     struct sigaction newAction;
+     struct sigaction oldAction;
+     newAction.sa_flags = SA_SIGINFO | SA_RESTART;
