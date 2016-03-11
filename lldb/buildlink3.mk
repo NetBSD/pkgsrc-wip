a71f9f5285cc7dc22beb557d3b071504f8635a95
Tobias Nygren <tnn@NetBSD.org>
2016-03-11 10:56:28 +0100

    delete clang. Updated in pkgsrc.

--- lldb/buildlink3.mk
+++ lldb/buildlink3.mk
@@ -9,8 +9,8 @@ BUILDLINK_API_DEPENDS.lldb+=	lldb>=3.8.0
 BUILDLINK_PKGSRCDIR.lldb?=	../../wip/lldb
 
 .include "../../textproc/libxml2/buildlink3.mk"
-.include "../../wip/llvm/buildlink3.mk"
-.include "../../wip/clang/buildlink3.mk"
+.include "../../lang/llvm/buildlink3.mk"
+.include "../../lang/clang/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../devel/swig/buildlink3.mk"
 .endif	# LLDB_BUILDLINK3_MK
