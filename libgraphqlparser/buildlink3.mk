40c4abe02a4a3d88c489881fb982de6d33648664
Kamil Rytarowski <n54@gmx.com>
2016-03-13 17:37:54 +0100

    libgraphqlparser: Add buildlink3.mk

---
+++ libgraphqlparser/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libgraphqlparser
+
+.if !defined(LIBGRAPHQLPARSER_BUILDLINK3_MK)
+LIBGRAPHQLPARSER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libgraphqlparser+=	libgraphqlparser>=0.4.1
+BUILDLINK_PKGSRCDIR.libgraphqlparser?=	../../wip/libgraphqlparser
+.endif	# LIBGRAPHQLPARSER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libgraphqlparser
