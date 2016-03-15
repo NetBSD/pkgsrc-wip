75989e26ecb3973d1348ded3807b549a6111e272
coypu <coypu@sdf.org>
2016-03-15 10:30:49 +0200

    gnome-menus: updated buildlink ver

--- gnome-menus/buildlink3.mk
+++ gnome-menus/buildlink3.mk
@@ -5,8 +5,7 @@ BUILDLINK_TREE+=	gnome-menus
 .if !defined(GNOME_MENUS_BUILDLINK3_MK)
 GNOME_MENUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnome-menus+=		gnome-menus>=2.13.5
-BUILDLINK_ABI_DEPENDS.gnome-menus+=	gnome-menus>=3.10.1nb1
+BUILDLINK_API_DEPENDS.gnome-menus+=	gnome-menus>=3.10.1
 BUILDLINK_PKGSRCDIR.gnome-menus?=	../../wip/gnome-menus
 
 .include "../../devel/glib2/buildlink3.mk"
