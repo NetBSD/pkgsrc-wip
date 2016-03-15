7ed36c7c453001f40597ea2dabb2147184000072
coypu <coypu@sdf.org>
2016-03-15 10:29:08 +0200

    epiphany: change buildlink ver, add license

--- epiphany/buildlink3.mk
+++ epiphany/buildlink3.mk
@@ -5,15 +5,14 @@ BUILDLINK_TREE+=	epiphany
 .if !defined(EPIPHANY_BUILDLINK3_MK)
 EPIPHANY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.epiphany+=		epiphany>=2.28
-BUILDLINK_ABI_DEPENDS.epiphany+=		epiphany>=2.30.6nb44
+BUILDLINK_API_DEPENDS.epiphany+=	epiphany>=2.28
+BUILDLINK_ABI_DEPENDS.epiphany+=	epiphany>=2.30.6nb44
 BUILDLINK_PKGSRCDIR.epiphany?=		../../www/epiphany
 
 .include "../../mk/bsd.fast.prefs.mk"
 
 .include "../../devel/GConf/buildlink3.mk"
 .include "../../devel/nss/buildlink3.mk"
-.include "../../www/webkit24-gtk/buildlink3.mk"
 .include "../../net/ORBit2/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
