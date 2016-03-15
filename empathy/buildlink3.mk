9314d57f37dc6e057540152bd85ebb1b0c15ae09
coypu <coypu@sdf.org>
2016-03-15 09:37:30 +0200

    empathy: dont build older gnome

--- empathy/buildlink3.mk
+++ empathy/buildlink3.mk
@@ -5,9 +5,8 @@ BUILDLINK_TREE+=	empathy
 .if !defined(EMPATHY_BUILDLINK3_MK)
 EMPATHY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.empathy+=	empathy>=2.24.1nb1
-BUILDLINK_ABI_DEPENDS.empathy+=	empathy>=2.34.0nb43
-BUILDLINK_PKGSRCDIR.empathy?=	../../chat/empathy
+BUILDLINK_API_DEPENDS.empathy+= empathy>=3.12.10
+BUILDLINK_PKGSRCDIR.empathy?=   ../../wip/empathy
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../wip/evolution-data-server/buildlink3.mk"
