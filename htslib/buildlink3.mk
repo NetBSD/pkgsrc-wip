40ded0b7359b21d52ed1c1ee88c95cb429979057
i3enedek <pkgsrc_git@i3enedek.neomailbox.net>
2016-03-19 01:35:51 +0000

    Importing htslib-1.3

---
+++ htslib/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	htslib
+
+.if !defined(HTSLIB_BUILDLINK3_MK)
+HTSLIB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.3
+BUILDLINK_PKGSRCDIR.htslib?=	../../wip/htslib
+.endif	# HTSLIB_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-htslib
