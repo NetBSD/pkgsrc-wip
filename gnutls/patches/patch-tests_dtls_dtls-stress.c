42f66db67895080abf078d3fcc818c2effe36b16
coypu <coypu@sdf.org>
2016-03-23 23:57:30 +0200

    gnutls: cleaner fix, obsolete patch

--- gnutls/patches/patch-tests_dtls_dtls-stress.c
+++ gnutls/patches/patch-tests_dtls_dtls-stress.c
@@ -1,19 +1,13 @@
 $NetBSD$
 
-Running 'make test':
-undefined reference to `rpl_strerror'
-
-https://gitlab.com/gnutls/gnutls/issues/81
-
 --- tests/dtls/dtls-stress.c.orig	2015-01-19 17:38:09.000000000 +0000
 +++ tests/dtls/dtls-stress.c
-@@ -346,8 +346,7 @@ static void _process_error_or_timeout(in
+@@ -98,6 +98,8 @@
+ #include <time.h>
+ #include <sys/wait.h>
  
- static void rperror(const char *name)
- {
--	fprintf(stdout, "%i %s| %s: %s\n", run_id, role_name, name,
--		strerror(errno));
-+	fprintf(stdout, "%i %s| %s\n", run_id, role_name, name);
- }
++#undef strerror
++
+ #if _POSIX_TIMERS && (_POSIX_TIMERS - 200112L) >= 0
  
- // }}}
+ // {{{ types
