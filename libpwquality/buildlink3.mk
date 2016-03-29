6a396870b4d3feae6986cb9f0113f31e4d77a497
Thomas Klausner <wiz@NetBSD.org>
2016-03-29 12:44:33 +0200

    libpwquality: Add bl3.mk (for gnome-control-center).

---
+++ libpwquality/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libpwquality
+
+.if !defined(LIBPWQUALITY_BUILDLINK3_MK)
+LIBPWQUALITY_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libpwquality+=	libpwquality>=1.3.0
+BUILDLINK_PKGSRCDIR.libpwquality?=	../../wip/libpwquality
+
+.include "../../wip/py-cracklib/buildlink3.mk"
+.endif	# LIBPWQUALITY_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libpwquality
