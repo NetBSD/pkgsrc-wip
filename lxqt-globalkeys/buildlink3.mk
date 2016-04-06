0d097d47de929fa34a37ac856ec05753e0c7b164
youri <youri.mout@gmail.com>
2016-04-06 22:20:52 +0200

    lxqt-globalkeys: update to 0.10.0

--- lxqt-globalkeys/buildlink3.mk
+++ lxqt-globalkeys/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	lxqt-globalkeys
 .if !defined(LXQT_GLOBALKEYS_BUILDLINK3_MK)
 LXQT_GLOBALKEYS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=0.9.0
+BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=0.10.0
 BUILDLINK_PKGSRCDIR.lxqt-globalkeys?=	../../wip/lxqt-globalkeys
 
 .include "../../wip/liblxqt/buildlink3.mk"
