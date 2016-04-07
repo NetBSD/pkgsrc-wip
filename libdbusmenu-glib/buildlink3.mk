f1a29b8813fa243f1ec71001fde0cde62059e845
youri <youri@NetBSD.org>
2016-04-07 18:26:43 +0200

    libdbusmenu-glib: fix clang build

--- libdbusmenu-glib/buildlink3.mk
+++ libdbusmenu-glib/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libdbusmenu-glib
 .if !defined(LIBDBUSMENU_GLIB_BUILDLINK3_MK)
 LIBDBUSMENU_GLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libdbusmenu-glib+=	libdbusmenu-glib>=12.10.2
+BUILDLINK_API_DEPENDS.libdbusmenu-glib+=libdbusmenu-glib>=12.10.2
 BUILDLINK_PKGSRCDIR.libdbusmenu-glib?=	../../wip/libdbusmenu-glib
 
 .include "../../devel/glib2/buildlink3.mk"
