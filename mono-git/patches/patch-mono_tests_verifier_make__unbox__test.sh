0f6980063a6159f6e7e2765fc396321081cf81d5
Kamil Rytarowski <n54@gmx.com>
2016-04-17 22:50:29 +0200

    mono-git: Fix portability of a shell construct

---
+++ mono-git/patches/patch-mono_tests_verifier_make__unbox__test.sh
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- mono/tests/verifier/make_unbox_test.sh.orig	2016-04-16 14:26:45.000000000 +0000
++++ mono/tests/verifier/make_unbox_test.sh
+@@ -12,7 +12,7 @@ TEST_TYPE2=$4
+ TEST_POST_OP=$5
+ TEST_NO_BOX=$6
+ TEST_BOX="box ${TEST_TYPE1}";
+-if [ "${TEST_NO_BOX}" == "yes" ]; then
++if [ "x${TEST_NO_BOX}" = "xyes" ]; then
+ 	TEST_BOX="";
+ fi
+ 
