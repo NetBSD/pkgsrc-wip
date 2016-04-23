bb90f695e662a5aab9da3a612a179df62218b10d
youri <youri.mout@gmail.com>
2016-04-23 23:51:51 +0200

    mate-desktop: Bump required version.

--- mate-desktop/buildlink3.mk
+++ mate-desktop/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	mate-desktop
 .if !defined(MATE_DESKTOP_BUILDLINK3_MK)
 MATE_DESKTOP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mate-desktop+=	mate-desktop>=1.8.1
+BUILDLINK_API_DEPENDS.mate-desktop+=	mate-desktop>=1.14.0
 BUILDLINK_PKGSRCDIR.mate-desktop?=	../../wip/mate-desktop
 
 .include "../../devel/dconf/buildlink3.mk"
