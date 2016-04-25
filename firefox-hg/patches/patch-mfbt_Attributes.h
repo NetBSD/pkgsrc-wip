696aed5b1c9e88e6b24e065bbf830deb49c1a5d0
Thomas Klausner <wiz@NetBSD.org>
2016-04-25 23:32:28 +0200

    firefox-hg: make patches apply again
    still fails to build with
    
    Unified_cpp_dom_media_webaudio1.cpp:(.text+0x19e5): undefined reference to `__c99_cabs'
    Unified_cpp_dom_media_webaudio1.cpp:(.text+0x19fa): undefined reference to `__c99_cabs'
    ../../dom/media/webaudio/Unified_cpp_dom_media_webaudio1.o: In function `mozilla::FFTBlock::AddConstantGroupDelay(double)':
    Unified_cpp_dom_media_webaudio1.cpp:(.text+0x1d1d): undefined reference to `__c99_cabs'
    ../../dom/media/webaudio/Unified_cpp_dom_media_webaudio1.o: In function `mozilla::FFTBlock::ExtractAverageGroupDelay()':
    Unified_cpp_dom_media_webaudio1.cpp:(.text+0x1e85): undefined reference to `__c99_cabs'
    ../../dom/media/webaudio/blink/Unified_cpp_webaudio_blink0.o: In function `WebCore::Biquad::setZeroPolePairs(std::complex<double> const&, std::complex<double> const&)':
    Unified_cpp_webaudio_blink0.cpp:(.text+0x122b): undefined reference to `__c99_cabs'
    ../../dom/media/webaudio/blink/Unified_cpp_webaudio_blink0.o:Unified_cpp_webaudio_blink0.cpp:(.text+0x1259): more undefined references to `__c99_cabs' follow

--- firefox-hg/patches/patch-mfbt_Attributes.h
+++ firefox-hg/patches/patch-mfbt_Attributes.h
@@ -1,6 +1,6 @@
 $NetBSD: patch-mfbt_Attributes.h,v 1.6 2015/08/17 07:30:48 thomasklausner Exp $
 
---- mfbt/Attributes.h.orig	2015-08-16 14:08:46.000000000 +0000
+--- mfbt/Attributes.h.orig	2016-04-25 13:39:04.000000000 +0000
 +++ mfbt/Attributes.h
 @@ -50,6 +50,7 @@
   * don't indicate support for them here, due to
@@ -10,7 +10,7 @@ $NetBSD: patch-mfbt_Attributes.h,v 1.6 2015/08/17 07:30:48 thomasklausner Exp $
  #  define MOZ_HAVE_NEVER_INLINE          __declspec(noinline)
  #  define MOZ_HAVE_NORETURN              __declspec(noreturn)
  #  if _MSC_VER >= 1900
-@@ -73,6 +74,9 @@
+@@ -72,6 +73,9 @@
  #  ifndef __has_extension
  #    define __has_extension __has_feature /* compatibility, for older versions of clang */
  #  endif
@@ -20,7 +20,7 @@ $NetBSD: patch-mfbt_Attributes.h,v 1.6 2015/08/17 07:30:48 thomasklausner Exp $
  #  if __has_extension(cxx_constexpr)
  #    define MOZ_HAVE_CXX11_CONSTEXPR
  #  endif
-@@ -86,6 +90,9 @@
+@@ -85,6 +89,9 @@
  #    define MOZ_HAVE_NORETURN            __attribute__((noreturn))
  #  endif
  #elif defined(__GNUC__)
@@ -29,4 +29,4 @@ $NetBSD: patch-mfbt_Attributes.h,v 1.6 2015/08/17 07:30:48 thomasklausner Exp $
 +#  endif
  #  if defined(__GXX_EXPERIMENTAL_CXX0X__) || __cplusplus >= 201103L
  #    define MOZ_HAVE_CXX11_CONSTEXPR
- #    if MOZ_GCC_VERSION_AT_LEAST(4, 8, 0)
+ #    define MOZ_HAVE_CXX11_CONSTEXPR_IN_TEMPLATES
