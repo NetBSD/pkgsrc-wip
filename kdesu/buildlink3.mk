de557fc957dd6a85c10272fe37dd80e988fb1355
Mark Davies <mark@ecs.vuw.ac.nz>
2016-04-28 14:11:47 +1200

    kdesu: Install kdesu-5.21.0
    KDESU provides functionality for building GUI front ends for
    (password asking) console mode programs. For example, kdesu and
    kdessh use it to interface with su and ssh respectively.

---
+++ kdesu/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	kdesu
+
+.if !defined(KDESU_BUILDLINK3_MK)
+KDESU_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kdesu+=	kdesu>=5.21.0
+BUILDLINK_PKGSRCDIR.kdesu?=	../../wip/kdesu
+
+.include "../../wip/kpty/buildlink3.mk"
+.include "../../wip/kservice/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KDESU_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kdesu
