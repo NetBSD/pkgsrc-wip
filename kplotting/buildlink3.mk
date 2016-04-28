437daba01106e5030c8ef36b0214679b1be8b431
Mark Davies <mark@ecs.vuw.ac.nz>
2016-04-28 13:47:42 +1200

    kplotting: add buildlink3.mk

---
+++ kplotting/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	kplotting
+
+.if !defined(KPLOTTING_BUILDLINK3_MK)
+KPLOTTING_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kplotting+=	kplotting>=5.21.0
+BUILDLINK_PKGSRCDIR.kplotting?=	../../wip/kplotting
+
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# KPLOTTING_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kplotting
