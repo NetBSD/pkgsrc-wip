6d64f162fb97f5e1352103406cf0d3940566a402
Kamil Rytarowski <n54@gmx.com>
2016-05-03 03:08:45 +0200

    coreclr-git: Switch to devel version of the LLVM stack

--- coreclr-git/buildlink3.mk
+++ coreclr-git/buildlink3.mk
@@ -15,10 +15,16 @@ BUILDLINK_FILES.coreclr+=	CoreCLR/inc/*.h
 BUILDLINK_FILES.coreclr+=	CoreCLR/inc/*.def
 
 .include "../../textproc/icu/buildlink3.mk"
-.include "../../lang/llvm/buildlink3.mk"
-.include "../../devel/lldb/buildlink3.mk"
-.include "../../lang/clang/buildlink3.mk"
-.include "../../lang/libunwind/buildlink3.mk"
+# Stable LLVM
+#.include "../../lang/llvm/buildlink3.mk"
+#.include "../../devel/lldb/buildlink3.mk"
+#.include "../../lang/clang/buildlink3.mk"
+#.include "../../lang/libunwind/buildlink3.mk"
+# Devel LLVM
+.include "../../wip/llvm-git/buildlink3.mk"
+.include "../../wip/lldb-git/buildlink3.mk"
+.include "../../wip/clang-git/buildlink3.mk"
+.include "../../wip/libunwind-git/buildlink3.mk"
 .endif	# CORECLR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-coreclr
