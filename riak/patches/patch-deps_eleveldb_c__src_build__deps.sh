00dd752c129cae8f767371b0605895272c4cd1fd
Filip Hajny <filip@joyent.com>
2016-05-06 10:25:04 +0000

    Update riak to 2.1.4.
    - eleveldb has been upgraded to version 2.0.17, which contains
      fixes for these issues:
      - Riak KV restarts and active anti-entropy (AAE) tree rebuilds
        could cause a segfault, terminating Riak KV's process.
      - LevelDB's tiered storage is susceptible to data loss if Riak
        KV is stopped and started with less than 60 Mb stored per
        vnode.
      - LevelDB's recovery option does not work with tier-storage due
        to failed parameter passing with Riak KV.
      - Riak KV's active anti-entropy (AAE) process has potential to
        induce a segfault, though this issue has only been seen in new
        development code and not in existing releases.
    - File permissions/ownership in packaged releases generated by the
      node_package library has been tightened.
    - The wording of a failed communication attempt when using
      command-line tools to interact with Riak KV has been changed, as
      the failure of attempt is not always due to a down node.

--- riak/patches/patch-deps_eleveldb_c__src_build__deps.sh
+++ riak/patches/patch-deps_eleveldb_c__src_build__deps.sh
@@ -1,9 +1,9 @@
 $NetBSD: patch-deps_eleveldb_c__src_build__deps.sh,v 1.1 2014/11/28 20:38:31 fhajny Exp $
 
 Do not detect lzf includes, breaks on NetBSD.
---- deps/eleveldb/c_src/build_deps.sh.orig	2014-11-05 19:41:07.000000000 +0000
-+++ deps/eleveldb/c_src/build_deps.sh	2014-11-27 16:04:35.000000000 +0000
-@@ -62,7 +62,7 @@ case "$1" in
+--- deps/eleveldb/c_src/build_deps.sh.orig	2016-04-12 14:46:02.000000000 +0000
++++ deps/eleveldb/c_src/build_deps.sh
+@@ -67,7 +67,7 @@ case "$1" in
      *)
          if [ ! -d snappy-$SNAPPY_VSN ]; then
              tar -xzf snappy-$SNAPPY_VSN.tar.gz
@@ -11,4 +11,4 @@ Do not detect lzf includes, breaks on NetBSD.
 +            (cd snappy-$SNAPPY_VSN && ./configure --prefix=$BASEDIR/system --libdir=$BASEDIR/system/lib --with-pic ac_cv_lib_lzf_lzf_compress=no)
          fi
  
-         (cd snappy-$SNAPPY_VSN && $MAKE && $MAKE install)
+         if [ ! -f system/lib/libsnappy.a ]; then
--- riak/patches/patch-deps_node__package_priv_base_env.sh
+++ riak/patches/patch-deps_node__package_priv_base_env.sh
@@ -3,7 +3,7 @@ $NetBSD: patch-deps_node__package_priv_base_env.sh,v 1.4 2015/02/23 17:32:17 fha
 Substitute whoami for id across SunOS platforms.
 Let us substitute run dir.
 
---- deps/node_package/priv/base/env.sh.orig	2015-02-11 20:15:28.000000000 +0000
+--- deps/node_package/priv/base/env.sh.orig	2016-03-21 18:53:19.000000000 +0000
 +++ deps/node_package/priv/base/env.sh
 @@ -30,7 +30,7 @@ APP_VERSION={{app_version}}
  # Variables needed to support creation of .pid files
@@ -14,16 +14,3 @@ Let us substitute run dir.
  PID_DIR=$RUN_DIR/$RUNNER_SCRIPT
  PID_FILE=$PID_DIR/$RUNNER_SCRIPT.pid
  
-@@ -44,7 +44,11 @@ fi
- # Registered process to wait for to consider start a success
- WAIT_FOR_PROCESS={{runner_wait_process}}
- 
--WHOAMI=`whoami`
-+if [ `uname -s` = 'SunOS' ]; then
-+    WHOAMI=$(/usr/xpg4/bin/id -un)
-+else
-+    WHOAMI=$(whoami)
-+fi
- 
- # Echo to stderr on errors
- echoerr() { echo "$@" 1>&2; }
