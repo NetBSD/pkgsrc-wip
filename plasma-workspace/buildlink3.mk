351f06c84b84d7a2ffdf7147857e161f10d08325
Mark Davies <mark@ecs.vuw.ac.nz>
2016-05-09 23:39:54 +1200

    plasma-workspace: Add plasma-workspace 5.6.3
    KDE Plasma Workspace

---
+++ plasma-workspace/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	plasma-workspace
+
+.if !defined(PLASMA_WORKSPACE_BUILDLINK3_MK)
+PLASMA_WORKSPACE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.plasma-workspace+=	plasma-workspace>=5.6.3
+BUILDLINK_PKGSRCDIR.plasma-workspace?=	../../wip/plasma-workspace
+
+.include "../../wip/knewstuff/buildlink3.mk"
+.include "../../wip/kxmlgui/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# PLASMA_WORKSPACE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-plasma-workspace
