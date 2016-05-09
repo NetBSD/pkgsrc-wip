f5b7add93ef8634213d9dd80b57a23a2c4d95d35
Mark Davies <mark@ecs.vuw.ac.nz>
2016-05-09 23:35:52 +1200

    dolphin5: Add dolphin 10.04.0
    Dolphin is the default file manager for KDE, designed with usability
    as a primary focus.

---
+++ dolphin5/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	dolphin
+
+.if !defined(DOLPHIN_BUILDLINK3_MK)
+DOLPHIN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.dolphin+=	dolphin>=16.04.0
+BUILDLINK_PKGSRCDIR.dolphin?=	../../wip/dolphin5
+
+#.include "../../wip/baloo-widgets/buildlink3.mk"
+#.include "../../wip/kactivities5/buildlink3.mk"
+#.include "../../wip/kcmutils/buildlink3.mk"
+#.include "../../wip/knewstuff/buildlink3.mk"
+#.include "../../wip/kparts/buildlink3.mk"
+#.include "../../wip/plasma-workspace/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# DOLPHIN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-dolphin
