42edb65dc7617dada4f2ad8c4c42f03b52950dc1
Mark Davies <mark@ecs.vuw.ac.nz>
2016-05-09 23:30:32 +1200

    baloo-widgets: Add baloo-widgets 16.04.0
    Baloo is a framework for searching and managing metadata.

---
+++ baloo-widgets/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	baloo-widgets
+
+.if !defined(BALOO_WIDGETS_BUILDLINK3_MK)
+BALOO_WIDGETS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.baloo-widgets+=	baloo-widgets>=16.04.0
+BUILDLINK_PKGSRCDIR.baloo-widgets?=	../../wip/baloo-widgets
+
+.include "../../wip/baloo5/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.endif	# BALOO_WIDGETS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-baloo-widgets
