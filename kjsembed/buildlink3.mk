887c83221c14aaca2d47bb97f486ab1751dc4f94
Mark Davies <mark@ecs.vuw.ac.nz>
2016-05-09 23:46:44 +1200

    kjsembed: Add kjsembed 5.21.0
    KSJEmbed provides a method of binding JavaScript objects to QObjects,
    so you can script your applications.

---
+++ kjsembed/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	kjsembed
+
+.if !defined(KJSEMBED_BUILDLINK3_MK)
+KJSEMBED_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.kjsembed+=	kjsembed>=5.21.0
+BUILDLINK_PKGSRCDIR.kjsembed?=	../../wip/kjsembed
+
+.include "../../wip/ki18n/buildlink3.mk"
+.include "../../wip/kjs/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtsvg/buildlink3.mk"
+.include "../../x11/qt5-qttools/buildlink3.mk"
+.endif	# KJSEMBED_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-kjsembed
