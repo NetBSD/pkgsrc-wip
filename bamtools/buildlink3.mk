2b28ae0b313fb4fd8072bf35f0ae9286a7ca8bf2
Jason Bacon <bacon4000@gmail.com>
2016-05-11 14:05:46 -0500

    Import /home/bacon/Pkgsrc/pkgsrc-2016Q1/wip/bamtools bamtools-2.4.0 as wip/bamtools.
    "BamTools provides both a programmer's API and an end-user's
    toolkit for handling BAM files."

---
+++ bamtools/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	bamtools
+
+.if !defined(BAMTOOLS_BUILDLINK3_MK)
+BAMTOOLS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.bamtools+=	bamtools>=2.4.0
+BUILDLINK_PKGSRCDIR.bamtools?=	../../wip/bamtools
+.endif	# BAMTOOLS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-bamtools
