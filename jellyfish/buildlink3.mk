2f77da432c3d8ce5ef3091cd9ef4353bce41f2cf
Jason Bacon <bacon4000@gmail.com>
2016-05-11 10:05:52 -0500

    Update jellyfish from jb-wip

--- jellyfish/buildlink3.mk
+++ jellyfish/buildlink3.mk
@@ -1,14 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/01/28 16:36:28 outpaddling Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.16.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.jellyfish?=	build
+# $NetBSD$
 
 BUILDLINK_TREE+=	jellyfish
 
