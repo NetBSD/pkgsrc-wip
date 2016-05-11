47935539698bb53685f697adca557392e34b3619
Mateusz Poszwa <old4@o2.pl>
2016-05-11 12:00:52 +0200

    uTox-git: Update a patch

--- uTox-git/patches/patch-src_main.h
+++ uTox-git/patches/patch-src_main.h
@@ -2,14 +2,14 @@ $NetBSD$
 
 The volatile macro is used in {amd64,i386,machine}/mcontext.h on NetBSD.
 
---- src/main.h.orig	2016-03-21 09:05:38.000000000 +0000
+--- src/main.h.orig	2016-05-11 09:22:11.668807995 +0000
 +++ src/main.h
-@@ -33,7 +33,7 @@
- #define countof(x) (sizeof(x)/sizeof(*(x)))
- 
+@@ -40,7 +40,7 @@
  //  fixes compile with apple headers
+ /*** This breaks both android and Windows video... but it's needed to fix complation in clang (Cocoa & asan)
+  ***  TODO fix them?
 -#ifndef __OBJC__
-+#if !defined(__OBJC__) && !defined(__NetBSD__)
++##if !defined(__OBJC__) && !defined(__NetBSD__)
  #define volatile(x) (*((volatile typeof(x)*)&x))
- #endif
+ #endif */
  
