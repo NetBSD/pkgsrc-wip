4adc15dee28b9e765d2e188939bafb1565d2a72a
Jason Bacon <bacon4000@gmail.com>
2016-05-12 12:54:23 -0500

    Import /home/bacon/Pkgsrc/pkgsrc-2016Q1/wip/slurm-devel slurm-16.05.0 as wip/slurm-devel.
    SLURM is an open-source resource manager designed for Linux
    clusters of all sizes. It provides three key functions. First it
    allocates exclusive and/or non-exclusive access to resources
    (computer nodes) to users for some duration of time so they can
    perform work. Second, it provides a framework for starting,
    executing, and monitoring work (typically a parallel job) on a
    set of allocated nodes. Finally, it arbitrates contention for
    resources by managing a queue of pending work.

---
+++ slurm-devel/patches/patch-src_slurmd_slurmstepd_task.c
@@ -0,0 +1,22 @@
+$NetBSD: patch-src_slurmd_slurmstepd_task.c,v 1.2 2015/04/20 13:08:37 outpaddling Exp $
+
+--- src/slurmd/slurmstepd/task.c.orig	2015-03-19 19:38:01.000000000 +0000
++++ src/slurmd/slurmstepd/task.c
+@@ -540,6 +540,17 @@ _make_tmpdir(stepd_step_rec_t *job)
+ 		 * still work with older systems we include this check.
+ 		 */
+ 
++/* FIXME: Come up with a real solution for EUID instead of substituting RUID */
++#if defined(__NetBSD__)
++#define eaccess(p,m) (access((p),(m)))
++#define HAVE_EACCESS 1
++#endif
++  
++#if defined(__FreeBSD__) || defined(__NetBSD__)
++#define __GLIBC__               (1) 
++#define __GLIBC_PREREQ(a,b)     (1) 
++#endif
++
+ #if defined(HAVE_FACCESSAT)
+ 		else if (faccessat(AT_FDCWD, tmpdir, X_OK|W_OK, AT_EACCESS))
+ #elif defined(HAVE_EACCESS)
