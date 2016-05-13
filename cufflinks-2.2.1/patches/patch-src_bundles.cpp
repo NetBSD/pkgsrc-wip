878045f761226ecd0506ed27773a07bdbcf3a6ec
Jason Bacon <bacon4000@gmail.com>
2016-05-12 21:59:52 -0500

    Import /home/bacon/Pkgsrc/pkgsrc-2016Q1/wip/cufflinks-2.2.1 cufflinks-2.2.1 | tail -n 1 as wip/cufflinks-2.2.1.
    Latest not-so-stable version.
    
    Cufflinks assembles transcripts, estimates their abundances, and tests for
    differential expression and regulation in RNA-Seq samples.
    
    WWW: http://port.home.page/

---
+++ cufflinks-2.2.1/patches/patch-src_bundles.cpp
@@ -0,0 +1,13 @@
+$NetBSD: patch-src_bundles.cpp,v 1.1 2014/09/04 21:51:02 outpaddling Exp $
+
+--- src/bundles.cpp.orig	2014-03-24 21:54:47.000000000 +0000
++++ src/bundles.cpp
+@@ -698,7 +698,7 @@ double BundleFactory::next_valid_alignme
+         if (!_hit_fac->get_hit_from_buf(hit_buf, tmp, false))
+             continue;
+         
+-		if (tmp.ref_id() == 12638153115695167477)  // corresponds to SAM "*" under FNV hash. unaligned read record 
++		if (tmp.ref_id() == 12638153115695167477ULL)  // corresponds to SAM "*" under FNV hash. unaligned read record 
+             continue;
+         
+ 		raw_mass += tmp.mass();
