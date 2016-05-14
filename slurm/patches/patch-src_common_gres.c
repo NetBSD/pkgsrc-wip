dd869b254a181af3d4a8ba4f15898fcb171c092d
Jason Bacon <bacon4000@gmail.com>
2016-05-14 14:36:52 -0500

    Successful build on NetBSD 6

--- slurm/patches/patch-src_common_gres.c
+++ slurm/patches/patch-src_common_gres.c
@@ -1,22 +1,23 @@
 $NetBSD$
 
---- gres.c.orig	2016-05-03 22:41:59.000000000 +0000
-+++ gres.c
-@@ -79,6 +79,20 @@ typedef cpuset_t cpu_set_t;
+--- src/common/gres.c.orig	2016-05-03 22:41:59.000000000 +0000
++++ src/common/gres.c
+@@ -79,6 +79,21 @@ typedef cpuset_t cpu_set_t;
  #include <sys/stat.h>
  #include <math.h>
  
 +#ifdef __NetBSD__
-+#define	CPU_ZERO(p) cpuset_zero((p))
++#define CPU_ZERO(p) cpuset_zero((p))
 +#define CPU_ISSET(i,c) cpuset_isset((i),(c))
 +#define sched_getaffinity sched_getaffinity_np
 +#define SCHED_GETAFFINITY_THREE_ARGS
 +
-+// http://ftp.netbsd.org/pub/NetBSD/NetBSD-current/src/common/lib/libc/sys/cpuset.c
++// From /usr/src/common/lib/libc/sys/cpuset.c
 +struct _cpuset {
-+	uint32_t	bits[0];
++       uint32_t        bits[0];
 +};
 +
++// Must come after #include <sched.h>
 +typedef cpuset_t cpu_set_t;
 +#endif
 +
