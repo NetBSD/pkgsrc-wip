be9741a334bc425f11d6131797961ab0f951bce0
Jason Bacon <bacon4000@gmail.com>
2016-05-14 14:01:00 -0500

    Add patches for NetBSD

---
+++ slurm/patches/patch-src_common_callerid.c
@@ -0,0 +1,17 @@
+$NetBSD$
+
+--- src/common/callerid.c.orig	2016-05-03 22:41:59.000000000 +0000
++++ src/common/callerid.c
+@@ -61,6 +61,12 @@
+ #define s6_addr32 __u6_addr.__u6_addr32
+ #endif
+ 
++#ifdef __NetBSD__
++#define s6_addr8  __u6_addr.__u6_addr8
++#define s6_addr16 __u6_addr.__u6_addr16
++#define s6_addr32 __u6_addr.__u6_addr32
++#endif
++
+ #if HAVE_DIRENT_H
+ #  include <dirent.h>
+ #endif
