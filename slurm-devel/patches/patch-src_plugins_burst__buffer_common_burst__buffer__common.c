52fadc6c372b61fa3aef90eb162981e3c2e6fe6f
Jason Bacon <bacon4000@gmail.com>
2016-05-15 10:02:47 -0500

    Patches for NetBSD

---
+++ slurm-devel/patches/patch-src_plugins_burst__buffer_common_burst__buffer__common.c
@@ -0,0 +1,14 @@
+$NetBSD$
+
+# Linux compat
+--- src/plugins/burst_buffer/common/burst_buffer_common.c.orig	2016-05-14 18:55:22.000000000 +0000
++++ src/plugins/burst_buffer/common/burst_buffer_common.c
+@@ -52,7 +52,7 @@
+ #include <sys/types.h>
+ #include <unistd.h>
+ 
+-#ifdef __FreeBSD__
++#if defined(__FreeBSD__) || defined(__NetBSD__)
+ #define POLLRDHUP POLLHUP
+ #include <signal.h>
+ #endif
