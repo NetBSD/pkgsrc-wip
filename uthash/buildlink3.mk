a5a467259c4ba9e48a0634e8fad80908dd879987
Izaac <izaac@setec.org>
2016-05-20 16:46:31 -0400

    fixup buildlink3.mk ... not in my local anymore

--- uthash/buildlink3.mk
+++ uthash/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	uthash
 .if !defined(UTHASH_BUILDLINK3_MK)
 UTHASH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.uthash+=	uthash>=1.9.8
-BUILDLINK_PKGSRCDIR.uthash?=	../../local/uthash
+BUILDLINK_API_DEPENDS.uthash+=	uthash>=1.9.9
+BUILDLINK_PKGSRCDIR.uthash?=	../../wip/uthash
 BUILDLINK_DEPMETHOD.uthash?=	build
 .endif	# UTHASH_BUILDLINK3_MK
 
