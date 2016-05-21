5ab07b337100c9e61c67a4e944fae1ccfb691ff9
Kamil Rytarowski <n54@gmx.com>
2016-05-22 00:38:45 +0200

    qt5-qtbase-git: First round of rediffing patches for 5.6

--- qt5-qtbase-git/patches/patch-qmake_generators_makefile.cpp
+++ qt5-qtbase-git/patches/patch-qmake_generators_makefile.cpp
@@ -2,10 +2,10 @@ $NetBSD: patch-qmake_generators_makefile.cpp,v 1.2 2015/10/26 19:03:59 adam Exp 
 
 * Libtoolized
 
---- qmake/generators/makefile.cpp.orig	2015-06-29 20:03:22.000000000 +0000
+--- qmake/generators/makefile.cpp.orig	2016-05-20 00:28:32.000000000 +0000
 +++ qmake/generators/makefile.cpp
-@@ -3256,7 +3256,7 @@ MakefileGenerator::writePkgConfigFile()
-         pkgConfiglibName = "-framework " + bundle + " ";
+@@ -3290,7 +3290,7 @@ MakefileGenerator::writePkgConfigFile()
+         pkgConfiglibName = bundle.toQString();
      } else {
          if (!project->values("QMAKE_DEFAULT_LIBDIRS").contains(libDir))
 -            t << "-L${libdir} ";
--- qt5-qtbase-git/patches/patch-src_corelib_io_qstorageinfo_unix.cpp
+++ qt5-qtbase-git/patches/patch-src_corelib_io_qstorageinfo_unix.cpp
@@ -3,19 +3,8 @@ $NetBSD: patch-src_corelib_io_qstorageinfo_unix.cpp,v 1.5 2015/10/26 19:03:59 ad
 * make statvfs available on non-NetBSD BSD platforms
 * fix build on SunOS
 
---- src/corelib/io/qstorageinfo_unix.cpp.orig	2015-10-13 04:35:31.000000000 +0000
+--- src/corelib/io/qstorageinfo_unix.cpp.orig	2016-05-20 00:28:32.000000000 +0000
 +++ src/corelib/io/qstorageinfo_unix.cpp
-@@ -68,8 +68,8 @@
- 
- #if defined(Q_OS_BSD4)
- #  if defined(Q_OS_NETBSD)
--     define QT_STATFSBUF struct statvfs
--     define QT_STATFS    ::statvfs
-+#    define QT_STATFSBUF struct statvfs
-+#    define QT_STATFS    ::statvfs
- #  else
- #    define QT_STATFSBUF struct statfs
- #    define QT_STATFS    ::statfs
 @@ -78,7 +78,7 @@
  #  if !defined(ST_RDONLY)
  #    define ST_RDONLY MNT_RDONLY
