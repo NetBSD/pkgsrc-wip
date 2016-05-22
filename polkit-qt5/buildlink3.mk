52467edf37a84e6643138e12923069dec059711f
Kamil Rytarowski <n54@gmx.com>
2016-05-22 03:28:52 +0200

    polkit-qt5: Update wip/polkit -> security/polkit

--- polkit-qt5/buildlink3.mk
+++ polkit-qt5/buildlink3.mk
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.polkit-qt5+=	polkit-qt5>=0.112.0
 BUILDLINK_ABI_DEPENDS.polkit-qt5+=	polkit-qt5>=0.112.0
 BUILDLINK_PKGSRCDIR.polkit-qt5?=	../../wip/polkit-qt5
 
-.include "../../wip/polkit/buildlink3.mk"
+.include "../../security/polkit/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# POLKIT_QT5_BUILDLINK3_MK
 
