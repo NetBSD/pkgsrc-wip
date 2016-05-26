7ed150e24f96fcb498a387a157c6ab6fa797dfa1
Kamil Rytarowski <n54@gmx.com>
2016-05-27 01:07:02 +0200

    pcmanfm-qt: Update patch of libfm: wip/ -> sysutils/

--- pcmanfm-qt/buildlink3.mk
+++ pcmanfm-qt/buildlink3.mk
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.pcmanfm-qt+=	pcmanfm-qt>=0.9.0
 BUILDLINK_PKGSRCDIR.pcmanfm-qt?=	../../wip/pcmanfm-qt
 
 .include "../../sysutils/menu-cache/buildlink3.mk"
-.include "../../wip/libfm/buildlink3.mk"
+.include "../../sysutils/libfm/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .include "../../x11/qt5-qttools/buildlink3.mk"
 .include "../../x11/qt5-qtx11extras/buildlink3.mk"
