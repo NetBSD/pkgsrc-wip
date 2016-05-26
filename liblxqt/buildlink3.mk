d406c5f7e8bb6038b344ad84589d0dc930f62749
Mark Davies <mark@ecs.vuw.ac.nz>
2016-05-27 00:39:43 +1200

    liblxqt: fix dependency locations

--- liblxqt/buildlink3.mk
+++ liblxqt/buildlink3.mk
@@ -8,7 +8,7 @@ LIBLXQT_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=0.9.0
 BUILDLINK_PKGSRCDIR.liblxqt?=	../../wip/liblxqt
 
-.include "../../wip/kwindowsystem/buildlink3.mk"
+.include "../../x11/kwindowsystem/buildlink3.mk"
 .include "../../wip/libqtxdg/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .include "../../x11/qt5-qtx11extras/buildlink3.mk"
