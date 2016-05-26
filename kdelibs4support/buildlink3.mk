e28cc1e49f44585c6e4c90db28a4d3fc7f09e591
Mark Davies <mark@ecs.vuw.ac.nz>
2016-05-26 23:07:17 +1200

    kdelibs4support: fix dependency locations

--- kdelibs4support/buildlink3.mk
+++ kdelibs4support/buildlink3.mk
@@ -8,10 +8,10 @@ KDELIBS4SUPPORT_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.kdelibs4support+=	kdelibs4support>=5.21.0
 BUILDLINK_PKGSRCDIR.kdelibs4support?=	../../wip/kdelibs4support
 
-.include "../../wip/kemoticons/buildlink3.mk"
-.include "../../wip/kitemmodels/buildlink3.mk"
-.include "../../wip/kunitconversion/buildlink3.mk"
-.include "../../wip/kparts/buildlink3.mk"
+.include "../../misc/kemoticons/buildlink3.mk"
+.include "../../devel/kitemmodels/buildlink3.mk"
+.include "../../misc/kunitconversion/buildlink3.mk"
+.include "../../devel/kparts/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# KDELIBS4SUPPORT_BUILDLINK3_MK
 
