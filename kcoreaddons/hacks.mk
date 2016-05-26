9b481c44bc13d536fe6fbd4235554926cb4e0c87
Mark Davies <mark@ecs.vuw.ac.nz>
2016-05-26 22:51:00 +1200

    remove kf5 - imported to pkgsrc.

--- kcoreaddons/hacks.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD$
-#
-.include "../../mk/bsd.fast.prefs.mk"
-
-# NetBSD >= 7 has posix_fallocate() but doesnt define _POSIX_ADVISORY_INFO
-
-.if ${OPSYS} == "NetBSD" && empty(MACHINE_PLATFORM:MNetBSD-[0-6].*-*)
-CXXFLAGS+=	-DKSDC_POSIX_FALLOCATE_SUPPORTED=1
-.endif
