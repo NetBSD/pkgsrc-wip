2331ebee8abf5a607cc92550ea8b15f0dace6c53
Mark Davies <mark@ecs.vuw.ac.nz>
2016-05-27 00:01:23 +1200

    {kde-cli-tools,khotkeys,kinfocenter,kwin,systemsettings} fix dependency locations

--- systemsettings/buildlink3.mk
+++ systemsettings/buildlink3.mk
@@ -8,7 +8,7 @@ SYSTEMSETTINGS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.systemsettings+=	systemsettings>=5.6.3
 BUILDLINK_PKGSRCDIR.systemsettings?=	../../wip/systemsettings
 
-.include "../../wip/kcmutils/buildlink3.mk"
+.include "../../devel/kcmutils/buildlink3.mk"
 .include "../../wip/khtml/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# SYSTEMSETTINGS_BUILDLINK3_MK
