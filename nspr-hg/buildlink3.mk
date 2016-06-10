ca7d1f1010779417eb7903bfc55d96186e735c0c
Thomas Klausner <wiz@NetBSD.org>
2016-06-10 13:35:40 +0200

    nspr-git: fix PKGNAME to nspr-* (remove "hg-")

--- nspr-hg/buildlink3.mk
+++ nspr-hg/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	nspr
 .if !defined(NSPR_BUILDLINK3_MK)
 NSPR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nspr+=	nspr-hg>=2014.09.01
+BUILDLINK_API_DEPENDS.nspr+=	nspr>=4.10
 BUILDLINK_PKGSRCDIR.nspr?=	../../wip/nspr-hg
 
 BUILDLINK_FILES.nspr+=          lib/nspr/*
