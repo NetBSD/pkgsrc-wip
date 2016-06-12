780d00e91ad3b1641348484c9fef4ef8177a147b
Kamil Rytarowski <n54@gmx.com>
2016-06-12 02:27:45 +0200

    bijiben: Continue porting to webkitgtk 4.0

--- bijiben/patches/patch-src_libbiji_editor_biji-editor-selection.c
+++ bijiben/patches/patch-src_libbiji_editor_biji-editor-selection.c
@@ -2,7 +2,7 @@ $NetBSD$
 
 --- src/libbiji/editor/biji-editor-selection.c.orig	2014-10-18 21:26:34.000000000 +0000
 +++ src/libbiji/editor/biji-editor-selection.c
-@@ -23,8 +23,8 @@
+@@ -23,8 +23,10 @@
  #include "biji-editor-selection.h"
  #include "biji-editor-utils.h"
   
@@ -10,6 +10,8 @@ $NetBSD$
 -#include <webkit/webkitdom.h>
 +#include <webkit2/webkit2.h>
 +#include <webkitdom/webkitdom.h>
++#define WEBKIT_DOM_USE_UNSTABLE_API
++#include <webkitdom/WebKitDOMDOMSelection.h>
  
  #include <string.h>
  #include <stdlib.h>
--- bijiben/patches/patch-src_libbiji_editor_biji-webkit-editor.c
+++ bijiben/patches/patch-src_libbiji_editor_biji-webkit-editor.c
@@ -11,3 +11,26 @@ $NetBSD$
    EEditorSelection *sel;
    GObject *spell_check;
  };
+@@ -149,19 +149,19 @@ biji_webkit_editor_apply_format (BijiWeb
+ void
+ biji_webkit_editor_cut (BijiWebkitEditor *self)
+ {
+-  webkit_web_view_cut_clipboard (WEBKIT_WEB_VIEW (self));
++  webkit_web_view_execute_editing_command (WEBKIT_WEB_VIEW (embed), WEBKIT_EDITING_COMMAND_CUT);
+ }
+ 
+ void
+ biji_webkit_editor_copy (BijiWebkitEditor *self)
+ {
+-  webkit_web_view_copy_clipboard (WEBKIT_WEB_VIEW (self));
++  webkit_web_view_execute_editing_command (WEBKIT_WEB_VIEW (embed), WEBKIT_EDITING_COMMAND_COPY);
+ }
+ 
+ void
+ biji_webkit_editor_paste (BijiWebkitEditor *self)
+ {
+-  webkit_web_view_paste_clipboard (WEBKIT_WEB_VIEW (self));
++  webkit_web_view_execute_editing_command (WEBKIT_WEB_VIEW (embed), WEBKIT_EDITING_COMMAND_PASTE);
+ }
+ 
+ static void
