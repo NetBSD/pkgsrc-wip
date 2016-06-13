8cc8680f0286101ffaa58aecb1fe3719c2df88a5
Mateusz Poszwa <old4@o2.pl>
2016-06-13 18:19:12 +0200

    toxic: Add ‘notify’ and ‘x11’ options

---
+++ toxic/options.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.toxic
+PKG_SUPPORTED_OPTIONS+= notify x11
+PKG_SUGGESTED_OPTIONS+= notify x11
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mnotify)
+.include "../../sysutils/libnotify/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mx11)
+.include "../../x11/libX11/buildlink3.mk"
+.endif
