8e6b1009e6e7a161e4ff23fc4f478b98cd6668f6
Youri Mouton <youri@netbsd.org>
2016-06-15 14:41:35 +0000

    Add wine-devel from pkgsrc for update.

---
+++ wine-devel/options.mk
@@ -0,0 +1,73 @@
+# $NetBSD: options.mk,v 1.7 2015/02/09 13:30:44 adam Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.wine
+PKG_SUPPORTED_OPTIONS=	cups dbus esound hal ldap opengl sane ssl x11
+PKG_SUGGESTED_OPTIONS=	opengl ssl x11
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		esd opengl x11
+
+.if !empty(PKG_OPTIONS:Mcups)
+.include "../../print/cups15/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-cups
+.endif
+
+.if !empty(PKG_OPTIONS:Mdbus)
+.include "../../sysutils/dbus/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-dbus
+.endif
+
+.if !empty(PKG_OPTIONS:Mesound)
+PLIST.esd=		yes
+.include "../../audio/esound/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mhal)
+.include "../../sysutils/hal/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-hal
+.endif
+
+.if !empty(PKG_OPTIONS:Mldap)
+.include "../../databases/openldap-client/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-ldap
+.endif
+
+.if !empty(PKG_OPTIONS:Mssl)
+.include "../../security/gnutls/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-gnutls
+.endif
+
+.if !empty(PKG_OPTIONS:Mx11)
+PLIST.x11=		yes
+.include "../../x11/libICE/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXcomposite/buildlink3.mk"
+.include "../../x11/libXcursor/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXi/buildlink3.mk"
+.include "../../x11/libXinerama/buildlink3.mk"
+.include "../../x11/libXrandr/buildlink3.mk"
+.include "../../x11/libXrender/buildlink3.mk"
+.include "../../x11/libXxf86vm/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-x
+.endif
+
+.if !empty(PKG_OPTIONS:Mopengl) && !empty(PKG_OPTIONS:Mx11)
+PLIST.opengl=		yes
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../graphics/glut/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-opengl
+.endif
+
+.if !empty(PKG_OPTIONS:Msane)
+.include "../../graphics/sane-backends/buildlink3.mk"
+.endif
