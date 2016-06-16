2169942040bedf1a7ed3b7772afe7b39a47fabd9
Mateusz Poszwa <old4@o2.pl>
2016-06-16 12:43:06 +0200

    i3-git: Update a patch

--- i3-git/patches/patch-src_log.c
+++ i3-git/patches/patch-src_log.c
@@ -3,9 +3,9 @@ $NetBSD: patch-src_log.c,v 1.3 2014/08/09 19:14:01 degroote Exp $
 Use ftruncate(2) instead of posix_fallocate(2) on NetBSD.
 Do not set process-shared attribute on NetBSD.
 
---- src/log.c.orig	2016-01-11 23:36:55.000000000 +0000
+--- src/log.c.orig	2016-06-16 10:26:19.201603208 +0000
 +++ src/log.c
-@@ -129,7 +129,7 @@ void open_logbuffer(void) {
+@@ -134,7 +134,7 @@ void open_logbuffer(void) {
          return;
      }
  
@@ -14,8 +14,8 @@ Do not set process-shared attribute on NetBSD.
      if (ftruncate(logbuffer_shm, logbuffer_size) == -1) {
          fprintf(stderr, "Could not ftruncate SHM segment for the i3 log: %s\n", strerror(errno));
  #else
-@@ -156,8 +156,10 @@ void open_logbuffer(void) {
- 
+@@ -162,8 +162,10 @@ void open_logbuffer(void) {
+ #if !defined(__OpenBSD__)
      pthread_condattr_t cond_attr;
      pthread_condattr_init(&cond_attr);
 +#if !defined(__NetBSD__)
@@ -23,5 +23,5 @@ Do not set process-shared attribute on NetBSD.
          fprintf(stderr, "pthread_condattr_setpshared() failed, i3-dump-log -f will not work!\n");
 +#endif
      pthread_cond_init(&(header->condvar), &cond_attr);
+ #endif
  
-     logwalk = logbuffer + sizeof(i3_shmlog_header);
