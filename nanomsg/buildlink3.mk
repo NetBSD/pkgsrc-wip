7cb7d3803ea168015a823f5a4e7746f5bf5d3b1c
Kamil Rytarowski <n54@gmx.com>
2016-06-18 09:50:12 +0200

    nanomsh: Update to 1.0.0 and reset inactive maintainer

--- nanomsg/buildlink3.mk
+++ nanomsg/buildlink3.mk
@@ -1,21 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/08/08 11:56:44 chybz Exp $
-# XXX
-# XXX This file was created automatically using createbuildlink-3.16.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.nanomsg?=	build
+# $NetBSD$
 
 BUILDLINK_TREE+=	nanomsg
 
 .if !defined(NANOMSG_BUILDLINK3_MK)
 NANOMSG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nanomsg+=	nanomsg>=0.4beta
+BUILDLINK_API_DEPENDS.nanomsg+=	nanomsg>=1.0.0
 BUILDLINK_PKGSRCDIR.nanomsg?=	../../wip/nanomsg
 .endif	# NANOMSG_BUILDLINK3_MK
 
