2a7d717c7c995009ffb8fb68e7b7303dc43ea1f8
Kamil Rytarowski <n54@gmx.com>
2016-06-18 16:50:30 +0200

    ardour: Include previous patch

---
+++ ardour/patches/patch-libs_pbd_mountpoint.cc
@@ -0,0 +1,16 @@
+$NetBSD$
+
+--- libs/pbd/mountpoint.cc.orig	2016-02-19 01:28:56.000000000 +0000
++++ libs/pbd/mountpoint.cc
+@@ -116,7 +116,11 @@ mountpoint (string path)
+ string
+ mountpoint (string path)
+ {
++#if defined(__NetBSD__)
++	struct statvfs *mntbufp = 0;
++#else
+ 	struct statfs *mntbufp = 0;
++#endif
+ 	int count;
+ 	unsigned int maxmatch = 0;
+ 	unsigned int matchlen;
