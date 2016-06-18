1d201e78792b325a0a98cc036137788f130308b4
Youri Mouton <youri@netbsd.org>
2016-06-18 21:11:01 +0200

    accountsservice: Update to 0.6.42.

--- accountsservice/patches/patch-src_wtmp-helper.c
+++ accountsservice/patches/patch-src_wtmp-helper.c
@@ -1,35 +1,17 @@
 $NetBSD$
 
---- src/wtmp-helper.c.orig	2014-11-04 03:46:00.000000000 +0000
+NetBSD call WTMPX_FILENAME _PATH_WTMPX.
+
+--- src/wtmp-helper.c.orig	2016-01-27 18:38:21.000000000 +0000
 +++ src/wtmp-helper.c
-@@ -56,12 +56,12 @@ user_previous_login_free (UserPreviousLo
- static gboolean
- wtmp_helper_start (void)
- {
--#if defined(UTXDB_LOG)
-+#if defined(HAVE_SETUTXDB)
-                 if (setutxdb (UTXDB_LOG, NULL) != 0) {
-                         return FALSE;
-                 }
--#elif defined(WTMPX_FILENAME)
--                if (utmpxname (WTMPX_FILENAME) != 0) {
-+#elif defined(PATH_WTMP)
-+                if (utmpxname (PATH_WTMP) != 0) {
-                         return FALSE;
-                 }
+@@ -27,6 +27,10 @@
  
-@@ -212,13 +212,7 @@ wtmp_helper_entry_generator (GHashTable 
- const gchar *
- wtmp_helper_get_path_for_monitor (void)
- {
--#if defined(WTMPX_FILENAME)
--        return WTMPX_FILENAME;
--#elif defined(__FreeBSD__)
--        return "/var/log/utx.log";
--#else
--#error Do not know which filename to watch for wtmp changes
--#endif
-+        return PATH_WTMP;
- }
+ #ifdef HAVE_UTMPX_H
  
- #else /* HAVE_UTMPX_H */
++#ifdef __NetBSD__
++#define WTMPX_FILENAME	_PATH_WTMPX
++#endif
++
+ #include <utmpx.h>
+ 
+ typedef struct {
