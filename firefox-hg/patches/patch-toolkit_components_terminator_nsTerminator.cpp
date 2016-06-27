ecd59e2e0402594766de166b508537b6096be8a6
Ryo ONODERA <ryoon@NetBSD.org>
2016-06-28 03:15:22 +0900

    www/firefox-hg: Update to latest aurora, 50.0 and fix segfault on exit

---
+++ firefox-hg/patches/patch-toolkit_components_terminator_nsTerminator.cpp
@@ -0,0 +1,25 @@
+$NetBSD$
+
+--- toolkit/components/terminator/nsTerminator.cpp.orig	2016-06-27 17:15:24.677082132 +0000
++++ toolkit/components/terminator/nsTerminator.cpp
+@@ -36,7 +36,7 @@
+ #if defined(XP_WIN)
+ #include <windows.h>
+ #else
+-#include <unistd.h>
++#include <time.h>
+ #endif
+ 
+ #include "mozilla/ArrayUtils.h"
+@@ -147,7 +147,10 @@ RunWatchdog(void* arg)
+ #if defined(XP_WIN)
+     Sleep(1000 /* ms */);
+ #else
+-    usleep(1000000 /* usec */);
++    struct timespec tickd;
++    tickd.tv_sec = 1;
++    tickd.tv_nsec = 0;
++    nanosleep(&tickd, NULL);
+ #endif
+ 
+     if (gHeartbeat++ < timeToLive) {
