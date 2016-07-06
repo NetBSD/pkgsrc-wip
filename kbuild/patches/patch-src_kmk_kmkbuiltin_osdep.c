9f7c30243e7bccf55df9317c90940a5b5e7d9cc6
Kamil Rytarowski <n54@gmx.com>
2016-07-06 02:25:11 +0200

    Import kbuild-0.1.9998.8.2814.25 as wip/kbuild.
    kBuild is a makefile framework for writing simple makefiles for complex tasks.

---
+++ kbuild/patches/patch-src_kmk_kmkbuiltin_osdep.c
@@ -0,0 +1,16 @@
+$NetBSD$
+
+--- src/kmk/kmkbuiltin/osdep.c.orig	2015-06-21 21:21:29.000000000 +0000
++++ src/kmk/kmkbuiltin/osdep.c
+@@ -41,8 +41,10 @@
+ #elif defined(__OpenBSD__)
+ # include "openbsd.c"
+ 
++#elif defined(__NetBSD__)
++# include "strtofflags.c"
++
+ #elif defined(__HAIKU__)
+ # include "haikufakes.c"
+ 
+ #endif
+-
---
+++ kbuild/patches/patch-src_sed_lib_utils.c
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- src/sed/lib/utils.c.orig	2015-06-21 21:21:26.000000000 +0000
++++ src/sed/lib/utils.c
+@@ -35,6 +35,8 @@
+ # include <stdlib.h>
+ #endif /* HAVE_STDLIB_H */
+ 
++#include <unistd.h>
++
+ #include "utils.h"
+ 
+ const char *myname;
