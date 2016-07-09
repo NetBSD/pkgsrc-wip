daba1b5c84fcf9c9217a8179059c2ca1b75e632f
Thomas Klausner <wiz@NetBSD.org>
2016-07-09 15:02:03 +0200

    *: remove python33 support

--- gdb/options.mk
+++ gdb/options.mk
@@ -8,6 +8,6 @@ PKG_SUGGESTED_OPTIONS=	python
 
 .if !empty(PKG_OPTIONS:Mpython)
 CONFIGURE_ARGS+=	--with-python=${PYTHONBIN}
-PYTHON_VERSIONS_INCOMPATIBLE=	33 34 35
+PYTHON_VERSIONS_INCOMPATIBLE=	34 35
 .include "../../lang/python/application.mk"
 .endif
