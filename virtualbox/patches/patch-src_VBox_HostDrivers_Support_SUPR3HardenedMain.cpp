aa65ac855f441b23846c9b22df4c297e0373d402
Kamil Rytarowski <n54@gmx.com>
2016-07-09 23:38:54 +0200

    virtualbox: Fix more build issues on NetBSD
    1. Fix code mistakes with double else pre-processor switch
    2. Pickup functional alternative to setresgid()/setresuid()
    3. Disable for now building of Ring0 code-fragments (kernel code)
    4. Disable FreeBSD specific headers generation on NetBSD from .m files
    5. Add new required build-dependency: Perl

--- virtualbox/patches/patch-src_VBox_HostDrivers_Support_SUPR3HardenedMain.cpp
+++ virtualbox/patches/patch-src_VBox_HostDrivers_Support_SUPR3HardenedMain.cpp
@@ -20,6 +20,15 @@ $NetBSD$
  # ifdef RT_OS_LINUX
      int cchLink = readlink("/proc/self/exe", &g_szSupLibHardenedExePath[0], sizeof(g_szSupLibHardenedExePath) - 1);
  
+@@ -868,7 +868,7 @@ static void supR3HardenedGetFullExePath(
+     sprintf(szFileBuf, "/proc/%ld/path/a.out", (long)getpid());
+     int cchLink = readlink(szFileBuf, &g_szSupLibHardenedExePath[0], sizeof(g_szSupLibHardenedExePath) - 1);
+ 
+-# else /* RT_OS_FREEBSD */
++# elif defined(RT_OS_FREEBSD)
+     int aiName[4];
+     aiName[0] = CTL_KERN;
+     aiName[1] = KERN_PROC;
 @@ -881,6 +881,19 @@ static void supR3HardenedGetFullExePath(
      g_szSupLibHardenedExePath[sizeof(g_szSupLibHardenedExePath) - 1] = '\0';
      int cchLink = suplibHardenedStrLen(g_szSupLibHardenedExePath); /* paranoid? can't we use cbPath? */
@@ -40,6 +49,15 @@ $NetBSD$
  # endif
      if (cchLink < 0 || cchLink == sizeof(g_szSupLibHardenedExePath) - 1)
          supR3HardenedFatal("supR3HardenedExecDir: couldn't read \"%s\", errno=%d cchLink=%d\n",
+@@ -1558,7 +1571,7 @@ static void supR3HardenedMainDropPrivile
+     egid = getegid();
+     rgid = sgid = getgid();
+ 
+-# elif defined(RT_OS_SOLARIS)
++# elif defined(RT_OS_SOLARIS) || defined(RT_OS_NETBSD)
+     /* Solaris doesn't have setresuid, but the setreuid interface is BSD
+        compatible and will set the saved uid to euid when we pass it a ruid
+        that isn't -1 (which we do). */
 @@ -2025,4 +2038,3 @@ DECLHIDDEN(int) SUPR3HardenedMain(const 
      g_enmSupR3HardenedMainState = SUPR3HARDENEDMAINSTATE_CALLED_TRUSTED_MAIN;
      return pfnTrustedMain(argc, argv, envp);
