af5284eae6e0b8fe866a7fa3354b50927447d434
Kamil Rytarowski <n54@gmx.com>
2016-07-10 22:03:42 +0200

    libiptcdata: Revamp blk3

--- libiptcdata/buildlink3.mk
+++ libiptcdata/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/04/11 12:50:04 jihbed Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	libiptcdata
 
 .if !defined(LIBIPTCDATA_BUILDLINK3_MK)
 LIBIPTCDATA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libiptcdata+=	libiptcdata>=1.0.3
+BUILDLINK_API_DEPENDS.libiptcdata+=	libiptcdata>=1.0.4
 BUILDLINK_PKGSRCDIR.libiptcdata?=	../../wip/libiptcdata
-
 
 .endif	# LIBIPTCDATA_BUILDLINK3_MK
 
