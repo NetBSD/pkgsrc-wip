33a098ecd34a8d14d8da1a65ae893aa8e69bf8a5
Benny Siegert <bsiegert@NetBSD.org>
2016-07-12 22:56:48 +0200

    New port: lumberjack-2.0.
    Lumberjack is a Go package for writing logs to rolling files.
    
    Lumberjack is intended to be one part of a logging infrastructure.
    It is not an all-in-one solution, but instead is a pluggable
    component at the bottom of the logging stack that simply controls the files
    to which logs are written.
    
    Lumberjack plays well with any logging package that can write to an
    io.Writer, including the standard library's log package.
    
    Lumberjack assumes that only one process is writing to the output files.
    Using the same lumberjack configuration from multiple processes on the same
    machine will result in improper behavior.

---
+++ lumberjack/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	lumberjack
+
+.if !defined(LUMBERJACK_BUILDLINK3_MK)
+LUMBERJACK_BUILDLINK3_MK:=
+
+BUILDLINK_CONTENTS_FILTER.lumberjack=	${EGREP} gopkg/
+BUILDLINK_DEPMETHOD.lumberjack?=		build
+
+BUILDLINK_API_DEPENDS.lumberjack+=	lumberjack>=2.0
+BUILDLINK_PKGSRCDIR.lumberjack?=	../../wip/lumberjack
+.endif	# LUMBERJACK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-lumberjack
