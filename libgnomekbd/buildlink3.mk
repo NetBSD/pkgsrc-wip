a3e5d989c3488bc1438d2fa0232f77751226d774
Youri Mouton <youri@NetBSD.org>
2016-07-12 19:28:16 +0200

    Import libgnomekbd-3.6.0 as wip/libgnomekbd.
    Gnome keyboard configuration library

---
+++ libgnomekbd/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.10 2016/03/12 11:28:37 ryoon Exp $
+
+BUILDLINK_TREE+=	libgnomekbd
+
+.if !defined(LIBGNOMEKBD_BUILDLINK3_MK)
+LIBGNOMEKBD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libgnomekbd+=	libgnomekbd>=3.6.0
+BUILDLINK_ABI_DEPENDS.libgnomekbd+=	libgnomekbd>=3.6.0
+BUILDLINK_PKGSRCDIR.libgnomekbd?=	../../x11/libgnomekbd
+
+.include "../../x11/libxklavier/buildlink3.mk"
+.endif # LIBGNOMEKBD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libgnomekbd
