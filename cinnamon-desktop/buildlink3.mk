4002a31cf4bf74c521b5c02188d68bd6824cdef9
Youri Mouton <youri@NetBSD.org>
2016-07-12 18:10:50 +0200

    cinnamon-desktop: Update to 3.0.2

--- cinnamon-desktop/buildlink3.mk
+++ cinnamon-desktop/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	cinnamon-desktop
 .if !defined(CINNAMON_DESKTOP_BUILDLINK3_MK)
 CINNAMON_DESKTOP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cinnamon-desktop+=	cinnamon-desktop>=2.2.3
-BUILDLINK_PKGSRCDIR.cinnamon-desktop?=	../../wip/cinnamon-desktop
+BUILDLINK_API_DEPENDS.cinnamon-desktop+=	cinnamon-desktop>=3.0.2
+BUILDLINK_PKGSRCDIR.cinnamon-desktop?=		../../wip/cinnamon-desktop
 
 .include "../../x11/gtk3/buildlink3.mk"
 .include "../../x11/libxkbfile/buildlink3.mk"
