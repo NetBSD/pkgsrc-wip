31efd70f7d53e5e7c5aa6625c9b24c3865c760b3
Youri Mouton <youri@NetBSD.org>
2016-07-14 15:51:32 +0200

    Import cinnamon-settings-daemon-3.0.1 as wip/cinnamon-settings-daemon.
    Cinnamon settings daemon.

---
+++ cinnamon-settings-daemon/buildlink3.mk
@@ -0,0 +1,26 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	cinnamon-settings-daemon
+
+.if !defined(CINNAMON_SETTINGS_DAEMON_BUILDLINK3_MK)
+CINNAMON_SETTINGS_DAEMON_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.cinnamon-settings-daemon+=	cinnamon-settings-daemon>=3.0.1
+BUILDLINK_PKGSRCDIR.cinnamon-settings-daemon?=		../../wip/cinnamon-settings-daemon
+
+.include "../../print/cups/buildlink3.mk"
+.include "../../audio/libcanberra/buildlink3.mk"
+.include "../../audio/pulseaudio/buildlink3.mk"
+.include "../../wip/cinnamon-desktop/buildlink3.mk"
+.include "../../devel/GConf/buildlink3.mk"
+.include "../../graphics/lcms2/buildlink3.mk"
+.include "../../sysutils/libnotify/buildlink3.mk"
+.include "../../sysutils/upower/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.include "../../x11/libxklavier/buildlink3.mk"
+.include "../../wip/libgnomekbd/buildlink3.mk"
+.include "../../wip/colord/buildlink3.mk"
+.endif	# CINNAMON_SETTINGS_DAEMON_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-cinnamon-settings-daemon
