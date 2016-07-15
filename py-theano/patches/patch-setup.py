d9708fb0e9d6c2da317635ef8239ed974e066a6a
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2016-07-15 21:43:02 +0100

    +new many package and update py-theano

--- py-theano/patches/patch-setup.py
+++ py-theano/patches/patch-setup.py
@@ -1,10 +1,18 @@
 $NetBSD$
 
---- setup.py.orig	2015-03-26 21:43:05.000000000 +0000
+--- setup.py.orig	2016-04-21 12:04:08.000000000 +0000
 +++ setup.py
-@@ -180,7 +180,7 @@ def do_setup():
-                    '*.h', 'ChangeLog'],
-               'theano.misc': ['*.sh']
+@@ -5,6 +5,7 @@
+ #   * Add download_url
+ 
+ import os
++import sys
+ import subprocess
+ import codecs
+ from fnmatch import fnmatchcase
+@@ -174,7 +175,7 @@ def do_setup():
+               'theano.misc': ['*.sh'],
+               'theano.d3viz' : ['html/*','css/*','js/*']
            },
 -          scripts=['bin/theano-cache', 'bin/theano-nose', 'bin/theano-test'],
 +          scripts=['bin/theano-cache'+sys.version[0:3], 'bin/theano-nose'+sys.version[0:3], 'bin/theano-test'+sys.version[0:3]],
