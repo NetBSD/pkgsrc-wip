ce9ae7d62e727b23d2a508bf5de47cf8f5c58114
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2016-07-17 02:53:52 +0100

    +(py-annarchy, py-chainer, py-cva, py-dask, py-filelock, py-hepcrawl, py-htmlparser, py-lmfit, py-upsilon, py-multiprocess, py-vespa) in wip

---
+++ py-filelock/patches/patch-setup.py
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- setup.py.orig	2015-03-25 10:28:09.000000000 +0000
++++ setup.py
+@@ -53,7 +53,6 @@ setup(
+     url = "https://github.com/benediktschmitt/py-filelock",
+     download_url = "https://github.com/benediktschmitt/py-filelock/archive/master.zip",
+     py_modules = ["filelock"],
+-    data_files = [("", ["LICENSE.rst", "README.rst"])],
+     license = license_,
+     classifiers = [
+         "License :: Public Domain",
