306f3861dfb7630844851cf20b5f04e35ab56763
Kamil Rytarowski <n54@gmx.com>
2016-07-21 22:57:32 +0200

    chromium-depot_tools: polish style

--- chromium-depot_tools/tool.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD: tool.mk,v 1.1 2014/08/31 17:10:48 heidnes Exp $
-
-.if !defined(CHROMIUM_DEPOT_TOOLS_MK)
-CHROMIUM_DEPOT_TOOLS_MK= defined
-
-PREPEND_PATH+=  ${PREFIX}/share/chromium-depot_tools
-MAKE_ENV+=	DEPOT_TOOLS_UPDATE=0
-
-.endif #CHROMIUM_DEPOT_TOOLS_MK
