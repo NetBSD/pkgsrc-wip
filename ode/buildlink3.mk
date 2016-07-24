a16b46b48e4551f79b37479f303e0bd9fab462fd
Kamil Rytarowski <n54@gmx.com>
2016-07-24 03:13:03 +0200

    ode: Revamp buildlink3.mk

--- ode/buildlink3.mk
+++ ode/buildlink3.mk
@@ -1,13 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.6 2013/01/19 12:25:39 othyro Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	ode
 
 .if !defined(ODE_BUILDLINK3_MK)
 ODE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ode+=	ode>=0.11.1
-BUILDLINK_ABI_DEPENDS.ode+=	ode>=0.11.1
+BUILDLINK_API_DEPENDS.ode+=	ode>=0.13
 BUILDLINK_PKGSRCDIR.ode?=	../../wip/ode
-.endif # ODE_BUILDLINK3_MK
+
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.endif	# ODE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ode
