efb93f3f5ae0070e554d15facdcadbfb2c4b8369
Kamil Rytarowski <n54@gmx.com>
2016-07-26 21:08:42 +0200

    wordwarvi: pkglint

--- wordwarvi/patches/patch-wordwarvi.c
+++ wordwarvi/patches/patch-wordwarvi.c
@@ -1,5 +1,7 @@
 $NetBSD: patch-ab,v 1.1.1.1 2008/12/25 12:27:03 thomasklausner Exp $
 
+Use standard getopt_long(3) - fixes NetBSD.
+
 --- wordwarvi.c.orig	2008-12-12 02:24:34.000000000 +0000
 +++ wordwarvi.c
 @@ -13078,7 +13078,7 @@ int main(int argc, char *argv[])
