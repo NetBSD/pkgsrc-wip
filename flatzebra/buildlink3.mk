29de6a985c92f0c18ddee58f03b4b1bcac93524d
Kamil Rytarowski <n54@gmx.com>
2016-07-28 21:07:39 +0200

    flatzebra: Cleanup blk3

--- flatzebra/buildlink3.mk
+++ flatzebra/buildlink3.mk
@@ -1,14 +1,4 @@
 # $NetBSD$
-# XXX
-# XXX This file was created automatically using createbuildlink-3.17.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.flatzebra?=	build
 
 BUILDLINK_TREE+=	flatzebra
 
@@ -18,15 +8,9 @@ FLATZEBRA_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.flatzebra+=	flatzebra>=0.1.6
 BUILDLINK_PKGSRCDIR.flatzebra?=	../../wip/flatzebra
 
-# XXX
-# XXX Uncomment and keep only the buildlink3 lines below which are directly
-# XXX needed for dependencies to compile, link, and run.  If this package
-# XXX provides a wrappered API or otherwise does not expose the APIs of the
-# XXX buildlink3 lines below to dependencies, remove them.
-# XXX
-#.include "../../audio/SDL_mixer/buildlink3.mk"
-#.include "../../devel/SDL/buildlink3.mk"
-#.include "../../graphics/SDL_image/buildlink3.mk"
+.include "../../audio/SDL_mixer/buildlink3.mk"
+.include "../../devel/SDL/buildlink3.mk"
+.include "../../graphics/SDL_image/buildlink3.mk"
 .endif	# FLATZEBRA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-flatzebra
