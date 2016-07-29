92c6040578dc8f58a8349230878fbd4afb6da6e2
Kamil Rytarowski <n54@gmx.com>
2016-07-30 01:42:16 +0200

    Import nvidia-texture-tools-2.0.8 as wip/nvidia-texture-tools.
    NVIDIA Texture Tools is a collection of image processing and texture
    manipulation tools, designed to be integrated in game tools and asset
    conditioning pipelines.  The primary features of the library are mipmap and
    normal map generation, format conversion and DXT compression.

---
+++ nvidia-texture-tools/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	nvidia-texture-tools
+
+.if !defined(NVIDIA_TEXTURE_TOOLS_BUILDLINK3_MK)
+NVIDIA_TEXTURE_TOOLS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.nvidia-texture-tools+=	nvidia-texture-tools>=2.0.8
+BUILDLINK_PKGSRCDIR.nvidia-texture-tools?=	../../local/nvidia-texture-tools
+
+.include "../../graphics/openexr/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+.endif	# NVIDIA_TEXTURE_TOOLS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-nvidia-texture-tools
