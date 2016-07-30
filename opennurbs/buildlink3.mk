7a3cb945f2191e5c92c9797a60fd4b7ca79791fe
Iain Hibbert <plunky@netbsd.org>
2016-07-30 22:01:25 +0100

    update openNURBS to version 2013-07-11

--- opennurbs/buildlink3.mk
+++ opennurbs/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	opennurbs
 .if !defined(OPENNURBS_BUILDLINK3_MK)
 OPENNURBS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.opennurbs+=	opennurbs>=20121024
+BUILDLINK_API_DEPENDS.opennurbs+=	opennurbs>=20130711
 BUILDLINK_PKGSRCDIR.opennurbs?=	../../wip/opennurbs
 
 .include "../../devel/zlib/buildlink3.mk"
