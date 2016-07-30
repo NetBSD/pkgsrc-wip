c4311e279635df38643aec5a652c567c186374bf
Kamil Rytarowski <n54@gmx.com>
2016-07-30 12:10:57 +0200

    0ad: Use gmake on NetBSD

--- 0ad/patches/patch-build_workspaces_update-workspaces.sh
+++ 0ad/patches/patch-build_workspaces_update-workspaces.sh
@@ -1,6 +1,6 @@
 $NetBSD$
 
---- build/workspaces/update-workspaces.sh.orig	2016-07-29 19:33:02.688237533 +0000
+--- build/workspaces/update-workspaces.sh.orig	2015-06-14 20:43:12.000000000 +0000
 +++ build/workspaces/update-workspaces.sh
 @@ -1,10 +1,5 @@
  #!/bin/sh
@@ -13,3 +13,12 @@ $NetBSD$
  die()
  {
    echo ERROR: $*
+@@ -26,7 +21,7 @@ JOBS=${JOBS:="-j2"}
+ # Some of our makefiles depend on GNU make, so we set some sane defaults if MAKE
+ # is not set.
+ case "`uname -s`" in
+-  "FreeBSD" | "OpenBSD" )
++  "FreeBSD" | "NetBSD" | "OpenBSD" )
+     MAKE=${MAKE:="gmake"}
+     ;;
+   * )
