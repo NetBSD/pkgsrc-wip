734ccc8c73ec7256b91c97061ecc286568e3ec9e
Rumko <rumko@NetBSD.org>
2016-08-07 09:13:29 +0200

    wip/py-pyside-shiboken: update to 1.2.4
    Excerpt from github change log:
    * Refactor and improve added function resolving
    * Write inititializations in topological order
    * Fix finding container base classes
    * Fix handling of includes like '#include BOOST_USER_CONFIG'
    * Fix buffer overrun processing macro definitions ending in a
    comment
    * Allocate and use separate structures for as_* fields in the
    PyTypeObject
    * Fix compilation issue on OS X 10.9
    * Fix omission of 'const' specifier when generating binding code for
    containers whose value type is 'const'
    * Fix building with python 3.3 and 3.4
    * Fix function rejections (i.e. support overloads)
    * Add parsing of 'noexcept' keyword
    * Return enums in declaration order (order added)
    * Process global enums in declaration order
    * When building the class topology, don't skip classes, even if we
    are not going to generate code for them
    
    Also take over maintainership.
    
    Ok@ rhialto

--- py-pyside-shiboken/buildlink3.mk
+++ py-pyside-shiboken/buildlink3.mk
@@ -5,6 +5,8 @@ BUILDLINK_TREE+=	py-pyside-shiboken
 .if !defined(PY_PYSIDE_SHIBOKEN_BUILDLINK3_MK)
 PY_PYSIDE_SHIBOKEN_BUILDLINK3_MK:=
 
+.include "../../lang/python/pyversion.mk"
+
 BUILDLINK_API_DEPENDS.py-pyside-shiboken+=	${PYPKGPREFIX}-pyside-shiboken>=1.2.1
 BUILDLINK_ABI_DEPENDS.py-pyside-shiboken+=	${PYPKGPREFIX}-pyside-shiboken>=1.2.1
 BUILDLINK_PKGSRCDIR.py-pyside-shiboken?=	../../wip/py-pyside-shiboken
