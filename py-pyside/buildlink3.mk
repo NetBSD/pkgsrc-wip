1f142d4e00b03a4e224a5ed04e3c178af0888ab6
Rumko <rumko@NetBSD.org>
2016-08-07 08:58:31 +0200

    wip/py-pyside: update to 1.2.4
    Change log:
    * use file system encoding instead of assumed 'ascii'
    * fix multiple rules for file moc_qpytextobject.cxx
    * Avoid narrowing conversion warning
    * Fix shadowed virtuals warning
    * fix QCoreApplication would deadlock on exit if the global
    QThreadPool.globalInstance() is running a QRunnable with python
    code
    * Fix possible deadlock on signal connect/emit
    
    Also take over maintainership.
    
    Ok@ rhialto

---
+++ py-pyside/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.1 2013/11/15 12:14:47 rhialto Exp $
+
+BUILDLINK_TREE+=	py-pyside
+
+.if !defined(PY_PYSIDE_BUILDLINK3_MK)
+PY_PYSIDE_BUILDLINK3_MK:=
+
+.include "../../lang/python/pyversion.mk"
+
+BUILDLINK_API_DEPENDS.py-pyside+=	${PYPKGPREFIX}-pyside>=1.2.4
+BUILDLINK_ABI_DEPENDS.py-pyside+=	${PYPKGPREFIX}-pyside>=1.2.4
+BUILDLINK_PKGSRCDIR.py-pyside?=	../../wip/py-pyside
+.endif # PY_PYSIDE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-py-pyside
