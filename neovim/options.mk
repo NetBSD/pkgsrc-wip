d2483756e6a64c6310f12d7bacdf419342c08f4a
Jonathan Buschmann <jonthn+pkgsrc@pinacea.com>
2016-08-10 21:39:51 +0200

    neovim: Changes to add Ruby support
      + Rework patch due to Ruby support changes
      + Add option to enable Ruby
      + Correctly de-activate jemalloc when not requested

--- neovim/options.mk
+++ neovim/options.mk
@@ -1,8 +1,8 @@
 # $NetBSD: options.mk,v 1.2 2015/04/30 22:22:00 jonthn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.neovim
-PKG_SUPPORTED_OPTIONS=	python jemalloc
-PKG_SUGGESTED_OPTIONS=	python
+PKG_SUPPORTED_OPTIONS=	python ruby jemalloc
+PKG_SUGGESTED_OPTIONS=	python ruby
 
 .include "../../mk/bsd.options.mk"
 
@@ -16,8 +16,19 @@ DEPENDS+=       ${PYPKGPREFIX}-neovim-[0-9]*:../../wip/py-neovim
 .  endif
 
 ###
+### Support Ruby (add dependency)
+###
+.  if !empty(PKG_OPTIONS:Mruby)
+DEPENDS+=       ${RUBY_PKGPREFIX}-neovim-[0-9]*:../../wip/ruby-neovim
+
+.include "../../lang/ruby/rubyversion.mk"
+.  endif
+
+###
 ### Use jemalloc for memory allocation
 ###
 .  if !empty(PKG_OPTIONS:Mjemalloc)
 .include "../../devel/jemalloc/buildlink3.mk"
+.  else
+CMAKE_ARGS+= -DENABLE_JEMALLOC=OFF
 .  endif
