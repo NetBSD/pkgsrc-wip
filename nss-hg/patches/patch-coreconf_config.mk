54fac0800718d64ac9d5a7e0b89d67034cd93baa
Thomas Klausner <wiz@NetBSD.org>
2016-08-15 22:32:03 +0200

    nss-hg: remove cast and DragonFlyBSD patches.
    cast patches are not currently necessary for building,
    and there is no progress in upstreaming them.

--- nss-hg/patches/patch-coreconf_config.mk
+++ nss-hg/patches/patch-coreconf_config.mk
@@ -1,19 +1,9 @@
 $NetBSD: patch-coreconf_config.mk,v 1.2 2014/10/05 20:12:38 thomasklausner Exp $
 
-chunk 1: Support DragonFly
-chunk 2: Set proper dynamic library runtime search path
+Set proper dynamic library runtime search path
 
 --- coreconf/config.mk.orig	2014-09-28 13:54:02.000000000 +0000
 +++ coreconf/config.mk
-@@ -31,7 +31,7 @@ endif
- #######################################################################
- 
- TARGET_OSES = FreeBSD BSD_OS NetBSD OpenUNIX OS2 QNX Darwin BeOS OpenBSD \
--              AIX RISCOS WINNT WIN95 Linux Android
-+              AIX RISCOS WINNT WIN95 Linux Android DragonFly
- 
- ifeq (,$(filter-out $(TARGET_OSES),$(OS_TARGET)))
- include $(CORE_DEPTH)/coreconf/$(OS_TARGET).mk
 @@ -183,6 +183,7 @@ endif
  DEFINES += -DUSE_UTIL_DIRECTLY
  USE_UTIL_DIRECTLY = 1
