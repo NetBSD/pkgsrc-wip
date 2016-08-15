21b7a4800b884be677fc68c14945e1e33a28c4ad
Kamil Rytarowski <n54@gmx.com>
2016-08-16 00:34:49 +0200

    virtualbox-svn: Regenerate patches for upstream changes

--- virtualbox-svn/patches/patch-src_VBox_Devices_Audio_DrvHostOSSAudio.cpp
+++ virtualbox-svn/patches/patch-src_VBox_Devices_Audio_DrvHostOSSAudio.cpp
@@ -1,6 +1,6 @@
 $NetBSD$
 
---- src/VBox/Devices/Audio/DrvHostOSSAudio.cpp.orig	2016-08-12 18:06:09.000000000 +0000
+--- src/VBox/Devices/Audio/DrvHostOSSAudio.cpp.orig	2016-08-15 22:22:04.307068286 +0000
 +++ src/VBox/Devices/Audio/DrvHostOSSAudio.cpp
 @@ -137,6 +137,7 @@ static OSSAUDIOCFG s_OSSConf =
  };
@@ -10,15 +10,16 @@ $NetBSD$
  /* http://www.df.lth.se/~john_e/gems/gem002d.html */
  static uint32_t popcount(uint32_t u)
  {
-@@ -147,6 +148,7 @@ static uint32_t popcount(uint32_t u)
+@@ -147,7 +148,7 @@ static uint32_t popcount(uint32_t u)
      u = ( u&0x0000ffff) + (u>>16);
      return u;
  }
+-
 +#endif
  
  static uint32_t lsbindex(uint32_t u)
  {
-@@ -1149,4 +1151,3 @@ const PDMDRVREG g_DrvHostOSSAudio =
+@@ -1174,4 +1175,3 @@ const PDMDRVREG g_DrvHostOSSAudio =
      /* u32EndVersion */
      PDM_DRVREG_VERSION
  };
