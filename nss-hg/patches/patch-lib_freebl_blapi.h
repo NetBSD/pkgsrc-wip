7859d37b962f8b7bf32bb5b4073c8cd7f0e0b7fe
Thomas Klausner <wiz@NetBSD.org>
2016-09-01 08:18:14 +0200

    nss-hg: adapt patch to upstream changes

--- nss-hg/patches/patch-lib_freebl_blapi.h
+++ nss-hg/patches/patch-lib_freebl_blapi.h
@@ -2,14 +2,14 @@ $NetBSD: patch-lib_freebl_blapi.h,v 1.2 2014/10/05 20:12:38 thomasklausner Exp $
 
 Avoid namespace collision (?)
 
---- lib/freebl/blapi.h.orig	2014-09-28 13:54:02.000000000 +0000
+--- lib/freebl/blapi.h.orig	2016-08-31 09:42:50.000000000 +0000
 +++ lib/freebl/blapi.h
-@@ -1189,6 +1189,8 @@ extern void SHA1_DestroyContext(SHA1Cont
- */
- extern void SHA1_Begin(SHA1Context *cx);
+@@ -12,6 +12,8 @@
+ #include "hasht.h"
+ #include "alghmac.h"
  
 +#define SHA1_Update NSS_SHA1_Update
 +
+ SEC_BEGIN_PROTOS
+ 
  /*
- ** Update the SHA-1 hash function with more data.
- **	"cx" the context
