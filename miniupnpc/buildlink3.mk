2e87f721d96d3ce74b6303df170a53533f1c9335
Stefan Hertenberger <stefan@hertenberger.bayern>
2016-09-03 08:02:18 +0200

    miniupnpc: missing files from previous commit

--- miniupnpc/buildlink3.mk
+++ miniupnpc/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:43:49 jsonn Exp $
-#
+# $NetBSD buildlink3.mk, v 1.0 2016/09/02 21:46:00 erde74 Exp$
 
 BUILDLINK_TREE+=	miniupnpc
 
 .if !defined(MINIUPNPC_BUILDLINK3_MK)
 MINIUPNPC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.miniupnpc+=	miniupnpc>=1.0nb11
+BUILDLINK_API_DEPENDS.miniupnpc+=	miniupnpc>=2.0
 BUILDLINK_PKGSRCDIR.miniupnpc?=	../../wip/miniupnpc
-.endif # MINIUPNPC_BUILDLINK3_MK
+.endif	# MINIUPNPC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-miniupnpc
