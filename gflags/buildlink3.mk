9826666cb6fdb317480372191fd39dca571ce854
Charlie Root <root@netbsddev.local.hpc.uwm.edu>
2016-09-12 12:39:27 -0500

    Upgrade to latest release and fix build issues

--- gflags/buildlink3.mk
+++ gflags/buildlink3.mk
@@ -1,13 +1,12 @@
 # $NetBSD: buildlink3.mk,v 1.1 2014/11/29 19:31:38 nros Exp $
-#
 
 BUILDLINK_TREE+=	gflags
 
 .if !defined(GFLAGS_BUILDLINK3_MK)
 GFLAGS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gflags+=	gflags>=2.1.1
-BUILDLINK_PKGSRCDIR.gflags?=	../../wip/gflags
+BUILDLINK_API_DEPENDS.gflags+=	gflags>=2.1.2
+BUILDLINK_PKGSRCDIR.gflags?=	../../jb-wip/gflags
 .endif	# GFLAGS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gflags
