728fcf054f95d53bf7900fee8afce73e330e6e21
Kamil Rytarowski <n54@gmx.com>
2016-09-12 23:31:30 +0200

    polly: pkglint

--- polly/buildlink3.mk
+++ polly/buildlink3.mk
@@ -8,7 +8,8 @@ POLLY_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.polly+=	polly>=4.0.0nb20160911
 BUILDLINK_PKGSRCDIR.polly?=	../../wip/polly-git
 
-.include "../../wip/llvm-git/buildlink3.mk"
+DEPENDS+=	llvm-${PKGVERSION_NOREV}{,nb*}:../../lang/llvm
+.include "../../lang/llvm/buildlink3.mk"
 .endif	# POLLY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-polly
