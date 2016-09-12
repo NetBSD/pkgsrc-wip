58fef13a8c6f71ae2cee92ddd9f2a1d3c6ede622
Kamil Rytarowski <n54@gmx.com>
2016-09-12 23:37:58 +0200

    polly: Fix buildlink paths

--- polly/buildlink3.mk
+++ polly/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	polly
 .if !defined(POLLY_BUILDLINK3_MK)
 POLLY_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.polly+=	polly>=4.0.0nb20160911
-BUILDLINK_PKGSRCDIR.polly?=	../../wip/polly-git
+BUILDLINK_API_DEPENDS.polly+=	polly>=3.8.1
+BUILDLINK_PKGSRCDIR.polly?=	../../wip/polly
 
 .include "../../lang/llvm/buildlink3.mk"
 .endif	# POLLY_BUILDLINK3_MK
