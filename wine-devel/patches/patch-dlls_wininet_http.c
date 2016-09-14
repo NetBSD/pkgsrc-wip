5873d289116ea2505a655b4478664e07b03f7787
coypu <coypu@sdf.org>
2016-09-14 22:52:51 +0300

    wine-devel: update to 1.9.18

--- wine-devel/patches/patch-dlls_wininet_http.c
+++ wine-devel/patches/patch-dlls_wininet_http.c
@@ -1,29 +1,22 @@
 $NetBSD$
 
---- dlls/wininet/http.c.orig	2016-06-10 14:04:01.000000000 +0000
+--- dlls/wininet/http.c.orig	2016-09-02 16:39:28.000000000 +0000
 +++ dlls/wininet/http.c
-@@ -29,6 +29,12 @@
- 
- #include "config.h"
- 
-+#include <stdarg.h>
-+#include <stdio.h>
-+#include <stdlib.h>
-+#include <time.h>
-+#include <assert.h>
-+
- #ifdef HAVE_ZLIB
+@@ -35,14 +35,14 @@
  #  include <zlib.h>
  #endif
-@@ -36,11 +42,6 @@
- #include "winsock2.h"
- #include "ws2ipdef.h"
  
--#include <stdarg.h>
--#include <stdio.h>
--#include <stdlib.h>
--#include <time.h>
--#include <assert.h>
+-#include "winsock2.h"
+-#include "ws2ipdef.h"
+-
+ #include <stdarg.h>
+ #include <stdio.h>
+ #include <time.h>
+ #include <assert.h>
+ 
++#include "winsock2.h"
++#include "ws2ipdef.h"
++
  #include "windef.h"
  #include "winbase.h"
  #include "wininet.h"
