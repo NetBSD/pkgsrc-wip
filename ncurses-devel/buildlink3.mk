c04bcdbaf2f673a8e4904ca11abdaca1bd9835dc
Kamil Rytarowski <n54@gmx.com>
2016-09-18 03:13:49 +0200

    ncurses-devel: g/c incomplete package

--- ncurses-devel/buildlink3.mk
+++
@@ -1,5 +0,0 @@
-# $NetBSD: buildlink3.mk,v 1.1 2008/03/25 12:49:19 bsadewitz Exp $
-
-.include "../../devel/ncurses/buildlink3.mk"
-
-BUILDLINK_PKGSRCDIR.ncurses:=	../../wip/ncurses-devel
--- ncurses-devel/builtin.mk
+++
@@ -1,3 +0,0 @@
-# $NetBSD: builtin.mk,v 1.1 2008/03/25 12:49:19 bsadewitz Exp $
-
-.include "../../devel/ncurses/builtin.mk"
