d267ce3e3870680fc5fce13b56ada0de04824004
Mateusz Poszwa <old4@o2.pl>
2016-09-22 22:29:24 +0200

    xcb-util-xrm: Rename to xcb-util-xrm-git

--- xcb-util-xrm-git/buildlink3.mk
+++ xcb-util-xrm-git/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	xcb-util-xrm
 XCB_UTIL_XRM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.xcb-util-xrm+=	xcb-util-xrm>=1.0
-BUILDLINK_PKGSRCDIR.xcb-util-xrm?=	../../wip/xcb-util-xrm
+BUILDLINK_PKGSRCDIR.xcb-util-xrm?=	../../wip/xcb-util-xrm-git
 
 .endif	# XCB_UTIL_XRM_BUILDLINK3_MK
 
