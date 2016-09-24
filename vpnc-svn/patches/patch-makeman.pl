26cd2106a3d00f5b99e4032ce452f04f223084e1
Kamil Rytarowski <n54@gmx.com>
2016-09-24 16:02:12 +0200

    vpnc-svn: Remote SVN repo is dead

--- vpnc-svn/patches/patch-makeman.pl
+++
@@ -1,8 +0,0 @@
-$NetBSD: patch-makeman.pl,v 1.2 2014/02/20 20:53:59 hfath Exp $
-
---- makeman.pl.orig	2014-02-20 20:27:20.000000000 +0000
-+++ makeman.pl
-@@ -1,2 +1,2 @@
--#! /usr/bin/env perl
-+#! @PERL5@ -w
- 
