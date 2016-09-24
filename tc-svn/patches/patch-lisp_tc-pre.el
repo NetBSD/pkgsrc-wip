8b54735068f8f1fe66c842dab697a503f101a368
Kamil Rytarowski <n54@gmx.com>
2016-09-24 18:26:02 +0200

    tc-svn: Dead upstream

--- tc-svn/patches/patch-lisp_tc-pre.el
+++
@@ -1,15 +0,0 @@
-$NetBSD: patch-lisp_tc-pre.el,v 1.1 2012/08/06 14:58:41 makoto Exp $
-
-One line left as old version, fix.
-
---- lisp/tc-pre.el.orig	2012-08-06 23:22:57.000000000 +0900
-+++ lisp/tc-pre.el	2012-08-06 23:21:29.000000000 +0900
-@@ -95,7 +95,7 @@
- 
- ;;;; Version
- 
--(defconst tcode-version "2.3.1")
-+(defconst tcode-version "3.0")
- 
- (defun tcode-version ()
-   "Tコード入力環境のバージョンを表示する。"
