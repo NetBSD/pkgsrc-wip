295cc2fe428581dce68eacc877008d442c317b77
Kamil Rytarowski <n54@gmx.com>
2016-09-24 19:59:08 +0200

    assimp: Fix path of buildlink3.mk

--- assimp/buildlink3.mk
+++ assimp/buildlink3.mk
@@ -7,7 +7,7 @@ ASSIMP_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.assimp+=	assimp>=0
 BUILDLINK_ABI_DEPENDS.assimp+=	assimp>=0
-BUILDLINK_PKGSRCDIR.assimp?=	../../devel/assimp
+BUILDLINK_PKGSRCDIR.assimp?=	../../wip/assimp
 
 .include "../../devel/zlib/buildlink3.mk"
 .endif # ASSIMP_BUILDLINK3_MK
