4c090f40bcd4ef03282a21da6df07d12f39f6938
Kamil Rytarowski <n54@gmx.com>
2016-09-24 20:45:15 +0200

    colord-gtk: Cleanup buildlink3.mk

--- colord-gtk/buildlink3.mk
+++ colord-gtk/buildlink3.mk
@@ -1,14 +1,4 @@
 # $NetBSD$
-# XXX
-# XXX This file was created automatically using createbuildlink-3.17.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.colord-gtk?=	build
 
 BUILDLINK_TREE+=	colord-gtk
 
@@ -18,15 +8,9 @@ COLORD_GTK_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.colord-gtk+=	colord-gtk>=0.1.26
 BUILDLINK_PKGSRCDIR.colord-gtk?=	../../wip/colord-gtk
 
-# XXX
-# XXX Uncomment and keep only the buildlink3 lines below which are directly
-# XXX needed for dependencies to compile, link, and run.  If this package
-# XXX provides a wrappered API or otherwise does not expose the APIs of the
-# XXX buildlink3 lines below to dependencies, remove them.
-# XXX
-#.include "../../x11/gtk3/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
 #.include "../../devel/gobject-introspection/buildlink3.mk"
-#.include "../../wip/colord/buildlink3.mk"
+.include "../../wip/colord/buildlink3.mk"
 .endif	# COLORD_GTK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-colord-gtk
