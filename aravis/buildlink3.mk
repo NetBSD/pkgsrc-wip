4ac2f64358bab248f25640b0fe1ab6b38127aa4f
Kamil Rytarowski <n54@gmx.com>
2016-09-24 19:53:57 +0200

    aravis: pkglint

--- aravis/buildlink3.mk
+++ aravis/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	aravis
 ARAVIS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.aravis+=	aravis>=0.1.0
-BUILDLINK_PKGSRCDIR.aravis?=	../../category/aravis
+BUILDLINK_PKGSRCDIR.aravis?=	../../wip/aravis
 
 .include "../../devel/gobject-introspection/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
