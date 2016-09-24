85081bac4c23ff3e38a18533b6c2cfff105f2d01
Kamil Rytarowski <n54@gmx.com>
2016-09-24 19:24:18 +0200

    svn-package.mk: Replace with svn-package2.mk

--- mk/svn-package.mk
+++ mk/svn-package.mk
@@ -1,105 +1,42 @@
-# $Id: svn-package.mk,v 1.18 2013/03/29 20:43:05 asau Exp $
-
-# This file provides simple access to Subversion repositories, so that packages
-# can be created from Subversion instead of from released tarballs.
-#
-# === User-settable variables ===
-#
-# CHECKOUT_DATE
-#	Date to check out in ISO format (YYYY-MM-DD).
-#
-# === Package-settable variables ===
+# This file provides simple access to svn repositories, so that packages
+# can be created from svn instead of from released tarballs.
 #
 # A package using this file shall define the following variables:
 #
-# SVN_REPOSITORIES
-#	A list of unique identifiers. For each of those identifiers, the
-#	following variables define the details of how to access the
-#	Subversion repository.
+#	SVN_REPOSITORIES
+#		A list of unique identifiers /id/ for which appropriate
+#		SVN_REPO must be defined.
 #
-#	SVN_ROOT must be defined.
+#	SVN_REPO.${id}
+#		The svn repository
 #
-# SVN_ROOT.${id}
-#	The SVNROOT for the SVN repository, including anonsvn password,
-#	if applicable.
+#	SVN_MODULE.${id}
+#		The svn module to check out.
 #
-# SVN_MODULE.${id}
-#	The SVN module to check out.
-#
-#	Default value: ${id}
-#
-# SVN_DISTBASE.${id}
-#	The prefix for cached archive file name.
-#
-#	Default value: ${PKGBASE}-${SVN_MODULE.${repo}}
+#               Default value: ${id}
 #
 # It may define the following variables:
 #
-# SVN_CERTS
-#	Files holding SSL certificates of the svn servers.
+#	SVN_BRANCH.${id}
+#		The branch to check out.
 #
-# SVN_TAG.${id}
-#	Overridable SVN tag for a repository.
+#	SVN_REVISION.${id}
+#		The revision to check out.
 #
-#	Default: ${SVN_TAG} (today at midnight)
-#
-# SVN_TAG
-#	The default SVN tag that is checked out. May be overridden by
-#	SVN_TAG.${id}.
-#
-#	Default value: today at midnight.
-#
-# === Variables defined here ===
-#
-# This file defines the following variables:
-#
-# SVN_ROOT_SOURCEFORGE
-#	Common SVN repository locations for use in the SVN_ROOT
-#	variables.
-#
-# It also provides default values for the following variables, differing
-# from the system-wide defaults:
-#
-# DISTFILES
-#	Is set to an empty list, since that is the right choice for most
-#	of the Subversion packages.
-#
-#
-# PKGREVISION
-#	If PKGREVISION is not set, set it to today's date.  This is
-#	useful for packages that automatically grab the latest code
-#	from subversion every time they are built.
-#	TODO: Reconsider this, and instead use the subversion revision
-#	number in the package version somehow.
-#
-# Keywords: subversion
-#
+#	SVN_ENV.${id}
+#		The environment for svn
 
 .if !defined(_PKG_MK_SVN_PACKAGE_MK)
 _PKG_MK_SVN_PACKAGE_MK=	# defined
 
-BUILD_DEPENDS+=		subversion-base>=1.0:../../devel/subversion-base
+BUILD_DEPENDS+=		subversion-base-[0-9]*:../../devel/subversion-base
 
 #
 # defaults for user-visible input variables
 #
 
 DISTFILES?=		# empty
-# Enforce PKGREVISION unless SVN_TAG is set
-.if empty(SVN_TAG)
-. if defined(CHECKOUT_DATE)
-PKGREVISION?=		$(CHECKOUT_DATE:S/-//g)
-. else
-PKGREVISION?=		$(_SVN_PKGVERSION:S/.//g)
-. endif
-.endif
-
-#
-# definition of user-visible output variables
-#
-
-# commonly used repositories
-SVN_ROOT_SOURCEFORGE=	https://svn.sourceforge.net/svnroot
+PKGREVISION?=		${_SVN_PKGVERSION:S/.//g}
 
 #
 # End of the interface part. Start of the implementation part.
@@ -114,9 +51,9 @@ PKG_FAIL_REASON+=	"[svn-package.mk] SVN_REPOSITORIES must be set."
 SVN_REPOSITORIES?=	# none
 .endif
 
-.for repo in ${SVN_REPOSITORIES}
-.  if !defined(SVN_ROOT.${repo})
-PKG_FAIL_REASON+=	"[svn-package.mk] SVN_ROOT."${repo:Q}" must be set."
+.for _repo_ in ${SVN_REPOSITORIES}
+.  if !defined(SVN_REPO.${_repo_})
+PKG_FAIL_REASON+=	"[svn-package.mk] SVN_REPO."${_repo_:Q}" must be set."
 .  endif
 .endfor
 
@@ -126,89 +63,75 @@ PKG_FAIL_REASON+=	"[svn-package.mk] SVN_ROOT."${repo:Q}" must be set."
 
 USE_TOOLS+=		date pax
 
-_SVN_RSH=		ssh
 _SVN_CMD=		svn
-_SVN_ENV=		# empty
-_SVN_ENV+=		SVN_RSH=${_SVN_RSH:Q}
-_SVN_FLAGS=		-q
-_SVN_CHECKOUT_FLAGS=	--config-dir=${_SVN_CONFIG_DIR} --non-interactive
 _SVN_CONFIG_DIR=	${WRKDIR}/.subversion
-_SVN_TODAY_CMD=		${DATE} -u +'%Y-%m-%d'
-_SVN_TODAY=		${_SVN_TODAY_CMD:sh}
+_SVN_CHECKOUT_FLAGS=	--config-dir=${_SVN_CONFIG_DIR} --non-interactive
 _SVN_PKGVERSION_CMD=	${DATE} -u +'%Y.%m.%d'
 _SVN_PKGVERSION=	${_SVN_PKGVERSION_CMD:sh}
 _SVN_DISTDIR=		${DISTDIR}/svn-packages
 
 #
 # Generation of repository-specific variables
-#
 
-# determine appropriate checkout date or tag
+
 .for repo in ${SVN_REPOSITORIES}
 SVN_MODULE.${repo}?=	${repo}
-.  if defined(SVN_TAG.${repo})
-_SVN_TAG_FLAG.${repo}=	-r${SVN_TAG.${repo}}
-_SVN_TAG.${repo}=	${SVN_TAG.${repo}}
-.  elif defined(SVN_TAG)
-_SVN_TAG_FLAG.${repo}=	-r${SVN_TAG}
-_SVN_TAG.${repo}=	${SVN_TAG}
-.  elif defined(CHECKOUT_DATE)
-_SVN_TAG_FLAG.${repo}=	-r{${CHECKOUT_DATE}}
-_SVN_TAG.${repo}=	${CHECKOUT_DATE}
+_SVN_ENV.${repo}=	${SVN_ENV.${repo}}
+
+# determine appropriate checkout option
+.  if defined(SVN_REVISION.${repo})
+_SVN_FLAG.${repo}=	-r ${SVN_REVISION.${repo}}
 .  else
-_SVN_TAG_FLAG.${repo}=	-r{${_SVN_TODAY} 00:00 +0000}
-_SVN_TAG.${repo}=	trunk
+_SVN_FLAG.${repo}=	-r HEAD
 .  endif
 
 # Cache support:
-.  if !defined(NO_SVN_CACHE.${repo}) \
-   || defined(NO_SVN_CACHE.${repo}) && empty(NO_SVN_CACHE.${repo}:M[Yy][Ee][Ss])
 #   cache file name
-.  if !defined(SVN_DISTBASE.${repo})
-SVN_DISTBASE.${repo}=	${PKGBASE}-${SVN_MODULE.${repo}}
-.  endif
-_SVN_DISTFILE.${repo}=	${SVN_DISTBASE.${repo}}-${_SVN_TAG.${repo}}.tar.gz
+_SVN_DISTFILE.${repo}=	${PKGBASE}-${SVN_MODULE.${repo}}-svnarchive.tar.gz
 
 #   command to extract cache file
 _SVN_EXTRACT_CACHED.${repo}=	\
 	if [ -f ${_SVN_DISTDIR}/${_SVN_DISTFILE.${repo}:Q} ]; then		\
-	  ${STEP_MSG} "Extracting cached Subversion archive "${_SVN_DISTFILE.${repo}:Q}"."; \
+	  ${STEP_MSG} "Extracting cached SVN archive "${_SVN_DISTFILE.${repo}:Q}"."; \
 	  gzip -d -c ${_SVN_DISTDIR}/${_SVN_DISTFILE.${repo}:Q} | pax -r;	\
-	  exit 0;							\
 	fi
 
 #   create cache archive
 _SVN_CREATE_CACHE.${repo}=	\
-	${STEP_MSG} "Creating cached Subversion archive "${_SVN_DISTFILE.${repo}:Q}"."; \
-	${MKDIR} ${_SVN_DISTDIR:Q};							\
-	pax -w ${SVN_MODULE.${repo}:Q} | gzip > ${_SVN_DISTDIR}/${_SVN_DISTFILE.${repo}:Q}
-.  else
-# I have to set them to noop:
-_SVN_DISTFILE.${repo}=		:
-_SVN_EXTRACT_CACHED.${repo}=	:
-_SVN_CREATE_CACHE.${repo}=	:
-.  endif
+	${STEP_MSG} "Creating cached SVN archive "${_SVN_DISTFILE.${repo}:Q}"."; \
+	${MKDIR} ${_SVN_DISTDIR:Q};					\
+	pax -w ${SVN_MODULE.${repo}:Q} | gzip > ${_SVN_DISTDIR}/${_SVN_DISTFILE.${repo}:Q}.tmp;	\
+	${MV} '${_SVN_DISTDIR}/${_SVN_DISTFILE.${repo}:Q}.tmp' '${_SVN_DISTDIR}/${_SVN_DISTFILE.${repo}:Q}'
+
+#   fetch svn repo or update cached one
+_SVN_FETCH_REPO.${repo}=	\
+	if [ ! -d ${SVN_MODULE.${repo}:Q} ]; then				\
+	  ${STEP_MSG} "Cloning SVN repository "${SVN_MODULE.${repo}:Q}".";	\
+	  ${SETENV} ${_SVN_ENV.${repo}} ${_SVN_CMD}				\
+	    checkout ${_SVN_FLAG.${repo}} ${SVN_CHECKOUT_FLAGS}			\
+	    ${SVN_REPO.${repo}:Q} ${SVN_MODULE.${repo}:Q};			\
+	else									\
+	  ${STEP_MSG} "Updating SVN repository "${_SVN_FLAG.${repo}:Q}".";	\
+	  ${SETENV} ${_SVN_ENV.${repo}} ${_SVN_CMD}				\
+	    update ${_SVN_FLAG.${repo}} ${SVN_CHECKOUT_FLAGS}			\
+	    ${SVN_MODULE.${repo}:Q};						\
+	fi
 .endfor
 
 pre-extract: do-svn-extract
 
-do-svn-extract: .PHONY
-.if defined(SVN_CERTS) && !empty(SVN_CERTS)
+.PHONY: do-svn-extract
+do-svn-extract:
+.for _repo_ in ${SVN_REPOSITORIES}
+.if defined(SVN_CERTS.${_repo_}) && !empty(SVN_CERTS.${_repo_})
 	${RUN}${MKDIR} -p ${_SVN_CONFIG_DIR}/auth/svn.ssl.server
-	${RUN}${CP} ${SVN_CERTS} ${_SVN_CONFIG_DIR}/auth/svn.ssl.server
+	${RUN}${CP} ${SVN_CERTS.${_repo_}} ${_SVN_CONFIG_DIR}/auth/svn.ssl.server
 .endif
-.for repo in ${SVN_REPOSITORIES}
-	${RUN} cd ${WRKDIR};						\
-	if [ ! -d ${_SVN_DISTDIR} ]; then mkdir -p ${_SVN_DISTDIR:Q}; fi;	\
-	${_SVN_EXTRACT_CACHED.${repo}};					\
-	${STEP_MSG} "Downloading "${SVN_MODULE.${repo}:Q}" from "${SVN_ROOT.${repo}:Q}"."; \
-	${SETENV} ${_SVN_ENV}						\
-		${_SVN_CMD} checkout ${_SVN_CHECKOUT_FLAGS}		\
-			${_SVN_TAG_FLAG.${repo}:Q}			\
-			${_SVN_FLAGS}		 			\
-			${SVN_ROOT.${repo}:Q}				\
-			${SVN_MODULE.${repo}:Q};			\
-	${_SVN_CREATE_CACHE.${repo}}
+	${RUN} cd ${WRKDIR};							\
+	if [ ! -d ${_SVN_DISTDIR:Q} ]; then mkdir -p ${_SVN_DISTDIR:Q}; fi;	\
+	${_SVN_EXTRACT_CACHED.${_repo_}};					\
+	${_SVN_FETCH_REPO.${_repo_}};						\
+	${_SVN_CREATE_CACHE.${_repo_}};
 .endfor
 
 .endif
