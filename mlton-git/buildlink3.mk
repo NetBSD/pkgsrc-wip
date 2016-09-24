254ab4fca991410c922aefc7ae38510006158278
Kamil Rytarowski <n54@gmx.com>
2016-09-24 14:10:46 +0200

    mlton-svn: Rename to mlton-git

--- mlton-git/buildlink3.mk
+++ mlton-git/buildlink3.mk
@@ -8,7 +8,7 @@ BUILDLINK_TREE+=	mlton
 MLTON_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mlton+=	mlton-*
-BUILDLINK_PKGSRCDIR.mlton?=	../../wip/mlton-svn
+BUILDLINK_PKGSRCDIR.mlton?=	../../wip/mlton-git
 BUILDLINK_INCDIRS.mlton= lib/mlton/include
 BUILDLINK_LIBDIRS.mlton= lib/mlton/self
 #BUILDLINK_CONTENTS_FILTER.mlton= ${EGREP} '.*\.a$$|.*\.h$$'
