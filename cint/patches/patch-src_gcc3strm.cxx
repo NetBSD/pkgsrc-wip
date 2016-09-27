2aafefdc02c16ea8d268c54d2a037e5248d8bdf0
Kamil Rytarowski <n54@gmx.com>
2016-09-27 21:36:17 +0200

    cint: Proper fix for gcc3strm (GCC >=3.3)

--- cint/patches/patch-src_gcc3strm.cxx
+++ cint/patches/patch-src_gcc3strm.cxx
@@ -1,26 +1,22 @@
 $NetBSD$
 
+Fix build with recent (>3.3) GCC.
+
 --- src/gcc3strm.cxx.orig	2006-09-28 09:01:54.000000000 +0000
 +++ src/gcc3strm.cxx
-@@ -308,13 +308,21 @@ static int G__G__stream_7_6_0(G__value* 
+@@ -308,13 +308,13 @@ static int G__G__stream_7_6_0(G__value* 
  
  static int G__G__stream_7_7_0(G__value* result7, G__CONST char* funcname, struct G__param* libp, int hash)
  {
-+#if defined(__NetBSD__)
-+//      G__letint(result7, 103, (long) ((fpos<mbstate_t>*) G__getstructoffset())->operator==(*(fpos<mbstate_t>*) libp->para[0].ref));
-+#else
-       G__letint(result7, 103, (long) ((fpos<mbstate_t>*) G__getstructoffset())->operator==(*(fpos<mbstate_t>*) libp->para[0].ref));
-+#endif
+-      G__letint(result7, 103, (long) ((fpos<mbstate_t>*) G__getstructoffset())->operator==(*(fpos<mbstate_t>*) libp->para[0].ref));
++   G__letint(result7, 103, (long) (*(fpos<mbstate_t>*) G__getstructoffset())==(*(fpos<mbstate_t>*) libp->para[0].ref));
     return(1 || funcname || hash || result7 || libp) ;
  }
  
  static int G__G__stream_7_8_0(G__value* result7, G__CONST char* funcname, struct G__param* libp, int hash)
  {
-+#if defined(__NetBSD__)
-+//      G__letint(result7, 103, (long) ((fpos<mbstate_t>*) G__getstructoffset())->operator!=(*(fpos<mbstate_t>*) libp->para[0].ref));
-+#else
-       G__letint(result7, 103, (long) ((fpos<mbstate_t>*) G__getstructoffset())->operator!=(*(fpos<mbstate_t>*) libp->para[0].ref));
-+#endif
+-      G__letint(result7, 103, (long) ((fpos<mbstate_t>*) G__getstructoffset())->operator!=(*(fpos<mbstate_t>*) libp->para[0].ref));
++   G__letint(result7, 103, (long) (*(fpos<mbstate_t>*) G__getstructoffset())!=(*(fpos<mbstate_t>*) libp->para[0].ref));
     return(1 || funcname || hash || result7 || libp) ;
  }
  
