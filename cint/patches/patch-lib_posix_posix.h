2f425f4acfe8220d6174af689091460d593b7f53
Kamil Rytarowski <n54@gmx.com>
2016-09-28 00:31:27 +0200

    cint: Backport patches from lang/cint

---
+++ cint/patches/patch-lib_posix_posix.h
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- lib/posix/posix.h.orig	2006-11-28 15:08:01.000000000 +0000
++++ lib/posix/posix.h
+@@ -283,7 +283,7 @@ extern int setpgrp(pid_t _pid,pid_t _pgr
+ #endif
+ #elif defined(G__SUN) || defined(__sun)
+ extern long setpgrp(void);
+-#elif defined(G__FBSD)||defined(__FreeBSD__)||defined(G__OBSD)||defined(__OpenBSD__)||((defined(G__alpha)||defined(__alpha))&&defined(G__GNUC))||((defined(G__alpha)||defined(__alpha))&&defined(G__GNUC))
++#elif defined(G__FBSD)||defined(__FreeBSD__)||defined(G__NBSD)||defined(__NetBSD__)||defined(G__OBSD)||defined(__OpenBSD__)||((defined(G__alpha)||defined(__alpha))&&defined(G__GNUC))||((defined(G__alpha)||defined(__alpha))&&defined(G__GNUC))
+ extern int setpgrp(pid_t _pid, pid_t _pgrp);
+ #elif defined(G__KCC) || defined(__KCC)
+ extern pid_t setpgrp(void);
