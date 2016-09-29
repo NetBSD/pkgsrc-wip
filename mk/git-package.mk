acd40b90b3be0c964e50b9df196c7132fea853e4
Kamil Rytarowski <n54@gmx.com>
2016-09-30 00:43:42 +0200

    git-package.mk: Add support for dumb http: servers - GIT_DEEP_CLONE

--- mk/git-package.mk
+++ mk/git-package.mk
@@ -90,7 +90,7 @@ _GIT_FETCH_FLAGS.${repo}=	--quiet --recurse-submodules=yes --tags
 _GIT_CLONE_FLAGS.${repo}=	--quiet --recursive
 
 # For revision checkout we need deep copy
-. if !defined(GIT_REVISION.${repo})
+. if !defined(GIT_REVISION.${repo}) && !defined(GIT_DEEP_CLONE.${repo})
 _GIT_FETCH_FLAGS.${repo}+=	--depth 1
 _GIT_CLONE_FLAGS.${repo}+=	--depth 1
 . endif
