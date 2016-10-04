ea540a88245846553776413fb10a0228c05caed8
coypu <coypu@sdf.org>
2016-10-04 10:41:22 +0300

    gcc7snapshot: update to 7-20161002
    remove gcc-java options, deleted upstream, all related options and
    patches. goodbye!
    
    disable libssp on netbsd, as with all the other gccs - conflicts with
    base libssp.

--- gcc7snapshot/options.mk
+++ gcc7snapshot/options.mk
@@ -2,7 +2,7 @@
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.${GCC_PKGNAME}
 PKG_SUPPORTED_OPTIONS=	nls gcc-inplace-math gcc-c++ gcc-fortran \
-			gcc-go gcc-objc gcc-objc++ gcc-graphite gcc-java
+			gcc-go gcc-objc gcc-objc++ gcc-graphite
 PKG_SUGGESTED_OPTIONS=	gcc-c++ gcc-fortran gcc-objc gcc-objc++ \
 			gcc-graphite gcc-inplace-math
 
@@ -106,61 +106,11 @@ LANGS+=			obj-c++
 LANGS+=			objc
 .endif
 
-.if !empty(PKG_OPTIONS:Mgcc-java)
-.  if empty(PKG_OPTIONS:Mgcc-c++)
-PKG_OPTIONS+=		gcc-c++
-.  endif
-
-LANGS+=			java
-
-# Add the  Eclipse Java compiler for to parse `.java' source files.
-ECJ_JAR=		ecj-4.5.jar
-SITES.${ECJ_JAR}=	ftp://sourceware.org/pub/java/
-DISTFILES+=		${ECJ_JAR}
-CONFIGURE_ARGS+=	--with-ecj-jar=${DISTDIR:Q}/${ECJ_JAR:Q}
-
-JAVA_NAME=		${GCC_PKGNAME}
-JAVA_HOME=		${PREFIX}/java/${JAVA_NAME}
-JAVA_WRAPPERS=		appletviewer jar jarsigner java javah keytool \
-			native2ascii orbd rmic rmid rmiregistry serialver \
-			tnameserv
-
-JAVA_ARCH=		${MACHINE_ARCH:S/x86_64/amd64/}
-
-PLIST_SRC+=		PLIST.java
-PLIST_SUBST+=		JAVA_NAME=${JAVA_NAME:Q}
-PLIST_SUBST+=		JAVA_ARCH=${JAVA_ARCH:Q}
-
-# Create a JPackage compatible SDK environment.
-CONFIGURE_ARGS+=	--enable-java-home
-CONFIGURE_ARGS+=	--with-os-directory=${LOWER_OPSYS:Q}
-CONFIGURE_ARGS+=	--with-arch-directory=${JAVA_ARCH:Q}
-CONFIGURE_ARGS+=	--with-jvm-root-dir=${JAVA_HOME:Q}
-CONFIGURE_ARGS+=	--with-java-home=${JAVA_HOME:Q}
-
-REPLACE_PYTHON=		libjava/contrib/aot-compile.in
-
 USE_TOOLS+=		unzip zip:run gmake:run
 CONFIGURE_ARGS+=	--with-system-zlib
 
-# libjava/contrib/aotcompile.py.in stores the path to a 'make' program so
-# we need to make sure we give it the installed 'make' and not the tool
-# wrapped one.
-CONFIGURE_ENV+=		PKGSRC_MAKE=${TOOLS_PATH.gmake}
-MAKE_ENV+=		PKGSRC_MAKE=${TOOLS_PATH.gmake}
-
-# fastjar-0.93 from pkgsrc/archivers/fastjar seems to trigger a build
-# failure (seen on NetBSD-5.0/i386) when building java.  So in case
-# the fastjar package is installed, make sure the configure script
-# doesn't pick it up.
-CONFIGURE_ENV+=		JAR=no
-MAKE_ENV+=		JAR=no
-MAKE_ENV+=		ac_cv_prog_JAR=no
-
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../lang/python/application.mk"
-.include "../../mk/java-env.mk"
-.endif
 
 .if !empty(PKG_OPTIONS:Mgcc-go)
 LANGS+=			go
