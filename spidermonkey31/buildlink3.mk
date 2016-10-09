a77dd0ea63716bafbf96f9c34b0b773351b34764
Kamil Rytarowski <n54@gmx.com>
2016-10-09 15:47:04 +0200

    spidermonkey31: Regenerate blk3

--- spidermonkey31/buildlink3.mk
+++ spidermonkey31/buildlink3.mk
@@ -1,14 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.1 2014/02/06 15:12:17 fhajny Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	spidermonkey31
 
 .if !defined(SPIDERMONKEY31_BUILDLINK3_MK)
 SPIDERMONKEY31_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.spidermonkey31+=	spidermonkey31>=31.2.0
+BUILDLINK_API_DEPENDS.spidermonkey31+=	spidermonkey31>=31.2.0.rc0
 BUILDLINK_PKGSRCDIR.spidermonkey31?=	../../wip/spidermonkey31
 
+.include "../../devel/libffi/buildlink3.mk"
 .include "../../devel/nspr/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../textproc/icu/buildlink3.mk"
 .endif	# SPIDERMONKEY31_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-spidermonkey31
