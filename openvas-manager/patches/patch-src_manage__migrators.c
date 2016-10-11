4e38776809b68f1601b488dc60de3762433adfc8
Kamil Rytarowski <n54@gmx.com>
2016-10-11 23:37:07 +0200

    openvas-manager: Fix build

---
+++ openvas-manager/patches/patch-src_manage__migrators.c
@@ -0,0 +1,26 @@
+$NetBSD$
+
+--- src/manage_migrators.c.orig	2015-08-03 05:52:13.000000000 +0000
++++ src/manage_migrators.c
+@@ -119,8 +119,10 @@
+  *       (name UNIQUE, password);
+  */
+ 
++#if !defined(__NetBSD__)
+ /* time.h in glibc2 needs this for strptime. */
+ #define _XOPEN_SOURCE
++#endif
+ 
+ #include <time.h>
+ #include <stdlib.h>
+@@ -132,6 +134,10 @@
+ #include <ctype.h>
+ #include <dirent.h>
+ 
++#if defined(__NetBSD__)
++#include <sys/wait.h>
++#endif
++
+ #include "manage_sql.h"
+ #include "sql.h"
+ #include "tracef.h"
