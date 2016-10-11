b628becc4b94fad521dda00ecae27139285e8f22
Kamil Rytarowski <n54@gmx.com>
2016-10-11 22:56:18 +0200

    openvas-libraries: pkglint

--- openvas-libraries/buildlink3.mk
+++ openvas-libraries/buildlink3.mk
@@ -6,13 +6,13 @@ BUILDLINK_TREE+=	openvas-libraries
 OPENVAS_LIBRARIES_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.openvas-libraries+=	openvas-libraries>=8.0.8
-BUILDLINK_PKGSRCDIR.openvas-libraries?=	../../wip/openvas-libraries
+BUILDLINK_PKGSRCDIR.openvas-libraries?=		../../wip/openvas-libraries
 
 .include "../../databases/hiredis/buildlink3.mk"
 .include "../../databases/openldap-client/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/libuuid/buildlink3.mk"
-.include "../../net/libpcap/buildlink3.mk";
+.include "../../net/libpcap/buildlink3.mk"
 .include "../../security/gnutls/buildlink3.mk"
 .include "../../security/gpgme/buildlink3.mk"
 .include "../../security/libgcrypt/buildlink3.mk"
