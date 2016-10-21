235be2b13001cfab12e2a867ad75a88de39935f1
Kamil Rytarowski <n54@gmx.com>
2016-10-21 23:17:52 +0200

    tcl-itcl3: pkglint & add ITCL3_VERSION

--- tcl-itcl3/buildlink3.mk
+++ tcl-itcl3/buildlink3.mk
@@ -6,7 +6,11 @@ BUILDLINK_TREE+=	tcl-itcl3
 TCL_ITCL3_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.tcl-itcl3+=	tcl-itcl3>=3.4.3
-BUILDLINK_PKGSRCDIR.tcl-itcl3?=	../../wip/tcl-itcl3
+BUILDLINK_PKGSRCDIR.tcl-itcl3?=		../../wip/tcl-itcl3
+
+# New versions (4.x) are bundled into Tcl 8.6+
+# ITCL_VERSION is reserved for Tcl's buildlink3.mk
+ITCL3_VERSION=	3.4.3
 
 .include "../../lang/tcl/buildlink3.mk"
 .endif	# TCL_ITCL3_BUILDLINK3_MK
