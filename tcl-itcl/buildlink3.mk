11b4d094f699bc76781da61ba2b734d774fc4275
Kamil Rytarowski <n54@gmx.com>
2016-10-21 03:15:58 +0200

    tcl-itcl: Fixes

--- tcl-itcl/buildlink3.mk
+++ tcl-itcl/buildlink3.mk
@@ -1,18 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.4 2013/01/07 13:30:24 thomasklausner Exp $
+# $NetBSD$
 
-BUILDLINK_TREE+=	tk-itk
+BUILDLINK_TREE+=	tcl-itcl
 
-.if !defined(TK_ITK_BUILDLINK3_MK)
-TK_ITK_BUILDLINK3_MK:=
+.if !defined(TCL_ITCL_BUILDLINK3_MK)
+TCL_ITCL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.tk-itk+=	tk-itk>=3.3
-BUILDLINK_PKGSRCDIR.tk-itk?=	../../wip/tk-itk
+BUILDLINK_API_DEPENDS.tcl-itcl+=	tcl-itcl>=itcl3.4.3
+BUILDLINK_PKGSRCDIR.tcl-itcl?=	../../wip/tcl-itcl
 
 .include "../../x11/libXt/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
-.include "../../lang/tcl-itcl/buildlink3.mk"
 .include "../../lang/tcl/buildlink3.mk"
 .include "../../x11/tk/buildlink3.mk"
-.endif	# TK_ITK_BUILDLINK3_MK
+.endif	# TCL_ITCL_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-tk-itk
+BUILDLINK_TREE+=	-tcl-itcl
