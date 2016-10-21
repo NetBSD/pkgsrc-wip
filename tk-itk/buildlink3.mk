95df7923afc1b2e93b2bb9d125068dc0b4e80dd8
Kamil Rytarowski <n54@gmx.com>
2016-10-21 03:16:07 +0200

    tk-itk: Fixes

--- tk-itk/buildlink3.mk
+++ tk-itk/buildlink3.mk
@@ -1,18 +1,18 @@
 # $NetBSD: buildlink3.mk,v 1.4 2013/01/07 13:30:24 thomasklausner Exp $
 
-BUILDLINK_TREE+=	tk85-itk
+BUILDLINK_TREE+=	tk-itk
 
-.if !defined(TK85_ITK_BUILDLINK3_MK)
-TK85_ITK_BUILDLINK3_MK:=
+.if !defined(TK_ITK_BUILDLINK3_MK)
+TK_ITK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.tk85-itk+=	tk85-itk>=3.3
-BUILDLINK_PKGSRCDIR.tk85-itk?=	../../wip/tk85-itk
+BUILDLINK_API_DEPENDS.tk-itk+=	tk-itk>=3.3
+BUILDLINK_PKGSRCDIR.tk-itk?=	../../wip/tk-itk
 
 .include "../../x11/libXt/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../lang/tcl-itcl/buildlink3.mk"
 .include "../../lang/tcl/buildlink3.mk"
 .include "../../x11/tk/buildlink3.mk"
-.endif	# TK85_ITK_BUILDLINK3_MK
+.endif	# TK_ITK_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-tk85-itk
+BUILDLINK_TREE+=	-tk-itk
