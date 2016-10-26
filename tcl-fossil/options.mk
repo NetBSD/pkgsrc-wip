5c7b0e57df143a2c358caa415f38cb986d0748ed
Kamil Rytarowski <n54@gmx.com>
2016-10-26 02:51:35 +0200

    tcl-fossil: Add new option for dtrace; add more debugging under debug

--- tcl-fossil/options.mk
+++ tcl-fossil/options.mk
@@ -1,14 +1,18 @@
 # $NetBSD: options.mk,v 1.3 2015/03/11 22:32:08 tnn Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.tcl
-PKG_SUPPORTED_OPTIONS=	threads debug
+PKG_SUPPORTED_OPTIONS=	debug dtrace
 PKG_SUGGESTED_OPTIONS=	threads
 
 .include "../../mk/bsd.prefs.mk"
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdebug)
-CONFIGURE_ARGS+=	--enable-symbols=mem
+CONFIGURE_ARGS+=	--enable-symbols=all
+.endif
+
+.if !empty(PKG_OPTIONS:Mdtrace)
+CONFIGURE_ARGS+=	--enable-dtrace
 .endif
 
 .if !empty(PKG_OPTIONS:Mthreads)
