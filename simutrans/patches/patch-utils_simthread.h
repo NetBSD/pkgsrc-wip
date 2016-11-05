c70039a411dbd0a678697a9669c15b127242d644
i3enedek <git@xn--rvztrtkrfrgp-bbb7j2b8f0b9d7a21oft.com>
2016-11-05 00:40:12 +0000

    simutrans: fix pthreads and fetch localized strings.

--- simutrans/patches/patch-utils_simthread.h
+++ simutrans/patches/patch-utils_simthread.h
@@ -2,10 +2,12 @@ $NetBSD$
 
 --- utils/simthread.h.orig	2015-11-29 19:41:00.000000000 +0000
 +++ utils/simthread.h
-@@ -1,5 +1,6 @@
- #ifndef simthread_h
- #define simthread_h
-+#define _USE_POSIX_BARRIERS
+@@ -24,7 +24,7 @@
  
- #ifdef MULTI_THREAD
+ // Mac OS X defines this initializers without _NP.
+ #ifndef PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP
+-#define PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP PTHREAD_RECURSIVE_MUTEX_INITIALIZER
++#define PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP PTHREAD_MUTEX_INITIALIZER
+ #endif
  
+ // use our implementation if no posix barriers are available
