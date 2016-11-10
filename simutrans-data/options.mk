aee508a5e38e75cf4aeb8d2aabfcdce4164264e1
i3enedek <git@xn--rvztrtkrfrgp-bbb7j2b8f0b9d7a21oft.com>
2016-11-10 14:43:03 +0000

    simutrans: fix shellscript wrapper and the options file I forgot to add to simutrans-data

---
+++ simutrans-data/options.mk
@@ -0,0 +1,28 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.simutrans
+PKG_SUPPORTED_OPTIONS+=		pak64 pak128 pak128de pak64jp pak96hd
+PKG_SUGGESTED_OPTIONS=		pak64
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mpak96hd)
+DISTFILES=-http://hd.simutrans.com/release/PakHD_v04B_100-0.zip
+.endif
+
+.if !empty(PKG_OPTIONS:Mpak64jp)
+DISTFILES=simupak64.japan-120-0-1.zip
+.endif
+
+.if !empty(PKG_OPTIONS:Mpak128de)
+DISTFILES=PAK128.german_0.9_120.x.zip
+.endif
+
+.if !empty(PKG_OPTIONS:Mpak64)
+DISTFILES+=simupak64-120-0-1.zip
+DISTNAME=simupak64-120-0-1
+.endif
+
+.if !empty(PKG_OPTIONS:Mpak128)
+DISTFILES=pak128-2.6--ST120.zip
+.endif
