c41dee2a6b590baf883bd0dfad265910e7fded60
Christian Koch <cfkoch@edgebsd.org>
2016-11-12 13:34:32 -0800

    skalibs: Updated to 2.4.0.1.

--- skalibs/buildlink3.mk
+++ skalibs/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	skalibs
 .if !defined(SKALIBS_BUILDLINK3_MK)
 SKALIBS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.skalibs+=		skalibs>=2.3.8.3
+BUILDLINK_API_DEPENDS.skalibs+=		skalibs>=2.4.0.0
 BUILDLINK_PKGSRCDIR.skalibs?=		../../wip/skalibs
 
 .endif # SKALIBS_BUILDLINK3_MK
