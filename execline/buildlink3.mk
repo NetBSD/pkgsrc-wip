e6dbf4a7fe59e3c2636fddac528c8c349cc9a5dd
Christian Koch <cfkoch@edgebsd.org>
2016-11-12 13:35:09 -0800

    execline: Update to 2.2.0.0.

--- execline/buildlink3.mk
+++ execline/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	execline
 .if !defined(EXECLINE_BUILDLINK3_MK)
 EXECLINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.execline+=	execline>=2.1.4.5
+BUILDLINK_API_DEPENDS.execline+=	execline>=2.2.0.0
 BUILDLINK_PKGSRCDIR.execline?=		../../wip/execline
 
 .include "../../wip/skalibs/buildlink3.mk"
