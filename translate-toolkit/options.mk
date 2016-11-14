8ac940cf3270bb13b949e8ad3f4236c85368b530
i3enedek <git@xn--rvztrtkrfrgp-bbb7j2b8f0b9d7a21oft.com>
2016-11-14 23:10:55 +0000

    translate-toolkit: initial import

---
+++ translate-toolkit/options.mk
@@ -0,0 +1,45 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.translate-toolkit
+PKG_SUPPORTED_OPTIONS+=		lxml levenshtein chardet tradostm doc
+PKG_SUGGESTED_OPTIONS=		lxml chardet tradostm doc
+PLIST_VARS+=			doc
+
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mlxml)
+DEPENDS+=	${PYPKGPREFIX}-lxml>=3.5:../../textproc/py-lxml
+.endif
+
+.if !empty(PKG_OPTIONS:Mlevenshtein)
+PKG_FAIL_REASON+=	"You will need to install python-Levenshtein manually using pip"
+.endif
+
+.if !empty(PKG_OPTIONS:Mchardet)
+DEPENDS+=	${PYPKGPREFIX}-chardet>=2.3:../../converters/py-chardet
+.endif
+
+.if !empty(PKG_OPTIONS:Mtradostm)
+DEPENDS+=	${PYPKGPREFIX}-beautifulsoup4>=4.3:../../www/py-beautifulsoup4
+.endif
+
+.if !empty(PKG_OPTIONS:Mdoc)
+BUILD_DEPENDS+=	${PYPKGPREFIX}-sphinx>=0.4:../../textproc/py-sphinx
+SUBST_CLASSES+=         sphinx
+SUBST_SED.sphinx+=      -e "s,sphinx-build,sphinx-build${PYVERSSUFFIX},"
+SUBST_FILES.sphinx+=    docs/Makefile
+SUBST_STAGE.sphinx=     pre-configure
+SUBST_MESSAGE.sphinx=   Fix sphinx command names.
+INSTALLATION_DIRS+=	${PKGMANDIR}/man1
+PLIST.doc=             yes
+pre-build:
+		${MKDIR} ${DESTDIR}${PREFIX}/${PKGMANDIR}/man1
+		cd ${WRKSRC}/docs && ${MAKE} man
+		${INSTALL_MAN} ${WRKSRC}/docs/_build/man/translatetoolkit.1 \
+		${DESTDIR}${PREFIX}/${PKGMANDIR}/man1
+		${RM} -rf ${WRKSRC}/docs
+		${RM} -rf ${DESTDIR}${PREFIX}/${PYSITELIB}/translate/docs/
+	
+.endif
+
---
+++ translate-toolkit/patches/patch-setup.py
@@ -0,0 +1,23 @@
+$NetBSD$
+
+--- setup.py.orig	2016-11-02 19:01:16.000000000 +0000
++++ setup.py
+@@ -145,9 +145,6 @@ translatescripts = [
+ translatebashscripts = [
+     join(*('tools', ) + script) for script in [
+         ('junitmsgfmt', ),
+-        ('mozilla', 'build_firefox.sh'),
+-        ('mozilla', 'buildxpi.py'),
+-        ('mozilla', 'get_moz_enUS.py'),
+         ('pocommentclean', ),
+         ('pocompendium', ),
+         ('pomigrate2', ),
+@@ -430,7 +427,7 @@ def getdatafiles():
+              for f in os.listdir(srcdir) if isfile(join(srcdir, f))])
+ 
+     docfiles = []
+-    for subdir in ['docs', 'share']:
++    for subdir in ['share']:
+         docwalk = os.walk(subdir)
+         for docs in docwalk:
+             files = listfiles(docs[0])
