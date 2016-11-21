e05a1988597cbdf747ccba9c8bb0742273789148
Mateusz Poszwa <old4@o2.pl>
2016-11-21 18:35:31 +0100

    libLLVM38: Add draft of libLLVM38-3.8.1

---
+++ libLLVM38/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.1 2016/03/10 15:01:52 tnn Exp $
+
+BUILDLINK_TREE+=	libLLVM38
+
+.if !defined(LIBLLVM38_BUILDLINK3_MK)
+LIBLLVM38_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libLLVM38+=	libLLVM38>=3.8.0
+BUILDLINK_PKGSRCDIR.libLLVM38?=	../../wip/libLLVM38
+
+LLVM_CONFIG_PATH?=		${BUILDLINK_PREFIX.libLLVM38}/libexec/libLLVM38/llvm-config
+
+.include "../../devel/zlib/buildlink3.mk"
+.endif	# LLVM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libLLVM38
