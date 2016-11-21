2e93a03aeb58a7a0755048abb802cf26a553b890
Yorick Hardy <yorickhardy@gmail.com>
2016-11-21 22:36:53 +0200

    bstone: build fixes for gcc 5.4.0

---
+++ bstone/patches/patch-bstone__memory__binary__reader.h
@@ -0,0 +1,14 @@
+$NetBSD$
+
+Missing include.
+
+--- bstone_memory_binary_reader.h.orig	2016-04-09 12:21:25.000000000 +0000
++++ bstone_memory_binary_reader.h
+@@ -32,6 +32,7 @@ Free Software Foundation, Inc.,
+ 
+ 
+ #include <cstdint>
++#include <memory>
+ #include <string>
+ 
+ 
