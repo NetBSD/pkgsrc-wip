853ad2920e71424bff4ae295284ef5ccfa819b70
Greg Troxel <gdt@ir.bbn.com>
2016-11-28 14:19:59 -0500

    bitcoin: Drop QMAKE and BOOST defines that are not used.

--- bitcoin/options.mk
+++ bitcoin/options.mk
@@ -7,22 +7,13 @@ PKG_SUGGESTED_OPTIONS+=		dbus qrcode
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mminiupnpc)
-QMAKE_OPTIONS+=	USE_UPNP=1
 .include "../../wip/miniupnpc/buildlink3.mk"
-.else
-QMAKE_OPTIONS+=	USE_UPNP=-
 .endif
 
 .if !empty(PKG_OPTIONS:Mdbus)
-QMAKE_OPTIONS+=	USE_DBUS=1
 .include "../../x11/qt4-qdbus/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mqrcode)
-QMAKE_OPTIONS+=	USE_QRCODE=1
 .include "../../converters/qrencode/buildlink3.mk"
-.endif
-
-.if !empty(PKG_OPTIONS:Minet6)
-QMAKE_OPTIONS+=	USE_IPV6=0
 .endif
