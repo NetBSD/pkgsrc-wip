e7cbdb95eb45836bf919a2e208f75c716cd01591
Greg Troxel <gdt@ir.bbn.com>
2016-11-29 13:04:16 -0500

    bitcoin: Switch to upstream's install target
    This commit removes large amounts of special-case code surrounding
    secp256k1, and replaces it with running the install target in the
    secp256k1 source directory.  It remains to understand why upstream's
    main install target does not do this, should this issue survive
    version upgrades.
    
    Move bitcoind to bin from sbin, because that's where upstream puts it.

--- bitcoin/files/bitcoind.sh
+++ bitcoin/files/bitcoind.sh
@@ -13,7 +13,7 @@ fi
 
 name="bitcoind"
 rcvar=$name
-command="@PREFIX@/sbin/bitcoind"
+command="@PREFIX@/bin/bitcoind"
 pidfile="@VARBASE@/run/bitcoind.pid"
 required_directory="@VARBASE@/bitcoin"
 command_args="-daemon -conf=@PREFIX@/etc/bitcoin.conf -pid=${pidfile} -datadir=${required_directory}"
