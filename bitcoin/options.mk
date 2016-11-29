f660194e73438a04d7dc06167a6f6c533a537145
Greg Troxel <gdt@ir.bbn.com>
2016-11-29 11:46:35 -0500

    bitcoin: Make qt4 an option, default off
    The qt4 build didn't work anyway, because rcc wasn't found.  For now,
    disable it, resulting in the same package but with lighter
    dependencies.

--- bitcoin/options.mk
+++ bitcoin/options.mk
@@ -4,10 +4,21 @@
 # TODO: create split package for qt gui
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.bitcoin
-PKG_SUPPORTED_OPTIONS+=		miniupnpc dbus qrcode
-PKG_SUGGESTED_OPTIONS+=		dbus qrcode
+PKG_SUPPORTED_OPTIONS+=		qt4 miniupnpc dbus
+PKG_SUGGESTED_OPTIONS+=		dbus
 
 .include "../../mk/bsd.options.mk"
+
+# This fails to work because rcc (/usr/pkg/bin/qt4/rcc) is not found
+# by configure.  Until fixed, leave the option off.
+.if !empty(PKG_OPTIONS:Mqt4)
+BUILDLINK_DEPMETHOD.qt4-tools=	full
+.include "../../x11/qt4-tools/buildlink3.mk"
+.include "../../x11/qt4-libs/buildlink3.mk"
+
+# qrencode is only used by the qt gui, and it is tiny compared to qt.
+.include "../../converters/qrencode/buildlink3.mk"
+.endif
 
 .if !empty(PKG_OPTIONS:Mminiupnpc)
 .include "../../wip/miniupnpc/buildlink3.mk"
@@ -17,10 +28,4 @@ PKG_SUGGESTED_OPTIONS+=		dbus qrcode
 # should just be enabled instead of being an option.
 .if !empty(PKG_OPTIONS:Mdbus)
 .include "../../x11/qt4-qdbus/buildlink3.mk"
-.endif
-
-# TODO: If this is small, and only useful with qt, just depend
-# on it when qt is enabled.
-.if !empty(PKG_OPTIONS:Mqrcode)
-.include "../../converters/qrencode/buildlink3.mk"
 .endif
