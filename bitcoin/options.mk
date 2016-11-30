405b8e8da506c521164bdbf5d23ad48f9da2ad9d
Greg Troxel <gdt@ir.bbn.com>
2016-11-30 09:08:37 -0500

    bitcoin: Drop dbus option
    The dbus option added qt4-dbus, but upstream's current build
    instructions do not mention this.  Without it, the package builds and
    runs, so just drop it.
    
    Add note that qt option should not be enabled by default, as it
    doubles the size of the package.  (And, upstream appears to have some
    split package support.)

--- bitcoin/options.mk
+++ bitcoin/options.mk
@@ -1,36 +1,28 @@
 # $NetBSD$
 
-# TODO: make qt optional, and support qt5
-# TODO: create split package for qt gui
+# TODO: support qt5 also (or perhaps instead)
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.bitcoin
-PKG_SUPPORTED_OPTIONS+=		qt4 miniupnpc dbus
-PKG_SUGGESTED_OPTIONS+=		dbus
+PKG_SUPPORTED_OPTIONS+=		qt4 miniupnpc
+# qt4 is off because it doubles the footprint of the package.
+# Please do not enable it by default; instead, create a split
+# package.
+PKG_SUGGESTED_OPTIONS+=
 
 .include "../../mk/bsd.options.mk"
 
-# This fails to work because rcc (/usr/pkg/bin/qt4/rcc) is not found
-# by configure.  Until fixed, leave the option off.
 .if !empty(PKG_OPTIONS:Mqt4)
-BUILDLINK_DEPMETHOD.qt4-tools=	full
+PLIST.qt4=	yes
+
+.include "../../converters/qrencode/buildlink3.mk"
 .include "../../devel/protobuf/buildlink3.mk"
+BUILDLINK_DEPMETHOD.qt4-tools=	full
 .include "../../x11/qt4-tools/buildlink3.mk"
 .include "../../x11/qt4-libs/buildlink3.mk"
 
-# qrencode is only used by the qt gui, and it is tiny compared to qt.
-.include "../../converters/qrencode/buildlink3.mk"
-
 CONFIGURE_ARGS+=	--with-qt-bindir=${QTDIR}/bin
-
-PLIST.qt4=	yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mminiupnpc)
 .include "../../wip/miniupnpc/buildlink3.mk"
-.endif
-
-# TODO: Add a comment explaining what this does, and decide if it
-# should just be enabled instead of being an option.
-.if !empty(PKG_OPTIONS:Mdbus)
-.include "../../x11/qt4-qdbus/buildlink3.mk"
 .endif
