7b04852a399097978cfa424290eae076114641ad
Ryo ONODERA <ryoon@NetBSD.org>
2016-12-01 23:06:06 +0900

    chromium-new: Add missing python dependencies and implement pulseaudio/alsa option

--- chromium-new/options.mk
+++ chromium-new/options.mk
@@ -1,9 +1,22 @@
 # $NetBSD: options.mk,v 1.32 2016/08/20 11:17:32 ryoon Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.chromium
-PKG_SUPPORTED_OPTIONS+=	debug
+PKG_OPTIONS_VAR=		PKG_OPTIONS.chromium
+PKG_OPTIONS_REQUIRED_GROUPS=	audio
+PKG_OPTIONS_GROUP.audio=	alsa pulseaudio
+PKG_SUPPORTED_OPTIONS+=		debug
+PKG_SUGGESTED_OPTIONS=		alsa
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Malsa)
+GN_ARGS+=	use_alsa=true
+GN_ARGS+=	use_pulseaudio=false
+.include "../../audio/alsa-lib/buildlink3.mk"
+.elif !empty(PKG_OPTIONS:Mpulseaudio)
+GN_ARGS+=	use_alsa=false
+GN_ARGS+=	use_pulseaudio=true
+.include "../../audio/pulseaudio/buildlink3.mk"
+.endif
 
 .if !empty(PKG_OPTIONS:Mdebug)
 BUILDTYPE=	Debug
