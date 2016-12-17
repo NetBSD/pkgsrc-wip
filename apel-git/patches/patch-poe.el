e9d568798465a2cd14ca9cd3a3bc6c77f4be047c
Makoto Fujiwara (CF-S9) <makoto@ki.nu>
2016-12-17 22:38:26 +0900

    (apel-git) Modify poe.el patch for emacs26 (actually 25.1.50 for now), \N to \\N, for old-style backquotes detected! issue

--- apel-git/patches/patch-poe.el
+++ apel-git/patches/patch-poe.el
@@ -1,5 +1,6 @@
 $NetBSD: patch-poe.el,v 1.1 2013/10/03 11:17:02 makoto Exp $
 
+(1)
 Derived from 
 apel (10.8+0.20120427-4), 0.20120427-4.debian.tar.gz 
 to fix Following problem:
@@ -12,9 +13,32 @@ to fix Following problem:
  Wrong type argument: sequencep, 769
  gmake: *** [elc] Error 255
  ----
+(2)
+Fix following problem for emacs-26 (25.1.50)
+Loading `poe': old-style backquotes detected!
+Invalid read syntax: "Expected opening brace after \\N"
+gmake: *** [Makefile:31: elc] Error 255
 
---- poe.el.orig	2013-10-03 19:21:38.000000000 +0900
-+++ poe.el	2013-10-03 19:33:10.000000000 +0900
+--- poe.el.orig	2016-03-12 23:30:31.000000000 +0900
++++ poe.el	2016-12-14 19:08:13.000000000 +0900
+@@ -1081,7 +1081,7 @@ then capitalize each word in NEWTEXT.
+ If third arg LITERAL is non-nil, insert NEWTEXT literally.
+ Otherwise treat `\' as special:
+   `\&' in NEWTEXT means substitute original matched text.
+-  `\N' means substitute what matched the Nth `\(...\)'.
++  `\\N' means substitute what matched the Nth `\(...\)'.
+        If Nth parens didn't match, substitute nothing.
+   `\\' means insert one `\'.
+ FIXEDCASE and LITERAL are optional arguments.
+@@ -1133,7 +1133,7 @@ then capitalize each word in NEWTEXT.
+ If third arg LITERAL is non-nil, insert NEWTEXT literally.
+ Otherwise treat `\' as special:
+   `\&' in NEWTEXT means substitute original matched text.
+-  `\N' means substitute what matched the Nth `\(...\)'.
++  `\\N' means substitute what matched the Nth `\(...\)'.
+        If Nth parens didn't match, substitute nothing.
+   `\\' means insert one `\'.
+ FIXEDCASE and LITERAL are optional arguments.
 @@ -1667,7 +1667,7 @@ See `walk-windows' for the meaning of MI
                       )))
        ;; arglist: (prefix &optional dir-flag suffix)
