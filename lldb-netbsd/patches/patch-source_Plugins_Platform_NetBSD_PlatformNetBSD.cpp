f240d89b30e61c44e9a09d5a2631df5a7343da76
Kamil Rytarowski <n54@gmx.com>
2016-12-19 18:17:25 +0100

    lldb-netbsd: Reduce diff in PlatformNetBSD::CreateInstance with Linux
    Sponsored by <The NetBSD Foundation>

--- lldb-netbsd/patches/patch-source_Plugins_Platform_NetBSD_PlatformNetBSD.cpp
+++ lldb-netbsd/patches/patch-source_Plugins_Platform_NetBSD_PlatformNetBSD.cpp
@@ -2,7 +2,15 @@ $NetBSD$
 
 --- source/Plugins/Platform/NetBSD/PlatformNetBSD.cpp.orig	2016-12-17 10:29:30.000000000 +0000
 +++ source/Plugins/Platform/NetBSD/PlatformNetBSD.cpp
-@@ -34,6 +34,12 @@ using namespace lldb;
+@@ -23,6 +23,7 @@
+ #include "lldb/Breakpoint/BreakpointSite.h"
+ #include "lldb/Core/Debugger.h"
+ #include "lldb/Core/Error.h"
++#include "lldb/Core/Log.h"
+ #include "lldb/Core/Module.h"
+ #include "lldb/Core/ModuleSpec.h"
+ #include "lldb/Core/PluginManager.h"
+@@ -34,10 +35,29 @@ using namespace lldb;
  using namespace lldb_private;
  using namespace lldb_private::platform_netbsd;
  
@@ -12,10 +20,52 @@ $NetBSD$
 +/// Code to handle the PlatformNetBSD settings
 +//------------------------------------------------------------------
 +
++//------------------------------------------------------------------
++
  PlatformSP PlatformNetBSD::CreateInstance(bool force, const ArchSpec *arch) {
-   // The only time we create an instance is when we are creating a remote
-   // netbsd platform
-@@ -73,8 +79,6 @@ const char *PlatformNetBSD::GetDescripti
+-  // The only time we create an instance is when we are creating a remote
+-  // netbsd platform
+-  const bool is_host = false;
++  Log *log(GetLogIfAllCategoriesSet(LIBLLDB_LOG_PLATFORM));
++  if (log) {
++    const char *arch_name;
++    if (arch && arch->GetArchitectureName())
++      arch_name = arch->GetArchitectureName();
++    else
++      arch_name = "<null>";
++
++    const char *triple_cstr =
++        arch ? arch->GetTriple().getTriple().c_str() : "<null>";
++
++    log->Printf("PlatformNetBSD::%s(force=%s, arch={%s,%s})", __FUNCTION__,
++                force ? "true" : "false", arch_name, triple_cstr);
++  }
+ 
+   bool create = force;
+   if (create == false && arch && arch->IsValid()) {
+@@ -51,8 +71,19 @@ PlatformSP PlatformNetBSD::CreateInstanc
+       break;
+     }
+   }
+-  if (create)
+-    return PlatformSP(new PlatformNetBSD(is_host));
++
++  if (create) {
++    if (log)
++      log->Printf("PlatformNetBSD::%s() creating remote-netbsd platform",
++                  __FUNCTION__);
++    return PlatformSP(new PlatformNetBSD(false));
++  }
++
++  if (log)
++    log->Printf(
++        "PlatformLinux::%s() aborting creation of remote-netbsd platform",
++        __FUNCTION__);
++
+   return PlatformSP();
+ }
+ 
+@@ -73,8 +104,6 @@ const char *PlatformNetBSD::GetDescripti
      return "Remote NetBSD user platform plug-in.";
  }
  
