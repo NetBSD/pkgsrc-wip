3d042a5d2d5302be5ff9145a8adc304883f36d1b
weabot <weabot@protonmail.com>
2017-01-06 19:44:29 +0000

    First commit of libusbmuxd

---
+++ libusbmuxd/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libusbmuxd
+
+.if !defined(LIBUSBMUXD_BUILDLINK3_MK)
+LIBUSBMUXD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libusbmuxd+=	libusbmuxd>=1.0.10
+BUILDLINK_PKGSRCDIR.libusbmuxd?=	../../local/libusbmuxd
+
+.endif	# LIBUSBMUXD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libusbmuxd
