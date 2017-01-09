b7e4151920ff96c95c4a9a79904885f68fb73fb6
Mateusz Poszwa <old4@o2.pl>
2017-01-10 00:00:52 +0100

    uTox-git: Switch to uTox/uTox repository

--- uTox-git/patches/patch-src_xlib_main.c
+++ uTox-git/patches/patch-src_xlib_main.c
@@ -2,32 +2,31 @@ $NetBSD$
 
 Avoid using Linux-specific headers when not compiling for Linux.
 
---- src/xlib/main.c.orig	2016-03-21 09:05:38.000000000 +0000
+--- src/xlib/main.c.orig	2017-01-09 21:54:26.000000000 +0000
 +++ src/xlib/main.c
-@@ -73,8 +73,9 @@ void postmessage(uint32_t msg, uint16_t 
+@@ -36,7 +36,9 @@ void postmessage_utox(UTOX_MSG msg, uint
      XFlush(display);
  }
  
--
 +#ifdef __LINUX__
  #include <linux/input.h>
 +#endif
- FILE *ptt_keyboard_handle;
+ FILE *   ptt_keyboard_handle;
  Display *ptt_display;
- void init_ptt(void){
-@@ -100,6 +101,7 @@ _Bool check_ptt_key(void){
+ void     init_ptt(void) {
+@@ -61,6 +63,7 @@ bool check_ptt_key(void) {
      int ptt_key;
  
      /* First, we try for direct access to the keyboard. */
 +#ifdef __LINUX__
-     ptt_key = KEY_LEFTCTRL;                                      // TODO allow user to change this...
+     ptt_key = KEY_LEFTCTRL; // TODO allow user to change this...
      if (ptt_keyboard_handle) {
          /* Nice! we have direct access to the keyboard! */
-@@ -117,6 +119,7 @@ _Bool check_ptt_key(void){
+@@ -79,6 +82,7 @@ bool check_ptt_key(void) {
              return 0;
          }
      }
 +#endif
      /* Okay nope, lets' fallback to xinput... *pouts*
       * Fall back to Querying the X for the current keymap. */
-     ptt_key = XKeysymToKeycode(display, XK_Control_L);
+     ptt_key       = XKeysymToKeycode(display, XK_Control_L);
--- uTox-git/patches/patch-src_xlib_video.c
+++ uTox-git/patches/patch-src_xlib_video.c
@@ -2,16 +2,16 @@ $NetBSD$
 
 The volatile macro is used in {amd64,i386,machine}/mcontext.h on NetBSD.
 
---- src/xlib/video.c.orig	2016-03-21 09:05:38.000000000 +0000
+--- src/xlib/video.c.orig	2017-01-09 21:54:26.000000000 +0000
 +++ src/xlib/video.c
-@@ -152,6 +152,10 @@ _Bool video_init(void *handle) {
-     if(isdesktop(handle)) {
+@@ -144,6 +144,10 @@ bool native_video_init(void *handle) {
+     if (isdesktop(handle)) {
          utox_v4l_fd = -1;
  
 +#ifndef volatile
 +#define volatile(x) (*((volatile typeof(x)*)&x))
 +#endif
 +
-         video_x = volatile(grabx);
-         video_y = volatile(graby);
-         video_width = volatile(grabpx);
+         video_x      = volatile(grabx);
+         video_y      = volatile(graby);
+         video_width  = volatile(grabpx);
