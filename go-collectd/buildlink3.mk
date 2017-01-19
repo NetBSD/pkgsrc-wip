4a1c0c406d1785a77f12baf363ae930ae35886bc
Havard Eidnes <he@NetBSD.org>
2017-01-19 18:23:18 +0100

    Add go-collectd, go language bindings towards collectd.
    This version is from 20150830.
    
    This is a very simple package and very much a Work in Progress, so
    expect things to move around and be renamed a lot.
    
    The repository is organized as follows:
     * Package collectd.org/api declares data structures you may already
       know from the collectd source code itself, such as ValueList.
     * Package collectd.org/exec declares some utilities for writing
       binaries to be executed with the exec plugin. It provides some
       utilities (getting the hostname, e.g.) and an executor which you
       may use to easily schedule function calls.
     * Package collectd.org/format declares functions for formatting
       ValueLists in other format. Right now, only PUTVAL is implemented.
       Eventually I plan to add parsers for some formats, such as the
       JSON export.
     * Package collectd.org/network implements collectd's binary network
       protocol. It offers client and server implementations, see
       network.Client and network.ListenAndWrite() for more details.

---
+++ go-collectd/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	go-collectd
+
+.if !defined(GO_COLLECTD_BUILDLINK3_MK)
+GO_COLLECTD_BUILDLINK3_MK:=
+
+BUILDLINK_CONTENTS_FILTER.go-collectd=	${EGREP} gopkg/
+BUILDLINK_DEPMETHOD.go-collectd?=	build
+
+BUILDLINK_API_DEPENDS.go-collectd+=	go-collectd>=20150830
+BUILDLINK_PKGSRCDIR.go-collectd?=	../../sysutils/go-collectd
+.endif	# GO_COLLECTD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-go-collectd
