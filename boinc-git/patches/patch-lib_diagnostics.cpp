6f26e94a593df5744f44cb2d2f69230f3881865f
Mateusz Poszwa <old4@o2.pl>
2017-01-25 20:41:50 +0100

    boinc-git: Update a patch

--- boinc-git/patches/patch-lib_diagnostics.cpp
+++ boinc-git/patches/patch-lib_diagnostics.cpp
@@ -2,14 +2,14 @@ $NetBSD$
 
 Use siginfo_t instead of struct siginfo, which may be a union.
 
---- lib/diagnostics.cpp.orig	2016-02-22 14:20:58.000000000 +0000
+--- lib/diagnostics.cpp.orig	2017-01-23 01:26:42.000000000 +0000
 +++ lib/diagnostics.cpp
-@@ -751,7 +751,7 @@ static char *xtoa(size_t x) {
- #endif
- 
- #ifdef HAVE_SIGACTION
--void boinc_catch_signal(int signal, struct siginfo *siginfo, void *sigcontext) {
-+void boinc_catch_signal(int signal, siginfo_t *siginfo, void *sigcontext) {
+@@ -754,7 +754,7 @@ static char *xtoa(size_t x) {
+ #ifdef ANDROID
+ void boinc_catch_signal(int signal, struct siginfo *siginfo, void *sigcontext) {
+ #else
+-void boinc_catch_signal(int signal, struct siginfo *, void *) {
++void boinc_catch_signal(int signal, siginfo_t *, void *) {
+ #endif  // ANDROID
  #else
  void boinc_catch_signal(int signal) {
- #endif
