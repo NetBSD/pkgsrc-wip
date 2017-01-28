a3f2f332b5b7767fdefdc285bbd77b92019f9676
Kamil Rytarowski <n54@gmx.com>
2017-01-28 16:30:41 +0100

    lldb-netbsd: Fix build after switch to llvm::call_once
    Fix regular compilation issues on NetBSD. Other targets not tested.
    
    Sponsored by <The NetBSD Foundation>

--- lldb-netbsd/patches/patch-source_Core_ModuleList.cpp
+++ lldb-netbsd/patches/patch-source_Core_ModuleList.cpp
@@ -2,21 +2,19 @@ $NetBSD$
 
 --- source/Core/ModuleList.cpp.orig	2016-12-17 10:30:30.000000000 +0000
 +++ source/Core/ModuleList.cpp
-@@ -1,4 +1,4 @@
--//===-- ModuleList.cpp ------------------------------------------*- C++ -*-===//
-+~//===-- ModuleList.cpp ------------------------------------------*- C++ -*-===//
- //
- //                     The LLVM Compiler Infrastructure
- //
-@@ -28,6 +28,7 @@
+@@ -26,8 +26,11 @@
+ #include "lldb/Symbol/SymbolFile.h"
+ #include "lldb/Symbol/VariableList.h"
  
++#include "llvm/Support/Threading.h"
++
  using namespace lldb;
  using namespace lldb_private;
 +using namespace llvm;
  
  ModuleList::ModuleList()
      : m_modules(), m_modules_mutex(), m_notifier(nullptr) {}
-@@ -644,8 +645,8 @@ size_t ModuleList::GetIndexForModule(con
+@@ -644,8 +647,8 @@ size_t ModuleList::GetIndexForModule(con
  
  static ModuleList &GetSharedModuleList() {
    static ModuleList *g_shared_module_list = nullptr;
--- lldb-netbsd/patches/patch-source_Plugins_Process_mach-core_ProcessMachCore.cpp
+++ lldb-netbsd/patches/patch-source_Plugins_Process_mach-core_ProcessMachCore.cpp
@@ -2,7 +2,15 @@ $NetBSD$
 
 --- source/Plugins/Process/mach-core/ProcessMachCore.cpp.orig	2016-12-17 10:30:06.000000000 +0000
 +++ source/Plugins/Process/mach-core/ProcessMachCore.cpp
-@@ -45,6 +45,7 @@
+@@ -39,12 +39,15 @@
+ // Needed for the plug-in names for the dynamic loaders.
+ #include "lldb/Utility/SafeMachO.h"
+ 
++#include "llvm/Support/Threading.h"
++
+ #include "Plugins/DynamicLoader/Darwin-Kernel/DynamicLoaderDarwinKernel.h"
+ #include "Plugins/DynamicLoader/MacOSX-DYLD/DynamicLoaderMacOSXDYLD.h"
+ #include "Plugins/ObjectFile/Mach-O/ObjectFileMachO.h"
  
  using namespace lldb;
  using namespace lldb_private;
@@ -10,7 +18,7 @@ $NetBSD$
  
  ConstString ProcessMachCore::GetPluginNameStatic() {
    static ConstString g_name("mach-o-core");
-@@ -579,9 +580,9 @@ Error ProcessMachCore::GetMemoryRegionIn
+@@ -579,9 +582,9 @@ Error ProcessMachCore::GetMemoryRegionIn
  void ProcessMachCore::Clear() { m_thread_list.Clear(); }
  
  void ProcessMachCore::Initialize() {
