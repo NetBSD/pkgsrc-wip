730522294465a41856ecdca895704b0b11ed1fbe
Kamil Rytarowski <n54@gmx.com>
2017-01-31 18:22:39 +0100

    lldb-netbsd: Update to 293625 (5.0.0svn)
    This repo will have reduced the patches/ content.
    Part of diffs applied upstream.
    
    Sponsored by <The NetBSD Foundation>

--- lldb-netbsd/buildlink3.mk
+++ lldb-netbsd/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	lldb
 .if !defined(LLDB_BUILDLINK3_MK)
 LLDB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lldb+=	lldb>=3.9.0nb20160121
+BUILDLINK_API_DEPENDS.lldb+=	lldb>=5.0.0nb20160121
 BUILDLINK_PKGSRCDIR.lldb?=	../../wip/lldb-netbsd
 
 .include "../../textproc/libxml2/buildlink3.mk"
