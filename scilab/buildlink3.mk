cb23401e0b17bedcdaba600e69fc64fbdefe6b61
Benny Siegert <bsiegert@gmail.com>
2017-02-04 13:34:35 +0100

    Start updating to 5.5.2.
    Everything is broken!

--- scilab/buildlink3.mk
+++ scilab/buildlink3.mk
@@ -6,17 +6,17 @@ BUILDLINK_TREE+=	scilab
 .if !defined(SCILAB_BUILDLINK3_MK)
 SCILAB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.scilab+=	scilab>=4.1
+BUILDLINK_API_DEPENDS.scilab+=	scilab>=5.5
 BUILDLINK_ABI_DEPENDS.scilab+=	scilab>=4.1
 BUILDLINK_PKGSRCDIR.scilab?=	../../math/scilab
 
 BUILDLINK_FILES.scilab+=	lib/scilab-${SCIVERSION}/Version.incl
 
-SCI=	${BUILDLINK_DIR}/lib/scilab-4.1
+SCI=	${BUILDLINK_DIR}/lib/scilab-5.5.2
 CONFIGURE_ENV+=	SCI=${SCI:Q}
 MAKE_ENV+=	SCI=${SCI:Q}
 
-SCIVERSION=	4.1
+SCIVERSION=	5.5.2
 .endif # SCILAB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-scilab
