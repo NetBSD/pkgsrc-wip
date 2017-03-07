5393f53a313ec5dfb3c6af6cd91ca58ab722f820
Leonardo Taccari <leot@NetBSD.org>
2017-03-07 10:45:02 +0100

    intel-vaapi-driver: avoid alloca(3) uses replacing it with malloc(3) and free(3)
    Suggested by upstream via pull request #77.

--- intel-vaapi-driver/patches/patch-src_i965__decoder__utils.c
+++ intel-vaapi-driver/patches/patch-src_i965__decoder__utils.c
@@ -1,25 +1,35 @@
 $NetBSD$
 
-Honor AC_FUNC_ALLOCA and include the proper header files to use alloca(3).
+Use malloc(3) and free(3) instead of alloca(3).
+
+Suggested by upstream via issue pull request #77.
 
 --- src/i965_decoder_utils.c.orig	2016-11-10 05:04:36.000000000 +0000
 +++ src/i965_decoder_utils.c
-@@ -23,7 +23,17 @@
+@@ -23,7 +23,6 @@
  
  #include "sysdeps.h"
  #include <limits.h>
 -#include <alloca.h>
-+#ifdef STDC_HEADERS
-+# include <stdlib.h>
-+# include <stddef.h>
-+#else
-+# ifdef HAVE_STDLIB_H
-+#  include <stdlib.h>
-+# endif
-+#endif
-+#ifdef HAVE_ALLOCA_H
-+# include <alloca.h>
-+#endif
  
  #include "intel_batchbuffer.h"
  #include "intel_media.h"
+@@ -343,7 +342,7 @@ avc_get_first_mb_bit_offset_with_epb(
+     if (buf_size > data_size)
+         buf_size = data_size;
+ 
+-    buf = alloca(buf_size);
++    buf = malloc(buf_size);
+     ret = dri_bo_get_subdata(
+         slice_data_bo, slice_param->slice_data_offset,
+         buf_size, buf
+@@ -355,6 +354,9 @@ avc_get_first_mb_bit_offset_with_epb(
+             i += 2, j++, n++;
+     }
+ 
++    if (buf)
++        free(buf);
++
+     out_slice_data_bit_offset = in_slice_data_bit_offset + n * 8;
+ 
+     if (mode_flag == ENTROPY_CABAC)
