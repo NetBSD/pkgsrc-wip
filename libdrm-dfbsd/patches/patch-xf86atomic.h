d9378c266395997ef1bb9467ad519ac75989a268
David Shao <davshao@gmail.com>
2017-03-08 15:11:53 -0800

    libdrm-dfbsd: Fix refined for atomic_add_unless and add libkms
    Changes:
    * NetBSD seems to prefer the previous form of atomic_add_unless
      with the incorrect comparison c == unless.
    
    * Add libkms option.
    
    * Change pthreadstubs option to be default.

--- libdrm-dfbsd/patches/patch-xf86atomic.h
+++ libdrm-dfbsd/patches/patch-xf86atomic.h
@@ -7,12 +7,15 @@ https://bugs.freedesktop.org/show_bug.cgi?id=100077
 
 --- xf86atomic.h.orig	2015-09-22 04:34:51.000000000 +0000
 +++ xf86atomic.h
-@@ -111,7 +111,7 @@ static inline int atomic_add_unless(atom
+@@ -111,7 +111,11 @@ static inline int atomic_add_unless(atom
  	c = atomic_read(v);
  	while (c != unless && (old = atomic_cmpxchg(v, c, c + add)) != c)
  		c = old;
--	return c == unless;
++#if defined(__FreeBSD__) || defined(__DragonFly__)
 +	return c != unless;
++#else
+ 	return c == unless;
++#endif
  }
  
  #endif
