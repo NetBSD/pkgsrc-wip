4e977b30af757cf9b688ac8be9babb00176d3fba
Kamil Rytarowski <n54@gmx.com>
2017-03-29 04:48:35 +0200

    lldb-netbsd: clang-format over elf-core plugin
    This is preparation for upstream.
    
    Sponsored by <The NetBSD Foundation>

--- lldb-netbsd/patches/patch-source_Plugins_Process_elf-core_ThreadElfCore.cpp
+++ lldb-netbsd/patches/patch-source_Plugins_Process_elf-core_ThreadElfCore.cpp
@@ -1,16 +1,16 @@
 $NetBSD$
 
---- source/Plugins/Process/elf-core/ThreadElfCore.cpp.orig	2017-02-27 20:47:52.000000000 +0000
+--- source/Plugins/Process/elf-core/ThreadElfCore.cpp.orig	2017-03-26 19:17:32.000000000 +0000
 +++ source/Plugins/Process/elf-core/ThreadElfCore.cpp
-@@ -18,6 +18,7 @@
- #include "Plugins/Process/Utility/RegisterContextFreeBSD_mips64.h"
- #include "Plugins/Process/Utility/RegisterContextFreeBSD_powerpc.h"
- #include "Plugins/Process/Utility/RegisterContextFreeBSD_x86_64.h"
-+#include "Plugins/Process/Utility/RegisterContextNetBSD_x86_64.h"
+@@ -21,6 +21,7 @@
  #include "Plugins/Process/Utility/RegisterContextLinux_i386.h"
  #include "Plugins/Process/Utility/RegisterContextLinux_s390x.h"
  #include "Plugins/Process/Utility/RegisterContextLinux_x86_64.h"
-@@ -110,6 +111,17 @@ ThreadElfCore::CreateRegisterContextForF
++#include "Plugins/Process/Utility/RegisterContextNetBSD_x86_64.h"
+ #include "Plugins/Process/Utility/RegisterContextOpenBSD_i386.h"
+ #include "Plugins/Process/Utility/RegisterContextOpenBSD_x86_64.h"
+ #include "Plugins/Process/Utility/RegisterInfoPOSIX_arm.h"
+@@ -112,6 +113,17 @@ ThreadElfCore::CreateRegisterContextForF
        break;
      }
  
@@ -28,3 +28,33 @@ $NetBSD$
      case llvm::Triple::Linux: {
        switch (arch.GetMachine()) {
        case llvm::Triple::arm:
+@@ -144,8 +156,8 @@ ThreadElfCore::CreateRegisterContextForF
+         reg_interface = new RegisterInfoPOSIX_arm(arch);
+         break;
+       case llvm::Triple::x86:
+-	reg_interface = new RegisterContextOpenBSD_i386(arch);
+-	break;
++        reg_interface = new RegisterContextOpenBSD_i386(arch);
++        break;
+       case llvm::Triple::x86_64:
+         reg_interface = new RegisterContextOpenBSD_x86_64(arch);
+         break;
+@@ -260,7 +272,6 @@ Error ELFLinuxPrStatus::Parse(DataExtrac
+   pr_cstime.tv_sec = data.GetPointer(&offset);
+   pr_cstime.tv_usec = data.GetPointer(&offset);
+ 
+-
+   return error;
+ }
+ 
+@@ -317,9 +328,7 @@ Error ELFLinuxPrPsInfo::Parse(DataExtrac
+ //----------------------------------------------------------------
+ // Parse SIGINFO from NOTE entry
+ //----------------------------------------------------------------
+-ELFLinuxSigInfo::ELFLinuxSigInfo() {
+-  memset(this, 0, sizeof(ELFLinuxSigInfo));
+-}
++ELFLinuxSigInfo::ELFLinuxSigInfo() { memset(this, 0, sizeof(ELFLinuxSigInfo)); }
+ 
+ Error ELFLinuxSigInfo::Parse(DataExtractor &data, const ArchSpec &arch) {
+   Error error;
