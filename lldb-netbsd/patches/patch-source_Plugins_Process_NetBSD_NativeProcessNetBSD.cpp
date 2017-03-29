67b71dc4cadba48ff0be0ab25e0e85c0726a5234
Kamil Rytarowski <n54@gmx.com>
2017-03-29 02:46:07 +0200

    lldb-netbsd: Resurrect PT_STEP, TRAP_TRACE, TRAP_EXEC
    This is work in progress to rebase to new trunk.
    
    Sponsored by <The NetBSD Foundation>

--- lldb-netbsd/patches/patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.cpp
+++ lldb-netbsd/patches/patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.cpp
@@ -1,2 +1,72 @@
 $NetBSD$
 
+--- source/Plugins/Process/NetBSD/NativeProcessNetBSD.cpp.orig	2017-03-29 00:14:15.000000000 +0000
++++ source/Plugins/Process/NetBSD/NativeProcessNetBSD.cpp
+@@ -235,6 +235,41 @@ void NativeProcessNetBSD::MonitorSIGTRAP
+       }
+       SetState(StateType::eStateStopped, true);
+       break;
++    case TRAP_TRACE:
++      for (const auto &thread_sp : m_threads) {
++        static_pointer_cast<NativeThreadNetBSD>(thread_sp)->SetStoppedByTrace();
++      }
++      SetState(StateType::eStateStopped, true);
++      break;
++    case TRAP_EXEC:
++      {
++      // Clear old threads
++      m_threads.clear();
++
++      // Initialize new thread
++      struct ptrace_lwpinfo info = {};
++      Error error = PtraceWrapper(PT_LWPINFO, pid, &info, sizeof(info));
++      if (error.Fail()) {
++        SetState(StateType::eStateInvalid);
++        return;
++      }
++      // Reinitialize from scratch threads and register them in process
++      while (info.pl_lwpid != 0) {
++        NativeThreadNetBSDSP thread_sp = AddThread(info.pl_lwpid);
++        thread_sp->SetStoppedByExec();
++        error = PtraceWrapper(PT_LWPINFO, pid, &info, sizeof(info));
++        if (error.Fail()) {
++          SetState(StateType::eStateInvalid);
++          return;
++        }
++      }
++
++      // Let our delegate know we have just exec'd.
++      NotifyDidExec();
++
++      SetState(StateType::eStateStopped, true);
++      }
++      break;
+     }
+   }
+ }
+@@ -403,7 +438,14 @@ Error NativeProcessNetBSD::Resume(const 
+     break;
+   }
+   case eStateStepping:
+-    return Error("Not implemented");
++    // Run the thread, possibly feeding it the signal.
++    Error error = NativeProcessNetBSD::PtraceWrapper(PT_STEP, GetID(),(void *)1, action->signal);
++    if (!error.Success())
++      return error;
++    for (const auto &thread_sp : m_threads) {
++      static_pointer_cast<NativeThreadNetBSD>(thread_sp)->SetStepping();
++    }
++    SetState(eStateStepping, true)
+     break;
+ 
+   case eStateSuspended:
+@@ -850,9 +892,6 @@ NativeThreadNetBSDSP NativeProcessNetBSD
+ ::pid_t NativeProcessNetBSD::Attach(lldb::pid_t pid, Error &error) {
+   Log *log(GetLogIfAllCategoriesSet(LIBLLDB_LOG_PROCESS));
+ 
+-  // Use a map to keep track of the threads which we have attached/need to
+-  // attach.
+-  Host::TidMap tids_to_attach;
+   if (pid <= 1) {
+     error.SetErrorToGenericError();
+     error.SetErrorString("Attaching to process 1 is not allowed.");
