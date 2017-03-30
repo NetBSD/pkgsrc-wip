1cbd314a074d34b7355d2824f7be384948a99849
Kamil Rytarowski <n54@gmx.com>
2017-03-30 20:39:28 +0200

    lldb-netbsd: Adapt local changes to upstream review
    https://reviews.llvm.org/D31450
    
    Sponsored by <The NetBSD Foundation>

--- lldb-netbsd/patches/patch-source_Plugins_DynamicLoader_POSIX-DYLD_DYLDRendezvous.cpp
+++ lldb-netbsd/patches/patch-source_Plugins_DynamicLoader_POSIX-DYLD_DYLDRendezvous.cpp
@@ -1,8 +1,16 @@
 $NetBSD$
 
---- source/Plugins/DynamicLoader/POSIX-DYLD/DYLDRendezvous.cpp.orig	2017-02-04 18:35:34.000000000 +0000
+--- source/Plugins/DynamicLoader/POSIX-DYLD/DYLDRendezvous.cpp.orig	2017-03-23 15:49:52.000000000 +0000
 +++ source/Plugins/DynamicLoader/POSIX-DYLD/DYLDRendezvous.cpp
-@@ -385,6 +385,7 @@ bool DYLDRendezvous::SOEntryIsMainExecut
+@@ -379,12 +379,13 @@ bool DYLDRendezvous::RemoveSOEntries() {
+ }
+ 
+ bool DYLDRendezvous::SOEntryIsMainExecutable(const SOEntry &entry) {
+-  // On Linux the executable is indicated by an empty path in the entry. On
+-  // FreeBSD and on Android it is the full path to the executable.
++  // On some systes the executable is indicated by an empty path in the entry.
++  // On others it is the full path to the executable.
+ 
    auto triple = m_process->GetTarget().GetArchitecture().GetTriple();
    switch (triple.getOS()) {
    case llvm::Triple::FreeBSD:
--- lldb-netbsd/patches/patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.cpp
+++ lldb-netbsd/patches/patch-source_Plugins_Process_NetBSD_NativeProcessNetBSD.cpp
@@ -2,7 +2,7 @@ $NetBSD$
 
 --- source/Plugins/Process/NetBSD/NativeProcessNetBSD.cpp.orig	2017-03-29 00:14:15.000000000 +0000
 +++ source/Plugins/Process/NetBSD/NativeProcessNetBSD.cpp
-@@ -235,6 +235,39 @@ void NativeProcessNetBSD::MonitorSIGTRAP
+@@ -235,6 +235,24 @@ void NativeProcessNetBSD::MonitorSIGTRAP
        }
        SetState(StateType::eStateStopped, true);
        break;
@@ -13,25 +13,10 @@ $NetBSD$
 +      SetState(StateType::eStateStopped, true);
 +      break;
 +    case TRAP_EXEC: {
-+      // Clear old threads
-+      m_threads.clear();
-+
-+      // Initialize new thread
-+      struct ptrace_lwpinfo info = {};
-+      Error error = PtraceWrapper(PT_LWPINFO, pid, &info, sizeof(info));
++      Error error = ReinitializeThreads();
 +      if (error.Fail()) {
 +        SetState(StateType::eStateInvalid);
 +        return;
-+      }
-+      // Reinitialize from scratch threads and register them in process
-+      while (info.pl_lwpid != 0) {
-+        NativeThreadNetBSDSP thread_sp = AddThread(info.pl_lwpid);
-+        thread_sp->SetStoppedByExec();
-+        error = PtraceWrapper(PT_LWPINFO, pid, &info, sizeof(info));
-+        if (error.Fail()) {
-+          SetState(StateType::eStateInvalid);
-+          return;
-+        }
 +      }
 +
 +      // Let our delegate know we have just exec'd.
@@ -42,7 +27,7 @@ $NetBSD$
      }
    }
  }
-@@ -389,11 +422,13 @@ Error NativeProcessNetBSD::Resume(const 
+@@ -389,11 +407,13 @@ Error NativeProcessNetBSD::Resume(const 
      return Error();
    }
  
@@ -58,7 +43,7 @@ $NetBSD$
      if (!error.Success())
        return error;
      for (const auto &thread_sp : m_threads) {
-@@ -403,7 +438,15 @@ Error NativeProcessNetBSD::Resume(const 
+@@ -403,7 +423,15 @@ Error NativeProcessNetBSD::Resume(const 
      break;
    }
    case eStateStepping:
@@ -75,7 +60,31 @@ $NetBSD$
      break;
  
    case eStateSuspended:
-@@ -850,9 +893,6 @@ NativeThreadNetBSDSP NativeProcessNetBSD
+@@ -732,22 +760,11 @@ Error NativeProcessNetBSD::LaunchInferio
+ 
+   ResolveProcessArchitecture(m_pid, m_arch);
+ 
+-  /* Initialize threads */
+-  struct ptrace_lwpinfo info = {};
+-  error = PtraceWrapper(PT_LWPINFO, pid, &info, sizeof(info));
++  error = ReinitializeThreads();
+   if (error.Fail()) {
+     SetState(StateType::eStateInvalid);
+     return error;
+   }
+-  while (info.pl_lwpid != 0) {
+-    NativeThreadNetBSDSP thread_sp = AddThread(info.pl_lwpid);
+-    thread_sp->SetStoppedBySignal(SIGSTOP);
+-    error = PtraceWrapper(PT_LWPINFO, pid, &info, sizeof(info));
+-    if (error.Fail()) {
+-      SetState(StateType::eStateInvalid);
+-      return error;
+-    }
+-  }
+ 
+   /* Set process stopped */
+   SetState(StateType::eStateStopped);
+@@ -850,9 +867,6 @@ NativeThreadNetBSDSP NativeProcessNetBSD
  ::pid_t NativeProcessNetBSD::Attach(lldb::pid_t pid, Error &error) {
    Log *log(GetLogIfAllCategoriesSet(LIBLLDB_LOG_PROCESS));
  
@@ -85,3 +94,53 @@ $NetBSD$
    if (pid <= 1) {
      error.SetErrorToGenericError();
      error.SetErrorString("Attaching to process 1 is not allowed.");
+@@ -874,21 +888,11 @@ NativeThreadNetBSDSP NativeProcessNetBSD
+   m_pid = pid;
+ 
+   /* Initialize threads */
+-  struct ptrace_lwpinfo info = {};
+-  error = PtraceWrapper(PT_LWPINFO, pid, &info, sizeof(info));
++  error = ReinitializeThreads();
+   if (error.Fail()) {
+     SetState(StateType::eStateInvalid);
+     return -1;
+   }
+-  while (info.pl_lwpid != 0) {
+-    NativeThreadNetBSDSP thread_sp = AddThread(info.pl_lwpid);
+-    thread_sp->SetStoppedBySignal(SIGSTOP);
+-    error = PtraceWrapper(PT_LWPINFO, pid, &info, sizeof(info));
+-    if (error.Fail()) {
+-      SetState(StateType::eStateInvalid);
+-      return -1;
+-    }
+-  }
+ 
+   // Let our process instance know the thread has stopped.
+   SetState(StateType::eStateStopped);
+@@ -989,3 +993,26 @@ NativeProcessNetBSD::GetAuxvData() const
+ 
+   return buf;
+ }
++
++Error NativeProcessNetBSD::ReinitializeThreads() {
++  // Clear old threads
++  m_threads.clear();
++
++  // Initialize new thread
++  struct ptrace_lwpinfo info = {};
++  Error error = PtraceWrapper(PT_LWPINFO, GetID(), &info, sizeof(info));
++  if (error.Fail()) {
++    return error;
++  }
++  // Reinitialize from scratch threads and register them in process
++  while (info.pl_lwpid != 0) {
++    NativeThreadNetBSDSP thread_sp = AddThread(info.pl_lwpid);
++    thread_sp->SetStoppedByExec();
++    error = PtraceWrapper(PT_LWPINFO, GetID(), &info, sizeof(info));
++    if (error.Fail()) {
++      return error;
++    }
++  }
++
++  return error;
++}
