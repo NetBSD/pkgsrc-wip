3e9d6b197192baf8e7014e69ed17fff62d168243
Jason Bacon <bacon@centosdev.local>
2017-03-30 11:57:02 -0500

    'Add py-isodate: 	ISO 8601 date/time/duration parser and formater'

---
+++ py-isodate/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	py27-isodate
+
+.if !defined(PY27_ISODATE_BUILDLINK3_MK)
+PY27_ISODATE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.py27-isodate+=	py27-isodate>=0.5.0
+BUILDLINK_PKGSRCDIR.py27-isodate?=	../../wip/py-isodate
+.endif	# PY27_ISODATE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-py27-isodate
