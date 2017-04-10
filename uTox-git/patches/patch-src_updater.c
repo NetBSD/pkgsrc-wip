070cfc52eea5fd8d0ba08a19e3cbd37fa58ce091
Mateusz Poszwa <old4@o2.pl>
2017-04-10 18:35:35 +0200

    uTox-git: Update patches

--- uTox-git/patches/patch-src_updater.c
+++ uTox-git/patches/patch-src_updater.c
@@ -2,13 +2,13 @@ $NetBSD$
 
 Include netinet/in.h for IPPROTO_TCP
 
---- src/updater.c.orig	2017-01-24 20:49:40.564174586 +0000
+--- src/updater.c.orig	2017-04-10 16:23:09.000000000 +0000
 +++ src/updater.c
-@@ -20,6 +20,7 @@
+@@ -25,6 +25,7 @@
  #include <sys/socket.h>
  #include <sys/types.h>
  #include <netdb.h>
 +#include <netinet/in.h>
  #endif
  
- 
+ #include "native/main.h" // Include after winsock2
