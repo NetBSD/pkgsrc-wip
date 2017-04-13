5368c997852b70130562c0fb1f990df855baadac
Benny Siegert <bsiegert@gmail.com>
2017-04-13 19:27:06 +0200

    gopkgdep: Try following the repos that "go get" downloads.
    There is a new section in the output of gopkgdep that lists all repos that
    "go get" needed to, well, get for this package. Perhaps that's exactly the
    signal we need.

--- gopkgdep/files/gopkgdep.sh
+++ gopkgdep/files/gopkgdep.sh
@@ -12,9 +12,13 @@ fi
 
 GOPATH=$(mktemp -d -t ${ME}) || exit 2
 export GOPATH
+OUT_TMPFILE=$(mktemp -t ${ME}-out) || exit 2
 
 echo "=> Downloading code for $1"
-go get "$1/..."
+go get -v "$1/..." 2> ${OUT_TMPFILE}
+
+echo "=> Repositories downloaded"
+awk '/\(download\)/ { print $1 }' ${OUT_TMPFILE}
 
 echo "=> Recursive dependencies (excluding stdlib)"
 go list -f '{{range .Deps}}{{.}}
