3131140b3912a3e8b591658290aae489aa628d9c
Leonardo Taccari <leot@NetBSD.org>
2017-04-14 12:53:11 +0200

    py-editorconfig-core: Update wip/py-editorconfig-core to 0.12.1
    pkgsrc changes:
     - Convert MASTER_SITES to MASTER_SITE_PYPI and adjust {DIST,PKG,EGG_}NAME
       accordingly
     - Empty the USE_LANGUAGES list (`c' is not needed)
     - Delete unused buildlink3.mk
    
    Changes:
    No changelog available unfortunately.

--- py-editorconfig-core/patches/patch-setup.py
+++ py-editorconfig-core/patches/patch-setup.py
@@ -1,8 +1,10 @@
 $NetBSD: patch-setup.py,v 1.2 2014/11/19 20:12:30 nros Exp $
+
 * remove non-versioned file. The console command for editorconfig-core 
   is in the editorconfig-core package. Removing this file removes the 
   conflict that this package would have with the editorconfig-core 
   package.
+
 --- setup.py.orig	2014-11-19 19:02:21.000000000 +0000
 +++ setup.py
 @@ -10,11 +10,6 @@ setup(
