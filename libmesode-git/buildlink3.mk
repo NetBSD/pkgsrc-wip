0c338508a8421b6467d2234b7ad7528e73ad98c3
Jonathan Schleifer <js@heap.zone>
2017-04-14 04:05:46 +0200

    libmesode: Add expat to buildlink
    This unbreaks profanity

--- libmesode-git/buildlink3.mk
+++ libmesode-git/buildlink3.mk
@@ -8,8 +8,9 @@ LIBMESODE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libmesode+=	libmesode>=0.9.1
 BUILDLINK_PKGSRCDIR.libmesode?=		../../wip/libmesode-git
 
-.include "../../security/openssl/buildlink3.mk"
+.include "../../textproc/expat/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
 .endif	# LIBMESODE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libmesode
