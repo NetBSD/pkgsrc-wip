88f909f047dfd0fec05b8d1a737358640f6fb556
Thomas Klausner <wiz@NetBSD.org>
2017-04-17 01:16:04 +0200

    litehtml: add new package
    litehtml is the lightweight HTML rendering engine with CSS2/CSS3
    support. Note, litehtml itself does not draw any text, pictures or
    other graphics and litehtml does not depend of any image/draw/font
    library. You are free to use any library to draw images, fonts and
    any other graphics. litehtml just parses HTML/CSS and places the
    HTML elements into right position (renders HTML). To draw the html
    elemens you have to implement the simple callback interface
    document_container.
    
    Untested, since no user in pkgsrc yet, but gnucash might use this
    in the future.

---
+++ litehtml/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	litehtml
+
+.if !defined(LITEHTML_BUILDLINK3_MK)
+LITEHTML_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.litehtml+=	litehtml>=0.4.0.20170407
+BUILDLINK_PKGSRCDIR.litehtml?=	../../wip/litehtml
+BUILDLINK_DEPMETHOD.litehtml?=	build
+.endif	# LITEHTML_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-litehtml
