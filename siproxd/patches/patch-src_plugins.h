848862022f968b399b0d4efde56c98a2390cda95
Georg Schwarz <schwarz@NetBSD.org>
2017-04-28 17:00:03 +0200

    wip/siproxd: upgraded to 0.8.2
    Siproxd 0.8.2 is a maintenance release which fixes some bugs

--- siproxd/patches/patch-src_plugins.h
+++ siproxd/patches/patch-src_plugins.h
@@ -1,13 +1,13 @@
-$NetBSD: patch-src_plugins.h,v 1.2 2013/12/28 14:42:29 imilh Exp $
+$NetBSD$
 
 using libtool 2.x
 http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/net-misc/siproxd/files/siproxd-libtool-2.4.patch?view=markup
 
---- src/plugins.h.orig	2011-05-28 15:47:15.000000000 +0000
-+++ src/plugins.h
-@@ -28,6 +28,12 @@
-    #include <ltdl.h>
- #endif
+--- src/plugins.h.orig	2015-10-15 15:29:20.000000000 +0200
++++ src/plugins.h	2017-04-03 22:25:39.000000000 +0200
+@@ -41,6 +41,12 @@
+ 
+ /* Plugins must return STS_SUCCESS / SUCCESS_FAILURE */
  
 +#ifndef lt__PROGRAM__LTX_preloaded_symbols
 +#define lt__PROGRAM__LTX_preloaded_symbols lt_libltdl_LTX_preloaded_symbols
@@ -16,5 +16,5 @@ http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/net-misc/siproxd/files/s
 +extern LT_DLSYM_CONST lt_dlsymlist lt__PROGRAM__LTX_preloaded_symbols[];
 +#endif
  
- /* Plugins must return STS_SUCCESS / SUCCESS_FAILURE */
- 
+ /*
+  * Processing stages for Plugins
