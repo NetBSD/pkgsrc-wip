9ff7d17cbc71c5bbec0fa043bbb5743abade6fc4
Kamil Rytarowski <n54@gmx.com>
2017-05-13 01:39:52 +0200

    qemu-git: Add draft patches to make it build
    This delta is scratch in preparation for cleanup.
    
    PLIST has been overwritten.

---
+++ qemu-git/patches/patch-disas_libvixl_vixl_a64_disasm-a64.cc
@@ -0,0 +1,14 @@
+$NetBSD$
+
+--- disas/libvixl/vixl/a64/disasm-a64.cc.orig	2017-04-25 12:42:53.000000000 +0000
++++ disas/libvixl/vixl/a64/disasm-a64.cc
+@@ -24,8 +24,8 @@
+ // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+-#include <cstdlib>
+ #include "vixl/a64/disasm-a64.h"
++#include <cstdlib>
+ 
+ namespace vixl {
+ 
