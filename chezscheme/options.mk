cd75c47c50d8309d25169eb953ec5eb24e50a450
Aleksej Lebedev <root@zta.lk>
2017-05-17 01:07:25 +0200

    chezscheme: added options.mk to support configure with --threads.

---
+++ chezscheme/options.mk
@@ -0,0 +1,11 @@
+PKG_OPTIONS_VAR=	PKG_OPTIONS.chezscheme
+PKG_SUPPORTED_OPTIONS=	threads
+PKG_SUGGESTED_OPTIONS=	threads
+
+.include "../../mk/bsd.prefs.mk"
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mthreads)
+CONFIGURE_ARGS+=--threads
+T=t
+.endif
