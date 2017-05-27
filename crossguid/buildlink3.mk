fba59b04426d0a060c733a1d8220ab955f2d73d4
coypu <coypu@sdf.org>
2017-05-27 21:09:20 +0300

    Add crossguid.
    CrossGuid is a minimal, cross platform, C++ GUID library.
    
    It uses the best native GUID/UUID generator on the given
    platform and had a generic class for parsing, stringifying,
    and comparing IDs.

---
+++ crossguid/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+BUILDLINK_TREE+=	crossguid
+
+.if !defined(CROSSGUID_BUILDLINK3_MK)
+CROSSGUID_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.crossguid+=	crossguid>=0.0_pre20160908
+BUILDLINK_PKGSRCDIR.crossguid?=	../../devel/crossguid
+
+.include "../../devel/libuuid/buildlink3.mk"
+.endif	# CROSSGUID_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-crossguid
