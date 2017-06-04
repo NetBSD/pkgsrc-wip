5c4894074359e86ef79f78b85d3728dbc17b7fb6
Kamil Rytarowski <n54@gmx.com>
2017-06-04 03:12:19 +0200

    compiler-rt-netbsd: Initial scratch for sanitizers/NetBSD
    Sponsored by <The NetBSD Foundation>

---
+++ compiler-rt-netbsd/patches/patch-cmake_config-ix.cmake
@@ -0,0 +1,31 @@
+$NetBSD$
+
+--- cmake/config-ix.cmake.orig	2017-06-03 23:53:56.000000000 +0000
++++ cmake/config-ix.cmake
+@@ -441,7 +441,7 @@ set(COMPILER_RT_SANITIZERS_TO_BUILD ${AL
+ list_replace(COMPILER_RT_SANITIZERS_TO_BUILD all "${ALL_SANITIZERS}")
+ 
+ if (SANITIZER_COMMON_SUPPORTED_ARCH AND NOT LLVM_USE_SANITIZER AND
+-    (OS_NAME MATCHES "Android|Darwin|Linux|FreeBSD" OR
++    (OS_NAME MATCHES "Android|Darwin|Linux|FreeBSD|NetBSD" OR
+     (OS_NAME MATCHES "Windows" AND (NOT MINGW AND NOT CYGWIN))))
+   set(COMPILER_RT_HAS_SANITIZER_COMMON TRUE)
+ else()
+@@ -460,7 +460,7 @@ else()
+   set(COMPILER_RT_HAS_ASAN FALSE)
+ endif()
+ 
+-if (OS_NAME MATCHES "Linux|FreeBSD|Windows")
++if (OS_NAME MATCHES "Linux|FreeBSD|NetBSD|Windows")
+   set(COMPILER_RT_ASAN_HAS_STATIC_RUNTIME TRUE)
+ else()
+   set(COMPILER_RT_ASAN_HAS_STATIC_RUNTIME FALSE)
+@@ -504,7 +504,7 @@ else()
+ endif()
+ 
+ if (COMPILER_RT_HAS_SANITIZER_COMMON AND UBSAN_SUPPORTED_ARCH AND
+-    OS_NAME MATCHES "Darwin|Linux|FreeBSD|Windows|Android")
++    OS_NAME MATCHES "Darwin|Linux|FreeBSD|NetBSD|Windows|Android")
+   set(COMPILER_RT_HAS_UBSAN TRUE)
+ else()
+   set(COMPILER_RT_HAS_UBSAN FALSE)
