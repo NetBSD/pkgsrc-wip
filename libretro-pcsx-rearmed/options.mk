cbd0cfd70f94cc505107af9f27db6a0ac02d54dc
Nia Alarie <nia.alarie@gmail.com>
2017-06-06 00:38:30 +0100

    libretro-pcsx-rearmed: Make this work on my Banana Pi.
    This seemed to assume that the configuration script got executed
    during the build process (it didn't) and that libpng gets linked in
    (it doesn't).
    
    With the dynarec, Ridge Racer NTSC-J works but Spyro 2 PAL doesn't.
    Pending further investigation.

--- libretro-pcsx-rearmed/options.mk
+++ libretro-pcsx-rearmed/options.mk
@@ -9,10 +9,19 @@ PKG_SUPPORTED_OPTIONS+=	dynarec
 PKG_SUGGESTED_OPTIONS+=	dynarec
 .endif
 
+.if !empty(MACHINE_ARCH:M*armv7*)
+PKG_SUPPORTED_OPTIONS+=	simd
+.endif
+
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mdynarec)
-# enabled by default
-.else
-CONFIGURE_ARGS+=	--disable-dynarec
+BUILD_MAKE_FLAGS+=	DRC_CACHE_BASE=0
+BUILD_MAKE_FLAGS+=	USE_DYNAREC=1
+.endif
+
+.if !empty(PKG_OPTIONS:Msimd)
+CFLAGS+=		-mfpu=neon # needed for assembly code to work
+BUILD_MAKE_FLAGS+=	HAVE_NEON=1
+BUILD_MAKE_FLAGS+=	BUILTIN_GPU=neon
 .endif
