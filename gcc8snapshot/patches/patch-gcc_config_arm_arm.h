a7bedfd4cde6bf21f305e9bbf10303571b9d8060
coypu <coypu@sdf.org>
2017-06-22 15:28:21 +0300

    gcc8snapshot: Update to the 20170618 snapshot

--- gcc8snapshot/patches/patch-gcc_config_arm_arm.h
+++ gcc8snapshot/patches/patch-gcc_config_arm_arm.h
@@ -7,14 +7,14 @@ Without it, we get an undefined reference.
 
 GCC bugzilla #77800
 
---- gcc/config/arm/arm.h.orig	2016-12-15 17:51:46.000000000 +0000
+--- gcc/config/arm/arm.h.orig	2017-06-16 21:04:23.000000000 +0000
 +++ gcc/config/arm/arm.h
-@@ -2231,7 +2231,7 @@ extern const char *arm_target_thumb_only
+@@ -2245,7 +2245,7 @@ extern const char *arm_target_thumb_only
  
  /* -mcpu=native handling only makes sense with compiler running on
     an ARM chip.  */
 -#if defined(__arm__)
-+#if defined(__arm__) && (defined(__linux__) || defined(__FreeBSD__))
++#if defined(__arm__) && (defined(__FreeBSD__) || defined(__linux__))
  extern const char *host_detect_local_cpu (int argc, const char **argv);
- # define EXTRA_SPEC_FUNCTIONS						\
-   { "local_cpu_detect", host_detect_local_cpu },			\
+ # define MCPU_MTUNE_NATIVE_FUNCTIONS			\
+   { "local_cpu_detect", host_detect_local_cpu },
