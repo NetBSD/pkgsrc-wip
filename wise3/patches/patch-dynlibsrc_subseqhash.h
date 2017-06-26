3b86acd486bdb4021512153ce9ddf322090a8a42
zmsaif <zmsaif@nmsu.edu>
2017-06-26 12:28:18 -0600

    new package

---
+++ wise3/patches/patch-dynlibsrc_subseqhash.h
@@ -0,0 +1,15 @@
+$NetBSD$
+
+--- dynlibsrc/subseqhash.h.orig	2014-06-19 15:10:23.000000000 +0000
++++ dynlibsrc/subseqhash.h
+@@ -6,8 +6,8 @@ extern "C" {
+ #include "sequence.h"
+ #include "seqlookup.h"
+ #include "linkedlist_lookpos.h"
+-#include "glib.h"
+-
++#include "/home/zmsaif/pkg-2017Q1/include/glib/glib-1.2/glib.h"
++#include "/home/zmsaif/pkg-2017Q1/include/glib/glib-2.0/glib.h"
+ #include "wisebase.h"
+ 
+ 
