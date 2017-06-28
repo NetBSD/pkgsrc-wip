3e7c52b97b4d6cb8ea69a081409ac818c812c34a
Kamil Rytarowski <n54@gmx.com>
2017-06-28 19:08:37 +0200

    compiler-rt-netbsd: Handle sigaction syscall renaming
    With this patch, llvm asan with with clang works.
    
    $ cat test.c
    
    int
    main(int argc, char **argv)
    {
            printf("%s", argv[argc+10000]);
            return 0;
    }
    
    $ /usr/pkg/bin/clang -fno-omit-frame-pointer -O0 -g -ggdb -U_FORTIFY_SOURCE -fsanitize=address -v test.c -o bin-czz
    
    $  ./bin-czz
    ASAN:DEADLYSIGNAL
    =================================================================
    ==13478==ERROR: AddressSanitizer: SEGV on unknown address 0x1fefffed72f6
    (pc 0x00000047fcb1 bp 0x7f7fff6a5ef0 sp 0x7f7fff6a5ed0 T0)
    ==13478==The signal is caused by a READ memory access.
        #0 0x47fcb0 in main /tmp/test.c:6:15
        #1 0x409dfa in ___start (/tmp/bin-czz+0x409dfa)
    
    AddressSanitizer can not provide additional info.
    SUMMARY: AddressSanitizer: SEGV /tmp/test.c:6:15 in main
    ==13478==ABORTING
    
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-lib_interception_interception__linux.cc
+++ compiler-rt-netbsd/patches/patch-lib_interception_interception__linux.cc
@@ -2,7 +2,7 @@ $NetBSD$
 
 --- lib/interception/interception_linux.cc.orig	2017-06-03 23:53:56.000000000 +0000
 +++ lib/interception/interception_linux.cc
-@@ -12,7 +12,7 @@
+@@ -12,14 +12,28 @@
  // Linux-specific interception methods.
  //===----------------------------------------------------------------------===//
  
@@ -11,7 +11,28 @@ $NetBSD$
  #include "interception.h"
  
  #include <dlfcn.h>   // for dlsym() and dlvsym()
-@@ -33,4 +33,4 @@ void *GetFuncAddrVer(const char *func_na
+ 
++#ifdef __NetBSD__
++static int mystrcmp(const char *s1, const char *s2) {
++  while (*s1 == *s2++)
++    if (*s1++ == 0)
++      return (0);
++  return (*(const unsigned char *)s1 - *(const unsigned char *)--s2);
++}
++#endif
++
+ namespace __interception {
+ bool GetRealFunctionAddress(const char *func_name, uptr *func_addr,
+     uptr real, uptr wrapper) {
++#ifdef __NetBSD__
++  // XXX: Until I come up with something better to deal with renames.
++  if (mystrcmp(func_name, "sigaction") == 0)
++    func_name = "__sigaction14";
++#endif
+   *func_addr = (uptr)dlsym(RTLD_NEXT, func_name);
+   return real == wrapper;
+ }
+@@ -33,4 +47,4 @@ void *GetFuncAddrVer(const char *func_na
  }  // namespace __interception
  
  
