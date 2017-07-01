a8ba06b819c5c2544fc00729453b97d1f396bede
Benny Siegert <bsiegert@gmail.com>
2017-07-01 11:07:50 +0000

    Bump required rakudo version to 2017.06.
    There have been significant changes, so dependent packages need to
    be rebuilt.

--- rakudo/buildlink3.mk
+++ rakudo/buildlink3.mk
@@ -5,11 +5,11 @@ BUILDLINK_TREE+=	rakudo
 .if !defined(RAKUDO_BUILDLINK3_MK)
 RAKUDO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rakudo+=		rakudo>=2015.12
-BUILDLINK_ABI_DEPENDS.rakudo+=		rakudo>=2015.12
+BUILDLINK_API_DEPENDS.rakudo+=		rakudo>=2017.06
+BUILDLINK_ABI_DEPENDS.rakudo+=		rakudo>=2017.06
 BUILDLINK_PKGSRCDIR.rakudo?=		../../wip/rakudo
 
-.include "../../lang/nqp/buildlink3.mk"
+.include "../../wip/nqp/buildlink3.mk"
 .endif # RAKUDO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-rakudo
