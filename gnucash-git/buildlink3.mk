611ecb3dc1eae58bab37d44375cdfa33e25fa09b
Thomas Klausner <wiz@NetBSD.org>
2017-07-29 00:11:50 +0200

    gnucash-git: add incomplete gnucash-git package

---
+++ gnucash-git/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.17 2012/09/15 10:04:36 obache Exp $
+
+BUILDLINK_TREE+=	gnucash
+
+.if !defined(GNUCASH_BUILDLINK3_MK)
+GNUCASH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gnucash+=	gnucash>=2.1.0
+BUILDLINK_ABI_DEPENDS.gnucash+=	gnucash>=2.4.11nb3
+BUILDLINK_PKGSRCDIR.gnucash?=	../../finance/gnucash
+
+.include "../../devel/glib2/buildlink3.mk"
+.endif # GNUCASH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gnucash
