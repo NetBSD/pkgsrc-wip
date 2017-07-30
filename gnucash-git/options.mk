e2cf9a18268d24eada4790912788f3e3adee85e4
Thomas Klausner <wiz@NetBSD.org>
2017-07-31 00:32:43 +0200

    gnucash-git: make python dependency optional, and disable it by default.
    With python enabled, gnucash dumps core on startup with a >4000 frames
    backtrace. (8.99.1/amd64)

--- gnucash-git/options.mk
+++ gnucash-git/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.3 2016/08/30 04:13:13 jnemeth Exp $
+# $NetBSD$
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnucash
-PKG_SUPPORTED_OPTIONS=	libdbi libofx
+PKG_SUPPORTED_OPTIONS=	libdbi libofx python
 PKG_SUGGESTED_OPTIONS=	libdbi libofx
 
 .include "../../mk/bsd.options.mk"
@@ -18,7 +18,27 @@ CONFIGURE_ARGS+=	--disable-ofx
 .if !empty(PKG_OPTIONS:Mlibdbi)
 CONFIGURE_ARGS+=	--enable-dbi
 PLIST.dbi=	yes
+
+# to make a configure test happy
+BUILD_DEPENDS+=		libdbi-driver-sqlite3-[0-9]*:../../databases/libdbi-driver-sqlite3
 .include "../../databases/libdbi/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-dbi
+.endif
+
+.if !empty(PKG_OPTIONS:Mpython)
+CONFIGURE_ARGS+=	--enable-python
+PYTHON_VERSIONS_INCOMPATIBLE=	34 35 36 # not yet ported as of 2.6.1
+REPLACE_PYTHON+=	src/python/pycons/*.py src/python/pycons/pycons
+PY_PATCHPLIST=		yes
+PLIST_SRC+=		PLIST.python
+
+DEPENDS+=		${PYPKGPREFIX}-gtk2-[0-9]*:../../x11/py-gtk2
+# /usr/pkg/share/gnucash/python/pycons/console.py imports readline
+DEPENDS+=		${PYPKGPREFIX}-readline-[0-9]*:../../devel/py-readline
+
+.include "../../lang/python/application.mk"
+.include "../../lang/python/extension.mk"
+.else
+CONFIGURE_ARGS+=	--disable-python
 .endif
