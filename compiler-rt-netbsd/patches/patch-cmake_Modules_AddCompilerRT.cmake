3319e9be14560e60e68105b0f62eee515b12e526
Kamil Rytarowski <n54@gmx.com>
2017-08-22 01:27:15 +0200

    compiler-rt-netbsd: Update to SVN r. 311407
    This code has incorporated fuzzer code (without NetBSD so far).
    NetBSD support will be reintroduced later.
    
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-cmake_Modules_AddCompilerRT.cmake
+++ compiler-rt-netbsd/patches/patch-cmake_Modules_AddCompilerRT.cmake
@@ -1,26 +1,2 @@
 $NetBSD$
 
---- cmake/Modules/AddCompilerRT.cmake.orig	2017-08-03 21:24:37.000000000 +0000
-+++ cmake/Modules/AddCompilerRT.cmake
-@@ -202,10 +202,10 @@ function(add_compiler_rt_runtime name ty
-     set_target_properties(${libname} PROPERTIES
-         OUTPUT_NAME ${output_name_${libname}})
-     set_target_properties(${libname} PROPERTIES FOLDER "Compiler-RT Runtime")
-+    if(LIB_LINK_LIBS)
-+      target_link_libraries(${libname} ${LIB_LINK_LIBS})
-+    endif()
-     if(${type} STREQUAL "SHARED")
--      if(LIB_LINK_LIBS)
--        target_link_libraries(${libname} ${LIB_LINK_LIBS})
--      endif()
-       if(WIN32 AND NOT CYGWIN AND NOT MINGW)
-         set_target_properties(${libname} PROPERTIES IMPORT_PREFIX "")
-         set_target_properties(${libname} PROPERTIES IMPORT_SUFFIX ".lib")
-@@ -320,6 +320,7 @@ macro(add_compiler_rt_test test_suite te
-     set(TEST_LINK_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${TEST_LINK_FLAGS}")
-     separate_arguments(TEST_LINK_FLAGS)
-   endif()
-+
-   add_custom_target(${test_name}
-     COMMAND ${COMPILER_RT_TEST_COMPILER} ${TEST_OBJECTS}
-             -o "${output_bin}"
--- compiler-rt-netbsd/patches/patch-lib_fuzzer_README.txt
+++
@@ -1,7 +0,0 @@
-$NetBSD$
-
---- lib/fuzzer/README.txt.orig	2017-08-19 18:02:19.281259472 +0000
-+++ lib/fuzzer/README.txt
-@@ -0,0 +1,2 @@
-+Move to http://llvm.org/docs/LibFuzzer.html
-+
