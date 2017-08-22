7769866f45ab45bace02f1b82cb574a5945ffeca
Kamil Rytarowski <n54@gmx.com>
2017-08-22 02:17:08 +0200

    compiler-rt-netbsd: Restore libfuzzer/NetBSD
    Part 4
    
    Restore lsan, profile and tsan options.
    
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-cmake_config-ix.cmake
+++ compiler-rt-netbsd/patches/patch-cmake_config-ix.cmake
@@ -2,6 +2,32 @@ $NetBSD$
 
 --- cmake/config-ix.cmake.orig	2017-08-21 23:23:37.000000000 +0000
 +++ cmake/config-ix.cmake
+@@ -511,7 +511,7 @@ else()
+ endif()
+ 
+ if (COMPILER_RT_HAS_SANITIZER_COMMON AND LSAN_SUPPORTED_ARCH AND
+-    OS_NAME MATCHES "Darwin|Linux|FreeBSD")
++    OS_NAME MATCHES "Darwin|Linux|FreeBSD|NetBSD")
+   set(COMPILER_RT_HAS_LSAN TRUE)
+ else()
+   set(COMPILER_RT_HAS_LSAN FALSE)
+@@ -525,14 +525,14 @@ else()
+ endif()
+ 
+ if (PROFILE_SUPPORTED_ARCH AND NOT LLVM_USE_SANITIZER AND
+-    OS_NAME MATCHES "Darwin|Linux|FreeBSD|Windows|Android")
++    OS_NAME MATCHES "Darwin|Linux|FreeBSD|Windows|Android|NetBSD")
+   set(COMPILER_RT_HAS_PROFILE TRUE)
+ else()
+   set(COMPILER_RT_HAS_PROFILE FALSE)
+ endif()
+ 
+ if (COMPILER_RT_HAS_SANITIZER_COMMON AND TSAN_SUPPORTED_ARCH AND
+-    OS_NAME MATCHES "Darwin|Linux|FreeBSD|Android")
++    OS_NAME MATCHES "Darwin|Linux|FreeBSD|Android|NetBSD")
+   set(COMPILER_RT_HAS_TSAN TRUE)
+ else()
+   set(COMPILER_RT_HAS_TSAN FALSE)
 @@ -581,7 +581,7 @@ else()
  endif()
  
