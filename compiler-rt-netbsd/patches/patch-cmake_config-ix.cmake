c0ba54f3e89f7d92c48da47d34d2a2fcbcfd57a6
Kamil Rytarowski <n54@gmx.com>
2017-08-22 02:00:47 +0200

    compiler-rt-netbsd: Restore libfuzzer/NetBSD
    Part 2.
    
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-cmake_config-ix.cmake
+++ compiler-rt-netbsd/patches/patch-cmake_config-ix.cmake
@@ -2,15 +2,6 @@ $NetBSD$
 
 --- cmake/config-ix.cmake.orig	2017-08-21 23:23:37.000000000 +0000
 +++ cmake/config-ix.cmake
-@@ -532,7 +532,7 @@ else()
- endif()
- 
- if (COMPILER_RT_HAS_SANITIZER_COMMON AND TSAN_SUPPORTED_ARCH AND
--    OS_NAME MATCHES "Darwin|Linux|FreeBSD|Android")
-+    OS_NAME MATCHES "Darwin|Linux|FreeBSD|Android|NetBSD")
-   set(COMPILER_RT_HAS_TSAN TRUE)
- else()
-   set(COMPILER_RT_HAS_TSAN FALSE)
 @@ -581,7 +581,7 @@ else()
  endif()
  
