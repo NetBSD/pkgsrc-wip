b1cf0d03975dfb1f2071c817103936092c02d14d
Yorick Hardy <yorickhardy@gmail.com>
2017-08-27 22:22:43 +0200

    luakit-git: update PLIST and patches

--- luakit-git/patches/patch-common_luautil.c
+++ luakit-git/patches/patch-common_luautil.c
@@ -2,15 +2,15 @@ $NetBSD$
 
 Also search in the pkgsrc xdg path.
 
---- common/luautil.c.orig	2017-06-05 19:43:00.000000000 +0000
+--- common/luautil.c.orig	2017-08-27 19:41:20.547896796 +0000
 +++ common/luautil.c
-@@ -169,6 +169,9 @@ luaH_add_paths(lua_State *L, const gchar
+@@ -161,6 +161,9 @@ luaH_add_paths(lua_State *L, const gchar
      /* add luakit install path */
      g_ptr_array_add(paths, g_build_filename(LUAKIT_INSTALL_PATH, "lib", NULL));
  
 +    /* add luakit xdg path */
 +    g_ptr_array_add(paths, g_build_filename(@PKGSRCXDGPATH@, "luakit", NULL));
 +
-     const gchar *path;
-     for (guint i = 0; i < paths->len; i++) {
-         path = paths->pdata[i];
+     /* add users config dir (see: XDG_CONFIG_DIR) */
+     if (config_dir)
+         g_ptr_array_add(paths, g_strdup(config_dir));
