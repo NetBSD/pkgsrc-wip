67d57ec080ef30dacf06fd58cb9bc1f2034e723d
Alexander Nasonov <alnsn@yandex.ru>
2017-08-27 16:20:57 +0100

    Add buildlink3.mk.

---
+++ go-flags/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	go-flags
+
+.if !defined(GO_FLAGS_BUILDLINK3_MK)
+GO_FLAGS_BUILDLINK3_MK:=
+
+BUILDLINK_CONTENTS_FILTER.go-flags=	${EGREP} gopkg/
+BUILDLINK_DEPMETHOD.go-flags?=		build
+
+BUILDLINK_API_DEPENDS.go-flags+=	go-flags>=1.3.0
+BUILDLINK_PKGSRCDIR.go-flags?=	../../wip/go-flags
+.endif	# GO_FLAGS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-go-flags
