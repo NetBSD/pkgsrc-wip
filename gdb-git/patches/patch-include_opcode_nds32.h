f2a86dc301a9688a5624da69042af6934814dacd
Kamil Rytarowski <n54@gmx.com>
2017-09-06 01:38:13 +0200

    gdb-git: Fix the GDB/NetBSD build with --enable-targets=all

---
+++ gdb-git/patches/patch-include_opcode_nds32.h
@@ -0,0 +1,15 @@
+$NetBSD$
+
+--- include/opcode/nds32.h.orig	2017-09-04 13:40:58.000000000 +0000
++++ include/opcode/nds32.h
+@@ -50,8 +50,8 @@ static const int nds32_r54map[] ATTRIBUT
+   -1, -1, -1, -1, -1, -1, -1, -1
+ };
+ 
+-#define __BIT(n)		(1 << (n))
+-#define __MASK(n)		(__BIT (n) - 1)
++#define __ONEBIT(n)		(1 << (n))
++#define __MASK(n)		(__ONEBIT (n) - 1)
+ #define __MF(v, off, bs)	(((v) & __MASK (bs)) << (off))
+ #define __GF(v, off, bs)	(((v) >> off) & __MASK (bs))
+ #define __SEXT(v, bs)		((((v) & ((1 << (bs)) - 1)) ^ (1 << ((bs) - 1))) - (1 << ((bs) - 1)))
---
+++ gdb-git/patches/patch-opcodes_nds32-dis.c
@@ -0,0 +1,18 @@
+$NetBSD$
+
+--- opcodes/nds32-dis.c.orig	2017-09-04 13:40:58.000000000 +0000
++++ opcodes/nds32-dis.c
+@@ -759,10 +759,10 @@ nds32_mask_opcode (uint32_t insn)
+       return MASK_OP (insn, 0);
+     case N32_OP6_ALU2:
+       /* FFBI */
+-      if (__GF (insn, 0, 7) == (N32_ALU2_FFBI | __BIT (6)))
++      if (__GF (insn, 0, 7) == (N32_ALU2_FFBI | __ONEBIT (6)))
+ 	return MASK_OP (insn, 0x7f);
+-      else if (__GF (insn, 0, 7) == (N32_ALU2_MFUSR | __BIT (6))
+-	       || __GF (insn, 0, 7) == (N32_ALU2_MTUSR | __BIT (6)))
++      else if (__GF (insn, 0, 7) == (N32_ALU2_MFUSR | __ONEBIT (6))
++	       || __GF (insn, 0, 7) == (N32_ALU2_MTUSR | __ONEBIT (6)))
+ 	/* RDOV CLROV */
+ 	return MASK_OP (insn, 0xf81ff);
+       return MASK_OP (insn, 0x1ff);
