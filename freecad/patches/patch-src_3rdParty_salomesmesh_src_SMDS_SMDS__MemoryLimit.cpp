e621c5e97b056324eef9ca5296fd99a49fbdf708
Kamil Rytarowski <n54@gmx.com>
2017-09-06 22:37:32 +0200

    freecad: fix build on NetBSD

--- freecad/patches/patch-src_3rdParty_salomesmesh_src_SMDS_SMDS__MemoryLimit.cpp
+++ freecad/patches/patch-src_3rdParty_salomesmesh_src_SMDS_SMDS__MemoryLimit.cpp
@@ -1,13 +1,13 @@
 $NetBSD$
 
---- src/3rdParty/salomesmesh/src/SMDS/SMDS_MemoryLimit.cpp.orig	2016-04-02 13:29:08.000000000 +0000
+--- src/3rdParty/salomesmesh/src/SMDS/SMDS_MemoryLimit.cpp.orig	2016-04-07 01:39:34.000000000 +0000
 +++ src/3rdParty/salomesmesh/src/SMDS/SMDS_MemoryLimit.cpp
 @@ -28,7 +28,7 @@
  // to the system. (PAL16631)
  //
  #ifndef WIN32
 -#if !(defined(__MACH__) && defined(__APPLE__))
-+#if !(defined(__MACH__) && defined(__APPLE__)) && !defined(__FreeBSD__)
++#if !(defined(__MACH__) && defined(__APPLE__)) && !defined(__FreeBSD__) && !defined(__NetBSD__)
  #include <sys/sysinfo.h>
  #endif
  #endif
@@ -16,7 +16,7 @@ $NetBSD$
  int main (int argc, char ** argv)
  {
 -#if (defined(__MACH__) && defined(__APPLE__))
-+#if (defined(__MACH__) && defined(__APPLE__)) || defined(__FreeBSD__)
++#if (defined(__MACH__) && defined(__APPLE__)) || defined(__FreeBSD__) || defined(__NetBSD__)
  	//do nothing
  #else
    // To better understand what is going on here, consult bug [SALOME platform 0019911]
--- freecad/patches/patch-src_3rdParty_salomesmesh_src_SMDS_SMDS__Mesh.cpp
+++ freecad/patches/patch-src_3rdParty_salomesmesh_src_SMDS_SMDS__Mesh.cpp
@@ -1,13 +1,13 @@
 $NetBSD$
 
---- src/3rdParty/salomesmesh/src/SMDS/SMDS_Mesh.cpp.orig	2016-04-02 13:29:08.000000000 +0000
+--- src/3rdParty/salomesmesh/src/SMDS/SMDS_Mesh.cpp.orig	2016-04-07 01:39:34.000000000 +0000
 +++ src/3rdParty/salomesmesh/src/SMDS/SMDS_Mesh.cpp
 @@ -43,7 +43,7 @@
  using namespace std;
  
  #ifndef WIN32
 -#if !(defined(__MACH__) && defined(__APPLE__))
-+#if !(defined(__MACH__) && defined(__APPLE__)) && !defined(__FreeBSD__)
++#if !(defined(__MACH__) && defined(__APPLE__)) && !defined(__FreeBSD__) && !defined(__NetBSD__)
  #include <sys/sysinfo.h>
  #endif
  #endif
@@ -16,7 +16,7 @@ $NetBSD$
  int SMDS_Mesh::CheckMemory(const bool doNotRaise) throw (std::bad_alloc)
  {
 -#if (defined(__MACH__) && defined(__APPLE__))
-+#if (defined(__MACH__) && defined(__APPLE__)) || defined(__FreeBSD__)
++#if (defined(__MACH__) && defined(__APPLE__)) || defined(__FreeBSD__) || defined(__NetBSD__)
  	return 1000;
  #else	
  #ifndef WIN32
