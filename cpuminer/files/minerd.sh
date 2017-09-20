f8a87b08c02b10fdfd71b77addff2a54bdf99412
Pierre Pronchery <khorben@defora.org>
2017-09-21 00:55:58 +0200

    Log to syslog instead of the standard error

--- cpuminer/files/minerd.sh
+++ cpuminer/files/minerd.sh
@@ -12,7 +12,7 @@ name="minerd"
 rcvar=$name
 command="@PREFIX@/bin/${name}"
 required_files="@PKG_SYSCONFDIR@/minerd.conf"
-minerd_flags="-B -c @PKG_SYSCONFDIR@/minerd.conf"
+minerd_flags="-B -S -c @PKG_SYSCONFDIR@/minerd.conf"
 
 load_rc_config $name
 run_rc_command "$1"
