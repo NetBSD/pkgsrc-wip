8f78685babd05bb128d43aa68f2a454ffbc7d494
Thomas Klausner <wiz@NetBSD.org>
2017-09-28 00:07:38 +0200

    evolution-rss: remove gecko option
    It tries using the non-existent xulrunner package.

--- evolution-rss/options.mk
+++ evolution-rss/options.mk
@@ -1,7 +1,7 @@
 # $NetBSD: options.mk,v 1.3 2014/08/03 22:16:38 thomasklausner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.evolution-rss
-PKG_SUPPORTED_OPTIONS=	webkit dbus gecko
+PKG_SUPPORTED_OPTIONS=	webkit dbus
 PKG_SUGGESTED_OPTIONS=	dbus
 
 .include "../../mk/bsd.options.mk"
@@ -13,12 +13,12 @@ PKG_SUGGESTED_OPTIONS=	dbus
 CONFIGURE_ARGS+=	--disable-dbus
 .endif
 
-.if !empty(PKG_OPTIONS:Mgecko)
-.   include "../../devel/xulrunner/buildlink3.mk"
-CONFIGURE_ARGS+=	--enable-gecko
-.else
-CONFIGURE_ARGS+=	--disable-gecko
-.endif
+#.if !empty(PKG_OPTIONS:Mgecko)
+#.   include "../../devel/xulrunner/buildlink3.mk"
+#CONFIGURE_ARGS+=	--enable-gecko
+#.else
+#CONFIGURE_ARGS+=	--disable-gecko
+#.endif
 
 .if !empty(PKG_OPTIONS:Mwebkit )
 .   include "../../www/webkit24-gtk/buildlink3.mk"
