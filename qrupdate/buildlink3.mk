0e9e2ba3ad42a83824d101a416f9e857f73ed9c6
Dr. Thomas Orgis <thomas.orgis@uni-hamburg.de>
2017-09-29 21:48:14 +0200

    qrupdate: add package

---
+++ qrupdate/buildlink3.mk
@@ -0,0 +1,11 @@
+# $NetBSD$
+BUILDLINK_TREE+=	qrupdate
+
+.if !defined(QRUPDATE_BUILDLINK3_MK)
+QRUPDATE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qrupdate+=	qrupdate>=1.1.2
+BUILDLINK_PKGSRCDIR.qrupdate?=	../../wip/qrupdate
+.endif	# QRUPDATE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qrupdate
