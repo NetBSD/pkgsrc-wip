54db6695fcef651d59b4e48134b3b38e29f93e4d
Kamil Rytarowski <n54@gmx.com>
2017-10-03 15:19:05 +0200

    compiler-rt-netbsd: tsan fixes for NetBSD
    Fix syscall(2)/__syscall(2) calls.
    Include sanitizer_platform_limits_netbsd.h unconditionally.
    Port GetArgsAndEnv() to NetBSD using "external ps_strings".
    Fix ReExec() on NetBSD.
    
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-lib_tsan_rtl_tsan__platform__linux.cc
+++ compiler-rt-netbsd/patches/patch-lib_tsan_rtl_tsan__platform__linux.cc
@@ -2,7 +2,7 @@ $NetBSD$
 
 --- lib/tsan/rtl/tsan_platform_linux.cc.orig	2017-07-16 22:40:10.000000000 +0000
 +++ lib/tsan/rtl/tsan_platform_linux.cc
-@@ -14,12 +14,16 @@
+@@ -14,11 +14,12 @@
  
  
  #include "sanitizer_common/sanitizer_platform.h"
@@ -12,15 +12,11 @@ $NetBSD$
  #include "sanitizer_common/sanitizer_common.h"
  #include "sanitizer_common/sanitizer_libc.h"
  #include "sanitizer_common/sanitizer_linux.h"
-+#if SANITIZER_NETBSD
 +#include "sanitizer_common/sanitizer_platform_limits_netbsd.h"
-+#else
  #include "sanitizer_common/sanitizer_platform_limits_posix.h"
-+#endif
  #include "sanitizer_common/sanitizer_posix.h"
  #include "sanitizer_common/sanitizer_procmaps.h"
- #include "sanitizer_common/sanitizer_stoptheworld.h"
-@@ -62,7 +66,7 @@
+@@ -62,7 +63,7 @@
  # undef sa_sigaction
  #endif
  
@@ -29,7 +25,7 @@ $NetBSD$
  extern "C" void *__libc_stack_end;
  void *__libc_stack_end = 0;
  #endif
-@@ -401,4 +405,4 @@ void cur_thread_finalize() {
+@@ -401,4 +402,4 @@ void cur_thread_finalize() {
  
  }  // namespace __tsan
  
