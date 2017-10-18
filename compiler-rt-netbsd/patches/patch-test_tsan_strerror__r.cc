076feff6aa327ac1b6ff4d8db493b1d6fa1b2a47
Kamil Rytarowski <n54@gmx.com>
2017-10-19 00:53:05 +0200

    compiler-rt-netbsd: Make a test portable to NetBSD
    Use strerror_r(3) in a more portable way.
    
    Sponsored by <The NetBSD Foundation>

---
+++ compiler-rt-netbsd/patches/patch-test_tsan_strerror__r.cc
@@ -0,0 +1,14 @@
+$NetBSD$
+
+--- test/tsan/strerror_r.cc.orig	2017-10-17 18:35:29.000000000 +0000
++++ test/tsan/strerror_r.cc
+@@ -11,7 +11,8 @@
+ char buffer[1000];
+ 
+ void *Thread(void *p) {
+-  return strerror_r(TEST_ERROR, buffer, sizeof(buffer));
++  strerror_r(TEST_ERROR, buffer, sizeof(buffer));
++  return buffer;
+ }
+ 
+ int main() {
