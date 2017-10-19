aef4cbdcd5ea0392905247369b315d974b28885a
Kamil Rytarowski <n54@gmx.com>
2017-10-19 15:17:19 +0200

    compiler-rt-netbsd: Sync local patches with pkgsrc-wip
    Part of the code contains new or revamped kludges, but it's a step
    forwards.
    
    check-tsan:
    
      Expected Passes    : 248
      Expected Failures  : 1
      Unsupported Tests  : 83
      Unexpected Failures: 44
    
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-lib_tsan_rtl_tsan__interceptors.cc
+++ compiler-rt-netbsd/patches/patch-lib_tsan_rtl_tsan__interceptors.cc
@@ -2,95 +2,127 @@ $NetBSD$
 
 --- lib/tsan/rtl/tsan_interceptors.cc.orig	2017-10-17 18:35:44.000000000 +0000
 +++ lib/tsan/rtl/tsan_interceptors.cc
-@@ -257,7 +257,7 @@ static ThreadSignalContext *SigCtx(Threa
-   return ctx;
- }
- 
--#if !SANITIZER_MAC
-+#if !SANITIZER_MAC && !SANITIZER_NETBSD
- static unsigned g_thread_finalize_key;
+@@ -43,8 +43,16 @@ using namespace __tsan;  // NOLINT
+ #if SANITIZER_NETBSD
+ #define dirfd(dirp) (*(int *)(dirp))
+ #define fileno_unlocked fileno
+-#define stdout __sF[1]
+-#define stderr __sF[2]
++
++#if _LP64
++#define __sF_size 152
++#else
++#define __sF_size 88
++#endif
++
++#define stdout ((char*)&__sF + (__sF_size * 1))
++#define stderr ((char*)&__sF + (__sF_size * 2))
++
  #endif
  
-@@ -387,9 +387,9 @@ static void at_exit_wrapper(void *arg) {
-   ThreadState *thr = cur_thread();
+ #if SANITIZER_ANDROID
+@@ -99,7 +107,7 @@ extern "C" int dirfd(void *dirp);
+ extern "C" int mallopt(int param, int value);
+ #endif
+ #if SANITIZER_NETBSD
+-extern __sanitizer_FILE **__sF;
++extern __sanitizer_FILE __sF[];
+ #else
+ extern __sanitizer_FILE *stdout, *stderr;
+ #endif
+@@ -388,8 +396,8 @@ static void at_exit_wrapper(void *arg) {
    uptr pc = 0;
    Acquire(thr, pc, (uptr)arg);
--  AtExitCtx *ctx = (AtExitCtx*)arg;
+   AtExitCtx *ctx = (AtExitCtx*)arg;
 -  ((void(*)(void *arg))ctx->f)(ctx->arg);
 -  InternalFree(ctx);
-+//  AtExitCtx *ctx = (AtExitCtx*)arg;
 +//  ((void(*)(void *arg))ctx->f)(ctx->arg);
 +//  InternalFree(ctx);
  }
  
  static int setup_at_exit_wrapper(ThreadState *thr, uptr pc, void(*f)(),
-@@ -858,7 +858,7 @@ void DestroyThreadState() {
+@@ -912,6 +920,38 @@ extern "C" void *__tsan_thread_start_fun
+   return res;
  }
- }  // namespace __tsan
  
--#if !SANITIZER_MAC
-+#if !SANITIZER_MAC && !SANITIZER_NETBSD
- static void thread_finalize(void *v) {
-   uptr iter = (uptr)v;
-   if (iter > 1) {
-@@ -888,7 +888,7 @@ extern "C" void *__tsan_thread_start_fun
-     ThreadState *thr = cur_thread();
-     // Thread-local state is not initialized yet.
-     ScopedIgnoreInterceptors ignore;
--#if !SANITIZER_MAC
-+#if !SANITIZER_MAC && !SANITIZER_NETBSD
-     ThreadIgnoreBegin(thr, 0);
-     if (pthread_setspecific(g_thread_finalize_key,
-                             (void *)GetPthreadDestructorIterations())) {
-@@ -1670,8 +1670,8 @@ TSAN_INTERCEPTOR(void*, tmpfile64, int f
- static void FlushStreams() {
-   // Flushing all the streams here may freeze the process if a child thread is
-   // performing file stream operations at the same time.
++static void FlushStreams() {
++  // Flushing all the streams here may freeze the process if a child thread is
++  // performing file stream operations at the same time.
++
++  REAL(fflush)(stdout);
++  REAL(fflush)(stderr);
++}
++
++static void finalize(void *arg) {
++  using namespace __tsan;
++  ThreadState *thr = cur_thread();
++  int status = Finalize(thr);
++  // Make sure the output is not lost.
++  FlushStreams();
++  if (status)
++    Die();
++}
++
++__attribute__((constructor))
++static void
++yikes1(void) {
++  if (REAL(__cxa_atexit)(&finalize, 0, 0)) {
++    Printf("ThreadSanitizer: failed to setup atexit callback\n");
++    Die();
++  }
++
++  if (pthread_key_create(&g_thread_finalize_key, &thread_finalize)) {
++    Printf("ThreadSanitizer: failed to create thread key\n");
++    Die();
++  }
++}
++
+ TSAN_INTERCEPTOR(int, pthread_create,
+     void *th, void *attr, void *(*callback)(void*), void * param) {
+   SCOPED_INTERCEPTOR_RAW(pthread_create, th, attr, callback, param);
+@@ -1667,13 +1707,6 @@ TSAN_INTERCEPTOR(void*, tmpfile64, int f
+ #define TSAN_MAYBE_INTERCEPT_TMPFILE64
+ #endif
+ 
+-static void FlushStreams() {
+-  // Flushing all the streams here may freeze the process if a child thread is
+-  // performing file stream operations at the same time.
 -  REAL(fflush)(stdout);
 -  REAL(fflush)(stderr);
-+//  REAL(fflush)(stdout);
-+//  REAL(fflush)(stderr);
- }
- 
+-}
+-
  TSAN_INTERCEPTOR(void, abort, int fake) {
-@@ -2439,6 +2439,7 @@ TSAN_INTERCEPTOR(void *, __tls_get_addr,
+   SCOPED_TSAN_INTERCEPTOR(abort, fake);
+   FlushStreams();
+@@ -2439,15 +2472,6 @@ TSAN_INTERCEPTOR(void *, __tls_get_addr,
  
  namespace __tsan {
  
-+#if 0
- static void finalize(void *arg) {
-   ThreadState *thr = cur_thread();
-   int status = Finalize(thr);
-@@ -2447,6 +2448,7 @@ static void finalize(void *arg) {
-   if (status)
-     Die();
- }
-+#endif
- 
+-static void finalize(void *arg) {
+-  ThreadState *thr = cur_thread();
+-  int status = Finalize(thr);
+-  // Make sure the output is not lost.
+-  FlushStreams();
+-  if (status)
+-    Die();
+-}
+-
  #if !SANITIZER_MAC && !SANITIZER_ANDROID
  static void unreachable() {
-@@ -2611,16 +2613,16 @@ void InitializeInterceptors() {
+   Report("FATAL: ThreadSanitizer: unreachable called\n");
+@@ -2611,6 +2635,7 @@ void InitializeInterceptors() {
    REAL(atexit) = (int(*)(void(*)()))unreachable;
  #endif
  
--  if (REAL(__cxa_atexit)(&finalize, 0, 0)) {
--    Printf("ThreadSanitizer: failed to setup atexit callback\n");
--    Die();
--  }
-+//  if (REAL(__cxa_atexit)(&finalize, 0, 0)) {
-+//    Printf("ThreadSanitizer: failed to setup atexit callback\n");
-+//    Die();
-+//  }
- 
- #if !SANITIZER_MAC
--  if (pthread_key_create(&g_thread_finalize_key, &thread_finalize)) {
--    Printf("ThreadSanitizer: failed to create thread key\n");
--    Die();
--  }
-+//  if (pthread_key_create(&g_thread_finalize_key, &thread_finalize)) {
-+//    Printf("ThreadSanitizer: failed to create thread key\n");
-+//    Die();
-+//  }
++#if 0
+   if (REAL(__cxa_atexit)(&finalize, 0, 0)) {
+     Printf("ThreadSanitizer: failed to setup atexit callback\n");
+     Die();
+@@ -2622,6 +2647,7 @@ void InitializeInterceptors() {
+     Die();
+   }
  #endif
++#endif
  
    FdInit();
+ }
