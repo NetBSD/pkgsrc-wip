a6f0146a7e841ace164ab19fe90e89d270e227e4
Kamil Rytarowski <n54@gmx.com>
2017-11-02 19:11:20 +0100

    compiler-rt-netbsd: Sync with local enhancements
    1. Drop the patch disabling SANITIZER_INTERCEPT_PHTREAD_MUTEX
    2. Disable hack in at_exit_wrapper() that disabled functionality of this
    function. There is required a fix for handling __cxa_atexit() with dso=NULL.
    Example patch:
    http://netbsd.org/~kamil/patch-00038-__cxa_atexit.txt
    3. Rewrite thread anhilation. Add purely NetBSD specific code, disable
    the usage of pthread_key_create(3) that does not work on NetBSD, because
    we still call at mutex loc/unlock after firing destructors. Intercept
    _lwp_exit(2) for this purpose.
    
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-lib_tsan_rtl_tsan__interceptors.cc
+++ compiler-rt-netbsd/patches/patch-lib_tsan_rtl_tsan__interceptors.cc
@@ -2,100 +2,66 @@ $NetBSD$
 
 --- lib/tsan/rtl/tsan_interceptors.cc.orig	2017-10-25 19:03:27.000000000 +0000
 +++ lib/tsan/rtl/tsan_interceptors.cc
-@@ -395,9 +395,9 @@ static void at_exit_wrapper(void *arg) {
-   ThreadState *thr = cur_thread();
-   uptr pc = 0;
-   Acquire(thr, pc, (uptr)arg);
--  AtExitCtx *ctx = (AtExitCtx*)arg;
--  ((void(*)(void *arg))ctx->f)(ctx->arg);
--  InternalFree(ctx);
-+//  AtExitCtx *ctx = (AtExitCtx*)arg;
-+//  ((void(*)(void *arg))ctx->f)(ctx->arg);
-+//  InternalFree(ctx);
+@@ -265,7 +265,7 @@ static ThreadSignalContext *SigCtx(Threa
+   return ctx;
  }
  
- static int setup_at_exit_wrapper(ThreadState *thr, uptr pc, void(*f)(),
-@@ -920,6 +920,39 @@ extern "C" void *__tsan_thread_start_fun
-   return res;
+-#if !SANITIZER_MAC
++#if !SANITIZER_MAC && !SANITIZER_NETBSD
+ static unsigned g_thread_finalize_key;
+ #endif
+ 
+@@ -866,7 +866,7 @@ void DestroyThreadState() {
  }
+ }  // namespace __tsan
  
-+static void FlushStreams() {
-+  // Flushing all the streams here may freeze the process if a child thread is
-+  // performing file stream operations at the same time.
-+
-+  REAL(fflush)(stdout);
-+  REAL(fflush)(stderr);
-+}
-+
-+static void finalize(void *arg) {
-+  using namespace __tsan;
-+  ThreadState *thr = cur_thread();
-+  int status = Finalize(thr);
-+  // Make sure the output is not lost.
-+  FlushStreams();
-+  if (status)
-+    Die();
-+}
-+
-+__attribute__((constructor))
-+//__attribute__((section(".init_array")))
-+static void
-+yikes1(void) {
-+  if (REAL(__cxa_atexit)(&finalize, 0, 0)) {
-+    Printf("ThreadSanitizer: failed to setup atexit callback\n");
-+    Die();
-+  }
-+
-+  if (pthread_key_create(&g_thread_finalize_key, &thread_finalize)) {
-+    Printf("ThreadSanitizer: failed to create thread key\n");
-+    Die();
-+  }
-+}
-+
- TSAN_INTERCEPTOR(int, pthread_create,
-     void *th, void *attr, void *(*callback)(void*), void * param) {
-   SCOPED_INTERCEPTOR_RAW(pthread_create, th, attr, callback, param);
-@@ -1675,12 +1708,12 @@ TSAN_INTERCEPTOR(void*, tmpfile64, int f
- #define TSAN_MAYBE_INTERCEPT_TMPFILE64
+-#if !SANITIZER_MAC
++#if !SANITIZER_MAC && !SANITIZER_NETBSD
+ static void thread_finalize(void *v) {
+   uptr iter = (uptr)v;
+   if (iter > 1) {
+@@ -896,7 +896,7 @@ extern "C" void *__tsan_thread_start_fun
+     ThreadState *thr = cur_thread();
+     // Thread-local state is not initialized yet.
+     ScopedIgnoreInterceptors ignore;
+-#if !SANITIZER_MAC
++#if !SANITIZER_MAC && !SANITIZER_NETBSD
+     ThreadIgnoreBegin(thr, 0);
+     if (pthread_setspecific(g_thread_finalize_key,
+                             (void *)GetPthreadDestructorIterations())) {
+@@ -2445,6 +2445,17 @@ TSAN_INTERCEPTOR(void *, __tls_get_addr,
+ }
  #endif
  
--static void FlushStreams() {
--  // Flushing all the streams here may freeze the process if a child thread is
--  // performing file stream operations at the same time.
--  REAL(fflush)(stdout);
--  REAL(fflush)(stderr);
--}
-+//static void FlushStreams() {
-+//  // Flushing all the streams here may freeze the process if a child thread is
-+//  // performing file stream operations at the same time.
-+//  REAL(fflush)(stdout);
-+//  REAL(fflush)(stderr);
-+//}
++#if SANITIZER_NETBSD
++TSAN_INTERCEPTOR(void, _lwp_exit) {
++  SCOPED_TSAN_INTERCEPTOR(_lwp_exit);
++  DestroyThreadState();
++  REAL(_lwp_exit)();
++}
++#define TSAN_MAYBE_INTERCEPT__LWP_EXIT TSAN_INTERCEPT(_lwp_exit)
++#else
++#define TSAN_MAYBE_INTERCEPT__LWP_EXIT
++#endif
++
+ namespace __tsan {
  
- TSAN_INTERCEPTOR(void, abort, int fake) {
-   SCOPED_TSAN_INTERCEPTOR(abort, fake);
-@@ -2619,16 +2652,16 @@ void InitializeInterceptors() {
-   REAL(atexit) = (int(*)(void(*)()))unreachable;
+ static void finalize(void *arg) {
+@@ -2613,6 +2624,8 @@ void InitializeInterceptors() {
+   TSAN_INTERCEPT(__tls_get_addr);
  #endif
  
--  if (REAL(__cxa_atexit)(&finalize, 0, 0)) {
--    Printf("ThreadSanitizer: failed to setup atexit callback\n");
--    Die();
--  }
-+//  if (REAL(__cxa_atexit)(&finalize, 0, 0)) {
-+//    Printf("ThreadSanitizer: failed to setup atexit callback\n");
-+//    Die();
-+//  }
++  TSAN_MAYBE_INTERCEPT__LWP_EXIT;
++
+ #if !SANITIZER_MAC && !SANITIZER_ANDROID
+   // Need to setup it, because interceptors check that the function is resolved.
+   // But atexit is emitted directly into the module, so can't be resolved.
+@@ -2624,7 +2637,7 @@ void InitializeInterceptors() {
+     Die();
+   }
  
- #if !SANITIZER_MAC
--  if (pthread_key_create(&g_thread_finalize_key, &thread_finalize)) {
--    Printf("ThreadSanitizer: failed to create thread key\n");
--    Die();
--  }
-+//  if (pthread_key_create(&g_thread_finalize_key, &thread_finalize)) {
-+//    Printf("ThreadSanitizer: failed to create thread key\n");
-+//    Die();
-+//  }
- #endif
- 
-   FdInit();
+-#if !SANITIZER_MAC
++#if !SANITIZER_MAC && !SANITIZER_NETBSD
+   if (pthread_key_create(&g_thread_finalize_key, &thread_finalize)) {
+     Printf("ThreadSanitizer: failed to create thread key\n");
+     Die();
