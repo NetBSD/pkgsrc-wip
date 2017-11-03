f0b85e825b86d39f4acc4275d29df1ac6a3634d8
Jason Bacon <bacon4000@gmail.com>
2017-11-03 11:42:31 -0500

    py-cutadapt: Fix bl3

--- py-cutadapt/buildlink3.mk
+++ py-cutadapt/buildlink3.mk
@@ -1,12 +1,12 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	cutadapt
+BUILDLINK_TREE+=	py-cutadapt
 
 .if !defined(CUTADAPT_BUILDLINK3_MK)
 CUTADAPT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cutadapt+=	cutadapt>=1.8.3
+BUILDLINK_API_DEPENDS.cutadapt+=	py-cutadapt>=1.14
 BUILDLINK_PKGSRCDIR.cutadapt?=	../../wip/py-cutadapt
 .endif	# CUTADAPT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-cutadapt
+BUILDLINK_TREE+=	-py-cutadapt
