2cfd0c411395de9848d5754ffd001d32ce7cd2e3
Jason Bacon <bacon4000@gmail.com>
2017-11-04 11:26:05 -0500

    py-cutadapt: Fix bl3 variables

--- py-cutadapt/buildlink3.mk
+++ py-cutadapt/buildlink3.mk
@@ -1,12 +1,12 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	py-cutadapt
+BUILDLINK_TREE+=	cutadapt
 
 .if !defined(CUTADAPT_BUILDLINK3_MK)
 CUTADAPT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cutadapt+=	py-cutadapt>=1.14
+BUILDLINK_API_DEPENDS.cutadapt+=	${PYPKGPREFIX}-cutadapt>=1.14
 BUILDLINK_PKGSRCDIR.cutadapt?=	../../wip/py-cutadapt
 .endif	# CUTADAPT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-py-cutadapt
+BUILDLINK_TREE+=	-cutadapt
