43e086801a546e74e985cda3339f74590d4502c1
Kamil Rytarowski <n54@gmx.com>
2017-11-06 05:06:54 +0100

    compiler-rt-netbsd: Upgrade to SVN r. 317430
    This revision incorporates NetBSD enhances in compiler-rt.
    
    Two major patches incorporated:
     - Improve detection of thread termination on NetBSD.
     - Stop tracking Linux specific on_exit().
    
    Revamp patch for tracking __cxa_atexit() and atexit(3) and make it
    compatible with NetBSD.
    
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-lib_tsan_rtl_tsan__interceptors.cc
+++ compiler-rt-netbsd/patches/patch-lib_tsan_rtl_tsan__interceptors.cc
@@ -1,67 +1,60 @@
 $NetBSD$
 
---- lib/tsan/rtl/tsan_interceptors.cc.orig	2017-10-25 19:03:27.000000000 +0000
+--- lib/tsan/rtl/tsan_interceptors.cc.orig	2017-11-06 03:58:05.389006908 +0000
 +++ lib/tsan/rtl/tsan_interceptors.cc
-@@ -265,7 +265,7 @@ static ThreadSignalContext *SigCtx(Threa
-   return ctx;
- }
+@@ -391,7 +391,28 @@ struct AtExitCtx {
+   void *arg;
+ };
  
--#if !SANITIZER_MAC
-+#if !SANITIZER_MAC && !SANITIZER_NETBSD
- static unsigned g_thread_finalize_key;
- #endif
- 
-@@ -866,7 +866,7 @@ void DestroyThreadState() {
- }
- }  // namespace __tsan
- 
--#if !SANITIZER_MAC
-+#if !SANITIZER_MAC && !SANITIZER_NETBSD
- static void thread_finalize(void *v) {
-   uptr iter = (uptr)v;
-   if (iter > 1) {
-@@ -896,7 +896,7 @@ extern "C" void *__tsan_thread_start_fun
-     ThreadState *thr = cur_thread();
-     // Thread-local state is not initialized yet.
-     ScopedIgnoreInterceptors ignore;
--#if !SANITIZER_MAC
-+#if !SANITIZER_MAC && !SANITIZER_NETBSD
-     ThreadIgnoreBegin(thr, 0);
-     if (pthread_setspecific(g_thread_finalize_key,
-                             (void *)GetPthreadDestructorIterations())) {
-@@ -2445,6 +2445,17 @@ TSAN_INTERCEPTOR(void *, __tls_get_addr,
- }
- #endif
- 
-+#if SANITIZER_NETBSD
-+TSAN_INTERCEPTOR(void, _lwp_exit) {
-+  SCOPED_TSAN_INTERCEPTOR(_lwp_exit);
-+  DestroyThreadState();
-+  REAL(_lwp_exit)();
+-static void at_exit_wrapper(void *arg) {
++static struct AtExitCtx **AtExitStack;
++static int AtExitStackSize;
++
++static void at_exit_wrapper() {
++  ThreadState *thr = cur_thread();
++  uptr pc = 0;
++
++  // Pop AtExitCtx from the top of the stack of callback functions
++  AtExitCtx *ctx = AtExitStack[AtExitStackSize - 1];
++  --AtExitStackSize;
++  if (InternalReallocArr(&AtExitStack, AtExitStackSize,
++                         sizeof(struct AtExitCtx *))) {
++    Printf("InternalReallocArr error");
++    Die();
++  }
++
++  Acquire(thr, pc, (uptr)ctx);
++  ((void(*)())ctx->f)();
++  InternalFree(ctx);
 +}
-+#define TSAN_MAYBE_INTERCEPT__LWP_EXIT TSAN_INTERCEPT(_lwp_exit)
-+#else
-+#define TSAN_MAYBE_INTERCEPT__LWP_EXIT
-+#endif
 +
- namespace __tsan {
- 
- static void finalize(void *arg) {
-@@ -2613,6 +2624,8 @@ void InitializeInterceptors() {
-   TSAN_INTERCEPT(__tls_get_addr);
- #endif
- 
-+  TSAN_MAYBE_INTERCEPT__LWP_EXIT;
-+
- #if !SANITIZER_MAC && !SANITIZER_ANDROID
-   // Need to setup it, because interceptors check that the function is resolved.
-   // But atexit is emitted directly into the module, so can't be resolved.
-@@ -2624,7 +2637,7 @@ void InitializeInterceptors() {
-     Die();
-   }
- 
--#if !SANITIZER_MAC
-+#if !SANITIZER_MAC && !SANITIZER_NETBSD
-   if (pthread_key_create(&g_thread_finalize_key, &thread_finalize)) {
-     Printf("ThreadSanitizer: failed to create thread key\n");
-     Die();
++static void cxa_at_exit_wrapper(void *arg) {
+   ThreadState *thr = cur_thread();
+   uptr pc = 0;
+   Acquire(thr, pc, (uptr)arg);
+@@ -430,7 +451,24 @@ static int setup_at_exit_wrapper(ThreadS
+   // Memory allocation in __cxa_atexit will race with free during exit,
+   // because we do not see synchronization around atexit callback list.
+   ThreadIgnoreBegin(thr, pc);
+-  int res = REAL(__cxa_atexit)(at_exit_wrapper, ctx, dso);
++  int res;
++  if (dso == 0) {
++    // NetBSD does not preserve the 2nd argument if dso is equal to 0
++    // Store ctx in a local stack-like structure
++    res = REAL(__cxa_atexit)((void (*)(void *a))at_exit_wrapper, 0, 0);
++    // Push AtExitCtx on the top of the stack of callback functions
++    if (res == 0) {
++      ++AtExitStackSize;
++      if (InternalReallocArr(&AtExitStack, AtExitStackSize,
++                             sizeof(struct AtExitCtx *))) {
++        Printf("InternalReallocArr error");
++        Die();
++      }
++      AtExitStack[AtExitStackSize - 1] = ctx;
++    }
++  } else {
++    res = REAL(__cxa_atexit)(cxa_at_exit_wrapper, ctx, dso);
++  }
+   ThreadIgnoreEnd(thr, pc);
+   return res;
+ }
