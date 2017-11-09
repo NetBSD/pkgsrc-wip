7e85a3e3b7a21cfe305f54c9efc77c4737c0c901
Kamil Rytarowski <n54@gmx.com>
2017-11-09 21:20:21 +0100

    clang-netbsd: Enable Memory sanitizer for NetBSD/x86_64
    Sponsored by <The NetBSD Foundation>

--- clang-netbsd/patches/patch-lib_Driver_ToolChains_NetBSD.cpp
+++ clang-netbsd/patches/patch-lib_Driver_ToolChains_NetBSD.cpp
@@ -1,10 +1,12 @@
 $NetBSD$
 
---- lib/Driver/ToolChains/NetBSD.cpp.orig	2017-08-29 00:48:11.000000000 +0000
+--- lib/Driver/ToolChains/NetBSD.cpp.orig	2017-10-17 16:48:56.000000000 +0000
 +++ lib/Driver/ToolChains/NetBSD.cpp
-@@ -431,6 +431,7 @@ SanitizerMask NetBSD::getSupportedSaniti
+@@ -430,7 +430,9 @@ SanitizerMask NetBSD::getSupportedSaniti
+   if (IsX86_64) {
      Res |= SanitizerKind::Fuzzer;
      Res |= SanitizerKind::FuzzerNoLink;
++    Res |= SanitizerKind::Memory;
      Res |= SanitizerKind::Thread;
 +    Res |= SanitizerKind::KernelAddress;
    }
