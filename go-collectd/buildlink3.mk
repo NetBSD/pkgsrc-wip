788df66ca456389d3ac9ea98f0bec59278202a52
Keke <kethzer.dr@gmail.com>
2017-11-24 15:14:21 -0400

    Update go-collectd

--- go-collectd/buildlink3.mk
+++ go-collectd/buildlink3.mk
@@ -6,10 +6,10 @@ BUILDLINK_TREE+=	go-collectd
 GO_COLLECTD_BUILDLINK3_MK:=
 
 BUILDLINK_CONTENTS_FILTER.go-collectd=	${EGREP} gopkg/
-BUILDLINK_DEPMETHOD.go-collectd?=	build
+BUILDLINK_DEPMETHOD.go-collectd?=		build
 
 BUILDLINK_API_DEPENDS.go-collectd+=	go-collectd>=20150830
-BUILDLINK_PKGSRCDIR.go-collectd?=	../../sysutils/go-collectd
+BUILDLINK_PKGSRCDIR.go-collectd?=	../../wip/go-collectd
 .endif	# GO_COLLECTD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-go-collectd
