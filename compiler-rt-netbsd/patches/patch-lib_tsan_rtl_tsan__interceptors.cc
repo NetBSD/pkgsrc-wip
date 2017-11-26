2150b32d653dcf0468a1ead36411cbd54f0c8129
Kamil Rytarowski <n54@gmx.com>
2017-11-26 21:30:48 +0100

    compiler-rt-netbsd: Upgrade to SVN r. 319004
    This revision contains upstreamed patch for Thread Joined/Exited race.
    
    Prevent Thread Exited/Joined events race
    https://reviews.llvm.org/D40457
    
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-lib_tsan_rtl_tsan__interceptors.cc
+++ compiler-rt-netbsd/patches/patch-lib_tsan_rtl_tsan__interceptors.cc
@@ -108,44 +108,6 @@ $NetBSD$
  
  #if !SANITIZER_MAC
  TSAN_INTERCEPTOR(void*, malloc, uptr size) {
-@@ -868,6 +933,7 @@ void DestroyThreadState() {
-   }
-   DTLS_Destroy();
-   cur_thread_finalize();
-+  thr->tctx->SetDestroyed();
- }
- }  // namespace __tsan
- 
-@@ -982,12 +1048,14 @@ TSAN_INTERCEPTOR(int, pthread_create,
- 
- TSAN_INTERCEPTOR(int, pthread_join, void *th, void **ret) {
-   SCOPED_INTERCEPTOR_RAW(pthread_join, th, ret);
--  int tid = ThreadTid(thr, pc, (uptr)th);
-+  ThreadContextBase *tctx = ThreadTctx(thr, pc, (uptr)th);
-   ThreadIgnoreBegin(thr, pc);
-   int res = BLOCK_REAL(pthread_join)(th, ret);
-   ThreadIgnoreEnd(thr, pc);
-   if (res == 0) {
--    ThreadJoin(thr, pc, tid);
-+    while (!tctx->GetDestroyed())
-+       internal_sched_yield();
-+    ThreadJoin(thr, pc, tctx->tid);
-   }
-   return res;
- }
-@@ -996,10 +1064,10 @@ DEFINE_REAL_PTHREAD_FUNCTIONS
- 
- TSAN_INTERCEPTOR(int, pthread_detach, void *th) {
-   SCOPED_TSAN_INTERCEPTOR(pthread_detach, th);
--  int tid = ThreadTid(thr, pc, (uptr)th);
-+  ThreadContextBase *tctx = ThreadTctx(thr, pc, (uptr)th);
-   int res = REAL(pthread_detach)(th);
-   if (res == 0) {
--    ThreadDetach(thr, pc, tid);
-+    ThreadDetach(thr, pc, tctx->tid);
-   }
-   return res;
- }
 @@ -2332,7 +2400,7 @@ static __sanitizer_sighandler_ptr signal
    internal_memset(&act.sa_mask, -1, sizeof(act.sa_mask));
    act.sa_flags = 0;
