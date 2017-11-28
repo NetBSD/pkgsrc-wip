12b49c11aae7bf00f442af3ab1c890a2376d9ade
Kamil Rytarowski <n54@gmx.com>
2017-11-28 09:27:18 +0100

    compiler-rt-netbsd: Update to SVN r. 319149
    Rediff local patch, partially merged upstream.
    
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-cmake_config-ix.cmake
+++ compiler-rt-netbsd/patches/patch-cmake_config-ix.cmake
@@ -1,24 +1,8 @@
 $NetBSD$
 
---- cmake/config-ix.cmake.orig	2017-11-06 03:58:05.000000000 +0000
+--- cmake/config-ix.cmake.orig	2017-11-28 08:25:28.951362549 +0000
 +++ cmake/config-ix.cmake
-@@ -511,28 +511,28 @@ else()
- endif()
- 
- if (COMPILER_RT_HAS_SANITIZER_COMMON AND LSAN_SUPPORTED_ARCH AND
--    OS_NAME MATCHES "Darwin|Linux|FreeBSD")
-+    OS_NAME MATCHES "Darwin|Linux|FreeBSD|NetBSD")
-   set(COMPILER_RT_HAS_LSAN TRUE)
- else()
-   set(COMPILER_RT_HAS_LSAN FALSE)
- endif()
- 
- if (COMPILER_RT_HAS_SANITIZER_COMMON AND MSAN_SUPPORTED_ARCH AND
--    OS_NAME MATCHES "Linux")
-+    OS_NAME MATCHES "Linux|NetBSD")
-   set(COMPILER_RT_HAS_MSAN TRUE)
- else()
-   set(COMPILER_RT_HAS_MSAN FALSE)
+@@ -525,7 +525,7 @@ else()
  endif()
  
  if (PROFILE_SUPPORTED_ARCH AND NOT LLVM_USE_SANITIZER AND
@@ -27,11 +11,3 @@ $NetBSD$
    set(COMPILER_RT_HAS_PROFILE TRUE)
  else()
    set(COMPILER_RT_HAS_PROFILE FALSE)
- endif()
- 
- if (COMPILER_RT_HAS_SANITIZER_COMMON AND TSAN_SUPPORTED_ARCH AND
--    OS_NAME MATCHES "Darwin|Linux|FreeBSD|Android")
-+    OS_NAME MATCHES "Darwin|Linux|FreeBSD|Android|NetBSD")
-   set(COMPILER_RT_HAS_TSAN TRUE)
- else()
-   set(COMPILER_RT_HAS_TSAN FALSE)
