56ebe2d0753ace577c40bc3cd39dbcaa3b2ffb30
Kamil Rytarowski <n54@gmx.com>
2017-12-04 23:57:24 +0100

    compiler-rt-netbsd: Handle ThreadExit in MSan/NetBSD
    Sponsored by <The NetBSD Foundation>

---
+++ compiler-rt-netbsd/patches/patch-lib_msan_msan__linux.cc
@@ -0,0 +1,62 @@
+$NetBSD$
+
+--- lib/msan/msan_linux.cc.orig	2017-11-16 17:55:36.000000000 +0000
++++ lib/msan/msan_linux.cc
+@@ -30,6 +30,7 @@
+ #include <sys/time.h>
+ #include <sys/resource.h>
+ 
++#include "interception/interception.h"
+ #include "sanitizer_common/sanitizer_common.h"
+ #include "sanitizer_common/sanitizer_procmaps.h"
+ 
+@@ -174,13 +175,28 @@ void InstallAtExitHandler() {
+ 
+ // ---------------------- TSD ---------------- {{{1
+ 
++#if !SANITIZER_NETBSD
+ static pthread_key_t tsd_key;
++#endif
++
+ static bool tsd_key_inited = false;
+ 
++#if SANITIZER_NETBSD
++INTERCEPTOR(void, _lwp_exit) {
++  CHECK(tsd_key_inited);
++  MsanTSDDtor(GetCurrentThread());
++  REAL(_lwp_exit)();
++}
++#endif
++
+ void MsanTSDInit(void (*destructor)(void *tsd)) {
+   CHECK(!tsd_key_inited);
+   tsd_key_inited = true;
++#if SANITIZER_NETBSD
++  INTERCEPT_FUNCTION(_lwp_exit);
++#else
+   CHECK_EQ(0, pthread_key_create(&tsd_key, destructor));
++#endif
+ }
+ 
+ static THREADLOCAL MsanThread* msan_current_thread;
+@@ -195,16 +211,20 @@ void SetCurrentThread(MsanThread *t) {
+   msan_current_thread = t;
+   // Make sure that MsanTSDDtor gets called at the end.
+   CHECK(tsd_key_inited);
++#if !SANITIZER_NETBSD
+   pthread_setspecific(tsd_key, (void *)t);
++#endif
+ }
+ 
+ void MsanTSDDtor(void *tsd) {
++#if !SANITIZER_NETBSD
+   MsanThread *t = (MsanThread*)tsd;
+   if (t->destructor_iterations_ > 1) {
+     t->destructor_iterations_--;
+     CHECK_EQ(0, pthread_setspecific(tsd_key, tsd));
+     return;
+   }
++#endif
+   msan_current_thread = nullptr;
+   // Make sure that signal handler can not see a stale current thread pointer.
+   atomic_signal_fence(memory_order_seq_cst);
