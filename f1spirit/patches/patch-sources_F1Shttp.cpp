0abccec459757fcc18e7e221229b4b904b5afd48
i3enedek <pkgsrc@xn--rvztrtkrfrgp-bbb7j2b8f0b9d7a21oft.com>
2017-12-26 02:38:08 +0000

    f1spirit: importing remake of f1spirit for the MSX and fixing up roadfighter

---
+++ f1spirit/patches/patch-sources_F1Shttp.cpp
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- sources/F1Shttp.cpp.orig	2009-02-06 09:54:49.000000000 +0000
++++ sources/F1Shttp.cpp
+@@ -3,7 +3,6 @@
+ #include "string.h"
+ 
+ #include <curl/curl.h>
+-#include <curl/types.h>
+ #include <curl/easy.h>
+ 
+ #include "F1Shttp.h"
