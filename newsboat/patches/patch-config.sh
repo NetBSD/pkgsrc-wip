6edc96f87ccb4316de179ca2cacd0d8f53ea17cb
Yorick Hardy <yorickhardy@gmail.com>
2017-12-26 19:53:12 +0200

    newsboat: update to newsboat-2.10.2
    Lists below only mention user-visible changes, but I would also like to
    acknowledge contributions from the following people: Alok Singh, Carno, Jonas
    Karlsson, Kamil Wsół, Mike Crute, Niels Kobschätzki, and maiki.
    
    - HTML anchors for all config commands in docs. You can now link to each command
        separately (#10) (Lysander Trischler)
    - Support for Inoreader (Bart Libert)
    - Slovak translation (František Hájik)
    
    - Enqueue *last* audio enclosure
        (https://github.com/akrennmair/newsbeuter/issues/604)
    - `text-width` doesn't apply if it's bigger than terminal width
        (https://github.com/akrennmair/newsbeuter/issues/602)
    - Translations: German (Lysander Trischler), Russian, Ukrainian
        (Alexander Batischev)
    
    - Build dependency on Perl (#6)
    - Test dependency on bc (Nikos Tsipinakis)
    
    - Do not create XDG data dir if not using XDG (#8)
    - When used with Newsblur, check on startup if cookie-cache exists or can be
        created, because integration doesn't work without cookies (#13)
    - Builds on AARCH64 and ARMHF (#43)
    - Only show an error message once when unknown option is supplied
        (Lysander Trischler)
    - License header used to say it's MIT/X Consortium License, whereas in reality
        it's a MIT License (discovered by Nikos Tsipinakis)
    - Cross-compilation made possible by conditionally assigning to RANLIB and AR in
        Makefile (Fredrik Fornwall)
    - Cookies actually get persisted (Simon Schuster, reported and tested by Håkan
        Jerning)
    - CJK text is wrapped at correct code-point boundaries (#71) (nmtake)
    - Don't segfault if `error-log` points to non-existent file (Simon Schuster)
    - Spanish translation (José Manuel García-Patos)

--- newsboat/patches/patch-config.sh
+++ newsboat/patches/patch-config.sh
@@ -4,7 +4,7 @@ Remove overly "smart" detection script for ncurses.  This doesn't work on NetBSD
 If libstfl is linked against the system-wide curses, you can't later link against
 ncurses.  Newsboat can use the ncursesw compat offered by NetBSD 6.x native curses.
 
---- config.sh.orig	2017-09-22 20:01:50.000000000 +0000
+--- config.sh.orig	2017-12-25 17:49:35.000000000 +0000
 +++ config.sh
 @@ -106,6 +106,8 @@ check_pkg "stfl" || fail "stfl"
  
@@ -14,4 +14,4 @@ ncurses.  Newsboat can use the ncursesw compat offered by NetBSD 6.x native curs
 +       echo "Assuming native NetBSD curses should be used"
  elif [ `uname -s` != "OpenBSD" ]; then
  	check_pkg "ncursesw" || \
- 	check_custom "ncursesw5" "ncursesw5-config" ||  fail "ncursesw"
+ 	check_custom "ncursesw5" "ncursesw5-config" || \
