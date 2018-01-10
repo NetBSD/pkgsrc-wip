98c31085f11e90153f025ef812d27a045e758ee2
Kamil Rytarowski <n54@gmx.com>
2018-01-10 07:01:37 +0100

    compiler-rt-netbsd: Intercept getgrouplist(3)/getgroupmembership(3)
    id(1) now runs under MSan.
    
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-lib_sanitizer__common_sanitizer__platform__interceptors.h
+++ compiler-rt-netbsd/patches/patch-lib_sanitizer__common_sanitizer__platform__interceptors.h
@@ -2,7 +2,7 @@ $NetBSD$
 
 --- lib/sanitizer_common/sanitizer_platform_interceptors.h.orig	2018-01-08 15:33:13.000000000 +0000
 +++ lib/sanitizer_common/sanitizer_platform_interceptors.h
-@@ -432,4 +432,9 @@
+@@ -432,4 +432,10 @@
  #define SANITIZER_INTERCEPT_SIGNAL_AND_SIGACTION (!SI_WINDOWS && SI_NOT_FUCHSIA)
  #define SANITIZER_INTERCEPT_BSD_SIGNAL SI_ANDROID
  
@@ -10,5 +10,6 @@ $NetBSD$
 +#define SANITIZER_INTERCEPT_DEVNAME SI_NETBSD
 +#define SANITIZER_INTERCEPT_SYSCTL SI_NETBSD
 +#define SANITIZER_INTERCEPT_STRLCPY SI_NETBSD
++#define SANITIZER_INTERCEPT_GETGROUPLIST SI_NETBSD
 +
  #endif  // #ifndef SANITIZER_PLATFORM_INTERCEPTORS_H
