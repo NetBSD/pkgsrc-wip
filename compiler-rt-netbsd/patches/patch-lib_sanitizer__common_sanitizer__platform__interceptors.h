9ca80ad5ef646c42ed819117ffa7eb6083ad720e
Kamil Rytarowski <n54@gmx.com>
2018-01-10 06:46:43 +0100

    compiler-rt-netbsd: Teach sanitizers about strlcpy(3)/strlcat(3)
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-lib_sanitizer__common_sanitizer__platform__interceptors.h
+++ compiler-rt-netbsd/patches/patch-lib_sanitizer__common_sanitizer__platform__interceptors.h
@@ -2,12 +2,13 @@ $NetBSD$
 
 --- lib/sanitizer_common/sanitizer_platform_interceptors.h.orig	2018-01-08 15:33:13.000000000 +0000
 +++ lib/sanitizer_common/sanitizer_platform_interceptors.h
-@@ -432,4 +432,8 @@
+@@ -432,4 +432,9 @@
  #define SANITIZER_INTERCEPT_SIGNAL_AND_SIGACTION (!SI_WINDOWS && SI_NOT_FUCHSIA)
  #define SANITIZER_INTERCEPT_BSD_SIGNAL SI_ANDROID
  
 +#define SANITIZER_INTERCEPT_KVM SI_NETBSD
 +#define SANITIZER_INTERCEPT_DEVNAME SI_NETBSD
 +#define SANITIZER_INTERCEPT_SYSCTL SI_NETBSD
++#define SANITIZER_INTERCEPT_STRLCPY SI_NETBSD
 +
  #endif  // #ifndef SANITIZER_PLATFORM_INTERCEPTORS_H
