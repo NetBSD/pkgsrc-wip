884e172d7769b9d1e92f9bd2609eb5042e6adcd6
Min Sik Kim <minskim@NetBSD.org>
2018-01-12 09:36:31 -0800

    py-Robinhood: Update to 1.0.1
    Changes:
    
    - Adds custom Exceptions
    - More consistent spacing
    - Documentation improvement

--- py-Robinhood/patches/patch-setup.py
+++ py-Robinhood/patches/patch-setup.py
@@ -1,7 +1,16 @@
 $NetBSD$
 
---- setup.py.orig	2017-07-31 18:28:59.000000000 +0000
+--- setup.py.orig	2017-11-07 15:29:40.000000000 +0000
 +++ setup.py
+@@ -6,7 +6,7 @@ from setuptools import setup, find_packa
+ from setuptools.command.test import test as TestCommand
+ 
+ HERE = path.abspath(path.dirname(__file__))
+-__version__ = '1.0.0'
++__version__ = '1.0.1'
+ 
+ REQUIRES = [
+     'requests~=2.13.0',
 @@ -107,8 +107,6 @@ setup(
      keywords='Robinhood trade API',
      packages=find_packages(),
