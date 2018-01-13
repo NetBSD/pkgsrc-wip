acdc3cba6bab4f8f5781676b367c1558824163f6
coypu <coypu@sdf.org>
2018-01-13 02:17:27 +0200

    gcc8snapshot: update to 20180107 snapshot, cleanup patches
    cilk: removed upstream.
    
    lto.c: only use workaround if WCONTINUED is not defined, and then try
    to fallback to WALLEXIT. apply this for everyone.
    newer netbsd (8) has WCONTINUED.
    
    i386/cpuinfo.h: different workaround applied upstream for bug.
    Maybe a problem for other OSes: we will apply the upstream variant if
    necessary, not a different one.
    
    libgfortran: limit "no weak references" support to just old_locale*
    
    system.h: remove unnecessary patching. netbsd-7 has putc_unlocked etc.
    libbacktrace: remove apparently unnecessary patch.
    decNumber.c: apparently unnecessary patch.
    libgo: remove unnecessary patch.
    libsupc++/unwind-cxx.h: remove unnecessary patch.
    
    sync with netbsd ctype (minimal changes needed to compile)
    
    Tested on netbsd-8.99.

--- gcc8snapshot/patches/patch-gcc_lto_lto.c
+++ gcc8snapshot/patches/patch-gcc_lto_lto.c
@@ -1,19 +1,19 @@
-$NetBSD: patch-gcc_lto_lto.c,v 1.1 2017/05/03 00:21:03 maya Exp $
+$NetBSD$
 
---- gcc/lto/lto.c.orig	2015-01-30 16:15:00.000000000 +0000
+Better fallback for netbsd<8 lacking WALLSIG.
+
+--- gcc/lto/lto.c.orig	2018-01-03 21:42:12.000000000 +0000
 +++ gcc/lto/lto.c
-@@ -2495,7 +2495,13 @@ wait_for_child ()
+@@ -2321,7 +2321,11 @@ wait_for_child ()
+   do
+     {
  #ifndef WCONTINUED
- #define WCONTINUED 0
+-#define WCONTINUED 0
++#  ifdef WALLSIG
++#    define WCONTINUED WALLSIG
++#  else
++#    define WCONTINUED 0
++#  endif
  #endif
--      int w = waitpid (0, &status, WUNTRACED | WCONTINUED);
-+
-+#if defined(__NetBSD__)
-+      int w = waitpid(0, &status, WUNTRACED | WALLSIG);
-+#else
-+      int w = waitpid(0, &status, WUNTRACED | WCONTINUED);
-+#endif
-+ 
+       int w = waitpid (0, &status, WUNTRACED | WCONTINUED);
        if (w == -1)
- 	fatal_error (input_location, "waitpid failed");
- 
--- gcc8snapshot/patches/patch-libstdc++-v3_config_os_bsd_netbsd_ctype__configure__char.cc
+++ gcc8snapshot/patches/patch-libstdc++-v3_config_os_bsd_netbsd_ctype__configure__char.cc
@@ -1,28 +1,50 @@
 $NetBSD: patch-libstdc++-v3_config_os_bsd_netbsd_ctype__configure__char.cc,v 1.1 2017/05/03 00:21:03 maya Exp $
 
-https://gcc.gnu.org/bugzilla/show_bug.cgi?id=64271
+Sync with netbsd ctype_configure_char.cc
 
---- libstdc++-v3/config/os/bsd/netbsd/ctype_configure_char.cc.orig	2013-02-03 17:54:05.000000000 +0000
+--- libstdc++-v3/config/os/bsd/netbsd/ctype_configure_char.cc.orig	2018-01-03 10:03:58.000000000 +0000
 +++ libstdc++-v3/config/os/bsd/netbsd/ctype_configure_char.cc
-@@ -38,11 +38,17 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
+@@ -38,29 +38,27 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
  
  // Information as gleaned from /usr/include/ctype.h
  
-+#ifndef _CTYPE_BL
-   extern "C" const u_int8_t _C_ctype_[];
-+#endif
- 
+-  extern "C" const u_int8_t _C_ctype_[];
+-
    const ctype_base::mask*
    ctype<char>::classic_table() throw()
-+#ifdef _CTYPE_BL
+-  { return _C_ctype_ + 1; }
 +  { return _C_ctype_tab_ + 1; }
-+#else
-   { return _C_ctype_ + 1; }
-+#endif
  
-   ctype<char>::ctype(__c_locale, const mask* __table, bool __del,
- 		     size_t __refs)
-@@ -69,14 +75,14 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
+-  ctype<char>::ctype(__c_locale, const mask* __table, bool __del,
+-		     size_t __refs)
+-  : facet(__refs), _M_del(__table != 0 && __del),
+-  _M_toupper(NULL), _M_tolower(NULL),
++  ctype<char>::ctype(__c_locale, const mask* __table, bool __del, 
++		     size_t __refs) 
++  : facet(__refs), _M_del(__table != 0 && __del), 
++  _M_toupper(NULL), _M_tolower(NULL), 
+   _M_table(__table ? __table : classic_table())
+-  {
++  { 
+     memset(_M_widen, 0, sizeof(_M_widen));
+     _M_widen_ok = 0;
+     memset(_M_narrow, 0, sizeof(_M_narrow));
+     _M_narrow_ok = 0;
+   }
+ 
+-  ctype<char>::ctype(const mask* __table, bool __del, size_t __refs)
+-  : facet(__refs), _M_del(__table != 0 && __del),
+-  _M_toupper(NULL), _M_tolower(NULL),
++  ctype<char>::ctype(const mask* __table, bool __del, size_t __refs) 
++  : facet(__refs), _M_del(__table != 0 && __del), 
++  _M_toupper(NULL), _M_tolower(NULL), 
+   _M_table(__table ? __table : classic_table())
+-  {
++  { 
+     memset(_M_widen, 0, sizeof(_M_widen));
+     _M_widen_ok = 0;
+     memset(_M_narrow, 0, sizeof(_M_narrow));
+@@ -69,14 +67,14 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
  
    char
    ctype<char>::do_toupper(char __c) const
@@ -39,14 +61,15 @@ https://gcc.gnu.org/bugzilla/show_bug.cgi?id=64271
  	++__low;
        }
      return __high;
-@@ -84,14 +90,14 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
+@@ -84,14 +82,14 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
  
    char
    ctype<char>::do_tolower(char __c) const
 -  { return ::tolower((int) __c); }
 +  { return ::tolower((int)(unsigned char) __c); }
  
-   const char*
+-  const char*
++  const char* 
    ctype<char>::do_tolower(char* __low, const char* __high) const
    {
      while (__low < __high)
