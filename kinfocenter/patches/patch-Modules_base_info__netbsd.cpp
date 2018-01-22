2b17f516f877eaa2b2824639e307f8d62c908fc9
Mark Davies <mark@ecs.vuw.ac.nz>
2018-01-22 23:32:21 +1300

    kinfocenter: update to 5.11.5

---
+++ kinfocenter/patches/patch-Modules_base_info__netbsd.cpp
@@ -0,0 +1,15 @@
+$NetBSD$
+
+<kio/global.h> not used (and not found)
+
+--- Modules/base/info_netbsd.cpp.orig	2018-01-02 11:44:59.901997533 +0000
++++ Modules/base/info_netbsd.cpp
+@@ -31,7 +31,7 @@
+ #include <QTreeWidgetItemIterator>
+ 
+ #include <QDebug>
+-#include <kio/global.h> /* for KIO::convertSize() */
++/* #include <kio/global.h> /* for KIO::convertSize() */
+ 
+ typedef struct {
+ 	int string;
