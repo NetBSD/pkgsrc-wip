095154ef9820e3986528b4d4ded2de051fc9654a
Mark Davies <mark@ecs.vuw.ac.nz>
2018-01-22 16:07:25 +1300

    libksysguard: update to 5.11.5

--- libksysguard/patches/patch-processcore_processes.cpp
+++ libksysguard/patches/patch-processcore_processes.cpp
@@ -2,7 +2,23 @@ $NetBSD: patch-libs_ksysguard_processcore_processes.cpp,v 1.1 2012/03/19 19:44:0
 
 --- processcore/processes.cpp.orig	2016-04-19 11:13:53.000000000 +0000
 +++ processcore/processes.cpp
-@@ -206,6 +206,7 @@ bool Processes::updateProcessInfo(Proces
+@@ -162,6 +162,7 @@ bool Processes::updateProcess( Process *
+ }
+ 
+ bool Processes::updateProcessInfo(Process *ps) {
++#ifndef Q_OS_NETBSD
+     //Now we can actually get the process info
+     qlonglong oldUserTime = ps->userTime();
+     qlonglong oldSysTime = ps->sysTime();
+@@ -181,6 +182,7 @@ bool Processes::updateProcessInfo(Proces
+         oldIoCharactersActuallyRead = ps->ioCharactersActuallyRead();
+         oldIoCharactersActuallyWritten = ps->ioCharactersActuallyWritten();
+     }
++#endif
+ 
+     ps->setChanges(Process::Nothing);
+     bool success;
+@@ -206,6 +208,7 @@ bool Processes::updateProcessInfo(Proces
          }
  #endif
          if(d->mUpdateFlags.testFlag(Processes::IOStatistics)) {
@@ -10,7 +26,7 @@ $NetBSD: patch-libs_ksysguard_processcore_processes.cpp,v 1.1 2012/03/19 19:44:0
              if( d->mHavePreviousIoValues ) {
                  ps->setIoCharactersReadRate((ps->ioCharactersRead() - oldIoCharactersRead) * 1000.0 / elapsedTime);
                  ps->setIoCharactersWrittenRate((ps->ioCharactersWritten() - oldIoCharactersWritten) * 1000.0 / elapsedTime);
-@@ -215,6 +216,7 @@ bool Processes::updateProcessInfo(Proces
+@@ -215,6 +218,7 @@ bool Processes::updateProcessInfo(Proces
                  ps->setIoCharactersActuallyWrittenRate((ps->ioCharactersActuallyWritten() - oldIoCharactersActuallyWritten) * 1000.0 / elapsedTime);
              } else
                  d->mHavePreviousIoValues = true;
