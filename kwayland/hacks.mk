0ae15a1047de54083dc8abd520f7d31bb044cc75
Mark Davies <mark@ecs.vuw.ac.nz>
2018-01-22 15:40:17 +1300

    kwayland: update to 5.41.0

--- kwayland/hacks.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD$
-#
-.include "../../mk/bsd.fast.prefs.mk"
-
-.if ${OPSYS} == "NetBSD"
-pre-configure:
-	mkdir ${BUILDLINK_DIR}/include/linux
-	cp ${FILESDIR}/input.h ${BUILDLINK_DIR}/include/linux/input.h
-.endif
