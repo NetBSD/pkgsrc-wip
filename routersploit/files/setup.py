6b0c699928c029157a369f253c94928b08da25fc
Leonardo Taccari <leot@NetBSD.org>
2018-01-25 21:59:16 +0100

    routersploit: Add install_requires to setup.py
    Register needed dependencies via install_requires of setup.py too
    and update PLIST accordingly.

--- routersploit/files/setup.py
+++ routersploit/files/setup.py
@@ -13,6 +13,12 @@ setup(
 	license = "BSD",
 	url = "https://github.com/reverse-shell/routersploit",
 	packages=find_packages(),
+	install_requires=[
+		'requests>=2.9.1',
+		'paramiko>=1.16.0',
+		'beautifulsoup4>=4.4.1',
+		'pysnmp>=4.3.2',
+	],
 	package_data = {'' : ['*.txt']},
 	include_package_data = True,
 	scripts = ('rsf.py',)
