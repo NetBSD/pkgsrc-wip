42770839e8bd891aae60e23fe2c122c18e3b9e67
Roland Illig <roland.illig@gmx.de>
2018-01-27 21:12:47 +0000

    wip/j*: fix pkglint indentation warnings
    pkglint -Wall -F -o aligned j*/

--- jellyfish/buildlink3.mk
+++ jellyfish/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	jellyfish
 JELLYFISH_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.jellyfish+=	jellyfish>=1.1.11
-BUILDLINK_PKGSRCDIR.jellyfish?=	../../wip/jellyfish
+BUILDLINK_PKGSRCDIR.jellyfish?=		../../wip/jellyfish
 .endif	# JELLYFISH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-jellyfish
