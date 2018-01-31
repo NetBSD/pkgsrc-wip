5466fac7c456b4d6789e25df402c5471e270d126
Thomas Klausner <tk@giga.or.at>
2018-01-31 11:39:05 +0100

    calibre3: fix build and PLIST.

--- calibre3/patches/patch-setup_build.py
+++ calibre3/patches/patch-setup_build.py
@@ -21,12 +21,3 @@ Fix linking against native X.
          win32 {{
              LIBS += {py_lib}
              TARGET_EXT = .dll
-@@ -456,7 +458,7 @@ class Build(Command):
-         if iswindows:
-             qmc += ['-spec', qmakespec]
-         fext = 'dll' if iswindows else 'dylib' if isosx else 'so'
--        name = '%s%s.%s' % ('release/' if iswindows else 'lib', sip['target'], fext)
-+        name = '%s%s.%s' % ('release/' if iswindows else '.libs/lib', sip['target'], fext)
-         try:
-             os.chdir(src_dir)
-             if self.newer(dest, sip['headers'] + sip['sources'] + ext.sources + ext.headers):
