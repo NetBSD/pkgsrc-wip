e5c337d15419fac96163d336fa4eb9df19c4760e
Nia Alarie <nia.alarie@gmail.com>
2018-02-11 23:42:36 +0000

    osg: Update to 3.4.1. Disabling some dependencies to match freebsd's defaults.

--- osg/patches/patch-applications_present3D_Cluster.cpp
+++ osg/patches/patch-applications_present3D_Cluster.cpp
@@ -1,24 +1,26 @@
-$NetBSD: patch-applications_present3D_Cluster.cpp,v 1.2 2012/05/20 19:28:19 marino Exp $
+$NetBSD$
 
 Support more platforms.
 
---- applications/present3D/Cluster.cpp.orig	2015-04-13 10:43:56.000000000 +0000
+--- applications/present3D/Cluster.cpp.orig	2017-08-28 16:50:49.000000000 +0000
 +++ applications/present3D/Cluster.cpp
-@@ -33,7 +33,7 @@
+@@ -33,7 +33,8 @@
  #if defined(__linux)
      #include <unistd.h>
      #include <linux/sockios.h>
--#elif defined(__FreeBSD__) || defined(__DragonFly__)
-+#elif defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__)
+-#elif defined(__FreeBSD__) || defined(__DragonFly__) || defined(__FreeBSD_kernel__)
++#elif defined(__FreeBSD__) || defined(__DragonFly__) || defined(__FreeBSD_kernel__) || \
++    defined(__NetBSD__) || defined(__OpenBSD__)
      #include <unistd.h>
      #include <sys/sockio.h>
  #elif defined(__sgi)
-@@ -338,7 +338,7 @@ void Receiver::sync( void )
+@@ -340,7 +341,8 @@ void Receiver::sync( void )
      }
  
  #if defined(__linux) || defined(__FreeBSD__) || defined( __APPLE__ ) || \
--    defined(__DragonFly__)
-+    defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__)
+-    defined(__DragonFly__) || defined(__FreeBSD_kernel__) || defined(__GNU__)
++    defined(__DragonFly__) || defined(__FreeBSD_kernel__) || defined(__GNU__) || \
++    defined(__NetBSD__) || defined(__OpenBSD__)
      socklen_t
  #else
      int
