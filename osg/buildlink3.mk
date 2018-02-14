ef42f434893dc9216682cbc89e6a98c501542387
Nia Alarie <nia.alarie@gmail.com>
2018-02-14 15:16:01 +0000

    osg: Fixing pkglint warnings

--- osg/buildlink3.mk
+++ osg/buildlink3.mk
@@ -9,21 +9,19 @@ BUILDLINK_API_DEPENDS.osg+=	osg>=2.8.3
 BUILDLINK_ABI_DEPENDS.osg+=	osg>=3.0.1nb37
 BUILDLINK_PKGSRCDIR.osg?=	../../wip/osg
 
-.include "../../graphics/cairo/buildlink3.mk"
-.include "../../graphics/freetype2/buildlink3.mk"
+.if ${OPSYS} != "Darwin"
 .include "../../graphics/glu/buildlink3.mk"
-.include "../../graphics/jasper/buildlink3.mk"
-.include "../../mk/jpeg.buildlink3.mk"
 .include "../../graphics/giflib/buildlink3.mk"
-.include "../../graphics/librsvg/buildlink3.mk"
-.include "../../graphics/MesaLib/buildlink3.mk"
-.include "../../graphics/openexr/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
-.include "../../www/curl/buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
+.include "../../multimedia/ffmpeg2/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/libXrandr/buildlink3.mk"
-.include "../../multimedia/ffmpeg2/buildlink3.mk"
+.endif
+.include "../../graphics/freetype2/buildlink3.mk"
+
 .endif	# OSG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-osg
