96532481e730cf4c43bc0dcc9b8a20fc7a3a1993
Kamil Rytarowski <n54@gmx.com>
2018-02-15 17:03:31 +0100

    compiler-rt-netbsd: Update to SVN r. 325249
    Patches merged upstream:
    
    r325245 - Enable syscall-specific functions in TSan/NetBSD
    r325246 - Enable test/asan for NetBSD
    r325248 - Implement a large part of NetBSD syscalls of netbsd_syscall_hooks.h
    
    I've helped to land FreeBSD code for XRay, NetBSD will follow it soon.
    
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-lib_tsan_rtl_tsan__interceptors.cc
+++ compiler-rt-netbsd/patches/patch-lib_tsan_rtl_tsan__interceptors.cc
@@ -1,13 +1,2 @@
 $NetBSD$
 
---- lib/tsan/rtl/tsan_interceptors.cc.orig	2018-02-15 04:24:50.078973737 +0000
-+++ lib/tsan/rtl/tsan_interceptors.cc
-@@ -2424,7 +2424,7 @@ struct ScopedSyscall {
-   }
- };
- 
--#if !SANITIZER_FREEBSD && !SANITIZER_MAC && !SANITIZER_NETBSD
-+#if !SANITIZER_FREEBSD && !SANITIZER_MAC
- static void syscall_access_range(uptr pc, uptr p, uptr s, bool write) {
-   TSAN_SYSCALL();
-   MemoryAccessRange(thr, pc, p, s, write);
