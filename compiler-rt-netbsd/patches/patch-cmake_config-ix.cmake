9ac779038d1899d40c282063fb4e1ce4721df5b0
Kamil Rytarowski <n54@gmx.com>
2018-02-19 19:41:37 +0100

    compiler-rt-netbsd: Finish the previous commit
    X-Ray initial NetBSD support.
    
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-cmake_config-ix.cmake
+++ compiler-rt-netbsd/patches/patch-cmake_config-ix.cmake
@@ -1,6 +1,6 @@
 $NetBSD$
 
---- cmake/config-ix.cmake.orig	2018-02-15 16:00:57.000000000 +0000
+--- cmake/config-ix.cmake.orig	2018-02-19 18:40:37.199984867 +0000
 +++ cmake/config-ix.cmake
 @@ -542,7 +542,7 @@ else()
  endif()
@@ -11,7 +11,7 @@ $NetBSD$
    set(COMPILER_RT_HAS_PROFILE TRUE)
  else()
    set(COMPILER_RT_HAS_PROFILE FALSE)
-@@ -590,14 +590,14 @@ else()
+@@ -590,7 +590,7 @@ else()
  endif()
  
  if (COMPILER_RT_HAS_SANITIZER_COMMON AND SCUDO_SUPPORTED_ARCH AND
@@ -20,11 +20,3 @@ $NetBSD$
    set(COMPILER_RT_HAS_SCUDO TRUE)
  else()
    set(COMPILER_RT_HAS_SCUDO FALSE)
- endif()
- 
- if (COMPILER_RT_HAS_SANITIZER_COMMON AND XRAY_SUPPORTED_ARCH AND
--    OS_NAME MATCHES "Darwin|Linux|FreeBSD")
-+    OS_NAME MATCHES "Darwin|Linux|FreeBSD|NetBSD")
-   set(COMPILER_RT_HAS_XRAY TRUE)
- else()
-   set(COMPILER_RT_HAS_XRAY FALSE)
