6a0f682ed0c864d9d3f06213a9f694a757ea6c88
Pierre Pronchery <khorben@defora.org>
2018-02-23 01:55:26 +0100

    Package bitcoin version 0.14.1
    This takes it one step closer to the current upstream.
    
    For the extensive release notes, please refer to:
    https://github.com/bitcoin/bitcoin/blob/master/doc/release-notes/release-notes-0.14.1.md

--- bitcoin/patches/patch-src_netbase.cpp
+++ bitcoin/patches/patch-src_netbase.cpp
@@ -4,18 +4,14 @@ It is unclear why this patch exists.  NetBSD 6 and 7 have AI_ADDRCONFIG,
 although NetBSD 5 does not.  Arguably upstream should have an autoconf
 test instead.  This is not known to be reported upstream.
 
---- src/netbase.cpp.orig	2015-07-10 17:23:55.000000000 +0000
+--- src/netbase.cpp.orig	2017-04-20 09:28:25.000000000 +0000
 +++ src/netbase.cpp
-@@ -126,8 +126,12 @@ bool static LookupIntern(const char *psz
- #ifdef WIN32
+@@ -95,7 +95,7 @@ bool static LookupIntern(const char *psz
+     aiHint.ai_socktype = SOCK_STREAM;
+     aiHint.ai_protocol = IPPROTO_TCP;
+     aiHint.ai_family = AF_UNSPEC;
+-#ifdef WIN32
++#ifndef AI_ADDRCONFIG
      aiHint.ai_flags = fAllowLookup ? 0 : AI_NUMERICHOST;
  #else
-+#if defined(__NetBSD__)
-+    aiHint.ai_flags = fAllowLookup ? 0 : AI_NUMERICHOST;
-+#else
      aiHint.ai_flags = fAllowLookup ? AI_ADDRCONFIG : AI_NUMERICHOST;
- #endif
-+#endif
- 
-     struct addrinfo *aiRes = NULL;
- #ifdef HAVE_GETADDRINFO_A
