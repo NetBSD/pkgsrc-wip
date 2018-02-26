6579596d5039d76668eefabd2503649acfffe607
Thomas Klausner <tk@giga.or.at>
2018-02-26 09:48:04 +0100

    gnucash-git: adapt to upstream changes.

--- gnucash-git/patches/patch-gnucash_gnucash-bin.c
+++ gnucash-git/patches/patch-gnucash_gnucash-bin.c
@@ -4,7 +4,7 @@ Allow starting gnucash in the background.
 python's readline module causes a SIGTTOU to be sent and if this
 is not handled (even by ignoring it), it stops the process.
 
---- gnucash/gnucash-bin.c.orig	2017-10-28 21:23:58.000000000 +0000
+--- gnucash/gnucash-bin.c.orig	2018-02-26 08:15:09.000000000 +0000
 +++ gnucash/gnucash-bin.c
 @@ -22,6 +22,7 @@
   */
@@ -14,7 +14,7 @@ is not handled (even by ignoring it), it stops the process.
  #include <stdlib.h>
  #include <stdio.h>
  #include <string.h>
-@@ -608,6 +609,8 @@ inner_main (void *closure, int argc, cha
+@@ -602,6 +603,8 @@ inner_main (void *closure, int argc, cha
      SCM main_mod;
      char* fn;
  
@@ -22,4 +22,4 @@ is not handled (even by ignoring it), it stops the process.
 +
      scm_c_eval_string("(debug-set! stack 200000)");
  
-     main_mod = scm_c_resolve_module("gnucash main");
+     main_mod = scm_c_resolve_module("gnucash utilities");
