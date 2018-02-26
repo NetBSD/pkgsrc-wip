2fbe496bcf4ccbe0abc51f74235fc40bf1ec3f9a
Pierre Pronchery <khorben@defora.org>
2018-02-26 03:53:04 +0100

    Add a buildlink3.mk file for py-pyvex
    I do not think this is really correct. vex should be packaged
    independently of its Python bindings at the very least - tricky given
    the build system from Angr.
    
    At least it gets me a few steps further with Angr.

---
+++ py-pyvex/buildlink3.mk
@@ -0,0 +1,12 @@
+BUILDLINK_TREE+=	py-pyvex
+
+.if !defined(PY-PYVEX_BUILDLINK3_MK)
+PY-PYVEX_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.py-pyvex+=	${PYPKGPREFIX}-pyvex>=7.8.2.21
+BUILDLINK_PKGSRCDIR.py-pyvex?=	../../wip/py-pyvex
+
+. include "../../emulators/unicorn/buildlink3.mk"
+.endif  # PY-PYVEX_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-py-pyvex
