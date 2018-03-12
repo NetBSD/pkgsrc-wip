9ff4f5142a832420770b61af519cd1cd16cfa343
Kamil Rytarowski <n54@gmx.com>
2018-03-12 21:43:40 +0100

    frida-libffi: Update buildlink3.mk

--- frida-libffi/buildlink3.mk
+++ frida-libffi/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2012/09/15 00:22:43 asau Exp $
+# $NetBSD$
 
-BUILDLINK_TREE+=	libffi
+BUILDLINK_TREE+=	frida-libffi
 
-.if !defined(LIBFFI_BUILDLINK3_MK)
-LIBFFI_BUILDLINK3_MK:=
+.if !defined(FRIDA_LIBFFI_BUILDLINK3_MK)
+FRIDA_LIBFFI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libffi+=	libffi>=1.20
-BUILDLINK_ABI_DEPENDS.libffi+=	libffi>=3.0.11
-BUILDLINK_PKGSRCDIR.libffi?=	../../devel/libffi
-.endif # LIBFFI_BUILDLINK3_MK
+BUILDLINK_API_DEPENDS.frida-libffi+=	frida-libffi>=3.0.13
+BUILDLINK_PKGSRCDIR.frida-libffi?=	../../wip/frida-libffi
+.endif	# FRIDA_LIBFFI_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-libffi
+BUILDLINK_TREE+=	-frida-libffi
