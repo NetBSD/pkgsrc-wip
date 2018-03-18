ce5f8ebd3a88d82b2b713b9aa6887492a1b99ebf
Yorick Hardy <yorickhardy@gmail.com>
2018-03-18 16:37:49 +0200

    SDLPoP: update to SDLPoP-1.18
    2018 March 17
    =================
    (version 1.18)
    
    FIXED: Fixed allow_triggering_any_tile hack to work with stuck-closed chompers.
    DONE: Linux: Added .desktop file and make install/uninstall.
    DONE: Added option to enable lighting.
    ADDED: You can make screenshots, both manually with a key or automatically from the command line.
    ADDED: You can also make screenshots of the whole level.
    ADDED: Whole-level screenshots can optionally show extra info, like door events.
    ADDED: You can also start level 0 (demo) and level 15 (potions) from the command line.
    ADDED: Automatically switch to keyboard or joystick/gamepad mode if there is input from that device.
    FIXED: Fixed indentation (use tabs) and alignment (use spaces).
    FIXED: Fixed compiling with MSVC and improve build scripts.
    FIXED: On Windows, use Unicode/UTF-8 (instead of the current codepage) for replay filenames and directory names.
    FIXED: Fixed the quicksave note in the splash: save = F6, load = F9.
    FIXED: Ctrl+S now also mutes music.
    FIXED: Fixed implicit-function-declaration warnings. (implemented by @Rondom)
    DONE: Use 8-bit audio samples as a workaround if using older (buggy) SDL2 versions (older than 2.0.4).
    FIXED: Removed redefinition of SDL_COMPILE_TIME_ASSERT.
    FIXED: Add (back) SDL_Joystick interface because not all joysticks are SDL_Controller compatible. (implemented by @vanfanel)
    DONE: Convert waves to mixer sounds when they are loaded, not when they are first played. (suggested by @vanfanel)
    FIXED: Updated Readme.txt - fix macOS instructions.
    DONE: Added option to force pixel perfect scaling (integer scale factors).
    FIXED: Show level number when cycling between replays with Tab.
    DONE: Added option to change scaling method (sharp, fuzzy, blurry).
    FIXED: Improved the accuracy of timers.
    DONE: The debug timer (press T while running with debug cheats on) is now drawn transparently.
    DONE: The left-to-right animation in the intro now runs at a fixed speed (instead of being CPU limited).
    DONE: Added in-game menu (press Escape or Backspace) with ability to change settings while in-game.
    CHANGE: Removed the startup prompt to enable or disable fixes/enhancements (no longer needed).
    FIXED: Fixed turning off music not working.
    DONE: Added option for disabling and enabling all customization options at once.
    DONE: Added option for changing the mods directory (where the game looks for custom levelsets).
    DONE: Added fix for hidden floors in level 12 not appearing until after the flashing stops.
    FIXED: Removed colored rectangles around hitpoints and the shadow, when using Brain's SNES graphics.
    DONE: Added most of the missing CusPop customization options.
    DONE: Added support for MIDI music.
    FIXED: Fixed teleporting to the room above by grabbing a ledge (integer overflow bug).
    FIXED: When playing back a recording, Alt+Tabbing back to SDLPoP stops the replay if Alt is released before Tab.

--- SDLPoP/patches/patch-options.c
+++ SDLPoP/patches/patch-options.c
@@ -3,24 +3,24 @@ $NetBSD$
 Use installed data path.
 Disable rumble/haptic support.
 
---- src/options.c.orig	2017-02-05 07:38:56.000000000 +0000
+--- src/options.c.orig	2018-03-17 16:33:14.000000000 +0000
 +++ src/options.c
-@@ -207,7 +207,8 @@ static int global_ini_callback(const cha
- 		process_word("pop_window_width", &pop_window_width, NULL);
- 		process_word("pop_window_height", &pop_window_height, NULL);
+@@ -185,7 +185,8 @@ static int global_ini_callback(const cha
  		process_boolean("use_correct_aspect_ratio", &use_correct_aspect_ratio);
--        process_boolean("enable_controller_rumble", &enable_controller_rumble);
-+        // process_boolean("enable_controller_rumble", &enable_controller_rumble);
-+        enable_controller_rumble = 0;
-         process_boolean("joystick_only_horizontal", &joystick_only_horizontal);
+ 		process_boolean("use_integer_scaling", &use_integer_scaling);
+ 		process_byte("scaling_type", &scaling_type, &scaling_type_names_list);
+-		process_boolean("enable_controller_rumble", &enable_controller_rumble);
++		// process_boolean("enable_controller_rumble", &enable_controller_rumble);
++		enable_controller_rumble = 0;
+ 		process_boolean("joystick_only_horizontal", &joystick_only_horizontal);
+ 		process_int("joystick_threshold", &joystick_threshold, NULL);
  
-         if (strcasecmp(name, "levelset") == 0) {
-@@ -369,7 +370,7 @@ static int mod_ini_callback(const char *
- }
+@@ -436,7 +437,7 @@ void set_options_to_default() {
  
  void load_global_options() {
--    ini_load("SDLPoP.ini", global_ini_callback); // global configuration
-+    ini_load(PKGSRC_DATA_PATH "SDLPoP.ini", global_ini_callback); // global configuration
+ 	set_options_to_default();
+-	ini_load(locate_file("SDLPoP.ini"), global_ini_callback); // global configuration
++	ini_load(PKGSRC_DATA_PATH "SDLPoP.ini", global_ini_callback); // global configuration
  }
  
  void check_mod_param() {
--- SDLPoP/patches/patch-seg009.c
+++ SDLPoP/patches/patch-seg009.c
@@ -3,59 +3,25 @@ $NetBSD$
 Use installed data path.
 Disable rumble/haptic support.
 
---- src/seg009.c.orig	2017-02-05 07:38:56.000000000 +0000
+--- src/seg009.c.orig	2018-03-17 16:33:14.000000000 +0000
 +++ src/seg009.c
-@@ -152,7 +152,7 @@ static FILE* open_dat_from_root_or_data_
- 	// if failed, try if the DAT file can be opened in the data/ directory, instead of the main folder
- 	if (fp == NULL) {
- 		char data_path[POP_MAX_PATH];
--		snprintf(data_path, sizeof(data_path), "data/%s", filename);
-+		snprintf(data_path, sizeof(data_path), "%s/data/%s", PKGSRC_DATA_PATH, filename);
+@@ -34,8 +34,8 @@ void sdlperror(const char* header) {
+ 	//quit(1);
+ }
  
- 		// verify that this is a regular file and not a directory (otherwise, don't open)
- 		struct stat path_stat;
-@@ -1644,7 +1644,7 @@ const int max_sound_id = 58;
- char** sound_names = NULL;
+-char exe_dir[POP_MAX_PATH] = ".";
+-bool found_exe_dir = false;
++char *exe_dir = PKGSRC_DATA_PATH;
++bool found_exe_dir = true;
  
- void load_sound_names() {
--	const char* names_path = "data/music/names.txt";
-+	const char* names_path = PKGSRC_DATA_PATH "data/music/names.txt";
- 	if (sound_names != NULL) return;
- 	FILE* fp = fopen(names_path,"rt");
- 	if (fp==NULL) return;
-@@ -1692,7 +1692,7 @@ sound_buffer_type* load_sound(int index)
- 				const char* ext=exts[i];
- 				struct stat info;
- 
--				snprintf(filename, sizeof(filename), "data/music/%s.%s", sound_name(index), ext);
-+				snprintf(filename, sizeof(filename), "%s/data/music/%s.%s", PKGSRC_DATA_PATH, sound_name(index), ext);
- 				// Skip nonexistent files:
- 				if (stat(filename, &info))
- 					continue;
-@@ -1933,7 +1933,7 @@ int __pascal far check_sound_playing() {
- // seg009:38ED
- void __pascal far set_gr_mode(byte grmode) {
+ void find_exe_dir() {
+ 	if (found_exe_dir) return;
+@@ -2108,7 +2108,7 @@ void __pascal far set_gr_mode(byte grmod
+ 	SDL_SetHint(SDL_HINT_WINDOWS_DISABLE_THREAD_NAMING, "1");
+ #endif
  	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_NOPARACHUTE |
--				 SDL_INIT_GAMECONTROLLER | SDL_INIT_HAPTIC ) != 0) {
-+				 SDL_INIT_GAMECONTROLLER ) != 0) {
+-	             SDL_INIT_GAMECONTROLLER | SDL_INIT_HAPTIC ) != 0) {
++	             SDL_INIT_GAMECONTROLLER ) != 0) {
  		sdlperror("SDL_Init");
  		quit(1);
  	}
-@@ -1957,7 +1957,7 @@ void __pascal far set_gr_mode(byte grmod
- 										  pop_window_width, pop_window_height, flags);
- 	renderer_ = SDL_CreateRenderer(window_, -1 , SDL_RENDERER_ACCELERATED );
- 
--	SDL_Surface* icon = IMG_Load("data/icon.png");
-+	SDL_Surface* icon = IMG_Load(PKGSRC_DATA_PATH "data/icon.png");
- 	if (icon == NULL) {
- 		sdlperror("Could not load icon");
- 	} else {
-@@ -2109,7 +2109,7 @@ void load_from_opendats_metadata(int res
- 			if (len >= 5 && filename_no_ext[len-4] == '.') {
- 				filename_no_ext[len-4] = '\0'; // terminate, so ".DAT" is deleted from the filename
- 			}
--			snprintf(image_filename,sizeof(image_filename),"data/%s/res%d.%s",filename_no_ext, resource_id, extension);
-+			snprintf(image_filename,sizeof(image_filename),"%s/data/%s/res%d.%s", PKGSRC_DATA_PATH, filename_no_ext, resource_id, extension);
- 			if (!use_custom_levelset) {
- 				//printf("loading (binary) %s",image_filename);
- 				fp = fopen(image_filename, "rb");
