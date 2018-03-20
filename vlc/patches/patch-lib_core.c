990dfe35ca1f8c34b57205648cb05ca61dd02448
Kamil Rytarowski <n54@gmx.com>
2018-03-20 14:59:06 +0100

    vlc: Fix build

---
+++ vlc/patches/patch-lib_core.c
@@ -0,0 +1,10 @@
+$NetBSD$
+
+--- lib/core.c.orig	2017-11-24 15:29:17.000000000 +0000
++++ lib/core.c
+@@ -257,4 +257,4 @@ int64_t libvlc_clock(void)
+     return mdate();
+ }
+ 
+-const char vlc_module_name[] = "libvlc";
++// const char vlc_module_name[] = "libvlc";
