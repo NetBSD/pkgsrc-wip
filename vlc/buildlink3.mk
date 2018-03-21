d8544bfa2b2b243579c772db264082cdd00c7de2
Kamil Rytarowski <n54@gmx.com>
2018-03-21 01:06:02 +0100

    vlc: Update blk3

--- vlc/buildlink3.mk
+++ vlc/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	vlc
 .if !defined(VLC_BUILDLINK3_MK)
 VLC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vlc+=	vlc>=2.2
+BUILDLINK_API_DEPENDS.vlc+=	vlc>=3.0.1
 BUILDLINK_PKGSRCDIR.vlc?=	../../multimedia/vlc
 
 .endif	# VLC_BUILDLINK3_MK
