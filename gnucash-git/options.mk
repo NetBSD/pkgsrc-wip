01ce8f5b71537342f5ff6cdc6669bcdef583de71
Thomas Klausner <tk@giga.or.at>
2018-03-25 11:30:46 +0200

    gnucash-git: sync with gnucash-devel

--- gnucash-git/options.mk
+++ gnucash-git/options.mk
@@ -2,43 +2,41 @@
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnucash
 PKG_SUPPORTED_OPTIONS=	libdbi libofx python
-PKG_SUGGESTED_OPTIONS=	libdbi libofx
+PKG_SUGGESTED_OPTIONS=	libdbi libofx python
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=	dbi ofx
-
 .if !empty(PKG_OPTIONS:Mlibofx)
-PLIST.ofx=	yes
+CMAKE_ARGS+=	-DWITH_OFX=ON
+PLIST_SRC+=	PLIST.ofx
 .include "../../finance/libofx/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=	--disable-ofx
+CMAKE_ARGS+=	-DWITH_OFX=OFF
 .endif
 
 .if !empty(PKG_OPTIONS:Mlibdbi)
-CONFIGURE_ARGS+=	--enable-dbi
-PLIST.dbi=		yes
-
-# to make a configure test happy
-BUILD_DEPENDS+=		libdbi-driver-sqlite3-[0-9]*:../../databases/libdbi-driver-sqlite3
+CMAKE_ARGS+=	-DWITH_SQL=ON
+PLIST_SRC+=	PLIST.dbi
+BUILD_DEPENDS+=	libdbi-driver-sqlite3-[0-9]*:../../databases/libdbi-driver-sqlite3
 .include "../../databases/libdbi/buildlink3.mk"
 .else
-CONFIGURE_ARGS+=	--disable-dbi
+CMAKE_ARGS+=	-DWITH_SQL=OFF
 .endif
 
 .if !empty(PKG_OPTIONS:Mpython)
-CONFIGURE_ARGS+=		--enable-python
-PYTHON_VERSIONS_INCOMPATIBLE=	34 35 36 # not yet ported as of 2.6.1
-REPLACE_PYTHON+=		src/python/pycons/*.py src/python/pycons/pycons
+CMAKE_ARGS+=			-DWITH_PYTHON=ON
+PYTHON_VERSIONS_INCOMPATIBLE=	27
+REPLACE_PYTHON+=		bindings/python/example_scripts/*.py
+REPLACE_PYTHON+=		bindings/python/example_scripts/rest-api/*.py
+REPLACE_PYTHON+=		gnucash/python/pycons/*.py
 PY_PATCHPLIST=			yes
 PLIST_SRC+=			PLIST.python
 
-DEPENDS+=		${PYPKGPREFIX}-gtk2-[0-9]*:../../x11/py-gtk2
 # /usr/pkg/share/gnucash/python/pycons/console.py imports readline
 DEPENDS+=		${PYPKGPREFIX}-readline-[0-9]*:../../devel/py-readline
 
 .include "../../lang/python/application.mk"
 .include "../../lang/python/extension.mk"
 .else
-CONFIGURE_ARGS+=	--disable-python
+CMAKE_ARGS+=			-DWITH_PYTHON=OFF
 .endif
