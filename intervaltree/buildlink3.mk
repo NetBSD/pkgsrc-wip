264ad87ebcd67fd61fd829ce2accb0a0b3815c95
Winston Weinert <winston@ml1.net>
2018-03-30 05:48:06 -0500

    intervaltree: update, patch Makefile to fix perms, general cleanup

--- intervaltree/buildlink3.mk
+++ intervaltree/buildlink3.mk
@@ -1,13 +1,4 @@
 # $NetBSD$
-# XXX
-# XXX This file was created automatically using createbuildlink-3.16.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
 
 BUILDLINK_TREE+=	intervaltree
 
@@ -15,7 +6,7 @@ BUILDLINK_TREE+=	intervaltree
 INTERVALTREE_BUILDLINK3_MK:=
 BUILDLINK_DEPMETHOD.intervaltree?=	build
 
-BUILDLINK_API_DEPENDS.intervaltree+=	intervaltree>=49a0e290efaaa760e0b03e52ca18ac03e7582f23
+BUILDLINK_API_DEPENDS.intervaltree+=	intervaltree>=0.0.0.20151213
 BUILDLINK_PKGSRCDIR.intervaltree?=	../../wip/intervaltree
 .endif	# INTERVALTREE_BUILDLINK3_MK
 
