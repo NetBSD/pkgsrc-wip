3c29741a7ab7db0abdea4a65b7f7ad7c6171cebf
Jason Bacon <bacon4000@gmail.com>
2018-03-30 12:17:41 -0500

    fastahack: Clean up

--- fastahack/buildlink3.mk
+++ fastahack/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	fastahack
 .if !defined(FASTAHACK_BUILDLINK3_MK)
 FASTAHACK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fastahack+=	fastahack>=0.0.0.20160702 
+BUILDLINK_API_DEPENDS.fastahack+=	fastahack>=0.0.0.20160702
 BUILDLINK_PKGSRCDIR.fastahack?=		../../wip/fastahack
 .endif	# FASTAHACK_BUILDLINK3_MK
 
