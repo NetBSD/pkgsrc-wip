1163fe76a0b004ae675fe8c9f5f38d5fd7b933eb
Winston Weinert <winston@ml1.net>
2018-03-30 05:23:52 -0500

    multichoose: Update, use upstream, own Makefile

--- multichoose/buildlink3.mk
+++ multichoose/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	multichoose
 .if !defined(MULTICHOOSE_BUILDLINK3_MK)
 MULTICHOOSE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.multichoose+=	multichoose>=d60d639af93f8720e019d824bb2405eb4523fb16
+BUILDLINK_API_DEPENDS.multichoose+=	multichoose>=0.0.0.20151117
 BUILDLINK_PKGSRCDIR.multichoose?=	../../wip/multichoose
 .endif	# MULTICHOOSE_BUILDLINK3_MK
 
