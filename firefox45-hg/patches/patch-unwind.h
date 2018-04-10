718dbb2cf44629416350c6250d9c96210b1f89ac
coypu <coypu@sdf.org>
2018-04-10 16:10:20 +0300

    firefox-hg, firefox-dfbsd, firefox45-hg: remove
    these packages are too old to be useful (60 soon to be released)

--- firefox45-hg/patches/patch-unwind.h
+++
@@ -1,9 +0,0 @@
-$NetBSD: patch-unwind.h,v 1.1 2014/09/29 10:46:26 thomasklausner Exp $
-
---- unwind.h.orig	2014-09-28 09:05:44.000000000 +0000
-+++ unwind.h
-@@ -0,0 +1,4 @@
-+#pragma GCC system_header
-+#pragma GCC visibility push(default)
-+#include_next <unwind.h>
-+#pragma GCC visibility pop
