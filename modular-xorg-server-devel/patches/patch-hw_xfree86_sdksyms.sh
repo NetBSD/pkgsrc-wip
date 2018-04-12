956d60339a4472739705548be18ad15a1ec028c6
Thomas Klausner <tk@giga.or.at>
2018-04-12 08:21:51 +0200

    modular-xorg-server-devel: fix build
    From Adam Jackson <ajax@redhat.com>.

--- modular-xorg-server-devel/patches/patch-hw_xfree86_sdksyms.sh
+++ modular-xorg-server-devel/patches/patch-hw_xfree86_sdksyms.sh
@@ -5,9 +5,12 @@ It returns pid_t which on NetBSD is #define pid_t __pid_t
 This slightly alters the GCC preprocessor output which this fragile
 code could not deal with when using GCC 5+
 
---- hw/xfree86/sdksyms.sh.orig	2016-03-08 20:22:17.000000000 +0000
+
+Ignore empty symbols, from Adam Jackson <ajax@redhat.com>
+
+--- hw/xfree86/sdksyms.sh.orig	2018-04-10 19:46:09.000000000 +0000
 +++ hw/xfree86/sdksyms.sh
-@@ -357,6 +357,17 @@ BEGIN {
+@@ -373,6 +373,17 @@ BEGIN {
                 n = 1;
              }
          }
@@ -25,3 +28,13 @@ code could not deal with when using GCC 5+
  
  	# type specifier may not be set, as in
  	#   extern _X_EXPORT unsigned name(...)
+@@ -415,7 +426,8 @@ BEGIN {
+ 	sub(/[^a-zA-Z0-9_].*/, "", symbol);
+ 
+ 	#print;
+-	printf("    (void *) &%-50s /* %s:%s */\n", symbol ",", header, line);
++	if (symbol != "")
++	    printf("    (void *) &%-50s /* %s:%s */\n", symbol ",", header, line);
+     }
+ }
+ 
