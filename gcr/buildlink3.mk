a4117a4688a57f8643e5bdfab95bf9615fa3ca15
Thomas Klausner <tk@giga.or.at>
2018-04-17 15:27:55 +0200

    p11-kit: remove, pkgsrc/security/p11-kit is newer

--- gcr/buildlink3.mk
+++ gcr/buildlink3.mk
@@ -8,7 +8,7 @@ GCR_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gcr+=	gcr>=3.18.0
 BUILDLINK_PKGSRCDIR.gcr?=	../../wip/gcr
 
-.include "../../wip/p11-kit/buildlink3.mk"
+.include "../../security/p11-kit/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
 .endif	# GCR_BUILDLINK3_MK
 
