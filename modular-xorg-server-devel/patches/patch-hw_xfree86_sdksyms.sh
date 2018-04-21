afc6836c67aeaf1ddac6b8717dde6589a5d481c8
Thomas Klausner <tk@giga.or.at>
2018-04-21 11:42:40 +0200

    modular-xorg-server-devel: fix uint32_t problem same way as pid_t problem.

--- modular-xorg-server-devel/patches/patch-hw_xfree86_sdksyms.sh
+++ modular-xorg-server-devel/patches/patch-hw_xfree86_sdksyms.sh
@@ -5,18 +5,15 @@ It returns pid_t which on NetBSD is #define pid_t __pid_t
 This slightly alters the GCC preprocessor output which this fragile
 code could not deal with when using GCC 5+
 
-
-Ignore empty symbols, from Adam Jackson <ajax@redhat.com>
-
 --- hw/xfree86/sdksyms.sh.orig	2018-04-10 19:46:09.000000000 +0000
 +++ hw/xfree86/sdksyms.sh
 @@ -373,6 +373,17 @@ BEGIN {
                 n = 1;
              }
          }
-+	# hack: pid_t becomes __pid_t on NetBSD.
++	# hack: pid_t becomes __pid_t on NetBSD, same for uint32_t -> __uint32_t.
 +	# GCC 5 inserts additional lines around this.
-+        if ($1 == "__pid_t" && NF == 1) {
++        if (($1 == "__pid_t" || $1 == "__uint32_t") && NF == 1) {
 +            getline;
 +            n++;
 +            # skip line numbers GCC 5 adds (after typedef return type?)
@@ -28,13 +25,3 @@ Ignore empty symbols, from Adam Jackson <ajax@redhat.com>
  
  	# type specifier may not be set, as in
  	#   extern _X_EXPORT unsigned name(...)
-@@ -415,7 +426,8 @@ BEGIN {
- 	sub(/[^a-zA-Z0-9_].*/, "", symbol);
- 
- 	#print;
--	printf("    (void *) &%-50s /* %s:%s */\n", symbol ",", header, line);
-+	if (symbol != "")
-+	    printf("    (void *) &%-50s /* %s:%s */\n", symbol ",", header, line);
-     }
- }
- 
