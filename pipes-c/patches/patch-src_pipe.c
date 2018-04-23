f90b1cd88ce8e4d95042dc83aa7322a9dc18f671
Travis Paul <tr@vispaul.me>
2018-04-23 14:28:49 +0000

    pipes-c: Update patches to match upstream fix

--- pipes-c/patches/patch-src_pipe.c
+++ pipes-c/patches/patch-src_pipe.c
@@ -1,19 +1,34 @@
 $NetBSD$
-# This enum has a symbol (UP) which conflicts with termcap.h
-# https://github.com/pipeseroni/pipes.c/pull/25
+# Add a CPIPES_ prefix to each member of the DIRECTIONS enum to prevent termcap.h conflict
+# Fixed upstream awaiting new release: https://github.com/pipeseroni/pipes.c/pull/25
 --- src/pipe.c.orig	2018-04-16 23:07:46.000000000 +0000
 +++ src/pipe.c
-@@ -62,6 +62,13 @@ static size_t charwidth(const char *pipe
- //              Right    Down   Left    Up
- char states[][2] = {{1,0}, {0,1}, {-1,0}, {0,-1}};
- 
-+enum DIRECTIONS {
-+    RIGHT = 0,
-+    DOWN = 1,
-+    LEFT = 2,
-+    UP = 3
-+};
-+
- const char * transition_char(char **list, int row, int col){
-     return list[row * 4 + col];
- }
+@@ -266,20 +266,20 @@ void assign_matrices(char *pipe_chars,
+                 break;
+             // Transition chars go ┓,┛,┗,┏
+             case 2:
+-                transition[RIGHT * 4 + DOWN] = c;
+-                transition[UP * 4 + LEFT] = c;
++                transition[CPIPES_RIGHT * 4 + CPIPES_DOWN] = c;
++                transition[CPIPES_UP * 4 + CPIPES_LEFT] = c;
+                 break;
+             case 3:
+-                transition[RIGHT * 4 + UP] = c;
+-                transition[DOWN * 4 + LEFT] = c;
++                transition[CPIPES_RIGHT * 4 + CPIPES_UP] = c;
++                transition[CPIPES_DOWN * 4 + CPIPES_LEFT] = c;
+                 break;
+             case 4:
+-                transition[LEFT * 4 + UP] = c;
+-                transition[DOWN * 4 + RIGHT] = c;
++                transition[CPIPES_LEFT * 4 + CPIPES_UP] = c;
++                transition[CPIPES_DOWN * 4 + CPIPES_RIGHT] = c;
+                 break;
+             case 5:
+-                transition[LEFT * 4 + DOWN] = c;
+-                transition[UP * 4 + RIGHT] = c;
++                transition[CPIPES_LEFT * 4 + CPIPES_DOWN] = c;
++                transition[CPIPES_UP * 4 + CPIPES_RIGHT] = c;
+                 break;
+             default:
+                 // No way to reach here.
--- pipes-c/patches/patch-src_pipe.h
+++ pipes-c/patches/patch-src_pipe.h
@@ -1,18 +1,20 @@
 $NetBSD$
-# This enum has a symbol (UP) which conflicts with termcap.h
-# https://github.com/pipeseroni/pipes.c/pull/25
+# Add a CPIPES_ prefix to each member of the DIRECTIONS enum to prevent termcap.h conflict
+# Fixed upstream awaiting new release: https://github.com/pipeseroni/pipes.c/pull/25
 --- src/pipe.h.orig	2018-04-16 23:07:46.000000000 +0000
 +++ src/pipe.h
-@@ -25,12 +25,6 @@ struct pipe {
-     int x, y;
+@@ -26,10 +26,10 @@ struct pipe {
  };
  
--enum DIRECTIONS {
+ enum DIRECTIONS {
 -    RIGHT = 0,
 -    DOWN = 1,
 -    LEFT = 2,
 -    UP = 3
--};
++    CPIPES_RIGHT = 0,
++    CPIPES_DOWN = 1,
++    CPIPES_LEFT = 2,
++    CPIPES_UP = 3
+ };
  struct palette;
  
- void init_pipe(struct pipe *pipe, struct palette *palette,
