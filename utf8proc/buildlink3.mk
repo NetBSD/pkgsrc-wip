afa84aaf248062db0c4e63edb7aacd0ea1b779b7
Min Sik Kim <msk@dolbo.net>
2018-04-30 00:18:46 -0700

    utf8proc: Update to 2.1.1
    Bug fixes only.

--- utf8proc/buildlink3.mk
+++ utf8proc/buildlink3.mk
@@ -5,9 +5,8 @@ BUILDLINK_TREE+=	utf8proc
 .if !defined(UTF8PROC_BUILDLINK3_MK)
 UTF8PROC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.utf8proc+=	utf8proc>=2.1.0
-BUILDLINK_PKGSRCDIR.utf8proc?=		../../converters/utf8proc
-BUILDLINK_DEPMETHOD.utf8proc?=		build
+BUILDLINK_API_DEPENDS.utf8proc+=	utf8proc>=2.1.1
+BUILDLINK_PKGSRCDIR.utf8proc?=		../../wip/utf8proc
 .endif	# UTF8PROC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-utf8proc
