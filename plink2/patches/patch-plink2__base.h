bf70c7adddc651804a9afe7b08696329cf5ca70f
Jason W. Bacon <bacon@NetBSD.org>
2018-05-03 21:12:49 -0500

    plink2: Work toward NetBSD 7.1 build

--- plink2/patches/patch-plink2__base.h
+++ plink2/patches/patch-plink2__base.h
@@ -1,15 +1,20 @@
 $NetBSD$
 
-# Fix build error
+# Portability
 
---- plink2_base.h.orig	2018-05-03 13:55:25 UTC
+--- plink2_base.h.orig	2018-05-03 22:24:28.000000000 +0000
 +++ plink2_base.h
-@@ -433,7 +433,7 @@ typedef uint32_t BoolErr;
+@@ -435,7 +435,12 @@ typedef uint32_t BoolErr;
  #  define FOPEN_RB "r"
  #  define FOPEN_WB "w"
  #  define FOPEN_AB "a"
 -#  ifdef __APPLE__
-+#  if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__)
++#  if defined(__APPLE__) || defined(__FreeBSD__)
++#    define fread_unlocked fread
++#    define fwrite_unlocked fwrite
++#  endif
++#  if defined(__NetBSD__)
++#    define ferror_unlocked ferror
  #    define fread_unlocked fread
  #    define fwrite_unlocked fwrite
  #  endif
