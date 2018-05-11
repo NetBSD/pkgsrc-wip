48fdceebaf7d6246234e60f6e79ba6dedb2a9cfd
Kamil Rytarowski <n54@gmx.com>
2018-05-11 02:10:17 +0200

    compiler-rt-netbsd: Fix syntax of a local patch
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-lib_fuzzer_FuzzerLoop.cpp
+++ compiler-rt-netbsd/patches/patch-lib_fuzzer_FuzzerLoop.cpp
@@ -1,13 +1,13 @@
 $NetBSD$
 
---- lib/fuzzer/FuzzerLoop.cpp.orig	2018-02-25 10:48:27.000000000 +0000
+--- lib/fuzzer/FuzzerLoop.cpp.orig	2018-05-10 23:16:36.000000000 +0000
 +++ lib/fuzzer/FuzzerLoop.cpp
-@@ -269,7 +269,7 @@ NO_SANITIZE_MEMORY
+@@ -270,7 +270,7 @@ NO_SANITIZE_MEMORY
  void Fuzzer::AlarmCallback() {
    assert(Options.UnitTimeoutSec > 0);
    // In Windows Alarm callback is executed by a different thread.
 -#if !LIBFUZZER_WINDOWS
-+#if !LIBFUZZER_WINDOWS && !LIBFUZZER_NETBSD # XXX: why?
++#if !LIBFUZZER_WINDOWS && !LIBFUZZER_NETBSD // XXX: why?
    if (!InFuzzingThread())
      return;
  #endif
