7427370bd2d9c7fd87dff0d41ff6bc8b7e51edee
Kamil Rytarowski <n54@gmx.com>
2018-05-11 03:17:25 +0200

    llvm-netbsd: Update to SVN r. 332071
    Patch merged upstream:
    r332069 - Register NetBSD/i386 in AddressSanitizer.cpp
    
    Sponsored by <The NetBSD Foundation>

--- llvm-netbsd/patches/patch-lib_Transforms_Instrumentation_AddressSanitizer.cpp
+++ llvm-netbsd/patches/patch-lib_Transforms_Instrumentation_AddressSanitizer.cpp
@@ -1,27 +1,16 @@
 $NetBSD$
 
---- lib/Transforms/Instrumentation/AddressSanitizer.cpp.orig	2018-02-16 03:29:35.000000000 +0000
+--- lib/Transforms/Instrumentation/AddressSanitizer.cpp.orig	2018-05-11 01:06:43.059844428 +0000
 +++ lib/Transforms/Instrumentation/AddressSanitizer.cpp
-@@ -107,7 +107,9 @@ static const uint64_t kMIPS64_ShadowOffs
- static const uint64_t kAArch64_ShadowOffset64 = 1ULL << 36;
- static const uint64_t kFreeBSD_ShadowOffset32 = 1ULL << 30;
+@@ -109,6 +109,7 @@ static const uint64_t kFreeBSD_ShadowOff
  static const uint64_t kFreeBSD_ShadowOffset64 = 1ULL << 46;
-+static const uint64_t kNetBSD_ShadowOffset32 = 1ULL << 30;
+ static const uint64_t kNetBSD_ShadowOffset32 = 1ULL << 30;
  static const uint64_t kNetBSD_ShadowOffset64 = 1ULL << 46;
 +static const uint64_t kNetBSDKasan_ShadowOffset64 = 0xdffffc0000000000;
  static const uint64_t kPS4CPU_ShadowOffset64 = 1ULL << 40;
  static const uint64_t kWindowsShadowOffset32 = 3ULL << 28;
  
-@@ -508,6 +510,8 @@ static ShadowMapping getShadowMapping(Tr
-       Mapping.Offset = kMIPS32_ShadowOffset32;
-     else if (IsFreeBSD)
-       Mapping.Offset = kFreeBSD_ShadowOffset32;
-+    else if (IsNetBSD)
-+      Mapping.Offset = kNetBSD_ShadowOffset32;
-     else if (IsIOS)
-       // If we're targeting iOS and x86, the binary is built for iOS simulator.
-       Mapping.Offset = IsX86 ? kIOSSimShadowOffset32 : kIOSShadowOffset32;
-@@ -526,9 +530,12 @@ static ShadowMapping getShadowMapping(Tr
+@@ -529,9 +530,12 @@ static ShadowMapping getShadowMapping(Tr
        Mapping.Offset = kSystemZ_ShadowOffset64;
      else if (IsFreeBSD)
        Mapping.Offset = kFreeBSD_ShadowOffset64;
