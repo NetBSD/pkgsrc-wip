50189344bd2e574f6001c6a673f3fef1922bf78a
Filip Hajny <filip@joyent.com>
2018-05-18 13:21:14 +0000

    mailman3: Update to 3.1.1.
    Bugs
    ----
    - An AttributeError: 'str' object has no attribute 'decode'
      exception in subject prefixing is fixed.
    - Syntactically invalid sender addresses are now ignored.
    - Messages with no syntactically valid senders are now automatically
      discarded.
    - Generated ``regexp`` tables for Postfix now account for possible
      ``+extra`` additions to the ``-bounces`` and ``-confirm`` addresses.
    - A list whose name is one of the ``admin``, ``bounces``, ``confirm``,
      etc. subaddresses can now be posted to.
    - Various message holds and rejects that gave 'N/A' as a reason now
      give an appropriate reason.
    - A missing html_to_plain_text_command is now properly detected and
      logged.
    - Bounce messages are now composed for proper translations.
    
    Configuration
    -------------
    - Mailman now also searches at ``/etc/mailman3/mailman.cfg`` for the
      configuration file.
    
    Interfaces
    ----------
    - A new template ``list:user:notice:rejected`` has been added for
      customizing the bounce message rejection notice.
    
    REST
    ----
    - Allow a mailing list's acceptable aliases to be cleared by calling
      ``DELETE`` on the list's ``config/acceptable_aliases`` resource.

--- mailman3/patches/patch-src_mailman_core_initialize.py
+++ mailman3/patches/patch-src_mailman_core_initialize.py
@@ -2,7 +2,7 @@ $NetBSD$
 
 Use PKG_SYSCONFDIR for default config file.
 
---- src/mailman/core/initialize.py.orig	2017-02-10 16:58:52.000000000 +0000
+--- src/mailman/core/initialize.py.orig	2017-11-18 01:23:39.000000000 +0000
 +++ src/mailman/core/initialize.py
 @@ -68,7 +68,7 @@ def search_for_configuration_file():
      if os.path.exists(config_path):
@@ -12,4 +12,4 @@ Use PKG_SYSCONFDIR for default config file.
 +    config_path = '@PKG_SYSCONFDIR@/mailman.cfg'
      if os.path.exists(config_path):
          return os.path.abspath(config_path)
-     # $argv0/../../etc/mailman.cfg
+     # /etc/mailman3/mailman.cfg
