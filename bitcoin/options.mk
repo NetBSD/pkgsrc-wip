1f3277c74d7882bb554694a51a87ae41c783292b
Pierre Pronchery <khorben@defora.org>
2018-05-20 00:22:41 +0200

    Add support for Qt 5 to wip/bitcoin

--- bitcoin/options.mk
+++ bitcoin/options.mk
@@ -1,9 +1,7 @@
 # $NetBSD$
 
-# TODO: support qt5 also (or perhaps instead)
-
 PKG_OPTIONS_VAR=		PKG_OPTIONS.bitcoin
-PKG_SUPPORTED_OPTIONS+=		qt4 miniupnpc
+PKG_SUPPORTED_OPTIONS+=		qt4 qt5 miniupnpc
 # qt4 is off because it doubles the footprint of the package.
 # Please do not enable it by default; instead, create a split
 # package.
@@ -12,13 +10,23 @@ PKG_SUGGESTED_OPTIONS+=
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mqt4)
-PLIST.qt4=	yes
+PLIST.qt=	yes
 
 .include "../../converters/qrencode/buildlink3.mk"
 .include "../../devel/protobuf/buildlink3.mk"
 BUILDLINK_DEPMETHOD.qt4-tools=	full
 .include "../../x11/qt4-tools/buildlink3.mk"
 .include "../../x11/qt4-libs/buildlink3.mk"
+
+CONFIGURE_ARGS+=	--with-qt-bindir=${QTDIR}/bin
+.endif
+
+.if !empty(PKG_OPTIONS:Mqt5)
+PLIST.qt=	yes
+
+.include "../../converters/qrencode/buildlink3.mk"
+.include "../../devel/protobuf/buildlink3.mk"
+.include "../../x11/qt5-qttools/buildlink3.mk"
 
 CONFIGURE_ARGS+=	--with-qt-bindir=${QTDIR}/bin
 .endif
