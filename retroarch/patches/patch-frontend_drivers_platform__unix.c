65c720fe71e4717e949925b65fc093df88427fd3
Nia Alarie <nia.alarie@gmail.com>
2018-05-23 10:58:10 +0100

    retroarch: Update paths to not search $PREFIX for things pkgsrc no longer installs (cheats are downloaded by RetroArch to ~/.config/retroarch).

--- retroarch/patches/patch-frontend_drivers_platform__unix.c
+++ retroarch/patches/patch-frontend_drivers_platform__unix.c
@@ -2,9 +2,9 @@ $NetBSD$
 
 Pick up paths from pkgsrc.
 
---- frontend/drivers/platform_unix.c.orig	2017-06-22 02:10:15.000000000 +0000
+--- frontend/drivers/platform_unix.c.orig	2018-05-04 03:45:22.000000000 +0000
 +++ frontend/drivers/platform_unix.c
-@@ -1775,32 +1775,15 @@ static void frontend_unix_get_env(int *a
+@@ -1818,32 +1818,15 @@ static void frontend_unix_get_env(int *a
     else
        snprintf(base_path, sizeof(base_path), "retroarch");
  
@@ -43,7 +43,7 @@ Pick up paths from pkgsrc.
  
     fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_MENU_CONFIG], base_path,
           "config", sizeof(g_defaults.dirs[DEFAULT_DIR_MENU_CONFIG]));
-@@ -1813,14 +1796,14 @@ static void frontend_unix_get_env(int *a
+@@ -1856,12 +1839,12 @@ static void frontend_unix_get_env(int *a
           "records_config", sizeof(g_defaults.dirs[DEFAULT_DIR_RECORD_CONFIG]));
     fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_RECORD_OUTPUT], base_path,
           "records", sizeof(g_defaults.dirs[DEFAULT_DIR_RECORD_OUTPUT]));
@@ -53,16 +53,12 @@ Pick up paths from pkgsrc.
 -         "database/rdb", sizeof(g_defaults.dirs[DEFAULT_DIR_DATABASE]));
 -   fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_SHADER], base_path,
 -         "shaders", sizeof(g_defaults.dirs[DEFAULT_DIR_SHADER]));
--   fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_CHEATS], base_path,
--         "cheats", sizeof(g_defaults.dirs[DEFAULT_DIR_CHEATS]));
 +   fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_CURSOR], PREFIX,
 +         "share/libretro/database/cursors", sizeof(g_defaults.dirs[DEFAULT_DIR_CURSOR]));
 +   fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_DATABASE], PREFIX,
 +         "share/libretro/database/rdb", sizeof(g_defaults.dirs[DEFAULT_DIR_DATABASE]));
 +   fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_SHADER], PREFIX,
 +         "share/libretro/shaders", sizeof(g_defaults.dirs[DEFAULT_DIR_SHADER]));
-+   fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_CHEATS], PREFIX,
-+         "share/libretro/cheats", sizeof(g_defaults.dirs[DEFAULT_DIR_CHEATS]));
+    fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_CHEATS], base_path,
+          "cheats", sizeof(g_defaults.dirs[DEFAULT_DIR_CHEATS]));
     fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_OVERLAY], base_path,
-          "overlay", sizeof(g_defaults.dirs[DEFAULT_DIR_OVERLAY]));
-    fill_pathname_join(g_defaults.dirs[DEFAULT_DIR_CORE_ASSETS], base_path,
