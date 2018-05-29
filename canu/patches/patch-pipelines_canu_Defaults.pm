4000a5adfcdb12dcec4dd1f697875baa9779e695
Jason Bacon <bacon@NetBSD.org>
2018-05-29 14:40:58 -0500

    canu: Improvements to SLURM resource limit control

--- canu/patches/patch-pipelines_canu_Defaults.pm
+++ canu/patches/patch-pipelines_canu_Defaults.pm
@@ -1,11 +1,21 @@
+$NetBSD$
+
+# Add slurm job limits
+
 --- pipelines/canu/Defaults.pm.orig	2018-02-27 13:46:07 UTC
 +++ pipelines/canu/Defaults.pm
-@@ -637,6 +637,9 @@ sub setExecDefaults ($$) {
+@@ -637,6 +637,15 @@ sub setExecDefaults ($$) {
      my $tag         = shift @_;
      my $name        = shift @_;
  
-+    $global{"slurmArrayConcurrentTasks"}  = undef;
-+    $synops{"slurmArrayConcurrentTasks"}  = "Max tasks that can run at once";
++    $global{"slurmCormhapTaskLimit"}  = undef;
++    $synops{"slurmCormhapTaskLimit"}  = "Max corhmap tasks that can run at once";
++
++    $global{"slurmArrayTaskLimit"}  = undef;
++    $synops{"slurmArrayTaskLimit"}  = "Max tasks that can run at once";
++
++    $global{"slurmArrayCoreLimit"}  = undef;
++    $synops{"slurmArrayCoreLimit"}  = "Max cores we can allocate at once";
 +
      $global{"gridOptions${tag}"}   = undef;
      $synops{"gridOptions${tag}"}   = "Grid engine options applied to $name jobs";
--- canu/patches/patch-pipelines_canu_Execution.pm
+++ canu/patches/patch-pipelines_canu_Execution.pm
@@ -1,3 +1,7 @@
+$NetBSD$
+
+# Add slurm job limits
+
 --- pipelines/canu/Execution.pm.orig	2018-02-27 13:46:07 UTC
 +++ pipelines/canu/Execution.pm
 @@ -303,10 +303,6 @@ sub skipStage ($$@) {
@@ -11,13 +15,56 @@
      return($installDir);
  }
  
-@@ -727,6 +723,9 @@ sub buildGridArray ($$$$) {
+@@ -694,8 +690,8 @@ sub submitScript ($$) {
  
-     $opt =~ s/ARRAY_NAME/$name/g;        #  Replace ARRAY_NAME with 'job name'
-     $opt =~ s/ARRAY_JOBS/$bgn-$end/g;    #  Replace ARRAY_JOBS with 'bgn-end'
-+    if ( defined(getGlobal("slurmArrayConcurrentTasks")) ) {
-+        $opt = $opt . '%' . getGlobal("slurmArrayConcurrentTasks");
+ 
+ 
+-sub buildGridArray ($$$$) {
+-    my ($name, $bgn, $end, $opt) = @_;
++sub buildGridArray (@) {
++    my ( $name, $bgn, $end, $opt, $thr ) = @_;
+     my  $off = 0;
+ 
+     #  In some grids (SGE)   this is the maximum size of an array job.
+@@ -725,8 +721,30 @@ sub buildGridArray ($$$$) {
+         $off = "-F \"$off\"";
+     }
+ 
+-    $opt =~ s/ARRAY_NAME/$name/g;        #  Replace ARRAY_NAME with 'job name'
+-    $opt =~ s/ARRAY_JOBS/$bgn-$end/g;    #  Replace ARRAY_JOBS with 'bgn-end'
++    if( $opt =~ m/(ARRAY_NAME)/ )
++    {
++       $opt =~ s/$1/$name/; # Replace ARRAY_NAME with 'job name'
++    }
++    elsif( $opt =~ m/(ARRAY_JOBS)/ )
++    {
++       $opt =~ s/$1/$bgn-$end/; # Replace ARRAY_JOBS with 'bgn-end'
++
++       if( lc( getGlobal( 'gridEngine' ) ) eq 'slurm' && $end > 1 )
++       {
++           if( $name =~ m/mhap/i && defined getGlobal( 'slurmCormhapTaskLimit' ) )
++           {
++               $opt .= '%' . getGlobal( 'slurmCormhapTaskLimit' );
++           }
++           elsif( defined getGlobal( 'slurmArrayTaskLimit' ) )
++           {
++               $opt .= '%' . getGlobal( 'slurmArrayTaskLimit' );
++           }
++           elsif( defined getGlobal( 'slurmArrayCoreLimit' ) )
++           {
++               $opt .= '%' . int( getGlobal( 'slurmArrayCoreLimit' ) / $thr );
++           }
++       }
 +    }
  
      return($opt, $off);
  }
+@@ -870,7 +888,7 @@ sub buildGridJob ($$$$$$$$$) {
+     my $jobNameT               = makeUniqueJobName($jobType, $asm);
+ 
+     my ($jobName,  $jobOff)    = buildGridArray($jobNameT, $bgnJob, $endJob, getGlobal("gridEngineArrayName"));
+-    my ($arrayOpt, $arrayOff)  = buildGridArray($jobNameT, $bgnJob, $endJob, getGlobal("gridEngineArrayOption"));
++    my ( $arrayOpt, $arrayOff ) = buildGridArray( $jobNameT, $bgnJob, $endJob, getGlobal( "gridEngineArrayOption" ), $thr );
+ 
+     my $outputOption           = buildOutputOption($path, $script);
+ 
