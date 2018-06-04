460863f06e5467edcc606ccd85aea4d11fcce853
Yorick Hardy <yorickhardy@gmail.com>
2018-06-04 20:10:38 +0200

    urlwatch: update to urlwatch-2.13
     # Changelog
    
     ## [2.13] -- 2018-06-03
    
     ### Added
     - Support for specifying a `diff_tool` (e.g. `wdiff`) for each job (Fixes #243)
     - Support for testing filters via `--test-filter JOB` (Fixes #237)
    
     ### Changed
     - Moved ChangeLog file to CHANGELOG.md and using Keep a Changelog format.
     - Force version check in `setup.py`, to exclude Python 2 (Fixes #244)
     - Remove default parameter from internal `html2text` module (Fixes #239)
     - Better error/exception reporting in `--verbose` mode (Fixes #164)
    
     ### Removed
     - Old ChangeLog entries

--- urlwatch/patches/patch-setup.py
+++ urlwatch/patches/patch-setup.py
@@ -2,11 +2,11 @@ $NetBSD$
 
 Use the pkgsrc directory structure.
 
---- setup.py.orig	2016-07-12 18:58:17.000000000 +0000
+--- setup.py.orig	2018-06-03 12:42:56.000000000 +0000
 +++ setup.py
-@@ -18,8 +18,8 @@ m['scripts'] = ['urlwatch']
- m['package_dir'] = {'': 'lib'}
+@@ -22,8 +22,8 @@ m['package_dir'] = {'': 'lib'}
  m['packages'] = ['urlwatch']
+ m['python_requires'] = '>3.3.0'
  m['data_files'] = [
 -    ('share/man/man1', ['share/man/man1/urlwatch.1']),
 -    ('share/urlwatch/examples', [
