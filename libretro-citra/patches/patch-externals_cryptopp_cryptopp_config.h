8d053d989be5b3bedf6fa167acff04e21c706715
Nia Alarie <nia.alarie@gmail.com>
2018-06-05 14:19:07 +0100

    libretro-citra: Update to 20180519

---
+++ libretro-citra/patches/patch-externals_cryptopp_cryptopp_config.h
@@ -0,0 +1,20 @@
+$NetBSD$
+
+uds_beacon.cpp:(.text+0xac9): undefined reference to `CryptoPP::AlignedDeallocate(void*)'
+
+--- externals/cryptopp/cryptopp/config.h.orig	2018-02-22 14:26:16.000000000 +0000
++++ externals/cryptopp/cryptopp/config.h
+@@ -688,12 +688,7 @@ NAMESPACE_END
+ 
+ // ***************** Miscellaneous ********************
+ 
+-// Nearly all Intel's and AMD's have SSE. Enable it independent of SSE ASM and intrinscs
+-#if (CRYPTOPP_BOOL_X86 || CRYPTOPP_BOOL_X32 || CRYPTOPP_BOOL_X64 || CRYPTOPP_BOOL_PPC32 || CRYPTOPP_BOOL_PPC64) && !defined(CRYPTOPP_DISABLE_ASM)
+-	#define CRYPTOPP_BOOL_ALIGN16 1
+-#else
+-	#define CRYPTOPP_BOOL_ALIGN16 0
+-#endif
++#define CRYPTOPP_BOOL_ALIGN16 0
+ 
+ // How to allocate 16-byte aligned memory (for SSE2)
+ // posix_memalign see https://forum.kde.org/viewtopic.php?p=66274
