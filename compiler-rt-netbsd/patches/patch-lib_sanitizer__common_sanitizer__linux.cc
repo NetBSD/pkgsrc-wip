d82013fcb3309b5e981e02eccd34363634812c62
Kamil Rytarowski <n54@gmx.com>
2018-06-05 22:44:24 +0200

    compiler-rt-netbsd: Fix build
    Remove leftover from local patches after the merge of CheckASLR().
    
    Sponsored by <The NetBSD Foundation>

--- compiler-rt-netbsd/patches/patch-lib_sanitizer__common_sanitizer__linux.cc
+++ compiler-rt-netbsd/patches/patch-lib_sanitizer__common_sanitizer__linux.cc
@@ -10,34 +10,3 @@ $NetBSD$
  #include <sys/sysctl.h>
  #include <sys/exec.h>
  extern struct ps_strings *__ps_strings;
-@@ -1954,6 +1955,30 @@ void MaybeReexec() {
-   // No need to re-exec on Linux.
- }
- 
-+void CheckASLR() {
-+#if SANITIZER_NETBSD
-+  int mib[3];
-+  int paxflags;
-+  size_t len = sizeof(int);
-+
-+  mib[0] = CTL_PROC;
-+  mib[1] = internal_getpid();
-+  mib[2] = PROC_PID_PAXFLAGS;
-+
-+  if (UNLIKELY(sysctl(mib, 3, &paxflags, &len, NULL, 0) == -1)) {
-+    Printf("sysctl failed\n");
-+    Die();
-+  }
-+
-+  if (UNLIKELY(paxflags & CTL_PROC_PAXFLAGS_ASLR)) {
-+    Printf("This sanitizer is not compatible with enabled ASLR\n");
-+    Die();
-+  }
-+#else
-+  // Do nothing
-+#endif
-+}
-+
- void PrintModuleMap() { }
- 
- void CheckNoDeepBind(const char *filename, int flag) {
