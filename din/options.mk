fd150f0742fdd9a06b1073b972c235d630865c0b
Thomas Klausner <tk@giga.or.at>
2018-06-17 17:05:00 +0200

    din: simplify
    Only depend on stuff if option is turned on.

--- din/options.mk
+++ din/options.mk
@@ -1,21 +1,23 @@
 # $NetBSD$
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.din
-PKG_SUPPORTED_OPTIONS=	jack alsa
+PKG_SUPPORTED_OPTIONS=	alsa jack
 PKG_SUGGESTED_OPTIONS=	jack
 
 .include "../../mk/bsd.options.mk"
-
-###
-### Support JACK
-###
-.if !empty(PKG_OPTIONS:Mjack)
-CXXFLAGS+=		-D__UNIX_JACK__
-.endif
 
 ###
 ### Support ALSA
 ###
 .if !empty(PKG_OPTIONS:Malsa)
 CXXFLAGS+=		-D__LINUX_ALSA__
+.include "../../audio/alsa-lib/buildlink3.mk"
+.endif
+
+###
+### Support JACK
+###
+.if !empty(PKG_OPTIONS:Mjack)
+CXXFLAGS+=		-D__UNIX_JACK__
+.include "../../audio/jack/buildlink3.mk"
 .endif
