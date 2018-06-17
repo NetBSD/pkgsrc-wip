99762ca444d14b5f1145525f2fa2780b98c80886
Nia Alarie <nia.alarie@gmail.com>
2018-06-17 18:41:52 +0100

    go-kr-text: Import go-kr-text-0.1.0 as wip/go-kr-text
    Go package for manipulating paragraphs of text

---
+++ go-kr-text/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	go-kr-text
+
+.if !defined(GO_KR_TEXT_BUILDLINK3_MK)
+GO_KR_TEXT_BUILDLINK3_MK=
+
+BUILDLINK_CONTENTS_FILTER.go-kr-text=	${EGREP} gopkg/
+BUILDLINK_DEPMETHOD.go-kr-text?=		build
+
+BUILDLINK_API_DEPENDS.go-kr-text+=		go-kr-text>=0.0
+BUILDLINK_PKGSRCDIR.go-kr-text?=		../../wip/go-kr-text
+.endif  # GO_KR_TEXT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-go-kr-text
