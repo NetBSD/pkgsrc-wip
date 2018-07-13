cb270b7669762b1c6dd781f9f57082e0ed615250
Olaf Seibert <rhialto@falu.nl>
2018-07-13 21:18:10 +0200

    golly: update to 3.2
    Also depend on wxGTK28, now that wxGTK30 seems to use gtk3+.
    
    Changes in version 3.2 (released July 2018)
    
    - Three-dimensional cellular automata can be explored using a new
      script: 3D.lua.
    - HashLife and the other hash-based algorithms are faster and more
      responsive.
    - Larger than Life now supports circular neighborhoods.
    - Added a Show Population option to the Control menu.
    - Added a Show Scroll Bars option to the View menu.
    - MAP rules can optionally end with base64 padding characters "==".
    - The overlay's paste command now supports multiple locations for batch
      draw.
    - Added new overlay command lines which can draw multiple unconnected
      lines.
    - Added a new ovtable command which provides a high performance table
      API for a subset of the overlay commands.
    - Added draw_line and fill_rect functions to the overlay's oplus
      package.
    - The oplus package also has new functions for creating a menu bar,
      pop-up menus and radio buttons. Menu buttons now support custom colors
      and shadows.
    - Added a round function to Lua's gplus package.
    - Added timing functions to the gplus package which can be used to
      profile Lua scripts.
    - Added savechanges, settitle and sleep commands for use in Lua
      scripts.
    - The getevent command now returns a "file" event when a user tries to
      open a file while a script is running. 3D.lua uses this event to open a
      .rle3 pattern or to run a .lua script.
    - The note and warn commands have an optional parameter to specify
      whether the dialog has a Cancel button.
    - The setoption command recognizes two new options: "showscrollbars"
      for showing or hiding the scroll bars, and "showtimeline" for showing
      or hiding the timeline.
    - Mouse wheel sensitivity can be adjusted in Preferences > View to make
      zooming in/out easier to control.
    - Fixed a bug when setting/getting cell values in very big hash-based
      universes.
    - Fixed a bug where patterns larger than bounded grids were not
      correctly clipped.
    - Fixed various problems with UTF8-encoded text in the clipboard and in
      Lua/Python scripts.
    - Fixed a bug that could crash Golly if a .rule file contained invalid
      XPM data.
    - The new credits.lua script displays animated credits and can be
      launched from a link in Help > Credits.  - New patterns:
      CY_Lee_computer.zip, switch-engine-ping-pong.rle,
      honey-farm-hasslers.rle, weekender-distaff.rle.gz,
      slow-salvo-MWSS-oscillator.rle.gz, syntheses-of-c2-spaceships.rle.gz.
    - Updated patterns: phase-shift.rle, traffic-light-hasslers.rle,
      2c5-orthogonal.rle, c3-orthogonal.rle, c4-diagonal.rle,
      c4-orthogonal.rle, short-thin.rle, smallest-low-period.rle,
      spaceship-types.rle.
    - Removed pattern: Patterns/Life/Syntheses/make-lightbulb.rle.
    - Updated the Life Lexicon to release 29.

--- golly/patches/patch-cmdline_bgolly.cpp
+++ golly/patches/patch-cmdline_bgolly.cpp
@@ -2,28 +2,28 @@ $NetBSD$
 
 popcount() is declared in <strings.h> included from <string.h>.
 
---- cmdline/bgolly.cpp.orig	2017-08-19 04:39:59.000000000 +0000
+--- cmdline/bgolly.cpp.orig	2018-01-21 02:19:18.000000000 +0000
 +++ cmdline/bgolly.cpp
-@@ -115,7 +115,7 @@ struct options {
- } ;
+@@ -122,7 +122,7 @@ struct options {
  bigint maxgen = -1, inc = 0 ;
  int maxmem = 256 ;
--int hyper, render, autofit, quiet, popcount, progress ;
-+int hyper, render, autofit, quiet, xpopcount, progress ;
+ int hyperxxx ;   // renamed hyper to avoid conflict with windows.h
+-int render, autofit, quiet, popcount, progress ;
++int render, autofit, quiet, xpopcount, progress ;
  int hashlife ;
  char *algoName = 0 ;
  int verbose ;
-@@ -143,7 +143,7 @@ options options[] = {
+@@ -152,7 +152,7 @@ options options[] = {
    { "-t", "--timeline", "Use timeline", 'b', &timeline },
    { "",   "--render", "Render (benchmarking)", 'b', &render },
    { "",   "--progress", "Render during progress dialog (debugging)", 'b', &progress },
 -  { "",   "--popcount", "Popcount (benchmarking)", 'b', &popcount },
-+  { "",   "--popcount", "Popcount (benchmarking)", 'b', &xpopcount },
++  { "",   "--xpopcount", "Popcount (benchmarking)", 'b', &xpopcount },
    { "",   "--scale", "Rendering scale", 's', &renderscale },
  //{ "",   "--stepthreshold", "Stepsize >= gencount/this (default 1)",
  //                                                          'i', &stepthresh },
-@@ -627,7 +627,7 @@ case 's':
-          else
+@@ -648,7 +648,7 @@ case 's':
+          } else
              cout << endl ;
        }
 -      if (popcount)
