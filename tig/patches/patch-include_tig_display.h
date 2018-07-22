6b050639998bb83e7891fa5bee0bd140f99af9ca
Leonardo Taccari <leot@NetBSD.org>
2018-07-22 18:37:31 +0200

    tig: Update wip/tig to 2.4.0
    pkgsrc changes:
     - Remove patches/patch-src_display.c (no longer needed, adjusted differently
       upstream)
     - Update TODO and add an entry about graphical artifacts that needs further
       investigations
    
    Changes:
    tig-2.4.0
    ---------
    Improvements:
    
     - Add 'send-child-enter' option to control interaction with child views. (#791)
     - Update make config defaults for Cygwin to ncurses6. (#792)
     - Build against netbsd-curses. (#789)
     - Change the blame view to render more like `git blame`. (#812)
     - Improve worktree and submodule support. (#459, #781, #783)
     - Support running Tig via a Git alias. (#763)
     - Use ISO-8601 letters for short relative dates. (#759, #760)
     - Change date formatting to show time zones by default. (#428, #811)
     - Use utf8proc to handle Unicode characters. (#827)
    
    Bug fixes:
    
     - Fix `file(1)` argument on Linux used for resolving encodings. (#788)
     - Fix underflow in the file search. (#800, #801)
     - Fix line numbers in grep view when scrolled. (#813)
     - Pass command line args through to the stage view. (#569, #823)
     - Fix resource leak. (#780)
     - Fix various compiler warnings and pointer arithmetic. (#799, #803)
     - Workaround potential null pointer dereferences. (#824)
     - Bind to single and double quotes by using the *<SingleQuote>* and
       *<DoubleQuote>* key mappings. (#821)
     - Make Tig the process-group leader and clean child processes. (#828, #837)
     - Fix sh compatibility in `contrib/tig-pick`. (#832)
     - Fix incorrect behaviour of up and down keys in diff view when opened from
       diff preview. (#802, #835)
     - Open the stage view when maximizing a split diff view of (un)staged changes.
       (#836)
     - Use fully qualified reference name for tags when conflicting with branch
       name. (#746, #787, #849)
     - Fix resize not working after entering command. (#845) (#846)
     - Use stack allocated memory to handle TIG_LS_REMOTE. (#839)
     - Fix deleted file mode line remains highlighted after hovering in diff or stage view. (#851)
     - Fix `TIG_LS_REMOTE` not working with git-ls-remote(1). (#853, #854)
    
    tig-2.3.3
    ---------
    Bug fixes:
    
     - Revert "Handle \n like \r (#758)". (GH #769)
     - Fix GH #164 by catching SIGHUP.
     - Change `refs_tags` type to `size_t`.
    
    tig-2.3.2
    ---------
    Bug fixes:
    
     - Fix busy loop detection to handle large repos. (GH #164)
    
    tig-2.3.1
    ---------
    Improvements:
    
     - Restore TTY attributes. (GH #725)
     - Handle `\n` like `\r`. (GH #758)
    
    Bug fixes:
    
     - Add workaround that detects busy loops when Tig loses the TTY. This may
       happen if Tig does not receive the HUP signal (e.g. when started with
       `nohup`). (GH #164)
     - Fix compatibility with ncurses-5.4 which caused copy-pasting to not work
       in the prompt. (GH #767)
     - tig(1): document correct environment variable. (GH #752)

--- tig/patches/patch-include_tig_display.h
+++ tig/patches/patch-include_tig_display.h
@@ -4,11 +4,11 @@ $NetBSD$
 
 XXX: Is this correct?
 
---- include/tig/display.h.orig	2017-09-29 15:15:26.000000000 +0000
+--- include/tig/display.h.orig	2018-07-21 19:09:17.000000000 +0000
 +++ include/tig/display.h
-@@ -58,8 +58,8 @@ void enable_mouse(bool enable);
- 
+@@ -60,8 +60,8 @@ void enable_mouse(bool enable);
  enum status_code open_script(const char *path);
+ bool is_script_executing(void);
  
 -#define get_cursor_pos(cursor_y, cursor_x) getyx(newscr, cursor_y, cursor_x)
 -#define set_cursor_pos(cursor_y, cursor_x) wmove(newscr, cursor_y, cursor_x)
