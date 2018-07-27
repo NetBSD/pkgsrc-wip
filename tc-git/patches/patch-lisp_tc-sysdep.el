f88e0a757a8383705853af7bd4ca6c376e92b01f
Makoto Fujiwara (CF-SX2) <makoto@ki.nu>
2018-07-28 07:05:31 +0900

    (inputmethod/tc-git) MASTER repository gets historical merge. Forked from the point of we used.

--- tc-git/patches/patch-lisp_tc-sysdep.el
+++ tc-git/patches/patch-lisp_tc-sysdep.el
@@ -4,13 +4,13 @@ $NetBSD: patch-lisp_tc-sysdep.el,v 1.1 2013/01/22 13:54:22 makoto Exp $
  You will get following error if C-s tried for incremental search.
  Symbol's function definition is void: isearch-last-command-char
 
---- work/tc/lisp/tc-sysdep.el.orig	2013-01-22 22:07:16.000000000 +0900
-+++ lisp/tc-sysdep.el	2013-01-22 22:10:14.000000000 +0900
+--- lisp/tc-sysdep.el.orig	2018-07-27 22:40:27.540117546 +0900
++++ lisp/tc-sysdep.el	2018-07-27 22:44:27.936253867 +0900
 @@ -228,7 +228,7 @@ BODY should be a list of lisp expression
  ;;;
  ;;; Fix incompatibilities between 18 and 19.
  ;;;
--(if (string-match "^\\(19\\|2[01234]\\)" emacs-version)
+-(if (string-match "^\\(19\\|2[0123]\\)" emacs-version)
 +(if (string-match "^\\(19\\|2[0-9]\\)" emacs-version)
      (progn
        (defun tcode-redo-command (ch)
