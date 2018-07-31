2c529f62d789e810cf41c50d5918432bf6407a2b
Jonathan Perkin <jperkin@joyent.com>
2018-07-31 10:23:57 +0100

    xapps: Build fixes.

--- xapps/buildlink3.mk
+++ xapps/buildlink3.mk
@@ -8,6 +8,7 @@ XAPPS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.xapps+=	xapps>=1.2.1
 BUILDLINK_PKGSRCDIR.xapps?=	../../wip/xapps
 
+.include "../../x11/libxkbfile/buildlink3.mk"
 .endif	# XAPPS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xapps
