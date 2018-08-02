20c42bbb39f6cbf70fbca39df3682852831f6291
coypu <coypu@sdf.org>
2018-08-02 14:26:25 +0300

    Add barrier v2.1.1
    Barrier is KVM software forked from Symless's synergy 1.9 codebase.
    Synergy was a commercialized reimplementation of the original CosmoSynergy
    written by Chris Schoeneman.
    
    Whereas synergy has moved beyond its goals from the 1.x era, barrier aims
    to maintain that simplicity. Barrier will let you use your keyboard and
    mouse from machine A to control machine B (or more). It's that simple.

---
+++ barrier/patches/patch-src_gui_src_ActionDialog.cpp
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/gui/src/ActionDialog.cpp.orig	2018-05-16 02:48:18.000000000 +0000
++++ src/gui/src/ActionDialog.cpp
+@@ -23,6 +23,7 @@
+ #include "ServerConfig.h"
+ #include "KeySequence.h"
+ 
++#include <QButtonGroup>
+ #include <QtCore>
+ #include <QtGui>
+ 
