4e5d1efb53b7c0183489cd41c7ba76d9f85f5958
Thomas Klausner <tk@giga.or.at>
2018-08-14 11:25:01 +0200

    lumina: use bsd.options.mk in options.mk

--- lumina/options.mk
+++ lumina/options.mk
@@ -1,8 +1,9 @@
 # $NetBSD$
+
 PKG_OPTIONS_VAR=	PKG_OPTIONS_VAR.lumina
 PKG_SUPPORTED_OPTIONS=	noxinput
 
-.include "../../mk/bsd.prefs.mk"
+.include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mnoxinput)
 EXTRA=	CONFIG+=NO_XINPUT
