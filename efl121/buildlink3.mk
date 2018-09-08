63d512c7e2b0ebf2bc63747d5dfc4e875aae4db0
andyb <root@Eve.m42.one>
2018-09-08 06:39:08 +0200

    Works on stable branche enlightenment desktop

--- efl121/buildlink3.mk
+++ efl121/buildlink3.mk
@@ -1,18 +1,23 @@
-# $NetBSD: buildlink3.mk,v,v 0.2 2018/09/06 16:15:10 andyb Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2018/08/16 18:54:28 adam Exp $
 
-BUILDLINK_TREE+=	efl121
+BUILDLINK_TREE+=        efl
 
-.if !defined(EFL121_BUILDLINK3_MK)
-EFL121_BUILDLINK3_MK:=
+BUILDLINK_PKGSRCDIR.efl?=     ../../wip/efl121
 
-BUILDLINK_API_DEPENDS.efl121+=	efl121>=1.2.1
-BUILDLINK_ABI_DEPENDS.efl121?=	efl121>=1.2.1nb10
-BUILDLINK_PKGSRCDIR.efl121?=	../../wip/efl121
-
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
+.include "../../x11/libXScrnSaver/buildlink3.mk"
+.include "../../x11/libXcomposite/buildlink3.mk"
+.include "../../x11/libXcursor/buildlink3.mk"
+.include "../../x11/libXdamage/buildlink3.mk"
+.include "../../x11/libXinerama/buildlink3.mk"
+.include "../../x11/libXp/buildlink3.mk"
+.include "../../x11/libXrandr/buildlink3.mk"
+.include "../../x11/libXrender/buildlink3.mk"
+.include "../../x11/libXtst/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../multimedia/gstreamer1/buildlink3.mk"
-.include "../../multimedia/gst-plugins1-base/buildlink3.mk"
 .include "../../lang/LuaJIT2/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../fonts/fontconfig/buildlink3.mk"
@@ -30,8 +35,7 @@ BUILDLINK_PKGSRCDIR.efl121?=	../../wip/efl121
 .include "../../sysutils/dbus/buildlink3.mk"
 .include "../../audio/pulseaudio/buildlink3.mk"
 .include "../../devel/bullet/buildlink3.mk"
-.include "../../mk/bsd.pkg.mk"
-
-.endif # EFL121_BUILDLINK3_MK
-
-BUILDLINK_TREE+=	-efl121
+.include "../../multimedia/gst-plugins1-base/buildlink3.mk"
+.include "../../graphics/hicolor-icon-theme/buildlink3.mk"
+.include "../../graphics/hicolor-icon-theme/buildlink3.mk"
+.include "../../devel/libuuid/buildlink3.mk"
