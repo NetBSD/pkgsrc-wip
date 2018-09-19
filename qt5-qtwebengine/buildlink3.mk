1f5f3772ff204cbc9a05b8602ae93cff1e706fb3
bgermann <bastiangermann@fishpost.de>
2018-09-19 13:31:42 +0200

    qt5-qtwebengine: correct buildlink

--- qt5-qtwebengine/buildlink3.mk
+++ qt5-qtwebengine/buildlink3.mk
@@ -1,17 +1,17 @@
 # $NetBSD: buildlink3.mk,v 1.29 2017/04/22 21:03:22 adam Exp $
 
-BUILDLINK_TREE+=	qt5-qtwebkit
+BUILDLINK_TREE+=	qt5-qtwebengine
 
-.if !defined(QT5_QTWEBKIT_BUILDLINK3_MK)
-QT5_QTWEBKIT_BUILDLINK3_MK:=
+.if !defined(QT5_QTWEBENGINE_BUILDLINK3_MK)
+QT5_QTWEBENGINE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qt5-qtwebkit+=	qt5-qtwebkit>=5.9.1
-BUILDLINK_ABI_DEPENDS.qt5-qtwebkit+=	qt5-qtwebkit>=5.9.1
-BUILDLINK_PKGSRCDIR.qt5-qtwebkit?=	../../wip/qt5-qtwebkit
+BUILDLINK_API_DEPENDS.qt5-qtwebengine+=	qt5-qtwebengine>=5.9.1
+BUILDLINK_ABI_DEPENDS.qt5-qtwebengine+=	qt5-qtwebengine>=5.9.1
+BUILDLINK_PKGSRCDIR.qt5-qtwebengine?=	../../wip/qt5-qtwebengine
 
-BUILDLINK_INCDIRS.qt5-qtwebkit+=	qt5/include
-BUILDLINK_LIBDIRS.qt5-qtwebkit+=	qt5/lib
-BUILDLINK_LIBDIRS.qt5-qtwebkit+=	qt5/plugins
+BUILDLINK_INCDIRS.qt5-qtwebengine+=	qt5/include
+BUILDLINK_LIBDIRS.qt5-qtwebengine+=	qt5/lib
+BUILDLINK_LIBDIRS.qt5-qtwebengine+=	qt5/plugins
 
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../graphics/libwebp/buildlink3.mk"
@@ -21,15 +21,15 @@ BUILDLINK_LIBDIRS.qt5-qtwebkit+=	qt5/plugins
 .include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
 .include "../../x11/libXcomposite/buildlink3.mk"
 .endif
-.include "../../wip/qt5-qtdeclarative/buildlink3.mk"
-.include "../../wip/qt5-qtlocation/buildlink3.mk"
-.include "../../wip/qt5-qtmultimedia/buildlink3.mk"
-.include "../../wip/qt5-qtsensors/buildlink3.mk"
-.include "../../wip/qt5-qtwebchannel/buildlink3.mk"
+.include "../../x11/qt5-qtdeclarative/buildlink3.mk"
+.include "../../x11/qt5-qtlocation/buildlink3.mk"
+.include "../../x11/qt5-qtmultimedia/buildlink3.mk"
+.include "../../x11/qt5-qtsensors/buildlink3.mk"
+.include "../../x11/qt5-qtwebchannel/buildlink3.mk"
 PYTHON_FOR_BUILD_ONLY=			yes
 .include "../../lang/python/tool.mk"
 .include "../../lang/ruby/buildlink3.mk"
 BUILDLINK_DEPMETHOD.${RUBY_BASE}=	build
-.endif	# QT5_QTWEBKIT_BUILDLINK3_MK
+.endif	# QT5_QTWEBENGINE_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-qt5-qtwebkit
+BUILDLINK_TREE+=	-qt5-qtwebengine
