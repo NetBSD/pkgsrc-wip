09ef6cd6faa56b086d477ada9300e925e0f95359
Leonardo Taccari <leot@NetBSD.org>
2018-09-27 15:07:08 +0200

    zathura-pdf-mupdf: Always pass the cookie instead of NULL
    (Still rendering issues though!)

--- zathura-pdf-mupdf/patches/patch-zathura-pdf-mupdf_utils.c
+++ zathura-pdf-mupdf/patches/patch-zathura-pdf-mupdf_utils.c
@@ -4,14 +4,23 @@ Port to mupdf-1.14.0
 
 --- zathura-pdf-mupdf/utils.c.orig	2018-03-17 19:47:01.000000000 +0000
 +++ zathura-pdf-mupdf/utils.c
-@@ -18,8 +18,8 @@ mupdf_page_extract_text(mupdf_document_t
+@@ -9,6 +9,8 @@ mupdf_page_extract_text(mupdf_document_t
+     return;
+   }
+ 
++  fz_cookie cookie = { 0 };
++
+   fz_device* volatile text_device = NULL;
+ 
+   fz_try (mupdf_page->ctx) {
+@@ -18,8 +20,8 @@ mupdf_page_extract_text(mupdf_document_t
      fz_disable_device_hints(mupdf_page->ctx, text_device, FZ_DONT_INTERPOLATE_IMAGES);
  
      fz_matrix ctm;
 -    fz_scale(&ctm, 1.0, 1.0);
 -    fz_run_page(mupdf_page->ctx, mupdf_page->page, text_device, &ctm, NULL);
 +    ctm = fz_scale(1.0, 1.0);
-+    fz_run_page(mupdf_page->ctx, mupdf_page->page, text_device, ctm, NULL);
++    fz_run_page(mupdf_page->ctx, mupdf_page->page, text_device, ctm, &cookie);
    } fz_always (mupdf_document->ctx) {
      fz_close_device(mupdf_page->ctx, text_device);
      fz_drop_device(mupdf_page->ctx, text_device);
