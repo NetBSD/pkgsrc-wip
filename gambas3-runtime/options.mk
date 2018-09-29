cd4356abb4066c253b753920a56aaea9280e9921
bgermann <bastiangermann@fishpost.de>
2018-09-29 18:37:45 +0200

    gambas3-runtime: Introduce more options

--- gambas3-runtime/options.mk
+++ gambas3-runtime/options.mk
@@ -3,27 +3,33 @@
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gambas3-runtime
 PKG_SUPPORTED_OPTIONS=	qt4 qt5 gtk2 gtk3
 PKG_SUGGESTED_OPTIONS=	qt4
-PLIST_VARS+=		gui
+PLIST_VARS+=		gui gui.opengl gui.qt
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgtk2)
-BUILD_DEPENDS+=	gambas3-gb-gtk-opengl-${GB_VERSION}:../../wip/gambas3-gb-gtk-opengl
-PLIST.gui=	yes
+BUILD_DEPENDS+=		gambas3-gb-gtk-opengl-${GB_VERSION}:../../wip/gambas3-gb-gtk-opengl
+PLIST.gui=		yes
+PLIST.gui.opengl=	yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mgtk3)
-BUILD_DEPENDS+=	gambas3-gb-gtk3-${GB_VERSION}:../../wip/gambas3-gb-gtk3
-PLIST.gui=	yes
+BUILD_DEPENDS+=		gambas3-gb-gtk3-${GB_VERSION}:../../wip/gambas3-gb-gtk3
+PLIST.gui=		yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mqt4)
-BUILD_DEPENDS+=	gambas3-gb-qt4-opengl-${GB_VERSION}:../../wip/gambas3-gb-qt4-opengl
-BUILD_DEPENDS+=	gambas3-gb-qt4-webkit-${GB_VERSION}:../../wip/gambas3-gb-qt4-webkit
-PLIST.gui=	yes
+BUILD_DEPENDS+=		gambas3-gb-qt4-opengl-${GB_VERSION}:../../wip/gambas3-gb-qt4-opengl
+BUILD_DEPENDS+=		gambas3-gb-qt4-webkit-${GB_VERSION}:../../wip/gambas3-gb-qt4-webkit
+PLIST.gui=		yes
+PLIST.gui.opengl=	yes
+PLIST.gui.qt=		yes
 .endif
 
 .if !empty(PKG_OPTIONS:Mqt5)
-BUILD_DEPENDS+=	gambas3-gb-qt5-${GB_VERSION}:../../wip/gambas3-gb-qt5
-PLIST.gui=	yes
+BUILD_DEPENDS+=		gambas3-gb-qt5-opengl-${GB_VERSION}:../../wip/gambas3-gb-qt5-opengl
+BUILD_DEPENDS+=		gambas3-gb-qt5-webkit-${GB_VERSION}:../../wip/gambas3-gb-qt5-webkit
+PLIST.gui=		yes
+PLIST.gui.opengl=	yes
+PLIST.gui.qt=		yes
 .endif
