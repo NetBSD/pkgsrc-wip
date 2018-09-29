d4e70c92e2a5199a397512b9694be854bee6165b
Leonardo Taccari <leot@NetBSD.org>
2018-09-29 15:42:53 +0200

    zathura-pdf-mupdf: Fix the rendering issue
    During the port to mupdf-1.14.0 instead of using mupdf_page->bbox due
    fz_bound_page() changes a local variable was used and hence the width and height
    was just 0 leading to the surprising results described in
    pdf_page_render_to_buffer() described in TODO... Just use mupdf_page->bbox as it
    was done.  Now zathura-pdf-mupdf works!

--- zathura-pdf-mupdf/patches/patch-zathura-pdf-mupdf_page.c
+++ zathura-pdf-mupdf/patches/patch-zathura-pdf-mupdf_page.c
@@ -4,13 +4,12 @@ Port to mupdf-1.14.0
 
 --- zathura-pdf-mupdf/page.c.orig	2018-03-17 19:47:01.000000000 +0000
 +++ zathura-pdf-mupdf/page.c
-@@ -30,14 +30,13 @@ pdf_page_init(zathura_page_t* page)
+@@ -30,14 +30,12 @@ pdf_page_init(zathura_page_t* page)
      goto error_free;
    }
  
 -  fz_bound_page(mupdf_document->ctx, (fz_page*) mupdf_page->page, &mupdf_page->bbox);
-+  fz_rect mediabox;
-+  mediabox = fz_bound_page(mupdf_document->ctx, (fz_page*) mupdf_page->page);
++  mupdf_page->bbox = fz_bound_page(mupdf_document->ctx, (fz_page*) mupdf_page->page);
  
    /* setup text */
    mupdf_page->extracted_text = false;
@@ -18,7 +17,7 @@ Port to mupdf-1.14.0
 -  fz_rect mediabox;
 -  mupdf_page->text = fz_new_stext_page(mupdf_page->ctx,
 -      fz_bound_page(mupdf_page->ctx, mupdf_page->page, &mediabox));
-+  mupdf_page->text = fz_new_stext_page(mupdf_page->ctx, mediabox);
++  mupdf_page->text = fz_new_stext_page(mupdf_page->ctx, mupdf_page->bbox);
    if (mupdf_page->text == NULL) {
      goto error_free;
    }
