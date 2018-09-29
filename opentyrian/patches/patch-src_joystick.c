6ec1285c5aa567c606a1d2e7a5f8d12fc89a0bb3
Yorick Hardy <yorickhardy@gmail.com>
2018-09-29 09:22:28 +0200

    opentyrian: fix buld error (unused variable)

--- opentyrian/patches/patch-src_joystick.c
+++ opentyrian/patches/patch-src_joystick.c
@@ -1,10 +1,21 @@
 $NetBSD: patch-src_joystick.c,v 1.1 2015/08/19 19:36:56 yhardy Exp $
 
+joystick_cfg_version is never used and triggers a warning (an error due to -Werror).
 Fix toupper() usage on NetBSD (compiled with -Werror).
 
 --- src/joystick.c.orig	2013-09-07 22:14:32.000000000 +0000
 +++ src/joystick.c
-@@ -490,8 +490,8 @@ void code_to_assignment( Joystick_assign
+@@ -42,7 +42,9 @@ bool ignore_joystick = false;
+ int joysticks = 0;
+ Joystick *joystick = NULL;
+ 
++/* not used
+ static const char joystick_cfg_version = 1;
++*/
+ static const int joystick_analog_max = 32767;
+ 
+ // eliminates axis movement below the threshold
+@@ -490,8 +492,8 @@ void code_to_assignment( Joystick_assign
  	else
  		--assignment->num;
  	
