7b62dd2f30bd54d4bb7ce82528b9e2d3d4c062fd
Leonardo Taccari <leot@NetBSD.org>
2018-10-03 13:53:48 +0200

    mupdf: Update wip/mupdf to 1.14.0
    Changes:
     - Compared to 1.14.0rc1 the only change was to mupdf-gl that now save history
       in all normal exit situations
    
    XXX: There is still not a formal announcement about 1.14.0 so it is probably
    XXX: better to wait for it and/or use it to update print/mupdf in the next days.

--- mupdf/patches/patch-platform_gl_gl-main.c
+++ mupdf/patches/patch-platform_gl_gl-main.c
@@ -2,7 +2,7 @@ $NetBSD$
 
 glutLeaveMainLoop() is present only on freeglut, define it otherwise.
 
---- platform/gl/gl-main.c.orig	2018-09-25 12:39:17.000000000 +0000
+--- platform/gl/gl-main.c.orig	2018-10-03 11:29:14.000000000 +0000
 +++ platform/gl/gl-main.c
 @@ -16,7 +16,7 @@
  char *realpath(const char *path, char *resolved_path); /* in gl-file.c */
@@ -10,6 +10,6 @@ glutLeaveMainLoop() is present only on freeglut, define it otherwise.
  
 -#ifdef __APPLE__
 +#ifndef FREEGLUT
+ static void cleanup(void);
  void glutLeaveMainLoop(void)
  {
- 	exit(0);
