c49a011fc9d7b9fd8d7bf99717d390d74348fcdd
Leonardo Taccari <leot@NetBSD.org>
2018-10-05 11:00:19 +0200

    webkit-gtk: Double-check registers used
    - Make every CPU(...) case separate despite possible sharing of _REG_* definitions
      (cosmetic)
    - Try to follow more closely other operating systems for CPU(ARM64) case
    
    XXX: Still not tested on !CPU(X86_64) though!

--- webkit-gtk/patches/patch-Source_JavaScriptCore_runtime_MachineContext.h
+++ webkit-gtk/patches/patch-Source_JavaScriptCore_runtime_MachineContext.h
@@ -13,7 +13,7 @@ XXX:
 
 --- Source/JavaScriptCore/runtime/MachineContext.h.orig	2018-08-21 16:06:10.000000000 +0000
 +++ Source/JavaScriptCore/runtime/MachineContext.h
-@@ -188,6 +188,18 @@ static inline void*& stackPointerImpl(mc
+@@ -188,6 +188,22 @@ static inline void*& stackPointerImpl(mc
  #error Unknown Architecture
  #endif
  
@@ -23,7 +23,11 @@ XXX:
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_UESP]);
 +#elif CPU(X86_64)
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_RSP]);
-+#elif CPU(ARM) || CPU(ARM64) || CPU(MIPS)
++#elif CPU(ARM)
++    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_SP]);
++#elif CPU(ARM64)
++    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_SP]);
++#elif CPU(MIPS)
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_SP]);
 +#else
 +#error Unknown Architecture
@@ -32,7 +36,7 @@ XXX:
  #elif OS(FUCHSIA) || defined(__GLIBC__) || defined(__BIONIC__)
  
  #if CPU(X86)
-@@ -335,6 +347,20 @@ static inline void*& framePointerImpl(mc
+@@ -335,6 +351,22 @@ static inline void*& framePointerImpl(mc
  #error Unknown Architecture
  #endif
  
@@ -42,7 +46,9 @@ XXX:
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_EBP]);
 +#elif CPU(X86_64)
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_RBP]);
-+#elif CPU(ARM) || CPU(ARM64)
++#elif CPU(ARM)
++    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_FP]);
++#elif CPU(ARM64)
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_FP]);
 +#elif CPU(MIPS)
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_S8]);
@@ -53,7 +59,7 @@ XXX:
  #elif OS(FUCHSIA) || defined(__GLIBC__) || defined(__BIONIC__)
  
  // The following sequence depends on glibc's sys/ucontext.h.
-@@ -482,6 +508,20 @@ static inline void*& instructionPointerI
+@@ -482,6 +514,22 @@ static inline void*& instructionPointerI
  #error Unknown Architecture
  #endif
  
@@ -63,7 +69,9 @@ XXX:
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_EIP]);
 +#elif CPU(X86_64)
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_RIP]);
-+#elif CPU(ARM) || CPU(ARM64)
++#elif CPU(ARM)
++    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_PC]);
++#elif CPU(ARM64)
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_PC]);
 +#elif CPU(MIPS)
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_EPC]);
@@ -74,7 +82,7 @@ XXX:
  #elif OS(FUCHSIA) || defined(__GLIBC__) || defined(__BIONIC__)
  
  // The following sequence depends on glibc's sys/ucontext.h.
-@@ -639,6 +679,20 @@ inline void*& argumentPointer<1>(mcontex
+@@ -639,6 +687,22 @@ inline void*& argumentPointer<1>(mcontex
  #error Unknown Architecture
  #endif
  
@@ -84,8 +92,10 @@ XXX:
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_EDX]);
 +#elif CPU(X86_64)
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_RSI]);
-+#elif CPU(ARM) || CPU(ARM64)
++#elif CPU(ARM)
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_R1]);
++#elif CPU(ARM64)
++    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_X1]);
 +#elif CPU(MIPS)
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_A1]);
 +#else
@@ -95,7 +105,7 @@ XXX:
  #elif OS(FUCHSIA) || defined(__GLIBC__) || defined(__BIONIC__)
  
  // The following sequence depends on glibc's sys/ucontext.h.
-@@ -756,6 +810,20 @@ inline void*& llintInstructionPointer(mc
+@@ -756,6 +820,22 @@ inline void*& llintInstructionPointer(mc
  #error Unknown Architecture
  #endif
  
@@ -105,8 +115,10 @@ XXX:
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_ESI]);
 +#elif CPU(X86_64)
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_R8]);
-+#elif CPU(ARM) || CPU(ARM64)
++#elif CPU(ARM)
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_R8]);
++#elif CPU(ARM64)
++    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_X4]);
 +#elif CPU(MIPS)
 +    return reinterpret_cast<void*&>((uintptr_t&) machineContext.__gregs[_REG_T4]);
 +#else
