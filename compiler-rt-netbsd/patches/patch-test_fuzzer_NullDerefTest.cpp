4aa03789f882f35f204511995fa0e6bd90386a00
Kamil Rytarowski <n54@gmx.com>
2018-10-09 12:41:42 +0200

    compiler-rt-netbsd: Rebase to SVN r. 344035
    Drop merged patches:
     - [compiler-rt] r344035 - Reflect the current reality and disable lsan tests on NetBSD
     - [compiler-rt] r344034 - Use PTHREAD_STACK_MIN conditionally in a test
    
    Drop unfinished stubs of support for:
     - profile
     - scudo
     - lsan
    
    These patches do not help much of now as the proper support for these features is missing.

--- compiler-rt-netbsd/patches/patch-test_fuzzer_NullDerefTest.cpp
+++
@@ -1,9 +0,0 @@
-$NetBSD$
-
---- test/fuzzer/NullDerefTest.cpp.orig	2018-08-21 21:25:32.000000000 +0000
-+++ test/fuzzer/NullDerefTest.cpp
-@@ -23,4 +23,3 @@ extern "C" int LLVMFuzzerTestOneInput(co
-   }
-   return 0;
- }
--
