0f4eb1446bab1829f82f9a94d67c09f1223bc634
Leonardo Taccari <leot@NetBSD.org>
2018-10-11 17:03:29 +0200

    vimb3-git: Check the credentials if possible

--- vimb3-git/patches/patch-src_ext-proxy.c
+++ vimb3-git/patches/patch-src_ext-proxy.c
@@ -4,14 +4,14 @@ Workaround for:
 
  <https://github.com/fanglingsu/vimb/issues/438>
 
---- src/ext-proxy.c.orig	2018-10-10 08:19:21.505729509 +0000
+--- src/ext-proxy.c.orig	2018-10-11 14:57:40.747987085 +0000
 +++ src/ext-proxy.c
-@@ -95,6 +95,8 @@ out:
+@@ -95,7 +95,7 @@ out:
  static gboolean on_authorize_authenticated_peer(GDBusAuthObserver *observer,
          GIOStream *stream, GCredentials *credentials, gpointer data)
  {
-+    return TRUE;
-+
-     gboolean authorized = FALSE;
+-    gboolean authorized = FALSE;
++    gboolean authorized = TRUE;
  
      if (credentials) {
+         GCredentials *own_credentials;
--- vimb3-git/patches/patch-src_webextension_ext-main.c
+++ vimb3-git/patches/patch-src_webextension_ext-main.c
@@ -4,14 +4,14 @@ Workaround for:
 
  <https://github.com/fanglingsu/vimb/issues/438>
 
---- src/webextension/ext-main.c.orig	2018-10-10 08:19:21.506806584 +0000
+--- src/webextension/ext-main.c.orig	2018-10-11 14:57:40.749928061 +0000
 +++ src/webextension/ext-main.c
-@@ -136,6 +136,8 @@ void webkit_web_extension_initialize_wit
+@@ -136,7 +136,7 @@ void webkit_web_extension_initialize_wit
  static gboolean on_authorize_authenticated_peer(GDBusAuthObserver *observer,
          GIOStream *stream, GCredentials *credentials, gpointer extension)
  {
-+    return TRUE;
-+
-     gboolean authorized = FALSE;
+-    gboolean authorized = FALSE;
++    gboolean authorized = TRUE;
      if (credentials) {
          GCredentials *own_credentials;
+ 
