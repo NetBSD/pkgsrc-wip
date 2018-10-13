b8d2711123a797d8b9c411b8fb1872906064754c
Min Sik Kim <minskim@NetBSD.org>
2018-10-13 10:03:33 -0700

    rust-bin: Update to 1.29.2
    This patch release introduces a workaround to a miscompilation bug
    introduced in Rust 1.29.0.

--- rust-bin/buildlink3.mk
+++ rust-bin/buildlink3.mk
@@ -7,6 +7,7 @@ RUST_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.rust-bin+=	rust-bin>=1.29.1
 BUILDLINK_PKGSRCDIR.rust-bin?=		../../wip/rust-bin
+BUILDLINK_DEPMETHOD.rust-bin?=		build
 
 BUILDLINK_PASSTHRU_DIRS+=	${PREFIX}/lib/rustlib
 .endif
