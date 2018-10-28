bd105a99c7c4979a3c964d9d704ee618d299aeff
Benny Siegert <bsiegert@gmail.com>
2018-10-28 17:23:24 +0100

    Set PKGNAME to enchant2.
    This package will be imported as textproc/enchant2, unless
    there are objections.

--- enchant/buildlink3.mk
+++ enchant/buildlink3.mk
@@ -1,17 +1,17 @@
 # $NetBSD: buildlink3.mk,v 1.16 2012/09/15 10:06:36 obache Exp $
 
-BUILDLINK_TREE+=	enchant
+BUILDLINK_TREE+=	enchant2
 
 .if !defined(ENCHANT_BUILDLINK3_MK)
 ENCHANT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.enchant+=	enchant>=1.1.3
-BUILDLINK_ABI_DEPENDS.enchant+=	enchant>=1.6.0nb5
-BUILDLINK_PKGSRCDIR.enchant?=	../../textproc/enchant
+BUILDLINK_API_DEPENDS.enchant+=	enchant2>=2
+BUILDLINK_ABI_DEPENDS.enchant+=	enchant>=2.2.3
+BUILDLINK_PKGSRCDIR.enchant?=	../../wip/enchant2
 
 # spelling libraries are loaded dynamically and do not need
 # to be included here
 .include "../../devel/glib2/buildlink3.mk"
 .endif # ENCHANT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-enchant
+BUILDLINK_TREE+=	-enchant2
