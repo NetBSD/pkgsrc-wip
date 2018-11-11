d0332439e52e8af75e1b31b187e7d8c0426f74c5
Yorick Hardy <yorickhardy@gmail.com>
2018-11-11 07:21:35 +0200

    wyeb-git: patch to check response status code before downloading files
    The second part of the patch to main.c prevents wyeb from trying to download
    urls which have failed (according to the http error code). This happens,
    for example, when a proxy server denies access to a URL.
    
    (wip/luakit{,-git} has the same issue.)

--- wyeb-git/patches/patch-main.c
+++ wyeb-git/patches/patch-main.c
@@ -2,6 +2,9 @@ $NetBSD$
 
 Use the correct data type for modification time.
 
+The second part of the patch prevents wyeb from trying to download
+urls which have failed (according to the http error code).
+
 --- main.c.orig	2018-11-10 14:18:57.000000000 +0000
 +++ main.c
 @@ -2895,7 +2895,7 @@ static char *histdata(bool rest, bool al
@@ -13,3 +16,13 @@ Use the correct data type for modification time.
  	for (int j = 2; j > 0; j--) for (int i = histfnum - 1; i >= 0; i--)
  	{
  		if (!rest && size && num >= size) break;
+@@ -3948,6 +3948,9 @@ static gboolean policycb(
+ 	WebKitResponsePolicyDecision *rdec = dec;
+ 	WebKitURIResponse *res = webkit_response_policy_decision_get_response(rdec);
+ 
++	if(!SOUP_STATUS_IS_SUCCESSFUL(webkit_uri_response_get_status_code(res)))
++		return false;
++
+ 	bool dl = false;
+ 	char *msr = getset(win, "dlmimetypes");
+ 	//for checking whether is sub frame or not.
