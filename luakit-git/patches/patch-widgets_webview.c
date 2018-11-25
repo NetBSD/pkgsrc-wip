1e74a5c81cf5172228550cf2bbc1f89e5b378fcc
Yorick Hardy <yorickhardy@gmail.com>
2018-11-25 11:22:39 +0200

    luakit-git: add a patch for luakit PR 737
    Only signal "mime-type-decision" for a succesful response
    (https://github.com/luakit/luakit/pull/737).

---
+++ luakit-git/patches/patch-widgets_webview.c
@@ -0,0 +1,17 @@
+$NetBSD$
+
+Only signal "mime-type-decision" for a succesful response.
+https://github.com/luakit/luakit/pull/737
+
+--- widgets/webview.c.orig	2018-10-07 06:06:13.000000000 +0000
++++ widgets/webview.c
+@@ -487,6 +487,9 @@ decide_policy_cb(WebKitWebView* UNUSED(v
+         const gchar *uri = webkit_uri_response_get_uri(r);
+         const gchar *mime = webkit_uri_response_get_mime_type(r);
+ 
++        if(!SOUP_STATUS_IS_SUCCESSFUL(webkit_uri_response_get_status_code(r)))
++            return FALSE;
++
+         luaH_object_push(L, w->ref);
+         lua_pushstring(L, uri);
+         lua_pushstring(L, mime);
