d73ef1d036e552fe95dac99702326e9e2cc819a8
coypu <coypu@sdf.org>
2018-11-27 14:58:56 +0200

    Add my WIP librsvg 2.44.9 (that's rust)

---
+++ librsvg/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.46 2018/11/14 22:21:42 kleink Exp $
+
+BUILDLINK_TREE+=	librsvg
+
+.if !defined(LIBRSVG_BUILDLINK3_MK)
+LIBRSVG_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.librsvg+=		librsvg>=2.12.6nb1
+BUILDLINK_ABI_DEPENDS.librsvg+=		librsvg>=2.40.20nb4
+BUILDLINK_PKGSRCDIR.librsvg?=		../../graphics/librsvg
+
+.include "../../devel/pango/buildlink3.mk"
+.include "../../graphics/cairo/buildlink3.mk"
+.include "../../graphics/gdk-pixbuf2/buildlink3.mk"
+.include "../../textproc/libcroco/buildlink3.mk"
+.endif # LIBRSVG_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-librsvg
