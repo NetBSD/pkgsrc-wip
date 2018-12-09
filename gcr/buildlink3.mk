07699d00a4cc3d6fc6a9e941b5eb321941192347
Leonardo Taccari <leot@NetBSD.org>
2018-12-09 14:07:27 +0100

    gcr: Include other bl3 (syncing them with Makefile)

--- gcr/buildlink3.mk
+++ gcr/buildlink3.mk
@@ -8,6 +8,9 @@ GCR_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gcr+=	gcr>=3.18.0
 BUILDLINK_PKGSRCDIR.gcr?=	../../wip/gcr
 
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/gobject-introspection/buildlink3.mk"
+.include "../../security/libgcrypt/buildlink3.mk"
 .include "../../security/p11-kit/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
 .endif	# GCR_BUILDLINK3_MK
