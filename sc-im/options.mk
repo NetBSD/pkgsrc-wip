329268c481c3fb4bf8944c61583d6d322bd4041c
Sijmen J. Mulder <ik@sjmulder.nl>
2018-12-10 18:13:15 +0100

    sc-im: Add XLSX writing and XLS reading support

--- sc-im/options.mk
+++ sc-im/options.mk
@@ -1,15 +1,22 @@
 # $NetBSD$
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sc-im
-PKG_SUPPORTED_OPTIONS+=	xlsx lua
+PKG_SUPPORTED_OPTIONS+=	xls xlsx lua
 
 .include "../../mk/bsd.options.mk"
 
+.if !empty(PKG_OPTIONS:Mxls)
+CFLAGS+=	-DXLS
+LDFLAGS+=	-lxlsreader
+.include "../../wip/libxls/buildlink3.mk"
+.endif
+
 # reader only, writer needs libxlsxwriter
 .if !empty(PKG_OPTIONS:Mxlsx)
-CFLAGS+=	-DXLSX
+CFLAGS+=	-DXLSX -DXLSX_WRITER
 .include "../../archivers/libzip/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
+.include "../../wip/libxlsxwriter/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mlua)
