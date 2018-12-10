bd85ccae7d574440885e4326a4b51e82f7975040
Leonardo Taccari <leot@NetBSD.org>
2018-12-10 09:42:51 +0100

    swi-prolog-lite: Remove inclusion of ncurses in bl3
    Both ncurses and gmp bl3 seems not needed as part of swi-prolog-lite
    bl3 (`pkg-config --cflags' and `pkg-config --libs' of swipl also
    does not seem to indicate possible needs of them).
    
    Please also note that it is only used by swi-prolog* packages that
    already directly include gmp and ncurses bl3.
    
    Remove corresponding entries in the TODO.

--- swi-prolog-lite/buildlink3.mk
+++ swi-prolog-lite/buildlink3.mk
@@ -9,8 +9,6 @@ BUILDLINK_API_DEPENDS.swi-prolog-lite+=	swi-prolog-lite>=5.2.9
 BUILDLINK_ABI_DEPENDS.swi-prolog-lite+=	swi-prolog-lite>=6.6.6nb2
 BUILDLINK_PKGSRCDIR.swi-prolog-lite?=	../../wip/swi-prolog-lite
 
-.include "../../devel/ncurses/buildlink3.mk"
-#.include "../../devel/gmp/buildlink3.mk" ?
 .endif	# SWI_PROLOG_LITE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-swi-prolog-lite
