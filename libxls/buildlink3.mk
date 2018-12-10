4e372f893dd8f4d603c9d5c3b7c340eafec44398
Sijmen J. Mulder <ik@sjmulder.nl>
2018-12-10 18:05:23 +0100

    Import libxls
    C library to extract cell data and some metadata from legacy Microsoft
    Excel files.  Also includes an 'xls2csv' tool.

---
+++ libxls/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libxls
+
+.if !defined(LIBXLS_BUILDLINK3_MK)
+LIBXLS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libxls+=	libxls>=1.4.0
+BUILDLINK_PKGSRCDIR.libxls?=	../../wip/libxls
+.endif
+
+BUILDLINK_TREE+=	-libxls
