f2c2eb63f04c9f7404ee2f206e344e5df42e0596
Yorick Hardy <yorickhardy@gmail.com>
2018-12-11 20:48:09 +0200

    openfodder: update to openfodder-1.5
    pkgsrc changes: make a separate package for the data
    No changelog provided.

--- openfodder/patches/patch-Source_stdafx.cpp
+++ openfodder/patches/patch-Source_stdafx.cpp
@@ -1,41 +1,30 @@
 $NetBSD$
 
-Check argc before accessing argv.
-
 Use PREFIX when searching for data.
 
---- Source/stdafx.cpp.orig	2018-02-01 19:54:26.000000000 +0000
+--- Source/stdafx.cpp.orig	2018-12-11 09:43:06.000000000 +0000
 +++ Source/stdafx.cpp
-@@ -39,7 +39,7 @@ int main(int argc, char *args[]) {
- 		if (strcmp( args[1], "skipintro" ) == 0)
- 			SkipIntro = true;
- 
--		if (strcmp( args[2], "map" ) == 0) {
-+		if ((argc > 3) && strcmp( args[2], "map" ) == 0) {
- 			MapNumber = atoi( args[3] );
- 		}
- 	}
-@@ -110,14 +110,19 @@ std::string local_PathGenerate( const st
-             if (path) {
-                 FinalPath = path;
-                 FinalPath.append("/.local/share/");
-+                filePathFinal << FinalPath << "OpenFodder/";
-+                if (!local_FileExists(filePathFinal.str())) {
-+                    FinalPath = "";
-+                    filePathFinal.str("");
-+                }
-             }
-         }
- 
-         // Fall back just incase
--        if(!FinalPath.size())
--            FinalPath = "/usr/local/share/";
--
--        filePathFinal << FinalPath << "OpenFodder/";
-+        if(!FinalPath.size()) {
-+            FinalPath = OPENFODDER_DATADIR;
+@@ -245,14 +245,19 @@ void local_BasePathGenerate() {
+         if (path) {
+             FinalPath = path;
+             FinalPath.append("/.local/share/");
 +            filePathFinal << FinalPath << "OpenFodder/";
-+        }
++            if (!local_FileExists(filePathFinal.str())) {
++                FinalPath = "";
++                filePathFinal.str("");
++            }
+         }
+     }
  
-     #endif
+     // Fall back just incase
+-    if (!FinalPath.size())
+-        FinalPath = "/usr/local/share/";
+-
+-    filePathFinal << FinalPath << "OpenFodder/";
++    if (!FinalPath.size()) {
++        FinalPath = OPENFODDER_DATADIR;
++        filePathFinal << FinalPath << "OpenFodder/";
++    }
+ 
+ #endif
  
