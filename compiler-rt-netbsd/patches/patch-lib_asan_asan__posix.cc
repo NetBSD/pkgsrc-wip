17dfd7b0cbd2dba7c8bf295321713f34da45a365
Kamil Rytarowski <n54@gmx.com>
2018-12-15 09:49:26 +0100

    compiler-rt-netbsd: Update to SVN r. 349257
    Merged upstream:
    [compiler-rt] r349257 - Fix internal_sleep() for NetBSD
    [compiler-rt] r349159 - Mark interception_failure_test.cc as passing for NetBSD and asan-dynamic-runtime
    [compiler-rt] r349156 - Set shared_libasan_path in lit tests for NetBSD
    
    Reworked TSD patches for ASan and MSan.
    
    Add a local patch for pthread_getname_np().

--- compiler-rt-netbsd/patches/patch-lib_asan_asan__posix.cc
+++ compiler-rt-netbsd/patches/patch-lib_asan_asan__posix.cc
@@ -2,49 +2,75 @@ $NetBSD$
 
 --- lib/asan/asan_posix.cc.orig	2018-08-21 21:25:41.000000000 +0000
 +++ lib/asan/asan_posix.cc
-@@ -40,31 +40,35 @@ void AsanOnDeadlySignal(int signo, void 
+@@ -30,6 +30,11 @@
+ #include <sys/resource.h>
+ #include <unistd.h>
+ 
++#if SANITIZER_NETBSD
++extern "C" int __cxa_thread_atexit(void (*)(void *), void *,
++                                   void *) SANITIZER_WEAK_ATTRIBUTE;
++#endif
++
+ namespace __asan {
+ 
+ void AsanOnDeadlySignal(int signo, void *siginfo, void *context) {
+@@ -40,6 +45,51 @@ void AsanOnDeadlySignal(int signo, void 
  
  // ---------------------- TSD ---------------- {{{1
  
--static pthread_key_t tsd_key;
-+struct TsdKey {
-+  void *data;
-+  void (*dst)(void *value);
-+  TsdKey() : data(nullptr), dst(nullptr) {}
-+  ~TsdKey() {
-+    if (dst)
-+      dst(data);
-+  }
-+};
++#if SANITIZER_NETBSD
++// Thread Static Data cannot be used in early init on NetBSD
++// Reuse the Asan TSD API for compatibility with existing code
++// with an alternative implementation.
 +
-+static thread_local TsdKey Tk;
++static bool tsd_key_inited = false;
++static void (*tsd_destructor)(void *tsd) = nullptr;
++static THREADLOCAL void *tsd_key = nullptr;
++
++void tsd_at_exit(void *tsd) {
++  CHECK_EQ(tsd, tsd_key);
++  CHECK(tsd_key);
++  (*tsd_destructor)(tsd_key);
++}
++
++void AsanTSDInit(void (*destructor)(void *tsd)) {
++  CHECK(!tsd_key_inited);
++  tsd_key_inited = true;
++  tsd_destructor = destructor;
++}
++
++void *AsanTSDGet() {
++  CHECK(tsd_key_inited);
++  return tsd_key;
++}
++
++void AsanTSDSet(void *tsd) {
++  CHECK(tsd_key_inited);
++  CHECK(tsd);
++  CHECK(!tsd_key);
++  tsd_key = tsd;
++  if (::__cxa_thread_atexit(tsd_at_exit, tsd, nullptr)) {
++    Printf("AddressSanitizer: failed to setup thread atexit callback");
++    Die();
++  }
++}
++
++void PlatformTSDDtor(void *tsd) {
++  CHECK(tsd_key_inited);
++  tsd_key = nullptr;
++  // Make sure that signal handler can not see a stale current thread pointer.
++  atomic_signal_fence(memory_order_seq_cst);
++  AsanThread::TSDDtor(tsd);
++}
++#else
+ static pthread_key_t tsd_key;
  static bool tsd_key_inited = false;
  void AsanTSDInit(void (*destructor)(void *tsd)) {
-   CHECK(!tsd_key_inited);
-   tsd_key_inited = true;
--  CHECK_EQ(0, pthread_key_create(&tsd_key, destructor));
-+  Tk.dst = destructor;
- }
- 
- void *AsanTSDGet() {
-   CHECK(tsd_key_inited);
--  return pthread_getspecific(tsd_key);
-+  return Tk.data;
- }
- 
- void AsanTSDSet(void *tsd) {
-   CHECK(tsd_key_inited);
--  pthread_setspecific(tsd_key, tsd);
-+  Tk.data = tsd;
- }
- 
- void PlatformTSDDtor(void *tsd) {
--  AsanThreadContext *context = (AsanThreadContext*)tsd;
--  if (context->destructor_iterations > 1) {
--    context->destructor_iterations--;
--    CHECK_EQ(0, pthread_setspecific(tsd_key, tsd));
--    return;
--  }
+@@ -67,6 +117,7 @@ void PlatformTSDDtor(void *tsd) {
+   }
    AsanThread::TSDDtor(tsd);
  }
++#endif
  }  // namespace __asan
+ 
+ #endif  // SANITIZER_POSIX
--- compiler-rt-netbsd/patches/patch-lib_msan_msan__linux.cc
+++ compiler-rt-netbsd/patches/patch-lib_msan_msan__linux.cc
@@ -2,61 +2,88 @@ $NetBSD$
 
 --- lib/msan/msan_linux.cc.orig	2018-08-21 21:25:44.000000000 +0000
 +++ lib/msan/msan_linux.cc
-@@ -31,6 +31,7 @@
- #include <sys/time.h>
- #include <sys/resource.h>
- 
-+#include "interception/interception.h"
+@@ -34,6 +34,11 @@
  #include "sanitizer_common/sanitizer_common.h"
  #include "sanitizer_common/sanitizer_procmaps.h"
  
-@@ -175,13 +176,28 @@ void InstallAtExitHandler() {
++#if SANITIZER_NETBSD
++extern "C" int __cxa_thread_atexit(void (*)(void *), void *,
++                                   void *) SANITIZER_WEAK_ATTRIBUTE;
++#endif
++
+ namespace __msan {
+ 
+ void ReportMapRange(const char *descr, uptr beg, uptr size) {
+@@ -175,19 +180,59 @@ void InstallAtExitHandler() {
  
  // ---------------------- TSD ---------------- {{{1
  
-+#if !SANITIZER_NETBSD
- static pthread_key_t tsd_key;
-+#endif
+-static pthread_key_t tsd_key;
++static THREADLOCAL MsanThread *msan_current_thread;
++
++MsanThread *GetCurrentThread() { return msan_current_thread; }
++
++#if SANITIZER_NETBSD
++// Thread Static Data cannot be used in early init on NetBSD
++// Reuse the Asan TSD API for compatibility with existing code
++// with an alternative implementation.
 +
  static bool tsd_key_inited = false;
- 
-+#if SANITIZER_NETBSD
-+INTERCEPTOR(void, _lwp_exit) {
-+  CHECK(tsd_key_inited);
-+  MsanTSDDtor(GetCurrentThread());
-+  REAL(_lwp_exit)();
-+}
-+#endif
++static void (*tsd_destructor)(void *tsd) = nullptr;
 +
++void tsd_at_exit(void *tsd) {
++  CHECK_EQ(tsd, (void *)msan_current_thread);
++  CHECK(tsd);
++  (*tsd_destructor)(tsd);
++}
+ 
  void MsanTSDInit(void (*destructor)(void *tsd)) {
    CHECK(!tsd_key_inited);
    tsd_key_inited = true;
-+#if SANITIZER_NETBSD
-+  INTERCEPT_FUNCTION(_lwp_exit);
+-  CHECK_EQ(0, pthread_key_create(&tsd_key, destructor));
++  tsd_destructor = destructor;
+ }
+ 
+-static THREADLOCAL MsanThread* msan_current_thread;
++void SetCurrentThread(MsanThread *t) {
++  CHECK(tsd_key_inited);
++  // Make sure we do not reset the current MsanThread.
++  CHECK_EQ(0, msan_current_thread);
++  msan_current_thread = t;
++  // Make sure that MsanTSDDtor gets called at the end.
++  CHECK(tsd_key_inited);
++  if (::__cxa_thread_atexit(tsd_at_exit, t, nullptr)) {
++    Printf("MemorySanitizer: failed to setup thread atexit callback");
++    Die();
++  }
++}
+ 
+-MsanThread *GetCurrentThread() {
+-  return msan_current_thread;
++void MsanTSDDtor(void *tsd) {
++  CHECK(tsd_key_inited);
++  CHECK_EQ(tsd, (void *)msan_current_thread);
++  msan_current_thread = nullptr;
++  // Make sure that signal handler can not see a stale current thread pointer.
++  atomic_signal_fence(memory_order_seq_cst);
++  MsanThread::TSDDtor(tsd);
++}
 +#else
-   CHECK_EQ(0, pthread_key_create(&tsd_key, destructor));
-+#endif
++static pthread_key_t tsd_key;
++static bool tsd_key_inited = false;
++
++void MsanTSDInit(void (*destructor)(void *tsd)) {
++  CHECK(!tsd_key_inited);
++  tsd_key_inited = true;
++  CHECK_EQ(0, pthread_key_create(&tsd_key, destructor));
  }
  
- static THREADLOCAL MsanThread* msan_current_thread;
-@@ -196,16 +212,20 @@ void SetCurrentThread(MsanThread *t) {
-   msan_current_thread = t;
-   // Make sure that MsanTSDDtor gets called at the end.
-   CHECK(tsd_key_inited);
-+#if !SANITIZER_NETBSD
-   pthread_setspecific(tsd_key, (void *)t);
-+#endif
- }
- 
- void MsanTSDDtor(void *tsd) {
-+#if !SANITIZER_NETBSD
-   MsanThread *t = (MsanThread*)tsd;
-   if (t->destructor_iterations_ > 1) {
-     t->destructor_iterations_--;
-     CHECK_EQ(0, pthread_setspecific(tsd_key, tsd));
-     return;
-   }
-+#endif
-   msan_current_thread = nullptr;
-   // Make sure that signal handler can not see a stale current thread pointer.
+ void SetCurrentThread(MsanThread *t) {
+@@ -211,6 +256,7 @@ void MsanTSDDtor(void *tsd) {
    atomic_signal_fence(memory_order_seq_cst);
+   MsanThread::TSDDtor(tsd);
+ }
++#endif
+ 
+ } // namespace __msan
+ 
