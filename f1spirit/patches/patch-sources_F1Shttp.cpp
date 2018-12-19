a823000d00f1b83aa5578d4956842cb864884406
i3enedek <pkgsrc@xn--rvztrtkrfrgp-bbb7j2b8f0b9d7a21oft.com>
2018-12-19 17:57:24 +0000

    f1spirit: Unbreak.

--- f1spirit/patches/patch-sources_F1Shttp.cpp
+++ f1spirit/patches/patch-sources_F1Shttp.cpp
@@ -1,7 +1,7 @@
 $NetBSD$
 
---- sources/F1Shttp.cpp.orig	2009-02-06 09:54:49.000000000 +0000
-+++ sources/F1Shttp.cpp
+--- F1Shttp.cpp.orig	2009-02-06 09:54:49.000000000 +0000
++++ F1Shttp.cpp
 @@ -3,7 +3,6 @@
  #include "string.h"
  
