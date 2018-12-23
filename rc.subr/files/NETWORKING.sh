6a521a98531fed81ab57d90db33de3656bef9ab2
Adam Ciarciński <aciarcinski@teonite.com>
2018-12-23 15:47:01 +0100

    Removed duplicates: p5-Pod-Usage, p5-Test-Most, p5-XML-Twig, prosody, rc.subr, revbump, samtools, simulavr, validns

--- rc.subr/files/NETWORKING.sh
+++
@@ -1,9 +0,0 @@
-#!@RCD_SCRIPTS_SHELL@
-#
-# $NetBSD: NETWORKING.sh,v 1.1.1.1 2003/09/13 22:47:59 jeremy-c-reed Exp $
-#
-
-# PROVIDE: NETWORKING NETWORK
-
-#	This is a dummy dependency, for services which require networking
-#	to be operational before starting.
