a648af686a9cc99b68b7d2ecff3fb7ed631daa04
Jason Bacon <bacon@NetBSD.org>
2018-12-22 20:12:05 -0600

    canu: Add NetBSD patches, successful build

---
+++ canu/patches/patch-utility_objectStore.C
@@ -0,0 +1,17 @@
+$NetBSD$
+
+# Portability
+--- utility/objectStore.C.orig	2018-12-23 01:57:06.000000000 +0000
++++ utility/objectStore.C
+@@ -286,6 +286,11 @@ fetchFromObjectStore(char *requested) {
+   //  Otherwise, we're still the parent, so wait for the (-1 == any) child
+   //  process to terminate.
+ 
++// NetBSD does not provide WEXITED so send empty bitmask
++#ifdef __NetBSD__
++#define	WEXITED	0
++#endif
++
+   waitpid(-1, &err, WEXITED);
+ 
+   if ((WIFEXITED(err)) &&
