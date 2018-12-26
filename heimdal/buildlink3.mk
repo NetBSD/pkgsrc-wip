78bba0bdba769c365b0cafcbbb2925ee14f41bfc
Sevan Janiyan <venture37@geeklan.co.uk>
2018-12-26 21:18:35 +0000

    security/heimdal update to v7.5.0 - See TODO file.

---
+++ heimdal/buildlink3.mk
@@ -0,0 +1,32 @@
+# $NetBSD: buildlink3.mk,v 1.57 2018/12/09 18:52:07 adam Exp $
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+BUILDLINK_TREE+=	heimdal
+
+.if !defined(HEIMDAL_BUILDLINK3_MK)
+HEIMDAL_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.heimdal+=	heimdal>=0.4e
+BUILDLINK_ABI_DEPENDS.heimdal+=	heimdal>=1.5.3nb19
+BUILDLINK_PKGSRCDIR.heimdal?=	../../security/heimdal
+BUILDLINK_INCDIRS.heimdal?=	include/krb5
+
+pkgbase := heimdal
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.heimdal:Mldap)
+.  include "../../databases/openldap-client/buildlink3.mk"
+.endif
+.include "../../security/openssl/buildlink3.mk"
+
+CHECK_BUILTIN.heimdal:=	yes
+.include "../../security/heimdal/builtin.mk"
+CHECK_BUILTIN.heimdal:=	no
+.if !empty(USE_BUILTIN.heimdal:M[nN][oO])
+.include "../../databases/sqlite3/buildlink3.mk"
+.include "../../mk/bdb.buildlink3.mk"
+.endif
+.endif # HEIMDAL_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-heimdal
