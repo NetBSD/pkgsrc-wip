b5699a3c51e3439557b3e9110a8cbc465d20b06a
Yorick Hardy <yorickhardy@gmail.com>
2019-01-01 15:31:32 +0200

    newsboat: update to newsboat-2.14
    Newsboat now depends on lang/rust to build. For those who prefer
    an older (rust independent) version, newsboat-2.13 is available
    as wip/newsboat213.
    
    * Changes for Newsboat
    
    ** 2.14 - 2018-12-29
    
    Lists below only mention user-visible changes, but I would also like to
    acknowledge contributions from the following people: Paul Woolcock, Raphael
    Nestler, Thanga Ayyanar A.
    
    *** Added
    - Dependency on Rust 1.25+. The compiler (`rustc`) and the build tool (`cargo`)
        are required
    - `download-filename-format` setting that controls how Podboat names the files.
        Default is the same as older versions of Podboat (Jagannathan Tiruvallur
        Eachambadi)
    - `podlist-format` setting that controls formatting of the items on Podboat's
        main screen. Also, a `%b` format specifier that contains just the basename
        of the download (e.g. "podcast.mp3" instead of "/home/name/podcast.mp3")
        (zaowen)
    - Human-readable message when Rust code panics (Alexander Batischev)
    - `unbind-key -a`, which unbinds all keys (Kamil Wsół)
    
    *** Changed
    - Look up `BROWSER` environment variable before defaulting to lynx(1) (Kamil
        Wsół) (#283)
    - Strip query parameters from downloaded podcasts' names (i.e. name them as
        "podcast.mp3", not "podcast.mp3?key=19ad740") (Jagannathan Tiruvallur
        Eachambadi)
    - Update translations: Russian, Ukrainian (Alexander Batischev), German
        (Lysander Trischler)
    - Document that minimum supported CURL version is 7.21.6. This has been the case
        since 2.10, but wasn't documented at the time (Alexander Batischev)
    - Update vendored version of nlohmann/json to 3.4.0
    - Update vendored version of Catch2 to 2.5.0
    
    *** Fixed
    - HTTP response 400 errors with Inoreader (Erik Li) (#175)
    - Podboat's crash (segmentation fault) when parsing a comment in the queue file.
        Comments aren't really supported by Podboat since it overwrites the file
        from time to time, but the crash is still unacceptable (Nikos Tsipinakis)
    - Newsboat displaying articles differently in "internal" and external pagers
        (Alexander Batischev)
    - One-paragraph items not rendered at all (Alexander Batischev)
    - Crash (segmentation fault) on feeds that don't provide a `url` attribute
        (ksunden)
    - Hangs when `highlight` rule matches an empty string (zaowen)
    - Article disappearing from the pager upon feed reload (zaowen)
        (https://github.com/akrennmair/newsbeuter/issues/534)

--- newsboat/patches/patch-config.sh
+++ newsboat/patches/patch-config.sh
@@ -4,14 +4,14 @@ Remove overly "smart" detection script for ncurses.  This doesn't work on NetBSD
 If libstfl is linked against the system-wide curses, you can't later link against
 ncurses.  Newsboat can use the ncursesw compat offered by NetBSD 6.x native curses.
 
---- config.sh.orig	2017-12-25 17:49:35.000000000 +0000
+--- config.sh.orig	2018-12-29 13:07:06.000000000 +0000
 +++ config.sh
-@@ -106,6 +106,8 @@ check_pkg "stfl" || fail "stfl"
- 
- if [ `uname -s` = "Darwin" ]; then
- 	check_custom "ncurses5.4" "ncurses5.4-config" || fail "ncurses5.4"
+@@ -110,6 +110,8 @@ if [ `uname -s` = "Darwin" ]; then
+     # rand crate needs Security framework, and rustc doesn't (can't) link it
+     # into libnewsboat.a
+     echo 'LDFLAGS+=-framework Security' >> config.mk
 +elif [ `uname -s` = "NetBSD" ]; then
-+       echo "Assuming native NetBSD curses should be used"
++	echo "Assuming native NetBSD curses should be used"
  elif [ `uname -s` != "OpenBSD" ]; then
  	check_pkg "ncursesw" || \
  	check_custom "ncursesw5" "ncursesw5-config" || \
