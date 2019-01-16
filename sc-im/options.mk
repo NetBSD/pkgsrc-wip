2557cc428023cc57cc7234d6215f0866a2cbb985
Sijmen J. Mulder <ik@sjmulder.nl>
2019-01-16 14:38:22 +0100

    sc-im: Remove xls option for now

--- sc-im/options.mk
+++ sc-im/options.mk
@@ -1,16 +1,10 @@
 # $NetBSD$
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sc-im
-PKG_SUPPORTED_OPTIONS+=	xls xlsx lua
-PKG_SUGGESTED_OPTIONS+=	xls xlsx lua
+PKG_SUPPORTED_OPTIONS+=	xlsx lua
+PKG_SUGGESTED_OPTIONS+=	xlsx lua
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Mxls)
-CFLAGS+=	-DXLS
-LDFLAGS+=	-lxlsreader
-.include "../../wip/libxls/buildlink3.mk"
-.endif
 
 .if !empty(PKG_OPTIONS:Mxlsx)
 CFLAGS+=	-DXLSX -DXLSX_EXPORT
