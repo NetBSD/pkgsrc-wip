f0d0dbe4c3ffd3034d6f228f9510ae22c9ad3ff0
Michael Baeuerle <michael.baeuerle@stz-e.de>
2019-01-21 10:16:45 +0100

    pinentry-fltk: Patch for keyboard shortcuts in buttons added
    GPG2 passes the button label string with hint for keyboard shortcut
    as underscore (e.g. "_OK"). This is now correctly converted to the
    internal representation of FLTK (&).

--- pinentry-fltk/patches/patch-fltk_main.cxx
+++ pinentry-fltk/patches/patch-fltk_main.cxx
@@ -1,10 +1,67 @@
 $NetBSD$
 
+Handle '_' in labels as keyboard shortcuts (used by GPG2).
 Fix format string handling (Patch from Debian).
 
 --- fltk/main.cxx.orig	2017-12-03 16:13:05.000000000 +0000
 +++ fltk/main.cxx
-@@ -241,12 +241,12 @@ static int fltk_cmd_handler(pinentry_t p
+@@ -78,6 +78,44 @@ static std::string escape_accel_utf8(con
+ 	return result;
+ }
+ 
++// For button labels
++// Accelerator '_' (used e.g. by GPG2) is converted to '&' (for FLTK)
++// '&' is escaped as in escape_accel_utf8()
++static std::string convert_accel_utf8(const char *s)
++{
++	static bool last_was_underscore = false;
++	std::string result;
++	if (NULL != s)
++	{
++		result.reserve(strlen(s));
++		for (const char *p = s; *p; ++p)
++		{
++			// & => &&
++			if ('&' == *p)
++				result.push_back(*p);
++			// _ => & (handle '__' as escaped underscore)
++			if ('_' == *p)
++			{
++				if (last_was_underscore)
++				{
++					result.push_back(*p);
++					last_was_underscore = false;
++				}
++				else
++					last_was_underscore = true;
++			}
++			else
++			{
++				if (last_was_underscore)
++					result.push_back('&');
++				result.push_back(*p);
++				last_was_underscore = false;
++			}
++		}
++	}
++	return result;
++}
++
+ class cancel_exception
+ {
+ 
+@@ -111,8 +149,8 @@ static int fltk_cmd_handler(pinentry_t p
+ 		// TODO: Add parent window to pinentry-fltk window
+ 		//if (pe->parent_wid){}
+ 		std::string title  = !is_empty(pe->title)?pe->title:PGMNAME;
+-		std::string ok 	   = escape_accel_utf8(pe->ok?pe->ok:(pe->default_ok?pe->default_ok:OK_STRING));
+-		std::string cancel = escape_accel_utf8(pe->cancel?pe->cancel:(pe->default_cancel?pe->default_cancel:CANCEL_STRING));
++		std::string ok 	   = convert_accel_utf8(pe->ok?pe->ok:(pe->default_ok?pe->default_ok:OK_STRING));
++		std::string cancel = convert_accel_utf8(pe->cancel?pe->cancel:(pe->default_cancel?pe->default_cancel:CANCEL_STRING));
+ 
+ 		if (!!pe->pin) // password (or confirmation)
+ 		{
+@@ -241,12 +279,12 @@ static int fltk_cmd_handler(pinentry_t p
  				if (pe->one_button)
  				{
  					fl_ok = ok.c_str();
@@ -19,7 +76,7 @@ Fix format string handling (Patch from Debian).
  					{
  					case 0: result = 1; break;
  					case 2: result = 0; break;
-@@ -256,7 +256,7 @@ static int fltk_cmd_handler(pinentry_t p
+@@ -256,7 +294,7 @@ static int fltk_cmd_handler(pinentry_t p
  				}
  				else
  				{
