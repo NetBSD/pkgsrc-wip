6b776bb3fa0a930f87936434f100bc9a5a0b00b2
Frederic Cambus <fred@statdns.com>
2019-01-24 15:37:44 +0100

    Import libansilove 1.1.0 as wip/libansilove.
    Library for converting ANSI, ASCII, and other formats to PNG, allowing
    to render ANSI and artscene-related file formats into PNG images, and
    supporting ANSI (.ANS), PCBoard (.PCB), Binary (.BIN), Artworx (.ADF),
    iCE Draw (.IDF), Tundra (.TND) and XBin (.XB) formats.
    
    It creates size optimized 4-bit PNG files and supports SAUCE (Standard
    Architecture for Universal Comment Extentions), 80x25 and 80x50 PC fonts
    (including all the 14 MS-DOS charsets), Amiga fonts, and iCE colors.

---
+++ libansilove/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libansilove
+
+.if !defined(LIBANSILOVE_BUILDLINK3_MK)
+LIBANSILOVE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libansilove+=	libansilove>=1.1.0
+BUILDLINK_PKGSRCDIR.libansilove?=	../../graphics/libansilove
+.endif	# LIBANSILOVE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libansilove
