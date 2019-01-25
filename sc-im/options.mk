6103b1679d4548130b79393d0d72b918cd6fd9a2
Sijmen J. Mulder <ik@sjmulder.nl>
2019-01-25 15:05:45 +0100

    sc-im: Make color optional

--- sc-im/options.mk
+++ sc-im/options.mk
@@ -1,10 +1,14 @@
 # $NetBSD$
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sc-im
-PKG_SUPPORTED_OPTIONS+=	xlsx lua
-PKG_SUGGESTED_OPTIONS+=	xlsx lua
+PKG_SUPPORTED_OPTIONS+=	color xlsx lua
+PKG_SUGGESTED_OPTIONS+=	color xlsx lua
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mcolor)
+CFLAGS+=	-DUSECOLORS
+.endif
 
 .if !empty(PKG_OPTIONS:Mxlsx)
 CFLAGS+=	-DXLSX -DXLSX_EXPORT
