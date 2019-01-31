e9796e8a6433fe60eadb79372997482aa28730a9
Sijmen J. Mulder <ik@sjmulder.nl>
2019-01-31 23:55:37 +0100

    libxls: Update to 1.5.0rc0

--- libxls/buildlink3.mk
+++ libxls/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libxls
 .if !defined(LIBXLS_BUILDLINK3_MK)
 LIBXLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxls+=	libxls>=1.4.0
+BUILDLINK_API_DEPENDS.libxls+=	libxls>=1.5.0rc0
 BUILDLINK_PKGSRCDIR.libxls?=	../../wip/libxls
 
 .include "../../converters/libiconv/buildlink3.mk"
