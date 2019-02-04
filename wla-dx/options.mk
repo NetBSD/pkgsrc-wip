49626c7d6cd60daac8709bd351b0b396daba46c7
Dean Matzkov <bapabooiee@gmail.com>
2019-02-03 18:05:06 -0700

    wla-dx: make building the documentation optional

---
+++ wla-dx/options.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR= PKG_OPTIONS.wla-dx
+PKG_SUPPORTED_OPTIONS= doc
+PKG_SUGGESTED_OPTIONS=
+
+PLIST_VARS+=	doc
+
+.include "../../mk/bsd.options.mk"
+
+
+.if !empty(PKG_OPTIONS:Mdoc)
+BUILD_DEPENDS+=     py[0-9]*-sphinx-[0-9]*:../../textproc/py-sphinx
+PLIST.doc=	yes
+.endif
