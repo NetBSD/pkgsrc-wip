d1bbedcb96d41cbf0af5ec9f7322be5fd2e2c1ac
David Shao <davshao@gmail.com>
2019-02-23 20:30:07 -0800

    xf86-video-intel-dfbsd: Update wip/xf86-video-intel-dfbsd through git 2019-01-21
    Changes:
    Switch to only using the git version

--- xf86-video-intel-dfbsd/patches/patch-src_intel__device.c
+++ xf86-video-intel-dfbsd/patches/patch-src_intel__device.c
@@ -1,23 +1,56 @@
 $NetBSD$
 
-From past DragonFly dports x11-drivers/xf86-video-intel29 and FreeBSD ports
+Patches from FreeBSD ports x11-drivers/xf86-video-intel
 
---- src/intel_device.c.orig	2014-11-18 21:50:39.000000000 +0000
+* WITH_GETLINE patch.
+
+* Patch to use "i915kms" as module name from FreeBSD ports
+x11-drivers/xf86-video-intel.
+
+Patch from DragonFly dports x11-drivers/xf86-video-intel29
+
+* i915 does not use hw.dri.0.busid so simplify to basics.
+
+--- src/intel_device.c.orig	2018-05-12 09:01:10.000000000 +0000
 +++ src/intel_device.c
-@@ -398,8 +398,16 @@ static int __intel_open_device__legacy(c
+@@ -28,6 +28,11 @@
+ #include "config.h"
+ #endif
+ 
++#if defined(__FreeBSD__)
++#define _WITH_GETLINE	/* to expose getline() in stdio.h on FreeBSD */
++#include <stdio.h>	/* for getline() */
++#endif
++
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <assert.h>
+@@ -204,6 +209,9 @@ static inline struct intel_device *intel
+ }
+ 
+ static const char *kernel_module_names[] ={
++#if defined(__FreeBSD__)
++	"i915kms",
++#endif
+ 	"i915",
+ 	NULL,
+ };
+@@ -424,6 +432,10 @@ static int __intel_open_device__legacy(c
+ 		 "pci:%04x:%02x:%02x.%d",
  		 pci->domain, pci->bus, pci->dev, pci->func);
  
- 	ret = drmCheckModesettingSupported(id);
-+#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
-+	if (ret || 1) {
-+		if (xf86LoadKernelModule("i915kms"))
-+#elif defined(__DragonFly__)
-+	if (ret || 1) {
-+		if (xf86LoadKernelModule("i915"))
++#if defined(__DragonFly__)
++	/* assume modesetting for i915, allow multiple loads and no fbcon */
++	load_i915_kernel_module();
 +#else
+ 	ret = drmCheckModesettingSupported(id);
  	if (ret) {
- 		if (xf86LoadKernelModule("i915"))
+ 		if (load_i915_kernel_module() == 0)
+@@ -433,6 +445,7 @@ static int __intel_open_device__legacy(c
+ 		/* Be nice to the user and load fbcon too */
+ 		(void)xf86LoadKernelModule("fbcon");
+ 	}
 +#endif
- 			ret = drmCheckModesettingSupported(id);
- 		if (ret)
- 			return -1;
+ 
+ 	return fd_set_nonblock(drmOpen(NULL, id));
+ }
--- xf86-video-intel-dfbsd/patches/patch-src_sna_kgem.c
+++ xf86-video-intel-dfbsd/patches/patch-src_sna_kgem.c
@@ -1,13 +1,31 @@
 $NetBSD$
 
-From DragonFly dports x11-drivers/xf86-video-intel29
+WITH_GETLINE patch from FreeBSD ports x11-drivers/xf86-video-intel
 
---- src/sna/kgem.c.orig	2014-12-09 16:08:21.000000000 +0000
+Patch for --enable-debug from DragonFly dports
+x11-drivers/xf86-video-intel29 
+
+Additional patch for DragonFly because struct drm_i915_gem_mmap
+has a field flags.
+
+--- src/sna/kgem.c.orig	2019-01-10 10:01:58.000000000 +0000
 +++ src/sna/kgem.c
-@@ -70,7 +70,11 @@ search_snoop_cache(struct kgem *kgem, un
- #define DBG_NO_CREATE2 0
+@@ -29,6 +29,11 @@
+ #include "config.h"
+ #endif
+ 
++#if defined(__FreeBSD__)
++#define _WITH_GETLINE	/* to expose getline() in stdio.h on FreeBSD */
++#include <stdio.h>	/* for getline() */
++#endif
++
+ #include "sna.h"
+ #include "sna_reg.h"
+ 
+@@ -71,7 +76,11 @@ search_snoop_cache(struct kgem *kgem, un
  #define DBG_NO_USERPTR 0
  #define DBG_NO_UNSYNCHRONIZED_USERPTR 0
+ #define DBG_NO_COHERENT_MMAP_GTT 0
 +#if defined(__DragonFly__)
 +#define DBG_NO_LLC 1
 +#else
@@ -16,7 +34,32 @@ From DragonFly dports x11-drivers/xf86-video-intel29
  #define DBG_NO_SEMAPHORES 0
  #define DBG_NO_MADV 0
  #define DBG_NO_UPLOAD_CACHE 0
-@@ -2802,11 +2806,21 @@ bool __kgem_ring_is_idle(struct kgem *kg
+@@ -682,7 +691,11 @@ retry_wc:
+ 
+ static void *__kgem_bo_map__cpu(struct kgem *kgem, struct kgem_bo *bo)
+ {
++#if defined(__DragonFly__)
++	struct local_i915_gem_mmap2 arg;
++#else
+ 	struct local_i915_gem_mmap arg;
++#endif
+ 	int err;
+ 
+ 	VG_CLEAR(arg);
+@@ -691,7 +704,12 @@ static void *__kgem_bo_map__cpu(struct k
+ retry:
+ 	arg.handle = bo->handle;
+ 	arg.size = bytes(bo);
++#if defined(__DragonFly__)
++	arg.flags = 0;
++	if ((err = do_ioctl(kgem->fd, LOCAL_IOCTL_I915_GEM_MMAP_v2, &arg))) {
++#else
+ 	if ((err = do_ioctl(kgem->fd, LOCAL_IOCTL_I915_GEM_MMAP, &arg))) {
++#endif
+ 		DBG(("%s: failed %d, throttling/cleaning caches\n",
+ 		     __FUNCTION__, err));
+ 		assert(err != -EINVAL || bo->prime);
+@@ -3313,11 +3331,21 @@ bool __kgem_ring_is_idle(struct kgem *kg
  	if (rq) {
  		struct kgem_request *tmp;
  
