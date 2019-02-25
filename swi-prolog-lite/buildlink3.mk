bcb76a023f72fe06cdd7cb104ccbc1452f26db05
Leonardo Taccari <leot@NetBSD.org>
2019-02-25 15:31:38 +0100

    swi-prolog-lite: Bump BUILDLINK_API_DEPENDS requirements
    Bump BUILDLINK_API_DEPENDS to 8.0.1 and remove no more needed ABI depends.
    (Previously, except on Linux, no shared libraries were provided and probably the
    buildlink3 requirements were not very important.)

--- swi-prolog-lite/buildlink3.mk
+++ swi-prolog-lite/buildlink3.mk
@@ -5,8 +5,7 @@ BUILDLINK_TREE+=	swi-prolog-lite
 .if !defined(SWI_PROLOG_LITE_BUILDLINK3_MK)
 SWI_PROLOG_LITE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.swi-prolog-lite+=	swi-prolog-lite>=5.2.9
-BUILDLINK_ABI_DEPENDS.swi-prolog-lite+=	swi-prolog-lite>=6.6.6nb2
+BUILDLINK_API_DEPENDS.swi-prolog-lite+=	swi-prolog-lite>=8.0.1
 BUILDLINK_PKGSRCDIR.swi-prolog-lite?=	../../wip/swi-prolog-lite
 
 .endif	# SWI_PROLOG_LITE_BUILDLINK3_MK
