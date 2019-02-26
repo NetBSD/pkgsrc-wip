9417cbb095cc587cdb94785a87aae8cf49a05911
Leonardo Taccari <leot@NetBSD.org>
2019-02-26 18:34:35 +0100

    zbar: Remove not used bl3
    zbar failed linking because ImageMagick didn't included the liblqr bl3.

--- zbar/buildlink3.mk
+++ zbar/buildlink3.mk
@@ -10,9 +10,7 @@ BUILDLINK_PKGSRCDIR.zbar?=	../../wip/zbar
 
 pkgbase:=	zbar
 .include "../../mk/pkg-build-options.mk"
-.include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/pcre/buildlink3.mk"
-.include "../../graphics/liblqr/buildlink3.mk"
+
 .endif # ZBAR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-zbar
