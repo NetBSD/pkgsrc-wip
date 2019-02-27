3b02de2c8747a9c57db4c2ca09b9bdde41cd1c4b
andyb <andyb@m42.one>
2019-02-27 10:51:42 +0100

    configures now and added gtk2/3 option, but not building @the moment

--- spacefm/options.mk
+++ spacefm/options.mk
@@ -7,14 +7,34 @@ PKG_OPTIONS_GROUP.gtk=          gtk2 gtk3
 
 PKG_SUGGESTED_OPTIONS=  gtk2
 
+.include "../../mk/bsd.options.mk"
+
 PLIST_VARS+=            gtk3
 .if !empty(PKG_OPTIONS:Mgtk2)
-CONFIGURE_ARGS+=        --enable-default-toolkit=cairo-gtk2
+CONFIGURE_ARGS+=        --with-gtk2
+GTK_CFLAGS=      $(pkg-config --cflags gtk+-2.0)
+GTK_LIBS=        $(pkg-config --libs gtk+-2.0)
+CFLAGS+=          $(pkg-config --cflags gtk+-2.0) $(pkg-config --cflags glib-2.0) $(pkg-config --cflags gdk) $(pkg-config --cflags libffmpegthumbnailer)
+LDFLAG+=         $(pkg-config --libs gtk+-2.0) $(pkg-config --libs glib-2.0) $(pkg-config --libs gdk) $(pkg-config --libs libffmpegthumbnailer)
+export FFMPEG_CFLAGS=   $(pkg-config --cflags libffmpegthumbnailer)
+export FFMPEG_LIBS=     $(pkg-config --libs libffmpegthumbnailer)
+AM_CFLAGS+=      $(pkg-config --cflags gtk+-2.0)
+CPPFLAGS+=       $(pkg-config --cflags gtk+-2.0) $(pkg-config --cflags glib-2.0) $(pkg-config --cflags gdk) $(pkg-config --cflags libffmpegthumbnailer)
+
 .include "../../x11/gtk2/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mgtk3)
-CONFIGURE_ARGS+=        --enable-default-toolkit=cairo-gtk3
+CONFIGURE_ARGS+=        --with-gtk3
+GTK_CFLAGS=      $(pkg-config --cflags gtk+-3.0)
+GTK_LIBS=        $(pkg-config --libs gtk+-3.0)
+CFLAGS+=          $(pkg-config --cflags gtk+-3.0) $(pkg-config --cflags glib-2.0) $(pkg-config --cflags gdk) $(pkg-config --cflags libffmpegthumbnailer)
+LDFLAG+=         $(pkg-config --libs gtk+-3.0) $(pkg-config --libs glib-2.0) $(pkg-config --libs gdk) $(pkg-config --libs libffmpegthumbnailer)
+export FFMPEG_CFLAGS=   $(pkg-config --cflags libffmpegthumbnailer)
+export FFMPEG_LIBS=     $(pkg-config --libs libffmpegthumbnailer)
+AM_CFLAGS+=      $(pkg-config --cflags gtk+-3.0)
+CPPFLAGS+=       $(pkg-config --cflags gtk+-3.0) $(pkg-config --cflags glib-2.0) $(pkg-config --cflags gdk) $(pkg-config --cflags libffmpegthumbnailer)
+
 .include "../../x11/gtk3/buildlink3.mk"
 PLIST.gtk3=             yes
 .endif
