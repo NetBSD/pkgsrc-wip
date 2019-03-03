fefc166fb104e74a31ba13a64b5c5894795bb5a5
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2019-03-03 01:02:03 +0100

    +new, update

---
+++ py-mapy-package/patches/patch-setup.py
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- setup.py.orig	2017-04-21 03:50:32.000000000 +0000
++++ setup.py
+@@ -20,7 +20,6 @@ setup(
+     keywords = "finite element modeling analysis",
+     url = "https://github.com/saullocastro/mapy",
+     packages=find_packages(),
+-    long_description=read('README.md'),
+     classifiers=[
+         "Development Status :: 3 - Alpha",
+         "Topic :: Scientific/Engineering :: Mathematics",
