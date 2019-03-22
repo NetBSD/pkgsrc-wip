9bf4926af0c3dabfaae4331ec0a89a249be885bf
Dr. Thomas Orgis <thomas.orgis@uni-hamburg.de>
2019-03-22 19:16:20 +0100

    openblas: update to 0.3.5

--- openblas/buildlink3.mk
+++ openblas/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	openblas
 .if !defined(OPENBLAS_BUILDLINK3_MK)
 OPENBLAS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.openblas+=	openblas>=0.2.20
+BUILDLINK_API_DEPENDS.openblas+=	openblas>=0.3.5
 BUILDLINK_PKGSRCDIR.openblas?=		../../wip/openblas
 .endif	# OPENBLAS_BUILDLINK3_MK
 
--- openblas/patches/patch-getarch.c
+++ openblas/patches/patch-getarch.c
@@ -1,9 +1,9 @@
 @NetBSD@
 
 Do not override our parallel make choice.
---- getarch.c.orig	2018-04-03 12:56:19.322128443 +0200
-+++ getarch.c	2018-04-03 12:57:42.424122776 +0200
-@@ -1134,21 +1134,6 @@
+--- getarch.c.orig	2018-12-31 23:09:59.000000000 +0100
++++ getarch.c	2019-03-22 17:24:40.477260427 +0100
+@@ -1236,19 +1236,6 @@
  #endif
  #endif
  
@@ -17,9 +17,7 @@ Do not override our parallel make choice.
 -#elif NO_PARALLEL_MAKE==1
 -    printf("MAKE += -j 1\n");
 -#else
--#ifndef OS_WINDOWS
 -    printf("MAKE += -j %d\n", get_num_cores());
--#endif
 -#endif
 -
      break;
