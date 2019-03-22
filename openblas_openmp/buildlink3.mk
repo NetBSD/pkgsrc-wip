d557444a5edb43cadceb51b3fe258bb39497e12e
Dr. Thomas Orgis <thomas.orgis@uni-hamburg.de>
2019-03-22 19:16:20 +0100

    openblas: update to 0.3.5

--- openblas_openmp/buildlink3.mk
+++ openblas_openmp/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	openblas_openmp
 .if !defined(OPENBLAS_OPENMP_BUILDLINK3_MK)
 OPENBLAS_OPENMP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.openblas_openmp+=	openblas_openmp>=0.2.20
+BUILDLINK_API_DEPENDS.openblas_openmp+=	openblas_openmp>=0.3.5
 BUILDLINK_PKGSRCDIR.openblas_openmp?=	../../wip/openblas_openmp
 .endif	# OPENBLAS_OPENMP_BUILDLINK3_MK
 
--- openblas_pthread/buildlink3.mk
+++ openblas_pthread/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	openblas_pthread
 .if !defined(OPENBLAS_PTHREAD_BUILDLINK3_MK)
 OPENBLAS_PTHREAD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.openblas_pthread+=	openblas_pthread>=0.2.20
+BUILDLINK_API_DEPENDS.openblas_pthread+=	openblas_pthread>=0.3.5
 BUILDLINK_PKGSRCDIR.openblas_pthread?=		../../wip/openblas_pthread
 .endif	# OPENBLAS_PTHREAD_BUILDLINK3_MK
 
