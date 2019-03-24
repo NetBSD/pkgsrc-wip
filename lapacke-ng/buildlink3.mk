0aed1105ce7b44d16b3966b3b3a6b33926a30003
Dr. Thomas Orgis <thomas.orgis@uni-hamburg.de>
2019-03-24 20:50:09 +0100

    lapacke-ng: rename buildlink3 guard

--- lapacke-ng/buildlink3.mk
+++ lapacke-ng/buildlink3.mk
@@ -2,8 +2,8 @@
 
 BUILDLINK_TREE+=	lapacke
 
-.if !defined(CBLAS_BUILDLINK3_MK)
-CBLAS_BUILDLINK3_MK:=
+.if !defined(LAPACKE_BUILDLINK3_MK)
+LAPACKE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.lapacke+=	lapacke-ng>=3.8.0
 BUILDLINK_PKGSRCDIR.lapacke?=	../../wip/lapacke-ng
