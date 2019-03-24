750ffef100d894fa48b2ecf1069f729e3b0b477e
Dr. Thomas Orgis <thomas.orgis@uni-hamburg.de>
2019-03-24 15:50:11 +0100

    lapacke-ng: buildlink cleanup

--- lapacke-ng/buildlink3.mk
+++ lapacke-ng/buildlink3.mk
@@ -5,9 +5,8 @@ BUILDLINK_TREE+=	lapacke
 .if !defined(CBLAS_BUILDLINK3_MK)
 CBLAS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cblas+=	cblas-ng>=3.8.0
+BUILDLINK_API_DEPENDS.lapacke+=	lapacke-ng>=3.8.0
 BUILDLINK_PKGSRCDIR.lapacke?=	../../wip/lapacke-ng
-BUILDLINK_DEPMETHOD.lapacke?=	full
 .endif
 
 BUILDLINK_TREE+=	-lapacke
