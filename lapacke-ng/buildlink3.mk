56487910330e0886cec7d11cbf7857d850b42146
Dr. Thomas Orgis <thomas.orgis@uni-hamburg.de>
2019-03-25 01:22:59 +0100

    lapacke-ng: use consinstent naming in bl3 file

--- lapacke-ng/buildlink3.mk
+++ lapacke-ng/buildlink3.mk
@@ -1,12 +1,12 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	lapacke
+BUILDLINK_TREE+=	lapacke-ng
 
-.if !defined(LAPACKE_BUILDLINK3_MK)
-LAPACKE_BUILDLINK3_MK:=
+.if !defined(LAPACKE_NG_BUILDLINK3_MK)
+LAPACKE_NG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lapacke+=	lapacke-ng>=3.8.0
-BUILDLINK_PKGSRCDIR.lapacke?=	../../wip/lapacke-ng
+BUILDLINK_API_DEPENDS.lapacke-ng+=	lapacke-ng>=3.8.0
+BUILDLINK_PKGSRCDIR.lapacke-ng?=	../../wip/lapacke-ng
 .endif
 
-BUILDLINK_TREE+=	-lapacke
+BUILDLINK_TREE+=	-lapacke-ng
