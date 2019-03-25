2ad6faa43f3a5b8c72d59940693c55d2b71351d9
Dr. Thomas Orgis <thomas.orgis@uni-hamburg.de>
2019-03-25 01:23:40 +0100

    cblas-ng: use consinstent naming in bl3 file

--- cblas-ng/buildlink3.mk
+++ cblas-ng/buildlink3.mk
@@ -1,12 +1,12 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	cblas
+BUILDLINK_TREE+=	cblas-ng
 
-.if !defined(CBLAS_BUILDLINK3_MK)
-CBLAS_BUILDLINK3_MK:=
+.if !defined(CBLAS_NG_BUILDLINK3_MK)
+CBLAS_NG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cblas+=	cblas-ng>=3.8.0
-BUILDLINK_PKGSRCDIR.cblas?=	../../wip/cblas-ng
+BUILDLINK_API_DEPENDS.cblas-ng+=	cblas-ng>=3.8.0
+BUILDLINK_PKGSRCDIR.cblas-ng?=	../../wip/cblas-ng
 .endif
 
-BUILDLINK_TREE+=	-cblas
+BUILDLINK_TREE+=	-cblas-ng
