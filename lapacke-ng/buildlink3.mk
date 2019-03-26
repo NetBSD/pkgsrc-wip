dc41a73f399900ba3a78c8800c70b6b4f188ecee
Dr. Thomas Orgis <thomas.orgis@uni-hamburg.de>
2019-03-26 16:29:38 +0100

    lapacke-ng: updated with wip/mk/blas.buildlink3.mk

--- lapacke-ng/buildlink3.mk
+++ lapacke-ng/buildlink3.mk
@@ -7,6 +7,8 @@ LAPACKE_NG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.lapacke-ng+=	lapacke-ng>=3.8.0
 BUILDLINK_PKGSRCDIR.lapacke-ng?=	../../wip/lapacke-ng
+
+.include	"../../wip/mk/blas.buildlink3.mk"
 .endif
 
 BUILDLINK_TREE+=	-lapacke-ng
