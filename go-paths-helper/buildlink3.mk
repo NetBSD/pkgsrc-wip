93cde536f25c49963216716660c9044951e2f896
Chris Lasocki <krz.lasocki@gmail.com>
2019-04-03 18:37:45 +0000

    go-paths-helper, go-properties-map: new packages

---
+++ go-paths-helper/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.2 2017/08/25 16:49:12 jperkin Exp $
+
+BUILDLINK_TREE+=	go-paths-helper
+
+.if !defined(GO_PATHS_HELPER_BUILDLINK3_MK)
+GO_PATHS_HELPER_BUILDLINK3_MK:=
+
+BUILDLINK_CONTENTS_FILTER.go-paths-helper=	${EGREP} gopkg/
+BUILDLINK_DEPMETHOD.go-paths-helper?=	build
+
+#UILDLINK_API_DEPENDS.go-paths-helper+=	go-paths-helper>=1.3.0
+BUILDLINK_PKGSRCDIR.go-paths-helper?=	../../wip/go-paths-helper
+
+.endif  # GO_PATHS_HELPER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	go-paths-helper
