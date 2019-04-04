374f43ba2a9186b4af39c468f2154bac491a92ed
Chris Lasocki <krz.lasocki@gmail.com>
2019-04-04 13:41:42 +0000

    go-properteis-map: further attempts at building,
    push for mailinglist thread

--- go-paths-helper/buildlink3.mk
+++ go-paths-helper/buildlink3.mk
@@ -5,10 +5,8 @@ BUILDLINK_TREE+=	go-paths-helper
 .if !defined(GO_PATHS_HELPER_BUILDLINK3_MK)
 GO_PATHS_HELPER_BUILDLINK3_MK:=
 
-BUILDLINK_CONTENTS_FILTER.go-paths-helper=	${EGREP} gopkg/
-BUILDLINK_DEPMETHOD.go-paths-helper?=	build
 
-#UILDLINK_API_DEPENDS.go-paths-helper+=	go-paths-helper>=1.3.0
+BUILDLINK_DEPMETHOD.go-paths-helper?=	build
 BUILDLINK_PKGSRCDIR.go-paths-helper?=	../../wip/go-paths-helper
 
 .endif  # GO_PATHS_HELPER_BUILDLINK3_MK
