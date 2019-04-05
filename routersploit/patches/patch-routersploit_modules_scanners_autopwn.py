604fad68069bd5177c44b129f31417685b843577
Leonardo Taccari <leot@NetBSD.org>
2019-04-05 16:39:26 +0200

    routersploit: Avoid hardcoded paths to make several modules working again
    scanners/autopwn and other modules accidentally assumed hardcoded
    paths relative to $PWD making running an installed rsf.py most
    likely unworkable.
    
    Address all hardcoded uses triggered by scanners/autopwn (and
    according a quick grep hopefully no other significant hardcoded
    $PWD assumptions shouldn't be around).
    
    Remove TODO, probably it can be considered ready to be imported!
    
    XXX: Share all these patches upstream!

---
+++ routersploit/patches/patch-routersploit_modules_scanners_autopwn.py
@@ -0,0 +1,18 @@
+$NetBSD$
+
+- Use MODULES_DIR instead of picking up modules in ${PWD} (and mostly likely
+  failing)
+
+--- routersploit/modules/scanners/autopwn.py.orig	2018-10-17 17:30:53.000000000 +0000
++++ routersploit/modules/scanners/autopwn.py
+@@ -34,8 +34,8 @@ class Exploit(Exploit):
+         self.vulnerabilities = []
+         self.creds = []
+         self.not_verified = []
+-        self._exploits_directories = [path.join("routersploit/modules/exploits/", module) for module in self.modules]
+-        self._creds_directories = [path.join("routersploit/modules/creds/", module) for module in self.modules]
++        self._exploits_directories = [path.join(utils.MODULES_DIR, "exploits", module) for module in self.modules]
++        self._creds_directories = [path.join(utils.MODULES_DIR, "creds", module) for module in self.modules]
+ 
+     def run(self):
+         self.vulnerabilities = []
