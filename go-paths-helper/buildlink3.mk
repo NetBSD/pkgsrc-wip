cc2912b8208d0ed7803fb4fc8c11b1b56c44331d
Chris Lasocki <krz.lasocki@gmail.com>
2019-04-15 16:35:12 +0000

    arduino-builder: fix version comp against commit hashes in blk3-s
    * fixed comparison of BUILDLINK_API_DEPENDS against commit hashes
      in go-properties-map and go-paths-helper, as some dependencies
      have no Gihub releases or versioning at all
    
    * go-properties-map now links properly to arduino-builder, one
      less build error.

--- go-paths-helper/buildlink3.mk
+++ go-paths-helper/buildlink3.mk
@@ -7,7 +7,7 @@ GO_PATHS_HELPER_BUILDLINK3_MK:=
 BUILDLINK_CONTENTS_FILTER.go-paths-helper=	${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-paths-helper?=		build
 
-BUILDLINK_API_DEPENDS.go-paths-helper+=	go-paths-helper>=c3c98d1bf2e1069f60ab84bff3a2eb3c5422f3b0
+BUILDLINK_API_DEPENDS.go-paths-helper+=	go-paths-helper>=0
 BUILDLINK_PKGSRCDIR.go-paths-helper?=	../../wip/go-paths-helper
 .endif	# GO_PATHS_HELPER_BUILDLINK3_MK
 
--- go-properties-map/buildlink3.mk
+++ go-properties-map/buildlink3.mk
@@ -7,7 +7,7 @@ GO_PROPERTIES_MAP_BUILDLINK3_MK:=
 BUILDLINK_CONTENTS_FILTER.go-properties-map=	${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-properties-map?=		build
 
-BUILDLINK_API_DEPENDS.go-properties-map+=	go-properties-map>=ad37f0cfeff29fadeabe6b2f7f852d8db1fb5c41
+BUILDLINK_API_DEPENDS.go-properties-map+=	go-properties-map>=0
 BUILDLINK_PKGSRCDIR.go-properties-map?=	../../wip/go-properties-map
 
 .include "../../wip/go-paths-helper/buildlink3.mk"
