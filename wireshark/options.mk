9f088b49de4ff67ff40e6a328cd7f5c29b76c581
Adam Ciarciński <aciarcinski@teonite.com>
2019-04-17 17:26:07 +0200

    fine tune wireshark

--- wireshark/options.mk
+++ wireshark/options.mk
@@ -1,66 +1,62 @@
 # $NetBSD: options.mk,v 1.22 2018/12/03 15:35:15 adam Exp $
 
-PKG_OPTIONS_VAR=		PKG_OPTIONS.wireshark
-PKG_SUPPORTED_OPTIONS=		http2 lua qt5
-PKG_SUGGESTED_OPTIONS=		qt5 lua
+PKG_OPTIONS_VAR=	PKG_OPTIONS.wireshark
+PKG_SUPPORTED_OPTIONS=	http2 lua qt5
+PKG_SUGGESTED_OPTIONS=	qt5 lua
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		icons lua mans qt
+PLIST_VARS+=		icons lua qt
+
+.if !empty(PKG_OPTIONS:Mhttp2)
+.  include "../../www/nghttp2/buildlink3.mk"
+.else
+CMAKE_ARGS+=		-DENABLE_NGHTTP2=OFF
+.endif
+
+.if !empty(PKG_OPTIONS:Mlua)
+LUA_VERSIONS_INCOMPATIBLE=	53
+.  include "../../lang/lua/buildlink3.mk"
+PLIST.lua=		yes
+.else
+CMAKE_ARGS+=		-DENABLE_LUA=OFF
+.endif
 
 .if !empty(PKG_OPTIONS:Mqt5)
 .  include "../../x11/qt5-qtsvg/buildlink3.mk"
 .  include "../../x11/qt5-qttools/buildlink3.mk"
+PLIST.qt=		yes
 .  if ${OPSYS} == "Darwin"
 .    include "../../x11/qt5-qtmacextras/buildlink3.mk"
 .  else
-.    include "../../x11/qt5-qtx11extras/buildlink3.mk"
-.  endif
-PLIST.qt=		yes
-.endif
-
-.if empty(PKG_OPTIONS:Mqt5)
-#CONFIGURE_ARGS+=	--disable-wireshark
-.else
-#CONFIGURE_ARGS+=	--enable-wireshark
-PLIST.mans=		yes
-INSTALLATION_DIRS+=	share/applications
 PLIST.icons=		yes
+.    include "../../x11/qt5-qtx11extras/buildlink3.mk"
 POST_INSTALL_TARGETS+=	install-icons
+INSTALLATION_DIRS+=	share/applications
 INSTALLATION_DIRS+=	share/icons/hicolor/scalable/apps
 ICON_SIZES=		16 32 48
 MIMEICON_SIZES=		16 24 32 48 64 128 256
 
-.  for d in ${ICON_SIZES}
+.    for d in ${ICON_SIZES}
 INSTALLATION_DIRS+=	share/icons/hicolor/${d}x${d}/apps
-.  endfor
+.    endfor
 
-.  for d in ${MIMEICON_SIZES}
+.    for d in ${MIMEICON_SIZES}
 INSTALLATION_DIRS+=	share/icons/hicolor/${d}x${d}/mimetypes
-.  endfor
+.    endfor
 
-.include "../../sysutils/desktop-file-utils/desktopdb.mk"
-.include "../../graphics/hicolor-icon-theme/buildlink3.mk"
+.    include "../../sysutils/desktop-file-utils/desktopdb.mk"
+.    include "../../graphics/hicolor-icon-theme/buildlink3.mk"
 
 .PHONY: install-icons
 install-icons:
 	${INSTALL_DATA} ${WRKSRC}/image/wsicon.svg \
 		${DESTDIR}${PREFIX}/share/icons/hicolor/scalable/apps/wireshark.svg
-.  for d in ${MIMEICON_SIZES}
+.    for d in ${MIMEICON_SIZES}
 	${INSTALL_DATA} ${WRKSRC}/image/WiresharkDoc-${d}.png \
 		${DESTDIR}${PREFIX}/share/icons/hicolor/${d}x${d}/mimetypes/application-vnd.tcpdump.pcap.png
-.  endfor
-.endif
-
-.if !empty(PKG_OPTIONS:Mlua)
-LUA_VERSIONS_INCOMPATIBLE=	53
-.include "../../lang/lua/buildlink3.mk"
-#CONFIGURE_ARGS+=	--with-lua=yes
-PLIST.lua=		yes
+.    endfor
+.  endif
 .else
-#CONFIGURE_ARGS+=	--with-lua=no
-.endif
-
-.if !empty(PKG_OPTIONS:Mhttp2)
-.include "../../www/nghttp2/buildlink3.mk"
+CMAKE_ARGS+=		-DBUILD_wireshark=OFF
 .endif
