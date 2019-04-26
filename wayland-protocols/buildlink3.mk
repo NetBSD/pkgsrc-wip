b0692008b48c2c58923236a0d3a47f251987310f
coypu <coypu@sdf.org>
2019-04-27 00:22:54 +0300

    add wayland-protocols version 1.17

---
+++ wayland-protocols/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	wayland-protocols
+
+.if !defined(WAYLAND_PROTOCOLS_BUILDLINK3_MK)
+WAYLAND_PROTOCOLS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.wayland-protocols+=	wayland-protocols>=1.17
+BUILDLINK_PKGSRCDIR.wayland-protocols?=		../../wip/wayland-protocols
+
+.endif	# WAYLAND_PROTOCOLS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-wayland
