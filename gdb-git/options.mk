905906f747d2ff26f520f48aa93b38be628ee651
coypu <coypu@sdf.org>
2019-04-26 16:41:53 +0300

    Get rid of python 3.4, 3.5 references.
    Cleanup some adjacent whitespace.
    If all versions of python are accepted, don't have a line for it
    firefox-webrtc: don't reject python3.4,3.5 if we already only
    accept 2.7

--- gdb-git/options.mk
+++ gdb-git/options.mk
@@ -7,7 +7,7 @@ PKG_SUGGESTED_OPTIONS=	python
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mpython)
-CONFIGURE_ARGS+=	--with-python=${PYTHONBIN}
-PYTHON_VERSIONS_INCOMPATIBLE=	34 35 36 37
+CONFIGURE_ARGS+=		--with-python=${PYTHONBIN}
+PYTHON_VERSIONS_INCOMPATIBLE=	36 37
 .include "../../lang/python/application.mk"
 .endif
