76fdee0dfedb115188dc2ef7f0592b273a9187c7
coypu <coypu@sdf.org>
2019-04-27 21:01:04 +0300

    plasma-workspace: fix build
    TRUE is not defined anywhere, so #define HAVE_X11 TRUE becomes
     #define HAVE_X11 0!
    
    (Perhaps this was working on a purely wayland setup?)

---
+++ plasma-workspace/patches/patch-libkworkspace_config-libkworkspace.h.cmake
@@ -0,0 +1,11 @@
+$NetBSD$
+
+Avoid HAVE_X11 TRUE, where TRUE is not defined yet!
+
+--- libkworkspace/config-libkworkspace.h.cmake.orig	2018-03-27 11:26:39.000000000 +0000
++++ libkworkspace/config-libkworkspace.h.cmake
+@@ -1,2 +1,2 @@
+ /* Define if you have X11 at all */
+-#define HAVE_X11 ${X11_FOUND}
+\ No newline at end of file
++#cmakedefine01 HAVE_X11
