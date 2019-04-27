6ddda63d61f979e40f560b2246a961c9db28c63c
coypu <coypu@sdf.org>
2019-04-27 22:36:37 +0300

    khotkeys: be happier about HAVE_X11
    ... Maybe the right way to fix all those (upstream) is to use
    X11_FOUND as a variable, which will make the extra variable
    redundant.

---
+++ khotkeys/patches/patch-config-X11.h.cmake
@@ -0,0 +1,11 @@
+$NetBSD$
+
+--- config-X11.h.cmake.orig	2018-03-27 11:10:16.000000000 +0000
++++ config-X11.h.cmake
+@@ -41,4 +41,4 @@
+ #cmakedefine HAS_RANDR_1_3 1
+ 
+ /* Define if you have X11 at all */
+-#define HAVE_X11 ${X11_FOUND}
+\ No newline at end of file
++#cmakedefine01 HAVE_X11
