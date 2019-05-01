854f67d5f627fa388c0a7cdd83b7ebcd65e8a559
coypu <coypu@sdf.org>
2019-05-01 20:28:53 +0300

    openssh8: remove INTERIX related patches.
    They make updating difficult, and I doubt we have real world interix
    users. It's likely bitrotted.
    
    Don't do our own assumptions that unistd.h is not available, more than
    upstream - I suspect all pkgsrc systems have it.
    
    For upstreaming, we'll need to rationalize a system that exists and
    can build openssh. I don't know of one, let's drop this.

--- openssh/patches/patch-auth.c
+++ openssh/patches/patch-auth.c
@@ -1,21 +1,11 @@
 $NetBSD: patch-auth.c,v 1.4 2016/01/18 12:53:26 jperkin Exp $
 
-* Replace uid 0 with ROOTUID macro.
 * Use login_getpwclass() instead of login_getclass() so that the root
   vs. default login class distinction is made correctly, from FrrrBSD's
   ports.
 
 --- auth.c.orig	2019-05-01 11:28:52.028281617 +0000
 +++ auth.c
-@@ -472,7 +472,7 @@ check_key_in_hostfiles(struct passwd *pw
- 		user_hostfile = tilde_expand_filename(userfile, pw->pw_uid);
- 		if (options.strict_modes &&
- 		    (stat(user_hostfile, &st) == 0) &&
--		    ((st.st_uid != 0 && st.st_uid != pw->pw_uid) ||
-+		    ((st.st_uid != ROOTUID && st.st_uid != pw->pw_uid) ||
- 		    (st.st_mode & 022) != 0)) {
- 			logit("Authentication refused for %.100s: "
- 			    "bad owner or modes for %.200s",
 @@ -599,7 +599,7 @@ getpwnamallow(struct ssh *ssh, const cha
  	if (!allowed_user(ssh, pw))
  		return (NULL);
