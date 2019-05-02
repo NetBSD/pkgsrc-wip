042c3425a21a0389356d18c8f8d7bc8efdf7f346
Frederic Cambus <fred@statdns.com>
2019-05-02 16:19:44 +0200

    libansilove: update to 1.1.2.
    ChangeLog:
    
    - Use 'xbin_fontsize' for character height instead of an hardcoded value,
      fixes a rendering bug reported by Bart Dumon
    - Rename the 'binary_palette' array to 'vga_palette'
    - More format validation checks for the XBin loader

--- libansilove/buildlink3.mk
+++ libansilove/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libansilove
 .if !defined(LIBANSILOVE_BUILDLINK3_MK)
 LIBANSILOVE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libansilove+=	libansilove>=1.1.1
+BUILDLINK_API_DEPENDS.libansilove+=	libansilove>=1.1.2
 BUILDLINK_PKGSRCDIR.libansilove?=	../../graphics/libansilove
 .endif	# LIBANSILOVE_BUILDLINK3_MK
 
