f39d1aabae0e17e27f1b059e30a29a982a5bdcca
Jason Bacon <bacon@NetBSD.org>
2019-05-09 09:49:27 -0500

    vsearch: Clean up patch

--- vsearch/patches/patch-src_vsearch.h
+++ vsearch/patches/patch-src_vsearch.h
@@ -1,10 +1,10 @@
 $NetBSD$
 
-# Portability
+# Add FreeBSD and NetBSD support
 
 --- src/vsearch.h.orig	2019-04-30 11:57:32.000000000 +0000
 +++ src/vsearch.h
-@@ -138,9 +138,7 @@
+@@ -138,30 +138,50 @@
  #define bswap_32(x) _byteswap_ulong(x)
  #define bswap_64(x) _byteswap_uint64(x)
  
@@ -15,7 +15,9 @@ $NetBSD$
  
  #define PROG_OS "macos"
  #include <sys/sysctl.h>
-@@ -149,20 +147,42 @@
+ #include <libkern/OSByteOrder.h>
++#include <sys/resource.h>
+ #define bswap_16(x) OSSwapInt16(x)
  #define bswap_32(x) OSSwapInt32(x)
  #define bswap_64(x) OSSwapInt64(x)
  
@@ -29,39 +31,37 @@ $NetBSD$
 -#endif
 -
  #include <sys/sysinfo.h>
-+#include <sys/resource.h>
  #include <byteswap.h>
++#include <sys/resource.h>
  
 -#endif
 +#elif defined(__FreeBSD__)
-+
+ 
 +#define PROG_OS "freebsd"
 +#include <sys/sysinfo.h>
 +#include <sys/resource.h>
-+
++#include <sys/endian.h>
 +#define bswap_16(x) bswap16(x)
 +#define bswap_32(x) bswap32(x)
 +#define bswap_64(x) bswap64(x)
 +
 +#elif defined(__NetBSD__)
- 
++
 +#define PROG_OS "netbsd"
- #include <sys/resource.h>
++#include <sys/resource.h>
 +#include <sys/types.h>
 +#include <sys/bswap.h>
-+
 +#define bswap_16(x) bswap16(x)
 +#define bswap_32(x) bswap32(x)
 +#define bswap_64(x) bswap64(x)
-+
-+// Alters behavior, but NetBSD 7 does not have this function
++// Alters behavior, but NetBSD 7 does not have getopt_long_only()
 +#define getopt_long_only getopt_long
 +
-+#else
++#else	// Other Unix
 +
 +#define PROG_OS "unknown"
 +#include <sys/sysinfo.h>
 +#include <byteswap.h>
+ #include <sys/resource.h>
  
  #endif
- 
