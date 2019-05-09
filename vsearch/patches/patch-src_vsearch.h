dfafb81e6901d4dbce5f5df37aa0ce1db597fc86
Jason Bacon <bacon@NetBSD.org>
2019-05-08 20:23:09 -0500

    vsearch: Patch for NetBSD

--- vsearch/patches/patch-src_vsearch.h
+++ vsearch/patches/patch-src_vsearch.h
@@ -2,21 +2,20 @@ $NetBSD$
 
 # Portability
 
---- src/vsearch.h.orig	2019-04-30 11:57:32 UTC
+--- src/vsearch.h.orig	2019-04-30 11:57:32.000000000 +0000
 +++ src/vsearch.h
-@@ -138,10 +138,8 @@
+@@ -138,9 +138,7 @@
  #define bswap_32(x) _byteswap_ulong(x)
  #define bswap_64(x) _byteswap_uint64(x)
  
 -#else
+-
+-#ifdef __APPLE__
 +#elif defined(__APPLE__)
  
--#ifdef __APPLE__
--
  #define PROG_OS "macos"
  #include <sys/sysctl.h>
- #include <libkern/OSByteOrder.h>
-@@ -149,20 +147,28 @@
+@@ -149,20 +147,42 @@
  #define bswap_32(x) OSSwapInt32(x)
  #define bswap_64(x) OSSwapInt64(x)
  
@@ -35,14 +34,28 @@ $NetBSD$
  
 -#endif
 +#elif defined(__FreeBSD__)
- 
++
 +#define PROG_OS "freebsd"
 +#include <sys/sysinfo.h>
- #include <sys/resource.h>
++#include <sys/resource.h>
 +
 +#define bswap_16(x) bswap16(x)
 +#define bswap_32(x) bswap32(x)
 +#define bswap_64(x) bswap64(x)
++
++#elif defined(__NetBSD__)
+ 
++#define PROG_OS "netbsd"
+ #include <sys/resource.h>
++#include <sys/types.h>
++#include <sys/bswap.h>
++
++#define bswap_16(x) bswap16(x)
++#define bswap_32(x) bswap32(x)
++#define bswap_64(x) bswap64(x)
++
++// Alters behavior, but NetBSD 7 does not have this function
++#define getopt_long_only getopt_long
 +
 +#else
 +
