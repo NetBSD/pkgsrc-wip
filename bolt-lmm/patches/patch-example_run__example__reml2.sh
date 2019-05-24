e0ee2dcb5878e58f3b3512484b79e35d5ffcde15
Jason Bacon <bacon@NetBSD.org>
2019-05-24 11:06:08 -0500

    bolt-lmm: Clean up and convert to use blas.buildlink.mk

--- bolt-lmm/patches/patch-example_run__example__reml2.sh
+++ bolt-lmm/patches/patch-example_run__example__reml2.sh
@@ -1,7 +1,8 @@
 $NetBSD$
 
-# Use PATH
---- example/run_example_reml2.sh.orig	2017-11-11 20:04:45.202815471 +0000
+# Run from installation instead of build dir
+
+--- example/run_example_reml2.sh.orig	2019-05-24 15:12:23.021935693 +0000
 +++ example/run_example_reml2.sh
 @@ -1,4 +1,6 @@
 -../bolt \
@@ -9,5 +10,5 @@ $NetBSD$
 +
 +bolt \
      --bfile=EUR_subset \
-     --phenoFile=EUR_subset.pheno.covars \
-     --phenoCol=PHENO \
+     --phenoFile=EUR_subset.pheno2.covars \
+     --exclude=EUR_subset.exclude2 \
--- bolt-lmm/patches/patch-src_StringUtils.cpp
+++ bolt-lmm/patches/patch-src_StringUtils.cpp
@@ -1,7 +1,8 @@
 $NetBSD$
 
-# Missing header
---- src/StringUtils.cpp.orig	2017-08-02 23:43:24.000000000 +0000
+# Portability
+
+--- src/StringUtils.cpp.orig	2019-05-24 15:09:56.720778471 +0000
 +++ src/StringUtils.cpp
 @@ -22,6 +22,7 @@
  #include <cstdio>
