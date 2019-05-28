7379e1c257173dc01920fa58d119cc78a1b3298e
Makoto Fujiwara (CF-SX3) <makoto@ki.nu>
2019-05-28 13:00:46 +0900

    (math/double-conversion) updated from 3.0.0 to 3.1.5
    3.1.5 Minor bug fixes in string->double conversion
       Fixes some issues in the string->double-conversion part, when
       Hexadecimal floats are allowed.
    
       Specifically, 0x (and invalid literal) would access out-of-bounds
       before the fix, and hexadecimal literals with exponents that exceeded
       the integer range could overflow the exponent counter and thus yield a
       wrong result (and not just infinity or 0).
    
    3.1.4 Internal Cleanup
    3.1.3 Minor Fix
    3.1.2 Minor Fixes

--- double-conversion/buildlink3.mk
+++ double-conversion/buildlink3.mk
@@ -2,13 +2,13 @@
 
 BUILDLINK_TREE+=	double-conversion
 
-.if !defined(DOUBLE-CONVERSION_BUILDLINK3_MK)
-DOUBLE-CONVERSION_BUILDLINK3_MK:=
+.if !defined(DOUBLE_CONVERSION_BUILDLINK3_MK)
+DOUBLE_CONVERSION_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.double-conversion+=	double-conversion>=3.0.0
 BUILDLINK_ABI_DEPENDS.double-conversion+=	double-conversion>=3.0.0
 BUILDLINK_PKGSRCDIR.double-conversion?=	../../wip/double-conversion
 
-.endif # DOUBLE-CONVERSION_BUILDLINK3_MK
+.endif # DOUBLE_CONVERSION_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-double-conversion
