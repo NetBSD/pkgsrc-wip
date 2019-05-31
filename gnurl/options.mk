ee8f95845f96a3f31ae65c96589940168d5f0b6b
ng0 <ng0@n0.is>
2019-05-31 11:18:46 +0000

    wip/gnurl: Update to 7.65.0
    Changelog:
    
    CHANGELOG
    ---------
    
    Changes, gnurl specific:
    
    * (build) nroff should no longer be a build-time dependency:
      * enforce built-in manual to be always 'off'
      * gnurl.1 is no longer assembled by a perl + nroff
      * gnurl.1 and some other pages are now mdoc documents
    * (build) started to adjust gnurl.1 post-mdoc conversion
    * (tests) python is detected through autoconfigure and not env(1)
    
    The usual curl Changelog applies, in particular I want to point
    out these fixes by curl:
    
    * CVE-2019-5435: Integer overflows in curl_url_set
      - https://curl.haxx.se/docs/CVE-2019-5435.html
    * CVE-2019-5436: tftp: use the current blksize for recvfrom()
      - https://curl.haxx.se/docs/CVE-2019-5436.html

--- gnurl/options.mk
+++ gnurl/options.mk
@@ -1,10 +1,16 @@
 # $NetBSD: options.mk,v 1.1 2019/02/01 15:21:29 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gnurl
-PKG_SUPPORTED_OPTIONS=	inet6
+PKG_SUPPORTED_OPTIONS=	inet6 tests
 PKG_SUGGESTED_OPTIONS=	inet6
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mtests)
+.include "../../lang/python/tool.mk"
+PYTHON_FOR_BUILD_ONLY=	yes
+USE_TOOLS+=	perl
+.endif
 
 .if !empty(PKG_OPTIONS:Minet6)
 CONFIGURE_ARGS+=	--enable-ipv6
