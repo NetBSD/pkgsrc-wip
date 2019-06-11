87b90e22db56647cd206135cbc74fb60555ea5aa
Naveen Narayanan <zerous@nocebo.space>
2019-06-11 07:59:59 +0200

    wine32:	Import Wine-4.4 (i386) as wip/wine32

---
+++ wine32/patches/patch-dlls_wininet_http.c
@@ -0,0 +1,24 @@
+$NetBSD$
+
+Fix headers
+
+--- dlls/wininet/http.c.orig	2019-03-15 20:18:31.000000000 +0000
++++ dlls/wininet/http.c
+@@ -35,14 +35,14 @@
+ #  include <zlib.h>
+ #endif
+ 
+-#include "winsock2.h"
+-#include "ws2ipdef.h"
+-
+ #include <stdarg.h>
+ #include <stdio.h>
+ #include <time.h>
+ #include <assert.h>
+ 
++#include "winsock2.h"
++#include "ws2ipdef.h"
++
+ #include "windef.h"
+ #include "winbase.h"
+ #include "wininet.h"
