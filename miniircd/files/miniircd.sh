04178e1b029bbe3326a7c1a4632a2e2c144c7ce4
Leonardo Taccari <leot@NetBSD.org>
2019-06-29 01:28:17 +0200

    miniircd: Avoid to create pidfile in rc.d script

--- miniircd/files/miniircd.sh
+++ miniircd/files/miniircd.sh
@@ -11,26 +11,9 @@ name="miniircd"
 rcvar=$name
 command="@PREFIX@/sbin/miniircd"
 command_interpreter="@PYTHONBIN@"
-pidfile="@VARBASE@/run/${name}/pid"
-command_args="-d --pid-file=${pidfile}"
-start_precmd="miniircd_precmd"
-stop_postcmd="miniircd_postcmd"
+command_args="-d"
 
 miniircd_flags=${miniircd_flags-"--setuid @MINIIRCD_USER@:@MINIIRCD_GROUP@"}
-
-miniircd_precmd()
-{
-	if [ ! -d "@VARBASE@/run/${name}" ]; then
-		@MKDIR@ "@VARBASE@/run/${name}"
-		@CHMOD@ 0700 "@VARBASE@/run/${name}"
-		@CHOWN@ @MINIIRCD_USER@:@MINIIRCD_GROUP@ "@VARBASE@/run/${name}"
-	fi
-}
-
-miniircd_postcmd()
-{
-	@RM@ "${pidfile}"
-}
 
 if [ -f /etc/rc.subr ]; then
 	load_rc_config $name
