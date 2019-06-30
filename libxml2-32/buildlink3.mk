94cb7a308846b2c0b4a824bcd596ab6329d877c9
Naveen Narayanan <zerous@nocebo.space>
2019-06-30 17:54:04 +0200

    libxml2-32: Add files from libxml2 package in pkgsrc

---
+++ libxml2-32/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.18 2012/06/14 07:39:37 sbd Exp $
+
+BUILDLINK_TREE+=	libxml2
+
+.if !defined(LIBXML2_BUILDLINK3_MK)
+LIBXML2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libxml2+=	libxml2>=2.6.2
+BUILDLINK_ABI_DEPENDS.libxml2+=	libxml2>=2.8.0nb2
+#BUILDLINK_PKGSRCDIR.libxml2?=	../../textproc/libxml2
+BUILDLINK_PKGSRCDIR.libxml2?=	../libxml2-32/
+BUILDLINK_LIBDIRS.libxml2=	lib/32
+
+BUILDLINK_FILES.libxml2+=	bin/xml2-config
+
+USE_FEATURES+=		glob
+
+.include "../../archivers/xz/buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.endif # LIBXML2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libxml2
