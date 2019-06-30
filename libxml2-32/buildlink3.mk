588d05cd6daf8b5bd0293901c5882bb9f5aa7cf9
Maya Rashish <maya@NetBSD.org>
2019-06-30 19:58:20 +0300

    libxml2-32: match PKGNAME.
    Requires the non-compat dependency too for headers!

--- libxml2-32/buildlink3.mk
+++ libxml2-32/buildlink3.mk
@@ -1,23 +1,27 @@
 # $NetBSD: buildlink3.mk,v 1.18 2012/06/14 07:39:37 sbd Exp $
 
-BUILDLINK_TREE+=	libxml2
+BUILDLINK_TREE+=	compat32_libxml2
 
-.if !defined(LIBXML2_BUILDLINK3_MK)
-LIBXML2_BUILDLINK3_MK:=
+.if !defined(compat32_libxml2_BUILDLINK3_MK)
+compat32_libxml2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxml2+=	libxml2>=2.6.2
-BUILDLINK_ABI_DEPENDS.libxml2+=	libxml2>=2.8.0nb2
-#BUILDLINK_PKGSRCDIR.libxml2?=	../../textproc/libxml2
-BUILDLINK_PKGSRCDIR.libxml2?=	../libxml2-32/
-BUILDLINK_LIBDIRS.libxml2=	lib/32
+BUILDLINK_API_DEPENDS.compat32_libxml2+=	compat32_libxml2>=2.6.2
+BUILDLINK_ABI_DEPENDS.compat32_libxml2+=	compat32_libxml2>=2.8.0nb2
+#BUILDLINK_PKGSRCDIR.compat32_libxml2?=		../../textproc/compat32_libxml2
+BUILDLINK_PKGSRCDIR.compat32_libxml2?=		../../wip/libxml2-32/
+BUILDLINK_LIBDIRS.compat32_libxml2=		lib/32
 
-BUILDLINK_FILES.libxml2+=	bin/xml2-config
+BUILDLINK_FILES.compat32_libxml2+=		bin/xml2-config
 
 USE_FEATURES+=		glob
+
+# We need the non-compat version for headers.
+BUILDLINK_DEPMETHOD.libxml2+=	build
+.include "../../textproc/libxml2/buildlink3.mk"
 
 .include "../../archivers/xz/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
-.endif # LIBXML2_BUILDLINK3_MK
+.endif # COMPAT32_LIBXML2_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-libxml2
+BUILDLINK_TREE+=	-compat32_libxml2
