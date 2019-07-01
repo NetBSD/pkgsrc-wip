6ba3dd282982e242aa64653b310872215ca4f482
Michael Baeuerle <michael.baeuerle@stz-e.de>
2019-07-01 14:07:20 +0200

    fltk13: Update to 1.3.5

---
+++ fltk13/buildlink3.mk
@@ -0,0 +1,26 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	fltk
+
+.if !defined(FLTK_BUILDLINK3_MK)
+FLTK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.fltk+=	fltk>=1.3.0
+BUILDLINK_ABI_DEPENDS.fltk?=	fltk>=1.3.3nb4
+BUILDLINK_PKGSRCDIR.fltk?=	../../x11/fltk13
+BUILDLINK_FILES.fltk+=		include/Fl/*
+
+.include "../../mk/bsd.fast.prefs.mk"
+.if ${OPSYS} != "Darwin"
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libXft/buildlink3.mk"
+.include "../../x11/libXinerama/buildlink3.mk"
+.endif
+.include "../../mk/jpeg.buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+.endif # FLTK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-fltk
