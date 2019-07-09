3c3a9b99efd146dd3ebbd28a85d0fcf8cbbfced7
Naveen Narayanan <zerous@nocebo.space>
2019-07-09 20:16:13 +0200

    libxslt-32: Adapt buildlink3.mk to make it work with libxslt-32

--- libxslt-32/buildlink3.mk
+++ libxslt-32/buildlink3.mk
@@ -1,16 +1,17 @@
 # $NetBSD: buildlink3.mk,v 1.19 2014/01/01 11:52:03 wiz Exp $
 
-BUILDLINK_TREE+=	libxslt
+BUILDLINK_TREE+=	compat32_libxslt
 
-.if !defined(LIBXSLT_BUILDLINK3_MK)
-LIBXSLT_BUILDLINK3_MK:=
+.if !defined(COMPAT32_LIBXSLT_BUILDLINK3_MK)
+COMPAT32_LIBXSLT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxslt+=	libxslt>=1.1.8
-BUILDLINK_ABI_DEPENDS.libxslt+=	libxslt>=1.1.28nb2
-BUILDLINK_PKGSRCDIR.libxslt?=	../../textproc/libxslt
+BUILDLINK_API_DEPENDS.compat32_libxslt+=	compat32_libxslt>=1.1.8
+BUILDLINK_ABI_DEPENDS.compat32_libxslt+=	compat32_libxslt>=1.1.28nb2
+BUILDLINK_PKGSRCDIR.compat32_libxslt?=	../../wip/libxslt-32
+BUILDLINK.LIBDIRS.compat32_libxslt=	lib/32
 
-.include "../../security/libgcrypt/buildlink3.mk"
+.include "../../wip/libgcrypt-32/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
-.endif # LIBXSLT_BUILDLINK3_MK
+.endif # COMPAT32_LIBXSLT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-libxslt
+BUILDLINK_TREE+=	-compat32_libxslt
