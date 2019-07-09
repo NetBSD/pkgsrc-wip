59d0c4c05450d9bd81a216bc761fbd4f6224bedd
Naveen Narayanan <zerous@nocebo.space>
2019-07-09 19:50:00 +0200

    mit-krb5-32: Fix buildlink3.mk

--- mit-krb5-32/buildlink3.mk
+++ mit-krb5-32/buildlink3.mk
@@ -1,12 +1,13 @@
 # $NetBSD: buildlink3.mk,v 1.15 2018/06/15 20:46:01 tez Exp $
 
-BUILDLINK_TREE+=	mit-krb5
+BUILDLINK_TREE+=	compat32_mit-krb5
 
 .if !defined(MIT_KRB5_BUILDLINK3_MK)
 MIT_KRB5_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mit-krb5+=	mit-krb5>=1.4
-BUILDLINK_PKGSRCDIR.mit-krb5?=		../../security/mit-krb5
+BUILDLINK_API_DEPENDS.compat32_mit-krb5+=	compat32_mit-krb5>=1.4
+BUILDLINK_PKGSRCDIR.compat32_mit-krb5?=		../../wip/mit-krb5-32
+BUILDLINK_LIBDIRS.compat32_mit-krb5=		lib/32
 .endif # MIT_KRB5_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-mit-krb5
+BUILDLINK_TREE+=	-compat32_mit-krb5
