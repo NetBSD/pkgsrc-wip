6bb8da8f18507083224f42872aeeeb99e5d0d3da
Havard Eidnes <he@uninett.no>
2019-07-12 19:28:43 +0200

    Minor unimportant adjustments.

--- quickjs/patches/patch-quickjs.c
+++ quickjs/patches/patch-quickjs.c
@@ -2,9 +2,9 @@ $NetBSD$
 
 Adapt to NetBSD.
 
---- quickjs.c.orig        2019-07-09 19:49:47.000000000 +0200
-+++ quickjs.c    2019-07-12 11:49:56.029913625 +0200
-@@ -1277,6 +1277,8 @@
+--- quickjs.c.orig	2019-07-09 17:49:47.000000000 +0000
++++ quickjs.c
+@@ -1277,6 +1277,8 @@ static inline size_t js_def_malloc_usabl
      return 0;
  #elif defined(__linux__)
      return malloc_usable_size(ptr);
@@ -13,7 +13,7 @@ Adapt to NetBSD.
  #else
      /* change this to `return 0;` if compilation fails */
      return malloc_usable_size(ptr);
-@@ -1351,6 +1353,8 @@
+@@ -1351,6 +1353,8 @@ static const JSMallocFunctions def_mallo
      NULL,
  #elif defined(__linux__)
      (size_t (*)(const void *))malloc_usable_size,
