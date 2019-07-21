9657f3d6e16cf490aa7bee0b1879266226874db7
Naveen Narayanan <zerous@nocebo.space>
2019-07-22 01:25:00 +0200

    libxslt-32: Install package to ${PREFIX}/emul/netbsd32/

--- libxslt-32/buildlink3.mk
+++ libxslt-32/buildlink3.mk
@@ -8,7 +8,7 @@ COMPAT32_LIBXSLT_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.compat32_libxslt+=	compat32_libxslt>=1.1.8
 BUILDLINK_ABI_DEPENDS.compat32_libxslt+=	compat32_libxslt>=1.1.28nb2
 BUILDLINK_PKGSRCDIR.compat32_libxslt?=	../../wip/libxslt-32
-BUILDLINK.LIBDIRS.compat32_libxslt=	lib/32
+BUILDLINK.LIBDIRS.compat32_libxslt=	emul/netbsd32/lib
 
 .include "../../wip/libgcrypt-32/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
