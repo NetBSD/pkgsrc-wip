9ccde062bdd8c786037e6a77b92379c91a1055af
Kamil Rytarowski <n54@gmx.com>
2019-07-22 07:14:04 +0200

    self-git: porting to NetBSD

--- self-git/patches/patch-vm_src_unix_os_os__includes__unix.hh
+++ self-git/patches/patch-vm_src_unix_os_os__includes__unix.hh
@@ -11,18 +11,20 @@ $NetBSD$
  # include <alloca.h>
  #endif
  
-@@ -78,6 +78,10 @@ extern "C" { 
+@@ -78,6 +78,12 @@ extern "C" { 
  #   include <termios.h>
  #   include <sys/ioctl.h>
  #   include <pthread.h>
 +# elif TARGET_OS_VERSION == NETBSD_VERSION
 +#   include <sys/types.h>
++#   include <sys/ioctl.h>
 +#   include <sys/sysctl.h>
 +#   include <termios.h>
++#   include <pthread.h>
  # else
  #   include <termio.h>
  # endif
-@@ -103,7 +107,8 @@ extern "C" { 
+@@ -103,7 +109,8 @@ extern "C" { 
    typedef sigcontext       self_sig_context_t;
    typedef struct sigstack  self_stack_t;
  # elif TARGET_OS_VERSION == MACOSX_VERSION \
@@ -32,7 +34,7 @@ $NetBSD$
    typedef siginfo_t        self_code_info_t;
    typedef ucontext_t       self_sig_context_t;
    typedef stack_t          self_stack_t;
-@@ -164,7 +169,8 @@ extern "C" {
+@@ -164,7 +171,8 @@ extern "C" {
  
  
  # if TARGET_OS_VERSION != SOLARIS_VERSION  \
@@ -42,7 +44,7 @@ $NetBSD$
    #  include <sys/vadvise.h>
     extern "C" {
       int vadvise(int);
-@@ -205,6 +211,8 @@ extern "C" {
+@@ -205,6 +213,8 @@ extern "C" {
  # define FORK vfork
  # elif  TARGET_OS_VERSION == LINUX_VERSION
  # define FORK vfork
@@ -51,7 +53,7 @@ $NetBSD$
  # else
     error which?
  #endif
-@@ -228,7 +236,8 @@ const int Last_OS_Signal = SIGUSR2+1+20;
+@@ -228,7 +238,8 @@ const int Last_OS_Signal = SIGUSR2+1+20;
  
  # if  TARGET_OS_VERSION == SOLARIS_VERSION \
    ||  TARGET_OS_VERSION ==  MACOSX_VERSION \
@@ -61,7 +63,7 @@ $NetBSD$
  
    typedef void (*Signal_Handler_t)(int sig, self_code_info_t *info, self_sig_context_t *con);
  
-@@ -247,4 +256,4 @@ extern "C" { int malloc_verify(); }
+@@ -247,4 +258,4 @@ extern "C" { int malloc_verify(); }
  
  # if TARGET_OS_VERSION == MACOSX_VERSION
    typedef size_t nbytes_t;
