26aa815c345009e4bc21b4f3e87234aaf4769ca0
Naveen Narayanan <zerous@nocebo.space>
2019-07-23 17:36:48 +0200

    compat32_lcms2: Adapt buildlink3.mk to use ${PREFIX}/emul/netbsd32/include

--- compat32_lcms2/buildlink3.mk
+++ compat32_lcms2/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	compat32_lcms2
 COMPAT32_LCMS2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.compat32_lcms2+=	compat32_lcms2>=2.2
-BUILDLINK_PKGSRCDIR.compat32_lcms2?=	../../wip/lcms2-32
+BUILDLINK_PKGSRCDIR.compat32_lcms2?=	../../wip/compat32_lcms2
 BUILDLINK_LIBDIRS.compat32_lcms2=	emul/netbsd32/lib
 BUILDLINK_INCDIRS.compat32_lcms2=	emul/netbsd32/include
 
@@ -16,10 +16,10 @@ BUILDLINK_TARGETS+=	buildlink-include-compat32_lcms2
 buildlink-include-compat32_lcms2:
 .for _h_ in lcms2.h
 	${RUN}								\
-	if [ ! -f ${BUILDLINK_DIR}/include/lcms2/${_h_} ]; then		\
-		${MKDIR} ${BUILDLINK_DIR}/include/lcms2;		\
-		${ECHO_BUILDLINK_MSG} "Linking lcms2/${_h_} to ${_h_} in ${BUILDLINK_DIR}/include"; \
-		${LN} -s ${BUILDLINK_PREFIX.lcms2}/include/${_h_} ${BUILDLINK_DIR}/include/lcms2/${_h_}; \
+	if [ ! -f ${BUILDLINK_DIR}/emul/netbsd32/include/lcms2/${_h_} ]; then		\
+		${MKDIR} ${BUILDLINK_DIR}/emul/netbsd32/include/lcms2;		\
+		${ECHO_BUILDLINK_MSG} "Linking lcms2/${_h_} to ${_h_} in ${BUILDLINK_DIR}/emul/netbsd32/include"; \
+		${LN} -s ${BUILDLINK_PREFIX.lcms2}/include/${_h_} ${BUILDLINK_DIR}/emul/netbsd32/include/lcms2/${_h_}; \
 	fi
 .endfor
 .endif # COMPAT32_LCMS2_BUILDLINK3_MK
