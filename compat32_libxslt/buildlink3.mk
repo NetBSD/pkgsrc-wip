20eb15563f4142d16f269cd4ff51f7af63ceef90
Naveen Narayanan <zerous@nocebo.space>
2019-07-23 18:16:39 +0200

    compat32_libxslt: Fix .include entries

--- compat32_libxslt/buildlink3.mk
+++ compat32_libxslt/buildlink3.mk
@@ -10,8 +10,8 @@ BUILDLINK_ABI_DEPENDS.compat32_libxslt+=	compat32_libxslt>=1.1.28nb2
 BUILDLINK_PKGSRCDIR.compat32_libxslt?=	../../wip/compat32_libxslt
 BUILDLINK.LIBDIRS.compat32_libxslt=	emul/netbsd32/lib
 
-.include "../../wip/libgcrypt-32/buildlink3.mk"
-.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../wip/compat32_libgcrypt/buildlink3.mk"
+.include "../../wip/compat32_libxml2/buildlink3.mk"
 .endif # COMPAT32_LIBXSLT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-compat32_libxslt
