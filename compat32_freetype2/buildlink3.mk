cbbffdeb459acb64f9bef2cba1cb22b3336a13dd
Naveen Narayanan <zerous@nocebo.space>
2019-07-23 17:14:25 +0200

    compat32_freetype2: Adapt buildlink3.mk to use new package name

--- compat32_freetype2/buildlink3.mk
+++ compat32_freetype2/buildlink3.mk
@@ -7,13 +7,13 @@ COMPAT32_FREETYPE2_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.compat32_freetype2+=	compat32_freetype2>=2.4.5
 BUILDLINK_ABI_DEPENDS.compat32_freetype2+=	compat32_freetype2>=2.4.11
-BUILDLINK_PKGSRCDIR.compat32_freetype2?=	../../wip/freetype2-32
+BUILDLINK_PKGSRCDIR.compat32_freetype2?=	../../wip/compat32_freetype2
 BUILDLINK_INCDIRS.compat32_freetype2?=		emul/netbsd32/include
 BUILDLINK_LIBDIRS.compat32_freetype2?=		emul/netbsd32/lib/freetype2
 
 BUILDLINK_FILES.compat32_freetype2+=	emul/netbsd32/bin/freetype-config
 
-FREETYPE_CONFIG?=	${BUILDLINK_PREFIX.freetype2}/emul/netbsd32/bin/freetype-config
+FREETYPE_CONFIG?=	${BUILDLINK_PREFIX.compat32_freetype2}/emul/netbsd32/bin/freetype-config
 CONFIGURE_ENV+=		FREETYPE_CONFIG=${FREETYPE_CONFIG:Q}
 
 .include "../../archivers/bzip2/buildlink3.mk"
@@ -24,11 +24,11 @@ pkgbase := compat32_freetype2
 
 .if !empty(PKG_BUILD_OPTIONS.compat32_freetype2:Mpng)
 CHECK_BUILTIN.compat32_freetype2:=	yes
-.include "../../wip/freetype2-32/builtin.mk"
+.include "../../wip/compat32_freetype2/builtin.mk"
 CHECK_BUILTIN.compat32_freetype2:=	no
 
 .  if empty(USE_BUILTIN.compat32_freetype2:M[yY][eE][sS])
-.    include "../../wip/png-32/buildlink3.mk"
+.    include "../../wip/compat32_png/buildlink3.mk"
 .  endif
 .endif
 
