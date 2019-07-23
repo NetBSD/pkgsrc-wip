91751d319f8c9b6a213b8ca6918f73a2691a0984
Naveen Narayanan <zerous@nocebo.space>
2019-07-23 14:15:56 +0200

    compat32_giflib: Fix BUILDLINK_PKGSRCDIR.<pkg> loc

--- compat32_giflib/buildlink3.mk
+++ compat32_giflib/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	compat32_giflib
 COMPAT32_GIFLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.compat32_giflib+=	compat32_giflib>=5.1.0
-BUILDLINK_PKGSRCDIR.compat32_giflib?=	../../wip/giflib-32
+BUILDLINK_PKGSRCDIR.compat32_giflib?=	../../wip/compat32_giflib
 BUILDLINK_LIBDIRS.compat32_giflib=	emul/netbsd32/lib
 BUILDLINK_INCDIRS.compat32_giflib=	emul/netbsd32/include
 
