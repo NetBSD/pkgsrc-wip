5670deca32d124dae6d2bf3bb1e8a3bc5fa89d78
Naveen Narayanan <zerous@nocebo.space>
2019-07-23 17:43:29 +0200

    compat32_tiff: Fix BUILDLINK_PKGSRCDIR.<pkg> location

--- compat32_tiff/buildlink3.mk
+++ compat32_tiff/buildlink3.mk
@@ -7,7 +7,7 @@ COMPAT32_TIFF_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.compat32_tiff+=	compat32_tiff>=3.6.1
 BUILDLINK_ABI_DEPENDS.compat32_tiff+=	compat32_tiff>=4.0.3nb5
-BUILDLINK_PKGSRCDIR.compat32_tiff?=	../../wip/tiff-32
+BUILDLINK_PKGSRCDIR.compat32_tiff?=	../../wip/compat32_tiff
 BUILDLINK_LIBDIRS.compat32_tiff=	emul/netbsd32/lib
 BUILDLINK_INCDIRS.compat32_tiff=	emul/netbsd32/include
 
