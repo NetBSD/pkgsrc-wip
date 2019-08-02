504134e116ae1eaf5cc4282164df6a993117c639
Havard Eidnes <he@NetBSD.org>
2019-08-02 15:38:02 +0200

    Update to quickjs version 2019-07-28:
    Pkgsrc changes:
     * On NetBSD, turn off atomics if we don't support 64-bit atomics,
       as the new code now assumes that works (doesn't on many 32-bit CPUs
       NetBSD runs on).  This may be fixed in a later release upstream.
    
    Upstream changes:
     * added dynamic import
     * added Promise.allSettled
     * added String.prototype.matchAll
     * added Object.fromEntries
     * reduced number of ticks in await
     * added BigInt support in Atomics
     * exported JS_NewPromiseCapability()
     * misc async function and async generator fixes
     * enabled hashbang support by default

--- quickjs/patches/patch-quickjs.c
+++ quickjs/patches/patch-quickjs.c
@@ -2,9 +2,24 @@ $NetBSD$
 
 Adapt to NetBSD.
 
---- quickjs.c.orig	2019-07-09 17:49:47.000000000 +0000
+--- quickjs.c.orig	2019-07-28 15:03:03.000000000 +0000
 +++ quickjs.c
-@@ -1277,6 +1277,8 @@ static inline size_t js_def_malloc_usabl
+@@ -106,6 +106,14 @@
+ #include <pthread.h>
+ #include <stdatomic.h>
+ #include <errno.h>
++
++#if defined(__NetBSD__)
++#if ATOMIC_LLONG_LOCK_FREE != 2
++/* The code currently assumes the presence of 64-bit atomics */
++/* ...but the current platform doesn't support it */
++#undef CONFIG_ATOMICS
++#endif
++#endif
+ #endif
+ 
+ enum {
+@@ -1293,6 +1301,8 @@ static inline size_t js_def_malloc_usabl
      return 0;
  #elif defined(__linux__)
      return malloc_usable_size(ptr);
@@ -13,7 +28,7 @@ Adapt to NetBSD.
  #else
      /* change this to `return 0;` if compilation fails */
      return malloc_usable_size(ptr);
-@@ -1351,6 +1353,8 @@ static const JSMallocFunctions def_mallo
+@@ -1367,6 +1377,8 @@ static const JSMallocFunctions def_mallo
      NULL,
  #elif defined(__linux__)
      (size_t (*)(const void *))malloc_usable_size,
