ab5247314f466f1725c8144d1bd7f3a15924d3bc
nia <nia@netbsd.org>
2019-08-06 15:29:21 +0100

    Add mlt/shotcut.
    Shotcut is a free, open source, cross-platform video editor. Major features
    include support for a wide range of formats; no import required meaning
    native timeline editing; Blackmagic Design support for input and preview
    monitoring; and resolution support to 4k.
    
    TODO: Segfaults when importing a clip.

---
+++ mlt/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.2 2014/03/30 19:42:53 abs Exp $
+
+BUILDLINK_TREE+=	mlt
+
+.if !defined(MLT_BUILDLINK3_MK)
+MLT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.mlt+=	mlt>=0.9.0
+BUILDLINK_ABI_DEPENDS.mlt+=	mlt>=0.9.0
+BUILDLINK_PKGSRCDIR.mlt?=	../../wip/mlt
+.endif # MLT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-mlt
