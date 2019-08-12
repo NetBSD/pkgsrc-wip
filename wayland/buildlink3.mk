d926f7361234613e959c6f59e192104006373646
nia <nia@netbsd.org>
2019-08-12 18:25:51 +0100

    wayland: needs libepoll-shim in buildlink3

--- wayland/buildlink3.mk
+++ wayland/buildlink3.mk
@@ -8,6 +8,10 @@ WAYLAND_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.wayland+=	wayland>=1.9.90
 BUILDLINK_PKGSRCDIR.wayland?=	../../wip/wayland
 
+.include "../../mk/bsd.prefs.mk"
+.if ${OPSYS} != "Linux"
+.  include "../../wip/libepoll-shim/buildlink3.mk"
+.endif
 .include "../../devel/libffi/buildlink3.mk"
 .endif	# WAYLAND_BUILDLINK3_MK
 
