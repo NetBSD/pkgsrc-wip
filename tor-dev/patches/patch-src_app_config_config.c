ab077fc4a54f02d4a911acf9f45c819ac3dbaf20
Alexander Nasonov <alnsn@yandex.ru>
2019-08-18 18:58:30 +0100

    Update wip/tor-dev to version 0.4.1.4-rc.
    Changes in version 0.4.1.4-rc - 2019-07-25
      Tor 0.4.1.4-rc fixes a few bugs from previous versions of Tor, and
      updates to a new list of fallback directories. If no new bugs are
      found, the next release in the 0.4.1.x serious should be stable.
    
      o Major bugfixes (circuit build, guard):
        - When considering upgrading circuits from "waiting for guard" to
          "open", always ignore circuits that are marked for close. Otherwise,
          we can end up in the situation where a subsystem is notified that
          a closing circuit has just opened, leading to undesirable
          behavior. Fixes bug 30871; bugfix on 0.3.0.1-alpha.
    
      o Minor features (continuous integration):
        - Our Travis configuration now uses Chutney to run some network
          integration tests automatically. Closes ticket 29280.
    
      o Minor features (fallback directory list):
        - Replace the 157 fallbacks originally introduced in Tor 0.3.5.6-rc
          in December 2018 (of which ~122 were still functional), with a
          list of 148 fallbacks (70 new, 78 existing, 79 removed) generated
          in June 2019. Closes ticket 28795.
    
      o Minor bugfixes (circuit padding):
        - On relays, properly check that a padding machine is absent before
          logging a warning about it being absent. Fixes bug 30649; bugfix
          on 0.4.1.1-alpha.
        - Add two NULL checks in unreachable places to silence Coverity (CID
          144729 and 1447291) and better future-proof ourselves. Fixes bug
          31024; bugfix on 0.4.1.1-alpha.
    
      o Minor bugfixes (crash on exit):
        - Avoid a set of possible code paths that could try to use freed
          memory in routerlist_free() while Tor was exiting. Fixes bug
          31003; bugfix on 0.1.2.2-alpha.
    
      o Minor bugfixes (logging):
        - Fix a conflict between the flag used for messaging-domain log
          messages, and the LD_NO_MOCK testing flag. Fixes bug 31080; bugfix
          on 0.4.1.1-alpha.
    
      o Minor bugfixes (memory leaks):
        - Fix a trivial memory leak when parsing an invalid value from a
          download schedule in the configuration. Fixes bug 30894; bugfix
          on 0.3.4.1-alpha.
    
      o Code simplification and refactoring:
        - Remove some dead code from circpad_machine_remove_token() to fix
          some Coverity warnings (CID 1447298). Fixes bug 31027; bugfix
          on 0.4.1.1-alpha.

--- tor-dev/patches/patch-src_app_config_config.c
+++ tor-dev/patches/patch-src_app_config_config.c
@@ -2,9 +2,9 @@ $NetBSD$
 
 tor -> tordev.
 
---- src/app/config/config.c.orig	2018-09-21 11:51:54.000000000 +0000
+--- src/app/config/config.c.orig	2019-06-11 11:29:32.000000000 +0000
 +++ src/app/config/config.c
-@@ -437,9 +437,9 @@ static config_var_t option_vars_[] = {
+@@ -460,9 +460,9 @@ static config_var_t option_vars_[] = {
    V(GeoIPv6File,                 FILENAME, "<default>"),
  #else
    V(GeoIPFile,                   FILENAME,
@@ -16,3 +16,12 @@ tor -> tordev.
  #endif /* defined(_WIN32) */
    OBSOLETE("Group"),
    V(GuardLifetime,               INTERVAL, "0 minutes"),
+@@ -7850,7 +7850,7 @@ get_data_directory(const char *val)
+                "\"%s\", which is probably not what you want.  Using "
+                "\"%s"PATH_SEPARATOR"tor\" instead", fn, LOCALSTATEDIR);
+       tor_free(fn);
+-      fn = tor_strdup(LOCALSTATEDIR PATH_SEPARATOR "tor");
++      fn = tor_strdup(LOCALSTATEDIR PATH_SEPARATOR "tordev");
+     }
+     return fn;
+   }
