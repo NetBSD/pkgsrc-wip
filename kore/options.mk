476553214e7e8b5a8912aa7ea7367168321876bd
Frederic Cambus <fred@statdns.com>
2019-08-19 12:06:50 +0200

    kore: add a 'debug' build option, and allow overriding PKGMANDIR.

--- kore/options.mk
+++ kore/options.mk
@@ -1,13 +1,18 @@
 # $NetBSD$
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kore
-PKG_SUPPORTED_OPTIONS=	curl
+PKG_SUPPORTED_OPTIONS=	curl debug
 
 .include "../../mk/bsd.options.mk"
 
-# Build with asynchronous curl support.
+# Compile in asynchronous curl support
 .if !empty(PKG_OPTIONS:Mcurl)
 .  include "../../www/curl/buildlink3.mk"
 MAKE_FLAGS+=	CURL=1
 USE_TOOLS+=	pkg-config
+.endif
+
+# Enable use of -d for debug
+.if !empty(PKG_OPTIONS:Mdebug)
+MAKE_FLAGS+=	DEBUG=1
 .endif
