797f5b12c35706c9b86a134182a3d4fcd23711c3
Mark Davies <mark@ecs.vuw.ac.nz>
2019-08-29 08:01:51 +1200

    kdecoration: add ki18n to buildlink3.mk

--- kdecoration/buildlink3.mk
+++ kdecoration/buildlink3.mk
@@ -8,6 +8,7 @@ KDECORATION_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.kdecoration+=	kdecoration>=5.6.3
 BUILDLINK_PKGSRCDIR.kdecoration?=	../../wip/kdecoration
 
+.include "../../devel/ki18n/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# KDECORATION_BUILDLINK3_MK
 
