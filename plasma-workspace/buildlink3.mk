d7e79a63e76b8b72f7a4c65adb814139e30f9837
Mark Davies <mark@ecs.vuw.ac.nz>
2019-08-29 23:56:51 +1200

    plasma-workspace: add kcmutils to buildlink3.mk

--- plasma-workspace/buildlink3.mk
+++ plasma-workspace/buildlink3.mk
@@ -8,6 +8,7 @@ PLASMA_WORKSPACE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.plasma-workspace+=	plasma-workspace>=5.6.3
 BUILDLINK_PKGSRCDIR.plasma-workspace?=		../../wip/plasma-workspace
 
+.include "../../devel/kcmutils/buildlink3.mk"
 .include "../../net/knewstuff/buildlink3.mk"
 .include "../../x11/kxmlgui/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
