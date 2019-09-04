0cb066bdec1d8b1ee550d8e9c1f4161c1eb29903
Michael Baeuerle <michael.baeuerle@stz-e.de>
2019-09-04 11:03:07 +0200

    inn: Import version 2.6.3 as wip/inn

---
+++ inn/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	inn
+
+.if !defined(INN_BUILDLINK3_MK)
+INN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.inn+=	inn>=2.3.4
+BUILDLINK_PKGSRCDIR.inn?=	../../news/inn
+.endif # INN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-inn
