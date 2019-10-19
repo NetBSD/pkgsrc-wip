ddb55b59a82f12ca6a957ecaa4c3007226eb26cf
Maya Rashish <maya@NetBSD.org>
2019-10-19 13:59:23 +0300

    qemu-nvmm: update to 4.1.0
    based on emulators/qemu's update.
    - Moved new files from patch-nvmm-support to files/, I had
    trouble maintaining new files created from patch in the past.
    - Changed PKGNAME to be distinct from emulators/qemu
    - Describe difference from emulators/qemu in DESCR

--- qemu-nvmm/patches/patch-hw_usb_dev-mtp.c
+++ qemu-nvmm/patches/patch-hw_usb_dev-mtp.c
@@ -1,12 +1,12 @@
-$NetBSD: patch-hw_usb_dev-mtp.c,v 1.2 2018/08/16 10:15:09 adam Exp $
+$NetBSD: patch-hw_usb_dev-mtp.c,v 1.4 2019/06/28 17:11:14 jperkin Exp $
 
-Support NAME_MAX.
+Support NAME_MAX and compat for O_DIRECTORY.
 
---- hw/usb/dev-mtp.c.orig	2018-08-14 19:10:34.000000000 +0000
+--- hw/usb/dev-mtp.c.orig	2019-04-23 18:14:46.000000000 +0000
 +++ hw/usb/dev-mtp.c
 @@ -26,6 +26,10 @@
- #include "hw/usb.h"
  #include "desc.h"
+ #include "qemu/units.h"
  
 +#ifndef NAME_MAX
 +#define NAME_MAX 255
@@ -15,3 +15,13 @@ Support NAME_MAX.
  /* ----------------------------------------------------------------------- */
  
  enum mtp_container_type {
+@@ -614,6 +618,9 @@ static void usb_mtp_object_readdir(MTPSt
+     }
+     o->have_children = true;
+ 
++#ifndef O_DIRECTORY
++#define O_DIRECTORY	0
++#endif
+     fd = open(o->path, O_DIRECTORY | O_CLOEXEC | O_NOFOLLOW);
+     if (fd < 0) {
+         return;
