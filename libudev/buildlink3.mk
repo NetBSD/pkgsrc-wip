0a1a3b8a6008e29d3d9b09269499e8d4aefb5c91
Denys Nykula <nykula@ukr.net>
2019-10-21 21:13:39 +0300

    libinput: add version 1.14.2
    Full input stack library processes user input events for display servers
    and other applications. Generates relative pointer events from touchpads.
    Records pressure. Scales coordinates. Accelerates pointer. Debounces
    clicks. Emulates soft buttons. Detects palm and gestures.

--- libudev/buildlink3.mk
+++ libudev/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=        libudev
 LIBUDEV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libudev+=    libudev>=228
-BUILDLINK_PKGSRCDIR.libudev?=      ../../local/libudev
+BUILDLINK_PKGSRCDIR.libudev?=      ../../wip/libudev
 .endif # LIBUDEV_BUILDLINK3_MK
 
 BUILDLINK_TREE+=        -libudev
