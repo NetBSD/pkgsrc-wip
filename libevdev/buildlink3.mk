831c429694d829f2c94721352739ca0c245440a3
Denys Nykula <nykula@ukr.net>
2019-10-21 20:52:07 +0300

    libevdev: add version 1.8.0
    Wrapper library for /dev/input/eventX listens to keyboard and mouse
    events. Maps codes and names. Finds buttons and LEDs. Checks the
    resolution, repeat delay and period. Modifies appearance and capability
    bits. Transparently synchronizes when the process does not read events
    fast enough and the kernel is forced to drop some events.

---
+++ libevdev/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=        libevdev
+
+.if !defined(LIBEVDEV_BUILDLINK3_MK)
+LIBEVDEV_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libevdev+=    libevdev>=1.8.0
+BUILDLINK_PKGSRCDIR.libevdev?=      ../../wip/libevdev
+.endif # LIBEVDEV_BUILDLINK3_MK
+
+BUILDLINK_TREE+=        -libevdev
