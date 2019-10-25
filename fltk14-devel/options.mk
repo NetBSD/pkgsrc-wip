ac23862d2b70257e4957d070c8ad7cf3d7652534
Michael Baeuerle <michael.baeuerle@stz-e.de>
2019-10-25 10:09:54 +0200

    fltk14-devel: Modify option handling
    - Explicitly enable nanosvg
    - Add option "xdbe" for X11 double buffer extension

--- fltk14-devel/options.mk
+++ fltk14-devel/options.mk
@@ -1,8 +1,8 @@
 # $NetBSD$
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fltk14
-PKG_SUPPORTED_OPTIONS=	opengl pango xft2 xinerama
-PKG_SUGGESTED_OPTIONS=	opengl pango xft2 xinerama
+PKG_SUPPORTED_OPTIONS=	opengl pango xdbe xft2 xinerama
+PKG_SUGGESTED_OPTIONS=	opengl pango xdbe xft2 xinerama
 PLIST_VARS+=		opengl
 
 .include "../../mk/bsd.prefs.mk"
@@ -38,6 +38,17 @@ PKG_FAIL_REASON+=	"pango option requires xft2 option."
 CONFIGURE_ARGS+=	--enable-pango
 .else
 CONFIGURE_ARGS+=	--disable-pango
+.endif
+
+# "xdbe" option: Use X11 double buffer extension
+# On older systems double buffering can be very slow. Disabling this option can
+# make the GUI more responsive. The drawback is potential flickering, e.g. in
+# a text field while a scrollbar is moved.
+# Note: This extension is implemented in X servers, no client library required.
+.if !empty(PKG_OPTIONS:Mxdbe)
+CONFIGURE_ARGS+=	--enable-xdbe
+.else
+CONFIGURE_ARGS+=	--disable-xdbe
 .endif
 
 # "xft2" option: Use client side font rendering for X11 backend
