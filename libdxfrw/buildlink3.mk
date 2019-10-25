f3fdbf6cdc54fa47fdf555af88930d1778b37c78
Kamil Rytarowski <n54@gmx.com>
2019-10-25 17:49:05 +0200

    wip/libdxfrw: import libdxfrw-0.5.12
    libdxfrw is a free C++ library to read and write DXF files in both formats,
    ascii and binary form.

---
+++ libdxfrw/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libdxfrw
+
+.if !defined(LIBDXFRW_BUILDLINK3_MK)
+LIBDXFRW_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libdxfrw+=	libdxfrw>=0.5.12
+BUILDLINK_PKGSRCDIR.libdxfrw?=	../../wip/libdxfrw
+.endif	# LIBDXFRW_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libdxfrw
