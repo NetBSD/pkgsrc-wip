77bd8f20ec5ed1161438c1d37bfdc3b46bd8d821
Kamil Rytarowski <n54@gmx.com>
2019-10-26 15:16:10 +0200

    qt5-qscintilla: Update to 2.11.2

--- qt5-qscintilla/buildlink3.mk
+++ qt5-qscintilla/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	qt5-qscintilla
 .if !defined(QT5_QSCINTILLA_BUILDLINK3_MK)
 QT5_QSCINTILLA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qt5-qscintilla+=	qt5-qscintilla>=2.8.4
-BUILDLINK_ABI_DEPENDS.qt5-qscintilla+=	qt5-qscintilla>=2.8.4
+BUILDLINK_API_DEPENDS.qt5-qscintilla+=	qt5-qscintilla>=2.11.2
+BUILDLINK_ABI_DEPENDS.qt5-qscintilla+=	qt5-qscintilla>=2.11.2
 BUILDLINK_PKGSRCDIR.qt5-qscintilla?=	../../wip/qt5-qscintilla
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
