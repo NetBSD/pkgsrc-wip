5b1a789c594d9c59df0058023f9bccdcd2f7074f
Niclas Rosenvik <nros@NetBSD.org>
2019-10-31 20:21:26 +0000

    xygrib: add installation of detailed maps and city data files
    Install detailed maps and city files when installing xygrib.

---
+++ xygrib/data.mk
@@ -0,0 +1,21 @@
+#$NetBSD$
+#
+XYGRIBDV=1.1.1
+XYGRIBDS=https://github.com/openGribs/XyGrib/releases/download/v${XYGRIBDV}/
+DISTFILES=${DISTNAME}${EXTRACT_SUFX}
+DISTFILES+=XyGrib___High_Resolution_Maps.tar.gz
+SITES.XyGrib___High_Resolution_Maps.tar.gz=${XYGRIBDS}
+DISTFILES+=XyGrib___cities_files.tar.gz
+SITES.XyGrib___cities_files.tar.gz=${XYGRIBDS}
+DIST_SUBDIR=XyGrib-${XYGRIBDV}
+
+INSTALL_CITIES_FILES=  cities_0-300.txt.gz
+INSTALL_CITIES_FILES+= cities_300-1k.txt.gz
+INSTALL_CITIES_FILES+= cities_1k-3k.txt.gz
+
+install-cities:
+.for f in ${INSTALL_CITIES_FILES}
+	${INSTALL_DATA} ${WRKDIR}/${f} \
+		${DESTDIR}${PREFIX}/share/openGribs/XyGrib/data/gis/${f}
+.endfor	
+post-install: install-cities
