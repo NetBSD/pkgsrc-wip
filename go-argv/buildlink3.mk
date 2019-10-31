f4451109b3a6502ec65fa77d8a6b5f803816b3f5
ng0 <ng0@NetBSD.org>
2019-10-31 10:31:12 +0000

    wip/go-argv: finalize package.

--- go-argv/buildlink3.mk
+++ go-argv/buildlink3.mk
@@ -8,7 +8,7 @@ GO_ARGV_BUILDLINK3_MK:=
 BUILDLINK_CONTENTS_FILTER.go-argv=	${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-argv?=		build
 
-BUILDLINK_API_DEPENDS.go-argv+=	go-argv>=1.3.0
+BUILDLINK_API_DEPENDS.go-argv+=		go-argv>=1.3.0
 BUILDLINK_PKGSRCDIR.go-argv?=		../../wip/go-argv
 .endif	# GO_ARGV_BUILDLINK3_MK
 
