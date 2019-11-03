66c38b0bdcd18bc0aba77277724f20fe450fbe65
Yorick Hardy <yorickhardy@gmail.com>
2019-11-03 08:10:23 +0200

    d2x-rebirth: update to d2x-rebirth-20191018
    No changelog provided (changes listed at https://github.com/dxx-rebirth/dxx-rebirth).
    
    pkgsrc: change "raspberrypi" option to the standard "rpi" option.

--- d2x-rebirth/options.mk
+++ d2x-rebirth/options.mk
@@ -1,17 +1,17 @@
 # $NetBSD: options.mk,v 1.1 2015/08/19 19:11:02 yhardy Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.d2x-rebirth
-PKG_SUPPORTED_OPTIONS=		raspberrypi
+PKG_SUPPORTED_OPTIONS=		rpi
 PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.prefs.mk"
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mraspberrypi)
+.if !empty(PKG_OPTIONS:Mrpi)
 SCONS_ARGS+=	raspberrypi=1
 SCONS_ARGS+=	rpi_vc_path=${PREFIX}
 .endif
 
-.if !empty(PKG_OPTIONS:Mraspberrypi)
+.if !empty(PKG_OPTIONS:Mrpi)
 .include "../../misc/raspberrypi-userland/buildlink3.mk"
 .endif
