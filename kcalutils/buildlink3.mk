9af6731d229eb72664aea56457ab9c858b51127c
Mark Davies <mark@ecs.vuw.ac.nz>
2019-11-11 23:06:33 +1300

    kcalutils: use packages from pkgsrc propper

--- kcalutils/buildlink3.mk
+++ kcalutils/buildlink3.mk
@@ -8,7 +8,7 @@ KCALUTILS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.kcalutils+=	kcalutils>=17.12.1
 BUILDLINK_PKGSRCDIR.kcalutils?=		../../wip/kcalutils
 
-.include "../../wip/kcalcore/buildlink3.mk"
+.include "../../time/kcalendarcore/buildlink3.mk"
 .include "../../wip/kidentitymanagement/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# KCALUTILS_BUILDLINK3_MK
