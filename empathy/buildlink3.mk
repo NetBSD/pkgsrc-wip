819a4158fdbdf12864aaa98325a5f7b75e682b37
Thomas Klausner <tk@giga.or.at>
2019-11-26 10:09:07 +0100

    *: fix path to evolution-data-server

--- empathy/buildlink3.mk
+++ empathy/buildlink3.mk
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.empathy+=	empathy>=3.12.10
 BUILDLINK_PKGSRCDIR.empathy?=	../../wip/empathy
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../wip/evolution-data-server/buildlink3.mk"
+.include "../../mail/evolution-data-server/buildlink3.mk"
 .include "../../sysutils/dbus/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
