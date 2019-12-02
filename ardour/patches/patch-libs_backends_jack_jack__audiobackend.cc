702cbfd0c03867f64860506128255d3b25429d97
nia <nia@NetBSD.org>
2019-12-02 13:35:00 +0000

    ardour: Fix remaining problems

--- ardour/patches/patch-libs_backends_jack_jack__audiobackend.cc
+++ ardour/patches/patch-libs_backends_jack_jack__audiobackend.cc
@@ -1,10 +1,26 @@
 $NetBSD$
 
+Default to 48000 since it seems to avoid some problems with the 44100
+fallback....
+
 Realtime JACK currently doesn't work on NetBSD.
 
 --- libs/backends/jack/jack_audiobackend.cc.orig	2017-09-15 20:56:23.000000000 +0000
 +++ libs/backends/jack/jack_audiobackend.cc
-@@ -504,7 +504,11 @@ JACKAudioBackend::setup_jack_startup_com
+@@ -118,6 +118,12 @@ JACKAudioBackend::set_driver (const std:
+ 	return 0;
+ }
+ 
++float
++JACKAudioBackend::default_sample_rate () const
++{
++	return 48000.0;
++}
++
+ vector<AudioBackend::DeviceStatus>
+ JACKAudioBackend::enumerate_devices () const
+ {
+@@ -504,7 +510,11 @@ JACKAudioBackend::setup_jack_startup_com
  	if (_target_sample_format == FormatInt16) {
  		options.force16_bit = _target_sample_format;
  	}
