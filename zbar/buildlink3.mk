7824871f15e31f46615a345f9b1369ba302610a7
ng0 <ng0@NetBSD.org>
2019-12-02 08:34:45 +0000

    wip/zbar: Add conditional to bl3 (thanks leot).

--- zbar/buildlink3.mk
+++ zbar/buildlink3.mk
@@ -10,11 +10,13 @@ BUILDLINK_PKGSRCDIR.zbar?=	../../wip/zbar
 
 pkgbase:=	zbar
 .include "../../mk/pkg-build-options.mk"
+.if !empty(PKG_OPTIONS:Mx11)
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../x11/libICE/buildlink3.mk"
 .include "../../x11/libSM/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/libXv/buildlink3.mk"
+.endif
 
 .endif # ZBAR_BUILDLINK3_MK
 
