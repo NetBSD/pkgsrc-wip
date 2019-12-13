5aad331e620c1a5d0049612111776e96154d943d
Michael Baeuerle <michael.baeuerle@stz-e.de>
2019-12-13 12:48:49 +0100

    eureka-devel: Update to Git revision b0bd3ab3e50c96c8974927e703ec0c31690c1274
    Added patch for wrong data type (Parameter for FatalError() function).
    Reported upstream (Ticket #37).

--- eureka-devel/options.mk
+++ eureka-devel/options.mk
@@ -11,6 +11,9 @@ PKG_SUGGESTED_OPTIONS=	opengl xdg-utils
 # If FLTK is compiled without opengl option, it is ignored.
 # If FLTK is compiled with opengl option, removing it here allows to force usage
 # of the software renderer (recommended without hardware acceleration).
+.if !empty(PKG_OPTIONS:Mopengl)
+# Handled by Makefile because the FLTK dependency (not included here yet)
+.endif
 
 # xdg-utils: Create dependency for xdg-utils (Portland project)
 # Can be used by FLTK for fl_open_uri() on X11
