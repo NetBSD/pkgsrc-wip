423ca46473171e18e1e329e4325893d462b56b84
Jonathan Buschmann <jonthn+pkgsrc@pinacea.com>
2020-01-01 22:02:10 +0800

    libunibilium: Update to 2.0.0
    2.0.0   2018-02-08
            - rewrite unibi_var_t internals (now the only official access is via
              the helper functions)
            - numeric capabilities are stored as ints now (used to be shorts)
            - add support for the ncurses 6.1 "wide integer" 32-bit file format
            - try ncursesw6-config, ncurses6-config, ncursesw5-config, and
              ncurses5-config to get the list of terminfo directories
              (TERMINFO_DIRS) when building
            - improve README.md with build instructions and prerequisites

--- libunibilium/buildlink3.mk
+++ libunibilium/buildlink3.mk
@@ -4,7 +4,7 @@ BUILDLINK_TREE+=	libunibilium
 .if !defined(LIBUNIBILIUM_BUILDLINK3_MK)
 LIBUNIBILIUM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libunibilium+=	libunibilium>=1.1.0
+BUILDLINK_API_DEPENDS.libunibilium+=	libunibilium>=2.0.0
 BUILDLINK_PKGSRCDIR.libunibilium?=	../../wip/libunibilium
 .endif	# LIBUNIBILIUM_BUILDLINK3_MK
 
