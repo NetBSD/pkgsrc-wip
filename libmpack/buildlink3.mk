403476512b6a0882a9e826514eb6fcbd3a3637c8
Jonathan Buschmann <jonthn+pkgsrc@pinacea.com>
2020-01-01 22:20:04 +0800

    wip/libmpack: import libmpack-1.0.5
    libmpack is a small binary serialization/RPC library that implements both
    the msgpack and msgpack-rpc specifications.

---
+++ libmpack/buildlink3.mk
@@ -0,0 +1,11 @@
+# $NetBSD$
+BUILDLINK_TREE+=	libmpack
+
+.if !defined(LIBMPACK_BUILDLINK3_MK)
+LIBMPACK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libmpack+=	libmpack>=1.0.0
+BUILDLINK_PKGSRCDIR.libmpack?=		../../wip/libmpack
+.endif	# LIBMPACK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libmpack
