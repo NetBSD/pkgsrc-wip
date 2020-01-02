0097ba79e38b23b4be17445c60c93c5399b19631
sxvghd <sxvghd@firemail.cc>
2020-01-02 19:35:09 +0100

    ngircd: more rc file fixes

--- ngircd/files/ngircd.sh
+++ ngircd/files/ngircd.sh
@@ -15,5 +15,7 @@ command="@PREFIX@/sbin/ngircd"
 required_files="@PKG_SYSCONFDIR@/ngircd.conf"
 #pidfile="@PKG_HOME@/${name}.pid"
 
-load_rc_config $name
-run_rc_command "$1"
+if [ -f /etc/rc.subr ]; then
+	load_rc_config $name
+	run_rc_command "$1"
+fi
