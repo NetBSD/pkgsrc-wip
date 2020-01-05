14f96e70b68d43d059609d35529584545992feed
pin <voidpin@protonmail.com>
2020-01-05 15:58:18 +0100

    libqtxdg, update to 3.4.0
    Update libqtxdg from 1.3.0 to 3.4.0
    
    Too many changes to list here, refer to upstream

--- libqtxdg/buildlink3.mk
+++ libqtxdg/buildlink3.mk
@@ -1,14 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/04/11 11:18:17 krytarowski Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	libqtxdg
 
 .if !defined(LIBQTXDG_BUILDLINK3_MK)
 LIBQTXDG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libqtxdg+=	libqtxdg>=1.2.0
+BUILDLINK_API_DEPENDS.libqtxdg+=	libqtxdg>=3.4.0
 BUILDLINK_PKGSRCDIR.libqtxdg?=		../../wip/libqtxdg
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qttools/buildlink3.mk"
+.include "../../x11/qt5-qtsvg/buildlink3.mk"
 .endif	# LIBQTXDG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libqtxdg
