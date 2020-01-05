b3d8a0e9a2164f4101bb1db340fae73c7d8da029
pin <voidpin@protonmail.com>
2020-01-05 15:53:21 +0100

    libsysstat, update to 0.4.2
    Update libsysstat from 0.3.0 to 0.4.2
    
    Too many changes to list after nearly 5 years, refer to upstream

--- libsysstat/buildlink3.mk
+++ libsysstat/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/04/11 14:59:45 krytarowski Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	libsysstat
 
 .if !defined(LIBSYSSTAT_BUILDLINK3_MK)
 LIBSYSSTAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libsysstat+=	libsysstat>=0.3.0
+BUILDLINK_API_DEPENDS.libsysstat+=	libsysstat>=0.4.2
 BUILDLINK_PKGSRCDIR.libsysstat?=	../../wip/libsysstat
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
