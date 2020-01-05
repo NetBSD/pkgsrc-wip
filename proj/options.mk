49e93fc781f220986c827e724a4c9c7513bc5bf9
Greg Troxel <gdt@lexort.com>
2020-01-05 12:24:27 -0500

    proj: Update to 6.2.1
    Packaging change: Make extra grids optional, mostly because tests must
    be done without them.
    
    Upstream changes (many bugfixes omitted):
    
    6.2.1 Release Notes
    -------------------
    
     Updates
     -------
    
     o Update the EPSG database to version 9.8.2
    
    6.2.0 Release Notes
    -------------------
    
     Updates
     -------
    
     o Introduced PROJJSON, a JSON encoding of WKT2 (#1547)
    
     o Support CRS instantiation of OGC URN's (#1505)
    
     o Expose scope and remarks of database objects (#1537)
    
     o EPSG Database updated to version 9.7.0 (#1558)
    
     o Added C API function proj_grid_get_info_from_database() (#1494)
    
     o Added C API function
       proj_operation_factory_context_set_discard_superseded() (#1534)
    
     o Added C API function proj_context_set_autoclose_database() (#1566)
    
     o Added C API function proj_create_crs_to_crs_from_pj() (#1567)
    
     o Added C API function proj_cleanup() (#1569)
    
    6.1.1 Release Notes
    -------------------
    
     Updates
     -------
    
     o Update EPSG registry to version 9.6.3 (1485)
    
    6.1.0 Release Notes
    -------------------
    
     Updates
     -------
    
     o Include custom ellipsoid definitions from QGIS (#1337)
    
     o Add "-k ellipsoid" option to projinfo (#1338)
    
     o Make cs2cs support 4D coordinates (#1355)
    
     o WKT2 parser: update to OGC 18-010r6 (#1360 #1366)
    
     o Update internal version of googletest to v1.8.1 (#1361)
    
     o Database update: EPSG v9.6.2 (#1462), IGNF v3.0.3, ESRI 10.7.0
       and add operation_version column (#1368)
    
     o Add proj_normalize_for_visualization() that attempts to apply axis
       ordering as used by most GIS applications and PROJ <6 (#1387)
    
     o Added noop operation (#1391)
    
     o Paths set by user take priority over PROJ_LIB for search paths (#1398)
    
     o Reduced database size (#1438)
    
     o add support for compoundCRS and concatenatedOperation named from
       their components (#1441)

---
+++ proj/options.mk
@@ -0,0 +1,21 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.proj
+
+PKG_SUPPORTED_OPTIONS=	proj-grids
+PKG_SUGGESTED_OPTIONS=	proj-grids
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		grids
+
+.if !empty(PKG_OPTIONS:Mproj-grids)
+DISTFILES+=	proj-datumgrid-1.8${EXTRACT_SUFX}
+DISTFILES+=	proj-datumgrid-europe-1.4${EXTRACT_SUFX}
+DISTFILES+=	proj-datumgrid-north-america-1.2${EXTRACT_SUFX}
+DISTFILES+=	proj-datumgrid-oceania-1.0${EXTRACT_SUFX}
+DISTFILES+=	proj-datumgrid-world-1.0${EXTRACT_SUFX}
+
+PLIST.grids=		yes
+.else
+.endif
