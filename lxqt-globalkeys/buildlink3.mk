98156cbb6d07fb0610d8040c69b1d508a58adea1
pin <voidpin@protonmail.com>
2020-01-05 16:15:23 +0100

    lxqt-globalkeys, update to 0.14.1
    Update lxqt-globalkeys from 0.10.0 to 0.14.1
    
    Too many changes to list, refer to upstream lxqt

--- lxqt-globalkeys/buildlink3.mk
+++ lxqt-globalkeys/buildlink3.mk
@@ -1,13 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2015/04/21 07:21:55 krytarowski Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	lxqt-globalkeys
 
 .if !defined(LXQT_GLOBALKEYS_BUILDLINK3_MK)
 LXQT_GLOBALKEYS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=0.10.0
+BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=0.14.3
 BUILDLINK_PKGSRCDIR.lxqt-globalkeys?=	../../wip/lxqt-globalkeys
 
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qttools/buildlink3.mk"
 .include "../../wip/liblxqt/buildlink3.mk"
 .endif	# LXQT_GLOBALKEYS_BUILDLINK3_MK
 
