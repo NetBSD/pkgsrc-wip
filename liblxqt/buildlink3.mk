85dd1cd5e40bbbb0a2fa45d223ee0816da219715
pin <voidpin@protonmail.com>
2020-01-05 16:05:07 +0100

    liblxqt, update to 0.14.1
    Update liblxqt from 0.10.0 to 0.14.1
    
    Too many changes to list here, refer to upstream

--- liblxqt/buildlink3.mk
+++ liblxqt/buildlink3.mk
@@ -1,18 +1,19 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/04/11 13:03:08 krytarowski Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	liblxqt
 
 .if !defined(LIBLXQT_BUILDLINK3_MK)
 LIBLXQT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=0.9.0
+BUILDLINK_API_DEPENDS.liblxqt+=	liblxqt>=0.14.1
 BUILDLINK_PKGSRCDIR.liblxqt?=	../../wip/liblxqt
 
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qttools/buildlink3.mk"
+.include "../../x11/qt5-qtx11extras/buildlink3.mk"
 .include "../../x11/kwindowsystem/buildlink3.mk"
 .include "../../wip/libqtxdg/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
-.include "../../x11/qt5-qtx11extras/buildlink3.mk"
-.include "../../x11/qt5-qttools/buildlink3.mk"
+.include "../../x11/libXScrnSaver/buildlink3.mk"
 .endif	# LIBLXQT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-liblxqt
