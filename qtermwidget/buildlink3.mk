4db7ccf8e13851d97aad8e15cce7209577788606
pin <voidpin@protonmail.com>
2020-01-14 09:28:43 +0100

    qtermwidget, build fixed
    Thanks to David H. Gutteridge for dropping the needed changes.

---
+++ qtermwidget/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	qtermwidget
+
+.if !defined(QTERMWIDGET_BUILDLINK3_MK)
+QTERMWIDGET_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=0.14.1
+BUILDLINK_PKGSRCDIR.qtermwidget?=	../../wip/qtermwidget
+
+.endif	# QTERMWIDGET_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qtermwidget
