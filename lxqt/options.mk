0ac64c8ac3024fff39fcc3f7e7d9efdefdd67167
ng0 <ng0@NetBSD.org>
2020-01-15 23:40:15 +0000

    lxqt: revert removal of options.mk, as we want to keep the future possibility of adding other users like for example i3. Add a todo note about the keyword warning.

---
+++ lxqt/options.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.lxqt
+PKG_SUPPORTED_OPTIONS=	openbox # which alternatives exist?
+PKG_SUGGESTED_OPTIONS=	openbox
+
+.include "../../mk/bsd.options.mk"
+
+# \todo: When importing, add openbox to options file.
+.if !empty(PKG_OPTIONS:Mopenbox)
+DEPENDS+=	openbox>=3.6.1:../../wm/openbox
+DEPENDS+=	obconf-qt>=0.14.1:../../wip/obconf-qt
+.endif
