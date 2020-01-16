e39df90861e2929e2571a2d8a38b12faeb39aada
pin <voidpin@protonmail.com>
2020-01-16 08:41:53 +0100

    qtermwidget-0.14.1, self-reviewed
    Remove unnecessary lines.
    Add hiden build dependency. The package fails to build when
    qt5-qttools is not installed but, doesn't specifically requests it.

--- qtermwidget/buildlink3.mk
+++ qtermwidget/buildlink3.mk
@@ -8,6 +8,7 @@ QTERMWIDGET_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.qtermwidget+=	qtermwidget>=0.14.1
 BUILDLINK_PKGSRCDIR.qtermwidget?=	../../wip/qtermwidget
 
+#.include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# QTERMWIDGET_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-qtermwidget
