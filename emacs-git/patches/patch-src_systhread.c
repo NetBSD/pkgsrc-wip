0f4ec83ad51d8de821f52e7335b4c7a12a328263
Thomas Klausner <tk@giga.or.at>
2020-01-31 00:02:29 +0100

    emacs-git: fix build on NetBSD; update PLIST

---
+++ emacs-git/patches/patch-src_systhread.c
@@ -0,0 +1,18 @@
+$NetBSD$
+
+Adapt pthread_setname_np calling convention for NetBSD.
+
+--- src/systhread.c.orig	2020-01-30 22:42:52.049505198 +0000
++++ src/systhread.c
+@@ -217,7 +217,11 @@ sys_thread_set_name (const char *name)
+  #ifdef HAVE_PTHREAD_SETNAME_NP_1ARG
+   pthread_setname_np (p_name);
+  #else
++  #ifdef __NetBSD__
++  pthread_setname_np (pthread_self (), "%s", p_name);
++  #else
+   pthread_setname_np (pthread_self (), p_name);
++  #endif
+  #endif
+ #endif
+ }
