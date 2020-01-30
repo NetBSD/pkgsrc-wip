41de5ad5e855cfaea0eb0155bd930657e32d8616
Yorick Hardy <yorickhardy@gmail.com>
2020-01-30 22:30:36 +0200

    wolf4SDL: update to git snapshot
    The master site and homepage are gone, but a git repository
    provides the release with a few updates (snapshots only).
    
    pkgsrc changes:
    
     * Install executables for all versions of the game.
     * Install the man page.
     * Search for data files in share/games/wolf3d.

---
+++ wolf4SDL/patches/patch-version.h
@@ -0,0 +1,20 @@
+$NetBSD$
+
+These defines are set in the Makefile to create the different
+versions of wolf4sdl, instead of building only one version.
+
+--- version.h.orig	2015-04-17 01:23:41.000000000 +0000
++++ version.h
+@@ -11,9 +11,9 @@
+ 
+ //#define SPEAR
+ //#define SPEARDEMO
+-#define UPLOAD
+-#define GOODTIMES
+-#define CARMACIZED
++//#define UPLOAD
++//#define GOODTIMES
++//#define CARMACIZED
+ //#define APOGEE_1_0
+ //#define APOGEE_1_1
+ //#define APOGEE_1_2
