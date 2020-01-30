9795985baa8e0301081f42b06fee1ef1b0ab5034
pin <voidpin@protonmail.com>
2020-01-30 10:18:57 +0100

    lxqt-build-tools and libqtxdg, clean-up
    Adjust path. Remove unnecessary dependencies.

--- libqtxdg/buildlink3.mk
+++ libqtxdg/buildlink3.mk
@@ -8,7 +8,6 @@ LIBQTXDG_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libqtxdg+=	libqtxdg>=3.4.0
 BUILDLINK_PKGSRCDIR.libqtxdg?=		../../wip/libqtxdg
 
-.include "../../x11/qt5-qttools/buildlink3.mk"
 .include "../../x11/qt5-qtsvg/buildlink3.mk"
 .endif	# LIBQTXDG_BUILDLINK3_MK
 
