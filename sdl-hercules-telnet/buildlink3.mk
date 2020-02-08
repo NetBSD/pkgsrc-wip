4467ef542608eca47a03a3db797bad8e1f909588
Olaf Seibert <rhialto@falu.nl>
2020-02-08 14:45:58 +0100

    wip/sdl-hercules-*: add required packages and update Hercules itself somewhat.

---
+++ sdl-hercules-telnet/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	sdl-hercules-telnet
+
+.if !defined(SDL_HERCULES_TELNET_BUILDLINK3_MK)
+SDL_HERCULES_TELNET_BUILDLINK3_MK:=
+BUILDLINK_DEPMETHOD.sdl-hercules-telnet?=	build
+
+BUILDLINK_API_DEPENDS.sdl-hercules-telnet+=	sdl-hercules-telnet>=1.0.0
+BUILDLINK_PKGSRCDIR.sdl-hercules-telnet?=	../../wip/sdl-hercules-telnet
+.endif	# SDL_HERCULES_TELNET_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-sdl-hercules-telnet
