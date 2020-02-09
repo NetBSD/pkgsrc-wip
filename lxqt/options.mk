539ec30f6a01e57de716ce6c3725fce282e94d46
pin <voidpin@protonmail.com>
2020-02-09 17:51:36 +0100

    lxqt-0.14.1, add import message

--- lxqt/options.mk
+++ lxqt/options.mk
@@ -8,6 +8,7 @@ PKG_SUGGESTED_OPTIONS=	openbox
 
 .include "../../mk/bsd.options.mk"
 
+# \todo: When importing, add openbox to options file.
 .if !empty(PKG_OPTIONS:Mopenbox)
 DEPENDS+=	openbox>=3.6.1:../../wm/openbox
 DEPENDS+=	obconf-qt>=0.14.1:../../wip/obconf-qt
