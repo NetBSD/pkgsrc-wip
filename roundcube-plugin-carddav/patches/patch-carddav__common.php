3c1a1c88875616fc2b3eb57513666b1220c09fd3
Frédéric Fauberteau <frederic@fauberteau.org>
2020-02-11 13:25:57 +0100

    roundcube-plugin-carddav: fix bug with huge files
    pkgsrc changes:
    ---------------
      * Fix bug when huge directories are fetched (Github #259)
      * Make standard the format of MESSAGE
      * Remove the inclusion of roundcube's Makefile.common
      * Install config.inc.php in EGDIR

--- roundcube-plugin-carddav/patches/patch-carddav__common.php
+++ roundcube-plugin-carddav/patches/patch-carddav__common.php
@@ -1,9 +1,19 @@
 $NetBSD$
 
-Change configuration file path from current directory to pkgsrc-compliant path.
+* Fix issue with large CardDAV collection (Github #259).
+* Make pkgsrc-compliant the path of configuration file.
 
 --- carddav_common.php.orig	2018-10-01 07:23:22.000000000 +0000
 +++ carddav_common.php
+@@ -114,7 +114,7 @@ class carddav_common
+ 		if(!self::check_contenttype($reply['headers']['content-type'], ';(text|application)/xml;'))
+ 			return false;
+ 
+-		$xml = new SimpleXMLElement($reply['body']);
++    $xml = simplexml_load_string($reply['body'], 'SimpleXMLElement', LIBXML_COMPACT | LIBXML_PARSEHUGE);
+ 		$this->registerNamespaces($xml);
+ 		return $xml;
+ 	}
 @@ -373,7 +373,7 @@ class carddav_common
  
  	$rcmail = rcmail::get_instance();
