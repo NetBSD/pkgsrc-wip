30e0f23cc14d68d5c2d536a8d66c3b4aed242ace
pin <voidpin@protonmail.com>
2020-02-18 09:09:12 +0100

    Correct path for liblxqt and libfm-qt
    after libqtxdg import

--- liblxqt/buildlink3.mk
+++ liblxqt/buildlink3.mk
@@ -11,7 +11,7 @@ BUILDLINK_PKGSRCDIR.liblxqt?=	../../wip/liblxqt
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .include "../../x11/qt5-qtx11extras/buildlink3.mk"
 .include "../../x11/kwindowsystem/buildlink3.mk"
-.include "../../wip/libqtxdg/buildlink3.mk"
+.include "../../x11/libqtxdg/buildlink3.mk"
 .include "../../x11/libXScrnSaver/buildlink3.mk"
 .endif	# LIBLXQT_BUILDLINK3_MK
 
