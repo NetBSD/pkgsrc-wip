5360ad1a8ee7c22723cb4650d7631b431a3ab8e5
pin <voidpin@protonmail.com>
2020-02-25 17:00:30 +0100

    lxqt-panel, add comment on libsysstat

--- lxqt-globalkeys/buildlink3.mk
+++ lxqt-globalkeys/buildlink3.mk
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.lxqt-globalkeys+=	lxqt-globalkeys>=0.14.3
 BUILDLINK_PKGSRCDIR.lxqt-globalkeys?=	../../wip/lxqt-globalkeys
 
 .include "../../x11/qt5-qtbase/buildlink3.mk"
-.include "../../wip/liblxqt/buildlink3.mk"
+.include "../../x11/liblxqt/buildlink3.mk"
 .endif	# LXQT_GLOBALKEYS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-lxqt-globalkeys
