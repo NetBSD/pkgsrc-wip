1c46799c0a96f01ec45ed1835919b229741da6d9
Olaf Seibert <rhialto@falu.nl>
2020-02-27 23:17:57 +0100

    sabnzbd: some cleanups.

--- sabnzbd/patches/patch-SABnzbd.py
+++ sabnzbd/patches/patch-SABnzbd.py
@@ -10,7 +10,7 @@ Fix path.
  
 +# Force python to load the patched version of cherrypy included with the port,
 +# instead of any version that may be installed otherwise.
-+sys.path.insert(0,'%%PREFIX%%/share/sabnzbd')
++sys.path.insert(0,'@PREFIX@/share/sabnzbd')
 +
  try:
      import Cheetah
@@ -21,7 +21,7 @@ Fix path.
      sabnzbd.MY_NAME = os.path.basename(sabnzbd.MY_FULLNAME)
 -    sabnzbd.DIR_PROG = os.path.dirname(sabnzbd.MY_FULLNAME)
 +    # sabnzbd.DIR_PROG = os.path.dirname(sabnzbd.MY_FULLNAME)
-+    sabnzbd.DIR_PROG = "%%PREFIX%%/share/sabnzbd"
++    sabnzbd.DIR_PROG = "@PREFIX@/share/sabnzbd"
      sabnzbd.DIR_INTERFACES = real_path(sabnzbd.DIR_PROG, DEF_INTERFACES)
      sabnzbd.DIR_LANGUAGE = real_path(sabnzbd.DIR_PROG, DEF_LANGUAGE)
      org_dir = os.getcwd()
