6352fdc04d004a3ec035d49d3d6122a948d6849c
AcidBear <jake.slazenger@yandex.com>
2020-03-13 15:02:52 +0300

    telegram-desktop: + some patches

---
+++ telegram-desktop/patches/patch-export_output_stats.h
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- Telegram/SourceFiles/export/output/export_output_stats.h.orig	2020-03-06 21:41:17.128893400 +0000
++++ Telegram/SourceFiles/export/output/export_output_stats.h
+@@ -8,6 +8,7 @@ https://github.com/telegramdesktop/tdesk
+ #pragma once
+ 
+ #include <atomic>
++#include "../export_pch.h"
+ 
+ namespace Export {
+ namespace Output {
