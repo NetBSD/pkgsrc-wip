7664a24bace211ad23f94dfb3ec022b0c3a51308
Kamil Rytarowski <n54@gmx.com>
2020-03-14 12:10:44 +0100

    gdb-netbsd: Remove local readline patches
    They are relevant for old readline that was upgraded in gdb.

--- gdb-netbsd/patches/patch-readline_examples_readlinebuf.h
+++
@@ -1,7 +0,0 @@
-Cannot strip away RCS IDs, please handle manually!
-These are the matching lines:
-   - * $Revision$
-   - * $Date$
-   - * $Author$
-Setting PKGDIFF_FMT might help.
-Otherwise you may need to run diff by hand.
