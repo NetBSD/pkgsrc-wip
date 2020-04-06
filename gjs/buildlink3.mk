2ec20adc6c05151be364632ae9d3bb03dcc6bfdb
Thomas Klausner <tk@giga.or.at>
2020-04-06 07:12:32 +0200

    gjs: add update to 1.64.1
    Issues with PaX mprotect, see TODO.

---
+++ gjs/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.2 2020/03/08 16:47:52 wiz Exp $
+
+BUILDLINK_TREE+=	gjs
+
+.if !defined(GJS_BUILDLINK3_MK)
+GJS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gjs+=	gjs>=1.64.1
+BUILDLINK_PKGSRCDIR.gjs?=	../../lang/gjs
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/gobject-introspection/buildlink3.mk"
+.include "../../lang/mozjs68/buildlink3.mk"
+.include "../../graphics/cairo-gobject/buildlink3.mk"
+.include "../../sysutils/dbus-glib/buildlink3.mk"
+.endif	# GJS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gjs
