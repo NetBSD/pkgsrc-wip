477393a699daddee26d226499f3948b372c132a9
pin <voidpin@protonmail.com>
2020-04-10 06:27:29 +0200

    sowm, use mixerctl instead of amixer on NetBSD

--- sowm/patches/patch-config.def.h
+++ sowm/patches/patch-config.def.h
@@ -1,6 +1,6 @@
 $NetBSD$
 
-Use amixer only on Linux
+Use mixerctl instead of amixer on NetBSD
 
 --- config.def.h.orig	2020-02-21 16:08:47.000000000 +0000
 +++ config.def.h
@@ -8,14 +8,14 @@ Use amixer only on Linux
  const char* scrot[]   = {"scr",            0};
  const char* briup[]   = {"bri", "10", "+", 0};
  const char* bridown[] = {"bri", "10", "-", 0};
-+#if defined(__Linux__)
++#if defined (__NetBSD__)
++const char* volup[] = {"mixerctl", "-w", "outputs.master+=5", 0};
++const char* voldown[] = {"mixerctl", "-w", "outputs.master-=5", 0};
++const char* volmute[] = {"mixerctl", "-w", "outputs.master=0", 0};
++#elif
  const char* voldown[] = {"amixer", "sset", "Master", "5%-",         0};
  const char* volup[]   = {"amixer", "sset", "Master", "5%+",         0};
  const char* volmute[] = {"amixer", "sset", "Master", "toggle",      0};
-+#elif (__NetBSD__)
-+const char* volup[] = {"mixerctl", "-w", "outputs.master+=5" };
-+const char* voldown[] = {"mixerctl", "-w", "outputs.master-=5" };
-+const char* volmute[] = {"mixerctl", "-w", "outputs.master=0" };
 +#endif
  const char* colors[]  = {"bud", "/home/goldie/Pictures/Wallpapers", 0};
  
