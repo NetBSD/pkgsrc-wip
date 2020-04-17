4f257029660dc41279f55554ae9bfcaf3bde4dd7
Aleksej Lebedev <root@zta.lk>
2020-04-17 15:42:03 +0200

    cppzmq: fixed buildlink3.mk
    libopenshot: allow for any version of python3,
                 drop the dependency on zeromq

--- cppzmq/buildlink3.mk
+++ cppzmq/buildlink3.mk
@@ -1,14 +1,4 @@
 # $NetBSD$
-# XXX
-# XXX This file was created automatically using createbuildlink-3.17.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.cppzmq?=	build
 
 BUILDLINK_TREE+=	cppzmq
 
@@ -17,14 +7,9 @@ CPPZMQ_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cppzmq+=	cppzmq>=4.6.0
 BUILDLINK_PKGSRCDIR.cppzmq?=	../../wip/cppzmq
+BUILDLINK_DEPMETHOD.cppzmq?=	build
 
-# XXX
-# XXX Uncomment and keep only the buildlink3 lines below which are directly
-# XXX needed for dependencies to compile, link, and run.  If this package
-# XXX provides a wrappered API or otherwise does not expose the APIs of the
-# XXX buildlink3 lines below to dependencies, remove them.
-# XXX
-#.include "../../net/zeromq/buildlink3.mk"
+.include "../../net/zeromq/buildlink3.mk"
 .endif	# CPPZMQ_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-cppzmq
