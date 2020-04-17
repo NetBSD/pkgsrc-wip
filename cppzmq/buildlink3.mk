779326c320860471bbaa4b5416e7b022c8b0f6d7
Aleksej Lebedev <root@zta.lk>
2020-04-17 12:12:41 +0200

    libopenshot: imported version 0.2.5 libopenshot-audio: added buildlink3.mk cppzmq: imported version 4.6.0 (needed by libopenshot) swig4: imported version 4.0.1 (used by libopenshot)

--- cppzmq/buildlink3.mk
+++ cppzmq/buildlink3.mk
@@ -8,15 +8,15 @@
 # XXX Packages that only install static libraries or headers should
 # XXX include the following line:
 # XXX
-# XXX	BUILDLINK_DEPMETHOD.libopenshot-audio?=	build
+# XXX	BUILDLINK_DEPMETHOD.cppzmq?=	build
 
-BUILDLINK_TREE+=	libopenshot-audio
+BUILDLINK_TREE+=	cppzmq
 
-.if !defined(LIBOPENSHOT_AUDIO_BUILDLINK3_MK)
-LIBOPENSHOT_AUDIO_BUILDLINK3_MK:=
+.if !defined(CPPZMQ_BUILDLINK3_MK)
+CPPZMQ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libopenshot-audio+=	libopenshot-audio>=0.2.0
-BUILDLINK_PKGSRCDIR.libopenshot-audio?=	../../wip/libopenshot-audio
+BUILDLINK_API_DEPENDS.cppzmq+=	cppzmq>=4.6.0
+BUILDLINK_PKGSRCDIR.cppzmq?=	../../wip/cppzmq
 
 # XXX
 # XXX Uncomment and keep only the buildlink3 lines below which are directly
@@ -24,11 +24,7 @@ BUILDLINK_PKGSRCDIR.libopenshot-audio?=	../../wip/libopenshot-audio
 # XXX provides a wrappered API or otherwise does not expose the APIs of the
 # XXX buildlink3 lines below to dependencies, remove them.
 # XXX
-#.include "../../audio/alsa-lib/buildlink3.mk"
-#.include "../../graphics/freetype2/buildlink3.mk"
-#.include "../../x11/libXrandr/buildlink3.mk"
-#.include "../../x11/libXinerama/buildlink3.mk"
-#.include "../../x11/libXcursor/buildlink3.mk"
-.endif	# LIBOPENSHOT_AUDIO_BUILDLINK3_MK
+#.include "../../net/zeromq/buildlink3.mk"
+.endif	# CPPZMQ_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-libopenshot-audio
+BUILDLINK_TREE+=	-cppzmq
