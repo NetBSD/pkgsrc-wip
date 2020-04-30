4071879e96df5a05d07a72ccb0a944bccc79fa1a
Michael Baeuerle <micha@NetBSD.org>
2020-04-30 13:59:17 +0200

    crispy-doom: Import version 5.8.0

---
+++ crispy-doom/patches/patch-Source_d__iwad.c
@@ -0,0 +1,16 @@
+$NetBSD$
+
+Add pkgsrc ${PREFIX}/share/doom to WAD file search path.
+
+--- src/d_iwad.c.orig	2020-04-14 06:39:44.000000000 +0000
++++ src/d_iwad.c
+@@ -653,6 +653,9 @@ static void AddXdgDirs(void)
+         env = "/usr/local/share:/usr/share";
+     }
+ 
++    // Add pkgsrc location for WADs
++    AddIWADPath(PKGSRC_PREFIX, PKGSRC_DOOMWADDIR);
++
+     // The "standard" location for IWADs on Unix that is supported by most
+     // source ports is /usr/share/games/doom - we support this through the
+     // XDG_DATA_DIRS mechanism, through which it can be overridden.
