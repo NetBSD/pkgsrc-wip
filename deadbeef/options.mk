e93712ce8fff37b146d7249ed2e8c982f36d82b7
nia <nia@netbsd.org>
2020-05-16 13:15:53 +0100

    deadbeef: Update to 1.8.3. Still fails to run.

--- deadbeef/options.mk
+++ deadbeef/options.mk
@@ -1,13 +1,18 @@
 # $NetBSD: options.mk,v 1.2 2019/06/17 08:08:31 nia Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.deadbeef
-PKG_SUPPORTED_OPTIONS=		alsa pulseaudio
-PKG_SUGGESTED_OPTIONS.Linux=	alsa
 
-PLIST_VARS+=	alsa pulse
+# Audio outputs
+PKG_SUPPORTED_OPTIONS+=		alsa pulseaudio
+# Additional codecs
+PKG_SUPPORTED_OPTIONS+=		ffmpeg musepack wavpack
+
+PKG_SUGGESTED_OPTIONS+=		ffmpeg musepack wavpack
+PKG_SUGGESTED_OPTIONS.Linux+=	alsa
 
 .include "../../mk/bsd.options.mk"
 
+PLIST_VARS+=		alsa
 .if !empty(PKG_OPTIONS:Malsa)
 PLIST.alsa=		yes
 .include "../../audio/alsa-lib/buildlink3.mk"
@@ -15,9 +20,34 @@ PLIST.alsa=		yes
 CONFIGURE_ARGS+=	--disable-alsa
 .endif
 
+PLIST_VARS+=		pulse
 .if !empty(PKG_OPTIONS:Mpulseaudio)
 PLIST.pulse=		yes
 .include "../../audio/pulseaudio/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--disable-pulse
+.endif
+
+PLIST_VARS+=		ffmpeg
+.if !empty(PKG_OPTIONS:Mffmpeg)
+PLIST.ffmpeg=		yes
+.include "../../multimedia/ffmpeg4/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-ffmpeg
+.endif
+
+PLIST_VARS+=		musepack
+.if !empty(PKG_OPTIONS:Mmusepack)
+PLIST.musepack=		yes
+.include "../../audio/musepack/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-musepack
+.endif
+
+PLIST_VARS+=		wavpack
+.if !empty(PKG_OPTIONS:Mwavpack)
+PLIST.wavpack=		yes
+.include "../../audio/wavpack/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-wavpack
 .endif
