ab7202b0110f298e96393b30dd87902649d28d80
nikita <nikita@NetBSD.org>
2020-05-16 21:42:52 +0200

    guile30: update TODO of failing build, adjust module.mk

--- guile30/module.mk
+++ guile30/module.mk
@@ -1,6 +1,6 @@
 # $NetBSD: module.mk,v 1.1 2019/11/12 12:41:19 ng0 Exp $
 #
-# Common build logic for Guile 2.2 modules using gnu configure.
+# Common build logic for Guile 3.0 modules using gnu configure.
 #
 # === Package-settable-variables ===
 #
@@ -11,14 +11,14 @@
 #	Possible values: yes no
 
 .if defined(GNU_CONFIGURE)
-GNU_CONFIGURE_PREFIX=	${PREFIX}/guile/2.2
-CONFIGURE_ARGS+=	--libdir=${PREFIX}/guile/2.2/lib
+GNU_CONFIGURE_PREFIX=	${PREFIX}/guile/3.0
+CONFIGURE_ARGS+=	--libdir=${PREFIX}/guile/3.0/lib
 
 .  if defined(GUILE_NEED_BOOTSTRAP)
 USE_TOOLS+=		automake autoconf autoreconf
 pre-configure:
-	(cd ${WRKSRC} && ${SETENV} ${CONFIGURE_ENV} autoreconf -I${PREFIX}/guile/2.2/share/aclocal -vif)
+	(cd ${WRKSRC} && ${SETENV} ${CONFIGURE_ENV} autoreconf -I${PREFIX}/guile/3.0/share/aclocal -vif)
 .  endif
 
-.include "../../lang/guile22/buildlink3.mk"
+.include "../../lang/guile30/buildlink3.mk"
 .endif
