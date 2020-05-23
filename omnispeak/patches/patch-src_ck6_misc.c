3ac8a66cd5d47559b87bce73dd97922d4d24a096
Yorick Hardy <yorickhardy@gmail.com>
2020-05-23 22:23:19 +0200

    omnispeak: fix omnispeak hangs by updating snapshot
    Version 1.0 was just a git snapshot, released only as binaries on
    the author's website, and no releases have been made since.
    Pkgsrc onispeak-1.0 was taken from the same snapshot.
    
    The latest git snapshot seems to fix some issues, so call it
    omnispeak-1.0nb1. No changelog provided.

--- omnispeak/patches/patch-src_ck6_misc.c
+++ omnispeak/patches/patch-src_ck6_misc.c
@@ -2,20 +2,20 @@ $NetBSD$
 
 Part of a patch set to search for data files in PREFIX.
 
---- src/ck6_misc.c.orig	2017-07-15 21:27:56.000000000 +0000
+--- src/ck6_misc.c.orig	2020-05-04 04:33:47.000000000 +0000
 +++ src/ck6_misc.c
-@@ -27,6 +27,7 @@ Foundation, Inc., 51 Franklin Street, Fi
- #include "ck_def.h"
- #include "ck_act.h"
- #include "ck6_ep.h"
+@@ -17,6 +17,7 @@ along with this program; if not, write t
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+  */
+ 
 +#include <limits.h>
  #include <stdio.h>
- 
- CK_EpisodeDef *ck6_episode;
-@@ -106,7 +107,7 @@ bool CK6_IsPresent()
+ #include "id_ca.h"
+ #include "id_in.h"
+@@ -117,7 +118,7 @@ bool CK6_IsPresent()
  	if (!CA_IsFilePresent("AUDIO.CK6"))
  		return false;
- 	
+ 
 -	char egaGraphName[] = "EGAGRAPH.CK6";
 +	char egaGraphName[PATH_MAX] = "EGAGRAPH.CK6";
  	CAL_AdjustFilenameCase(egaGraphName);
--- omnispeak/patches/patch-src_id_ca.c
+++ omnispeak/patches/patch-src_id_ca.c
@@ -2,16 +2,16 @@ $NetBSD$
 
 Search for data files in PREFIX.
 
---- src/id_ca.c.orig	2017-07-15 21:27:56.000000000 +0000
+--- src/id_ca.c.orig	2020-05-04 04:33:47.000000000 +0000
 +++ src/id_ca.c
-@@ -32,6 +32,7 @@ Foundation, Inc., 51 Franklin Street, Fi
- #include "ck_cross.h"
+@@ -31,6 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fi
  #include "ck_def.h"
+ #include "ck_ep.h"
  
 +#include <limits.h>
- #include <string.h>
  #include <stdio.h>
- #include "SDL_endian.h"
+ #include <string.h>
+ #ifdef WITH_SDL
 @@ -85,6 +86,25 @@ bool CAL_AdjustFilenameCase(char *filena
  
  	// We didn't find a matching file.
@@ -38,21 +38,21 @@ Search for data files in PREFIX.
  	return false;
  }
  #else
-@@ -161,7 +181,7 @@ void	(*ca_finishCacheBox)	(void);
+@@ -164,7 +184,7 @@ void (*ca_finishCacheBox)(void);
  // Does a file exist (with filename case correction)
  bool CA_IsFilePresent(const char *filename)
  {
 -	static char newname[16];
 +	static char newname[PATH_MAX];
- 	strcpy(newname,filename);
+ 	strcpy(newname, filename);
  	if (!CAL_AdjustFilenameCase(newname))
  	{
-@@ -175,7 +195,7 @@ bool CA_IsFilePresent(const char *filena
+@@ -178,7 +198,7 @@ bool CA_IsFilePresent(const char *filena
  // valid until the NEXT invocation of this function.
- char* CAL_AdjustExtension(const char *filename)
+ char *CAL_AdjustExtension(const char *filename)
  {
 -	static char newname[16];
 +	static char newname[PATH_MAX];
- 	strcpy(newname,filename);
+ 	strcpy(newname, filename);
  	size_t fnamelen = strlen(filename);
- 	newname[fnamelen-3] = ck_currentEpisode->ext[0];
+ 	newname[fnamelen - 3] = ck_currentEpisode->ext[0];
