04882bd2f418f5b0ba9a5f12a66d3acfb291155a
Hauke Fath <hauke@NetBSD.org>
2020-06-08 13:14:48 +0200

    ccnet-server: Upgrade to 7.1.4
    Use the provided autogen.sh to run autohell's black majic
    The package is python-3 ready
    Ignore builtin v1 libevent

--- ccnet-server/buildlink3.mk
+++ ccnet-server/buildlink3.mk
@@ -1,12 +1,12 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	ccnet
+BUILDLINK_TREE+=	ccnet-server
 
-.if !defined(CCNET_BUILDLINK3_MK)
-CCNET_BUILDLINK3_MK:=
+.if !defined(CCNET_SERVER_BUILDLINK3_MK)
+CCNET_SERVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ccnet+=	ccnet-server>=6.0.0
-BUILDLINK_PKGSRCDIR.ccnet?=	../../wip/ccnet-server
-.endif	# CCNET_BUILDLINK3_MK
+BUILDLINK_API_DEPENDS.ccnet-server+=	ccnet-server>=6.0.0
+BUILDLINK_PKGSRCDIR.ccnet-server?=	../../wip/ccnet-server
+.endif	# CCNET_SERVER_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-ccnet
+BUILDLINK_TREE+=	-ccnet-server
