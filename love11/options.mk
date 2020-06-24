6e6c23be72614f0ff3f61925a9595ef36a672f25
nia <nia@netbsd.org>
2020-06-24 17:02:30 +0100

    love11: Clean up includes, support building w/o luajit

---
+++ love11/options.mk
@@ -0,0 +1,21 @@
+# $NetBSD: options.mk,v 1.2 2020/06/24 13:32:09 nia Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.love
+
+.include "../../lang/LuaJIT2/platform.mk"
+.if !empty(PLATFORM_SUPPORTS_LUAJIT:M[Yy][Ee][Ss])
+PKG_SUPPORTED_OPTIONS=	luajit
+PKG_SUGGESTED_OPTIONS=	luajit
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mluajit)
+# JIT needs WX mappings
+NOT_PAX_MPROTECT_SAFE+=		love11/bin/love
+.  include "../../lang/LuaJIT2/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=		--with-lua=lua
+CONFIGURE_ARGS+=		--with-luaversion=-5.1
+.  include "../../lang/lua51/buildlink3.mk"
+.endif
