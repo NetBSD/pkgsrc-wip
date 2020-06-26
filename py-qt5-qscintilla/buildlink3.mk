182c47446f86bd85ecd73ca608d1ebabf677b3cd
Adam Ciarciński <aciarcinski@teonite.com>
2020-06-26 09:44:50 +0200

    Add {py-}qt5-qscintilla

---
+++ py-qt5-qscintilla/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD: buildlink3.mk,v 1.6 2020/06/02 08:23:07 adam Exp $
+
+BUILDLINK_TREE+=	py-qt5-qscintilla
+
+.if !defined(PY_QT5_QSCINTILLA_BUILDLINK3_MK)
+PY_QT5_QSCINTILLA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.py-qt5-qscintilla+=	${PYPKGPREFIX}-qt5-qscintilla>=2.11.2
+BUILDLINK_ABI_DEPENDS.py-qt5-qscintilla+=	${PYPKGPREFIX}-qt5-qscintilla>=2.11.2nb7
+BUILDLINK_PKGSRCDIR.py-qt5-qscintilla?=		../../wip/py-qt5-qscintilla
+
+.include "../../wip/py-sip/buildlink3.mk"
+.include "../../wip/py-qt5/buildlink3.mk"
+.include "../../wip/qt5-qtbase/buildlink3.mk"
+.include "../../wip/qt5-qscintilla/buildlink3.mk"
+.endif	# PY_QT5_QSCINTILLA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-py-qt5-qscintilla
---
+++ qt5-qscintilla/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.5 2020/04/12 08:28:14 adam Exp $
+
+BUILDLINK_TREE+=	qt5-qscintilla
+
+.if !defined(QT5_QSCINTILLA_BUILDLINK3_MK)
+QT5_QSCINTILLA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt5-qscintilla+=	qt5-qscintilla>=2.11.2
+BUILDLINK_ABI_DEPENDS.qt5-qscintilla+=	qt5-qscintilla>=2.11.2nb5
+BUILDLINK_PKGSRCDIR.qt5-qscintilla?=	../../wip/qt5-qscintilla
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${OPSYS} == "Darwin"
+.include "../../wip/qt5-qtmacextras/buildlink3.mk"
+.endif
+.include "../../wip/qt5-qtbase/buildlink3.mk"
+.endif	# QT5_QSCINTILLA_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt5-qscintilla
