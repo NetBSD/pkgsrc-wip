3bfdd58e61113aa636d25ba9dd3a87a9623a64b3
Amitai Schleier <schmonz@NetBSD.org>
2020-07-14 15:59:59 -0400

    Replace platform detection with initial builtin.mk.
    While here, bump BUILDLINK_API_DEPENDS for security fixes and heed pkglint.

--- libbsd/buildlink3.mk
+++ libbsd/buildlink3.mk
@@ -1,19 +1,12 @@
 # $NetBSD: buildlink3.mk,v 1.2 2013/01/03 21:41:42 mrampke Exp $
 
-# check if available on this platform
-#
-.include "Makefile.common"
-.if defined(USE_LIBBSD)
-
 BUILDLINK_TREE+=	libbsd
 
-.  if !defined(LIBBSD_BUILDLINK3_MK)
+.if !defined(LIBBSD_BUILDLINK3_MK)
 LIBBSD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.4.2
+BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.10.0
 BUILDLINK_PKGSRCDIR.libbsd?=	../../wip/libbsd
-.  endif	# LIBBSD_BUILDLINK3_MK
+.endif	# LIBBSD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libbsd
-
-.endif
