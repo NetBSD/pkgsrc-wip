8f6ec6fcd2062ff4d26adaeeef570950feb38a60
Jason Bacon <bacon@NetBSD.org>
2020-07-21 15:34:40 -0500

    samtools: Patch Makefile to respect pkgsrc env, add bash for test target

---
+++ samtools/patches/patch-test_test.pl
@@ -0,0 +1,24 @@
+$NetBSD$
+
+# bash path
+
+--- test/test.pl.orig	2019-12-06 16:40:42.000000000 +0000
++++ test/test.pl
+@@ -907,7 +907,7 @@ sub test_usage
+     my $command = $args{cmd};
+     my $commandpath = $$opts{bin}."/".$command;
+     my ($ret,$out,$err) = _cmd("$commandpath $args{redirection}");
+-    if ( $err =~ m/\/bin\/bash.*no.*such/i ) { failed($opts,msg=>$test,reason=>"could not run $commandpath: $out"); return; }
++    if ( $err =~ m/\/bash.*no.*such/i ) { failed($opts,msg=>$test,reason=>"could not run $commandpath: $out"); return; }
+ 
+     my @sections = ($err =~ m/(^[A-Za-z]+.*?)(?:(?=^[A-Za-z]+:)|\z)/msg);
+ 
+@@ -965,7 +965,7 @@ sub test_usage_subcommand
+     my $commandpath = $$opts{bin}."/".$command;
+     my ($ret,$out,$err) = _cmd("$commandpath $subcommand $args{redirection}");
+ 
+-    if ( $err =~ m/\/bin\/bash.*no.*such/i ) { failed($opts,msg=>$test,reason=>"could not run $commandpath $subcommand: $out"); return; }
++    if ( $err =~ m/\/bash.*no.*such/i ) { failed($opts,msg=>$test,reason=>"could not run $commandpath $subcommand: $out"); return; }
+ 
+     if ( $err =~ m/not.*implemented/is ) { failed($opts,msg=>$test,reason=>"subcommand indicates it is not implemented",expect_fail=>1); return; }
+ 
