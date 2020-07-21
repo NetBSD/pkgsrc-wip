e867210a68d5a4527e047df889e8907566a6c4d2
Jason Bacon <bacon@NetBSD.org>
2020-07-21 16:09:17 -0500

    bcftools: Upgrade to 1.10.2 and clean up

--- bcftools/patches/patch-misc_run-roh.pl
+++ bcftools/patches/patch-misc_run-roh.pl
@@ -1,15 +1,24 @@
 $NetBSD$
 
-# Portability
+# bash path
 
---- misc/run-roh.pl.orig	2017-10-18 09:33:24 UTC
+--- misc/run-roh.pl.orig	2019-12-19 09:57:32.000000000 +0000
 +++ misc/run-roh.pl
-@@ -97,7 +97,7 @@ sub cmd
+@@ -110,7 +110,7 @@ sub cmd
+ 
+     if ( $args{verbose} ) { print STDERR $cmd,"\n"; }
+ 
+-    # Why not to use backticks? Perl calls /bin/sh, which is often bash. To get the correct
++    # Why not to use backticks? Perl calls bash, which is often bash. To get the correct
+     #   status of failing pipes, it must be called with the pipefail option.
+ 
+     my $kid_io;
+@@ -127,7 +127,7 @@ sub cmd
      else 
      {      
          # child
 -        exec('/bin/bash', '-o','pipefail','-c', $cmd) or error("Failed to run the command [/bin/sh -o pipefail -c $cmd]: $!");
-+        exec('/bin/sh', '-c', $cmd) or error("Failed to run the command [/bin/sh -c $cmd]: $!");
++        exec('bash', '-c', $cmd) or error("Failed to run the command [bash -c $cmd]: $!");
      }
  
      if ( exists($args{exit_on_error}) && !$args{exit_on_error} ) { return @out; }
---
+++ bcftools/patches/patch-test_test.pl
@@ -0,0 +1,42 @@
+$NetBSD$
+
+# Portable bash path
+
+--- test/test.pl.orig	2020-07-21 20:47:32.977230067 +0000
++++ test/test.pl
+@@ -610,7 +610,7 @@ sub _cmd
+     else
+     {
+         # child
+-        exec('/bin/bash', '-o','pipefail','-c', $cmd) or error("Cannot execute the command [/bin/sh -o pipefail -c $cmd]: $!");
++        exec('bash', '-o','pipefail','-c', $cmd) or error("Cannot execute the command [/bin/sh -o pipefail -c $cmd]: $!");
+     }
+     return ($? >> 8, join('',@out));
+ }
+@@ -622,7 +622,7 @@ sub _cmd3
+     my $pid = fork();
+     if ( !$pid )
+     {
+-        exec('/bin/bash', '-o','pipefail','-c', "($cmd) 2>$tmp.e >$tmp.o");
++        exec('bash', '-o','pipefail','-c', "($cmd) 2>$tmp.e >$tmp.o");
+     }
+     waitpid($pid,0);
+ 
+@@ -1063,7 +1063,7 @@ sub test_usage
+     my $command = $args{cmd};
+     my $commandpath = $$opts{bin}."/".$command;
+     my ($ret,$out) = _cmd("$commandpath $args{redirection} 2>&1");
+-    if ( $out =~ m/\/bin\/bash.*no.*such/i ) { failed($opts,$test,"could not run $commandpath: $out"); return; }
++    if ( $out =~ m/bash.*no.*such/i ) { failed($opts,$test,"could not run $commandpath: $out"); return; }
+ 
+     my @sections = ($out =~ m/(^[A-Za-z]+.*?)(?:(?=^[A-Za-z]+:)|\z)/msg);
+ 
+@@ -1118,7 +1118,7 @@ sub test_usage_subcommand
+     my $subcommand = $args{subcmd};
+     my $commandpath = $$opts{bin}."/".$command;
+     my ($ret,$out) = _cmd("$commandpath $subcommand $args{redirection} 2>&1");
+-    if ( $out =~ m/\/bin\/bash.*no.*such/i ) { failed($opts,$test,"could not run $commandpath $subcommand: $out"); return; }
++    if ( $out =~ m/bash.*no.*such/i ) { failed($opts,$test,"could not run $commandpath $subcommand: $out"); return; }
+ 
+     my @sections = ($out =~ m/(^[A-Za-z]+.*?)(?:(?=^[A-Za-z]+:)|\z)/msg);
+ 
