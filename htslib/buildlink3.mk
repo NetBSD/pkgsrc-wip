b99d85413dcea6be379823bc28821d76d8678ec5
Jason Bacon <bacon@NetBSD.org>
2020-07-23 10:27:12 +0000

    htslib: Import bl3 fix from pkgsrc

--- htslib/buildlink3.mk
+++ htslib/buildlink3.mk
@@ -5,8 +5,9 @@ BUILDLINK_TREE+=	htslib
 .if !defined(HTSLIB_BUILDLINK3_MK)
 HTSLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.10.2
-BUILDLINK_PKGSRCDIR.htslib?=	../../wip/htslib
+BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.8
+BUILDLINK_ABI_DEPENDS.htslib+=	htslib>=1.10.2
+BUILDLINK_PKGSRCDIR.htslib?=	../../biology/htslib
 
 .endif	# HTSLIB_BUILDLINK3_MK
 
