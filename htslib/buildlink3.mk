2aa119c2112ce794c385a610effa3b68e9c249a9
Thomas Klausner <tk@giga.or.at>
2020-07-25 09:46:13 +0200

    Revert "htslib: remove, used for updating biology/htslib"
    This reverts commit 1955414241d219682c08b5c6a2ea665dcf92ac5c.

---
+++ htslib/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	htslib
+
+.if !defined(HTSLIB_BUILDLINK3_MK)
+HTSLIB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.8
+BUILDLINK_ABI_DEPENDS.htslib+=	htslib>=1.10.2
+BUILDLINK_PKGSRCDIR.htslib?=	../../wip/htslib
+
+.endif	# HTSLIB_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-htslib
