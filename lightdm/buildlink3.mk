9b2b2ef3f5f707f8661ae06aa8770581eb4e80e8
Frédéric Fauberteau <triaxx@NetBSD.org>
2020-07-26 14:46:32 +0200

    lightdm: Fix blk3.mk
    BUILDLINK_PKGSRCDIR.lightdm was set to ../../local/lightdm that was a
    local development directory.

--- lightdm/buildlink3.mk
+++ lightdm/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	lightdm
 LIGHTDM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.lightdm+=	lightdm>=1.30.0
-BUILDLINK_PKGSRCDIR.lightdm?=	../../local/lightdm # s,local,x11,
+BUILDLINK_PKGSRCDIR.lightdm?=	../../wip/lightdm # s,local,x11,
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../x11/libxklavier/buildlink3.mk"
