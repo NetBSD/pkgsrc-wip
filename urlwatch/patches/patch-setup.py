0d4d2527f4b64c861728211709194f39d68adf30
Yorick Hardy <yorickhardy@gmail.com>
2020-08-01 21:05:05 +0200

    urlwatch: update to urlwatch-2.21
    No changelog provided. However, note that some features are deprecated:
    
      https://urlwatch.readthedocs.io/en/latest/deprecated.html

--- urlwatch/patches/patch-setup.py
+++ urlwatch/patches/patch-setup.py
@@ -2,11 +2,11 @@ $NetBSD$
 
 Use the pkgsrc directory structure.
 
---- setup.py.orig	2018-06-03 12:42:56.000000000 +0000
+--- setup.py.orig	2020-07-31 05:37:07.000000000 +0000
 +++ setup.py
-@@ -22,8 +22,8 @@ m['package_dir'] = {'': 'lib'}
+@@ -24,8 +24,8 @@ m['package_dir'] = {'': 'lib'}
  m['packages'] = ['urlwatch']
- m['python_requires'] = '>3.3.0'
+ m['python_requires'] = '>=3.5'
  m['data_files'] = [
 -    ('share/man/man1', ['share/man/man1/urlwatch.1']),
 -    ('share/urlwatch/examples', [
