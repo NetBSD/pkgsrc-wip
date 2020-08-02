2b4156d790397360836067e3db40778fd605266a
Tobias Nygren <tnn@NetBSD.org>
2020-08-02 08:23:40 +0200

    gnuradio-wavelet: catch up w/ gnuradio-core

--- gnuradio-wavelet/buildlink3.mk
+++ gnuradio-wavelet/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=	gnuradio-wavelet
 .if !defined(GNURADIO_WAVELET_BUILDLINK3_MK)
 GNURADIO_WAVELET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-wavelet+=	gnuradio-wavelet>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-wavelet+=	gnuradio-wavelet>=3.7.5
-BUILDLINK_PKGSRCDIR.gnuradio-wavelet?=		../../ham/gnuradio-wavelet
+BUILDLINK_API_DEPENDS.gnuradio-wavelet+=	gnuradio-wavelet>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-wavelet+=	gnuradio-wavelet>=3.8.1.0
+BUILDLINK_PKGSRCDIR.gnuradio-wavelet?=		../../wip/gnuradio-wavelet
 .endif # GNURADIO_WAVELET_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnuradio-wavelet
