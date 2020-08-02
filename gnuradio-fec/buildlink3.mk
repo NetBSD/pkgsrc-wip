353d61e17ec6fd8cd58437aabaf5fb0c60134a7e
Tobias Nygren <tnn@NetBSD.org>
2020-08-02 08:35:47 +0200

    gnuradio-fec: catch up w/ gnuradio-core

--- gnuradio-fec/buildlink3.mk
+++ gnuradio-fec/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=	gnuradio-fec
 .if !defined(GNURADIO_FEC_BUILDLINK3_MK)
 GNURADIO_FEC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-fec+=	gnuradio-fec>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-fec+=	gnuradio-fec>=3.7.5
-BUILDLINK_PKGSRCDIR.gnuradio-fec?=	../../ham/gnuradio-fec
+BUILDLINK_API_DEPENDS.gnuradio-fec+=	gnuradio-fec>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-fec+=	gnuradio-fec>=3.8.1.0
+BUILDLINK_PKGSRCDIR.gnuradio-fec?=	../../wip/gnuradio-fec
 .endif # GNURADIO_FEC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnuradio-fec
