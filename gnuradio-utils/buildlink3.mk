dc52c0b5e602d77550969cdce5dfba4ffaa966ca
Tobias Nygren <tnn@NetBSD.org>
2020-08-02 08:35:04 +0200

    gnuradio-utils: catch up w/ gnuradio-core

--- gnuradio-utils/buildlink3.mk
+++ gnuradio-utils/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=	gnuradio-utils
 .if !defined(GNURADIO_UTILS_BUILDLINK3_MK)
 GNURADIO_UTILS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-utils+=	gnuradio-utils>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-utils+=	gnuradio-utils>=3.7.5
-BUILDLINK_PKGSRCDIR.gnuradio-utils?=	../../ham/gnuradio-utils
+BUILDLINK_API_DEPENDS.gnuradio-utils+=	gnuradio-utils>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-utils+=	gnuradio-utils>=3.8.1.0
+BUILDLINK_PKGSRCDIR.gnuradio-utils?=	../../wip/gnuradio-utils
 .endif # GNURADIO_UTILS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnuradio-utils
