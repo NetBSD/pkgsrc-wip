4a5ef9c7f2a03b2cdb5cce7783dcd98a5494723f
Tobias Nygren <tnn@NetBSD.org>
2020-08-02 09:28:05 +0200

    gnuradio-dtv: catch up w/ gnuradio-core

--- gnuradio-dtv/buildlink3.mk
+++ gnuradio-dtv/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=	gnuradio-dtv
 .if !defined(GNURADIO_DTV_BUILDLINK3_MK)
 GNURADIO_DTV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-dtv+=	gnuradio-dtv>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-dtv+=	gnuradio-dtv>=3.7.5
-BUILDLINK_PKGSRCDIR.gnuradio-dtv?=	../../ham/gnuradio-dtv
+BUILDLINK_API_DEPENDS.gnuradio-dtv+=	gnuradio-dtv>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-dtv+=	gnuradio-dtv>=3.8.1.0
+BUILDLINK_PKGSRCDIR.gnuradio-dtv?=	../../wip/gnuradio-dtv
 .endif # GNURADIO_DTV_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnuradio-dtv
