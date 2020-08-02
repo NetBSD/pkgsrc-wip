7415f6f9e3dc94e2e6a48b054953df229d1a5e49
Tobias Nygren <tnn@NetBSD.org>
2020-08-02 11:45:22 +0200

    gnuradio-video-sdl: catch up w/ gnuradio-core

--- gnuradio-video-sdl/buildlink3.mk
+++ gnuradio-video-sdl/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=	gnuradio-video-sdl
 .if !defined(GNURADIO_VIDEO_SDL_BUILDLINK3_MK)
 GNURADIO_VIDEO_SDL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-video-sdl+=	gnuradio-video-sdl>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-video-sdl+=	gnuradio-video-sdl>=3.7.5
-BUILDLINK_PKGSRCDIR.gnuradio-video-sdl?=	../../ham/gnuradio-video-sdl
+BUILDLINK_API_DEPENDS.gnuradio-video-sdl+=	gnuradio-video-sdl>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-video-sdl+=	gnuradio-video-sdl>=3.8.1.0
+BUILDLINK_PKGSRCDIR.gnuradio-video-sdl?=	../../wip/gnuradio-video-sdl
 .endif # GNURADIO_VIDEO_SDL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnuradio-video-sdl
