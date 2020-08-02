d5fc0674c2100bbcd042adeb36a7f67525ab4127
Tobias Nygren <tnn@NetBSD.org>
2020-08-02 08:21:40 +0200

    gnuradio-zeromq: catch up w/ gnuradio-core

--- gnuradio-zeromq/buildlink3.mk
+++ gnuradio-zeromq/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=	gnuradio-zeromq
 .if !defined(GNURADIO_ZEROMQ_BUILDLINK3_MK)
 GNURADIO_ZEROMQ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-zeromq+=	gnuradio-zeromq>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-zeromq+=	gnuradio-zeromq>=3.7.5
-BUILDLINK_PKGSRCDIR.gnuradio-zeromq?=	../../ham/gnuradio-zeromq
+BUILDLINK_API_DEPENDS.gnuradio-zeromq+=	gnuradio-zeromq>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-zeromq+=	gnuradio-zeromq>=3.8.1.0
+BUILDLINK_PKGSRCDIR.gnuradio-zeromq?=	../../wip/gnuradio-zeromq
 .endif # GNURADIO_ZEROMQ_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnuradio-zeromq
