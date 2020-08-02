54af4f5a138de093f45887ad4d8e45aa9f040bb6
Tobias Nygren <tnn@NetBSD.org>
2020-08-02 10:36:00 +0200

    gnuradio-trellis: catch up w/ gnuradio-core

--- gnuradio-trellis/buildlink3.mk
+++ gnuradio-trellis/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=	gnuradio-trellis
 .if !defined(GNURADIO_TRELLIS_BUILDLINK3_MK)
 GNURADIO_TRELLIS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-trellis+=	gnuradio-trellis>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-trellis+=	gnuradio-trellis>=3.7.5
-BUILDLINK_PKGSRCDIR.gnuradio-trellis?=		../../ham/gnuradio-trellis
+BUILDLINK_API_DEPENDS.gnuradio-trellis+=	gnuradio-trellis>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-trellis+=	gnuradio-trellis>=3.8.1.0
+BUILDLINK_PKGSRCDIR.gnuradio-trellis?=		../../wip/gnuradio-trellis
 .endif # GNURADIO_TRELLIS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnuradio-trellis
