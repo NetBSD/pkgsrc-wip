a92c7bb146510a201e65f209584e172c4b1f2d71
Tobias Nygren <tnn@NetBSD.org>
2020-08-02 08:52:44 +0200

    gnuradio-vocoder: catch up w/ gnuradio-core

--- gnuradio-vocoder/buildlink3.mk
+++ gnuradio-vocoder/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=	gnuradio-vocoder
 .if !defined(GNURADIO_VOCODER_BUILDLINK3_MK)
 GNURADIO_VOCODER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.7.5
-BUILDLINK_PKGSRCDIR.gnuradio-vocoder?=		../../ham/gnuradio-vocoder
+BUILDLINK_API_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-vocoder+=	gnuradio-vocoder>=3.8.1.0
+BUILDLINK_PKGSRCDIR.gnuradio-vocoder?=		../../wip/gnuradio-vocoder
 .endif # GNURADIO_VOCODER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnuradio-vocoder
