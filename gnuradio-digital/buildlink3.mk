485f1e2e2222f3b0f5eb0879496ddc3303baf160
Tobias Nygren <tnn@NetBSD.org>
2020-08-02 09:04:20 +0200

    gnuradio-digital: catch up w/ gnuradio-core

--- gnuradio-digital/buildlink3.mk
+++ gnuradio-digital/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=	gnuradio-digital
 .if !defined(GNURADIO_DIGITAL_BUILDLINK3_MK)
 GNURADIO_DIGITAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnuradio-digital+=	gnuradio-digital>=3.7.5
-BUILDLINK_ABI_DEPENDS.gnuradio-digital+=	gnuradio-digital>=3.7.5
-BUILDLINK_PKGSRCDIR.gnuradio-digital?=		../../ham/gnuradio-digital
+BUILDLINK_API_DEPENDS.gnuradio-digital+=	gnuradio-digital>=3.8.1.0
+BUILDLINK_ABI_DEPENDS.gnuradio-digital+=	gnuradio-digital>=3.8.1.0
+BUILDLINK_PKGSRCDIR.gnuradio-digital?=		../../wip/gnuradio-digital
 .endif # GNURADIO_DIGITAL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnuradio-digital
