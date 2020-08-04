2e0e3b142708da67199047d8c86f600e041c65be
Paul Ripke <stix@stix.id.au>
2020-08-04 16:29:31 +1000

    threadingbuildingblocks: Update to 2020.3.
    See package CHANGES for long list of changes.
    
    Tested as an indirect dependency of graphics/blender on netbsd-9 x86-64.

--- threadingbuildingblocks/buildlink3.mk
+++ threadingbuildingblocks/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	threadingbuildingblocks
 .if !defined(THREADINGBUILDINGBLOCKS_BUILDLINK3_MK)
 THREADINGBUILDINGBLOCKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.threadingbuildingblocks+=	threadingbuildingblocks>=2017.20160916
+BUILDLINK_API_DEPENDS.threadingbuildingblocks+=	threadingbuildingblocks>=2018.5
 BUILDLINK_PKGSRCDIR.threadingbuildingblocks?=	../../wip/threadingbuildingblocks
 .endif	# THREADINGBUILDINGBLOCKS_BUILDLINK3_MK
 
