a6df6a2619f25f9e9551eb0c1e88a20a221f4ac2
Thomas Klausner <tk@giga.or.at>
2020-08-13 10:15:14 +0200

    syncserver-dependencies: add nginx option and make it default

--- syncserver-dependencies/options.mk
+++ syncserver-dependencies/options.mk
@@ -1,12 +1,17 @@
 # $NetBSD: options.mk,v 1.2 2015/01/25 20:05:54 thomasklausner Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sync-server-dependencies
-PKG_SUPPORTED_OPTIONS=	apache
-PKG_SUGGESTED_OPTIONS=	apache
+PKG_SUPPORTED_OPTIONS=	apache nginx
+PKG_SUGGESTED_OPTIONS=	nginx
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mapache)
 DEPENDS+=	${PYPKGPREFIX}-${APACHE_PKG_PREFIX}-mod_wsgi-[0-9]*:../../www/py-mod_wsgi
 .include "../../mk/apache.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mnginx)
+DEPENDS+=	nginx-[0-9]*:../../www/nginx
+DEPENDS+=	${PYPKGPREFIX}-gunicorn-[0-9]*:../../www/py-gunicorn
 .endif
