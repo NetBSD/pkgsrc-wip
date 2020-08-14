0ab581a2d3289da61cb437d256f9a5a94a9bc00c
Sergey A. Osokin <osa@FreeBSD.org>
2020-08-13 22:12:16 -0400

    Update patches.

--- unit/patches/patch-src_nxt__conn__write.c
+++ unit/patches/patch-src_nxt__conn__write.c
@@ -1,25 +1,22 @@
-$NetBSD$
-
-
 --- src/nxt_conn_write.c.orig	2020-05-28 12:04:00.000000000 -0400
-+++ src/nxt_conn_write.c	2020-08-08 16:49:50.752693316 -0400
++++ src/nxt_conn_write.c	2020-08-12 14:52:46.453112517 -0400
 @@ -266,6 +266,21 @@
      res = sendfile(s, fd, &pos, size);
  #endif
  
-+#ifdef NXT_HAVE_NETBSD_SENDFILE
-+    struct stat fileinfo;
-+    void *fmem = NULL;
-+    res = fstat(fd, &fileinfo);
-+    if (res == 0) {
-+        fmem = mmap(NULL, fileinfo.st_size, PROT_READ, MAP_FILE | MAP_SHARED, fd, 0);
++#ifdef NXT_HAVE_NETBSD_MMAP_WRITE
++    int   err;
++    void *map;
++
++    map = mmap(NULL, size, PROT_READ, MAP_FILE | MAP_SHARED, fd, pos);
++    if (nxt_slow_path(map == MAP_FAILED)) {
++        return -1;
 +    }
-+    if (fmem != NULL) {
-+        res = write(s, fmem, fileinfo.st_size);
-+        munmap(fmem, fileinfo.st_size);
-+    } else {
-+        res = -1;
-+    }
++
++    res = write(s, map, size);
++    err = errno;
++    (void) munmap(map, size);
++    errno = err;
 +#endif 
 +
      return res;
