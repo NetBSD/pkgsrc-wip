2ee23116212052a09de1a06e2c1f352b09f6a0b6
jaypatelani <jaypatelani@gmail.com>
2020-09-04 16:26:45 +0530

    fixes for cinnamon-screensaver

--- cinnamon-screensaver/buildlink3.mk
+++ cinnamon-screensaver/buildlink3.mk
@@ -1,13 +1,17 @@
 # $NetBSD$
-
 BUILDLINK_TREE+=	cinnamon-screensaver
 
 .if !defined(CINNAMON_SCREENSAVER_BUILDLINK3_MK)
 CINNAMON_SCREENSAVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cinnamon-screensaver+=	cinnamon-screensaver>=3.8.1
+BUILDLINK_API_DEPENDS.cinnamon-screensaver+=	cinnamon-screensaver>=4.6.0
 BUILDLINK_PKGSRCDIR.cinnamon-screensaver?=	../../wip/cinnamon-screensaver
 
+.include "../../devel/gobject-introspection/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.include "../../sysutils/accountsservice/buildlink3.mk"
+.include "../../wip/cinnamon-desktop/buildlink3.mk"
+.include "../../wip/xapps/buildlink3.mk"
 .endif	# CINNAMON_SCREENSAVER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-cinnamon-screensaver
