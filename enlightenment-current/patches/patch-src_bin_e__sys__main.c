4eceed4736316b0df883b6818fb33f1c4db5322c
kikadf <kikadf.01@gmail.com>
2020-09-07 09:31:56 +0200

    enlightenment-current: sysactions.conf path fix on NetBSD

---
+++ enlightenment-current/patches/patch-src_bin_e__sys__main.c
@@ -0,0 +1,18 @@
+$NetBSD$
+
+* NetBSD support
+
+--- src/bin/e_sys_main.c.orig	2020-07-26 16:39:12.080648000 +0000
++++ src/bin/e_sys_main.c
+@@ -565,7 +565,11 @@ auth_etc_enlightenment_sysactions(char *
+    int allow = 0;
+    int deny = 0;
+ 
++#if defined (__NetBSD__)
++   snprintf(file, sizeof(file), "/usr/pkg/etc/enlightenment/sysactions.conf");
++#else
+    snprintf(file, sizeof(file), "/etc/enlightenment/sysactions.conf");
++#endif
+    f = fopen(file, "r");
+    if (!f)
+      {
