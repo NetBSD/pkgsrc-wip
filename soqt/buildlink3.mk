6374e5ad4495b1bc6e8345330416c6d8c09b11ff
Pierre Pronchery <khorben@defora.org>
2020-09-15 01:21:52 +0200

    soqt: fix the link to MesaLib

--- soqt/buildlink3.mk
+++ soqt/buildlink3.mk
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.soqt+=	soqt>=1.4.1
 BUILDLINK_PKGSRCDIR.soqt?=	../../wip/soqt
 
 .include "../../graphics/Coin/buildlink3.mk"
-.include "../../graphics/Mesa/buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../x11/qt4-libs/buildlink3.mk"
 .endif	# SOQT_BUILDLINK3_MK
 
