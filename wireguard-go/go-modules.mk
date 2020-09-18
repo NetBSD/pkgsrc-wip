79a7e4ba004d5f05ce873c6cabfa12917819be42
Jonathan Schleifer <js@nil.im>
2020-09-18 20:45:05 +0200

    Incomplete early import of wireguard-go; DON'T USE

---
+++ wireguard-go/go-modules.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20190308221718-c2843e01d9a2.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20191002192127-34f69633bfdc.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20191002192127-34f69633bfdc.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190404232315-eb5bcb51f2a3.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20191003171128-d98b1b443823.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20191003171128-d98b1b443823.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190215142949-d0b11bdaac8a.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190412213103-97732733099d.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200302150141-5c8b2ff67527.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200302150141-5c8b2ff67527.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.2.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.2.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180917221912-90fa682c2a6e.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180917221912-90fa682c2a6e.zip
