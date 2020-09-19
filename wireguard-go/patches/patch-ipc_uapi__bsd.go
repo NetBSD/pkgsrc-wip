d0a852c51b9f5784480ffb83d729794e84658108
Benny Siegert <bsiegert@gmail.com>
2020-09-19 19:02:27 +0000

    wireguard-go: fix NetBSD build
    Note that I haven't tested this, only the build.
    
    - add NetBSD support for ipc and tun subpackages (= openbsd)
    - set GO_BUILD_PATTERN so that we don't build all code in all dependencies
    - comment out dependency to non-existent wireguard-tools package

---
+++ wireguard-go/patches/patch-ipc_uapi__bsd.go
@@ -0,0 +1,12 @@
+$NetBSD$
+
+Add NetBSD to the list of OSes for this file.
+
+--- ipc/uapi_bsd.go.orig	2020-03-20 18:00:53.000000000 +0000
++++ ipc/uapi_bsd.go
+@@ -1,4 +1,4 @@
+-// +build darwin freebsd openbsd
++// +build darwin freebsd openbsd netbsd
+ 
+ /* SPDX-License-Identifier: MIT
+  *
