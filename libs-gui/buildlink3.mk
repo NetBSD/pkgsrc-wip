0e3d5ce2d0c33a8d437fcb0e1bfc4b18ecf88332
Makoto Fujiwara <makoto@ki.nu>
2020-09-28 08:29:32 +0900

    (x11/gnustep-gui) Name PKGNAME as x11/gnustep-gui

--- libs-gui/buildlink3.mk
+++ libs-gui/buildlink3.mk
@@ -1,15 +1,15 @@
-# $NetBSD$
+# $NetBSD: buildlink3.mk,v 1.34 2020/06/02 08:23:02 adam Exp $
 
-BUILDLINK_TREE+=	libs-gui
+BUILDLINK_TREE+=	gnustep-gui
 
-.if !defined(LIBS_GUI_BUILDLINK3_MK)
-LIBS_GUI_BUILDLINK3_MK:=
+.if !defined(GNUSTEP_GUI_BUILDLINK3_MK)
+GNUSTEP_GUI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libs-gui+=	libs-gui>=0.28
-BUILDLINK_ABI_DEPENDS.libs-gui+=	libs-gui>=0.28
-BUILDLINK_PKGSRCDIR.libs-gui?=		../../wip/libs-gui
+BUILDLINK_API_DEPENDS.gnustep-gui+=	gnustep-gui>=0.28
+BUILDLINK_ABI_DEPENDS.gnustep-gui+=	gnustep-gui>=0.28
+BUILDLINK_PKGSRCDIR.gnustep-gui?=	../../x11/gnustep-gui
 
 .include "../../devel/gnustep-base/buildlink3.mk"
-.endif # LIBS_GUI_BUILDLINK3_MK
+.endif # GNUSTEP_GUI_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-libs-gui
+BUILDLINK_TREE+=	-gnustep-gui
