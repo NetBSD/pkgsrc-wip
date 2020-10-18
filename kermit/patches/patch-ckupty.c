72db3b5d9e405a11210e31eddcca28bce2dfe4a4
Greg Troxel <gdt@lexort.com>
2020-10-18 11:36:41 -0400

    kermit: Update to pre-alpha03
    This is very very WIP-y, more so than usual.
    
    Besides the update:
    
    * delete ancient patch with no effect
    
    We added this in 2012, and the issue has apparently been addressed in
    some other way.  Upstream has opined that it's not necessary, and
    building without it did not exhibit the warning for which it was
    added.
    
    * Document a patch better
    
    Note that the TERMIO regularizaiton is not appropriate for upstream.
    
    * Drop patches Overtaken By Upstream
    
    Upstream has revamped the strategy for declartion of errno, and thus
    these are no longer needed.
    
    * Drop errno-declaring patch
    
    Upstream addressed this more broadly and our patch is not longer
    needed.
    
    * Drop errno.h include, rather than adding one
    
    Likely will be applied upstream.

--- kermit/patches/patch-ckupty.c
+++ kermit/patches/patch-ckupty.c
@@ -1,6 +1,12 @@
 $NetBSD: patch-ckupty.c,v 1.1 2015/11/07 23:20:59 dholland Exp $
 
 Always use termios, never sgtty.h.
+\todo Explain this more clearly.
+
+This patch appears to be regularizing the use of TERMIO, following the
+package's inclusion of mk/termcap.buildlink3.mk.  This is contrary to
+upstream's approach which uses each systems's native libs.  Therefore,
+this patch should not be submitted to upstream.
 
 --- ckupty.c~	2011-06-13 15:34:13.000000000 +0000
 +++ ckupty.c
