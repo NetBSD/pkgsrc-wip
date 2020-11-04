bda6ae87ff355002f274205378b5dc992fd7a84a
Jason Bacon <bacon@NetBSD.org>
2020-11-04 10:58:09 -0600

    biolibc: Fix version in bl3

--- biolibc/buildlink3.mk
+++ biolibc/buildlink3.mk
@@ -8,7 +8,7 @@ BUILDLINK_TREE+=	biolibc
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.1.0.16
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.1.0.33
 BUILDLINK_PKGSRCDIR.biolibc?=	../../wip/biolibc
 BUILDLINK_DEPMETHOD.biolibc?=	build
 .endif	# BIOLIBC_BUILDLINK3_MK
