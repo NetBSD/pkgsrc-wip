365607c37ed258ce79410007431f12644a9afa9d
nia <nia@netbsd.org>
2020-11-04 11:07:08 +0100

    gnome-tracker: update to 3.0.1

--- gnome-tracker/buildlink3.mk
+++ gnome-tracker/buildlink3.mk
@@ -8,11 +8,6 @@ GNOME_TRACKER_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gnome-tracker+=	gnome-tracker>=1.8.0
 BUILDLINK_PKGSRCDIR.gnome-tracker?=	../../wip/gnome-tracker
 
-.include "../../textproc/icu/buildlink3.mk"
-.include "../../textproc/libxml2/buildlink3.mk"
-.include "../../devel/libuuid/buildlink3.mk"
-.include "../../databases/sqlite3/buildlink3.mk"
-.include "../../graphics/png/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .endif	# GNOME_TRACKER_BUILDLINK3_MK
 
