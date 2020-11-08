c94e5aacb060e54ae09fc2cc19362466a3fe6238
snow flurry <snow@datagirl.xyz>
2020-11-08 14:39:44 -0800

    Radicale3: /etc/rc.d script changes
    - Redirect stderr to logfile
    - Use $command/$command_args instead of $start_cmd

--- Radicale3/files/radicale.sh
+++ Radicale3/files/radicale.sh
@@ -10,19 +10,29 @@ $_rc_subr_loaded . /etc/rc.subr
 
 name="radicale"
 rcvar=$name
-radicale_user=@RADICALE_USER@
-radicale_group=@RADICALE_GROUP@
-command="@PREFIX@/bin/radicale"
-start_cmd="radicale_start"
-command_interpreter="@PYTHONBIN@"
-required_files="@PKG_SYSCONFDIR@/radicale/config"
-pidfile="@VARBASE@/run/${name}.pid"
-
-radicale_start()
-{
-    nohup ${command} ${command_args} </dev/null >/dev/null 2>&1 &
-    echo $! >${pidfile}
-}
 
 load_rc_config $name
+
+: ${radicale_user:=@RADICALE_USER@}
+: ${radicale_group:=@RADICALE_GROUP@}
+: ${radicale_pidfile:="@VARBASE@/run/${name}.pid"}
+: ${radicale_logfile:="@VARBASE@/log/${name}.log"}
+
+command="@PREFIX@/bin/radicale"
+command_args=" </dev/null >/dev/null 2>${radicale_logfile} & echo \$! >${radicale_pidfile}"
+command_interpreter="@PYTHONBIN@"
+required_files="@PKG_SYSCONFDIR@/radicale/config"
+
+start_precmd="radicale_precmd"
+
+radicale_precmd()
+{
+    @TOUCH@ ${radicale_logfile} && \
+    @CHOWN@ ${radicale_user}:${radicale_group} ${radicale_logfile} && \
+    @CHMOD@ 0750 ${radicale_logfile} && \
+    @TOUCH@ ${radicale_pidfile} && \
+    @CHOWN@ ${radicale_user}:${radicale_group} ${radicale_pidfile} && \
+    @CHMOD@ 0750 ${radicale_pidfile}
+}
+
 run_rc_command "$1"
