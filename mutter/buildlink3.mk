69a1987e38581a91dcbd05fa7e721ac6526a7f2f
nia <nia@netbsd.org>
2020-11-10 01:29:29 +0100

    mutter: Sort includes. Remove reference to wip graphene.

--- mutter/buildlink3.mk
+++ mutter/buildlink3.mk
@@ -9,19 +9,11 @@ BUILDLINK_API_DEPENDS.mutter+=	mutter>=3.36.3
 BUILDLINK_PKGSRCDIR.mutter?=	../../wip/mutter
 
 .include "../../devel/gobject-introspection/buildlink3.mk"
-.include "../../x11/startup-notification/buildlink3.mk"
-#.include "../../x11/libSM/buildlink3.mk"
-#.include "../../wip/libinput/buildlink3.mk"
-#.include "../../audio/libcanberra/buildlink3.mk"
-#.include "../../x11/libICE/buildlink3.mk"
-#.include "../../x11/libxkbfile/buildlink3.mk"
-#.include "../../wip/gnome-desktop3/buildlink3.mk"
+.include "../../graphics/graphene/buildlink3.mk"
 .include "../../textproc/json-glib/buildlink3.mk"
-#.include "../../wip/gnome-settings-daemon/buildlink3.mk"
-#.include "../../sysutils/gsettings-desktop-schemas/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
-.include "../../wip/graphene/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/startup-notification/buildlink3.mk"
 .endif	# MUTTER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mutter
