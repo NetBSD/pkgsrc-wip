88098f7c2be077ef3a3be2e7903c1246ce5447f1
Amitai Schleier <schmonz-web-git@schmonz.com>
2020-11-15 22:11:42 +0100

    Set INCDIRS and LIBDIRS on these new buildlinks.

--- s6-dns/buildlink3.mk
+++ s6-dns/buildlink3.mk
@@ -7,6 +7,8 @@ S6_DNS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.s6-dns+=	s6-dns>=2.3.3.0
 BUILDLINK_PKGSRCDIR.s6-dns?=	../../wip/s6-dns
+BUILDLINK_INCDIRS.s6-dns+=	include/s6-dns
+BUILDLINK_LIBDIRS.s6-dns+=	lib/s6-dns
 BUILDLINK_DEPMETHOD.s6-dns?=	build
 .endif	# S6_DNS_BUILDLINK3_MK
 
