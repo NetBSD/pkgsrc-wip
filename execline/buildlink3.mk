a126c7661b8cdeebd83c6cdba2f7b7e2b1eaa17b
Amitai Schleier <schmonz-web-git@schmonz.com>
2020-11-15 21:26:46 +0100

    execline libs are static, so build-only depends.

--- execline/buildlink3.mk
+++ execline/buildlink3.mk
@@ -7,8 +7,7 @@ EXECLINE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.execline+=	execline>=2.2.0.0
 BUILDLINK_PKGSRCDIR.execline?=		../../wip/execline
-
-.include "../../devel/skalibs/buildlink3.mk"
+BUILDLINK_DEPMETHOD.execline?=		build
 .endif # EXECLINE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-execline
