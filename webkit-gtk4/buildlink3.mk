5447e72891e7417ed0e79d74685698de58b056ed
Dan Cîrnaț <dan@alt.md>
2020-11-21 12:48:33 +0100

    webkit-gtk4: Fix buildlink package name and version

--- webkit-gtk4/buildlink3.mk
+++ webkit-gtk4/buildlink3.mk
@@ -5,14 +5,14 @@ BUILDLINK_TREE+=	webkit-gtk4
 .if !defined(WEBKIT_GTK_BUILDLINK3_MK)
 WEBKIT_GTK4_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.webkit-gtki4+=	webkit-gtk>=2.8
-BUILDLINK_ABI_DEPENDS.webkit-gtk4?=	webkit-gtk>=2.30.2nb1
-BUILDLINK_PKGSRCDIR.webkit-gtk?=	../../wip/webkit-gtk4
+BUILDLINK_API_DEPENDS.webkit-gtk4+=	webkit-gtk4>=2.8
+BUILDLINK_ABI_DEPENDS.webkit-gtk4?=	webkit-gtk4>=2.30.2
+BUILDLINK_PKGSRCDIR.webkit-gtk4?=	../../wip/webkit-gtk4
 
 # This is necessary to match the package requirement.
 GCC_REQD+=	7.3
 
-pkgbase := webkit-gtk
+pkgbase := webkit-gtk4
 .include "../../mk/pkg-build-options.mk"
 
 .if !empty(PKG_BUILD_OPTIONS.webkit-gtk4:Menchant)
@@ -31,7 +31,7 @@ pkgbase := webkit-gtk
 .include "../../security/libsecret/buildlink3.mk"
 .include "../../textproc/icu/buildlink3.mk"
 .include "../../textproc/libxslt/buildlink3.mk"
-.include "../../x11/gtk4/buildlink3.mk"
+.include "../../wip/gtk4/buildlink3.mk"
 .include "../../x11/libXdamage/buildlink3.mk"
 .include "../../x11/libXt/buildlink3.mk"
 .endif # WEBKIT_GTK4_BUILDLINK3_MK
