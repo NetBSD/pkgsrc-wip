ea861b54a472fa453dca563e9316bee0366c7fa7
Amitai Schleier <schmonz-web-git@schmonz.com>
2020-12-01 20:51:26 +0100

    Seems callers need to -lbearssl, so buildlink that.

--- libtls-bearssl/buildlink3.mk
+++ libtls-bearssl/buildlink3.mk
@@ -8,6 +8,8 @@ LIBTLS_BEARSSL_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libtls-bearssl+=	libtls-bearssl>=0.2
 BUILDLINK_PKGSRCDIR.libtls-bearssl?=	../../wip/libtls-bearssl
 BUILDLINK_DEPMETHOD.libtls-bearssl?=	build
+
+.include "../../security/bearssl/buildlink3.mk"
 .endif	# LIBTLS_BEARSSL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libtls-bearssl
