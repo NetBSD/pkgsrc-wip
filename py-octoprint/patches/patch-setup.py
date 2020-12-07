c4842d9cdb815e3f72343f99a3c17e6ad5699602
Pierre Pronchery <khorben@defora.org>
2020-12-07 21:38:41 +0100

    py-octoprint: update to version 1.5.1
    Changes from 1.5.0 to 1.5.1:
    
     * #3844 - Fix api/files/<origin>/<path> not returning children for
       folders
     * #3852 - Fix service discovery messing with hostname resolution. Still
       unclear on how that even happens and unable to reproduce, but at
       least simply completely ignoring loopback devices seems to do the
       trick.

--- py-octoprint/patches/patch-setup.py
+++ py-octoprint/patches/patch-setup.py
@@ -2,23 +2,23 @@ $NetBSD$
 
 Override some version checks
 
---- setup.py.orig	2020-08-06 10:41:11.000000000 +0000
+--- setup.py.orig	2020-12-04 09:41:45.000000000 +0000
 +++ setup.py
-@@ -33,7 +33,7 @@ INSTALL_REQUIRES = [
- 	"markupsafe>=1.1,<2.0",      # Jinja dependency, newer versions require Python 3
- 	"tornado==5.1.1",            # newer versions require Python 3
- 	"markdown>=3.1,<3.2",        # newer versions require Python 3
--	"rsa==4.0",                  # newer versions require Python 3
-+	"rsa>=4.0",                  # newer versions require Python 3
- 	"regex!=2018.11.6",          # avoid broken 2018.11.6. See #2874
- 
- 	# anything below this should be checked on releases for new versions
-@@ -68,7 +68,7 @@ INSTALL_REQUIRES = [
- 	"filetype>=1.0.7,<2",
- 
- 	# vendor bundle dependencies
--	"unidecode>=0.04.14,<0.05",  # dependency of awesome-slugify
-+	"unidecode>=0.04.14",  # dependency of awesome-slugify
- 	"blinker>=1.4,<2"            # dependency of flask_principal
+@@ -32,7 +32,7 @@ INSTALL_REQUIRES = [
+     "markupsafe>=1.1,<2.0",  # Jinja dependency, newer versions require Python 3
+     "tornado==5.1.1",  # newer versions require Python 3
+     "markdown>=3.1,<3.2",  # newer versions require Python 3
+-    "rsa==4.0",  # newer versions require Python 3
++    "rsa>=4.0",  # newer versions require Python 3
+     "regex!=2018.11.6",  # avoid broken 2018.11.6. See #2874
+     # anything below this should be checked on releases for new versions
+     "flask>=1.1.2,<2",
+@@ -63,7 +63,7 @@ INSTALL_REQUIRES = [
+     "sentry-sdk>=0.15.1,<1",
+     "filetype>=1.0.7,<2",
+     # vendor bundled dependencies
+-    "unidecode>=0.04.14,<0.05",  # dependency of awesome-slugify
++    "unidecode>=0.04.14",  # dependency of awesome-slugify
+     "blinker>=1.4,<2",  # dependency of flask_principal
  ]
  
