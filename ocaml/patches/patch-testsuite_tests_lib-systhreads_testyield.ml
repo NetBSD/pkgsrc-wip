eb52654fd209df40e792691a602f0656269a12d0
Havard Eidnes <he@NetBSD.org>
2020-12-08 11:30:05 +0100

    Pull over the NetBSD/powerpc adaptations from main pkgsrc.
    Also, limit the number of threads in the testyield.ml test to 2,
    because NetBSD apparently only ever schedules 2 of the 4 threads.

---
+++ ocaml/patches/patch-testsuite_tests_lib-systhreads_testyield.ml
@@ -0,0 +1,18 @@
+$NetBSD$
+
+On NetBSD, the "thread scheduler" appears to just schedule two
+threads out of 4 when all they in essence to is "yield", and not
+do round-robin scheduling, causing the test to loop.  Reduce the
+number of threads in the test to 2.
+
+--- testsuite/tests/lib-systhreads/testyield.ml.orig	2020-09-01 12:47:58.000000000 +0000
++++ testsuite/tests/lib-systhreads/testyield.ml
+@@ -8,7 +8,7 @@
+    *** native
+ *)
+ 
+-let threads = 4
++let threads = 2
+ 
+ let are_ready = ref 0
+ 
