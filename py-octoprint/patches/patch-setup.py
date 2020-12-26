853bf0341fae8a585bcf66e42cb6b33829acf07b
Pierre Pronchery <khorben@defora.org>
2020-12-26 20:14:27 +0100

    py-octoprint: update to version 1.5.2
    From the list of changes:
    
     * #3855 & #3867 - Fix settings merging on the frontend causing
       issues with array values.
     * Pin watchdog dependency to 0.10.4 to work around backwards
       incompatibility with the just release 0.10.5 under Python 3.

--- py-octoprint/patches/patch-setup.py
+++ py-octoprint/patches/patch-setup.py
@@ -2,7 +2,7 @@ $NetBSD$
 
 Override some version checks
 
---- setup.py.orig	2020-12-04 09:41:45.000000000 +0000
+--- setup.py.orig	2020-12-10 11:34:21.000000000 +0000
 +++ setup.py
 @@ -32,7 +32,7 @@ INSTALL_REQUIRES = [
      "markupsafe>=1.1,<2.0",  # Jinja dependency, newer versions require Python 3
@@ -13,6 +13,15 @@ Override some version checks
      "regex!=2018.11.6",  # avoid broken 2018.11.6. See #2874
      # anything below this should be checked on releases for new versions
      "flask>=1.1.2,<2",
+@@ -46,7 +46,7 @@ INSTALL_REQUIRES = [
+     "PyYAML>=5.3.1,<6",
+     "pyserial>=3.4,<4",
+     "netaddr>=0.7.19,<1",
+-    "watchdog==0.10.4",  # watchdog dropped Py>3.6 on 0.10.5 point release...
++    "watchdog>=0.10.3",  # watchdog dropped Py>3.6 on 0.10.5 point release...
+     "sarge==0.1.5post0",
+     "netifaces>=0.10.9,<1",
+     "pylru>=1.2,<2",
 @@ -63,7 +63,7 @@ INSTALL_REQUIRES = [
      "sentry-sdk>=0.15.1,<1",
      "filetype>=1.0.7,<2",
