2690b50c772e9981d21b9fff78583592954085b7
Leonardo Taccari <leot@NetBSD.org>
2020-12-28 13:01:14 +0100

    docker-cli: Add reference to upstream PR

--- docker-cli/patches/patch-vendor_github.com_moby_sys_mount_flags__bsd.go
+++ docker-cli/patches/patch-vendor_github.com_moby_sys_mount_flags__bsd.go
@@ -2,6 +2,10 @@ $NetBSD$
 
 Add support for NetBSD.
 
+Shared upstream via:
+
+ https://github.com/moby/sys/pull/60
+
 --- vendor/github.com/moby/sys/mount/flags_bsd.go.orig	2020-12-15 02:27:14.000000000 +0000
 +++ vendor/github.com/moby/sys/mount/flags_bsd.go
 @@ -1,4 +1,4 @@
