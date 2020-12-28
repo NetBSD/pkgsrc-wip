39f009a594ff5ad2c32ed2d51d2dbdd0e48c975d
Leonardo Taccari <leot@NetBSD.org>
2020-12-28 12:28:12 +0100

    docker-cli: Update to 20.10.1
    pkgsrc changes:
     - Simplify handling of GITHUB_*/DISTNAME/PKGNAME/GO_DIST_BASE to make it easier
       to follow
    
    Changes:
    20.10.1
    =======
    Builder
    -------
     - buildkit: updated to v0.8.1 with various bugfixes moby/moby#41793
    
    Packaging
    ---------
     - Revert a change in the systemd unit that could prevent docker from
       starting due to a startup order conflict docker/docker-ce-packaging#514
     - buildx updated to v0.5.0 docker/docker-ce-packaging#515

---
+++ docker-cli/patches/patch-vendor_github.com_moby_sys_mount_flags__bsd.go
@@ -0,0 +1,12 @@
+$NetBSD$
+
+Add support for NetBSD.
+
+--- vendor/github.com/moby/sys/mount/flags_bsd.go.orig	2020-12-15 02:27:14.000000000 +0000
++++ vendor/github.com/moby/sys/mount/flags_bsd.go
+@@ -1,4 +1,4 @@
+-// +build freebsd openbsd
++// +build freebsd netbsd openbsd
+ 
+ package mount
+ 
