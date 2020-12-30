7d6d29d091ad4184034aaf94db3fc2b254c7e938
Makoto Fujiwara (mef) <makoto@ki.nu>
2020-12-30 13:02:03 +0900

    (lang/rakudo-star) make now goes further (No moar now)
    - have DESTDIR, PATH environment to install.bash
    - Update TODO (for current error message)

--- rakudo-star/patches/patch-lib_actions_install.bash
+++ rakudo-star/patches/patch-lib_actions_install.bash
@@ -3,15 +3,24 @@ $NetBSD$
 One part wants bmake, the other wants gmake
 
 --- ./lib/actions/install.bash.orig	2020-10-26 00:18:32.000000000 +0900
-+++ ./lib/actions/install.bash	2020-12-27 14:24:37.872924232 +0900
++++ ./lib/actions/install.bash	2020-12-27 15:10:57.489393386 +0900
+@@ -155,7 +155,7 @@ build_moarvm() {
+ 	perl Configure.pl \
+ 		"$@" \
+ 		&& make \
+-		&& make install \
++		&& env DESTDIR=@DESTDIR@ make install \
+ 		|| return
+ }
+ 
 @@ -166,8 +166,8 @@ build_nqp() {
  	perl Configure.pl \
  		--backend="$RSTAR_BACKEND" \
  		"$@" \
 -		&& make \
 -		&& make install \
-+		&& @@GMAKE@@ \
-+		&& @@GMAKE@@ install \
++		&& env PATH=@PREFIX@/bin gmake \
++		&& env DESTDIR=@DESTDIR@ PATH=@PREFIX@/bin gmake install \
  		|| return
  }
  
@@ -21,8 +30,8 @@ One part wants bmake, the other wants gmake
  		"$@" \
 -		&& make \
 -		&& make install \
-+		&& @@GMAKE@@ \
-+		&& @@GMAKE@@ install \
++		&& /usr/pkg/bin/gmake \
++		&& env DESTDIR=@DESTDIR@ /usr/pkg/bin/gmake install \
  		|| return
  }
  
