83dc055128a1e54812bdb856d41a57458ac757b4
Leonardo Taccari <leot@NetBSD.org>
2021-01-01 22:01:00 +0100

    tdlib-purple: Update to 0.7.5
    Changes:
    0.7.5
    -----
     - Add support for enable/disable read receipts
     - Misc bug fixes and improvements

---
+++ tdlib-purple/patches/patch-tdlib-purple.cpp
@@ -0,0 +1,17 @@
+$NetBSD$
+
+Fix a copypasto in order to make read receipts really configurable.
+
+--- tdlib-purple.cpp.orig	2021-01-01 15:49:27.000000000 +0000
++++ tdlib-purple.cpp
+@@ -1013,8 +1013,8 @@ static void tgprpl_init (PurplePlugin *p
+ 
+     if (canDisableReadReceipts()) {
+         opt = purple_account_option_bool_new ("Send read receipts",
+-                                              AccountOptions::EnableSecretChats,
+-                                              AccountOptions::EnableSecretChatsDefault);
++                                              AccountOptions::ReadReceipts,
++                                              AccountOptions::ReadReceiptsDefault);
+         prpl_info.protocol_options = g_list_append(prpl_info.protocol_options, opt);
+     }
+ }
