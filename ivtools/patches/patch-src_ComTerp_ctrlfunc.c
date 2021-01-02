0f231885d3a726521614cf2f1b79b56c53fdb413
Makoto Fujiwara (mef) <makoto@ki.nu>
2021-01-02 23:07:46 +0900

    (graphics/ivtools) fix patch end-of-line, regen distinfo

--- ivtools/patches/patch-src_ComTerp_ctrlfunc.c
+++ ivtools/patches/patch-src_ComTerp_ctrlfunc.c
@@ -3,7 +3,7 @@ $NetBSD$
 Add definition for usleep
 
 --- src/ComTerp/ctrlfunc.c.orig	2013-02-13 17:57:54.000000000 +0900
-+++ src/ComTerp/ctrlfunc.c	2021-01-01 21:47:50.772502505 +0900
++++ src/ComTerp/ctrlfunc.c	2021-01-02 23:01:38.296553409 +0900
 @@ -22,6 +22,7 @@
   * 
   */
@@ -12,4 +12,3 @@ Add definition for usleep
  #include <fstream.h>
  #include <iostream>
  #include <ComTerp/comhandler.h>
- 
