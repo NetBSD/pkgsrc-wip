887ddd099b8a826908b89dc09558228d2fbbac75
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2021-01-04 16:22:01 +0100

    UPDATE: wip/py-checkov to new version: 1.0.684

--- py-checkov/patches/patch-setup.py
+++ py-checkov/patches/patch-setup.py
@@ -1,6 +1,6 @@
 $NetBSD$
 
---- setup.py.orig	2020-12-08 19:14:16.000000000 +0000
+--- setup.py.orig	2020-12-31 08:39:27.000000000 +0000
 +++ setup.py
 @@ -1,6 +1,7 @@
  #!/usr/bin/env python
@@ -10,7 +10,7 @@ $NetBSD$
  from importlib import util
  from os import path
  
-@@ -100,7 +101,7 @@ setup(
+@@ -59,7 +60,7 @@ setup(
      author_email="meet@bridgecrew.io",
      url="https://github.com/bridgecrewio/checkov",
      packages=setuptools.find_packages(exclude=["tests*","integration_tests*"]),
