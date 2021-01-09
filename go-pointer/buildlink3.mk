0a80957db960b122d7688cd9b2eacbd5c292d662
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2021-01-09 18:18:42 +0100

    go-pointer: lint Makefile use pkglint -F

--- go-pointer/buildlink3.mk
+++ go-pointer/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	go-pointer
 GO_POINTER_BUILDLINK3_MK:=
 
 BUILDLINK_CONTENTS_FILTER.go-pointer=	${EGREP} gopkg/
-BUILDLINK_DEPMETHOD.go-pointer?=		build
+BUILDLINK_DEPMETHOD.go-pointer?=	build
 
 BUILDLINK_API_DEPENDS.go-pointer+=	go-pointer>=20200722
 BUILDLINK_PKGSRCDIR.go-pointer?=	../../wip/go-pointer
