81d884fabe81bc8273a54a9ededccdc23325b981
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2021-01-13 15:40:33 +0100

    Update wip/go-backoff move to go-backoff-4.1.0nb13

--- go-backoff/buildlink3.mk
+++ go-backoff/buildlink3.mk
@@ -1,14 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/03/12 21:15:31 tnn2 Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	go-backoff
 
 .if !defined(GO_BACKOFF_BUILDLINK3_MK)
 GO_BACKOFF_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.go-backoff?=	build
-BUILDLINK_API_DEPENDS.go-backoff+=	go-backoff>=20141124
-BUILDLINK_PKGSRCDIR.go-backoff?=	../../wip/go-backoff
+BUILDLINK_CONTENTS_FILTER.go-backoff=	${EGREP} gopkg/
+BUILDLINK_DEPMETHOD.go-backoff?=		build
 
+BUILDLINK_API_DEPENDS.go-backoff+=	go-backoff>=4.1.0nb13
+BUILDLINK_PKGSRCDIR.go-backoff?=	../../wip/go-backoff
 .endif	# GO_BACKOFF_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-go-backoff
