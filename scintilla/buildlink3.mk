88d6a728f826e92f850d7ce6cc6d7f417e36fdcc
Silas <silas_nfm21@nocafe.net>
2021-01-14 19:20:53 -0300

    scintilla: bump scintilla version for buildlink3.mk
    Decision made after advice here:
    https://mail-index.netbsd.org/tech-pkg/2021/01/06/msg024494.html

--- scintilla/buildlink3.mk
+++ scintilla/buildlink3.mk
@@ -7,8 +7,8 @@ SCINTILLA_BUILDLINK3_MK:=
 
 BUILDLINK_DEPMETHOD.scintilla?=	build
 
-BUILDLINK_API_DEPENDS.scintilla+=	scintilla>=2.23
-BUILDLINK_ABI_DEPENDS.scintilla+=	scintilla>=2.29nb7
+BUILDLINK_API_DEPENDS.scintilla+=	scintilla>=4.4.6
+BUILDLINK_ABI_DEPENDS.scintilla+=	scintilla>=4.4.6
 BUILDLINK_PKGSRCDIR.scintilla?=		../../devel/scintilla
 
 #.include "../../x11/gtk2/buildlink3.mk"
