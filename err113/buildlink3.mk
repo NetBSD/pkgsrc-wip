0aed2d701cd92acebc1054e72036d23e7e39d1da
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2021-01-25 23:53:41 +0100

    Import err113-20210110 to wip

--- err113/buildlink3.mk
+++ err113/buildlink3.mk
@@ -1,19 +1,19 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	go-err113
+BUILDLINK_TREE+=	err113
 
-.if !defined(GO_ERR113_BUILDLINK3_MK)
-GO_ERR113_BUILDLINK3_MK:=
+.if !defined(ERR113_BUILDLINK3_MK)
+ERR113_BUILDLINK3_MK:=
 
-BUILDLINK_CONTENTS_FILTER.go-err113=	${EGREP} gopkg/
-BUILDLINK_DEPMETHOD.go-err113?=		build
+BUILDLINK_CONTENTS_FILTER.err113=	${EGREP} gopkg/
+BUILDLINK_DEPMETHOD.err113?=		build
 
-BUILDLINK_API_DEPENDS.go-err113+=	go-err113>=0.1.0
-BUILDLINK_PKGSRCDIR.go-err113?=		../../wip/go-err113
+BUILDLINK_API_DEPENDS.err113+=	err113>=20210110
+BUILDLINK_PKGSRCDIR.err113?=	../../wip/err113
 
 .include "../../devel/go-mod/buildlink3.mk"
 .include "../../devel/go-tools/buildlink3.mk"
 .include "../../devel/go-xerrors/buildlink3.mk"
-.endif	# GO_ERR113_BUILDLINK3_MK
+.endif	# ERR113_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-go-err113
+BUILDLINK_TREE+=	-err113
