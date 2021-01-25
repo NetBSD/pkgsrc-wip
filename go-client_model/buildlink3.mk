1a4f5826a20131c88990a508494555fd636a0d61
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2021-01-25 01:35:26 +0100

    go-client_model: set buildlink3.mk

--- go-client_model/buildlink3.mk
+++ go-client_model/buildlink3.mk
@@ -9,7 +9,7 @@ BUILDLINK_CONTENTS_FILTER.go-client_model=	${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-client_model?=		build
 
 BUILDLINK_API_DEPENDS.go-client_model+=	go-client_model>=0.2.0
-BUILDLINK_PKGSRCDIR.go-client_model?=	../../category/go-client_model
+BUILDLINK_PKGSRCDIR.go-client_model?=	../../wip/go-client_model
 
 .include "../../devel/go-protobuf/buildlink3.mk"
 .endif	# GO_CLIENT_MODEL_BUILDLINK3_MK
