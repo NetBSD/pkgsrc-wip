1ba4d557806ba434a9ced7e1ef2f38f075db98ab
Michael Baeuerle <micha@NetBSD.org>
2021-01-29 13:47:15 +0100

    pinentry: Fix references to wip and regenerate distinfo

--- pinentry/buildlink3.mk
+++ pinentry/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2018/01/07 13:04:30 rillig Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	pinentry
 
@@ -7,7 +7,7 @@ PINENTRY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.pinentry+=	pinentry>=0.7.1
 BUILDLINK_ABI_DEPENDS.pinentry+=	pinentry>=0.7.1nb3
-BUILDLINK_PKGSRCDIR.pinentry?=		../../security/pinentry
+BUILDLINK_PKGSRCDIR.pinentry?=		../../wip/pinentry
 .endif # PINENTRY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-pinentry
