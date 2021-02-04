5cc4cf8f886e53c3ea5d7067fe251a4ff3cf33ef
Mike M. Volokhov <mike.volokhov@gmail.com>
2021-02-04 23:00:03 +0200

    glm: update to version 0.9.9.8.
    For full list of the changes please see Release Notes section
    in the readme.md file.
    
    pkgsrc changes: glm abandoned installation scripts - return to manual
    installation as defined in the graphics/glm.

---
+++ glm/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.3 2015/08/09 11:17:00 wiz Exp $
+
+BUILDLINK_TREE+=	glm
+
+.if !defined(GLM_BUILDLINK3_MK)
+GLM_BUILDLINK3_MK:=
+
+BUILDLINK_DEPMETHOD.glm?=	build
+
+BUILDLINK_API_DEPENDS.glm+=	glm>=0.9.9
+BUILDLINK_PKGSRCDIR.glm?=	../../wip/glm
+.endif	# GLM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-glm
