390fe0c55c2228ce6f92da801f172942845a2d55
Frédéric Fauberteau <triaxx@NetBSD.org>
2021-02-12 11:44:50 +0100

    libXaw3dXft: Fix dependencies

--- libXaw3dXft/buildlink3.mk
+++ libXaw3dXft/buildlink3.mk
@@ -12,6 +12,8 @@ BUILDLINK_PKGSRCDIR.libXaw3dXft?=	../../wip/libXaw3dXft
 .include "../../x11/libXext/buildlink3.mk"
 .include "../../x11/libXft/buildlink3.mk"
 .include "../../x11/libXmu/buildlink3.mk"
+.include "../../x11/libXpm/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
 .endif	# LIBXAW3DXFT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libXaw3dXft
