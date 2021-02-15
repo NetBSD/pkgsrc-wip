40d5a9bd3b177047733947eee82da99d591c64f8
Frederic Cambus <fred@statdns.com>
2021-02-15 09:35:04 +0100

    trader: update to 7.16.
    Version 7.16 (19th January, 2021)
    ---------------------------------
    
    Added code to allow the ``TEXTDOMAINDIR`` environment variable to specify
    where compiled translations are located.  Updated the documentation to
    mention this new feature.
    
    Added an application metadata file that conforms to the AppStream 0.12
    specification.  Updated all translations to suit, using parts of existing
    strings.
    
    Updated the German translation, with thanks to Mario Blättermann.

--- trader/patches/patch-src_utils.c
+++ trader/patches/patch-src_utils.c
@@ -4,11 +4,11 @@ Do not use err_exit() to generate error messages when locale initialization
 fails as in this case curs_set() is called before initscr(), which causes
 trader to crash when built with NetBSD curses.
 
---- src/trader.c.orig	2021-01-12 23:39:07.351997639 +0000
-+++ src/trader.c
-@@ -151,8 +151,9 @@ int main (int argc, char *argv[])
+--- src/utils.c.orig	2021-01-19 05:42:38.000000000 +0000
++++ src/utils.c
+@@ -591,8 +591,9 @@ extern void init_locale (void)
  
-     // Initialise the locale
+     // Initialise the current locale
      if (setlocale(LC_ALL, "") == NULL) {
 -	err_exit("could not set locale "
 -		 "(check LANG, LC_ALL and LANGUAGE in environment)");
