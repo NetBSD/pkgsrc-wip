ee3563a8a61c29804914012297355b896dd71836
Frederic Cambus <fred@statdns.com>
2021-02-17 18:22:31 +0100

    trader: avoid using strfmon(3) on NetBSD, use fallback code.
    There are formatting issues when LANG is set to anything else than C.

--- trader/patches/patch-src_utils.c
+++ trader/patches/patch-src_utils.c
@@ -1,8 +1,11 @@
 $NetBSD$
 
-Do not use err_exit() to generate error messages when locale initialization
-fails as in this case curs_set() is called before initscr(), which causes
-trader to crash when built with NetBSD curses.
+- Do not use err_exit() to generate error messages when locale initialization
+  fails as in this case curs_set() is called before initscr(), which causes
+  trader to crash when built with NetBSD curses.
+
+- Avoid using strfmon(3) on NetBSD, there are formatting issues when LANG is
+  set to anything else than C.
 
 --- src/utils.c.orig	2021-01-19 05:42:38.000000000 +0000
 +++ src/utils.c
@@ -18,3 +21,15 @@ trader to crash when built with NetBSD curses.
      }
  
      // Use correct message catalogs for the locale
+@@ -682,7 +683,11 @@ ssize_t xwcsfmon (wchar_t *restrict buf,
+        produce "12345" instead of something like "-$123.45"!  The
+        following code overcomes these limitations by using snprintf(). */
+ 
++#if defined(__NetBSD__)
++    if (0) {
++#else
+     if (! is_posix_locale) {
++#endif
+ 	n = strfmon(s, BUFSIZE, format, val);
+     } else {
+ 	/* The current implementation assumes the monetary decimal point
