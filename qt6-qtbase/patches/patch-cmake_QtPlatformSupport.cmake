e050bf1a5df49ee12238e98d1544a3b5c7ee81fb
Niclas Rosenvik <nros@NetBSD.org>
2021-02-19 14:21:01 +0000

    qt6-qtbase: proper -fp-model when gcc fix
    add patch that fixes the problem that the -fp-model switch gets added
    when gcc is used. The line that sets ICC checks for icl or icc in
    CMAKE_C_COMPILER and it can include the whole path to the compiler.
    this causes ICC to be set if the pkgsrc directory is in a path that
    has a directory with a name that includes icc or icl.

--- qt6-qtbase/patches/patch-cmake_QtPlatformSupport.cmake
+++ qt6-qtbase/patches/patch-cmake_QtPlatformSupport.cmake
@@ -2,7 +2,12 @@ $NetBSD$
 
 * add a common system type for all platforms that support X11
 
---- cmake/QtPlatformSupport.cmake.orig	2020-12-04 10:14:27.000000000 +0000
+* Fix ICC check, since CMAKE_C_COMPILER can include the whole path to the
+  compiler icc|icl matches directory names that include icl or icc
+
+* Fix QCC check for the same reason as the ICC check
+
+--- cmake/QtPlatformSupport.cmake.orig	2021-01-26 18:29:22.000000000 +0000
 +++ cmake/QtPlatformSupport.cmake
 @@ -20,6 +20,8 @@ qt_set01(WASM CMAKE_SYSTEM_NAME STREQUAL
  
@@ -13,3 +18,14 @@ $NetBSD$
  qt_set01(WINRT WIN32 AND CMAKE_VS_PLATFORM_TOOSLET STREQUAL "winrt") # FIXME: How to identify this?
  
  qt_set01(IOS APPLE AND CMAKE_SYSTEM_NAME STREQUAL "iOS")
+@@ -31,8 +33,8 @@ qt_set01(MACOS APPLE AND NOT UIKIT)
+ qt_set01(GCC CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+ qt_set01(CLANG CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+ qt_set01(APPLECLANG CMAKE_CXX_COMPILER_ID MATCHES "AppleClang")
+-qt_set01(ICC CMAKE_C_COMPILER MATCHES "icc|icl")
+-qt_set01(QCC CMAKE_C_COMPILER MATCHES "qcc") # FIXME: How to identify this?
++qt_set01(ICC CMAKE_CXX_COMPILER_ID MATCHES "Intel")
++qt_set01(QCC CMAKE_CXX_COMPILER_ID STREQUAL "QCC")
+ 
+ if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+     set(QT_64BIT TRUE)
