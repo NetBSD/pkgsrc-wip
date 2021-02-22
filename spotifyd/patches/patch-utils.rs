124e53acca32516c60a12b86fe7524b8c574b9b8
atomicules <base@atomicules.co.uk>
2021-02-22 20:31:26 +0000

    Update spotifyd to 0.3.0
    Important, you'll like hit this [upstream issue][1] and see something
    like:
    
    ```
    thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value:
    Error { inner: ErrorInner { kind: Custom, line: Some(64), col: 17, at:
    Some(1937), message: "invalid type: integer `90`, expected a string",
    key: ["global", "initial_volume"] } }', src/config.rs:468:67
    note: run with `RUST_BACKTRACE=1` environment variable to display a
    backtrace
    ```
    
    This is due to the config format changing and values needing quoting.
    
    Adds a new patch [based on this PR][2].
    
    [1]: https://github.com/Spotifyd/spotifyd/issues/765
    [2]: https://github.com/Spotifyd/spotifyd/pull/796

---
+++ spotifyd/patches/patch-utils.rs
@@ -0,0 +1,15 @@
+$NetBSD$
+
+See: https://github.com/Spotifyd/spotifyd/pull/796
+
+--- src/utils.rs.orig	2021-01-14 14:52:28.000000000 +0000
++++ src/utils.rs
+@@ -3,7 +3,7 @@ use std::env;
+ #[cfg(target_os = "macos")]
+ use whoami;
+ 
+-#[cfg(target_os = "linux")]
++#[cfg(any(target_os = "netbsd", target_os = "linux"))]
+ fn get_shell_ffi() -> Option<String> {
+     use libc::{geteuid, getpwuid_r};
+     use std::{ffi::CStr, mem, ptr};
