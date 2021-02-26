b8da0436ad3ddfb8b1ef3c78e634434477ccf04c
Mark Davies <mark@ecs.vuw.ac.nz>
2021-02-27 10:41:12 +1300

    akonadi-contacts: fix buildlink3.mk

--- akonadi-contacts/buildlink3.mk
+++ akonadi-contacts/buildlink3.mk
@@ -9,8 +9,8 @@ BUILDLINK_API_DEPENDS.akonadi-contacts+=	akonadi-contacts>=17.12.1
 BUILDLINK_PKGSRCDIR.akonadi-contacts?=		../../wip/akonadi-contacts
 
 .include "../../wip/akonadi-mime/buildlink3.mk"
-.include "../../wip/kcontacts/buildlink3.mk"
-.include "../../wip/kmime/buildlink3.mk"
+.include "../../misc/kcontacts/buildlink3.mk"
+.include "../../mail/kmime/buildlink3.mk"
 .include "../../graphics/prison/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# AKONADI_CONTACTS_BUILDLINK3_MK
