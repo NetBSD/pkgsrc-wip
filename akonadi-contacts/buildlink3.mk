15cfbeabf1d15eb6456c9f893453bf600e9bb0b0
Mark Davies <mark@ecs.vuw.ac.nz>
2021-02-27 10:37:03 +1300

    akonadi-contacts: update to 20.04.1

--- akonadi-contacts/buildlink3.mk
+++ akonadi-contacts/buildlink3.mk
@@ -6,12 +6,11 @@ BUILDLINK_TREE+=	akonadi-contacts
 AKONADI_CONTACTS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.akonadi-contacts+=	akonadi-contacts>=17.12.1
-BUILDLINK_ABI_DEPENDS.akonadi-contacts?=		akonadi-contacts>=20.04.1nb1
 BUILDLINK_PKGSRCDIR.akonadi-contacts?=		../../wip/akonadi-contacts
 
 .include "../../wip/akonadi-mime/buildlink3.mk"
-.include "../../misc/kcontacts/buildlink3.mk"
-.include "../../mail/kmime/buildlink3.mk"
+.include "../../wip/kcontacts/buildlink3.mk"
+.include "../../wip/kmime/buildlink3.mk"
 .include "../../graphics/prison/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# AKONADI_CONTACTS_BUILDLINK3_MK
