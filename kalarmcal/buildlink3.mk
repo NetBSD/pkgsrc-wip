a5484c554efe86acb4cdefcc57b8d9c73414f87d
Mark Davies <mark@ecs.vuw.ac.nz>
2021-02-27 23:13:04 +1300

    kalarmcal: update to 20.04.1

--- kalarmcal/buildlink3.mk
+++ kalarmcal/buildlink3.mk
@@ -6,11 +6,10 @@ BUILDLINK_TREE+=	kalarmcal
 KALARMCAL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.kalarmcal+=	kalarmcal>=17.12.1
-BUILDLINK_ABI_DEPENDS.kalarmcal?=		kalarmcal>=20.04.1nb1
 BUILDLINK_PKGSRCDIR.kalarmcal?=		../../wip/kalarmcal
 
 .include "../../wip/akonadi/buildlink3.mk"
-.include "../../time/kcalcore/buildlink3.mk"
+.include "../../time/kcalutils/buildlink3.mk"
 .include "../../time/kholidays/buildlink3.mk"
 .include "../../misc/kidentitymanagement/buildlink3.mk"
 .include "../../x11/kdelibs4support/buildlink3.mk"
