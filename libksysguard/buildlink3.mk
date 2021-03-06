bb65a44205cc31b78e90da1211d5ff8b196dbc26
Mark Davies <mark@ecs.vuw.ac.nz>
2021-03-06 17:05:17 +1300

    libksysguard: qtwebkit should not be in buildlink3.mk

--- libksysguard/buildlink3.mk
+++ libksysguard/buildlink3.mk
@@ -6,13 +6,12 @@ BUILDLINK_TREE+=	libksysguard
 LIBKSYSGUARD_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libksysguard+=	libksysguard>=5.6.3
-BUILDLINK_ABI_DEPENDS.libksysguard?=	libksysguard>=5.18.5nb1
 BUILDLINK_PKGSRCDIR.libksysguard?=	../../wip/libksysguard
 
 .include "../../x11/plasma-framework/buildlink3.mk"
 .include "../../x11/libXres/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
-.include "../../x11/qt5-qtwebkit/buildlink3.mk"
+#.include "../../x11/qt5-qtwebengine/buildlink3.mk"
 .endif	# LIBKSYSGUARD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libksysguard
