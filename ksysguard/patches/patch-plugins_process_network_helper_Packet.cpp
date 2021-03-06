1db17e45d6a145e76808a9289284c96329dd9625
Mark Davies <mark@ecs.vuw.ac.nz>
2021-03-06 17:11:53 +1300

    ksysguard: remove unneeded dependencies

--- ksysguard/patches/patch-plugins_process_network_helper_Packet.cpp
+++ ksysguard/patches/patch-plugins_process_network_helper_Packet.cpp
@@ -1,13 +1,18 @@
 $NetBSD$
 
+BSD's use if_ether.h
+
 --- plugins/process/network/helper/Packet.cpp.orig	2020-05-05 14:44:00.000000000 +0000
 +++ plugins/process/network/helper/Packet.cpp
-@@ -23,7 +23,7 @@
+@@ -23,7 +23,11 @@
  
  #include <sys/types.h>
  #include <arpa/inet.h>
--#include <net/ethernet.h>
++#ifdef __linux__
+ #include <net/ethernet.h>
++#else
 +#include <net/if_ether.h>
++#endif
  #include <netinet/in.h>
  #include <netinet/ip.h>
  #include <netinet/ip6.h>
