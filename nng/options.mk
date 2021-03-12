342a2e1fe9669c44bca74161812f5e20c6dceeca
Pierre Pronchery <khorben@defora.org>
2021-03-12 03:05:27 +0100

    nng: build shared libraries by default

--- nng/options.mk
+++ nng/options.mk
@@ -2,12 +2,12 @@
 
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nng
-PKG_SUPPORTED_OPTIONS=	doc
+PKG_SUPPORTED_OPTIONS=	doc static
 PKG_SUGGESTED_OPTIONS+=	doc
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		doc
+PLIST_VARS+=		doc shared static
 
 .if !empty(PKG_OPTIONS:Mdoc)
 .include "../../lang/ruby/rubyversion.mk"
@@ -15,4 +15,11 @@ BUILD_DEPENDS+=		${RUBY_PKGPREFIX}-asciidoctor-[0-9]*:../../textproc/ruby-asciid
 CMAKE_ARGS+=		-DNNG_ENABLE_DOC=1
 CMAKE_ARGS+=		-DASCIIDOCTOR=${PREFIX}/bin/asciidoctor${RUBY_VER}
 PLIST.doc=		yes
+.endif
+
+.if !empty(PKG_OPTIONS:Mstatic)
+PLIST.static=		yes
+.else
+CMAKE_ARGS+=		-DBUILD_SHARED_LIBS:BOOL=ON
+PLIST.shared=		yes
 .endif
