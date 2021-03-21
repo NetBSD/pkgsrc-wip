864dfbd75792a5d837fb338d62f460c40e996d57
Jason Bacon <bacon@NetBSD.org>
2021-03-21 08:45:47 -0500

    biolibc: Clean up bl3

--- biolibc/buildlink3.mk
+++ biolibc/buildlink3.mk
@@ -1,14 +1,11 @@
 # $NetBSD$
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
 
 BUILDLINK_TREE+=	biolibc
 
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.1.0.33
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.1.1
 BUILDLINK_PKGSRCDIR.biolibc?=	../../wip/biolibc
 BUILDLINK_DEPMETHOD.biolibc?=	build
 .endif	# BIOLIBC_BUILDLINK3_MK
