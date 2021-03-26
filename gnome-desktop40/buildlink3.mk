aa9cf09aa6ca94d211ae65a3370cb37ecb33be97
Dan CÃirnat <cirnatdan@NetBSD.org>
2021-03-26 21:23:14 +0100

    mutter: Add XCURSOR_PATH in xinit scripts. Fix build

--- gnome-desktop40/buildlink3.mk
+++ gnome-desktop40/buildlink3.mk
@@ -2,12 +2,12 @@
 
 BUILDLINK_TREE+=	gnome-desktop40
 
-.if !defined(GNOME_DESKTOP3_BUILDLINK3_MK)
-GNOME_DESKTOP3_BUILDLINK3_MK:=
+.if !defined(GNOME_DESKTOP40_BUILDLINK3_MK)
+GNOME_DESKTOP40_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnome-desktop3+=	gnome-desktop40>=3.4.2
-BUILDLINK_ABI_DEPENDS.gnome-desktop3+=	gnome-desktop40>=3.36
-BUILDLINK_PKGSRCDIR.gnome-desktop3?=	../../x11/gnome-desktop40
+BUILDLINK_API_DEPENDS.gnome-desktop40+=	gnome-desktop40>=3.4.2
+BUILDLINK_ABI_DEPENDS.gnome-desktop40+=	gnome-desktop40>=3.36
+BUILDLINK_PKGSRCDIR.gnome-desktop40?=	../../wip/gnome-desktop40
 
 BUILDLINK_API_DEPENDS.gsettings-desktop-schemas+=	gsettings-desktop-schemas>=3.27.0
 .include "../../wip/gsettings-desktop-schemas/buildlink3.mk"
