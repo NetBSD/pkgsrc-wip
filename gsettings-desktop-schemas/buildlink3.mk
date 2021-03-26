1d50848d240e64bf5c9243c60be3e5215292b438
Dan CÃirnat <cirnatdan@NetBSD.org>
2021-03-26 11:22:06 +0100

    gsettings-desktop-schemas: Import gsettings-desktop-schemas-40.0 as wip/gsettings-desktop-schemas
    Needed for GNOME 40

---
+++ gsettings-desktop-schemas/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.2 2018/01/07 13:04:31 rillig Exp $
+
+# Just for the pkg_config .pc file
+BUILDLINK_TREE+=	gsettings-desktop-schemas
+
+.if !defined(GSETTINGS_DESKTOP_SCHEMAS_BUILDLINK3_MK)
+GSETTINGS_DESKTOP_SCHEMAS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gsettings-desktop-schemas+=	gsettings-desktop-schemas>=40.0
+BUILDLINK_PKGSRCDIR.gsettings-desktop-schemas?=		../../sysutils/gsettings-desktop-schemas
+
+.endif # GSETTINGS_DESKTOP_SCHEMAS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gsettings-desktop-schemas
