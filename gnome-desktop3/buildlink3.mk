fca0ba311703a574e43743c2909373ec1dd7f183
Dan CÃirnat <cirnatdan@NetBSD.org>
2021-04-06 11:11:13 +0200

    gnome-desktop: Rename wip/gnome-desktop40 to wip/gnome-desktop3
    There's not a huge change from the 3.x release to 40.0

--- gnome-desktop3/buildlink3.mk
+++ gnome-desktop3/buildlink3.mk
@@ -1,13 +1,13 @@
 # $NetBSD: buildlink3.mk,v 1.33 2020/11/04 12:12:48 nia Exp $
 
-BUILDLINK_TREE+=	gnome-desktop40
+BUILDLINK_TREE+=	gnome-desktop3
 
-.if !defined(GNOME_DESKTOP40_BUILDLINK3_MK)
-GNOME_DESKTOP40_BUILDLINK3_MK:=
+.if !defined(GNOME_DESKTOP3_BUILDLINK3_MK)
+GNOME_DESKTOP3_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnome-desktop40+=	gnome-desktop40>=3.4.2
-BUILDLINK_ABI_DEPENDS.gnome-desktop40+=	gnome-desktop40>=3.36
-BUILDLINK_PKGSRCDIR.gnome-desktop40?=	../../devel/gnome-desktop40
+BUILDLINK_API_DEPENDS.gnome-desktop3+=	gnome-desktop3>=3.4.2
+BUILDLINK_ABI_DEPENDS.gnome-desktop3+=	gnome-desktop3>=3.36
+BUILDLINK_PKGSRCDIR.gnome-desktop3?=	../../wip/gnome-desktop33
 
 BUILDLINK_API_DEPENDS.gsettings-desktop-schemas+=	gsettings-desktop-schemas>=3.27.0
 .include "../../wip/gsettings-desktop-schemas/buildlink3.mk"
@@ -16,4 +16,4 @@ BUILDLINK_API_DEPENDS.gsettings-desktop-schemas+=	gsettings-desktop-schemas>=3.2
 .include "../../textproc/iso-codes/buildlink3.mk"
 .endif
 
-BUILDLINK_TREE+=	-gnome-desktop40
+BUILDLINK_TREE+=	-gnome-desktop3
