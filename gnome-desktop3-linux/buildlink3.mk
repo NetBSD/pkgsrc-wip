ef93a7c3a6517480ec5dbf3a788d51969040d081
Dan Cîrnaț <cirnatdan@pkgsrc.org>
2021-04-19 21:39:01 +0200

    gnome-desktop: Import gnome-desktop-40.0 as wip/gnome-desktop3-linux
    This WIP package is a demonstration of using the libseccomp builtin,
    fixing the build of gnome-desktop on Linux

---
+++ gnome-desktop3-linux/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.33 2020/11/04 12:12:48 nia Exp $
+
+BUILDLINK_TREE+=	gnome-desktop3
+
+.if !defined(GNOME_DESKTOP3_BUILDLINK3_MK)
+GNOME_DESKTOP3_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gnome-desktop3+=	gnome-desktop3>=3.4.2
+BUILDLINK_ABI_DEPENDS.gnome-desktop3+=	gnome-desktop3>=3.36
+BUILDLINK_PKGSRCDIR.gnome-desktop3?=	../../wip/gnome-desktop3-linux
+
+.include "../../wip/mk/libseccomp.buildlink3.mk"
+BUILDLINK_API_DEPENDS.gsettings-desktop-schemas+=	gsettings-desktop-schemas>=3.27.0
+.include "../../sysutils/gsettings-desktop-schemas/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.include "../../x11/xkeyboard-config/buildlink3.mk"
+.include "../../textproc/iso-codes/buildlink3.mk"
+.endif
+
+BUILDLINK_TREE+=	-gnome-desktop3
