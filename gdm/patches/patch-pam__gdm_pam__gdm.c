e791378cd14d270061d49d231e2d5e6841642612
Dan Cîrnaț <cirnatdan@pkgsrc.org>
2021-04-21 12:53:12 +0200

    gdm: Make pam patch rely on OPENPAM rather than the __linux__ constant

--- gdm/patches/patch-pam__gdm_pam__gdm.c
+++ gdm/patches/patch-pam__gdm_pam__gdm.c
@@ -2,20 +2,20 @@ $NetBSD$
 
 Include only supported pam headers
 
---- pam_gdm/pam_gdm.c.orig	2020-12-15 18:16:43.000000000 +0000
+--- pam_gdm/pam_gdm.c.orig	2021-03-30 13:25:30.000000000 +0000
 +++ pam_gdm/pam_gdm.c
 @@ -21,11 +21,13 @@
  
  #include <unistd.h>
  
-+#ifdef __linux__
++#include <security/pam_modules.h>
++#ifndef OPENPAM
  #include <security/_pam_macros.h>
  #include <security/pam_ext.h>
  #include <security/pam_misc.h>
 -#include <security/pam_modules.h>
  #include <security/pam_modutil.h>
 +#endif
-+#include <security/pam_modules.h>
  
  #ifdef HAVE_KEYUTILS
  #include <keyutils.h>
