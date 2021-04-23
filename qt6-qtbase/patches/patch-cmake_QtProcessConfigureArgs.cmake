e10ac32568b54e5fd3074e3fc0b98c41c0599863
Niclas Rosenvik <nros@netbsd.org>
2021-04-23 15:46:30 +0000

    qt6-qtbase: add configure option to avoid cmake archiving api
    add configure options to avoid cmake archiving api
    it needs to be avoided since kibarchive in pkgsrc is built without
    zstd support

--- qt6-qtbase/patches/patch-cmake_QtProcessConfigureArgs.cmake
+++ qt6-qtbase/patches/patch-cmake_QtProcessConfigureArgs.cmake
@@ -3,7 +3,10 @@ $NetBSD$
 * add function so that libraries such as socket and nsl 
   can be added to tests
 
---- cmake/QtProcessConfigureArgs.cmake.orig	2021-01-26 18:29:22.000000000 +0000
+* add support for avoiding the cmake archiving api using configure since
+  libarchive in pkgsrc that cmake uses is built without zstd support
+
+--- cmake/QtProcessConfigureArgs.cmake.orig	2021-02-11 10:23:05.000000000 +0000
 +++ cmake/QtProcessConfigureArgs.cmake
 @@ -133,7 +133,7 @@ defstub(qt_feature_definition)
  defstub(qt_find_package)
@@ -14,3 +17,11 @@ $NetBSD$
  
  ####################################################################################################
  # Define functions/macros that are called in qt_cmdline.cmake files
+@@ -633,6 +633,7 @@ translate_boolean_input(precompile_heade
+ translate_boolean_input(ccache QT_USE_CCACHE)
+ translate_boolean_input(shared BUILD_SHARED_LIBS)
+ translate_boolean_input(warnings_are_errors WARNINGS_ARE_ERRORS)
++translate_boolean_input(avoid_cmake_archiving_api QT_AVOID_CMAKE_ARCHIVING_API)
+ translate_string_input(qt_namespace QT_NAMESPACE)
+ translate_string_input(qt_libinfix QT_LIBINFIX)
+ translate_string_input(qreal QT_COORD_TYPE)
