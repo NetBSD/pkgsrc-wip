aee5a01868340470a246e6463e4803654e572e33
Yorick Hardy <yorickhardy@gmail.com>
2021-05-01 12:14:28 +0200

    commandergenius: update to Commander-Genius-2.8.0
    Changelog:
    
    v2.8.0:
    -------
    - Remove legacy SDL 1.2 code at many parts
    - More stable graphics engine
    - SDL2 is not an option anymore
    - Improved logging
    - Open GL 2.1 is minimum now
    - Lots of OpenGL fixes
    - Boost library now is optional
    - C++17
    
    v2.7.9:
    -------
    - Added support for Flatpak
    
    v2.7.8:
    -------
    - More logging. Code cleanups

---
+++ commandergenius/patches/patch-GsKit_base_utils_property__tree_tinyxml2.cpp
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Use fseeko and ftello on NetBSD as well,
+
+--- GsKit/base/utils/property_tree/tinyxml2.cpp.orig	2021-04-11 19:10:59.000000000 +0000
++++ GsKit/base/utils/property_tree/tinyxml2.cpp
+@@ -103,7 +103,7 @@ distribution.
+ #if defined(_WIN64)
+ 	#define TIXML_FSEEK _fseeki64
+ 	#define TIXML_FTELL _ftelli64
+-#elif defined(__APPLE__) || (__FreeBSD__)
++#elif defined(__APPLE__) || (__FreeBSD__) || defined(__NetBSD__)
+ 	#define TIXML_FSEEK fseeko
+ 	#define TIXML_FTELL ftello
+ #elif defined(ANDROID)
