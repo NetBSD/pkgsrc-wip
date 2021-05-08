e89a5a5f59973d0c0c70da2801d972be711566eb
Jaromir Dolecek <jdolecek@NetBSD.org>
2021-05-08 19:37:10 +0200

    switch to mysql-client for the buildlink instead of mariadb-client
    this is required so it works for packages which use BUILDLINK_*.mysql-client,
    like for example databases/php-mysqli

--- mariadb104-client/buildlink3.mk
+++ mariadb104-client/buildlink3.mk
@@ -1,15 +1,19 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	mariadb-client
+# This buildlink file uses mysql-client and not mariadb-client,
+# because some packages depending on it expect BUILDLINK_*.mysql-client
+# to be set and will fail build if they get BUILDLINK_*.mariadb-client
+# instead.
+BUILDLINK_TREE+=	mysql-client
 
 .if !defined(MARIADB_CLIENT_BUILDLINK3_MK)
 MARIADB_CLIENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mariadb-client+=	mariadb-client>=10.4.0
-BUILDLINK_ABI_DEPENDS.mariadb-client+=	mariadb-client>=10.4.0
-BUILDLINK_PKGSRCDIR.mariadb-client?=	../../wip/mariadb104-client
-BUILDLINK_INCDIRS.mariadb-client+=	include/mysql
-BUILDLINK_LIBDIRS.mariadb-client+=	lib
+BUILDLINK_API_DEPENDS.mysql-client+=	mariadb-client>=10.4.0
+BUILDLINK_ABI_DEPENDS.mysql-client+=	mariadb-client>=10.4.0
+BUILDLINK_PKGSRCDIR.mysql-client?=	../../wip/mariadb104-client
+BUILDLINK_INCDIRS.mysql-client+=	include/mysql
+BUILDLINK_LIBDIRS.mysql-client+=	lib
 
 pkgbase := mariadb-client
 .include "../../mk/pkg-build-options.mk"
@@ -20,4 +24,4 @@ pkgbase := mariadb-client
 .include "../../devel/zlib/buildlink3.mk"
 .endif # MARIADB_CLIENT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-mariadb-client
+BUILDLINK_TREE+=	-mysql-client
