231056c473762e5ed1c64d4e6fe156a7e766637b
Jason Bacon <bacon@NetBSD.org>
2021-05-10 10:31:48 -0500

    libxtend: Switch bl3 to shared lib

--- libxtend/buildlink3.mk
+++ libxtend/buildlink3.mk
@@ -5,9 +5,7 @@ BUILDLINK_TREE+=		libxtend
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.libxtend?=	build
-
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.0.1.13
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.0.1.20
 BUILDLINK_PKGSRCDIR.libxtend?=		../../wip/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
