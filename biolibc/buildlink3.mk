307f646b131b09e7565fd1e5749c231209041364
Jason Bacon <bacon@NetBSD.org>
2021-05-18 10:08:35 -0500

    biolibc: Update bl3 for shared lib

--- biolibc/buildlink3.mk
+++ biolibc/buildlink3.mk
@@ -5,9 +5,8 @@ BUILDLINK_TREE+=	biolibc
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.1.1
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.1.1.35
 BUILDLINK_PKGSRCDIR.biolibc?=	../../wip/biolibc
-BUILDLINK_DEPMETHOD.biolibc?=	build
 .endif	# BIOLIBC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-biolibc
