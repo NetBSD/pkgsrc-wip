2e6325c95939868a01a917ba9631a72e77497044
= <triaxx@NetBSD.org>
2021-05-19 12:47:27 +0200

    xpaint: Update to 3.1.3
    pkgsrc changes:
    ---------------
      * Add patch to fix build on FreeBSD by using HAVE_DIRENT_H
    
    upstream changes:
    -----------------
    Version 3.1.3
      * graphic.c : the "Text" menu has been substantially improved.
        Text and TeX/LaTeX input of arbitrary length, size, color can
        now be edited and converted to either a bitmap image or
        vector PostScript/PDF with embedded fonts.

---
+++ xpaint/patches/patch-fileBrowser.c
@@ -0,0 +1,22 @@
+$NetBSD$
+
+--- fileBrowser.c.orig	2021-01-21 22:38:23.000000000 +0100
++++ fileBrowser.c	2021-05-19 10:27:34.463903000 +0200
+@@ -48,7 +48,7 @@
+ #include <stdio.h>
+ #include <sys/types.h>
+ #include <sys/stat.h>
+-#if defined(SYSV) || defined(SVR4) || defined(__CYGWIN__) || defined(__VMS )
++#if HAVE_DIRENT_H
+ #include <dirent.h>
+ #else
+ #include <sys/dir.h>
+@@ -563,7 +563,7 @@
+ setCWD(arg_t * arg, char *dir)
+ {
+     DIR *dirp;
+-#if defined(SYSV) || defined(SVR4) || defined(__alpha) || defined(__CYGWIN__)
++#if HAVE_DIRENT_H
+     struct dirent *e;
+ #else
+     struct direct *e;
