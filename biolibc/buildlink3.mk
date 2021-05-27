15004774a8b9d162b0a340cdc745d4c710a2836f
Jason Bacon <bacon@NetBSD.org>
2021-05-27 16:09:21 -0500

    biolibc: Update to 0.1.2.10
    vcfio.h: include stdbool.h

--- biolibc/buildlink3.mk
+++ biolibc/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	biolibc
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.1.2.9
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.1.2.10
 BUILDLINK_PKGSRCDIR.biolibc?=	../../wip/biolibc
 .endif	# BIOLIBC_BUILDLINK3_MK
 
