3469d7fc35b6eb4f1fccf0ff4992c6b5af8faee7
Mark Davies <mark@ecs.vuw.ac.nz>
2021-06-17 22:11:50 +1200

    akonadi: add additional files to buildlink

--- akonadi/buildlink3.mk
+++ akonadi/buildlink3.mk
@@ -8,6 +8,9 @@ AKONADI_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.akonadi+=	akonadi>=17.12.1
 BUILDLINK_PKGSRCDIR.akonadi?=	../../wip/akonadi
 
+BUILDLINK_FILES.akonadi+=	share/dbus-1/interfaces/org.freedesktop.Akonadi.*.xml
+BUILDLINK_FILES.akonadi+=	share/kf5/akonadi/kcfg2dbus.xsl
+
 .include "../../devel/kio/buildlink3.mk"
 .include "../../devel/kitemmodels/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
