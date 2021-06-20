887e64177afc51d17b2e69dcde2a170cbc3cddd8
Brook Milligan <brook@nmsu.edu>
2021-06-20 15:13:33 -0600

    xmatchview: fix test/runXMV.sh to run xmatchview directly
    Originally, test/runXMV.sh was used to run a test case.  That is
    now done with a Makefile.  It is preferable for test/runXMV.sh to
    work correctly once installed, rather than for the test, so fix
    the path to xmatchview to run the installed program.

--- xmatchview/patches/patch-test_runXMV.sh
+++ xmatchview/patches/patch-test_runXMV.sh
@@ -9,5 +9,5 @@ Run script directly so python version is correct.
  
  # source PATH-TO-SOURCE (IF NEEDED)
 -python3 ../xmatchview.py -x $1 -s $3 -q $2 -a $4 -m $5 -b 10 -r 1 -c $6 -f png -y $7 -e $8 -p ../../tarballs/fonts
-+../xmatchview.py -x $1 -s $3 -q $2 -a $4 -m $5 -b 10 -r 1 -c $6 -f png -y $7 -e $8 -p ../../tarballs/fonts
++xmatchview.py -x $1 -s $3 -q $2 -a $4 -m $5 -b 10 -r 1 -c $6 -f png -y $7 -e $8 -p ../../tarballs/fonts
  
