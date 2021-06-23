43ab34541c44e1312796c3539dfa6024d3a51103
Tobias Nygren <tnn@NetBSD.org>
2021-06-24 00:11:00 +0200

    varnish: include libvarnishapi dependencies in bl3

--- varnish/buildlink3.mk
+++ varnish/buildlink3.mk
@@ -8,6 +8,9 @@ VARNISH_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.varnish+=	varnish>=6.6.0
 BUILDLINK_PKGSRCDIR.varnish?=	../../wip/varnish
 
+.include "../../devel/libexecinfo/buildlink3.mk"
+.include "../../devel/pcre/buildlink3.mk"
+
 .endif	# VARNISH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-varnish
