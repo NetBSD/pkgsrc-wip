0fb343e83694951d0fe247b0929f6347221f611e
Michael Baeuerle <micha@NetBSD.org>
2021-06-28 10:08:23 +0200

    fltk14-devel: Fix bl3 file

--- fltk14-devel/buildlink3.mk
+++ fltk14-devel/buildlink3.mk
@@ -57,7 +57,7 @@ pkgbase := fltk
 .  endif
 .endif
 
-BUILDLINK_API_DEPENDS.zlib+=<-->zlib>=1.2.3
+BUILDLINK_API_DEPENDS.zlib+=	zlib>=1.2.3
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
