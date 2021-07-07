061a146411e42b28a4bd9aa8b5a65bf06b074363
Jason Bacon <bacon@NetBSD.org>
2021-07-07 15:10:14 -0500

    biolibc: Update to 0.1.3.10
    Document dsv functions
    Factor out biostring.h

--- biolibc/buildlink3.mk
+++ biolibc/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	biolibc
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.1.3.8
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.1.3.10
 BUILDLINK_PKGSRCDIR.biolibc?=	../../wip/biolibc
 .endif	# BIOLIBC_BUILDLINK3_MK
 
