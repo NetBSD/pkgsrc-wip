73b6ef6b68ea910d08ee7cb5e25ed961e9beda43
Leonardo Taccari <leot@NetBSD.org>
2021-08-11 02:05:36 +0200

    semgrep: Update to 0.60.0
    Changes:
    Probably quite a lot to mention, please give a look to:
    
     <https://github.com/returntocorp/semgrep/releases>
    
    ...given that we are on pkgsrc-wip!
    
    (And that finally we also have semgrep-core!)

--- semgrep/patches/patch-setup.py
+++ semgrep/patches/patch-setup.py
@@ -2,22 +2,21 @@ $NetBSD$
 
 Avoid too strict version requirements.
 
---- setup.py.orig	2020-12-16 21:19:50.000000000 +0000
+--- setup.py.orig	2021-07-27 18:59:00.000000000 +0000
 +++ setup.py
-@@ -144,13 +144,13 @@ setup(
-     install_requires=[
+@@ -101,12 +101,12 @@ setuptools.setup(
          "attrs>=19.3.0",
          "colorama>=0.4.3",
--        "junit_xml==1.9",
-+        "junit_xml>=1.9",
          "requests>=2.22.0",
-         # exact version because of unstable API
--        "ruamel.yaml==0.16.10",
-+        "ruamel.yaml>=0.16.10",
+-        "ruamel.yaml>=0.16.0,<0.18",
++        "ruamel.yaml>=0.16.0",
          "tqdm>=4.46.1",
          "packaging>=20.4",
--        "jsonschema~=3.2.0",
-+        "jsonschema>=3.2.0",
-     ],
-     entry_points={"console_scripts": ["semgrep=semgrep.__main__:main"]},
-     packages=setuptools.find_packages(),
+         "jsonschema~=3.2.0",
+-        "wcmatch==8.2",
+-        "peewee~=3.14.4",
++        "wcmatch>=8.2",
++        "peewee>=3.14.4",
+         # Include 'setuptools' for 'pkg_resources' usage. We shouldn't be
+         # overly prescriptive and pin the version for two reasons: 1) because
+         # it may interfere with other 'setuptools' installs on the system,
