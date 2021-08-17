32a8bddea7cc0134d8ab87626e1a127784dc2948
Thomas Klausner <tk@giga.or.at>
2021-08-18 00:33:42 +0200

    gst-plugins1-good: some progress, need to link against ${LIBOSSAUDIO} next

--- gst-plugins1-base/buildlink3.mk
+++ gst-plugins1-base/buildlink3.mk
@@ -10,6 +10,7 @@ BUILDLINK_ABI_DEPENDS.gst-plugins1-base+=	gst-plugins1-base>=1.18
 BUILDLINK_PKGSRCDIR.gst-plugins1-base?=		../../wip/gst-plugins1-base
 
 .include "../../wip/gstreamer1/buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../devel/orc/buildlink3.mk"
 .endif # GST_PLUGINS1_BASE_BUILDLINK3_MK
 
