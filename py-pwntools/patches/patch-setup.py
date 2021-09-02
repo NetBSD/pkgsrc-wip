5df4cdb96e593cffaa25718ae1bab13653ed3919
Leonardo Taccari <leot@NetBSD.org>
2021-09-02 12:04:40 +0200

    pwntools: Update to 4.6.0
    pkgsrc changes:
     - Avoid to call pandoc if it is installed to make the build a bit more
       reproducible.
    
    Changes:
    4.6.0
    -----
      * #1429 Add a mechanism for ret2csu (originally #1138)
      * #1566 Add ignore_config argument to pwnlib.tubes.ssh and improve
        allow_agent implementation
      * #1652 Add process.readmem and process.writemem
      * #1739 Add/fix shellcraft.linux.kill() / shellcraft.linux.killparent()
      * #1746 Prefer Python3 over Python2 for spawning remote processes over SSH
      * #1751 Fix process() with executable relative to cwd
      * #1753 major change: less unconditional imports in pwnlib
      * #1776 mips: do not use $t0 temporary variable in dupio
      * #1846 support launching GDB in more different terminals
    
    4.5.1
    -----
      * #1902 Always specify -F and -P for tmux in run_in_new_termianl
    
    4.5.0
    -----
      * #1261 Misc run_in_new_terminal improvements (notably gdb terminated by
        default)
      * #1695 Allow using GDB Python API
      * #1735 Python 3.9 support in safeeval
      * #1738 Which function support custom search path
          + process also looks now at env['PATH'] to find the path for the
            executable
      * #1742 New baremetal os to debug binaries executed with qemu-system-$(arch)
      * #1757 update cache directories
      * #1758 Remove eval from cli
      * #1780 Re-add Python2 to the official Dockerfile
      * #1941 Disable all Android tests, pwnlib.adb is no longer supported in CI
      * #1811 Remove unnecessary pwn.toplevel.__all__
      * #1827 Support $XDG_CONFIG_HOME dir for pwn.conf
      * #1841 Add colored_traceback
      * #1839 run_in_new_terminal now creates a runner script if given a list or
        tuple
      * #1833 Add pwnlib.filesystem module
      * #1852 Fix atexit on Python 3
    
    4.4.0
    -----
      * #1541 Use context.newline for tubes by default
      * #1602 Fix bytes handling in ssh tubes
      * #1606 Fix asm() and disasm() for MSP430, S390
      * #1616 Fix cyclic cli for 64 bit integers
      * #1632 Enable usage of Pwntools in jupyter
      * #1633 Open a shell if pwn template cannot download the remote file
      * #1644 Enable and support SNI for SSL-wrapped tubes
      * #1651 Make pwn shellcraft faster
      * #1654 Docker images (pwntools/pwntools:stable etc) now use Python3 by
        default, and includes assemblers for a few common architectures
      * #1667 Add i386 encoder ascii_shellcode (Fixed docs in #1693)
      * Fix syscall instruction lists for SROP on i386 and amd64
      * Fix migration to another ROP
      * #1673 Add base= argument to ROP.chain() and ROP.dump()
      * #1675 Gdbserver now correctly accepts multiple libraries in LD_PRELOAD and
        LD_LIBRARY_PATH
      * #1678 ROPGadget multibr
      * #1682 ROPGadget multibr fix
      * #1687 Actually import requests when doing from pwn import *
      * #1688 Add __setattr__ and __call__ interfaces to ROP for setting registers
      * #1692 Remove python2 shebangs where appropriate
      * #1703 Update libcdb buildid offsets for amd64 and i386
      * #1704 Try https://libc.rip/ for libcdb lookup

--- py-pwntools/patches/patch-setup.py
+++ py-pwntools/patches/patch-setup.py
@@ -1,8 +1,9 @@
 $NetBSD$
 
-Relax version requirements.
+- Relax version requirements
+- Avoid to needs and call pandoc to convert description
 
---- setup.py.orig	2020-11-29 23:44:02.000000000 +0000
+--- setup.py.orig	2021-07-12 21:07:26.000000000 +0000
 +++ setup.py
 @@ -60,7 +60,7 @@ install_requires     = ['paramiko>=1.15.
                          'psutil>=3.3.0',
@@ -11,5 +12,28 @@ Relax version requirements.
 -                        'unicorn>=1.0.2rc1,<1.0.2rc4', # see unicorn-engine/unicorn#1100, unicorn-engine/unicorn#1170, Gallopsled/pwntools#1538
 +                        'unicorn>=1.0.2rc1', # see unicorn-engine/unicorn#1100, unicorn-engine/unicorn#1170, Gallopsled/pwntools#1538
                          'six>=1.12.0',
- ]
+                         'rpyc',
+                         'colored_traceback',
+@@ -76,14 +76,6 @@ if not os.path.exists(PythonH):
+     print("$ apt-get install python-dev", file=sys.stderr)
+     sys.exit(-1)
  
+-# Convert README.md to reStructuredText for PyPI
+-long_description = ''
+-try:
+-    long_description = subprocess.check_output(['pandoc', 'README.md', '--to=rst'], universal_newlines=True)
+-except Exception as e:
+-    print("Failed to convert README.md through pandoc, proceeding anyway", file=sys.stderr)
+-    traceback.print_exc()
+-
+ setup(
+     name                 = 'pwntools',
+     python_requires      = '>=2.7',
+@@ -105,7 +97,6 @@ setup(
+     entry_points = {'console_scripts': console_scripts},
+     scripts              = glob.glob("bin/*"),
+     description          = "Pwntools CTF framework and exploit development library.",
+-    long_description     = long_description,
+     author               = "Gallopsled et al.",
+     author_email         = "pwntools-users@googlegroups.com",
+     url                  = 'https://pwntools.com',
