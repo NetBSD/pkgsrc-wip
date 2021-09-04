58dd67d0ff96a11ffd27deef2f3ce77f9b869f68
Mark Davies <mark@ecs.vuw.ac.nz>
2021-09-04 15:27:06 +1200

    systemsettings: make 5.18.5 build

--- systemsettings/buildlink3.mk
+++ systemsettings/buildlink3.mk
@@ -6,11 +6,9 @@ BUILDLINK_TREE+=	systemsettings
 SYSTEMSETTINGS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.systemsettings+=	systemsettings>=5.6.3
-BUILDLINK_ABI_DEPENDS.systemsettings?=	systemsettings>=5.18.5nb1
 BUILDLINK_PKGSRCDIR.systemsettings?=	../../wip/systemsettings
 
-.include "../../devel/kcmutils/buildlink3.mk"
-.include "../../www/khtml/buildlink3.mk"
+.include "../../wip/plasma-workspace/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# SYSTEMSETTINGS_BUILDLINK3_MK
 
