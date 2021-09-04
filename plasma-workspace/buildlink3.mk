1411160b932a3a20cde250f510c62fc4af56a210
Mark Davies <mark@ecs.vuw.ac.nz>
2021-09-04 15:20:32 +1200

    plasma-workspace: add kitemmodels and kactivities-stats to buildlink

--- plasma-workspace/buildlink3.mk
+++ plasma-workspace/buildlink3.mk
@@ -10,7 +10,9 @@ BUILDLINK_ABI_DEPENDS.plasma-workspace?=		plasma-workspace>=5.18.5nb1
 BUILDLINK_PKGSRCDIR.plasma-workspace?=		../../wip/plasma-workspace
 
 .include "../../devel/kcmutils/buildlink3.mk"
+.include "../../devel/kitemmodels/buildlink3.mk"
 .include "../../net/knewstuff/buildlink3.mk"
+.include "../../x11/kactivities-stats/buildlink3.mk"
 .include "../../x11/kxmlgui/buildlink3.mk"
 .include "../../x11/qt5-qtbase/buildlink3.mk"
 .endif	# PLASMA_WORKSPACE_BUILDLINK3_MK
