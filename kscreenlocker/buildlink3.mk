696acf726552f934b70bd69a409b54d00214bcdf
Mark Davies <mark@ecs.vuw.ac.nz>
2021-09-04 14:36:40 +1200

    kscreenlocker: add buildlink files required by plasma-workspace

--- kscreenlocker/buildlink3.mk
+++ kscreenlocker/buildlink3.mk
@@ -8,6 +8,9 @@ KSCREENLOCKER_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.kscreenlocker+=	kscreenlocker>=5.6.3
 BUILDLINK_PKGSRCDIR.kscreenlocker?=	../../wip/kscreenlocker
 
+BUILDLINK_FILES.kscreenlocker+=	share/dbus-1/interfaces/kf5_org.freedesktop.ScreenSaver.xml
+BUILDLINK_FILES.kscreenlocker+=	share/dbus-1/interfaces/org.kde.screensaver.xml
+
 .include "../../devel/kdeclarative/buildlink3.mk"
 .include "../../devel/kidletime/buildlink3.mk"
 .include "../../devel/kwayland/buildlink3.mk"
