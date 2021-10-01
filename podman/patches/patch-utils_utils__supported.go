642df522784c06073b72703c55f917de9f3f1618
Leonardo Taccari <leot@NetBSD.org>
2021-10-01 22:31:02 +0200

    podman: Import podman-3.4.0
    Podman (the POD MANager) is a tool for managing containers and
    images, volumes mounted into those containers, and pods made from
    groups of containers. Podman is based on libpod, a library for
    container lifecycle management that is also contained in this
    repository. The libpod library provides APIs for managing containers,
    pods, container images, and volumes.
    
    On operating systems other than Linux, it can spawn and talk to a
    Linux virtual machine.
    
    Based on sysutils/podman.

---
+++ podman/patches/patch-utils_utils__supported.go
@@ -0,0 +1,10 @@
+$NetBSD$
+
+--- utils/utils_supported.go.orig	2021-09-30 18:44:31.000000000 +0000
++++ utils/utils_supported.go
+@@ -1,4 +1,4 @@
+-// +build linux darwin
++// +build linux darwin netbsd
+ 
+ package utils
+ 
