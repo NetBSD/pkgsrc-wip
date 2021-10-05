40cb7798b82a9e9bd9a97db95c9db00e3bf1c81d
Niclas Rosenvik <nros@NetBSD.org>
2021-10-05 15:13:48 +0000

    update litehtml to version 0.5.0.20211003
    update litehtml to version 0.5.0.20211003
    so that it installs cmake package information.
    set it to install shared lib instead of static.

--- litehtml/buildlink3.mk
+++ litehtml/buildlink3.mk
@@ -7,7 +7,6 @@ LITEHTML_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.litehtml+=	litehtml>=0.4.0.20170407
 BUILDLINK_PKGSRCDIR.litehtml?=		../../wip/litehtml
-BUILDLINK_DEPMETHOD.litehtml?=		build
 .endif	# LITEHTML_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-litehtml
