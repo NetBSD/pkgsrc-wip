596776ced1122ca98e4bd4ae64b514e4003ee067
Leonardo Taccari <leot@NetBSD.org>
2021-10-06 22:59:16 +0200

    py-awslogs: Import py-awslogs-0.14.0
    awslogs is a simple command line tool for querying groups, streams and events
    from Amazon CloudWatch logs.
    
    One of the most powerful features is to query events from several streams and
    consume them (ordered) in pseudo-realtime using your favourite tools such as
    grep.

---
+++ py-awslogs/patches/patch-setup.py
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Do not install tests.
+
+--- setup.py.orig	2020-07-10 07:01:23.000000000 +0000
++++ setup.py
+@@ -19,7 +19,7 @@ setup(
+     description='awslogs is a simple command line tool to read aws cloudwatch logs.',
+     long_description='awslogs is a simple command line tool to read aws cloudwatch logs.',
+     keywords="aws logs cloudwatch",
+-    packages=find_packages(),
++    packages=find_packages(exclude=('tests',)),
+     platforms='any',
+     python_requires=">=3.5.*",
+     install_requires=install_requires,
