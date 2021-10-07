b68d43da3955b5fbdd74c9055034d6abbf4ad770
Jason Bacon <bacon@NetBSD.org>
2021-10-07 09:14:52 -0500

    libxtend: Update to 0.1.3.19
    Add numeric comparison functions for qsort(), etc.

--- libxtend/buildlink3.mk
+++ libxtend/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=		libxtend
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.3.16
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.3.19
 BUILDLINK_PKGSRCDIR.libxtend?=		../../wip/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
