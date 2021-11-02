b2638eb23f5a5d0dff41e113955cd3437d3c27d8
irvise <irvise@irvise.xyz>
2021-11-02 20:06:59 +0100

    gcc10-aux: fix installation. Adapt buildlink3.
    Package should be now ready for review and consideration.

--- gcc10-aux/buildlink3.mk
+++ gcc10-aux/buildlink3.mk
@@ -1,24 +1,24 @@
 # $NetBSD: buildlink3.mk,v 1.1 2020/05/10 15:02:44 maya Exp $
 
-BUILDLINK_TREE+=	gcc10
+BUILDLINK_TREE+=	gcc10-aux
 
-.if !defined(GCC10_BUILDLINK3_MK)
-GCC10_BUILDLINK3_MK:=
+.if !defined(GCC10_AUX_BUILDLINK3_MK)
+GCC10_AUX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gcc10+=	gcc10>=${_GCC_REQD}
-BUILDLINK_ABI_DEPENDS.gcc10+=	gcc10>=10.0
-BUILDLINK_PKGSRCDIR.gcc10=	../../wip/gcc10-aux
-BUILDLINK_DEPMETHOD.gcc10?=	build
+BUILDLINK_API_DEPENDS.gcc10-aux+=	gcc10-aux>=${_GCC_REQD}
+BUILDLINK_ABI_DEPENDS.gcc10-aux+=	gcc10-aux>=10.0
+BUILDLINK_PKGSRCDIR.gcc10-aux=	../../wip/gcc10-aux
+BUILDLINK_DEPMETHOD.gcc10-aux?=	build
 
-BUILDLINK_PASSTHRU_DIRS+=	${BUILDLINK_PREFIX.gcc10}/gcc10
+BUILDLINK_PASSTHRU_DIRS+=	${BUILDLINK_PREFIX.gcc10-aux}/gcc10-aux
 
-BUILDLINK_FILES.gcc10=		# empty
-BUILDLINK_AUTO_VARS.gcc10=	no
+BUILDLINK_FILES.gcc10-aux=		# empty
+BUILDLINK_AUTO_VARS.gcc10-aux=	no
 
-pkgbase := gcc10
+pkgbase := gcc10-aux
 .include "../../mk/pkg-build-options.mk"
 .include "../../mk/dlopen.buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 .endif
 
-BUILDLINK_TREE+=	-gcc10
+BUILDLINK_TREE+=	-gcc10-aux
