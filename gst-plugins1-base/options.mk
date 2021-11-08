a917642be3c3ec2c5dd7f88cc7dc2abae467be71
Thomas Klausner <tk@giga.or.at>
2021-11-08 20:16:29 +0100

    gstreamer1 + plugins: more cleanups

--- gst-plugins1-base/options.mk
+++ gst-plugins1-base/options.mk
@@ -1,6 +1,6 @@
 # $NetBSD: options.mk,v 1.9 2019/10/03 20:45:19 nia Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.gst-plugins1-bad
+PKG_OPTIONS_VAR=	PKG_OPTIONS.gst-plugins1-base
 PKG_SUPPORTED_OPTIONS=	introspection
 PKG_SUGGESTED_OPTIONS=	introspection
 
