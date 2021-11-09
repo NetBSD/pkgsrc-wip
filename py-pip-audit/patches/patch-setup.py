29a82e2634cfc4c42d54fd93c47c42d7b912f9ab
Thomas Klausner <tk@giga.or.at>
2021-11-10 00:25:23 +0100

    py-pip-audit: update to 0.0.5
    Bugfix release for 0.0.4 with the patches from pkgsrc and a couple more.

--- py-pip-audit/patches/patch-setup.py
+++ py-pip-audit/patches/patch-setup.py
@@ -1,23 +1,15 @@
 $NetBSD$
 
-Fix dataclasses dependency
-https://github.com/trailofbits/pip-audit/pull/114/files
-
 Do not insist on one particular version of CacheControl.
 
---- setup.py.orig	2021-11-09 16:11:51.000000000 +0000
+--- setup.py.orig	2021-11-09 22:42:57.000000000 +0000
 +++ setup.py
-@@ -31,11 +31,11 @@ setup(
-         "pip-api>=0.0.23",
-         "packaging>=21.0.0",
-         # TODO: Remove this once 3.7 is our minimally supported version.
--        "dataclasses>=0.6",
-+        "dataclasses>=0.6; python_version < '3.7'",
+@@ -35,7 +35,7 @@ setup(
          "progress>=1.6",
          "resolvelib>=0.8.0",
          "html5lib>=1.1",
--        "CacheControl==0.12.6",
-+        "CacheControl>=0.12.6",
+-        "CacheControl==0.12.10",
++        "CacheControl>=0.12.10",
          "lockfile>=0.12.2",
      ],
      extras_require={
