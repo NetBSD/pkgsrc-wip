167ace2e2cda4aa209027580b632806a74c28682
nia <nia@NetBSD.org>
2021-11-16 15:42:17 +0100

    openh264: update to 2.1.1
    - avoid building browser plugin for now. not much use to us
      because we're pkgsrc and not pkgbin and thus don't get to be
      exempt
    - avoid overriding pkgsrc's own stack protector settings
    - avoid running git to determine the version
    - use TOOL_DEPENDS for nasm

--- openh264/buildlink3.mk
+++ openh264/buildlink3.mk
@@ -6,9 +6,8 @@ BUILDLINK_TREE+=	openh264
 OPENH264_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.openh264+=	openh264>=1.5.0
-BUILDLINK_PKGSRCDIR.openh264?=	../../wip/openh264
+BUILDLINK_PKGSRCDIR.openh264?=		../../wip/openh264
 
-#.include "../../multimedia/gmp-api/buildlink3.mk"
 .endif	# OPENH264_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-openh264
--- openh264/files/gmpopenh264.js
+++
@@ -1,3 +0,0 @@
-pref("media.gmp-gmpopenh264.autoupdate", false);
-pref("media.gmp-gmpopenh264.lastUpdate", @LASTUPDATE_TIMESTAMP@);
-pref("media.gmp-gmpopenh264.version", "@VERSION@");
