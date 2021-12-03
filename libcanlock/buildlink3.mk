a5ad8e5dc2a4845a7f5f5d147369412e6b6eec02
Michael Baeuerle <micha@NetBSD.org>
2021-12-03 09:46:34 +0100

    libcanlock: Update to 3.3.0
    No changes from RC3.
    
    Change bl3 API and ABI depends back to 3.0.0 (both are backward
    compatible to this version).

--- libcanlock/buildlink3.mk
+++ libcanlock/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libcanlock
 .if !defined(LIBCANLOCK_BUILDLINK3_MK)
 LIBCANLOCK_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libcanlock+=	libcanlock>=3.3.0
+BUILDLINK_API_DEPENDS.libcanlock+=	libcanlock>=3.0.0
 BUILDLINK_PKGSRCDIR.libcanlock?=	../../wip/libcanlock
 .endif	# LIBCANLOCK_BUILDLINK3_MK
 
