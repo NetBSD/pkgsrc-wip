0579fdcf8cbff61a69d839c1ba01e9883cb71a1c
atomicules <base@atomicules.co.uk>
2021-12-11 15:31:53 +0000

    spotifyd: update to 0.3.3

--- spotifyd/patches/patch-utils.rs
+++ spotifyd/patches/patch-utils.rs
@@ -1,12 +1,10 @@
 $NetBSD$
 
-See: https://github.com/Spotifyd/spotifyd/pull/796
-
---- src/utils.rs.orig	2021-01-14 14:52:28.000000000 +0000
+--- src/utils.rs.orig	2021-12-07 20:10:37.000000000 +0000
 +++ src/utils.rs
-@@ -3,7 +3,7 @@ use std::env;
- #[cfg(target_os = "macos")]
- use whoami;
+@@ -1,7 +1,7 @@
+ use log::trace;
+ use std::env;
  
 -#[cfg(any(target_os = "freebsd", target_os = "linux"))]
 +#[cfg(any(target_os = "netbsd", target_os = "freebsd", target_os = "linux"))]
