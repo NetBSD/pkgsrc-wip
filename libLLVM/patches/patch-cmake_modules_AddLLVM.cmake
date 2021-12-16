67c3ce9f8f01659fabd29963b6a81d33db7ef200
Tobias Nygren <tnn@NetBSD.org>
2021-12-16 11:34:43 +0100

    libLLVM: bump to version 13

--- libLLVM/patches/patch-cmake_modules_AddLLVM.cmake
+++ libLLVM/patches/patch-cmake_modules_AddLLVM.cmake
@@ -5,41 +5,32 @@ Make sure llvm-config goes in libexec/libLLVM to avoid conflict.
 Don't use non-portable -z discard-unused on SunOS.
 Don't use relative @rpath in llvm-config on Darwin.
 
---- cmake/modules/AddLLVM.cmake.orig	2021-06-28 16:23:38.000000000 +0000
+--- cmake/modules/AddLLVM.cmake.orig	2021-09-24 16:18:10.000000000 +0000
 +++ cmake/modules/AddLLVM.cmake
-@@ -594,12 +594,12 @@ function(llvm_add_library name)
-       set(library_name ${output_name}-${LLVM_VERSION_MAJOR}${LLVM_VERSION_SUFFIX})
+@@ -616,10 +616,6 @@ function(llvm_add_library name)
        set(api_name ${output_name}-${LLVM_VERSION_MAJOR}.${LLVM_VERSION_MINOR}.${LLVM_VERSION_PATCH}${LLVM_VERSION_SUFFIX})
        set_target_properties(${name} PROPERTIES OUTPUT_NAME ${library_name})
--      llvm_install_library_symlink(${api_name} ${library_name} SHARED
--        COMPONENT ${name}
--        ALWAYS_GENERATE)
--      llvm_install_library_symlink(${output_name} ${library_name} SHARED
--        COMPONENT ${name}
--        ALWAYS_GENERATE)
-+#      llvm_install_library_symlink(${api_name} ${library_name} SHARED
-+#        COMPONENT ${name}
-+#        ALWAYS_GENERATE)
-+#      llvm_install_library_symlink(${output_name} ${library_name} SHARED
-+#        COMPONENT ${name}
-+#        ALWAYS_GENERATE)
+       if(UNIX)
+-        llvm_install_library_symlink(${api_name} ${library_name} SHARED
+-          COMPONENT ${name})
+-        llvm_install_library_symlink(${output_name} ${library_name} SHARED
+-          COMPONENT ${name})
+       endif()
      endif()
    endif()
- 
-@@ -802,12 +802,6 @@ macro(add_llvm_library name)
-         set_property(GLOBAL PROPERTY LLVM_HAS_EXPORTS True)
+@@ -820,11 +816,6 @@ macro(add_llvm_library name)
        endif()
  
+       get_target_export_arg(${name} LLVM export_to_llvmexports ${umbrella})
 -      install(TARGETS ${name}
 -              ${export_to_llvmexports}
 -              LIBRARY DESTINATION lib${LLVM_LIBDIR_SUFFIX} COMPONENT ${name}
 -              ARCHIVE DESTINATION lib${LLVM_LIBDIR_SUFFIX} COMPONENT ${name}
 -              RUNTIME DESTINATION bin COMPONENT ${name})
--
+ 
        if (NOT LLVM_ENABLE_IDE)
          add_llvm_install_targets(install-${name}
-                                  DEPENDS ${name}
-@@ -1015,7 +1009,7 @@ function(process_llvm_pass_plugins)
+@@ -1033,7 +1024,7 @@ function(process_llvm_pass_plugins)
  
        ## Part 1: Extension header to be included whenever we need extension
        #  processing.
@@ -48,8 +39,8 @@ Don't use relative @rpath in llvm-config on Darwin.
        set(llvm_cmake_builddir "${LLVM_BINARY_DIR}/${LLVM_INSTALL_PACKAGE_DIR}")
        file(WRITE
            "${llvm_cmake_builddir}/LLVMConfigExtensions.cmake"
-@@ -1219,7 +1213,7 @@ macro(add_llvm_tool name)
- 
+@@ -1234,7 +1225,7 @@ macro(add_llvm_tool name)
+       get_target_export_arg(${name} LLVM export_to_llvmexports)
        install(TARGETS ${name}
                ${export_to_llvmexports}
 -              RUNTIME DESTINATION ${LLVM_TOOLS_INSTALL_DIR}
@@ -57,12 +48,12 @@ Don't use relative @rpath in llvm-config on Darwin.
                COMPONENT ${name})
  
        if (NOT LLVM_ENABLE_IDE)
-@@ -2099,7 +2093,7 @@ function(llvm_setup_rpath name)
+@@ -2118,7 +2109,7 @@ function(llvm_setup_rpath name)
    endif()
  
    if (APPLE)
 -    set(_install_name_dir INSTALL_NAME_DIR "@rpath")
 +    set(_install_name_dir INSTALL_NAME_DIR "${CMAKE_INSTALL_PREFIX}/lib/libLLVM")
      set(_install_rpath "@loader_path/../lib${LLVM_LIBDIR_SUFFIX}" ${extra_libdir})
-   elseif(UNIX)
-     set(_install_rpath "\$ORIGIN/../lib${LLVM_LIBDIR_SUFFIX}" ${extra_libdir})
+   elseif(${CMAKE_SYSTEM_NAME} MATCHES "AIX" AND BUILD_SHARED_LIBS)
+     # $ORIGIN is not interpreted at link time by aix ld.
