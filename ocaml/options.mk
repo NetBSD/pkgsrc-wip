cbb3a4b6a95c87743580ceada6d69be6d96a17a8
Jaap Boender <jaapb@netbsd.org>
2021-12-22 13:02:22 +0100

    ocaml: first steps towards package update to ocaml 4.13.1

--- ocaml/options.mk
+++ ocaml/options.mk
@@ -1,8 +1,9 @@
 # $NetBSD: options.mk,v 1.5 2020/12/09 10:33:04 jaapb Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ocaml
-PKG_SUPPORTED_OPTIONS=	pic flambda spacetime
+PKG_SUPPORTED_OPTIONS=	pic flambda native
 PKG_SUGGESTED_OPTIONS=	pic
+PLIST_VARS+=	pic flambda native
 
 .include "../../mk/bsd.prefs.mk"
 .include "../../mk/bsd.options.mk"
@@ -19,8 +20,11 @@ CONFIGURE_ARGS+=	--enable-flambda
 CONFIGURE_ARGS+=	--disable-flambda
 .endif
 
-.if !empty(PKG_OPTIONS:Mspacetime)
-CONFIGURE_ARGS+=	--enable-spacetime
+.if !empty(PKG_OPTIONS:Mnative)
+CONFIGURE_ARGS+=	--enable-native-compiler
+BUILD_TARGET=		world.opt
+PLIST.native=		yes	
 .else
-CONFIGURE_ARGS+=	--disable-spacetime
+CONFIGURE_ARGS+=	--disable-native-compiler
+BUILD_TARGET=		world
 .endif
