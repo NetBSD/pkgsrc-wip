9bd0c3a3b8da08ae4b5124ab2f4cd4893dc5b400
Jay Patel <jaypatel.ani@gmail.com>
2021-12-24 20:10:00 +0530

    patch  and makefile update cjs 5.2.0

--- cjs/buildlink3.mk
+++ cjs/buildlink3.mk
@@ -8,7 +8,7 @@ CJS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.cjs+=	cjs>=3.8.0
 BUILDLINK_PKGSRCDIR.cjs?=	../../wip/cjs
 
-.include "../../lang/spidermonkey52/buildlink3.mk"
+.include "../../lang/spidermonkey185/buildlink3.mk"
 .include "../../graphics/cairo-gobject/buildlink3.mk"
 .include "../../devel/gobject-introspection/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
