29ba58d5ce5cce761d1ab41aaa5ceaff31359bb6
Tobias Nygren <tnn@NetBSD.org>
2022-01-14 15:13:26 +0100

    MesaLib: update to 21.3.4 (bugfixes)
    I plan to land this in pkgsrc proper with libLLVM 13.0.1.
    Testing and feedback welcome.

--- MesaLib/patches/patch-src_gallium_drivers_freedreno_freedreno__util.h
+++ MesaLib/patches/patch-src_gallium_drivers_freedreno_freedreno__util.h
@@ -2,27 +2,30 @@ $NetBSD$
 
 gettid() is linux-specific
 
---- src/gallium/drivers/freedreno/freedreno_util.h.orig	2021-12-01 19:04:14.000000000 +0000
+--- src/gallium/drivers/freedreno/freedreno_util.h.orig	2022-01-12 21:32:28.000000000 +0000
 +++ src/gallium/drivers/freedreno/freedreno_util.h
-@@ -107,12 +107,21 @@ extern bool fd_binning_enabled;
- #include <unistd.h>
+@@ -108,6 +108,7 @@ extern bool fd_binning_enabled;
  #include <sys/types.h>
+ #include <sys/syscall.h>
  
 +#if defined(__linux__)
  #define DBG(fmt, ...)                                                          \
     do {                                                                        \
        if (FD_DBG(MSGS))                                                        \
-          mesa_logi("%5d: %s:%d: " fmt, gettid(), __FUNCTION__, __LINE__,       \
-                    ##__VA_ARGS__);                                             \
+@@ -115,6 +116,16 @@ extern bool fd_binning_enabled;
+                                         __FUNCTION__, __LINE__,                \
+                                         ##__VA_ARGS__);                        \
     } while (0)
 +#else
 +#define DBG(fmt, ...)                                                          \
 +   do {                                                                        \
 +      if (FD_DBG(MSGS))                                                        \
-+         mesa_logi("%s:%d: " fmt, __FUNCTION__, __LINE__,                      \
-+                   ##__VA_ARGS__);                                             \
++         mesa_logi("%s:%d: " fmt,                                              \
++                                        __FUNCTION__, __LINE__,                \
++                                        ##__VA_ARGS__);                        \
 +   } while (0)
 +#endif
++
  
  #define perf_debug_message(debug, type, ...)                                   \
     do {                                                                        \
--- MesaLib/patches/patch-src_util_libsync.h
+++ MesaLib/patches/patch-src_util_libsync.h
@@ -2,7 +2,7 @@ $NetBSD$
 
 sys/ioccom.h for _IOWR
 
---- src/util/libsync.h.orig	2021-12-29 21:05:19.000000000 +0000
+--- src/util/libsync.h.orig	2022-01-12 21:32:28.000000000 +0000
 +++ src/util/libsync.h
 @@ -34,6 +34,9 @@
  #include <stdint.h>
