2d49af6d6c391d78607c809ce47529412a19f0fc
davidsainty <david.sainty@gmail.com>
2022-01-18 00:50:10 +1300

    Correct patch-src_zm__thread.h and patch-src_zm_signal_cpp  to make all the funtional changes as patched against ZoneMinder 1.29.0.  Intended to repair the build against NetBSD 9.0.

--- zoneminder/patches/patch-src_zm__thread.h
+++ zoneminder/patches/patch-src_zm__thread.h
@@ -8,9 +8,9 @@ portable than syscall(SYS_gettid).
 
 \todo Fix correctly upstream.
 
---- src/zm_thread.h.orig	2019-02-22 15:38:47.000000000 +0000
-+++ src/zm_thread.h
-@@ -22,16 +22,17 @@
+--- src/zm_thread.h.orig	2019-02-23 04:38:47.000000000 +1300
++++ src/zm_thread.h	2022-01-18 00:14:30.000000000 +1300
+@@ -22,20 +22,21 @@
  
  #include <unistd.h>
  #include <pthread.h>
@@ -31,18 +31,22 @@ portable than syscall(SYS_gettid).
  class ThreadException : public Exception
  {
  private:
-@@ -55,7 +56,9 @@ private:
+-#ifndef SOLARIS
++#ifndef USE_PTHREAD
+   pid_t pid() {
+     pid_t tid; 
+ #ifdef __FreeBSD__ 
+@@ -55,7 +56,8 @@
    pthread_t pid() { return( pthread_self() ); }
  #endif
  public:
 -  ThreadException( const std::string &message ) : Exception( stringtf( "(%d) "+message, (long int)pid() ) ) {
 +  /* The type of pid() varies by OS */
 +  ThreadException( const std::string &message ) : Exception( stringtf( ("(%jd) "+message).c_str(), (intmax_t)pid() ) ) {
-+
    }
  };
  
-@@ -217,7 +220,7 @@ protected:
+@@ -217,7 +219,7 @@
  
    Mutex mThreadMutex;
    Condition mThreadCondition;
@@ -51,7 +55,7 @@ portable than syscall(SYS_gettid).
    pid_t mPid;
  #else
    pthread_t mPid;
-@@ -230,7 +233,7 @@ protected:
+@@ -230,7 +232,7 @@
    Thread();
    virtual ~Thread();
  
@@ -60,7 +64,7 @@ portable than syscall(SYS_gettid).
    pid_t id() const
    {
      pid_t tid; 
-@@ -269,7 +272,7 @@ public:
+@@ -269,7 +271,7 @@
    void kill( int signal );
    bool isThread()
    {
