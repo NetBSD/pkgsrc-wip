09a61db8d7ee57cbf7f73da6cf881903c46934a8
Jason Bacon <bacon@NetBSD.org>
2022-01-23 19:13:20 -0600

    lpjs: Clean up rc scripts

--- lpjs/files/lpjs_compd.sh
+++ lpjs/files/lpjs_compd.sh
@@ -1,17 +1,32 @@
-#!@RCD_SCRIPTS_SHELL@
+#!/bin/sh
 # $NetBSD: lpjs_compd.sh,v 1.1 2018/04/22 21:31:09 bacon Exp $
 #
 # PROVIDE: lpjs_compd
 # REQUIRE: DAEMON munge
 # KEYWORD: shutdown
 
-. /etc/rc.subr
+$_rc_subr_loaded . /etc/rc.subr
 
 name="lpjs_compd"
 rcvar=${name}
-command="@PREFIX@/sbin/lpjs_compd --daemonize"
+command="@PREFIX@/sbin/$name"
+start_cmd="lpjs_start"
+stop_cmd="lpjs_stop"
+sig_stop="-INT"
 
 lpjs_compd_user="root"
+
+lpjs_start()
+{
+    echo "Starting $name..."
+    $command --daemonize
+}
+
+lpjs_stop()
+{
+    echo "Stopping $name..."
+    pkill $sig_stop $name
+}
 
 load_rc_config $name
 run_rc_command "$1"
--- lpjs/files/lpjs_dispatchd.sh
+++ lpjs/files/lpjs_dispatchd.sh
@@ -1,15 +1,30 @@
-#!@RCD_SCRIPTS_SHELL@
+#!/bin/sh
 # $NetBSD: lpjs_dispatchd.sh,v 1.1 2018/04/22 21:31:09 bacon Exp $
 #
 # PROVIDE: lpjs_dispatchd
 # REQUIRE: DAEMON munge
 # KEYWORD: shutdown
 
-. /etc/rc.subr
+$_rc_subr_loaded . /etc/rc.subr
 
 name="lpjs_dispatchd"
 rcvar=${name}
-command="@PREFIX@/sbin/lpjs_dispatchd --daemonize"
+command="@PREFIX@/sbin/$name"
+start_cmd="lpjs_start"
+stop_cmd="lpjs_stop"
+sig_stop="-INT"
+
+lpjs_start()
+{
+    echo "Starting $name..."
+    $command --daemonize
+}
+
+lpjs_stop()
+{
+    echo "Stopping $name..."
+    pkill $sig_stop $name
+}
 
 load_rc_config $name
 run_rc_command "$1"
