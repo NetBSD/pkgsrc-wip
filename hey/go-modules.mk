8dc30fe92891a53df6d89455e164aae9a515cd3e
Leonardo Taccari <leot@NetBSD.org>
2022-02-07 09:57:50 +0100

    hey: Import hey-0.1.4
    hey is a tiny program that sends some load to a web application.

---
+++ hey/go-modules.mk
@@ -0,0 +1,6 @@
+# $NetBSD$
+
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20181017193950-04a2e542c03f.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20181017193950-04a2e542c03f.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.zip
