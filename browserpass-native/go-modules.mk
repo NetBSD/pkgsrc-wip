e41fa7a387bf9583caa2a7f85e5fe018a21248c5
Iku Iwasa <iku.iwasa@gmail.com>
2022-02-12 14:16:08 +0900

    browserpass-native: Update to 3.0.8
    pkgsrc changes:
    
    * Use go-modules.mk.
    
    upstream changes:
    
    * No functional changes
    * Add pre-built binaries for Linux ARM and Apple M1
    * Update PGP key used for signing commits, tags and releases (new key is
    signed with the old one)

---
+++ browserpass-native/go-modules.mk
@@ -0,0 +1,21 @@
+# $NetBSD$
+
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.0.mod
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.1.mod
+GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.1.zip
+GO_MODULE_FILES+=	github.com/mattn/go-zglob/@v/v0.0.3.mod
+GO_MODULE_FILES+=	github.com/mattn/go-zglob/@v/v0.0.3.zip
+GO_MODULE_FILES+=	github.com/pmezard/go-difflib/@v/v1.0.0.mod
+GO_MODULE_FILES+=	github.com/pmezard/go-difflib/@v/v1.0.0.zip
+GO_MODULE_FILES+=	github.com/rifflock/lfshook/@v/v0.0.0-20180920164130-b9218ef580f5.mod
+GO_MODULE_FILES+=	github.com/rifflock/lfshook/@v/v0.0.0-20180920164130-b9218ef580f5.zip
+GO_MODULE_FILES+=	github.com/sirupsen/logrus/@v/v1.8.1.mod
+GO_MODULE_FILES+=	github.com/sirupsen/logrus/@v/v1.8.1.zip
+GO_MODULE_FILES+=	github.com/stretchr/objx/@v/v0.1.0.mod
+GO_MODULE_FILES+=	github.com/stretchr/objx/@v/v0.1.0.zip
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.2.2.mod
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.3.0.mod
+GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.3.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20191026070338-33540a1f6037.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220207234003-57398862261d.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220207234003-57398862261d.zip
