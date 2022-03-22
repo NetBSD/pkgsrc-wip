07771e503a0700ddcdf324e126e224edbbc3ff41
nikita <nikita@NetBSD.org>
2022-03-22 21:12:04 +0100

    gnurl: pkglint fixes

--- gnurl/options.mk
+++ gnurl/options.mk
@@ -6,13 +6,13 @@ PKG_SUGGESTED_OPTIONS=	inet6
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Minet6)
+.if ${PKG_OPTIONS:Minet6}
 CONFIGURE_ARGS+=	--enable-ipv6
 .else
 CONFIGURE_ARGS+=	--disable-ipv6
 .endif
 
-.if !empty(PKG_OPTIONS:Mtests)
+.if ${PKG_OPTIONS:Mtests}
 .include "../../lang/python/pyversion.mk"
 USE_TOOLS+=		perl:test
 PYTHON_FOR_BUILD_ONLY=	test
