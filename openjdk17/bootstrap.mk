f533d0333b9490bd1266d003811096f44dba5348
Ryo ONODERA <ryoon@NetBSD.org>
2022-04-03 09:28:41 +0900

    openjdk17: Fix more NetBSD kqueue values
    * Update bootstyrap kit with new kqueue values.
    * Bump PKGREVISION.

--- openjdk17/bootstrap.mk
+++ openjdk17/bootstrap.mk
@@ -10,7 +10,7 @@
 #.endif
 
 ONLY_FOR_PLATFORM+=		NetBSD-*-x86_64
-BOOT.nb10-amd64=		bootstrap-jdk-1.17.0.2.8-netbsd-10-amd64-20220324.tar.xz
+BOOT.nb10-amd64=		bootstrap-jdk-1.17.0.2.8-netbsd-10-amd64-20220401.tar.xz
 #SITES.${BOOT.nb10-amd64}=	${MASTER_SITE_LOCAL:=openjdk17/}
 SITES.${BOOT.nb10-amd64}=	https://www.ryoon.net/~ryoon/
 .if !empty(MACHINE_PLATFORM:MNetBSD-*-x86_64) || make(distinfo)
