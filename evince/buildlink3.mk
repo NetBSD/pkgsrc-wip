07820a1b35e1dc60ca323cf9e67b5eb0fdd7e8f2
Thomas Klausner <tk@giga.or.at>
2022-04-06 00:47:10 +0200

    wip/evince: import evince-42.1
    Update/replacement candidate for print/evince3.
    
    Evince is a document viewer for multiple document formats like PDF,
    PostScript, and many others.

---
+++ evince/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.30 2021/12/08 16:02:32 adam Exp $
+
+BUILDLINK_TREE+=	evince
+
+.if !defined(EVINCE_BUILDLINK3_MK)
+EVINCE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.evince+=	evince>=3.0
+BUILDLINK_ABI_DEPENDS.evince+=	evince>=3.34.2nb16
+BUILDLINK_PKGSRCDIR.evince?=	../../wip/evince
+
+.include "../../x11/gtk3/buildlink3.mk"
+.endif # EVINCE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-evince
