b611e96e55de3393226d5b3d79b46dfdc61a299d
Ryo ONODERA <ryoon@NetBSD.org>
2022-04-09 02:27:53 +0900

    openjdk17: Add bootstrap kit for NetBSD/amd64 9
    And it seems that NetBSD 9 does not support ELF GNU Hash.

--- openjdk17/bootstrap.mk
+++ openjdk17/bootstrap.mk
@@ -9,11 +9,20 @@
 #EXTRACT_ONLY+=			${BOOT.nb7-i386}
 #.endif
 
-ONLY_FOR_PLATFORM+=		NetBSD-*-x86_64
+ONLY_FOR_PLATFORM+=		NetBSD-9.[0-8]*-x86_64
+BOOT.nb9-amd64=			bootstrap-jdk-1.17.0.2.8-netbsd-9-amd64-20220408.tar.xz
+#SITES.${BOOT.nb9-amd64}=	${MASTER_SITE_LOCAL:=openjdk17/}
+SITES.${BOOT.nb9-amd64}=	https://www.ryoon.net/~ryoon/
+.if !empty(MACHINE_PLATFORM:MNetBSD-9.[0-8]*-x86_64) || make(distinfo)
+DISTFILES+=			${BOOT.nb9-amd64}
+EXTRACT_ONLY+=			${BOOT.nb9-amd64}
+.endif
+
+ONLY_FOR_PLATFORM+=		NetBSD-9.9*-x86_64
 BOOT.nb10-amd64=		bootstrap-jdk-1.17.0.2.8-netbsd-10-amd64-20220401.tar.xz
 #SITES.${BOOT.nb10-amd64}=	${MASTER_SITE_LOCAL:=openjdk17/}
 SITES.${BOOT.nb10-amd64}=	https://www.ryoon.net/~ryoon/
-.if !empty(MACHINE_PLATFORM:MNetBSD-*-x86_64) || make(distinfo)
+.if !empty(MACHINE_PLATFORM:MNetBSD-9.9*-x86_64) || make(distinfo)
 DISTFILES+=			${BOOT.nb10-amd64}
 EXTRACT_ONLY+=			${BOOT.nb10-amd64}
 .endif
