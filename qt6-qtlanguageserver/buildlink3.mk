22e315af5bb1d23118bb03f54dec94541647134f
Niclas Rosenvik <nros@pkgsrc.org>
2022-04-14 18:10:50 +0200

    qt6-qtlanguageserver: add missing buildlink3.mk file

---
+++ qt6-qtlanguageserver/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	qt6-qtlanguageserver
+
+.if !defined(QT6_QTLANGUAGESERVER_BUILDLINK3_MK)
+QT6_QTLANGUAGESERVER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qt6-qtlanguageserver+=	qt6-qtlanguageserver>=6.3.0
+BUILDLINK_PKGSRCDIR.qt6-qtlanguageserver?=	../../wip/qt6-qtlanguageserver
+
+.include "../../wip/qt6-qtbase/buildlink3.mk"
+.endif	# QT6_QTLANGUAGESERVER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qt6-qtlanguageserver
