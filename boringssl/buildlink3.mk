b5b824d1b78ed0274408401e9967d74efc8b30e0
Thomas Klausner <tk@giga.or.at>
2022-04-20 16:39:55 +0200

    boringssl: update to 20220419
    No concise changelog found.
    
    pkgsrc changes: use upstream install target, remove obsolete patches

--- boringssl/buildlink3.mk
+++ boringssl/buildlink3.mk
@@ -1,13 +1,14 @@
 # $NetBSD$
 
-BUILDLINK_DEPMETHOD.boringssl?=	build
 BUILDLINK_TREE+=		boringssl
 
 .if !defined(BORINGSSL_BUILDLINK3_MK)
 BORINGSSL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.boringssl+=	boringssl>=20151230nb6
+BUILDLINK_API_DEPENDS.boringssl+=	boringssl>=20220419
+
 BUILDLINK_PKGSRCDIR.boringssl?=		../../wip/boringssl
+BUILDLINK_DEPMETHOD.boringssl?=		build
 .endif	# BORINGSSL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-boringssl
