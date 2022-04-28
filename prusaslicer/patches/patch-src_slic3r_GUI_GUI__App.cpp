4a32e1d7a6f5588b42816b8041600c122245d2a5
Paul Ripke <stix@stix.id.au>
2022-04-28 11:25:57 +1000

    prusaslicer: Update to release 2.4.2
    PrusaSlicer 2.4.2-rc2
    
    Bugs fixed
    
    - When opening files through macOS finder on latest macOS, the model was not
      displayed in the scene until further action (e.g. slicing) #8170
    - Fixed a crash when Unsaved Changes dialog attempted to show a modified
      config option which is not available in current mode (e.g. Start G-Code in
      Simple mode). #7905
    - Fixed a bug that when the user was closing the application and replied 'Yes'
      in the dialog asking about unsaved project while also checking 'Remember my
      choice', it made it impossible to close PrusaSlicer in the next runs when
      there were no objects in the plater #8076, #8122
    - Loading of a G-Code in G-Code Viewer or loading config from G-Code file
      sometimes failed because of a bug in parsing of the configuration block at
      the end.
    - Fixed incorrect rendering of some characters used in the recently updated
      Simplified Chinese translation #8171
    - Fixed incorrect number of toolchanges reported in "Sliced Info" box when
      wipe tower was used. The annotations of toolchanges in G-Code were also
      numbered wrong #8186
    - Fixed a crash when using the "Import SL1 Archive" function and either
      cancelling the dialog or confirming a non-existing file #8175
    
    Other
    
    - Recycling symbols in Shape Gallery were mirrored so the user does not have
      to flip them when using them as negative volumes at the base of the print
      (which is the usual use case). Also, PLA recycling symbol was added #7800
    
    PrusaSlicer 2.4.2-rc1
    
    Bugs fixed
    
    - SLA mode incorrectly handled 3MFs from 3rd party software, resulting in
      misplaced SLA support points and holes in some cases (#6100, #7359, #7189,
      #7826, #7659). This is very old issue.
    - Vase mode preview did not show in cases when Custom Start G-Code lacked
      moves in Z #8032
    - When loading a 3rd party 3MF using drag and drop. PrusaSlicer no longer asks
      if the 3MF shall be loaded as a project (which makes no sense since it
      contains no config) and loads the geometry only
    - Using the Layer Height Modifier with the wipe tower was impossible with
      multiple objects. The wipe tower reported that the layer height profile is
      different, even though it was in fact set the same on all objects (#7711,
      #7903, #8044)
    - Fixed a crash on "Convert from/to units" when several objects were selected
    - Automatic color change for logo crashed PrusaSlicer in certain cases (when a
      one-layer object was present) #8156
    - macOS specific: It is now possible to restore PrusaSlicer by clicking the
      dock icon after minimizing it. Partial fix was already in 2.4.1, this
      release should fix it for good on all macOS versions both Intel and ARM
      (#6364, related to #7981).
    - Linux specific: Fixed a crash on opening projects in some cases (#8078,
      #8097)
    
    Localization
    
    - Fixed minor errors in German translation #8024
    - Updated Simplified Chinese localization (#8105 - thanks @kang-kang)
    
    Profiles
    
    - Added Snapmaker bundle (Snapmaker A250, Snapmaker A350). Based on
      https://github.com/nivekmai/snapmaker-prusa by @nivekmai, @WilliamBosacker,
      @mrworf
    - Added profiles for Zonestar printers (Z5, Z6, Z5X, Z8, Z9), based on
      slic3r/slic3r-profiles#19 by @ZONESTAR3D

--- prusaslicer/patches/patch-src_slic3r_GUI_GUI__App.cpp
+++ prusaslicer/patches/patch-src_slic3r_GUI_GUI__App.cpp
@@ -2,7 +2,7 @@ $NetBSD$
 
 Treat NetBSD like Linux.
 
---- src/slic3r/GUI/GUI_App.cpp.orig	2022-03-10 10:35:03.000000000 +0000
+--- src/slic3r/GUI/GUI_App.cpp.orig	2022-04-22 11:01:19.000000000 +0000
 +++ src/slic3r/GUI/GUI_App.cpp
 @@ -374,7 +374,7 @@ private:
  };
@@ -31,7 +31,7 @@ Treat NetBSD like Linux.
              set_data_dir(wxStandardPaths::Get().GetUserDataDir().ToUTF8().data());
          #else
              // Since version 2.3, config dir on Linux is in ${XDG_CONFIG_HOME}.
-@@ -1072,7 +1072,7 @@ bool GUI_App::on_init_inner()
+@@ -1070,7 +1070,7 @@ bool GUI_App::on_init_inner()
      wxCHECK_MSG(wxDirExists(resources_dir), false,
          wxString::Format("Resources path does not exist or is not a directory: %s", resources_dir));
  
@@ -40,7 +40,7 @@ Treat NetBSD like Linux.
      if (! check_old_linux_datadir(GetAppName())) {
          std::cerr << "Quitting, user chose to move their data to new location." << std::endl;
          return false;
-@@ -1173,7 +1173,7 @@ bool GUI_App::on_init_inner()
+@@ -1171,7 +1171,7 @@ bool GUI_App::on_init_inner()
          if (!default_splashscreen_pos)
              // revert "restore_win_position" value if application wasn't crashed
              get_app_config()->set("restore_win_position", "1");
@@ -49,16 +49,16 @@ Treat NetBSD like Linux.
          wxYield();
  #endif
          scrn->SetText(_L("Loading configuration")+ dots);
-@@ -1313,7 +1313,7 @@ bool GUI_App::on_init_inner()
+@@ -1311,7 +1311,7 @@ bool GUI_App::on_init_inner()
  
          // An ugly solution to GH #5537 in which GUI_App::init_opengl (normally called from events wxEVT_PAINT
          // and wxEVT_SET_FOCUS before GUI_App::post_init is called) wasn't called before GUI_App::post_init and OpenGL wasn't initialized.
 -#ifdef __linux__
 +#if defined(__linux__) || defined(__NetBSD__)
-         if (update_gui_after_init && m_opengl_initialized) {
+         if (! m_post_initialized && m_opengl_initialized) {
  #else
-         if (update_gui_after_init) {
-@@ -1899,7 +1899,7 @@ bool GUI_App::switch_language()
+         if (! m_post_initialized) {
+@@ -1897,7 +1897,7 @@ bool GUI_App::switch_language()
      }
  }
  
@@ -67,7 +67,7 @@ Treat NetBSD like Linux.
  static const wxLanguageInfo* linux_get_existing_locale_language(const wxLanguageInfo* language,
                                                                  const wxLanguageInfo* system_language)
  {
-@@ -2088,7 +2088,7 @@ bool GUI_App::load_language(wxString lan
+@@ -2086,7 +2086,7 @@ bool GUI_App::load_language(wxString lan
  				m_language_info_best = wxLocale::FindLanguageInfo(best_language);
  	        	BOOST_LOG_TRIVIAL(trace) << boost::format("Best translation language detected (may be different from user locales): %1%") % m_language_info_best->CanonicalName.ToUTF8().data();
  			}
@@ -76,7 +76,7 @@ Treat NetBSD like Linux.
              wxString lc_all;
              if (wxGetEnv("LC_ALL", &lc_all) && ! lc_all.IsEmpty()) {
                  // Best language returned by wxWidgets on Linux apparently does not respect LC_ALL.
-@@ -2141,7 +2141,7 @@ bool GUI_App::load_language(wxString lan
+@@ -2139,7 +2139,7 @@ bool GUI_App::load_language(wxString lan
      } else if (m_language_info_system != nullptr && language_info->CanonicalName.BeforeFirst('_') == m_language_info_system->CanonicalName.BeforeFirst('_'))
          language_info = m_language_info_system;
  
@@ -85,7 +85,7 @@ Treat NetBSD like Linux.
      // If we can't find this locale , try to use different one for the language
      // instead of just reporting that it is impossible to switch.
      if (! wxLocale::IsAvailable(language_info->Language)) {
-@@ -2241,7 +2241,7 @@ void GUI_App::add_config_menu(wxMenuBar 
+@@ -2239,7 +2239,7 @@ void GUI_App::add_config_menu(wxMenuBar 
          local_menu->Append(config_id_base + ConfigMenuSnapshots, _L("&Configuration Snapshots") + dots, _L("Inspect / activate configuration snapshots"));
          local_menu->Append(config_id_base + ConfigMenuTakeSnapshot, _L("Take Configuration &Snapshot"), _L("Capture a configuration snapshot"));
          local_menu->Append(config_id_base + ConfigMenuUpdate, _L("Check for Configuration Updates"), _L("Check for configuration updates"));
@@ -94,7 +94,7 @@ Treat NetBSD like Linux.
          //if (DesktopIntegrationDialog::integration_possible())
          local_menu->Append(config_id_base + ConfigMenuDesktopIntegration, _L("Desktop Integration"), _L("Desktop Integration"));    
  #endif //(__linux__) && defined(SLIC3R_DESKTOP_INTEGRATION)        
-@@ -2285,7 +2285,7 @@ void GUI_App::add_config_menu(wxMenuBar 
+@@ -2283,7 +2283,7 @@ void GUI_App::add_config_menu(wxMenuBar 
  		case ConfigMenuUpdate:
  			check_updates(true);
  			break;
@@ -103,7 +103,7 @@ Treat NetBSD like Linux.
          case ConfigMenuDesktopIntegration:
              show_desktop_integration_dialog();
              break;
-@@ -2899,7 +2899,7 @@ bool GUI_App::run_wizard(ConfigWizard::R
+@@ -2905,7 +2905,7 @@ bool GUI_App::run_wizard(ConfigWizard::R
  
  void GUI_App::show_desktop_integration_dialog()
  {
