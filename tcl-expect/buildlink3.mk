d2aee49c7faf96f7e471f676ab5ae6eca61a3afb
Boyd Lynn Gerber <gerberb@zenez.com>
2022-05-04 09:53:04 -0600

    Updated expect5.45.4 with patches for SCO/Xinuos OS's.

--- tcl-expect/buildlink3.mk
+++ tcl-expect/buildlink3.mk
@@ -7,7 +7,7 @@ TCL_EXPECT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.tcl-expect+=	tcl-expect>=5.38.0nb1
 BUILDLINK_ABI_DEPENDS.tcl-expect+=	tcl-expect>=5.45.0nb3
-BUILDLINK_ABI_DEPENDS.tcl-expect+=	tcl-expect>=5.45.4nb3
+BUILDLINK_ABI_DEPENDS.tcl-expect+=	tcl-expect>=5.45.4nb1
 BUILDLINK_PKGSRCDIR.tcl-expect?=	../../wip/tcl-expect
 
 .include "../../lang/tcl/buildlink3.mk"
