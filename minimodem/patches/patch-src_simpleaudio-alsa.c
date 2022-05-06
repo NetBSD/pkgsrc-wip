41f0b58798662df435882a66f600876386461ea9
Thomas Klausner <tk@giga.or.at>
2022-05-06 14:37:08 +0200

    minimodem: add new package
    Minimodem is a command-line program which decodes (or generates) audio
    modem tones at any specified baud rate, using various framing protocols.
    It acts a general-purpose software FSK modem, and includes support for
    various standard FSK protocols such as Bell103, Bell202, RTTY, TTY/TDD,
    NOAA SAME, and Caller-ID.
    
    Minimodem can play and capture audio modem tones in real-time via the
    system audio device, or in batched mode via audio files.
    
    Minimodem can be used to transfer data between nearby computers using an
    audio cable (or just via sound waves), or between remote computers using
    radio, telephone, or another audio communications medium.

---
+++ minimodem/patches/patch-src_simpleaudio-alsa.c
@@ -0,0 +1,18 @@
+$NetBSD$
+
+Non-Linux portability.
+
+--- src/simpleaudio-alsa.c.orig	2015-04-22 23:56:58.000000000 +0000
++++ src/simpleaudio-alsa.c
+@@ -59,7 +59,11 @@ sa_alsa_read( simpleaudio *sa, void *buf
+ 	    snd_pcm_prepare(pcm);
+ 	} else  {
+ 	    fprintf(stderr, "snd_pcm_readi: %s\n", snd_strerror(r));
++#if defined(ESTRPIPE)
+ 	    if (r == -EAGAIN || r== -ESTRPIPE)
++#else
++	    if (r == -EAGAIN)
++#endif
+ 		snd_pcm_wait(pcm, 1000);
+ 	    else
+ 		return r;
