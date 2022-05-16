5c8a552378a868042241aab6a7af3a14a2a7e102
Havard Eidnes <he@NetBSD.org>
2022-05-16 22:10:18 +0000

    rust: make RUST_BACKTRACE work on NetBSD as well.
    Thanks to rvp at https://www.unitedbsd.com/d/815-rust-backtrace/
    for the relevant hints.

---
+++ rust/patches/patch-library_backtrace_src_symbolize_gimli.rs
@@ -0,0 +1,14 @@
+$NetBSD$
+
+Add NetBSD to the family who uses dl_iterate_phdr().
+
+--- library/backtrace/src/symbolize/gimli.rs.orig	2022-04-04 09:41:26.000000000 +0000
++++ library/backtrace/src/symbolize/gimli.rs
+@@ -177,6 +177,7 @@ cfg_if::cfg_if! {
+             target_os = "linux",
+             target_os = "fuchsia",
+             target_os = "freebsd",
++            target_os = "netbsd",
+             target_os = "openbsd",
+             all(target_os = "android", feature = "dl_iterate_phdr"),
+         ),
