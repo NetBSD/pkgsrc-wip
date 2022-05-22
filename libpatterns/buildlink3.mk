42f8b966a4e711e382c505e2b66b7c77c355dd97
Charlotte Koch <charlotte@NetBSD.org>
2022-05-21 23:16:25 -0700

    libpatterns: Update to 1.0.0

--- libpatterns/buildlink3.mk
+++ libpatterns/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libpatterns
 .if !defined(libpatterns_BUILDLINK3_MK)
 libpatterns_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libpatterns+=	libpatterns>=0.1.0
+BUILDLINK_API_DEPENDS.libpatterns+=	libpatterns>=1.0.0
 BUILDLINK_PKGSRCDIR.libpatterns?=	../../wip/libpatterns
 
 .endif # libpatterns_BUILDLINK3_MK
