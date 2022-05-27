cba93dfe17b04f29d198011d700d2082cdefc17c
Atsushi Toyokura <asteria.at@gmail.com>
2022-05-27 22:37:41 +0900

    php-basercms: align with www/php-basercms

--- php-basercms/options.mk
+++ php-basercms/options.mk
@@ -3,9 +3,9 @@
 PKG_OPTIONS_VAR=	PKG_OPTIONS.php-basercms
 
 PKG_OPTIONS_REQUIRED_GROUPS=	db
-PKG_OPTIONS_GROUP.db=		mysql sqlite3 pgsql
+PKG_OPTIONS_GROUP.db=		mysql pgsql sqlite
 
-PKG_SUGGESTED_OPTIONS=	sqlite3
+PKG_SUGGESTED_OPTIONS=	mysql
 
 .include "../../mk/bsd.options.mk"
 
@@ -14,7 +14,7 @@ PKG_SUGGESTED_OPTIONS=	sqlite3
 ###
 .if !empty(PKG_OPTIONS:Mmysql)
 DEPENDS+=	${PHP_PKG_PREFIX}-pdo_mysql>=5.6.0:../../databases/php-pdo_mysql
-.elif !empty(PKG_OPTIONS:Msqlite3)
+.elif !empty(PKG_OPTIONS:Msqlite)
 DEPENDS+=	${PHP_PKG_PREFIX}-pdo_sqlite>=5.6.0:../../databases/php-pdo_sqlite
 DEPENDS+=	${PHP_PKG_PREFIX}-sqlite3>=5.6.0:../../databases/php-sqlite3
 .elif !empty(PKG_OPTIONS:Mpgsql)
