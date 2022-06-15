ab96fcc11853bddd3bff7f6d97e1d043a1ffd791
Greg Troxel <gdt@lexort.com>
2022-06-15 06:55:56 -0400

    unison-snapshot: Update to 2.52.60.0
    The big user-visible change is switching to lablgtk3.

--- unison-snapshot/options.mk
+++ unison-snapshot/options.mk
@@ -6,12 +6,11 @@ PKG_SUPPORTED_OPTIONS=	lablgtk
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mlablgtk)
-MAKE_FLAGS+=	UISTYLE=gtk2
+MAKE_FLAGS+=	UISTYLE=gtk3
 
 DEPENDS+=	font-schumacher-misc>=1.0:../../fonts/font-schumacher-misc
 
-BUILDLINK_API_DEPENDS.ocaml-lablgtk+= ocaml-lablgtk>=2.16.0
-.include "../../x11/ocaml-lablgtk/buildlink3.mk"
+.include "../../x11/ocaml-lablgtk3/buildlink3.mk"
 .else
 MAKE_FLAGS+=	UISTYLE=text
 .endif
