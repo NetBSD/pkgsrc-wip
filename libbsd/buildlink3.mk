839778a1d58df35f687d41bbd1510889fd15025f
Paolo Vincenzo Olivo <vms@retrobsd.ddns.net>
2022-06-30 16:32:04 +0200

    devel/libbsd: update to 11.6.0

--- libbsd/buildlink3.mk
+++ libbsd/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	libbsd
 .if !defined(LIBBSD_BUILDLINK3_MK)
 LIBBSD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.11.5
-BUILDLINK_PKGSRCDIR.libbsd?=	../../wip/libbsd
+BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.11.6
+BUILDLINK_PKGSRCDIR.libbsd?=	../../devel/libbsd
 .endif	# LIBBSD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libbsd
