715d7d67c5eeb99d820694334da604caf47999ca
Paolo Vincenzo Olivo <vms@retrobsd.ddns.net>
2022-06-30 15:36:07 +0200

    devel/libbsd: import libbsd-0.11.5 as wip/libbsd

---
+++ libbsd/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libbsd
+
+.if !defined(LIBBSD_BUILDLINK3_MK)
+LIBBSD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.11.5
+BUILDLINK_PKGSRCDIR.libbsd?=	../../wip/libbsd
+.endif	# LIBBSD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libbsd
