cc344e8053c3973d62b920904342b08b18ab0a43
mayuresh <mayuresh@acm.org>
2022-07-04 14:33:52 +0530

    build completes but see TODO

---
+++ ltsmin/patches/patch-src_pins-lib_modules_dve-pins.c
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/pins-lib/modules/dve-pins.c.orig	2018-07-03 19:28:10.000000000 +0000
++++ src/pins-lib/modules/dve-pins.c
+@@ -6,6 +6,7 @@
+ #include <stdlib.h>
+ #include <sys/types.h>
+ #include <sys/stat.h>
++#include <sys/wait.h>
+ #include <unistd.h>
+ 
+ #include <dm/dm.h>
---
+++ ltsmin/patches/patch-src_pins2lts-sym_pins2lts-sym.c
@@ -0,0 +1,14 @@
+$NetBSD$
+
+--- src/pins2lts-sym/pins2lts-sym.c.orig	2018-07-03 19:28:10.000000000 +0000
++++ src/pins2lts-sym/pins2lts-sym.c
+@@ -1,7 +1,9 @@
+ #include <hre/config.h>
+ 
+ #include <float.h>
++#ifdef __APPLE__
+ #include <alloca.h>
++#endif
+ #include <assert.h>
+ #include <dirent.h>
+ #include <limits.h>
