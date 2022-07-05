4595700130d6b1122e02fe00a3d23f8a653dfe59
mayuresh <mayuresh@acm.org>
2022-07-05 14:43:30 +0530

    patches trimmed thanks to comments by Roland

--- ltsmin/patches/patch-src_hre_hre__pthread.c
+++ ltsmin/patches/patch-src_hre_hre__pthread.c
@@ -2,36 +2,18 @@ $NetBSD$
 
 --- src/hre/hre_pthread.c.orig	2018-07-03 19:28:10.000000000 +0000
 +++ src/hre/hre_pthread.c
-@@ -255,12 +255,20 @@ create_shared_region(size_t size, bool p
-     }
-     int flag = public ? PTHREAD_PROCESS_SHARED : PTHREAD_PROCESS_PRIVATE;
-     pthread_mutexattr_init(&shared->mutexattr);
-+#ifndef __NetBSD__
-     if (pthread_mutexattr_setpshared(&shared->mutexattr, flag)){
-+#else
-+    if (1) {
+@@ -17,6 +17,14 @@
+ 
+ #include <hre/provider.h>
+ 
++#if defined(__NetBSD__)
++#  include <sys/param.h>
++#  if !__NetBSD_Prereq__(11, 0, 0)
++#    define pthread_mutexattr_setpshared(attr, flag) 1
++#    define pthread_condattr_setpshared(attr, flag) 1
++#  endif
 +#endif
-         AbortCall("pthread_mutexattr_setpshared");
-     }
-     pthread_mutex_init(&shared->mutex,&shared->mutexattr);
-     pthread_condattr_init(&shared->condattr);
-+#ifndef __NetBSD__
-     if (pthread_condattr_setpshared(&shared->condattr, flag)){
-+#else
-+    if (1) {
-+#endif
-         AbortCall("pthread_condattr_setpshared");
-     }
-     return shared;
-@@ -583,7 +591,11 @@ void HREenableFork(int procs, bool selec
-     Debug("Enabling process runtime environment.");
-     pthread_condattr_t attr;
-     pthread_condattr_init(&attr);
-+#ifndef __NetBSD__
-     int res=pthread_condattr_setpshared(&attr,PTHREAD_PROCESS_SHARED);
-+#else
-+    int res = 1;
-+#endif
-     pthread_condattr_destroy(&attr);
-     if (res){
-         Warning(infoLong,"multi-process disabled: inter process locks are not supported");
++
+ /**
+  * For pthreads we only use a small shared region for the queues.
+  * For forks all shared objects are allocated on the region, so we try to claim
