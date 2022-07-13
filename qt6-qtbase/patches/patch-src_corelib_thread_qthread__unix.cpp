98f2f0557b1bf5f856bfc5ccd3880eafcb902536
Niclas Rosenvik <nros@pkgsrc.org>
2022-07-13 22:06:34 +0200

    qt6-qtbase: fix crash of qt6 apps that uses threads
    qt6-qtbase: fix crash of qt6 apps that uses threads.
    clear_thread_data does not ensure that current_thread_datakey
    is obtained from pthread_key_create use set_thread_data to
    ensure it is.

---
+++ qt6-qtbase/patches/patch-src_corelib_thread_qthread__unix.cpp
@@ -0,0 +1,18 @@
+$NetBSD$
+
+* Make sure current_thread_data_key has been obtained from pthread_key_create
+  before before it, set_thread_data ensures this. Fixes crash when closing threaded
+  Qt6 apps on NetBSD.
+
+--- src/corelib/thread/qthread_unix.cpp.orig	2022-07-13 13:11:12.532990932 +0000
++++ src/corelib/thread/qthread_unix.cpp
+@@ -171,8 +171,7 @@ static void set_thread_data(QThreadData 
+ 
+ static void clear_thread_data()
+ {
+-    currentThreadData = nullptr;
+-    pthread_setspecific(current_thread_data_key, nullptr);
++    set_thread_data(nullptr);
+ }
+ 
+ template <typename T>
