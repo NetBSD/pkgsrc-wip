a263ba8ddd315e700ef240b1c3b16fc57ccfa436
mayuresh <mayuresh@acm.org>
2022-07-14 11:01:16 +0530

    buddy: buildlink added

---
+++ buddy/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:24:10 joerg Exp $
+
+BUILDLINK_TREE+=	buddy
+
+.if !defined(BUDDY_BUILDLINK3_MK)
+BUDDY_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.buddy+=	buddy>=2.4
+BUILDLINK_PKGSRCDIR.buddy?=	../../wip/buddy
+.endif # BUDDY_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-buddy
