9745fd058e5ea00054531311cae0856a5642ef19
Jonathan Perkin <jonathan@perkin.org.uk>
2022-07-14 09:53:52 +0100

    rust: Fix illumos build + install.

--- rust/patches/patch-src_bootstrap_builder.rs
+++ rust/patches/patch-src_bootstrap_builder.rs
@@ -2,12 +2,12 @@ $NetBSD: patch-src_bootstrap_builder.rs,v 1.11 2020/07/06 13:56:32 he Exp $
 
 Do not install 'src'.
 Use @PREFIX@, not $ORIGIN in rpath.
-
 Fix RPATH for pkgsrc.
+Pull in upstream commit 5122bb55 to fix install on illumos.
 
---- src/bootstrap/builder.rs.orig	2021-09-06 18:42:35.000000000 +0000
+--- src/bootstrap/builder.rs.orig	2022-06-27 13:37:07.000000000 +0000
 +++ src/bootstrap/builder.rs
-@@ -520,7 +520,6 @@ impl<'a> Builder<'a> {
+@@ -659,7 +659,6 @@ impl<'a> Builder<'a> {
                  install::Clippy,
                  install::Miri,
                  install::Analysis,
@@ -15,7 +15,7 @@ Fix RPATH for pkgsrc.
                  install::Rustc
              ),
              Kind::Run => describe!(run::ExpandYamlAnchors, run::BuildManifest, run::BumpStage0),
-@@ -1186,7 +1185,7 @@ impl<'a> Builder<'a> {
+@@ -1370,7 +1369,7 @@ impl<'a> Builder<'a> {
                  Some("-Wl,-rpath,@loader_path/../lib")
              } else if !target.contains("windows") {
                  rustflags.arg("-Clink-args=-Wl,-z,origin");
@@ -24,3 +24,13 @@ Fix RPATH for pkgsrc.
              } else {
                  None
              };
+@@ -1433,7 +1432,8 @@ impl<'a> Builder<'a> {
+             let needs_unstable_opts = target.contains("linux")
+                 || target.contains("windows")
+                 || target.contains("bsd")
+-                || target.contains("dragonfly");
++                || target.contains("dragonfly")
++                || target.contains("illumos");
+ 
+             if needs_unstable_opts {
+                 rustflags.arg("-Zunstable-options");
