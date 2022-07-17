58d7b4778ae1d89d76bf76caa9c00457e4539752
Yorick Hardy <yorickhardy@gmail.com>
2022-07-18 00:52:52 +0200

    urlwatch: update to urlwatch-2.25
    No changelog provided.

--- urlwatch/patches/patch-setup.py
+++ urlwatch/patches/patch-setup.py
@@ -2,16 +2,25 @@ $NetBSD$
 
 Use the pkgsrc directory structure.
 
---- setup.py.orig	2020-12-19 11:27:43.000000000 +0000
+--- setup.py.orig	2022-03-15 13:44:20.000000000 +0000
 +++ setup.py
-@@ -24,8 +24,8 @@ m['package_dir'] = {'': 'lib'}
+@@ -25,16 +25,16 @@ m['package_dir'] = {'': 'lib'}
  m['packages'] = ['urlwatch']
  m['python_requires'] = '>=3.6'
  m['data_files'] = [
--    ('share/man/man1', ['share/man/man1/urlwatch.1']),
--    ('share/urlwatch/examples', [
-+    (os.getenv('PKGMANDIR') + '/man1', ['share/man/man1/urlwatch.1']),
-+    ('share/examples/urlwatch', [
-         'share/urlwatch/examples/hooks.py.example',
-         'share/urlwatch/examples/urls.yaml.example',
+-    ('share/man/man1', [
++    (os.getenv('PKGMANDIR') + '/man1', [
+         'share/man/man1/urlwatch.1',
      ]),
+-    ('share/man/man5', [
++    (os.getenv('PKGMANDIR') + '/man5', [
+         'share/man/man5/urlwatch-config.5',
+         'share/man/man5/urlwatch-filters.5',
+         'share/man/man5/urlwatch-jobs.5',
+         'share/man/man5/urlwatch-reporters.5',
+     ]),
+-    ('share/man/man7', [
++    (os.getenv('PKGMANDIR') + '/man7', [
+         'share/man/man7/urlwatch-cookbook.7',
+         'share/man/man7/urlwatch-deprecated.7',
+         'share/man/man7/urlwatch-intro.7',
