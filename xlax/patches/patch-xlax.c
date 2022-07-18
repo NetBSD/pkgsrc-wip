b4a865380c83abcd369f3b918b7ef13c7ed6481a
Paolo Vincenzo Olivo <vms@retrobsd.ddns.net>
2022-07-18 11:04:49 +0200

    xlax: use libbsd on Linux to support strlcpy()

--- xlax/patches/patch-xlax.c
+++ xlax/patches/patch-xlax.c
@@ -1,13 +1,18 @@
 $NetBSD$
 
 Fix warnings about prepocessor defined macros. 
+Use libbsd on Linux to support strlcpy().
 
 --- xlax.c.orig	2008-07-31 20:18:25.000000000 +0000
 +++ xlax.c
-@@ -30,6 +30,7 @@
+@@ -30,6 +30,11 @@
   *
   */
  
++#ifdef __linux__
++#include <bsd/string.h>
++#endif
++
 +#include "stddef.h"
  #include "xlax.h"
  #include "vroot.h"
