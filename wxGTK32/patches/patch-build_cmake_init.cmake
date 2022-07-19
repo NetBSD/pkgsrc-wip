68c6b37a541f04f9cbc0a12bdc6b9ac25a3d77cd
Thomas Klausner <wiz@gatalith.at>
2022-07-19 12:13:12 +0200

    wxGTK32: add more upstream bug report links

--- wxGTK32/patches/patch-build_cmake_init.cmake
+++ wxGTK32/patches/patch-build_cmake_init.cmake
@@ -1,6 +1,7 @@
 $NetBSD$
 
 Not defined in NetBSD build, don't know what should be added instead.
+https://github.com/wxWidgets/wxWidgets/issues/22649
 
 --- build/cmake/init.cmake.orig	2022-07-06 14:19:50.000000000 +0000
 +++ build/cmake/init.cmake
