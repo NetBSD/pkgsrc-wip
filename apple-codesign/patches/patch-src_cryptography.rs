993755c0589008fd6bd4e6ed36645d3d09c56c62
Thomas Klausner <wiz@gatalith.at>
2022-08-10 12:09:25 +0200

    apple-codesign: new package
    Works, ready to import if someone finds it useful.
    
    The apple-codesign Rust crate and its corresponding rcodesign CLI
    tool implement code signing for Apple platforms.
    
    It supports the following features:
    
        Signing Mach-O binaries (the executable file format on Apple
        operating systems).
    
        Signing, notarizing, and stapling directory bundles (e.g. .app
        directories).
    
        Signing, notarizing, and stapling XAR archives / .pkg installers.
    
        Signing, notarizing, and stapling disk images / .dmg files.

---
+++ apple-codesign/patches/patch-src_cryptography.rs
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Remove unused include.
+
+--- src/cryptography.rs.orig	2022-08-07 20:37:36.000000000 +0000
++++ src/cryptography.rs
+@@ -10,7 +10,7 @@ use {
+         AppleCodesignError,
+     },
+     bytes::Bytes,
+-    der::{asn1, Decodable, Document, Encodable},
++    der::{asn1, Document, Encodable},
+     elliptic_curve::{
+         sec1::{FromEncodedPoint, ModulusSize, ToEncodedPoint},
+         AffinePoint, Curve, FieldSize, ProjectiveArithmetic, SecretKey as ECSecretKey,
---
+++ apple-codesign/patches/patch-src_macho.rs
@@ -0,0 +1,24 @@
+$NetBSD$
+
+Remove unused member.
+
+--- src/macho.rs.orig	2022-08-07 20:37:36.000000000 +0000
++++ src/macho.rs
+@@ -630,8 +630,6 @@ pub fn semver_to_macho_target_version(ve
+ 
+ /// Represents a semi-parsed Mach[-O] binary.
+ pub struct MachFile<'a> {
+-    data: &'a [u8],
+-
+     machos: Vec<MachOBinary<'a>>,
+ }
+ 
+@@ -663,7 +661,7 @@ impl<'a> MachFile<'a> {
+             }
+         };
+ 
+-        Ok(Self { data, machos })
++        Ok(Self { machos })
+     }
+ 
+     /// Whether this Mach-O data has multiple architectures.
