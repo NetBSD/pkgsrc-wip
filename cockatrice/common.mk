ebf017ccb2542c94127679e99e9ed08dc4eb3da6
Charlotte Koch <charlotte@NetBSD.org>
2022-08-14 14:22:28 -0700

    Combine the common bits between cockatrice and cockatrice-server

---
+++ cockatrice/common.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+DISTNAME=	cockatrice-${PKGVERSION_NOREV}
+CATEGORIES=	games
+MASTER_SITES=	${MASTER_SITE_GITHUB:=Cockatrice/}
+GITHUB_TAG=	2021-01-26-Release-${PKGVERSION_NOREV}
+
+USE_CMAKE=	yes
+USE_LANGUAGES=	c c++
+
+WRKSRC=		${WRKDIR}/Cockatrice-${GITHUB_TAG}
+
+.include "../../devel/protobuf/buildlink3.mk"
+.include "../../graphics/hicolor-icon-theme/buildlink3.mk"
+.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt5-qtwebsockets/buildlink3.mk"
