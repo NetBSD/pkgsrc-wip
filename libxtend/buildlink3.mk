9a4a799b62c628a32c6efe6109791677a404424f
Jason Bacon <bacon@NetBSD.org>
2022-08-20 16:51:57 -0500

    libxtend: Update to 0.1.6.12
    Add xt_dprintf() for systems with no dprintf()

--- libxtend/buildlink3.mk
+++ libxtend/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=		libxtend
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.6.9
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.6.12
 BUILDLINK_PKGSRCDIR.libxtend?=		../../wip/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
