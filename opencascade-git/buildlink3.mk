c82fc9b9de7052b6d871347bdc0a3c2dffe86308
Paul Ripke <stix@stix.id.au>
2022-08-25 18:24:47 +1000

    opencascade-git: Add a new package based on opencascade, tracking git tag 7.6.3.
    Successfully tested as a dependency of the new wip/prusaslicer release candidate.

---
+++ opencascade-git/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	opencascade
+
+.if !defined(OPENCASCADE_BUILDLINK3_MK)
+OPENCASCADE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.opencascade+=	opencascade>=7.0.0
+BUILDLINK_PKGSRCDIR.opencascade?=	../../wip/opencascade-git
+
+.include "../../lang/tcl/buildlink3.mk"
+.include "../../x11/tk/buildlink3.mk"
+.endif	# OPENCASCADE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-opencascade
