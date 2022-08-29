0c0ee609fca39301c3671d02180c79afe8d2789c
Dan Cîrnaț <dan@alt.md>
2022-08-29 19:47:19 +0200

    epiphany: Update wip/epiphany to 42.2
    42.2 - April 21, 2022
    =====================
    
     * Fix Save As context menu items (#1760)
     * Fix CVE-2022-29536 (#1766)
    
    42.1 - April 12, 2022
    =====================
    
     * Fix regressions in "ask on download" dialog (#1747, #1749)
     * Fix bookmarks tag label alignment (from !1074)
     * Fix blurry favicons in bookmarks popover (!1101)
    
    42.0 - March 17, 2022
    =====================
    
     * Fix crashes when loading broken session state (#1686)
     * Fix crashes when entering reader mode with JS disabled (#1698)
     * Rework search engine manager code (!1055, Vanadiae)
     * Fix various memory leaks (!1071)
     * Fix icons on Synced Tabs dialog (!1077)
     * Update appdata screenshot (!1079, Sophie Herold)
     * Updated translations
    
    42.beta - February 11, 2022
    ===========================
    
     * Update to latest readability.js (!1048)
     * Fix label for Show downloads list shortcut on shortcuts dialog (!1054, Vanadiae)
     * Update to highlight.js 11.4.0 (!1056)
     * Update to PDF.js 2.12.313 (!1057)
     * Fix handling of mailto: URIs with multiple emails (!1065, Vanadiae)
     * Don't depend on web-browser-symbolic icon (!1069, Vanadiae)
    
    42.alpha - January 6, 2022
    ==========================
    
     * Rename metainfo from "GNOME Web" to "Web" (!1016, Sophie Herold)
     * Update to PDF.js 2.11.338 (!1017)
     * New network error icon (!1018, Jakub Steiner)
     * Support system dark preference (!1019, !1029, !1030)
     * Leak fixes (!1020), more leak fixes (!1042, Phaedrus Leeds)
     * Update to highlight.js 11.3.1 (!1022)
     * Enable accelerated compositing mode always (!1028)
     * Fix build with meson 0.60.0 (!1031, rvalue)
     * Many cleanups in preparation for GTK 4 (!1034, !1035, !1038)
     * Web app delete button is now red (!1040)
     * Detect Snap sandbox (!1041, Phaedrus Leeds)
     * Cleanup icons (!1043, Sam Hewitt)
     * Allow opening URIs via desktop actions (!1049, Sonny Piers)
     * Port to libportal 0.5 (!1051, Patrick Griffis)
     * Various search engine fixes (!1052, Vanadiae)
    
    41.3 - December 20, 2021
    ========================
    
     * Fix Reload buttons on error pages (!1050)
     * Fix delete button in about:applications (!1050)
    
    41.2 - December 16, 2021
    ========================
    
     * Fix reader mode (!1047)
    
    41.1 - December 16, 2021
    ========================
    
     * Fix crash opening PDFs not loaded via HTTP (#1611)
     * Fix CVE-2021-45085, CVE-2021-45086, CVE-2021-45087, CVE-2021-45088 (#1612)
     * Fix web applications with non-Latin characters (#1626, #1627)
     * Fix close button position in Firefox Sync dialog (#1647)
     * Properly quote desktop file Exec line (!1013, Martin Puppe)
     * Somewhat improve password manager robustness (!1014)
     * Use correct tab close button position (!1025)
     * Various fixes for Firefox bookmark import (!1036)

--- epiphany/patches/patch-lib_ephy-sync-utils.c
+++ epiphany/patches/patch-lib_ephy-sync-utils.c
@@ -1,13 +1,24 @@
-$NetBSD$
+$NetBSD: patch-lib_ephy-sync-utils.c,v 1.2 2022/03/10 21:28:03 prlw1 Exp $
 
---- lib/ephy-sync-utils.c.orig	2021-03-19 19:44:15.365009000 +0000
+NetBSD does not have getrandom().
+
+--- lib/ephy-sync-utils.c.orig	2021-12-21 00:24:23.810496800 +0000
 +++ lib/ephy-sync-utils.c
-@@ -187,6 +187,8 @@ ephy_sync_utils_generate_random_bytes (v
+@@ -32,7 +32,7 @@
+ #include <string.h>
+ #if defined(__linux__)
+ #include <sys/random.h>
+-#elif defined(__FreeBSD__) || defined(__OpenBSD__)
++#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+ #include <unistd.h>
+ #endif
+ 
+@@ -187,7 +187,7 @@ ephy_sync_utils_generate_random_bytes (v
    if (getentropy (out, num_bytes) == -1) {
      g_error ("Failed to get entropy: %s", g_strerror (errno));
    }
-+#elif defined(__NetBSD__)
-+  arc4random_buf (out, num_bytes); 
- #else
+-#else
++#elif !defined(__NetBSD__)
    do {
      ret = getrandom (out, num_bytes, 0);
+   } while (ret < (gssize)num_bytes && errno == EINTR);
