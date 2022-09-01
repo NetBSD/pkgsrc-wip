278e48fc655730e4f01d758c4bc9ecb6012c73dd
Niclas Rosenvik <nros@pkgsrc.org>
2022-09-01 17:20:17 +0200

    qt6-qtbase: add comment to patch about it being sent upstream

--- qt6-qtbase/patches/patch-src_corelib_thread_qthread__unix.cpp
+++ qt6-qtbase/patches/patch-src_corelib_thread_qthread__unix.cpp
@@ -4,6 +4,9 @@ $NetBSD$
   before before it, set_thread_data ensures this. Fixes crash when closing threaded
   Qt6 apps on NetBSD.
 
+  Sent to and applied upstream:
+  https://codereview.qt-project.org/c/qt/qtbase/+/421914
+
 --- src/corelib/thread/qthread_unix.cpp.orig	2022-07-13 13:11:12.532990932 +0000
 +++ src/corelib/thread/qthread_unix.cpp
 @@ -171,8 +171,7 @@ static void set_thread_data(QThreadData 
