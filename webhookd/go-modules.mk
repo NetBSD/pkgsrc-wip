349b1e86c3a01884c8ad50e25a4ec89b68ef4211
Leonardo Taccari <leot@NetBSD.org>
2022-09-12 14:10:29 +0200

    webhookd: Import webhookd-1.15.0
    A very simple webhook server to launch shell scripts.

---
+++ webhookd/go-modules.mk
@@ -0,0 +1,21 @@
+# $NetBSD$
+
+GO_MODULE_FILES+=	github.com/go-fed/httpsig/@v/v1.1.0.mod
+GO_MODULE_FILES+=	github.com/go-fed/httpsig/@v/v1.1.0.zip
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.14.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.14.zip
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20190308221718-c2843e01d9a2.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20200622213623-75b288015ac9.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20211202192323-5770296d904e.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20211202192323-5770296d904e.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20190404232315-eb5bcb51f2a3.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20211112202133-69e39bad7dc2.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20211112202133-69e39bad7dc2.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190215142949-d0b11bdaac8a.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20190412213103-97732733099d.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210630005230-0f9fa26af87c.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210630005230-0f9fa26af87c.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.6.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.6.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180917221912-90fa682c2a6e.mod
