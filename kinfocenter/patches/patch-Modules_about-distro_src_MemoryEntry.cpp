0aaaeedced9c43b51f8a5613fe90e41bc4d401b7
Mark Davies <mark@ecs.vuw.ac.nz>
2022-09-29 16:23:45 +1300

    kinfocenter: update to 5.24.6

--- kinfocenter/patches/patch-Modules_about-distro_src_MemoryEntry.cpp
+++ kinfocenter/patches/patch-Modules_about-distro_src_MemoryEntry.cpp
@@ -2,18 +2,18 @@ $NetBSD$
 
 get Memory for NetBSD
 
---- Modules/about-distro/src/Module.cpp.orig	2020-05-05 14:39:33.000000000 +0000
-+++ Modules/about-distro/src/Module.cpp
-@@ -41,7 +41,7 @@
+--- Modules/about-distro/src/MemoryEntry.cpp.orig	2022-09-20 21:37:31.837549485 +0000
++++ Modules/about-distro/src/MemoryEntry.cpp
+@@ -9,7 +9,7 @@
  
  #ifdef Q_OS_LINUX
  #include <sys/sysinfo.h>
 -#elif defined(Q_OS_FREEBSD)
 +#elif defined(Q_OS_FREEBSD) || defined(Q_OS_NETBSD)
+ // clang-format off
  #include <sys/types.h>
  #include <sys/sysctl.h>
- #endif
-@@ -66,6 +66,15 @@ static qlonglong calculateTotalRam()
+@@ -38,6 +38,15 @@ qlonglong MemoryEntry::calculateTotalRam
      sysctlbyname("hw.physmem", &memory, &len, NULL, 0);
  
      ret = memory;
