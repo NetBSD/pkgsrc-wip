c64f95e00e75c6adc1cf28ab3074f07d055e88dc
Adam Ciarciński <aciarcinski@teonite.com>
2022-10-03 09:01:18 +0200

    Added flang, mlir

---
+++ mlir/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	mlir
+
+.if !defined(MLIR_BUILDLINK3_MK)
+MLIR_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.mlir+=	mlir>=14.0.6
+BUILDLINK_PKGSRCDIR.mlir?=	../../wip/mlir
+
+BUILDLINK_FILES.mlir+=		bin/mlir-tblgen
+
+.include "../../lang/llvm/buildlink3.mk"
+.endif	# MLIR_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-mlir
