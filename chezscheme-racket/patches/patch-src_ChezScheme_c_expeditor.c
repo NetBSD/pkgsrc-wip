ee33f780731a13e07982eb786e8670bc23d3993c
Johann Rudloff <johann@sinyax.net>
2022-10-13 17:12:59 +0200

    chezscheme-racket: update to version 9.5.9.2
    This is the version of Chez Scheme included in the Racket 8.6 release.

--- chezscheme-racket/patches/patch-src_ChezScheme_c_expeditor.c
+++ chezscheme-racket/patches/patch-src_ChezScheme_c_expeditor.c
@@ -3,9 +3,9 @@ $NetBSD$
 * The special case for NetBSD is not required, but prevents term.h from being
   found
 
---- src/ChezScheme/c/expeditor.c.orig	2021-10-24 04:17:58.000000000 +0000
+--- src/ChezScheme/c/expeditor.c.orig	2022-07-08 06:15:57.000000000 +0000
 +++ src/ChezScheme/c/expeditor.c
-@@ -539,9 +539,6 @@ static void s_ee_write_char(wchar_t c) {
+@@ -654,9 +654,6 @@ static void s_ee_set_color(int color_id,
  # define CHTYPE int
  # include </usr/include/curses.h>
  # include </usr/include/term.h>
