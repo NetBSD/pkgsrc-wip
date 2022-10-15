45575a612dd0529aca055102d5dc75b95ef58691
Andrius Varanavicius <vezhlys@netscape.net>
2022-10-15 13:54:43 +0300

    codelite: apply patch from upstream to fix myxh_progrid.cpp build failure.

--- codelite/patches/patch-wxcrafter_myxh__propgrid.cpp
+++ codelite/patches/patch-wxcrafter_myxh__propgrid.cpp
@@ -1,14 +1,15 @@
+$NetBSD$
 
---- wxcrafter/myxh_propgrid.cpp.orig	2022-10-15 00:18:54.895365650 +0300
+Fix from upstream: SetSplitterLeft was internal function in wxWidgets and became protected.
+
+--- wxcrafter/myxh_propgrid.cpp.orig	2022-04-18 08:09:00.000000000 +0300
 +++ wxcrafter/myxh_propgrid.cpp
-@@ -47,8 +47,8 @@ wxObject* MyWxPropGridXmlHandler::DoCrea
-         CreateChildren(m_pgmgr, true /*only this handler*/);
+@@ -48,7 +48,7 @@ wxObject* MyWxPropGridXmlHandler::DoCrea
  
          // add the splitter property after the children were added
--        int splitterLeft = GetBool("splitterleft");
+         int splitterLeft = GetBool("splitterleft");
 -        if(splitterLeft) { m_pgmgr->GetPage(0)->SetSplitterLeft(); }
-+        //int splitterLeft = GetBool("splitterleft");
-+        //if(splitterLeft) { m_pgmgr->GetPage(0)->SetSplitterLeft(); }
++        if(splitterLeft) { m_pgmgr->SetSplitterLeft(); }
  
          int splitterPos = GetLong("splitterpos", wxNOT_FOUND);
          if(splitterPos != wxNOT_FOUND) { m_pgmgr->GetPage(0)->SetSplitterPosition(splitterPos); }
