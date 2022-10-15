cadffc7a0cb0b750bfed8907e002d9d7a5398d63
Andrius Varanavicius <vezhlys@netscape.net>
2022-10-15 19:19:50 +0300

    codelite: add RCSID to patches and revise them to rearrange imports instead of duplication.

--- codelite/patches/patch-CodeLite_tags_storage_sqlite3.h
+++ codelite/patches/patch-CodeLite_tags_storage_sqlite3.h
@@ -1,11 +1,21 @@
+$NetBSD$
 
---- CodeLite/tags_storage_sqlite3.h.orig	2022-10-15 00:18:53.652035696 +0300
+--- CodeLite/tags_storage_sqlite3.h	2022-05-03 22:38:34.770034020 +0300
 +++ CodeLite/tags_storage_sqlite3.h
-@@ -25,6 +25,7 @@
+@@ -25,6 +25,8 @@
  #ifndef CODELITE_TAGS_DATABASE_H
  #define CODELITE_TAGS_DATABASE_H
  
 +#include <wx/filename.h>
++
  #include "codelite_exports.h"
  #include "entry.h"
  #include "fileentry.h"
+@@ -33,7 +35,6 @@
+ #include "wxStringHash.h"
+ 
+ #include <unordered_map>
+-#include <wx/filename.h>
+ #include <wx/wxsqlite3.h>
+ 
+ /**
--- codelite/patches/patch-Plugin_DiffSideBySidePanel.h
+++ codelite/patches/patch-Plugin_DiffSideBySidePanel.h
@@ -1,12 +1,21 @@
+$NetBSD$
 
---- Plugin/DiffSideBySidePanel.h.orig	2022-10-15 00:18:53.650326388 +0300
+--- Plugin/DiffSideBySidePanel.h.orig	2022-04-18 08:09:00.000000000 +0300
 +++ Plugin/DiffSideBySidePanel.h
-@@ -26,6 +26,8 @@
+@@ -26,13 +26,15 @@
  #ifndef DIFFSIDEBYSIDEPANEL_H
  #define DIFFSIDEBYSIDEPANEL_H
  
 +#include <wx/filename.h>
++
 +#include "wxcrafter_plugin.h"
  #include "DiffConfig.h"
  #include "clDTL.h"
  #include "clPluginsFindBar.h"
+ #include "wxcrafter_plugin.h"
+ 
+ #include <vector>
+-#include <wx/filename.h>
+ 
+ class clToolBar;
+ class WXDLLIMPEXP_SDK DiffSideBySidePanel : public DiffSideBySidePanelBase
