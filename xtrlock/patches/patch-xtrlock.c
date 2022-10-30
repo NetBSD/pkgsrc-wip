4690e7949c2d7e4ad0c6cb34460a345f4f77caef
Paolo Vincenzo Olivo <vms@retrobsd.ddns.net>
2022-10-30 13:11:02 +0100

    xtrlock: simplify patch (NFC)

--- xtrlock/patches/patch-xtrlock.c
+++ xtrlock/patches/patch-xtrlock.c
@@ -4,19 +4,20 @@ Portability fixes for BSD systems.
 
 --- xtrlock.c.orig	2021-02-13 15:34:52.000000000 +0000
 +++ xtrlock.c
-@@ -31,11 +31,15 @@
+@@ -31,11 +31,16 @@
  #include <grp.h>
  #include <limits.h>
  #include <string.h>
-+#if !(defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__) || defined(__OpenBSD__) || defined(__APPLE__))
++#include <sys/param.h>
++#ifndef BSD
  #include <crypt.h>
 +#endif
  #include <unistd.h>
  #include <math.h>
  #include <ctype.h>
-+#if !(defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__) || defined(__OpenBSD__) || defined(__APPLE__))
++#ifndef BSD
  #include <values.h>
 +#endif
-
+ 
  #ifdef SHADOW_PWD
  #include <shadow.h>
