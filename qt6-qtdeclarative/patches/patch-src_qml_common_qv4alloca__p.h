cf485317bfaa0e0fe7d84e44decc0bf77d84c44c
Niclas Rosenvik <nros@pkgsrc.org>
2022-11-03 09:21:44 +0100

    qt6-qtdeclarative: regenerate patch

--- qt6-qtdeclarative/patches/patch-src_qml_common_qv4alloca__p.h
+++ qt6-qtdeclarative/patches/patch-src_qml_common_qv4alloca__p.h
@@ -2,17 +2,17 @@ $NetBSD: patch-src_qml_common_qv4alloca__p.h,v 1.1 2020/01/21 20:25:37 adam Exp 
 
 Make sure we pick up gcc's builtin alloca on NetBSD
 
---- src/qml/common/qv4alloca_p.h.orig	2017-11-30 15:10:22.000000000 +0000
+--- src/qml/common/qv4alloca_p.h.orig	2022-09-23 07:21:41.000000000 +0000
 +++ src/qml/common/qv4alloca_p.h
-@@ -78,9 +78,13 @@
+@@ -42,9 +42,13 @@
  #define Q_ALLOCA_DECLARE(type, name) \
      type *name = 0
  
 +#if defined(__GNUC__)
-+#define Q_ALLOCA_ASSIGN(type, name, size) \
+ #define Q_ALLOCA_ASSIGN(type, name, size) \
 +    name = static_cast<type*>(__builtin_alloca(size))
 +#else
- #define Q_ALLOCA_ASSIGN(type, name, size) \
++#define Q_ALLOCA_ASSIGN(type, name, size) \
      name = static_cast<type*>(alloca(size))
 -
 +#endif
