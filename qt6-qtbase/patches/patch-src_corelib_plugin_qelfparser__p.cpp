7bd11e1fea2279951afdc7bd6d6d5a2ed5aaf900
Niclas Rosenvik <nros@pkgsrc.org>
2022-11-03 13:38:16 +0100

    qt6-qtbase: make a better patch for fixing missing ELFOSABI_GNU

--- qt6-qtbase/patches/patch-src_corelib_plugin_qelfparser__p.cpp
+++ qt6-qtbase/patches/patch-src_corelib_plugin_qelfparser__p.cpp
@@ -3,18 +3,17 @@ $NetBSD$
 * Support older ELFOSABI_LINUX that has been replaced with ELFOSABI_GNU.
   Needed on NetBSD 9.
 
---- src/corelib/plugin/qelfparser_p.cpp.orig	2022-10-04 19:58:26.404142170 +0000
+--- src/corelib/plugin/qelfparser_p.cpp.orig	2022-09-19 10:23:04.000000000 +0000
 +++ src/corelib/plugin/qelfparser_p.cpp
-@@ -329,7 +329,11 @@ Q_DECL_UNUSED Q_DECL_COLD_FUNCTION stati
-     case ELFOSABI_SYSV:     d << " (SYSV"; break;
-     case ELFOSABI_HPUX:     d << " (HP-UX"; break;
-     case ELFOSABI_NETBSD:   d << " (NetBSD"; break;
--    case ELFOSABI_GNU:      d << " (GNU/Linux"; break;
-+#if defined(ELFOSABI_LINUX)
-+    case ELFOSABI_LINUX:    d << " (GNU/Linux"; break;
-+#else
-+    case ELFOSABI_GNU:      d << " (GNU/Linux"; break;   
+@@ -20,6 +20,11 @@
+ #  error "Need ELF header to parse plugins."
+ #endif
+ 
++// support older ELFOSABI define for GNU/Linux
++#if !defined(ELFOSABI_GNU) && defined(ELFOSABI_LINUX)
++#define ELFOSABI_GNU ELFOSABI_LINUX
 +#endif
-     case ELFOSABI_SOLARIS:  d << " (Solaris"; break;
-     case ELFOSABI_AIX:      d << " (AIX"; break;
-     case ELFOSABI_IRIX:     d << " (IRIX"; break;
++
+ QT_BEGIN_NAMESPACE
+ 
+ using namespace Qt::StringLiterals;
