4b91691ee51080e5464c46c65e213afacd0a1394
Thomas Klausner <wiz@gatalith.at>
2022-11-11 19:43:57 +0100

    Revert "+ Update cddlib"
    This reverts commit e4903efd05964cc177ce4d52a318502505b775ae.
    
    Committed the files in the wrong directory, breaking the top-level Makefile.

--- cddlib/buildlink3.mk
+++ cddlib/buildlink3.mk
@@ -1,17 +1,14 @@
 # $NetBSD$
-# XXX
 
 BUILDLINK_TREE+=	cddlib
 
 .if !defined(CDDLIB_BUILDLINK3_MK)
 CDDLIB_BUILDLINK3_MK:=
 
-BUILDLINK_DEPMETHOD.cddlib?=	build
-
-BUILDLINK_API_DEPENDS.cddlib+=	cddlib>=0.94m
+BUILDLINK_API_DEPENDS.cddlib+=	cddlib>=0.94j
 BUILDLINK_PKGSRCDIR.cddlib?=	../../wip/cddlib
 
-.include "../../devel/gmp/buildlink3.mk"
+#.include "../../devel/gmp/buildlink3.mk"
 .endif	# CDDLIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-cddlib
