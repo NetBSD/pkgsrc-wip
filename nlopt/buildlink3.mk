441133131b050b3d2e92a5f2995f112958b33c19
Paul Ripke <stix@stix.id.au>
2022-11-11 23:59:39 +1100

    nlopt: Expose language API support through packacge options.
    TODO:
    - GUILE support broken.
    - Octave version extraction for PLIST.

--- nlopt/buildlink3.mk
+++ nlopt/buildlink3.mk
@@ -5,9 +5,28 @@ BUILDLINK_TREE+=	nlopt
 .if !defined(NLOPT_BUILDLINK3_MK)
 NLOPT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.nlopt+=	nlopt>=2.6.2
-BUILDLINK_ABI_DEPENDS.nlopt+=	nlopt>=2.6.2
+BUILDLINK_API_DEPENDS.nlopt+=	nlopt>=2.7.1
+BUILDLINK_ABI_DEPENDS.nlopt+=	nlopt>=2.7.1
 BUILDLINK_PKGSRCDIR.nlopt?=	../../wip/nlopt
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+pkgbase := nlopt
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.nlopt:Mguile}
+.  include "../../lang/guile30/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.nlopt:Moctave}
+.  include "../../math/octave/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.nlopt:Mpython}
+.  include "../../lang/python/extension.mk"
+.endif
+
+.include "../../devel/gmp/buildlink3.mk"
 .endif # NLOPT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-nlopt
---
+++ nlopt/options.mk
@@ -0,0 +1,42 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.nlopt
+PKG_SUPPORTED_OPTIONS=		octave python
+PKG_SUGGESTED_OPTIONS=
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_SRC=		PLIST
+
+CMAKE_ARGS+=		-DNLOPT_SWIG:BOOL=TRUE
+CMAKE_ARGS+=		-DNLOPT_FORTRAN:BOOL=FALSE	# just builds tests
+CMAKE_ARGS+=		-DNLOPT_MATLAB:BOOL=FALSE
+
+# GUILE fails to be found by cmake?
+#.if !empty(PKG_OPTIONS:Mguile)
+#.include "../../lang/guile30/buildlink3.mk"
+#CMAKE_ARGS+=		-DNLOPT_GUILE:BOOL=TRUE
+#.else
+#CMAKE_ARGS+=		-DNLOPT_GUILE:BOOL=FALSE
+#.endif
+
+
+.if !empty(PKG_OPTIONS:Moctave)
+.include "../../math/octave/buildlink3.mk"
+CMAKE_ARGS+=		-DNLOPT_OCTAVE:BOOL=TRUE
+PLIST_SRC+=		PLIST.octave
+PLIST_SUBST+=		OCTAVE_VERSION=6.4.0
+.else
+CMAKE_ARGS+=		-DNLOPT_OCTAVE:BOOL=FALSE
+.endif
+
+
+.if !empty(PKG_OPTIONS:Mpython)
+.include "../../lang/python/extension.mk"
+.include "../../math/py-numpy/buildlink3.mk"
+CMAKE_ARGS+=		-DNLOPT_PYTHON:BOOL=TRUE
+PLIST_SRC+=		PLIST.python
+PLIST_SUBST+=		PYSITELIB=${PYSITELIB}
+.else
+CMAKE_ARGS+=		-DNLOPT_PYTHON:BOOL=FALSE
+.endif
