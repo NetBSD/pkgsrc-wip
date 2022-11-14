2b2e514dafebf889ae2a2618ff61159610981ce9
Charlotte Koch <charlotte@NetBSD.org>
2022-11-13 22:36:57 -0800

    libdivecomputer: Simpler patch recommended by upstream

--- libdivecomputer/patches/patch-src_serial_posix.c
+++ libdivecomputer/patches/patch-src_serial_posix.c
@@ -1,16 +1,10 @@
-$NetBSD$
-
-Check for another header where select(2) might be located.
-
---- src/serial_posix.c.orig	2021-05-07 12:23:36.000000000 -0700
-+++ src/serial_posix.c	2022-11-11 23:11:32.355847371 -0800
-@@ -33,6 +33,9 @@
+--- src/serial_posix.c.orig	2022-11-13 22:34:30.225666053 -0800
++++ src/serial_posix.c	2022-11-13 22:35:04.795132804 -0800
+@@ -30,6 +30,7 @@
+ #include <fcntl.h>	// fcntl
+ #include <termios.h>	// tcgetattr, tcsetattr, cfsetispeed, cfsetospeed, tcflush, tcsendbreak
+ #include <sys/ioctl.h>	// ioctl
++#include <sys/select.h>	// select
  #ifdef HAVE_LINUX_SERIAL_H
  #include <linux/serial.h>
- #endif
-+#ifdef HAVE_SYS_SELECT_H
-+#include <sys/select.h>
-+#endif
- #ifdef HAVE_IOKIT_SERIAL_IOSS_H
- #include <IOKit/serial/ioss.h>
  #endif
