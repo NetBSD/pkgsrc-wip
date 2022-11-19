5ce674fabfb638e0e596325ae69bc63caddbceb5
Leonardo Taccari <leot@NetBSD.org>
2022-11-19 21:04:43 +0100

    docker-cli: Update to 20.10.21
    pkgsrc changes:
     - Populate version
    
    Changes:
    No changelog available.

--- docker-cli/patches/patch-vendor_github.com_docker_docker_pkg_system_rm.go
+++ docker-cli/patches/patch-vendor_github.com_docker_docker_pkg_system_rm.go
@@ -4,10 +4,12 @@ Fix build for NetBSD.
 
 github.com/moby/sys/mount needs to be ported on NetBSD.
 
---- vendor/github.com/docker/docker/pkg/system/rm.go.orig	2020-12-15 02:27:14.000000000 +0000
+--- vendor/github.com/docker/docker/pkg/system/rm.go.orig	2022-10-25 17:53:02.000000000 +0000
 +++ vendor/github.com/docker/docker/pkg/system/rm.go
-@@ -1,4 +1,4 @@
+@@ -1,5 +1,5 @@
+-//go:build !darwin && !windows
 -// +build !darwin,!windows
++//go:build !darwin && !netbsd && !windows
 +// +build !darwin,!netbsd,!windows
  
  package system // import "github.com/docker/docker/pkg/system"
