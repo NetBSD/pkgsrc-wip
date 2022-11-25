6c8f90f75d584319f4f98ae9c27052888dc3c7a1
K.I.A.Derouiche <kamelderouiche@yahoo.com>
2022-11-25 10:53:26 +0000

    Update m4ri new version

--- m4ri/buildlink3.mk
+++ m4ri/buildlink3.mk
@@ -1,15 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/12/29 22:29:49 jihbed Exp $
+# $NetBSD$
+#
 
 BUILDLINK_TREE+=	m4ri
 
 .if !defined(M4RI_BUILDLINK3_MK)
 M4RI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.m4ri+=	m4ri>=20111203
+BUILDLINK_DEPMETHOD.m4ri?=	build
+
+BUILDLINK_API_DEPENDS.m4ri+=	m4ri>=20200125
 BUILDLINK_PKGSRCDIR.m4ri?=	../../wip/m4ri
 
-
-#.include "../../graphics/png/buildlink3.mk"
+.include "../../parallel/openmp/buildlink3.mk"
 .endif	# M4RI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-m4ri
