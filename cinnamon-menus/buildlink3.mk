0ddec422e2d6d4a1cb5924c9fb20946696f6a954
K.I.A.Derouiche <kamelderouiche@yahoo.com>
2022-11-25 20:58:38 +0000

    new version  cinnamon-menus to 5.6.0

--- cinnamon-menus/buildlink3.mk
+++ cinnamon-menus/buildlink3.mk
@@ -1,13 +1,17 @@
-# $NetBSD: buildlink3.mk,v 1.1 2015/02/19 21:21:12 jihbed Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	cinnamon-menus
 
 .if !defined(CINNAMON_MENUS_BUILDLINK3_MK)
 CINNAMON_MENUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cinnamon-menus+=	cinnamon-menus>=2.2.0
+BUILDLINK_DEPMETHOD.cinnamon-menus?=	build
+
+BUILDLINK_API_DEPENDS.cinnamon-menus+=	cinnamon-menus>=5.6.0
 BUILDLINK_PKGSRCDIR.cinnamon-menus?=	../../wip/cinnamon-menus
 
+
+.include "../../devel/gobject-introspection/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .endif	# CINNAMON_MENUS_BUILDLINK3_MK
 
