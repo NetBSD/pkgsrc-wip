16aa22cd07351ae73d6b77db8ed4f88f7e29bb0a
K.I.A.Derouiche <kamelderouiche@yahoo.com>
2022-11-26 19:50:09 +0000

    xapps: Update new version to 2.4.1

--- xapps/buildlink3.mk
+++ xapps/buildlink3.mk
@@ -5,20 +5,20 @@ BUILDLINK_TREE+=	xapp
 .if !defined(XAPP_BUILDLINK3_MK)
 XAPP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xapp+=	xapp>=2.2.6
-BUILDLINK_PKGSRCDIR.xapp?=	../../wip/xapps
+BUILDLINK_DEPMETHOD.xapp?=	build
 
-.include "../../devel/libdbusmenu-gtk3/buildlink3.mk"
+BUILDLINK_API_DEPENDS.xapp+=	xapp>=2.4.1
+BUILDLINK_PKGSRCDIR.xapp?=	../../category/xapps
+
 .include "../../devel/gobject-introspection/buildlink3.mk"
-.include "../../devel/py-gobject3/buildlink3.mk"
+.include "../../devel/libdbusmenu-gtk3/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/py-gobject3-common/buildlink3.mk"
-.include "../../fonts/fontconfig/buildlink3.mk"
-.include "../../graphics/freetype2/buildlink3.mk"
-.include "../../lang/vala/buildlink3.mk"
+.include "../../sysutils/dbus/buildlink3.mk"
 .include "../../x11/gtk3/buildlink3.mk"
-.include "../../x11/libxklavier/buildlink3.mk"
 .include "../../x11/libgnomekbd/buildlink3.mk"
 .include "../../x11/libxkbfile/buildlink3.mk"
+.include "../../lang/vala/buildlink3.mk"
 .endif	# XAPP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xapp
