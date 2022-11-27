f3833436a83a563de4eef71c0569dc41621c8d58
Greg Troxel <gdt@lexort.com>
2022-11-27 09:17:01 -0500

    proj: Update to 8.1.1

--- proj/buildlink3.mk
+++ proj/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2018/03/02 01:13:10 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2022/11/27 13:30:45 gdt Exp $
 
 BUILDLINK_TREE+=	proj
 
@@ -6,8 +6,11 @@ BUILDLINK_TREE+=	proj
 PROJ_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.proj+=	proj>=5.0.0
-BUILDLINK_ABI_DEPENDS.proj+=	proj>=7.0.0
+BUILDLINK_ABI_DEPENDS.proj+=	proj>=9.0.0
 BUILDLINK_PKGSRCDIR.proj?=	../../wip/proj
+.include "../../databases/sqlite3/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+.include "../../www/curl/buildlink3.mk"
 .endif # PROJ_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-proj
