e53afcba4d29bf5cbb33e660511c527d3cdcdef6
Paolo Vincenzo Olivo <vms@retrobsd.ddns.net>
2022-12-01 10:09:58 +0100

    libbsd: improve buildlink file.

--- libbsd/buildlink3.mk
+++ libbsd/buildlink3.mk
@@ -5,8 +5,13 @@ BUILDLINK_TREE+=	libbsd
 .if !defined(LIBBSD_BUILDLINK3_MK)
 LIBBSD_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.11.6
+BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.11.0
 BUILDLINK_PKGSRCDIR.libbsd?=	../../devel/libbsd
+
+BUILDLINK_CFLAGS.libbsd+=       -I${BUILDLINK_PREFIX.libbsd}/include/bsd -DLIBBSD_OVERLAY
+BUILDLINK_LIBS.libbsd+=         -lbsd -lmd
+
+.include "../../devel/libmd/buildlink3.mk"
 .endif	# LIBBSD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libbsd
