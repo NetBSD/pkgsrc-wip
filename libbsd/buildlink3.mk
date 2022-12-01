e586747637e04216ffd3682248406f330b2d157e
Paolo Vincenzo Olivo <vms@retrobsd.ddns.net>
2022-12-01 13:52:46 +0100

    libbsd: prefer BUILDLINK_LDFLAGS

--- libbsd/buildlink3.mk
+++ libbsd/buildlink3.mk
@@ -8,8 +8,8 @@ LIBBSD_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libbsd+=	libbsd>=0.11.0
 BUILDLINK_PKGSRCDIR.libbsd?=	../../devel/libbsd
 
-BUILDLINK_CFLAGS.libbsd+=       -I${BUILDLINK_PREFIX.libbsd}/include/bsd -DLIBBSD_OVERLAY
-BUILDLINK_LIBS.libbsd+=         -lbsd -lmd
+BUILDLINK_CFLAGS.libbsd+=	-I${BUILDLINK_PREFIX.libbsd}/include/bsd -DLIBBSD_OVERLAY
+BUILDLINK_LDFLAGS.libbsd+=	-lbsd -lmd
 
 .include "../../devel/libmd/buildlink3.mk"
 .endif	# LIBBSD_BUILDLINK3_MK
