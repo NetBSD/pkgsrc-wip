bc9588387874fe0641e32594e838b0be7c50c9bb
K.I.A.Derouiche <kamelderouiche@yahoo.com>
2022-12-02 22:34:50 +0000

    Update granite to 7.1.0

--- granite/buildlink3.mk
+++ granite/buildlink3.mk
@@ -5,17 +5,17 @@ BUILDLINK_TREE+=	granite
 .if !defined(GRANITE_BUILDLINK3_MK)
 GRANITE_BUILDLINK3_MK:=
 
+
 BUILDLINK_DEPMETHOD.granite?=	build
 
-BUILDLINK_API_DEPENDS.granite+=	granite>=5.5.0
+BUILDLINK_API_DEPENDS.granite+=	granite>=7.1.0
 BUILDLINK_PKGSRCDIR.granite?=	../../wip/granite
 
-.include "../../x11/gtk3/buildlink3.mk"
-.include "../../lang/vala/buildlink3.mk"
+
+.include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/libgee/buildlink3.mk"
-.include "../../devel/wayland/buildlink3.mk"
-.include "../../devel/wayland-protocols/buildlink3.mk"
-.include "../../x11/libxkbcommon/buildlink3.mk"
+.include "../../lang/vala/buildlink3.mk"
+.include "../../x11/gtk4/buildlink3.mk"
 .endif	# GRANITE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-granite
