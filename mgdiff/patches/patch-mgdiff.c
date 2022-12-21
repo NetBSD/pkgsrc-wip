aeadb9b36554ae9b6f342f92acff18ca737d8d5e
Paolo Vincenzo Olivo <vms@retrobsd.ddns.net>
2022-12-21 14:58:58 +0100

    mgdiff: fix a couple of compiler warnings.

--- mgdiff/patches/patch-mgdiff.c
+++ mgdiff/patches/patch-mgdiff.c
@@ -4,6 +4,51 @@ Prevent unsafe use of tmpnam().
 
 --- mgdiff.c.orig	1994-09-29 01:56:53.000000000 +0000
 +++ mgdiff.c
+@@ -520,7 +520,7 @@ static void drawit (Widget w, XtPointer 
+ /* ARGSUSED */
+ static void file_cb (Widget w, XtPointer closure, XtPointer call_data)
+ {
+-    switch ((int) closure) {
++    switch ((uintptr_t) closure) {
+     case 0:			/* open */
+ 	toggle_open_sensitive (False);
+ 	set_cursor (toplevel);
+@@ -560,7 +560,7 @@ static void file_cb (Widget w, XtPointer
+ /* ARGSUSED */
+ static void view_cb (Widget w, XtPointer closure, XtPointer call_data)
+ {
+-    switch ((int) closure) {
++    switch ((uintptr_t) closure) {
+     case 0:			/* previous */
+ 	prev_diff (NULL, NULL, NULL);
+ 	break;
+@@ -582,7 +582,7 @@ static void view_cb (Widget w, XtPointer
+ /* ARGSUSED */
+ static void select_cb (Widget w, XtPointer closure, XtPointer call_data)
+ {
+-    switch ((int) closure) {
++    switch ((uintptr_t) closure) {
+     case 0:			/* left */
+ 	select_all (LEFT);
+ 	break;
+@@ -604,7 +604,7 @@ static void select_cb (Widget w, XtPoint
+ /* ARGSUSED */
+ static void options_cb (Widget w, XtPointer closure, XtPointer call_data)
+ {
+-    switch ((int) closure) {
++    switch ((uintptr_t) closure) {
+     case 0:			/* toggle overview area */
+ 	overview_flag = !overview_flag;
+ 	if (overview_flag) {
+@@ -660,7 +660,7 @@ static void options_cb (Widget w, XtPoin
+ /* ARGSUSED */
+ static void helpmenu_cb (Widget w, XtPointer closure, XtPointer call_data)
+ {
+-    switch ((int) closure) {
++    switch ((uintptr_t) closure) {
+     case 0:			/* version */
+ 	show_version (toplevel);
+ 	break;
 @@ -1110,7 +1110,9 @@ int main (int argc, char *argv[])
  	 */
      case 3:
