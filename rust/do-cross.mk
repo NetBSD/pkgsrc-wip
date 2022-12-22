6ec6a17b0e0bc0e6cbe599ba5af7835ab01f2995
Havard Eidnes <he@NetBSD.org>
2022-12-22 15:50:48 +0000

    rust: do-cross.mk: undo space/tab to appease pkglint.

--- rust/do-cross.mk
+++ rust/do-cross.mk
@@ -5,15 +5,15 @@
 VERSION!=	make show-var VARNAME=PKGVERSION
 V_NOREV!=	make show-var VARNAME=PKGVERSION_NOREV
 
-SHORT_TARGETS+= 	armv7
-SHORT_TARGETS+= 	armv6
-SHORT_TARGETS+= 	sparc64
-SHORT_TARGETS+= 	powerpc
-SHORT_TARGETS+= 	powerpc90
-SHORT_TARGETS+= 	arm64
-SHORT_TARGETS+= 	arm64_be
-SHORT_TARGETS+= 	i386
-#SHORT_TARGETS+= 	mipsel	# produces mips32 (not mips1) executables
+SHORT_TARGETS+=		armv7
+SHORT_TARGETS+=		armv6
+SHORT_TARGETS+=		sparc64
+SHORT_TARGETS+=		powerpc
+SHORT_TARGETS+=		powerpc90
+SHORT_TARGETS+=		arm64
+SHORT_TARGETS+=		arm64_be
+SHORT_TARGETS+=		i386
+#SHORT_TARGETS+=	mipsel	# produces mips32 (not mips1) executables
 
 # Conditional local overrides of ROOT.* variables:
 .sinclude "local-roots.mk"
