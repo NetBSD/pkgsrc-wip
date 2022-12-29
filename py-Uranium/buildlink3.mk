8630e70223e61d71f299e9c4cd5eaedf897bd24e
Atsushi Toyokura <asteria.at@gmail.com>
2022-12-29 17:15:13 +0900

    py-Uranium: use x11/py-qt5 DEPENDS

--- py-Uranium/buildlink3.mk
+++ py-Uranium/buildlink3.mk
@@ -12,7 +12,6 @@ BUILDLINK_PKGSRCDIR.py-Uranium?=	../../wip/py-Uranium
 
 .include "../../math/py-numpy/buildlink3.mk"
 .include "../../wip/libArcus/buildlink3.mk"
-.include "../../x11/py-qt5/buildlink3.mk"
 .endif	# PY_URANIUM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-py-Uranium
