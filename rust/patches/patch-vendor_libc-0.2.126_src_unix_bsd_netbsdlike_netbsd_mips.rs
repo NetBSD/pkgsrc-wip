165260204b4613c8537b95045a60ce5071bfc6a8
Havard Eidnes <he@NetBSD.org>
2022-12-29 14:25:11 +0000

    rust: fix our "mipsel" target so that it can work.
    Earlier failed attempts:
     * cpu = "mips1", would have been more "portable" to older CPUs,
       but llvm failed to generate code for this cpu in at least one
       instance.  The "mips1" target in llvm is marked "highly experimental",
       so this is perhaps not surprising.
     * cpu = "mips3", an earlier mistake.  Produces "of course" 64-bit
       instructions, so gives "illegal instruction" on e.g. cobalt and
       32-bit-only CPUs.  Despite this also being marked "highly experimental"
       in llvm, this build completed.
    
    So ... we give up some portability to the older CPUs, and generate
    code for cpu = "mips32" with soft-float, and this produces a working
    rustc / cargo combination, at least to the point that devel/cbindgen
    can be built using a MIPSSIM kernel on qemu.
    
    A native build of rust has yet to be attempted, it will be difficult
    with the 508MB max memory limit for the 32-bit mips port, so the
    next best is to try in a o32 chroot on a mips64 kernel which can
    now get more memory.

---
+++ rust/patches/patch-vendor_libc-0.2.126_src_unix_bsd_netbsdlike_netbsd_mips.rs
@@ -0,0 +1,29 @@
+$NetBSD$
+
+Replicate patch from unversioned libc.
+
+--- vendor/libc-0.2.126/src/unix/bsd/netbsdlike/netbsd/mips.rs.orig	2022-12-28 21:30:25.222843485 +0000
++++ vendor/libc-0.2.126/src/unix/bsd/netbsdlike/netbsd/mips.rs
+@@ -0,0 +1,22 @@
++use PT_FIRSTMACH;
++
++pub type c_long = i32;
++pub type c_ulong = u32;
++pub type c_char = i8;
++pub type __cpu_simple_lock_nv_t = ::c_int;
++
++// should be pub(crate), but that requires Rust 1.18.0
++cfg_if! {
++    if #[cfg(libc_const_size_of)] {
++        #[doc(hidden)]
++        pub const _ALIGNBYTES: usize = ::mem::size_of::<::c_longlong>() - 1;
++    } else {
++        #[doc(hidden)]
++        pub const _ALIGNBYTES: usize = 8 - 1;
++    }
++}
++
++pub const PT_GETREGS: ::c_int = PT_FIRSTMACH + 1;
++pub const PT_SETREGS: ::c_int = PT_FIRSTMACH + 2;
++pub const PT_GETFPREGS: ::c_int = PT_FIRSTMACH + 3;
++pub const PT_SETFPREGS: ::c_int = PT_FIRSTMACH + 4;
