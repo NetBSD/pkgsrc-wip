9ba2eb9a080f0e8749ba286072f2f7da2a9a4e9d
Jason W. Bacon <bacon4000@gmail.com>
2022-12-30 17:38:44 -0600

    Finish removing obsolete packages

--- blasr_libcpp/patches/patch-pbdata_utils_SMRTTitle.cpp
+++
@@ -1,9 +0,0 @@
-$NetBSD: patch-pbdata_utils_SMRTTitle.cpp,v 1.1 2015/09/16 02:36:53 outpaddling Exp $
-
---- pbdata/utils/SMRTTitle.cpp.orig	2015-08-12 17:41:23.000000000 +0000
-+++ pbdata/utils/SMRTTitle.cpp
-@@ -1,3 +1,4 @@
-+#include <cstdlib>
- #include "utils/SMRTTitle.hpp"
- 
- /// Parse a Pacbio read name, it is a SMRTTitle, get movieName, 
