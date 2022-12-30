01ee593e64aa971b88d7a1751a987786babe6f3c
Niclas Rosenvik <youremailsarecrap@gmail.com>
2022-12-30 12:40:08 +0100

    Update libzippp to version 6.0
    Remove patches from upstream.
    Update BUILDLINK_API_DEPENDS due
    to the breaking changes below.
    
    Upstream changes from release notes:
    
    * Adds support for compression method
    * Adds support for cancel option
    * Adds support of writeable in-memory buffer
      (breaking change)
    * Imporovment of custom error handling
      (breaking change)
    * Fixes wrong inflated size
    * Fixes progression hole
    * Fixes memory leak when open failed
    * Fixes compilation warnings

--- libzippp/buildlink3.mk
+++ libzippp/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libzippp
 .if !defined(LIBZIPPP_BUILDLINK3_MK)
 LIBZIPPP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libzippp+=	libzippp>=5.0
+BUILDLINK_API_DEPENDS.libzippp+=	libzippp>=6.0
 BUILDLINK_PKGSRCDIR.libzippp?=		../../wip/libzippp
 
 .endif	# LIBZIPPP_BUILDLINK3_MK
