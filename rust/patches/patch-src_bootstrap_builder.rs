30dec24333d846e56b1ec7dd1ae83e3fa19caf59
Adam Ciarciński <aciarcinski@teonite.com>
2023-01-12 11:26:08 +0100

    rust: install rust-src, required for rust-analyzer

--- rust/patches/patch-src_bootstrap_builder.rs
+++ rust/patches/patch-src_bootstrap_builder.rs
@@ -1,19 +1,10 @@
 $NetBSD: patch-src_bootstrap_builder.rs,v 1.11 2020/07/06 13:56:32 he Exp $
 
-Do not install 'src'.
 Use @PREFIX@, not $ORIGIN in rpath.
 
 --- src/bootstrap/builder.rs.orig	2022-12-12 16:02:12.000000000 +0000
 +++ src/bootstrap/builder.rs
-@@ -745,7 +745,6 @@ impl<'a> Builder<'a> {
-                 install::Clippy,
-                 install::Miri,
-                 install::Analysis,
--                install::Src,
-                 install::Rustc
-             ),
-             Kind::Run => describe!(
-@@ -1711,7 +1710,7 @@ impl<'a> Builder<'a> {
+@@ -1710,7 +1710,7 @@ impl<'a> Builder<'a> {
                  Some("-Wl,-rpath,@loader_path/../lib")
              } else if !target.contains("windows") {
                  rustflags.arg("-Clink-args=-Wl,-z,origin");
