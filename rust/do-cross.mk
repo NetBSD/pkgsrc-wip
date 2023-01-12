15216a49baea72af49d02ff20c7ad7169b476871
Havard Eidnes <he@NetBSD.org>
2023-01-12 11:58:16 +0000

    rust: enable the mipsel target in do-cross.mk.

--- rust/do-cross.mk
+++ rust/do-cross.mk
@@ -13,7 +13,7 @@ SHORT_TARGETS+=		powerpc90
 SHORT_TARGETS+=		arm64
 SHORT_TARGETS+=		arm64_be
 SHORT_TARGETS+=		i386
-#SHORT_TARGETS+=	mipsel	# produces mips32 (not mips1) executables
+SHORT_TARGETS+=		mipsel	# produces mips32 (not mips1) executables
 
 # Conditional local overrides of ROOT.* variables:
 .sinclude "local-roots.mk"
