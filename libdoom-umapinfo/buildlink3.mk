149bd924391ab1a997c6a1efe26f2b749c616bc4
Michael Baeuerle <micha@NetBSD.org>
2023-01-23 11:39:02 +0100

    libdoom-umapinfo: Update to 0.8.1
    0.8.1  2023-01-21  Development snapshot
    ---------------------------------------
    
    Important notes:
    - API and ABI are not stable yet
    
    Bug fixes:
    - String comparison fixed
    
    0.8.0  2023-01-21  Development snapshot
    ---------------------------------------
    
    Important notes:
    - API and ABI are not stable yet
    
    New features and improvements:
    - New API to read ID of thing
    
    0.7.0  2023-01-20  Development snapshot
    ---------------------------------------
    
    Important notes:
    - API and ABI are not stable yet
    
    Bug fixes:
    - Handling of keyword "author" fixed
    
    New features and improvements:
    - New API to read ID of key (to avoid double string comparison)

--- libdoom-umapinfo/buildlink3.mk
+++ libdoom-umapinfo/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libdoom-umapinfo
 .if !defined(LIBDOOM_UMAPINFO_BUILDLINK3_MK)
 LIBDOOM_UMAPINFO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libdoom-umapinfo+=	libdoom-umapinfo>=0.6.0
+BUILDLINK_API_DEPENDS.libdoom-umapinfo+=	libdoom-umapinfo>=0.8.1
 BUILDLINK_PKGSRCDIR.libdoom-umapinfo?=		../../wip/libdoom-umapinfo
 .endif	# LIBDOOM_UMAPINFO_BUILDLINK3_MK
 
