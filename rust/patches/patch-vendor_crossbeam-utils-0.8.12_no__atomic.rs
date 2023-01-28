178b744d058df3fd33d31b0a282f0337d558e940
Havard Eidnes <he@NetBSD.org>
2023-01-28 15:21:46 +0000

    rust: properly sort out patches and cargo checksum patching.

--- rust/patches/patch-vendor_crossbeam-utils-0.8.12_no__atomic.rs
+++ rust/patches/patch-vendor_crossbeam-utils-0.8.12_no__atomic.rs
@@ -2,11 +2,11 @@ $NetBSD$
 
 --- vendor/crossbeam-utils-0.8.12/no_atomic.rs.orig	2023-01-25 01:49:15.000000000 +0000
 +++ vendor/crossbeam-utils-0.8.12/no_atomic.rs
-@@ -41,6 +41,7 @@ const NO_ATOMIC_64: &[&str] = &[
+@@ -40,6 +40,7 @@ const NO_ATOMIC_64: &[&str] = &[
+     "mipsel-unknown-linux-gnu",
      "mipsel-unknown-linux-musl",
      "mipsel-unknown-linux-uclibc",
-     "mipsel-unknown-none",
 +    "mipsel-unknown-netbsd",
+     "mipsel-unknown-none",
      "mipsisa32r6-unknown-linux-gnu",
      "mipsisa32r6el-unknown-linux-gnu",
-     "msp430-none-elf",
