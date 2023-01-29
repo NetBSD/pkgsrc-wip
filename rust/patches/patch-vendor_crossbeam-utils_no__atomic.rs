91e208989cf3a524fc93c5d25197a4d734748c7c
Havard Eidnes <he@NetBSD.org>
2023-01-29 13:34:57 +0000

    rust: commit overlooked patch.

---
+++ rust/patches/patch-vendor_crossbeam-utils_no__atomic.rs
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- vendor/crossbeam-utils/no_atomic.rs.orig	2023-01-25 01:49:15.000000000 +0000
++++ vendor/crossbeam-utils/no_atomic.rs
+@@ -41,6 +41,7 @@ const NO_ATOMIC_64: &[&str] = &[
+     "mipsel-unknown-linux-gnu",
+     "mipsel-unknown-linux-musl",
+     "mipsel-unknown-linux-uclibc",
++    "mipsel-unknown-netbsd",
+     "mipsel-unknown-none",
+     "mipsisa32r6-unknown-linux-gnu",
+     "mipsisa32r6el-unknown-linux-gnu",
