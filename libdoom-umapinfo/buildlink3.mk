0254b8abfbad171f42f41d56e959ee08a5c2656c
Michael Baeuerle <micha@NetBSD.org>
2023-02-27 13:27:35 +0100

    libdoom-umapinfo: Update to 1.0.0rc3
    - Key codes fixed.

--- libdoom-umapinfo/buildlink3.mk
+++ libdoom-umapinfo/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libdoom-umapinfo
 .if !defined(LIBDOOM_UMAPINFO_BUILDLINK3_MK)
 LIBDOOM_UMAPINFO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libdoom-umapinfo+=	libdoom-umapinfo>=1.0.0rc2
+BUILDLINK_API_DEPENDS.libdoom-umapinfo+=	libdoom-umapinfo>=1.0.0rc3
 BUILDLINK_PKGSRCDIR.libdoom-umapinfo?=		../../wip/libdoom-umapinfo
 .endif	# LIBDOOM_UMAPINFO_BUILDLINK3_MK
 
