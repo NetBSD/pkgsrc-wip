9e39114171007e5c2d76bdcd7a53049b90e03fd1
Leonardo Taccari <leot@NetBSD.org>
2023-03-03 15:08:50 +0100

    yamlfmt: Import yamlfmt-0.7.1 as wip/yamlfmt
    yamlfmt is an extensible command line tool or library to format
    YAML files.

---
+++ yamlfmt/go-modules.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+GO_MODULE_FILES+=	github.com/!rage!cage64/multilinediff/@v/v0.2.0.mod
+GO_MODULE_FILES+=	github.com/!rage!cage64/multilinediff/@v/v0.2.0.zip
+GO_MODULE_FILES+=	github.com/bmatcuk/doublestar/v4/@v/v4.2.0.mod
+GO_MODULE_FILES+=	github.com/bmatcuk/doublestar/v4/@v/v4.2.0.zip
+GO_MODULE_FILES+=	github.com/braydonk/yaml/@v/v0.4.0.mod
+GO_MODULE_FILES+=	github.com/braydonk/yaml/@v/v0.4.0.zip
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.9.mod
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.9.zip
+GO_MODULE_FILES+=	github.com/mitchellh/mapstructure/@v/v1.5.0.mod
+GO_MODULE_FILES+=	github.com/mitchellh/mapstructure/@v/v1.5.0.zip
+GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod
