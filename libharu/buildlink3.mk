e46d38402af7d1afb1a8aba6713fccc439783572
Atsushi Toyokura <asteria.at@gmail.com>
2023-03-18 15:47:54 +0900

    libharu: Update to 2.4.3
    Version 2.4.3 (2022.10.14)
    - Add static hpdf_version.h header by @vszakats in #241
    - hpdf_version.h included again by hpdf.h #241 #246
    - File attachment issue resolved @hvanbrug #159
    - Renamed *_LIBZ defines to _*ZLIB, thanks to @karstenBriksoft #249,
      enables compression of PDF files again.
    
    Version 2.4.2 (2022.9.11)
    - Reinstated hpdf_version.h #237 #240
    
    Version 2.4.1 (2022.9.5)
    - Fixed library name #236 from @jschueller
    - Set correct version number #237 pointed out by @xantares
    
    Version 2.4.0 (2022.8.7)
    - Add support for free-form triangle shading objects. by
      @allisonvacanti in #157
    - Fix config constant to match use in hpdf_mmgr.c by @bvirlet in #167
    - Improve small number writing in HPDF_FToA. by @allisonvacanti in
      #187
    - Fix missing /CapHeight key in font definition by @yabaud in #138
    - Change HPDF_Page_CreateXObjectFromImage zoom parameter type to
      HPDF... by @extensia in #114
    - Fix another case of png files with background mask save uncompressed
      by @igor-niv in #221
    - Avoid issue with libtiff duplicate symbols by @bvirlet in #168
    - Reajust bit_depth of png image after striping depth from 16 to 8. by
      @joelhecht in #125
    - Fixed typo in Japanese font name: Mincyo -> Mincho by @qtamaki in
      #80
    - Fix various typos by @luzpaz in #226
    - hpdf.h: add missing HPDF_Boolean typedef by @mathstuf in #189
    - Moved to a CMake only build environment.
    - Fix bad unicode in comment by @gix in #229
    - Fix various typos by @luzpaz in #230
    
    Version 2.3.0 RC3 (2013.10.24)
    - Support huge fonts
    
    Version 2.3.0 RC2 (2011.10.12)
    - Changed package naming, no other changes.
    
    Version 2.3.0 RC1 (2011.09.28)
    - Added support for 3dMeasures of subtype PD3 and 3DC, projection
      annotations, ExData and javascript attached to a U3D model. (Robert
      Würfel)
    - Added support for 1- and 2-byte UTF8 codes. (Clayman)
    - Added full PDF/A1-b support. (Petr Pytelka)
    - Added support for CCITT compression for B/W images. (Petr Pytelka)
    - Add support for TwoPageLeft and TwoPageRight layouts. (Vincent
      Dupont)
    - Const-ified arrays used in the sources. (Ilkka Lehtoranta)
    - Fixed build with libpng 1.5.0
    - Fixed bug in HPDF_GetContents() - isize variable was not
      initialized. (Vincent Dupont)
    - Fixed possible endless loop in PNG handling code.
      (reported by Mathew Waters)
    - Fixed several issues based on the warnings generated by
      clang-analyzer. (Daniel Höpfl)
    - Fixed quite a number of warnings. (Davide Achilli)
    - Added 'd' postfix to debug build, fixed wrong filename. (Wim Dumon)
    - Fixed HPDF_Text_Rect() not to split words in some obscure cases.

--- libharu/buildlink3.mk
+++ libharu/buildlink3.mk
@@ -1,12 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:43:48 jsonn Exp $
+# $NetBSD: buildlink3.mk,v 1.1 2021/05/04 14:12:05 prlw1 Exp $
 
 BUILDLINK_TREE+=	libharu
 
 .if !defined(LIBHARU_BUILDLINK3_MK)
 LIBHARU_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libharu+=	libharu>=2.1.0
-BUILDLINK_PKGSRCDIR.libharu?=	../../wip/libharu
-.endif # LIBHARU_BUILDLINK3_MK
+BUILDLINK_API_DEPENDS.libharu+=	libharu>=2.4.0
+BUILDLINK_PKGSRCDIR.libharu?=	../../print/libharu
+
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.endif	# LIBHARU_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libharu
