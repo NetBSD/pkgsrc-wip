f1bb47f940fd06b58ab412325e7791d89ccb6278
Thomas Klausner <wiz@gatalith.at>
2023-04-05 10:25:51 +0200

    gcc10-libjit: add bl3.mk

---
+++ gcc10-libjit/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	gcc10-libjit
+
+.if !defined(GCC10_LIBJIT_BUILDLINK3_MK)
+GCC10_LIBJIT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gcc10-libjit+=	gcc10-libjit>=10.4.0
+BUILDLINK_PKGSRCDIR.gcc10-libjit?=	../../wip/gcc10-libjit
+.endif	# GCC10_LIBJIT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gcc10-libjit
