001b13b2e74b0e824fb2a5ca19b31215b1bb9dd1
pin <voidpin@protonmail.com>
2023-04-15 14:38:26 +0200

    alacritty-dev & elvish-dev: import packages
    I use these everyday, store them here for easy access.

---
+++ elvish-dev/go-modules.mk
@@ -0,0 +1,18 @@
+# $NetBSD$
+
+GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.15.mod
+GO_MODULE_FILES+=	github.com/creack/pty/@v/v1.1.15.zip
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.9.mod
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.9.zip
+GO_MODULE_FILES+=	github.com/gorilla/websocket/@v/v1.4.1.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.17.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.17.zip
+GO_MODULE_FILES+=	github.com/sourcegraph/go-lsp/@v/v0.0.0-20200429204803-219e11d77f5d.mod
+GO_MODULE_FILES+=	github.com/sourcegraph/go-lsp/@v/v0.0.0-20200429204803-219e11d77f5d.zip
+GO_MODULE_FILES+=	github.com/sourcegraph/jsonrpc2/@v/v0.2.0.mod
+GO_MODULE_FILES+=	github.com/sourcegraph/jsonrpc2/@v/v0.2.0.zip
+GO_MODULE_FILES+=	go.etcd.io/bbolt/@v/v1.3.7.mod
+GO_MODULE_FILES+=	go.etcd.io/bbolt/@v/v1.3.7.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220811171246-fbc7d0a398ab.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.5.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.5.0.zip
