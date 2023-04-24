cf36fcc174d73f9cc1fae67fb4bfc3687741d642
vsyakovetsky <vsyakovetsky@gmail.com>
2023-04-24 11:56:51 +0300

    mtr-graph: update to 0.85.119

--- mtr-graph/options.mk
+++ mtr-graph/options.mk
@@ -2,22 +2,32 @@
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.mtr-graph
 PKG_SUPPORTED_OPTIONS=		inet6
-PKG_SUPPORTED_OPTIONS+=		ipinfo
+PKG_SUPPORTED_OPTIONS+=		mpls
+PKG_SUPPORTED_OPTIONS+=		dns
 PKG_SUPPORTED_OPTIONS+=		idn
 PKG_SUPPORTED_OPTIONS+=		unicode
-PKG_SUPPORTED_OPTIONS+=		sys-getopt
+PKG_SUPPORTED_OPTIONS+=		ipinfo
 PKG_OPTIONS_OPTIONAL_GROUPS+=	graphcairo
 PKG_OPTIONS_GROUP.graphcairo+=	graphcairo-xcb
 PKG_OPTIONS_GROUP.graphcairo+=	graphcairo-xlib
-PKG_SUGGESTED_OPTIONS+=		ipinfo
+PKG_SUGGESTED_OPTIONS+=		mpls
+PKG_SUGGESTED_OPTIONS+=		dns
 PKG_SUGGESTED_OPTIONS+=		unicode
-PKG_SUGGESTED_OPTIONS+=		sys-getopt
+PKG_SUGGESTED_OPTIONS+=		ipinfo
 PKG_OPTIONS_LEGACY_OPTS=	libidn:idn
 
 .include "../../mk/bsd.options.mk"
 
 .if empty(PKG_OPTIONS:Minet6)
 CONFIGURE_ARGS+=	--disable-ipv6
+.endif
+
+.if empty(PKG_OPTIONS:Mdns)
+CONFIGURE_ARGS+=	--disable-dns
+.endif
+
+.if empty(PKG_OPTIONS:Mmpls)
+CONFIGURE_ARGS+=	--without-mpls
 .endif
 
 .if empty(PKG_OPTIONS:Mipinfo)
@@ -50,6 +60,3 @@ CONFIGURE_ARGS+=	--with-libidn
 CONFIGURE_ARGS+=	--with-unicode
 .endif
 
-.if !empty(PKG_OPTIONS:Msys-getopt)
-CONFIGURE_ARGS+=	--with-sys-getopt
-.endif
