27c8c3d72164be3a8c40355e13df5a348ad6d8eb
nikita <nikita@NetBSD.org>
2023-04-27 23:17:53 +0200

    libnitrokey: tests

--- libnitrokey/options.mk
+++ libnitrokey/options.mk
@@ -2,7 +2,7 @@
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.py-pynitrokey
 
-PKG_SUPPORTED_OPTIONS+=		doc
+PKG_SUPPORTED_OPTIONS+=		doc tests
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,4 +10,10 @@ PKG_SUPPORTED_OPTIONS+=		doc
 DEPENDS+=	doxygen-[0-9]*:../../devel/doxygen
 DEPENDS+=	graphviz-[0-9]*:../../graphics/graphviz
 # else switch this off in cmake
+.endif
+
+.if ${PKG_OPTIONS:Mtests}
+# These tests do not require any device to be connected
+CMAKE_ARGS+=	-DCOMPILE_OFFLINE_TESTS=ON
+TEST_DEPENDS+=	catch2-[0-9]*:../../devel/catch2
 .endif
