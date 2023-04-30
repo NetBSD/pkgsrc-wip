3bb079acc34e652f0d3a22b66141a018940d4266
nikita <nikita@NetBSD.org>
2023-04-30 11:03:58 +0200

    zig-git: options (debug building)

---
+++ zig-git/options.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.zig-git
+PKG_SUPPORTED_OPTIONS=	debug
+
+.include "../../mk/bsd.options.mk"
+
+.if ${PKG_OPTIONS:Mdebug}
+CMAKE_ARGS+=		-DCMAKE_BUILD_TYPE=Debug
+.else
+CMAKE_ARGS+=		-DCMAKE_BUILD_TYPE=Release
+.endif
