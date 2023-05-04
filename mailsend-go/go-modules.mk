b6eb9ac9ee2e26a3af4b66d26f4da41e7dbef952
Iku Iwasa <iquiw@NetBSD.org>
2023-05-04 13:42:45 +0900

    mailsend-go: switch to use go-module.mk instead of go-package.mk
    Pkgsrc changes:
    
    * Remove wip/go-gomail dependency.

---
+++ mailsend-go/go-modules.mk
@@ -0,0 +1,6 @@
+# $NetBSD$
+
+GO_MODULE_FILES+=	gopkg.in/alexcesaro/quotedprintable.v3/@v/v3.0.0-20150716171945-2caba252f4dc.mod
+GO_MODULE_FILES+=	gopkg.in/alexcesaro/quotedprintable.v3/@v/v3.0.0-20150716171945-2caba252f4dc.zip
+GO_MODULE_FILES+=	gopkg.in/gomail.v2/@v/v2.0.0-20160411212932-81ebce5c23df.mod
+GO_MODULE_FILES+=	gopkg.in/gomail.v2/@v/v2.0.0-20160411212932-81ebce5c23df.zip
