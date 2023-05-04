52b6992eea32aea5ca008e0dd6ab96b8e3a39372
nikita <nikita@NetBSD.org>
2023-05-04 16:09:31 +0200

    0ad, 0ad-data: fix build, works. some hacks in place.

--- 0ad/options.mk
+++ 0ad/options.mk
@@ -2,6 +2,8 @@
 PKG_OPTIONS_VAR=	PKG_OPTIONS.0ad
 PKG_SUPPORTED_OPTIONS+=	tests editor
 
+PLIST_VARS+=		editor
+
 .include "../../mk/bsd.options.mk"
 
 .if ${PKG_OPTIONS:Mtests}
@@ -10,8 +12,12 @@ CONFIGURE_TESTS?=		--with-tests
 CONFIGURE_TESTS?=		--without-tests
 .endif
 
+# TODO: not entirely working binary (loads, complains about files not found),
+# something is missing.
 .if ${PKG_OPTIONS:Meditor}
 CONFIGURE_EDITOR?=
+.include "../../x11/wxGTK32/buildlink3.mk"
+PLIST.editor=			yes
 .else
 CONFIGURE_EDITOR?=		--disable-atlas
 .endif
