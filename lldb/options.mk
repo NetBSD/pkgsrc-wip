74f9f294dc35b0557161b5c6b60c49b34c5eaad2
nikita <nikita@NetBSD.org>
2023-05-10 17:52:38 +0200

    lldb: apply build from linux.

--- lldb/options.mk
+++ lldb/options.mk
@@ -2,7 +2,8 @@
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.lldb
 
-PKG_SUPPORTED_OPTIONS+=	tests
+PKG_SUPPORTED_OPTIONS+=	tests curses
+PKG_SUGGESTED_OPTIONS=	curses
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,4 +12,36 @@ CMAKE_ARGS+=	-DLLVM_INCLUDE_TESTS=ON
 .else
 # py-lit, py-psutil, lld
 CMAKE_ARGS+=	-DLLVM_INCLUDE_TESTS=OFF
+.endif
+
+.if ${PKG_OPTIONS:Mcurses}
+.include "../../mk/curses.buildlink3.mk"
+.include "../../mk/readline.buildlink3.mk"
+
+.  if ${OPSYS} == "NetBSD"
+.    if exists(/usr/include/panel.h)
+CMAKE_ARGS+=	LLDB_ENABLE_CURSES=1
+.    else
+.include "../../devel/ncurses/buildlink3.mk"
+.      if exists(${BUILDLINK_PREFIX.ncurses}/include/panel.h)
+CMAKE_ARGS+=    LLDB_ENABLE_CURSES=1
+.      else
+CMAKE_ARGS+=    LLDB_ENABLE_CURSES=0
+.      endif
+.    endif
+CMAKE_ARGS+=	LLDB_ENABLE_CURSES=0
+.  endif
+
+.  if ${OPSYS} != "NetBSD"
+.include "../../devel/ncurses/buildlink3.mk"
+.    if exists(${BUILDLINK_PREFIX.ncurses}/include/panel.h)
+CMAKE_ARGS+=    LLDB_ENABLE_CURSES=1
+.    else
+CMAKE_ARGS+=    LLDB_ENABLE_CURSES=0
+.    endif
+.  endif
+
+.else
+
+CMAKE_ARGS+=	LLDB_ENABLE_CURSES=0
 .endif
