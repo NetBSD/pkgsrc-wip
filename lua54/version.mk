daf914dc2d370810f50e6b5c7ed777c29d27c39f
nikita <nikita@NetBSD.org>
2023-05-15 22:47:01 +0200

    remove lua54, imported

--- lua54/version.mk
+++
@@ -1,6 +0,0 @@
-# $NetBSD: version.mk,v 1.5 2022/03/06 07:46:10 nia Exp $
-
-LUA_VERSION=	5.4.6
-
-LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
-LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
