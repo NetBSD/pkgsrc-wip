f32762e4b167129f5a75826089607eeaba4b36c5
Thomas Klausner <wiz@gatalith.at>
2023-05-17 17:50:28 +0200

    libproxy: finish package

--- libproxy/buildlink3.mk
+++ libproxy/buildlink3.mk
@@ -7,7 +7,7 @@ LIBPROXY_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libproxy+=	libproxy>=0.2.3
 BUILDLINK_ABI_DEPENDS.libproxy+=	libproxy>=0.4.11
-BUILDLINK_PKGSRCDIR.libproxy?=		../../www/libproxy
+BUILDLINK_PKGSRCDIR.libproxy?=		../../wip/libproxy
 .endif # LIBPROXY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libproxy
