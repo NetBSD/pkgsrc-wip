4eecabbabcfedf4ca2a50198acf25a358be761ef
Thomas Klausner <wiz@gatalith.at>
2023-05-22 15:55:10 +0200

    libproxy: another problem fixed

--- libproxy/patches/patch-src_libproxy_proxy.h
+++ libproxy/patches/patch-src_libproxy_proxy.h
@@ -3,13 +3,16 @@ $NetBSD$
 Fix C++ usage of header.
 https://github.com/libproxy/libproxy/issues/226
 
+Fix Use glib-object header instead of gio
+https://github.com/libproxy/libproxy/issues/230
+
 --- src/libproxy/proxy.h.orig	2023-05-15 13:09:42.000000000 +0000
 +++ src/libproxy/proxy.h
 @@ -22,12 +22,12 @@
  
  #pragma once
  
-+#include <gio/gio.h>
++#include <glib-object.h>
 +
  #ifdef __cplusplus
  extern "C" {
