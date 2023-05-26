6084bc0f4fdc14b8c6fd00da33b20cc3bd4c5ff3
Havard Eidnes <he@NetBSD.org>
2023-05-26 10:06:54 +0000

    ncbi-blast+: ensure bswap_{32,64}() gets defined on NetBSD.
    Remove dependency on having __BSWAP_RENAME defined, and
    also remove conditional on __bswap_32 not being defined.
    
    Should fix build problem on NetBSD.
    
    Bump PKGREVISION.

---
+++ ncbi-blast+/patches/patch-src_util_checksum_farmhash_farmhash.cc
@@ -0,0 +1,21 @@
+$NetBSD$
+
+Ensure bswap_32() and bswap_64() gets defined on NetBSD.
+
+--- src/util/checksum/farmhash/farmhash.cc.orig	2018-09-26 12:38:06.000000000 +0000
++++ src/util/checksum/farmhash/farmhash.cc
+@@ -158,12 +158,12 @@
+ 
+ #include <sys/types.h>
+ #include <machine/bswap.h>
+-#if defined(__BSWAP_RENAME) && !defined(__bswap_32)
++#if defined(bswap_32) && defined(bswap_64)
+ #undef bswap_32
+ #undef bswap_64
++#endif
+ #define bswap_32(x) bswap32(x)
+ #define bswap_64(x) bswap64(x)
+-#endif
+ 
+ #elif defined(__HAIKU__)
+ 
