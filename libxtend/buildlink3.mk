9c1720f25b9a110fade42a85ce447386fc835a02
Jason W. Bacon <bacon4000@gmail.com>
2023-06-03 17:22:33 -0500

    libxtend: Update to 0.1.7.19
    xt_get_user_name(): Get text user name of calling process

--- libxtend/buildlink3.mk
+++ libxtend/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=		libxtend
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.7.18
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.7.19
 BUILDLINK_PKGSRCDIR.libxtend?=		../../wip/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
