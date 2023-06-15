d9d56436b2b4d8234bec905b93c5df23d104087a
Adam Ciarciński <aciarcinski@teonite.com>
2023-06-15 17:20:45 +0200

    llvm family: updated to 16.0.6

--- llvm/version.mk
+++ llvm/version.mk
@@ -5,14 +5,16 @@
 # used by lang/clang
 # used by lang/clang-tools-extra
 # used by lang/compiler-rt
+# used by lang/flang
 # used by lang/libcxx
 # used by lang/libcxxabi
 # used by lang/libunwind
+# used by lang/mlir
 # used by lang/wasi-compiler-rt
 # used by lang/wasi-libcxx
 # used by parallel/openmp
 
-LLVM_VERSION=	16.0.5
+LLVM_VERSION=	16.0.6
 MASTER_SITES=	${MASTER_SITE_GITHUB:=llvm/}
 GITHUB_PROJECT=	llvm-project
 GITHUB_RELEASE=	llvmorg-${PKGVERSION_NOREV}
