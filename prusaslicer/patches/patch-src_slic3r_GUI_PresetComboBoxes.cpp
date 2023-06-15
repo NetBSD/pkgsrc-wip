543027c429b0bb413c8b98162e2925c33e86ebc6
Paul Ripke <stix@stix.id.au>
2023-06-15 19:21:07 +1000

    prusaslicer: update to 2.6.0rc2.

--- prusaslicer/patches/patch-src_slic3r_GUI_PresetComboBoxes.cpp
+++ prusaslicer/patches/patch-src_slic3r_GUI_PresetComboBoxes.cpp
@@ -2,23 +2,14 @@ $NetBSD$
 
 Treat NetBSD like Linux.
 
---- src/slic3r/GUI/PresetComboBoxes.cpp.orig	2021-12-17 14:00:02.000000000 +0000
+--- src/slic3r/GUI/PresetComboBoxes.cpp.orig	2023-06-14 14:36:02.000000000 +0000
 +++ src/slic3r/GUI/PresetComboBoxes.cpp
-@@ -584,7 +584,7 @@ PlaterPresetComboBox::PlaterPresetComboB
-     {
-         // In a case of a physical printer, for its editing open PhysicalPrinterDialog
-         if (m_type == Preset::TYPE_PRINTER
--#ifdef __linux__
-+#if defined(__linux__) || defined(__NetBSD__)
-             // To edit extruder color from the sidebar
-             || m_type == Preset::TYPE_FILAMENT
- #endif //__linux__
-@@ -716,7 +716,7 @@ void PlaterPresetComboBox::show_edit_men
-     append_menu_item(menu, wxID_ANY, _L("Edit preset"), "",
+@@ -779,7 +779,7 @@ void PlaterPresetComboBox::show_edit_men
          [this](wxCommandEvent&) { this->switch_to_tab(); }, "cog", menu, []() { return true; }, wxGetApp().plater());
  
+     if (m_type == Preset::TYPE_FILAMENT) {
 -#ifdef __linux__
 +#if defined(__linux__) || defined(__NetBSD__)
-     // To edit extruder color from the sidebar
-     if (m_type == Preset::TYPE_FILAMENT) {
+         // To edit extruder color from the sidebar
          append_menu_item(menu, wxID_ANY, _L("Change extruder color"), "",
+             [this](wxCommandEvent&) { this->change_extruder_color(); }, "funnel", menu, []() { return true; }, wxGetApp().plater());
