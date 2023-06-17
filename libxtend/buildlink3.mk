5b83626cc9dde2a9b9cde1a3d93d7b7fc48b5e22
Jason W. Bacon <bacon4000@gmail.com>
2023-06-17 09:09:06 -0500

    libxtend: Update to 0.1.8.1
    xt_fopen(): stat() file before opening for read.
    popen() does not return NULL when gunzip -c fails.

--- libxtend/buildlink3.mk
+++ libxtend/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=		libxtend
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.8
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.8.1
 BUILDLINK_PKGSRCDIR.libxtend?=		../../wip/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
