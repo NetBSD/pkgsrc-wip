1d6d88e977a5625ec976abdd100d57be9e61cf19
Olaf Seibert <rhialto@falu.nl>
2023-07-01 19:35:03 +0200

    vlang: some cleanups.

--- vlang/example.v
+++
@@ -1,7 +0,0 @@
-fn main() {
-    areas := ['game', 'web', 'tools', 'science', 'systems',
-              'embedded', 'drivers', 'GUI', 'mobile']
-    for area in areas {
-        println('Hello, $area developers!')
-    }
-}
