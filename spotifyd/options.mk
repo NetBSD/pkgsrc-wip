9415b855483e22c73e30a90e841f625b2d496453
pin <voidpin@protonmail.com>
2023-07-11 10:26:47 +0200

    spotifyd: fix options.mk

--- spotifyd/options.mk
+++ spotifyd/options.mk
@@ -10,18 +10,24 @@ PKG_SUGGESTED_OPTIONS=		portaudio
 
 .if !empty(PKG_OPTIONS:Malsa)
 CARGO_FEATURES+=	alsa_backend
+RUSTFLAGS+=		-C link-arg=-L${BUILDLINK_PREFIX.alsa-lib}/lib
 RUSTFLAGS+=		-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.alsa-lib}/lib
 .include "../../audio/alsa-lib/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mportaudio)
 CARGO_FEATURES+=	portaudio_backend
+RUSTFLAGS+=		-C link-arg=-L${BUILDLINK_PREFIX.portaudio}/lib
+RUSTFLAGS+=		-C link-arg=-L${BUILDLINK_PREFIX.portaudio}/${BUILDLINK_LIBDIRS.portaudio}
+RUSTFLAGS+=		-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.portaudio}/lib
 RUSTFLAGS+=		-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.portaudio}/${BUILDLINK_LIBDIRS.portaudio}
 .include "../../audio/portaudio/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Mpulseaudio)
 CARGO_FEATURES+=	pulseaudio_backend
+RUSTFLAGS+=		-C link-arg=-L${BUILDLINK_PREFIX.pulseaudio}/lib
+RUSTFLAGS+=		-C link-arg=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.pulseaudio}/lib
 .include "../../audio/pulseaudio/buildlink3.mk"
 .endif
 
