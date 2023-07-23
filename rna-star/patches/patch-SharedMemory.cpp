b037b372c82546bb1882be1a45b30b86b6f02cb8
Jason Bacon <bacon@NetBSD.org>
2023-07-23 13:06:33 -0500

    rna-star: Successful compile on Darwin

--- rna-star/patches/patch-SharedMemory.cpp
+++ rna-star/patches/patch-SharedMemory.cpp
@@ -2,14 +2,14 @@ $NetBSD$
 
 # Portability
 
---- SharedMemory.cpp.orig	2023-07-23 17:37:47.154665669 +0000
+--- SharedMemory.cpp.orig	2022-11-01 14:50:35.000000000 +0000
 +++ SharedMemory.cpp
 @@ -12,7 +12,7 @@
  #include <errno.h>
  #include <sys/types.h>
  
 -#if defined(COMPILE_FOR_MAC) || defined(__FreeBSD__)
-+#if defined(COMPILE_FOR_MAC) || defined(__FreeBSD__) || defined(__NetBSD__)
++#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__)
    //some Mac's idiosyncrasies: standard SHM libraries are very old and missing some definitions
    #define SHM_NORESERVE 0
  #endif
