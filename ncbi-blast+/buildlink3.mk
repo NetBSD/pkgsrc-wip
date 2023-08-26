ba63127d2f74d28e72ffba9aec7d49014c859e2f
Jason W. Bacon <bacon@NetBSD.org>
2023-08-26 09:25:46 -0500

    ncbi-blast+: Update to 2.14.1
    Also clean up Makefile and bl3

--- ncbi-blast+/buildlink3.mk
+++ ncbi-blast+/buildlink3.mk
@@ -1,12 +1,15 @@
-# $NetBSD: buildlink3.mk,v 1.6 2023/05/28 22:48:27 bacon Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2023/08/14 05:23:51 wiz Exp $
 
 BUILDLINK_TREE+=	ncbi-blast+
 
 .if !defined(NCBI_BLAST+_BUILDLINK3_MK)
 NCBI_BLAST+_BUILDLINK3_MK:=
 
+USE_CXX_FEATURES+=			c++17
+
 BUILDLINK_API_DEPENDS.ncbi-blast++=	ncbi-blast+>=2.14.0
-BUILDLINK_PKGSRCDIR.ncbi-blast+?=	../../biology/ncbi-blast+
+BUILDLINK_ABI_DEPENDS.ncbi-blast++=	ncbi-blast+>=2.14.0nb1
+BUILDLINK_PKGSRCDIR.ncbi-blast+?=	../../wip/ncbi-blast+
 
 .endif	# NCBI_BLAST+_BUILDLINK3_MK
 
