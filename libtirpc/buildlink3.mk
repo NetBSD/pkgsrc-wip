0eb65c02db1705e5a8a4f03900b794606a488960
Paolo Vincenzo Olivo <vins@NetBSD.org>
2023-08-29 13:10:23 +0200

    libtirpc: finish package
    * Revise Makefile and buildlink3.mk
    * Write builtin.mk
    * Fix compile warning.

--- libtirpc/buildlink3.mk
+++ libtirpc/buildlink3.mk
@@ -7,6 +7,10 @@ LIBTIRPC_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libtirpc+=	libtirpc>=1.3.3
 BUILDLINK_PKGSRCDIR.libtirpc?=		../../devel/libtirpc
+
+BUILDLINK_CFLAGS.libtirpc+=	-I${BUILDLINK_PREFIX.libtirpc}/include/tiprc
+BUILDLINK_LDFLAGS.libtirpc+=	-ltirpc
+
 .endif	# LIBTIRPC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libtirpc
