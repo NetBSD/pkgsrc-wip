ea5dc7df0df34bd00fa09c0c9279c0a960329e7e
Jason W. Bacon <bacon@NetBSD.org>
2023-08-31 16:20:54 -0500

    libxtend: Update to 0.1.9.12
    fast-file: Properly separate private struct def and typedef

--- libxtend/buildlink3.mk
+++ libxtend/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=		libxtend
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.9.6
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.9.12
 BUILDLINK_PKGSRCDIR.libxtend?=		../../wip/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
