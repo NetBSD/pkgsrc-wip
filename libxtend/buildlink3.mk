2e4e705cd8d5cd60af6686e0f94eb0ff8d93fa71
Jason W. Bacon <bacon@NetBSD.org>
2023-09-04 13:14:17 -0500

    libxtend: Update to 0.1.9.17
    Document xt_ff_read(), xt_ff_seeko(), xt_ff_tmpfile()

--- libxtend/buildlink3.mk
+++ libxtend/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=		libxtend
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.9.15
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.9.17
 BUILDLINK_PKGSRCDIR.libxtend?=		../../wip/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
