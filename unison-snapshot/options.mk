ec8540a63635782cfa10acbf4aea5cf0aca7809f
Greg Troxel <gdt@lexort.com>
2023-09-04 21:51:05 -0400

    unison-snapshot: Update to 2.53.3.0.51

--- unison-snapshot/options.mk
+++ unison-snapshot/options.mk
@@ -2,15 +2,15 @@
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.unison
 PKG_SUPPORTED_OPTIONS=	lablgtk
+PKG_SUGGESTED_OPTIONS=	lablgtk
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mlablgtk)
-MAKE_FLAGS+=	UISTYLE=gtk3
+PLIST_VARS+=	gtk3
 
+.if !empty(PKG_OPTIONS:Mlablgtk)
+PLIST.gtk3=	yes
 DEPENDS+=	font-schumacher-misc>=1.0:../../fonts/font-schumacher-misc
 
 .include "../../x11/ocaml-lablgtk3/buildlink3.mk"
-.else
-MAKE_FLAGS+=	UISTYLE=text
 .endif
