847aed1bea9c767c725d2499c880a29a794299a3
Greg Troxel <gdt@lexort.com>
2023-09-19 08:49:13 -0400

    unison-snapshot: disable GUI by default
    Upstream makefiles are broken; they look in the wrong place for the
    lablgtk libs and conclude they are missing.  To be fixed upstream and
    then this will be updated.

--- unison-snapshot/options.mk
+++ unison-snapshot/options.mk
@@ -2,7 +2,8 @@
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.unison
 PKG_SUPPORTED_OPTIONS=	lablgtk
-PKG_SUGGESTED_OPTIONS=	lablgtk
+# upstream makefiles are broken for now
+#PKG_SUGGESTED_OPTIONS=	lablgtk
 
 .include "../../mk/bsd.options.mk"
 
