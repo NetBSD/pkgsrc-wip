21909e4121b57448c94337dcc71e36d110464a2a
Benny Siegert <bsiegert@NetBSD.org>
2023-10-18 13:40:34 +0200

    esbuild: update to 0.19.5

--- esbuild/go-modules.mk
+++ esbuild/go-modules.mk
@@ -1,4 +1,4 @@
 # $NetBSD$
 
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200501145240-bc7a7d42d5c3.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200501145240-bc7a7d42d5c3.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.zip
