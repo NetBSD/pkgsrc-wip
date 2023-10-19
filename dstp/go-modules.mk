3fb7a2e786c72538fe2bf9884ab5e1c01431c573
Benny Siegert <bsiegert@NetBSD.org>
2023-10-19 20:40:07 +0200

    dstp: update to 0.4.0
    Also change to a go module build.

---
+++ dstp/go-modules.mk
@@ -0,0 +1,27 @@
+===> Extracting for dstp-0.4.0
+# $NetBSD$
+
+GO_MODULE_FILES+=	github.com/fatih/color/@v/v1.13.0.mod
+GO_MODULE_FILES+=	github.com/fatih/color/@v/v1.13.0.zip
+GO_MODULE_FILES+=	github.com/go-ping/ping/@v/v0.0.0-20211014180314-6e2b003bffdd.mod
+GO_MODULE_FILES+=	github.com/go-ping/ping/@v/v0.0.0-20211014180314-6e2b003bffdd.zip
+GO_MODULE_FILES+=	github.com/google/uuid/@v/v1.2.0.mod
+GO_MODULE_FILES+=	github.com/google/uuid/@v/v1.2.0.zip
+GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.1.9.mod
+GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.1.9.zip
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.12.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.14.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.14.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20210316092652-d523dce5a7f4.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20210316092652-d523dce5a7f4.zip
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.0.0-20210220032951-036812b2e83c.mod
+GO_MODULE_FILES+=	golang.org/x/sync/@v/v0.0.0-20210220032951-036812b2e83c.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200116001909-b77594299b42.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20200223170610-d5e6a3e2c0ae.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20201119102817-f84b799fce68.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210315160823-c6e025ad8005.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210630005230-0f9fa26af87c.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210630005230-0f9fa26af87c.zip
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20201126162022-7de9c90e9dd1.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.3.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180917221912-90fa682c2a6e.mod
