af0606a3675dd924cf7af5b3733857d7fbaec490
Jason W. Bacon <bacon@NetBSD.org>
2023-10-23 07:04:17 -0500

    libxtend: Update to 0.2.0 release

--- libxtend/buildlink3.mk
+++ libxtend/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=		libxtend
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.1.9.35
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.2.0
 BUILDLINK_PKGSRCDIR.libxtend?=		../../wip/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
