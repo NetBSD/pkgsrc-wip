0218080a33a8509ee5efe5949c902099617bfa73
Manuel Kuklinski <mk@asdfghasdfgh.de>
2023-11-03 07:11:54 +0100

    sdl12-compat: updatetd to version 1.2.68
    Tested linking against audio/din to resolve graphic errors (just a black
    screen) on Darwin, but no luck - have to contact upstream of
    devel/sdl12-compat or audio/din.

--- sdl12-compat/buildlink3.mk
+++ sdl12-compat/buildlink3.mk
@@ -1,21 +1,31 @@
-# $NetBSD: buildlink3.mk,v 1.2 2022/04/21 06:56:18 nia Exp $
+# $NetBSD$
+# XXX
+# XXX This file was created automatically using createbuildlink-3.19.
+# XXX After this file has been verified as correct, the comment lines
+# XXX beginning with "XXX" should be removed.  Please do not commit
+# XXX unverified buildlink3.mk files.
 
 BUILDLINK_TREE+=	sdl12-compat
 
 .if !defined(SDL12_COMPAT_BUILDLINK3_MK)
 SDL12_COMPAT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.sdl12-compat+=	sdl12-compat>=1.2.52
+# XXX
+# XXX Packages that only install static libraries or headers should
+# XXX include the following line:
+# XXX
+# XXX	BUILDLINK_DEPMETHOD.sdl12-compat?=	build
+
+BUILDLINK_API_DEPENDS.sdl12-compat+=	sdl12-compat>=1.2.52nb3
 BUILDLINK_PKGSRCDIR.sdl12-compat?=	../../wip/sdl12-compat
-BUILDLINK_INCDIRS.sdl12-compat+=	include/SDL
 
-.include "../../mk/bsd.fast.prefs.mk"
-
-.if ${OPSYS} != "Darwin"
-.  include "../../graphics/MesaLib/buildlink3.mk"
-.  include "../../graphics/glu/buildlink3.mk"
-.endif
-
+# XXX
+# XXX Uncomment and keep only the buildlink3 lines below which are directly
+# XXX needed for dependencies to compile, link, and run.  If this package
+# XXX provides a wrappered API or otherwise does not expose the APIs of the
+# XXX buildlink3 lines below to dependencies, remove them.
+# XXX
+#.include "../../devel/SDL2/buildlink3.mk"
 .endif	# SDL12_COMPAT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-sdl12-compat
