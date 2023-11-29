0f7511b19a4910d3cee36a108e99f56a30484ec3
Adam Ciarciński <aciarcinski@teonite.com>
2023-11-29 09:16:14 +0100

    llvm: updated to 17.0.6

--- clang/patches/patch-lib_Basic_Targets_OSTargets.h
+++ clang/patches/patch-lib_Basic_Targets_OSTargets.h
@@ -6,29 +6,6 @@ which assumes __float128.
 
 --- lib/Basic/Targets/OSTargets.h.orig	2023-10-03 06:37:35.000000000 +0000
 +++ lib/Basic/Targets/OSTargets.h
-@@ -425,12 +425,22 @@ protected:
-     Builder.defineMacro("__unix__");
-     if (Opts.POSIXThreads)
-       Builder.defineMacro("_REENTRANT");
-+    if (this->HasFloat128)
-+      Builder.defineMacro("__FLOAT128__");
-   }
- 
- public:
-   NetBSDTargetInfo(const llvm::Triple &Triple, const TargetOptions &Opts)
-       : OSTargetInfo<Target>(Triple, Opts) {
-     this->MCountName = "__mcount";
-+    switch (Triple.getArch()) {
-+    default:
-+      break;
-+    case llvm::Triple::x86:
-+    case llvm::Triple::x86_64:
-+      this->HasFloat128 = true;
-+      break;
-+    }
-   }
- };
- 
 @@ -628,22 +638,15 @@ protected:
      DefineStd(Builder, "unix", Opts);
      Builder.defineMacro("__svr4__");
