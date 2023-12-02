250e5885ee91b5581cf1eb5385f798320d6e51b7
Paul Ripke <stix@stix.id.au>
2023-12-02 15:34:38 +1100

    libbgcode: add buildlink3.mk

---
+++ libbgcode/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libbgcode
+
+.if !defined(LIBBGCODE_BUILDLINK3_MK)
+LIBBGCODE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libbgcode+=	libbgcode>=20231123
+BUILDLINK_PKGSRCDIR.libbgcode?=	../../wip/libbgcode
+
+.include "../../devel/boost-headers/buildlink3.mk"
+.include "../../devel/boost-libs/buildlink3.mk"
+.include "../../devel/catch2/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../wip/heatshrink/buildlink3.mk"
+.endif	# LIBBGCODE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libbgcode
