2a4f57f06bc6204c56cfd2c6db3ee7498bddffdf
Aleksey Cheusov <vle@gmx.net>
2023-12-02 13:37:47 +0400

    Convert mk-configure package to mk-configure-current

---
+++ mk-configure-current/mk-c.mk
@@ -0,0 +1,31 @@
+# $NetBSD$
+
+TOOL_DEPENDS+=	${BUILDLINK_API_DEPENDS.mk-c}:${BUILDLINK_PKGSRCDIR.mk-c}
+
+MAKE_PROGRAM=		${PREFIX}/bin/mkcmake
+MAKE_ENV+=		MANDIR=${PREFIX}/${PKGMANDIR}
+MAKE_ENV+=		SYSCONFDIR=${PKG_SYSCONFDIR}
+MAKE_ENV+=		MKCOMPILERSETTINGS=yes
+CONFIGURE_TARGET?=	configure
+
+.if !target(do-configure)
+do-configure:
+	set -e; cd ${WRKSRC}; \
+	env ${MAKE_ENV} \
+	    ${MAKE_PROGRAM} ${MAKE_FLAGS} ${BUILD_MAKE_FLAGS} \
+		-f ${MAKE_FILE} ${CONFIGURE_TARGET}
+.endif
+
+###
+BUILDLINK_TREE+=	mk-c
+
+.if !defined(MK_CONFIGURE_BUILDLINK3_MK)
+MK_CONFIGURE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.mk-c+=	mk-configure>=0.28.0
+BUILDLINK_PKGSRCDIR.mk-c=	../../devel/mk-configure
+BUILDLINK_DEPMETHOD.mk-c=	build
+BUILDLINK_INCDIRS.mk-c=		include share/mk-configure/features
+.endif # MK_CONFIGURE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-mk-c
