640ebc690e6c3d581a1965c38bf889614aa3df54
Paul Ripke <stix@stix.id.au>
2023-12-02 15:14:44 +1100

    heatshrink: Switch to cmake to satisfy libbgcode.

---
+++ heatshrink/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	heatshrink
+
+.if !defined(HEATSHRINK_BUILDLINK3_MK)
+HEATSHRINK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.heatshrink+=	heatshrink>=0.4.0
+BUILDLINK_PKGSRCDIR.heatshrink?=	../../wip/heatshrink
+.endif	# HEATSHRINK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-heatshrink
