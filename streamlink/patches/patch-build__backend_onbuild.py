b12d10e3f2e50127c727427dfaea0feb65361015
Yorick Hardy <yorickhardy@gmail.com>
2023-12-23 12:23:16 +0200

    streamlink: update to streamlink-6.5.0
    == streamlink 6.5.0 (2023-12-16)
    
    Release highlights:
    
    - Fixed: `UserWarning` being emitted by recent `pycountry` releases when
      parsing certain language codes
    - Fixed: trace logging setup in `WebsocketClient` implementation
    - Updated plugins:
      - btv: switched to HLS multivariant playlists
      - gulli: rewritten plugin
      - twitch: removed/disabled `--twitch-disable-reruns`
      - twitch: enabled `check_streams` HLS option, to ensure early stream
                availability without querying the delayed Twitch API
      - twitch: removed unnecessary Twitch API error messages for offline channels
      - wasd: removed plugin
    - Build: added support for `versioningit >=3.0.0`, with backward compatibility

--- streamlink/patches/patch-build__backend_onbuild.py
+++ streamlink/patches/patch-build__backend_onbuild.py
@@ -2,24 +2,30 @@ $NetBSD$
 
 Match the call parameters with how it is used by our py-versioningit package.
 
---- build_backend/onbuild.py.orig	2023-10-26 13:05:52.000000000 +0000
+--- build_backend/onbuild.py.orig	2023-12-16 06:18:37.000000000 +0000
 +++ build_backend/onbuild.py
-@@ -8,8 +8,8 @@ from typing import Any, Dict, Generator,
+@@ -18,11 +18,12 @@ except ImportError:  # pragma: no cover
  def onbuild(
-     build_dir: Union[str, Path],
+     *,
      is_source: bool,
 -    template_fields: Dict[str, Any],
 -    params: Dict[str, Any],
-+    version: str,
-+    params: Dict[str, Any] = {},
++    template_fields: Optional[Dict[str, Any]] = {},
++    params: Optional[Dict[str, Any]] = {},
+     # backward compatibility for `versioningit <3.0.0`
+     build_dir: Optional[Union[str, Path]] = None,
+     file_provider: Optional[SetuptoolsFileProvider] = None,
++    version: Optional[str] = None
  ):
      """
      Remove the ``versioningit`` build-requirement from Streamlink's source distribution.
-@@ -25,7 +25,6 @@ def onbuild(
+@@ -45,7 +46,8 @@ def onbuild(
+         raise RuntimeError("Missing file_provider or build_dir")
  
-     base_dir: Path = Path(build_dir).resolve()
      pkg_dir: Path = base_dir / "src" if is_source else base_dir
 -    version: str = template_fields["version"]
++    if version == None:
++        version = template_fields["version"]
      cmproxy: Proxy[str]
  
      # Remove versioningit from ``build-system.requires`` in ``pyproject.toml``
