22eba8692b1845d199fbf5eafca7b0214adee8b1
Leonardo Taccari <leot@NetBSD.org>
2023-12-31 12:16:32 +0100

    mpv: (partial) Update to 0.37.0
    Update - at least `patch`-target speaking - to mpv-0.37.0, i.e. update all the
    checksums and adjust several patches.
    
    - patch-osdep_threads.c logic was moved to patch-osdep_threads-posix.h
    - remove some bits that were applied upstream for patch-meson.build
    
    Only `patch` target tried, not `build` tested yet!
    
    (Actual changelog will be added to COMMIT_MSG, sorry!)

---
+++ mpv/patches/patch-osdep_threads-posix.h
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Add support for NetBSD.
+
+--- osdep/threads-posix.h.orig	2023-12-31 11:13:03.078250654 +0000
++++ osdep/threads-posix.h
+@@ -232,6 +232,8 @@ static inline void mp_thread_set_name(co
+     pthread_set_name_np(pthread_self(), name);
+ #elif HAVE_OSX_THREAD_NAME
+     pthread_setname_np(name);
++#elif HAVE_NETBSD_THREAD_NAME
++    pthread_setname_np(pthread_self(), "%s", name);
+ #endif
+ }
+ 
