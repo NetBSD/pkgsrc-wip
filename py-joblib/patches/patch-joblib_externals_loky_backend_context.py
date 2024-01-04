5e70489ea5d153caafb2fdbd21644f27bebd519d
Jason W. Bacon <bacon@NetBSD.org>
2024-01-04 08:59:52 -0600

    py-joblib: Revert to subprocesses for sysctl
    Python has no portable sysctl interface

--- py-joblib/patches/patch-joblib_externals_loky_backend_context.py
+++ py-joblib/patches/patch-joblib_externals_loky_backend_context.py
@@ -1,36 +1,41 @@
 $NetBSD$
 
-# Add BSD support, eliminate subprocess on Darwin
+# Add BSD support
 
 --- joblib/externals/loky/backend/context.py.orig	2023-06-29 15:14:21.000000000 +0000
 +++ joblib/externals/loky/backend/context.py
-@@ -11,6 +11,7 @@
- #
- import os
- import sys
-+import sysctl
- import math
- import subprocess
- import traceback
-@@ -267,13 +268,14 @@ def _count_physical_cores():
-             ]
-             cpu_count_physical = sum(map(int, cpu_info))
-         elif sys.platform == "darwin":
--            cpu_info = subprocess.run(
--                "sysctl -n hw.physicalcpu".split(),
--                capture_output=True,
--                text=True,
--            )
--            cpu_info = cpu_info.stdout
--            cpu_count_physical = int(cpu_info)
-+            cpu_count_physical = int(sysctl.filter("hw.physicalcpu")[0].value);
+@@ -245,6 +245,8 @@ def _count_physical_cores():
+         return physical_cores_cache, exception
+ 
+     # Not cached yet, find it
++    # Using subprocesses is inefficient, but python has no portable
++    # sysctl interface at this time
+     try:
+         if sys.platform == "linux":
+             cpu_info = subprocess.run(
+@@ -274,6 +276,25 @@ def _count_physical_cores():
+             )
+             cpu_info = cpu_info.stdout
+             cpu_count_physical = int(cpu_info)
 +        elif sys.platform.startswith('freebsd'):
-+            cpu_count_physical = int(sysctl.filter("kern.smp.cores")[0].value);
++            cpu_info = subprocess.run(
++                "sysctl -n kern.smp.cores".split(),
++                capture_output=True,
++                text=True,
++            )
++            cpu_info = cpu_info.stdout
++            cpu_count_physical = int(cpu_info)
 +        elif sys.platform.startswith('netbsd'):
 +            # FIXME: This reports the number of hyperthreads
 +            # We want independent cores to prevent oversubscription
-+            cpu_count_physical = int(sysctl.filter("hw.ncpu")[0].value);
 +        # FIXME: Add OpenBSD, Dragonfly (probably the same as FreeBSD)
++            cpu_info = subprocess.run(
++                "sysctl -n hw.ncpu".split(),
++                capture_output=True,
++                text=True,
++            )
++            cpu_info = cpu_info.stdout
++            cpu_count_physical = int(cpu_info)
          else:
              raise NotImplementedError(f"unsupported platform: {sys.platform}")
  
