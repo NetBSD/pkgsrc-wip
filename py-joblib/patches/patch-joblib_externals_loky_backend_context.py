1af02d6905d6e38cbb218d2938d05bfe1938c107
Jason W. Bacon <bacon@NetBSD.org>
2024-01-05 09:17:51 -0600

    py-joblib: Improve patch documentation

--- py-joblib/patches/patch-joblib_externals_loky_backend_context.py
+++ py-joblib/patches/patch-joblib_externals_loky_backend_context.py
@@ -4,16 +4,17 @@ $NetBSD$
 
 --- joblib/externals/loky/backend/context.py.orig	2023-06-29 15:14:21.000000000 +0000
 +++ joblib/externals/loky/backend/context.py
-@@ -245,6 +245,8 @@ def _count_physical_cores():
+@@ -245,6 +245,9 @@ def _count_physical_cores():
          return physical_cores_cache, exception
  
      # Not cached yet, find it
 +    # Using subprocesses is inefficient, but python has no portable
 +    # sysctl interface at this time
++    # FIXME: Add OpenBSD, Dragonfly
      try:
          if sys.platform == "linux":
              cpu_info = subprocess.run(
-@@ -274,6 +276,25 @@ def _count_physical_cores():
+@@ -274,6 +277,26 @@ def _count_physical_cores():
              )
              cpu_info = cpu_info.stdout
              cpu_count_physical = int(cpu_info)
@@ -26,9 +27,10 @@ $NetBSD$
 +            cpu_info = cpu_info.stdout
 +            cpu_count_physical = int(cpu_info)
 +        elif sys.platform.startswith('netbsd'):
-+            # FIXME: This reports the number of hyperthreads
-+            # We want independent cores to prevent oversubscription
-+        # FIXME: Add OpenBSD, Dragonfly (probably the same as FreeBSD)
++            # FIXME: hw.ncpu reports the number of hyperthreads.
++            # We prefer independent cores to prevent oversubscription.
++            # NetBSD does not currently expose physical core counts,
++            # but this is under discussion in PR kern/57816.
 +            cpu_info = subprocess.run(
 +                "sysctl -n hw.ncpu".split(),
 +                capture_output=True,
