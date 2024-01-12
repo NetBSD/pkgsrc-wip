eb685f9e960c1cde84d69837defe6f5c7891a106
Kevin Bloom <kevin.bloom@posteo.net>
2024-01-11 21:27:46 -0500

    s-nail: use PKGBASE instead of confusing variables

--- s-nail/patches/patch-mk_make-install.sh
+++ s-nail/patches/patch-mk_make-install.sh
@@ -8,14 +8,14 @@ Put the default config in EGDIR so we can use CONF_FILES
  __mkdir "${VAL_BINDIR}"
  __mkdir "${VAL_MANDIR}/man1"
  __mkdir "${VAL_SYSCONFDIR}"
-+__mkdir "${VAL_PREFIX}/share/examples/${VAL_SID}${VAL_MAILX}"
++__mkdir "${VAL_PREFIX}/share/examples/${PKGBASE}"
  
  __stripfile "${OBJDIR}"/"${VAL_UAGENT}"
  __copyfile 0755 "${OBJDIR}"/"${VAL_UAGENT}" "${VAL_BINDIR}"/"${VAL_UAGENT}"
  __copyfile 0444 "${OBJDIR}"/uman.1 "${VAL_MANDIR}"/man1/"${VAL_UAGENT}".1
  if [ -f "${DESTDIR}${VAL_SYSCONFDIR}/${VAL_SYSCONFRC}" ]; then :; else
 -   __copyfile 0444 "${OBJDIR}"/urc.rc "${VAL_SYSCONFDIR}/${VAL_SYSCONFRC}"
-+   __copyfile 0444 "${OBJDIR}"/urc.rc "${VAL_PREFIX}/share/examples/${VAL_SID}${VAL_MAILX}/${VAL_SYSCONFRC}"
++   __copyfile 0444 "${OBJDIR}"/urc.rc "${VAL_PREFIX}/share/examples/${PKGBASE}/${VAL_SYSCONFRC}"
  fi
  
  if [ "${OPT_DOTLOCK}" != 0 ]; then
