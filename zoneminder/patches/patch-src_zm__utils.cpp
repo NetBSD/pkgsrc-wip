2bd4dbfe9713490160be702fbd1b96eb45812eb4
Greg Troxel <gdt@lexort.com>
2024-01-14 14:25:25 -0500

    zoneminder: Add patch extending BSDish systems to NetBSD

--- zoneminder/patches/patch-src_zm__utils.cpp
+++ zoneminder/patches/patch-src_zm__utils.cpp
@@ -15,7 +15,14 @@ Fix time types (sizes).
  }
  
  /* Detect special hardware features, such as SIMD instruction sets */
-@@ -238,6 +238,10 @@ void HwCapsDetect() {
+@@ -231,13 +231,15 @@ void HwCapsDetect() {
+   unsigned long auxval = 0;
+   elf_aux_info(AT_HWCAP, &auxval, sizeof(auxval));
+   if (auxval & HWCAP_NEON) {
+-  #error Unsupported OS.
+-  #endif
+     Debug(1,"Detected ARM (AArch32) processor with Neon");
+     neonversion = 1;
    } else {
      Debug(1,"Detected ARM (AArch32) processor");
    }
