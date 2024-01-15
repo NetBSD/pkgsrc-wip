d3bfc9bea89faa52a142ecb432a19f5271338a65
Greg Troxel <gdt@lexort.com>
2024-01-15 09:41:08 -0500

    zoneminder: Clean up previous fixes and modernize
    There were a lot of workarounds for problems in previous versions.
    Many of those are no longer needed.  Remove workarounds, some by
    inspection, some by build testing.  Leave a few commented if unsure to
    reduce future work.
    
    Add workaround from wiz@ to find target perl vs tool perl so that zm's
    @PERL@ substitution gets the right answer.
    
    Finish COMMIT_MSG.
    
    Change to ffmpeg6 and add new dep lib.
    
    Prune empty patch that adjusted unspecified array [].  It might end up
    being necessary but the number of not upstream patches is too hard to
    deal with at the moment.

--- zoneminder/patches/patch-src_zm__rtp__ctrl.h
+++
@@ -1,7 +0,0 @@
-$NetBSD: patch-src_zm__rtp__ctrl.h,v 1.1 2020/07/12 16:58:42 gdt Exp $
-
-Work around:
-  zm_rtp_ctrl.h:128:31: error: flexible array member 'RtpCtrlThread::RtcpPacket::<unnamed union>::Bye::srcN' in an otherwise empty 'struct RtpCtrlThread::RtcpPacket::<unnamed union>::Bye'
-
-Not reported upstream because pkgsrc is out of date; to be done if this is not resolved by updating.
-
