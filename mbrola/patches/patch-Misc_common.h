391095dcbc9466adcff1624a6a58c4bdf591666d
adr <adr@sdf.org>
2024-01-24 20:06:07 +0000

    mbrola, mbrolavox-*: Preparing to import to pkgsrc

--- mbrola/patches/patch-Misc_common.h
+++ mbrola/patches/patch-Misc_common.h
@@ -1,5 +1,7 @@
 $NetBSD$
 
+Add NetBSD target and check for _LITTLE_ENDIAN
+
 --- Misc/common.h.orig	2019-12-17 16:05:14.000000000 +0000
 +++ Misc/common.h
 @@ -70,11 +70,11 @@
@@ -16,14 +18,3 @@ $NetBSD$
  #  define LITTLE_ENDIAN
  #else
  #  define BIG_ENDIAN
-@@ -100,6 +100,10 @@ void swab( const char *from, char *to, i
- #endif
- #endif
- 
-+#if defined(LITTLE_ENDIAN) && defined(BIG_ENDIAN)
-+#error "Both LITTLE_ENDIAN and BIG_ENDIAN are defined and this should not happen."
-+#endif
-+
- /* memmove is not defined on SUN4, but memcpy will do */
- #ifdef TARGET_OS_SUN4
- #define memmove memcpy
