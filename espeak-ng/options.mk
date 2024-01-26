1d9820bb9c118629515110009ec359cf8d5213e0
adr <adr@sdf.org>
2024-01-26 05:07:37 +0000

    audio/espeak-ng: Fixes, almost ready for inclusion in pkgsrc.

--- espeak-ng/options.mk
+++ espeak-ng/options.mk
@@ -1,13 +1,10 @@
 # $NetBSD: $
 
-PKG_OPTIONS_VAR=		PKG_OPTIONS.espeak-ng
-
-PKG_SUPPORTED_OPTIONS+=		sonic
-
+PKG_OPTIONS_VAR=	PKG_OPTIONS.espeak-ng
+PKG_SUPPORTED_OPTIONS+=	sonic
+PKG_SUGGESTED_OPTIONS+=	sonic
 
 .include "../../mk/bsd.options.mk"
-
-PLIST_VARS+=	${PKG_SUPPORTED_OPTIONS}
 
 .if !empty(PKG_OPTIONS:Msonic)
 .  include "../../audio/sonic/buildlink3.mk"
