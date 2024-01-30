986859c22ac5e23565eb8790777453d53228b8b3
Matthew Danielson <matthewd@fastmail.us>
2024-01-30 06:40:23 -0800

    py-apache-arrow: Update to 15.0.0
    Changes same as to apache-arrow: Update to 15.0.0

--- py-apache-arrow/patches/patch-python_setup.py
+++ py-apache-arrow/patches/patch-python_setup.py
@@ -1,13 +1,15 @@
 $NetBSD$
 
---- python/setup.py.orig	2023-11-19 21:12:52.091720220 +0000
+Setuptools will try and download these versions... tell it not to.
+
+--- python/setup.py.orig	2024-01-30 14:19:14.017798084 +0000
 +++ python/setup.py
 @@ -492,7 +492,7 @@ setup(
                                   'pyarrow/_generated_version.py'),
          'version_scheme': guess_next_dev_version
      },
 -    setup_requires=['setuptools_scm < 8.0.0', 'cython >= 0.29.31'] + setup_requires,
-+    setup_requires=['setuptools_scm < 9.0.0', 'cython >= 0.29.31'] + setup_requires,
++    setup_requires=[],
      install_requires=install_requires,
      tests_require=['pytest', 'pandas', 'hypothesis'],
      python_requires='>=3.8',
