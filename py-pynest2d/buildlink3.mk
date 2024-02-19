7aec55a73487578fef36e99928c5546108f9cdda
Atsushi Toyokura <asteria.at@gmail.com>
2024-02-19 22:27:06 +0900

    py-pynest2d: replace x11/py-sip for wip/py-sip4

--- py-pynest2d/buildlink3.mk
+++ py-pynest2d/buildlink3.mk
@@ -9,7 +9,7 @@ BUILDLINK_API_DEPENDS.py-pynest2d+=	${PYPKGPREFIX}-pynest2d>=4.11.0
 BUILDLINK_PKGSRCDIR.py-pynest2d?=	../../wip/py-pynest2d
 
 .include "../../wip/libnest2d-ultimaker/buildlink3.mk"
-.include "../../x11/py-sip/buildlink3.mk"
+.include "../../wip/py-sip4/buildlink3.mk"
 .endif	# PY_PYNEST2D_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-py-pynest2d
