07f65cf93145450eb0ce675df77b9754668f5bd4
Atsushi Toyokura <asteria.at@gmail.com>
2024-02-19 22:26:47 +0900

    libSavitar: replace x11/py-sip for wip/py-sip4

--- libSavitar/buildlink3.mk
+++ libSavitar/buildlink3.mk
@@ -8,7 +8,7 @@ LIBSAVITAR_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libSavitar+=	libSavitar>=4.11.0
 BUILDLINK_PKGSRCDIR.libSavitar?=	../../wip/libSavitar
 
-.include "../../x11/py-sip/buildlink3.mk"
+.include "../../wip/py-sip4/buildlink3.mk"
 .endif	# LIBSAVITAR_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libSavitar
