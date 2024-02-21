e9f3c9b6d91bb8a8ef28bda8f0b066f0a0183995
Thomas Klausner <wiz@gatalith.at>
2024-02-21 14:32:58 +0100

    dunst-git: add patch comments, add missed patch to distinfo

--- dunst-git/patches/patch-test_utils.c
+++ dunst-git/patches/patch-test_utils.c
@@ -1,5 +1,8 @@
 $NetBSD$
 
+Only use Linux-only header on Linux.
+https://github.com/dunst-project/dunst/pull/1288
+
 --- test/utils.c.orig	2024-02-21 12:52:50.623620603 +0000
 +++ test/utils.c
 @@ -1,4 +1,6 @@
