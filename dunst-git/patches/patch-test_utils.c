804abf8111fcd354f773d938db617abac6343475
Thomas Klausner <wiz@gatalith.at>
2024-02-21 13:55:38 +0100

    dunst-git: add package tracking dunst git head
    for testing patches

---
+++ dunst-git/patches/patch-test_utils.c
@@ -0,0 +1,11 @@
+$NetBSD$
+
+--- test/utils.c.orig	2024-02-21 12:52:50.623620603 +0000
++++ test/utils.c
+@@ -1,4 +1,6 @@
++#if defined(__linux__)
+ #include <features.h>
++#endif
+ 
+ #include "../src/utils.c"
+ #include "greatest.h"
