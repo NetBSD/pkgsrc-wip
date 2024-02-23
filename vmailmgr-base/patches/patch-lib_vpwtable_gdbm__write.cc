0ffa89b7839d0289aede0dcf3774c97c12b2e86b
Amitai Schleier <schmonz-web-git@schmonz.com>
2024-02-22 20:21:45 -0500

    vmailmgr-base: add 'gdbm' and 'kerberos' options.

--- vmailmgr-base/patches/patch-lib_vpwtable_gdbm__write.cc
+++ vmailmgr-base/patches/patch-lib_vpwtable_gdbm__write.cc
@@ -2,9 +2,16 @@ $NetBSD$
 
 Patch from FreeBSD ports.
 
---- lib/vpwtable/gdbm_write.cc.orig	2024-02-20 03:22:19.220947617 +0000
+--- lib/vpwtable/gdbm_write.cc.orig	2015-09-01 16:03:21.000000000 +0000
 +++ lib/vpwtable/gdbm_write.cc
-@@ -22,8 +22,8 @@
+@@ -16,14 +16,15 @@
+ 
+ #include <config.h>
+ #include <errno.h>
++#include <unistd.h>
+ #include "vpwtable.h"
+ #include <gdbm.h>
+ 
  class gdbm_vpwtable_writer : public vpwtable_writer
  {
  private:
@@ -15,14 +22,17 @@ Patch from FreeBSD ports.
    GDBM_FILE out;
    bool opened;
  public:
-@@ -60,9 +60,9 @@ bool gdbm_vpwtable_writer::operator!() c
+@@ -59,10 +60,10 @@ bool gdbm_vpwtable_writer::operator!() c
+ 
  bool gdbm_vpwtable_writer::put(const vpwentry& vpw)
  {
-   mystring name = vpw->name.lower();
+-  mystring name = vpw->name.lower();
 -  datum key = { (char*)name.c_str(), name.length() };
-+  datum key = { (char*)name.c_str(), static_cast<int>(name.length()) };
-   mystring binary = vpw->to_record();
+-  mystring binary = vpw->to_record();
 -  datum data = { (char*)binary.c_str(), binary.length() };
++  mystring name = vpw.name.lower();
++  datum key = { (char*)name.c_str(), static_cast<int>(name.length()) };
++  mystring binary = vpw.to_record();
 +  datum data = { (char*)binary.c_str(), static_cast<int>(binary.length()) };
    return gdbm_store(out, key, data, GDBM_INSERT) == 0;
  }
