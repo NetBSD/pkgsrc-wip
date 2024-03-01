f6282363253be33f9cb3aaf5bcae34addc55c037
Malte Dehling <mdehling@gmail.com>
2024-03-01 14:13:48 -0800

    py-notmuch2: Convert egg to wheel

--- py-notmuch2/patches/patch-__notmuch__config.py
+++ py-notmuch2/patches/patch-__notmuch__config.py
@@ -3,8 +3,8 @@ $NetBSD$
 This file would normally be created by the configure script, but we
 don't run it.
 
---- bindings/python-cffi/_notmuch_config.py.orig	2024-02-17 05:03:35.281766718 +0000
-+++ bindings/python-cffi/_notmuch_config.py
+--- _notmuch_config.py.orig	2024-03-01 22:02:43.485120189 +0000
++++ _notmuch_config.py
 @@ -0,0 +1,3 @@
 +NOTMUCH_VERSION_FILE='../../version.txt'
 +NOTMUCH_INCLUDE_DIR='../../lib'
