97efc7c8fe06677de222f76d420afc20f6748dcf
adr <adr@sdf.org>
2024-03-10 15:24:34 +0000

    yasr: Port ready to import to Pkgsrc.

--- yasr/patches/patch-yasr_yasr.h
+++ yasr/patches/patch-yasr_yasr.h
@@ -4,7 +4,24 @@ Remove size of array declarations defined elsewhere
 
 --- yasr/yasr.h.orig	2023-04-08 21:22:57.000000000 +0000
 +++ yasr/yasr.h
-@@ -304,11 +304,11 @@ extern int cl_synthport;
+@@ -128,6 +128,7 @@ enum mpunct
+ 
+ #define OPT_STR_SIZE 256
+ 
++#define TTS_BUF_SIZE 256
+ typedef struct Tts Tts;
+ struct Tts
+ {
+@@ -140,7 +141,7 @@ struct Tts
+   int obufhead, obuflen, obuftail;
+   int oflag;			/* set to 1 every time tts_send is called */
+   int outlen;
+-  wchar_t buf[256];
++  wchar_t buf[TTS_BUF_SIZE];
+   int synth;
+   pid_t pid;
+   char port[OPT_STR_SIZE];
+@@ -304,11 +305,11 @@ extern int cl_synthport;
  extern Opt opt[];
  extern int synthopt;
  extern char *conffile;
