016ada418ac8dfcaa23688958b0843a4f7c0d274
Charlotte Koch <charlotte@NetBSD.org>
2024-03-13 21:47:17 -0700

    ppsspp: Update to 1.17.1
    But we have new build problems now

--- ppsspp/patches/patch-ppsspp_config.h
+++ ppsspp/patches/patch-ppsspp_config.h
@@ -1,10 +1,10 @@
 $NetBSD$
 
-NetBSD can benefit from the macros which allow PPSSPP to build on OpenBSD.
+NetBSD can benefit from the functionality provided for OpenBSD.
 
---- ppsspp_config.h.orig	2023-06-28 20:56:35.580340514 -0700
-+++ ppsspp_config.h	2023-06-28 20:56:48.149019856 -0700
-@@ -104,7 +104,7 @@
+--- ppsspp_config.h.orig	2024-03-13 21:19:45.715917776 -0700
++++ ppsspp_config.h	2024-03-13 21:19:58.391397099 -0700
+@@ -115,7 +115,7 @@
      #define PPSSPP_PLATFORM_LINUX 1
  #elif defined(__linux__)
      #define PPSSPP_PLATFORM_LINUX 1
