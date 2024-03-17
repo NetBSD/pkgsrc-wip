cacbca88b4b603d3908481e55c5c44a63c09ae8a
Malte Dehling <mdehling@gmail.com>
2024-03-17 14:36:42 -0700

    RE-flex: Fix documentation issues
    Unbreak doxygen v1.10 documentation builds in a way that hopefully
    doesn't break building with earlier versions.

--- RE-flex/patches/patch-src_reflex.cpp
+++ RE-flex/patches/patch-src_reflex.cpp
@@ -1,9 +1,30 @@
 $NetBSD$
 
-cast chars to unsigned for ctype functions
+- cast chars to unsigned for ctype functions
+- fix doxygen errors
 
---- src/reflex.cpp.orig	2024-03-16 22:12:32.810243725 +0000
+--- src/reflex.cpp.orig	2024-03-11 17:59:51.000000000 +0000
 +++ src/reflex.cpp
+@@ -194,8 +194,8 @@ static const char *options_table[] = {
+ /// - `x` for `\xXX` 8-bit character encoding in hexadecimal
+ /// - `y` for `\y` word boundary
+ /// - `z` for `\z` end of input anchor
+-/// - ``` for `\`` begin of input anchor
+-/// - `'` for `\'` end of input anchor
++/// - \c \` for \c \\\` begin of input anchor
++/// - \c ' for \c \\' end of input anchor
+ /// - `<` for `\<` left word boundary
+ /// - `>` for `\>` right word boundary
+ /// - `A` for `\A` begin of input anchor
+@@ -216,7 +216,7 @@ static const char *options_table[] = {
+ /// - '1' to '9' for backreferences (not applicable to lexer specifications)
+ ///
+ /// Note that 'p' is a special case to support Unicode-based matchers that
+-/// natively support UTF8 patterns and Unicode classes \p{C}, \P{C}, \w, \W,
++/// natively support UTF8 patterns and Unicode classes \\p{C}, \P{C}, \w, \W,
+ /// \d, \D, \l, \L, \u, \U, \N, and \x{X}.  Basically, 'p' prevents conversion
+ /// of Unicode patterns to UTF8.  This special case does not support {NAME}
+ /// expansions in bracket lists such as [a-z||{upper}] and {lower}{+}{upper}
 @@ -274,10 +274,10 @@ static const Reflex::Library library_tab
  ////////////////////////////////////////////////////////////////////////////////
  
@@ -231,3 +252,12 @@ cast chars to unsigned for ctype functions
    {
      do
      {
+@@ -1915,7 +1915,7 @@ void Reflex::write_prelude()
+     *out << "\n// --perf-report option requires a timer:\n#include <reflex/timer.h>\n";
+ }
+ 
+-/// Write Flex-compatible #defines to lex.yy.cpp
++/// Write Flex-compatible \#defines to lex.yy.cpp
+ void Reflex::write_defines()
+ {
+   if (!out->good())
