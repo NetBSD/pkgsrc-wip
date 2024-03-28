623898ecd6cd8c593bed363c42e5894d9b524167
Brian Ewell <brian@brianewell.com>
2024-03-28 20:32:39 +0000

    databases/timescaledb-tune: Bumped version from 0.14.3 to 0.16.0

---
+++ timescaledb-tune/go-modules.mk
@@ -0,0 +1,13 @@
+# $NetBSD $
+
+GO_MODULE_FILES+=	github.com/fatih/color/@v/v1.9.0.mod
+GO_MODULE_FILES+=	github.com/fatih/color/@v/v1.9.0.zip
+GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.1.4.mod
+GO_MODULE_FILES+=	github.com/mattn/go-colorable/@v/v0.1.4.zip
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.11.mod
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.11.zip
+GO_MODULE_FILES+=	github.com/mattn/go-isatty/@v/v0.0.8.mod
+GO_MODULE_FILES+=	github.com/pbnjay/memory/@v/v0.0.0-20190104145345-974d429e7ae4.mod
+GO_MODULE_FILES+=	github.com/pbnjay/memory/@v/v0.0.0-20190104145345-974d429e7ae4.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.1.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.1.0.zip
