523273f4ad478a29ad77a73dd345b8cf2d26740e
Thomas Klausner <wiz@NetBSD.org>
2024-04-11 11:30:23 +0200

    libgit2: upgrade candidate for devel/libgit2

---
+++ libgit2/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.14 2023/08/14 05:24:11 wiz Exp $
+
+BUILDLINK_TREE+=	libgit2
+
+.if !defined(LIBGIT2_BUILDLINK3_MK)
+LIBGIT2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libgit2+=	libgit2>=0.28.0
+BUILDLINK_ABI_DEPENDS.libgit2+=	libgit2>=1.6.4nb2
+BUILDLINK_PKGSRCDIR.libgit2?=	../../wip/libgit2
+
+.include "../../devel/pcre2/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.endif	# LIBGIT2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libgit2
