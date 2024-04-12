2cf9c30bd3a90292a3b89d9b3a0bbae410c8447b
Kevin Bloom <kevin.bloom@posteo.net>
2024-04-12 14:44:13 -0400

    cups-filter: fixed issues with cupsd

--- cups-filters/buildlink3.mk
+++ cups-filters/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=	cups-filters
 .if !defined(CUPS_FILTERS_BUILDLINK3_MK)
 CUPS_FILTERS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.cups-filters+=	cups-filters>=1.8.2
-BUILDLINK_ABI_DEPENDS.cups-filters+=	cups-filters>=1.28.16nb5
-BUILDLINK_PKGSRCDIR.cups-filters?=	../../print/cups-filters
+BUILDLINK_API_DEPENDS.cups-filters+=	cups-filters>=2.0.0
+BUILDLINK_ABI_DEPENDS.cups-filters+=	cups-filters>=2.0.0
+BUILDLINK_PKGSRCDIR.cups-filters?=	../../wip/cups-filters
 
 .include "../../print/cups-base/buildlink3.mk"
 .endif # CUPS_FILTERS_BUILDLINK3_MK
