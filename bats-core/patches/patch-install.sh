6395062b794d0d851edc55fb8fc1dcfb49824443
Iku Iwasa <iquiw@NetBSD.org>
2024-04-20 17:19:23 +0900

    bats-core: update to 1.11.0
    Added:
    
    * hardened CI scripts by using hashes instead of versions for actions and
      restricting permissions
    * add security.md
    * add codespell CI checks
    * dynamic test registration via bats_test_function
    * add check that Bats is executed with Bash >= 3.2
    
    Fixed:
    
    * install.sh now works for deviating lib/ dirs (like lib32,lib64)
    * catch unset BATS_TEST_SOURCE in lib/bats-core/tracing.bash so
    * set -u/set -o nounset works as expected
    * fix --gather-test-outputs-in fails on tests with multiple /
    * install does not create unused /usr/share/bats anymore
    * ensure IFS is unchanged in {setup,teardown}{_suite,_file,}, @test and
      free code
    * junit formatter: remove ANSI Codes to avoid invalid XML character
    
    Changed:
    
    * update Docker image with the latest bats-file version 0.4.0
    * update Docker image with the latest bats-detik version 1.3.0
    
    Documentation
    
    * clarify docker usage
    * update Arch Linux package URL in installation.rst
    * rename bash-bats to bats for Arch Linux in installation.rst
    * fix FAQ entry about setup-/teardown_suite, as they are available now
    * added logo

--- bats-core/patches/patch-install.sh
+++ bats-core/patches/patch-install.sh
@@ -2,14 +2,15 @@ $NetBSD$
 
 Change manpages installation directory.
 
---- install.sh.orig	2021-10-22 08:06:36.000000000 +0000
+--- install.sh.orig	2024-03-24 15:01:17.000000000 +0000
 +++ install.sh
-@@ -13,11 +13,11 @@ if [[ -z "$PREFIX" ]]; then
+@@ -14,13 +14,13 @@ if [[ -z "$PREFIX" ]]; then
    exit 1
  fi
  
 -install -d -m 755 "$PREFIX"/{bin,libexec/bats-core,"${LIBDIR}"/bats-core,share/man/man{1,7}}
 +install -d -m 755 "$PREFIX"/{bin,libexec/bats-core,"${LIBDIR}"/bats-core,"$PKGMANDIR"/man{1,7}}
+ 
  install -m 755 "$BATS_ROOT/bin"/* "$PREFIX/bin"
  install -m 755 "$BATS_ROOT/libexec/bats-core"/* "$PREFIX/libexec/bats-core"
  install -m 755 "$BATS_ROOT/lib/bats-core"/* "$PREFIX/${LIBDIR}/bats-core"
@@ -18,4 +19,5 @@ Change manpages installation directory.
 +install -m 644 "$BATS_ROOT/man/bats.1" "$PREFIX/$PKGMANDIR/man1"
 +install -m 644 "$BATS_ROOT/man/bats.7" "$PREFIX/$PKGMANDIR/man7"
  
- echo "Installed Bats to $PREFIX/bin/bats"
+ read -rd '' BATS_EXE_CONTENTS <"$PREFIX/bin/bats" || true
+ BATS_EXE_CONTENTS=${BATS_EXE_CONTENTS/"BATS_BASE_LIBDIR=lib"/"BATS_BASE_LIBDIR=${LIBDIR}"}
