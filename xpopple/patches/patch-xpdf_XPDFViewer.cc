417e0e0211c073aec89433c20fc49703c85581f4
Michael Baeuerle <micha@NetBSD.org>
2024-04-22 17:16:57 +0200

    xpopple: Update to git revision 9fae34b25c6e8e5d4e95bcc67531963c1c80cc33

--- xpopple/patches/patch-xpdf_XPDFViewer.cc
+++ xpopple/patches/patch-xpdf_XPDFViewer.cc
@@ -1,10 +1,35 @@
 $NetBSD$
 
 Include XPM icon. Required for HAVE_X11_XPM_H.
+Include "PDFDoc.h" early to avoid namespace clash with X11 macro "None":
 
---- xpdf/XPDFViewer.cc.orig	2022-09-28 11:53:00.000000000 +0000
+/usr/pkgsrc/work/wip/xpopple/work/.buildlink/include/X11/X.h:115:30: error: expected identifier before numeric constant
+  115 | #define None                 0L /* universal null resource or null atom */
+      |                              ^~
+/usr/pkgsrc/work/wip/xpopple/work/.buildlink/include/poppler/XRef.h:104:5: note: in expansion of macro 'None'
+  104 |     None, /* No compression */
+      |     ^~~~
+
+--- xpdf/XPDFViewer.cc.orig	2024-04-22 14:53:38.000000000 +0000
 +++ xpdf/XPDFViewer.cc
-@@ -103,6 +103,9 @@
+@@ -11,6 +11,8 @@
+ 
+ #include <poppler-config.h>
+ 
++#include "PDFDoc.h"
++
+ #include <stdlib.h>
+ #include <string.h>
+ #include <ctype.h>
+@@ -33,7 +35,6 @@
+ #include "Error.h"
+ #include "GlobalParams.h"
+ #include "XPDFParams.h"
+-#include "PDFDoc.h"
+ #include "Link.h"
+ #include "ErrorCodes.h"
+ #include "Outline.h"
+@@ -103,6 +104,9 @@
  // GUI includes
  //------------------------------------------------------------------------
  
