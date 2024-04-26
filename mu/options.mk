978413acbaef042110b214a2395104124dd91d35
Thomas Klausner <wiz@NetBSD.org>
2024-04-26 16:25:17 +0200

    mu: clean up, simplify
    XXX: options.mk needs fixing for meson
    XXX: guile option needs testing

--- mu/options.mk
+++ mu/options.mk
@@ -14,14 +14,17 @@ INFO_FILES=	yes
 USE_TOOLS+=	makeinfo
 PLIST_SRC+=	PLIST.emacs
 .else
+# TODO: update for meson
 CONFIGURE_ARGS+= --disable-mu4e
 .endif
 
 .if !empty(PKG_OPTIONS:Mguile)
 .include "../../lang/guile22/buildlink3.mk"
 PLIST_SRC+=	PLIST.guile
+# TODO: update for meson
 CONFIGURE_ENV+= ac_cv_path_GUILE=guile
 USE_TOOLS+= makeinfo
 .else
+# TODO: update for meson
 CONFIGURE_ARGS+= --disable-guile
 .endif
