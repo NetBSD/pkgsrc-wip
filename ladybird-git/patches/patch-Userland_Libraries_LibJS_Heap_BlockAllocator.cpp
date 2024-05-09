c6a62279a9012aee8d988d9d874b1ffc601dd927
Thomas Klausner <wiz@NetBSD.org>
2024-05-09 21:17:32 +0200

    ladybird-git: update using patches from upstream and a new one
    Still core dumps, a problem in getaddrinfo, see
    https://github.com/SerenityOS/serenity/issues/23375

--- ladybird-git/patches/patch-Userland_Libraries_LibJS_Heap_BlockAllocator.cpp
+++ ladybird-git/patches/patch-Userland_Libraries_LibJS_Heap_BlockAllocator.cpp
@@ -1,19 +1,28 @@
 $NetBSD$
 
-Fix compilation on NetBSD.
-https://github.com/SerenityOS/serenity/issues/23375
+Fix NetBSD & FreeBSD compatibility.
+https://github.com/SerenityOS/serenity/pull/24273
 
---- Userland/Libraries/LibJS/Heap/BlockAllocator.cpp.orig	2024-02-27 18:49:14.164744463 +0000
+--- Userland/Libraries/LibJS/Heap/BlockAllocator.cpp.orig	2024-05-09 18:56:01.000000000 +0000
 +++ Userland/Libraries/LibJS/Heap/BlockAllocator.cpp
-@@ -52,8 +52,10 @@ void* BlockAllocator::allocate_block([[m
-         return block;
+@@ -55,9 +55,9 @@ void* BlockAllocator::allocate_block([[m
      }
  
--#ifdef AK_OS_SERENITY
-+#if defined(AK_OS_SERENITY)
-     auto* block = (HeapBlock*)serenity_mmap(nullptr, HeapBlock::block_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_RANDOMIZED | MAP_PRIVATE, 0, 0, HeapBlock::block_size, name);
-+#elif defined(AK_OS_NETBSD)
-+    auto* block = (HeapBlock*)mmap(nullptr, HeapBlock::block_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+ #ifdef AK_OS_SERENITY
+-    auto* block = (HeapBlock*)serenity_mmap(nullptr, HeapBlock::block_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_RANDOMIZED | MAP_PRIVATE, 0, 0, HeapBlock::block_size, name);
++    auto* block = (HeapBlock*)serenity_mmap(nullptr, HeapBlock::block_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_RANDOMIZED | MAP_PRIVATE, -1, 0, HeapBlock::block_size, name);
  #else
-     auto* block = (HeapBlock*)mmap(nullptr, HeapBlock::block_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
+-    auto* block = (HeapBlock*)mmap(nullptr, HeapBlock::block_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
++    auto* block = (HeapBlock*)mmap(nullptr, HeapBlock::block_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
  #endif
+     VERIFY(block != MAP_FAILED);
+     LSAN_REGISTER_ROOT_REGION(block, HeapBlock::block_size);
+@@ -74,7 +74,7 @@ void BlockAllocator::deallocate_block(vo
+         perror("munmap");
+         VERIFY_NOT_REACHED();
+     }
+-    if (mmap(block, HeapBlock::block_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED, 0, 0) != block) {
++    if (mmap(block, HeapBlock::block_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED, -1, 0) != block) {
+         perror("mmap");
+         VERIFY_NOT_REACHED();
+     }
