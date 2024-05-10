6bf6fd5fb5a203a8539762e87a2a948d8de545db
Thomas Klausner <wiz@NetBSD.org>
2024-05-10 08:36:13 +0200

    boringssl: modernize package, install shared libraries

---
+++ boringssl/go-modules.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20210513164829-c07d793c2f9a.mod
+GO_MODULE_FILES+=	golang.org/x/crypto/@v/v0.0.0-20210513164829-c07d793c2f9a.zip
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20210226172049-e18ecbb05110.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20210614182718-04defd469f4e.mod
+GO_MODULE_FILES+=	golang.org/x/net/@v/v0.0.0-20210614182718-04defd469f4e.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20201119102817-f84b799fce68.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210423082822-04245dca01da.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20210423082822-04245dca01da.zip
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20201126162022-7de9c90e9dd1.mod
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.0.0-20201126162022-7de9c90e9dd1.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.3.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.6.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.3.6.zip
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180917221912-90fa682c2a6e.mod
+GO_MODULE_FILES+=	golang.org/x/tools/@v/v0.0.0-20180917221912-90fa682c2a6e.zip
