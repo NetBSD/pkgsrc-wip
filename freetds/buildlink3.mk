c3a05fa5bc272ed8c67d737b1541b7b304e33a05
Bartosz Kuzma <bartosz.kuzma@gmail.com>
2024-05-10 02:22:17 +0000

    Fix buildlink path

--- freetds/buildlink3.mk
+++ freetds/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	freetds
 FREETDS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.freetds+=	freetds>=1.4
-BUILDLINK_PKGSRCDIR.freetds?=	../../databases/freetds
+BUILDLINK_PKGSRCDIR.freetds?=	../../wip/freetds
 
 .include "../../converters/libiconv/buildlink3.mk"
 
