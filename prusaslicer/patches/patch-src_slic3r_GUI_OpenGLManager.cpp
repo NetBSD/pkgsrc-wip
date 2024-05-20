8cf1f08932b7bc640ac4581f0384e6ece586f5fa
Paul Ripke <stix@stix.id.au>
2024-05-20 10:26:35 +1000

    prusaslicer: Allow running on machines with OpenGL 3.0 & 3.1
    Discussion in:
    https://github.com/prusa3d/PrusaSlicer/issues/12334

--- prusaslicer/patches/patch-src_slic3r_GUI_OpenGLManager.cpp
+++ prusaslicer/patches/patch-src_slic3r_GUI_OpenGLManager.cpp
@@ -4,9 +4,13 @@ Workaround EGL initialisation failure.
 See discussions at, eg:
 https://github.com/nigels-com/glew/issues/273
 
---- src/slic3r/GUI/OpenGLManager.cpp.orig	2023-06-02 13:41:15.000000000 +0000
+Allow running on machines with OpenGL 3.0 & 3.1.
+Discussion in:
+https://github.com/prusa3d/PrusaSlicer/issues/12334
+
+--- src/slic3r/GUI/OpenGLManager.cpp.orig	2024-02-23 10:58:40.000000000 +0000
 +++ src/slic3r/GUI/OpenGLManager.cpp
-@@ -336,7 +336,7 @@ bool OpenGLManager::init_gl()
+@@ -344,7 +344,7 @@ bool OpenGLManager::init_gl()
          glewExperimental = true;
  #endif // ENABLE_GL_CORE_PROFILE || ENABLE_OPENGL_ES
          GLenum err = glewInit();
@@ -15,3 +19,12 @@ https://github.com/nigels-com/glew/issues/273
              BOOST_LOG_TRIVIAL(error) << "Unable to init glew library: " << glewGetErrorString(err);
              return false;
          }
+@@ -373,7 +373,7 @@ bool OpenGLManager::init_gl()
+ #if ENABLE_OPENGL_ES
+         bool valid_version = s_gl_info.is_version_greater_or_equal_to(2, 0);
+ #elif ENABLE_GL_CORE_PROFILE
+-        const bool valid_version = s_gl_info.is_version_greater_or_equal_to(3, 2);
++        const bool valid_version = s_gl_info.is_version_greater_or_equal_to(3, 0);
+ #else
+         bool valid_version = s_gl_info.is_version_greater_or_equal_to(2, 0);
+ #endif // ENABLE_OPENGL_ES
