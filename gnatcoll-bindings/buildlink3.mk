6036b6c6abf8c98223310dfd02c1a43f0f13202e
Dmytro Kazankov <dmytro.kazankov@gmail.com>
2024-05-21 12:29:59 +0300

    devel/gnatcoll-bindings: Add 24.0.0
    * The GNAT Components Collection - Bindings Ada library.
    * Development package - only static libraries are provided.

---
+++ gnatcoll-bindings/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.0 2024/05/14 09:30:00 dkazankov Exp $
+
+BUILDLINK_TREE+=	gnatcoll-bindings
+
+.if !defined(GNATCOLL_BINDINGS_BUILDLINK3_MK)
+GNATCOLL_BINDINGS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gnatcoll-bindings+=	gnatcoll-bindings>=24.0.0
+BUILDLINK_PKGSRCDIR.gnatcoll-bindings=		../../wip/gnatcoll-bindings
+BUILDLINK_DEPMETHOD.gnatcoll-bindings?=		build
+
+BUILDLINK_FILES.gnatcoll-bindings=	include/gnatcoll_*/* lib/gnatcoll_*/*
+BUILDLINK_FILES.gnatcoll-bindings+=	share/gpr/manifests/gnatcoll_* share/gpr/gnatcoll_*.gpr
+
+BUILDLINK_CONTENTS_FILTER.gnatcoll-bindings=	\
+	${EGREP} '(include/.*\.ads$$|lib/.*\.ali$$|lib/.*\.a$$|share/gpr/manifests/.*|share/gpr/.*\.gpr)'
+
+.endif
+
+BUILDLINK_TREE+=	-gnatcoll-bindings
---
+++ gnatcoll-bindings/options.mk
@@ -0,0 +1,53 @@
+# $NetBSD: options.mk,v 1.0 2024/05/13 15:00:00 dkazankov Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.gnatcoll-bindings
+PKG_SUPPORTED_OPTIONS=	gmp iconv lzma omp python python3 readline syslog zlib
+PKG_SUGGESTED_OPTIONS=	gmp iconv lzma omp readline syslog zlib
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgmp)
+.  include "../../devel/gmp/buildlink3.mk"
+BINDINGS+=	gmp
+.endif
+
+.if !empty(PKG_OPTIONS:Miconv)
+.  include "../../converters/libiconv/buildlink3.mk"
+BINDINGS+=	iconv
+.endif
+
+.if !empty(PKG_OPTIONS:Mlzma)
+BINDINGS+=	lzma
+.endif
+
+.if !empty(PKG_OPTIONS:Momp)
+BINDINGS+=	omp
+.endif
+
+.if !empty(PKG_OPTIONS:Mpython)
+PYTHON_VERSIONS_ACCEPTED+=	27
+.  include "../../lang/python27/buildlink3.mk"
+BINDINGS+=	python
+.endif
+
+.if !empty(PKG_OPTIONS:Mpython3)
+PYTHON_VERSIONS_ACCEPTED+=	312 311 310 39 38
+.  include "../../lang/python312/buildlink3.mk"
+BINDINGS+=	python3
+.endif
+
+.if !empty(PKG_OPTIONS:Mreadline)
+.  include "../../devel/readline/buildlink3.mk"
+BINDINGS+=	readline
+.endif
+
+.if !empty(PKG_OPTIONS:Msyslog)
+BINDINGS+=	syslog
+.endif
+
+.if !empty(PKG_OPTIONS:Mzlib)
+.  include "../../devel/zlib/buildlink3.mk"
+BINDINGS+=	zlib
+.endif
