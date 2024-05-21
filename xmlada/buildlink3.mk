71a2b3d070ca15ddb695ab4258354370b433dbfa
Dmytro Kazankov <dmytro.kazankov@gmail.com>
2024-05-21 12:25:12 +0300

    devel/xmlada: Add 24.0.0
    * XML/Ada - an XML parser for Ada95.
    * The package is needed to build new gprlib (LIBGPR) and gnatcoll-core packages.
    * Development package - only static libraries are provided.
      For shared libraries, a separate package is supposed to be created (libxmlada?).

---
+++ xmlada/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.0 2024/05/06 16:00:00 dkazankov Exp $
+
+BUILDLINK_TREE+=	xmlada
+
+.if !defined(XMLADA_BUILDLINK3_MK)
+XMLADA_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.xmlada+=	xmlada>=24.0.0
+BUILDLINK_PKGSRCDIR.xmlada=	../../wip/xmlada
+BUILDLINK_DEPMETHOD.xmlada?=	build
+
+BUILDLINK_AUTO_VARS.xmlada=	no
+BUILDLINK_AUTO_DIRS.xmlada=	no
+
+BUILDLINK_FILES.xmlada=		include/xmlada/* lib/xmlada/*
+BUILDLINK_FILES.xmlada+=	share/gpr/manifests/xmlada share/gpr/xmlada*.gpr
+
+BUILDLINK_CONTENTS_FILTER.xmlada=	\
+	${EGREP} '(include/.*\.ads$$|lib/.*\.ali$$|lib/.*\.a$$|share/gpr/manifests/.*|share/gpr/.*\.gpr$$)'
+
+.endif
+
+BUILDLINK_TREE+=	-xmlada
