571885cef855962e946e26f61e3e3ec45b7cf23e
Dmytro Kazankov <dmytro.kazankov@gmail.com>
2024-05-21 12:27:42 +0300

    devel/gprlib: Add 24.0.0
    * LIBGPR library is part of GPRbuild project.
    * The package is needed to build gnatcoll-core package.
    * Development package - only static libraries are provided.
    * For shared libraries, a separate package is supposed to be created
      (libgnatprj?).
    * libgpr.so name conflicts with the name of internal gRPC library,
      so libgnatprj (as linux distros did) for library and gprlib for package
      names were chosen.

---
+++ gprlib/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.0 2024/05/06 18:00:00 dkazankov Exp $
+
+BUILDLINK_TREE+=	gprlib
+
+.if !defined(GPRLIB_BUILDLINK3_MK)
+GPRLIB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gprlib+=	gprlib>=24.0.0
+BUILDLINK_PKGSRCDIR.gprlib=	../../wip/gprlib
+BUILDLINK_DEPMETHOD.gprlib?=	build
+
+BUILDLINK_AUTO_VARS.gprlib=	no
+BUILDLINK_AUTO_DIRS.gprlib=	no
+
+BUILDLINK_FILES.gprlib=		include/gpr/* lib/gpr/*
+BUILDLINK_FILES.gprlib+=	share/gpr/manifests/gpr share/gpr/gpr.gpr
+
+BUILDLINK_CONTENTS_FILTER.gprlib=	\
+	${EGREP} '(include/.*\.ads$$|lib/.*\.ali$$|lib/.*\.a$$|share/gpr/manifests/.*|share/gpr/.*\.gpr$$|/gpr\.adb$$|/gpr-util-put_resource_usage__unix\.adb$$)'
+
+.endif
+
+BUILDLINK_TREE+=	-gprlib
