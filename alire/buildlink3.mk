a9a60fea6f1e712f3d30f7e1073e626dcef3022c
Dmytro Kazankov <dmytro.kazankov@gmail.com>
2024-05-21 12:18:29 +0300

    devel/alire: Update to 2.0.1nb1
    * Package updated to upstream 2.0.1 release.
    * Build type changed to "release" to avoid runtime exceptions:
      some patches added/changed, Makefile modified.
    * Build process moved to gprbuild.mk, so wip version of gprbuild
      required to build

--- alire/buildlink3.mk
+++ alire/buildlink3.mk
@@ -6,8 +6,15 @@ BUILDLINK_TREE+=	alire
 ALIRE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.alire+=	alire>=2.0.0rc1
-BUILDLINK_PKGSRCDIR.alire=	../../devel/alire
+BUILDLINK_PKGSRCDIR.alire=	../../wip/alire
 BUILDLINK_DEPMETHOD.alire?=	build
+
+BUILDLINK_AUTO_VARS.alire=	no
+BUILDLINK_AUTO_DIRS.alire=	no
+
+BUILDLINK_FILES.alire=			bin/alr share/gpr/manifests/alr*
+BUILDLINK_CONTENTS_FILTER.alire=	\
+	${EGREP} '(bin/alr|share/gpr/manifests/alr.*)'
 
 .endif
 
