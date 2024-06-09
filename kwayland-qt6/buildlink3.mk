0c2d36794debbc0e52ca8088f114b74b7aa48979
Mark Davies <mark@ecs.vuw.ac.nz>
2024-06-09 12:58:00 +1200

    kwayland-qt6: tidy up

--- kwayland-qt6/buildlink3.mk
+++ kwayland-qt6/buildlink3.mk
@@ -1,16 +1,16 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	kwayland
+BUILDLINK_TREE+=	plasma6-kwayland
 
-.if !defined(KWAYLAND_BUILDLINK3_MK)
-KWAYLAND_BUILDLINK3_MK:=
+.if !defined(PLASMA6_KWAYLAND_BUILDLINK3_MK)
+PLASMA6_KWAYLAND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kwayland+=	kwayland>=6.0.5
-BUILDLINK_PKGSRCDIR.kwayland?=	../../wip/kwayland-qt6
+BUILDLINK_API_DEPENDS.plasma6-kwayland+=	plasma6-kwayland>=6.0.5
+BUILDLINK_PKGSRCDIR.plasma6-kwayland?=		../../wip/kwayland-qt6
 
 .include "../../devel/wayland-protocols/buildlink3.mk"
 .include "../../x11/qt6-qtbase/buildlink3.mk"
 .include "../../devel/qt6-qtwayland/buildlink3.mk"
-.endif	# KWAYLAND_BUILDLINK3_MK
+.endif	# PLASMA6_KWAYLAND_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-kwayland
+BUILDLINK_TREE+=	-plasma6-kwayland
