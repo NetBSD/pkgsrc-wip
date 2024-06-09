8053c3165c43b134f81c3019a12e3b2ca0d71823
Mark Davies <mark@ecs.vuw.ac.nz>
2024-06-09 12:56:36 +1200

    layer-shell-qt6: tidy up.

--- layer-shell-qt6/buildlink3.mk
+++ layer-shell-qt6/buildlink3.mk
@@ -1,18 +1,18 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	layer-shell-qt
+BUILDLINK_TREE+=	plasma6-layer-shell-qt
 
-.if !defined(LAYER_SHELL_QT_BUILDLINK3_MK)
-LAYER_SHELL_QT_BUILDLINK3_MK:=
+.if !defined(PLASMA6_LAYER_SHELL_QT_BUILDLINK3_MK)
+PLASMA6_LAYER_SHELL_QT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.layer-shell-qt+=	layer-shell-qt>=6.0.5
-BUILDLINK_PKGSRCDIR.layer-shell-qt?=	../../wip/layer-shell-qt6
+BUILDLINK_API_DEPENDS.plasma6-layer-shell-qt+=	plasma6-layer-shell-qt>=6.0.5
+BUILDLINK_PKGSRCDIR.plasma6-layer-shell-qt?=	../../wip/layer-shell-qt6
 
 .include "../../devel/wayland-protocols/buildlink3.mk"
 .include "../../x11/qt6-qtbase/buildlink3.mk"
 .include "../../devel/qt6-qtwayland/buildlink3.mk"
 .include "../../lang/qt6-qtdeclarative/buildlink3.mk"
 .include "../../x11/libxkbcommon/buildlink3.mk"
-.endif	# LAYER_SHELL_QT_BUILDLINK3_MK
+.endif	# PLASMA6_LAYER_SHELL_QT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-layer-shell-qt
+BUILDLINK_TREE+=	-plasma6-layer-shell-qt
