84d00c00827c607b6beaad2a91c98f6d13c56d08
Mark Davies <mark@ecs.vuw.ac.nz>
2024-06-09 12:58:34 +1200

    libkscreen-qt6: tidy up

--- libkscreen-qt6/buildlink3.mk
+++ libkscreen-qt6/buildlink3.mk
@@ -1,17 +1,17 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	libkscreen
+BUILDLINK_TREE+=	plasma6-libkscreen
 
-.if !defined(LIBKSCREEN_BUILDLINK3_MK)
-LIBKSCREEN_BUILDLINK3_MK:=
+.if !defined(PLASMA6_LIBKSCREEN_BUILDLINK3_MK)
+PLASMA6_LIBKSCREEN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkscreen+=	libkscreen>=6.0.5
-BUILDLINK_PKGSRCDIR.libkscreen?=	../../wip/libkscreen-qt6
+BUILDLINK_API_DEPENDS.plasma6-libkscreen+=	plasma6-libkscreen>=6.0.5
+BUILDLINK_PKGSRCDIR.plasma6-libkscreen?=	../../wip/libkscreen-qt6
 
 .include "../../devel/kf6-kconfig/buildlink3.mk"
 .include "../../wip/kwayland-qt6/buildlink3.mk"
 .include "../../devel/plasma-wayland-protocols/buildlink3.mk"
 .include "../../x11/qt6-qtbase/buildlink3.mk"
-.endif	# LIBKSCREEN_BUILDLINK3_MK
+.endif	# PLASMA6_LIBKSCREEN_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-libkscreen
+BUILDLINK_TREE+=	-plasma6-libkscreen
