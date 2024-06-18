82a6160b37b42ec6b333189411ba26742f87326d
Jonathan Perkin <jonathan@perkin.org.uk>
2024-06-18 15:56:14 +0100

    rust179: Add initial version.
    Based on rust178, with a bunch of patches removed as the source files no
    longer exist.  Tested on SmartOS.

---
+++ rust179/platform.mk
@@ -0,0 +1,26 @@
+# $NetBSD: platform.mk,v 1.6 2020/12/26 10:04:51 nia Exp $
+
+# This file encodes whether a given platform has support for rust.
+
+# Platforms where rust ought to work but does not require a link to an
+# open PR.
+
+.if !defined(PLATFORM_SUPPORTS_RUST)
+
+# Rust needs NetBSD>7
+.for rust_arch in aarch64 earmv7hf i386 powerpc riscv64 sparc64 x86_64
+.  for rust_os in Darwin FreeBSD Linux NetBSD SunOS
+.    if ${OPSYS} != "NetBSD" || empty(OS_VERSION:M[0-7].*)
+RUST_PLATFORMS+=	${rust_os}-*-${rust_arch}
+.    endif
+.  endfor
+.endfor
+
+.for rust_platform in ${RUST_PLATFORMS}
+.  if !empty(MACHINE_PLATFORM:M${rust_platform})
+PLATFORM_SUPPORTS_RUST=		yes
+.  endif
+.endfor
+PLATFORM_SUPPORTS_RUST?=	no
+
+.endif # !defined(PLATFORM_SUPPORTS_RUST)
