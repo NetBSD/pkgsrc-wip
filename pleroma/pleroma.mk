3b51a28c6e37321a5cc51cf33b67810d3eeb48cb
xorrvin <xorrvin@pm.me>
2024-06-21 03:16:13 +0200

    pleroma: fix permissions on install and update scripts to mksh

--- pleroma/pleroma.mk
+++ pleroma/pleroma.mk
@@ -6,6 +6,7 @@
 
 RM?=rm
 LN?=ln
+SED?=sed
 PAX?=pax
 FIND?=find
 ECHO?=echo
@@ -15,7 +16,7 @@ INSTALL?=install
 INSTALL_DIR?=$(INSTALL) -m755 -d
 INSTALL_DATA?=$(INSTALL) -m644
 INSTALL_PROGRAM?=$(INSTALL) -m755
-ERTS_VERSION=erts-$(shell erl -eval 'io:format(erlang:system_info(version)), halt().' -noshell)
+#ERTS_VERSION=erts-$(shell erl -eval 'io:format(erlang:system_info(version)), halt().' -noshell)
 
 all: release
 deps: | deps/.deps-done
@@ -26,6 +27,7 @@ clean:
 	$(RM) -rf deps
 	$(RM) -rf release
 	$(RM) -rf config/prod.secret.exs
+	$(RM) -rf $(TARGET_PREFIX)/*
 
 config/.config-done:
 	$(MKDIR) -p config
@@ -40,14 +42,16 @@ deps/.deps-done: | config/.config-done
 release/.release-done: | deps/.deps-done
 	$(MKDIR) -p release
 	$(MIX) release --path release
+
+	$(FIND) release/bin/pleroma* -type f -exec $(SED) -i "1s|.*|\#!$(PLEROMA_SHELL)|" {} ";"
+	$(FIND) release/releases/$(PLEROMA_VERSION) -type f \( -name elixir -or -name env.sh -or -name iex \) \
+		-exec $(SED) -i "1s|.*|\#!$(PLEROMA_SHELL)|" {} ";"
+
 	$(TOUCH) $@
 
 install: release/.release-done
-	$(RM) -rf $(TARGET_PREFIX)/*
 	$(INSTALL_DIR) $(PLEROMA_PREFIX)
 	$(INSTALL_DATA) release/OTP_VERSION	$(PLEROMA_PREFIX)
-
-	$(INSTALL_DIR) $(PLEROMA_PREFIX)/etc/pleroma
 
 	$(INSTALL_DIR) release/bin $(TARGET_PREFIX)/bin
 	$(INSTALL_PROGRAM) $(wildcard release/bin/*) $(TARGET_PREFIX)/bin
@@ -57,3 +61,11 @@ install: release/.release-done
 
 	$(MKDIR) -p $(PLEROMA_PREFIX)/releases
 	$(PAX) -rw -pp -s ':^release/releases::' release/releases $(PLEROMA_PREFIX)/releases
+
+	$(INSTALL_DIR) $(TARGET_PREFIX)/etc/pleroma
+	$(INSTALL_DIR) $(TARGET_PREFIX)/var/pleroma
+	$(INSTALL_DIR) $(TARGET_PREFIX)/share/examples/pleroma
+
+# only install if nginx option is enabled
+#	$(INSTALL_DATA) installation/pleroma.nginx $(TARGET_PREFIX)/share/examples
+	$(INSTALL_DATA) installation/netbsd/rc.d/pleroma $(TARGET_PREFIX)/share/examples/pleroma/pleroma.rc
