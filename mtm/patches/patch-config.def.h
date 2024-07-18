bf26728aca549efd1e9696bf208cea659a381918
Kevin Bloom <kevin.bloom@posteo.net>
2024-07-17 21:05:07 -0400

    mtm: added links to PRs

--- mtm/patches/patch-config.def.h
+++ mtm/patches/patch-config.def.h
@@ -1,19 +1,10 @@
 $NetBSD$
 
-NetBSD also uses util.h
+NetBSD also uses util.h. https://github.com/deadpixi/mtm/pull/78
 
---- config.def.h.orig	2021-10-08 20:38:13.000000000 +0000
+--- config.def.h.orig	2024-07-18 00:58:43.999974063 +0000
 +++ config.def.h
-@@ -63,6 +63,8 @@
- #ifndef NCURSESW_INCLUDE_H
-     #if defined(__APPLE__) || !defined(__linux__) || defined(__FreeBSD__) || defined(__OpenBSD__)
-         #define NCURSESW_INCLUDE_H <curses.h>
-+    #elif defined(__NetBSD__)
-+        #define NCURSESW_INCLUDE_H <ncurses/curses.h>
-     #else
-         #define NCURSESW_INCLUDE_H <ncursesw/curses.h>
-     #endif
-@@ -71,7 +73,7 @@
+@@ -71,7 +71,7 @@
  
  /* Includes needed to make forkpty(3) work. */
  #ifndef FORKPTY_INCLUDE_H
