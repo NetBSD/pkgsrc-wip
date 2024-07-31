909b4d934b29799837a46f760c67bd84d45ff6f7
Thomas Klausner <wiz@NetBSD.org>
2024-07-31 07:01:05 +0200

    ngtop: new package
    Untested, feel free to import if it works for you.
    
    ngtop is a command-line program to query request counts from nginx's
    access.log files.

---
+++ ngtop/go-modules.mk
@@ -0,0 +1,8 @@
+# $NetBSD$
+
+GO_MODULE_FILES+=	github.com/alecthomas/kong/@v/v0.9.0.mod
+GO_MODULE_FILES+=	github.com/alecthomas/kong/@v/v0.9.0.zip
+GO_MODULE_FILES+=	github.com/mattn/go-sqlite3/@v/v1.14.22.mod
+GO_MODULE_FILES+=	github.com/mattn/go-sqlite3/@v/v1.14.22.zip
+GO_MODULE_FILES+=	github.com/mileusna/useragent/@v/v1.3.4.mod
+GO_MODULE_FILES+=	github.com/mileusna/useragent/@v/v1.3.4.zip
