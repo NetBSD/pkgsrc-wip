69f6c2dda0388ee7db1496fdc30424a4fb50b140
Havard Eidnes <he@NetBSD.org>
2024-08-04 14:49:12 +0000

    rust178: turn off thread-local storage for NetBSD on 32-bit arm.
    This is due to NetBSD PR#58550, which is fixed with
      https://mail-index.netbsd.org/source-changes/2020/06/16/msg118407.html
    but isn't present in netbsd-9.

---
+++ rust178/patches/patch-compiler_rustc__target_src_spec_targets_armv6__unknown__netbsd__eabihf.rs
@@ -0,0 +1,18 @@
+$NetBSD$
+
+Turn off use of thread local storage on 32-bit arm, due to
+NetBSD PR#58550, which is fixed with
+https://mail-index.netbsd.org/source-changes/2020/06/16/msg118407.html
+but isn't present in netbsd-9, which presently has revisions 1.1-1.44 +
+1.46, but not 1.45 which fixes htis problem.
+
+--- compiler/rustc_target/src/spec/targets/armv6_unknown_netbsd_eabihf.rs.orig	2024-08-04 09:47:25.621735345 +0000
++++ compiler/rustc_target/src/spec/targets/armv6_unknown_netbsd_eabihf.rs
+@@ -17,6 +17,7 @@ pub fn target() -> Target {
+             features: "+v6,+vfp2,-d32".into(),
+             max_atomic_width: Some(64),
+             mcount: "__mcount".into(),
++            has_thread_local: false, // Attempt to get 9.0 working...
+             ..base::netbsd::opts()
+         },
+     }
