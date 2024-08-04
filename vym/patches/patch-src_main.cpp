d654d6ef082ed7b266faffa0584bd6757040586e
Iris <iris000@duck.com>
2024-08-04 18:56:24 +0000

    vym: Update to 2.9.28
    https://github.com/insilmaril/vym/blob/release/release-notes-2.9.md
    
    8f4e86ca Version 2.9.28
    c6c3823b Added Sven Höxter to aboutDialog for his patches
    cba17cc6 Feature: link app icon as a mimetype icon for the hicolor default theme (#109)
    01aae901 Fix: do not install manpage in doc dir (#107)
    201ca770 Bugfix: Open french documention if required
    97e0e837 Spelling fix: remove duplicate word (#108)
    1a3d001d Spelling fixes (#105)
    7eb79dde Desktop file improvements (#106)
    92a762ed Merge branch 'release' of github.com:insilmaril/vym into release
    d9d7b12a Updated project name
    5248d9a0 Fixed typo in version number
    0e87888d Updated year in README.md
    acdf0677 Updated release notes

--- vym/patches/patch-src_main.cpp
+++ vym/patches/patch-src_main.cpp
@@ -1,6 +1,10 @@
---- src/main.cpp.orig	2021-06-16 16:05:40.556415348 +0000
+$NetBSD$
+
+Fix hardcoded path
+
+--- src/main.cpp.orig	2024-05-10 15:50:35.000000000 +0000
 +++ src/main.cpp
-@@ -293,8 +293,8 @@
+@@ -301,8 +301,8 @@
      // Only Windows 10 has tar. Older windows versions not supported.
      zipToolPath = "tar";
  #else
--- vym/patches/patch-src_mainwindow.cpp
+++ vym/patches/patch-src_mainwindow.cpp
@@ -1,6 +1,10 @@
---- src/mainwindow.cpp.orig	2021-06-16 16:05:40.556415348 +0000
+$NetBSD$
+
+Fix hardcoded path
+
+--- src/mainwindow.cpp.orig	2024-05-10 15:50:35.000000000 +0000
 +++ src/mainwindow.cpp
-@@ -6572,6 +6572,7 @@
+@@ -7031,6 +7031,7 @@
      searchList << "doc"; // relative path for easy testing in tarball
      searchList << "/usr/share/doc/vym";      // Debian
      searchList << "/usr/share/doc/packages"; // Knoppix
