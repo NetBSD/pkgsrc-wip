fe00f07927d9155aa0c54c8d1a48699da17b556d
Brook Milligan <brook@nmsu.edu>
2024-08-06 17:15:05 -0600

    necat: fix build and clean-up

--- necat/patches/patch-src_pipeline_main.mk
+++ necat/patches/patch-src_pipeline_main.mk
@@ -2,8 +2,8 @@ $NetBSD$
 
 Do not set execute permissions on a non-executable file.
 
---- pipeline/main.mk.orig	2020-08-03 07:31:39.000000000 +0000
-+++ pipeline/main.mk
+--- src/pipeline/main.mk.orig	2020-08-03 07:31:39.000000000 +0000
++++ src/pipeline/main.mk
 @@ -23,7 +23,6 @@ ${TARGET_DIR}/Necat.pm: pipeline/Necat.p
  ${TARGET_DIR}/necat.sh: pipeline/necat.sh
  	@if [ ! -e ${TARGET_DIR} ] ; then mkdir -p ${TARGET_DIR} ; fi
