653e5a311a2bb2c2867f2889a6764c7b89b73bd3
Thomas Klausner <wiz@NetBSD.org>
2024-08-14 10:20:53 +0200

    xmoji: fix kqueue handling

--- xmoji/patches/patch-src_bin_xmoji_filewatcher.c
+++ xmoji/patches/patch-src_bin_xmoji_filewatcher.c
@@ -2,9 +2,16 @@ $NetBSD$
 
 https://github.com/Zirias/xmoji/pull/6
 
---- src/bin/xmoji/filewatcher.c.orig	2024-08-07 16:43:16.520801223 +0000
+--- src/bin/xmoji/filewatcher.c.orig	2024-08-12 16:29:36.000000000 +0000
 +++ src/bin/xmoji/filewatcher.c
-@@ -17,6 +17,9 @@
+@@ -1,4 +1,6 @@
++#ifdef __linux__
+ #define _POSIX_C_SOURCE 200809L
++#endif
+ 
+ /* Feature test hack ahead!
+  *
+@@ -17,6 +19,9 @@
  #    define _BSD_SOURCE
  #  endif
  #endif
