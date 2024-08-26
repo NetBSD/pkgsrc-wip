4d209b535c9677fb54db5492b498b8d01e281581
Atsushi Toyokura <asteria.at@gmail.com>
2024-08-26 23:42:45 +0900

    libSavitar: fix build

--- libSavitar/buildlink3.mk
+++ libSavitar/buildlink3.mk
@@ -8,6 +8,8 @@ LIBSAVITAR_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libSavitar+=	libSavitar>=4.11.0
 BUILDLINK_PKGSRCDIR.libSavitar?=	../../wip/libSavitar
 
+BUILDLINK_DEPMETHOD.py-sip4?=	build
+
 .include "../../wip/py-sip4/buildlink3.mk"
 .endif	# LIBSAVITAR_BUILDLINK3_MK
 
