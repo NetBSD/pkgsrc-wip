e15ac791b0269f477066b04f0ef66d70cd3c6d17
mayuresh <mayuresh@acm.org>
2024-08-26 15:48:13 +0530

    soplex: buildlink added

---
+++ soplex/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	soplex
+
+.if !defined(SOPLEX_BUILDLINK3_MK)
+SOPLEX_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.soplex+=	soplex>=7.1.0.0
+BUILDLINK_PKGSRCDIR.soplex?=	../../wip/soplex
+
+.include "../../math/mpfr/buildlink3.mk"
+.include "../../devel/boost-libs/buildlink3.mk"
+.include "../../devel/gmp/buildlink3.mk"
+.endif	# SOPLEX_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-soplex
