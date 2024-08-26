58d7fd1466203e34588af6e47d4a5cffa3bc1a7c
mayuresh <mayuresh@acm.org>
2024-08-26 18:47:55 +0530

    scip: initial revision

---
+++ scip/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	scip
+
+.if !defined(SCIP_BUILDLINK3_MK)
+SCIP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.scip+=	scip>=9.1.0
+BUILDLINK_PKGSRCDIR.scip?=	../../wip/scip
+
+.include "../../wip/soplex/buildlink3.mk"
+.include "../../math/ipopt/buildlink3.mk"
+.include "../../devel/boost-libs/buildlink3.mk"
+.include "../../devel/gmp/buildlink3.mk"
+.include "../../devel/readline/buildlink3.mk"
+.endif	# SCIP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-scip
