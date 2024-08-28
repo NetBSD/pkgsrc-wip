57625e01ac00d37503cd8ee974277c5631994b48
kikadf <kikadf.01@gmail.com>
2024-08-28 18:57:41 +0200

    sndio: fix rc file

--- sndio/files/sndio.sh
+++ sndio/files/sndio.sh
@@ -9,20 +9,8 @@
 . /etc/rc.subr
 
 name="sndio"
-rcvar=$name
+rcvar=${name}
 command="@PREFIX@/bin/sndiod"
-pidfile="@VARBASE@/run/sndio/pid"
-start_precmd=sndio_prestart
 
-sndio_prestart() {
-	dir="@VARBASE@/run/sndio"
-	if @TEST@ ! -d $dir; then
-		@MKDIR@ $dir
-		@CHMOD@ 0755 $dir
-		@CHOWN@ @DBUS_USER@:@DBUS_GROUP@ $dir
-	fi
-	@PREFIX@/bin/dbus-uuidgen --ensure
-}
-
-load_rc_config $name
+load_rc_config ${name}
 run_rc_command "$1"
