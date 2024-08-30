12b2fd797c6815c7538a470bbabdc65567a68fd0
Havard Eidnes <he@NetBSD.org>
2024-08-30 10:54:29 +0000

    rust180: Add initial version.
    Based on wip/rust179, with patches adapted / re-applied to new
    crates etc.  Build-verified natively on NetBSD/amd64, and cross-
    builds OK for all the NetBSD targets on NetBSD/amd64.
    
    Further testing / verification remains.

---
+++ rust180/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.14 2023/10/25 05:50:43 pin Exp $
+
+BUILDLINK_TREE+=	rust
+
+#
+# DO NOT include this directly! Use rust.mk instead.
+#
+
+.if !defined(RUST_BUILDLINK3_MK)
+RUST_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.rust+=	rust>=1.56.1
+BUILDLINK_PKGSRCDIR.rust?=	../../wip/rust
+BUILDLINK_PASSTHRU_DIRS+=	${PREFIX}/lib/rustlib
+.endif
+
+BUILDLINK_TREE+=	-rust
