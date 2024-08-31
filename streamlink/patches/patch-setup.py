3671acad725c6c18cf07f8df19f30f73573741b2
Yorick Hardy <yorickhardy@gmail.com>
2024-08-31 11:41:39 +0200

    streamlink: update to streamlink-6.9.0
    Changelog:
    
    == streamlink 6.9.0 (2024-08-12)
    
    - Added: `streamlink.plugin.api.webbrowser` subpackage with the
             `aws_waf` module
    - Added: `max_buffer_size` kwarg to `CDPClient.session()`
    - Added: webbrowser `User-Agent` header override in headless mode
    - Changed: default value of `--webbrowser-headless` from `True`
               to `False`
    - Changed: unicode characters to be unescaped in JSON output,
               if possible
    - Fixed: potential character encoding issues in Streamlink's logger
    - Updated plugins:
      - nicolive: updated plugin matcher
      - okru: fixed validation schema
      - radionet: removed plugin
      - sportschau: fixed plugin
      - tiktok: fixed room ID validation schema
      - tvp: added support for sport.tvp.pl
      - twitch: added `--twitch-force-client-integrity`
      - twitch: fixed broken client-integrity token decoding+parsing
      - twitch: removed the `headless=False` override
      - vimeo: removed error messages for unsupported DASH streams
      - vk: fixed validation schema
      - vtvgo: resolved AWS Web Application Firewall bot detection
      - yupptv: fixed plugin, added ad filtering
    - Docs: added webbrowser API metadata to plugin descriptions
    - Docs: updated build-dependencies and the furo theme

--- streamlink/patches/patch-setup.py
+++ streamlink/patches/patch-setup.py
@@ -2,17 +2,17 @@ $NetBSD$
 
 Install manpages in ${PKGMANDIR}.
 
---- setup.py.orig	2024-03-23 12:22:35.022673000 +0000
+--- setup.py.orig	2024-08-12 13:19:52.995938500 +0000
 +++ setup.py
 @@ -1,6 +1,6 @@
  #!/usr/bin/env python
  import sys
 -from os import path
 +from os import getenv, path
- from sys import argv, exit, version_info
  from textwrap import dedent
  
-@@ -65,7 +65,7 @@ data_files = [
+ 
+@@ -64,7 +64,7 @@ data_files = [
      # man page
      #  requires pre-built man page file via sphinx (docs-requirements.txt)
      #  `make --directory=docs clean man`
