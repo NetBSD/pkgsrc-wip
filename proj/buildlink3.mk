1da38224b6182f9aef55a63c4a03b5b778dc00db
Greg Troxel <gdt@lexort.com>
2024-09-01 18:27:46 -0400

    proj: sync with pkgsrc, preparing for next update

--- proj/buildlink3.mk
+++ proj/buildlink3.mk
@@ -1,16 +1,20 @@
-# $NetBSD: buildlink3.mk,v 1.13 2022/11/27 13:55:49 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2024/09/01 14:56:33 gdt Exp $
 
 BUILDLINK_TREE+=	proj
 
 .if !defined(PROJ_BUILDLINK3_MK)
+
 PROJ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.proj+=	proj>=5.0.0
-BUILDLINK_ABI_DEPENDS.proj+=	proj>=8.0.0
+# Proj withdrew the old API with 8.0.0.
+BUILDLINK_API_DEPENDS.proj+=	proj>=8.0.0
+BUILDLINK_ABI_DEPENDS.proj+=	proj>=9.4.1
 BUILDLINK_PKGSRCDIR.proj?=	../../wip/proj
+
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
 .include "../../www/curl/buildlink3.mk"
+
 .endif # PROJ_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-proj
