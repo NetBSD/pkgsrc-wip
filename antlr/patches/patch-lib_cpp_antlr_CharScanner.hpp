95efc0725ceb4c9ded9fc9aa343be8294a422202
mayuresh <mayuresh@acm.org>
2024-09-17 18:34:03 +0530

    antlr: package restored, removed RMD160 signature, removed files/ as the script included wasn't getting packaged, removed TODO about testing  - have been using this package for long.
    This is a legacy parser generator, not maintained upstream. However its newer
    versions are not backward compatible. Hence some dependent packages require
    keeping this alive.

---
+++ antlr/patches/patch-lib_cpp_antlr_CharScanner.hpp
@@ -0,0 +1,16 @@
+$NetBSD$
+
+# Missing headers
+
+--- lib/cpp/antlr/CharScanner.hpp.orig	2006-11-01 21:37:17.000000000 +0000
++++ lib/cpp/antlr/CharScanner.hpp
+@@ -24,6 +24,9 @@
+ # include <stdio.h>
+ #endif
+ 
++#include <cstdio>
++#include <cstring>
++
+ #include <antlr/TokenStream.hpp>
+ #include <antlr/RecognitionException.hpp>
+ #include <antlr/SemanticException.hpp>
