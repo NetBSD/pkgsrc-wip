bb69a63bbef48983943b0ec06115805d5d059f6d
vsyakovetsky <vsyakovetsky@gmail.com>
2024-09-18 01:24:18 +0300

    mtr-graph: bump to 0.85.159 (cmake mtr085)

--- mtr-graph/options.mk
+++ mtr-graph/options.mk
@@ -1,62 +1,56 @@
 # $NetBSD$
 
-PKG_OPTIONS_VAR=		PKG_OPTIONS.mtr-graph
-PKG_SUPPORTED_OPTIONS=		inet6
-PKG_SUPPORTED_OPTIONS+=		mpls
+PKG_OPTIONS_VAR=		PKG_OPTIONS.mtr085
+
 PKG_SUPPORTED_OPTIONS+=		dns
 PKG_SUPPORTED_OPTIONS+=		idn
 PKG_SUPPORTED_OPTIONS+=		unicode
 PKG_SUPPORTED_OPTIONS+=		ipinfo
-PKG_OPTIONS_OPTIONAL_GROUPS+=	graphcairo
-PKG_OPTIONS_GROUP.graphcairo+=	graphcairo-xcb
-PKG_OPTIONS_GROUP.graphcairo+=	graphcairo-xlib
-PKG_SUGGESTED_OPTIONS+=		mpls
-PKG_SUGGESTED_OPTIONS+=		dns
-PKG_SUGGESTED_OPTIONS+=		unicode
-PKG_SUGGESTED_OPTIONS+=		ipinfo
+PKG_SUPPORTED_OPTIONS+=		inet6
+PKG_SUPPORTED_OPTIONS+=		mpls
+PKG_SUPPORTED_OPTIONS+=		graph
+
+PKG_SUGGESTED_OPTIONS+=	dns
+PKG_SUGGESTED_OPTIONS+=	idn
+PKG_SUGGESTED_OPTIONS+=	unicode
+PKG_SUGGESTED_OPTIONS+=	ipinfo
+PKG_SUGGESTED_OPTIONS+=	inet6
+PKG_SUGGESTED_OPTIONS+=	mpls
+
 PKG_OPTIONS_LEGACY_OPTS=	libidn:idn
 
 .include "../../mk/bsd.options.mk"
 
 .if empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGS+=	--disable-ipv6
+CMAKE_CONFIGURE_ARGS+=	-DIPV6=OFF
 .endif
 
 .if empty(PKG_OPTIONS:Mdns)
-CONFIGURE_ARGS+=	--disable-dns
+CMAKE_CONFIGURE_ARGS+=	-DDNS=OFF
 .endif
 
 .if empty(PKG_OPTIONS:Mmpls)
-CONFIGURE_ARGS+=	--without-mpls
+CMAKE_CONFIGURE_ARGS+=	-DMPLS=OFF
 .endif
 
 .if empty(PKG_OPTIONS:Mipinfo)
-CONFIGURE_ARGS+=	--without-ipinfo
+CMAKE_CONFIGURE_ARGS+=	-DIPINFO=OFF
 .endif
 
-.if !empty(PKG_OPTIONS:Mgraphcairo-xcb)
-USE_TOOLS+=		pkg-config
+.if !empty(PKG_OPTIONS:Mgraph)
 .include "../../graphics/cairo/buildlink3.mk"
 .include "../../devel/pango/buildlink3.mk"
 .include "../../x11/libxcb/buildlink3.mk"
 .include "../../x11/xcb-util-keysyms/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-graphcairo-xcb
-.else
-.  if !empty(PKG_OPTIONS:Mgraphcairo-xlib)
-USE_TOOLS+=		pkg-config
-.include "../../graphics/cairo/buildlink3.mk"
-.include "../../devel/pango/buildlink3.mk"
-.include "../../x11/libX11/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-graphcairo-xlib
-.  endif
+CMAKE_CONFIGURE_ARGS+=	-DGRAPH=ON
 .endif
 
 .if !empty(PKG_OPTIONS:Midn)
-.include "../../devel/libidn/buildlink3.mk"
-CONFIGURE_ARGS+=	--with-libidn
+.include "../../devel/libidn2/buildlink3.mk"
+.else
+CMAKE_CONFIGURE_ARGS+=	-DIDN=OFF
 .endif
 
-.if !empty(PKG_OPTIONS:Municode)
-CONFIGURE_ARGS+=	--with-unicode
+.if empty(PKG_OPTIONS:Municode)
+CMAKE_CONFIGURE_ARGS+=	-DUNICODE=OFF
 .endif
-
