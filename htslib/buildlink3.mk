b1b35f5ebe8bfddfa1b5f144b08d896c85a3047f
Jason W. Bacon <bacon4000@gmail.com>
2024-09-29 12:51:32 -0500

    htslib: Bump minimum API version

--- htslib/buildlink3.mk
+++ htslib/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	htslib
 .if !defined(HTSLIB_BUILDLINK3_MK)
 HTSLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.17
-BUILDLINK_ABI_DEPENDS.htslib+=	htslib>=1.17
+BUILDLINK_API_DEPENDS.htslib+=	htslib>=1.21
+BUILDLINK_ABI_DEPENDS.htslib+=	htslib>=1.21
 BUILDLINK_PKGSRCDIR.htslib?=	../../wip/htslib
 
 .endif	# HTSLIB_BUILDLINK3_MK
