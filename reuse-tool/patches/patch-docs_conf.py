121e6edfeec494edb935db2b63c5f60c1c634ae1
Michael Baeuerle <micha@NetBSD.org>
2024-10-09 17:15:42 +0200

    reuse-tool: Reproducible build

--- reuse-tool/patches/patch-docs_conf.py
+++ reuse-tool/patches/patch-docs_conf.py
@@ -1,17 +1,34 @@
 $NetBSD$
 
+Disable "apidoc" (not required for building man pages).
 Disable "intersphinx_mapping" (wants do fetch stuff from network).
 pkgsrc-wip package for py-furo is broken (not required for building man pages).
 
 --- docs/conf.py.orig	2024-07-08 11:58:05.000000000 +0000
 +++ docs/conf.py
-@@ -69,12 +69,12 @@ apidoc_extra_args = ["--maxdepth", "2"]
+@@ -52,29 +52,15 @@ extensions = [
+     "sphinx.ext.intersphinx",
+     "sphinx.ext.viewcode",
+     "sphinx.ext.napoleon",
+-    "sphinxcontrib.apidoc",
+ ]
  
- autodoc_member_order = "bysource"
+ templates_path = ["_templates"]
+ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
  
+-# -- Extensions configuration ------------------------------------------------
+-
+-apidoc_module_dir = str(ROOT_DIR / "src/reuse")
+-# apidoc_output_dir = "api"
+-# apidoc_excluded_paths = []
+-apidoc_separate_modules = True
+-apidoc_toc_file = False
+-apidoc_extra_args = ["--maxdepth", "2"]
+-
+-autodoc_member_order = "bysource"
+-
 -intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
-+#intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
- 
+-
  # -- Options for HTML output -------------------------------------------------
  # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
  
