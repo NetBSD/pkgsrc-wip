7b512a553d511c49986e4f7f9b34eb5033d7b892
Olaf Seibert <rhialto@falu.nl>
2024-10-17 18:57:27 +0200

    vlang: update to 0.4.8

--- vlang/patches/patch-vlib_builtin_builtin__d__gcboehm.c.v
+++ vlang/patches/patch-vlib_builtin_builtin__d__gcboehm.c.v
@@ -1,8 +1,8 @@
 $NetBSD$
 
---- vlib/builtin/builtin_d_gcboehm.c.v.orig	2023-07-01 15:24:08.744354932 +0000
+--- vlib/builtin/builtin_d_gcboehm.c.v.orig	2024-09-28 15:50:00.000000000 +0000
 +++ vlib/builtin/builtin_d_gcboehm.c.v
-@@ -25,6 +25,11 @@ $if dynamic_boehm ? {
+@@ -33,6 +33,11 @@ $if dynamic_boehm ? {
  				#flag -I/usr/local/include
  				#flag -L/usr/local/lib
  			}
@@ -11,18 +11,30 @@ $NetBSD$
 +				#flag -L@PREFIX@/lib
 +				#flag -Wl,-R,@PREFIX@/lib
 +			}
- 			#flag -lgc
- 		}
- 	}
-@@ -57,6 +62,11 @@ $if dynamic_boehm ? {
- 			#flag -lgc
+ 			$if freebsd {
+ 				#flag -lgc-threaded
+ 			} $else {
+@@ -72,6 +77,23 @@ $if dynamic_boehm ? {
+ 			#flag -lgc-threaded
  		}
  		#flag -lpthread
 +	} $else $if netbsd {
++		// Tested on NetBSD 10.0, with gcc
 +		#flag -DGC_BUILTIN_ATOMIC=1
++		#flag -DBUS_PAGE_FAULT=T_PAGEFLT
 +		#flag -I@PREFIX@/include
 +		#flag $first_existing("@PREFIX@/lib/libgc.a", "@PREFIX@/lib/libgc.a")
++		$if !tinyc {
++			#flag -DUSE_MMAP
++			//#flag -I @VEXEROOT/thirdparty/libgc/include
++			//#flag @VEXEROOT/thirdparty/libgc/gc.o
++		}
++		$if tinyc {
++			//#flag -I/usr/local/include
++			//#flag $first_existing("/usr/local/lib/libgc-threaded.a", "/usr/lib/libgc-threaded.a")
++			//#flag -lgc-threaded
++		}
 +		#flag -lpthread
  	} $else $if openbsd {
+ 		// Tested on OpenBSD 7.5, with clang, gcc and tcc
  		#flag -DGC_BUILTIN_ATOMIC=1
- 		#flag -I/usr/local/include
