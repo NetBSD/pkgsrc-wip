c056ba767e0a5ba10e97e931b5ccfe328ad18baa
vsyakovetsky <vsyakovetsky@gmail.com>
2024-11-09 18:01:40 +0200

    mtr-graph: bump to 0.85.185

--- mtr-graph/options.mk
+++ mtr-graph/options.mk
@@ -8,7 +8,6 @@ PKG_SUPPORTED_OPTIONS+=		unicode
 PKG_SUPPORTED_OPTIONS+=		ipinfo
 PKG_SUPPORTED_OPTIONS+=		inet6
 PKG_SUPPORTED_OPTIONS+=		mpls
-PKG_SUPPORTED_OPTIONS+=		graph
 
 PKG_SUGGESTED_OPTIONS+=	dns
 PKG_SUGGESTED_OPTIONS+=	idn
@@ -37,18 +36,8 @@ MESON_ARGS+=	-DMPLS=false
 MESON_ARGS+=	-DIPINFO=false
 .endif
 
-.if !empty(PKG_OPTIONS:Mgraph)
-.include "../../graphics/cairo/buildlink3.mk"
-.include "../../devel/pango/buildlink3.mk"
-.include "../../x11/libxcb/buildlink3.mk"
-.include "../../x11/xcb-util-keysyms/buildlink3.mk"
-MESON_ARGS+=	-DGRAPH=true
-.endif
-
 .if !empty(PKG_OPTIONS:Midn)
 .include "../../devel/libidn2/buildlink3.mk"
-.else
-MESON_ARGS+=	-DIDN=false
 .endif
 
 .if empty(PKG_OPTIONS:Municode)
