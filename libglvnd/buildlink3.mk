f5d96b165859e8d8da2fdb05f19a9801770381a5
Kevin Bloom <kevin.bloom@posteo.net>
2024-11-19 16:51:20 -0500

    libglvnd: added library for wip/anvil

---
+++ libglvnd/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD:$
+
+BUILDLINK_TREE+=	libglvnd
+
+.if !defined(LIBGLVND_BUILDLINK3_MK)
+LIBGLVND_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libglvnd+=	libglvnd>=1.7.0
+BUILDLINK_PKGSRCDIR.libglvnd?=	../../wip/libglvnd
+
+.endif	# LIBGLVND_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libglvnd
