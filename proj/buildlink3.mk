2b0e6da4fdbc2b5ba595115caecb5e70fa92598c
Greg Troxel <gdt@lexort.com>
2024-11-26 09:20:24 -0500

    proj: Catch up to pkgsrc
    preparatory to testing an RC.

--- proj/buildlink3.mk
+++ proj/buildlink3.mk
@@ -1,15 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.24 2024/09/01 14:56:33 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2024/11/14 22:20:02 wiz Exp $
 
 BUILDLINK_TREE+=	proj
 
 .if !defined(PROJ_BUILDLINK3_MK)
-
 PROJ_BUILDLINK3_MK:=
 
 # Proj withdrew the old API with 8.0.0.
 BUILDLINK_API_DEPENDS.proj+=	proj>=8.0.0
-BUILDLINK_ABI_DEPENDS.proj+=	proj>=9.4.1
-BUILDLINK_PKGSRCDIR.proj?=	../../wip/proj
+BUILDLINK_ABI_DEPENDS.proj+=	proj>=9.5.0nb3
+BUILDLINK_PKGSRCDIR.proj?=	../../geography/proj
 
 .include "../../databases/sqlite3/buildlink3.mk"
 .include "../../graphics/tiff/buildlink3.mk"
