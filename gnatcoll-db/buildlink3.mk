b55197d0181b65c7b00be363129f5f824f399803
Dmytro Kazankov <dmytro.kazankov@gmail.com>
2024-11-27 23:48:17 +0200

    devel/gnatcoll-db: Add 25.0.0
    * This is the DB module of the GNAT Components Collection

---
+++ gnatcoll-db/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD: buildlink3.mk,v 1.0 2024/05/14 09:30:00 dkazankov Exp $
+
+BUILDLINK_TREE+=	gnatcoll-db
+
+.if !defined(GNATCOLL_DB_BUILDLINK3_MK)
+GNATCOLL_DB_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gnatcoll-db+=	gnatcoll-db>=24.0.0
+BUILDLINK_PKGSRCDIR.gnatcoll-db=	../../wip/gnatcoll-db
+BUILDLINK_DEPMETHOD.gnatcoll-db?=	build
+
+BUILDLINK_FILES.gnatcoll-db=	include/gnatcoll_*/* lib/gnatcoll* lib/gnatcoll_*/*
+BUILDLINK_FILES.gnatcoll-db+=	share/gpr/manifests/gnatcoll_* share/gpr/gnatcoll_*.gpr
+BUILDLINK_FILES.gnatcoll-db+=	bin/gnatcoll_*2ada bin/gnatinspect
+
+BUILDLINK_CONTENTS_FILTER.gnatcoll-db=	\
+	${EGREP} '(include/.*\.ads$$|lib/.*\.ali$$|lib/.*\.a$$|lib/gnatcoll.*\.so$$|share/gpr/manifests/.*|share/gpr/.*\.gpr$$|bin/gnatcoll_.*2ada$$|bin/gnatinspect$$)'
+
+pkgbase := gnatcoll-db
+.include "../../mk/pkg-build-options.mk"
+.endif
+
+BUILDLINK_TREE+=	-gnatcoll-db
---
+++ gnatcoll-db/options.mk
@@ -0,0 +1,59 @@
+# $NetBSD: options.mk,v 1.0 2024/05/13 15:00:00 dkazankov Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.gnatcoll-db
+PKG_SUPPORTED_OPTIONS=	gnatcoll_db2ada gnatcoll_postgres2ada gnatcoll_sqlite2ada gnatcoll_all2ada
+PKG_SUPPORTED_OPTIONS+=	gnatinspect postgres sql sqlite xref
+PKG_SUGGESTED_OPTIONS=	gnatcoll_db2ada gnatcoll_postgres2ada gnatcoll_sqlite2ada gnatcoll_all2ada
+PKG_SUGGESTED_OPTIONS+=	gnatinspect postgres sql sqlite xref
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+.include "../../mk/bsd.options.mk"
+
+CONFIGURE_DIRS=
+BUILD_DIRS=
+INSTALL_DIRS=
+
+# Build order is important
+
+.if !empty(PKG_OPTIONS:Msql)
+CONFIGURE_DIRS+=	sql
+BUILD_DIRS+=		sql
+INSTALL_DIRS+=		sql
+.endif
+
+.if !empty(PKG_OPTIONS:Msqlite)
+#.  include "../../databases/sqlite3/buildlink3.mk"
+CONFIGURE_DIRS+=	sqlite
+BUILD_DIRS+=		sqlite
+INSTALL_DIRS+=		sqlite
+.endif
+
+.if !empty(PKG_OPTIONS:Mpostgres)
+.  include "../../wip/gnatcoll-bindings/buildlink3.mk"
+.  include "../../mk/pgsql.buildlink3.mk"
+CONFIGURE_DIRS+=	postgres
+BUILD_DIRS+=		postgres
+INSTALL_DIRS+=		postgres
+.endif
+
+.if !empty(PKG_OPTIONS:Mxref)
+.  include "../../wip/gnatcoll-bindings/buildlink3.mk"
+CONFIGURE_DIRS+=	xref
+BUILD_DIRS+=		xref
+INSTALL_DIRS+=		xref
+.endif
+
+.if !empty(PKG_OPTIONS:Mgnatinspect)
+.  include "../../wip/gnatcoll-bindings/buildlink3.mk"
+CONFIGURE_DIRS+=	gnatinspect
+BUILD_DIRS+=		gnatinspect
+INSTALL_DIRS+=		gnatinspect
+.endif
+
+.if !empty(PKG_OPTIONS:Mgnatcoll_db2ada) || !empty(PKG_OPTIONS:Mgnatcoll_postgres2ada) || \
+    !empty(PKG_OPTIONS:Mgnatcoll_sqlite2ada) || !empty(PKG_OPTIONS:Mgnatcoll_all2ada)
+CONFIGURE_DIRS+=	gnatcoll_db2ada
+BUILD_DIRS+=		gnatcoll_db2ada
+INSTALL_DIRS+=		gnatcoll_db2ada
+.endif
