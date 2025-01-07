6e5fb502bcda337944e4cc279ca8c4321046b7c8
Kevin Bloom <kevin.bloom@posteo.net>
2025-01-06 21:59:52 -0500

    nativefiledialog-extended: imported
    New requirement for wip/naev12.
    So far this compiles, I have not tested it yet.

---
+++ nativefiledialog-extended/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD: buildlink3.mk,v 1.1 2024/10/09 14:05:56 ktnb Exp $
+
+BUILDLINK_TREE+=	nativefiledialog-extended
+
+.if !defined(NATIVEFILEDIALOG-EXTENDED_BUILDLINK3_MK)
+NATIVEFILEDIALOG-EXTENDED_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.nativefiledialog-extended+=	nativefiledialog-extended>=1.2.1
+BUILDLINK_PKGSRCDIR.nativefiledialog-extended?=	../../wip/nativefiledialog-extended
+
+.endif	# NATIVEFILEDIALOG-EXTENDED_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-nativefiledialog-extended
