dbadfb5affefc5d7be9e1aea887e57afeac8e225
Leonardo Taccari <leot@NetBSD.org>
2025-01-11 18:00:36 +0100

    kubeconform: Update to 0.6.7
    Changes:
    - Mostly minor bug fixes and a lot of dependencies updates

--- kubeconform/go-modules.mk
+++ kubeconform/go-modules.mk
@@ -1,11 +1,12 @@
 # $NetBSD$
 
-GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.1.mod
-GO_MODULE_FILES+=	github.com/santhosh-tekuri/jsonschema/v5/@v/v5.1.1.mod
-GO_MODULE_FILES+=	github.com/santhosh-tekuri/jsonschema/v5/@v/v5.1.1.zip
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.9.mod
+GO_MODULE_FILES+=	github.com/hashicorp/go-cleanhttp/@v/v0.5.2.mod
+GO_MODULE_FILES+=	github.com/hashicorp/go-cleanhttp/@v/v0.5.2.zip
+GO_MODULE_FILES+=	github.com/hashicorp/go-retryablehttp/@v/v0.7.7.mod
+GO_MODULE_FILES+=	github.com/hashicorp/go-retryablehttp/@v/v0.7.7.zip
+GO_MODULE_FILES+=	github.com/santhosh-tekuri/jsonschema/v5/@v/v5.3.1.mod
+GO_MODULE_FILES+=	github.com/santhosh-tekuri/jsonschema/v5/@v/v5.3.1.zip
 GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod
-GO_MODULE_FILES+=	gopkg.in/yaml.v2/@v/v2.2.8.mod
-GO_MODULE_FILES+=	gopkg.in/yaml.v2/@v/v2.4.0.mod
-GO_MODULE_FILES+=	gopkg.in/yaml.v2/@v/v2.4.0.zip
-GO_MODULE_FILES+=	sigs.k8s.io/yaml/@v/v1.2.0.mod
-GO_MODULE_FILES+=	sigs.k8s.io/yaml/@v/v1.2.0.zip
+GO_MODULE_FILES+=	sigs.k8s.io/yaml/@v/v1.4.0.mod
+GO_MODULE_FILES+=	sigs.k8s.io/yaml/@v/v1.4.0.zip
