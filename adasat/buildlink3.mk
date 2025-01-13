95c9c62c71a7041495777064bafb9a09d6cca8cb
Dmytro Kazankov <dmytro.kazankov@gmail.com>
2025-01-13 22:10:06 +0200

    adasat: buildlink file list fixed

--- adasat/buildlink3.mk
+++ adasat/buildlink3.mk
@@ -9,11 +9,11 @@ BUILDLINK_API_DEPENDS.adasat+=	adasat>=24.0
 BUILDLINK_PKGSRCDIR.adasat=	../../wip/adasat
 BUILDLINK_DEPMETHOD.adasat?=	build
 
-BUILDLINK_FILES.adasat=		include/adasat*/* lib/adasat*/*
+BUILDLINK_FILES.adasat=		include/adasat*/* lib/libadasat* lib/adasat*/*
 BUILDLINK_FILES.adasat+=	share/gpr/manifests/adasat share/gpr/adasat.gpr
 
 BUILDLINK_CONTENTS_FILTER.adasat=	\
-	${EGREP} '(include/.*\.ads$$|lib/.*\.ali$$|lib/.*\.a$$|share/gpr/manifests/.*|share/gpr/.*\.gpr$$)'
+	${EGREP} '(include/.*\.ads$$|lib/.*\.ali$$|lib/.*\.a$$|lib/.*\.so.*$$|share/gpr/manifests/.*|share/gpr/.*\.gpr$$)'
 
 .endif
 
