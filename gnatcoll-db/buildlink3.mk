2efbbc2f300c112dd20e54e3db84b201dc8762f3
Dmytro Kazankov <dmytro.kazankov@gmail.com>
2025-01-13 22:07:58 +0200

    gnatcoll-db: buildlink file list fixed

--- gnatcoll-db/buildlink3.mk
+++ gnatcoll-db/buildlink3.mk
@@ -9,12 +9,12 @@ BUILDLINK_API_DEPENDS.gnatcoll-db+=	gnatcoll-db>=24.0.0
 BUILDLINK_PKGSRCDIR.gnatcoll-db=	../../wip/gnatcoll-db
 BUILDLINK_DEPMETHOD.gnatcoll-db?=	build
 
-BUILDLINK_FILES.gnatcoll-db=	include/gnatcoll_*/* lib/gnatcoll* lib/gnatcoll_*/*
+BUILDLINK_FILES.gnatcoll-db=	include/gnatcoll_*/* lib/libgnatcoll* lib/gnatcoll_*/*
 BUILDLINK_FILES.gnatcoll-db+=	share/gpr/manifests/gnatcoll_* share/gpr/gnatcoll_*.gpr
 BUILDLINK_FILES.gnatcoll-db+=	bin/gnatcoll_*2ada bin/gnatinspect
 
 BUILDLINK_CONTENTS_FILTER.gnatcoll-db=	\
-	${EGREP} '(include/.*\.ads$$|lib/.*\.ali$$|lib/.*\.a$$|lib/gnatcoll.*\.so$$|share/gpr/manifests/.*|share/gpr/.*\.gpr$$|bin/gnatcoll_.*2ada$$|bin/gnatinspect$$)'
+	${EGREP} '(include/.*\.ads$$|lib/.*\.ali$$|lib/.*\.a$$|lib/libgnatcoll.*\.so.*$$|share/gpr/manifests/.*|share/gpr/.*\.gpr$$|bin/gnatcoll_.*2ada$$|bin/gnatinspect$$)'
 
 pkgbase := gnatcoll-db
 .include "../../mk/pkg-build-options.mk"
