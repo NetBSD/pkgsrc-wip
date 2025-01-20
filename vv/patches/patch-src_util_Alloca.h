bcfb725b60e670f05da5c2768c456f6c95a98c0f
Thomas Klausner <wiz@NetBSD.org>
2025-01-20 14:36:32 +0100

    vv: add package
    works, but needs to be cleaned up for non-NetBSD

---
+++ vv/patches/patch-src_util_Alloca.h
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/util/Alloca.h.orig	2024-12-23 10:46:59.000000000 +0000
++++ src/util/Alloca.h
+@@ -1,6 +1,6 @@
+ #pragma once
+ 
+-#ifdef __FreeBSD__
++#if defined(__FreeBSD__) || defined(__NetBSD__)
+ #  include <stdlib.h>
+ #else
+ #  include <alloca.h>
