9b5102824cde9ee14702a54d03a71c201f02a5be
Mayuresh <mayuresh@acm.org>
2025-01-29 22:59:01 +0530

    yosys-dev: upgraded to 0.49, builds without Makefile patch, removed patch

--- yosys-dev/patches/patch-kernel_driver.cc
+++ yosys-dev/patches/patch-kernel_driver.cc
@@ -2,11 +2,11 @@ $NetBSD$
 
 pkgsrc cxxopts include path adjusted
 
---- kernel/driver.cc.orig	2024-11-19 15:16:25.907765578 +0000
+--- kernel/driver.cc.orig	2025-01-29 17:09:04.808014166 +0000
 +++ kernel/driver.cc
-@@ -19,7 +19,7 @@
- 
+@@ -20,7 +20,7 @@
  #include "kernel/yosys.h"
+ #include "kernel/hashlib.h"
  #include "libs/sha1/sha1.h"
 -#include "libs/cxxopts/include/cxxopts.hpp"
 +#include "cxxopts.hpp"
