0ab417cf50d25786ca1b4fbdd1e92a191d90f90a
Michael Baeuerle <micha@NetBSD.org>
2025-01-31 17:05:58 +0100

    eureka1: Switch to FLTK 1.4
    - Add COMMIT_MSG

--- eureka1/options.mk
+++ eureka1/options.mk
@@ -6,7 +6,7 @@ PKG_SUGGESTED_OPTIONS=	opengl xdg-utils
 
 .include "../../mk/bsd.options.mk"
 
-.include "../../x11/fltk13/buildlink3.mk"
+.include "../../x11/fltk/buildlink3.mk"
 
 # opengl: Use OpenGL for rendering
 # If FLTK is compiled without opengl option, it is always disabled here too.
