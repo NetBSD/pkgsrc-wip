1ebc002f92aec8cd0c238d35c8be243d6b97f3f4
Michael Baeuerle <micha@NetBSD.org>
2025-01-31 13:39:35 +0100

    FLTK 1.4 transition: Switch development and test packages to x11/fltk
    Package x11/fltk now provides FLTK 1.4.

--- eureka-devel/options.mk
+++ eureka-devel/options.mk
@@ -6,10 +6,10 @@ PKG_SUGGESTED_OPTIONS=	opengl xdg-utils
 
 .include "../../mk/bsd.options.mk"
 
-.include "../../wip/fltk14-devel/buildlink3.mk"
+.include "../../x11/fltk/buildlink3.mk"
 
 # Add missing include path for FLTK (eureka does not use fltk-config)
-.if empty(PKG_OPTIONS.fltk14-devel:Mcairo)
+.if empty(PKG_OPTIONS.fltk:Mcairo)
 CXXFLAGS=	-I"${PREFIX}/include/cairo"
 .endif
 
@@ -17,7 +17,7 @@ CXXFLAGS=	-I"${PREFIX}/include/cairo"
 # If FLTK is compiled without opengl option, it is always disabled here too.
 # If FLTK is compiled with opengl option, disabling it here allows to force
 # usage of the software renderer (recommended without hardware acceleration).
-.if empty(PKG_OPTIONS.fltk14-devel:Mopengl) || empty(PKG_OPTIONS:Mopengl)
+.if empty(PKG_OPTIONS.fltk:Mopengl) || empty(PKG_OPTIONS:Mopengl)
 CMAKE_CONFIGURE_ARGS+=	-DENABLE_OPENGL=off
 .endif
 # xdg-utils: Create dependency for xdg-utils (Portland project)
