d4e9936c4c36bef4bbe8cd978c43e8fdbcd260c2
Jason Bacon <bacon@NetBSD.org>
2025-02-03 08:34:47 -0600

    haplohseq: ioService -> ioContext

--- haplohseq/patches/patch-src_HaplohSeq.cpp
+++ haplohseq/patches/patch-src_HaplohSeq.cpp
@@ -1,6 +1,6 @@
 $NetBSD$
 
---- src/HaplohSeq.cpp.orig	2025-01-26 13:47:41.918585702 +0000
+--- src/HaplohSeq.cpp.orig	2021-01-23 22:44:43.000000000 +0000
 +++ src/HaplohSeq.cpp
 @@ -195,14 +195,14 @@ void HaplohSeq::runBafHaplohseq(	std::st
  	for (unsigned int i = 0; i < numParamEventStarts; i++) {
@@ -9,15 +9,15 @@ $NetBSD$
 -		boost::asio::io_service ioService;
 -	//	boost::asio::io_service::work work(ioService);
 -		boost::shared_ptr<boost::asio::io_service::work> work(new boost::asio::io_service::work(ioService));
-+		boost::asio::io_context ioService;
-+	//	boost::asio::io_context::work work(ioService);
-+		boost::shared_ptr<boost::asio::io_context::work> work(new boost::asio::io_context::work(ioService));
++		boost::asio::io_context ioContext;
++	//	boost::asio::io_context::work work(ioContext);
++		boost::shared_ptr<boost::asio::io_context::work> work(new boost::asio::io_context::work(ioContext));
  		boost::thread_group threadPool;
  
  		// Spawn worker threads
  		for (std::size_t t = 0; t < numThreads; t++) {
 -			threadPool.create_thread(boost::bind((boost::asio::io_context::count_type (boost::asio::io_service::*)())&boost::asio::io_service::run, &ioService));
-+			threadPool.create_thread(boost::bind((boost::asio::io_context::count_type (boost::asio::io_context::*)())&boost::asio::io_context::run, &ioService));
++			threadPool.create_thread(boost::bind((boost::asio::io_context::count_type (boost::asio::io_context::*)())&boost::asio::io_context::run, &ioContext));
  		}
  
  		std::map<std::string, boost::shared_ptr<Hmm> > chrHmms;
@@ -26,7 +26,7 @@ $NetBSD$
  			std::cout << "Posting job to thread pool for chromosome " << chr << "\n";
  			// IMPORTANT: do not pass shared_ptr by reference because the shared_ptr expects a new pointer to the object
 -			ioService.post(boost::bind(&HaplohSeq::runBafHaplohseqChr, this,
-+			ioService.get_executor().post(boost::bind(&HaplohSeq::runBafHaplohseqChr, this,
++			ioContext.post(boost::bind(&HaplohSeq::runBafHaplohseqChr, this,
  													chr,
  													boost::ref(chrHmms),
  													boost::ref(fp),
@@ -37,24 +37,25 @@ $NetBSD$
 -		boost::asio::io_service ioService;
 -	//	boost::asio::io_service::work work(ioService);
 -		boost::shared_ptr<boost::asio::io_service::work> work(new boost::asio::io_service::work(ioService));
-+		boost::asio::io_context ioService;
-+	//	boost::asio::io_context::work work(ioService);
-+		boost::shared_ptr<boost::asio::io_context::work> work(new boost::asio::io_context::work(ioService));
++		boost::asio::io_context ioContext;
++	//	boost::asio::io_context::work work(ioContext);
++		boost::shared_ptr<boost::asio::io_context::work> work(new boost::asio::io_context::work(ioContext));
  		boost::thread_group threadPool;
  
  		// Spawn worker threads
  		for (std::size_t t = 0; t < numThreads; t++) {
 -			threadPool.create_thread(boost::bind((boost::asio::io_context::count_type (boost::asio::io_service::*)())&boost::asio::io_service::run, &ioService));
-+			threadPool.create_thread(boost::bind((boost::asio::io_context::count_type (boost::asio::io_context::*)())&boost::asio::io_context::run, &ioService));
++			threadPool.create_thread(boost::bind((boost::asio::io_context::count_type (boost::asio::io_context::*)())&boost::asio::io_context::run, &ioContext));
  		}
  
  		std::map<std::string, boost::shared_ptr<Hmm> > chrHmms;
-@@ -438,7 +438,7 @@ void HaplohSeq::runVcfHaplohseq(	std::st
+@@ -438,7 +438,8 @@ void HaplohSeq::runVcfHaplohseq(	std::st
  
  //			std::cout << "Posting job to thread pool for chromosome " << chr << "\n";
  			// IMPORTANT: do not pass shared_ptr by reference because the shared_ptr expects a new pointer to the object
 -			ioService.post(boost::bind(&HaplohSeq::runVcfHaplohseqChr, this,
-+			ioService.get_executor().post(boost::bind(&HaplohSeq::runVcfHaplohseqChr, this,
++			// https://github.com/boostorg/asio/issues/110
++			ioContext.get_executor().post(boost::bind(&HaplohSeq::runVcfHaplohseqChr, this,
  													chr,
  													boost::ref(chrHmms),
  													boost::ref(fp),
