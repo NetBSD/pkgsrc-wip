57dc3128d3b3a1bac68d83eca4cb3ed211b534d4
Jonathan Perkin <jperkin@smartos.org>
2025-02-04 17:14:10 +0000

    rust184: Restore $ORIGIN rpath fix.

--- rust184/patches/patch-src_bootstrap_src_core_builder_cargo.rs
+++ rust184/patches/patch-src_bootstrap_src_core_builder_cargo.rs
@@ -7,8 +7,17 @@ Adapt fix to
 https://github.com/rust-lang/rust/issues/133629
 (files restructured upstream)
 
---- src/bootstrap/src/core/builder/cargo.rs.orig	2025-02-02 15:19:31.785846508 +0000
+--- src/bootstrap/src/core/builder/cargo.rs.orig	2025-01-27 23:20:59.000000000 +0000
 +++ src/bootstrap/src/core/builder/cargo.rs
+@@ -238,7 +238,7 @@ impl Cargo {
+                 Some(format!("-Wl,-rpath,@loader_path/../{libdir}"))
+             } else if !target.is_windows() && !target.contains("aix") && !target.contains("xous") {
+                 self.rustflags.arg("-Clink-args=-Wl,-z,origin");
+-                Some(format!("-Wl,-rpath,$ORIGIN/../{libdir}"))
++                Some(format!("-Wl,-rpath,@PREFIX@/lib"))
+             } else {
+                 None
+             };
 @@ -646,7 +646,10 @@ impl Builder<'_> {
                  // Build proc macros both for the host and the target unless proc-macros are not
                  // supported by the target.
