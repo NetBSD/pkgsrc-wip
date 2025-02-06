c76c9fc85eaafd68a98881b0975693b4c3b2d58a
Jason W. Bacon <jtocino@gmx.com>
2025-02-06 07:39:20 -0600

    libxtend: Update to 0.2.0.37
    xt_ff_read_line_malloc() -> xt_ff_gets_malloc()

--- libxtend/buildlink3.mk
+++ libxtend/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=		libxtend
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.2.0.36
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.2.0.37
 BUILDLINK_PKGSRCDIR.libxtend?=		../../wip/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
