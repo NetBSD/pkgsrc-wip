75b3ecf8d49eadd76aeeae919f5d81c88a972060
Thomas Klausner <wiz@NetBSD.org>
2025-02-11 16:16:48 +0100

    libvips: fix buildlink3.mk file

--- libvips/buildlink3.mk
+++ libvips/buildlink3.mk
@@ -8,19 +8,11 @@ LIBVIPS_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libvips+=	libvips>=8.16.0
 BUILDLINK_PKGSRCDIR.libvips?=	../../wip/libvips
 
-#.include "../../devel/zlib/buildlink3.mk"
-#.include "../../graphics/libexif/buildlink3.mk"
-#.include "../../graphics/libheif/buildlink3.mk"
-#.include "../../graphics/libjxl/buildlink3.mk"
-#.include "../../graphics/librsvg/buildlink3.mk"
-#.include "../../graphics/libwebp/buildlink3.mk"
-#.include "../../graphics/openexr/buildlink3.mk"
-#.include "../../graphics/openjpeg/buildlink3.mk"
-#.include "../../graphics/png/buildlink3.mk"
-#.include "../../print/poppler/buildlink3.mk"
-#.include "../../textproc/expat/buildlink3.mk"
-#.include "../../devel/glib2/buildlink3.mk"
-#.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../graphics/libexif/buildlink3.mk"
+.include "../../graphics/libjxl/buildlink3.mk"
+.include "../../graphics/librsvg/buildlink3.mk"
+.include "../../textproc/expat/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
 .endif	# LIBVIPS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libvips
