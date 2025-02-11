9e11422ba06d6fcf7dd1ed4fe8f117de30b0afc2
Thomas Klausner <wiz@NetBSD.org>
2025-02-11 16:10:02 +0100

    libvips: finish package
    Add lots more dependencies, so this has proper graphics support.
    Fix python interpreter path.
    Fix buildlink3.mk file.

--- libvips/buildlink3.mk
+++ libvips/buildlink3.mk
@@ -1,13 +1,26 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	vips
+BUILDLINK_TREE+=	libvips
 
-.if !defined(VIPS_BUILDLINK3_MK)
-VIPS_BUILDLINK3_MK:=
+.if !defined(LIBVIPS_BUILDLINK3_MK)
+LIBVIPS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.vips+=	vips>=8.2.2
-BUILDLINK_PKGSRCDIR.vips?=	../../wip/libvips
+BUILDLINK_API_DEPENDS.libvips+=	libvips>=8.16.0
+BUILDLINK_PKGSRCDIR.libvips?=	../../wip/libvips
 
-.endif	# VIPS_BUILDLINK3_MK
+#.include "../../devel/zlib/buildlink3.mk"
+#.include "../../graphics/libexif/buildlink3.mk"
+#.include "../../graphics/libheif/buildlink3.mk"
+#.include "../../graphics/libjxl/buildlink3.mk"
+#.include "../../graphics/librsvg/buildlink3.mk"
+#.include "../../graphics/libwebp/buildlink3.mk"
+#.include "../../graphics/openexr/buildlink3.mk"
+#.include "../../graphics/openjpeg/buildlink3.mk"
+#.include "../../graphics/png/buildlink3.mk"
+#.include "../../print/poppler/buildlink3.mk"
+#.include "../../textproc/expat/buildlink3.mk"
+#.include "../../devel/glib2/buildlink3.mk"
+#.include "../../textproc/libxml2/buildlink3.mk"
+.endif	# LIBVIPS_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-vips
+BUILDLINK_TREE+=	-libvips
--- libvips/options.mk
+++ libvips/options.mk
@@ -1,8 +1,8 @@
 # $NetBSD$
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.vips
-PKG_SUPPORTED_OPTIONS=	jpeg png exif tiff gobject-introspection
-PKG_SUGGESTED_OPTIONS=	jpeg png
+PKG_OPTIONS_VAR=	PKG_OPTIONS.libvips
+PKG_SUPPORTED_OPTIONS=	tiff introspection
+PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,34 +10,14 @@ PKG_SUGGESTED_OPTIONS=	jpeg png
 # so it makes sense to explicitly disabled them
 # if they are not going to be used
 
-# libvips advises jpeg-turbo / mozjpeg
-.if !empty(PKG_OPTIONS:Mjpeg)
-.include "../../graphics/libjpeg-turbo/buildlink3.mk"
-.else
-MESON_ARGS+= -Djpeg=disabled
-.endif
-
-.if !empty(PKG_OPTIONS:Mpng)
-.include "../../graphics/png/buildlink3.mk"
-.else
-MESON_ARGS+= -Dpng=disabled
-.endif
-
-.if !empty(PKG_OPTIONS:Mexif)
-.include "../../graphics/libexif/buildlink3.mk"
-.else
-MESON_ARGS+= -Dexif=disabled
-.endif
-
 .if !empty(PKG_OPTIONS:Mtiff)
 .include "../../graphics/tiff/buildlink3.mk"
 .else
 MESON_ARGS+= -Dtiff=disabled
 .endif
 
-.if !empty(PKG_OPTIONS:Mgobject-introspection)
+.if !empty(PKG_OPTIONS:Mintrospection)
 .include "../../devel/gobject-introspection/buildlink3.mk"
 .else
 MESON_ARGS+= -Dintrospection=disabled
 .endif
-
