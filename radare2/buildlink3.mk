aa33464943bfe9754f068451afe12c87a6a88d97
Makoto mef20 Fujiwara <makoto@if.t.u-tokyo.ac.jp>
2025-03-02 14:21:33 +0900

    wip/radare2: import radare2-5.9.8
    radare2 is an advanced commandline hexadecimal editor
    with a set of tools for working with binary files.
    
    Most of tools that come with radare2 have been designed
    for reverse engineering file formats, firmwares, programs,
    libraries, etc.
    
    Radare2 project started as a forensics tool, a scriptable
    commandline hexadecimal editor able to open disk files,
    but later support for analyzing binaries, disassembling
    code, debugging programs, attaching to remote gdb servers..

---
+++ radare2/buildlink3.mk
@@ -0,0 +1,17 @@
+# $NetBSD: buildlink3.mk,v 1.23 2024/11/14 22:19:36 wiz Exp $
+
+BUILDLINK_TREE+=	radare2
+
+.if !defined(RADARE2_BUILDLINK3_MK)
+RADARE2_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.radare2+=	radare2>=5.7.8
+BUILDLINK_ABI_DEPENDS.radare2?=	radare2>=5.9.6nb1
+BUILDLINK_PKGSRCDIR.radare2?=	../../devel/radare2
+
+.include "../../devel/capstone/buildlink3.mk"
+.include "../../sysutils/file/buildlink3.mk"
+.include "../../archivers/libzip/buildlink3.mk"
+.endif	# RADARE2_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-radare2
