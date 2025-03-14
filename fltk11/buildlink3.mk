05f48e803de2e42161cfcdc5df6f5844c937f533
Michael Baeuerle <micha@NetBSD.org>
2025-03-14 18:05:41 +0100

    wip/fltk11: Do not use namespaces of x11/fltk
    - Declare "fltk13" package (this one) as successor for FLTK 1.3.x
    - Declare conflict with FLTK >1.4.0 (from x11/fltk)
    - Bump PKGREVISION

--- fltk11/buildlink3.mk
+++ fltk11/buildlink3.mk
@@ -1,14 +1,14 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	fltk
+BUILDLINK_TREE+=	fltk11
 
-.if !defined(FLTK_BUILDLINK3_MK)
-FLTK_BUILDLINK3_MK:=
+.if !defined(FLTK11_BUILDLINK3_MK)
+FLTK11_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fltk+=	fltk>=1.1.5rc1<1.3
-BUILDLINK_ABI_DEPENDS.fltk?=	fltk>=1.1.10nb7<1.3
-BUILDLINK_PKGSRCDIR.fltk?=	../../wip/fltk11
-BUILDLINK_FILES.fltk+=		include/Fl/*
+BUILDLINK_API_DEPENDS.fltk11+=	fltk11>=1.1.5rc1<1.3
+BUILDLINK_ABI_DEPENDS.fltk11?=	fltk11>=1.1.10nb7<1.3
+BUILDLINK_PKGSRCDIR.fltk11?=	../../wip/fltk11
+BUILDLINK_FILES.fltk11+=	include/Fl/*
 
 .include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../graphics/glu/buildlink3.mk"
@@ -16,6 +16,6 @@ BUILDLINK_FILES.fltk+=		include/Fl/*
 .include "../../graphics/png/buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
 .include "../../x11/libXext/buildlink3.mk"
-.endif # FLTK_BUILDLINK3_MK
+.endif # FLTK11_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-fltk
+BUILDLINK_TREE+=	-fltk11
