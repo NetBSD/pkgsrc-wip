f877e27a7bb2343c5b22da2987123b7cc9980f37
Paul Ripke <stix@stix.id.au>
2025-04-02 21:05:45 +1100

    soqt: Switch to build against qt6, and the wip version of Coin.

--- soqt/buildlink3.mk
+++ soqt/buildlink3.mk
@@ -8,9 +8,9 @@ SOQT_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.soqt+=	soqt>=1.4.1
 BUILDLINK_PKGSRCDIR.soqt?=	../../wip/soqt
 
-.include "../../graphics/Coin/buildlink3.mk"
+.include "../../wip/Coin/buildlink3.mk"
 .include "../../graphics/MesaLib/buildlink3.mk"
-.include "../../x11/qt5-qtbase/buildlink3.mk"
+.include "../../x11/qt6-qtbase/buildlink3.mk"
 .endif	# SOQT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-soqt
