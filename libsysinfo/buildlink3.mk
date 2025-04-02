192260162248fd7a4b061f1969dd87fbae493d29
Jason W. Bacon <bacon4000@gmail.com>
2025-04-02 10:19:19 -0500

    libsysinfo: Add bl3

---
+++ libsysinfo/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD$
+# XXX
+# XXX This file was created automatically using createbuildlink-3.19.
+# XXX After this file has been verified as correct, the comment lines
+# XXX beginning with "XXX" should be removed.  Please do not commit
+# XXX unverified buildlink3.mk files.
+
+BUILDLINK_TREE+=	libsysinfo
+
+.if !defined(LIBSYSINFO_BUILDLINK3_MK)
+LIBSYSINFO_BUILDLINK3_MK:=
+
+BUILDLINK_DEPMETHOD.libsysinfo?=	build
+
+BUILDLINK_API_DEPENDS.libsysinfo+=	libsysinfo>=0.0.3
+BUILDLINK_PKGSRCDIR.libsysinfo?=	../../wip/libsysinfo
+.endif	# LIBSYSINFO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libsysinfo
