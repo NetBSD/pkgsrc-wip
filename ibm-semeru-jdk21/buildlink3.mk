e2936dcd5827fe505b6707dd74852b6c317fb0a7
Hauke Fath <hauke@NetBSD.org>
2025-04-04 13:48:56 +0200

    Pkglint fallout.

--- ibm-semeru-jdk21/buildlink3.mk
+++ ibm-semeru-jdk21/buildlink3.mk
@@ -2,8 +2,8 @@
 
 BUILDLINK_TREE+=	ibm-semeru-jdk21
 
-.if !defined(IBM-SEMERU_JDK21_BUILDLINK3_MK)
-IBM-SEMERU_JDK21_BUILDLINK3_MK:=
+.if !defined(IBM_SEMERU_JDK21_BUILDLINK3_MK)
+IBM_SEMERU_JDK21_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ibm-semeru-jdk21+=	ibm-semeru-jdk21-[0-9]*
 BUILDLINK_ABI_DEPENDS.ibm-semeru-jdk21+=	ibm-semeru-jdk21>=21.0.5
@@ -11,6 +11,6 @@ BUILDLINK_PKGSRCDIR.ibm-semeru-jdk21?=		../../wip/ibm-semeru-jdk21
 BUILDLINK_JAVA_PREFIX.ibm-semeru-jdk21=		${PREFIX}/java/ibm-semeru-jdk21
 
 UNLIMIT_RESOURCES+=	datasize
-.endif # IBM-SEMERU_JDK21_BUILDLINK3_MK
+.endif # IBM_SEMERU_JDK21_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ibm-semeru-jdk21
