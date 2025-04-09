bf8df4033bbc8058499d28be96b77a0fca29c625
Jason W. Bacon <jtocino@gmx.com>
2025-04-09 15:15:10 -0500

    biolibc: Update to 0.2.6.11
    bl_fastq_find_5p_low_qual(): Don't quit if 1st base is good

--- biolibc/buildlink3.mk
+++ biolibc/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	biolibc
 .if !defined(BIOLIBC_BUILDLINK3_MK)
 BIOLIBC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.6.10
+BUILDLINK_API_DEPENDS.biolibc+=	biolibc>=0.2.6.11
 BUILDLINK_PKGSRCDIR.biolibc?=	../../wip/biolibc
 
 .include "../../wip/libxtend/buildlink3.mk"
