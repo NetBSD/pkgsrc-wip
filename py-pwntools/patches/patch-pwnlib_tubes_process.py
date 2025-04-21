ad84ce90ec5dd3e9df11014c16f887088ffcb131
Leonardo Taccari <iamleot@gmail.com>
2025-04-21 16:25:49 +0200

    py-pwntools: Add a kludge to fix pwn.process()
    Avoid to invoke to set internal_preexec_fn in NetBSD because otherwise
    it fails and pwn.process() is not usable.
    
    Add a big XXX: comment in the patch and a reproducer.
    
    (The logic was not present in (much) older pwntools.)
    
    This should be investigated better and fixed properly.

---
+++ py-pwntools/patches/patch-pwnlib_tubes_process.py
@@ -0,0 +1,48 @@
+$NetBSD$
+
+Avoid to invoke preexec_fn on NetBSD.
+
+It fails due some exception.
+
+Easily reproducible via:
+
+ python3 -c 'import pwn; pwn.process("true")'
+
+--- pwnlib/tubes/process.py.orig	2025-04-21 14:06:00.563442247 +0000
++++ pwnlib/tubes/process.py
+@@ -13,6 +13,7 @@ import subprocess
+ import sys
+ import time
+ 
++IS_NETBSD = sys.platform.startswith('netbsd')
+ IS_WINDOWS = sys.platform.startswith('win')
+ 
+ if IS_WINDOWS:
+@@ -306,7 +307,26 @@ class process(tube):
+             # Create the PTY if necessary
+             stdin, stdout, stderr, master, slave = self._handles(*handles)
+ 
+-            internal_preexec_fn = self.__preexec_fn
++            if IS_NETBSD:
++                # XXX: On NetBSD running
++                # XXX:  `python3 -c 'import pwn; pwn.process("true")'`
++                # XXX: Fails with:
++                # XXX: [-] Starting local process '/usr/bin/true': Failed
++                # XXX: Traceback (most recent call last):
++                # XXX:   File "<string>", line 1, in <module>
++                # XXX:   File "/usr/pkg/lib/python3.12/site-packages/pwnlib/tubes/process.py", line 359, in __init__
++                # XXX:     self.proc = subprocess.Popen(args = args,
++                # XXX:                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
++                # XXX:   File "/usr/pkg/lib/python3.12/subprocess.py", line 1026, in __init__
++                # XXX:     self._execute_child(args, executable, preexec_fn, close_fds,
++                # XXX:   File "/usr/pkg/lib/python3.12/subprocess.py", line 1958, in _execute_child
++                # XXX:     raise child_exception_type(err_msg)
++                # XXX: subprocess.SubprocessError: Exception occurred in preexec_fn.
++                # XXX: Avoid to use the self.__preexec_fn completely.
++                # XXX: This should be investigated and addressed properly!
++                internal_preexec_fn = None
++            else:
++                internal_preexec_fn = self.__preexec_fn
+ 
+         #: Arguments passed on argv
+         self.argv = argv_val
