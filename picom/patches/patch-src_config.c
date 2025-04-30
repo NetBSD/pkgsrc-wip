74d99192f532ce1e9ee615731b49c17baaeae538
Thomas Klausner <wiz@NetBSD.org>
2025-05-01 00:29:24 +0200

    picom: fix a compilation error

---
+++ picom/patches/patch-src_config.c
@@ -0,0 +1,18 @@
+$NetBSD$
+
+../src/config.c: In function 'parse_window_shader_prefix_with_cwd':
+../src/config.c:625:78: error: parameter name omitted
+  625 | void *parse_window_shader_prefix_with_cwd(const char *src, const char **end, void *) {
+      |                                                                              ^~~~~~
+
+--- src/config.c.orig	2025-04-30 22:28:32.936786751 +0000
++++ src/config.c
+@@ -622,7 +622,7 @@ void *parse_window_shader_prefix(const c
+ 	*end = endptr + 1;
+ 	return shader_source;
+ }
+-void *parse_window_shader_prefix_with_cwd(const char *src, const char **end, void *) {
++void *parse_window_shader_prefix_with_cwd(const char *src, const char **end, void *foo) {
+ 	scoped_charp cwd = getcwd(NULL, 0);
+ 	return parse_window_shader_prefix(src, end, cwd);
+ }
