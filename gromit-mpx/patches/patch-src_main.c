27c3d8e1f6744d4a566e1ab5fb915f7be9709232
Leonardo Taccari <leot@NetBSD.org>
2025-05-04 21:36:39 +0200

    gromit-mpx: Conditionalize appindicator support
    In this way we can possibly upstream such patches so all package
    managers could benefit of them to possibly avoid a dbus dependency.

--- gromit-mpx/patches/patch-src_main.c
+++ gromit-mpx/patches/patch-src_main.c
@@ -1,41 +1,44 @@
 $NetBSD$
 
-Disable appindicator3 support to avoid dbus dependency.
-
-From OpenBSD ports.
+Permit to disable appindicator3 support to avoid dbus dependency.
+Inspired by OpenBSD ports.
 
 --- src/main.c.orig	2024-09-09 18:39:05.000000000 +0000
 +++ src/main.c
-@@ -864,15 +864,6 @@ void setup_main_app (GromitData *data, i
+@@ -864,6 +864,7 @@ void setup_main_app (GromitData *data, i
    if (activate)
      acquire_grab (data, NULL); /* grab all */
  
--  /*
--     TRAY ICON
--  */
--  data->trayicon = app_indicator_new (PACKAGE_NAME,
--				      "net.christianbeier.Gromit-MPX",
--				      APP_INDICATOR_CATEGORY_APPLICATION_STATUS);
--
--  app_indicator_set_status (data->trayicon, APP_INDICATOR_STATUS_ACTIVE);
--
++#ifdef ENABLE_APPINDICATOR
+   /*
+      TRAY ICON
+   */
+@@ -872,6 +873,7 @@ void setup_main_app (GromitData *data, i
+ 				      APP_INDICATOR_CATEGORY_APPLICATION_STATUS);
+ 
+   app_indicator_set_status (data->trayicon, APP_INDICATOR_STATUS_ACTIVE);
++#endif
  
  
-   /* create the menu */
-@@ -1019,7 +1010,6 @@ void setup_main_app (GromitData *data, i
+ 
+@@ -1019,7 +1021,9 @@ void setup_main_app (GromitData *data, i
    gtk_widget_show (quit_item);
  
  
--  app_indicator_set_menu (data->trayicon, GTK_MENU(menu));
++#ifdef ENABLE_APPINDICATOR
+   app_indicator_set_menu (data->trayicon, GTK_MENU(menu));
++#endif
  
    /*
      Build the support menu
-@@ -1267,8 +1257,4 @@ int main (int argc, char **argv)
+@@ -1267,8 +1271,10 @@ int main (int argc, char **argv)
  
  void indicate_active(GromitData *data, gboolean YESNO)
  {
--    if(YESNO)
--	app_indicator_set_icon(data->trayicon, "net.christianbeier.Gromit-MPX.active");
--    else
--	app_indicator_set_icon(data->trayicon, "net.christianbeier.Gromit-MPX");
++#ifdef ENABLE_APPINDICATOR
+     if(YESNO)
+ 	app_indicator_set_icon(data->trayicon, "net.christianbeier.Gromit-MPX.active");
+     else
+ 	app_indicator_set_icon(data->trayicon, "net.christianbeier.Gromit-MPX");
++#endif
  }
--- gromit-mpx/patches/patch-src_main.h
+++ gromit-mpx/patches/patch-src_main.h
@@ -1,28 +1,31 @@
 $NetBSD$
 
-Disable appindicator3 support to avoid dbus dependency.
-
-From OpenBSD ports.
+Permit to disable appindicator3 support to avoid dbus dependency.
+Inspired by OpenBSD ports.
 
 --- src/main.h.orig	2024-09-09 18:39:05.000000000 +0000
 +++ src/main.h
-@@ -29,11 +29,6 @@
+@@ -29,11 +29,13 @@
  #include <glib/gi18n.h>
  #include <gdk/gdk.h>
  #include <gtk/gtk.h>
--#ifdef APPINDICATOR_IS_LEGACY
--#include <libappindicator/app-indicator.h>
--#else
--#include <libayatana-appindicator/app-indicator.h>
--#endif
++#ifdef ENABLE_APPINDICATOR
+ #ifdef APPINDICATOR_IS_LEGACY
+ #include <libappindicator/app-indicator.h>
+ #else
+ #include <libayatana-appindicator/app-indicator.h>
+ #endif
++#endif
  
  #define GROMIT_MOUSE_EVENTS ( GDK_BUTTON_MOTION_MASK | \
                                GDK_BUTTON_PRESS_MASK | \
-@@ -116,7 +111,6 @@ typedef struct
+@@ -116,7 +118,9 @@ typedef struct
  typedef struct
  {
    GtkWidget   *win;
--  AppIndicator *trayicon;
++#ifdef ENABLE_APPINDICATOR
+   AppIndicator *trayicon;
++#endif
  
    GdkCursor   *paint_cursor;
    GdkCursor   *erase_cursor;
