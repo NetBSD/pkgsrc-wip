900772a9b4976995eaffc2d5960d816bc0272222
Havard Eidnes <he@NetBSD.org>
2025-05-14 12:56:01 +0200

    py-netsnmp-cffi: fix the time_t issue using cffi mechanism "int...".

--- py-netsnmp-cffi/patches/patch-src_netsnmpy_netsnmp__ffi.py
+++ py-netsnmp-cffi/patches/patch-src_netsnmpy_netsnmp__ffi.py
@@ -1,36 +1,16 @@
 $NetBSD$
 
-Provide a hook to replace correct type for time_t and suseconds_t.
-See pre-configure target.
+Use the new way of dealing with different-sized
+time_t types in FFI.
 
-Also handle the difference in sockaddr layout on Linux & BSDs.
-
---- src/netsnmpy/netsnmp_ffi.py.orig	2025-02-19 13:40:58.000000000 +0000
+--- src/netsnmpy/netsnmp_ffi.py.orig	2025-05-14 09:52:14.408968553 +0000
 +++ src/netsnmpy/netsnmp_ffi.py
-@@ -12,6 +12,17 @@ typedef struct _callback_data {
-     void          *reserved;
-     unsigned long  session_id;
- } _callback_data;
-+typedef struct linux_sockaddr_in {
-+    unsigned short sa_family;
-+    unsigned short sa_port;
-+    char           sa_data[14];
-+} linux_sockaddr_in;
-+typedef struct bsd_sockaddr_in {
-+    uint8_t  sa_len;
-+    uint8_t  sa_family;
-+    unsigned short sa_port;
-+    char     sa_data[14];
-+} bsd_sockaddr_in;
- """
- _CDEF = f"""
- /* Typedefs and structs we will be needing access to */
-@@ -20,9 +31,11 @@ typedef unsigned short u_short;
+@@ -20,9 +20,11 @@ typedef unsigned short u_short;
  typedef unsigned char u_char;
  typedef unsigned int u_int;
  typedef unsigned long oid;
-+typedef @TIME_T@ time_t; /* @TIME_T@ substituted by pre-configure target */
-+typedef @SUSECONDS_T@ suseconds_t; /* also done by pre-configure target */
++typedef int... time_t;
++typedef int... suseconds_t;
  typedef struct timeval {{
 -    long tv_sec;
 -    long tv_usec;
