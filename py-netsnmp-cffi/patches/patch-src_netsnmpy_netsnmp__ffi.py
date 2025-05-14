26fbb62093f4d739a9f54bc3f46b915e52cb9d65
Havard Eidnes <he@NetBSD.org>
2025-05-14 17:04:12 +0200

    py-netsnmp-cffi: undo some of the too aggressive removal I did...
    Bump PKGREVISION.

--- py-netsnmp-cffi/patches/patch-src_netsnmpy_netsnmp__ffi.py
+++ py-netsnmp-cffi/patches/patch-src_netsnmpy_netsnmp__ffi.py
@@ -3,9 +3,29 @@ $NetBSD$
 Use the new way of dealing with different-sized
 time_t types in FFI.
 
+Also handle the difference in sockaddr layout on Linux & BSDs.
+
 --- src/netsnmpy/netsnmp_ffi.py.orig	2025-05-14 09:52:14.408968553 +0000
 +++ src/netsnmpy/netsnmp_ffi.py
-@@ -20,9 +20,11 @@ typedef unsigned short u_short;
+@@ -12,6 +12,17 @@ typedef struct _callback_data {
+     void          *reserved;
+     unsigned long  session_id;
+ } _callback_data;
++typedef struct linux_sockaddr_in {
++     unsigned short sa_family;
++     unsigned short sa_port;
++     char           sa_data[14];
++} linux_sockaddr_in;
++typedef struct bsd_sockaddr_in {
++     uint8_t  sa_len;
++     uint8_t  sa_family;
++     unsigned short sa_port;
++     char     sa_data[14];
++} bsd_sockaddr_in;
+ """
+ _CDEF = f"""
+ /* Typedefs and structs we will be needing access to */
+@@ -20,9 +31,11 @@ typedef unsigned short u_short;
  typedef unsigned char u_char;
  typedef unsigned int u_int;
  typedef unsigned long oid;
