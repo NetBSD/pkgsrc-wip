dfdc44dea2c7ca43db98d9bea1f3945f35f6524b
Atsushi Toyokura <asteria.at@gmail.com>
2025-05-20 00:02:56 +0900

    libvidstab: Update to 1.1.1
    v1.1.1 (2022-05-31)
    
    Merge pull request #115 from Karry/sse-condition
    
    v1.1.0 (2017-05-30)
    
    use openMP for parallel execution (tag v1.0.0 was wrong, but never
    mind)
    
    v1.0.0 (2017-05-30)
    
    for semantic versioning reasons, there is no difference in code to
    0.98

--- libvidstab/buildlink3.mk
+++ libvidstab/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	libvidstab
 .if !defined(LIBVIDSTAB_BUILDLINK3_MK)
 LIBVIDSTAB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libvidstab+=	libvidstab>=0.98b
+BUILDLINK_API_DEPENDS.libvidstab+=	libvidstab>=1.1.1
 BUILDLINK_PKGSRCDIR.libvidstab?=	../../wip/libvidstab
 .endif	# LIBVIDSTAB_BUILDLINK3_MK
 
