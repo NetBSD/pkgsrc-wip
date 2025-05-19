948527f578d9545ec63f737a25ed53b9f860cb97
Atsushi Toyokura <asteria.at@gmail.com>
2025-05-19 23:32:43 +0900

    qwt6-qt4: Fixed PKGNAME

--- qwt6-qt4/buildlink3.mk
+++ qwt6-qt4/buildlink3.mk
@@ -1,15 +1,15 @@
 # $NetBSD: buildlink3.mk,v 1.20 2023/11/12 13:24:29 wiz Exp $
 
-BUILDLINK_TREE+=	qwt
+BUILDLINK_TREE+=	qwt6-qt4
 
-.if !defined(QWT_BUILDLINK3_MK)
-QWT_BUILDLINK3_MK:=
+.if !defined(QWT6_QT4_BUILDLINK3_MK)
+QWT6_QT4_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.qwt+=	qwt>=6.0.1
-BUILDLINK_ABI_DEPENDS.qwt+=	qwt>=6.0.1nb22
-BUILDLINK_PKGSRCDIR.qwt?=	../../wip/qwt6-qt4
+BUILDLINK_API_DEPENDS.qwt6-qt4+=	qwt6-qt4>=6.0.1
+BUILDLINK_ABI_DEPENDS.qwt6-qt4+=	qwt6-qt4>=6.0.1
+BUILDLINK_PKGSRCDIR.qwt6-qt4?=		../../wip/qwt6-qt4
 
 .include "../../x11/qt4-libs/buildlink3.mk"
-.endif	# QWT_BUILDLINK3_MK
+.endif	# QWT6_QT4_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-qwt
+BUILDLINK_TREE+=	-qwt6-qt4
