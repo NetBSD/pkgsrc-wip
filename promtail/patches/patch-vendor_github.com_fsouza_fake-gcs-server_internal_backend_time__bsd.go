f29427b2a5ee1de62a531689cd3a0d7346d01646
Leonardo Taccari <leot@NetBSD.org>
2025-07-17 20:03:27 +0200

    loki: Update to 3.5.2
    Changes:
    Too long to be mentioned, sorry.
    
    Please give a look to:
    
     <https://grafana.com/docs/loki/latest/release-notes/>

---
+++ promtail/patches/patch-vendor_github.com_fsouza_fake-gcs-server_internal_backend_time__bsd.go
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Add support for NetBSD.
+
+--- vendor/github.com/fsouza/fake-gcs-server/internal/backend/time_bsd.go.orig	2025-07-17 17:24:12.781746870 +0000
++++ vendor/github.com/fsouza/fake-gcs-server/internal/backend/time_bsd.go
+@@ -2,7 +2,7 @@
+ // Use of this source code is governed by a BSD-style
+ // license that can be found in the LICENSE file.
+ 
+-//go:build darwin || freebsd
++//go:build darwin || freebsd || netbsd
+ 
+ package backend
+ 
