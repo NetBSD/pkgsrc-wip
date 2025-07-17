366e7adbc656319f6c777315d7c041983c008f49
Leonardo Taccari <leot@NetBSD.org>
2025-07-17 20:19:18 +0200

    loki: Add upstream PR
    Patch was shared upstream. Add a reference to it so that we can more
    easily track it.

--- logcli/patches/patch-vendor_github.com_fsouza_fake-gcs-server_internal_backend_time__bsd.go
+++ logcli/patches/patch-vendor_github.com_fsouza_fake-gcs-server_internal_backend_time__bsd.go
@@ -2,6 +2,10 @@ $NetBSD$
 
 Add support for NetBSD.
 
+Shared upstream via:
+
+ <https://github.com/fsouza/fake-gcs-server/pull/1989>
+
 --- vendor/github.com/fsouza/fake-gcs-server/internal/backend/time_bsd.go.orig	2025-07-17 17:24:12.781746870 +0000
 +++ vendor/github.com/fsouza/fake-gcs-server/internal/backend/time_bsd.go
 @@ -2,7 +2,7 @@
