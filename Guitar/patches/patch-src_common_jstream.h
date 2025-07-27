6f9b168c2953212dca7ca279b6ec58ea8481b68f
Thomas Klausner <wiz@NetBSD.org>
2025-07-27 10:30:25 +0200

    Guitar: switch to upstream version of patch

--- Guitar/patches/patch-src_common_jstream.h
+++ Guitar/patches/patch-src_common_jstream.h
@@ -1,6 +1,6 @@
 $NetBSD$
 
-src/common/jstream.h:1564:39: error: 'nullptr_t' was not declared in this scope; did you mean 'std::nullptr_t'?
+https://github.com/0-wiz-0/Guitar/commit/ad88334a0ba555585ea653924fb749c5805d9aa1
 
 --- src/common/jstream.h.orig	2025-07-21 19:46:10.990061174 +0000
 +++ src/common/jstream.h
@@ -9,7 +9,7 @@ src/common/jstream.h:1564:39: error: 'nullptr_t' was not declared in this scope;
  static inline bool is_null(Variant const &v)
  {
 -	return std::holds_alternative<nullptr_t>(v);
-+	return std::holds_alternative<std::nullptr_t>(v);
++	return std::holds_alternative<null_t>(v);
  }
  static inline bool is_boolean(Variant const &v)
  {
