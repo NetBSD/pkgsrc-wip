93bf8e607791e12ad39c4e1267f695097b7816df
Thomas Klausner <wiz@NetBSD.org>
2025-07-31 20:44:58 +0200

    libimobiledevice: update to 1.3.0 + git commits of today
    1.3.0 does not build against the dependencies

---
+++ libimobiledevice/patches/patch-src_lockdown.c
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Fix ctype(3) usage.
+
+--- src/lockdown.c.orig	2025-07-31 18:39:12.420822229 +0000
++++ src/lockdown.c
+@@ -1487,7 +1487,7 @@ static void str_remove_spaces(char *sour
+ {
+ 	char *dest = source;
+ 	while (*source != 0) {
+-		if (!isspace(*source)) {
++		if (!isspace((unsigned char)*source)) {
+ 			*dest++ = *source; /* copy */
+ 		}
+ 		source++;
