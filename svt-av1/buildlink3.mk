278ec5190dc86d0ba81d450343cc5862f3f65e4f
Thomas Klausner <wiz@NetBSD.org>
2025-08-04 15:00:16 +0200

    svt-av1: upgrade candidate

---
+++ svt-av1/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.2 2024/08/21 17:00:41 wiz Exp $
+
+BUILDLINK_TREE+=	svt-av1
+
+.if !defined(SVT_AV1_BUILDLINK3_MK)
+SVT_AV1_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.svt-av1+=	svt-av1>=1.4.1
+BUILDLINK_ABI_DEPENDS.svt-av1?=	svt-av1>=3.0.0
+BUILDLINK_PKGSRCDIR.svt-av1?=	../../wip/svt-av1
+
+.endif	# SVT_AV1_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-svt-av1
