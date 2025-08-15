c5d68666124b9c2de8a443fe7db032aa9694786c
Thomas Klausner <wiz@NetBSD.org>
2025-08-15 13:55:29 +0200

    git-who: add new package

---
+++ git-who/go-modules.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+GO_MODULE_FILES+=	github.com/bmatcuk/doublestar/v4/@v/v4.8.1.mod
+GO_MODULE_FILES+=	github.com/bmatcuk/doublestar/v4/@v/v4.8.1.zip
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.6.0.mod
+GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.6.0.zip
+GO_MODULE_FILES+=	github.com/mattn/go-runewidth/@v/v0.0.16.mod
+GO_MODULE_FILES+=	github.com/mattn/go-runewidth/@v/v0.0.16.zip
+GO_MODULE_FILES+=	github.com/rivo/uniseg/@v/v0.2.0.mod
+GO_MODULE_FILES+=	github.com/rivo/uniseg/@v/v0.2.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.29.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.29.0.zip
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.28.0.mod
+GO_MODULE_FILES+=	golang.org/x/term/@v/v0.28.0.zip
