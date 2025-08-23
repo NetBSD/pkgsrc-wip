2637040a424d7ca89204108e55f18356513c8961
Thomas Klausner <wiz@NetBSD.org>
2025-08-23 21:15:11 +0200

    clamav: pkglint cleanup

--- clamav/buildlink3.mk
+++ clamav/buildlink3.mk
@@ -7,7 +7,7 @@ CLAMAV_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.clamav+=	clamav>=0.95.3nb1
 BUILDLINK_ABI_DEPENDS.clamav+=	clamav>=0.103.12nb4
-BUILDLINK_PKGSRCDIR.clamav?=	../../security/clamav
+BUILDLINK_PKGSRCDIR.clamav?=	../../wip/clamav
 
 .include "../../archivers/bzip2/buildlink3.mk"
 .include "../../devel/pcre2/buildlink3.mk"
