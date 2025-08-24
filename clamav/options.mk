258b768850c79d20f348ace2f9b1338f8ea1e159
Manuel Bouyer <bouyer@antioche.eu.org>
2025-08-24 18:28:40 +0200

    clamav: only set ALLOW_NETWORK_ACCESS if we're running 'make test'. From Roland Illig

--- clamav/options.mk
+++ clamav/options.mk
@@ -31,7 +31,9 @@ CMAKE_CONFIGURE_ARGS+=	-DENABLE_EXPERIMENTAL=on
 .if ${PKG_OPTIONS:Mclamav-unit-test}
 CMAKE_CONFIGURE_ARGS+=	-DENABLE_TESTS=on
 TEST_TARGET=			check
-ALLOW_NETWORK_ACCESS=		yes
+.if make(test)
+ALLOW_NETWORK_ACCESS=		yes                 
+.endif
 BUILDLINK_DEPMETHOD.check=	build
 .  include "../../devel/check/buildlink3.mk"
 .endif
