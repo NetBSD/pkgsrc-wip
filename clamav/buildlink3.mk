4a6d54a9373a0dfa1dce3894e442f5518db904e4
Manuel Bouyer <bouyer@antioche.eu.org>
2025-08-24 11:38:55 +0200

    clamav: adjust buildlink3 for new version. security/ecap_clamav_adapter builds with -fpermissive

--- clamav/buildlink3.mk
+++ clamav/buildlink3.mk
@@ -5,14 +5,17 @@ BUILDLINK_TREE+=	clamav
 .if !defined(CLAMAV_BUILDLINK3_MK)
 CLAMAV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clamav+=	clamav>=0.95.3nb1
-BUILDLINK_ABI_DEPENDS.clamav+=	clamav>=0.103.12nb4
+BUILDLINK_API_DEPENDS.clamav+=	clamav>=1.4.3
+BUILDLINK_ABI_DEPENDS.clamav+=	clamav>=1.4.3
 BUILDLINK_PKGSRCDIR.clamav?=	../../wip/clamav
 
 .include "../../archivers/bzip2/buildlink3.mk"
+.include "../../archivers/libmspack/buildlink3.mk"
 .include "../../devel/pcre2/buildlink3.mk"
 .include "../../textproc/json-c/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
 .endif	# CLAMAV_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-clamav
