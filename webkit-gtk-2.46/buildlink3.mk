9cbe0f917497cd248229b10b436b97dad7f3ee3c
Adam Ciarciński <adam@defguard.net>
2025-08-26 20:59:14 +0200

    webkit-gtk-2.46: one step closer to build on Aarch64

--- webkit-gtk-2.46/buildlink3.mk
+++ webkit-gtk-2.46/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	webkit-gtk
 WEBKIT_GTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.webkit-gtk+=	webkit-gtk>=2.8
-BUILDLINK_ABI_DEPENDS.webkit-gtk?=	webkit-gtk>=2.36.8nb1
+BUILDLINK_ABI_DEPENDS.webkit-gtk+=	webkit-gtk>=2.36.8nb1
 BUILDLINK_PKGSRCDIR.webkit-gtk?=	../../wip/webkit-gtk-2.46
 
 # This is necessary to match the package requirement.
