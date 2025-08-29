4152bfa420f969e71566b07d1002eb60a5d4ca84
Thomas Klausner <wiz@NetBSD.org>
2025-08-29 02:19:58 +0200

    ocaml: restore pic option for testing

--- ocaml/options.mk
+++ ocaml/options.mk
@@ -1,8 +1,8 @@
 # $NetBSD: options.mk,v 1.6 2022/05/24 18:25:38 jaapb Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ocaml
-PKG_SUPPORTED_OPTIONS=	flambda test
-PKG_SUGGESTED_OPTIONS=	flambda test
+PKG_SUPPORTED_OPTIONS=	flambda pic test
+PKG_SUGGESTED_OPTIONS=	flambda pic test
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,6 +10,12 @@ PKG_SUGGESTED_OPTIONS=	flambda test
 CONFIGURE_ARGS+=	--enable-flambda
 .else
 CONFIGURE_ARGS+=	--disable-flambda
+.endif
+
+.if !empty(PKG_OPTIONS:Mpic)
+CONFIGURE_ARGS+=	--with-pic
+.else
+CONFIGURE_ARGS+=	--without-pic
 .endif
 
 .if !empty(PKG_OPTIONS:Mtest)
