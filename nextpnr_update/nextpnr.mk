72697c9aae28f57c03a8069d355c3b7abb272928
adr <adr@sdf.org>
2025-08-31 20:37:15 +0100

    icestorm_update: Update devel/icestorm to version 0.0.20250603 nextpnr-ice40_update: Update devel/nextpnr-ice40 to version 0.8 nextpnr_update: Update devel/nextpnr to version 0.8

---
+++ nextpnr_update/nextpnr.mk
@@ -0,0 +1,35 @@
+# $NetBSD: nextpnr.mk,v 1.6 2024/10/14 06:45:39 wiz Exp $
+
+NEXTPNR_VERSION=0.8
+DISTNAME=	nextpnr-${NEXTPNR_VERSION}
+PKGNAME=	nextpnr-${NEXTPNR_TARGET}-${NEXTPNR_VERSION}
+.if defined(HIMBAECHEL_UARCH)
+PKGNAME=	PKGNAME-${HIMBAECHEL_UARCH}
+.endif
+CATEGORIES=	devel
+MASTER_SITES=	${MASTER_SITE_GITHUB:=YosysHQ/}
+GITHUB_PROJECT=	nextpnr
+GITHUB_TAG=	${DISTNAME}
+WRKSRC=		${WRKDIR}/nextpnr-${DISTNAME}
+
+MAINTAINER?=	thorpej@NetBSD.org
+HOMEPAGE=	https://github.com/YosysHQ/nextpnr
+COMMENT?=	A portable FPGA place and route tool
+LICENSE=	isc
+
+DISTINFO_FILE?=	${.CURDIR}/../../devel/nextpnr/distinfo
+PATCHDIR?=	${.CURDIR}/../../devel/nextpnr/patches
+
+USE_LANGUAGES=	c c++
+
+CMAKE_CONFIGURE_ARGS+=	-DCURRENT_GIT_VERSION=${NEXTPNR_VERSION}
+CMAKE_CONFIGURE_ARGS+=	-DARCH=${NEXTPNR_TARGET}
+.if defined(NEXTPNR_TARGET_ARGS)
+CMAKE_CONFIGURE_ARGS+=	${NEXTPNR_TARGET_ARGS}
+.endif
+
+.include "../../devel/cmake/build.mk"
+.include "../../devel/boost-libs/buildlink3.mk"
+.include "../../math/eigen3/buildlink3.mk"
+.include "../../lang/python/application.mk"
+.include "../../mk/bsd.pkg.mk"
