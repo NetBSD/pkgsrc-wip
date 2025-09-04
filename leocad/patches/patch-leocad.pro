c6d96c94c62a05c4413d2e5e59bc28bb7035caf0
Paul Ripke <stix@stix.id.au>
2025-09-04 21:17:25 +1000

    leocad: Update to 25.09
    Clean up patches, and build without PCH in a cleaner fashion.
    
    Release notes:
    
    - Added new Train Track Layout Editor.
    - Updated the Minifig Wizard.
    - Updated the Properties Widget.
    - Added Blender Render Support.

--- leocad/patches/patch-leocad.pro
+++ leocad/patches/patch-leocad.pro
@@ -2,9 +2,9 @@ $NetBSD$
 
 Disable PCH.
 
---- leocad.pro.orig	2021-06-05 23:41:42.000000000 +0000
+--- leocad.pro.orig	2025-09-02 07:47:24.896568456 +0000
 +++ leocad.pro
-@@ -40,7 +40,6 @@ win32-msvc* {
+@@ -47,7 +47,6 @@ win32-msvc* {
  	QMAKE_LFLAGS += /INCREMENTAL
  	LIBS += -ladvapi32 -lshell32 -lopengl32 -luser32
  } else {
--- leocad/patches/patch-qt_lc__renderdialog.cpp
+++ leocad/patches/patch-qt_lc__renderdialog.cpp
@@ -2,9 +2,9 @@ $NetBSD$
 
 Treat all Unices like Linux.
 
---- qt/lc_renderdialog.cpp.orig	2021-06-05 23:41:42.000000000 +0000
+--- qt/lc_renderdialog.cpp.orig	2025-09-01 20:49:34.000000000 +0000
 +++ qt/lc_renderdialog.cpp
-@@ -183,7 +183,7 @@ void lcRenderDialog::on_RenderButton_cli
+@@ -292,7 +292,7 @@ void lcRenderDialog::RenderPOVRay()
  	POVRayPath = QDir::cleanPath(QCoreApplication::applicationDirPath() + QLatin1String("/povconsole32-sse2.exe"));
  #endif
  
@@ -13,25 +13,16 @@ Treat all Unices like Linux.
  	POVRayPath = lcGetProfileString(LC_PROFILE_POVRAY_PATH);
  	Arguments.append("+FN");
  	Arguments.append("-D");
-@@ -194,7 +194,7 @@ void lcRenderDialog::on_RenderButton_cli
- #endif
+@@ -332,7 +332,7 @@ void lcRenderDialog::RenderPOVRay()
+ 	}
  
- 	mProcess = new QProcess(this);
+ 	lcRenderProcess* Process = new lcRenderProcess(this);
 -#ifdef Q_OS_LINUX
-+#ifdef Q_OS_UNIX
- 	connect(mProcess, SIGNAL(readyReadStandardError()), this, SLOT(ReadStdErr()));
++#ifdef Q_OS_UNUX
+ 	connect(Process, SIGNAL(readyReadStandardError()), this, SLOT(ReadStdErr()));
  #endif
- 	mProcess->start(POVRayPath, Arguments);
-@@ -221,7 +221,7 @@ void lcRenderDialog::ReadStdErr()
- {
- 	QString StdErr = QString(mProcess->readAllStandardError());
- 	mStdErrList.append(StdErr);
--#ifdef Q_OS_LINUX
-+#ifdef Q_OS_UNIX
- 	QRegExp RegexPovRayProgress("Rendered (\\d+) of (\\d+) pixels.*");
- 	RegexPovRayProgress.setCaseSensitivity(Qt::CaseInsensitive);
- 	if (RegexPovRayProgress.indexIn(StdErr) == 0)
-@@ -240,7 +240,7 @@ void lcRenderDialog::Update()
+ 	QStringList POVEnv = QProcess::systemEnvironment();
+@@ -662,7 +662,7 @@ void lcRenderDialog::Update()
  
  	if (mProcess->state() == QProcess::NotRunning)
  	{
