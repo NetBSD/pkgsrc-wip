9b042bbaa415c224ddab8ddc56f08a981d6b5356
adr <adr@sdf.org>
2025-09-08 17:03:23 +0100

    nextpnr: update to 0.9
    * Adding support for gatemate FPGAs
    * Improvements to Gowin support
    * Miscellaneous small iCE40 and ECP5 bugfixes

--- nextpnr_update/nextpnr.mk
+++ nextpnr_update/nextpnr.mk
@@ -1,6 +1,6 @@
-# $NetBSD: nextpnr.mk,v 1.6 2024/10/14 06:45:39 wiz Exp $
+# $NetBSD:$
 
-NEXTPNR_VERSION=0.0.20250829
+NEXTPNR_VERSION=0.9
 DISTNAME=	nextpnr-${NEXTPNR_VERSION}
 .if defined(HIMBAECHEL_UARCH)
 PKGNAME=	nextpnr-${NEXTPNR_TARGET}-${HIMBAECHEL_UARCH}-${NEXTPNR_VERSION}
@@ -10,12 +10,11 @@ PKGNAME=    nextpnr-${NEXTPNR_TARGET}-${NEXTPNR_VERSION}
 CATEGORIES=	devel
 MASTER_SITES=	${MASTER_SITE_GITHUB:=YosysHQ/}
 GITHUB_PROJECT=	nextpnr
-GITHUB_TAG=	bc086c012f509dab61a44eabb8cac47a48ebc60f
-WRKSRC=		${WRKDIR}/nextpnr-${GITHUB_TAG}
+GITHUB_TAG=		${DISTNAME}
 
-MAINTAINER?=	thorpej@NetBSD.org
+MAINTAINER?=thorpej@NetBSD.org
 HOMEPAGE=	https://github.com/YosysHQ/nextpnr
-COMMENT?=	A portable FPGA place and route tool
+COMMENT?=	Portable FPGA place and route tool
 LICENSE=	isc
 
 DISTINFO_FILE?=	${.CURDIR}/../../devel/nextpnr/distinfo
