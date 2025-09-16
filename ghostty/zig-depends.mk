9c2c8e4324a6f079b6792aec5d43488f03abf074
Amitai Schleier <schmonz-web-git@schmonz.com>
2025-09-16 13:52:01 -0400

    Add not-yet-building ghostty.

---
+++ ghostty/zig-depends.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+ZIG_PACKAGE_DEPENDS+=	https://github.com/mitchellh/libxev/archive/7f803181b158a10fec8619f793e3b4df515566cb.tar.gz
+ZIG_PACKAGE_DEPENDS+=	git+https://github.com/rockorager/libvaxis#1f41c121e8fc153d9ce8c6eb64b2bbab68ad7d23
+ZIG_PACKAGE_DEPENDS+=	https://github.com/vancluever/z2d/archive/refs/tags/v0.8.1.tar.gz
+ZIG_PACKAGE_DEPENDS+=	https://github.com/mitchellh/zig-objc/archive/c9e917a4e15a983b672ca779c7985d738a2d517c.tar.gz
+ZIG_PACKAGE_DEPENDS+=	https://deps.files.ghostty.org/zig_js-12205a66d423259567764fa0fc60c82be35365c21aeb76c5a7dc99698401f4f6fefc.tar.gz
+ZIG_PACKAGE_DEPENDS+=	https://deps.files.ghostty.org/ziglyph-b89d43d1e3fb01b6074bc1f7fc980324b04d26a5.tar.gz
+ZIG_PACKAGE_DEPENDS+=	https://codeberg.org/ifreund/zig-wayland/archive/f3c5d503e540ada8cbcb056420de240af0c094f7.tar.gz
+ZIG_PACKAGE_DEPENDS+=	https://github.com/natecraddock/zf/archive/7aacbe6d155d64d15937ca95ca6c014905eb531f.tar.gz
+ZIG_PACKAGE_DEPENDS+=	https://github.com/jcollie/ghostty-gobject/releases/download/0.15.1-2025-09-04-48-1/ghostty-gobject-0.15.1-2025-09-04-48-1.tar.zst
+ZIG_PACKAGE_DEPENDS+=	https://deps.files.ghostty.org/wayland-9cb3d7aa9dc995ffafdbdef7ab86a949d0fb0e7d.tar.gz
+ZIG_PACKAGE_DEPENDS+=	https://deps.files.ghostty.org/wayland-protocols-258d8f88f2c8c25a830c6316f87d23ce1a0f12d9.tar.gz
+ZIG_PACKAGE_DEPENDS+=	https://deps.files.ghostty.org/plasma_wayland_protocols-12207e0851c12acdeee0991e893e0132fc87bb763969a585dc16ecca33e88334c566.tar.gz
+ZIG_PACKAGE_DEPENDS+=	https://deps.files.ghostty.org/JetBrainsMono-2.304.tar.gz
+ZIG_PACKAGE_DEPENDS+=	https://deps.files.ghostty.org/NerdFontsSymbolsOnly-3.4.0.tar.gz
+ZIG_PACKAGE_DEPENDS+=	https://deps.files.ghostty.org/ghostty-themes-20250915-162204-b1fe546.tgz
