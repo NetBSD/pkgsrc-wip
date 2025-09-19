9aa800ebd8a5f480df1cd4cbcb77ea8430947fba
kikadf <kikadf.01@gmail.com>
2025-09-19 16:57:44 +0200

    libopeninput: fix buildlink3

--- libopeninput/buildlink3.mk
+++ libopeninput/buildlink3.mk
@@ -9,6 +9,8 @@ BUILDLINK_API_DEPENDS.libopeninput+=	libopeninput>=1.26.2
 BUILDLINK_ABI_DEPENDS.libopeninput+=	libopeninput>=1.26.2
 BUILDLINK_PKGSRCDIR.libopeninput?=	../../wip/libopeninput
 
+.include "../../wip/input-headers/buildlink3.mk"
+
 .endif # LIBOPENINPUT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libopeninput
