c56955c56c6b4f0a3979cde937e383671ce19d68
kikadf <kikadf.01@gmail.com>
2025-09-19 16:58:17 +0200

    libevdev-chunk: fix buildlink3

--- libevdev-chunk/buildlink3.mk
+++ libevdev-chunk/buildlink3.mk
@@ -9,6 +9,8 @@ BUILDLINK_API_DEPENDS.libevdev-chunk+=	libevdev-chunk>=20231111
 BUILDLINK_ABI_DEPENDS.libevdev-chunk+=	libevdev-chunk>=20231111
 BUILDLINK_PKGSRCDIR.libevdev-chunk?=	../../wip/libevdev-chunk
 
+.include "../../wip/input-headers/buildlink3.mk"
+
 .endif # LIBEVDEV_CHUNK_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libevdev-chunk
