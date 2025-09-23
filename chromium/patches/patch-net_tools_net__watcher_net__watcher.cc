660dadcd059d90c796423b31b0da78db52294e20
kikadf <kikadf.01@gmail.com>
2025-09-23 13:35:53 +0200

    chromium: fix patch

--- chromium/patches/patch-net_tools_net__watcher_net__watcher.cc
+++ chromium/patches/patch-net_tools_net__watcher_net__watcher.cc
@@ -4,7 +4,7 @@ $NetBSD$
 * Based on OpenBSD's chromium patches, and
   pkgsrc's qt5-qtwebengine patches
 
---- net/tools/net_watcher/net_watcher.cc.orig	2025-09-08 23:21:33.000000000 +0000
+--- net/tools/net_watcher/net_watcher.cc.orig	2025-09-16 20:11:32.000000000 +0000
 +++ net/tools/net_watcher/net_watcher.cc
 @@ -31,7 +31,7 @@
  #include "net/proxy_resolution/proxy_config_service.h"
@@ -24,15 +24,6 @@ $NetBSD$
  // Flag to specifies which network interfaces to ignore. Interfaces should
  // follow as a comma seperated list.
  const char kIgnoreNetifFlag[] = "ignore-netif";
-@@ -143,7 +143,7 @@ class NetWatcher :
- }  // namespace
- 
- int main(int argc, char* argv[]) {
--#if BUILDFLAG(IS_APPLE)
-+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
-   base::apple::ScopedNSAutoreleasePool pool;
- #endif
-   base::AtExitManager exit_manager;
 @@ -160,7 +160,7 @@ int main(int argc, char* argv[]) {
  
    NetWatcher net_watcher;
