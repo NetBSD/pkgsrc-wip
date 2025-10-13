c7dd1baf90df6b53397f5b8d209aea8881fe49b7
Iku Iwasa <iquiw@NetBSD.org>
2025-10-13 15:19:14 +0900

    browserpass-native: update to 3.1.2
    * No functional changes, mostly doc updates
    * Go module name has now /v3 suffix in line with the Go versioning
      recommendations
    * git tags are now prefixed with v in line with the Go packaging
      recommendations

--- browserpass-native/go-modules.mk
+++ browserpass-native/go-modules.mk
@@ -1,18 +1,20 @@
 # $NetBSD$
 
+GO_MODULE_FILES+=	github.com/browserpass/browserpass-native/@v/v0.0.0-20250425203345-8419b15841c9.mod
+GO_MODULE_FILES+=	github.com/browserpass/browserpass-native/@v/v0.0.0-20250425203345-8419b15841c9.zip
 GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.0.mod
 GO_MODULE_FILES+=	github.com/davecgh/go-spew/@v/v1.1.1.mod
-GO_MODULE_FILES+=	github.com/mattn/go-zglob/@v/v0.0.4.mod
-GO_MODULE_FILES+=	github.com/mattn/go-zglob/@v/v0.0.4.zip
+GO_MODULE_FILES+=	github.com/mattn/go-zglob/@v/v0.0.6.mod
+GO_MODULE_FILES+=	github.com/mattn/go-zglob/@v/v0.0.6.zip
 GO_MODULE_FILES+=	github.com/pmezard/go-difflib/@v/v1.0.0.mod
 GO_MODULE_FILES+=	github.com/rifflock/lfshook/@v/v0.0.0-20180920164130-b9218ef580f5.mod
 GO_MODULE_FILES+=	github.com/rifflock/lfshook/@v/v0.0.0-20180920164130-b9218ef580f5.zip
-GO_MODULE_FILES+=	github.com/sirupsen/logrus/@v/v1.9.0.mod
-GO_MODULE_FILES+=	github.com/sirupsen/logrus/@v/v1.9.0.zip
+GO_MODULE_FILES+=	github.com/sirupsen/logrus/@v/v1.9.3.mod
+GO_MODULE_FILES+=	github.com/sirupsen/logrus/@v/v1.9.3.zip
 GO_MODULE_FILES+=	github.com/stretchr/objx/@v/v0.1.0.mod
 GO_MODULE_FILES+=	github.com/stretchr/testify/@v/v1.7.0.mod
 GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.0.0-20220715151400-c0bba94af5f8.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.5.0.mod
-GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.5.0.zip
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.36.0.mod
+GO_MODULE_FILES+=	golang.org/x/sys/@v/v0.36.0.zip
 GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod
 GO_MODULE_FILES+=	gopkg.in/yaml.v3/@v/v3.0.0-20200313102051-9f266ea9e77c.mod
