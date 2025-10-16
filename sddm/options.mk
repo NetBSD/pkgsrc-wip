6d8d90efc9583a596a3ddee4bbf8531ed785ac51
Stepan Ipatov <st.ipatov@gmail.com>
2025-10-16 01:16:03 +0000

    sddm: Minor PLIST fixes

--- sddm/options.mk
+++ sddm/options.mk
@@ -5,11 +5,14 @@ PKG_OPTIONS_REQUIRED_GROUPS+=	gui
 PKG_OPTIONS_GROUP.gui=		qt5 qt6
 PKG_SUGGESTED_OPTIONS+=		qt6
 
+PLIST_VARS+=	qt5
+PLIST_VARS+=	qt6
+
 .include "../../mk/bsd.prefs.mk"
 .include "../../mk/bsd.options.mk"
 
 .if ${PKG_OPTIONS:Mqt5}
-PLIST_SRC=PLIST.qt5
+PLIST.qt5=	yes
 CMAKE_CONFIGURE_ARGS+=	-DBUILD_WITH_QT6=OFF
 BUILDLINK_API_DEPENDS.qt5-qtbase+=     qt5-qtbase>=5.15.17
 .include "../../x11/qt5-qtbase/buildlink3.mk"
@@ -18,6 +21,7 @@ BUILDLINK_API_DEPENDS.qt5-qtbase+=     qt5-qtbase>=5.15.17
 .endif
 
 .if ${PKG_OPTIONS:Mqt6}
+PLIST.qt6=	yes
 CMAKE_CONFIGURE_ARGS+=	-DBUILD_WITH_QT6=ON
 .include "../../x11/qt6-qtbase/buildlink3.mk"
 .include "../../lang/qt6-qtdeclarative/buildlink3.mk"
