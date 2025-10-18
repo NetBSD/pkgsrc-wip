9d37c8640e040e81c225468421f9677e7f7bd231
kami <kisskamizeg@gmail.com>
2025-10-18 13:08:53 +0200

    suricata: fix rc script to run in pcap mode and config yaml location, TODO update

--- suricata/files/suricata.sh
+++ suricata/files/suricata.sh
@@ -10,14 +10,14 @@ if [ -f /etc/rc.subr ]; then
 	. /etc/rc.subr
 fi
 
-: ${suricata_conf:=@PKG_SYSCONFDIR@/suricata.yaml}
+: ${suricata_conf:=@PKG_SYSCONFDIR@/suricata/suricata.yaml}
 
 name="suricata"
 rcvar=$name
 command="@PREFIX@/sbin/${name}"
 pidfile="@VARBASE@/run/${name}.pid"
 required_files="${suricata_conf}"
-command_args="-D"
+command_args="-D --pcap"
 
 if [ -f /etc/rc.subr -a -f /etc/rc.conf -a -f /etc/rc.d/DAEMON ]; then
 	load_rc_config $name
