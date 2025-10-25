62dd5e9d49991a428ebd2d18a2e0b8581522be28
Stepan Ipatov <st.ipatov@gmail.com>
2025-10-25 01:44:49 +0000

    py-electrum: add new package 4.6.2

---
+++ py-electrum/options.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.py-electrum
+PKG_SUPPORTED_OPTIONS=		x11
+PKG_SUGGESTED_OPTIONS=		x11
+
+.include "../../mk/bsd.prefs.mk"
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mx11)
+DEPENDS+=	${PYPKGPREFIX}-qt6>=6.9.0:../../x11/py-qt6
+#DEPENDS+=	${PYPKGPREFIX}-qdarkstyle>=3.2:../../wip/py-qdarkstyle
+.endif
---
+++ py-electrum/patches/patch-setup.py
@@ -0,0 +1,27 @@
+$NetBSD$
+
+Unconditional installation of data files.
+
+--- setup.py.orig	2025-10-24 15:49:25.764050171 +0000
++++ setup.py
+@@ -30,15 +30,11 @@ version_spec = importlib.util.spec_from_
+ version_module = version = importlib.util.module_from_spec(version_spec)
+ version_spec.loader.exec_module(version_module)
+ 
+-data_files = []
+-
+-if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
+-    # note: we can't use absolute paths here. see #7787
+-    data_files += [
+-        (os.path.join('share', 'applications'),               ['electrum.desktop']),
+-        (os.path.join('share', 'pixmaps'),                    ['electrum/gui/icons/electrum.png']),
+-        (os.path.join('share', 'icons/hicolor/128x128/apps'), ['electrum/gui/icons/electrum.png']),
+-    ]
++data_files = [
++    (os.path.join('share', 'applications'),               ['electrum.desktop']),
++    (os.path.join('share', 'pixmaps'),                    ['electrum/gui/icons/electrum.png']),
++    (os.path.join('share', 'icons/hicolor/128x128/apps'), ['electrum/gui/icons/electrum.png']),
++]
+ 
+ extras_require = {
+     'hardware': requirements_hw,
