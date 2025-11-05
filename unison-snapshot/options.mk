fd8295db35a62bdeb40c6d4768037099def397d1
Greg Troxel <gdt@lexort.com>
2025-11-04 19:24:22 -0500

    unison-snapshot: Merge changes from net/unison
      - $NetBSD$, for the compulsive
      - rename libinotify option to inotify to match existing option usage
      - [not merge] set up for unison beyond 2.53.8

--- unison-snapshot/options.mk
+++ unison-snapshot/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.9 2024/05/07 23:52:11 gdt Exp $
+# $NetBSD: options.mk,v 1.10 2025/11/05 00:21:39 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.unison
-PKG_SUPPORTED_OPTIONS=	lablgtk libinotify
+PKG_SUPPORTED_OPTIONS=	lablgtk inotify
 PKG_SUGGESTED_OPTIONS=	lablgtk
 
 # gtk3 is true if unison-gui is built.
@@ -13,7 +13,7 @@ PLIST_VARS+=	gtk3 monitor
 # \todo Perhaps, just enable unless libinotify says NOT_FOR_PLATFORM.
 
 .if ${MACHINE_PLATFORM:MNetBSD-*}
-PKG_SUGGESTED_OPTIONS+=	libinotify
+PKG_SUGGESTED_OPTIONS+=	inotify
 .endif
 
 .include "../../mk/bsd.options.mk"
@@ -25,7 +25,7 @@ DEPENDS+=	font-schumacher-misc>=1.0:../../fonts/font-schumacher-misc
 .include "../../x11/ocaml-lablgtk3/buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mlibinotify)
+.if !empty(PKG_OPTIONS:Minotify)
 PLIST.monitor=	yes
 
 .include "../../devel/libinotify/buildlink3.mk"
