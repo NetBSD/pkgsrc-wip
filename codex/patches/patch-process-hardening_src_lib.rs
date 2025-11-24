93cd15f01f4e59275a00b86f000ea803e68e1921
Thomas Klausner <wiz@NetBSD.org>
2025-11-24 11:24:44 +0100

    codex: update to 0.63.0.

--- codex/patches/patch-process-hardening_src_lib.rs
+++ codex/patches/patch-process-hardening_src_lib.rs
@@ -1,21 +1,15 @@
 $NetBSD$
 
-Allow define on NetBSD.
+Fix build on NetBSD.
+https://github.com/openai/codex/pull/7238
 
---- process-hardening/src/lib.rs.orig	2025-11-10 10:10:29.289117665 +0000
+--- process-hardening/src/lib.rs.orig	2025-11-24 10:15:16.068328981 +0000
 +++ process-hardening/src/lib.rs
-@@ -1,3 +1,5 @@
-+use libc;
-+
- /// This is designed to be called pre-main() (using `#[ctor::ctor]`) to perform
- /// various process hardening steps, such as
- /// - disabling core dumps
-@@ -20,7 +22,7 @@ const PRCTL_FAILED_EXIT_CODE: i32 = 5;
- #[cfg(target_os = "macos")]
- const PTRACE_DENY_ATTACH_FAILED_EXIT_CODE: i32 = 6;
- 
--#[cfg(any(target_os = "linux", target_os = "android", target_os = "macos"))]
-+#[cfg(any(target_os = "linux", target_os = "android", target_os = "macos", target_os = "netbsd"))]
+@@ -29,6 +29,7 @@ const PTRACE_DENY_ATTACH_FAILED_EXIT_COD
+     target_os = "android",
+     target_os = "macos",
+     target_os = "freebsd",
++    target_os = "netbsd",
+     target_os = "openbsd"
+ ))]
  const SET_RLIMIT_CORE_FAILED_EXIT_CODE: i32 = 7;
- 
- #[cfg(any(target_os = "linux", target_os = "android"))]
