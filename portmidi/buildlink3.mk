b95a6d8b81d8a9bf71cf08a97fe9c616fbfefb82
Thomas Klausner <wiz@NetBSD.org>
2025-11-29 18:54:20 +0100

    portmidi: fix package for NetBSD

--- portmidi/buildlink3.mk
+++ portmidi/buildlink3.mk
@@ -1,20 +1,9 @@
 # $NetBSD$
-# XXX
-# XXX This file was created automatically using createbuildlink-3.19.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
 
 BUILDLINK_TREE+=	portmidi
 
 .if !defined(PORTMIDI_BUILDLINK3_MK)
 PORTMIDI_BUILDLINK3_MK:=
-
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX	BUILDLINK_DEPMETHOD.portmidi?=	build
 
 BUILDLINK_API_DEPENDS.portmidi+=	portmidi>=2.0.4
 BUILDLINK_PKGSRCDIR.portmidi?=		../../wip/portmidi
