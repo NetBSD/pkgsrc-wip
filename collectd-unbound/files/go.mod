f70a2bdf80d36024a595f8f039e83f47398d3b2b
Thomas Klausner <wiz@NetBSD.org>
2025-11-29 21:11:18 +0100

    go-collectd: implement some suggestions by bsiegert
    to make build progress further

---
+++ collectd-unbound/files/go.mod
@@ -0,0 +1,7 @@
+module github.com/falzm/collectd-unbound
+
+go 1.25.4
+
+require collectd.org v0.6.0
+
+require go.uber.org/multierr v1.11.0 // indirect
---
+++ collectd-unbound/go-modules.mk
@@ -0,0 +1,6 @@
+# $NetBSD$
+
+GO_MODULE_FILES+=	collectd.org/@v/v0.6.0.mod
+GO_MODULE_FILES+=	collectd.org/@v/v0.6.0.zip
+GO_MODULE_FILES+=	go.uber.org/multierr/@v/v1.11.0.mod
+GO_MODULE_FILES+=	go.uber.org/multierr/@v/v1.11.0.zip
