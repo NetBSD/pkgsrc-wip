8faf0edc1cee29dd8f32091040e44ed8bc02060e
Thomas Klausner <wiz@NetBSD.org>
2025-11-29 22:33:53 +0100

    audacity: add comment to patch

--- audacity/patches/patch-libraries_lib-nyquist-effects_NyquistBase.cpp
+++ audacity/patches/patch-libraries_lib-nyquist-effects_NyquistBase.cpp
@@ -1,5 +1,11 @@
 $NetBSD$
 
+Fix
+audacity-sources-3.7.5/libraries/lib-nyquist-effects/NyquistBase.cpp: In member function 'bool NyquistBase::ProcessOne(NyxContext&, EffectOutputTracks*)':
+audacity-sources-3.7.5/libraries/lib-nyquist-effects/NyquistBase.cpp:1304:28: error: no matching function for call to 'max(const double&, float&)'
+ 1304 |          maxPeak = std::max(std::max(fabs(min), fabs(max)), maxPeak);
+      |                    ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 --- libraries/lib-nyquist-effects/NyquistBase.cpp.orig	2025-11-29 18:03:50.854130389 +0000
 +++ libraries/lib-nyquist-effects/NyquistBase.cpp
 @@ -1301,7 +1301,7 @@ bool NyquistBase::ProcessOne(
