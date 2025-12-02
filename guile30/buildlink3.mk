4bbbb6890a5db6450f96536f5356f5abb7135ccb
Thomas Klausner <wiz@NetBSD.org>
2025-12-02 23:17:41 +0100

    guile30: add upgrade candidate to 3.0.11, from gdt

---
+++ guile30/buildlink3.mk
@@ -0,0 +1,29 @@
+# $NetBSD: buildlink3.mk,v 1.3 2025/05/22 09:22:00 adam Exp $
+
+BUILDLINK_TREE+=	guile30
+
+.if !defined(GUILE30_BUILDLINK3_MK)
+GUILE30_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.guile30+=	guile30>=3.0
+BUILDLINK_ABI_DEPENDS.guile30+=	guile30>=3.0.8nb1
+BUILDLINK_PKGSRCDIR.guile30?=	../../wip/guile30
+
+BUILDLINK_PASSTHRU_DIRS+=		${PREFIX}/guile/3.0
+BUILDLINK_FILES.guile30+=		guile/3.0/bin/*
+BUILDLINK_FNAME_TRANSFORM.guile30+=	-e s,guile/3.0/bin,bin,
+BUILDLINK_FNAME_TRANSFORM.guile30+=	-e s,guile/3.0/lib/pkgconfig,lib/pkgconfig,
+
+GUILE30_SUBDIR=		guile/3.0
+
+.include "../../devel/gmp/buildlink3.mk"
+.include "../../devel/libffi/buildlink3.mk"
+.include "../../devel/libltdl/buildlink3.mk"
+.include "../../devel/readline/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../textproc/libunistring/buildlink3.mk"
+.include "../../devel/boehm-gc/buildlink3.mk"
+.endif # GUILE30_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-guile30
