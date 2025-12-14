da23380851fd106391a6a69171b4c0cfa9b6ddae
Havard Eidnes <he@NetBSD.org>
2025-12-14 19:19:44 +0000

    rust192: transform the riscv64.rs patches so that this builds again.
    (...I really should get these submitted upstream, should I not? ...)
    At least they don't look horrible now, even though they are
    ... written as error-message-based development.

--- rust192/patches/patch-vendor_libc-0.2.177_src_unix_bsd_netbsdlike_netbsd_riscv64.rs
+++ rust192/patches/patch-vendor_libc-0.2.177_src_unix_bsd_netbsdlike_netbsd_riscv64.rs
@@ -2,7 +2,7 @@ $NetBSD$
 
 --- vendor/libc-0.2.177/src/unix/bsd/netbsdlike/netbsd/riscv64.rs.orig	2025-12-14 06:33:12.291690472 +0000
 +++ vendor/libc-0.2.177/src/unix/bsd/netbsdlike/netbsd/riscv64.rs
-@@ -1,16 +1,16 @@
+@@ -1,27 +1,58 @@
 -use PT_FIRSTMACH;
 +use crate::PT_FIRSTMACH;
  
@@ -22,58 +22,48 @@ $NetBSD$
          __spare: [crate::__greg_t; 7],
      }
  }
-@@ -22,6 +22,55 @@ s_no_extra_traits! {
-     }
- }
  
+-s_no_extra_traits! {
+-    pub union __fpreg {
+-        pub u_u64: u64,
+-        pub u_d: c_double,
++#[derive(Clone,Copy)]
++pub union __fpreg {
++    pub u_u64: u64,
++    pub u_d: c_double,
++}
++
 +cfg_if! {
 +    if #[cfg(feature = "extra_traits")] {
-+//      use ::self::fmt;
-+//      use ::self::hash;
-+        use core::marker::Copy;
-+        use core::clone::Clone;
-+        #[cfg(libc_union)]
 +        impl PartialEq for __fpreg {
 +            fn eq(&self, other: &__fpreg) -> bool {
 +                unsafe {
-+                    self.u_64 == other.u_64
++                    self.u_u64 == other.u_u64
 +                        || self.u_d == other.u_d
 +                }
 +            }
 +        }
-+        #[cfg(libc_union)]
 +        impl Eq for __fpreg {}
-+        #[cfg(libc_union)]
-+        impl ::fmt::Debug for __fpreg {
-+            fn fmt(&self, f: &mut ::fmt::Formatter) -> ::fmt::Result {
++        impl hash::Hash for __fpreg {
++            fn hash<H: hash::Hasher>(&self, state: &mut H) {
++                unsafe {
++                    self.u_u64.hash(state);
++                }
++            }
++        }
++        impl fmt::Debug for __fpreg {
++            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 +                unsafe {
 +                    f.debug_struct("__fpreg")
-+                        .field("u_u64", &self.u_64)
++                        .field("u_u64", &self.u_u64)
 +                        .field("u_d", &self.u_d)
 +                        .finish()
 +                }
 +            }
 +        }
-+        #[cfg(libc_union)]
-+        impl ::hash::Hash for __fpreg {
-+            fn hash<H: ::hash::Hasher>(&self, state: &mut H) {
-+                unsafe {
-+                    self.u_64.hash(state);
-+                    self.u_d.hash(state);
-+                }
-+            }
-+        }
-+        #[cfg(libc_union)]
-+        impl ::Copy for __fpreg {}
-+        #[cfg(libc_union)]
-+        impl ::Clone for __fpreg {
-+            fn clone(&self) -> __fpreg {
-+                *self
-+            }
-+        }
-+    }
-+}
-+
+     }
+ }
+ 
 +
  pub(crate) const _ALIGNBYTES: usize = size_of::<c_long>() - 1;
  
