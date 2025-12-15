390c319ceeabb04d9e8df8e4bba0cbd99f0fccf4
Leonardo Taccari <leot@NetBSD.org>
2025-12-15 13:07:34 +0100

    visidata: avoid all unconditional curses.mousemask()
    Upstream #2851 (<https://github.com/saulpw/visidata/pull/2851>)
    addressed a problematic call to curses.mousemask() but there were still
    some other lefts.
    
    Catched via "e" command and mechanically via a `grep mousemask`.

---
+++ visidata/patches/patch-visidata___input.py
@@ -0,0 +1,34 @@
+$NetBSD$
+
+Conditionalize mousemask.
+
+Not all curses support mouse, e.g. NetBSD curses(3).
+
+Related to upstream #2851.
+
+--- visidata/_input.py.orig	2025-12-15 11:55:26.043085221 +0000
++++ visidata/_input.py
+@@ -60,16 +60,18 @@ def acceptThenFunc(*longnames):
+ class EnableCursor:
+     def __enter__(self):
+         with suppress(curses.error):
+-            curses.mousemask(0)
++            if hasattr(curses, 'mousemask'):
++                curses.mousemask(0)
+             curses.curs_set(1)
+ 
+     def __exit__(self, exc_type, exc_val, tb):
+         with suppress(curses.error):
+             curses.curs_set(0)
+-            if vd.options.mouse_interval:
+-                curses.mousemask(curses.MOUSE_ALL if hasattr(curses, "MOUSE_ALL") else 0xffffffff)
+-            else:
+-                curses.mousemask(0)
++            if hasattr(curses, 'mousemask'):
++                if vd.options.mouse_interval:
++                    curses.mousemask(curses.MOUSE_ALL if hasattr(curses, "MOUSE_ALL") else 0xffffffff)
++                else:
++                    curses.mousemask(0)
+ 
+ 
+ def until_get_wch(scr):
---
+++ visidata/patches/patch-visidata_movement.py
@@ -0,0 +1,21 @@
+$NetBSD$
+
+Conditionalize mousemask.
+
+Not all curses support mouse, e.g. NetBSD curses(3).
+
+Related to upstream #2851.
+
+--- visidata/movement.py.orig	2025-12-15 11:59:00.204848702 +0000
++++ visidata/movement.py
+@@ -189,8 +189,8 @@ BaseSheet.addCommand('g^^', 'jump-first'
+ 
+ BaseSheet.addCommand('BUTTON1_RELEASED', 'no-op', 'pass', 'do nothing')
+ 
+-BaseSheet.addCommand(None, 'mouse-enable', 'mm, _ = curses.mousemask(-1); status("mouse "+("ON" if mm else "OFF"))', 'enable mouse events')
+-BaseSheet.addCommand(None, 'mouse-disable', 'mm, _ = curses.mousemask(0); status("mouse "+("ON" if mm else "OFF"))', 'disable mouse events')
++BaseSheet.addCommand(None, 'mouse-enable', 'mm, _ = curses.mousemask(-1) if hasattr(curses, "mousemask") else None; status("mouse "+("ON" if mm else "OFF"))', 'enable mouse events')
++BaseSheet.addCommand(None, 'mouse-disable', 'mm, _ = curses.mousemask(0) if hasattr(curses, "mousemask") else None; status("mouse "+("ON" if mm else "OFF"))', 'disable mouse events')
+ 
+ 
+ vd.addGlobals({'rotateRange': rotateRange})
