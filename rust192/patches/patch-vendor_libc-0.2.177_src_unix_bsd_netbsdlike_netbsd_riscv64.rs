66deaf7785412491816f742f7916f7e557d11e22
Havard Eidnes <he@NetBSD.org>
2025-12-19 22:53:08 +0000

    rust192: transform the NetBSD riscv64 libc segment...
    ...to something which also passes the upstream automated checks.
    Pull request submitted at
      https://github.com/rust-lang/libc/pull/4886

--- rust192/patches/patch-vendor_libc-0.2.177_src_unix_bsd_netbsdlike_netbsd_riscv64.rs
+++ rust192/patches/patch-vendor_libc-0.2.177_src_unix_bsd_netbsdlike_netbsd_riscv64.rs
@@ -1,12 +1,25 @@
 $NetBSD$
 
---- vendor/libc-0.2.177/src/unix/bsd/netbsdlike/netbsd/riscv64.rs.orig	2025-12-14 06:33:12.291690472 +0000
-+++ vendor/libc-0.2.177/src/unix/bsd/netbsdlike/netbsd/riscv64.rs
-@@ -1,27 +1,58 @@
+Unbreak this module.
+
+I admit to this entirely being based on "cargo cult" and based on
+what the compiler told me, but at least the rust compiler (cross-)builds
+for NetBSD/risc64 with this, whereas it did not earlier.
+
+--- vendor/libc-0.2.175/src/unix/bsd/netbsdlike/netbsd/riscv64.rs.orig	2025-11-02 16:57:14.244169978 +0000
++++ vendor/libc-0.2.175/src/unix/bsd/netbsdlike/netbsd/riscv64.rs
+@@ -1,24 +1,59 @@
 -use PT_FIRSTMACH;
-+use crate::PT_FIRSTMACH;
++use core::clone::Clone;
++use core::cmp::{
++    Eq,
++    PartialEq,
++};
++use core::fmt::Debug;
++use core::marker::Copy;
  
  use crate::prelude::*;
++use crate::PT_FIRSTMACH;
  
  pub type __greg_t = u64;
  pub type __cpu_simple_lock_nv_t = c_int;
@@ -27,44 +40,38 @@ $NetBSD$
 -    pub union __fpreg {
 -        pub u_u64: u64,
 -        pub u_d: c_double,
-+#[derive(Clone,Copy)]
 +pub union __fpreg {
 +    pub u_u64: u64,
 +    pub u_d: c_double,
 +}
 +
-+cfg_if! {
-+    if #[cfg(feature = "extra_traits")] {
-+        impl PartialEq for __fpreg {
-+            fn eq(&self, other: &__fpreg) -> bool {
-+                unsafe {
-+                    self.u_u64 == other.u_u64
-+                        || self.u_d == other.u_d
-+                }
-+            }
++impl core::cmp::PartialEq for __fpreg {
++    fn eq(&self, other: &__fpreg) -> bool {
++        unsafe { self.u_u64 == other.u_u64 || self.u_d == other.u_d }
++    }
++}
++impl core::cmp::Eq for __fpreg {}
++impl core::marker::Copy for __fpreg {}
++impl core::clone::Clone for __fpreg {
++    fn clone(&self) -> __fpreg {
++        *self
++    }
++}
++impl hash::Hash for __fpreg {
++    fn hash<H: hash::Hasher>(&self, state: &mut H) {
++        unsafe {
++            self.u_u64.hash(state);
 +        }
-+        impl Eq for __fpreg {}
-+        impl hash::Hash for __fpreg {
-+            fn hash<H: hash::Hasher>(&self, state: &mut H) {
-+                unsafe {
-+                    self.u_u64.hash(state);
-+                }
-+            }
-+        }
-+        impl fmt::Debug for __fpreg {
-+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-+                unsafe {
-+                    f.debug_struct("__fpreg")
-+                        .field("u_u64", &self.u_u64)
-+                        .field("u_d", &self.u_d)
-+                        .finish()
-+                }
-+            }
++    }
++}
++impl core::fmt::Debug for __fpreg {
++    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
++        unsafe {
++            f.debug_struct("__fpreg")
++                .field("u_u64", &self.u_u64)
++                .field("u_d", &self.u_d)
++                .finish()
 +        }
      }
  }
  
-+
- pub(crate) const _ALIGNBYTES: usize = size_of::<c_long>() - 1;
- 
- pub const PT_GETREGS: c_int = PT_FIRSTMACH + 0;
