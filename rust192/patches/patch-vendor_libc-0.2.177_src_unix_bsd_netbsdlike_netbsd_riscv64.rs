b4846798e1e0eca88d59f9f0d7dd4b35da6259ac
Havard Eidnes <he@NetBSD.org>
2025-12-28 23:08:25 +0000

    rust192: use the NetBSD/riscv64 file which passes libc self-tests.
    Also change how NetBSD defines pthread_spin_t, use __pthread_spin_t
    which is typically arch-defined via __cpu_simple_lock_nv_t.

--- rust192/patches/patch-vendor_libc-0.2.177_src_unix_bsd_netbsdlike_netbsd_riscv64.rs
+++ rust192/patches/patch-vendor_libc-0.2.177_src_unix_bsd_netbsdlike_netbsd_riscv64.rs
@@ -8,43 +8,42 @@ for NetBSD/risc64 with this, whereas it did not earlier.
 
 --- vendor/libc-0.2.177/src/unix/bsd/netbsdlike/netbsd/riscv64.rs.orig	2025-11-02 16:57:14.244169978 +0000
 +++ vendor/libc-0.2.177/src/unix/bsd/netbsdlike/netbsd/riscv64.rs
-@@ -1,24 +1,59 @@
+@@ -1,28 +1,53 @@
 -use PT_FIRSTMACH;
-+use core::clone::Clone;
-+use core::cmp::{
-+    Eq,
-+    PartialEq,
-+};
-+use core::fmt::Debug;
-+use core::marker::Copy;
- 
+-
  use crate::prelude::*;
 +use crate::PT_FIRSTMACH;
  
  pub type __greg_t = u64;
- pub type __cpu_simple_lock_nv_t = c_int;
- pub type __gregset = [__greg_t; _NGREG];
+-pub type __cpu_simple_lock_nv_t = c_int;
+-pub type __gregset = [__greg_t; _NGREG];
 -pub type __fregset = [__freg; _NFREG];
-+pub type __fpregset = [__fpreg; _NFREG];
- 
- s! {
-     pub struct mcontext_t {
-         pub __gregs: __gregset,
+-
+-s! {
+-    pub struct mcontext_t {
+-        pub __gregs: __gregset,
 -        pub __fregs: __fpregset,
-+        pub __fpregs: __fpregset,
-         __spare: [crate::__greg_t; 7],
-     }
+-        __spare: [crate::__greg_t; 7],
+-    }
++pub union __fpreg {
++    pub u_u64: u64,
++    pub u_d: c_double,
++}
++pub type __cpu_simple_lock_nv_t = c_uint;
++pub type __gregset_t = [__greg_t; _NGREG];
++pub type __fregset_t = [__fpreg; _NFREG];
++
++#[derive(Debug)]
++pub struct mcontext_t {
++    pub __gregs: __gregset_t,
++    pub __fregs: __fregset_t,
++    __spare: [crate::__greg_t; 7],
  }
  
 -s_no_extra_traits! {
 -    pub union __fpreg {
 -        pub u_u64: u64,
 -        pub u_d: c_double,
-+pub union __fpreg {
-+    pub u_u64: u64,
-+    pub u_d: c_double,
-+}
-+
 +impl core::cmp::PartialEq for __fpreg {
 +    fn eq(&self, other: &__fpreg) -> bool {
 +        unsafe { self.u_u64 == other.u_u64 || self.u_d == other.u_d }
@@ -75,3 +74,8 @@ for NetBSD/risc64 with this, whereas it did not earlier.
      }
  }
  
+-pub(crate) const _ALIGNBYTES: usize = size_of::<c_long>() - 1;
++pub(crate) const _ALIGNBYTES: usize = 0xf;
+ 
+ pub const PT_GETREGS: c_int = PT_FIRSTMACH + 0;
+ pub const PT_SETREGS: c_int = PT_FIRSTMACH + 1;
