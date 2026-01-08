22601e77d704ec48bb744f154b522f2db9389182
Yorick Hardy <yorickhardy@gmail.com>
2026-01-08 14:11:54 +0000

    wip/cyclone{,-bootstrap}: change patch to use uintptr_t instead of void*

--- cyclone/patches/patch-gc.c
+++ cyclone/patches/patch-gc.c
@@ -3,7 +3,7 @@ $NetBSD$
 Concurrencykit returns an integer as a pointer here, and the warning
 -Wint-conversion results in a compiler error. Concurrencykit is
 choosing an integer size to match the pointer size (using implementations
-which are per-compiler/architecture), so just cast to (void*) to
+which are per-compiler/architecture), so just cast to (uintptr_t) to
 give a valid pointer type for ck_pr_add_ptr.
 
 --- gc.c.orig	2025-12-27 10:58:55.457517996 +0000
@@ -13,10 +13,10 @@ give a valid pointer type for ck_pr_add_ptr.
        gc_heap_merge(hdest, hsrc);
        ck_pr_add_ptr(&(dest->cached_heap_total_sizes[heap_type]),
 -                    ck_pr_load_ptr(&(src->cached_heap_total_sizes[heap_type])));
-+                    (void*)ck_pr_load_ptr(&(src->cached_heap_total_sizes[heap_type])));
++                    (uintptr_t)ck_pr_load_ptr(&(src->cached_heap_total_sizes[heap_type])));
        ck_pr_add_ptr(&(dest->cached_heap_free_sizes[heap_type]),
 -                    ck_pr_load_ptr(&(src->cached_heap_free_sizes[heap_type])));
-+                    (void*)ck_pr_load_ptr(&(src->cached_heap_free_sizes[heap_type])));
++                    (uintptr_t)ck_pr_load_ptr(&(src->cached_heap_free_sizes[heap_type])));
      }
    }
    ck_pr_add_int(&(dest->heap_num_huge_allocations),
