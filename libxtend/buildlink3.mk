2be78611c1a845f5f76d0c723146a09938fd263a
Jason Bacon <bacon@NetBSD.org>
2026-01-12 08:13:06 -0600

    libxtend: Update to 0.2.1.12
    xt_spawnvp(): Report failed exec() to stderr

--- libxtend/buildlink3.mk
+++ libxtend/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=		libxtend
 .if !defined(LIBXTEND_BUILDLINK3_MK)
 LIBXTEND_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.2.1.7
+BUILDLINK_API_DEPENDS.libxtend+=	libxtend>=0.2.1.12
 BUILDLINK_PKGSRCDIR.libxtend?=		../../wip/libxtend
 .endif	# LIBXTEND_BUILDLINK3_MK
 
