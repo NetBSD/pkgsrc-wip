059599a689e476c63cdc8c9d8d5ce9cd06342ec7
Mayuresh <mayuresh@warunjikar.in>
2026-01-21 11:59:30 +0530

    emscripten: very touchy about llvm version, downgraded to highest that works with llvm 19

--- emscripten/patches/patch-tools_config.py
+++ emscripten/patches/patch-tools_config.py
@@ -3,11 +3,11 @@ $NetBSD$
 Default cache is in install area. Known issue.
 https://github.com/emscripten-core/emscripten/issues/15374
 
---- tools/config.py.orig	2026-01-21 05:22:06.504048502 +0000
+--- tools/config.py.orig	2024-07-18 16:08:07.000000000 +0000
 +++ tools/config.py
-@@ -24,7 +24,7 @@ LLVM_ADD_VERSION = None
- CLANG_ADD_VERSION = None
- CLOSURE_COMPILER = None
+@@ -33,7 +33,7 @@ WASMER = None
+ WASMTIME = None
+ WASM_ENGINES: List[List[str]] = []
  FROZEN_CACHE = None
 -CACHE = None
 +CACHE = os.environ['HOME'] + '/.cache'
