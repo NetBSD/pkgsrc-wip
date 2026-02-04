1e9fd8cd4593848a9731c0eae325cccd7498f909
Olaf Seibert <rhialto@falu.nl>
2026-02-04 18:41:47 +0100

    dovecot24: bl file needs some dovecot->dovecot24 changes too

--- dovecot24/buildlink3.mk
+++ dovecot24/buildlink3.mk
@@ -1,22 +1,20 @@
 # $NetBSD: buildlink3.mk,v 1.40 2023/10/24 22:09:42 wiz Exp $
 
-BUILDLINK_TREE+=	dovecot
+BUILDLINK_TREE+=	dovecot24
 
 .if !defined(DOVECOT_BUILDLINK3_MK)
 DOVECOT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.dovecot+=		dovecot>=2.2.0
+BUILDLINK_API_DEPENDS.dovecot+=		dovecot>=2.4.0
 # must match current package version for plugins to load
-BUILDLINK_ABI_DEPENDS.dovecot+=		dovecot>=2.3.21nb1
-BUILDLINK_PKGSRCDIR.dovecot?=		../../mail/dovecot2
+BUILDLINK_ABI_DEPENDS.dovecot+=		dovecot>=2.4.0
+BUILDLINK_PKGSRCDIR.dovecot?=		../../wip/dovecot24
 
-pkgbase:=	dovecot
+pkgbase:=	dovecot24
 .include "../../mk/pkg-build-options.mk"
-.if ${PKG_BUILD_OPTIONS.dovecot:Mssl}
-.  include "../../security/openssl/buildlink3.mk"
-.endif
+.include "../../security/openssl/buildlink3.mk"
 .include "../../archivers/bzip2/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .endif # DOVECOT_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-dovecot
+BUILDLINK_TREE+=	-dovecot24
