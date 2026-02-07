68650317c23882dc364c1fb722b377d2feaaaf61
Olaf Seibert <rhialto@falu.nl>
2026-02-07 16:51:00 +0100

    dovecot24: get rid of _NETBSD_SOURCE.

--- dovecot24/patches/patch-src_lib_lib.h
+++ dovecot24/patches/patch-src_lib_lib.h
@@ -1,6 +1,9 @@
 $NetBSD$
 
-Fix re-definition warnings like
+- Fix needing to define _NETBSD_SOURCE by disabling the whole dubious lot
+  of visibility defines.
+
+- Fix re-definition warnings like
 
 CC       welcome-plugin.lo
 In file included from welcome-plugin.c:3:
@@ -13,8 +16,33 @@ In file included from /usr/include/string.h:127,
    95 | #define memcpy(dst, src, len) __ssp_bos_check3(memcpy, dst, src, len)
       |
 
---- src/lib/lib.h.orig	2026-02-03 17:35:03.025316093 +0000
-+++ src/lib/lib.h
+--- ./src/lib/lib.h.orig	2025-10-29 06:58:41.000000000 +0000
++++ ./src/lib/lib.h
+@@ -1,15 +1,15 @@
+ #ifndef LIB_H
+ #define LIB_H
+ 
+-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
+-#  define __BSD_VISIBLE 1
+-#elif defined(__APPLE__)
+-#  define _DARWIN_C_SOURCE 1
+-#endif
+-#define _BSD_SOURCE 1
+-#define _DEFAULT_SOURCE 1
+-#define _POSIX_C_SOURCE 200809L
+-#define _XOPEN_SOURCE 700
++//#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
++//#  define __BSD_VISIBLE 1
++//#elif defined(__APPLE__)
++//#  define _DARWIN_C_SOURCE 1
++//#endif
++//#define _BSD_SOURCE 1
++//#define _DEFAULT_SOURCE 1
++//#define _POSIX_C_SOURCE 200809L
++//#define _XOPEN_SOURCE 700
+ 
+ /* default lib includes */
+ #ifdef HAVE_CONFIG_H
 @@ -76,6 +76,7 @@ static inline void *i_memcpy(void *dest,
  	return memcpy(dest, src, n);
  }
