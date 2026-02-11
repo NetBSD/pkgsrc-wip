14f201d69bf6ff6e31803761fcd6d7b9f32417d6
Michael Baeuerle <micha@NetBSD.org>
2026-02-11 18:08:45 +0100

    avr-gcc: Update to 15.2

--- avr-gcc/buildlink3.mk
+++ avr-gcc/buildlink3.mk
@@ -5,9 +5,9 @@ BUILDLINK_TREE+=	avr-gcc
 .if !defined(AVR_GCC_BUILDLINK3_MK)
 AVR_GCC_BUILDLINK3_MK:=
 
-USE_CXX_FEATURES+=	c++11
+USE_CXX_FEATURES+=	c++14
 
-BUILDLINK_API_DEPENDS.avr-gcc+=	avr-gcc>=14.1.0
+BUILDLINK_API_DEPENDS.avr-gcc+=	avr-gcc>=15.2.0
 BUILDLINK_PKGSRCDIR.avr-gcc?=	../../wip/avr-gcc
 .endif	# AVR_GCC_BUILDLINK3_MK
 
