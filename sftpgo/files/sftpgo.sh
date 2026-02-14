313cc1876e24af12b6b71fcfe270864ce1a95c26
Bartosz Kuzma <bartosz.kuzma@gmail.com>
2026-02-14 10:44:59 +0100

    Add rc.d script for sftpgo

--- sftpgo/files/sftpgo.sh
+++ sftpgo/files/sftpgo.sh
@@ -1,32 +1,39 @@
 #!@RCD_SCRIPTS_SHELL@
-
+#
+# $NetBSD$
+#
 # PROVIDE: sftpgo
 # REQUIRE: DAEMON
-
-if [ -f /etc/rc.subr ]; then
-	. /etc/rc.subr
-fi
+# KEYWORD: shutdown
 
 name="sftpgo"
-rcvar=$name
-command="@PREFIX@/sbin/bitlbee"
-pidfile="@VARBASE@/run/${name}/pid"
-command_args="-F"	# run as a fork daemon
-start_precmd="bitlbee_precmd"
 
-bitlbee_precmd()
-{
-	if [ ! -d "@VARBASE@/run/${name}" ]; then
-		@MKDIR@ "@VARBASE@/run/${name}"
-		@CHMOD@ 0700 "@VARBASE@/run/${name}"
-		@CHOWN@ @BITLBEE_USER@:@BITLBEE_GROUP@ "@VARBASE@/run/${name}"
-	fi
-}
+command="@PREFIX@/sbin/daemonize"
 
-if [ -f /etc/rc.subr ]; then
-	load_rc_config $name
+pidfile="@VARBASE@/run/${name}.pid"
+
+task="@PREFIX@/bin/${name}"
+task_args="serve --config-dir @VARBASE@/sftpgo --config-file @PKG_SYSCONFDIR@/sftpgo.json"
+
+procname="${task}"
+
+command_args="-p ${pidfile} \
+	-u @SFTPGO_USER@ \
+	-c @VARBASE@/${name} \
+	-e @VARBASE@/${name}/${name}.stderr \
+	-o @VARBASE@/${name}/${name}.stdout \
+	${task} ${task_args}"
+
+if [ -f @SYSCONFBASE@/rc.subr ]; then
+    	. @SYSCONFBASE@/rc.subr
+
+	rcvar=${name}
+	pidfile="@VARBASE@/run/${name}.pid"
+
+	load_rc_config ${name}
 	run_rc_command "$1"
 else
-	echo -n "${name}"
-	${command} ${bitlbee_flags} ${command_args}
+        @ECHO@ -n " ${name}"
+
+	${command} ${command_args}
 fi
