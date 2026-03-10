8e6a39b5f47d22f3c73665dbdcad826d85136e6f
kikadf <kikadf.01@gmail.com>
2026-03-10 12:42:47 +0100

    libopeninput: update to 1.30.2

--- libopeninput/patches/patch-src_wscons.c
+++ libopeninput/patches/patch-src_wscons.c
@@ -1,16 +1,13 @@
 $NetBSD$
 
 * Handle attached-detached devices with udev monitor
-* Handle auto-repeat per device instead of globally
 
---- src/wscons.c.orig	2026-01-30 14:33:32.122725330 +0000
+--- src/wscons.c.orig	2026-03-08 12:38:09.000000000 +0000
 +++ src/wscons.c
-@@ -45,53 +45,121 @@ static void
+@@ -46,51 +46,121 @@ static void
  wscons_device_init_pointer_acceleration(struct wscons_device *device,
                struct motion_filter *filter);
  
--static int old_value = -1;
--
 -static int
 -udev_input_enable(struct libinput *libinput)
 +static void
@@ -68,6 +65,7 @@ $NetBSD$
 +			}
  		}
  	}
+-	return 0;
 +
 +out:
 +	udev_device_unref(udev_device);
@@ -94,7 +92,7 @@ $NetBSD$
 +	if (udev_monitor_enable_receiving(input->udev_monitor)) {
 +		log_info(libinput, "udev: failed to bind the udev monitor\n");
 +		goto err;
-+	}
++ 	}
 +
 +	fd = udev_monitor_get_fd(input->udev_monitor);
 +	input->udev_monitor_source =
@@ -103,7 +101,7 @@ $NetBSD$
 +		goto err;
 +	}
 +
- 	return 0;
++ 	return 0;
 +
 +err:
 +	if (input->udev_monitor) {
@@ -154,40 +152,9 @@ $NetBSD$
  	}
  }
  
-@@ -128,13 +196,13 @@ wscons_process(struct libinput_device *d
- 		key = wsevent->value;
- 		if (wsevent->type == WSCONS_EVENT_KEY_UP) {
- 			kstate = LIBINPUT_KEY_STATE_RELEASED;
--			old_value = -1;
-+			dev->old_value = -1;
- 		} else {
- 			kstate = LIBINPUT_KEY_STATE_PRESSED;
- 			/* ignore auto-repeat */
--			if (key == old_value)
-+			if (key == dev->old_value)
- 				return;
--			old_value = key;
-+			dev->old_value = key;
- 		}
- 		keyboard_notify_key(device, time,
- 		    wskey_transcode(wscons_device(device)->scanCodeMap, key), kstate);
-@@ -239,7 +307,7 @@ wscons_device_dispatch(void *data)
- 	ssize_t len;
- 	int count, i;
- 
--	len = read(device->fd, wsevents, sizeof(struct wscons_event));
-+	len = read(device->fd, wsevents, sizeof(wsevents));
- 	if (len <= 0 || (len % sizeof(struct wscons_event)) != 0)
- 		return;
- 
-@@ -282,20 +350,27 @@ wscons_seat_get(struct libinput *libinpu
- 
- LIBINPUT_EXPORT struct libinput *
- libinput_udev_create_context(const struct libinput_interface *interface,
--			     void *user_data,
--			     struct udev *udev)
-+				 void *user_data,
-+				 struct udev *udev)
+@@ -295,17 +365,24 @@ libinput_udev_create_context(const struc
+ 			     void *user_data,
+ 			     struct udev *udev)
  {
 -	struct libinput *libinput;
 +	struct udev_input *input;
@@ -195,14 +162,14 @@ $NetBSD$
 -	libinput = calloc(1, sizeof(*libinput));
 -	if (libinput == NULL)
 +	if (!interface || !udev)
++ 		return NULL;
++
++	input = calloc(1, sizeof(*input));
++	if (!input)
  		return NULL;
  
 -	if (libinput_init(libinput, interface, &interface_backend, user_data) != 0) {
 -		free(libinput);
-+	input = calloc(1, sizeof(*input));
-+	if (!input)
-+		return NULL;
-+
 +	if (libinput_init(&input->base, interface, &interface_backend, user_data) != 0) {
 +		libinput_unref(&input->base);
 +		free(input);
@@ -216,7 +183,7 @@ $NetBSD$
  }
  
  LIBINPUT_EXPORT int
-@@ -307,6 +382,7 @@ libinput_udev_assign_seat(struct libinpu
+@@ -317,6 +394,7 @@ libinput_udev_assign_seat(struct libinpu
  	uint64_t time;
  	struct timespec ts;
  	struct libinput_event *event;
@@ -224,7 +191,7 @@ $NetBSD$
  
  	/* Add standard devices */
  	for (int i = 0; i < 10; i++) {
-@@ -332,6 +408,10 @@ libinput_udev_assign_seat(struct libinpu
+@@ -342,6 +420,10 @@ libinput_udev_assign_seat(struct libinpu
  		post_device_event(device, time, LIBINPUT_EVENT_DEVICE_ADDED,
  		    event);
  	}
@@ -235,37 +202,7 @@ $NetBSD$
  	return 0;
  }
  
-@@ -517,6 +597,8 @@ wscons_device_init(struct wscons_device 
- {
- 	struct libinput_device *device = &wscons_device->base;
- 
-+	wscons_device->old_value = -1;
-+
- 	if (strncmp(device->devname, "/dev/wsmouse", 12) == 0) {
- 		/* XXX handle tablets and touchpanel */
- 		wscons_device->capability = LIBINPUT_DEVICE_CAP_POINTER;
-@@ -576,8 +658,18 @@ libinput_path_add_device(struct libinput
- 	return device;
- 
- err:
--	if (device != NULL)
--		close_restricted(libinput, device->fd);
-+	if (device != NULL) {
-+		if (device->source)
-+			libinput_remove_source(libinput, device->source);
-+		if (device->devname) {
-+			free(device->devname);
-+			device->devname = NULL;
-+		}
-+		if (device->fd >= 0)
-+			close_restricted(libinput, device->fd);
-+	} else if (fd >= 0) {
-+		close_restricted(libinput, fd);
-+	}
- 	free(wscons_device);
- 	return NULL;
- }
-@@ -587,6 +679,7 @@ libinput_path_remove_device(struct libin
+@@ -609,6 +691,7 @@ libinput_path_remove_device(struct libin
  {
  	struct libinput *libinput = device->seat->libinput;
  
--- libopeninput/patches/patch-src_wscons.h
+++ libopeninput/patches/patch-src_wscons.h
@@ -1,18 +1,10 @@
 $NetBSD$
 
-* Handle auto-repeat per device instead of globally
 * Handle attached-detached devices with udev monitor
 
---- src/wscons.h.orig	2026-01-30 14:33:45.725593515 +0000
+--- src/wscons.h.orig	2026-03-08 12:38:09.000000000 +0000
 +++ src/wscons.h
-@@ -18,12 +18,19 @@ struct wscons_device {
- 	struct libinput_device base;
- 	enum libinput_device_capability capability;
- 	struct TransMapRec *scanCodeMap;
-+	int old_value;
- 	struct {
- 		struct libinput_device_config_accel config;
- 		struct motion_filter *filter;
+@@ -25,6 +25,12 @@ struct wscons_device {
  	} pointer;
  };
  
