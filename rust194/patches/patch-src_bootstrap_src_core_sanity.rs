1e30675a49330d041b6a4f119137d0d422658799
Havard Eidnes <he@NetBSD.org>
2026-04-06 08:37:18 +0000

    rust194: for now don't make "known target in 'unknown' list" fatal.
    The "m68k-unknown-netbsd" entry may be in that list for a while,
    until upstream LLVM is fixed to not only emit 16-bit PC-relative
    relocations, which are ... inadequate for rust.
    
    This should get rid of this as a fatal error if you need to rebuild
    newer rust compilers using older pkgsrc rust compilers, which have
    since 1.93 made m68k-unknown-netbsd a "known" target.

--- rust194/patches/patch-src_bootstrap_src_core_sanity.rs
+++ rust194/patches/patch-src_bootstrap_src_core_sanity.rs
@@ -1,7 +1,8 @@
 $NetBSD$
 
 Temporarily add m68k-unknown-netbsd to STAGE0_MISSING_TARGETS.
-...while we bootstrap the target.
+...while we bootstrap the target, and don't make the presence
+while it's a known target a fatal error.
 
 --- src/bootstrap/src/core/sanity.rs.orig	2026-03-02 23:18:25.000000000 +0000
 +++ src/bootstrap/src/core/sanity.rs
@@ -13,3 +14,15 @@ Temporarily add m68k-unknown-netbsd to STAGE0_MISSING_TARGETS.
  ];
  
  /// Minimum version threshold for libstdc++ required when using prebuilt LLVM
+@@ -266,7 +267,10 @@ than building it.
+                 for duplicated_target in duplicated_targets {
+                     println!("  {duplicated_target}");
+                 }
+-                std::process::exit(1);
++// For now don't make this a fatal error.
++// e.g. m68k-unknown-netbsd may be in the list for a while
++// until LLVM is fixed to not emit 16-bit PC-relative relocations only...
++//              std::process::exit(1);
+             }
+ 
+             // Check if it's a built-in target.
