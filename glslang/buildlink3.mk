16e431bd096a9af849ce5fc83acdeeee7b0b2775
kikadf <kikadf.01@gmail.com>
2026-04-13 16:29:54 +0200

    glslang: switch to the vulkan-sdk release

--- glslang/buildlink3.mk
+++ glslang/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	glslang
 .if !defined(GLSLANG_BUILDLINK3_MK)
 GLSLANG_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.glslang+=	glslang>=15.0.0
+BUILDLINK_API_DEPENDS.glslang+=	glslang>=1.4.341.0
 BUILDLINK_PKGSRCDIR.glslang?=	../../wip/glslang
 
 .endif	# GLSLANG_BUILDLINK3_MK
