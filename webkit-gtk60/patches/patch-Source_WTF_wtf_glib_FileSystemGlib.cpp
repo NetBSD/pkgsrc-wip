13a67277876aa95d6137794bd0647428481bdccd
Thomas Klausner <wiz@NetBSD.org>
2026-04-24 17:50:53 +0200

    webkit-gtk60: fix two bugs in procfs handling

---
+++ webkit-gtk60/patches/patch-Source_WTF_wtf_glib_FileSystemGlib.cpp
@@ -0,0 +1,30 @@
+$NetBSD$
+
+The file name link has a different name in NetBSD's procfs than on Linux.
+Avoid providing zero-length paths.
+
+--- Source/WTF/wtf/glib/FileSystemGlib.cpp.orig	2026-04-23 20:09:38.532658486 +0000
++++ Source/WTF/wtf/glib/FileSystemGlib.cpp
+@@ -89,12 +89,21 @@ CString currentExecutablePath()
+         return { };
+     return CString(unsafeMakeSpan(readBuffer, static_cast<size_t>(result)));
+ }
++#elif OS(NETBSD)
++CString currentExecutablePath()
++{
++    static char readLinkBuffer[PATH_MAX];
++    ssize_t result = readlink("/proc/curproc/exe", readLinkBuffer, PATH_MAX);
++    if (result <= 0)
++        return { };
++    return CString(unsafeMakeSpan(readLinkBuffer, static_cast<size_t>(result)));
++}
+ #elif OS(UNIX)
+ CString currentExecutablePath()
+ {
+     static char readLinkBuffer[PATH_MAX];
+     ssize_t result = readlink("/proc/curproc/file", readLinkBuffer, PATH_MAX);
+-    if (result == -1)
++    if (result <= 0)
+         return { };
+     return CString(unsafeMakeSpan(readLinkBuffer, static_cast<size_t>(result)));
+ }
