c87ac40c120ba784fde9afb4baaeec23ef2f6457
Thomas Klausner <wiz@NetBSD.org>
2026-04-24 17:52:38 +0200

    webkit-gtk60: regen patch

--- webkit-gtk60/patches/patch-Source_WebCore_style_values_non-standard_StyleWebKitBoxReflect.h
+++ webkit-gtk60/patches/patch-Source_WebCore_style_values_non-standard_StyleWebKitBoxReflect.h
@@ -5,12 +5,12 @@ X11/X.h defines Below=1 and Above=0, which breaks ReflectionDirection enum.
 Must be in this file directly due to #pragma once preventing re-inclusion
 of RenderStyleConstants.h after X11 headers are included.
 
---- Source/WebCore/style/values/non-standard/StyleWebKitBoxReflect.h.orig	2025-04-07 18:43:10.000000000 +0000
+--- Source/WebCore/style/values/non-standard/StyleWebKitBoxReflect.h.orig	2026-02-23 14:40:55.954298000 +0000
 +++ Source/WebCore/style/values/non-standard/StyleWebKitBoxReflect.h
-@@ -28,6 +28,12 @@
+@@ -29,6 +29,12 @@
  #include <WebCore/StyleLengthWrapper.h>
  #include <WebCore/StyleMaskBorder.h>
-
+ 
 +// X11/X.h defines Below=1 and Above=0, conflicting with ReflectionDirection enum
 +#ifdef Below
 +#undef Below
@@ -19,4 +19,4 @@ of RenderStyleConstants.h after X11 headers are included.
 +
  namespace WebCore {
  namespace Style {
-
+ 
