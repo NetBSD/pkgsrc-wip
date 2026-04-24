a7d49f80bf781f89025b69d741b0741c546b4dd2
Thomas Klausner <wiz@NetBSD.org>
2026-04-24 17:52:51 +0200

    webkit-gtk60: regen patch

--- webkit-gtk60/patches/patch-Source_bmalloc_libpas_src_libpas_pas__monotonic__time.c
+++ webkit-gtk60/patches/patch-Source_bmalloc_libpas_src_libpas_pas__monotonic__time.c
@@ -11,19 +11,19 @@ $NetBSD$
  #include <time.h>
  #endif
  
-@@ -88,6 +88,15 @@ uint64_t pas_get_current_monotonic_time_
-     return ts.tv_sec * 1.0e9 + ts.tv_nsec;
- }
- 
+@@ -85,6 +85,15 @@ uint64_t pas_get_current_monotonic_time_nanoseconds(vo
+ {
+     struct timespec ts;
+     clock_gettime(CLOCK_MONOTONIC_COARSE, &ts);
++    return ts.tv_sec * 1.0e9 + ts.tv_nsec;
++}
++
 +#elif PAS_OS(OPENBSD) || PAS_OS(NETBSD)
 +
 +uint64_t pas_get_current_monotonic_time_nanoseconds(void)
 +{
 +    struct timespec ts;
 +    clock_gettime(CLOCK_MONOTONIC, &ts);
-+    return ts.tv_sec * 1.0e9 + ts.tv_nsec;
-+}
-+
- #elif PAS_PLATFORM(PLAYSTATION)
+     return ts.tv_sec * 1.0e9 + ts.tv_nsec;
+ }
  
- uint64_t pas_get_current_monotonic_time_nanoseconds(void)
