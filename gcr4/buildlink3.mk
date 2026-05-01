ab1ec865d25be77123a799d8feda21dd406256a9
Paolo Vincenzo Olivo <vms@retrobsd.ddns.net>
2026-05-01 18:38:32 +0200

    gcr4: update to 4.4.0.1
    Review the package. Move some dependencies to options.
    Remove content from Makefile which predates meson building.

--- gcr4/buildlink3.mk
+++ gcr4/buildlink3.mk
@@ -1,20 +1,35 @@
-# $NetBSD: buildlink3.mk,v 1.19 2024/11/17 07:16:36 wiz Exp $
+# $NetBSD$
 
-BUILDLINK_TREE+=	gcr4
+BUILDLINK_TREE+=	gcr
 
-.if !defined(GCR4_BUILDLINK3_MK)
-GCR4_BUILDLINK3_MK:=
+.if !defined(GCR_BUILDLINK3_MK)
+GCR_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gcr4+=	gcr4>=4.3.0
-BUILDLINK_ABI_DEPENDS.gcr4+=	gcr4>=4.3.0
-BUILDLINK_PKGSRCDIR.gcr4?=	../../wip/gcr4
+BUILDLINK_API_DEPENDS.gcr+=	gcr>=4.3.0
+BUILDLINK_PKGSRCDIR.gcr?=	../../wip/gcr4
 
 BUILDLINK_API_DEPENDS.glib2+=	glib2>=2.38
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/gobject-introspection/buildlink3.mk"
-.include "../../security/libgcrypt/buildlink3.mk"
+.include "../../security/libsecret/buildlink3.mk"
 .include "../../security/p11-kit/buildlink3.mk"
-.include "../../x11/gtk4/buildlink3.mk"
-.endif	# GCR4_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-gcr4
+pkgbase :=      gcr
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.gcr:Mgtk4}
+.  include "../../x11/gtk4/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.gcr:Mintrospection}
+.  include "../../devel/gobject-introspection/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.gcr:Mlibgcrypt}
+.  include "../../security/libgcrypt/buildlink3.mk"
+.elif ${PKG_BUILD_OPTIONS.gcr:Mgnutls}
+.  include "../../security/gnutls/buildlink3.mk"
+.endif
+
+.endif	# GCR_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gcr
