dc78a00d8a0f9b6da827a87da7763b622817dc6a
Thomas Klausner <wiz@NetBSD.org>
2026-05-04 22:30:39 +0200

    webkit-gtk60: add upstream pull request URL

--- webkit-gtk60/patches/patch-Source_WTF_wtf_glib_FileSystemGlib.cpp
+++ webkit-gtk60/patches/patch-Source_WTF_wtf_glib_FileSystemGlib.cpp
@@ -2,6 +2,7 @@ $NetBSD$
 
 The file name link has a different name in NetBSD's procfs than on Linux.
 Avoid providing zero-length paths.
+https://github.com/WebKit/WebKit/pull/64201
 
 --- Source/WTF/wtf/glib/FileSystemGlib.cpp.orig	2026-04-23 20:09:38.532658486 +0000
 +++ Source/WTF/wtf/glib/FileSystemGlib.cpp
