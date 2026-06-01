3c76777c75af607c04589d81cfa0377a31d21cd6
Greg Troxel <gdt@lexort.com>
2026-06-01 19:52:51 -0400

    bup: Update to 0.33.51.?
    This is a massive, heavyhanded rototill from a 2015 packaging of bup
    0.27.  It starts with sysutils/bup and builds 0.34-to-be instead.
    
    I'm declaring .51 to be pre-alpha and using the next digit as the
    number of commits past the 0.33.x branchpoint.  Later official alpha,
    beta, rc, will be 71, 81, 91, as they happen.
    
    Some of the accomodations here should probably be hoisted to
    sysutils/bup.  Some probably don't apply, as bup had a big build
    system rototill.

--- bup-git/options.mk
+++ bup-git/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2014/07/20 08:52:16 thomasklausner Exp $
+# $NetBSD: options.mk,v 1.2 2019/11/19 15:01:25 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.bup
 PKG_SUPPORTED_OPTIONS=	fuse
-# fuse is disabled by default because it doesn't build on Mac, ad
+# fuse is disabled by default because it doesn't build on Mac, and
 # because the fuse implementation in bup doesn't work on NetBSD.
 # After it's fixed upstream, an OS-specific default is probably
 # appropriate.
