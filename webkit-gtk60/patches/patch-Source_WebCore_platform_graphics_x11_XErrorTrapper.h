c45d703e82059655d98a9e0965a5f586152b50c3
Thomas Klausner <wiz@NetBSD.org>
2026-06-05 17:28:21 +0200

    webkit-gtk60: update comments, remove bmalloc patches
    Since 2.52.4, the default on NetBSD is to use the system allocator,
    so the NetBSD patches for bmalloc are not needed any longer.

--- webkit-gtk60/patches/patch-Source_WebCore_platform_graphics_x11_XErrorTrapper.h
+++ webkit-gtk60/patches/patch-Source_WebCore_platform_graphics_x11_XErrorTrapper.h
@@ -1,16 +1,5 @@
 $NetBSD$
 
-[Swift GTK] Undefined Success
-https://bugs.webkit.org/show_bug.cgi?id=314395
-
-Reviewed by Adrian Perez de Castro.
-
-The X11 header defines Success. This causes difficulty when this header is
-transitively included in some Swift/C++ import scenarios, since the definition
-of Success causes problems interpreting the 'Success' case of other enums
-across the codebase. Undefine Success since we don't currently need it.
-
-Additional ones in
 https://github.com/WebKit/WebKit/pull/64743
 
 --- Source/WebCore/platform/graphics/x11/XErrorTrapper.h.orig	2026-05-17 18:13:35.817512800 +0000
