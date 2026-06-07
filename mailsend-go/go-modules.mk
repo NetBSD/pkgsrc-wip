cdbf7af3c6f4ea86a5d11fa362e64179bf077a84
Iku Iwasa <iquiw@NetBSD.org>
2026-06-07 10:37:58 +0900

    mailsend-go: update to 1.0.12
    * Add STARTTLS downgrade protection. A MITM attacker can strip STARTTLS
      from the server's EHLO response, causing the client to send credentials in
      plaintext. The bug is not in mailsend-go itself, rather it is in the
      gomail.v2. The fix is in my fork of gomail via the new RequireSTARTTLS
      field on the Dialer. mailsend-go sets it when credentials are provided and
      SSL is not in use, so the connection returns an error instead of falling
      back to plaintext.
    * Remove unconditional debug print that leaked SMTP password to stdout.
    * A warning is now printed to stderr when TLS certificate verification is
      disabled. Use -verifyCert if desired.

--- mailsend-go/go-modules.mk
+++ mailsend-go/go-modules.mk
@@ -1,6 +1,6 @@
 # $NetBSD$
 
-GO_MODULE_FILES+=	github.com/muquit/gomail/@v/v0.0.0-20250704205629-a34fa7a15a08.mod
-GO_MODULE_FILES+=	github.com/muquit/gomail/@v/v0.0.0-20250704205629-a34fa7a15a08.zip
+GO_MODULE_FILES+=	github.com/muquit/gomail/@v/v1.0.2.mod
+GO_MODULE_FILES+=	github.com/muquit/gomail/@v/v1.0.2.zip
 GO_MODULE_FILES+=	github.com/muquit/quotedprintable/@v/v0.0.0-20250204043250-71206103869d.mod
 GO_MODULE_FILES+=	github.com/muquit/quotedprintable/@v/v0.0.0-20250204043250-71206103869d.zip
