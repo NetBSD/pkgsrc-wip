a846798df38fac415d421b72ab9110aea71d983d
kikadf <kikadf.01@gmail.com>
2026-06-19 03:00:44 +0200

    chromium: make wayland support optional
    Fix build on NetBSD-10: disable wayland support with native X11_TYPE
    
    ../../ui/ozone/platform/wayland/host/drm_syncobj_ioctl_wrapper.cc:50:10: error: use of undeclared identifier 'drmSyncobjEventfd'
       50 |   return drmSyncobjEventfd(fd_.get(), handle, point, ev_fd, flags);
          |          ^~~~~~~~~~~~~~~~~

--- chromium/options.mk
+++ chromium/options.mk
@@ -3,12 +3,21 @@
 PKG_OPTIONS_VAR=		PKG_OPTIONS.chromium
 PKG_OPTIONS_REQUIRED_GROUPS=	audio
 PKG_OPTIONS_GROUP.audio=	alsa pulseaudio sunaudio
-PKG_SUPPORTED_OPTIONS+=		debug
+PKG_SUPPORTED_OPTIONS+=		debug wayland
 
 .if ${OPSYS} == "NetBSD"
 PKG_SUGGESTED_OPTIONS+=		sunaudio debug
 .else
 PKG_SUGGESTED_OPTIONS+=		pulseaudio
+.endif
+
+.include "../../devel/wayland/platform.mk"
+.if ${PLATFORM_SUPPORTS_WAYLAND} == "yes"
+.  if ${OPSYS} == "NetBSD" && ${OPSYS_VERSION} < 110000 && ${X11_TYPE} == "native"
+# Need libdrm >= 2.4.116 for wayland support
+.  else
+PKG_SUGGESTED_OPTIONS+=	wayland
+.  endif
 .endif
 
 .include "../../mk/bsd.options.mk"
@@ -33,6 +42,15 @@ GN_ARGS+=	use_pulseaudio=true
 .else
 WITH_PA=	no
 GN_ARGS+=	use_pulseaudio=false
+.endif
+
+.if !empty(PKG_OPTIONS:Mwayland)
+GN_ARGS+=	use_system_libffi=true
+.include "../../devel/input-headers/buildlink3.mk"
+.include "../../devel/libffi/buildlink3.mk"
+.include "../../devel/wayland/buildlink3.mk"
+.else
+GN_ARGS+=	ozone_platform_wayland=false
 .endif
 
 .if !empty(PKG_OPTIONS:Mdebug)
