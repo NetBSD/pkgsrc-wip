6320406cc8d0dc2b56abfd383386ba917061be18
Santhosh Raju <fox@NetBSD.org>
2026-06-24 12:54:25 +0200

    bottom: add missed out patch files.

--- bottom/patches/patch-src_collection_disks.rs
+++ bottom/patches/patch-src_collection_disks.rs
@@ -2,24 +2,25 @@ $NetBSD$
 
 Disable catch-all for disks in NetBSD.
 
---- src/collection/disks.rs.orig	2026-04-18 02:12:01.054885511 +0000
+--- src/collection/disks.rs.orig	2026-06-22 06:25:54.000000000 +0000
 +++ src/collection/disks.rs
-@@ -21,6 +21,9 @@ cfg_if! {
-     } else if #[cfg(target_os = "macos")] {
+@@ -25,6 +25,10 @@ cfg_select! {
          mod unix;
          pub(crate) use self::unix::*;
-+    } else if #[cfg(target_os = "netbsd")] {
+     }
++    target_os = "netbsd" => {
 +        mod netbsd;
-+        pub(crate) use self::netbsd::*;
-     } else {
++        pub(crate) use self::unix::*;
++    }    
+     _ => {
          mod other;
          pub(crate) use self::other::*;
-@@ -79,7 +82,7 @@ cfg_if! {
- 
+@@ -87,7 +91,7 @@ cfg_select! {
              Ok(io_hash)
          }
--    } else if #[cfg(not(target_os = "freebsd"))] {
-+    } else if #[cfg(not(any(target_os = "freebsd", target_os = "netbsd")))] {
-         pub fn get_io_usage() -> anyhow::Result<IoHarvest> {
+     }
+-    not(target_os = "freebsd") => {
++    not(all(target_os = "freebsd", target_os = "netbsd")) => {
+         use crate::collection::DataCollector;
+         pub fn get_io_usage(_collector: &DataCollector) -> anyhow::Result<IoHarvest> {
              anyhow::bail!("Unsupported OS");
-         }
--- bottom/patches/patch-src_collection_processes.rs
+++ bottom/patches/patch-src_collection_processes.rs
@@ -2,14 +2,14 @@ $NetBSD$
 
 Enable process info collection for NetBSD.
 
---- src/collection/processes.rs.orig	2026-04-18 01:11:47.496423789 +0000
+--- src/collection/processes.rs.orig	2026-06-24 10:52:04.504828582 +0000
 +++ src/collection/processes.rs
-@@ -171,7 +171,7 @@ impl DataCollector {
-                     self,
+@@ -181,7 +181,7 @@ impl DataCollector {
                      time_diff,
                  )
--            } else if #[cfg(any(target_os = "freebsd", target_os = "macos", target_os = "windows", target_os = "android", target_os = "ios"))] {
-+            } else if #[cfg(any(target_os = "freebsd", target_os = "netbsd", target_os = "macos", target_os = "windows", target_os = "android", target_os = "ios"))] {
+             }
+-            any(target_os = "freebsd", target_os = "macos", target_os = "windows", target_os = "android", target_os = "ios") => {
++            any(target_os = "freebsd", target_os = "macos", target_os = "netbsd", target_os = "windows", target_os = "android", target_os = "ios") => {
                  sysinfo_process_data(self)
-             } else {
-                 Err(crate::collection::error::CollectionError::Unsupported)
+             }
+             _ => {
