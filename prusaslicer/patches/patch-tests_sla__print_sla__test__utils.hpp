ad131262bdddb3b457777e5a4038c879e1fd016e
Paul Ripke <stix@stix.id.au>
2026-07-08 22:00:47 +1000

    prusaslicer: Fix build with new catch2 3.15 from pkgsrc-2026Q2.

---
+++ prusaslicer/patches/patch-tests_sla__print_sla__test__utils.hpp
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- tests/sla_print/sla_test_utils.hpp.orig	2026-07-07 12:54:44.228906196 +0000
++++ tests/sla_print/sla_test_utils.hpp
+@@ -3,6 +3,7 @@
+ 
+ #include <catch2/catch_test_macros.hpp>
+ #include <catch2/catch_approx.hpp>
++#include <catch2/interfaces/catch_interfaces_capture.hpp>
+ #include <test_utils.hpp>
+ 
+ // Debug
