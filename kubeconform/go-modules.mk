fcbe982e50c2e69393f19269361b491bd1c2fd70
Leonardo Taccari <leot@NetBSD.org>
2026-07-25 12:08:48 +0200

    kubeconform: Update to 0.8.0
    Changes:
    - Bug fixes

--- kubeconform/go-modules.mk
+++ kubeconform/go-modules.mk
@@ -1,12 +1,18 @@
 # $NetBSD$
 
-GO_MODULE_FILES+=	github.com/google/go-cmp/@v/v0.5.9.mod
 GO_MODULE_FILES+=	github.com/hashicorp/go-cleanhttp/@v/v0.5.2.mod
 GO_MODULE_FILES+=	github.com/hashicorp/go-cleanhttp/@v/v0.5.2.zip
-GO_MODULE_FILES+=	github.com/hashicorp/go-retryablehttp/@v/v0.7.7.mod
-GO_MODULE_FILES+=	github.com/hashicorp/go-retryablehttp/@v/v0.7.7.zip
-GO_MODULE_FILES+=	github.com/santhosh-tekuri/jsonschema/v5/@v/v5.3.1.mod
-GO_MODULE_FILES+=	github.com/santhosh-tekuri/jsonschema/v5/@v/v5.3.1.zip
-GO_MODULE_FILES+=	gopkg.in/check.v1/@v/v0.0.0-20161208181325-20d25e280405.mod
-GO_MODULE_FILES+=	sigs.k8s.io/yaml/@v/v1.4.0.mod
-GO_MODULE_FILES+=	sigs.k8s.io/yaml/@v/v1.4.0.zip
+GO_MODULE_FILES+=	github.com/hashicorp/go-retryablehttp/@v/v0.7.8.mod
+GO_MODULE_FILES+=	github.com/hashicorp/go-retryablehttp/@v/v0.7.8.zip
+GO_MODULE_FILES+=	github.com/santhosh-tekuri/jsonschema/v6/@v/v6.0.2.mod
+GO_MODULE_FILES+=	github.com/santhosh-tekuri/jsonschema/v6/@v/v6.0.2.zip
+GO_MODULE_FILES+=	go.yaml.in/yaml/v2/@v/v2.4.4.mod
+GO_MODULE_FILES+=	go.yaml.in/yaml/v2/@v/v2.4.4.zip
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.37.0.mod
+GO_MODULE_FILES+=	golang.org/x/text/@v/v0.37.0.zip
+GO_MODULE_FILES+=	k8s.io/kube-openapi/@v/v0.0.0-20260603220949-865597e52e25.mod
+GO_MODULE_FILES+=	k8s.io/kube-openapi/@v/v0.0.0-20260603220949-865597e52e25.zip
+GO_MODULE_FILES+=	k8s.io/utils/@v/v0.0.0-20260210185600-b8788abfbbc2.mod
+GO_MODULE_FILES+=	k8s.io/utils/@v/v0.0.0-20260210185600-b8788abfbbc2.zip
+GO_MODULE_FILES+=	sigs.k8s.io/yaml/@v/v1.6.0.mod
+GO_MODULE_FILES+=	sigs.k8s.io/yaml/@v/v1.6.0.zip
