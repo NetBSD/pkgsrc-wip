10392168a6315634efc85213bcab7bd36b78b646
Ryo ONODERA <ryoon@NetBSD.org>
2026-08-11 02:02:21 +0900

    wip/tcl-expect-git: import tcl-expect-5.45.5
    Expect is a program that performs programmed dialogue with other
    interactive programs.  It is briefly described by its man page, expect(1).
    
    It uses pseudo-tty's to control the child processes, so it is not affected
    by programs that refuse to read from stdin or pipes. (eg: passwd(1) etc).

---
+++ tcl-expect-git/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD: buildlink3.mk,v 1.9 2014/01/11 14:42:00 adam Exp $
+
+BUILDLINK_TREE+=	tcl-expect
+
+.if !defined(TCL_EXPECT_BUILDLINK3_MK)
+TCL_EXPECT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.tcl-expect+=	tcl-expect>=5.38.0nb1
+BUILDLINK_ABI_DEPENDS.tcl-expect+=	tcl-expect>=5.45.4nb1
+BUILDLINK_PKGSRCDIR.tcl-expect?=	../../wip/tcl-expect-git
+
+.include "../../lang/tcl/buildlink3.mk"
+.endif # TCL_EXPECT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-tcl-expect
