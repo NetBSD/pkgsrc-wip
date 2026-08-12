076f43a295b53c2096c9197df4fe8750a924ee64
Stepan Ipatov <st.ipatov@gmail.com>
2026-08-12 04:39:08 +0200

    www/privoxy: Update to 4.2.0

---
+++ privoxy/options.mk
@@ -0,0 +1,28 @@
+# $NetBSD: options.mk,v 1.1 2018/03/21 23:18:09 gdt Exp $
+#
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.privoxy
+PKG_SUPPORTED_OPTIONS+=		inet6 zlib zstd
+PKG_SUGGESTED_OPTIONS+=		inet6 zlib zstd
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--enable-ipv6-support
+.else
+CONFIGURE_ARGS+=	--disable-ipv6-support
+.endif
+
+.if !empty(PKG_OPTIONS:Mzlib)
+CONFIGURE_ARGS+=	--enable-zlib --enable-compression
+.include "../../devel/zlib/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-zlib --disable-compression
+.endif
+
+.if !empty(PKG_OPTIONS:Mzstd)
+CONFIGURE_ARGS+=	--with-zstd
+.include "../../archivers/zstd/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-zstd
+.endif
