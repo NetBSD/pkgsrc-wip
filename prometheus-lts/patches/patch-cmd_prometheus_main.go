ac4f0df7f6488d6a5050041a058e74057eaa312d
Leonardo Taccari <leot@NetBSD.org>
2026-08-17 15:37:24 +0200

    prometheus-lts: Prepare for 3.13.2 update
    Still not updated, only did the dance.

--- prometheus-lts/patches/patch-cmd_prometheus_main.go
+++ prometheus-lts/patches/patch-cmd_prometheus_main.go
@@ -2,18 +2,18 @@ $NetBSD: patch-cmd_prometheus_main.go,v 1.3 2022/03/11 19:56:00 tnn Exp $
 
 Add prefix for SYSCONFDIR and VARBASE to store configuration file and metrics data at the correct location.
 
---- cmd/prometheus/main.go.orig	2026-01-21 13:27:59.000000000 +0000
+--- cmd/prometheus/main.go.orig	2026-07-30 11:04:45.000000000 +0000
 +++ cmd/prometheus/main.go
-@@ -354,7 +354,7 @@ func main() {
+@@ -407,7 +407,7 @@ func main() {
  	a.HelpFlag.Short('h')
  
  	a.Flag("config.file", "Prometheus configuration file path.").
 -		Default("prometheus.yml").StringVar(&cfg.configFile)
 +		Default("@PKG_SYSCONFDIR@/prometheus.yml").StringVar(&cfg.configFile)
  
- 	a.Flag("config.auto-reload-interval", "Specifies the interval for checking and automatically reloading the Prometheus configuration file upon detecting changes.").
- 		Default("30s").SetValue(&cfg.autoReloadInterval)
-@@ -414,10 +414,10 @@ func main() {
+ 	a.Flag("config.auto-reload", "Enable automatic configuration file reloading. See also --config.auto-reload-interval.").
+ 		Default("false").BoolVar(&cfg.enableAutoReload)
+@@ -470,10 +470,10 @@ func main() {
  		Default("false").BoolVar(&cfg.web.EnableOTLPWriteReceiver)
  
  	a.Flag("web.console.templates", "Path to the console template directory, available at /consoles.").
@@ -26,7 +26,7 @@ Add prefix for SYSCONFDIR and VARBASE to store configuration file and metrics da
  
  	a.Flag("web.page-title", "Document title of Prometheus instance.").
  		Default("Prometheus Time Series Collection and Processing Server").StringVar(&cfg.web.PageTitle)
-@@ -426,7 +426,7 @@ func main() {
+@@ -482,7 +482,7 @@ func main() {
  		Default(".*").StringVar(&cfg.corsRegexString)
  
  	serverOnlyFlag(a, "storage.tsdb.path", "Base path for metrics storage.").
