ef168e3808b56f76105d165d358d993ec3dde836
pin <voidpin@protonmail.com>
2026-08-21 18:31:33 +0200

    rust-beta: update 1.99.0-beta.1

--- rust-beta/patches/patch-src_llvm-project_llvm_utils_FileCheck_FileCheck.cpp
+++ rust-beta/patches/patch-src_llvm-project_llvm_utils_FileCheck_FileCheck.cpp
@@ -1,15 +1,15 @@
-$NetBSD: patch-src_llvm-project_llvm_utils_FileCheck_FileCheck.cpp,v 1.13 2024/04/18 09:29:42 pin Exp $
+$NetBSD$
 
 Avoid ambiguous function call.
 
---- src/llvm-project/llvm/utils/FileCheck/FileCheck.cpp.orig	2019-02-12 15:22:48.000000000 +0000
+--- src/llvm-project/llvm/utils/FileCheck/FileCheck.cpp.orig	2026-08-21 10:04:43.213547864 +0000
 +++ src/llvm-project/llvm/utils/FileCheck/FileCheck.cpp
-@@ -592,7 +592,7 @@ static void DumpAnnotatedInput(raw_ostre
+@@ -929,7 +929,7 @@ static void DumpAnnotatedInput(raw_ostream &OS, const 
    unsigned LineCount = InputFileText.count('\n');
    if (InputFileEnd[-1] != '\n')
      ++LineCount;
--  unsigned LineNoWidth = std::log10(LineCount) + 1;
-+  unsigned LineNoWidth = std::log10((float)LineCount) + 1;
+-  unsigned LineNoWidth = NumDigitsBase10(LineCount);
++  unsigned LineNoWidth = NumDigitsBase10((float)LineCount);
    // +3 below adds spaces (1) to the left of the (right-aligned) line numbers
    // on input lines and (2) to the right of the (left-aligned) labels on
    // annotation lines so that input lines and annotation lines are more
