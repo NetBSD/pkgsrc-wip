6c0d309b2f69f2ee515d3ee6817f72c7f8c33063
Kate <kit-ty-kate@exn.st>
2026-08-25 11:33:43 +0100

    ocaml-opam: add new package
    Differences from pkgsrc's misc/ocaml-opam:
    - update from 2.1.3 to 2.5.2 (fixing two CVEs in the process)
    - the -full tarball is now used instead of the one autogenerated by
      github
    - rsync is now a runtime dependency (it is technically optional but it
      helps users to have it for the moment)
    - ocaml-dune is now explicitly a build dependency but no longer used
      as-is. It is now used through gmake
    - ocaml-cmdliner, ocaml-re, ocamlgraph, dose3, ocaml-cudf and
      ocaml-opam-file-format are no longer dependencies
    - the vendored versions of all the dependencies contained by the -full
      archive is now used. This allows to simplify the build process
      greatly and avoids having to import all the new dependencies on new opam
      releases. This is also what a majority of BSD, Linux and macOS distributions
      out-there do.
    - The opam-solver library is no longer installed. This could not be used
      on its own anyway because the OCaml build process would require other
      modules that are not installed whose checksum wouldn't match if they
      were built separately.
    - Dummy and unused META, dune-package and opam files generated by dune
      are no longer installed
    - CHANGES, LICENSE and README.md are no longer installed

---
+++ ocaml-opam/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	ocaml-opam
+
+.if !defined(OCAML_OPAM_BUILDLINK3_MK)
+OCAML_OPAM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocaml-opam+=	ocaml-opam>=2.5.0
+BUILDLINK_ABI_DEPENDS.ocaml-opam+=	ocaml-opam>=2.5.2
+BUILDLINK_PKGSRCDIR.ocaml-opam?=	../../misc/ocaml-opam
+
+.endif	# OCAML_OPAM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocaml-opam
