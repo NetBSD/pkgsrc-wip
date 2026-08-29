e641ca4207fc469fc417f383e7729f85d611b6bb
moihack <moihack@noreply.localhost>
2026-08-29 15:27:00 +0200

    efl: sync with upstream patch
    This was upstreamed as part of pull request:
    https://git.enlightenment.org/enlightenment/efl/pulls/130

--- efl/patches/patch-src_lib_eina_eina__file__posix.c
+++ efl/patches/patch-src_lib_eina_eina__file__posix.c
@@ -1,27 +1,21 @@
 $NetBSD$
 
-Add Dirent / getdents wrapper.
+Add getdents wrapper. Use NetBSD dirent structure.
+Merged upstream: https://git.enlightenment.org/enlightenment/efl/pulls/130
 
---- src/lib/eina/eina_file_posix.c.orig	2023-12-23 15:56:36.000000000 +0000
+--- src/lib/eina/eina_file_posix.c.orig	2025-03-17 15:43:43.000000000 +0000
 +++ src/lib/eina/eina_file_posix.c
-@@ -1299,6 +1299,16 @@ typedef struct
+@@ -1285,6 +1285,9 @@ typedef struct
     unsigned char  ____pad[4];
     char           d_name[4096];
  } Dirent;
 +#elif defined(__NetBSD__)
 +# define do_getdents(fd, buf, size) getdents(fd, buf, size)
-+typedef struct
-+{
-+   ino_t          d_fileno;
-+   uint16_t       d_reclen;
-+   uint16_t       d_namlen;
-+   uint8_t        d_type;
-+   char           d_name[4096];
-+} Dirent;
++typedef struct dirent Dirent;
  #elif defined(__linux__)
  # define do_getdents(fd, buf, size) syscall(SYS_getdents64, fd, buf, size)
  // getdents64 added un glibc 2.30 ... so use raw syscall - will work
-@@ -1322,7 +1332,7 @@ eina_file_close_from(int fd, int *except
+@@ -1308,7 +1311,7 @@ eina_file_close_from(int fd, int *except
  #else
  #ifdef HAVE_DIRENT_H
  //# if 0
