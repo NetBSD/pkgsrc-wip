d85161a9b9f21a137775d14ec6361fcb7122d8b5
Pierre Pronchery <khorben@defora.org>
2026-09-01 21:12:29 +0200

    forgejo-runner: implement a log file
    Tested on NetBSD/amd64, macOS/amd64.

--- forgejo-runner/files/forgejo-runner.sh
+++ forgejo-runner/files/forgejo-runner.sh
@@ -15,13 +15,24 @@ name="forgejo_runner"
 rcvar=${name}
 required_files="@PKG_SYSCONFDIR@/config.yaml"
 command="@PREFIX@/bin/forgejo-runner"
-command_args="--config @PKG_SYSCONFDIR@/config.yaml daemon > /dev/null &"
+command_args="--config @PKG_SYSCONFDIR@/config.yaml daemon >> @VARBASE@/log/forgejo-runner.log 2>&1 &"
+start_precmd="forgejo_runner_start_precmd"
 
 forgejo_runner_env="HOME=@FORGEJO_RUNNER_USER_HOME@"
 forgejo_runner_env="${forgejo_runner_env} PATH=@PREFIX@/bin:$PATH"
 
 forgejo_runner_user="@FORGEJO_RUNNER_USER@"
 forgejo_runner_group="@FORGEJO_RUNNER_GROUP@"
+
+forgejo_runner_start_precmd()
+{
+	logfile="@VARBASE@/log/forgejo-runner.log"
+
+	if [ ! -f "$logfile" ]; then
+		touch "$logfile"
+		chown "$forgejo_runner_user:$forgejo_runner_group" "$logfile" || true
+	fi
+}
 
 cd /
 
